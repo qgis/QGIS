1c032077efd97584bbec60d51c72ce33ee900122
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-07-26 16:48:31 +0000

    added GNU statement
    git-svn-id: http://svn.osgeo.org/qgis/trunk@44 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgspoint.cpp
+++ src/qgspoint.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          qgspoint.cpp -  description
+                             -------------------
+    begin                : Sat Jun 22 2002
+    copyright            : (C) 2002 by Gary E.Sherman
+    email                : sherman@mrcc.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "qgspoint.h"
 QgsPoint::QgsPoint(){
 }
