b3140c675557520de58a4a224d8b88af8fa27220
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-07-27 17:57:31 +0000

    initial import into new directory
    git-svn-id: http://svn.osgeo.org/qgis/trunk@49 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgsmarkersymbol.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+                          qgsmarkersymbol.h  -  description
+                             -------------------
+    begin                : Sat Jun 22 2002
+    copyright            : (C) 2002 by Gary E.Sherman
+    email                : sherman@mrcc.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSMARKERSYMBOL_H
+#define QGSMARKERSYMBOL_H
+
+class QString;
+
+/*! \class QgsMarkerSymbol
+ * \brief Symbol for displaying markers (points)
+ */
+class QgsMarkerSymbol : public QgsSymbol{
+ public:
+    //! Constructor
+    QgsMarkerSymbol();
+    //! Destructor
+    ~QgsMarkerSymbol();
+          /*! Comparison operator
+      @return True if symbols are equal
+    */
+    bool operator==(const QgsMarkerSymbol &r1);
+    /*! Assignment operator
+     * @param r1 QgsMarkerSymbol to assign from
+     */
+    QgsMarkerSymbol & operator=(const QgsMarkerSymbol &r1);
+ private:
+  
+};
+#endif // QGSMARKERSYMBOL_H
