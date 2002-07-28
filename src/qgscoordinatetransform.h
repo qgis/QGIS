cf84a0c6e7aa2353617a640fb9da2074f7b9639b
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-07-28 15:53:27 +0000

    mouse tracking enabled
    git-svn-id: http://svn.osgeo.org/qgis/trunk@68 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgscoordinatetransform.h
+++ src/qgscoordinatetransform.h
@@ -17,25 +17,65 @@
 #ifndef QGSCOORDINATETRANSFORM_H
 #define QGSCOORDINATETRANSFORM_H
 class QgsPoint;
-
+class QPoint;
+/* \class QgsCoordinateTransform
+* \brief Class for doing transforms between map coordinates and device coordinates.
+*
+* This class can convert device coordinates to map coordinates and vice versa.
+*/
 class QgsCoordinateTransform{
  public:
     QgsCoordinateTransform(double mupp=0, double ymax = 0, double ymin=0,
 			   double xmin = 0);
     ~QgsCoordinateTransform();
+    /*! Transform the point from map (world) coordinates to device coordinates
+    *@ param p Point to transform
+    * @return QgsPoint in device coordinates
+    */
     QgsPoint transform(QgsPoint p);
+    /*! Transform the point specified by x,y from map (world) coordinates to device coordinates
+    *@ param x x cordinate o point to transform
+	*@param y y coordinate of point to transform
+    * @return QgsPoint in device coordinates
+    */
     QgsPoint transform(double x, double y);
+       /*! Tranform device coordinates to map (world)  coordinates
+    * @param x x coordinate of point to be converted to map cooordinates
+    * @param y y coordinate of point to be converted to map cooordinates
+    * @return QgsPoint in map coordinates
+    */
+    QgsPoint toMapCoordinates(int x, int y);
+     /*! Tranform device coordinates to map (world)  coordinates
+    * @param p Point to be converted to map cooordinates
+    * @return QgsPoint in map coorndiates
+    */
+    QgsPoint toMapCoordinates(QPoint p);
+    QgsPoint toMapPoint(int x, int y);
+    /*! Set map units per pixel
+    * @param mupp Map units per pixel
+    */
     void setMapUnitsPerPixel(double mupp);
+    //! Set maximum y value
     void setYmax(double ymax);
+    //! Set minimum y value
     void setYmin(double ymin);
+    //! set minimum x value
     void setXmin(double xmin);
+    /*! Set parameters for use in tranfsorming coordinates
+    * @param mupp Map units per pixel
+    * @param xmin Minimum x value
+    * @param ymin Minimum y value
+    * @param ymax Maximum y value
+    */
     void setParameters(double mupp, double xmin, double ymin, double ymax);
+    //! String representation of the parameters used in the transform
     QString showParameters();
  private:
     double mapUnitsPerPixel;
     double yMax;
     double yMin;
     double xMin;
+    double xMax;
 
 };
 #endif // QGSCOORDINATETRANSFORM_H
