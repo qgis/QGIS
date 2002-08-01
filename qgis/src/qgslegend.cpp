0089fb83ad75fde4a6457ffa0ce9f3ceea0b8262
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-08-01 05:27:15 +0000

    legend support class
    git-svn-id: http://svn.osgeo.org/qgis/trunk@78 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/qgslegend.cpp
@@ -0,0 +1,24 @@
+/***************************************************************************
+                          qgslegend.cpp  -  description
+                             -------------------
+    begin                : Sun Jul 28 2002
+    copyright            : (C) 2002 by Gary E.Sherman
+    email                : sherman at mrcc dot com
+               Romans 3:23=>Romans 6:23=>Romans 10:9,10=>Romans 12
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgslegend.h"
+
+QgsLegend::QgsLegend(QWidget *parent, const char *name) :QScrollView(parent,name){
+}
+QgsLegend::~QgsLegend(){
+}
