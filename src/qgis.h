2ed7edfda1b129650de61fc2feda95bba861f64e
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-08-21 05:28:26 +0000

    version number change
    git-svn-id: http://svn.osgeo.org/qgis/trunk@111 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgis.h
+++ src/qgis.h
@@ -17,6 +17,6 @@ enum WKBTYPE{
 	WKBMultiLineString,
 	WKBMultiPolygon
     };	
-static const char *qgisVersion = "0.0.5-alpha";
+static const char *qgisVersion = "0.0.5-alpha build 20020820";
 }
 #endif
