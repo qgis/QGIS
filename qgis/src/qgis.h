5c00a9cf0989d37d2a9f92d50f43659dbf2b1fde
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-10-11 05:10:44 +0000

    mod to description
    git-svn-id: http://svn.osgeo.org/qgis/trunk@127 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/src/qgis.h
+++ qgis/src/qgis.h
@@ -3,7 +3,10 @@
 /*!  \mainpage Quantum GIS
 *
 *  \section about  About QGis
-* blah blah blah
+* QGis aims to be an easy to use desktop GIS tool. Initial focus is on viewing spatial
+* and tabular data from common data stores, including Shapefiles and PostGIS.
+*
+* This API documentation provides information about all classes that make up QGis.
 *
 */
 
