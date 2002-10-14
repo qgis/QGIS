09be7250fec54194e47bf256f5902147583e4ba7
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-10-14 15:43:42 +0000

    stub classes for plugin support
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@138 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/qgispluginguielement.h
@@ -0,0 +1,17 @@
+/*! \class QgisPluginGuiElement
+* \brief Base class for a GUI element (menu, toolbar, etc) of a plugin
+*
+* QgsPluginGuiElement provides information about a GUI element that
+* will be added to the QGis interface when the plugin is loaded
+*/
+class QgisPluginGuiElement {
+public:
+//! Constructor
+	QgisPluginGuiElement();
+	//! Type of element (see ELEMENTS enum in qgisplugin.h)
+	virtual QGIS_GUI_TYPE type();
+	//! destructor
+	virtual ~QgisPluginGuiElement();
+private:
+	
+};
