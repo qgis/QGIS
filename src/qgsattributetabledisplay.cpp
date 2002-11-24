5cc852de2b8cc0743a40ae5aa2ee18712493de6a
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2002-11-24 00:03:23 +0000

    attribute display dialog
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@148 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/qgsattributetabledisplay.cpp
@@ -0,0 +1,30 @@
+/***************************************************************************
+                          QgsAttributeTableDisplay.cpp  -  description
+                             -------------------
+    begin                : Sat Nov 23 2002
+    copyright            : (C) 2002 by Gary E.Sherman
+    email                : sherman at mrcc dot com
+       Romans 3:23=>Romans 6:23=>Romans 5:8=>Romans 10:9,10=>Romans 12
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsattributetabledisplay.h"
+
+QgsAttributeTableDisplay::QgsAttributeTableDisplay(){
+}
+QgsAttributeTableDisplay::~QgsAttributeTableDisplay(){
+}
+QgsAttributeTable * QgsAttributeTableDisplay::table(){
+  return tblAttributes;
+}
+void QgsAttributeTableDisplay::setTitle(QString title){
+	setCaption(title);
+}
