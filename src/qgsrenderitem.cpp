7d95dd42b17bcd8a03b9aed2dae68baef24a069e
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2003-01-07 06:05:36 +0000

    renderer classes
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@170 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/qgsrenderitem.cpp
@@ -0,0 +1,35 @@
+/***************************************************************************
+                         qgsrenderer.cpp  -  description
+                             -------------------
+    begin                : Sat Jan 4 2003
+    copyright            : (C) 2003 by Gary E.Sherman
+    email                : sherman @ mrcc.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qstring.h>
+#include "qgssymbol.h"
+
+#include "qgsrenderitem.h"
+
+QgsRenderItem::QgsRenderItem(){
+};
+
+QgsRenderItem::QgsRenderItem(QgsSymbol symbol, QString _value, QString _label) :
+sym(symbol), value(_value), label(_label){
+
+}
+QgsSymbol *QgsRenderItem::getSymbol(){
+	return &sym;
+}
+void QgsRenderItem::setSymbol(QgsSymbol s){
+	sym = s;
+}
