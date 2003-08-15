8198aaa1f14aedd51f6e48c4353f8687bd9cde09
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2003-08-15 02:41:56 +0000

    maplayer plugin test
    git-svn-id: http://svn.osgeo.org/qgis/trunk@239 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/maplayer/maplayertest.h
@@ -0,0 +1,27 @@
+#ifndef QGSMAPLAYERTESTPLUGIN_H
+#define QGSMAPLAYERTESTPLUGIN_H
+#include <qmainwindow.h>
+#include <qmenubar.h>
+#include "../../src/qgsmaplayerinterface.h"
+#include "../../src/qgscoordinatetransform.h"
+class MapLayerTest : public QgsMapLayerInterface{
+Q_OBJECT
+public:
+MapLayerTest();
+void setQgisMainWindow(QMainWindow *app);
+void setCoordinateTransform(QgsCoordinateTransform *xform);
+int getInt();
+void initGui();
+public slots:
+void open();
+void unload();
+void draw();
+private:
+	QMainWindow *qgisApp;
+	QMenuBar *menu;
+	int menuId;
+	QgsCoordinateTransform *coordTransform;
+};
+
+
+#endif //QGSMAPLAYERTESTPLUGIN_H
