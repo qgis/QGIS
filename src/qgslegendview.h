e6bc2dd3e95f3419c5722f5069ddd3dd5423a59f
stevehalasz <stevehalasz@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2003-10-02 10:36:59 +0000

    QListView subclass to provide drag-n-drop in the legend
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@291 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/qgslegendview.h
@@ -0,0 +1,42 @@
+//
+//
+// C++ Interface: $MODULE$
+//
+// Description: Subclassed QListView that handles drag-n-drop for changing layer order
+//
+//
+// Author: Steve Halasz <stevehalasz at users.sourceforge.net>, (C) 2003
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include <qlistview.h>
+
+class QgsLegendView : public QListView
+{
+	Q_OBJECT
+	
+public:
+	QgsLegendView( QWidget *parent=0, const char *name=0 );
+	
+protected:
+	// override these to provide drag-n-drop
+	void contentsDragEnterEvent( QDragEnterEvent *e );
+	void contentsDropEvent( QDropEvent *e );
+	void contentsMouseMoveEvent( QMouseEvent *e );
+	void contentsMousePressEvent( QMouseEvent *e );
+	void contentsMouseReleaseEvent( QMouseEvent *e );
+	
+signals:
+	// broadcast that the stacking order has changed
+	// so the map canvas can be redrawn
+	void zOrderChanged( QgsLegendView *lv );
+	
+private:
+	// location of mouse press
+	QPoint presspos;
+	// keep track of if the mouse is pressed or not
+	bool mousePressed;
+	// keep track of the Item being dragged
+	QListViewItem *movingItem;
+};
