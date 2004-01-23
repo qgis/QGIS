704e1406750ff8d9b31bd1b7ae6ef019538a2f6e
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-01-23 07:37:32 +0000

    more progress towards a buffer function
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@565 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/geoprocessing/qgsdlgpgbuffer.cpp
+++ plugins/geoprocessing/qgsdlgpgbuffer.cpp
@@ -22,6 +22,32 @@ QString QgsDlgPgBuffer::bufferLayerName(){
 bool QgsDlgPgBuffer::addLayerToMap(){
   return chkAddToMap->isChecked();
 }
+QString QgsDlgPgBuffer::geometryColumn(){
+  return txtGeometryColumn->text();
+}
+QString QgsDlgPgBuffer::srid(){
+  return txtSrid->text();
+}
+QString QgsDlgPgBuffer::objectIdColumn(){
+  return cmbFields->currentText();
+}
+QString QgsDlgPgBuffer::schema(){
+  return cmbSchema->currentText();
+}
 void QgsDlgPgBuffer::addFieldItem(QString field){
   cmbFields->insertItem(field);
 }
+void QgsDlgPgBuffer::addSchema(QString schema){
+  cmbSchema->insertItem(schema);
+}
+void QgsDlgPgBuffer::setSrid(QString srid){
+  txtSrid->setText(srid);
+}
+void QgsDlgPgBuffer::setBufferLayerName(QString name){
+  txtBufferedLayerName->setText(name);
+}
+void QgsDlgPgBuffer::setGeometryColumn(QString name){
+  txtGeometryColumn->setText(name);
+}
+
+
