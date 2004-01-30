4cf974d6bc808c540cc0a86df91f946bf0c07828
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-01-30 23:43:47 +0000

    Restored help viewer after accidental overwrite
    git-svn-id: http://svn.osgeo.org/qgis/trunk@647 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgshelpviewer.h
+++ src/qgshelpviewer.h
@@ -1,11 +1,21 @@
-/****************************************************************************
-** Form interface generated from reading ui file 'qgshelpviewer.ui'
-**
-** Created: Fre Jan 30 07:57:36 2004
-**      by: The User Interface Compiler ($Id$)
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
+/***************************************************************************
+                          qgshelpviewer.h 
+ Simple help browser
+                             -------------------
+    begin                : 2004-01-28
+    copyright            : (C) 2004 by Gary E.Sherman
+    email                : sherman at mrcc.com
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ /* $Id$ */
 
 #ifndef QGSHELPVIEWER_H
 #define QGSHELPVIEWER_H
@@ -16,28 +26,19 @@
 class QVBoxLayout;
 class QHBoxLayout;
 class QGridLayout;
-class QSpacerItem;
 class QPushButton;
 class QTextBrowser;
-
-class QgsHelpViewer : public QDialog
+class QString;
+#include "qgshelpviewerbase.h"
+class QgsHelpViewer : public QgsHelpViewerBase
 {
     Q_OBJECT
 
 public:
     QgsHelpViewer( QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
     ~QgsHelpViewer();
+    void showContent(QString path, QString doc);
 
-    QPushButton* buttonCancel;
-    QTextBrowser* textBrowser;
-
-protected:
-    QGridLayout* QgsHelpViewerLayout;
-    QHBoxLayout* Layout1;
-    QSpacerItem* Horizontal_Spacing2;
-
-protected slots:
-    virtual void languageChange();
 
 };
 
