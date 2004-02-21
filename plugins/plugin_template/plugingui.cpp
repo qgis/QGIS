ff7ec71bd288659d805663af5bb3cb4ee6b4141d
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-02-21 22:28:45 +0000

    This is the initial commit for the qgis plugin template. The template consists of:
    class Plugin (a subclass of QgisPlugin)
    class PluginGuiBase (supplied as a simple qdialog ui)
    class PluginGui (the subclass of PluginGuiBase)
    
    The only thing the user would need to change in the makefile is the name of the .so
    so you just copy the plugin template dir to a new dir under plugins, change the .so name in the makefile add the plugin dir the configure.in and the plugin dir makefile.
    
    I will be adding a bash script file soon that automates the above process. This template development is in progress.
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk@881 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/plugin_template/plugingui.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tim Sutton                                      *
+ *   tim@linfiniti.com                                                     *
+ *                                                                         *
+ *   This is a plugin generated from the QGIS plugin template              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#include "openmodellergui.h"
+
+//qt includes
+
+//standard includes
+
+PluginGui::PluginGui() : PluginGuiBase()
+{
+  
+}
+
+PluginGui::PluginGui( QWidget* parent , const char* name , bool modal , WFlags fl  )
+: PluginGuiBase( parent, name, modal, fl )
+{
+   
+}  
+PluginGui::~PluginGui()
+{
+}
+
+void PluginGui::pbnOK_clicked()
+{
+  //
+  // If you have a produced a raster layer using your plugin, you can ask qgis to 
+  // add it to the view using:
+  // emit drawRasterLayer(QString("layername"));
+  //
+} 
