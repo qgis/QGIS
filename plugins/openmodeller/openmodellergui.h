0dba04bc5fb8c672da2388fdace93c2716a689ab
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-02-26 06:05:02 +0000

    The openmodeller plugin files and gui
    git-svn-id: http://svn.osgeo.org/qgis/trunk@943 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/openmodeller/openmodellergui.h
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tim Sutton                                      *
+ *   tim@linfiniti.com                                                     *
+ *                                                                         *
+ *   Gyps - Species Distribution Modelling Toolkit                         *
+ *   This toolkit provides data transformation and visualisation           *
+ *   tools for use in species distribution modelling tools such as GARP,   *
+ *   CSM, Bioclim etc.                                                     *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#ifndef OPENMODELLERGUI_H
+#define OPENMODELLERGUI_H
+
+#include <openmodellerguibase.h>
+
+/**
+@author Tim Sutton
+*/
+class OpenModellerGui : public OpenModellerGuiBase
+{
+Q_OBJECT
+public:
+    OpenModellerGui();
+    OpenModellerGui( QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
+    ~OpenModellerGui();
+  void OpenModellerGui::parseAndRun(QString theParametersFileNameQString);
+  void makeConfigFile();
+  
+  //
+  // The following methods reimplement methods by the same name of the parent class
+  //    
+    
+    void cboModelAlgorithm_activated(  const QString & );
+  /** This method overrides the virtual OpenModellerGuiBase method (slot) of the same name. */
+  void formSelected(const QString &thePageNameQString);
+  void leLocalitiesFileName_textChanged( const QString &theFileNameQString );
+  void pbnSelectOutputFile_clicked();
+  void pbnRemoveParameter_clicked();
+  void pbnAddParameter_clicked();
+  void pbnRemoveLayerFile_clicked();
+  void pbnSelectLayerFile_clicked();
+  void pbnSelectLocalitiesFile_clicked();
+  void leLocalitiesFileName_returnPressed();
+  void accept();
+
+private:
+    QString modelNameQString;
+    QString localitiesFileNameQString;
+    QString coordinateSystemQString;
+    QString taxonNameQString;
+    QStringList layerNamesQStringList;
+    QString maskNameQString;
+    QStringList extraParametersQStringList;
+    QString outputFileNameQString;
+    bool useTheseSettingsAgainFlag;
+signals:
+   void drawRasterLayer(QString);
+};
+
+#endif
---
+++ plugins/openmodeller/openmodellerguibase.ui.h
@@ -0,0 +1,76 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename functions or slots use
+** Qt Designer which will update this file, preserving your code. Create an
+** init() function in place of a constructor, and a destroy() function in
+** place of a destructor.
+*****************************************************************************/
+
+
+void OpenModellerGuiBase::init()
+{
+   //hide some controls that have not yet been implemented
+  cbxDefaultToLastChoices->hide();
+  textLabel3->hide();
+  progressBar1->hide();
+}
+
+void OpenModellerGuiBase::pbRun_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::pbnSelectOutputFile_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::leOutputFileName_textChanged( const QString & )
+{
+
+}
+
+
+void OpenModellerGuiBase::pbnRemoveParameter_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::pbnAddParameter_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::pbnRemoveLayerFile_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::pbnSelectLayerFile_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::pbnSelectLocalitiesFile_clicked()
+{
+
+}
+
+
+void OpenModellerGuiBase::leLocalitiesFileName_returnPressed()
+{
+
+}
+
+
+void OpenModellerGuiBase::formSelected(const QString & )
+{
+
+}
