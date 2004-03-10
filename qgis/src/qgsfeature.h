5292710f0f4fdd21df891f00d6c36ac82f09c8b8
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-03-10 23:23:06 +0000

    Changes to support upcoming data provider api changes related to plugging memory leaks
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1010 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/src/qgsfeature.h
+++ qgis/src/qgsfeature.h
@@ -1,9 +1,9 @@
 /***************************************************************************
-    qgsfeature.h - Spatial Feature Class
-     --------------------------------------
-    Date                 : 09-Sep-2003
-    Copyright            : (C) 2003 by Gary E.Sherman
-    email                : sherman at mrcc.com
+  qgsfeature.h - Spatial Feature Class
+  --------------------------------------
+Date                 : 09-Sep-2003
+Copyright            : (C) 2003 by Gary E.Sherman
+email                : sherman at mrcc.com
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -25,51 +25,69 @@ class QString;
 
 /** \class QgsFeature - Feature attribute class.
  * Encapsulates a single feature including id and field/value.
-  *@author Gary E.Sherman
-  */
+ *@author Gary E.Sherman
+ */
 
 class QgsFeature {
 
-public:
-	//! Constructor
-	QgsFeature();
-	QgsFeature(int id);
-	//! Destructor
-	~QgsFeature();
-	/**
-	* Get the feature id for this feature
-	* @return Feature id
-	*/
-	int featureId();
-	/**
-	* Get the attributes for this feature.
-	* @return A std::map containing the field name/value mapping
-	*/
-	const std::vector<QgsFeatureAttribute>& attributeMap();
-  /** 
-  * Add an attribute to the map
-  */
-  void addAttribute(QString field, QString value);
-	/**
-	* Get the fields for this feature
-	* @return A std::map containing field position (index) and field name
-	*/
-	const std::map<int, QString>& fields();
-	
-	unsigned char * getGeometry();
-	char * wellKnownText(); 
-	void setGeometry(unsigned char *);
+  public:
+    //! Constructor
+    QgsFeature();
+    QgsFeature(int id);
+    //! Destructor
+    ~QgsFeature();
+    /**
+     * Get the feature id for this feature
+     * @return Feature id
+     */
+    int featureId();
+    /**
+     * Set the feature id for this feature
+     * @param id Feature id
+     */
+     void setFeatureId(int id);
+    /**
+     * Get the attributes for this feature.
+     * @return A std::map containing the field name/value mapping
+     */
+    const std::vector<QgsFeatureAttribute>& attributeMap();
+    /** 
+     * Add an attribute to the map
+     */
+    void addAttribute(QString field, QString value);
+    /**
+     * Get the fields for this feature
+     * @return A std::map containing field position (index) and field name
+     */
+    const std::map<int, QString>& fields();
+    /**
+     * Return the validity of this feature. This is normally set by
+     * the provider to indicate some problem that makes the feature
+     * invalid or to indicate a null feature.
+     */
+    bool isValid();
+    /** 
+     * Set the validity of the feature.
+     */
+    void setValid(bool validity);
+
+    unsigned char * getGeometry();
+    char * wellKnownText(); 
+    void setGeometry(unsigned char *);
     void setWellKnownText(char *);
-private:
-	//! feature id
-	int fId;
-	//! std::map containing field name/value pairs
-	std::vector<QgsFeatureAttribute> attributes;
-	//! std::map containing the field index and name
-	std::map<int, QString> fieldNames;
-	//! pointer to gemetry in WKB format
-	unsigned char *geometry;
-    char * wkt;
+  private:
+    //! feature id
+    int mFid;
+    //! std::map containing field name/value pairs
+    std::vector<QgsFeatureAttribute> attributes;
+    //! std::map containing the field index and name
+    std::map<int, QString> fieldNames;
+    //! pointer to gemetry in WKB format
+    unsigned char *geometry;
+    //! Well Known Text representation of the geometry
+    char * mWKT;
+    //! Flag to indicate if this feature is valid
+    bool mValid;
 };
 
 #endif
