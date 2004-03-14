f19cb4f8fd9ec0431e11bf15be4a688d0cfd74d5
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-03-14 15:43:25 +0000

    added a dialog for a possible single marker renderer
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@1038 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/qgssimadialog.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+                          qgssimadialog.h 
+ Single marker renderer dialog
+                             -------------------
+    begin                : March 2004
+    copyright            : (C) 2004 by Marco Hugentobler
+    email                : mhugent@geo.unizh.ch
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ /* $Id$ */
+
+#include "qgssimadialogbase.h"
+
+class QgsSiMaDialog: public QgsSiMaDialogBase
+{
+    Q_OBJECT
+ public:
+    QgsSiMaDialog();
+    ~QgsSiMaDialog();
+};
