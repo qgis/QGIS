7b5bab3e484fca0ae462e9a531083f0b77e71910
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-03-27 11:55:07 +0000

    added classes for a dialog to select markers
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1107 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/qgsmarkerdialog.cpp
@@ -0,0 +1,11 @@
+#include "qgsmarkerdialog.h"
+
+QgsMarkerDialog::QgsMarkerDialog(): QgsMarkerDialogBase()
+{
+
+}
+
+QgsMarkerDialog::~QgsMarkerDialog()
+{
+
+}
---
+++ qgis/src/qgsmarkerdialog.h
@@ -0,0 +1,13 @@
+#ifndef QGSMARKERDIALOG_H
+#define QGSMARKERDIALOG_H
+
+#include "qgsmarkerdialogbase.uic.h"
+
+class QgsMarkerDialog: public QgsMarkerDialogBase
+{
+ public:
+    QgsMarkerDialog();
+    virtual ~QgsMarkerDialog();
+};
+
+#endif
