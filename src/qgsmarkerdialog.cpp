13cc29b5d684b664c723c0d5d9b6753e03054d76
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-03-28 09:49:36 +0000

    dialog to choose marker symbols can now be used. Small problems related to keyboard focus are still subject of work
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1114 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsmarkerdialog.cpp
+++ src/qgsmarkerdialog.cpp
@@ -1,11 +1,118 @@
+/***************************************************************************
+                         qgsmarkerdialog.cpp  -  description
+                             -------------------
+    begin                : March 2004
+    copyright            : (C) 2004 by Marco Hugentobler
+    email                : mhugent@geo.unizh.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id */
+
 #include "qgsmarkerdialog.h"
+#include <qdir.h>
+#include <qfiledialog.h>
+#include <qiconview.h>
+#include <qlineedit.h>
+#include <qpicture.h>
+#include <qpushbutton.h>
+#include <qpainter.h>
 
-QgsMarkerDialog::QgsMarkerDialog(): QgsMarkerDialogBase()
+QgsMarkerDialog::QgsMarkerDialog(QString startdir): QgsMarkerDialogBase(), mCurrentDir(startdir)
 {
-
+    QObject::connect(mOkButton,SIGNAL(clicked()),this,SLOT(accept()));
+    QObject::connect(mCancelButton,SIGNAL(clicked()),this,SLOT(reject()));
+    QObject::connect(mBrowseDirectoriesButton,SIGNAL(clicked()),this,SLOT(changeDirectory()));
+    QObject::connect(mIconView,SIGNAL(currentChanged(QIconViewItem*)),this,SLOT(updateSelectedMarker()));
+    QObject::connect(mDirectoryEdit,SIGNAL(returnPressed()),this,SLOT(setCurrentDirFromText()));
+    mDirectoryEdit->setText(startdir);
+    visualizeMarkers(startdir);
 }
 
 QgsMarkerDialog::~QgsMarkerDialog()
 {
 
 }
+
+void QgsMarkerDialog::updateSelectedMarker()
+{
+    QIconViewItem* current=mIconView->currentItem();
+    if(current)
+    {
+	mSelectedMarker=current->text();
+    }
+    else
+    {
+	mSelectedMarker="";
+    }
+}
+
+QString QgsMarkerDialog::selectedMarker()
+{
+    return mCurrentDir+"/"+mSelectedMarker;
+}
+
+void QgsMarkerDialog::changeDirectory()
+{
+    QString newdir=QFileDialog::getExistingDirectory(mCurrentDir,this,"get existing directory","Choose a directory",TRUE);
+    if (!newdir.isEmpty())
+    {
+	mCurrentDir=newdir;
+	visualizeMarkers(mCurrentDir);
+	mDirectoryEdit->setText(mCurrentDir);
+    }
+}
+
+void QgsMarkerDialog::setCurrentDirFromText()
+{   
+    QString newdir=mDirectoryEdit->text();
+    qWarning("newdir: "+newdir);
+    QDir dir(newdir);
+    if(dir.exists())
+    {
+	mCurrentDir=newdir;
+	
+	visualizeMarkers(mCurrentDir);	
+    }
+    else
+    {
+	mDirectoryEdit->setText(mCurrentDir);
+    }
+}
+
+void QgsMarkerDialog::visualizeMarkers(QString directory)
+{
+    mIconView->clear();
+
+    QDir dir(directory);
+    QStringList files=dir.entryList("*.svg;*.SVG");
+    
+    for(QStringList::Iterator it = files.begin(); it != files.end(); ++it )
+    {
+	qWarning(*it);
+	
+	//render the SVG file to a pixmap and put it into mIconView
+	/*QPicture pic;
+	pic.load(mCurrentDir+(*it),"svg");
+	QIconViewItem* ivi=new QIconViewItem(mIconView,*it,pic);*/
+	
+	//use the QPixmap way, as the QPicture version does not seem to work properly
+	QPicture pic;
+	pic.load(mCurrentDir+"/"+(*it),"svg");
+	QPixmap pix;
+	pix.resize(pic.boundingRect().width(),pic.boundingRect().height());
+	pix.fill();
+	QPainter p(&pix);
+	p.drawPicture(0,0,pic);
+	QIconViewItem* ivi=new QIconViewItem(mIconView,*it,pix);
+	
+    }
+}
+
--- src/qgsmarkerdialog.h
+++ src/qgsmarkerdialog.h
@@ -1,13 +1,51 @@
+/***************************************************************************
+                         qgsmarkerdialog.h  -  description
+                             -------------------
+    begin                : March 2004
+    copyright            : (C) 2004 by Marco Hugentobler
+    email                : mhugent@geo.unizh.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id */
+
+
 #ifndef QGSMARKERDIALOG_H
 #define QGSMARKERDIALOG_H
 
 #include "qgsmarkerdialogbase.uic.h"
+#include <qdir.h>
 
 class QgsMarkerDialog: public QgsMarkerDialogBase
 {
+    Q_OBJECT
  public:
-    QgsMarkerDialog();
+    QgsMarkerDialog(QString startdir=QDir::homeDirPath());
     virtual ~QgsMarkerDialog();
+    /**Returns the path of the selected SVG marker*/
+    QString selectedMarker();
+    
+    public slots:
+    /**Brings up the file dialog and triggers visualizeMarkers*/
+    void changeDirectory();
+    /**Resets the current directory and the icons if a valid path is inserted into mDirectoryEdit*/
+    void setCurrentDirFromText();
+    /**Queries mIconView for selected marker symbols*/
+    void updateSelectedMarker();
+
+ protected:
+	/**Current Directory*/
+	QString mCurrentDir;
+	QString mSelectedMarker;
+	/**Renders the SVG pictures of directory to mIconView*/
+	void visualizeMarkers(QString directory);
 };
 
 #endif
