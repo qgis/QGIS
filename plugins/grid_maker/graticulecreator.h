c3f8a67b839517ee2fbada35a0bb695707c5ca58
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-04-05 20:53:17 +0000

    Renamed from waypointtoshape
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1158 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/grid_maker/graticulecreator.h
@@ -0,0 +1,29 @@
+#ifndef _GRATICULECREATOR_H_
+#define _GRATICULECREATOR_H_
+
+#include "shapefile.h"
+#include "utils.h"
+
+//qt includes
+#include <qstring.h>
+#include <qfile.h>
+
+class GraticuleCreator
+{
+    public:
+        GraticuleCreator::GraticuleCreator(QString theOutputFileName, QString theInputFileName ); 
+        ~GraticuleCreator() {};
+        DBFHandle GraticuleCreator::createDbf (QString theDbfName ) ;
+        SHPHandle GraticuleCreator::createShapeFile(QString theFileName ); 
+        void writeDbfRecord (DBFHandle theDbfHandle, int theRecordIdInt, QString theLabel) ;
+        void writePoint(SHPHandle theShapeHandle, int theRecordInt, double theXFloat, double y ); 
+        static void WriteLine(SHPHandle theShapeHandle, 
+                int theRecordInt, 
+                int theCoordinateCountInt, 
+                double * theXArrayDouble, 
+                double * theYArrayDouble ); 
+        void generatePoints (QString theInputFileName, DBFHandle theDbfHandle, SHPHandle theShapeHandle) ;
+
+    private:
+};
+#endif
