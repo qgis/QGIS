c879661c5e4e7464648938c88801612a916e131d
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-04-22 05:25:10 +0000

    Forced name of .pro file to qgis_ts.pro so no assumptions are made about the current source directory. This name was chosen to minimize potential clash with an existing qgis.pro file, although such a file is not used in building QGIS.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@1232 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- create_qm_files.sh
+++ create_qm_files.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+# create_qm_files.sh,v 1.2 2004/04/22 05:25:10 gsherman Exp
 # Create the .qm files from the translation files
 # 1. create a clean Qt .pro file for the project
 # 2. run lrelease using the .pro file from step 1
 # 3. remove the .pro
 echo Creating qmake project file
-qmake -project
+# force the output name to be qgis_ts.pro
+qmake -project -o qgis_ts.pro
 echo Creating qm files
-lrelease -verbose qgis.pro
+lrelease -verbose qgis_ts.pro
 echo Removing qmake project file
-rm qgis.pro
+rm qgis_ts.pro
