c05fb48c53d6480559d3bb3b35c34058046aac25
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-05-05 09:30:32 +0000

    Initial commit - no implementaiton yet.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1311 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/copyright_label/plugin.cpp
@@ -0,0 +1,175 @@
+/***************************************************************************
+  plugin.cpp 
+  Import tool for various worldmap analysis output files
+Functions:
+
+-------------------
+begin                : Jan 21, 2004
+copyright            : (C) 2004 by Tim Sutton
+email                : tim@linfiniti.com
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/*  $Id$ */
+
+// includes
+
+#include <qgisapp.h>
+#include <qgsmaplayer.h>
+#include <qgsrasterlayer.h>
+#include "plugin.h"
+
+
+#include <qtoolbar.h>
+#include <qmenubar.h>
+#include <qmessagebox.h>
+#include <qpopupmenu.h>
+#include <qlineedit.h>
+#include <qaction.h>
+#include <qapplication.h>
+#include <qcursor.h>
+
+//non qt includes
+#include <iostream>
+
+//the gui subclass
+#include "plugingui.h"
+
+// xpm for creating the toolbar icon
+#include "icon.xpm"
+// 
+static const char * const ident_ = "$Id$";
+
+static const char * const name_ = "CopyrightLabel";
+static const char * const description_ = "This plugin overlays copyright information on the rendered map.";
+static const char * const version_ = "Version 0.1";
+static const QgisPlugin::PLUGINTYPE type_ = QgisPlugin::UI;
+
+
+/**
+ * Constructor for the plugin. The plugin is passed a pointer to the main app
+ * and an interface object that provides access to exposed functions in QGIS.
+ * @param qgis Pointer to the QGIS main window
+ * @param _qI Pointer to the QGIS interface object
+ */
+Plugin::Plugin(QgisApp * theQGisApp, QgisIface * theQgisInterFace):
+          qgisMainWindowPointer(theQGisApp), 
+          qGisInterface(theQgisInterFace),
+          QgisPlugin(name_,description_,version_,type_)
+{
+}
+
+Plugin::~Plugin()
+{
+
+}
+
+/*
+ * Initialize the GUI interface for the plugin 
+ */
+void Plugin::initGui()
+{
+  // add a menu with 2 items
+  QPopupMenu *pluginMenu = new QPopupMenu(qgisMainWindowPointer);
+
+  pluginMenu->insertItem(QIconSet(icon),"&CopyrightLabel", this, SLOT(run()));
+
+  menuBarPointer = ((QMainWindow *) qgisMainWindowPointer)->menuBar();
+
+  menuIdInt = qGisInterface->addMenu("&Decorations", pluginMenu);
+  // Create the action for tool
+  QAction *myQActionPointer = new QAction("Copyright Label", QIconSet(icon), "&Wmi",0, this, "run");
+  // Connect the action to the run
+  connect(myQActionPointer, SIGNAL(activated()), this, SLOT(run()));
+  // Add the toolbar
+  toolBarPointer = new QToolBar((QMainWindow *) qgisMainWindowPointer, "Decorations");
+  toolBarPointer->setLabel("Copyright Label");
+  // Add the zoom previous tool to the toolbar
+  myQActionPointer->addTo(toolBarPointer);
+
+
+}
+//method defined in interface
+void Plugin::help()
+{
+  //implement me!
+}
+
+// Slot called when the buffer menu item is activated
+void Plugin::run()
+{
+  PluginGui *myPluginGui=new PluginGui(qgisMainWindowPointer,"Copyright Label",true,0);
+  //listen for when the layer has been made so we can draw it
+  connect(myPluginGui, SIGNAL(drawRasterLayer(QString)), this, SLOT(drawRasterLayer(QString)));
+  connect(myPluginGui, SIGNAL(drawVectorLayer(QString,QString,QString)), this, SLOT(drawVectorLayer(QString,QString,QString)));
+  myPluginGui->show();
+}
+//!draw a raster layer in the qui - intended to respond to signal sent by diolog when it as finished creating
+//layer
+void Plugin::drawRasterLayer(QString theQString)
+{
+  qGisInterface->addRasterLayer(theQString);
+}
+//!draw a vector layer in the qui - intended to respond to signal sent by diolog when it as finished creating a layer
+////needs to be given vectorLayerPath, baseName, providerKey ("ogr" or "postgres");
+void Plugin::drawVectorLayer(QString thePathNameQString, QString theBaseNameQString, QString theProviderQString)
+{
+ qGisInterface->addVectorLayer( thePathNameQString, theBaseNameQString, theProviderQString);
+}
+
+// Unload the plugin by cleaning up the GUI
+void Plugin::unload()
+{
+  // remove the GUI
+  menuBarPointer->removeItem(menuIdInt);
+  delete toolBarPointer;
+}
+/** 
+ * Required extern functions needed  for every plugin 
+ * These functions can be called prior to creating an instance
+ * of the plugin class
+ */
+// Class factory to return a new instance of the plugin class
+extern "C" QgisPlugin * classFactory(QgisApp * theQGisAppPointer, QgisIface * theQgisInterfacePointer)
+{
+  return new Plugin(theQGisAppPointer, theQgisInterfacePointer);
+}
+
+// Return the name of the plugin - note that we do not user class members as
+// the class may not yet be insantiated when this method is called.
+extern "C" QString name()
+{
+    return name_;
+}
+
+// Return the description
+extern "C" QString description()
+{
+    return description_;
+}
+
+// Return the type (either UI or MapLayer plugin)
+extern "C" int type()
+{
+    return type_;
+}
+
+// Return the version number for the plugin
+extern "C" QString version()
+{
+  return version_;
+}
+
+// Delete ourself
+extern "C" void unload(QgisPlugin * thePluginPointer)
+{
+  delete thePluginPointer;
+}
---
+++ plugins/copyright_label/plugingui.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tim Sutton                                      *
+ *   tim@linfiniti.com                                                     *
+ *                                                                         *
+ *   This is a plugin generated from the QGIS plugin template              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#include "plugingui.h"
+
+//qt includes
+
+//standard includes
+
+PluginGui::PluginGui() : PluginGuiBase()
+{
+  
+}
+
+PluginGui::PluginGui( QWidget* parent , const char* name , bool modal , WFlags fl  )
+: PluginGuiBase( parent, name, modal, fl )
+{
+   
+}  
+PluginGui::~PluginGui()
+{
+}
+
+void PluginGui::pbnOK_clicked()
+{
+  //
+  // If you have a produced a raster layer using your plugin, you can ask qgis to 
+  // add it to the view using:
+  // emit drawRasterLayer(QString("layername"));
+  // or for a vector layer
+  // emit drawVectorLayer(QString("pathname"),QString("layername"),QString("provider name (either ogr or postgres"));
+  //
+  //close the dialog
+  done(1);
+} 
+void PluginGui::pbnCancel_clicked()
+{
+ close(1);
+}
