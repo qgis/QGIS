c50175f4d48d4ceb2e3822b08531275fe3001fd0
sbr00pwb <sbr00pwb@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-05-06 15:33:53 +0000

    Revised the dialog to allow the setting of font face and colour to be set
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@1330 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/copyright_label/pluginguibase.ui.h
+++ plugins/copyright_label/pluginguibase.ui.h
@@ -6,8 +6,10 @@
 ** init() function in place of a constructor, and a destroy() function in
 ** place of a destructor.
 *****************************************************************************/
-
-
+#include <qcolordialog.h> 
+#include <qcolor.h>
+#include <qfontdialog.h>
+#include <qfont.h>
 
 void PluginGuiBase::pbnOK_clicked()
 {
@@ -18,4 +20,28 @@ void PluginGuiBase::pbnOK_clicked()
 void PluginGuiBase::pbnCancel_clicked()
 {
 
+}
+
+
+void PluginGuiBase::btnFontColour_clicked()
+{
+    QColor myQColor = QColorDialog::getColor();
+    txtCopyrightText->setPaletteForegroundColor(myQColor);
+}
+
+
+void PluginGuiBase::btnFontFace_clicked()
+{
+    bool ok;
+    QFont myFont = QFontDialog::getFont(
+                    &ok, QFont( "Helvetica", 10 ), this );
+    if ( ok ) {
+        // font is set to the font the user selected
+	txtCopyrightText->setCurrentFont(myFont);
+    } else {
+        // the user canceled the dialog; font is set to the initial
+        // value, in this case Helvetica [Cronyx], 10
+    }
+    
+    
 }
