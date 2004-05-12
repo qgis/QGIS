d95ce2036819a45faffbc89f8ed6e8ab51c2b9c9
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-05-12 18:03:45 +0000

    Overloaded qwidget paint and resize events to draw north arrow so we dont have strange enpty looking dialogs.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1381 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/plugins/north_arrow/plugingui.cpp
+++ qgis/plugins/north_arrow/plugingui.cpp
@@ -22,12 +22,13 @@
 
 PluginGui::PluginGui() : PluginGuiBase()
 {
-  
+
   //temporary hack until this is implemented
   tabNorthArrowOptions->removePage( tabIcon );
+  rotatePixmap(0);
 }
 
-PluginGui::PluginGui( QWidget* parent , const char* name , bool modal , WFlags fl  )
+    PluginGui::PluginGui( QWidget* parent , const char* name , bool modal , WFlags fl  )
 : PluginGuiBase( parent, name, modal, fl )
 {
   //temporary hack until this is implemented
@@ -52,19 +53,20 @@ void PluginGui::pbnOK_clicked()
 } 
 void PluginGui::pbnCancel_clicked()
 {
- close(1);
+  close(1);
 }
 
 void PluginGui::setRotation(int theInt)
 {
   rotatePixmap(theInt);
+  sliderRotation->setValue(theInt);
 }
-  
+
 
 //overides function byt the same name created in .ui
 void PluginGui::spinSize_valueChanged( int theInt)
 {
-  
+
 }
 
 //overides function byt the same name created in .ui
@@ -75,69 +77,86 @@ void PluginGui::sliderRotation_valueChanged( int theInt)
 
 void PluginGui::rotatePixmap(int theRotationInt)
 {
-    QPixmap myQPixmap;
+  QPixmap myQPixmap;
 
 
-    QString myFileNameQString = QString(PKGDATAPATH) + QString("/images/north_arrows/default.png");
-    //std::cout << "Trying to load " << myFileNameQString << std::cout;
-    if (myQPixmap.load(myFileNameQString))
-    {
-        QPixmap  myPainterPixmap(myQPixmap.height(),myQPixmap.width());
-        myPainterPixmap.fill();
-        QPainter myQPainter;
-        myQPainter.begin(&myPainterPixmap);	
+  QString myFileNameQString = QString(PKGDATAPATH) + QString("/images/north_arrows/default.png");
+  //std::cout << "Trying to load " << myFileNameQString << std::cout;
+  if (myQPixmap.load(myFileNameQString))
+  {
+    QPixmap  myPainterPixmap(myQPixmap.height(),myQPixmap.width());
+    myPainterPixmap.fill();
+    QPainter myQPainter;
+    myQPainter.begin(&myPainterPixmap);	
 
-        double centerXDouble = myQPixmap.width()/2;
-	double centerYDouble = myQPixmap.height()/2;
-        //save the current canvas rotation
-        myQPainter.save();
-	//myQPainter.translate( (int)centerXDouble, (int)centerYDouble );
-	
-        //rotate the canvas
-        myQPainter.rotate( theRotationInt );
-	//work out how to shift the image so that it appears in the center of the canvas
-	//(x cos a + y sin a - x, -x sin a + y cos a - y)
-	const double PI = 3.14159265358979323846;
-	double myRadiansDouble = (PI/180) * theRotationInt;
-        int xShift = static_cast<int>((
-	              (centerXDouble * cos(myRadiansDouble)) + 
-	              (centerYDouble * sin(myRadiansDouble))
-		     ) - centerXDouble);
-        int yShift = static_cast<int>((
-	              (-centerXDouble * sin(myRadiansDouble)) + 
-	              (centerYDouble * cos(myRadiansDouble))
-		     ) - centerYDouble);	
-		     	     
-		     
+    double centerXDouble = myQPixmap.width()/2;
+    double centerYDouble = myQPixmap.height()/2;
+    //save the current canvas rotation
+    myQPainter.save();
+    //myQPainter.translate( (int)centerXDouble, (int)centerYDouble );
 
-	//draw the pixmap in the proper position
-      	myQPainter.drawPixmap(xShift,yShift,myQPixmap);	
+    //rotate the canvas
+    myQPainter.rotate( theRotationInt );
+    //work out how to shift the image so that it appears in the center of the canvas
+    //(x cos a + y sin a - x, -x sin a + y cos a - y)
+    const double PI = 3.14159265358979323846;
+    double myRadiansDouble = (PI/180) * theRotationInt;
+    int xShift = static_cast<int>((
+                (centerXDouble * cos(myRadiansDouble)) + 
+                (centerYDouble * sin(myRadiansDouble))
+                ) - centerXDouble);
+    int yShift = static_cast<int>((
+                (-centerXDouble * sin(myRadiansDouble)) + 
+                (centerYDouble * cos(myRadiansDouble))
+                ) - centerYDouble);	
 
 
-        //unrotate the canvase again
-        myQPainter.restore();
-        myQPainter.end();
 
-        //determine the center of the canvas given that we will bitblt form the origin of the narrow
-        int myCenterXInt = static_cast<int>((pixmapLabel->width()-myQPixmap.width())/2);
-        int myCenterYInt = static_cast<int>((pixmapLabel->height()-myQPixmap.height())/2);
-	bitBlt ( pixmapLabel, myCenterXInt,myCenterYInt, &myPainterPixmap, 0, 0, -1 , -1, Qt::CopyROP, false);
-	 
-	
-        //pixmapLabel1->setPixmap(myPainterPixmap);            
-    }
-    else
-    {
-        QPixmap  myPainterPixmap(200,200);
-        myPainterPixmap.fill();
-        QPainter myQPainter;
-        myQPainter.begin(&myPainterPixmap);	
-        QFont myQFont("time", 18, QFont::Bold);
-        myQPainter.setFont(myQFont);
-        myQPainter.setPen(Qt::black);
-        myQPainter.drawText(10, 20, QString("Pixmap Not Found"));
-        myQPainter.end();
-        pixmapLabel->setPixmap(myPainterPixmap);    
-    }
+    //draw the pixmap in the proper position
+    myQPainter.drawPixmap(xShift,yShift,myQPixmap);	
 
+
+    //unrotate the canvase again
+    myQPainter.restore();
+    myQPainter.end();
+
+    //determine the center of the canvas given that we will bitblt form the origin of the narrow
+    int myCenterXInt = static_cast<int>((pixmapLabel->width()-myQPixmap.width())/2);
+    int myCenterYInt = static_cast<int>((pixmapLabel->height()-myQPixmap.height())/2);
+    bitBlt ( pixmapLabel, myCenterXInt,myCenterYInt, &myPainterPixmap, 0, 0, -1 , -1, Qt::CopyROP, false);
+
+
+    //pixmapLabel1->setPixmap(myPainterPixmap);            
+  }
+  else
+  {
+    QPixmap  myPainterPixmap(200,200);
+    myPainterPixmap.fill();
+    QPainter myQPainter;
+    myQPainter.begin(&myPainterPixmap);	
+    QFont myQFont("time", 18, QFont::Bold);
+    myQPainter.setFont(myQFont);
+    myQPainter.setPen(Qt::black);
+    myQPainter.drawText(10, 20, QString("Pixmap Not Found"));
+    myQPainter.end();
+    pixmapLabel->setPixmap(myPainterPixmap);    
+  }
+
+}
+
+// Called when the widget needs to be updated.
+//
+
+void PluginGui::paintEvent( QPaintEvent * thePaintEvent)
+{
+  rotatePixmap(sliderRotation->value());  
+}
+
+//
+// Called when the widget has been resized.
+// 
+
+void PluginGui::resizeEvent( QResizeEvent * theResizeEvent)
+{
+  rotatePixmap(sliderRotation->value());  
 }
