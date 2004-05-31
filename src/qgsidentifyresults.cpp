d8b59a280f63ebcfc981d005bfc30d52a924e468
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-05-31 19:40:38 +0000

    Changed QgsIdentifyResultsBase to inherit from QWidget instead of QDialog so window position can be saved/restored from user settings each time.
    Changed qgis.h int version number to 300 (should have been done at release)
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1493 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsidentifyresults.cpp
+++ src/qgsidentifyresults.cpp
@@ -1,10 +1,10 @@
 /***************************************************************************
-                          qgsidentifyresults.cpp  -  description
-                             -------------------
-    begin                : Fri Oct 25 2002
-    copyright            : (C) 2002 by Gary E.Sherman
-    email                : sherman at mrcc dot com
-       Romans 3:23=>Romans 6:23=>Romans 5:8=>Romans 10:9,10=>Romans 12
+  qgsidentifyresults.cpp  -  description
+  -------------------
+begin                : Fri Oct 25 2002
+copyright            : (C) 2002 by Gary E.Sherman
+email                : sherman at mrcc dot com
+Romans 3:23=>Romans 6:23=>Romans 5:8=>Romans 10:9,10=>Romans 12
  ***************************************************************************/
 
 /***************************************************************************
@@ -15,8 +15,14 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
- /* $Id$ */
+/* $Id$ */
+#include <iostream>
 #include <qlistview.h>
+#include <qsettings.h>
+#include <qpoint.h>
+#include <qsize.h>
+#include <qevent.h>
+
 #include "qgsidentifyresults.h"
 
 QgsIdentifyResults::QgsIdentifyResults()
@@ -25,8 +31,48 @@ QgsIdentifyResults::QgsIdentifyResults()
 
 QgsIdentifyResults::~QgsIdentifyResults()
 {
-}
 
+}
+// Slot called when user clicks the Close button
+// (saves the current window size/position)
+void QgsIdentifyResults::close()
+{
+  saveWindowLocation();
+  hide();
+}
+// Save the current window size/position before closing 
+// from window menu or X in titlebar
+void QgsIdentifyResults::closeEvent(QCloseEvent *e)
+{
+  saveWindowLocation();
+  e->accept();
+}
+// Restore last window position/size and show the window
+void QgsIdentifyResults::restorePosition()
+{
+
+  QSettings settings;
+  int ww = settings.readNumEntry("/qgis/Windows/Identify/w", 281);
+  int wh = settings.readNumEntry("/qgis/Windows/Identify/h", 316);
+  int wx = settings.readNumEntry("/qgis/Windows/Identify/x", 100);
+  int wy = settings.readNumEntry("/qgis/Windows/Identify/y", 100);
+  //std::cerr << "Setting geometry: " << wx << ", " << wy << ", " << ww << ", " << wh << std::endl;
+  resize(ww,wh);
+  move(wx,wy);
+  QgsIdentifyResultsBase::show();
+  //std::cerr << "Current geometry: " << x() << ", " << y() << ", " << width() << ", " << height() << std::endl; 
+}
+// Save the current window location (store in ~/.qt/qgisrc)
+void QgsIdentifyResults::saveWindowLocation()
+{
+  QSettings settings;
+  QPoint p = this->pos();
+  QSize s = this->size();
+  settings.writeEntry("/qgis/Windows/Identify/x", p.x());
+  settings.writeEntry("/qgis/Windows/Identify/y", p.y());
+  settings.writeEntry("/qgis/Windows/Identify/w", s.width());
+  settings.writeEntry("/qgis/Windows/Identify/h", s.height());
+} 
 /** add an attribute and its value to the list */
 void QgsIdentifyResults::addAttribute(QListViewItem * fnode, QString field, QString value)
 {
