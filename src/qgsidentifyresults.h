eaefa5e0f585f3877d6a8fb104099a6cc2c021fd
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-05-31 19:40:38 +0000

    Changed QgsIdentifyResultsBase to inherit from QWidget instead of QDialog so window position can be saved/restored from user settings each time.
    Changed qgis.h int version number to 300 (should have been done at release)
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@1493 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsidentifyresults.h
+++ src/qgsidentifyresults.h
@@ -1,10 +1,10 @@
 /***************************************************************************
-                          qgsidentifyresults.h  -  description
-                             -------------------
-    begin                : Fri Oct 25 2002
-    copyright            : (C) 2002 by Gary E.Sherman
-    email                : sherman at mrcc dot com
-        Romans 3:23=>Romans 6:23=>Romans 5:8=>Romans 10:9,10=>Romans 12
+  qgsidentifyresults.h  -  description
+  -------------------
+begin                : Fri Oct 25 2002
+copyright            : (C) 2002 by Gary E.Sherman
+email                : sherman at mrcc dot com
+Romans 3:23=>Romans 6:23=>Romans 5:8=>Romans 10:9,10=>Romans 12
  ***************************************************************************/
 
 /***************************************************************************
@@ -22,20 +22,27 @@
 #include "qgsidentifyresultsbase.uic.h"
 
 /**
-  *@author Gary E.Sherman
-  */
+ *@author Gary E.Sherman
+ */
 
 class QgsIdentifyResults:public QgsIdentifyResultsBase
 {
+  Q_OBJECT;
   public:
-	QgsIdentifyResults();
-	~QgsIdentifyResults();
+  QgsIdentifyResults();
+  ~QgsIdentifyResults();
   /** Add an attribute to the feature display node */
-	void addAttribute(QListViewItem *parent, QString field, QString value);
+  void addAttribute(QListViewItem *parent, QString field, QString value);
   /** Add a feature node to the feature display */
-	QListViewItem * addNode(QString label);
+  QListViewItem * addNode(QString label);
   /** Set the title for the identify results dialog */
-	void setTitle(QString title);
+  void setTitle(QString title);
+  void saveWindowLocation();
+  void restorePosition();  
+  void close();
+  void closeEvent(QCloseEvent *e);
+  //void accept();
+  //void reject();
 };
 
 #endif
