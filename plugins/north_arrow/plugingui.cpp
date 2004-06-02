690e496439cedfbd44a8dd3f164d588be5832894
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-06-02 13:05:07 +0000

    Hide the dialog before updating props.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1510 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/north_arrow/plugingui.cpp
+++ plugins/north_arrow/plugingui.cpp
@@ -42,13 +42,8 @@ PluginGui::~PluginGui()
 
 void PluginGui::pbnOK_clicked()
 {
-  //
-  // If you have a produced a raster layer using your plugin, you can ask qgis to 
-  // add it to the view using:
-  // emit drawRasterLayer(QString("layername"));
-  // or for a vector layer
-  // emit drawVectorLayer(QString("pathname"),QString("layername"),QString("provider name (either ogr or postgres"));
-  //
+  // Hide the dialog
+  hide();
   //close the dialog
   emit rotationChanged(sliderRotation->value());
   emit changePlacement(cboPlacement->currentText());
