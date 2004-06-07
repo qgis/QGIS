e466391d9c716e5602602077c5c98c4b60105dcc
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-06-07 08:43:53 +0000

    Implemented map overview using maplayers rather than snapshots of a raster layer.
    Implemented QgsMapLayerRegistry  - s singleton object that keeps track of loaded layers. When a layer is added an entry is made in the registry. When a layer is removed, the registry emits a layerWillBeRemoved signal that is connected to any mapvcanvas, legend etc that may be using the layer. The objects using the layer can then remove any reference they make to the layer - after which the registry deletes the layer object.
    
    This fixes a problem with adding an overview map which caused qgis to crash when a layer was removed because it was trying to delete the same pointer twice.
    
    Added a better implementation of the overview map below map legend.
    
    Refactoring in qgis app - all private members now adhere to qgis naming conventions (prefixed with m).
    
    
    Import Note *** ONLY THE MAPLAYER REGISTRY SHOULD DELETE LAYERS NOW ***
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1530 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/qgsmaplayerregistry.cpp
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *  QgsMapLayerRegistry.cpp  -  Singleton class for tracking mMapLayers.
+ *                         -------------------
+ * begin                : Sun June 02 2004
+ * copyright            : (C) 2004 by Tim Sutton
+ * email                : tim@linfiniti.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+#include <iostream>
+#include "qgsmaplayerregistry.h"
+//
+// Static calls to enforce singleton behaviour
+//
+QgsMapLayerRegistry *QgsMapLayerRegistry::mInstance = 0;
+QgsMapLayerRegistry *QgsMapLayerRegistry::instance()
+{
+  if (mInstance == 0)
+  {
+    mInstance = new QgsMapLayerRegistry();
+  }
+  return mInstance;
+}
+
+//
+// Main class begins now...
+//
+
+QgsMapLayerRegistry::QgsMapLayerRegistry(QObject *parent, const char *name) : QObject(parent,name) 
+{
+#ifdef QGISDEBUG
+  std::cout << "---------------> qgs map layer registry created " << std::endl;
+#endif
+  // constructor does nothing
+}
+
+
+QgsMapLayer * QgsMapLayerRegistry::mapLayer(QString theLayerId)
+{
+  QgsMapLayer * myMapLayer = mMapLayers[theLayerId];
+  if (myMapLayer)
+  {
+    return myMapLayer;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void QgsMapLayerRegistry::addMapLayer( QgsMapLayer * theMapLayer)
+{
+#ifdef QGISDEBUG
+  std::cout << "qgsmaplayerregistry::addMaplayer - " << theMapLayer->name() << std::endl;
+#endif
+  mMapLayers[theMapLayer->getLayerID()] = theMapLayer;
+  emit layerWasAdded(theMapLayer);
+}
+
+void QgsMapLayerRegistry::removeMapLayer(QString theLayerId)
+{
+#ifdef QGISDEBUG
+  std::cout << "qgsmaplayerregistry::removemaplayer - emitting signal to notify all users of this layer to release it." << std::endl;
+#endif
+  emit layerWillBeRemoved(theLayerId); 
+#ifdef QGISDEBUG
+  std::cout << "qgsmaplayerregistry::removemaplayer - deleting map layer." << std::endl;
+#endif
+  delete mMapLayers[theLayerId]; 
+#ifdef QGISDEBUG
+  std::cout << "qgsmaplayerregistry::removemaplayer - unregistering map layer." << std::endl;
+#endif
+  mMapLayers.erase(theLayerId);
+#ifdef QGISDEBUG
+  std::cout << "qgsmaplayerregistry::removemaplayer - operation complete." << std::endl;
+#endif
+}
+
+void QgsMapLayerRegistry::removeAllMapLayers()
+{
+  //emit layerWillBeRemoved(theLayerId); 
+  //delete mMapLayers[theLayerId]; 
+  //mMapLayers.erase(theLayerId);
+}
---
+++ src/qgsmaplayerregistry.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+                          qgsmaplayerregistry.h    
+           Singleton class for keeping track of loaded layers
+                             -------------------
+    begin                : Sun June 04 2004
+    copyright            : (C) 2004 by Tim Sutton
+    email                : tim@linfiniti.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ /* $Id$ */
+ 
+#ifndef QGSMAPLAYERREGISTRY_H
+#define QGSMAPLAYERREGISTRY_H
+#include <map>
+#include <qobject.h>
+#include "qgsmaplayer.h"
+class QString;
+class QStringList;
+/**
+* \class QgsMapLayerRegistry
+* \brief This class tracks map layers that are currently loaded an provides
+* a means to fetch a pointer to a map layer and delete it
+*/
+class QgsMapLayerRegistry : public QObject
+{
+Q_OBJECT;
+public:
+ //! Returns the instance pointer, creating the object on the first call
+ static QgsMapLayerRegistry * instance();
+ //! Retrieve a list of all the registered layer ids
+ QStringList mapLayerList();
+ //! Retrieve a pointer to a loaded plugin by id
+ QgsMapLayer * mapLayer(QString theLayerId);
+ //! Add a layer to the map of loaded layers
+ void addMapLayer(QgsMapLayer * theMapLayer);
+ //! Remove a layer from qgis - any canvases using that layer will need to remove it
+ void removeMapLayer(QString theLayerId);
+ //! Remove all registered layers 
+ void removeAllMapLayers();
+signals:
+ void layerWillBeRemoved(QString theLayerId);
+ void layerWasAdded(QgsMapLayer * theMapLayer);
+protected:
+//! protected constructor
+ QgsMapLayerRegistry( QObject * parent = 0, const char * name = 0 );
+private:
+ static QgsMapLayerRegistry* mInstance;
+ std::map<QString,QgsMapLayer*> mMapLayers;
+};
+#endif //QgsMapLayerRegistry_H
+
