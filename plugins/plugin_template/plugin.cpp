c722ce45fe4d9b1f0634b32c20ceee0a689a7101
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-06-17 12:27:44 +0000

    Added more comments, renamed some vars, added win32 support out of the box (gsherman can you test this?).
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1610 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/plugin_template/plugin.cpp
+++ plugins/plugin_template/plugin.cpp
@@ -1,13 +1,13 @@
 /***************************************************************************
-  plugin.cpp 
-  Import tool for various worldmap analysis output files
-Functions:
-
--------------------
-begin                : Jan 21, 2004
-copyright            : (C) 2004 by Tim Sutton
-email                : tim@linfiniti.com
-
+ *  File Name:               plugin.cpp 
+ * 
+ *  [plugindescription]
+ *
+ *--------------------------------------------------------------------------
+ *    begin                : Jan 21, 2004
+ *    copyright            : (C) 2004 by Tim Sutton
+ *    email                : tim@linfiniti.com
+ *
  ***************************************************************************/
 
 /***************************************************************************
@@ -20,13 +20,36 @@ email                : tim@linfiniti.com
  ***************************************************************************/
 /*  $Id$ */
 
-// includes
+/***************************************************************************
+ *   QGIS Programming conventions:
+ *   
+ *   mVariableName - a class level member variable
+ *   sVariableName - a static class level member variable
+ *   variableName() - accessor for a class member (no 'get' in front of name)
+ *   setVariableName() - mutator for a class member (prefix with 'set')
+ *
+ *   Additional useful conventions:
+ *
+ *   theVariableName - a method parameter (prefix with 'the')
+ *   myVariableName - a locally declared variable within a method ('my' prefix)
+ * 
+ *   DO: Use mixed case variable names - myVariableName
+ *   DON'T: separate variable names using underscores: my_variable_name (NO!)
+ *
+ * **************************************************************************/
+
+//
+// Required qgis includes
+// 
 
 #include <qgisapp.h>
 #include <qgsmaplayer.h>
 #include <qgsrasterlayer.h>
 #include "plugin.h"
 
+//
+// Required QT includes
+//
 
 #include <qtoolbar.h>
 #include <qmenubar.h>
@@ -37,33 +60,49 @@ email                : tim@linfiniti.com
 #include <qapplication.h>
 #include <qcursor.h>
 
+//
 //non qt includes
+//
 #include <iostream>
 
+//
 //the gui subclass
+//
 #include "plugingui.h"
 
+//
 // xpm for creating the toolbar icon
-#include "icon.xpm"
 // 
-static const char * const ident_ = "$Id$";
+#include "icon.xpm"
 
-static const char * const name_ = "[pluginname]";
-static const char * const description_ = "[plugindescription]";
-static const char * const version_ = "Version 0.1";
-static const QgisPlugin::PLUGINTYPE type_ = QgisPlugin::UI;
+#ifdef WIN32
+#define QGISEXTERN extern "C" __declspec( dllexport )
+#else
+#define QGISEXTERN extern "C"
+#endif
 
+static const char * const sIdent = "$Id$";
+static const char * const sName = "[pluginname]";
+static const char * const sDescription = "[plugindescription]";
+static const char * const sVersion = "Version 0.1";
+static const QgisPlugin::PLUGINTYPE sType = QgisPlugin::UI;
+
+//////////////////////////////////////////////////////////////////////
+//
+// THE FOLLOWING METHODS ARE MANDATORY FOR ALL PLUGINS
+//
+//////////////////////////////////////////////////////////////////////
 
 /**
  * Constructor for the plugin. The plugin is passed a pointer to the main app
  * and an interface object that provides access to exposed functions in QGIS.
- * @param qgis Pointer to the QGIS main window
- * @param _qI Pointer to the QGIS interface object
+ * @param theQGisApp - Pointer to the QGIS main window
+ * @param theQGisInterface - Pointer to the QGIS interface object
  */
-Plugin::Plugin(QgisApp * theQGisApp, QgisIface * theQgisInterFace):
-          qgisMainWindowPointer(theQGisApp), 
-          qGisInterface(theQgisInterFace),
-          QgisPlugin(name_,description_,version_,type_)
+Plugin::Plugin(QgisApp * theQGisApp, QgisIface * theQgisInterface):
+                 mQGisApp(theQGisApp), 
+                 mQGisIface(theQgisInterface),
+                 QgisPlugin(sName,sDescription,sVersion,sType)
 {
 }
 
@@ -77,24 +116,19 @@ Plugin::~Plugin()
  */
 void Plugin::initGui()
 {
-  // add a menu with 2 items
-  QPopupMenu *pluginMenu = new QPopupMenu(qgisMainWindowPointer);
-
+  QPopupMenu *pluginMenu = new QPopupMenu(mQGisApp);
   pluginMenu->insertItem(QIconSet(icon),"&[menuitemname]", this, SLOT(run()));
-
-  menuBarPointer = ((QMainWindow *) qgisMainWindowPointer)->menuBar();
-
-  menuIdInt = qGisInterface->addMenu("&[menuname]", pluginMenu);
+  mMenuBarPointer = ((QMainWindow *) mQGisApp)->menuBar();
+  mMenuId = mQGisIface->addMenu("&[menuname]", pluginMenu);
   // Create the action for tool
-  QAction *myQActionPointer = new QAction("[menuitemname]", QIconSet(icon), "&Wmi",0, this, "run");
+  QAction *myQActionPointer = new QAction("[menuitemname]", QIconSet(icon), "&icon",0, this, "run");
   // Connect the action to the run
   connect(myQActionPointer, SIGNAL(activated()), this, SLOT(run()));
   // Add the toolbar
-  toolBarPointer = new QToolBar((QMainWindow *) qgisMainWindowPointer, "[menuname]");
-  toolBarPointer->setLabel("[menuitemname]");
+  mToolBarPointer = new QToolBar((QMainWindow *) mQGisApp, "[menuname]");
+  mToolBarPointer->setLabel("[menuitemname]");
   // Add the zoom previous tool to the toolbar
-  myQActionPointer->addTo(toolBarPointer);
-
+  myQActionPointer->addTo(mToolBarPointer);
 
 }
 //method defined in interface
@@ -106,32 +140,60 @@ void Plugin::help()
 // Slot called when the buffer menu item is activated
 void Plugin::run()
 {
-  PluginGui *myPluginGui=new PluginGui(qgisMainWindowPointer,"[menuitemname]",true,0);
+  PluginGui *myPluginGui=new PluginGui(mQGisApp,"[menuitemname]",true,0);
   //listen for when the layer has been made so we can draw it
   connect(myPluginGui, SIGNAL(drawRasterLayer(QString)), this, SLOT(drawRasterLayer(QString)));
   connect(myPluginGui, SIGNAL(drawVectorLayer(QString,QString,QString)), this, SLOT(drawVectorLayer(QString,QString,QString)));
   myPluginGui->show();
-}
-//!draw a raster layer in the qui - intended to respond to signal sent by diolog when it as finished creating
-//layer
-void Plugin::drawRasterLayer(QString theQString)
-{
-  qGisInterface->addRasterLayer(theQString);
-}
-//!draw a vector layer in the qui - intended to respond to signal sent by diolog when it as finished creating a layer
-////needs to be given vectorLayerPath, baseName, providerKey ("ogr" or "postgres");
-void Plugin::drawVectorLayer(QString thePathNameQString, QString theBaseNameQString, QString theProviderQString)
-{
- qGisInterface->addVectorLayer( thePathNameQString, theBaseNameQString, theProviderQString);
 }
 
 // Unload the plugin by cleaning up the GUI
 void Plugin::unload()
 {
   // remove the GUI
-  menuBarPointer->removeItem(menuIdInt);
-  delete toolBarPointer;
+  mMenuBarPointer->removeItem(mMenuId);
+  delete mToolBarPointer;
 }
+
+//////////////////////////////////////////////////////////////////////
+//
+//                  END OF MANDATORY PLUGIN METHODS
+//
+//////////////////////////////////////////////////////////////////////
+//
+// The following methods are provided to demonstrate how you can 
+// load a vector or raster layer into the main gui. Please delete
+// if you are not intending to use these. Note also that there are
+// ways in which layers can be loaded.
+//
+
+//!draw a raster layer in the qui - intended to respond to signal sent by diolog when it as finished creating
+//layer
+void Plugin::drawRasterLayer(QString theQString)
+{
+  mQGisIface->addRasterLayer(theQString);
+}
+
+//!draw a vector layer in the qui - intended to respond to signal sent by 
+// dialog when it as finished creating a layer. It needs to be given 
+// vectorLayerPath, baseName, providerKey ("ogr" or "postgres");
+void Plugin::drawVectorLayer(QString thePathNameQString, QString theBaseNameQString, QString theProviderQString)
+{
+  mQGisIface->addVectorLayer( thePathNameQString, theBaseNameQString, theProviderQString);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//  THE FOLLOWING CODE IS AUTOGENERATED BY THE PLUGIN BUILDER SCRIPT
+//    YOU WOULD NORMALLY NOT NEED TO MODIFY THIS, AND YOUR PLUGIN
+//      MAY NOT WORK PROPERLY IF YOU MODIFY THIS INCORRECTLY
+//
+//
+//////////////////////////////////////////////////////////////////////////
+
+
 /** 
  * Required extern functions needed  for every plugin 
  * These functions can be called prior to creating an instance
@@ -147,25 +209,25 @@ extern "C" QgisPlugin * classFactory(QgisApp * theQGisAppPointer, QgisIface * th
 // the class may not yet be insantiated when this method is called.
 extern "C" QString name()
 {
-    return name_;
+  return sName;
 }
 
 // Return the description
 extern "C" QString description()
 {
-    return description_;
+  return sDescription;
 }
 
 // Return the type (either UI or MapLayer plugin)
 extern "C" int type()
 {
-    return type_;
+  return sType;
 }
 
 // Return the version number for the plugin
 extern "C" QString version()
 {
-  return version_;
+  return sVersion;
 }
 
 // Delete ourself
