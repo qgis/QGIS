b72faa195338cf01d7c5441fae27e49e6b8c7111
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-06-19 05:42:57 +0000

    Hopefully last of win32 fixes needed for 0.4 release
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1628 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/plugin_template/plugin.cpp
+++ plugins/plugin_template/plugin.cpp
@@ -82,7 +82,7 @@
 #endif
 
 static const char * const sIdent = "$Id$";
-static const char * const sName = "[pluginname]";
+static const char * const sName = "[menuitemname]";
 static const char * const sDescription = "[plugindescription]";
 static const char * const sVersion = "Version 0.1";
 static const QgisPlugin::PLUGINTYPE sType = QgisPlugin::UI;
@@ -200,38 +200,79 @@ void Plugin::drawVectorLayer(QString thePathNameQString, QString theBaseNameQStr
  * of the plugin class
  */
 // Class factory to return a new instance of the plugin class
+#ifdef WIN32
+QGISEXTERN QgisPlugin * classFactory(QgisApp * theQGisAppPointer, QgisIface * theQgisInterfacePointer)
+{
+  return new Plugin(theQGisAppPointer, theQgisInterfacePointer);
+}
+#else
 extern "C" QgisPlugin * classFactory(QgisApp * theQGisAppPointer, QgisIface * theQgisInterfacePointer)
 {
   return new Plugin(theQGisAppPointer, theQgisInterfacePointer);
 }
-
+#endif
 // Return the name of the plugin - note that we do not user class members as
 // the class may not yet be insantiated when this method is called.
+#ifdef WIN32
+QGISEXTERN QString name()
+{
+  return sName;
+}
+#else
 extern "C" QString name()
 {
   return sName;
 }
+#endif
 
 // Return the description
+#ifdef WIN32
+QGISEXTERN QString description()
+{
+  return sDescription;
+}
+#else
 extern "C" QString description()
 {
   return sDescription;
 }
+#endif
 
 // Return the type (either UI or MapLayer plugin)
+#ifdef WIN32
+QGISEXTERN int type()
+{
+  return sType();
+}
+#else
 extern "C" int type()
 {
   return sType;
 }
+#endif
 
 // Return the version number for the plugin
+#ifdef WIN32
+QGISEXTERN QString version()
+{
+  return pluginVersion;
+}
+#else
 extern "C" QString version()
 {
   return sVersion;
 }
+#endif
 
 // Delete ourself
+#ifdef WIN32
+QGISEXTERN void unload(QgisPlugin * thePluginPointer)
+{
+  delete thePluginPointer;
+}
+#else
 extern "C" void unload(QgisPlugin * thePluginPointer)
 {
   delete thePluginPointer;
 }
+#endif
