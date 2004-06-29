d05d6d9f59121cb6e7e08caab202c28b90801c77
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-06-29 04:29:18 +0000

    Updated comments/documentation
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@1700 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsacetaterectangle.h
+++ src/qgsacetaterectangle.h
@@ -27,56 +27,56 @@ class QgsCoordinateTransform;
 #include "qgsacetateobject.h"
 
 /** \class QgsAcetateRectangle
-* \brief A rectangle drawn on the acetate layer of a map canvas
-*
-* An acetate object is a graphic or text object that is drawn on top of the map canvas 
-* after rendering of all map elements is completed. Acetate objects can be drawn in
-* device coordinates or map coordinates. Drawing in map coordinates requires passing
-* a QgsCoordinateTransform object to the draw function.
-*
-*/ 
+ * \brief A rectangle drawn on the acetate layer of a map canvas
+ *
+ * An acetate object is a graphic or text object that is drawn on top of the map canvas 
+ * after rendering of all map elements is completed. Acetate objects can be drawn in
+ * device coordinates or map coordinates. Drawing in map coordinates requires passing
+ * a QgsCoordinateTransform object to the draw function.
+ *
+ */ 
 class QgsAcetateRectangle : public QgsAcetateObject {
-public:
-  /**
-   * Constructor. Constructs an object with the specified origin. If the object is
-   * spatially referenced, the origin should be in map coordinates.
-   */
-  QgsAcetateRectangle(QgsPoint &origin, const QgsRect &rectangle);
-	/** 
-	 * Destructor
-	 */
-	~QgsAcetateRectangle();
-  /**
-   * Draw the rectangle using the Qpainter and applying a coordinate transform if
-   * specified.
-   * @param painter Painter to use for drawing
-   * @param cXf Coordinate transform to use in drawing map coordinate on the device. If 
-   * this parameter is not specified, coordinates are assumed to be device coordinates
-   * rather than map coordinates.
-   */
-  void  draw (QPainter * painter, QgsCoordinateTransform * cXf=0);
-   /** 
-    * Set the origin point
-    * @param value Point of origin
-    */
-  void setOrigin (QgsPoint value );
- /**
-  * Returns the point of origin
-  */
-  QgsPoint origin();
-  /**
-   * Returns the current rectangle
-   */
-  QgsRect rectangle();
-  /** Sets the rectangle
-   * @param rect The rectanlge
-   */
-  void setRectangle(QgsRect value);
-private:
-  //! Origin of the object in device or map coordinates 
-   QgsPoint mOrigin;
-   //! Rectangle
-   QgsRect mRectangle;
+  public:
+    /**
+     * Constructor. Constructs an object with the specified origin. If the object is
+     * spatially referenced, the origin should be in map coordinates.
+     */
+    QgsAcetateRectangle(QgsPoint &origin, const QgsRect &rectangle);
+    /** 
+     * Destructor
+     */
+    ~QgsAcetateRectangle();
+    /**
+     * Draw the rectangle using the Qpainter and applying a coordinate transform if
+     * specified.
+     * @param painter Painter to use for drawing
+     * @param cXf Coordinate transform to use in drawing map coordinate on the device. If 
+     * this parameter is not specified, coordinates are assumed to be device coordinates
+     * rather than map coordinates.
+     */
+    void  draw (QPainter * painter, QgsCoordinateTransform * cXf=0);
+    /** 
+     * Set the origin point
+     * @param value Point of origin
+     */
+    void setOrigin (QgsPoint value );
+    /**
+     * Returns the point of origin
+     */
+    QgsPoint origin();
+    /**
+     * Returns the current rectangle
+     */
+    QgsRect rectangle();
+    /** Sets the rectangle
+     * @param rect The rectanlge
+     */
+    void setRectangle(QgsRect value);
+  private:
+    //! Origin of the object in device or map coordinates 
+    QgsPoint mOrigin;
+    //! Rectangle
+    QgsRect mRectangle;
 };
 #endif //QGSACETATERECTANGLE_H
 
