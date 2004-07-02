836f1f96369be6ddb513afab499303e913ab870d
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-07-02 08:02:33 +0000

    Changes for 0.4 release - the 'Baby' is born
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1726 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/src/qgis.h
+++ qgis/src/qgis.h
@@ -19,9 +19,9 @@ namespace QGis
   // Version string 
   static const char *qgisVersion = VERSION;
   // Version number used for comparing versions using the "Check QGIS Version" function
-  static const int qgisVersionInt =300;
+  static const int qgisVersionInt =400;
   // Release name
-  static const char *qgisReleaseName = "Madison";
+  static const char *qgisReleaseName = "Baby";
   
   // Enumerations
   //
