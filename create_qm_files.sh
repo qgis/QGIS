5e3c3472930c538145d7f7b51458f164745200e0
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-07-14 18:16:00 +0000

    Changed output to qgis_qm.pro so as not to blast the windows qmake project file (qgis.pro)
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@1758 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- create_qm_files.sh
+++ create_qm_files.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-# create_qm_files.sh,v 1.3 2004/07/10 06:45:34 gsherman Exp
+# create_qm_files.sh,v 1.4 2004/07/14 18:16:00 gsherman Exp
 # Create the .qm files from the translation files
 # 1. create a clean Qt .pro file for the project
 # 2. run lrelease using the .pro file from step 1
 # 3. remove the .pro
+# Note the .pro file must NOT be named qgis.pro as this
+# name is reserved for the Windows qmake project file
+# create_qm_files.sh,v 1.4 2004/07/14 18:16:00 gsherman Exp
 echo Creating qmake project file
-# force the output name to be qgis_ts.pro
-$QTDIR/bin/qmake -project -o qgis_ts.pro
+# force the output name to be qgis_qm.pro
+$QTDIR/bin/qmake -project -o qgis_qm.pro
 echo Creating qm files
-$QTDIR/bin/lrelease -verbose qgis_ts.pro
+$QTDIR/bin/lrelease -verbose qgis_qm.pro
 echo Removing qmake project file
-rm qgis_ts.pro
+rm qgis_qm.pro
