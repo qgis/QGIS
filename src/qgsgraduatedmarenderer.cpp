eb95c7b06a528ce28a77f57d3b2c5a413308eac0
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-07-20 18:18:45 +0000

    enabled getNextFeatures(std::list<int>) for rendering of vector features. This saves time because only the features needed for classification have to be copied. The postgres-gurus are kindly invited to improve the inefficient implementation of getNextFeature(std::list<int>) in the postgresproviders (done by me) :-)
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1792 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsgraduatedmarenderer.cpp
+++ src/qgsgraduatedmarenderer.cpp
@@ -89,8 +89,8 @@ void QgsGraduatedMaRenderer::renderFeature(QPainter* p, QgsFeature* f,QPicture* 
 
     //first find out the value for the classification attribute
     std::vector < QgsFeatureAttribute > vec = f->attributeMap();
-    //double value = vec[0].fieldValue().toDouble();
-    double value = vec[mClassificationField].fieldValue().toDouble();
+    double value = vec[0].fieldValue().toDouble();
+    //double value = vec[mClassificationField].fieldValue().toDouble();
 
     std::list < QgsRangeRenderItem * >::iterator it;
     //first find the first render item which contains the feature
