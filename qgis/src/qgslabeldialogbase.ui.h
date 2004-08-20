977a1e436175cd8c9cb4d18de7b8d6a4d8261724
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-08-20 08:24:42 +0000

    New dialog for qgs labels coutesy of Pete! Its not wired in to the vector props gui yet.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@1912 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/qgslabeldialogbase.ui.h
@@ -0,0 +1,16 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename functions or slots use
+** Qt Designer which will update this file, preserving your code. Create an
+** init() function in place of a constructor, and a destroy() function in
+** place of a destructor.
+*****************************************************************************/
+#include <iostream>
+
+void QgsLabelDialogBase::init ()
+{
+    std::cout << "Label Dialog Base Created!" << std::endl;   
+}
+
+
