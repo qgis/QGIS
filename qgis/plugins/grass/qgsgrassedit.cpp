e523b4d7239e7cc61af009139f7dccf100d139e7
rabla <rabla@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-09-29 09:51:53 +0000

    isEditable conflict
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_5_candidate@2058 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/plugins/grass/qgsgrassedit.cpp
+++ qgis/plugins/grass/qgsgrassedit.cpp
@@ -105,7 +105,7 @@ QgsGrassEdit::QgsGrassEdit ( QgisApp *qgisApp, QgisIface *iface,
     //TODO dynamic_cast ?
     mProvider = (QgsGrassProvider *) vector->getDataProvider();
 
-    if ( !(mProvider->isEditable()) ) {
+    if ( !(mProvider->isGrassEditable()) ) {
 	QMessageBox::warning( 0, "Warning", "You are not owner of the mapset, "
 		                 "cannot open the vector for editing." );
 	return;
