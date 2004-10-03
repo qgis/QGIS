bd1f4bbee4ba62c8cc54e0ace83e35088eef2456
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-10-03 00:21:18 +0000

    added install_guide directory and files
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_5_candidate@2088 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/doc/install_guide/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate labels original text with physical files.
+
+
+$key = q/label_introduction/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.70)
+# labels from external_latex_labels array.
+
+
+1;
+
