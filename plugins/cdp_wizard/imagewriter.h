a9fb753965170a86bfd6b30e4cefb478713f7626
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-11-05 10:25:37 +0000

    Added class to write processed climate layer to an image.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2211 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/cdp_wizard/imagewriter.h
@@ -0,0 +1,45 @@
+#ifndef _IMAGEWRITER_H_
+#define _IMAGEWRITER_H_
+
+#include <qstring.h>
+#include "gdal_priv.h"
+/** This class will write a png image given an input gdal compatible datasource.
+ * The output file will be colour mapped using a pseudocolor renderer.
+ **/
+struct RasterBandStats {
+	QString bandName;
+	int bandNo;
+	double minValDouble;
+	double maxValDouble;
+	//the distance between min & max
+	double rangeDouble;
+	double meanDouble;
+	double sumSqrDevDouble; //used to calculate stddev
+	double stdDevDouble;
+	double sumDouble;
+	int elementCountInt;
+	double noDataDouble;
+};
+
+class ImageWriter
+{
+
+public:
+
+  /** Default constructor */
+  ImageWriter();
+  /** Destructor  */
+   ~ImageWriter();
+  /** Constructor taking the name of the file to open. */
+  void writeImage(QString theInputFileString, QString theOutputFileString);
+private:
+  /** Calculate image statistics */
+  void calculateStats(RasterBandStats * theRasterBandStats,GDALDataset * gdalDataset);
+  //
+  //   Private attributes
+  //
+
+};
+
+#endif
+
