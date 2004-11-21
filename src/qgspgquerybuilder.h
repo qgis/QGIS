7c967e3a19f238a751fe6248637dde8661d7bde8
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-11-21 03:53:02 +0000

    Updates to query builder ui
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@2302 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgspgquerybuilder.h
+++ src/qgspgquerybuilder.h
@@ -27,39 +27,66 @@ extern "C"
 #include "qgspgquerybuilderbase.uic.h"
 #endif
 class QgsField;
-class QgsPgQueryBuilder : public QgsPgQueryBuilderBase
-{ 
-  Q_OBJECT
-  public:
-    //! Default constructor
-    QgsPgQueryBuilder(QWidget *parent = 0, const char *name=0);
-    //! Constructor which also takes the table name and PG connection
-    // pointer
-    QgsPgQueryBuilder(QString tableName, PGconn *con, 
-        QWidget *parent = 0, const char *name=0);
-    ~QgsPgQueryBuilder();
-    void setConnection(PGconn *con);
-  private:
-    //! Populate the field list for the selected table
-    void populateFields();
-    //! Get sample distinct values for the selected field. The sample size
-    // is limited to an arbitrary value
-    void getSampleValues();
-    //! Get all distinct values for the field
-    void getAllValues();
-    //! Test the constructed sql statement to see if the database likes it
-    void testSql();
+/*!
+ * \class QgsPgQueryBuider
+ * \brief Query Builder for PostgreSQL layers.
+ *
+ * The query builder allows interactive creation of a SQL for limiting the
+ * features displayed in a database layer.  The fields in the table are
+ * displayed and sample values (or all values) can be viewed to aid in
+ * constructing the query. A test function returns the number of features that
+ * will be returned.
+ *
+ */
+class QgsPgQueryBuilder : public QgsPgQueryBuilderBase { Q_OBJECT public:
+  //! Default constructor - not very useful
+  QgsPgQueryBuilder(QWidget *parent = 0, const char *name=0);
 
-    // private members
-    
-    //! PostgreSQL conneciton
-   PGconn *mPgConnection;
-   //! Table name
-   QString mTableName;
-   //! Vector of QgsField objects
-   std::vector<QgsField> mFields;
-   //! Map that holds field information
-   std::map<QString, QgsField> mFieldMap;
+  /*! Constructor which also takes the table name and PG connection pointer
+  * @param tableName Name of the table being queried
+  * @param con PostgreSQL connection from the Add PostGIS Layer dialog
+  * @param parent Parent widget
+  * @param name Name of the widget
+  */
+  QgsPgQueryBuilder(QString tableName, PGconn *con, QWidget *parent = 0,
+      const char *name=0); ~QgsPgQueryBuilder(); void setConnection(PGconn
+        *con); 
+  private:
+  /*! 
+   * Populate the field list for the selected table
+   */ 
+  void populateFields();
+
+  /*! 
+   * Get sample distinct values for the selected field. The sample size is
+   * limited to an arbitrary value (currently set to 25). The fields
+   * are inserted into the field list box.
+   */
+  void getSampleValues();
+  
+  /*! 
+   * Get all distinct values for the field. Values are inserted
+   * into the value list box
+   */
+  void getAllValues();
+  
+  /*! Test the constructed sql statement to see if the database likes it.
+   * The number of rows that would be returned is displayed in a message box.
+   * The test uses a "select count(*) from ..." query to test the SQL 
+   * statement.
+   */
+  void testSql();
+
+  // private members
+
+  //! PostgreSQL connection object
+  PGconn *mPgConnection;
+  //! Table name
+  QString mTableName;
+  //! Vector of QgsField objects
+  std::vector<QgsField> mFields;
+  //! Map that holds field information, keyed by field name
+  std::map<QString, QgsField> mFieldMap;
 
 };
 #endif //QGSPGQUERYBUILDER_H
