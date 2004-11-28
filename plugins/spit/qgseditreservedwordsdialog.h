ccc435df049ddd6d2a535d7b9f2c3dc99247de23
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-11-28 05:46:10 +0000

    Dialog for editing column names prior to loading into postgresql
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@2358 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/spit/qgseditreservedwordsdialog.h
@@ -0,0 +1,20 @@
+#ifndef QGSEDITRESERVEDWORDSDIALOG_H
+#define QGSEDITRESERVEDWORDSDIALOG_H
+#include "qgseditreservedwordsbase.h"
+class QgsEditReservedWordsDialog : public QgsEditReservedWordsBase
+{
+  Q_OBJECT
+  public:
+    QgsEditReservedWordsDialog(QWidget *parent=0, const char *name=0);
+    ~QgsEditReservedWordsDialog();
+    void addColumn(QString column, bool isReserved, int index);
+    void setReservedWords(const QStringList &);
+    QStringList columnNames();
+  public slots:
+    void checkWord(QListViewItem *, int , const QString&);
+    void editWord(QListViewItem *);
+
+
+
+};
+#endif //QGSEDITRESERVEDWORDSDIALOG_H
