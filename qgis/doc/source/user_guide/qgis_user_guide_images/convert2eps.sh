ebd1ae7c9237426610f93fa7fb08a6711623d01f
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-12-20 03:44:41 +0000

    Final doc updates - again
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_6-candidate@2521 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/doc/source/user_guide/qgis_user_guide_images/convert2eps.sh
+++ qgis/doc/source/user_guide/qgis_user_guide_images/convert2eps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for MY_FILENAME in $(ls *.png); do 
-    FILENAMEBASE=`echo $MY_FILENAME |sed 's/\.dat//g'`
+    FILENAMEBASE=`echo $MY_FILENAME |sed 's/\.png//g'`
     convert $MY_FILENAME $FILENAMEBASE.eps  
 done
