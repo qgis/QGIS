7357157cbaebccd06e74e933c44f8f0d104605eb
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-12-30 02:59:30 +0000

    Added refactored qgsmaptopixel
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2537 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/qgsmaptopixel.cpp
@@ -0,0 +1,77 @@
+/***************************************************************************
+                qgsmaptopixel.cpp  -  description
+                             -------------------
+    begin                : Sat Jun 22 2002
+    copyright            : (C) 2002 by Gary E.Sherman
+    email                : sherman at mrcc.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ /* $Id$ */
+#include <qstring.h>
+#include <qtextstream.h>
+#include <qpoint.h>
+#include "qgsmaptopixel.h"
+
+QgsPoint QgsMapToPixel::toMapPoint(int x, int y)
+{
+  double mx = x * mapUnitsPerPixel + xMin;
+  double my = -1 * ((y - yMax) * mapUnitsPerPixel - yMin);
+  return QgsPoint(mx, my);
+}
+
+QgsPoint QgsMapToPixel::toMapCoordinates(QPoint p)
+{
+  QgsPoint mapPt = toMapPoint(p.x(), p.y());
+  return QgsPoint(mapPt);
+}
+
+QgsPoint QgsMapToPixel::toMapCoordinates(int x, int y)
+{
+  return toMapPoint(x, y);
+}
+
+void QgsMapToPixel::setMapUnitsPerPixel(double mupp)
+{
+  mapUnitsPerPixel = mupp;
+}
+
+void QgsMapToPixel::setYmax(double ymax)
+{
+  yMax = ymax;
+}
+
+void QgsMapToPixel::setYmin(double ymin)
+{
+  yMin = ymin;
+}
+
+void QgsMapToPixel::setXmin(double xmin)
+{
+  xMin = xmin;
+}
+
+void QgsMapToPixel::setParameters(double mupp, double xmin, double ymin, double ymax)
+{
+  mapUnitsPerPixel = mupp;
+  xMin = xmin;
+  yMin = ymin;
+  yMax = ymax;
+
+}
+
+QString QgsMapToPixel::showParameters()
+{
+  QString rep;
+  QTextOStream(&rep) << "Map units/pixel: " << mapUnitsPerPixel
+    << " X minimum: " << xMin << " Y minimum: " << yMin << " Y maximum: " << yMax;
+  return rep;
+
+}
