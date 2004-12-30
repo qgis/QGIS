c1683917ad08e5fe54b5687f5441927a168fe065
larsl <larsl@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-12-30 21:18:01 +0000

    ** Implemented getProjectionWKT() in QgsGPXProvider
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2546 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- providers/gpx/qgsgpxprovider.cpp
+++ providers/gpx/qgsgpxprovider.cpp
@@ -120,6 +120,22 @@ QgsGPXProvider::~QgsGPXProvider() {
 }
 
 
+QString QgsGPXProvider::getProjectionWKT() {
+  return 
+    "GEOGCS[\"WGS 84\", "
+    "  DATUM[\"WGS_1984\", "
+    "    SPHEROID[\"WGS 84\",6378137,298.257223563, "
+    "      AUTHORITY[\"EPSG\",7030]], "
+    "    TOWGS84[0,0,0,0,0,0,0], "
+    "    AUTHORITY[\"EPSG\",6326]], "
+    "  PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",8901]], "
+    "  UNIT[\"DMSH\",0.0174532925199433,AUTHORITY[\"EPSG\",9108]], "
+    "  AXIS[\"Lat\",NORTH], "
+    "  AXIS[\"Long\",EAST], "
+    "  AUTHORITY[\"EPSG\",4326]]";
+}
+
+
 /**
  * Get the first feature resulting from a select operation
  * @return QgsFeature
