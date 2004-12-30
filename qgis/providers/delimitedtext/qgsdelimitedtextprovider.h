7aa70bb7dc14b33a7b3a448bfe7852607a55ecad
mcoletti <mcoletti@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-12-30 22:20:49 +0000

    - rolled forward endian() changes in QgsDataProvider class hierarchy
    - rolled forward const-correctness changes from src directory
    
    - in delimited text provider plug-in consolidated how data is parsed into a
      single function from three almost identical, but separate, functions; now
      uses QDataStream for building WKBPoint objects
    
    - QgsDelimitedTextProvider::getFirstFeature() now works
    
    S: ----------------------------------------------------------------------
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2549 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/providers/delimitedtext/qgsdelimitedtextprovider.h
+++ qgis/providers/delimitedtext/qgsdelimitedtextprovider.h
@@ -14,12 +14,20 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
+
 /* $Id$ */
-#include <map>
+
 #include "../../src/qgsvectordataprovider.h"
+
+
+#include <map>
+
+
 class QgsFeature;
 class QgsField;
 class QFile;
+
+
 /**
 \class QgsDelimitedTextProvider
 \brief Data provider for delimited text files.
@@ -33,26 +41,87 @@ class QFile;
 *
 * Example uri = "/home/foo/delim.txt?delimiter=|"
 */
-class QgsDelimitedTextProvider : public QgsVectorDataProvider {
+class QgsDelimitedTextProvider : public QgsVectorDataProvider
+{
 public:
-  QgsDelimitedTextProvider(QString uri=0);
-  virtual ~QgsDelimitedTextProvider();
-  /** Used to ask the layer for its projection as a WKT string. Implements virtual method of same name in QgsDataProvider. */
+
+  QgsDelimitedTextProvider(QString uri = 0);
+
+  virtual ~ QgsDelimitedTextProvider();
+
+  /** get the first feature
+
+    mFile should be open with the file pointer at the record of the next
+    feature, or EOF.  The feature found on the current line is parsed.
+
+    @param feature the feature object to be populated with next feature
+    @param fetchAttributes is true if you want to get the feature attributes, too
+
+    The search will be limited to the search region if one is active.
+
+    @return found QgsFeature, or null if not found
+  */
+  QgsFeature *getFirstFeature(bool fetchAttributes = false);
+
+  /** Get the next feature, if any
+
+    mFile should be open with the file pointer at the record of the next
+    feature, or EOF.  The feature found on the current line is parsed.
+
+    @param feature the feature object to be populated with next feature
+    @param fetchAttributes is true if you want to get the feature attributes, too
+
+    The search will be limited to the search region if one is active.
+
+  * @return found QgsFeature, or null if not found
+  */
+  QgsFeature *getNextFeature(bool fetchAttributes = false);
+
+  /** get the next feature, if any
+
+    mFile should be open with the file pointer at the record of the next
+    feature, or EOF.  The feature found on the current line is parsed.
+
+    @param feature the feature object to be populated with next feature
+    @param fetchAttributes is true if you want to get the feature attributes, too
+
+    @return false if unable to get the next feature
+
+    @note that feature will also be set invalid if unable to locate the next
+    feature
+
+    The search will be limited to the search region if one is active.
+
+  /** Used to ask the layer for its projection as a WKT string. Implements
+  virtual method of same name in QgsDataProvider. */
   QString getProjectionWKT()  {return QString("Not implemented yet");} ;
-/**
+
+  /**
   * Get the first feature resulting from a select operation
   * @return QgsFeature
   */
-  QgsFeature * getFirstFeature(bool fetchAttributes=false);
-  /** 
-  * Get the next feature resutling from a select operation
-  * @return QgsFeature
-  */
-  QgsFeature * getNextFeature(bool fetchAttributes=false);
-  bool getNextFeature(QgsFeature &feature, bool fetchAttributes=false);
+  bool getNextFeature(QgsFeature & feature, bool fetchAttributes = false);
 
-  QgsFeature * getNextFeature(std::list<int>& attlist);
-  
+  /** get the next feature, if any
+
+    mFile should be open with the file pointer at the record of the next
+    feature, or EOF.  The feature found on the current line is parsed.
+
+    @param feature the feature object to be populated with next feature @param
+           attlist is list of attributes by their field positions to fetch
+                   with object
+
+    @return false if unable to get the next feature
+
+    @note That feature will also be set invalid if unable to locate the next
+    feature.
+
+    The search will be limited to the search region if one is active.
+
+  */
+  QgsFeature *getNextFeature( std::list<int> const & desiredAttributes );
+
+
   /** Get the feature type. This corresponds to 
       WKBPoint,
       WKBLineString,
@@ -63,74 +132,70 @@ public:
   * as defined in qgis.h
   * This provider will always return WKBPoint
   */
-  int geometryType();
+  int geometryType() const;
     /** 
     * Get the number of features in the layer
     */
-    long featureCount();
+  long featureCount() const;
     /** 
     * Get the number of fields in the layer
     */
-    int fieldCount();
+  int fieldCount() const;
   /**
   * Select features based on a bounding rectangle. Features can be retrieved 
   * with calls to getFirstFeature and getNextFeature.
   * @param mbr QgsRect containing the extent to use in selecting features
   */
-  void select(QgsRect *mbr, bool useIntersect=false);
+  void select(QgsRect * mbr, bool useIntersect = false);
   /** 
     * Set the data source specification. This may be a path or database
   * connection string
   * @uri data source specification
   */
   void setDataSourceUri(QString uri);
-  
+
     /** 
   * Get the data source specification. This may be a path or database
   * connection string
   * @return data source specification
   */
   QString getDataSourceUri();
-  
+
   /**
   * Identify features within the search radius specified by rect
   * @param rect Bounding rectangle of search radius
   * @return std::vector containing QgsFeature objects that intersect rect
   */
-  virtual std::vector<QgsFeature>& identify(QgsRect *rect);
-
-  /** Return endian-ness for this layer
-  */  
-  int endian();
+  virtual std::vector < QgsFeature > &identify(QgsRect * rect);
 
   /** Return the extent for this data layer
   */
-  virtual QgsRect * extent();
+  virtual QgsRect *extent();
   /**
   * Get the attributes associated with a feature
   */
- void getFeatureAttributes(int key, QgsFeature *f); 
+  void getFeatureAttributes(int key, QgsFeature * f);
  /**
  * Get the field information for the layer
  */
- std::vector<QgsField>& fields();
- 
- /* Reset the layer (ie move the file pointer to the head
-    of the file.
- */
- void reset();
+    std::vector<QgsField> const & fields() const;
+
+  /* Reset the layer (ie move the file pointer to the head
+     of the file.
+   */
+  void reset();
 
  /**Returns the minimum value of an attribute
     @param position the number of the attribute*/
- QString minValue(int position);
+  QString minValue(int position);
 
  /**Returns the maximum value of an attribute
     @param position the number of the attribute*/
- QString maxValue(int position);
+  QString maxValue(int position);
 
  /**Returns true if this is a valid delimited file
  */
- bool isValid();
+  bool isValid();
 
  /**
   * Check to see if the point is withn the selection
@@ -139,55 +204,88 @@ public:
   * @param y Y value of point
   * @return True if point is within the rectangle
   */
- bool boundsCheck(double x, double y);
+  bool boundsCheck(double x, double y);
 
- //! We support saving as shapefile - used to add item to the
- //  layers context menu
- bool supportsSaveAsShapefile();
- 
+  //! We support saving as shapefile - used to add item to the
+  //  layers context menu
+  bool supportsSaveAsShapefile();
+
   //! Save the layer as a shapefile
   bool saveAsShapefile();
-  QgsDataSourceURI * getURI(){ return 0;};
+  QgsDataSourceURI *getURI()
+  {
+    return 0;
+  };
 private:
+
+  /** get the next feature, if any
+
+    mFile should be open with the file pointer at the record of the next
+    feature, or EOF.  The feature found on the current line is parsed.
+
+    @param feature the feature object to be populated with next feature
+    @param getAttributes is true if you want to get the feature attributes, too
+    
+    @param desiredAttributes attributes fields to be collected for the feature
+                             as denoted by their position
+
+    @return false if unable to get the next feature
+  */
+  bool getNextFeature_( QgsFeature & feature, 
+                        bool getAttributes, 
+                        std::list<int> const * desiredAttributes = 0 );
+
   void fillMinMaxCash();
-  int * getFieldLengths();
+
+  int *getFieldLengths();
+
   //! Fields
-  std::vector<QgsField> attributeFields;
+  std::vector < QgsField > attributeFields;
+
   //! Map to store field position by name
-  std::map<QString, int> fieldPositions;
+  std::map < QString, int >fieldPositions;
+
   QString mDataSourceUri;
   QString mFileName;
   QString mDelimiter;
   QString mXField;
   QString mYField;
+
   //! Layer extent
   QgsRect *mExtent;
+
   //! Current selection rectangle
+
   QgsRect *mSelectionRectangle;
+
   //! Text file
   QFile *mFile;
+
   bool mValid;
+
   int mGeomType;
+
   long mNumberFeatures;
+
   //! Feature id
   long mFid;
-  enum ENDIAN
-  {
-    NDR = 1,
-    XDR = 0
-  };
+
   /**Flag indicating, if the minmaxcache should be renewed (true) or not (false)*/
   bool mMinMaxCacheDirty;
+
   /**Matrix storing the minimum and maximum values*/
-  double** mMinMaxCache;
+  double **mMinMaxCache;
+
   /**Fills the cash and sets minmaxcachedirty to false*/
   void mFillMinMaxCash();
-struct wkbPoint{
-  char byteOrder;
-  unsigned wkbType;
-  double x;
-  double y;
-};
-wkbPoint mWKBpt;
-  
+
+  struct wkbPoint
+  {
+    unsigned char byteOrder;
+    Q_UINT32 wkbType;
+    double x;
+    double y;
+  };
+  wkbPoint mWKBpt;
+
 };
