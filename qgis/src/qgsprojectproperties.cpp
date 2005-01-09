7dde67b2398fba721d7c18e7028449eccd7134b2
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-01-09 02:34:16 +0000

    Updates for user defined selection colour....still needs some attention
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2600 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/src/qgsprojectproperties.cpp
+++ qgis/src/qgsprojectproperties.cpp
@@ -24,6 +24,7 @@
 #include "qgsproject.h"
 #include "qgsmaplayer.h"
 #include "qgsmaplayerregistry.h"
+#include "qgsrenderer.h"
 
 //qt includes
 #include <qcombobox.h>
@@ -204,6 +205,7 @@ void QgsProjectProperties::apply()
     QgsProject::instance()->writeEntry("Gui","/SelectionColorRedPart",myColour.red());
     QgsProject::instance()->writeEntry("Gui","/SelectionColorGreenPart",myColour.green());
     QgsProject::instance()->writeEntry("Gui","/SelectionColorBluePart",myColour.blue()); 
+    QgsRenderer::mSelectionColor=QColor(0,0,0);
             
 }
 
