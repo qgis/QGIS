ce022560f165547bc12a5588897805ac7c43dc89
mcoletti <mcoletti@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-01-19 21:22:30 +0000

    Now test for reading empty key entries correctly.  Made member names more standards compliant.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2672 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- tests/qgsproject/projecttest.h
+++ tests/qgsproject/projecttest.h
@@ -17,6 +17,7 @@ class ProjectTest : public CppUnit::TestFixture
     CPPUNIT_TEST( testTitle );
     CPPUNIT_TEST( testMapUnits );
     CPPUNIT_TEST( testDirtyFlag );
+    CPPUNIT_TEST( readNullEntries );
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -27,37 +28,76 @@ class ProjectTest : public CppUnit::TestFixture
     */
     void setUp()
     {
-        file = "test.project";
-        title = "test title";
-    }
+        mFile = "test.project";
+        mTitle = "test title";
+        mScope = "project_test";
+
+        mNumValueKey = "/values/num";
+
+        mDoubleValueKey = "/values/double";
+
+        mBoolValueKey = "/values/bool";
+
+        mStringValueKey = "/values/string";
+
+        mStringListValueKey = "/values/stringlist";
+
+
+        mNumValueConst = 42;
+
+        mDoubleValueConst = 12345.6789;
+
+        mBoolValueConst = true;
+
+        mStringValueConst = "Test String";
+
+        mStringListValueConst += "first";
+        mStringListValueConst += "second";
+        mStringListValueConst += "third";
+    } // setUp
+
     
 
     void testFileName()
     {
-        QgsProject::instance()->filename( file );
+        QgsProject::instance()->dirty( false );
+        QgsProject::instance()->filename( mFile );
 
-        CPPUNIT_ASSERT( file == QgsProject::instance()->filename() );
-    }
+        CPPUNIT_ASSERT( mFile == QgsProject::instance()->filename() );
+        CPPUNIT_ASSERT( QgsProject::instance()->dirty() );
+    } // testFileName
+
     
 
     void testTitle()
     {
-        QgsProject::instance()->title( title );
+        QgsProject::instance()->dirty( false );
+        QgsProject::instance()->title( mTitle );
 
-        CPPUNIT_ASSERT( title == QgsProject::instance()->title() );
-    }
+        CPPUNIT_ASSERT( mTitle == QgsProject::instance()->title() );
+        CPPUNIT_ASSERT( QgsProject::instance()->dirty() );
+    } // testTitle
+
 
     void testMapUnits()
     {
+        QgsProject::instance()->dirty( false );
         QgsProject::instance()->mapUnits( QgsScaleCalculator::METERS );
         CPPUNIT_ASSERT( QgsScaleCalculator::METERS == QgsProject::instance()->mapUnits() );
+        CPPUNIT_ASSERT( QgsProject::instance()->dirty() );
 
+        QgsProject::instance()->dirty( false );
         QgsProject::instance()->mapUnits( QgsScaleCalculator::FEET );
         CPPUNIT_ASSERT( QgsScaleCalculator::FEET == QgsProject::instance()->mapUnits() );
+        CPPUNIT_ASSERT( QgsProject::instance()->dirty() );
 
+        QgsProject::instance()->dirty( false );
         QgsProject::instance()->mapUnits( QgsScaleCalculator::DEGREES );
         CPPUNIT_ASSERT( QgsScaleCalculator::DEGREES == QgsProject::instance()->mapUnits() );
-    }
+        CPPUNIT_ASSERT( QgsProject::instance()->dirty() );
+    } // testMapUnits
+
+
 
     void testDirtyFlag()
     {
@@ -66,15 +106,86 @@ class ProjectTest : public CppUnit::TestFixture
 
         QgsProject::instance()->dirty( false );
         CPPUNIT_ASSERT( ! QgsProject::instance()->dirty() );
-    }
+    } // testDirtyFlag
+
     
+    /**
+       Reading entries that are known not to exist should fail and use default
+       values.
+     */
+    void readNullEntries()
+    {
+        bool status;
+
+
+        bool b = QgsProject::instance()->readBoolEntry( mScope, mBoolValueKey, false, &status);
+
+        CPPUNIT_ASSERT( false == b && ! status );
+
+
+        int i = QgsProject::instance()->readNumEntry( mScope, mNumValueKey, 13, &status);
+
+        CPPUNIT_ASSERT( 13 == i && ! status );
+
+
+        double d = QgsProject::instance()->readDoubleEntry( mScope, mDoubleValueKey, 99.0, &status);
+
+        CPPUNIT_ASSERT( 99.0 == d && ! status );
+
+
+        QString s = QgsProject::instance()->readEntry( mScope, mStringValueKey, "FOO", &status);
+
+        CPPUNIT_ASSERT( "FOO" == s && ! status );
+
+
+        QStringList sl = QgsProject::instance()->readListEntry( mScope, mStringListValueKey, &status);
+
+        CPPUNIT_ASSERT( sl.empty() && ! status );
+
+    } // readNullEntries
+
+
 private:
 
     /// file name for project file
-    QString file;
+    QString mFile;
 
     /// test project title
-    QString title;
+    QString mTitle;
+
+    /// test project scope
+    QString mScope;
+
+    /// num value key
+    QString mNumValueKey;
+
+    /// double value key
+    QString mDoubleValueKey;
+
+    /// bool value key
+    QString mBoolValueKey;
+
+    /// string value key
+    QString mStringValueKey;
+
+    /// string list value key
+    QString mStringListValueKey;
+
+    /// num value const
+    int mNumValueConst;
+
+    /// double value const
+    double mDoubleValueConst;
+
+    /// bool value const
+    bool mBoolValueConst;
+
+    /// string value const
+    QString mStringValueConst;
+
+    /// string list value const
+    QStringList mStringListValueConst;
+
 
 }; // class ProjectTest
 
