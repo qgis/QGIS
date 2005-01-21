d2adc1ff5d920a1d4468a31c8ae06764f0e6c9b6
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-01-21 05:30:20 +0000

    Changes from mloskot to allow editing of geometry type
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2684 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/plugins/spit/qgseditreservedwordsdialog.h
+++ qgis/plugins/spit/qgseditreservedwordsdialog.h
@@ -3,14 +3,14 @@
 #include "qgseditreservedwordsbase.h"
 class QgsEditReservedWordsDialog : public QgsEditReservedWordsBase
 {
-  Q_OBJECT
-  public:
+    Q_OBJECT
+public:
     QgsEditReservedWordsDialog(QWidget *parent=0, const char *name=0);
     ~QgsEditReservedWordsDialog();
     void addColumn(QString column, bool isReserved, int index);
     void setReservedWords(const QStringList &);
     QStringList columnNames();
-  public slots:
+public slots:
     void checkWord(QListViewItem *, int , const QString&);
     void editWord(QListViewItem *);
 
