ee344acff02afae7810073cf2729a9a70fe41537
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-02-16 15:49:33 +0000

    Little batch script to automate checkinstall process
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2816 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ plugins/openmodeller/doCheckInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##
+## A simple bash script to perform a checkinstall
+##
+
+# Accept default answers to all questions
+# Set name
+# Set version
+# Set software group
+# The package maintainer (.deb)
+
+checkinstall --default --pkgname=openmodeller-gui --pkgversion=0.2.1pre --pkggroup=GIS --maintainer=tim@linfiniti.com        
+
