cf1a01dff6348683accc60367fabb98b66f9dc4e
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-02-16 15:59:04 +0000

    Desctiption of qgis for checkinstall and little helper script to automate checkinstall
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@2817 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ doCheckInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##
+## A simple bash script to perform a checkinstall
+##
+
+# Accept default answers to all questions
+# Set name
+# Set version
+# Set software group
+# The package maintainer (.deb)
+
+checkinstall --default --pkgname=qgis --pkgversion=0.7.0pre --pkggroup=GIS --maintainer=tim@linfiniti.com        
+
