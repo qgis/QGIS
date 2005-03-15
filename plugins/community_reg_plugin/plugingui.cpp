fff6c8702b022885cb52c133503f2361669371f1
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-03-15 01:38:05 +0000

    Initial import. Not funtional yet. Not added to parent make yet to avoid building this dir before the plugin works properly
    git-svn-id: http://svn.osgeo.org/qgis/trunk@2966 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/community_reg_plugin/plugingui.cpp
+++ plugins/community_reg_plugin/plugingui.cpp
@@ -16,6 +16,13 @@
 #include <qurl.h>
 #include <qlineedit.h>
 #include <qhttp.h>
+#include <qregexp.h>
+#include <qcursor.h> 
+#include <qapplication.h> 
+
+//plugin includes
+#include "qgslocationcapturewidget.h"
+
 //standard includes
 
 PluginGui::PluginGui() : PluginGuiBase()
@@ -26,60 +33,76 @@ PluginGui::PluginGui() : PluginGuiBase()
     PluginGui::PluginGui( QWidget* parent , const char* name , bool modal , WFlags fl  )
 : PluginGuiBase( parent, name, modal, fl )
 {
-
-  connect(&mConnection, SIGNAL(done(bool)), this, SLOT(registrationDone(bool)));
+  mConnection = new QHttp();
+  connect(mConnection, SIGNAL(done(bool)), this, SLOT(registrationDone(bool)));
 }  
 PluginGui::~PluginGui()
 {
+  delete mConnection;
+  delete mHttp;
 }
 
 void PluginGui::pbnOK_clicked()
 {
-  QUrl targetURL("http://community.qgis.org";);
-  QString host = targetURL.host();
-  int port = targetURL.port();
-  http = new QHttp(host, port == -1 ? 80 : port);
-  connect(http, SIGNAL(requestStarted(int)), this, 
+  QUrl myTargetUrl("http://community.qgis.org");
+  QString myHost = myTargetUrl.host();
+  int myPort = myTargetUrl.port();
+  mHttp = new QHttp(myHost, myPort == -1 ? 80 : myPort);
+  connect(mHttp, SIGNAL(requestStarted(int)), this, 
           SLOT(slotRequestStarted(int)));
-  connect(http, SIGNAL(requestFinished(int, bool)), this, 
+  connect(mHttp, SIGNAL(requestFinished(int, bool)), this, 
           SLOT(slotRequestFinished(int, bool)));
-  connect(http, SIGNAL(done(bool)), this, SLOT(slotDone(bool)));
-  connect(http, SIGNAL(dataReadProgress(int,int)), this, 
+  connect(mHttp, SIGNAL(done(bool)), this, SLOT(slotDone(bool)));
+  connect(mHttp, SIGNAL(dataReadProgress(int,int)), this, 
           SLOT(slotDataReadProgress(int,int)));
-  connect(http, SIGNAL(responseHeaderReceived(const 
+  connect(mHttp, SIGNAL(responseHeaderReceived(const 
                   QHttpResponseHeader&)),
           this, SLOT(slotResponseHeaderReceived(const QHttpResponseHeader 
                   &)));
-  QByteArray *array=new QByteArray();
+  QByteArray *myByteArray=new QByteArray();
   QString tmp = "name=shie";
-  array->setRawData(tmp.latin1(), tmp.length());     
-  http->post("/qgis_users/index.php", *array);
+  myByteArray->setRawData(tmp.latin1(), tmp.length());     
+  mHttp->post("/qgis_users/index.php", *myByteArray);
   done(1);
 } 
 void PluginGui::pbnCancel_clicked()
 {
   close(1);
 }
-void PluginGui::search()
+
+void PluginGui::pbnGetCoords_clicked()
 {
-  if (connection.state() == QHttp::HostLookup
-          || connection.state() == QHttp::Connecting
-          || connection.state() == QHttp::Sending
-          || connection.state() == QHttp::Reading) {
-    connection.abort();
+  QgsLocationCaptureWidget * myWidget = new QgsLocationCaptureWidget();
+  myWidget->show();
+  delete myWidget;
+}
+
+
+
+void PluginGui::submit()
+{
+  if (mConnection->state() == QHttp::HostLookup
+          || mConnection->state() == QHttp::Connecting
+          || mConnection->state() == QHttp::Sending
+          || mConnection->state() == QHttp::Reading) {
+    mConnection->abort();
   }
 
-  if (myLineEdit->text() == "") 
+  if (leName->text().isEmpty() | 
+      leEmail->text().isEmpty() |
+      leLatitude->text().isEmpty() |
+      leLongitude->text().isEmpty()   
+      ) 
   {
     QMessageBox::critical(this, "Empty query",
-            "Please type a search string.",
+            "Please type a submit string.",
             QMessageBox::Ok, QMessageBox::NoButton);
   } 
   else 
   {
     QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
 
-    connection.setHost("community.qgis.org");
+    mConnection->setHost("community.qgis.org");
 
     QHttpRequestHeader myHeader("POST", "/qgis_users/index.php");
     myHeader.setValue("Host", "community.qgis.org");
@@ -103,40 +126,45 @@ void PluginGui::search()
     QUrl::encode(myEncodedLongitude);
     QString myPostString = "formAction=save";
     myPostString += "&fldname=" + myEncodedName;
-    myPostString += "&fldemail="=myEncodedEmail;
-    myPostString += "&fldimage_url="=myEncoded;
-    myPostString += "&fldhome_url="=myEncoded;
-    myPostString += "&fldcountry="=myEncoded;
-    myPostString += "&fldplace_description="=myEncoded;
-    myPostString += "&fldlatitude="=myEncoded;
-    myPostString += "&fldlongitude="=myEncoded;
+    myPostString += "&fldemail="+myEncodedEmail;
+    myPostString += "&fldimage_url="+myEncodedImageUrl;
+    myPostString += "&fldhome_url="+myEncodedHomeUrl;
+    myPostString += "&fldcountry="+myEncodedCountry;
+    myPostString += "&fldplace_description="+myEncodedPlaceDescription;
+    myPostString += "&fldlatitude="+myEncodedLatitude;
+    myPostString += "&fldlongitude="+myEncodedLongitude;
 
-    connection.request(myHeader, myPostString.utf8());
+    mConnection->request(myHeader, myPostString.utf8());
   }
 
 }
 
-void PluginGui::searchDone( bool error )
+void PluginGui::submitDone( bool error )
 {
   if (error) {
-    QMessageBox::critical(this, "Error searching",
-            "An error occurred when searching: "
-            + connection.errorString(),
+    QMessageBox::critical(this, "Error submiting",
+            "An error occurred when submiting: "
+            + mConnection->errorString(),
             QMessageBox::Ok, QMessageBox::NoButton);
   } else {
-    QString result(connection.readAll());
+    QString result(mConnection->readAll());
+    /*
+     * TODO Implement checking of returned submission results page
+     * 
+     QRegExp rx("<a href=\"(http://lists\\.trolltech\\.com/qt-interest/.*)\">(.*)</a>");
+     rx.setMinimal(TRUE);
+     int pos = 0;
+     while (pos >= 0) 
+     {
+     pos = rx.search(result, pos);
+     if (pos > -1) 
+     {
+     pos += rx.matchedLength();
+     new QListViewItem(myListView, rx.cap(2), rx.cap(1));
+     }
+     }
+     */
+    QApplication::restoreOverrideCursor();
 
-    QRegExp rx("<a href=\"(http://lists\\.trolltech\\.com/qt-interest/.*)\">(.*)</a>");
-    rx.setMinimal(TRUE);
-    int pos = 0;
-    while (pos >= 0) {
-      pos = rx.search(result, pos);
-      if (pos > -1) {
-        pos += rx.matchedLength();
-        new QListViewItem(myListView, rx.cap(2), rx.cap(1));
-      }
-    }
   }
-
-  QApplication::restoreOverrideCursor();
-
+}
