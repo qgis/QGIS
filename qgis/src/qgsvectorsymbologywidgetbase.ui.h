11b17b13fa495352fe954a42026d331cf911fca8
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-03-24 09:12:02 +0000

    Minor changes to style widgets and addition of vector symbology widget that will eventually replace all the other renderer widgets. This work will not be continued until after 0.7 release.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@3013 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/qgsvectorsymbologywidgetbase.ui.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you want to add, delete, or rename functions or slots, use
+** Qt Designer to update this file, preserving your code.
+**
+** You should not define a constructor or destructor in this file.
+** Instead, write your code in functions called init() and destroy().
+** These will automatically be called by the form's constructor and
+** destructor.
+*****************************************************************************/
+
+
+
+void QgsVectorSymbologyWidgetBase::listView1_selectionChanged( QListViewItem * )
+{
+
+}
