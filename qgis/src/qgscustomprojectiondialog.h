2aaaaa036161413b053af19cfdf37e40823055d7
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-04-10 23:50:32 +0000

    Cleaned out some duplicate includes from widgets/projectionselector/qgsprojectionselector.cpp
    Added initial start of user custom projection dialog (doesnt do anything much yet and not wired into gui yet)
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk@3118 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/qgscustomprojectiondialog.h
@@ -0,0 +1,40 @@
+//
+// C++ Interface: qgscustomprojectiondialog
+//
+// Description: 
+//
+//
+// Author: Tim Sutton tim@linfiniti.com, (C) 2005
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef QGSCUSTOMPROJECTIONDIALOG_H
+#define QGSCUSTOMPROJECTIONDIALOG_H
+
+#include <qdir.h>
+#include <qgscustomprojectiondialogbase.uic.h>
+
+/**
+The custom projection widget is used to define the projection family, ellipsoid and paremters needed by proj4 to assemble a customised projection definition. The resulting projection will be store in an sqlite backend.
+
+@author Tim Sutton
+*/
+class QgsCustomProjectionDialog : public QgsCustomProjectionDialogBase
+{
+Q_OBJECT
+public:
+    QgsCustomProjectionDialog(QWidget* parent , const char* name , WFlags fl=0);
+    //a recursive function to make a directory and its ancestors
+    bool makeDir(QDir &theQDir);
+public slots:    
+    ~QgsCustomProjectionDialog();
+    void pbnHelp_clicked();
+    void pbnOK_clicked();
+    void pbnApply_clicked();
+    void pbnCancel_clicked();
+    void cboProjectionFamily_textChanged( const QString & );
+
+};
+
+#endif
