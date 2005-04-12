c12a870fe2e74990ef7afba391e1c766ad3acc1e
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-04-12 11:51:30 +0000

    Fix for [ 1181249 ] Crash when loading shape files
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@3137 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsvectorlayer.cpp
+++ src/qgsvectorlayer.cpp
@@ -2607,9 +2607,20 @@ void QgsVectorLayer::setCoordinateSystem()
       }
       else ///qgis/projections/defaultBehaviour==useDefault
       {
-        //shamelessly hard coded for now
-        //XXX TODO get this from options dlg
-        mySourceWKT = mySettings.readEntry("/qgis/projections/defaultProjectionWKT");
+	  //default to geo / wgs84 for now 
+	  QString myGeoWKT =    "GEOGCS[\"WGS 84\", "
+	  "  DATUM[\"WGS_1984\", "
+	  "    SPHEROID[\"WGS 84\",6378137,298.257223563, "
+	  "      AUTHORITY[\"EPSG\",7030]], "
+	  "    TOWGS84[0,0,0,0,0,0,0], "
+	  "    AUTHORITY[\"EPSG\",6326]], "
+	  "  PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",8901]], "
+	  "  UNIT[\"DMSH\",0.0174532925199433,AUTHORITY[\"EPSG\",9108]], "
+	  "  AXIS[\"Lat\",NORTH], "
+	  "  AXIS[\"Long\",EAST], "
+	  "  AUTHORITY[\"EPSG\",4326]]";
+        mySourceWKT = mySettings.readEntry("/qgis/projections/defaultProjectionWKT",myGeoWKT);
+	
       }
     }
 
