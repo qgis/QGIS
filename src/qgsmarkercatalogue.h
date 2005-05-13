5bdb63fa02b9c445453380f38848ad9b54f89ce6
rabla <rabla@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-05-13 15:40:26 +0000

    Qt bug fix for markers
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@3371 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsmarkercatalogue.h
+++ src/qgsmarkercatalogue.h
@@ -43,7 +43,7 @@ public:
     /** Returns picture of the marker
      * \param fullName full name, e.g. hard:circle, svg:/home/usr1/marker1.svg
      */
-    QPicture marker ( QString fullName, int size, QPen pen, QBrush brush, int oversampling = 1 );
+    QPicture marker ( QString fullName, int size, QPen pen, QBrush brush, int oversampling = 1, bool qtBug = true );
 
 private:
 
@@ -56,7 +56,7 @@ private:
     QStringList mList;
 
     /** Hard coded */
-    QPicture hardMarker ( QString name, int size, QPen pen, QBrush brush, int oversampling = 1 );
+    QPicture hardMarker ( QString name, int size, QPen pen, QBrush brush, int oversampling = 1, bool qtBug = true );
 
     /** Hard coded */
     QPicture svgMarker ( QString name, int size, int oversampling = 1 );
