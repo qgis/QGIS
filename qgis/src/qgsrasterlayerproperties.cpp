ca7695278425717e9a631913d5f454912f8ded82
stevehalasz <stevehalasz@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-05-25 02:39:39 +0000

    Update raster name before updating legend pixmap. Fix for bug #1205392.
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_7-candidate@3457 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/src/qgsrasterlayerproperties.cpp
+++ qgis/src/qgsrasterlayerproperties.cpp
@@ -374,14 +374,14 @@ void QgsRasterLayerProperties::apply()
 
   }
 
-
+	rasterLayer->setLayerName(leDisplayName->text());
+	
   //update the legend pixmap
   pixmapLegend->setPixmap(rasterLayer->getLegendQPixmap());
   pixmapLegend->setScaledContents(true);
   pixmapLegend->repaint(false);
   rasterLayer->updateItemPixmap();
-
-  rasterLayer->setLayerName(leDisplayName->text());
+	
   //see if the user would like debug overlays
   if (cboxShowDebugInfo->isChecked()
           == true)
