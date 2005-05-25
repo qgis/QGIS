f6c0bd60ee7c93dc0d62a3cc66d4485e04fd7c6e
stevehalasz <stevehalasz@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-05-25 02:51:11 +0000

    Merge fix for bug #1205392 (raster layer name not updated) from Release-0_7-candidate branch.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@3459 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/qgsrasterlayerproperties.cpp
+++ src/qgsrasterlayerproperties.cpp
@@ -373,15 +373,15 @@ void QgsRasterLayerProperties::apply()
     }
 
   }
-
-
+  
+  rasterLayer->setLayerName(leDisplayName->text());
+  
   //update the legend pixmap
   pixmapLegend->setPixmap(rasterLayer->getLegendQPixmap());
   pixmapLegend->setScaledContents(true);
   pixmapLegend->repaint(false);
   rasterLayer->updateItemPixmap();
-
-  rasterLayer->setLayerName(leDisplayName->text());
+  
   //see if the user would like debug overlays
   if (cboxShowDebugInfo->isChecked()
           == true)
