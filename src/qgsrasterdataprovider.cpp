244cdb7b38d3f1ebc9ce611768b761d417f48f8a
morb_au <morb_au@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-05-28 02:29:45 +0000

    Added in some more support files for WMS.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@3478 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/qgsrasterdataprovider.cpp
@@ -0,0 +1,27 @@
+/***************************************************************************
+    qgsrasterdataprovider.cpp - DataProvider Interface for raster layers
+     --------------------------------------
+    Date                 : Mar 11, 2005
+    Copyright            : (C) 2005 by Brendan Morley
+    email                : morb at ozemail dot com dot au
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+#include "qgsrasterdataprovider.h"
+
+
+QgsRasterDataProvider::QgsRasterDataProvider()
+{
+
+}
+
+
