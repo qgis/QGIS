abfb12cbd9fbc8c82604d2bfc2154a0dfb139364
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-06-11 00:48:46 +0000

    Fixed erro with configure preventing the plugin from building
    git-svn-id: http://svn.osgeo.org/qgis/trunk@3568 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- plugins/cdp_wizard/autogen.sh
+++ plugins/cdp_wizard/autogen.sh
@@ -93,7 +93,7 @@ if [ "$LT_ERROR" = "1" ]; then
 fi
 
 echo Configuring build environment for QGIS
-aclocal \
+aclocal -I /usr/local/share/aclocal/ \
   && libtoolize --force --copy \
   && automake --add-missing --foreign --copy \
   && autoconf --force \
