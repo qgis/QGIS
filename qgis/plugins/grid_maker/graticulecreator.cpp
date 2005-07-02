fb3890db4ca400e8a97cfecc69549df20a9b09f0
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-07-02 23:42:59 +0000

    i18n patch for file names
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_7-candidate@3690 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/plugins/grid_maker/graticulecreator.cpp
+++ qgis/plugins/grid_maker/graticulecreator.cpp
@@ -23,19 +23,26 @@ GraticuleCreator::GraticuleCreator(QString theOutputFileName,
     /* Open and prepare output files */
     myDbfHandle = createDbf(theOutputFileName);
     myShapeHandle = createShapeFile(theOutputFileName);
-    writeProjectionFile(theOutputFileName);
-    //test the write point routine....
-    //generatePoints(theInputFileName,myDbfHandle,myShapeHandle);
-    generateGraticule(myDbfHandle,
-                      myShapeHandle,
-                      theXIntervalDouble,
-                      theYIntervalDouble,
-                      theXOriginDouble,
-                      theYOriginDouble,
-                      theXEndPointDouble,
-                      theYEndPointDouble);
-    DBFClose( myDbfHandle );
-    SHPClose( myShapeHandle );
+    if (myDbfHandle && myShapeHandle)
+    {
+      writeProjectionFile(theOutputFileName);
+      //test the write point routine....
+      //generatePoints(theInputFileName,myDbfHandle,myShapeHandle);
+      generateGraticule(myDbfHandle,
+                        myShapeHandle,
+                        theXIntervalDouble,
+                        theYIntervalDouble,
+                        theXOriginDouble,
+                        theYOriginDouble,
+                        theXEndPointDouble,
+                       theYEndPointDouble);
+      DBFClose( myDbfHandle );
+      SHPClose( myShapeHandle );
+    }
+    else
+    {
+      std::cout << "Error creating the output files!" << std::endl;
+    }
     return;
 }
 
@@ -47,16 +54,19 @@ DBFHandle GraticuleCreator::createDbf (QString theDbfName )
     QFileInfo myFileInfo( theDbfName );
     QString myBaseString = myFileInfo.dirPath()+QString("/")+myFileInfo.baseName();  // excludes any extension
     //create the dbf
-    myDbfHandle = DBFCreate( myBaseString+".dbf" );
+    myDbfHandle = DBFCreate( (const char *)((myBaseString + ".dbf").local8Bit()) );
+    if (!myDbfHandle)
+    {
+            return NULL;
+    }
     //create an index field named after the base part of the file name
-
-    DBFAddField( myDbfHandle, myBaseString+"_id", FTInteger, 11, 0 );
+    DBFAddField( myDbfHandle,  (const char *)((myBaseString + "_id").local8Bit()), FTInteger, 11, 0 );
     //create a second arbitary attribute field
     DBFAddField( myDbfHandle, "Date", FTString, 12, 0 );
     //close the dbf
     DBFClose( myDbfHandle );
     //reopen
-    myDbfHandle = DBFOpen( myBaseString+".dbf", "r+b" );
+    myDbfHandle = DBFOpen( (const char *)((myBaseString + ".dbf").local8Bit()), "r+b" );
     //exit this fn giving
     return myDbfHandle;
 }
@@ -65,7 +75,7 @@ SHPHandle GraticuleCreator::createShapeFile(QString theFileName )
 {
     SHPHandle myShapeHandle;
     //myShapeHandle = SHPCreate(theFileName, SHPT_POINT );
-    myShapeHandle = SHPCreate(theFileName, SHPT_ARC );
+    myShapeHandle = SHPCreate( (const char *)(theFileName.local8Bit()), SHPT_ARC );
     return myShapeHandle;
 }
 
