502b2fecf10a704bf0bf63b06f2785fd11985729
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-07-22 18:23:30 +0000

    forgot to add these two files before the last commit
    git-svn-id: http://svn.osgeo.org/qgis/trunk@3773 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ qgis/src/legend/qgslegendlayerfilegroup.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+                         qgslegendlayerfilegroup.h  -  description
+                             -------------------
+    begin                : Juli 2005
+    copyright            : (C) 2005 by Marco Hugentobler
+    email                : marco.hugentobler@autoform.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgslegenditem.h"
+
+class QgsLegendLayerFileGroup: public QgsLegendItem
+{
+ public:
+    QgsLegendLayerFileGroup(QListViewItem* theItem, QString theString);
+    bool isLeafNode() {return false;}
+    bool accept(LEGEND_ITEM_TYPE type);
+};
