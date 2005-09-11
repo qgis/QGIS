acb55956d50201b6c177692a666ac353110b93d3
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-09-11 21:42:02 +0000

    fixed postgis editing bug. Postgis1.0 prefers bytea (oct) instead of hex for WKB geometries. However, i cannot test, if the older postgis versions like oct bytea
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_7-candidate@3900 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/providers/postgres/qgspostgresprovider.cpp
+++ qgis/providers/postgres/qgspostgresprovider.cpp
@@ -1542,14 +1542,23 @@ bool QgsPostgresProvider::addFeature(QgsFeature* f)
     unsigned char* geom=f->getGeometry();
     for(int i=0;i<f->getGeometrySize();++i)
     {
-      QString hex=QString::number((int)geom[i],16).upper();
-      if(hex.length()==1)
-      {
-        hex="0"+hex;
-      }
-      insert+=hex;
+	//Postgis 1.0 wants bytea instead of hex
+	QString oct = QString::number((int)geom[i], 8);
+	if(oct.length()==3)
+	{
+	    oct="\\\\"+oct;
+	}
+	else if(oct.length()==1)
+	{
+	    oct="\\\\00"+oct;
+	}
+	else if(oct.length()==2)
+	{
+	    oct="\\\\0"+oct; 
+	}
+	insert+=oct;
     }
-    insert+="',"+srid+")";
+    insert+="::bytea',"+srid+")";
 
     //add the field values to the insert statement
     for(std::vector<QgsFeatureAttribute>::iterator it=attributevec.begin();it!=attributevec.end();++it)
