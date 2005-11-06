160ed3498b0627903f5c2dea15f0f5a5e7826df7
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-11-06 00:15:35 +0000

    Merve from 0.7 branch
    git-svn-id: http://svn.osgeo.org/qgis/trunk@4162 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ helpviewer/qgshelpserver.cpp
@@ -0,0 +1,65 @@
+/***************************************************************************
+                           qgshelpserver.cpp
+    Receive help context numbers from client process for help viewer
+                             -------------------
+    begin                : 2005-07-07
+    copyright            : (C) 2005 by Tom Elwertowski
+    email                : telwertowski at comcast.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+#include "qgshelpserver.h"
+
+// The communications technique used here has been adapted from Qt Assistant.
+// See qt/tools/assistant/ main.cpp and lib/qassistantclient.cpp (Qt 3.3.4).
+
+QgsHelpContextServer::QgsHelpContextServer(QObject *parent) :
+   QServerSocket(0x7f000001, 0, 1, parent)
+{
+  // Superclass listens for localhost connection
+}
+
+QgsHelpContextServer::~QgsHelpContextServer()
+{
+  // Socket is automatically deleted here because it is a QQbject child
+}
+
+void QgsHelpContextServer::newConnection(int socket)
+{
+  // Create socket in response to new connection
+  QgsHelpContextSocket *helpSocket = new QgsHelpContextSocket(socket, this);
+  // Pass context from socket upwards
+  connect(helpSocket, SIGNAL(setContext(const QString&)),
+    SIGNAL(setContext(const QString&)));
+}
+
+QgsHelpContextSocket::QgsHelpContextSocket(int socket, QObject *parent) :
+  QSocket(parent, 0)
+{
+  connect(this, SIGNAL(readyRead()), SLOT(readClient()));
+  setSocket(socket);
+}
+
+QgsHelpContextSocket::~QgsHelpContextSocket()
+{
+}
+
+void QgsHelpContextSocket::readClient()
+{
+  // Read context numbers (one per line) and pass upwards
+  QString contextId;
+  while ( canReadLine() )
+  {
+    contextId = readLine();
+    contextId.remove('\n');
+    emit setContext(contextId);
+  }
+}
