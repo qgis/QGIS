cd64a08047d95e5bab00128bd64886f118a141f1
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-11-21 05:51:31 +0000

    Added helpviewer subdir to the top-level Makefile.am Changes to helpviewer classes to allow compilation under Qt 4.0.1
    Works on Linux; untested on other platforms.
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@4252 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- helpviewer/qgshelpserver.cpp
+++ helpviewer/qgshelpserver.cpp
@@ -22,7 +22,7 @@
 // See qt/tools/assistant/ main.cpp and lib/qassistantclient.cpp (Qt 3.3.4).
 
 QgsHelpContextServer::QgsHelpContextServer(QObject *parent) :
-   Q3ServerSocket(0x7f000001, 0, 1, parent)
+   Q3ServerSocket(0x7f000001, 0, parent)
 {
   // Superclass listens for localhost connection
 }
