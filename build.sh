b5d9170fdbcffd48c8a23e264a0a10b6e64091cb
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-12-30 12:27:27 +0000

    Added a simple little bash script to build qgis
    git-svn-id: http://svn.osgeo.org/qgis/trunk@4411 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# A simple script to build QGIS
+#       Tim Sutton 2005
+#
+if [ ! $1 ]
+then
+echo "Usage: ${0} install_prefix"
+echo "e.g."
+echo "${0} \$HOME/apps/"
+ exit 1
+fi 
+export QTDIR=/usr/local/Trolltech/Qt-4.1.0/
+export PATH=$QTDIR/bin:$PATH
+export LD_LIBRARY_PATH=$QTDIR/lib
+./autogen.sh --enable-debug --prefix=${1} --with-qtdir=$QTDIR --with-grass=/usr/lib/grass && make && sudo make install
