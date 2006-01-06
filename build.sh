108ceb8306bc0f54d37b65e04230d8804518df6b
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-01-06 01:07:40 +0000

    Added some compiler flags to build script
    git-svn-id: http://svn.osgeo.org/qgis/trunk@4480 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ echo "e.g."
 echo "${0} \$HOME/apps/"
  exit 1
 fi 
+# disable default optimisations to improve debugging
+export CXXFLAGS="-g -Wall"
+export CFLAGS="-g -Wall"
 export QTDIR=/usr/local/Trolltech/Qt-4.1.0/
 export PATH=$QTDIR/bin:$PATH
 export LD_LIBRARY_PATH=$QTDIR/lib
