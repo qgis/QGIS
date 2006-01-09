91c51cd5eba11abb6f1b48fa4f4f4db973d2a6cc
telwertowski <telwertowski@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-01-09 08:44:47 +0000

    Add QgsRubberBand class to display measurement lines for QgsMeasure.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@4627 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/gui/qgsrubberband.cpp
@@ -0,0 +1,83 @@
+/***************************************************************************
+    qgsrubberband.cpp - Rubberband widget for drawing multilines and polygons
+     --------------------------------------
+    Date                 : 07-Jan-2006
+    Copyright            : (C) 2006 by Tom Elwertowski
+    Email                : telwertowski at users dot sourceforge dot net
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+#include "qgsrubberband.h"
+#include <QPainter>
+
+/*!
+  \class QgsRubberBand
+  \brief The QgsRubberBand class provides a transparent overlay widget
+  for tracking the mouse while drawing polylines or polygons.
+*/
+QgsRubberBand::QgsRubberBand(QWidget * parent, bool isPolygon)
+: QWidget(parent), mIsPolygon(isPolygon)
+{
+  setGeometry(parent->rect()); // this widget is same size as parent
+  mPoints.append(QPoint()); // addPoint assumes an initial allocated point
+}
+
+QgsRubberBand::~QgsRubberBand()
+{}
+
+/*!
+  Remove all points from the shape being created.
+*/
+void QgsRubberBand::reset(bool isPolygon)
+{
+  mPoints.resize(1); // addPoint assumes an initial allocated point
+  mIsPolygon = isPolygon;
+  update();
+}
+
+/*!
+  Add a point to the shape being created.
+*/
+void QgsRubberBand::addPoint(const QPoint & p)
+{
+  mPoints.last() = p; // Current mouse position becomes added point
+  mPoints.append(p); // Allocate new point to continue tracking current mouse position
+  update();
+}
+
+/*!
+  Update the line between the last added point and the mouse position.
+*/
+void QgsRubberBand::movePoint(const QPoint & p)
+{
+  mPoints.last() = p; // Update current mouse position
+  update();
+}
+
+/*!
+  Draw the shape in response to an update event.
+*/
+void QgsRubberBand::paintEvent(QPaintEvent * event)
+{
+  if (mPoints.count() > 1)
+  {
+    QPainter p(this);
+    p.setPen(Qt::lightGray);
+    p.setBrush(QBrush(Qt::lightGray, Qt::Dense6Pattern));
+    if (mIsPolygon)
+    {
+      p.drawPolygon(mPoints);
+    }
+    else
+    {
+      p.drawPolyline(mPoints);
+    }
+  }
+}
