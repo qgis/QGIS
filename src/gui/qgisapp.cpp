7a8787ae611bb30f7377c4f01a785e630420d704
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-02-11 06:12:18 +0000

    fix for activeLayer()
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@4826 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/qgisapp.cpp
+++ src/gui/qgisapp.cpp
@@ -3995,51 +3995,49 @@ void QgisApp::zoomToLayerExtent()
   // zoom only if one or more layers loaded
   if(QgsMapLayerRegistry::instance()->count() > 0)
   {
-    // get the selected item
-    QTreeWidgetItem *li = mMapLegend->currentItem();
-    QgsLegendLayerFile* llf = dynamic_cast<QgsLegendLayerFile*>(li);
-    if(llf)
-    {
-      QgsMapLayer *layer = llf->layer();
-      // Check if the layer extent has to be transformed to the map canvas
-      // coordinate system
-#ifdef QGISDEBUG
-      std::cout << "Layer extent is : " << (layer->extent()).stringRep().toLocal8Bit().data() << std::endl;
-#endif
-      if (QgsProject::instance()->readNumEntry("SpatialRefSys","/ProjectionsEnabled",0)!=0)
+    
+    QgsMapLayer *layer = mMapLegend->currentLayer();
+    if(layer)
       {
-        QgsCoordinateTransform *ct = layer->coordinateTransform();
-        try {
-          QgsRect transformedExtent = ct->transform(layer->extent());
-          mMapCanvas->setExtent(transformedExtent);
+	// Check if the layer extent has to be transformed to the map canvas
+	// coordinate system
 #ifdef QGISDEBUG
-          std::cout << "Canvas extent is : " << transformedExtent.stringRep().toLocal8Bit().data() << std::endl;
+	std::cout << "Layer extent is : " << (layer->extent()).stringRep().toLocal8Bit().data() << std::endl;
 #endif
-        }
-        catch(QgsCsException &cse)
-        {
+	if (QgsProject::instance()->readNumEntry("SpatialRefSys","/ProjectionsEnabled",0)!=0)
+	  {
+	    QgsCoordinateTransform *ct = layer->coordinateTransform();
+	    try {
+	      QgsRect transformedExtent = ct->transform(layer->extent());
+	      mMapCanvas->setExtent(transformedExtent);
 #ifdef QGISDEBUG
-          std::cout << "Caught transform error in zoomToLayerExtent(). "
-            << "Setting untransformed extents." << std::endl;
+	      std::cout << "Canvas extent is : " << transformedExtent.stringRep().toLocal8Bit().data() << std::endl;
+#endif
+	    }
+	    catch(QgsCsException &cse)
+	      {
+#ifdef QGISDEBUG
+		std::cout << "Caught transform error in zoomToLayerExtent(). "
+			  << "Setting untransformed extents." << std::endl;
 #endif	
-          mMapCanvas->setExtent(layer->extent());
-        }
-      }
-      else
-      {
-        mMapCanvas->setExtent(layer->extent());
-      }
-      mMapCanvas->clear();
+		mMapCanvas->setExtent(layer->extent());
+	      }
+	  }
+	else
+	  {
+	    mMapCanvas->setExtent(layer->extent());
+	  }
+	mMapCanvas->clear();
       // For Qt4, deprecate direct calling of render().  Let render() be called by the 
       // paint event loop of the map canvas widget.
       // XXX - this doesn't work -- or nobody implemented it so render() is still enabled
       // [gsherman]
-      mMapCanvas->render();
-      mMapCanvas->update();
+	mMapCanvas->render();
+	mMapCanvas->update();
 
-      // notify the project we've made a change
-      QgsProject::instance()->dirty(true);
-    }
+	// notify the project we've made a change
+	QgsProject::instance()->dirty(true);
+      }
   }
 } // QgisApp::zoomToLayerExtent()
 
@@ -4697,34 +4695,18 @@ void QgisApp::openURL(QString url, bool useQgisDocDirectory)
 /** Get a pointer to the currently selected map layer */
 QgsMapLayer *QgisApp::activeLayer()
 {
-  QTreeWidgetItem *lvi = mMapLegend->currentItem();
-  QgsMapLayer *layer = 0;
-  if (lvi)
-  {
-    QgsLegendLayerFile* llf = dynamic_cast<QgsLegendLayerFile*>(lvi);
-    if(llf)
-    {
-      layer = llf->layer();
-    }
-  }
-  return layer;
+  return (mMapLegend->currentLayer());
 }
 
 QString QgisApp::activeLayerSource()
 {
   QString source;
-  QTreeWidgetItem *lvi = mMapLegend->currentItem();
-  QgsMapLayer *layer = 0;
-  if (lvi)
-  {
-    QgsLegendLayerFile* llf = dynamic_cast<QgsLegendLayerFile*>(lvi);
-    if(llf)
+  QgsMapLayer* layer = mMapLegend->currentLayer();
+  if(layer)
     {
-      layer = llf->layer();
+      return (layer->source());
     }
-    source = layer->source();
-  }
-  return source;
+  return "";
 }
 
 /** Add a vector layer directly without prompting user for location
