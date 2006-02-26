d6a85c2c67ec846d90729b9200e9d55b6f59bc1b
morb_au <morb_au@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-02-26 01:35:08 +0000

    Speculative fix for the "Add WMS Layer" dialog crash if you press "Connect" on an empty server list - "Connect" should now be disabled if no servers are available.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@4908 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/qgsserversourceselect.cpp
+++ src/gui/qgsserversourceselect.cpp
@@ -55,7 +55,6 @@ QgsServerSourceSelect::QgsServerSourceSelect(QgisApp * app, QWidget * parent, Qt
   hbox->addStretch();
   btnGrpImageEncoding->setLayout(hbox);
 
-  btnAdd->setEnabled(false);
   populateConnectionList();
   // connect the double-click signal to the addSingleLayer slot in the parent
 
@@ -78,6 +77,21 @@ void QgsServerSourceSelect::populateConnectionList()
   {
     cmbConnections->insertItem(*it);
     ++it;
+  }
+
+  if (keys.begin() != keys.end())
+  {
+    // Connections available - enable various buttons
+    btnConnect->setEnabled(TRUE);
+    btnEdit->setEnabled(TRUE);
+    btnDelete->setEnabled(TRUE);
+  }
+  else
+  {
+    // No connections available - disable various buttons
+    btnConnect->setEnabled(FALSE);
+    btnEdit->setEnabled(FALSE);
+    btnDelete->setEnabled(FALSE);
   }
 }
 void QgsServerSourceSelect::on_btnNew_clicked()
