f8d614d57976618b02fcd219e195229d25978a07
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-03-09 16:32:54 +0000

    Minor change to the python test script
    git-svn-id: http://svn.osgeo.org/qgis/trunk@4989 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- tools/mapserver_export/test_export.py
+++ tools/mapserver_export/test_export.py
@@ -2,7 +2,7 @@
 
 # test script to export a sample QGIS project file to mapserver
 import ms_export
-ex = ms_export.Qgis2Map('./lakes.qgs', './lakes.map')
+ex = ms_export.Qgis2Map('./test1.qgs', './test1.map')
 ex.setOptions( 'Meters', 'PNG', 'TestMap', '800', '600', '','','template', 'header', 'footer')
 
 ex.writeMapFile()
