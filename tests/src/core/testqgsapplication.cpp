7b40c5576e2f700d7dcb5aa49c1f780aa05d6417
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-03 13:31:35 +0000

    Further additions to unit test framework. Disabled in build system now pending resolution of compilation issues.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5141 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tests/src/core/testqgsapplication.cpp
@@ -0,0 +1,29 @@
+#include <QtTest>
+#include <qgsapplication.h>
+#include "testqgsapplication.h"
+#include <QObject>
+#include <QString>
+
+
+TestQgsApplication::TestQgsApplication() : QObject()
+{
+
+}
+
+TestQgsApplication::~TestQgsApplication()
+{
+
+}
+
+void TestQgsApplication::authorsFilePath()
+{
+  QString myPath = QgsApplication::authorsFilePath();
+  QVERIFY(!myPath.isNull());
+}
+
+QTEST_MAIN(TestQgsApplication)
+#include "testqgsapplication.moc.cpp"
+
+
+
+
