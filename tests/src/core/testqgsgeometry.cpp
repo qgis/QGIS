1110aaf5763d0ce5ff8bf7c2f12618bb818d80ac
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-09 13:33:37 +0000

    Improvements to unit builder script - you can pass it the class nema to build the test for from the cli and it will not prompts you and just create everything - to be used in source dir parsing script that Ill add soon... dded new test unit for qgsgeometry
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5241 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tests/src/core/testqgsgeometry.cpp
@@ -0,0 +1,100 @@
+#include <QtTest>
+#include <QObject>
+#include <QString>
+#include <QObject>
+//header for class being tested
+#include <qgsgeometry.h>
+
+class TestQgsGeometry: public QObject
+{
+  Q_OBJECT;
+  private slots:
+        void QgsGeometryQgsGeometry()
+{
+
+};
+    void QgsGeometrysetWkbAndOwnership()
+{
+
+};
+    void QgsGeometrywkbBuffer()
+{
+
+};
+    void QgsGeometrywkbSize()
+{
+
+};
+    void QgsGeometrywkt()
+{
+
+};
+    void QgsGeometrysetGeos()
+{
+
+};
+    void QgsGeometryclosestVertex()
+{
+
+};
+    void QgsGeometryinsertVertexBefore()
+{
+
+};
+    void QgsGeometrymoveVertexAt()
+{
+
+};
+    void QgsGeometrydeleteVertexAt()
+{
+
+};
+    void QgsGeometryvertexAt()
+{
+
+};
+    void QgsGeometryclosestVertexWithContext()
+{
+
+};
+    void QgsGeometryclosestSegmentWithContext()
+{
+
+};
+    void QgsGeometryboundingBox()
+{
+
+};
+    void QgsGeometryintersects()
+{
+
+};
+    void QgsGeometryexportToWkt()
+{
+
+};
+    void QgsGeometrygeosGeometry()
+{
+
+};
+    void QgsGeometryexportWkbToGeos()
+{
+
+};
+    void QgsGeometryexportGeosToWkb()
+{
+
+};
+    void QgsGeometrydistanceSquaredPointToSegment()
+{
+
+};
+
+};
+
+QTEST_MAIN(TestQgsGeometry)
+#include "testqgsgeometry.moc.cpp"
+
+
+
+
