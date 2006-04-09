72ec5c0cbffd3ca9f6653797ee6f365af0f55613
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-09 01:13:15 +0000

    Added unit test for testqgsfeature.cpp (stubs only thus far)
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5237 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tests/src/core/testqgsfeature.cpp
@@ -0,0 +1,168 @@
+#include <QtTest>
+#include <QObject>
+#include <QString>
+#include <QObject>
+//header for class being tested
+#include <qgsfeature.h>
+
+class TestQgsFeature: public QObject
+{
+  Q_OBJECT;
+  private slots:
+        void QgsFeatureQgsFeature()
+{
+
+};
+    void QgsFeaturefeatureId()
+{
+
+};
+    void QgsFeatureattributeMap()
+{
+
+};
+    void QgsFeatureaddAttribute()
+{
+
+};
+    void QgsFeaturedeleteAttribute()
+{
+
+};
+    void QgsFeaturechangeAttributeValue()
+{
+
+};
+    void QgsFeaturechangeAttributeName()
+{
+
+};
+    void QgsFeaturefields()
+{
+
+};
+    void QgsFeaturegeometry()
+{
+
+};
+    void QgsFeaturegeometryAndOwnership()
+{
+
+};
+    void QgsFeaturegetGeometry()
+{
+
+};
+    void QgsFeaturegetCommittedGeometry()
+{
+
+};
+    void QgsFeaturegetModifiedGeometry()
+{
+
+};
+    void QgsFeaturegetGeometrySize()
+{
+
+};
+    void QgsFeaturegetCommittedGeometrySize()
+{
+
+};
+    void QgsFeaturegetModifiedGeometrySize()
+{
+
+};
+    void QgsFeaturewellKnownText()
+{
+
+};
+    void QgsFeaturesetFeatureId()
+{
+
+};
+    void QgsFeaturetypeName()
+{
+
+};
+    void QgsFeaturesetGeometry()
+{
+
+};
+    void QgsFeaturesetGeometryAndOwnership()
+{
+
+};
+    void QgsFeaturesetModifiedGeometry()
+{
+
+};
+    void QgsFeatureinsertVertexBefore()
+{
+
+};
+    void QgsFeaturemoveVertexAt()
+{
+
+};
+    void QgsFeaturevertexAt()
+{
+
+};
+    void QgsFeatureisValid()
+{
+
+};
+    void QgsFeaturesetValid()
+{
+
+};
+    void QgsFeatureisDirty()
+{
+
+};
+    void QgsFeatureresetDirty()
+{
+
+};
+    void QgsFeatureintersects()
+{
+
+};
+    void QgsFeatureexportToWKT()
+{
+
+};
+    void QgsFeatureclosestVertex()
+{
+
+};
+    void QgsFeaturedistanceSquaredPointToSegment()
+{
+
+};
+    void QgsFeatureclosestSegmentWithContext()
+{
+
+};
+    void QgsFeatureclosestSegment()
+{
+
+};
+    void QgsFeatureboundingBox()
+{
+
+};
+    void QgsFeaturegeosGeometry()
+{
+
+};
+
+};
+
+QTEST_MAIN(TestQgsFeature)
+#include "testqgsfeature.moc.cpp"
+
+
+
+
