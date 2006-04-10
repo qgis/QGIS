4f7a822005820836215b1d81a62b488114508377
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-10 22:43:36 +0000

    Added directory and beginnings of test framework for gui lib
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5247 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tests/src/gui/test_suite_builder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LIST=`ls ../../../src/gui/ |grep .cpp |grep ^qgs |grep -v ~$ |grep -v moc.cpp$ | sed 's/.cpp//g' |awk '$1=$1' RS= |sort`
+for FILE in $LIST
+do 
+  CLASSNAME=`grep -o "::Qgs[A-Za-z0-9]*(" ../../../src/gui/${FILE}.cpp |head -1 | sed 's/:://g'| sed 's/(//g'`
+  ./test_builder.pl $CLASSNAME
+  #svn add test${FILE}.cpp
+done
+make install
---
+++ tests/src/gui/testqgisapp.cpp
@@ -0,0 +1,484 @@
+#include <QtTest>
+#include <QObject>
+#include <QString>
+#include <QObject>
+//header for class being tested
+#include <qgisapp.h>
+
+class TestQgisApp: public QObject
+{
+  Q_OBJECT;
+  private slots:
+        void QgisApptr()
+{
+
+};
+    void QgisAppQgisApp()
+{
+
+};
+    void QgisAppreadSettings()
+{
+
+};
+    void QgisAppcreateActions()
+{
+
+};
+    void QgisAppcreateActionGroups()
+{
+
+};
+    void QgisAppcreateMenus()
+{
+
+};
+    void QgisAppcreateToolBars()
+{
+
+};
+    void QgisAppcreateStatusBar()
+{
+
+};
+    void QgisAppthemePath()
+{
+
+};
+    void QgisAppsetTheme()
+{
+
+};
+    void QgisAppsetupConnections()
+{
+
+};
+    void QgisAppcreateCanvas()
+{
+
+};
+    void QgisAppcreateOverview()
+{
+
+};
+    void QgisAppcreateLegend()
+{
+
+};
+    void QgisAppcreateDB()
+{
+
+};
+    void QgisAppupdateRecentProjectPaths()
+{
+
+};
+    void QgisAppsaveRecentProjectPath()
+{
+
+};
+    void QgisAppsaveWindowState()
+{
+
+};
+    void QgisApprestoreWindowState()
+{
+
+};
+    void QgisAppabout()
+{
+
+};
+    void QgisApprestoreSessionPlugins()
+{
+
+};
+    void QgisAppaddLayer()
+{
+
+};
+    void QgisAppisValidVectorFileName()
+{
+
+};
+    void QgisAppaddDatabaseLayer()
+{
+
+};
+    void QgisAppaddWmsLayer()
+{
+
+};
+    void QgisAppfileExit()
+{
+
+};
+    void QgisAppfileNew()
+{
+
+};
+    void QgisAppnewVectorLayer()
+{
+
+};
+    void QgisAppfileOpen()
+{
+
+};
+    void QgisAppaddProject()
+{
+
+};
+    void QgisAppfileSave()
+{
+
+};
+    void QgisAppfileSaveAs()
+{
+
+};
+    void QgisAppopenProject()
+{
+
+};
+    void QgisAppopenLayer()
+{
+
+};
+    void QgisAppfilePrint()
+{
+
+};
+    void QgisAppsaveMapAsImage()
+{
+
+};
+    void QgisAppaddAllToOverview()
+{
+
+};
+    void QgisAppremoveAllFromOverview()
+{
+
+};
+    void QgisApphideAllLayers()
+{
+
+};
+    void QgisAppshowAllLayers()
+{
+
+};
+    void QgisAppexportMapServer()
+{
+
+};
+    void QgisAppzoomIn()
+{
+
+};
+    void QgisAppzoomOut()
+{
+
+};
+    void QgisAppzoomToSelected()
+{
+
+};
+    void QgisApppan()
+{
+
+};
+    void QgisAppzoomFull()
+{
+
+};
+    void QgisAppzoomPrevious()
+{
+
+};
+    void QgisAppidentify()
+{
+
+};
+    void QgisAppmeasure()
+{
+
+};
+    void QgisAppmeasureArea()
+{
+
+};
+    void QgisAppattributeTable()
+{
+
+};
+    void QgisAppdeleteSelected()
+{
+
+};
+    void QgisAppcapturePoint()
+{
+
+};
+    void QgisAppcaptureLine()
+{
+
+};
+    void QgisAppcapturePolygon()
+{
+
+};
+    void QgisAppselect()
+{
+
+};
+    void QgisAppaddVertex()
+{
+
+};
+    void QgisAppmoveVertex()
+{
+
+};
+    void QgisAppdeleteVertex()
+{
+
+};
+    void QgisAppeditCut()
+{
+
+};
+    void QgisAppeditCopy()
+{
+
+};
+    void QgisAppeditPaste()
+{
+
+};
+    void QgisApppasteTransformations()
+{
+
+};
+    void QgisApprefreshMapCanvas()
+{
+
+};
+    void QgisAppshowMouseCoordinate()
+{
+
+};
+    void QgisAppshowScale()
+{
+
+};
+    void QgisApptestButton()
+{
+
+};
+    void QgisAppinOverview()
+{
+
+};
+    void QgisAppremoveLayer()
+{
+
+};
+    void QgisAppremoveAllLayers()
+{
+
+};
+    void QgisAppzoomToLayerExtent()
+{
+
+};
+    void QgisAppgetInterface()
+{
+
+};
+    void QgisAppshowPluginManager()
+{
+
+};
+    void QgisApploadPlugin()
+{
+
+};
+    void QgisApptestMapLayerPlugins()
+{
+
+};
+    void QgisApptestPluginFunctions()
+{
+
+};
+    void QgisAppcheckQgisVersion()
+{
+
+};
+    void QgisAppsocketConnected()
+{
+
+};
+    void QgisAppsocketConnectionClosed()
+{
+
+};
+    void QgisAppsocketError()
+{
+
+};
+    void QgisAppsocketReadyRead()
+{
+
+};
+    void QgisAppoptions()
+{
+
+};
+    void QgisApphelpContents()
+{
+
+};
+    void QgisApphelpQgisHomePage()
+{
+
+};
+    void QgisApphelpQgisSourceForge()
+{
+
+};
+    void QgisAppopenURL()
+{
+
+};
+    void QgisAppactiveLayer()
+{
+
+};
+    void QgisAppactiveLayerSource()
+{
+
+};
+    void QgisAppaddVectorLayer()
+{
+
+};
+    void QgisAppaddMapLayer()
+{
+
+};
+    void QgisAppsetExtent()
+{
+
+};
+    void QgisAppsaveDirty()
+{
+
+};
+    void QgisAppwhatsThis()
+{
+
+};
+    void QgisAppmenuMapByName()
+{
+
+};
+    void QgisAppmenuMapById()
+{
+
+};
+    void QgisApppopulateMenuMaps()
+{
+
+};
+    void QgisAppaddPluginMenu()
+{
+
+};
+    void QgisAppgetPluginMenu()
+{
+
+};
+    void QgisAppremovePluginMenuItem()
+{
+
+};
+    void QgisAppremovePluginToolBarIcon()
+{
+
+};
+    void QgisAppprojectionsEnabled()
+{
+
+};
+    void QgisAppshowProgress()
+{
+
+};
+    void QgisAppshowExtents()
+{
+
+};
+    void QgisAppupdateMouseCoordinatePrecision()
+{
+
+};
+    void QgisAppshowStatusMessage()
+{
+
+};
+    void QgisAppprojectPropertiesProjections()
+{
+
+};
+    void QgisAppprojectProperties()
+{
+
+};
+    void QgisAppgetLayerRegistry()
+{
+
+};
+    void QgisAppclipboard()
+{
+
+};
+    void QgisAppactivateDeactivateLayerRelatedActions()
+{
+
+};
+    void QgisAppshowCapturePointCoordinate()
+{
+
+};
+    void QgisAppaddRasterLayer()
+{
+
+};
+    void QgisAppdebugHook()
+{
+
+};
+    void QgisAppcustomProjection()
+{
+
+};
+    void QgisAppshowBookmarks()
+{
+
+};
+    void QgisAppnewBookmark()
+{
+
+};
+
+};
+
+QTEST_MAIN(TestQgisApp)
+#include "testqgisapp.moc.cpp"
+
+
+
+
