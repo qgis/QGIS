2f61769fc03ea4d7891f44a21ab809693408dd64
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-11 09:10:35 +0000

    Initial framwork for raster tests
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5254 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tests/src/raster/testqgsrasterlayer.cpp
@@ -0,0 +1,280 @@
+#include <QtTest>
+#include <QObject>
+#include <QString>
+#include <QObject>
+//header for class being tested
+#include <qgsrasterlayer.h>
+
+class TestQgsRasterLayer: public QObject
+{
+  Q_OBJECT;
+  private slots:
+        void QgsRasterLayerbuildSupportedRasterFileFilter()
+{
+
+};
+    void QgsRasterLayerisSupportedRasterDriver()
+{
+
+};
+    void QgsRasterLayerisValidRasterFileName()
+{
+
+};
+    void QgsRasterLayerQgsRasterLayer()
+{
+
+};
+    void QgsRasterLayerreadFile()
+{
+
+};
+    void QgsRasterLayergetProjectionWKT()
+{
+
+};
+    void QgsRasterLayercloseDataset()
+{
+
+};
+    void QgsRasterLayerupdate()
+{
+
+};
+    void QgsRasterLayershowLayerProperties()
+{
+
+};
+    void QgsRasterLayertriggerRepaint()
+{
+
+};
+    void QgsRasterLayergetDrawingStyleAsQString()
+{
+
+};
+    void QgsRasterLayersetDrawingStyle()
+{
+
+};
+    void QgsRasterLayerhasBand()
+{
+
+};
+    void QgsRasterLayerdrawThumbnail()
+{
+
+};
+    void QgsRasterLayergetPaletteAsPixmap()
+{
+
+};
+    void QgsRasterLayerdraw()
+{
+
+};
+    void QgsRasterLayerdrawSingleBandGray()
+{
+
+};
+    void QgsRasterLayerdrawSingleBandPseudoColor()
+{
+
+};
+    void QgsRasterLayerdrawPalettedSingleBandColor()
+{
+
+};
+    void QgsRasterLayerdrawPalettedSingleBandGray()
+{
+
+};
+    void QgsRasterLayerdrawPalettedSingleBandPseudoColor()
+{
+
+};
+    void QgsRasterLayerdrawPalettedMultiBandColor()
+{
+
+};
+    void QgsRasterLayerdrawMultiBandSingleBandGray()
+{
+
+};
+    void QgsRasterLayerdrawMultiBandSingleBandPseudoColor()
+{
+
+};
+    void QgsRasterLayerdrawMultiBandColor()
+{
+
+};
+    void QgsRasterLayerfilterLayer()
+{
+
+};
+    void QgsRasterLayershowDebugOverlay()
+{
+
+};
+    void QgsRasterLayergetRasterBandStats()
+{
+
+};
+    void QgsRasterLayergetRasterBandNumber()
+{
+
+};
+    void QgsRasterLayergetRasterBandName()
+{
+
+};
+    void QgsRasterLayerhasStats()
+{
+
+};
+    void QgsRasterLayergetRasterBandStats()
+{
+
+};
+    void QgsRasterLayersetRedBandName()
+{
+
+};
+    void QgsRasterLayersetGreenBandName()
+{
+
+};
+    void QgsRasterLayersetBlueBandName()
+{
+
+};
+    void QgsRasterLayersetGrayBandName()
+{
+
+};
+    void QgsRasterLayergetLegendQPixmap()
+{
+
+};
+    void QgsRasterLayergetDetailedLegendQPixmap()
+{
+
+};
+    void QgsRasterLayersubLayers()
+{
+
+};
+    void QgsRasterLayersetLayerOrder()
+{
+
+};
+    void QgsRasterLayersetSubLayerVisibility()
+{
+
+};
+    void QgsRasterLayerupdateProgress()
+{
+
+};
+    void QgsRasterLayerpopupTransparencySliderMoved()
+{
+
+};
+    void QgsRasterLayersetTransparency()
+{
+
+};
+    void QgsRasterLayergetTransparency()
+{
+
+};
+    void QgsRasterLayergetMetadata()
+{
+
+};
+    void QgsRasterLayerbuildPyramids()
+{
+
+};
+    void QgsRasterLayerbuildRasterPyramidList()
+{
+
+};
+    void QgsRasterLayerisEditable()
+{
+
+};
+    void QgsRasterLayerreadColorTable()
+{
+
+};
+    void QgsRasterLayerwriteXML()
+{
+
+};
+    void QgsRasterLayerinOverview()
+{
+
+};
+    void QgsRasterLayeridentify()
+{
+
+};
+    void QgsRasterLayeridentifyAsHtml()
+{
+
+};
+    void QgsRasterLayerpopulateHistogram()
+{
+
+};
+    void QgsRasterLayerQgsRasterLayer()
+{
+
+};
+    void QgsRasterLayersetDataProvider()
+{
+
+};
+    void QgsRasterLayerusesProvider()
+{
+
+};
+    void QgsRasterLayerproviderKey()
+{
+
+};
+    void QgsRasterLayershowStatusMessage()
+{
+
+};
+    void QgsRasterLayerlayerTypeIconPath()
+{
+
+};
+    void QgsRasterLayerrefreshLegend()
+{
+
+};
+    void QgsRasterLayererrorCaptionString()
+{
+
+};
+    void QgsRasterLayererrorString()
+{
+
+};
+    void QgsRasterLayergetDataProvider()
+{
+
+};
+
+};
+
+QTEST_MAIN(TestQgsRasterLayer)
+#include "testqgsrasterlayer.moc.cpp"
+
+
+
+
