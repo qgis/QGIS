4fabdaadc2a9f015b67a4656fda8c3740d5414a3
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-16 01:57:40 +0000

    Preliminary support for static builds. Does not work for me yet due to no static sqlite libe being found. Previous invocation mechanism should still work fine
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5288 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- build.sh
+++ build.sh
@@ -22,10 +22,29 @@ then
    AUTOGEN_FLAGS="--enable-debug"
    export CFLAGS="-g -Wall"
    export CXXFLAGS="-g -Wall"
+ elif [ $2 = static ]
+ then
+   #this is to tell linker to statically linke
+   #against deps like gdal etc - useful for
+   #trying to build a distributeable binary of qgis
+   export LDFLAGS=-static
  fi
   
- export QTDIR=/usr/local/Trolltech/Qt-4.1.0
+ #qt installed from source
+ #export QTDIR=/usr/local/Trolltech/Qt-4.1.0
+ #qt installed from debian apt
+ export QTDIR=/usr
  export PATH=$QTDIR/bin:$PATH
  export LD_LIBRARY_PATH=$QTDIR/lib
- ./autogen.sh $AUTOGEN_FLAGS --prefix=${1} --with-qtdir=$QTDIR --with-grass=/usr/lib/grass && make && make install
+
+ # Note: --enable-static=no tells compiler 
+ # 'dont build static versions of qgis .o files'
+ # This only applies to qgis interal libs and speeds 
+ # up the compilation process. See discussion on 
+ # http://logs.qgis.org/slogs/%23qgis.2006-04-15.log at 17:06:10
+ # for additional details
+ ./autogen.sh $AUTOGEN_FLAGS --prefix=${1} \
+                             --enable-static=no \
+                             --with-qtdir=$QTDIR \
+                             --with-grass=/usr/lib/grass && make && make install
 
