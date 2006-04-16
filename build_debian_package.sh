8334b2740df3afd066cf97a70af0e24027396bcb
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-04-16 21:16:53 +0000

    A script to automate building of the debian package
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@5291 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ build_debian_package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+if [ -d `pwd`/src ]
+then
+  #src exists so we are prolly in the right dir
+  echo "good we are in  qgis checkout dir!"
+else
+  echo "You must run this from the top level qgis checkout dir!"
+  exit 1
+fi
+
+if [ -d `pwd`/debian ]
+then
+  cd debian
+  svn update
+  cd ..
+else
+  svn co https://svn.qgis.org/repos/qgis/trunk/debian
+fi
+
+export DEBFULLNAME="Tim Sutton"
+export DEBEMAIL=tim@linfiniti.com
+dch -v 0.7.9+svn`date +%d%M%Y`
+fakeroot dpkg-buildpackage
