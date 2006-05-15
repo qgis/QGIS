38336d05f0cd249ab67cbaeb456690aaf320da32
g_j_m <g_j_m@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-05-15 09:34:05 +0000

    Tidy up a few debugging output statements
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@5457 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -57,8 +57,6 @@
 #define QGISEXTERN extern "C"
 #endif
 
-#define QGISDEBUG
-
 const QString POSTGRES_KEY = "postgres";
 const QString POSTGRES_DESCRIPTION = "PostgreSQL/PostGIS data provider";
 
@@ -74,7 +72,6 @@ QgsPostgresProvider::QgsPostgresProvider(QString const & uri)
   // For postgres, the connection information is passed as a space delimited
   // string:
   //  host=192.168.1.5 dbname=test port=5342 user=gsherman password=xxx table=tablename
-  std::cout << "Data source uri is " << uri.toLocal8Bit().data() << std::endl;
 
   // Strip the table and sql statement name off and store them
   int sqlStart = uri.find(" sql");
