443e85bfb659cd1978fd0c0374952ddf7692acf6
g_j_m <g_j_m@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-07-20 09:34:04 +0000

    Use icon overlays for the editable and overview status of a layer instead of separate icons in separate columns. Still only applies to the actual layer file entry in the legend, but removes the need for the extra columns. However, I'm hopeless at icon drawing... These changes are a variation on the suggestions in ticket #191.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5616 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/legend/qgslegend.cpp
+++ src/legend/qgslegend.cpp
@@ -69,22 +69,8 @@ QgsLegend::QgsLegend(QgisApp* app, QWidget * parent, const char *name)
   setFont(f);
   setBackgroundColor(QColor(192, 192, 192));
 
-  setColumnCount(3);  // main column, overview indicator and editing indicator
-
-  header()->setDefaultAlignment(Qt::AlignLeft);
-
-  QStringList myList;
-  myList += tr("Layer Name");
-  myList += tr("Overview");
-  myList += tr("Editing");
-
-  setHeaderLabels(myList);
-  header()->resizeSection(0, 100);
-  header()->resizeSection(1, 24);  // Enough to fit an overview action icon
-  header()->resizeSection(2, 17);  // Enough to fit an editing icon
-
-  //added by Tim to hide the header - header is unneccessary
-  // header()->setHidden(1);  // morb_au - experiment
+  setColumnCount(1);
+  header()->setHidden(1);
   setRootIsDecorated(true);
 
 }
