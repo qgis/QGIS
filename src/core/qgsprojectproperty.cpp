80821514d682e3fe0c243c6e92c0152122023fb6
g_j_m <g_j_m@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-08-10 10:51:04 +0000

    Applied patches r5669, r5668, r5667, r5666, as applied to the lib-refactoring branch, to head. These were signed/unsigned int mods and boolean comparison mismatches. Many of the changes in these commits had already been done in head
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5681 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/qgsprojectproperty.cpp
+++ src/core/qgsprojectproperty.cpp
@@ -112,7 +112,7 @@ bool QgsPropertyValue::readXML(QDomNode & keyNode)
 
         case QVariant::StringList:
         {
-            size_t i = 0;
+            int i = 0;
             QDomNodeList values = keyNode.childNodes();
 
             // all the QStringList values will be inside <value> elements
@@ -296,7 +296,7 @@ bool QgsPropertyValue::readXML(QDomNode & keyNode)
               break;
             */
         default :
-            qDebug( "%s:%d unsupported value type %s .. not propertly translated to QVariant in qgsproject.cpp:%d",
+            qDebug( "%s:%d unsupported value type %s .. not propertly translated to QVariant in qgsproject.cpp:",
                     __FILE__, __LINE__, (const char*)typeString.utf8() );
     }
 
@@ -363,7 +363,7 @@ QVariant QgsPropertyKey::value() const
 {
     QgsProperty * foundQgsProperty;
 
-    if ( foundQgsProperty = properties_.find( name()) )
+    if ( 0 == ( foundQgsProperty = properties_.find( name()) ) )
     {                        // recurse down to next key
         return foundQgsProperty->value();
     } else
@@ -441,7 +441,7 @@ void QgsPropertyKey::dump( size_t tabs ) const
 
 bool QgsPropertyKey::readXML(QDomNode & keyNode)
 {
-    size_t i = 0;
+    int i = 0;
     QDomNodeList subkeys = keyNode.childNodes();
 
     while (i < subkeys.count())
