3e87973a1d71bc67947143f4f711100526087f7a
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-10-07 22:00:35 +0000

    I18n on shapefileName
    git-svn-id: http://svn.osgeo.org/qgis/trunk@5927 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/providers/mysql/qgsmysqlprovider.cpp
+++ src/providers/mysql/qgsmysqlprovider.cpp
@@ -843,7 +843,7 @@ bool QgsMySQLProvider::saveAsShapefile()
       }
       OGRDataSource *poDS;
       // create the data source
-      poDS = poDriver->CreateDataSource((const char *) shapefileName, NULL);
+      poDS = poDriver->CreateDataSource((const char *) shapefileName.toLocal8Bit(), NULL);
       if (poDS != NULL)
       {
         QTextCodec* saveCodec = QTextCodec::codecForName(enc.toLocal8Bit().data());
