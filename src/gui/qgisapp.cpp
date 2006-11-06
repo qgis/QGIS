23edbb60a183486aa9e9b01e1d5b5498003964d9
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2006-11-06 16:08:30 +0000

    commited some test code in qgisapp.cpp in last commit
    git-svn-id: http://svn.osgeo.org/qgis/trunk@6056 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/qgisapp.cpp
+++ src/gui/qgisapp.cpp
@@ -1820,7 +1820,7 @@ bool QgisApp::addLayer(QStringList const &theLayerQStringList, const QString& en
 
     // create the layer
 
-    QgsVectorLayer *layer = new QgsVectorLayer(*it, base, "GML");
+    QgsVectorLayer *layer = new QgsVectorLayer(*it, base, "ogr");
     Q_CHECK_PTR( layer );
     // set the visibility based on user preference for newly added
     // layers
