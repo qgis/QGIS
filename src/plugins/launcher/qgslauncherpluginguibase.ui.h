fdcb0bdfa5b0ae0c2f743b1447aff24743867adb
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-03-15 02:57:07 +0000

    Launcher plugin added to core plugins to boost gsherman's karma
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_8_0@6802 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/plugins/launcher/qgslauncherpluginguibase.ui.h
@@ -0,0 +1,28 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename functions or slots use
+** Qt Designer which will update this file, preserving your code. Create an
+** init() function in place of a constructor, and a destroy() function in
+** place of a destructor.
+*****************************************************************************/
+
+
+
+
+void QgsLauncherPluginGuiBase::cleanUp()
+{
+
+}
+
+
+void QgsLauncherPluginGuiBase::chooseProgram()
+{
+
+}
+
+
+void QgsLauncherPluginGuiBase::runProgram()
+{
+
+}
