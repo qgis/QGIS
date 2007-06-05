3dbd11b2c8a4afc0732e62ef09e3758619669b65
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-06-05 17:04:40 +0000

    Insert version number from capabilities document into GetMap request
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@6982 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/providers/wms/qgswmsprovider.cpp
+++ src/providers/wms/qgswmsprovider.cpp
@@ -445,7 +445,7 @@ QImage* QgsWmsProvider::draw(QgsRect  const & viewExtent, int pixelWidth, int pi
 
   url += "SERVICE=WMS";
   url += "&";
-  url += "VERSION=1.1.1";
+  url += "VERSION=" + mCapabilities.version;
   url += "&";
   url += "REQUEST=GetMap";
   url += "&";
@@ -474,7 +474,7 @@ QImage* QgsWmsProvider::draw(QgsRect  const & viewExtent, int pixelWidth, int pi
   mGetFeatureInfoUrlBase = baseUrl;
   mGetFeatureInfoUrlBase += "SERVICE=WMS";
   mGetFeatureInfoUrlBase += "&";
-  mGetFeatureInfoUrlBase += "VERSION=1.1.1";
+  mGetFeatureInfoUrlBase += "VERSION=" + mCapabilities.version;
   mGetFeatureInfoUrlBase += "&";
   mGetFeatureInfoUrlBase += "REQUEST=GetFeatureInfo";
   mGetFeatureInfoUrlBase += "&";
