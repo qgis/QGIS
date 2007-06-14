798b42ebb02a6176314d9902929b3d75df01a23b
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-06-14 01:34:08 +0000

    Set qgis release name in main.cpp Set qgis version in configure for those using auto*
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-0_8_0@7031 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -65,7 +65,7 @@ static const char * const ident_ = "$Id$";
  */
 void usage( std::string const & appName )
 {
-  std::cerr << "Quantum GIS - " << VERSION << " 'Simon' (" 
+  std::cerr << "Quantum GIS - " << VERSION << " 'Titan' (" 
             << QGSSVNVERSION << ")\n" 
       << "Quantum GIS (QGIS) is a viewer for spatial data sets, including\n" 
       << "raster and vector data.\n"  
