2f62deb4f4162cb8c941fc2030280070adbe3b1c
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-09-16 20:34:54 +0000

    Updated/added copyright statements as part of provenance review for OSGeo incubation. No code changes.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@7205 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/providers/ogr/qgsogrfactory.h
+++ src/providers/ogr/qgsogrfactory.h
@@ -1,14 +1,18 @@
-//
-// C++ Interface: qgsogrfactory
-//
-// Description: 
-//
-//
-// Author: Christoph Spoerri <spoerri@sourceforge.net>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+     qgsogrfactory.h
+     --------------------------------------
+    Date                 : Sun Sep 16 12:18:58 AKDT 2007
+    Copyright            : (C) 2004 Christoph Spoerri 
+    Email                : <spoerri@sourceforge.net>
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSOGRFACTORY_H
 #define QGSOGRFACTORY_H
 
--- tests/src/gui/testqgspluginmanager.cpp
+++ tests/src/gui/testqgspluginmanager.cpp
@@ -1,3 +1,17 @@
+/***************************************************************************
+     testqgspluginmanager.cpp
+     --------------------------------------
+    Date                 : Sun Sep 16 12:30:05 AKDT 2007
+    Copyright            : (C) 2007 by Gary E. Sherman
+    Email                : sherman at mrcc dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include <QtTest>
 #include <QObject>
 #include <QString>
