a24a811653244597a2d6fac7fa4ae8874fa0fa82
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-09-16 02:54:22 +0000

    Updated/added copyright statements as part of provenance review for OSGeo incubation. No code changes.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@7203 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/renderer/qgsrenderer.cpp
+++ src/core/renderer/qgsrenderer.cpp
@@ -1,4 +1,20 @@
+/***************************************************************************
+                           qgsrenderer.cpp 
+                       
+                             -------------------
+	   begin                : Sat Jan 4 2003
+	   copyright            : (C) 2003 by Gary E.Sherman
+	   email                : sherman at mrcc.com
+***************************************************************************/
 
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "qgsrenderer.h"
 
 #include <QColor>
