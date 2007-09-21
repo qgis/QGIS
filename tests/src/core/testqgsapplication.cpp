3f5d5247e16f1155cadecbce3a11d256bbc9b647
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-09-21 19:02:56 +0000

    Tidy up to qgis unit tests:  - install then in same dir as qgis  - prefix installed tests with qgis_
    Added test to make sure theme exists
    
    
    git-svn-id: http://svn.osgeo.org/qgis/trunk@7216 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- tests/src/core/testqgsapplication.cpp
+++ tests/src/core/testqgsapplication.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
-     testqgsapplication.cpp
-     --------------------------------------
-    Date                 : Sun Sep 16 12:22:49 AKDT 2007
-    Copyright            : (C) 2007 by Gary E. Sherman
-    Email                : sherman at mrcc dot com
+  testqgsapplication.cpp
+  --------------------------------------
+Date                 : Sun Sep 16 12:22:49 AKDT 2007
+Copyright            : (C) 2007 by Gary E. Sherman
+Email                : sherman at mrcc dot com
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -13,21 +13,11 @@
  *                                                                         *
  ***************************************************************************/
 #include <QtTest>
-#include <QObject>
-#include <QString>
-#include <QStringList>
-#include <QObject>
 #include <iostream>
 
-#include <QApplication>
-#include <QImage>
-#include <QPainter>
+#include <QPixmap>
 
 #include <qgsapplication.h>
-#include <qgsmaplayerregistry.h>
-#include <qgsmaprender.h>
-#include <qgsspatialrefsys.h>
-#include <qgsvectorlayer.h>
 
 //header for class being tested
 #include <qgsapplication.h>
@@ -36,127 +26,40 @@ class TestQgsApplication: public QObject
 {
   Q_OBJECT;
   private slots:
-        void QgsApplicationQgsApplication()
-        {
-          // init QGIS's paths - true means that all path will be inited from prefix
-          QString qgisPath = QCoreApplication::applicationDirPath ();
-          QgsApplication::setPrefixPath(qgisPath, TRUE);
+    void getPaths();
+    void checkTheme();
+};
 
-          std::cout << "Prefix  PATH: " << QgsApplication::prefixPath().toLocal8Bit().data() << std::endl;
-          std::cout << "Plugin  PATH: " << QgsApplication::pluginPath().toLocal8Bit().data() << std::endl;
-          std::cout << "PkgData PATH: " << QgsApplication::pkgDataPath().toLocal8Bit().data() << std::endl;
-          std::cout << "User DB PATH: " << QgsApplication::qgisUserDbFilePath().toLocal8Bit().data() << std::endl;
-
-          QgsApplication::initQgis();
-/*          QStringList myLayerList;
-          myLayerList << "../../../points.shp";
-          QStringList::Iterator myIterator = myLayerList.begin();
-          while( myIterator!= myLayerList.end() )
-          {
-            QString myLayerName=*myIterator;
-            QgsVectorLayer* layer = new QgsVectorLayer(myLayerName, myLayerName, "ogr");
-            if (layer && layer->isValid())
-            {
-              std::cout << "Added layer is valid " << std::endl;
-            }
-            // something's wrong
-            else if (layer)
-            {
-              //test must be failed here!
-              delete layer;
-            }
-            //Register this layer with the layers registry
-            QgsMapLayerRegistry::instance()->addMapLayer(layer);
-            ++myIterator;
-
-          }
-
-
-          // create image and painter for it
-          QImage img(QSize(800,600), QImage::Format_ARGB32_Premultiplied);
-          QColor bgColor(255,255,255);
-          img.fill(bgColor.rgb());
-          QPainter p;
-          p.begin(&img);
-          p.setRenderHint(QPainter::Antialiasing); // use antialiasing!
-
-          QgsMapRender mapRender;
-
-          // set layers
-          mapRender.setLayerSet(myLayerList);
-
-          // use projection
-          mapRender.setProjectionsEnabled(TRUE);
-          QgsSpatialRefSys srs(32633); // postgis srid: wgs84 / utm zone 33N
-          mapRender.setDestinationSrs(srs);
-
-          // set extent
-          QgsRect r = mapRender.fullExtent();
-          r.scale(1.1); // make rectangle a bit bigger
-          mapRender.setExtent(r);
-
-          // set output size
-          mapRender.setOutputSize(img.size(), img.logicalDpiX());
-
-          // render!
-          mapRender.render(&p);
-
-          // painting done
-          p.end();
-
-          // delete layers
-          QgsMapLayerRegistry::instance()->removeAllMapLayers();
-
-          // save image
-          img.save("render.png","png");*/
-          QgsApplication::exitQgis();
-        };
-    void QgsApplicationselectTheme()
+void TestQgsApplication::getPaths()
 {
+  // init QGIS's paths - true means that all path will be inited from prefix
+  QString qgisPath = QCoreApplication::applicationDirPath () + "/../";
+  QgsApplication::setPrefixPath(qgisPath, TRUE);
+
+  std::cout << "Prefix  PATH: " << QgsApplication::prefixPath().toLocal8Bit().data() << std::endl;
+  std::cout << "Plugin  PATH: " << QgsApplication::pluginPath().toLocal8Bit().data() << std::endl;
+  std::cout << "PkgData PATH: " << QgsApplication::pkgDataPath().toLocal8Bit().data() << std::endl;
+  std::cout << "User DB PATH: " << QgsApplication::qgisUserDbFilePath().toLocal8Bit().data() << std::endl;
 
 };
-    void QgsApplicationauthorsFilePath()
+
+void TestQgsApplication::checkTheme()
 {
-
-};
-    void QgsApplicationdeveloperPath()
-{
-
-};
-    void QgsApplicationhelpAppPath()
-{
-
-};
-    void QgsApplicationi18nPath()
-{
-
-};
-    void QgsApplicationqgisMasterDbFilePath()
-{
-
-};
-    void QgsApplicationqgisSettingsDirPath()
-{
-
-};
-    void QgsApplicationqgisUserDbFilePath()
-{
-
-};
-    void QgsApplicationsplashPath()
-{
-
-};
-    void QgsApplicationsrsDbFilePath()
-{
-
-};
-    void QgsApplicationsvgPath()
-{
+  QString qgisPath = QCoreApplication::applicationDirPath () + "/../";
+  QgsApplication::setPrefixPath(qgisPath, TRUE);
+  std::cout << "Prefix  PATH: " << QgsApplication::prefixPath().toLocal8Bit().data() << std::endl;
+  std::cout << "Plugin  PATH: " << QgsApplication::pluginPath().toLocal8Bit().data() << std::endl;
+  std::cout << "PkgData PATH: " << QgsApplication::pkgDataPath().toLocal8Bit().data() << std::endl;
+  std::cout << "User DB PATH: " << QgsApplication::qgisUserDbFilePath().toLocal8Bit().data() << std::endl;
+  QString myIconPath = QgsApplication::themePath();
+  QPixmap myPixmap;
+  myPixmap.load(myIconPath+"/mIconProjectionDisabled.png");
+  qDebug("Checking if a theme icon exists:");
+  qDebug(myIconPath.toLocal8Bit()+"/mIconProjectionDisabled.png");
+  QVERIFY(!myPixmap.isNull());
 
 };
 
-};
 
 QTEST_MAIN(TestQgsApplication)
 #include "moc_testqgsapplication.cxx"
