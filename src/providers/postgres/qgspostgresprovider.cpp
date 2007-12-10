a12d7a9baaccead470f7adb9d301fc02745191c5
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-12-10 12:44:23 +0000

    fix for #860
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@7756 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -137,6 +137,9 @@ const QString POSTGRES_DESCRIPTION = "PostgreSQL/PostGIS data provider";
   }
   PQclear(schema);
 
+  if(mSchemaName=="")
+    mSchemaName=mCurrentSchema;
+
   if (!getGeometryDetails()) // gets srid and geometry type
   {
     // the table is not a geometry table
@@ -2515,7 +2518,7 @@ bool QgsPostgresProvider::getGeometryDetails()
 
   PGresult *result = executeDbCommand(connection, sql);
 
-  QgsDebugMsg("geometry column query returned " + QString(PQntuples(result)));
+  QgsDebugMsg("geometry column query returned " + QString::number(PQntuples(result)));
   QgsDebugMsg("column number of srid is " + QString::number(PQfnumber(result, "srid")));
 
   if (PQntuples(result) > 0)
