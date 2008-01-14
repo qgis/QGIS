0b22e534e0d8e8014e7840ea04dde6804b929802
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-01-14 15:12:11 +0000

    Upgrade point size when reading old project to make it look the same. Also change default point size to same loko as previously
    git-svn-id: http://svn.osgeo.org/qgis/trunk@7962 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/symbology/qgssymbol.cpp
+++ src/core/symbology/qgssymbol.cpp
@@ -40,7 +40,7 @@ QgsSymbol::QgsSymbol(QGis::VectorType t, QString lvalue, QString uvalue, QString
       mLabel(label),
       mType(t),
       mPointSymbolName( "hard:circle" ),
-      mPointSize( 11 ),
+      mPointSize( 10 ),
       mPointSymbolImage(1,1, QImage::Format_ARGB32_Premultiplied),
       mWidthScale(1.0),
       mCacheUpToDate( false ),
