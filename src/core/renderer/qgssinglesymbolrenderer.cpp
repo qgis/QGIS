b58e7a7f57d571d4b06d6d4ec10fea4ba3bda4f2
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-01-17 19:45:43 +0000

    Let single symbol render mark borders of areas when selected, just as the other renderers
    git-svn-id: http://svn.osgeo.org/qgis/trunk@7998 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/renderer/qgssinglesymbolrenderer.cpp
+++ src/core/renderer/qgssinglesymbolrenderer.cpp
@@ -132,8 +132,9 @@ void QgsSingleSymbolRenderer::renderFeature(QPainter * p, QgsFeature & f, QImage
     {
       QPen pen=mSymbol->pen();
       pen.setWidthF ( widthScale * pen.width() );
-      if (mVectorType == QGis::Line)
-	pen.setColor(mSelectionColor);
+      // We set pen color in case it is an area with no brush (transparent).
+      // Previously, this was only done for lines. Why?
+      pen.setColor(mSelectionColor);
       QBrush brush=mSymbol->brush();
       brush.setColor(mSelectionColor);
       p->setPen(pen);
