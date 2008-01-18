d49ca2038d8c66f1d93c5d2a06268ff7edd1d228
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-01-18 21:01:07 +0000

    More translation clean-ups
    git-svn-id: http://svn.osgeo.org/qgis/trunk@8002 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/qgslayerprojectionselector.cpp
+++ src/gui/qgslayerprojectionselector.cpp
@@ -28,6 +28,16 @@ QgsLayerProjectionSelector::QgsLayerProjectionSelector(QWidget *parent,
   : QDialog(parent, fl)
 {
   setupUi(this);
+  
+  // Set up text edit pane
+  QString format("<h2>%1</h2>%2 %3");
+  QString header = tr("Define this layer's projection:");
+  QString sentence1 = tr("This layer appears to have no projection specification.");
+  QString sentence2 = tr("By default, this layer will now have its projection set to that of the project"
+			 ", but you may override this by selecting a different projection below.");
+  textEdit->setHtml(format.arg(header).arg(sentence1)
+		    .arg(sentence2));
+
   connect(pbnOK, SIGNAL(clicked()), this, SLOT( accept()));
 
   QApplication::restoreOverrideCursor();
