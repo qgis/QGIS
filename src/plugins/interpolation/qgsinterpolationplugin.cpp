61875443e556a077d7cd21b79234bfc16d8a45b0
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-08-23 12:09:50 +0000

    Interpolation plugin for triangle based or inverse distance interpolation
    git-svn-id: http://svn.osgeo.org/qgis/trunk@9128 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/plugins/interpolation/qgsinterpolationplugin.cpp
@@ -0,0 +1,93 @@
+/***************************************************************************
+                              qgsinterpolationplugin.cpp    
+                              --------------------------
+  begin                : Marco 10, 2008
+  copyright            : (C) 2008 by Marco Hugentobler
+  email                : marco dot hugentobler at karto dot baug dot ethz dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsinterpolationplugin.h"
+#include "qgisinterface.h"
+#include "qgsinterpolationdialog.h"
+
+#include "mIconInterpolation.xpm"
+
+static const QString name_ = QObject::tr("Interpolation plugin");
+static const QString description_ = QObject::tr("A plugin for interpolation based on vertices of a vector layer");
+static const QString version_ = QObject::tr("Version 0.001");
+
+QgsInterpolationPlugin::QgsInterpolationPlugin(QgisInterface* iface): mIface(iface), mInterpolationAction(0)
+{
+
+}
+  
+QgsInterpolationPlugin::~QgsInterpolationPlugin()
+{
+
+}
+
+void QgsInterpolationPlugin::initGui()
+{
+  if(mIface)
+    {
+      mInterpolationAction = new QAction(QIcon(mIconInterpolation), tr("&Interpolation"), 0);
+      QObject::connect(mInterpolationAction, SIGNAL(triggered()), this, SLOT(showInterpolationDialog()));
+      mIface->addToolBarIcon(mInterpolationAction);
+      mIface->addPluginMenu(tr("&Interpolation"), mInterpolationAction);
+    }
+}
+
+void QgsInterpolationPlugin::unload()
+{
+  mIface->removePluginMenu(tr("&Interpolation"), mInterpolationAction);
+  mIface->removeToolBarIcon(mInterpolationAction);
+  delete mInterpolationAction;
+}
+
+void QgsInterpolationPlugin::showInterpolationDialog()
+{
+  QgsInterpolationDialog dialog(0, mIface);
+  dialog.exec();
+}
+
+QGISEXTERN QgisPlugin * classFactory(QgisInterface * theQgisInterfacePointer)
+{
+  return new QgsInterpolationPlugin(theQgisInterfacePointer);
+}
+
+QGISEXTERN QString name()
+{
+  return name_;
+}
+
+QGISEXTERN QString description()
+{
+  return description_;
+}
+
+QGISEXTERN QString version()
+{
+  return version_;
+}
+
+QGISEXTERN int type()
+{
+  return QgisPlugin::UI;
+}
+
+QGISEXTERN void unload(QgisPlugin* theInterpolationPluginPointer)
+{
+  delete theInterpolationPluginPointer;
+}
+
+
+
---
+++ src/plugins/interpolation/qgstininterpolatordialog.cpp
@@ -0,0 +1,46 @@
+/***************************************************************************
+                              qgstininterpolatordialog.cpp    
+                              ----------------------------
+  begin                : March 29, 2008
+  copyright            : (C) 2008 by Marco Hugentobler
+  email                : marco dot hugentobler at karto dot baug dot ethz dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgstininterpolatordialog.h"
+#include "qgstininterpolator.h"
+
+QgsTINInterpolatorDialog::QgsTINInterpolatorDialog(QWidget* parent, QgisInterface* iface): QgsInterpolatorDialog(parent, iface)
+{
+  setupUi(this);
+  //enter available interpolation methods
+  mInterpolationComboBox->insertItem(0, tr("Linear interpolation"));
+  //mInterpolationComboBox->insertItem(1, tr("Clough-Toucher interpolation"));
+}
+
+QgsTINInterpolatorDialog::~QgsTINInterpolatorDialog()
+{
+  
+}
+
+QgsInterpolator* QgsTINInterpolatorDialog::createInterpolator() const
+{
+  QList<QgsVectorLayer*> inputLayerList;
+
+  QList< QPair <QgsVectorLayer*, QgsInterpolator::InputType> >::const_iterator data_it = mInputData.constBegin();
+  for(; data_it != mInputData.constEnd(); ++data_it)
+    {
+      inputLayerList.push_back(data_it->first);
+    }
+
+  QgsTINInterpolator* theInterpolator = new QgsTINInterpolator(inputLayerList);
+  return theInterpolator;
+}
