95768365816b7c41ab6198c69c6e81b2be1a9615
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-08-23 19:38:17 +0000

    Use fileName() instead of setFilename() for filename accessor
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@9135 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/qgsmaplayer.cpp
+++ src/core/qgsmaplayer.cpp
@@ -481,7 +481,7 @@ QString QgsMapLayer::loadNamedStyle ( const QString theURI, bool &theResultFlag)
   }
   else
   { 
-    QFileInfo project( QgsProject::instance()->setFilename() );
+    QFileInfo project( QgsProject::instance()->fileName() );
     QgsDebugMsg( QString("project filename: %1").arg( project.absoluteFilePath() ) );
 
     QString qml;
