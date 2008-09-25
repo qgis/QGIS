903a0ba9885bbd64899ed0296128df46dcd0902e
macho <macho@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-09-25 20:58:30 +0000

    running update script and try to implement corrupted qgis_tr.ts file - hope it worked
    git-svn-id: http://svn.osgeo.org/qgis/trunk@9408 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- i18n/qgis_lt.ts
+++ i18n/qgis_lt.ts
@@ -13357,9 +13357,6 @@ Should the existing classes be deleted before classification?</source>
     </message>
 </context>
 <context>
-    <name>RepositoryDetailsDialog</name>
-</context>
-<context>
     <name>SplashScreen</name>
     <message>
         <source>Quantum GIS - </source>
