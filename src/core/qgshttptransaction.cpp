6d8848a47962a51ad8e1ae59c62efa5bb1d23b70
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-11-24 15:48:13 +0000

    Remove server url from path. Some server reject such requests
    git-svn-id: http://svn.osgeo.org/qgis/trunk@9696 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/qgshttptransaction.cpp
+++ src/core/qgshttptransaction.cpp
@@ -113,22 +113,19 @@ bool QgsHttpTransaction::getSynchronously( QByteArray &respondedContent, int red
   // includes the scheme, host and port (the
   // http://www.address.bit:80), so remove that from the url before
   // executing an http GET.
-  //
-  // gsherman 2008-10-24 - Not sure if the above still holds true. Commenting
-  // out the removal for testing purposes
-  // QString pathAndQuery = httpurl.remove( 0,
-  //                                       httpurl.indexOf( qurl.path() ) );
+
+  QString pathAndQuery = httpurl.remove( 0, httpurl.indexOf( qurl.path() ) );
 
 
   if ( !postData ) //do request with HTTP GET
   {
-    header.setRequest( "GET", httpurl );
+    header.setRequest( "GET", pathAndQuery );
     // do GET using header containing user-agent
     httpid = http->request( header );
   }
   else //do request with HTTP POST
   {
-    header.setRequest( "POST", httpurl );
+    header.setRequest( "POST", pathAndQuery );
     // do POST using header containing user-agent
     httpid = http->request( header, *postData );
   }
