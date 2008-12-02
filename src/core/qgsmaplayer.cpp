3d97dc44e331c1449e463a22d5048455b03bdd1b
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-12-02 13:02:30 +0000

    fix #1441
    git-svn-id: http://svn.osgeo.org/qgis/trunk@9728 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/qgsmaplayer.cpp
+++ src/core/qgsmaplayer.cpp
@@ -452,6 +452,9 @@ bool QgsMapLayer::loadNamedStyleFromDb( const QString db, const QString theURI, 
 
   QgsDebugMsg( QString( "Trying to load style for \"%1\" from \"%2\"" ).arg( theURI ).arg( db ) );
 
+  if( !QFile( db ).exists() )
+    return false;
+
   myResult = sqlite3_open( db.toUtf8().data(), &myDatabase );
   if ( myResult != SQLITE_OK )
   {
