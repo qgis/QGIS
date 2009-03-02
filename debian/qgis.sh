e5043fc64c368396d01e6f9a2377c9cca0cdd04d
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-03-02 20:30:13 +0000

    debian packaging update: allow building with GRASS 6.4 on sid
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@10255 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- debian/qgis.sh
+++ debian/qgis.sh
@@ -4,9 +4,9 @@ LD_LIBRARY_PATH=/usr/lib/grass/lib
 export LD_LIBRARY_PATH
 
 if [ "$LD_LIBRARY_PATH" = "" ]; then
-	LD_LIBRARY_PATH=/usr/lib/grass/lib
+	LD_LIBRARY_PATH=/usr/lib/grass/lib:/usr/lib/grass64/lib
 else
-	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/grass/lib
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/grass/lib:/usr/lib/grass64/lib
 fi
 
 exec $0.bin "$@"
