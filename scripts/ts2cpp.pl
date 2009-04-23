d603484801e699374e117a1664aba96700370a01
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-04-23 13:43:45 +0000

    add translations strings for python plugins automatically.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@10633 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ scripts/ts2cpp.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use XML::Simple;
+use Data::Dumper;
+
+die "usage: $0 source.ts dest.cpp\n" unless @ARGV==2 && -f $ARGV[0];
+
+my $xml = XMLin($ARGV[0], ForceArray=>1);
+
+open F, ">$ARGV[1]";
+
+print F <<EOF;
+/*
+ This is NOT a proper c++ source code. This file is only designed to be caught
+ by qmake and included in lupdate. It contains all translateable strings collected
+ by pylupdate4.
+*/
+
+EOF
+
+die "context expected" unless exists $xml->{context};
+
+foreach my $context ( @{ $xml->{context} } ) {
+	print F "\n// context: $context->{name}->[0]\n\n";
+
+	foreach my $message ( @{ $context->{message} } ) {
+		$message->{source}->[0] =~ s/"/\\"/g;
+		$message->{source}->[0] =~ s/\n/\\n/g;
+
+		print F "translate( \"$context->{name}->[0]\", \"$message->{source}->[0]\");\n";
+	}
+}
+
+close F;
