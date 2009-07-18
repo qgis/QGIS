5274c8f18748ec394f00e201791c23b28f3ac532
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-07-18 17:52:06 +0000

    fix #1789
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@11105 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/qgspgquerybuilder.h
+++ src/app/qgspgquerybuilder.h
@@ -153,8 +153,8 @@ class QgsPgQueryBuilder : public QDialog, private Ui::QgsPgQueryBuilderBase
     QStandardItemModel *mModelFields;
     //! Model for values ListView
     QStandardItemModel *mModelValues;
-    //! Actual field char?
-    bool mActualFieldIsChar;
+    //! value needs to be quoted (char, boolean)
+    bool mQuoteValue;
     //! Previous field row to delete model
     int mPreviousFieldRow;
 };
