650f3bda066619003dfedbfff2e231a1b042f2d6
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-08-05 08:15:05 +0000

    replace osgeo4w path in qgisconfig.py
    git-svn-id: http://svn.osgeo.org/qgis/trunk@11271 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- ms-windows/osgeo4w/preremove.bat
+++ ms-windows/osgeo4w/preremove.bat
@@ -1,3 +1,4 @@
 del "%OSGEO4W_STARTMENU%\Quantum GIS (trunk).lnk"
 del "%ALLUSERSPROFILE%\Desktop\Quantum GIS (trunk).lnk"
-del "%OSGEO4W_ROOT%\bin\qgis-dev.bat
+del "%OSGEO4W_ROOT%"\bin\qgis-dev.bat
+del "%OSGEO4W_ROOT%"\apps\qgis-dev\python\qgis\qgisconfig.py
