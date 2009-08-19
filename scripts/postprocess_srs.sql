4b4614f3e76c06ccf7b452319667f784f5170a88
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-08-19 21:22:31 +0000

    * Added script for updating the srs.db from the GDAL installation (thanks msieczka !) * Added a small README on the process on how to do the above * Change the CRS recognition when reading from projects, so it tries to match the proj4 string if valid. This is a must when the srs.db now is updated * Changed to read ellipsoid an projection acronyms from srs.db nistead of qgis.db (mostly) * Misc. clean up of comments and deletion of stale code. * Fixes #1534.
    git-svn-id: http://svn.osgeo.org/qgis/trunk@11447 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- scripts/postprocess_srs.sql
+++ scripts/postprocess_srs.sql
@@ -3,18 +3,18 @@
 -- sqlite3 srs.db <postprocess_srs.sql
 
 -- Swedish RT-90 projections with increased accuracy
-REPLACE INTO tbl_srs VALUES(984,'RT90 7.5 gon V','tmerc','GRS80',
-'+proj=tmerc +lat_0=0 +lon_0=11.306250000000 +k=1.000006000000 +x_0=1500025.141 +y_0=-667.282 +ellps=GRS80 +units=m +no_defs',3019,3019,0);
-REPLACE INTO tbl_srs VALUES(985,'RT90 5 gon V','tmerc','GRS80',
-'+proj=tmerc +lat_0=0  +lon_0=13.55626666666 +k=1.000005800000 +x_0=1500044.695 +y_0=-667.130 +ellps=GRS80 +units=m +no_defs',3020,3020,0);
-REPLACE INTO tbl_srs VALUES(986,'RT90 2.5 gon V','tmerc','GRS80',
-'+proj=tmerc +lat_0=0 +lon_0=15.806284529444 +k=1.000005610240 +x_0=1500064.274 +y_0=-667.711 +ellps=GRS80 +units=m +no_defs',3021,3021,0);
-REPLACE INTO tbl_srs VALUES(987,'RT90 0 gon','tmerc','GRS80',
-'+proj=tmerc +lat_0=0 +lon_0=18.056300000000 +k=1.000005400000 +x_0=1500083.521 +y_0=-668.844 +ellps=GRS80 +units=m +no_defs',3022,3022,0);
-REPLACE INTO tbl_srs VALUES(988,'RT90 2.5 gon O','tmerc','GRS80',
-'+proj=tmerc +lat_0=0 +lon_0=20.306316666666 +k=1.000005200000 +x_0=1500102.765 +y_0=-670.706 +ellps=GRS80 +units=m +no_defs',3023,3023,0);
-REPLACE INTO tbl_srs VALUES(989,'RT90 5 gon O','tmerc','GRS80',
-'+proj=tmerc +lat_0=0 +lon_0=22.556333333333 +k=1.000004900000 +x_0=1500121.846 +y_0=-672.557 +ellps=GRS80 +units=m +no_defs',3024,3024,0);
+INSERT INTO tbl_srs VALUES(NULL,'RT90 7.5 gon V from WGS84','tmerc','GRS80',
+'+proj=tmerc +lat_0=0 +lon_0=11.306250000000 +k=1.000006000000 +x_0=1500025.141 +y_0=-667.282 +ellps=GRS80 +units=m +no_defs',93019,93019,0);
+INSERT INTO tbl_srs VALUES(NULL,'RT90 5 gon V from WGS84','tmerc','GRS80',
+'+proj=tmerc +lat_0=0  +lon_0=13.55626666666 +k=1.000005800000 +x_0=1500044.695 +y_0=-667.130 +ellps=GRS80 +units=m +no_defs',93020,93020,0);
+INSERT INTO tbl_srs VALUES(NULL,'RT90 2.5 gon V from WGS84','tmerc','GRS80',
+'+proj=tmerc +lat_0=0 +lon_0=15.806284529444 +k=1.000005610240 +x_0=1500064.274 +y_0=-667.711 +ellps=GRS80 +units=m +no_defs',93021,93021,0);
+INSERT INTO tbl_srs VALUES(NULL,'RT90 0 gon from WGS84','tmerc','GRS80',
+'+proj=tmerc +lat_0=0 +lon_0=18.056300000000 +k=1.000005400000 +x_0=1500083.521 +y_0=-668.844 +ellps=GRS80 +units=m +no_defs',93022,93022,0);
+INSERT INTO tbl_srs VALUES(NULL,'RT90 2.5 gon O from WGS84','tmerc','GRS80',
+'+proj=tmerc +lat_0=0 +lon_0=20.306316666666 +k=1.000005200000 +x_0=1500102.765 +y_0=-670.706 +ellps=GRS80 +units=m +no_defs',93023,93023,0);
+INSERT INTO tbl_srs VALUES(NULL,'RT90 5 gon O from WGS84','tmerc','GRS80',
+'+proj=tmerc +lat_0=0 +lon_0=22.556333333333 +k=1.000004900000 +x_0=1500121.846 +y_0=-672.557 +ellps=GRS80 +units=m +no_defs',93024,93024,0);
 
 -- S-JTSK/Krovak (Greenwich) (ticket #728)
-insert into tbl_srs values (3520,'S-JTSK (Greenwich) / Krovak','krovak','bessel','+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +pm=greenwich +units=m +no_defs','102067','102067','0'); 
+INSERT INTO tbl_srs VALUES (NULL,'S-JTSK (Greenwich) / Krovak','krovak','bessel','+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +pm=greenwich +units=m +no_defs','102067','102067','0'); 
--- src/core/qgscoordinatereferencesystem.cpp
+++ src/core/qgscoordinatereferencesystem.cpp
@@ -935,48 +935,59 @@ bool QgsCoordinateReferenceSystem::readXML( QDomNode & theNode )
   {
     QDomNode myNode = srsNode.namedItem( "proj4" );
     QDomElement myElement = myNode.toElement();
-    setProj4String( myElement.text() );
 
-    myNode = srsNode.namedItem( "srsid" );
-    myElement = myNode.toElement();
-    setInternalId( myElement.text().toLong() );
-
-    myNode = srsNode.namedItem( "srid" );
-    myElement = myNode.toElement();
-    setSrid( myElement.text().toLong() );
-
-    myNode = srsNode.namedItem( "epsg" );
-    myElement = myNode.toElement();
-    setEpsg( myElement.text().toLong() );
-
-    myNode = srsNode.namedItem( "description" );
-    myElement = myNode.toElement();
-    setDescription( myElement.text() );
-
-    myNode = srsNode.namedItem( "projectionacronym" );
-    myElement = myNode.toElement();
-    setProjectionAcronym( myElement.text() );
-
-    myNode = srsNode.namedItem( "ellipsoidacronym" );
-    myElement = myNode.toElement();
-    setEllipsoidAcronym( myElement.text() );
-
-    myNode = srsNode.namedItem( "geographicflag" );
-    myElement = myNode.toElement();
-    if ( myElement.text().compare( "true" ) )
+    if ( createFromProj4( myElement.text() ) )
     {
-      setGeographicFlag( true );
+      // createFromProj4() sets everything, inlcuding map units
+      QgsDebugMsg( "Setting from proj4 string" );
     }
     else
     {
-      setGeographicFlag( false );
+      QgsDebugMsg( "Setting from elements one by one" );
+
+      setProj4String( myElement.text() );
+
+      myNode = srsNode.namedItem( "srsid" );
+      myElement = myNode.toElement();
+      setInternalId( myElement.text().toLong() );
+
+      myNode = srsNode.namedItem( "srid" );
+      myElement = myNode.toElement();
+      setSrid( myElement.text().toLong() );
+
+      myNode = srsNode.namedItem( "epsg" );
+      myElement = myNode.toElement();
+      setEpsg( myElement.text().toLong() );
+
+      myNode = srsNode.namedItem( "description" );
+      myElement = myNode.toElement();
+      setDescription( myElement.text() );
+
+      myNode = srsNode.namedItem( "projectionacronym" );
+      myElement = myNode.toElement();
+      setProjectionAcronym( myElement.text() );
+
+      myNode = srsNode.namedItem( "ellipsoidacronym" );
+      myElement = myNode.toElement();
+      setEllipsoidAcronym( myElement.text() );
+
+      myNode = srsNode.namedItem( "geographicflag" );
+      myElement = myNode.toElement();
+      if ( myElement.text().compare( "true" ) )
+      {
+        setGeographicFlag( true );
+      }
+      else
+      {
+        setGeographicFlag( false );
+      }
+
+      //make sure the map units have been set
+      setMapUnits();
+
+      //@TODO this srs needs to be validated!!!
+      mIsValidFlag = true;//shamelessly hard coded for now
     }
-
-    //make sure the map units have been set
-    setMapUnits();
-
-    //@TODO this srs needs to be validated!!!
-    mIsValidFlag = true;//shamelessly hard coded for now
   }
   else
   {
