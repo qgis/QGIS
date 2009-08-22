3707092fe60dfd5afdc6bc41921a288f81637cb1
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-08-22 15:24:11 +0000

    update osgeo4w build script
    git-svn-id: http://svn.osgeo.org/qgis/branches/Release-1_2_0@11483 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- ms-windows/osgeo4w/postinstall.bat
+++ ms-windows/osgeo4w/postinstall.bat
@@ -1,6 +1,6 @@
-textreplace -std -t bin\qgis-dev.bat
-textreplace -std -t apps\qgis-dev\python\qgis\qgisconfig.py
+textreplace -std -t bin\qgis-unstable.bat
+textreplace -std -t apps\qgis-unstable\python\qgis\qgisconfig.py
 
 mkdir "%OSGEO4W_STARTMENU%"
-xxmklink "%OSGEO4W_STARTMENU%\Quantum GIS (trunk).lnk"       "%OSGEO4W_ROOT%\bin\qgis-dev.bat" " " \ "Quantum GIS - Desktop GIS (trunk)" 1 "%OSGEO4W_ROOT%\apps\qgis-dev\icons\QGIS.ico"
-xxmklink "%ALLUSERSPROFILE%\Desktop\Quantum GIS (trunk).lnk" "%OSGEO4W_ROOT%\bin\qgis-dev.bat" " " \ "Quantum GIS - Desktop GIS (trunk)" 1 "%OSGEO4W_ROOT%\apps\qgis-dev\icons\QGIS.ico"
+xxmklink "%OSGEO4W_STARTMENU%\Quantum GIS (1.2).lnk"       "%OSGEO4W_ROOT%\bin\qgis-unstable.bat" " " \ "Quantum GIS - Desktop GIS (1.2)" 1 "%OSGEO4W_ROOT%\apps\qgis-unstable\icons\QGIS.ico"
+xxmklink "%ALLUSERSPROFILE%\Desktop\Quantum GIS (1.2).lnk" "%OSGEO4W_ROOT%\bin\qgis-unstable.bat" " " \ "Quantum GIS - Desktop GIS (1.2)" 1 "%OSGEO4W_ROOT%\apps\qgis-unstable\icons\QGIS.ico"
