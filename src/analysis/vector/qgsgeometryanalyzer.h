b2249ccc987897251837c1d38d8fe1810b988011
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-09-11 06:33:30 +0000

    Added buffering method to qgsgeometryanalyzer
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@11623 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/analysis/vector/qgsgeometryanalyzer.h
+++ src/analysis/vector/qgsgeometryanalyzer.h
@@ -26,6 +26,9 @@
 #include "qgsfield.h"
 #include "qgsdistancearea.h"
 
+class QgsVectorFileWriter;
+class QProgressDialog;
+
 
 /** \ingroup analysis
  * The QGis class provides vector geometry analysis functions
@@ -35,73 +38,86 @@ class ANALYSIS_EXPORT QgsGeometryAnalyzer
 {
   public:
 
-    /** 
-     * Convert a vector layer from single part geometry 
-     * to multipart geometry for a given field 
+    /**
+     * Convert a vector layer from single part geometry
+     * to multipart geometry for a given field
      *
      * */
     bool singlepartsToMultipart( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding,
-        const int fieldIndex );
+                                 const QString& shapefileName,
+                                 const QString& fileEncoding,
+                                 const int fieldIndex );
 
-    /** 
+    /**
      * Convert multipart features to multiple singlepart features. Creates
      * simple polygons and lines.
      */
     bool multipartToSingleparts( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding );
+                                 const QString& shapefileName,
+                                 const QString& fileEncoding );
 
-    /** 
+    /**
      * Extract nodes from line and polygon vector layers and output them as
      * points.
      * */
     bool extractNodes( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding );
+                       const QString& shapefileName,
+                       const QString& fileEncoding );
 
     /**
      * Convert polygon features to line features. Multipart polygons are
      * converted to multiple singlepart lines.
      */
     bool polygonsToLines( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding );
-        
+                          const QString& shapefileName,
+                          const QString& fileEncoding );
+
     /**
      * Add vector layer geometry info to point (XCOORD, YCOORD), line (LENGTH),
      * or polygon (AREA, PERIMETER) layer.
      */
     bool exportGeometryInformation( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding );
-    
+                                    const QString& shapefileName,
+                                    const QString& fileEncoding );
+
     /**
      * Simplify (generalise) line or polygon vector layers using (a modified)
      * Douglas-Peucker algorithm.
      */
     bool simplifyGeometry( QgsVectorLayer* layer,
-        const QString shapefileName,
-        const QString fileEncoding,
-        const double tolerance );
+                           const QString shapefileName,
+                           const QString fileEncoding,
+                           const double tolerance );
 
     /**
      * Calculate the true centroids, or 'center of mass' for each polygon in an
      * input polygon layer.
      */
     bool polygonCentroids( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding );
+                           const QString& shapefileName,
+                           const QString& fileEncoding );
 
     /**
-     * Create a polygon based on the extents of all features (or all 
+     * Create a polygon based on the extents of all features (or all
      * selected features if applicable) and write it out to a shp.
      */
     bool layerExtent( QgsVectorLayer* layer,
-        const QString& shapefileName,
-        const QString& fileEncoding );
-                             
+                      const QString& shapefileName,
+                      const QString& fileEncoding );
+
+    /**Create buffers for a vector layer and write it to a new shape file
+      @param layer input vector layer
+      @param shapefileName path to the output shp
+      @param fileEncoding encoding of the output file
+      @param bufferDistance distance for buffering (if no buffer field is specified)
+      @param onlySelectedFeatures if true, only selected features are considered, else all the features
+      @param dissolve if true, merge all the buffers to a big multipolygon
+      @param bufferDistanceField index of the attribute field that contains the buffer distance (or -1 if all features have the same buffer distance)
+      @param p progress dialog (or 0 if no progress dialog is to be shown)
+      @note: added in version 1.3*/
+    bool buffer( QgsVectorLayer* layer, const QString& shapefileName, double bufferDistance, \
+                 bool onlySelectedFeatures = false, bool dissolve = false, int bufferDistanceField = -1, QProgressDialog* p = 0 );
+
   private:
 
     QList<double> simpleMeasure( QgsGeometry* geometry );
@@ -112,6 +128,9 @@ class ANALYSIS_EXPORT QgsGeometryAnalyzer
     QgsGeometry* extractAsMulti( QgsGeometry* geometry );
     QgsGeometry* convertGeometry( QgsGeometry* geometry );
     QList<QgsPoint> extractPoints( QgsGeometry* geometry );
+    /**Helper function to buffer an individual feature*/
+    void bufferFeature( QgsFeature& f, int nProcessedFeatures, QgsVectorFileWriter* vfw, bool dissolve, QgsGeometry** dissolveGeometry, \
+                        double bufferDistance, int bufferDistanceField );
 
 };
 #endif //QGSVECTORANALYZER
