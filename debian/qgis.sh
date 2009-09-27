83c78fa9168744ab2d5f86c5aff39b0f52dfa371
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-09-27 01:19:07 +0000

    debian packaging update
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@11718 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- debian/qgis.sh
+++ debian/qgis.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=/usr/lib/grass/lib
-export LD_LIBRARY_PATH
-
 if [ "$LD_LIBRARY_PATH" = "" ]; then
 	LD_LIBRARY_PATH=/usr/lib/grass/lib:/usr/lib/grass64/lib
 else
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/grass/lib:/usr/lib/grass64/lib
 fi
+
+export LD_LIBRARY_PATH
 
 exec $0.bin "$@"
