4437567aad1d776718eb9f8c3945b07e095edcf7
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-11-08 19:34:54 +0000

    UI overhaul bonanza...hope this doesnt break anything...I guess we should say [FEATURE] at some point as the map composer dialogs have been somewhat overhauled....
    git-svn-id: http://svn.osgeo.org/qgis/trunk@12031 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/composer/qgscomposerscalebarwidget.cpp
+++ src/app/composer/qgscomposerscalebarwidget.cpp
@@ -28,7 +28,7 @@ QgsComposerScaleBarWidget::QgsComposerScaleBarWidget( QgsComposerScaleBar* scale
 
   //add widget for general composer item properties
   QgsComposerItemWidget* itemPropertiesWidget = new QgsComposerItemWidget( this, scaleBar );
-  gridLayout->addWidget( itemPropertiesWidget );
+  toolBox->addItem( itemPropertiesWidget, tr( "General options" ) );
 
   blockMemberSignals( true );
   mStyleComboBox->insertItem( 0, tr( "Single Box" ) );
