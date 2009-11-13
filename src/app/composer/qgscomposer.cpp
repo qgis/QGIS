534e1bcf5b85cda33385f6b5b395e4434aed4d9c
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-11-13 15:56:59 +0000

    Initialize portrait / landscape in print dialog based on composition width and height. Fixes bug #2080
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@12101 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/composer/qgscomposer.cpp
+++ src/app/composer/qgscomposer.cpp
@@ -453,8 +453,20 @@ void QgsComposer::on_mActionExportAsPDF_triggered()
 void QgsComposer::on_mActionPrint_triggered()
 {
   QPrinter printer;
+  //portrait or landscape
+  if ( mComposition )
+  {
+    if ( mComposition->paperWidth() >= mComposition->paperHeight() )
+    {
+      printer.setOrientation( QPrinter::Landscape );
+    }
+    else
+    {
+      printer.setOrientation( QPrinter::Portrait );
+    }
+  }
+  QPrintDialog printDialog( &printer, 0 );
 
-  QPrintDialog printDialog( &printer );
   if ( printDialog.exec() != QDialog::Accepted )
     return;
 
