e3fbe088a3aeaa1eb8e712e9d55db2cf05334fda
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-11-27 07:21:13 +0000

    remove widget hint for composer manager
    git-svn-id: http://svn.osgeo.org/qgis/trunk@12265 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -3733,7 +3733,7 @@ void QgisApp::newPrintComposer()
 
 void QgisApp::showComposerManager()
 {
-  QgsComposerManager m( this, 0, Qt::WindowStaysOnTopHint );
+  QgsComposerManager m( this, 0);
   m.exec();
 }
 
