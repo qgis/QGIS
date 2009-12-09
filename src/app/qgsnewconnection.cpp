5527f983d75636551d62dfecccbee17e6d8119c6
borysiasty <borysiasty@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-12-09 12:17:32 +0000

    fix #2223. Patch from Alex Bruy
    git-svn-id: http://svn.osgeo.org/qgis/trunk@12383 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/qgsnewconnection.cpp
+++ src/app/qgsnewconnection.cpp
@@ -117,7 +117,7 @@ void QgsNewConnection::testConnection()
   }
   else
   {
-    QMessageBox::information( this, tr( "Test connection" ), tr( "Connection failed - Check settings and try again.\n\nExtended error information:\n%1" ).arg( PQerrorMessage( pd ) ) );
+    QMessageBox::information( this, tr( "Test connection" ), tr( "Connection failed - Check settings and try again.\n\nExtended error information:\n%1" ).arg( QString::fromUtf8( PQerrorMessage( pd ) ) ) );
   }
   // free pg connection resources
   PQfinish( pd );
