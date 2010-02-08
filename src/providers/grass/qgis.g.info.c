adf8e0e8a39b167174633589ba3fc0e96a62d70b
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-02-08 23:46:35 +0000

    remove glocale and package names again
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@12902 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/providers/grass/qgis.g.info.c
+++ src/providers/grass/qgis.g.info.c
@@ -1,143 +1,141 @@
 /****************************************************************************
- * 
+ *
  * MODULE:       qgis.g.info
  * AUTHOR(S):    Radim Blazek <radim.blazek gmail.com>
- *               using various GRASS modules 
+ *               using various GRASS modules
  * PURPOSE:      get informations about locations,mapsets,maps
- * COPYRIGHT:    (C) 2010 by Radim Blazek 
+ * COPYRIGHT:    (C) 2010 by Radim Blazek
  *
  *               This program is free software under the GNU General Public
  *               License (>=v2).
  *
  *****************************************************************************/
-#define PACKAGE "grassmods"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <grass/gis.h>
 #include <grass/raster.h>
 #include <grass/display.h>
-#include <grass/glocale.h>
 #include <grass/gprojects.h>
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
-    struct GModule *module;
-    struct Option *info_opt, *rast_opt, *vect_opt, *coor_opt;
-    struct Cell_head window;
-    
-    /* Initialize the GIS calls */
-    G_gisinit(argv[0]);
+  struct GModule *module;
+  struct Option *info_opt, *rast_opt, *vect_opt, *coor_opt;
+  struct Cell_head window;
 
-    module = G_define_module();
-    module->description = ("Get info about locations,mapsets,maps");
+  /* Initialize the GIS calls */
+  G_gisinit( argv[0] );
 
-    info_opt = G_define_option();
-    info_opt->key = "info";
-    info_opt->type = TYPE_STRING;
-    info_opt->description = "info key";
-    info_opt->options = "proj,window,query";
+  module = G_define_module();
+  module->description = ( "Get info about locations,mapsets,maps" );
 
-    rast_opt = G_define_standard_option(G_OPT_R_INPUT);
-    rast_opt->key = "rast";
-    rast_opt->required = NO;
+  info_opt = G_define_option();
+  info_opt->key = "info";
+  info_opt->type = TYPE_STRING;
+  info_opt->description = "info key";
+  info_opt->options = "proj,window,query";
 
-    vect_opt = G_define_standard_option(G_OPT_V_INPUT);
-    vect_opt->key = "vect";
-    vect_opt->required = NO;
+  rast_opt = G_define_standard_option( G_OPT_R_INPUT );
+  rast_opt->key = "rast";
+  rast_opt->required = NO;
 
-    coor_opt = G_define_option();
-    coor_opt->key = "coor";
-    coor_opt->type = TYPE_DOUBLE;
-    coor_opt->multiple = YES;
-     
-    if (G_parser(argc, argv))
-        exit(EXIT_FAILURE);
+  vect_opt = G_define_standard_option( G_OPT_V_INPUT );
+  vect_opt->key = "vect";
+  vect_opt->required = NO;
+
+  coor_opt = G_define_option();
+  coor_opt->key = "coor";
+  coor_opt->type = TYPE_DOUBLE;
+  coor_opt->multiple = YES;
+
+  if ( G_parser( argc, argv ) )
+    exit( EXIT_FAILURE );
 
 
-    if ( strcmp("proj",info_opt->answer) == 0 ) 
+  if ( strcmp( "proj", info_opt->answer ) == 0 )
+  {
+    G_get_window( &window );
+    /* code from g.proj */
+    if ( window.proj != PROJECTION_XY )
     {
-        G_get_window(&window);
-        /* code from g.proj */
-        if (window.proj != PROJECTION_XY) {
-            struct Key_Value *projinfo, *projunits;
-            char *wkt;
-            projinfo = G_get_projinfo();
-            projunits = G_get_projunits();
-            wkt = GPJ_grass_to_wkt( projinfo, projunits,  0, 0 );
-            fprintf (stdout, "%s", wkt);
-        }
+      struct Key_Value *projinfo, *projunits;
+      char *wkt;
+      projinfo = G_get_projinfo();
+      projunits = G_get_projunits();
+      wkt = GPJ_grass_to_wkt( projinfo, projunits,  0, 0 );
+      fprintf( stdout, "%s", wkt );
     }
-    else if ( strcmp("window",info_opt->answer) == 0 ) 
+  }
+  else if ( strcmp( "window", info_opt->answer ) == 0 )
+  {
+    if ( rast_opt->answer )
     {
-        if ( rast_opt->answer ) 
-        {
-          G_get_cellhd( rast_opt->answer, "", &window);
-          fprintf (stdout, "%f,%f,%f,%f", window.west, window.south,window.east,window.north );
-        }
-        else if ( vect_opt->answer )
-        {
-          G_fatal_error ("Not yet supported");
-        }
+      G_get_cellhd( rast_opt->answer, "", &window );
+      fprintf( stdout, "%f,%f,%f,%f", window.west, window.south, window.east, window.north );
     }
-    else if ( strcmp("query",info_opt->answer) == 0 ) 
+    else if ( vect_opt->answer )
     {
-        double x, y;
-        int row, col;
-        x = atof ( coor_opt->answers[0] );
-        y = atof ( coor_opt->answers[1] );
-        if ( rast_opt->answer ) 
-        {
-          int fd;
-          RASTER_MAP_TYPE rast_type;
-          DCELL *dcell;
-          CELL *cell;
-          G_get_cellhd( rast_opt->answer, "", &window);
-          G_set_window(&window);
-          fd = G_open_cell_old( rast_opt->answer, "");
-          col = (int) G_easting_to_col( x, &window);
-          row = (int) G_northing_to_row( y, &window);
-          if (col == window.cols) col--;
-          if (row == window.rows) row--;
+      G_fatal_error( "Not yet supported" );
+    }
+  }
+  else if ( strcmp( "query", info_opt->answer ) == 0 )
+  {
+    double x, y;
+    int row, col;
+    x = atof( coor_opt->answers[0] );
+    y = atof( coor_opt->answers[1] );
+    if ( rast_opt->answer )
+    {
+      int fd;
+      RASTER_MAP_TYPE rast_type;
+      DCELL *dcell;
+      CELL *cell;
+      G_get_cellhd( rast_opt->answer, "", &window );
+      G_set_window( &window );
+      fd = G_open_cell_old( rast_opt->answer, "" );
+      col = ( int ) G_easting_to_col( x, &window );
+      row = ( int ) G_northing_to_row( y, &window );
+      if ( col == window.cols ) col--;
+      if ( row == window.rows ) row--;
 
-          if ( col < 0 || col > window.cols || row < 0 || row > window.rows  )
+      if ( col < 0 || col > window.cols || row < 0 || row > window.rows )
+      {
+        fprintf( stdout, "value:null\n" );
+      }
+      else
+      {
+        rast_type = G_get_raster_map_type( fd );
+        cell = G_allocate_c_raster_buf();
+        dcell = G_allocate_d_raster_buf();
+
+        if ( rast_type == CELL_TYPE )
+        {
+          if ( G_get_c_raster_row( fd, cell, row ) < 0 )
           {
-            fprintf (stdout, "value:null\n");
+            G_fatal_error(( "Unable to read raster map <%s> row %d" ),
+                          rast_opt->answer, row );
           }
-          else 
-          {
-            rast_type = G_get_raster_map_type(fd);
-            cell = G_allocate_c_raster_buf();
-            dcell = G_allocate_d_raster_buf();
-
-            if (rast_type == CELL_TYPE) 
-            {
-              if (G_get_c_raster_row(fd, cell, row) < 0) 
-              {
-                G_fatal_error(("Unable to read raster map <%s> row %d"),
-                                      rast_opt->answer, row);
-              }
-              fprintf (stdout, "value:%d\n", cell[col] );
-            } 
-            else 
-            { 
-              if (G_get_d_raster_row(fd, dcell, row) < 0) 
-              {
-                G_fatal_error(("Unable to read raster map <%s> row %d"),
-                                      rast_opt->answer, row);
-              }
-              fprintf (stdout, "value:%f\n", dcell[col] );
-            }
-          }
-          G_close_cell( fd );
+          fprintf( stdout, "value:%d\n", cell[col] );
         }
-        else if ( vect_opt->answer )
+        else
         {
-          G_fatal_error ("Not yet supported");
+          if ( G_get_d_raster_row( fd, dcell, row ) < 0 )
+          {
+            G_fatal_error(( "Unable to read raster map <%s> row %d" ),
+                          rast_opt->answer, row );
+          }
+          fprintf( stdout, "value:%f\n", dcell[col] );
         }
+      }
+      G_close_cell( fd );
     }
+    else if ( vect_opt->answer )
+    {
+      G_fatal_error( "Not yet supported" );
+    }
+  }
 
-    exit(EXIT_SUCCESS);
+  exit( EXIT_SUCCESS );
 }
 
