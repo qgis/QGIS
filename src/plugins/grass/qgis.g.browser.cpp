d320689fbb8500f37432e50939a2037ab6e27a18
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-02-11 07:41:35 +0000

    fix windows build errors
    git-svn-id: http://svn.osgeo.org/qgis/trunk@12923 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/plugins/grass/qgis.g.browser.cpp
+++ src/plugins/grass/qgis.g.browser.cpp
@@ -4,15 +4,23 @@
 #include <QUrl>
 #include <QDesktopServices>
 
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
+
 // Open a URL by default browser
-int main(int argc, char **argv) 
+int main( int argc, char **argv )
 {
-  if ( argc < 2 ) 
-  {  
-    fprintf ( stderr, "URL argument missing\n" );
-    exit (1);
+  if ( argc < 2 )
+  {
+    fprintf( stderr, "URL argument missing\n" );
+    exit( 1 );
   }
-  QDesktopServices::openUrl ( QUrl ( argv[1] ) );
-  sleep(1); // not nice but if it exits immediately the page sometimes does not open
-  exit ( 0 );
+  QDesktopServices::openUrl( QUrl( argv[1] ) );
+#ifdef Q_OS_WIN
+  Sleep( 1000 );
+#else
+  sleep( 1 ); // not nice but if it exits immediately the page sometimes does not open
+#endif
+  exit( 0 );
 }
