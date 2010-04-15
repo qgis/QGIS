91bc447fbff079925190a924657c63f521f40e20
cfarmer <cfarmer@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-04-15 00:29:39 +0000

    Much faster centroid computation, and simplified code. Also adds Alexander Bruy as fTools contributor.
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13314 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- python/plugins/fTools/doAbout.py
+++ python/plugins/fTools/doAbout.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # licensed under the terms of GNU GPL 2
 # 
 # This program is free software; you can redistribute it and/or modify
@@ -52,6 +53,11 @@ licensed under the terms of GNU GPL 2
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+fTools DEVELOPERS:
+Carson J. Q. Farmer
+Alexander Bruy
+**If you have contributed code to fTools and I haven't mentioned your name here, please contact me and I will add your name.
 
 ACKNOWLEDGEMENTS:
 The following individuals (whether they know it or not) have contributed ideas, help, testing, code, and guidence towards this project, and I thank them.
