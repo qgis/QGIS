38e0d17c547db1194a0ceb24b7ff1bff5c59392f
wonder <wonder@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-04-18 19:25:19 +0000

    SVG fill widget: don't require user to "activate" the svg item (by double clicking) - apply new fill when current item is changed
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13325 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/symbology-ng/qgssymbollayerv2widget.h
+++ src/gui/symbology-ng/qgssymbollayerv2widget.h
@@ -242,7 +242,7 @@ class GUI_EXPORT QgsSVGFillSymbolLayerWidget : public QgsSymbolLayerV2Widget, pr
     void on_mBrowseToolButton_clicked();
     void on_mTextureWidthSpinBox_valueChanged( double d );
     void on_mSVGLineEdit_textChanged( const QString & text );
-    void on_mSvgListWidget_itemActivated( QListWidgetItem* item );
+    void on_mSvgListWidget_currentItemChanged( QListWidgetItem* item, QListWidgetItem* previous );
     void on_mChangeOutlinePushButton_clicked();
 };
 
