9516c3a9967d63bc6c00fe2a8611cb99ad318577
wonder <wonder@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-04-20 12:25:42 +0000

    [FEATURE] gradient color ramps now support multiple stops - for adding intermediate colors
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13333 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/symbology-ng/qgsvectorgradientcolorrampv2dialog.cpp
+++ src/gui/symbology-ng/qgsvectorgradientcolorrampv2dialog.cpp
@@ -4,7 +4,8 @@
 #include "qgsvectorcolorrampv2.h"
 
 #include <QColorDialog>
-
+#include <QInputDialog>
+#include <QPainter>
 
 QgsVectorGradientColorRampV2Dialog::QgsVectorGradientColorRampV2Dialog( QgsVectorGradientColorRampV2* ramp, QWidget* parent )
     : QDialog( parent ), mRamp( ramp )
@@ -15,11 +16,54 @@ QgsVectorGradientColorRampV2Dialog::QgsVectorGradientColorRampV2Dialog( QgsVecto
   connect( btnColor1, SIGNAL( clicked() ), this, SLOT( setColor1() ) );
   connect( btnColor2, SIGNAL( clicked() ), this, SLOT( setColor2() ) );
 
+  // handle stops
+  QgsVectorGradientColorRampV2::StopsMap stops = ramp->stops();
+  groupStops->setChecked( !stops.isEmpty() );
+
+  QgsVectorGradientColorRampV2::StopsMap::iterator i;
+  QList<QTreeWidgetItem *> items;
+  for ( i = stops.begin(); i != stops.end(); ++i )
+  {
+    QStringList lst;
+    lst << "." << QString::number( i.key()*100, 'f', 0 );
+    QTreeWidgetItem* item = new QTreeWidgetItem( lst );
+
+    setStopColor( item, i.value() );
+    item->setData( 0, StopOffsetRole, i.key() );
+
+    items.append( item );
+  }
+  treeStops->insertTopLevelItems( 0, items );
+  treeStops->resizeColumnToContents( 0 );
+  treeStops->sortByColumn( 1, Qt::AscendingOrder );
+  treeStops->setSortingEnabled( true );
+
+  connect( groupStops, SIGNAL( toggled( bool ) ), this, SLOT( toggledStops( bool ) ) );
+  connect( btnAddStop, SIGNAL( clicked() ), this, SLOT( addStop() ) );
+  connect( btnRemoveStop, SIGNAL( clicked() ), this, SLOT( removeStop() ) );
+  connect( treeStops, SIGNAL( itemDoubleClicked( QTreeWidgetItem*, int ) ), this, SLOT( stopDoubleClicked( QTreeWidgetItem*, int ) ) );
+
   updatePreview();
 }
 
 void QgsVectorGradientColorRampV2Dialog::updatePreview()
 {
+  // update ramp stops from the tree widget
+  QgsVectorGradientColorRampV2::StopsMap map;
+  if ( groupStops->isChecked() )
+  {
+    int count = treeStops->topLevelItemCount();
+    for ( int i = 0; i < count; i++ )
+    {
+      QTreeWidgetItem* item = treeStops->topLevelItem( i );
+      double key = item->data( 0, StopOffsetRole ).toDouble();
+      QColor c = item->data( 0, StopColorRole ).value<QColor>();
+      map.insert( key, c );
+    }
+  }
+  mRamp->setStops( map );
+
+  // generate the preview
   QSize size( 300, 40 );
   lblPreview->setPixmap( QgsSymbolLayerV2Utils::colorRampPreviewPixmap( mRamp, size ) );
 
@@ -42,5 +86,102 @@ void QgsVectorGradientColorRampV2Dialog::setColor2()
   if ( !color.isValid() )
     return;
   mRamp->setColor2( color );
+  updatePreview();
+}
+
+void QgsVectorGradientColorRampV2Dialog::setStopColor( QTreeWidgetItem* item, QColor color )
+{
+  QSize iconSize( 16, 16 );
+  QPixmap pixmap( iconSize );
+  pixmap.fill( QColor( 0, 0, 0, 0 ) );
+  QRect rect( 1, 1, iconSize.width() - 2, iconSize.height() - 2 );
+
+  // draw a slightly rounded rectangle
+  QPainter p;
+  p.begin( &pixmap );
+  p.setPen( Qt::NoPen );
+  p.setRenderHint( QPainter::Antialiasing );
+  p.setBrush( color );
+  p.drawRoundedRect( rect, 4, 4 );
+  p.end();
+
+  item->setIcon( 0, QIcon( pixmap ) );
+  item->setData( 0, StopColorRole, color );
+  item->setText( 0, color.name() );
+}
+
+void QgsVectorGradientColorRampV2Dialog::stopDoubleClicked( QTreeWidgetItem* item, int column )
+{
+  if ( column == 0 )
+  {
+    QColor color = QColorDialog::getColor( item->data( 0, StopColorRole ).value<QColor>(), this );
+    if ( !color.isValid() )
+      return;
+    setStopColor( item, color );
+
+    updatePreview();
+  }
+  else
+  {
+    bool ok;
+    double key = item->data( 0, StopOffsetRole ).toDouble();
+    int val = ( int )( key * 100 );
+    val = QInputDialog::getInt( this, tr( "Offset of the stop" ),
+                                tr( "Please enter offset in percents (%) of the new stop" ),
+                                val, 0, 100, 1, &ok );
+    if ( !ok )
+      return;
+
+    double newkey = val / 100.0;
+    item->setText( 1, QString::number( val ) );
+
+    item->setData( 0, StopOffsetRole, newkey );
+
+    updatePreview();
+  }
+}
+
+void QgsVectorGradientColorRampV2Dialog::addStop()
+{
+  QColor color = QColorDialog::getColor( QColor(), this );
+  if ( !color.isValid() )
+    return;
+
+  bool ok;
+  int val = 50;
+  val = QInputDialog::getInt( this, tr( "Offset of the stop" ),
+                              tr( "Please enter offset in percents (%) of the new stop" ),
+                              val, 0, 100, 1, &ok );
+  if ( !ok )
+    return;
+
+  double key = val / 100.0;
+  QStringList lst;
+  lst << "." << QString::number( val, 'f', 0 );
+  QTreeWidgetItem* item = new QTreeWidgetItem( lst );
+
+  setStopColor( item, color );
+  item->setData( 0, StopOffsetRole, key );
+
+  treeStops->addTopLevelItem( item );
+
+  treeStops->resizeColumnToContents( 0 );
+
+  updatePreview();
+}
+
+void QgsVectorGradientColorRampV2Dialog::removeStop()
+{
+  QTreeWidgetItem* item = treeStops->currentItem();
+  if ( !item )
+    return;
+  int index = treeStops->indexOfTopLevelItem( item );
+  treeStops->takeTopLevelItem( index );
+
+  updatePreview();
+}
+
+void QgsVectorGradientColorRampV2Dialog::toggledStops( bool on )
+{
   updatePreview();
 }
