1c4d6c68b4dcce78d2950170f676d2d9a7fd3179
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-04-22 07:02:46 +0000

    fix windows link error
    git-svn-id: http://svn.osgeo.org/qgis/trunk@13343 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/qgsmaptoolpan.cpp
+++ src/gui/qgsmaptoolpan.cpp
@@ -43,6 +43,10 @@ void QgsMapToolPan::canvasMoveEvent( QMouseEvent * e )
   }
 }
 
+void QgsMapToolPan::canvasPressEvent( QMouseEvent * e )
+{
+}
+
 void QgsMapToolPan::canvasReleaseEvent( QMouseEvent * e )
 {
   if ( e->button() == Qt::LeftButton )
