ff5e8a8121cb648704e74c5f6fec3bd7d53dc496
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-05-10 11:44:57 +0000

    Add gpl headers for residual plot item
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13457 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/plugins/georeferencer/qgsresidualplotitem.h
+++ src/plugins/georeferencer/qgsresidualplotitem.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+    qgsresidualplotitem.h
+     --------------------------------------
+    Date                 : 10-May-2010
+    Copyright            : (c) 2010 by Marco Hugentobler
+    Email                : marco at sourcepole dot ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id */
+
+
 #ifndef QGSRESIDUALPLOTITEM_H
 #define QGSRESIDUALPLOTITEM_H
 
