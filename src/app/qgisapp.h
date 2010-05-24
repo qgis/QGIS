519c04bae4b4f9e0c76cfd548d0e462443ac1240
kyngchaos <kyngchaos@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-05-24 21:36:30 +0000

    remove postgres threading (r13557); fix missing 'new spatialite layer' function (#2745); misc cleanup
    git-svn-id: http://svn.osgeo.org/qgis/trunk@13567 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/qgisapp.h
+++ src/app/qgisapp.h
@@ -500,10 +500,10 @@ class QgisApp : public QMainWindow
     void fileNew( bool thePromptToSaveFlag );
     //! Create a new empty vector layer
     void newVectorLayer();
-#ifdef HAVE_SPATIALITE
+    //#ifdef HAVE_SPATIALITE
     //! Create a new empty spatialite layer
     void newSpatialiteLayer();
-#endif
+    //#endif
     //! Print the current map view frame
     void newPrintComposer();
     void showComposerManager();
