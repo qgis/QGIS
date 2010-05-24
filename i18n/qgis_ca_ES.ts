a42c32242408ab922a18d3c8a3c15369f7cc7cdf
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-05-24 10:59:49 +0000

    es_CA should be ca_ES
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@13556 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- i18n/qgis_ca_ES.ts
+++ i18n/qgis_ca_ES.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.0" language="ca_ES">
 <context>
     <name>CoordinateCapture</name>
     <message>
