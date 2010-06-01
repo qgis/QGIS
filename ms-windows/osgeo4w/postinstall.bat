0d8940d48b61016b3fb93586a208d0be1411d549
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-06-01 21:16:01 +0000

    osgeo4w update: qgisconfig.py was dropped
    git-svn-id: http://svn.osgeo.org/qgis/trunk@13620 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- ms-windows/osgeo4w/postinstall.bat
+++ ms-windows/osgeo4w/postinstall.bat
@@ -1,5 +1,4 @@
 textreplace -std -t bin\@package@.bat
-textreplace -std -t apps\@package@\python\qgis\qgisconfig.py
 
 mkdir "%OSGEO4W_STARTMENU%"
 xxmklink "%OSGEO4W_STARTMENU%\Quantum GIS (@version@).lnk"       "%OSGEO4W_ROOT%\bin\@package@.bat" " " \ "Quantum GIS - Desktop GIS (@version@)" 1 "%OSGEO4W_ROOT%\apps\@package@\icons\QGIS.ico"
