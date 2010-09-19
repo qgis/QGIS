8e443953f46e9939282d7eecef9c6d840f52c36d
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-09-19 15:25:48 +0000

    comment fixes
    git-svn-id: http://svn.osgeo.org/qgis/trunk@14252 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/legend/qgslegendpropertygroup.cpp
+++ src/app/legend/qgslegendpropertygroup.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Tim Sutton   *
- *   aps02ts@macbuntu   *
+ *   Copyright (C) 2005 by Tim Sutton                                      *
+ *   aps02ts@macbuntu                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
