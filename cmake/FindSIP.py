7172f01cc4423f6098b8aec51ca08c32b0c10465
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-10-03 10:07:45 +0000

    fix for #2985 (take II)
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@14324 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- cmake/FindSIP.py
+++ cmake/FindSIP.py
@@ -9,6 +9,7 @@ import sipconfig
 
 sipcfg = sipconfig.Configuration()
 print("sip_version:%06.0x" % sipcfg.sip_version)
+print("sip_version_num:%d" % sipcfg.sip_version)
 print("sip_version_str:%s" % sipcfg.sip_version_str)
 print("sip_bin:%s" % sipcfg.sip_bin)
 print("default_sip_dir:%s" % sipcfg.default_sip_dir)
