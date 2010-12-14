8655c440b260ae42bddb9be158c6e949a174ce5d
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2010-12-14 16:13:42 +0000

    Set view box for svg export explicitely (thanks to JD)
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@14910 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/composer/qgscomposer.cpp
+++ src/app/composer/qgscomposer.cpp
@@ -818,6 +818,7 @@ void QgsComposer::on_mActionExportAsSVG_triggered()
   //height in pixel
   int height = ( int )( mComposition->paperHeight() * mComposition->printResolution() / 25.4 );
   generator.setSize( QSize( width, height ) );
+  generator.setViewBox( QRect( 0, 0, width, height ) );
   generator.setResolution( mComposition->printResolution() ); //because the rendering is done in mm, convert the dpi
 
   QPainter p( &generator );
