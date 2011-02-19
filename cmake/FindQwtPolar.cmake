cf9ae27d689f3ddc8ea06a49201aa3da55cabd4a
jef <jef@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2011-02-19 20:09:21 +0000

    apply #3506
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@15215 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- cmake/FindQwtPolar.cmake
+++ cmake/FindQwtPolar.cmake
@@ -7,19 +7,18 @@
 #
 # Once run this will define: 
 # 
-# QWTPOLAR_FOUND       = system has QWTPolar lib
-# QWTPOLAR_LIBRARY     = full path to the QWTPolar library
+# QWTPOLAR_FOUND       = system has QwtPolar lib
+# QWTPOLAR_LIBRARY     = full path to the QwtPolar library
 # QWTPOLAR_INCLUDE_DIR = where to find headers 
 #
 
 
-#MESSAGE("Searching for QWTPolar")
 FIND_PATH(QWTPOLAR_INCLUDE_DIR NAMES qwt_polar.h PATHS
   /usr/include
   /usr/local/include
   "$ENV{LIB_DIR}/include" 
   "$ENV{INCLUDE}" 
-  PATH_SUFFIXES qwt-qt4 qwt qwt5
+  PATH_SUFFIXES qwtpolar
   )
 
 FIND_LIBRARY(QWTPOLAR_LIBRARY NAMES qwtpolar PATHS 
@@ -35,10 +34,10 @@ ENDIF (QWTPOLAR_INCLUDE_DIR AND QWTPOLAR_LIBRARY)
 
 IF (QWTPOLAR_FOUND)
   IF (NOT QWTPOLAR_FIND_QUIETLY)
-    MESSAGE(STATUS "Found QWTPolar: ${QWTPOLAR_LIBRARY}")
+    MESSAGE(STATUS "Found QwtPolar: ${QWTPOLAR_LIBRARY}")
   ENDIF (NOT QWTPOLAR_FIND_QUIETLY)
 ELSE (QWTPOLAR_FOUND)
   IF (QWTPOLAR_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could not find QWTPolar")
+    MESSAGE(FATAL_ERROR "Could not find QwtPolar")
   ENDIF (QWTPOLAR_FIND_REQUIRED)
 ENDIF (QWTPOLAR_FOUND)
