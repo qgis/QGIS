qgis/QGIS

commit 14781c9e215f73e56fa1944415bc4b027b5440d1

fix some warnings and other cleanups
git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@15539 c8812cc2-4d05-0410-92ff-de0c093fc19c

src/core/raster/qgsrasterlayer.cpp
@@ -768,7 +768,6 @@ bool QgsRasterLayer::draw( QgsRenderContext& rendererContext )
   }
 
   const QgsMapToPixel& theQgsMapToPixel = rendererContext.mapToPixel();
-  const QgsRectangle& theViewExtent = rendererContext.extent();
 
   QgsRectangle myProjectedViewExtent;
   QgsRectangle myProjectedLayerExtent;

debian/pencil.uu
@@ -1,44 +1,0 @@
-begin 644 pencil.png
-MB5!.1PT*&@H````-24A$4@```"`````@"`8```!S>GKT````!&=!34$``*_(
-M-P6*Z0```!ET15AT4V]F='=A<F4`061O8F4@26UA9V5296%D>7')93P```:D
-M241!5'C:8OS__S_#0`*``&)B&&``$$`#[@"``&*!,1@9&2DS25:<@4%*@H'A
-M'Q,$@\!?Q@"&7XP+&9@8WC*P_J]B8/J_`J;\_YDS8!H@@*@3`M+"#`QR$JAB
-MOYBBV+G^+I((?L/':O-.D8'YWV*&/XRUZ%H!`HB%8LO5@!9+B3(P_/R'\,]/
-MIF(NH=\]`?D/&%XI,#&<?<G$\$/E(\OW==)-#-]8?S`P_^^&:0<((,I"P$":
-M@4%&B('ASU^$V`^F+@'9;STAU7<99'6^,LC__\?`Q\S!P*GYAX'#[QG0C?_;
-M&?XSVL"4`P00>0Y@`J878RD&!A$>H.7_8*)L##\8%TIH?BX-K;K+(";W@^';
-M5U8&;J9O#/8B7`S<S((,[-H_&5@-/C`S_&9LA6D"""#2HX"#F8'!5)*!@9.3
-M@>$WR')FD*@`T/+EBF;O/;S2GC)P<`.3P`\6AM__?S*(LZLS!(FE,;B(_F>8
-M^'0:PQVU[PP?SS!:P(P#""#2'"#`!K0<F-J96:"6`P/P/X,<PT_&-9I.KTW=
-MXUXP,#$S,?S^"?3DOY\,,IP&#-8BJ0R2'-H,/UGW,AC\8&"X=0R88-^]AN<&
-M@``BW@%"0,LMQ8!9#!C\?Z&EYW\&/8:_#.MTO%XJ.X>\`48OT/+?C`Q___UB
-M4."V`EJ>S"#"IL1P^><JAM4G-C!LF2C"\/7\VXD,;Y^4PHP%""#B'"##`8QS
-M8&+[#;84`OXQ.##\_[]*+^"%J(7;!P:F?Z!`^<_P[_\?!C5>9P9+X40&/E9Q
-MAC/?YS.LW+N+8<=4<8;?5U[6,'Q\T8IL-$``$7:`)A<#@Q8?,%*1ZHR_C*%,
-MK'\7:/N^YM(V_\K``LP$?YD@;M/A]V$P$XQG8&?A9CCZ92K#LBW'&?;/%OOY
-M_\;C'(;/[^:@&P\00/@=8`I,34I`!_P`&0TM*?\RY#!S_9ZDYO.&44'[!P,K
-MT'(F%E!)RL)@R!_$8"(8!<IJ#`<^]#(L7GV9X>1BD?<,MQ\D,'S]O`F;%0`!
-MA-L!%D"+%8%!_Q/9YPS-+`*_:N0]WC*(*_P&^YP5F#38F-D9C`0B&0P$0AE^
-M,7QBV/MZ`L.B)?<9+J\6>,1PYUXDPX\?QW!9`Q!`V!U@R,[`H,P&M9P1HNX/
-MPS1FT1^IXD[O&00D_C*P`N.<%1CL?*P\#)9"<0S:?/X,7QB>,.QXVL^P8/YK
-MAGM;N"\RW+T?R?#K]W5\@0P00)@.X`+F:SV@`[[#?<X+]/EB1JGO_KS6'QFX
-M!?\SL`&E6("6BW$*,3B()3&H\[HSO/UWG6'S/:#/YWYE>+:+=1_#_8<QP$+J
-M.:$D!A!`J`X0!%INPPE-<&"?2P(M7\T@_\V:U?`S`R<P5MA!10_0<AEN*08/
-MJ11@BK=G>/;W),/::U,8EL[^R_#N`,,JA@=/4X`YY#,Q&0P@@!`.D&8%)CJ@
-MY0+`O/83+*P!-&0=@^HW30:-;PSL0&D.1HCE*GQ*#/ZR:4#+31D>_-[#L/S<
-M+(95<UD8OA[],YGAT=LBH-X_Q!8O``&$<(`=,,&!\Q-4_-_?%0PJWS49U'X!
-M*R^@Y4"+F8!85TB;(50AG4&95YOAYL]U#(N.+6+8N("3X=>)[S4,SSZUDEJR
-M`P00P@%,H(C]!70[4.C_7U\N?C;]7XK?P!4=*S/$YQ9BI@P1BFD,<CSR#)>^
-M+V!8L&\-P_:E/#__G_R2P_#ZVQQRZC6``$+4ALSP4`.6I=]+:JQ\&%S$],"!
-MR09T@+.T/4.,<BZ#)+<,PXF/TQ@F;U[!L&T^U_O_1S^%DVLY"``$$"($V']"
-M?/_OEY.TF(15JJD'@Q.C-<.Q=\4,WO)V#(EJJ0S<;"P,NU[V,"S9?I#ARB;>
-M1PRG/T8R?/ESC)(F!4``82D'_I86VG@S"`E)`"L2'8;%5A,8-KXYQ/#^WVN&
-M'8\6,:S<>I+A\4Y@'C\#M/S'O^N4-J@``@C5`4Q_31G_L+J),ZDR,/$#:SYF
-M`08_X0A@ON=F*%Q7S_#P[@.&[Z<X]C&<^1P+K.R?4:,Y"1!`J`Y@_E?X_[4T
-M8W[_3@9A"3F&+YR?&98>W\AP]-*YWV_NO;_`\)!I$\.#KQ.!->%G!BH!@`!B
-MA/6,&#,8#1A^LYUB>";'RL#U'I@FOOQ@>/7S/,-]AMT,KQAV`<O9"T!E7ZEE
-M,<Q>@`!"A``S0P;#]]_`E'CG(,--H*6/&/8!_7D%U,RD9<<$((#`+@&[1H4A
-ME(&'P00HQDJ/'A',7H``8ASHSBE```UXWQ`@@`;<`0`!!@!1Y1-PU(R$<P``
-*``!)14Y$KD)@@@``
-`
-end

commit 0697d5ba2a7e19693abc059c5105d3e1fd36e933

fix build error introduced with r15539
git-svn-id: http://svn.osgeo.org/qgis/trunk@15540 c8812cc2-4d05-0410-92ff-de0c093fc19c

src/core/raster/qgsrasterlayer.cpp
@@ -804,7 +804,7 @@ bool QgsRasterLayer::draw( QgsRenderContext& rendererContext )
     return true;
   }
 
-  QgsDebugMsg( "theViewExtent is " + theViewExtent.toString() );
+  QgsDebugMsg( "theViewExtent is " + rendererContext.extent().toString() );
   QgsDebugMsg( "myProjectedViewExtent is " + myProjectedViewExtent.toString() );
   QgsDebugMsg( "myProjectedLayerExtent is " + myProjectedLayerExtent.toString() );
   QgsDebugMsg( "myRasterExtent is " + myRasterExtent.toString() );