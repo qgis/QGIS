653de0ed140223cf2dd2943bdaffc0f8e4eebaea
brushtyler <brushtyler@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2011-03-23 16:57:22 +0000

    Added the default color configuration file for DEM tool
    git-svn-id: http://svn.osgeo.org/qgis/trunk@15582 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- python/plugins/GdalTools/__init__.py
+++ python/plugins/GdalTools/__init__.py
@@ -22,7 +22,7 @@ def name():
 def description():
   return "Integrate gdal tools into qgis"
 def version(): 
-  return "Version 1.2.24" 
+  return "Version 1.2.25" 
 def qgisMinimumVersion():
   return "1.0"
 def icon():
