234565fd541058052d6a1fed55d591df52627d17
alexbruy <alexbruy@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2011-04-03 17:15:18 +0000

    add docstring for previous commit
    git-svn-id: http://svn.osgeo.org/qgis/trunk/qgis@15665 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/gui/qgisinterface.h
+++ src/gui/qgisinterface.h
@@ -238,6 +238,8 @@ class GUI_EXPORT QgisInterface : public QObject
     virtual QToolBar *attributesToolBar() = 0;
     virtual QToolBar *pluginToolBar() = 0;
     virtual QToolBar *helpToolBar() = 0;
+    /** \note added in 1.7
+    */
     virtual QToolBar *rasterToolBar() = 0;
 
     //! File menu actions
