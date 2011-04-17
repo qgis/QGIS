65f697ac555141eeda5fd5df5788f66662ec3c6f
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2011-04-17 10:52:18 +0000

    higher tolerance for segment snap
    git-svn-id: http://svn.osgeo.org/qgis/trunk@15742 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/qgspoint.cpp
+++ src/core/qgspoint.cpp
@@ -275,7 +275,7 @@ double QgsPoint::sqrDistToSegment( double x1, double y1, double x2, double y2, Q
 
   double dist = sqrDist( minDistPoint );
   //prevent rounding errors if the point is directly on the segment
-  if ( doubleNear( dist, 0.0, 0.000000001 ) )
+  if ( doubleNear( dist, 0.0, 0.00000001 ) )
   {
     minDistPoint.setX( m_x );
     minDistPoint.setY( m_y );
