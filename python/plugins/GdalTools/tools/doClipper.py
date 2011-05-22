cd8ecd6a6ea90000867e2a92e53995f6996c5221
Alexander Bruy <alexander.bruy@gmail.com>
2011-05-22 21:12:04 +0300

    add checkbox for "-dstalpha" option in GDALTools Clipper tool

--- python/plugins/GdalTools/tools/doClipper.py
+++ python/plugins/GdalTools/tools/doClipper.py
@@ -28,6 +28,7 @@ class GdalToolsDialog(QWidget, Ui_Widget, BasePluginWidget):
           (self.outSelector, SIGNAL("filenameChanged()") ),
           (self.noDataSpin, SIGNAL("valueChanged(int)"), self.noDataCheck, "1.7.0"),
           (self.maskSelector, SIGNAL("filenameChanged()"), self.maskModeRadio, "1.6.0"),
+          (self.alphaBandCheck, SIGNAL( "stateChanged( int )") ),
           (self.extentSelector, [SIGNAL("selectionStarted()"), SIGNAL("newExtentDefined()")], self.extentModeRadio), 
           (self.modeStackedWidget, SIGNAL("currentIndexChanged(int)"))
         ]
@@ -138,7 +139,8 @@ class GdalToolsDialog(QWidget, Ui_Widget, BasePluginWidget):
           arguments << "-q"
           arguments << "-cutline"
           arguments << mask
-          arguments << "-dstalpha"
+          if self.alphaBandCheck.isChecked():
+            arguments << "-dstalpha"
 
       outputFn = self.getOutputFileName()
       if not outputFn.isEmpty():
