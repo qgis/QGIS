6039882d27732a0d7cdfbee3c564ff893bc9cc24
Tim Sutton <tim@linfiniti.com>
2011-05-24 23:37:05 +0200

    Updated docs to remove svn reference in CODING and refer to GIT equivalents

--- doc/build.sh
+++ doc/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 txt2tags -o ../INSTALL -t txt INSTALL.t2t 
 txt2tags -o index.html -t html INSTALL.t2t
+txt2tags -o INSTALL.tex -t tex INSTALL.t2t
+pdflatex INSTALL.tex
+mv INSTALL.pdf ..
 txt2tags -o ../CODING -t txt CODING.t2t
 txt2tags -o CODING.html -t html CODING.t2t
+txt2tags -o CODING.tex -t tex CODING.t2t
+pdflatex CODING.tex
+mv CODING.pdf ..
