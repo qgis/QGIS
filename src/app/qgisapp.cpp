bc30e89b5f4b1e2b63f0cdb1d4f9174daffaa265
Steven Mizuno <>
2011-06-02 17:21:03 +0200

    applied #3863 (fixes #2774)

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -6518,7 +6518,6 @@ void QgisApp::showBookmarks()
   {
     bookmarks = new QgsBookmarks( this, Qt::WindowMinMaxButtonsHint );
   }
-  bookmarks->restorePosition();
   bookmarks->show();
   bookmarks->raise();
   bookmarks->setWindowState( bookmarks->windowState() & ~Qt::WindowMinimized );
