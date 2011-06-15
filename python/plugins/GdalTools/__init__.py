016cc0b357375ad4a9e68b23c238e88dc62910e4
Giuseppe Sucameli <brush.tyler@gmail.com>
2011-06-16 01:51:22 +0200

    Correct the output label's text in GdalTools Contour

--- python/plugins/GdalTools/__init__.py
+++ python/plugins/GdalTools/__init__.py
@@ -22,7 +22,7 @@ def name():
 def description():
   return "Integrate gdal tools into qgis"
 def version(): 
-  return "Version 1.2.26" 
+  return "Version 1.2.27" 
 def qgisMinimumVersion():
   return "1.0"
 def icon():
