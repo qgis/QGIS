c769cdda95e735989ad3f21a8dd29507fa22db77
Tim Sutton <tim@linfiniti.com>
2011-06-28 12:21:33 +0200

    A neater fix for missing map parameter as suggested by Juergen

--- src/mapserver/qgswmsserver.cpp
+++ src/mapserver/qgswmsserver.cpp
@@ -106,33 +106,12 @@ QDomDocument QgsWMSServer::getCapabilities()
   //Some client requests already have http://<SERVER_NAME> in the REQUEST_URI variable
   QString hrefString;
   QString requestUrl = getenv( "REQUEST_URI" );
-  QString mapUrl = requestUrl;
-  requestUrl.truncate( requestUrl.indexOf( "?" ) + 1 );
-  
-  // find out if a map parameter was specified and ensure
-  // it is propogated to the onlineresource
-  mapUrl.replace( requestUrl, "" );
-  QStringList myTokens = mapUrl.split( "&" );
-  QString myMap = "";
-  QStringListIterator myIterator( myTokens );
-  while ( myIterator.hasNext() )
-  {
-    QString myString = myIterator.next();
-    if ( myString.contains( "map=", Qt::CaseInsensitive ) )
-    {
-      myMap = myString;
-    }
-  }
-
-
-  if ( requestUrl.contains( "http" ) )
-  {
-    hrefString = requestUrl;
-  }
-  else
-  {
-    hrefString = "http://" + QString( getenv( "SERVER_NAME" ) ) + requestUrl + myMap;
-  }
+  QUrl mapUrl( requestUrl );
+  mapUrl.setHost( QString( getenv( "SERVER_NAME" ) ) );
+  mapUrl.removeQueryItem( "REQUEST" );
+  mapUrl.removeQueryItem( "VERSION" );
+  mapUrl.removeQueryItem( "SERVICE" );
+  hrefString = mapUrl.toString();
 
 
   // SOAP platform
