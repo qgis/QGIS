405226893f5bb62e68b68222717ac642137d9737
William Kyngesburye <kyngchaos@kyngchaos.com>
2011-07-04 11:15:40 -0500

    OS X frameworks for qgis libs; dev frameworks install option; cmake cleanup

--- cmake/FindQGIS.cmake
+++ cmake/FindQGIS.cmake
@@ -4,6 +4,7 @@
 ##
 ## QGIS_CORE_LIBRARY     = full path to the CORE library
 ## QGIS_GUI_LIBRARY      = full path to the GUI library
+## QGIS_ANALYSIS_LIBRARY = full path to the ANALYSIS library
 ## QGIS_PLUGIN_DIR       = full path to where QGIS plugins are installed
 ## QGIS_INCLUDE_DIR      = where to find headers 
 ##
@@ -66,21 +67,34 @@ IF(WIN32)
         "$ENV{OSGEO4W_ROOT}/lib"
         "$ENV{PROGRAMFILES}/Quantum GIS/lib"
     )
+    FIND_LIBRARY(QGIS_ANALYSIS_LIBRARY
+      NAMES qgis_analysis
+      PATHS 
+        "$ENV{LIB_DIR}" 
+        "$ENV{LIB}" 
+        "$ENV{OSGEO4W_ROOT}/lib"
+        "$ENV{PROGRAMFILES}/Quantum GIS/lib"
+    )
   ENDIF (MSVC)
 ELSE(WIN32)
   IF(UNIX) 
     # try to use bundle on mac
     IF (APPLE)
       #MESSAGE("Searching for QGIS in /Applications/QGIS.app/Contents/MacOS")
-      #SET (QGIS_MAC_PATH /Applications/qgis1.0.0.app/Contents/MacOS)
-      SET (QGIS_MAC_PATH /Applications/qgis1.1.0.app/Contents/MacOS)
+      SET (QGIS_MAC_PATH /Applications/QGIS.app/Contents/MacOS)
       SET (QGIS_LIB_DIR ${QGIS_MAC_PATH}/lib)
-      SET (QGIS_PLUGIN_DIR ${QGIS_MAC_PATH}/lib/qgis CACHE STRING INTERNAL)
-      # set INCLUDE_DIR to prefix+include
-      SET(QGIS_INCLUDE_DIR ${QGIS_MAC_PATH}/include/qgis CACHE STRING INTERNAL)
-      ## extract link dirs 
-      SET(QGIS_CORE_LIBRARY ${QGIS_LIB_DIR}/libqgis_core.dylib CACHE STRING INTERNAL)
-      SET(QGIS_GUI_LIBRARY ${QGIS_LIB_DIR}/libqgis_gui.dylib CACHE STRING INTERNAL)
+      SET (QGIS_FW_DIR ${QGIS_MAC_PATH}/../Frameworks)
+      SET (QGIS_PLUGIN_DIR ${QGIS_MAC_PATH}/../PlugIns/qgis CACHE STRING INTERNAL)
+      # set INCLUDE_DIR to frameworks
+      SET(QGIS_INCLUDE_DIR
+        ${QGIS_FW_DIR}/qgis_core.framework/Headers
+        ${QGIS_FW_DIR}/qgis_gui.framework/Headers
+        ${QGIS_FW_DIR}/qgis_analysis.framework/Headers
+        CACHE STRING INTERNAL)
+      # extract link dirs 
+      SET(QGIS_CORE_LIBRARY ${QGIS_FW_DIR}/qgis_core.framework/qgis_core CACHE STRING INTERNAL)
+      SET(QGIS_GUI_LIBRARY ${QGIS_FW_DIR}/qgis_gui.framework/qgis_gui CACHE STRING INTERNAL)
+      SET(QGIS_ANALYSIS_LIBRARY ${QGIS_FW_DIR}/qgis_analysis.framework/qgis_analysis CACHE STRING INTERNAL)
     ELSE (APPLE)
       #MESSAGE("Searching for QGIS in /usr/bin; /usr/local/bin")
       FIND_PATH(QGIS_PLUGIN_DIR
@@ -115,18 +129,27 @@ ELSE(WIN32)
           /usr/local/lib
           "$ENV{LIB_DIR}/lib/" 
       )
+      FIND_LIBRARY(QGIS_ANALYSIS_LIBRARY
+        NAMES qgis_analysis
+        PATHS 
+          /usr/lib64
+          /usr/lib
+          /usr/local/lib
+          "$ENV{LIB_DIR}/lib/" 
+      )
     ENDIF (APPLE)
   ENDIF(UNIX)
 ENDIF(WIN32)
 
-IF (QGIS_INCLUDE_DIR AND QGIS_CORE_LIBRARY AND QGIS_GUI_LIBRARY)
+IF (QGIS_INCLUDE_DIR AND QGIS_CORE_LIBRARY AND QGIS_GUI_LIBRARY AND QGIS_ANALYSIS_LIBRARY)
    SET(QGIS_FOUND TRUE)
-ENDIF (QGIS_INCLUDE_DIR AND QGIS_CORE_LIBRARY AND QGIS_GUI_LIBRARY)
+ENDIF (QGIS_INCLUDE_DIR AND QGIS_CORE_LIBRARY AND QGIS_GUI_LIBRARY AND QGIS_ANALYSIS_LIBRARY)
 
 IF (QGIS_FOUND)
    IF (NOT QGIS_FIND_QUIETLY)
      MESSAGE(STATUS "Found QGIS core: ${QGIS_CORE_LIBRARY}")
      MESSAGE(STATUS "Found QGIS gui: ${QGIS_GUI_LIBRARY}")
+     MESSAGE(STATUS "Found QGIS analysis: ${QGIS_ANALYSIS_LIBRARY}")
      MESSAGE(STATUS "Found QGIS plugins directory: ${QGIS_PLUGIN_DIR}")
    ENDIF (NOT QGIS_FIND_QUIETLY)
 ELSE (QGIS_FOUND)
