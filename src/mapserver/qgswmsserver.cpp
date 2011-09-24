e6718b654a73a3c07eaaaa1f178a51d51265ef0b
Juergen E. Fischer <jef@norbit.de>
2011-09-24 13:59:02 +0200

    - debian packaging update for 1.7.1 - composer/wmsserver fix for lenny (Qt <4.5)

--- src/mapserver/qgswmsserver.cpp
+++ src/mapserver/qgswmsserver.cpp
@@ -362,7 +362,9 @@ class QgsPaintEngineHack : public QPaintEngine
                   // | QPaintEngine::PerspectiveTransform
                   | QPaintEngine::BlendModes
                   // | QPaintEngine::ObjectBoundingModeGradients
+#if QT_VERSION >= 0x040500
                   | QPaintEngine::RasterOpModes
+#endif
                   | QPaintEngine::PaintOutsidePaintEvent
                 );
     }
