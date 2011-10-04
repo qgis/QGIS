9ec80235045ad3af2f64eab1226c408326bdd991
Tim Sutton <tim@linfiniti.com>
2011-10-04 23:19:26 +0200

    Dont use gdal c++ api. Also gdal drivers now unloaded without restart

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -33,8 +33,8 @@
 
 #include "qgsconfig.h"
 
+#include <gdal.h>
 #include <ogr_api.h>
-#include <gdal_priv.h>
 #include <cpl_conv.h> // for setting gdal options
 
 QObject * QgsApplication::mFileOpenEventReceiver;
@@ -745,5 +745,5 @@ void QgsApplication::applyGdalSkippedDrivers()
   QgsDebugMsg( "Gdal Skipped driver list set to:" );
   QgsDebugMsg( myDriverList );
   CPLSetConfigOption( "GDAL_SKIP", myDriverList.toUtf8() );
-  GetGDALDriverManager()->AutoSkipDrivers();
+  GDALAllRegister(); //to update driver list and skip missing ones
 }
