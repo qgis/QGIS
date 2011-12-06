c3694c765dca8f470eb2e3014c6ca6cc09d47dda
Werner Macho <werner.macho@gmail.com>
2011-12-06 11:51:17 +0100

    translation update: small typo fix

--- i18n/qgis_de.ts
+++ i18n/qgis_de.ts
@@ -1177,7 +1177,7 @@ Soll sie dem Projekt als neuer Layer hinzugefügt werden?</translation>
     <message>
         <location filename="../src/ui/qgssymbolv2propertiesdialogbase.ui" line="31"/>
         <source>Symbol layer type</source>
-        <translation>Symbolayertyp</translation>
+        <translation type="unfinished">Symbollayertyp</translation>
     </message>
     <message>
         <location filename="../src/ui/qgssymbolv2propertiesdialogbase.ui" line="20"/>
