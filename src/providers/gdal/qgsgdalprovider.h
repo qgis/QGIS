6dae2f18a1f6528ea1f7d11c04b65b523ac70d79
Juergen E. Fischer <jef@norbit.de>
2011-12-22 23:48:03 +0100

    gdal provider: add statusChanged signal and fix debugging output

--- src/providers/gdal/qgsgdalprovider.h
+++ src/providers/gdal/qgsgdalprovider.h
@@ -256,6 +256,9 @@ class QgsGdalProvider : public QgsRasterDataProvider
     /** Emit a signal to notify of the progress event. */
     void emitProgress( int theType, double theProgress, QString theMessage );
 
+  signals:
+    void statusChanged( QString );
+
   private:
     // initialize CRS from wkt
     bool crsFromWkt( const char *wkt );
