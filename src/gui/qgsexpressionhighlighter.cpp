d7b14999a03d9966da9afb42fc8c8c644117524f
Nathan Woodrow <woodrow.nathan@gmail.com>
2011-12-30 17:14:42 +1000

    Wrap field name in quotes when added to expression string

--- src/gui/qgsexpressionhighlighter.cpp
+++ src/gui/qgsexpressionhighlighter.cpp
@@ -20,25 +20,6 @@ QgsExpressionHighlighter::QgsExpressionHighlighter( QTextDocument *parent)
 {
   HighlightingRule rule;
 
-//     keywordFormat.setForeground(Qt::darkBlue);
-//     keywordFormat.setFontWeight(QFont::Bold);
-//     QStringList keywordPatterns;
-//     keywordPatterns << "\\bchar\\b" << "\\bclass\\b" << "\\bconst\\b"
-//                     << "\\bdouble\\b" << "\\benum\\b" << "\\bexplicit\\b"
-//                     << "\\bfriend\\b" << "\\binline\\b" << "\\bint\\b"
-//                     << "\\blong\\b" << "\\bnamespace\\b" << "\\boperator\\b"
-//                     << "\\bprivate\\b" << "\\bprotected\\b" << "\\bpublic\\b"
-//                     << "\\bshort\\b" << "\\bsignals\\b" << "\\bsigned\\b"
-//                     << "\\bslots\\b" << "\\bstatic\\b" << "\\bstruct\\b"
-//                     << "\\btemplate\\b" << "\\btypedef\\b" << "\\btypename\\b"
-//                     << "\\bunion\\b" << "\\bunsigned\\b" << "\\bvirtual\\b"
-//                     << "\\bvoid\\b" << "\\bvolatile\\b";
-//     foreach (const QString &pattern, keywordPatterns) {
-//         rule.pattern = QRegExp(pattern);
-//         rule.format = keywordFormat;
-//         highlightingRules.append(rule);
-//     }
-
   quotationFormat.setForeground( Qt::darkGreen );
   rule.pattern = QRegExp( "\'.*\'" );
   rule.format = quotationFormat;
