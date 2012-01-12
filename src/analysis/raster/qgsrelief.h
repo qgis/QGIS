8cd37c1e043339d5ea63e30e29d03ce8805b364d
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2012-01-12 11:34:25 +0100

    Add missing copyright notices

--- src/analysis/raster/qgsrelief.h
+++ src/analysis/raster/qgsrelief.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          qgsrelief.h  -  description
+                          ---------------------------
+    begin                : November 2011
+    copyright            : (C) 20011 by Marco Hugentobler
+    email                : marco dot hugentobler at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSRELIEF_H
 #define QGSRELIEF_H
 
