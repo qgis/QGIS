d3a96e9bf38ded15a4cbda14071049f1e4cc71e3
Alexander Bruy <alexander.bruy@gmail.com>
2012-01-21 15:53:23 +0200

    clear collected plugins path before collecting svg path

--- src/app/qgsoptions.cpp
+++ src/app/qgsoptions.cpp
@@ -548,6 +548,7 @@ void QgsOptions::saveOptions()
   settings.setValue( "plugins/searchPathsForPlugins", myPaths );
 
   //search directories for svgs
+  myPath.clear();
   for ( int i = 0; i < mListSVGPaths->count(); ++i )
   {
     if ( i != 0 )
