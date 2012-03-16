c5769bf3799c785ab90d4f37fa1a0c7fe88cfc3f
Giuseppe Sucameli <brush.tyler@gmail.com>
2012-03-16 16:50:59 +0100

    Append Filter tag to Rule tag when exporting Categorized/Graduated rendererV2 to SLD doc (fix #5191)

--- src/core/symbology-ng/qgsgraduatedsymbolrendererv2.cpp
+++ src/core/symbology-ng/qgsgraduatedsymbolrendererv2.cpp
@@ -103,27 +103,24 @@ void QgsRendererRangeV2::toSld( QDomDocument &doc, QDomElement &element, QgsStri
   if ( !mSymbol || props.value( "attribute", "" ).isEmpty() )
     return;
 
+  QString attrName = props[ "attribute" ];
+
   QDomElement ruleElem = doc.createElement( "se:Rule" );
   element.appendChild( ruleElem );
 
-  QString valueStr = QString( "range: %1 - %2" ).arg( mLowerValue ).arg( mUpperValue );
-
   QDomElement nameElem = doc.createElement( "se:Name" );
-  nameElem.appendChild( doc.createTextNode( !mLabel.isEmpty() ? mLabel : valueStr ) );
+  nameElem.appendChild( doc.createTextNode( mLabel ) );
   ruleElem.appendChild( nameElem );
 
-  QString descrName = props.value( "version", "1.1" ) < "1.1" ? "Abstract" : "se:Description";
-  QString descrValue = QString( "Graduated symbol rendererV2 - %1" ).arg( valueStr );
-
-  QDomElement descrElem = doc.createElement( descrName );
-  descrElem.appendChild( doc.createTextNode( descrValue ) );
+  QDomElement descrElem = doc.createElement( "se:Description" );
+  QString descrStr = QString( "range: %1 - %2" ).arg( mLowerValue ).arg( mUpperValue );
+  descrElem.appendChild( doc.createTextNode( !mLabel.isEmpty() ? mLabel : descrStr ) );
   ruleElem.appendChild( descrElem );
 
   // create the ogc:Filter for the range
   QDomElement filterElem = doc.createElement( "ogc:Filter" );
-
   QString filterFunc = QString( "%1 > %2 AND %1 <= %3" )
-      .arg( props[ "attribute" ] )
+      .arg( attrName.replace( "\"", "\"\"" ) )
       .arg( mLowerValue ).arg( mUpperValue );
   QgsSymbolLayerV2Utils::createFunctionElement( doc, filterElem, filterFunc );
 
