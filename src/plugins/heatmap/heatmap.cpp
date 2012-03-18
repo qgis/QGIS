fc8b0c42a285438934b099523268fba641ecfe5c
Arunmozhi <aruntheguy@gmail.com>
2012-03-18 22:56:59 +0530

    merge pull request #104: - redesigned the dialog, added a options for - weighted buffer - raster size - moved the advanced stuff under advancedGroupBox - finished the GUI code - finally heatmap hits v0.2 Users can now  - specify the rows and columns for the raster  - specify the cell size of the raster  - Choose a field as weight for each point  - Choose a field to act as the buffer radius for each point  - Buffer radius can be specified in real-world units like meters & mapunits - indentation updated

--- src/plugins/heatmap/heatmap.cpp
+++ src/plugins/heatmap/heatmap.cpp
@@ -30,6 +30,9 @@
 #include "qgsgeometry.h"
 #include "qgsvectorlayer.h"
 #include "qgsvectordataprovider.h"
+#include "qgsdistancearea.h"
+#include "qgscoordinatereferencesystem.h"
+#include "qgslogger.h"
 
 // Qt4 Related Includes
 #include <QAction>
@@ -44,7 +47,7 @@
 static const QString sName = QObject::tr( "Heatmap" );
 static const QString sDescription = QObject::tr( "Creates a Heatmap raster for the input point vector" );
 static const QString sCategory = QObject::tr( "Raster" );
-static const QString sPluginVersion = QObject::tr( "Version 0.1" );
+static const QString sPluginVersion = QObject::tr( "Version 0.2" );
 static const QgisPlugin::PLUGINTYPE sPluginType = QgisPlugin::UI;
 static const QString sPluginIcon = ":/heatmap/heatmap.png";
 
@@ -94,14 +97,219 @@ void Heatmap::help()
 // not be enough
 void Heatmap::run()
 {
-  HeatmapGui *myPluginGui = new HeatmapGui( mQGisIface->mainWindow(), QgisGui::ModalDialogFlags );
-  myPluginGui->setAttribute( Qt::WA_DeleteOnClose );
+  HeatmapGui d( mQGisIface->mainWindow(), QgisGui::ModalDialogFlags );
 
-  // Connect the createRaster signal to createRaster Slot
-  connect( myPluginGui, SIGNAL( createRaster( QgsVectorLayer*, int, float, QString, QString ) ),
-           this, SLOT( createRaster( QgsVectorLayer*, int, float, QString, QString ) ) );
+  if ( d.exec() == QDialog::Accepted )
+  {
+    // everything runs here
 
-  myPluginGui->show();
+    // Get the required data from the dialog
+    QgsRectangle myBBox = d.bbox();
+    int columns = d.columns();
+    int rows = d.rows();
+    float cellsize = d.cellSizeX(); // or d.cellSizeY();  both have the same value
+    float myDecay = d.decayRatio();
+
+    // Getting the rasterdataset in place
+    GDALAllRegister();
+
+    GDALDataset *emptyDataset;
+    GDALDriver *myDriver;
+
+    myDriver = GetGDALDriverManager()->GetDriverByName( d.outputFormat().toUtf8() );
+    if ( myDriver == NULL )
+    {
+      QMessageBox::information( 0, tr( "GDAL driver error" ), tr( "Cannot open the driver for the specified format" ) );
+      return;
+    }
+
+    double geoTransform[6] = { myBBox.xMinimum(), cellsize, 0, myBBox.yMinimum(), 0, cellsize };
+    emptyDataset = myDriver->Create( d.outputFilename().toUtf8(), columns, rows, 1, GDT_Float32, NULL );
+    emptyDataset->SetGeoTransform( geoTransform );
+
+    GDALRasterBand *poBand;
+    poBand = emptyDataset->GetRasterBand( 1 );
+    poBand->SetNoDataValue( NO_DATA );
+
+    float* line = ( float * ) CPLMalloc( sizeof( float ) * columns );
+    for ( int i = 0; i < columns ; i++ )
+      line[i] = NO_DATA;
+    // Write the empty raster
+    for ( int i = 0; i < rows ; i++ )
+    {
+      poBand->RasterIO( GF_Write, 0, 0, columns, 1, line, columns, 1, GDT_Float32, 0, 0 );
+    }
+
+    CPLFree( line );
+    //close the dataset
+    GDALClose(( GDALDatasetH ) emptyDataset );
+
+    // open the raster in GA_Update mode
+    GDALDataset *heatmapDS;
+    heatmapDS = ( GDALDataset * ) GDALOpen( d.outputFilename().toUtf8(), GA_Update );
+    if ( !heatmapDS )
+    {
+      QMessageBox::information( 0, tr( "Raster update error" ), tr( "Could not open the created raster for updating. The heatmap was not generated." ) );
+      return;
+    }
+    poBand = heatmapDS->GetRasterBand( 1 );
+    // Start working on the input vector
+    QgsVectorLayer* inputLayer = d.inputVectorLayer();
+    QgsVectorDataProvider* myVectorProvider = inputLayer->dataProvider();
+    if ( !myVectorProvider )
+    {
+      QMessageBox::information( 0, tr( "Point layer error" ), tr( "Could not identify the vector data provider." ) );
+      return;
+    }
+
+    QgsAttributeList myAttrList;
+    int rField = 0;
+    int wField = 0;
+    if ( d.variableRadius() )
+    {
+      rField = d.radiusField();
+      myAttrList.append( rField );
+      QgsDebugMsg( tr( "Radius Field index received: %1" ).arg( rField ) );
+    }
+    if ( d.weighted() )
+    {
+      wField = d.weightField();
+      myAttrList.append( wField );
+    }
+    // This might have attributes or mightnot have attibutes at all
+    // based on the variableRadius() and weighted()
+    myVectorProvider->select( myAttrList );
+    int totalFeatures = myVectorProvider->featureCount();
+    int counter = 0;
+
+    QProgressDialog p( "Creating Heatmap ... ", "Abort", 0, totalFeatures );
+    p.setWindowModality( Qt::WindowModal );
+
+    QgsFeature myFeature;
+
+    while ( myVectorProvider->nextFeature( myFeature ) )
+    {
+      counter++;
+      p.setValue( counter );
+      if ( p.wasCanceled() )
+      {
+        QMessageBox::information( 0, tr( "Heatmap generation aborted" ), tr( "QGIS will now load the partially-computed raster." ) );
+        break;
+      }
+
+      QgsGeometry* myPointGeometry;
+      myPointGeometry = myFeature.geometry();
+      // convert the geometry to point
+      QgsPoint myPoint;
+      myPoint = myPointGeometry->asPoint();
+      // avoiding any empty points or out of extent points
+      if (( myPoint.x() < myBBox.xMinimum() ) || ( myPoint.y() < myBBox.yMinimum() ) )
+      {
+        continue;
+      }
+      float radius;
+      if ( d.variableRadius() )
+      {
+        QgsAttributeMap myAttrMap = myFeature.attributeMap();
+        radius = myAttrMap.value( rField ).toFloat();
+      }
+      else
+      {
+        radius = d.radius();
+      }
+      //convert the radius to map units if it is in meters
+      if ( d.radiusUnit() == HeatmapGui::Meters )
+      {
+        radius = mapUnitsOf( radius, inputLayer->crs() );
+      }
+      // convert radius in map units to pixel count
+      int myBuffer = radius / cellsize;
+      if ( radius - ( cellsize * myBuffer ) > 0.5 )
+      {
+        ++myBuffer;
+      }
+      int blockSize = 2 * myBuffer + 1; //Block SIDE would be more appropriate
+      // calculate the pixel position
+      unsigned int xPosition, yPosition;
+      xPosition = (( myPoint.x() - myBBox.xMinimum() ) / cellsize ) - myBuffer;
+      yPosition = (( myPoint.y() - myBBox.yMinimum() ) / cellsize ) - myBuffer;
+
+      // get the data
+      float *dataBuffer = ( float * ) CPLMalloc( sizeof( float ) * blockSize * blockSize );
+      poBand->RasterIO( GF_Read, xPosition, yPosition, blockSize, blockSize,
+                        dataBuffer, blockSize, blockSize, GDT_Float32, 0, 0 );
+
+      float weight = 1.0;
+      if ( d.weighted() )
+      {
+        QgsAttributeMap myAttrMap = myFeature.attributeMap();
+        weight = myAttrMap.value( wField ).toFloat();
+      }
+
+      for ( int xp = 0; xp <= myBuffer; xp++ )
+      {
+        for ( int yp = 0; yp <= myBuffer; yp++ )
+        {
+          float distance = sqrt( pow( xp, 2.0 ) + pow( yp, 2.0 ) );
+          float pixelValue = weight * ( 1 - (( 1 - myDecay ) * distance / myBuffer ) );
+
+          // clearing anamolies along the axes
+          if ( xp == 0 && yp == 0 )
+          {
+            pixelValue /= 4;
+          }
+          else if ( xp == 0 || yp == 0 )
+          {
+            pixelValue /= 2;
+          }
+
+          if ( distance <= myBuffer )
+          {
+            int pos[4];
+            pos[0] = ( myBuffer + xp ) * blockSize + ( myBuffer + yp );
+            pos[1] = ( myBuffer + xp ) * blockSize + ( myBuffer - yp );
+            pos[2] = ( myBuffer - xp ) * blockSize + ( myBuffer + yp );
+            pos[3] = ( myBuffer - xp ) * blockSize + ( myBuffer - yp );
+            for ( int p = 0; p < 4; p++ )
+            {
+              if ( dataBuffer[ pos[p] ] == NO_DATA )
+              {
+                dataBuffer[ pos[p] ] = 0;
+              }
+              dataBuffer[ pos[p] ] += pixelValue;
+            }
+          }
+        }
+      }
+
+      poBand->RasterIO( GF_Write, xPosition, yPosition, blockSize, blockSize,
+                        dataBuffer, blockSize, blockSize, GDT_Float32, 0, 0 );
+      CPLFree( dataBuffer );
+    }
+    //Finally close the dataset
+    GDALClose(( GDALDatasetH ) heatmapDS );
+
+    // Open the file in QGIS window
+    mQGisIface->addRasterLayer( d.outputFilename(), QFileInfo( d.outputFilename() ).baseName() );
+  }
+}
+
+/*
+ *
+ * Local functions
+ *
+ */
+float Heatmap::mapUnitsOf( float meters, QgsCoordinateReferenceSystem layerCrs )
+{
+  // Worker to transform metres input to mapunits
+  QgsDistanceArea da;
+  da.setSourceCrs( layerCrs.srsid() );
+  da.setEllipsoid( layerCrs.ellipsoidAcronym() );
+  if ( da.geographic() )
+  {
+    da.setProjectionsEnabled( true );
+  }
+  return meters / da.measureLine( QgsPoint( 0.0, 0.0 ), QgsPoint( 0.0, 1.0 ) );
 }
 
 // Unload the plugin by cleaning up the GUI
@@ -111,169 +319,6 @@ void Heatmap::unload()
   mQGisIface->removePluginRasterMenu( tr( "&Heatmap" ), mQActionPointer );
   mQGisIface->removeRasterToolBarIcon( mQActionPointer );
   delete mQActionPointer;
-}
-
-// The worker
-void Heatmap::createRaster( QgsVectorLayer* theVectorLayer, int theBuffer, float theDecay, QString theOutputFilename, QString theOutputFormat )
-{
-  // generic variables
-  int xSize, ySize;
-  double xResolution, yResolution;
-  double rasterX, rasterY;
-
-  // Getting the rasterdataset in place
-  GDALAllRegister();
-
-  GDALDataset *emptyDataset;
-  GDALDriver *myDriver;
-
-  myDriver = GetGDALDriverManager()->GetDriverByName( theOutputFormat.toUtf8() );
-  if ( myDriver == NULL )
-  {
-    QMessageBox::information( 0, tr( "GDAL driver error" ), tr( "Cannot open the driver for the specified format" ) );
-    return;
-  }
-
-  // bounding box info
-  QgsRectangle myBBox = theVectorLayer->extent();
-  // fixing  a base width of 500 px/cells
-  xSize = 500;
-  xResolution = myBBox.width() / xSize;
-  yResolution = xResolution;
-  ySize = myBBox.height() / yResolution;
-  // add extra extend to cover the corner points' heat region
-  xSize = xSize + ( theBuffer * 2 ) + 10 ;
-  ySize = ySize + ( theBuffer * 2 ) + 10 ;
-  // Define the new lat,lon for the buffered raster area
-  rasterX = myBBox.xMinimum() - ( theBuffer + 5 ) * xResolution;
-  rasterY = myBBox.yMinimum() - ( theBuffer + 5 ) * yResolution;
-
-  double geoTransform[6] = { rasterX, xResolution, 0, rasterY, 0, yResolution };
-
-  emptyDataset = myDriver->Create( theOutputFilename.toUtf8(), xSize, ySize, 1, GDT_Float32, NULL );
-
-  emptyDataset->SetGeoTransform( geoTransform );
-
-  GDALRasterBand *poBand;
-  poBand = emptyDataset->GetRasterBand( 1 );
-  poBand->SetNoDataValue( NO_DATA );
-
-  float* line = ( float * ) CPLMalloc( sizeof( float ) * xSize );
-  for ( int i = 0; i < xSize; i++ )
-    line[i] = NO_DATA;
-  // Write the empty raster
-  for ( int i = 0; i < ySize ; i++ )
-  {
-    poBand->RasterIO( GF_Write, 0, 0, xSize, 1, line, xSize, 1, GDT_Float32, 0, 0 );
-  }
-
-  CPLFree( line );
-  //close the dataset
-  GDALClose(( GDALDatasetH ) emptyDataset );
-
-  // open the raster in GA_Update mode
-  GDALDataset *heatmapDS;
-  heatmapDS = ( GDALDataset * ) GDALOpen( theOutputFilename.toUtf8(), GA_Update );
-  if ( !heatmapDS )
-  {
-    QMessageBox::information( 0, tr( "Raster update error" ), tr( "Could not open the created raster for updating. The heatmap was not generated." ) );
-    return;
-  }
-  poBand = heatmapDS->GetRasterBand( 1 );
-  // Get the data buffer ready
-  int blockSize = 2 * theBuffer + 1; // block SIDE would have been more appropriate
-  // Open the vector features
-  QgsVectorDataProvider* myVectorProvider = theVectorLayer->dataProvider();
-  if ( !myVectorProvider )
-  {
-    QMessageBox::information( 0, tr( "Point layer error" ), tr( "Could not identify the vector data provider." ) );
-    return;
-  }
-  QgsAttributeList dummyList;
-  myVectorProvider->select( dummyList );
-
-  int totalFeatures = myVectorProvider->featureCount();
-  int counter = 0;
-
-  QProgressDialog p( "Creating Heatmap ... ", "Abort", 0, totalFeatures );
-  p.setWindowModality( Qt::WindowModal );
-
-  QgsFeature myFeature;
-
-  while ( myVectorProvider->nextFeature( myFeature ) )
-  {
-    counter++;
-    p.setValue( counter );
-    if ( p.wasCanceled() )
-    {
-      QMessageBox::information( 0, tr( "Heatmap generation aborted" ), tr( "QGIS will now load the partially-computed raster." ) );
-      break;
-    }
-
-    QgsGeometry* myPointGeometry;
-    myPointGeometry = myFeature.geometry();
-    // convert the geometry to point
-    QgsPoint myPoint;
-    myPoint = myPointGeometry->asPoint();
-    // avoiding any empty points or out of extent points
-    if (( myPoint.x() < rasterX ) || ( myPoint.y() < rasterY ) )
-    {
-      continue;
-    }
-    // calculate the pixel position
-    unsigned int xPosition, yPosition;
-    xPosition = (( myPoint.x() - rasterX ) / xResolution ) - theBuffer;
-    yPosition = (( myPoint.y() - rasterY ) / yResolution ) - theBuffer;
-
-    // get the data
-    float *dataBuffer = ( float * ) CPLMalloc( sizeof( float ) * blockSize * blockSize );
-    poBand->RasterIO( GF_Read, xPosition, yPosition, blockSize, blockSize, dataBuffer, blockSize, blockSize, GDT_Float32, 0, 0 );
-
-    for ( int xp = 0; xp <= theBuffer; xp++ )
-    {
-      for ( int yp = 0; yp <= theBuffer; yp++ )
-      {
-        float distance = sqrt( pow( xp, 2.0 ) + pow( yp, 2.0 ) );
-        float pixelValue = 1 - (( 1 - theDecay ) * distance / theBuffer );
-
-        // clearing anamolies along the axes
-        if ( xp == 0 && yp == 0 )
-        {
-          pixelValue /= 4;
-        }
-        else if ( xp == 0 || yp == 0 )
-        {
-          pixelValue /= 2;
-        }
-
-        if ( distance <= theBuffer )
-        {
-          int pos[4];
-          pos[0] = ( theBuffer + xp ) * blockSize + ( theBuffer + yp );
-          pos[1] = ( theBuffer + xp ) * blockSize + ( theBuffer - yp );
-          pos[2] = ( theBuffer - xp ) * blockSize + ( theBuffer + yp );
-          pos[3] = ( theBuffer - xp ) * blockSize + ( theBuffer - yp );
-          for ( int p = 0; p < 4; p++ )
-          {
-            if ( dataBuffer[ pos[p] ] == NO_DATA )
-            {
-              dataBuffer[ pos[p] ] = 0;
-            }
-            dataBuffer[ pos[p] ] += pixelValue;
-          }
-        }
-      }
-    }
-
-    poBand->RasterIO( GF_Write, xPosition, yPosition, blockSize, blockSize, dataBuffer, blockSize, blockSize, GDT_Float32, 0, 0 );
-    CPLFree( dataBuffer );
-  }
-
-  //Finally close the dataset
-  GDALClose(( GDALDatasetH ) heatmapDS );
-
-  // Open the file in QGIS window
-  mQGisIface->addRasterLayer( theOutputFilename, QFileInfo( theOutputFilename ).baseName() );
 }
 
 //////////////////////////////////////////////////////////////////////////
--- src/plugins/heatmap/heatmapgui.cpp
+++ src/plugins/heatmap/heatmapgui.cpp
@@ -16,6 +16,11 @@
 #include "qgsmaplayer.h"
 #include "qgsmaplayerregistry.h"
 #include "qgsvectorlayer.h"
+#include "qgsvectordataprovider.h"
+#include "qgslogger.h"
+#include "qgsgeometry.h"
+#include "qgscoordinatereferencesystem.h"
+#include "qgsdistancearea.h"
 
 // GDAL includes
 #include "gdal_priv.h"
@@ -34,6 +39,8 @@ HeatmapGui::HeatmapGui( QWidget* parent, Qt::WFlags fl )
     : QDialog( parent, fl )
 {
   setupUi( this );
+
+  QgsDebugMsg( tr( "Creating Heatmap Dialog" ) );
 
   // Adding point layers to the mInputVectorCombo
   foreach( QgsMapLayer *l, QgsMapLayerRegistry::instance()->mapLayers() )
@@ -70,6 +77,7 @@ HeatmapGui::HeatmapGui( QWidget* parent, Qt::WFlags fl )
   }
   mFormatCombo->setCurrentIndex( myTiffIndex );
 
+  updateBBox();
   //finally set right the ok button
   enableOrDisableOkButton();
 }
@@ -78,70 +86,13 @@ HeatmapGui::~HeatmapGui()
 {
 }
 
+/*
+ *
+ * Private Slots
+ *
+ */
 void HeatmapGui::on_mButtonBox_accepted()
 {
-  // Variables to be emitted with the createRaster signal
-  int bufferDistance;
-  float decayRatio;
-  QString outputFileName;
-  QString outputFormat;
-
-  QString dummyText;
-
-  // The input vector layer
-  QString myLayerId = mInputVectorCombo->itemData( mInputVectorCombo->currentIndex() ).toString();
-
-  QgsVectorLayer* inputLayer = qobject_cast<QgsVectorLayer *>( QgsMapLayerRegistry::instance()->mapLayer( myLayerId ) );
-  if ( !inputLayer )
-  {
-    QMessageBox::information( 0, tr( "Layer not found" ), tr( "Layer %1 not found." ).arg( myLayerId ) );
-    return;
-  }
-
-  // The buffer distance
-  dummyText = mBufferLineEdit->text();
-  bufferDistance = dummyText.toInt();
-  if ( bufferDistance == 0 )
-  {
-    QMessageBox::information( 0, tr( "Invalid buffer value" ), tr( "Buffer distance cannot be zero. Please enter a valid value." ) );
-    return;
-  }
-  // The decay ratio
-  dummyText = mDecayLineEdit->text();
-  decayRatio = dummyText.toFloat();
-
-  // The output filename
-  outputFileName = mOutputRasterLineEdit->text();
-  QFileInfo myFileInfo( outputFileName );
-  if ( outputFileName.isEmpty() || !myFileInfo.dir().exists() )
-  {
-    QMessageBox::information( 0, tr( "Invalid output filename" ), tr( "Please enter a valid output file path and name." ) );
-    return;
-  }
-
-  // The output format
-  outputFormat = mFormatCombo->itemData( mFormatCombo->currentIndex() ).toString();
-
-  // append the file format if the suffix is empty
-  QString suffix = myFileInfo.suffix();
-  if ( suffix.isEmpty() )
-  {
-    QMap<QString, QString>::const_iterator it = mExtensionMap.find( outputFormat );
-    if ( it != mExtensionMap.end() && it.key() == outputFormat )
-    {
-      // making sure that there is really a extension value available
-      // Some drivers donot seem to have any extension at all
-      if ( it.value() != NULL || it.value() != "" )
-      {
-        outputFileName.append( "." );
-        outputFileName.append( it.value() );
-      }
-    }
-  }
-
-  emit createRaster( inputLayer, bufferDistance, decayRatio, outputFileName, outputFormat );
-
-  //and finally
   accept();
 }
 
@@ -180,6 +131,101 @@ void HeatmapGui::on_mOutputRasterLineEdit_editingFinished()
   enableOrDisableOkButton();
 }
 
+void HeatmapGui::on_advancedGroupBox_toggled( bool enabled )
+{
+  if ( enabled )
+  {
+    // if there are no layers point layers then show error dialog and toggle
+    if ( mInputVectorCombo->count() == 0 )
+    {
+      QMessageBox::information( 0, tr( "No valid layers found!" ), tr( "Advanced options cannot be enabled." ) );
+      advancedGroupBox->setChecked( false );
+      return;
+    }
+    // if there are layers then populate fields
+    populateFields();
+    updateBBox();
+  }
+}
+
+void HeatmapGui::on_rowLineEdit_editingFinished()
+{
+  mRows = rowLineEdit->text().toInt();
+  mYcellsize = mBBox.height() / mRows;
+  mXcellsize = mYcellsize;
+  mColumns = mBBox.width() / mXcellsize + 1;
+
+  updateSize();
+}
+
+void HeatmapGui::on_columnLineEdit_editingFinished()
+{
+  mColumns = columnLineEdit->text().toInt();
+  mXcellsize = mBBox.width() / mColumns;
+  mYcellsize = mXcellsize;
+  mRows = mBBox.height() / mYcellsize + 1;
+
+  updateSize();
+}
+
+void HeatmapGui::on_cellXLineEdit_editingFinished()
+{
+  mXcellsize = cellXLineEdit->text().toFloat();
+  mYcellsize = mXcellsize;
+  mRows = mBBox.height() / mYcellsize + 1;
+  mColumns = mBBox.width() / mXcellsize + 1;
+
+  updateSize();
+}
+
+void HeatmapGui::on_cellYLineEdit_editingFinished()
+{
+  mYcellsize = cellYLineEdit->text().toFloat();
+  mXcellsize = mYcellsize;
+  mRows = mBBox.height() / mYcellsize + 1;
+  mColumns = mBBox.width() / mXcellsize + 1;
+
+  updateSize();
+}
+
+void HeatmapGui::on_radiusFieldUnitCombo_currentIndexChanged( int index )
+{
+  updateBBox();
+  // DebugMsg to avoid index not used warning
+  QgsDebugMsg( tr( "Unit index set to %1" ).arg( index ) );
+}
+
+void HeatmapGui::on_mRadiusUnitCombo_currentIndexChanged( int index )
+{
+  QgsDebugMsg( tr( "Unit index set to %1" ).arg( index ) );
+  updateBBox();
+}
+
+void HeatmapGui::on_mInputVectorCombo_currentIndexChanged( int index )
+{
+  if ( advancedGroupBox->isChecked() )
+  {
+    populateFields();
+    updateBBox();
+  }
+  QgsDebugMsg( tr( "Input vector index changed to %1" ).arg( index ) );
+}
+
+void HeatmapGui::on_radiusFieldCombo_currentIndexChanged( int index )
+{
+  updateBBox();
+  QgsDebugMsg( tr( "Radius Field index changed to %1" ).arg( index ) );
+}
+
+void HeatmapGui::on_mBufferLineEdit_editingFinished()
+{
+  updateBBox();
+}
+/*
+ *
+ * Private Functions
+ *
+ */
 void HeatmapGui::enableOrDisableOkButton()
 {
   bool enabled = true;
@@ -191,3 +237,206 @@ void HeatmapGui::enableOrDisableOkButton()
   }
   mButtonBox->button( QDialogButtonBox::Ok )->setEnabled( enabled );
 }
+
+void HeatmapGui::populateFields()
+{
+  QgsVectorLayer* inputLayer = inputVectorLayer();
+  // The fields
+  QgsVectorDataProvider* provider = inputLayer->dataProvider();
+  QgsFieldMap fieldMap = provider->fields();
+  // Populate fields
+  radiusFieldCombo->clear();
+  weightFieldCombo->clear();
+
+  QMap<int, QgsField>::const_iterator i = fieldMap.constBegin();
+  while ( i != fieldMap.constEnd() )
+  {
+    radiusFieldCombo->addItem( i.value().name(), QVariant( i.key() ) );
+    weightFieldCombo->addItem( i.value().name(), QVariant( i.key() ) );
+    ++i;
+  }
+
+}
+
+void HeatmapGui::updateSize()
+{
+  rowLineEdit->setText( QString::number( mRows ) );
+  columnLineEdit->setText( QString::number( mColumns ) );
+  cellXLineEdit->setText( QString::number( mXcellsize ) );
+  cellYLineEdit->setText( QString::number( mYcellsize ) );
+}
+
+void HeatmapGui::updateBBox()
+{
+  // Set the row/cols and cell sizes here
+  QgsVectorLayer *inputLayer = inputVectorLayer();
+
+  mBBox = inputLayer->extent();
+  QgsCoordinateReferenceSystem layerCrs = inputLayer->crs();
+
+  float radiusInMapUnits;
+  if ( useRadius->isChecked() )
+  {
+    float maxInField = inputLayer->maximumValue( radiusFieldCombo->itemData( radiusFieldCombo->currentIndex() ).toInt() ).toFloat();
+
+    if ( radiusFieldUnitCombo->currentIndex() == HeatmapGui::Meters )
+    {
+      radiusInMapUnits = mapUnitsOf( maxInField, layerCrs );
+    }
+    else if ( radiusFieldUnitCombo->currentIndex() == HeatmapGui::MapUnits )
+    {
+      radiusInMapUnits = maxInField;
+    }
+  }
+  else
+  {
+    float radiusValue = mBufferLineEdit->text().toFloat();
+    if ( mRadiusUnitCombo->currentIndex() == HeatmapGui::Meters )
+    {
+      radiusInMapUnits = mapUnitsOf( radiusValue, layerCrs );
+    }
+    else if ( mRadiusUnitCombo->currentIndex() == HeatmapGui::MapUnits )
+    {
+      radiusInMapUnits = radiusValue;
+    }
+  }
+  // get the distance converted into map units
+  mBBox.setXMinimum( mBBox.xMinimum() - radiusInMapUnits );
+  mBBox.setYMinimum( mBBox.yMinimum() - radiusInMapUnits );
+  mBBox.setXMaximum( mBBox.xMaximum() + radiusInMapUnits );
+  mBBox.setYMaximum( mBBox.yMaximum() + radiusInMapUnits );
+  mRows = 500;
+  mYcellsize = mBBox.height() / mRows;
+  mXcellsize = mYcellsize;
+  // +1 should be  added wherever a fractional part of cell might occur
+  mColumns = mBBox.width() / mXcellsize + 1;
+  mRows += 1;
+
+  if ( advancedGroupBox->isChecked() )
+  {
+    updateSize();
+  }
+}
+
+float HeatmapGui::mapUnitsOf( float meters, QgsCoordinateReferenceSystem layerCrs )
+{
+  // converter function to transform metres input to mapunits
+  // so that bounding box can be updated
+  QgsDistanceArea da;
+  da.setSourceCrs( layerCrs.srsid() );
+  da.setEllipsoid( layerCrs.ellipsoidAcronym() );
+  if ( da.geographic() )
+  {
+    da.setProjectionsEnabled( true );
+  }
+  double unitDistance = da.measureLine( QgsPoint( 0.0, 0.0 ), QgsPoint( 0.0, 1.0 ) );
+  QgsDebugMsg( tr( "Converted %1 meters to %2 mapunits" ).arg( meters ).arg( meters / unitDistance ) );
+  return  meters / unitDistance;
+}
+/*
+ *
+ * Public functions
+ *
+ */
+
+bool HeatmapGui::weighted()
+{
+  return useWeight->isChecked();
+}
+
+bool HeatmapGui::variableRadius()
+{
+  return useRadius->isChecked();
+}
+
+float HeatmapGui::radius()
+{
+  float radius = mBufferLineEdit->text().toInt();
+  if ( mRadiusUnitCombo->currentIndex() == HeatmapGui::Meters )
+  {
+    radius = mapUnitsOf( radius, inputVectorLayer()->crs() );
+  }
+  return radius;
+}
+
+int HeatmapGui::radiusUnit()
+{
+  if ( useRadius->isChecked() )
+  {
+    return radiusFieldUnitCombo->currentIndex();
+  }
+  return mRadiusUnitCombo->currentIndex();
+}
+
+float HeatmapGui::decayRatio()
+{
+  return mDecayLineEdit->text().toFloat();
+}
+
+int HeatmapGui::radiusField()
+{
+  int radiusindex;
+  radiusindex = radiusFieldCombo->currentIndex();
+  return radiusFieldCombo->itemData( radiusindex ).toInt();
+}
+
+int HeatmapGui::weightField()
+{
+  int weightindex;
+  weightindex = weightFieldCombo->currentIndex();
+  return weightFieldCombo->itemData( weightindex ).toInt();
+}
+
+QString HeatmapGui::outputFilename()
+{
+  QString outputFileName;
+  QString outputFormat;
+
+  outputFileName = mOutputRasterLineEdit->text();
+  QFileInfo myFileInfo( outputFileName );
+  if ( outputFileName.isEmpty() || !myFileInfo.dir().exists() )
+  {
+    QMessageBox::information( 0, tr( "Invalid output filename" ), tr( "Please enter a valid output file path and name." ) );
+    return NULL;
+  }
+
+  // The output format
+  outputFormat = mFormatCombo->itemData( mFormatCombo->currentIndex() ).toString();
+  // append the file format if the suffix is empty
+  QString suffix = myFileInfo.suffix();
+  if ( suffix.isEmpty() )
+  {
+    QMap<QString, QString>::const_iterator it = mExtensionMap.find( outputFormat );
+    if ( it != mExtensionMap.end() && it.key() == outputFormat )
+    {
+      // making sure that there is really a extension value available
+      // Some drivers donot seem to have any extension at all
+      if ( it.value() != NULL || it.value() != "" )
+      {
+        outputFileName.append( "." );
+        outputFileName.append( it.value() );
+      }
+    }
+  }
+
+  return outputFileName;
+}
+
+QString HeatmapGui::outputFormat()
+{
+  return mFormatCombo->itemData( mFormatCombo->currentIndex() ).toString();
+}
+
+QgsVectorLayer* HeatmapGui::inputVectorLayer()
+{
+  QString myLayerId = mInputVectorCombo->itemData( mInputVectorCombo->currentIndex() ).toString();
+
+  QgsVectorLayer* inputLayer = qobject_cast<QgsVectorLayer *>( QgsMapLayerRegistry::instance()->mapLayer( myLayerId ) );
+  if ( !inputLayer )
+  {
+    QMessageBox::information( 0, tr( "Layer not found" ), tr( "Layer %1 not found." ).arg( myLayerId ) );
+    return NULL;
+  }
+  return inputLayer;
+}
+
