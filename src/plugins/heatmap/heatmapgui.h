9fe4e2b82087c9af537e53a1cfc6786a7149a19c
Arunmozhi <aruntheguy@gmail.com>
2012-03-18 22:56:59 +0530

    merge pull request #104: - redesigned the dialog, added a options for - weighted buffer - raster size - moved the advanced stuff under advancedGroupBox - finished the GUI code - finally heatmap hits v0.2 Users can now  - specify the rows and columns for the raster  - specify the cell size of the raster  - Choose a field as weight for each point  - Choose a field to act as the buffer radius for each point  - Buffer radius can be specified in real-world units like meters & mapunits - indentation updated

--- src/plugins/heatmap/heatmapgui.h
+++ src/plugins/heatmap/heatmapgui.h
@@ -16,8 +16,10 @@
 #include <ui_heatmapguibase.h>
 
 #include "qgsvectorlayer.h"
+#include "qgscoordinatereferencesystem.h"
+#include "qgsgeometry.h"
 /**
-@author Tim Sutton
+@author Arunmozhi
 */
 class HeatmapGui : public QDialog, private Ui::HeatmapGuiBase
 {
@@ -26,29 +28,98 @@ class HeatmapGui : public QDialog, private Ui::HeatmapGuiBase
     HeatmapGui( QWidget* parent = 0, Qt::WFlags fl = 0 );
     ~HeatmapGui();
 
+    // Buffer unit type
+    // Should have been private, made public to be used in heatmap.cpp
+    enum mBufferType
+    {
+      Meters,
+      MapUnits
+    };
+
+    /** Returns whether to apply weighted heat */
+    bool weighted();
+
+    /** Returns whether the radius is static or based on a field */
+    bool variableRadius();
+
+    /** Returns the fixed radius value */
+    float radius();
+
+    /** Return the radius Unit (meters/map units) */
+    int radiusUnit();
+
+    /** Return the decay ratio */
+    float decayRatio();
+
+    /** Return the attribute field for variable radius */
+    int radiusField();
+
+    /** Returns the attrinute field for weighted heat */
+    int weightField();
+
+    /** Returns the output filename/path */
+    QString outputFilename();
+
+    /** Returns the GDAL Format for output raster */
+    QString outputFormat();
+
+    /** Returns the input Vector layer */
+    QgsVectorLayer* inputVectorLayer();
+
+    /** Returns the no of rows for the raster */
+    int rows() { return mRows; }
+
+    /** Returns the no of columns in the raster */
+    int columns() { return mColumns; }
+
+    /** Returns the cell size X value */
+    float cellSizeX() { return mXcellsize; }
+
+    /** Returns the cell size Y valuue */
+    float cellSizeY() { return mYcellsize; }
+
+    /** Return the BBox */
+    QgsRectangle bbox() { return mBBox; }
+
   private:
     QMap<QString, QString> mExtensionMap;
+
+    // bbox of layer for lineedit changes
+    QgsRectangle mBBox;
+    float mXcellsize, mYcellsize;
+    int mRows, mColumns;
+
+    /** Function to check wether all constrains are satisfied and enable the OK button */
     void enableOrDisableOkButton();
+
+    /** Populate the attribute fields from selected vector for radius&weight */
+    void populateFields();
+
+    /** Set the mBBox value - mainly used for updation purpose */
+    void updateBBox();
+
+    /** Update the LineEdits cellsize and row&col values  */
+    void updateSize();
+
+    /** Convert Maters value to the corresponding map units based on Layer projection */
+    float mapUnitsOf( float meters, QgsCoordinateReferenceSystem layerCrs );
 
   private slots:
     void on_mButtonBox_accepted();
     void on_mButtonBox_rejected();
     void on_mButtonBox_helpRequested();
-    void on_mBrowseButton_clicked(); // Function to open the file dialog
+    void on_mBrowseButton_clicked();
     void on_mOutputRasterLineEdit_editingFinished();
-
-  signals:
-    /*
-     * Signal: createRaster
-     * Params:
-     *         QgsVectorLayer* -> Input point layer
-     *         int             -> Buffer distance
-     *         float           -> Decay ratio
-     *         QString         -> Output filename
-     *         QString         -> Output Format Short Name
-     */
-    void createRaster( QgsVectorLayer*, int, float, QString, QString );
-
+    void on_advancedGroupBox_toggled( bool enabled );
+    void on_rowLineEdit_editingFinished();
+    void on_columnLineEdit_editingFinished();
+    void on_cellXLineEdit_editingFinished();
+    void on_cellYLineEdit_editingFinished();
+    void on_radiusFieldCombo_currentIndexChanged( int index );
+    void on_radiusFieldUnitCombo_currentIndexChanged( int index );
+    void on_mRadiusUnitCombo_currentIndexChanged( int index );
+    void on_mInputVectorCombo_currentIndexChanged( int index );
+    void on_mBufferLineEdit_editingFinished();
 };
 
 #endif
