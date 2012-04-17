24924a9e3a9b69eacc39d71fdbbe2eb2279031e4
Alexander Bruy <alexander.bruy@gmail.com>
2012-04-17 16:26:37 +0300

    fix About dialog in DB Manager

--- python/plugins/db_manager/dlg_about.py
+++ python/plugins/db_manager/dlg_about.py
@@ -3,30 +3,30 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-from .ui.DlgAbout_ui import Ui_DlgAbout
+from .ui.ui_DlgAbout import Ui_DlgAbout
 from .__init__ import name, description, version
 import platform
 
 try:
-	import resources
+  import resources
 except ImportError:
-	import resources_rc
+  import resources_rc
 
 class DlgAbout(QDialog, Ui_DlgAbout):
 
-	def __init__(self, parent=None):
-		QDialog.__init__(self, parent)
-		self.setupUi(self)
+  def __init__(self, parent=None):
+    QDialog.__init__(self, parent)
+    self.setupUi(self)
 
-		self.logo.setPixmap( QPixmap( ":/faunalia/logo" ) )
-		self.title.setText( name() )
-		self.description.setText( description() )
+    self.logo.setPixmap( QPixmap( ":/faunalia/logo" ) )
+    self.title.setText( name() )
+    self.description.setText( description() )
 
-		text = self.txt.toHtml()
-		text = text.replace( "$PLUGIN_NAME$", name() )
+    text = self.txt.toHtml()
+    text = text.replace( "$PLUGIN_NAME$", name() )
 
-		subject = "Help: %s" % name()
-		body = """\n\n
+    subject = "Help: %s" % name()
+    body = """\n\n
 --------
 Plugin name: %s
 Plugin version: %s
@@ -35,14 +35,14 @@ Platform: %s - %s
 --------
 """ % ( name(), version(), platform.python_version(), platform.system(), platform.version() )
 
-		mail = QUrl( "mailto:abc@abc.com" )
-		mail.addQueryItem( "subject", subject )
-		mail.addQueryItem( "body", body )
+    mail = QUrl( "mailto:abc@abc.com" )
+    mail.addQueryItem( "subject", subject )
+    mail.addQueryItem( "body", body )
 
-		text = text.replace( "$MAIL_SUBJECT$", unicode(mail.encodedQueryItemValue( "subject" )) )
-		text = text.replace( "$MAIL_BODY$", unicode(mail.encodedQueryItemValue( "body" )) )
+    text = text.replace( "$MAIL_SUBJECT$", unicode(mail.encodedQueryItemValue( "subject" )) )
+    text = text.replace( "$MAIL_BODY$", unicode(mail.encodedQueryItemValue( "body" )) )
 
-		self.txt.setHtml(text)
+    self.txt.setHtml(text)
 
 
 
