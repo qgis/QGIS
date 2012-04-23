a8a4c9dba8ec3a4ec27fc3ab8ae107e506921d4b
volayaf <volayaf@881b9c09-3ef8-f3c2-ec3d-21d735c97f4d>
2012-04-23 15:40:59 +0000

    added #5460
    git-svn-id: http://sextante.googlecode.com/svn/trunk/soft/bindings/qgis-plugin@143 881b9c09-3ef8-f3c2-ec3d-21d735c97f4d

--- src/sextante/gui/HTMLViewerDialog.py
+++ src/sextante/gui/HTMLViewerDialog.py
@@ -13,15 +13,27 @@ class HTMLViewerDialog(QtGui.QDialog):
     def setupUi(self):
         self.resize(600, 500)
         self.webView = QtWebKit.QWebView()
-        self.setWindowTitle("")
+        self.setWindowTitle("Help")
+        self.closeButton = QtGui.QPushButton()
+        self.closeButton.setText("Close")
+        self.closeButton.setMaximumWidth(150)
         self.horizontalLayout= QtGui.QHBoxLayout()
         self.horizontalLayout.setSpacing(2)
         self.horizontalLayout.setMargin(0)
-        self.horizontalLayout.addWidget(self.webView)
-        self.setLayout(self.horizontalLayout)
+        self.horizontalLayout.addStretch(1000)
+        self.horizontalLayout.addWidget(self.closeButton)
+        self.verticalLayout= QtGui.QVBoxLayout()
+        self.verticalLayout.setSpacing(2)
+        self.verticalLayout.setMargin(0)
+        self.verticalLayout.addWidget(self.webView)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.setLayout(self.verticalLayout)
+        QObject.connect(self.closeButton, QtCore.SIGNAL("clicked()"), self.closeWindow)
         try:
             url = QtCore.QUrl(self.filename)
             self.webView.load(url)
         except:
             pass
 
+    def closeWindow(self):
+        self.close()
