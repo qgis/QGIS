049f5ca83a93b5900d2c828b213db566c4f7ac9c
Alexander Bruy <alexander.bruy@gmail.com>
2012-05-22 14:22:26 +0300

    fix #5617. Patch submitted by Salvatore Larosa, thanks!

--- python/plugins/fTools/tools/doGeoprocessing.py
+++ python/plugins/fTools/tools/doGeoprocessing.py
@@ -1147,18 +1147,20 @@ class geoprocessingThread( QThread ):
                 temp_list = int_geom.asGeometryCollection()
                 for i in temp_list:
                   if i.type() == geom.type():
+                    int_geom = QgsGeometry( i )
                     try:
-                      outFeat.setGeometry( j )
+                      outFeat.setGeometry( int_geom )
                       outFeat.setAttributeMap( ftools_utils.combineVectorAttributes( atMapA, atMapB ) )
                       writer.addFeature( outFeat )
                     except Exception, err:
                       FEATURE_EXCEPT = False
-              try:
-                outFeat.setGeometry( int_geom )
-                outFeat.setAttributeMap( ftools_utils.combineVectorAttributes( atMapA, atMapB ) )
-                writer.addFeature( outFeat )
-              except Exception, err:
-                FEATURE_EXCEPT = False
+              else:
+                try:
+                  outFeat.setGeometry( int_geom )
+                  outFeat.setAttributeMap( ftools_utils.combineVectorAttributes( atMapA, atMapB ) )
+                  writer.addFeature( outFeat )
+                except Exception, err:
+                  FEATURE_EXCEPT = False
             else:
               # this only happends if the bounding box
               # intersects, but the geometry doesn't
