qgis/QGIS

commit 04aab2938c164d3c4a9ee9da576aabca13c227ca

do not show file extension in TOC, add QqsLayerItem::layerName() for getting layer name (bug #5621)

src/providers/ogr/qgsogrdataitems.cpp
@@ -78,7 +78,8 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
   // we are able to assign CRS only to shapefiles :-(
   if ( driverName == "ESRI Shapefile" )
   {
-    QString layerName = mPath.left( mPath.indexOf( ".shp", Qt::CaseInsensitive ) );
+    // QString layerName = mPath.left( mPath.indexOf( ".shp", Qt::CaseInsensitive ) );
+    QString lyrName = layerName();
     QString wkt = crs.toWkt();
 
     // save ordinary .prj file
@@ -86,7 +87,7 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
     OSRMorphToESRI( hSRS ); // this is the important stuff for shapefile .prj
     char* pszOutWkt = NULL;
     OSRExportToWkt( hSRS, &pszOutWkt );
-    QFile prjFile( layerName + ".prj" );
+    QFile prjFile( lyrName + ".prj" );
     if ( prjFile.open( QIODevice::WriteOnly ) )
     {
       QTextStream prjStream( &prjFile );
@@ -95,14 +96,14 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
     }
     else
     {
-      QgsMessageLog::logMessage( tr( "Couldn't open file %1.prj" ).arg( layerName ), tr( "OGR" ) );
+      QgsMessageLog::logMessage( tr( "Couldn't open file %1.prj" ).arg( lyrName ), tr( "OGR" ) );
       return false;
     }
     OSRDestroySpatialReference( hSRS );
     CPLFree( pszOutWkt );
 
     // save qgis-specific .qpj file (maybe because of better wkt compatibility?)
-    QFile qpjFile( layerName + ".qpj" );
+    QFile qpjFile( lyrName + ".qpj" );
     if ( qpjFile.open( QIODevice::WriteOnly ) )
     {
       QTextStream qpjStream( &qpjFile );
@@ -111,7 +112,7 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
     }
     else
     {
-      QgsMessageLog::logMessage( tr( "Couldn't open file %1.qpj" ).arg( layerName ), tr( "OGR" ) );
+      QgsMessageLog::logMessage( tr( "Couldn't open file %1.qpj" ).arg( lyrName ), tr( "OGR" ) );
       return false;
     }
 
@@ -121,6 +122,15 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
   // It it is impossible to assign a crs to an existing layer
   // No OGR_L_SetSpatialRef : http://trac.osgeo.org/gdal/ticket/4032
   return false;
+}
+
+QString QgsOgrLayerItem::layerName() const
+{
+  QFileInfo info( name() );
+  if ( info.suffix() == "gz" )
+    return info.baseName();
+  else
+    return info.completeBaseName();
 }
 
 // -------

src/app/qgisapp.cpp
@@ -6920,8 +6920,8 @@ bool QgisApp::addRasterLayers( QStringList const &theFileNameQStringList, bool g
       QFileInfo myFileInfo( *myIterator );
       // get the directory the .adf file was in
       QString myDirNameQString = myFileInfo.path();
-      //extract basename with extension
-      QString myBaseNameQString = myFileInfo.completeBaseName() + "." + myFileInfo.suffix();
+      //extract basename
+      QString myBaseNameQString = myFileInfo.completeBaseName();
       //only allow one copy of a ai grid file to be loaded at a
       //time to prevent the user selecting all adfs in 1 dir which
       //actually represent 1 coverage,

src/app/qgsbrowserdockwidget.cpp
@@ -303,7 +303,7 @@ void QgsBrowserDockWidget::addLayer( QgsLayerItem *layerItem )
   QgsDebugMsg( providerKey + " : " + uri );
   if ( type == QgsMapLayer::VectorLayer )
   {
-    QgisApp::instance()->addVectorLayer( uri, layerItem->name(), providerKey );
+    QgisApp::instance()->addVectorLayer( uri, layerItem->layerName(), providerKey );
   }
   if ( type == QgsMapLayer::RasterLayer )
   {
@@ -333,7 +333,7 @@ void QgsBrowserDockWidget::addLayer( QgsLayerItem *layerItem )
     QgsDebugMsg( "rasterLayerPath = " + rasterLayerPath );
     QgsDebugMsg( "layers = " + layers.join( " " ) );
 
-    QgisApp::instance()->addRasterLayer( rasterLayerPath, layerItem->name(), providerKey, layers, styles, format, crs );
+    QgisApp::instance()->addRasterLayer( rasterLayerPath, layerItem->layerName(), providerKey, layers, styles, format, crs );
   }
 }
 

src/core/qgsdataitem.h
@@ -194,6 +194,8 @@ class CORE_EXPORT QgsLayerItem : public QgsDataItem
     static const QIcon &iconTable();
     static const QIcon &iconRaster();
     static const QIcon &iconDefault();
+
+    virtual QString layerName() const { return name(); }
 };
 
 

src/core/qgsmimedatautils.cpp
@@ -20,7 +20,7 @@
 static const char* QGIS_URILIST_MIMETYPE = "application/x-vnd.qgis.qgis.uri";
 
 QgsMimeDataUtils::Uri::Uri( QgsLayerItem* layerItem )
-    : providerKey( layerItem->providerKey() ), name( layerItem->name() ), uri( layerItem->uri() )
+    : providerKey( layerItem->providerKey() ), name( layerItem->layerName() ), uri( layerItem->uri() )
 {
   switch ( layerItem->mapLayerType() )
   {

src/providers/gdal/qgsgdaldataitems.cpp
@@ -103,6 +103,15 @@ QVector<QgsDataItem*> QgsGdalLayerItem::createChildren( )
   return children;
 }
 
+QString QgsGdalLayerItem::layerName() const
+{
+  QFileInfo info( name() );
+  if ( info.suffix() == "gz" )
+    return info.baseName();
+  else
+    return info.completeBaseName();
+}
+
 // ---------------------------------------------------------------------------
 
 static QString filterString;

src/providers/gdal/qgsgdaldataitems.h
@@ -34,6 +34,7 @@ class QgsGdalLayerItem : public QgsLayerItem
 
     QVector<QgsDataItem*> createChildren();
 
+    QString layerName() const;
 };
 
 

src/providers/ogr/qgsogrdataitems.h
@@ -28,6 +28,7 @@ class QgsOgrLayerItem : public QgsLayerItem
 
     bool setCrs( QgsCoordinateReferenceSystem crs );
     Capability capabilities();
+    QString layerName() const;
 };
 
 class QgsOgrDataCollectionItem : public QgsDataCollectionItem

commit 9859709e924be5d0193fde5cc1fd82b6e332d879

fix set crs to OGR data items (broken in 04aab2938)

src/providers/ogr/qgsogrdataitems.cpp
@@ -78,8 +78,7 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
   // we are able to assign CRS only to shapefiles :-(
   if ( driverName == "ESRI Shapefile" )
   {
-    // QString layerName = mPath.left( mPath.indexOf( ".shp", Qt::CaseInsensitive ) );
-    QString lyrName = layerName();
+    QString layerName = mPath.left( mPath.indexOf( ".shp", Qt::CaseInsensitive ) );
     QString wkt = crs.toWkt();
 
     // save ordinary .prj file
@@ -87,7 +86,7 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
     OSRMorphToESRI( hSRS ); // this is the important stuff for shapefile .prj
     char* pszOutWkt = NULL;
     OSRExportToWkt( hSRS, &pszOutWkt );
-    QFile prjFile( lyrName + ".prj" );
+    QFile prjFile( layerName + ".prj" );
     if ( prjFile.open( QIODevice::WriteOnly ) )
     {
       QTextStream prjStream( &prjFile );
@@ -96,14 +95,14 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
     }
     else
     {
-      QgsMessageLog::logMessage( tr( "Couldn't open file %1.prj" ).arg( lyrName ), tr( "OGR" ) );
+      QgsMessageLog::logMessage( tr( "Couldn't open file %1.prj" ).arg( layerName ), tr( "OGR" ) );
       return false;
     }
     OSRDestroySpatialReference( hSRS );
     CPLFree( pszOutWkt );
 
     // save qgis-specific .qpj file (maybe because of better wkt compatibility?)
-    QFile qpjFile( lyrName + ".qpj" );
+    QFile qpjFile( layerName + ".qpj" );
     if ( qpjFile.open( QIODevice::WriteOnly ) )
     {
       QTextStream qpjStream( &qpjFile );
@@ -112,7 +111,7 @@ bool QgsOgrLayerItem::setCrs( QgsCoordinateReferenceSystem crs )
     }
     else
     {
-      QgsMessageLog::logMessage( tr( "Couldn't open file %1.qpj" ).arg( lyrName ), tr( "OGR" ) );
+      QgsMessageLog::logMessage( tr( "Couldn't open file %1.qpj" ).arg( layerName ), tr( "OGR" ) );
       return false;
     }
 