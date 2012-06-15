9772d841bbc201222238a3f8aa0e56eaa9348fcd
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2012-06-15 16:47:33 +0200

    Forgot to add license header

--- src/gui/raster/qgsrasterrendererwidget.cpp
+++ src/gui/raster/qgsrasterrendererwidget.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgsrasterrendererwidget.cpp
+                         ---------------------------
+    begin                : June 2012
+    copyright            : (C) 2012 by Marco Hugentobler
+    email                : marco at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsrasterrendererwidget.h"
 #include "qgsrasterdataprovider.h"
 #include "qgsrasterlayer.h"
