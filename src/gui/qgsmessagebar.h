45942aa59c1b269e34d6f85ce106ffd9d2648cde
Giuseppe Sucameli <brush.tyler@gmail.com>
2012-08-30 16:35:15 +0200

    move QgsMessageBar class to GUI library, improve its doc

--- src/gui/qgsmessagebar.h
+++ src/gui/qgsmessagebar.h
@@ -31,29 +31,46 @@ class QToolButton;
 
 /** \ingroup gui
  * A bar for displaying non-blocking messages to the user.
- * \note added in 1.8
+ * \note added in 1.9
  */
-class QgsMessageBar: public QFrame
+class GUI_EXPORT QgsMessageBar: public QFrame
 {
     Q_OBJECT
+
   public:
     QgsMessageBar( QWidget *parent = 0 );
     ~QgsMessageBar();
 
+    /*! display a widget on the bar after hiding the currently visible one
+     *  and putting it in a stack
+     * @param level is 0 for information, 1 for warning, 2 for critical
+     */
     void pushWidget( QWidget *widget, int level = 0 );
-    void pushWidget( QWidget *widget, const QString &styleSheet );
 
+    /*! remove the passed widget from the bar (if previously added),
+     *  then display the next one in the stack if any or hide the bar
+     *  @return true if the widget was removed, false otherwise
+     */
     bool popWidget( QWidget *widget );
 
+    //! make out a widget containing a message to be displayed on the bar
     static QWidget* createMessage( const QString &text, QWidget *parent = 0 ) { return createMessage( QString::null, text, QIcon(), parent ); }
+    //! make out a widget containing icon and message to be displayed on the bar
     static QWidget* createMessage( const QString &text, const QIcon &icon, QWidget *parent = 0 ) { return createMessage( QString::null, text, icon, parent ); }
+    //! make out a widget containing title and message to be displayed on the bar
     static QWidget* createMessage( const QString &title, const QString &text, QWidget *parent = 0 ) { return createMessage( title, text, QIcon(), parent ); }
+    //! make out a widget containing icon, title and message to be displayed on the bar
     static QWidget* createMessage( const QString &title, const QString &text, const QIcon &icon, QWidget *parent = 0 );
 
   signals:
+    //! emitted when a widget was removed from the bar
     void widgetRemoved( QWidget *widget );
 
   public slots:
+    /*! remove the currently displayed widget from the bar and
+     *  display the next in the stack if any or hide the bar
+     *  @return true if the widget was removed, false otherwise
+     */
     bool popWidget();
 
   private:
@@ -71,6 +88,9 @@ class QgsMessageBar: public QFrame
         QWidget *mWidget;
         QString mStyleSheet;
     };
+
+    //! display a widget on the bar
+    void pushWidget( QWidget *widget, const QString &styleSheet );
 
     void popItem( QgsMessageBarItem *item );
     void pushItem( QgsMessageBarItem *item );
