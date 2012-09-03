c322cd52cf82afeb10beefa6efa42da2bdcea46f
szekerest <szekerest@gmail.com>
2012-09-03 11:01:10 +0200

    Specify port 1433 for FreeTDS by default. Allow to specify server/database/uid/password when the driver setting is present (fixes #6259, fixes #6263)

--- src/providers/mssql/qgsmssqlprovider.cpp
+++ src/providers/mssql/qgsmssqlprovider.cpp
@@ -214,19 +214,20 @@ QSqlDatabase QgsMssqlProvider::GetDatabase( QString driver, QString host, QStrin
 #ifdef WIN32
     connectionString = "driver={SQL Server}";
 #else
-    connectionString = "driver={FreeTDS}";
+    connectionString = "driver={FreeTDS};port=1433";
 #endif
-    if ( !host.isEmpty() )
-      connectionString += ";server=" + host;
-
-    if ( !database.isEmpty() )
-      connectionString += ";database=" + database;
-
-    if ( password.isEmpty() )
-      connectionString += ";trusted_connection=yes";
-    else
-      connectionString += ";uid=" + username + ";pwd=" + password;
   }
+
+  if ( !host.isEmpty() )
+    connectionString += ";server=" + host;
+
+  if ( !database.isEmpty() )
+    connectionString += ";database=" + database;
+
+  if ( password.isEmpty() )
+    connectionString += ";trusted_connection=yes";
+  else
+    connectionString += ";uid=" + username + ";pwd=" + password;
 
   if ( !username.isEmpty() )
     db.setUserName( username );
