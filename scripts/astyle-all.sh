8a9b27fc2c73604d100d50e5dd9403b93596c726
Marco Bernasocchi <marco@opengis.ch>
2012-09-06 09:17:54 +0700

    removing obsolete qtmain_android.cpp

--- scripts/astyle-all.sh
+++ scripts/astyle-all.sh
@@ -8,7 +8,7 @@ export elcr="$(tput el)$(tput cr)"
 
 find src tests -type f -print | while read f; do
 	case "$f" in
-        src/app/gps/qwtpolar-*|src/app/qtmain_android.cpp|src/core/spatialite/*|src/core/spatialindex/src/*|src/core/gps/qextserialport/*|src/plugins/grass/qtermwidget/*|src/astyle/*|python/pyspatialite/*|src/providers/sqlanywhere/sqlanyconnection/*)
+        src/app/gps/qwtpolar-*|src/core/spatialite/*|src/core/spatialindex/src/*|src/core/gps/qextserialport/*|src/plugins/grass/qtermwidget/*|src/astyle/*|python/pyspatialite/*|src/providers/sqlanywhere/sqlanyconnection/*)
                 echo $f skipped
                 continue
                 ;;
