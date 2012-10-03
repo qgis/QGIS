fa7d689f3e18ce6c8d99cf7fe4d94bc202cfa580
Alexander Bruy <alexander.bruy@gmail.com>
2012-10-03 19:18:28 +0300

    review buffer tools

--- python/plugins/sextante/ftools/FixedDistanceBuffer.py
+++ python/plugins/sextante/ftools/FixedDistanceBuffer.py
@@ -1,14 +1,19 @@
-from sextante.core.GeoAlgorithm import GeoAlgorithm
 import os.path
+
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+
 from qgis.core import *
-from sextante.parameters.ParameterVector import ParameterVector
+
+from sextante.core.GeoAlgorithm import GeoAlgorithm
 from sextante.core.QGisLayers import QGisLayers
-from sextante.outputs.OutputVector import OutputVector
+
+from sextante.parameters.ParameterVector import ParameterVector
 from sextante.parameters.ParameterBoolean import ParameterBoolean
 from sextante.parameters.ParameterNumber import ParameterNumber
+
+from sextante.outputs.OutputVector import OutputVector
+
 from sextante.ftools import Buffer as buff
 
 class FixedDistanceBuffer(GeoAlgorithm):
@@ -24,27 +29,28 @@ class FixedDistanceBuffer(GeoAlgorithm):
     def getIcon(self):
         return QtGui.QIcon(os.path.dirname(__file__) + "/icons/buffer.png")
 
-    def processAlgorithm(self, progress):
-        vlayer = QGisLayers.getObjectFromUri(self.getParameterValue(FixedDistanceBuffer.INPUT))
-        vprovider = vlayer.dataProvider()
-        allAttrs = vprovider.attributeIndexes()
-        vprovider.select(allAttrs)
-        writer = self.getOutputFromName(FixedDistanceBuffer.OUTPUT).getVectorWriter(vprovider.fields(), QGis.WKBPolygon, vprovider.crs() )
-        useSelection = self.getParameterValue(FixedDistanceBuffer.USE_SELECTED)
-        distance = self.getParameterValue(FixedDistanceBuffer.DISTANCE)
-        dissolve = self.getParameterValue(FixedDistanceBuffer.DISSOLVE)
-        segments = int(self.getParameterValue(FixedDistanceBuffer.SEGMENTS))
-        layer = QGisLayers.getObjectFromUri(self.getParameterValue(FixedDistanceBuffer.INPUT))
-        buff.buffering(progress, writer, distance, None, useSelection, False, layer, dissolve, segments)
-
     def defineCharacteristics(self):
         self.name = "Fixed distance buffer"
         self.group = "Geoprocessing tools"
-        self.addParameter(ParameterVector(FixedDistanceBuffer.INPUT, "Input layer", ParameterVector.VECTOR_TYPE_ANY))
-        self.addParameter(ParameterBoolean(FixedDistanceBuffer.USE_SELECTED, "Use selected features", False))
-        self.addParameter(ParameterNumber(FixedDistanceBuffer.DISTANCE, "Distance", 0, default=10 ))
-        self.addParameter(ParameterNumber(FixedDistanceBuffer.SEGMENTS, "Segments", 1, default=10 ))
-        self.addParameter(ParameterBoolean(FixedDistanceBuffer.DISSOLVE, "Dissolve result", True))
-        self.addOutput(OutputVector(FixedDistanceBuffer.OUTPUT, "Buffer"))
 
-    #=========================================================
+        self.addParameter(ParameterVector(self.INPUT, "Input layer", ParameterVector.VECTOR_TYPE_ANY))
+        self.addParameter(ParameterBoolean(self.USE_SELECTED, "Use selected features", False))
+        self.addParameter(ParameterNumber(self.DISTANCE, "Distance", 0.0, default=10.0))
+        self.addParameter(ParameterNumber(self.SEGMENTS, "Segments", 1, default=5))
+        self.addParameter(ParameterBoolean(self.DISSOLVE, "Dissolve result", False))
+
+        self.addOutput(OutputVector(self.OUTPUT, "Buffer"))
+
+    def processAlgorithm(self, progress):
+        layer = QGisLayers.getObjectFromUri(self.getParameterValue(self.INPUT))
+        useSelection = self.getParameterValue(self.USE_SELECTED)
+        distance = self.getParameterValue(self.DISTANCE)
+        dissolve = self.getParameterValue(self.DISSOLVE)
+        segments = int(self.getParameterValue(self.SEGMENTS))
+
+        provider = layer.dataProvider()
+        writer = self.getOutputFromName(self.OUTPUT).getVectorWriter(layer.pendingFields(),
+                     QGis.WKBPolygon, provider.crs())
+
+        buff.buffering(progress, writer, distance, None, useSelection, False,
+                       layer, dissolve, segments)
