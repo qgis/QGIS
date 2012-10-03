9f13601f801bbf944ed2f77f713d0e41c4013ff2
Alexander Bruy <alexander.bruy@gmail.com>
2012-10-03 18:58:56 +0300

    review Random selection and Random selection within subsets

--- python/plugins/sextante/ftools/RandomSelection.py
+++ python/plugins/sextante/ftools/RandomSelection.py
@@ -1,15 +1,20 @@
-from sextante.core.GeoAlgorithm import GeoAlgorithm
 import os.path
+import random
+
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+
 from qgis.core import *
-from sextante.parameters.ParameterVector import ParameterVector
+
+from sextante.core.GeoAlgorithm import GeoAlgorithm
+from sextante.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
 from sextante.core.QGisLayers import QGisLayers
-from sextante.outputs.OutputVector import OutputVector
+
 from sextante.parameters.ParameterSelection import ParameterSelection
+from sextante.parameters.ParameterVector import ParameterVector
 from sextante.parameters.ParameterNumber import ParameterNumber
-import random
+
+from sextante.outputs.OutputVector import OutputVector
 
 class RandomSelection(GeoAlgorithm):
 
@@ -17,30 +22,41 @@ class RandomSelection(GeoAlgorithm):
     OUTPUT = "OUTPUT"
     METHOD = "METHOD"
     NUMBER = "NUMBER"
-    PERCENTAGE = "PERCENTAGE"
+
+    METHODS = ["Number of selected features",
+               "Percentage of selected features"
+              ]
 
     def getIcon(self):
         return QtGui.QIcon(os.path.dirname(__file__) + "/icons/random_selection.png")
 
-    def processAlgorithm(self, progress):
-        filename = self.getParameterValue(RandomSelection.INPUT)
-        layer = QGisLayers.getObjectFromUri(filename)
-        method = self.getParameterValue(self.METHOD)
-        if method == 0:
-            value = int(self.getParameterValue(self.NUMBER))
-        else:
-            value = self.getParameterValue(self.PERCENTAGE)
-            value = int(round((value / 100.0000), 4) * layer.featureCount())
-        selran = random.sample(xrange(0, layer.featureCount()), value)
-        layer.setSelectedFeatures(selran)
-        self.setOutputValue(self.OUTPUT, filename)
-
-
     def defineCharacteristics(self):
         self.name = "Random selection"
         self.group = "Research tools"
-        self.addParameter(ParameterVector(RandomSelection.INPUT, "Input layer", ParameterVector.VECTOR_TYPE_ANY))
-        self.addParameter(ParameterSelection(RandomSelection.METHOD, "Method", ["Number of selected features", "Percentage of selected features"]))
-        self.addParameter(ParameterNumber(RandomSelection.NUMBER, "Number of selected features", 1, None, 10))
-        self.addParameter(ParameterNumber(RandomSelection.PERCENTAGE, "Percentage of selected features", 0, 100, 50))
-        self.addOutput(OutputVector(RandomSelection.OUTPUT, "Selection", True))
+
+        self.addParameter(ParameterVector(self.INPUT, "Input layer", ParameterVector.VECTOR_TYPE_ANY))
+        self.addParameter(ParameterSelection(self.METHOD, "Method", self.METHODS, 0))
+        self.addParameter(ParameterNumber(self.NUMBER, "Number/persentage of selected features", 1, None, 10))
+        self.addOutput(OutputVector(self.OUTPUT, "Selection", True))
+
+    def processAlgorithm(self, progress):
+        filename = self.getParameterValue(self.INPUT)
+        layer = QGisLayers.getObjectFromUri(filename)
+        method = self.getParameterValue(self.METHOD)
+
+        featureCount = layer.featureCount()
+
+        value = int(self.getParameterValue(self.NUMBER))
+
+        if method == 0:
+            if value > featureCount:
+                raise GeoAlgorithmExecutionException("Selected number is greater that feature count. Choose lesser value and try again.")
+        else:
+            if value > 100:
+                raise GeoAlgorithmExecutionException("Persentage can't be greater than 100. Set corrent value and try again.")
+            value = int(round((value / 100.0000), 4) * featureCount)
+
+        selran = random.sample(xrange(0, featureCount), value)
+
+        layer.setSelectedFeatures(selran)
+        self.setOutputValue(self.OUTPUT, filename)
