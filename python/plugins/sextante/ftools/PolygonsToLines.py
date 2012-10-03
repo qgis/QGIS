3fbc3af901caf316cb510e2690096d3ee9352a5c
Alexander Bruy <alexander.bruy@gmail.com>
2012-10-03 18:20:29 +0300

    review Lines to polygons and Polygons to lines tools

--- python/plugins/sextante/ftools/PolygonsToLines.py
+++ python/plugins/sextante/ftools/PolygonsToLines.py
@@ -1,12 +1,15 @@
-from sextante.core.GeoAlgorithm import GeoAlgorithm
 import os.path
+
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+
 from qgis.core import *
-from sextante.parameters.ParameterVector import ParameterVector
+
+from sextante.core.GeoAlgorithm import GeoAlgorithm
 from sextante.core.QGisLayers import QGisLayers
-from sextante.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
+
+from sextante.parameters.ParameterVector import ParameterVector
+
 from sextante.outputs.OutputVector import OutputVector
 
 class PolygonsToLines(GeoAlgorithm):
@@ -17,52 +20,63 @@ class PolygonsToLines(GeoAlgorithm):
     def getIcon(self):
         return QtGui.QIcon(os.path.dirname(__file__) + "/icons/to_lines.png")
 
+    def defineCharacteristics(self):
+        self.name = "Polygons to lines"
+        self.group = "Geometry tools"
+
+        self.addParameter(ParameterVector(self.INPUT, "Input layer", ParameterVector.VECTOR_TYPE_POLYGON))
+
+        self.addOutput(OutputVector(self.OUTPUT, "Output layer"))
+
     def processAlgorithm(self, progress):
-        vlayer = QGisLayers.getObjectFromUri(self.getParameterValue(PolygonsToLines.INPUT))
-        vprovider = vlayer.dataProvider()
-        allAttrs = vprovider.attributeIndexes()
-        vprovider.select( allAttrs )
-        fields = vprovider.fields()
-        writer = self.getOutputFromName(self.OUTPUT).getVectorWriter(fields, QGis.WKBLineString, vprovider.crs() )
+        settings = QSettings()
+        encoding = settings.value("/UI/encoding", "System").toString()
+
+        layer = QGisLayers.getObjectFromUri(self.getParameterValue(self.INPUT))
+        output = self.getOutputValue(self.OUTPUT)
+
+        provider = layer.dataProvider()
+        layer.select(layer.pendingAllAttributesList())
+
+        writer = self.getOutputFromName(self.OUTPUT).getVectorWriter(layer.pendingFields(),
+                     QGis.WKBLineString, provider.crs())
+
         inFeat = QgsFeature()
         outFeat = QgsFeature()
         inGeom = QgsGeometry()
         outGeom = QgsGeometry()
-        nFeat = vprovider.featureCount()
-        nElement = 0
-        while vprovider.nextFeature(inFeat):
-          multi = False
-          nElement += 1
-          progress.setPercentage(int(nElement/nFeat * 100))
-          inGeom = inFeat.geometry()
-          if inGeom.isMultipart():
-            multi = True
-          atMap = inFeat.attributeMap()
-          lineList = self.extractAsLine( inGeom )
-          outFeat.setAttributeMap( atMap )
-          for h in lineList:
-            outFeat.setGeometry( outGeom.fromPolyline( h ) )
-            writer.addFeature( outFeat )
+
+        current = 0
+        total = 100.0 / float(provider.featureCount())
+
+        while layer.nextFeature(inFeat):
+            multi = False
+            inGeom = inFeat.geometry()
+            if inGeom.isMultipart():
+                multi = True
+            atMap = inFeat.attributeMap()
+            lineList = self.extractAsLine(inGeom)
+            outFeat.setAttributeMap(atMap)
+            for h in lineList:
+              outFeat.setGeometry(outGeom.fromPolyline(h))
+              writer.addFeature(outFeat)
+
+            current += 1
+            progress.setPercentage(int(current * total))
+
         del writer
 
-    def extractAsLine( self, geom ):
-        multi_geom = QgsGeometry()
-        temp_geom = []
-        if geom.type() == 2:
+    def extractAsLine(self, geom):
+        multiGeom = QgsGeometry()
+        lines = []
+        if geom.type() == QGis.Polygon:
           if geom.isMultipart():
-            multi_geom = geom.asMultiPolygon()
-            for i in multi_geom:
-              temp_geom.extend(i)
+              multiGeom = geom.asMultiPolygon()
+              for i in multiGeom:
+                  lines.extend(i)
           else:
-            multi_geom = geom.asPolygon()
-            temp_geom = multi_geom
-          return temp_geom
+              multiGeom = geom.asPolygon()
+              lines = multiGeom
+          return lines
         else:
-          return []
-
-    def defineCharacteristics(self):
-        self.name = "Polygons to lines"
-        self.group = "Geometry tools"
-        self.addParameter(ParameterVector(PolygonsToLines.INPUT, "Input layer", ParameterVector.VECTOR_TYPE_POLYGON))
-        self.addOutput(OutputVector(PolygonsToLines.OUTPUT, "Output layer"))
-    #=========================================================
+            return []
