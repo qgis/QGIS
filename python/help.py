7b4a9b68a32f75a876b8b19552cb5cc7dbe9fd52
Juergen E. Fischer <jef@norbit.de>
2012-10-04 19:33:47 +0200

    add copyright notices

--- python/help.py
+++ python/help.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    help.py
+    ---------------------
+    Date                 : September 2012
+    Copyright            : (C) 2012 by Salvatore Larosa
+    Email                : lrssvtml at gmail dot com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Salvatore Larosa'
+__date__ = 'September 2012'
+__copyright__ = '(C) 2012, Salvatore Larosa'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
 from PyQt4 import QtCore, QtGui, QtWebKit
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
