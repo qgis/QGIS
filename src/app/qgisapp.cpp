63ebe67ad1f3a2cdd175edf26425b3081fd35acc
Etienne Tourigny <etourigny.dev@gmail.com>
2012-11-20 16:02:46 -0200

    fix legend refresh

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -7968,7 +7968,7 @@ void QgisApp::showLayerProperties( QgsMapLayer *ml )
     else
     {
       rlp = new QgsRasterLayerProperties( ml, mMapCanvas, this );
-      connect( rlp, SIGNAL( refreshLegend( QString, QgsLegendItem::Expansion ) ), mMapLegend, SLOT( refreshLayerSymbology( QString, QgsLegendItem::Expansion ) ) );
+      connect( rlp, SIGNAL( refreshLegend( QString, bool ) ), mMapLegend, SLOT( refreshLayerSymbology( QString, bool ) ) );
     }
 
     rlp->exec();
