95b506882dd420d98ebfe63eebacc73012c77348
Alexander Bruy <alexander.bruy@gmail.com>
2012-12-01 16:48:56 +0200

    create history dialog from .ui file

--- python/plugins/sextante/gui/HistoryDialog.py
+++ python/plugins/sextante/gui/HistoryDialog.py
@@ -25,56 +25,34 @@ __revision__ = '$Format:%H$'
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from PyQt4 import QtCore, QtGui
+
 from sextante.core.SextanteLog import SextanteLog
 
-class HistoryDialog(QtGui.QDialog):
+from sextante.ui.ui_DlgHistory import Ui_DlgHistory
+
+class HistoryDialog(QDialog, Ui_DlgHistory):
     def __init__(self):
-        QtGui.QDialog.__init__(self)
-        self.setModal(True)
-        self.setupUi()
+        QDialog.__init__(self)
+        self.setupUi(self)
 
-    def setupUi(self):
-        self.resize(650, 505)
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setSpacing(2)
-        self.verticalLayout.setMargin(0)
-        self.tree = QtGui.QTreeWidget()
-        self.tree.setHeaderHidden(True)
+        self.groupIcon = QIcon()
+        self.groupIcon.addPixmap(self.style().standardPixmap(QStyle.SP_DirClosedIcon),
+                QIcon.Normal, QIcon.Off)
+        self.groupIcon.addPixmap(self.style().standardPixmap(QStyle.SP_DirOpenIcon),
+                QIcon.Normal, QIcon.On)
+
+        self.keyIcon = QIcon()
+        self.keyIcon.addPixmap(self.style().standardPixmap(QStyle.SP_FileIcon))
+
+        self.clearButton = QPushButton(self.tr("Clear"))
+        self.clearButton.setToolTip(self.tr("Clear history and log"))
+        self.buttonBox.addButton(self.clearButton, QDialogButtonBox.ActionRole)
+
         self.tree.doubleClicked.connect(self.executeAlgorithm)
-        self.verticalLayout.addWidget(self.tree)
         self.tree.currentItemChanged.connect(self.changeText)
-        self.groupIcon = QtGui.QIcon()
-        self.groupIcon.addPixmap(self.style().standardPixmap(QtGui.QStyle.SP_DirClosedIcon),
-                QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.groupIcon.addPixmap(self.style().standardPixmap(QtGui.QStyle.SP_DirOpenIcon),
-                QtGui.QIcon.Normal, QtGui.QIcon.On)
-        self.keyIcon = QtGui.QIcon()
-        self.keyIcon.addPixmap(self.style().standardPixmap(QtGui.QStyle.SP_FileIcon))
+        self.clearButton.clicked.connect(self.clearLog)
+
         self.fillTree()
-        self.text = QtGui.QTextEdit()
-        self.verticalLayout.addWidget(self.text)
-        self.text.setReadOnly(True)
-        self.closeButton = QtGui.QPushButton()
-        self.closeButton.setText("Close")
-        self.clearButton = QtGui.QPushButton()
-        self.clearButton.setText("Clear history")
-        self.horizontalLayout= QtGui.QHBoxLayout()
-        self.horizontalLayout.setSpacing(2)
-        self.horizontalLayout.setMargin(0)
-        self.horizontalLayout.addStretch(1000)
-        self.horizontalLayout.addWidget(self.clearButton)
-        self.horizontalLayout.addWidget(self.closeButton)
-        QObject.connect(self.closeButton, QtCore.SIGNAL("clicked()"), self.closeWindow)
-        QObject.connect(self.clearButton, QtCore.SIGNAL("clicked()"), self.clearLog)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.setWindowTitle("History")
-        self.setLayout(self.verticalLayout)
-        QtCore.QMetaObject.connectSlotsByName(self)
-
-
-    def closeWindow(self):
-        self.close()
 
     def clearLog(self):
         SextanteLog.clearLog()
@@ -84,7 +62,7 @@ class HistoryDialog(QtGui.QDialog):
         self.tree.clear()
         elements = SextanteLog.getLogEntries()
         for category in elements.keys():
-            groupItem = QtGui.QTreeWidgetItem()
+            groupItem = QTreeWidgetItem()
             groupItem.setText(0,category)
             groupItem.setIcon(0, self.groupIcon)
             for entry in elements[category]:
@@ -92,7 +70,6 @@ class HistoryDialog(QtGui.QDialog):
                 item.setIcon(0, self.keyIcon)
                 groupItem.insertChild(0,item)
             self.tree.addTopLevelItem(groupItem)
-
 
     def executeAlgorithm(self):
         item = self.tree.currentItem()
@@ -102,14 +79,12 @@ class HistoryDialog(QtGui.QDialog):
                 script+=item.entry.text.replace("runalg(","runandload(")
                 exec(script)
 
-
     def changeText(self):
         item = self.tree.currentItem()
         if isinstance(item, TreeLogEntryItem):
                 self.text.setText(item.entry.text.replace("|","\n"))
 
-
-class TreeLogEntryItem(QtGui.QTreeWidgetItem):
+class TreeLogEntryItem(QTreeWidgetItem):
     def __init__(self, entry, isAlg):
         QTreeWidgetItem.__init__(self)
         self.entry = entry
