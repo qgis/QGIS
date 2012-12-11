d5bc39cb8ccc047543d37359ae7fc2d34c89a538
Werner Macho <werner.macho@gmail.com>
2012-12-11 16:19:13 +0100

    updates to licenses
    Again some updates to clean the whole QGIS tree of unlicensed files.
    Main purpose is to make the source tree checkable by debian licensechecker to clearly state the license of QGIS and find out if everything is compatible.

--- src/gui/symbology-ng/characterwidget.cpp
+++ src/gui/symbology-ng/characterwidget.cpp
@@ -4,6 +4,11 @@
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
+** This library/program is free software; you can redistribute it
+** and/or modify it under the terms of the GNU Library General Public
+** License as published by the Free Software Foundation; either
+** version 2 of the License, or ( at your option ) any later version.
+**
 ** This file is part of the examples of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
@@ -14,13 +19,12 @@
 ** a written agreement between you and Nokia.
 **
 ** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
+** Alternatively, this file may be used under the terms of the
+** GNU Lesser General Public License version 2.1 as published by the Free Software
 ** Foundation and appearing in the file LICENSE.LGPL included in the
 ** packaging of this file.  Please review the following information to
 ** ensure the GNU Lesser General Public License version 2.1 requirements
 ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
 ** In addition, as a special exception, Nokia gives you certain additional
 ** rights.  These rights are described in the Nokia Qt LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
--- src/gui/symbology-ng/characterwidget.h
+++ src/gui/symbology-ng/characterwidget.h
@@ -4,6 +4,11 @@
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
+** This library/program is free software; you can redistribute it
+** and/or modify it under the terms of the GNU Library General Public
+** License as published by the Free Software Foundation; either
+** version 2 of the License, or ( at your option ) any later version.
+**
 ** This file is part of the examples of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
