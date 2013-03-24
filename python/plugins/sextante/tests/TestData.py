47101f38801b53e06b6dc8574978501e1e49ebc3
Victor Olaya <volayaf@gmail.com>
2013-03-24 15:26:56 +0100

    [sextante] Modified test data and example models

--- python/plugins/sextante/tests/TestData.py
+++ python/plugins/sextante/tests/TestData.py
@@ -1,7 +1,30 @@
 import os.path
+from sextante.core.QGisLayers import QGisLayers
 
 dataFolder = os.path.join(os.path.dirname(__file__), 'data')
-raster = os.path.join(dataFolder, "raster.tif")
-points = os.path.join(dataFolder, "points.shp")
-lines = os.path.join(dataFolder, "lines.shp")
-polygons = os.path.join(dataFolder, "polygons.shp")
+
+def points():
+    return os.path.join(dataFolder, "points.shp")
+
+def raster():
+    return os.path.join(dataFolder, "raster.tif")
+
+def lines():
+    return os.path.join(dataFolder, "lines.shp")
+
+def polygons():
+    return os.path.join(dataFolder, "polygons.shp")
+
+def polygons2():
+    return os.path.join(dataFolder, "polygons2.shp")
+
+def union():
+    return os.path.join(dataFolder, "union.shp")
+
+def loadTestData():
+    QGisLayers.load(points(), "points");
+    QGisLayers.load(polygons(), "polygons");
+    QGisLayers.load(polygons2(), "polygons2");
+    QGisLayers.load(lines(), "lines");
+    QGisLayers.load(raster(), "raster");
+    QGisLayers.load(union(), "union");
