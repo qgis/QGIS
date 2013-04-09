8c714a9d41ba21ad15495021b10684695be82877
Victor Olaya <volayaf@gmail.com>
2013-04-09 23:11:11 +0200

    [sextante] more tests

---
+++ python/plugins/sextante/tests/SextanteTests.py
@@ -0,0 +1,26 @@
+'''Convenience module to create a test suite will all SEXTANTE tests''' 
+import unittest
+from sextante.tests import QgisAlgsTest
+from sextante.tests import ParametersTest
+from sextante.tests import ModelerAlgorithmTest
+from sextante.tests import SextanteToolsTest
+from sextante.tests import ScriptTest
+from sextante.tests import SagaTest
+from sextante.tests import GeoAlgorithmTest
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTests(QgisAlgsTest.suite())
+    suite.addTests(ModelerAlgorithmTest.suite())
+    suite.addTests(SagaTest.suite())
+    suite.addTests(ScriptTest.suite())
+    suite.addTests(SextanteToolsTest.suite())    
+    #suite.addTests(ParametersTest.suite())
+    suite.addTests(GeoAlgorithmTest.suite())
+    return suite
+
+def runtests():
+    result = unittest.TestResult()
+    testsuite = suite()
+    testsuite.run(result)
+    return result
