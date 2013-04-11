6d6021558be4ddf5160661e3c5304add61292b9b
PhilippeDorelon <philippe.dorelon@gmail.com>
2013-04-11 08:52:10 -0300

    Update qgsattributetablemodel.cpp

--- src/gui/attributetable/qgsattributetablemodel.cpp
+++ src/gui/attributetable/qgsattributetablemodel.cpp
@@ -294,8 +294,7 @@ void QgsAttributeTableModel::loadAttributes()
 
   mFieldCount = attributes.size();
   mAttributes = attributes;
-  mValueMaps.clear();
-
+  
   if ( ins )
   {
     endInsertColumns();
