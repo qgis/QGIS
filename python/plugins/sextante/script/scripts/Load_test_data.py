b291a36bc30905f3746fa01606847ab1e5291602
Werner Macho <werner.macho@gmail.com>
2013-04-13 11:04:22 +0200

    adding licences for Victor :)

--- python/plugins/sextante/script/scripts/Load_test_data.py
+++ python/plugins/sextante/script/scripts/Load_test_data.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    Load_test_data.py
+    ---------------------
+    Date                 : March 2013
+    Copyright            : (C) 2013 by Victor Olaya
+    Email                : volayaf at gmail dot com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Victor Olaya'
+__date__ = 'March 2013'
+__copyright__ = '(C) 2013, Victor Olaya'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
 ##[Example scripts]=group
 
 sextante.loadTestData()
