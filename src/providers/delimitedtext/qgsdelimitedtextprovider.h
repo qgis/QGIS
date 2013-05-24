bdcc01e9f9bf6031b788eac2f4cf65c93283ace6
Chris Crook <ccrook@linz.govt.nz>
2013-05-25 10:17:00 +1200

    Adding CSVT file reading - using CSVT file alongside data file to determine field types

--- src/providers/delimitedtext/qgsdelimitedtextprovider.h
+++ src/providers/delimitedtext/qgsdelimitedtextprovider.h
@@ -201,6 +201,14 @@ class QgsDelimitedTextProvider : public QgsVectorDataProvider
     */
     bool boundsCheck( QgsGeometry *geom );
 
+    /**
+     * Try to read field types from CSVT (or equialent xxxT) file.
+     * @param filename The name of the file from which to read the field types
+     * @param message  Pointer to a string to receive a status message
+     * @return A list of field type strings, empty if not found or not valid
+     */
+    QStringList readCsvtFieldTypes( QString filename, QString *message = 0 );
+
   private slots:
 
     void onFileUpdated();
