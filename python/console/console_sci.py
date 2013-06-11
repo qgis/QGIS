3b4dfd724df915ff790c7ff152b772f111fa53db
Salvatore Larosa <lrssvtml@gmail.com>
2013-06-11 14:15:59 +0200

    [FEATURE] [pyqgis-console] Module to generate prepared APIs for calltips and auto-completion

--- python/console/console_sci.py
+++ python/console/console_sci.py
@@ -189,8 +189,11 @@ class ShellScintilla(QsciScintilla, code.InteractiveInterpreter):
 
         self.api = QsciAPIs(self.lexer)
         chekBoxAPI = self.settings.value("pythonConsole/preloadAPI", True, type=bool)
+        chekBoxPreparedAPI = self.settings.value("pythonConsole/usePreparedAPIFile", False, type=bool)
         if chekBoxAPI:
-            self.api.loadPrepared( QgsApplication.pkgDataPath() + "/python/qsci_apis/pyqgis_master.pap" )
+            self.api.loadPrepared(QgsApplication.pkgDataPath() + "/python/qsci_apis/pyqgis_master.pap")
+        elif chekBoxPreparedAPI:
+            self.api.loadPrepared(self.settings.value("pythonConsole/preparedAPIFile"))
         else:
             apiPath = self.settings.value("pythonConsole/userAPI")
             for i in range(0, len(apiPath)):
