20b45c31ebac57a6064b9415fae8824af9b1c201
vmora <vincent.mora@oslandia.com>
2013-07-11 15:59:41 +0200

    - test that fails because of issue7244 (split several polygons of a   multipolygon at the same time

---
+++ tests/src/python/test_qgsissue7244.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""QGIS Unit tests for QgsSpatialiteProvider
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+__author__ = 'Vincent Mora'
+__date__ = '09/07/2013'
+__copyright__ = 'Copyright 2013, The QGIS Project'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import os
+import random
+
+from qgis.core import *
+from qgis.gui import *
+
+from utilities import (getQgisTestApp,
+                       TestCase,
+                       unittest
+                       )
+
+from pyspatialite import dbapi2 as sqlite3
+
+# Convenience instances in case you may need them
+QGISAPP, CANVAS, IFACE, PARENT = getQgisTestApp()
+
+
+def die(error_message):
+    raise Exception(error_message)
+
+class TestQgsSpatialiteProvider(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """Run before all tests"""
+        # create test db
+        if os.path.exists("test.sqlite") :
+            os.remove("test.sqlite")
+        con = sqlite3.connect("test.sqlite")
+        cur = con.cursor()
+        sql = "SELECT InitSpatialMetadata()"
+        cur.execute(sql)
+
+        # simple table with primary key
+        sql = "CREATE TABLE test_mpg (id SERIAL PRIMARY KEY, name STRING NOT NULL)"
+        cur.execute(sql)
+        sql = "SELECT AddGeometryColumn('test_mpg', 'geometry', 4326, 'MULTIPOLYGON', 'XY')"
+        cur.execute(sql)
+        sql = "INSERT INTO test_mpg (name, geometry) "
+        sql +=    "VALUES ('multipolygon with 8 squares', GeomFromText('MULTIPOLYGON("
+        for i in range(0,4,2):
+            for j in range (0,4,2):
+                sql += "(("
+                sql += str(i)   + " " + str(j)   + ","
+                sql += str(i+1) + " " + str(j)   + ","
+                sql += str(i+1) + " " + str(j+1) + ","
+                sql += str(i)   + " " + str(j+1) + ","
+                sql += str(i)   + " " + str(j) 
+                sql += ")),"
+        sql = sql[:-1] # remove last comma
+        sql += ")', 4326))"
+        cur.execute(sql)
+ 
+        con.commit()
+        con.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Run after all tests"""
+        # for the time beeing, keep the file to check with qgis
+        #if os.path.exists("test.sqlite") :
+        #    os.remove("test.sqlite")
+        pass
+
+    def setUp(self):
+        """Run before each test."""
+        pass
+
+    def tearDown(self):
+        """Run after each test."""
+        pass
+
+    def test_SplitMultipolygon(self):
+        """Create spatialite database"""
+        layer = QgsVectorLayer("dbname=test.sqlite table=test_mpg (geometry)", "test_mpg", "spatialite")
+        assert(layer.isValid())
+        assert(layer.hasGeometryType())
+        layer.featureCount() == 1 or die("we should have 1 features")
+        layer.startEditing()
+        layer.splitFeatures([QgsPoint(0.5, -0.5), QgsPoint(0.5, 1.5)], 0)==0 or die("error in split of one polygon of multipolygon")
+        layer.splitFeatures([QgsPoint(2.5, -0.5), QgsPoint(2.5, 4)], 0)==0 or die("error in split of two polygons of multipolygon at a time")
+        layer.commitChanges() or die("this commit should work")
+        #layer.featureCount() == 9 or die("we should have 9 features after 2 split")
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
