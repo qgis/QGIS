0ed2db501d73b5e1a696f1adcba36e79e94efc5a
Daniel Vaz <danielvaz@gmail.com>
2013-07-18 22:43:44 -0300

    Fix #4417
    Fixes indentation on qgscustomwidgets.h
    Add new custom widget (EQSlide - Extended QSlider) that shows the current value on QSlider control.

--- src/gui/qgscustomwidgets.h
+++ src/gui/qgscustomwidgets.h
@@ -2,12 +2,23 @@
 #include <QPainter>
 #include <QPaintEvent>
 #include <QSize>
+#include <QSlider>
 
 class EQDial : public QDial
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    EQDial( QWidget *parent = NULL );
+  EQDial( QWidget *parent = 0 );
 protected:
-    virtual void paintEvent( QPaintEvent * event );
+  virtual void paintEvent( QPaintEvent * event );
+};
+
+class EQSlider : public QSlider
+{
+  Q_OBJECT
+public:
+  EQSlider( QWidget *parent = 0 );
+  EQSlider( Qt::Orientation orientation, QWidget * parent = 0 );
+protected:
+  virtual void paintEvent( QPaintEvent * event );
 };
