17323dd938c6e2feec3c715268166fbc9939b567
Alexander Bruy <alexander.bruy@gmail.com>
2013-09-15 19:42:41 +0300

    fix typo

--- python/plugins/processing/modeler/ModelerDialog.py
+++ python/plugins/processing/modeler/ModelerDialog.py
@@ -83,7 +83,7 @@ class ModelerDialog(QDialog, Ui_DlgModeler):
         self.saveAsButton = QPushButton(self.tr("Save as..."))
         self.saveAsButton.setToolTip(self.tr("Save current model as"))
         self.buttonBox.addButton(self.saveAsButton, QDialogButtonBox.ActionRole)
-        self.saveAsImageButton = QPushButton(self.tr("Export as image..."))
+        self.exportAsImageButton = QPushButton(self.tr("Export as image..."))
         self.exportAsImageButton.setToolTip(self.tr("Export current model to image"))
         self.buttonBox.addButton(self.saveAsImageButton, QDialogButtonBox.ActionRole)
 
