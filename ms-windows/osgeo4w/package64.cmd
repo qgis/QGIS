1ca2258a5bfcb9382ef3b7ba55e49676ec0301d2
Juergen E. Fischer <jef@norbit.de>
2013-09-24 22:21:54 +0200

    some more syncs with release-2_0

--- ms-windows/osgeo4w/package64.cmd
+++ ms-windows/osgeo4w/package64.cmd
@@ -206,7 +206,6 @@ tar -C %OSGEO4W_ROOT% -cjf %PACKAGENAME%-common-%VERSION%-%PACKAGE%.tar.bz2 ^
 	"apps/%PACKAGENAME%/resources/symbology-ng-style.db" ^
 	"apps/%PACKAGENAME%/resources/cpt-city-qgis-min/" ^
 	"apps/%PACKAGENAME%/svg/" ^
-	"apps/%PACKAGENAME%/cpt-city-qgis-min/" ^
 	"apps/%PACKAGENAME%/crssync.exe" ^
 	"etc/postinstall/%PACKAGENAME%-common.bat" ^
 	>>%LOG% 2>&1
