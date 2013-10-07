b3b54d1fe28509afaeafbfe3ae4a2c43888e0f2e
Matthias Kuhn <matthias.kuhn@gmx.ch>
2013-10-07 17:50:58 +0200

    Fix header

--- tests/src/python/test_qgsrelation.py
+++ tests/src/python/test_qgsrelation.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-"""QGIS Unit tests for QgsVectorLayer.
+"""QGIS Unit tests for QgsRelation.
 
 .. note:: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 """
-__author__ = 'Tim Sutton'
-__date__ = '20/08/2012'
-__copyright__ = 'Copyright 2012, The QGIS Project'
+__author__ = 'Matthias Kuhn'
+__date__ = '07/10/2013'
+__copyright__ = 'Copyright 2013, The QGIS Project'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
