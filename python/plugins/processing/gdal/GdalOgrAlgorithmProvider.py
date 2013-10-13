e6ba40585cdeb8985433dcf521c0e9c6eb55a3b1
Alexander Bruy <alexander.bruy@gmail.com>
2013-10-13 14:00:25 +0300

    [processing] add gdal slope algorithm (addresses #7201)

--- python/plugins/processing/gdal/GdalOgrAlgorithmProvider.py
+++ python/plugins/processing/gdal/GdalOgrAlgorithmProvider.py
@@ -55,6 +55,7 @@ from processing.gdal.fillnodata import fillnodata
 from processing.gdal.extractprojection import ExtractProjection
 from processing.gdal.gdal2xyz import gdal2xyz
 from processing.gdal.hillshade import hillshade
+from processing.gdal.slope import slope
 
 from processing.gdal.ogr2ogr import Ogr2Ogr
 from processing.gdal.ogrinfo import OgrInfo
@@ -106,7 +107,7 @@ class GdalOgrAlgorithmProvider(AlgorithmProvider):
             rgb2pct(), pct2rgb(), merge(), polygonize(), gdaladdo(),
             ClipByExtent(), ClipByMask(), contour(), rasterize(), proximity(),
             sieve(), fillnodata(), ExtractProjection(), gdal2xyz(),
-            hillshade(),
+            hillshade(), slope(),
             # ----- OGR tools -----
             OgrInfo(), Ogr2Ogr(), OgrSql(),
             ]
