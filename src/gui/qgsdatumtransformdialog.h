63fbc7526ad362efda3ae88437c7d85a1710e16e
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2013-10-31 16:55:16 +0100

    Add export macro

--- src/gui/qgsdatumtransformdialog.h
+++ src/gui/qgsdatumtransformdialog.h
@@ -3,7 +3,7 @@
 
 #include "ui_qgsdatumtransformdialogbase.h"
 
-class QgsDatumTransformDialog: public QDialog, private Ui::QgsDatumTransformDialogBase
+class GUI_EXPORT QgsDatumTransformDialog: public QDialog, private Ui::QgsDatumTransformDialogBase
 {
   public:
     QgsDatumTransformDialog( const QString& layerName, const QList< QList< int > >& dt, QWidget * parent = 0, Qt::WindowFlags f = 0 );
