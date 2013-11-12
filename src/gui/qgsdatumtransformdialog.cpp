c01c18ab91f689c2a6484af9857f27e066a183c5
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2013-11-12 12:38:35 +0100

    Add missing license headers

--- src/gui/qgsdatumtransformdialog.cpp
+++ src/gui/qgsdatumtransformdialog.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgsdatumtransformdialog.cpp
+                         ---------------------------
+    begin                : November 2013
+    copyright            : (C) 2013 by Marco Hugentobler
+    email                : marco.hugentobler at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsdatumtransformdialog.h"
 #include "qgscoordinatetransform.h"
 
