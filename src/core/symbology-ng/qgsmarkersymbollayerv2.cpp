222946b36a63b573713a748288ea36f7f8565afc
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2013-11-21 15:45:38 +0100

    Implement simple marker rect / square

--- src/core/symbology-ng/qgsmarkersymbollayerv2.cpp
+++ src/core/symbology-ng/qgsmarkersymbollayerv2.cpp
@@ -709,17 +709,36 @@ void QgsSimpleMarkerSymbolLayerV2::writeDxf( QgsDxfExport& e, double mmMapUnitSc
     size *= mmMapUnitScaleFactor;
   }
   double halfSize = size / 2.0;
+  int colorIndex = QgsDxfExport::closestColorMatch( mBrush.color().rgb() );
 
   if ( mName == "circle" )
   {
     e.writeGroup( 0, "CIRCLE" );
-    e.writeGroup( 8, 0 );
-    int colorIndex = QgsDxfExport::closestColorMatch( mBrush.color().rgb() );
+    e.writeGroup( 8, "0" );
+
     e.writeGroup( 62, colorIndex );
     e.writeGroup( 10, halfSize );
     e.writeGroup( 20, halfSize );
     e.writeGroup( 30, 0.0 );
     e.writeGroup( 40, halfSize );
+  }
+  else if ( mName == "square" || mName == "rectangle" )
+  {
+    e.writeGroup( 0, "SOLID" );
+    e.writeGroup( 8, "0" );
+    e.writeGroup( 62, colorIndex );
+    e.writeGroup( 10, 0.0 );
+    e.writeGroup( 20, 0.0 );
+    e.writeGroup( 30, 0.0 );
+    e.writeGroup( 11, size );
+    e.writeGroup( 21, 0.0 );
+    e.writeGroup( 31, 0.0 );
+    e.writeGroup( 12, 0 );
+    e.writeGroup( 22, size );
+    e.writeGroup( 32, 0.0 );
+    e.writeGroup( 13, size );
+    e.writeGroup( 23, size );
+    e.writeGroup( 33, 0.0 );
   }
 }
 
