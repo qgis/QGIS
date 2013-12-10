e4244b1c6c0c0427634253260cdb58ed7bb17f29
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2013-12-10 14:46:57 +0100

    Add license header for dxfexportdialog

--- src/app/qgsdxfexportdialog.h
+++ src/app/qgsdxfexportdialog.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgsdxfexportdialog.h
+                         --------------------
+    begin                : September 2013
+    copyright            : (C) 2013 by Marco Hugentobler
+    email                : marco at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSDXFEXPORTDIALOG_H
 #define QGSDXFEXPORTDIALOG_H
 
