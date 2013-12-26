9c2c890ee6d33dc8b39d04e7aee46376a6cd3008
nyalldawson <nyall.dawson@gmail.com>
2013-12-26 21:47:00 +1100

    [tests] Fix failing diagram and blend modes tests

--- tests/src/core/testqgsblendmodes.cpp
+++ tests/src/core/testqgsblendmodes.cpp
@@ -52,7 +52,7 @@ class TestQgsBlendModes: public QObject
     bool imageCheck( QString theType ); //as above
     QgsMapRenderer * mpMapRenderer;
     QgsMapLayer * mpPointsLayer;
-    QgsMapLayer * mpPolysLayer;
+    QgsVectorLayer * mpPolysLayer;
     QgsVectorLayer * mpLinesLayer;
     QgsRasterLayer* mRasterLayer1;
     QgsRasterLayer* mRasterLayer2;
@@ -85,6 +85,7 @@ void TestQgsBlendModes::initTestCase()
   QFileInfo myPolyFileInfo( myPolysFileName );
   mpPolysLayer = new QgsVectorLayer( myPolyFileInfo.filePath(),
                                      myPolyFileInfo.completeBaseName(), "ogr" );
+  mpPolysLayer->setSimplifyDrawingHints( QgsVectorLayer::NoSimplification );
   QgsMapLayerRegistry::instance()->addMapLayers(
     QList<QgsMapLayer *>() << mpPolysLayer );
 
@@ -93,6 +94,7 @@ void TestQgsBlendModes::initTestCase()
   QFileInfo myLineFileInfo( myLinesFileName );
   mpLinesLayer = new QgsVectorLayer( myLineFileInfo.filePath(),
                                      myLineFileInfo.completeBaseName(), "ogr" );
+  mpLinesLayer->setSimplifyDrawingHints( QgsVectorLayer::NoSimplification );
   QgsMapLayerRegistry::instance()->addMapLayers(
     QList<QgsMapLayer *>() << mpLinesLayer );
 
