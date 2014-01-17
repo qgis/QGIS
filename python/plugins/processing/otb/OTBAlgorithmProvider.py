4f443dd384d40ee309f652285ceb9fac381b6b7e
Alexia Mondot <alexia.mondot@c-s.fr>
2014-01-17 19:39:17 +0100

    Update OTB processing modules

--- python/plugins/processing/otb/OTBAlgorithmProvider.py
+++ python/plugins/processing/otb/OTBAlgorithmProvider.py
@@ -6,7 +6,12 @@
     ---------------------
     Date                 : August 2012
     Copyright            : (C) 2012 by Victor Olaya
+                           (C) 2013 by CS Systemes d'information
     Email                : volayaf at gmail dot com
+                           otb at c-s dot fr
+    Contributors         : Victor Olaya
+                           Julien Malik  - Changing the way to load algorithms : loading from xml
+                           Oscar Picas   - Changing the way to load algorithms : loading from xml
 ***************************************************************************
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -20,19 +25,16 @@
 __author__ = 'Victor Olaya'
 __date__ = 'August 2012'
 __copyright__ = '(C) 2012, Victor Olaya'
-
 # This will get replaced with a git SHA1 when you do a git archive
-
 __revision__ = '$Format:%H$'
 
 import os
-from PyQt4.QtGui import *
+import PyQt4.QtGui
 from processing.core.AlgorithmProvider import AlgorithmProvider
 from processing.core.ProcessingConfig import ProcessingConfig, Setting
-from processing.core.ProcessingLog import ProcessingLog
 from processing.otb.OTBUtils import OTBUtils
 from processing.otb.OTBAlgorithm import OTBAlgorithm
-
+from processing.core.ProcessingLog import ProcessingLog
 
 class OTBAlgorithmProvider(AlgorithmProvider):
 
@@ -41,14 +43,15 @@ class OTBAlgorithmProvider(AlgorithmProvider):
         self.activate = True
         self.createAlgsList()
 
+
     def getDescription(self):
-        return 'Orfeo Toolbox (Image analysis)'
+        return "Orfeo Toolbox (Image analysis)"
 
     def getName(self):
-        return 'otb'
+        return "otb"
 
     def getIcon(self):
-        return QIcon(os.path.dirname(__file__) + '/../images/otb.png')
+        return PyQt4.QtGui.QIcon(os.path.dirname(__file__) + "/../images/otb.png")
 
     def _loadAlgorithms(self):
         self.algs = self.preloadedAlgs
@@ -57,36 +60,24 @@ class OTBAlgorithmProvider(AlgorithmProvider):
         self.preloadedAlgs = []
         folder = OTBUtils.otbDescriptionPath()
         for descriptionFile in os.listdir(folder):
-            if descriptionFile.endswith('txt'):
+            if descriptionFile.endswith("xml"):
                 try:
                     alg = OTBAlgorithm(os.path.join(folder, descriptionFile))
-                    if alg.name.strip() != '':
+ 
+                    if alg.name.strip() != "":
                         self.preloadedAlgs.append(alg)
                     else:
-                        ProcessingLog.addToLog(ProcessingLog.LOG_ERROR,
-                                'Could not open OTB algorithm: '
-                                + descriptionFile)
-                except Exception, e:
-                    ProcessingLog.addToLog(ProcessingLog.LOG_ERROR,
-                            'Could not open OTB algorithm: ' + descriptionFile)
+                        ProcessingLog.addToLog(ProcessingLog.LOG_ERROR, "Could not open OTB algorithm: " + descriptionFile)
+                except Exception,e:
+                    ProcessingLog.addToLog(ProcessingLog.LOG_ERROR, "Could not open OTB algorithm: " + descriptionFile)
+
 
     def initializeSettings(self):
         AlgorithmProvider.initializeSettings(self)
-        ProcessingConfig.addSetting(Setting(self.getDescription(),
-                                    OTBUtils.OTB_FOLDER,
-                                    'OTB command line tools folder',
-                                    OTBUtils.otbPath()))
-        ProcessingConfig.addSetting(Setting(self.getDescription(),
-                                    OTBUtils.OTB_LIB_FOLDER,
-                                    'OTB applications folder',
-                                    OTBUtils.otbLibPath()))
-        ProcessingConfig.addSetting(Setting(self.getDescription(),
-                                    OTBUtils.OTB_SRTM_FOLDER,
-                                    'SRTM tiles folder',
-                                    OTBUtils.otbSRTMPath()))
-        ProcessingConfig.addSetting(Setting(self.getDescription(),
-                                    OTBUtils.OTB_GEOID_FILE, 'Geoid file',
-                                    OTBUtils.otbGeoidPath()))
+        ProcessingConfig.addSetting(Setting(self.getDescription(), OTBUtils.OTB_FOLDER, "OTB command line tools folder", OTBUtils.otbPath()))
+        ProcessingConfig.addSetting(Setting(self.getDescription(), OTBUtils.OTB_LIB_FOLDER, "OTB applications folder", OTBUtils.otbLibPath()))
+        ProcessingConfig.addSetting(Setting(self.getDescription(), OTBUtils.OTB_SRTM_FOLDER, "SRTM tiles folder", OTBUtils.otbSRTMPath()))
+        ProcessingConfig.addSetting(Setting(self.getDescription(), OTBUtils.OTB_GEOID_FILE, "Geoid file", OTBUtils.otbGeoidPath()))
 
     def unload(self):
         AlgorithmProvider.unload(self)
