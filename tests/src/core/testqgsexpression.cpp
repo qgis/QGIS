1c2c03aebf149047cc7ef9006f1ce56430845c28
Mathieu Pellerin (nirvn) <nirvn.asia@gmail.com>
2014-02-17 14:32:37 +0700

    [expression] rework the wordwrap to allow for maximum / minimum line length to wrap, fix test and add two more

--- tests/src/core/testqgsexpression.cpp
+++ tests/src/core/testqgsexpression.cpp
@@ -315,7 +315,9 @@ class TestQgsExpression: public QObject
       QTest::newRow( "title" ) << "title(' HeLlO   WORLD ')" << false << QVariant( " Hello   World " );
       QTest::newRow( "trim" ) << "trim('   Test String ')" << false << QVariant( "Test String" );
       QTest::newRow( "trim empty string" ) << "trim('')" << false << QVariant( "" );
-      QTest::newRow( "wordwrap" ) << "wordwrap('university of qgis')" << false << QVariant( "university\nof qgis" );
+      QTest::newRow( "wordwrap" ) << "wordwrap('university of qgis',' ',13)" << false << QVariant( "university of\nqgis" );
+      QTest::newRow( "wordwrap" ) << "wordwrap('university of qgis',' ',-3)" << false << QVariant( "university\nof qgis" );
+      QTest::newRow( "wordwrap" ) << "wordwrap('university of qgis\nsupports many multiline',' ',-5)" << false << QVariant( "university\nof qgis\nsupports\nmany multiline" );
       QTest::newRow( "format" ) << "format('%1 %2 %3 %1', 'One', 'Two', 'Three')" << false << QVariant( "One Two Three One" );
 
       // implicit conversions
