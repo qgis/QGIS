22b2d58f7298be6043abf4101e680979c74c3a2a
Juergen E. Fischer <jef@norbit.de>
2014-02-18 16:57:48 +0100

    fix second occurrency too (followup 580f3dc80; fixes #9593)

--- i18n/qgis_fr.ts
+++ i18n/qgis_fr.ts
@@ -35801,8 +35801,7 @@ at line %2 column %3</source>
         <location filename="../src/app/qgsguivectorlayertools.cpp" line="133"/>
         <location filename="../src/app/qgsguivectorlayertools.cpp" line="154"/>
         <source>Commit errors</source>
-        <translation>Erreurs de commi:
-%s</translation>
+        <translation>Erreurs de commi</translation>
     </message>
     <message>
         <location filename="../src/app/qgsguivectorlayertools.cpp" line="134"/>
