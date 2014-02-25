b8d310d8ec4c202cf36cac51b121de5395b19002
Juergen E. Fischer <jef@norbit.de>
2014-02-26 00:17:54 +0100

    fix license display

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -441,7 +441,7 @@ const QString QgsApplication::translatorsFilePath()
 */
 const QString QgsApplication::licenceFilePath()
 {
-  return ABISYM( mPkgDataPath ) + QString( "/doc/LICENCE" );
+  return ABISYM( mPkgDataPath ) + QString( "/doc/LICENSE" );
 }
 
 /*!
