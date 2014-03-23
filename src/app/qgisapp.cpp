5b4d076898e7489f94ea12ab773ec5872d4dc88c
Richard Duivenvoorde <richard@duif.net>
2014-03-23 18:13:05 +0100

    revert 637359a and manually merge pull request #1232
    Signed-off-by: Juergen E. Fischer <jef@norbit.de>

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -1101,7 +1101,6 @@ void QgisApp::createActions()
   connect( mActionConfigureShortcuts, SIGNAL( triggered() ), this, SLOT( configureShortcuts() ) );
   connect( mActionStyleManagerV2, SIGNAL( triggered() ), this, SLOT( showStyleManagerV2() ) );
   connect( mActionCustomization, SIGNAL( triggered() ), this, SLOT( customize() ) );
-  connect( mActionResetUIdefaults, SIGNAL( triggered() ), this, SLOT( restoreDefaultWindowState() ) );
 
 #ifdef Q_WS_MAC
   // Window Menu Items
@@ -2371,20 +2370,6 @@ void QgisApp::restoreWindowState()
   }
 
 }
-
-void QgisApp::restoreDefaultWindowState()
-{
-  if ( QMessageBox::warning( this, tr( "Restore UI defaults" ), tr( "Are you sure to reset the UI to default?" ), QMessageBox::Ok | QMessageBox::Cancel ) == QMessageBox::Cancel )
-    return;
-
-  saveWindowState();
-
-  QSettings settings;
-  settings.remove( "/UI/state" );
-
-  restoreWindowState();
-}
-
 ///////////// END OF GUI SETUP ROUTINES ///////////////
 void QgisApp::sponsors()
 {
