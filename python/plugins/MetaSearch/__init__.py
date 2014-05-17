25885b8a0cfbc01764f63feb98d0eddeaaf65f3e
Juergen E. Fischer <jef@norbit.de>
2014-05-17 11:29:36 +0200

    move internal python extensions to common directory and fix metasearch install

--- python/plugins/MetaSearch/__init__.py
+++ python/plugins/MetaSearch/__init__.py
@@ -14,7 +14,7 @@
 #
 # This code is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # A copy of the GNU General Public License is available on the World Wide Web
@@ -26,9 +26,6 @@
 
 import os
 import site
-
-site.addsitedir(os.path.abspath('%s/ext-libs' % os.path.dirname(__file__)))
-
 
 def classFactory(iface):
     """invoke plugin"""
