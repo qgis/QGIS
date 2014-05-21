0e48b3a720e5ee726c9f0d27089da773becc837b
Alexander Bruy <alexander.bruy@gmail.com>
2014-05-21 11:54:31 +0300

    [processing] fix number input panel

--- python/plugins/processing/ui/ui_widgetNumberInput.py
+++ python/plugins/processing/ui/ui_widgetNumberInput.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'widgetNumberInput.ui'
 #
-# Created: Thu May 15 14:48:14 2014
-#      by: PyQt4 UI code generator 4.9.6
+# Created: Wed May 21 11:41:55 2014
+#      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,27 +12,16 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+    _fromUtf8 = lambda s: s
 
 class Ui_widgetNumberInput(object):
     def setupUi(self, widgetNumberInput):
         widgetNumberInput.setObjectName(_fromUtf8("widgetNumberInput"))
-        widgetNumberInput.resize(205, 49)
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(widgetNumberInput)
-        self.horizontalLayout_2.setSpacing(0)
-        self.horizontalLayout_2.setMargin(0)
-        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        widgetNumberInput.resize(205, 24)
+        widgetNumberInput.setSpacing(0)
+        self.horizontalLayout = QtGui.QHBoxLayout(widgetNumberInput)
         self.horizontalLayout.setSpacing(2)
+        self.horizontalLayout.setMargin(0)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.spnValue = QtGui.QDoubleSpinBox(widgetNumberInput)
         self.spnValue.setDecimals(6)
@@ -43,13 +32,12 @@ class Ui_widgetNumberInput(object):
         self.btnCalc = QtGui.QToolButton(widgetNumberInput)
         self.btnCalc.setObjectName(_fromUtf8("btnCalc"))
         self.horizontalLayout.addWidget(self.btnCalc)
-        self.horizontalLayout_2.addLayout(self.horizontalLayout)
 
         self.retranslateUi(widgetNumberInput)
         QtCore.QMetaObject.connectSlotsByName(widgetNumberInput)
 
     def retranslateUi(self, widgetNumberInput):
-        widgetNumberInput.setWindowTitle(_translate("widgetNumberInput", "Form", None))
-        self.btnCalc.setToolTip(_translate("widgetNumberInput", "Open number input dialog", None))
-        self.btnCalc.setText(_translate("widgetNumberInput", "...", None))
+        widgetNumberInput.setWindowTitle(QtGui.QApplication.translate("widgetNumberInput", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnCalc.setToolTip(QtGui.QApplication.translate("widgetNumberInput", "Open number input dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnCalc.setText(QtGui.QApplication.translate("widgetNumberInput", "...", None, QtGui.QApplication.UnicodeUTF8))
 
