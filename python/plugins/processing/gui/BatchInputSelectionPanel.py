a794c71e45413cba285000c5388a366442cdf05c
Victor Olaya <volayaf@gmail.com>
2014-06-28 16:59:53 +0200

    [processing] fixed #10445

--- python/plugins/processing/gui/BatchInputSelectionPanel.py
+++ python/plugins/processing/gui/BatchInputSelectionPanel.py
@@ -95,7 +95,7 @@ class BatchInputSelectionPanel(QtGui.QWidget):
                 if isinstance(self.param, ParameterMultipleInput):
                     self.text.setText(';'.join(layers[idx].name() for idx in selected))
                 else:
-                    rowdif = len(layers) - (self.table.rowCount() - self.row)
+                    rowdif = len(selected) - (self.table.rowCount() - self.row)
                     for i in range(rowdif):
                         self.batchDialog.addRow()
                     for i, layeridx in enumerate(selected):
