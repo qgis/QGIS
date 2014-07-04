12f52690f318f117088eef06b2112c1268d9889c
Nyall Dawson <nyall.dawson@gmail.com>
2014-07-04 22:38:34 +1000

    Allow preparing expressions in QgsDataDefined without requiring a layer

--- src/core/qgsdatadefined.cpp
+++ src/core/qgsdatadefined.cpp
@@ -65,7 +65,17 @@ bool QgsDataDefined::prepareExpression( QgsVectorLayer* layer )
     }
   }
 
-  mExpression->prepare( layer->pendingFields() );
+  if ( layer )
+  {
+    mExpression->prepare( layer->pendingFields() );
+  }
+  else
+  {
+    //preparing expression without a layer set, so pass empty field list
+    QgsFields empty;
+    mExpression->prepare( empty );
+  }
+
   if ( mExpression->hasEvalError() )
   {
     QgsDebugMsg( "Prepare error:" + mExpression->evalErrorString() );
