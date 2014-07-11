f5b84ca101ff3c2708bca13b2c5361a0cf6bb50b
Victor Olaya <volayaf@gmail.com>
2014-07-11 14:22:05 +0200

    [processing] fixed frequency algorithm

--- python/plugins/processing/algs/qgis/scripts/Frequency_analysis.py
+++ python/plugins/processing/algs/qgis/scripts/Frequency_analysis.py
@@ -8,16 +8,16 @@ from PyQt4.QtCore import *
 from processing.core.TableWriter import TableWriter
 from collections import defaultdict
 
-layer = processing.getObject(input)
+layer = processing.getObject(Input)
 inputFields = layer.pendingFields()
 fieldIdxs = []
-fields = fields.split(',')
+fields = Fields.split(',')
 for f in fields:  
     idx = inputFields.indexFromName(f)  
     if  idx == -1:
         raise GeoAlgorithmExecutionException('Field not found:' + f)
     fieldIdxs.append(idx)
-writer = TableWriter(output, None,  fields + ['FREQ'])
+writer = TableWriter(Output, None,  fields + ['FREQ'])
 
 counts = {}
 feats = processing.features(layer)
@@ -26,8 +26,7 @@ counts = defaultdict(int)
 for i, feat in enumerate(feats):
     progress.setPercentage(int(100 * i / nFeats))    
     attrs = feat.attributes()
-    clazz = tuple([attrs[idx] for idx in fieldIdxs])
-    print clazz
+    clazz = tuple([attrs[idx] for idx in fieldIdxs])    
     counts[clazz] += 1
 
 for c in counts:    
