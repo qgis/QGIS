57a4aedea711c9c0baf47f2cc1fc2dd96c453c9e
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-07-23 14:32:04 +0200

    Use icons on fields properties, disable "editable" where inappropriate

--- src/app/qgsattributetypedialog.cpp
+++ src/app/qgsattributetypedialog.cpp
@@ -62,6 +62,13 @@ QgsAttributeTypeDialog::QgsAttributeTypeDialog( QgsVectorLayer *vl , int fieldId
                                         + 2 );
   selectionListWidget->setMaximumWidth( selectionListWidget->sizeHintForColumn( 0 )
                                         + 2 );
+
+  if ( vl->pendingFields().fieldOrigin( fieldIdx ) == QgsFields::OriginJoin ||
+       vl->pendingFields().fieldOrigin( fieldIdx ) == QgsFields::OriginExpression )
+  {
+    isFieldEditableCheckBox->setEnabled( false );
+  }
+
   QSettings settings;
   restoreGeometry( settings.value( "/Windows/QgsAttributeTypeDialog/geometry" ).toByteArray() );
 }
