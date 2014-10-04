7960dc21e37d67c81e79f870109d622f5cc05198
Alexander Bruy <alexander.bruy@gmail.com>
2014-10-04 16:18:09 +0300

    [processing] always pass number of cores as integer (fix #10894)

--- python/plugins/processing/algs/taudem/TauDEMAlgorithm.py
+++ python/plugins/processing/algs/taudem/TauDEMAlgorithm.py
@@ -88,7 +88,7 @@ class TauDEMAlgorithm(GeoAlgorithm):
         commands = []
         commands.append(os.path.join(TauDEMUtils.mpiexecPath(), 'mpiexec'))
 
-        processNum = ProcessingConfig.getSetting(TauDEMUtils.MPI_PROCESSES)
+        processNum = int(ProcessingConfig.getSetting(TauDEMUtils.MPI_PROCESSES))
         if processNum <= 0:
             raise GeoAlgorithmExecutionException('Wrong number of MPI \
                 processes used.\nPlease set correct number before running \
