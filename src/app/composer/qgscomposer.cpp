qgis/QGIS

commit 7773d4e0a8ff764352dc50525f9d4c6bcf26e7d3

[composer] Ensure that composers are deleted before compositions
Fixes some rare crashes when loading projects.

src/app/qgisapp.cpp
@@ -5465,8 +5465,19 @@ void QgisApp::deletePrintComposers()
   for ( ; it != mPrintComposers.end(); ++it )
   {
     emit composerWillBeRemoved(( *it )->view() );
-    delete(( *it )->composition() );
+
+    //save a reference to the composition
+    QgsComposition* composition = ( *it )->composition();
+
+    //first, delete the composer. This must occur before deleting the composition as some of the cleanup code in
+    //composer or in composer item widgets may require the composition to still be around
     delete( *it );
+
+    //next, delete the composition
+    if ( composition )
+    {
+      delete composition;
+    }
   }
   mPrintComposers.clear();
   mLastComposerId = 0;

commit 7798bd8abbcdd8015d0dc8540bd9dcf5313a71e6

[composer] Fix crash caused by 7773d4e
Don't set the parent object of a composition to a composer view, as
we manage deletion of compositions manually and don't always want
the composition deleted when a view a deleted.

src/app/composer/qgscomposer.cpp
@@ -504,7 +504,6 @@ QgsComposer::QgsComposer( QgisApp *qgis, const QString& title )
   connectCompositionSlots();
   connectOtherSlots();
 
-  mComposition->setParent( mView );
   mView->setComposition( mComposition );
   //this connection is set up after setting the view's composition, as we don't want setComposition called
   //for new composers

src/app/qgisapp.cpp
@@ -5375,7 +5375,19 @@ void QgisApp::deleteComposer( QgsComposer* c )
   mPrintComposersMenu->removeAction( c->windowAction() );
   markDirty();
   emit composerRemoved( c->view() );
+
+  //save a reference to the composition
+  QgsComposition* composition = c->composition();
+
+  //first, delete the composer. This must occur before deleting the composition as some of the cleanup code in
+  //composer or in composer item widgets may require the composition to still be around
   delete c;
+
+  //next, delete the composition
+  if ( composition )
+  {
+    delete composition;
+  }
 }
 
 QgsComposer* QgisApp::duplicateComposer( QgsComposer* currentComposer, QString title )