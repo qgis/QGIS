d64525c0b651845f9583a2f1a2efdc26b303035c
rldhont <rldhont@gmail.com>
2014-10-24 11:29:04 +0200

    [BUGFIX] #11484 QGIS server: embedded layers are not published (GetCapabilities, GetMap, etc)
    Issue #1072 QGIS server: embedded groups do not render in GetMap requests if
     leave layers are requested has been fixed 40d91457f6f069886c5ba1c28db1efb2ff4ccac7
    
    But another fix about embedded layers lacks. A fixme annotation is in the
     QgsServerProjectParser::createLayerFromElement. This one is about return the
     layer if it is embedded.
    
    In QgsServerProjectParser::createLayerFromElement, we need to generate a
     QgsServerProjectParser of the parent project to return the mapLayer for
     embedded layers. To do it, I use QgsCacheConfig to get the
     QgsServerProjectParser for the parent project.
    
    Funded by 3Liz.

--- src/mapserver/qgsserverprojectparser.cpp
+++ src/mapserver/qgsserverprojectparser.cpp
@@ -225,24 +225,15 @@ QgsMapLayer* QgsServerProjectParser::createLayerFromElement( const QDomElement& 
   }
   else if ( elem.attribute( "embedded" ) == "1" ) //layer is embedded from another project file
   {
-    //todo: fixme
-    /*
     QString project = convertToAbsolutePath( elem.attribute( "project" ) );
     QgsDebugMsg( QString( "Project path: %1" ).arg( project ) );
-
-    QgsProjectParser* otherConfig = dynamic_cast<QgsProjectParser*>( QgsConfigCache::instance()->searchConfiguration( project ) );
+    
+    QgsServerProjectParser* otherConfig = QgsConfigCache::instance()->serverConfiguration( project );
     if ( !otherConfig )
     {
       return 0;
     }
-
-    QHash< QString, QDomElement >::const_iterator layerIt = otherConfig->mProjectLayerElementsById.find( elem.attribute( "id" ) );
-    if ( layerIt == otherConfig->mProjectLayerElementsById.constEnd() )
-    {
-      return 0;
-    }
-    return otherConfig->createLayerFromElement( layerIt.value() );
-    */
+    return otherConfig->mapLayerFromLayerId( elem.attribute( "id" ), useCache );
   }
 
   if ( layer )
