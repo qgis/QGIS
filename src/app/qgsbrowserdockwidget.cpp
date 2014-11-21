9837f430b313eceeee246f966fc60115df3ee722
Larry Shaffer <larrys@dakotacarto.com>
2014-11-20 18:46:58 -0700

    Followup to d897aa5; fix crash on Mac

--- src/app/qgsbrowserdockwidget.cpp
+++ src/app/qgsbrowserdockwidget.cpp
@@ -811,6 +811,9 @@ QStringList QgsBrowserDockWidget::expandedPathsList( const QModelIndex & proxyIn
 {
   QStringList paths;
 
+  if ( !proxyIndex.isValid() )
+    return paths;
+
   for ( int i = 0; i < mProxyModel->rowCount( proxyIndex ); i++ )
   {
     QModelIndex childProxyIndex = mProxyModel->index( i, 0, proxyIndex );
