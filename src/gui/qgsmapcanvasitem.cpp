d36597a69a22cb5c88e9cd7b4063538e4586d85b
Juergen E. Fischer <jef@norbit.de>
2014-11-22 20:39:43 +0100

    better fix for b99ab0a

--- src/gui/qgsmapcanvasitem.cpp
+++ src/gui/qgsmapcanvasitem.cpp
@@ -27,11 +27,13 @@
 #include "qgslogger.h"
 
 QgsMapCanvasItem::QgsMapCanvasItem( QgsMapCanvas* mapCanvas )
-    : QGraphicsItem( 0, mapCanvas->scene() )
+    : QGraphicsItem()
     , mMapCanvas( mapCanvas )
     , mPanningOffset( 0, 0 )
     , mItemSize( 0, 0 )
 {
+  Q_ASSERT( mapCanvas && mapCanvas->scene() );
+  mapCanvas->scene()->addItem( this );
 }
 
 QgsMapCanvasItem::~QgsMapCanvasItem()
