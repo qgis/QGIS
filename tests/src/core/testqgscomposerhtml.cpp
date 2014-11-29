3434ece71fd7efb436e1f63d2a5bd837fc803090
Nyall Dawson <nyall.dawson@gmail.com>
2014-11-29 11:09:55 +1100

    [composer] Make html tests text-free
    Minimises cross platform rendering differences

--- tests/src/core/testqgscomposerhtml.cpp
+++ tests/src/core/testqgscomposerhtml.cpp
@@ -91,7 +91,7 @@ void TestQgsComposerHtml::sourceMode()
   htmlFrame->setFrameEnabled( true );
   htmlItem->addFrame( htmlFrame );
   htmlItem->setContentMode( QgsComposerHtml::ManualHtml );
-  htmlItem->setHtml( QString( "<p style=\"font-family: %1\"><i>Test manual <b>html</b></i></p>" ).arg( mTestFont.family() ) );
+  htmlItem->setHtml( QString( "<body style=\"margin: 10px;\"><div style=\"width: 100px; height: 50px; background-color: red;\"></div></body>" ) );
   htmlItem->loadHtml();
 
   QgsCompositionChecker checker( "composerhtml_manual", mComposition );
@@ -108,10 +108,10 @@ void TestQgsComposerHtml::userStylesheets()
   htmlFrame->setFrameEnabled( true );
   htmlItem->addFrame( htmlFrame );
   htmlItem->setContentMode( QgsComposerHtml::ManualHtml );
-  htmlItem->setHtml( QString( "<p style=\"font-family: %1\"><i>Test user stylesheets <b>html</b></i></p>" ).arg( mTestFont.family() ) );
+  htmlItem->setHtml( QString( "<body style=\"margin: 10px;\"><div style=\"width: 100px; height: 50px; background-color: red;\"></div></body>" ) );
 
   //set user stylesheet
-  htmlItem->setUserStylesheet( QString( "b { color: red; } i { color: green; }" ) );
+  htmlItem->setUserStylesheet( QString( "div { background-color: green !important; }" ) );
   //setting user stylesheet enabled automatically loads html
   htmlItem->setUserStylesheetEnabled( true );
 
@@ -130,7 +130,7 @@ void TestQgsComposerHtml::evalExpressions()
   htmlItem->addFrame( htmlFrame );
   htmlItem->setContentMode( QgsComposerHtml::ManualHtml );
   htmlItem->setEvaluateExpressions( true );
-  htmlItem->setHtml( QString( "<p style=\"font-family: %1\">Test expressions = <i>[% 1 + 2 + 3%]</i></p>" ).arg( mTestFont.family() ) );
+  htmlItem->setHtml( QString( "<body style=\"margin: 10px;\"><div style=\"width: [% 10 * 10 %]px; height: [% 30 + 20 %]px; background-color: [% 'yel' || 'low' %];\"></div></body>" ) );
 
   htmlItem->loadHtml();
 
@@ -149,7 +149,7 @@ void TestQgsComposerHtml::evalExpressionsOff()
   htmlItem->addFrame( htmlFrame );
   htmlItem->setContentMode( QgsComposerHtml::ManualHtml );
   htmlItem->setEvaluateExpressions( false );
-  htmlItem->setHtml( QString( "<p style=\"font-family: %1\">Test expressions = <i>[% 1 + 2 + 3%]</i></p>" ).arg( mTestFont.family() ) );
+  htmlItem->setHtml( QString( "<body style=\"margin: 10px;\"><div style=\"width: [% 10 * 10 %]px; height: [% 30 + 20 %]px; background-color: [% 'yel' || 'low' %];\"></div></body>" ) );
   htmlItem->loadHtml();
 
   QgsCompositionChecker checker( "composerhtml_expressions_disabled", mComposition );
@@ -165,7 +165,7 @@ void TestQgsComposerHtml::table()
   QgsComposerFrame* htmlFrame = new QgsComposerFrame( mComposition, htmlItem, 0, 0, 100, 200 );
   htmlFrame->setFrameEnabled( true );
   htmlItem->addFrame( htmlFrame );
-  htmlItem->setUrl( QUrl( QString( "file:///%1" ).arg( QString( TEST_DATA_DIR ) + QDir::separator() +  "html_table.html" ) ) );
+  htmlItem->setUrl( QUrl( QString( "file:///%1" ).arg( QString( TEST_DATA_DIR ) + QDir::separator() +  "test_html.html" ) ) );
 
   QgsCompositionChecker checker( "composerhtml_table", mComposition );
   bool result = checker.testComposition( mReport );
@@ -183,7 +183,7 @@ void TestQgsComposerHtml::tableMultiFrame()
   htmlItem->setUseSmartBreaks( false );
 
   //page1
-  htmlItem->setUrl( QUrl( QString( "file:///%1" ).arg( QString( TEST_DATA_DIR ) + QDir::separator() +  "html_table.html" ) ) );
+  htmlItem->setUrl( QUrl( QString( "file:///%1" ).arg( QString( TEST_DATA_DIR ) + QDir::separator() +  "test_html.html" ) ) );
   htmlItem->frame( 0 )->setFrameEnabled( true );
   QgsCompositionChecker checker1( "composerhtml_multiframe1", mComposition );
   bool result = checker1.testComposition( mReport );
@@ -206,14 +206,14 @@ void TestQgsComposerHtml::htmlMultiFrameSmartBreak()
   htmlItem->setUseSmartBreaks( true );
 
   //page1
-  htmlItem->setUrl( QUrl( QString( "file:///%1" ).arg( QString( TEST_DATA_DIR ) + QDir::separator() +  "html_table.html" ) ) );
+  htmlItem->setUrl( QUrl( QString( "file:///%1" ).arg( QString( TEST_DATA_DIR ) + QDir::separator() +  "test_html.html" ) ) );
   htmlItem->frame( 0 )->setFrameEnabled( true );
   QgsCompositionChecker checker1( "composerhtml_smartbreaks1", mComposition );
   bool result = checker1.testComposition( mReport );
 
   //page2
   QgsCompositionChecker checker2( "composerhtml_smartbreaks2", mComposition );
-  result = checker2.testComposition( mReport, 1 ) && result;
+  result = checker2.testComposition( mReport, 1, 3000 ) && result;
 
   mComposition->removeMultiFrame( htmlItem );
   delete htmlItem;
