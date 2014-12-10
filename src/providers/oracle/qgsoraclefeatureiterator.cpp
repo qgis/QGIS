e5e85a5445b469de99b7bfc7c70fe2534ce76482
Juergen E. Fischer <jef@norbit.de>
2014-12-10 17:15:37 +0100

    oracle provider: don't reject (none no)geometry queries on tables without geometry (fixes #11843)

--- src/providers/oracle/qgsoraclefeatureiterator.cpp
+++ src/providers/oracle/qgsoraclefeatureiterator.cpp
@@ -137,7 +137,7 @@ bool QgsOracleFeatureIterator::fetchFeature( QgsFeature& feature )
 
     int col = 0;
 
-    if (( mRequest.flags() & QgsFeatureRequest::NoGeometry ) == 0 ||
+    if ((( mRequest.flags() & QgsFeatureRequest::NoGeometry ) == 0 && !mSource->mGeometryColumn.isNull() ) ||
         (( mRequest.flags() & QgsFeatureRequest::ExactIntersect ) != 0 && !mConnection->hasSpatial() ) )
     {
       QByteArray *ba = static_cast<QByteArray*>( mQry.value( col++ ).data() );
@@ -262,16 +262,11 @@ bool QgsOracleFeatureIterator::close()
 
 bool QgsOracleFeatureIterator::openQuery( QString whereClause )
 {
-  if (( mRequest.flags() & QgsFeatureRequest::NoGeometry ) == 0 && mSource->mGeometryColumn.isNull() )
-  {
-    return false;
-  }
-
   try
   {
     QString query = "SELECT ", delim = "";
 
-    if (( mRequest.flags() & QgsFeatureRequest::NoGeometry ) == 0 )
+    if (( mRequest.flags() & QgsFeatureRequest::NoGeometry ) == 0 && !mSource->mGeometryColumn.isNull() )
     {
       query += QgsOracleProvider::quotedIdentifier( mSource->mGeometryColumn );
       delim = ",";
