a518e9f639eb2e40c17b18b7b60ebac0973855fb
Denis Rouzaud <denis.rouzaud@gmail.com>
2014-12-11 14:04:44 +0100

    [fix #11475] expressions: add left and right associavity for dump of binary operators
    complete test for left associativity

--- src/core/qgsexpression.h
+++ src/core/qgsexpression.h
@@ -513,6 +513,8 @@ class CORE_EXPORT QgsExpression
         virtual void accept( Visitor& v ) const override { v.visit( *this ); }
 
         int precedence() const;
+        bool leftAssociative() const;
+        bool rightAssociative() const;
 
       protected:
         bool compare( double diff );
