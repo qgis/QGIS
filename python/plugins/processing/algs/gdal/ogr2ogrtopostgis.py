2363ae56abe350ee059f31d46dc11bd3e19a695d
volaya <volayaf@gmail.com>
2014-12-16 14:02:20 +0100

    [processing] indentation fix

--- python/plugins/processing/algs/gdal/ogr2ogrtopostgis.py
+++ python/plugins/processing/algs/gdal/ogr2ogrtopostgis.py
@@ -90,7 +90,7 @@ class Ogr2OgrToPostGis(OgrAlgorithm):
                           'localhost', optional=False))
         self.addParameter(ParameterString(self.PORT, 'Port',
                           '5432', optional=False))
-	self.addParameter(ParameterString(self.USER, 'Username',
+        self.addParameter(ParameterString(self.USER, 'Username',
                           '', optional=False))
         self.addParameter(ParameterString(self.DBNAME, 'Database Name',
                           '', optional=False))
@@ -98,7 +98,7 @@ class Ogr2OgrToPostGis(OgrAlgorithm):
                           '', optional=False))
         self.addParameter(ParameterString(self.SCHEMA, 'Schema name',
                           'public', optional=True))
-	self.addParameter(ParameterString(self.TABLE, 'Table name, leave blank to use input name',
+        self.addParameter(ParameterString(self.TABLE, 'Table name, leave blank to use input name',
                           '', optional=True))
         self.addParameter(ParameterString(self.PK, 'Primary Key',
                           'id', optional=True))
@@ -188,9 +188,9 @@ class Ogr2OgrToPostGis(OgrAlgorithm):
         arguments.append(dimstring)
         arguments.append(ogrLayer)
         if index:
-           arguments.append(indexstring)
+            arguments.append(indexstring)
         if launder:
-           arguments.append(launderstring)
+            arguments.append(launderstring)
         if append:
             arguments.append('-append')
         if addfields:
@@ -223,11 +223,11 @@ class Ogr2OgrToPostGis(OgrAlgorithm):
             arguments.append(regionCoords[1])
             arguments.append(regionCoords[3])
             if clip:
-	      arguments.append('-clipsrc spat_extent')
+                arguments.append('-clipsrc spat_extent')
         if skipfailures:
-           arguments.append('-skipfailures')
+            arguments.append('-skipfailures')
         if where:
-           arguments.append(wherestring)
+            arguments.append(wherestring)
         if len(simplify) > 0:
             arguments.append('-simplify')
             arguments.append(simplify)
