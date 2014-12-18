d288a53c0de20191f25656e860c8cfbb5174d194
Denis Rouzaud <denis.rouzaud@gmail.com>
2014-12-18 08:07:19 +0100

    [fix #11872] do not call activateLayerRelatedAction after selection change if layer is not current layer

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -8972,7 +8972,10 @@ void QgisApp::selectionChanged( QgsMapLayer *layer )
   {
     showStatusMessage( tr( "%n feature(s) selected on layer %1.", "number of selected features", vlayer->selectedFeatureCount() ).arg( vlayer->name() ) );
   }
-  activateDeactivateLayerRelatedActions( layer );
+  if ( layer == activeLayer() )
+  {
+    activateDeactivateLayerRelatedActions( layer );
+  }
 }
 
 void QgisApp::legendLayerSelectionChanged( void )
