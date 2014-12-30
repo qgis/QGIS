4f4b891556fef1cfcc0df12fdf0bc1d5087301de
Nathan Woodrow <madmanwoo@gmail.com>
2014-12-31 08:37:42 +1000

    Format source

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -656,8 +656,8 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, QWidget * parent, 
   addDockWidget( Qt::BottomDockWidgetArea, mLogDock );
   mLogDock->setWidget( mLogViewer );
   mLogDock->hide();
-    connect( mMessageButton, SIGNAL( toggled( bool ) ), mLogDock, SLOT( setVisible(bool)) );
-    connect( mLogDock, SIGNAL( visibilityChanged( bool ) ), mMessageButton, SLOT( setChecked(bool)) );
+  connect( mMessageButton, SIGNAL( toggled( bool ) ), mLogDock, SLOT( setVisible( bool ) ) );
+  connect( mLogDock, SIGNAL( visibilityChanged( bool ) ), mMessageButton, SLOT( setChecked( bool ) ) );
   mVectorLayerTools = new QgsGuiVectorLayerTools();
 
   // Init the editor widget types
@@ -1821,15 +1821,15 @@ void QgisApp::createStatusBar()
   statusBar()->addPermanentWidget( mOnTheFlyProjectionStatusButton, 0 );
   statusBar()->showMessage( tr( "Ready" ) );
 
-    mMessageButton = new QToolButton( statusBar() );
-    mMessageButton->setAutoRaise( true );
-    mMessageButton->setIcon( QgsApplication::getThemeIcon( "bubble.svg" ) );
-    mMessageButton->setText("Messages");
-    mMessageButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-    mMessageButton->setObjectName( "mMessageLogViewerButton" );
+  mMessageButton = new QToolButton( statusBar() );
+  mMessageButton->setAutoRaise( true );
+  mMessageButton->setIcon( QgsApplication::getThemeIcon( "bubble.svg" ) );
+  mMessageButton->setText( "Messages" );
+  mMessageButton->setToolButtonStyle( Qt::ToolButtonTextBesideIcon );
+  mMessageButton->setObjectName( "mMessageLogViewerButton" );
   mMessageButton->setMaximumHeight( mScaleLabel->height() );
-    mMessageButton->setCheckable( true );
-    statusBar()->addPermanentWidget( mMessageButton, 0 );
+  mMessageButton->setCheckable( true );
+  statusBar()->addPermanentWidget( mMessageButton, 0 );
 }
 
 void QgisApp::setIconSizes( int size )
