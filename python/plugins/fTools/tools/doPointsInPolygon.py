98a72425a38ae4aec3269e82a082101caf0252b0
Sören Gebbert <soerengebbert@googlemail.com>
2015-01-16 14:11:33 +0100

    Integration of the modified fTools plugin into master

--- python/plugins/fTools/tools/doPointsInPolygon.py
+++ python/plugins/fTools/tools/doPointsInPolygon.py
@@ -34,6 +34,10 @@ import ftools_utils
 from qgis.core import *
 from ui_frmPointsInPolygon import Ui_Dialog
 
+
+typeInt = 1
+typeDouble = 2
+
 class Dialog(QDialog, Ui_Dialog):
 
     def __init__(self, iface):
@@ -46,6 +50,9 @@ class Dialog(QDialog, Ui_Dialog):
         self.btnClose = self.buttonBox.button( QDialogButtonBox.Close )
 
         QObject.connect(self.toolOut, SIGNAL("clicked()"), self.outFile)
+        QObject.connect(self.inPoint, SIGNAL("currentIndexChanged(QString)"), self.listPointFields)
+        QObject.connect(self.inPoint, SIGNAL("activated(QString)"), self.listPointFields)
+        
         self.progressBar.setValue(0)
         self.populateLayers()
 
@@ -57,6 +64,26 @@ class Dialog(QDialog, Ui_Dialog):
         self.inPoint.clear()
         layers = ftools_utils.getLayerNames([QGis.Point])
         self.inPoint.addItems(layers)
+        
+    def listPointFields(self):
+        if self.inPoint.currentText() == "":
+            pass
+
+        inPnts = ftools_utils.getVectorLayerByName(self.inPoint.currentText())
+        if inPnts:
+            pointFieldList = ftools_utils.getFieldList(inPnts)
+            
+            self.attributeList.clear()
+            for field in pointFieldList:
+                if field.type() == QVariant.Int or field.type() ==QVariant.Double:
+                    if field.type() == QVariant.Int:
+                        global typeInt
+                        item = QListWidgetItem(str(field.name()),  None,  typeInt)
+                    else:
+                        global typeDouble
+                        item = QListWidgetItem(str(field.name()),  None,  typeDouble)
+                    item.setToolTip("Attribute <%s> of type %s"%(field.name(),  field.typeName()))
+                    self.attributeList.addItem(item)
 
     def outFile(self):
         self.outShape.clear()
@@ -84,13 +111,14 @@ class Dialog(QDialog, Ui_Dialog):
 
             polyProvider = inPoly.dataProvider()
             pointProvider = inPnts.dataProvider()
-            if polyProvider.crs() <> pointProvider.crs():
+            if polyProvider.crs() != pointProvider.crs():
                 QMessageBox.warning(self, self.tr("CRS warning!"),
                                     self.tr("Warning: Input layers have non-matching CRS.\nThis may cause unexpected results."))
 
             self.btnOk.setEnabled(False)
 
-            self.workThread = PointsInPolygonThread(inPoly, inPnts, self.lnField.text(), self.outShape.text(), self.encoding)
+            self.workThread = PointsInPolygonThread(self, inPoly, inPnts, self.lnField.text(), self.outShape.text(), self.encoding, 
+                                                    self.attributeList,  self.statisticSelector)
 
             QObject.connect(self.workThread, SIGNAL("rangeChanged(int)"), self.setProgressRange)
             QObject.connect(self.workThread, SIGNAL("updateProgress()"), self.updateProgress)
@@ -141,7 +169,7 @@ class Dialog(QDialog, Ui_Dialog):
         self.btnOk.setEnabled(True)
 
 class PointsInPolygonThread(QThread):
-    def __init__( self, inPoly, inPoints, fieldName, outPath, encoding ):
+    def __init__( self, widget,  inPoly, inPoints, fieldName, outPath, encoding,  attributeList,  statisticSelector):
         QThread.__init__( self, QThread.currentThread() )
         self.mutex = QMutex()
         self.stopMe = 0
@@ -152,6 +180,9 @@ class PointsInPolygonThread(QThread):
         self.fieldName = fieldName
         self.outPath = outPath
         self.encoding = encoding
+        self.attributeList = attributeList
+        self.statistics = statisticSelector.currentText()
+        self.widget = widget
 
     def run(self):
         self.mutex.lock()
@@ -163,11 +194,23 @@ class PointsInPolygonThread(QThread):
         polyProvider = self.layerPoly.dataProvider()
         pointProvider = self.layerPoints.dataProvider()
 
-        fieldList = ftools_utils.getFieldList(self.layerPoly)
+        fieldList = ftools_utils.getFieldList(self.layerPoly)        
         index = polyProvider.fieldNameIndex(unicode(self.fieldName))
         if index == -1:
             index = polyProvider.fields().count()
             fieldList.append( QgsField(unicode(self.fieldName), QVariant.Int, "int", 10, 0, self.tr("point count field")) )
+
+        # Add the selected vector fields to the output polygon vector layer
+        selectedItems = self.attributeList.selectedItems()
+        for item in selectedItems:
+            global typeDouble
+            columnName = unicode(item.text() + "_" + self.statistics)
+            index = polyProvider.fieldNameIndex(unicode(columnName))
+            if index == -1:
+                if item.type() == typeDouble or self.statistics == "mean":
+                    fieldList.append( QgsField(columnName, QVariant.Double, "double", 10, 2,  "Value") )
+                else:
+                    fieldList.append( QgsField(columnName, QVariant.Int, "int", 10, 0,  "Value") )
 
         sRs = polyProvider.crs()
         if QFile(self.outPath).exists():
@@ -189,7 +232,7 @@ class PointsInPolygonThread(QThread):
         while polyFit.nextFeature(polyFeat):
             inGeom = polyFeat.geometry()
             atMap = polyFeat.attributes()
-            outFeat.setAttributes(atMap)
+            outFeat.setAttributes(atMap)   
             outFeat.setGeometry(inGeom)
 
             count = 0
@@ -202,12 +245,17 @@ class PointsInPolygonThread(QThread):
                 hasIntersection = False
 
             if hasIntersection:
+                valueList = {}
+                for item in selectedItems:
+                    valueList[item.text()] = []
                 for p in pointList:
                     pointProvider.getFeatures( QgsFeatureRequest().setFilterFid( p ) ).nextFeature( pntFeat )
                     tmpGeom = QgsGeometry(pntFeat.geometry())
                     if inGeom.intersects(tmpGeom):
                         count += 1
-
+                        for item in selectedItems:
+                            valueList[item.text()].append(pntFeat.attribute(item.text()))
+                            
                     self.mutex.lock()
                     s = self.stopMe
                     self.mutex.unlock()
@@ -215,10 +263,27 @@ class PointsInPolygonThread(QThread):
                         interrupted = True
                         break
 
-            atMap.append(count)
+                atMap.append(count)
+
+                # Compute the statistical values for selected vector attributes
+                for item in selectedItems:
+                    values = valueList[item.text()]
+                    if values and len(values) > 0:
+                        if self.statistics == "sum":
+                            value = reduce(myAdder,  values)
+                        elif self.statistics == "mean":
+                            value = reduce(myAdder,  values) / float(len(values))
+                        elif self.statistics == "min":
+                            values.sort()
+                            value = values[0]
+                        elif self.statistics == "max":
+                            values.sort()
+                            value = values[-1]
+                        atMap.append(value)
+
             outFeat.setAttributes(atMap)
             writer.addFeature(outFeat)
-
+            
             self.emit( SIGNAL( "updateProgress()" ) )
 
             self.mutex.lock()
@@ -241,3 +306,6 @@ class PointsInPolygonThread(QThread):
         self.mutex.unlock()
 
         QThread.wait( self )
+        
+def myAdder(x,y): 
+    return x+y
