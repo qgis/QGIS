e96b204b09f71d3fd6812fb2ac3aab44335419d6
Nyall Dawson <nyall.dawson@gmail.com>
2015-02-12 21:10:06 +1100

    Fix failing pal labeling canvas test
    Make sure image dpi respects mapsettings dpi in
    QgsMapRendererSequentialJob.
    
    Also apply a workaround to avoid segfault on end of test. This
    workaround probably should be moved to QgsApplication::exitQgis()
    but I'll do that after release in case of regressions.

--- tests/src/python/test_qgspallabeling_canvas.py
+++ tests/src/python/test_qgspallabeling_canvas.py
@@ -17,9 +17,10 @@ __copyright__ = 'Copyright 2013, The QGIS Project'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
+import qgis
 import sys
 import os
-from PyQt4.QtCore import qDebug
+from PyQt4.QtCore import qDebug, QThreadPool
 
 from utilities import (
     getTempfilePath,
@@ -49,6 +50,8 @@ class TestCanvasBase(TestQgsPalLabeling):
         TestQgsPalLabeling.tearDownClass()
         cls.removeMapLayer(cls.layer)
         cls.layer = None
+        #avoid crash on finish, probably related to https://bugreports.qt.io/browse/QTBUG-35760
+        QThreadPool.globalInstance().waitForDone()
 
     def setUp(self):
         """Run before each test."""
