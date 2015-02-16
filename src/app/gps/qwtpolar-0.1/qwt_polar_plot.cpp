e338daf206106bff9c6b84df94cfc7670c32f9cd
Nyall Dawson <nyall.dawson@gmail.com>
2015-02-16 20:17:55 +1100

    Fix misuse of enum in qwt_polar_plot

--- src/app/gps/qwtpolar-0.1/qwt_polar_plot.cpp
+++ src/app/gps/qwtpolar-0.1/qwt_polar_plot.cpp
@@ -753,7 +753,7 @@ QwtScaleMap QwtPolarPlot::scaleMap( int scaleId, const double radius ) const
   map.setScaleInterval( sd->lowerBound(), sd->upperBound() );
 #endif
 
-  if ( scaleId == QwtPolar::Azimuth )
+  if ( scaleId == QwtPolar::ScaleAzimuth )
   {
     map.setPaintXInterval( d_data->azimuthOrigin,
                            d_data->azimuthOrigin + M_2PI );
@@ -826,7 +826,7 @@ void QwtPolarPlot::initPlot( const QwtText &title )
   {
     ScaleData &scaleData = d_data->scaleData[scaleId];
 
-    if ( scaleId == QwtPolar::Azimuth )
+    if ( scaleId == QwtPolar::ScaleAzimuth )
     {
       scaleData.minValue = 0.0;
       scaleData.maxValue = 360.0;
