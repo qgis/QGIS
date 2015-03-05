4e34296b98caee19d67c438fd40b0612dafa35e2
Juergen E. Fischer <jef@norbit.de>
2015-03-05 02:20:56 +0100

    osgeo4w: also remove nightly shortcuts
    (cherry picked from commit f40d547aecad8f71eb5ac7fdbf21a784926ab28c)

---
+++ ms-windows/osgeo4w/preremove-dev.bat
@@ -0,0 +1,11 @@
+del "%OSGEO4W_STARTMENU%\QGIS Desktop @version@ (Nightly).lnk"
+del "%OSGEO4W_STARTMENU%\QGIS Browser @version@ (Nightly).lnk"
+del "%OSGEO4W_STARTMENU%\Qt Designer with QGIS @version@ custom widgets (Nightly).lnk"
+del "%ALLUSERSPROFILE%\Desktop\QGIS Desktop @version@ (Nightly).lnk"
+del "%ALLUSERSPROFILE%\Desktop\QGIS Browser @version@ (Nightly).lnk"
+del "%ALLUSERSPROFILE%\Desktop\Qt Designer with QGIS @version@ custom widgets (Nightly).lnk"
+del "%OSGEO4W_ROOT%"\bin\@package@.bat
+del "%OSGEO4W_ROOT%"\bin\@package@-browser.bat
+del "%OSGEO4W_ROOT%"\bin\designer-@package@.bat
+del "%OSGEO4W_ROOT%"\apps\@package@\python\qgis\qgisconfig.py
+del "%OSGEO4W_ROOT%"\apps\@package@\bin\qgis.reg
