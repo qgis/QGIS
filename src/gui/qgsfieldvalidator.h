bfb8ab6893d5bf77b4ae92c6c90e0b5b9c7e9ae7
Juergen E. Fischer <jef@norbit.de>
2015-03-18 16:05:22 +0100

    edit widgets: keep default values of new feature's attributes until they are added (fixes #12391 & #12386)

--- src/gui/qgsfieldvalidator.h
+++ src/gui/qgsfieldvalidator.h
@@ -31,7 +31,7 @@ class GUI_EXPORT QgsFieldValidator : public QValidator
     Q_OBJECT
 
   public:
-    QgsFieldValidator( QObject *parent, const QgsField &field, QString dateFormat = "yyyy-MM-dd" );
+    QgsFieldValidator( QObject *parent, const QgsField &field, QString defaultValue, QString dateFormat = "yyyy-MM-dd" );
     ~QgsFieldValidator();
 
     virtual State validate( QString &, int & ) const override;
@@ -46,6 +46,7 @@ class GUI_EXPORT QgsFieldValidator : public QValidator
     QValidator *mValidator;
     QgsField mField;
     QString mNullValue;
+    QString mDefaultValue;
     QString mDateFormat;
 };
 
