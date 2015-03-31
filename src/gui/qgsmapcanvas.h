c3321e5d62bbedc947ba5517b9d27430b483b343
rldhont <rldhont@gmail.com>
2015-03-31 10:42:03 +0200

    [BUGFIX] Update DatumTransformInfo on layerCrsChanged
    The QgsMapCanvas datumTransformInfo is not updated after a layerCrsChanged.
     This causes a bug in QGIS-Server which does not use the right srcAuthId.
    
    To update QgsMapCanvas datumTransformInfo, the user had to change the map
     canvas CRS or to disable/enable transform.
    
    This patch add a SLOT to the QgsMapLayer layerCrs Changed SIGNAL to update
     QgsMapCanvas datumTransformInfo.

--- src/gui/qgsmapcanvas.h
+++ src/gui/qgsmapcanvas.h
@@ -409,6 +409,9 @@ class GUI_EXPORT QgsMapCanvas : public QGraphicsView
 
     //! This slot is connected to the visibility change of one or more layers
     void layerStateChange();
+    
+    //! This slot is connected to the layer's CRS change
+    void layerCrsChange();
 
     //! Whether to suppress rendering or not
     void setRenderFlag( bool theFlag );
