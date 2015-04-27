8388ae018c71fb1be58d1c7a575bfd37e883b5d6
elpaso <apasotti@gmail.com>
2015-04-27 14:19:53 +0200

    Fixed error when passing unicode error message to DlgDbError

--- python/plugins/db_manager/db_model.py
+++ python/plugins/db_manager/db_model.py
@@ -155,7 +155,7 @@ class ConnectionItem(TreeItem):
                     return False
 
             except BaseError, e:
-                DlgDbError.showError(unicode(e), None)
+                DlgDbError.showError(e, None)
                 return False
 
         database = connection.database()
