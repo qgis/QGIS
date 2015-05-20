e32105397a8c118f85309c6aa8a3a27893a971d4
Tobias Reber <tobias.reber@bd.so.ch>
2015-05-20 17:16:07 +0200

    Hej fra Noedebo

---
+++ tests/src/python/test_qgsrangewidgets.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""QGIS Unit tests for edit widgets.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+__author__ = 'Tobias Reber'
+__date__ = '20/05/2015'
+__copyright__ = 'Copyright 2015, The QGIS Project'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import qgis
+import os
+
+from qgis.core import QgsFeature, QgsGeometry, QgsPoint, QgsVectorLayer, NULL
+
+from qgis.gui import QgsEditorWidgetRegistry
+
+from PyQt4 import QtCore
+
+from utilities import (unitTestDataPath,
+                       getQgisTestApp,
+                       TestCase,
+                       unittest
+                       )
+QGISAPP, CANVAS, IFACE, PARENT = getQgisTestApp()
+
+
+class TestQgsRangeWidget(TestCase):
+
+    def setUp(self):
+        """
+        create a layer with one feature
+        """
+        QgsEditorWidgetRegistry.initEditors()
+        self.layer = QgsVectorLayer("Point?crs=EPSG:21781&field=fldtxt:string&field=fldint:integer",
+                               "addfeat", "memory")
+        pr = self.layer.dataProvider()
+        f = QgsFeature()
+        f.setAttributes(["Hello World", 123])
+        f.setGeometry(QgsGeometry.fromPoint(QgsPoint(600000,200000)))
+
+        reg = QgsEditorWidgetRegistry.instance()
+        configWdg = reg.createConfigWidget('Range', self.layer, 1, None)
+        config = configWdg.config()
+        self.rangewidget = reg.create('Range', self.layer, 1, config, None, None )
+       
+
+    def test_range_widget_numbers(self):
+        """
+        are the numbers being returned correctly
+        """
+        self.rangewidget.setValue(1)
+        assert self.rangewidget.value() == 1
+
+        self.rangewidget.setValue(0)
+        assert self.rangewidget.value() == 0
+    
+
+    def test_range_widget_null(self):
+        """
+        Is None being returned as expected
+        """
+        self.rangewidget.setValue(NULL)
+        assert self.rangewidget.value() == 0
+
+        self.rangewidget.setValue(None)
+        assert self.rangewidget.value() == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
+
