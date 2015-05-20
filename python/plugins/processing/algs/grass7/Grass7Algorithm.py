028467eaf93ca5422625326e254de25197388a96
Alexander Bruy <alexander.bruy@gmail.com>
2015-05-20 11:24:05 +0300

    [processing] fix handling of the HTML output from the  GRASS7 algorithms (fix #12710)

--- python/plugins/processing/algs/grass7/Grass7Algorithm.py
+++ python/plugins/processing/algs/grass7/Grass7Algorithm.py
@@ -81,7 +81,7 @@ class Grass7Algorithm(GeoAlgorithm):
         return QIcon(os.path.join(pluginPath, 'images', 'grass.png'))
 
     def help(self):
-        return False, 'http://grass.osgeo.org/grass70/manuals/' + self.grassName \
+        return False, 'http://grass.osgeo.org/grass70/manuals/' + self.grass7Name \
             + '.html'
 
     def getParameterDescriptions(self):
@@ -107,7 +107,7 @@ class Grass7Algorithm(GeoAlgorithm):
     def defineCharacteristicsFromFile(self):
         lines = open(self.descriptionFile)
         line = lines.readline().strip('\n').strip()
-        self.grassName = line
+        self.grass7Name = line
         line = lines.readline().strip('\n').strip()
         self.name = line
         line = lines.readline().strip('\n').strip()
@@ -291,7 +291,7 @@ class Grass7Algorithm(GeoAlgorithm):
 
         # 2: Set parameters and outputs
 
-        command = self.grassName
+        command = self.grass7Name
         for param in self.parameters:
             if param.value is None or param.value == '':
                 continue
@@ -361,7 +361,7 @@ class Grass7Algorithm(GeoAlgorithm):
                 commands.append('g.region raster=' + out.name + uniqueSufix)
                 outputCommands.append('g.region raster=' + out.name
                                       + uniqueSufix)
-                if self.grassName == 'r.composite':
+                if self.grass7Name == 'r.composite':
                     command = 'r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW"'
                     command += ' input='
                     command += out.name + uniqueSufix
@@ -370,9 +370,9 @@ class Grass7Algorithm(GeoAlgorithm):
                     command = 'r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW"'
                     command += ' input='
 
-                if self.grassName == 'r.horizon':
+                if self.grass7Name == 'r.horizon':
                     command += out.name + uniqueSufix + '_0'
-                elif self.grassName == 'r.composite':
+                elif self.grass7Name == 'r.composite':
                     commands.append(command)
                     outputCommands.append(command)
                 else:
