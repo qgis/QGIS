qgis/QGIS

commit e568493653a8cd5921ad1835df7911d252f00940

Fix use of uninitialized values when no ellipsoid set (fix #11848)

src/core/qgsdistancearea.cpp
@@ -785,6 +785,12 @@ double QgsDistanceArea::getQbar( double x )
 
 void QgsDistanceArea::computeAreaInit()
 {
+  //don't try to perform calculations if no ellipsoid
+  if ( mEllipsoid == GEO_NONE )
+  {
+    return;
+  }
+
   double a2 = ( mSemiMajor * mSemiMajor );
   double e2 = 1 - ( a2 / ( mSemiMinor * mSemiMinor ) );
   double e4, e6;

commit 19c1dc69e9ff09cbb4323cb73a64c7cd805dc63f

Fix #12406 (measured area is wrong when computed on ellipsoid)
Introduced in e568493

src/core/qgsdistancearea.cpp
@@ -232,10 +232,11 @@ bool QgsDistanceArea::setEllipsoid( const QString& ellipsoid )
   // set transformation from project CRS to ellipsoid coordinates
   mCoordTransform->setDestCRS( destCRS );
 
+  mEllipsoid = ellipsoid;
+
   // precalculate some values for area calculations
   computeAreaInit();
 
-  mEllipsoid = ellipsoid;
   return true;
 }
 