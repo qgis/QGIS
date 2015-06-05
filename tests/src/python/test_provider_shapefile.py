476ee1d4adf6093d32aa72082b0303adec44a849
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-06-05 14:25:21 +0200

    Change test description (Minor change)

--- tests/src/python/test_provider_shapefile.py
+++ tests/src/python/test_provider_shapefile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""QGIS Unit tests for the postgres provider.
+"""QGIS Unit tests for the OGR/Shapefile provider.
 
 .. note:: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
