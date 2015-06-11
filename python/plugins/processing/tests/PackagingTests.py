61698fc63c658fbe968958881d9fff07f3d6ad1d
volaya <volayaf@gmail.com>
2015-06-11 09:28:02 +0200

    [processing] updated packaging tests

--- python/plugins/processing/tests/PackagingTests.py
+++ python/plugins/processing/tests/PackagingTests.py
@@ -7,13 +7,14 @@ import sys
 from processing.algs.saga.SagaUtils import *
 from processing.core.ProcessingConfig import ProcessingConfig
 from processing.algs.grass.GrassUtils import GrassUtils
+from processing.algs.otb.OTBUtils import OTBUtils
 
 class PackageTests(unittest.TestCase):
 
     def testSaga(self):
         folder = ProcessingConfig.getSetting(SAGA_FOLDER)
         ProcessingConfig.removeSetting(SAGA_FOLDER)
-        self.assertEqual("2.1.4", getSagaInstalledVersion(True))
+        self.assertTrue(getSagaInstalledVersion(True) in ["2.1.2", "2.1.3", "2.1.4"])
         ProcessingConfig.setSettingValue(SAGA_FOLDER, folder)
 
     def testGrass(self):
@@ -22,6 +23,11 @@ class PackageTests(unittest.TestCase):
         msg = GrassUtils.checkGrassIsInstalled()
         self.assertIsNone(msg)
         ProcessingConfig.setSettingValue(GrassUtils.GRASS_FOLDER, folder)
+
+
+    def testOtb(self):
+        folder = OTBUtils.findOtbPath()
+        self.assertIsNotNone(folder)            
 
 def runTests():
     t = unittest.TestLoader().loadTestsFromTestCase(PackageTests)
