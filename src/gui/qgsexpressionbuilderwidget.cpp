709c14391b1d5e40ff944f4834facd19bd111cad
Nyall Dawson <nyall.dawson@gmail.com>
2015-06-11 12:02:27 +1000

    Change expression help from free form html files to structured json
    A new script then parses these json help files into the html help
    contents, in a standardised manner. Previously, the function help
    was a bunch of different formats and structures.
    
    Now, a custom stylesheet can be applied (and later tweaked!) to
    format these help. Eg, this commit colors variables and function
    names in the syntax section for greater readability.

--- src/gui/qgsexpressionbuilderwidget.cpp
+++ src/gui/qgsexpressionbuilderwidget.cpp
@@ -707,6 +707,19 @@ void QgsExpressionBuilderWidget::setExpressionState( bool state )
   mExpressionValid = state;
 }
 
+QString QgsExpressionBuilderWidget::helpStylesheet() const
+{
+  //start with default QGIS report style
+  QString style = QgsApplication::reportStyleSheet();
+
+  //add some tweaks
+  style += " .functionname {color: #0a6099; font-weight: bold;} "
+           " .argument {font-family: monospace; color: #bf0c0c; font-style: italic; } "
+           " td.argument { padding-right: 10px; }";
+
+  return style;
+}
+
 QString QgsExpressionBuilderWidget::loadFunctionHelp( QgsExpressionItem* expressionItem )
 {
   if ( !expressionItem )
@@ -725,8 +738,7 @@ QString QgsExpressionBuilderWidget::loadFunctionHelp( QgsExpressionItem* express
       helpContents = QgsExpression::helptext( name );
   }
 
-  QString myStyle = QgsApplication::reportStyleSheet();
-  return "<head><style>" + myStyle + "</style></head><body>" + helpContents + "</body>";
+  return "<head><style>" + helpStylesheet() + "</style></head><body>" + helpContents + "</body>";
 }
 
 
