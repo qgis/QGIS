b0e7e7045f48a1a7ff84f036bd71bd4a73bf8539
Juergen E. Fischer <jef@norbit.de>
2015-07-31 10:38:27 +0200

    osgeo4w: switch to grass 7.0.1 from RC2

--- ms-windows/osgeo4w/package-nightly.cmd
+++ ms-windows/osgeo4w/package-nightly.cmd
@@ -56,7 +56,7 @@ if "%ARCH%"=="x86" goto devenv_x86
 goto devenv_x86_64
 
 :devenv_x86
-set GRASS_VERSIONS=6.4.4 7.0.1RC2
+set GRASS_VERSIONS=6.4.4 7.0.1
 call "%PF86%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 if exist "c:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" call "c:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x86 /Release
 path %path%;%PF86%\Microsoft Visual Studio 10.0\VC\bin
@@ -68,7 +68,7 @@ set CMAKE_OPT=^
 	-D WITH_GRASS=TRUE ^
 	-D WITH_GRASS7=TRUE ^
 	-D GRASS_PREFIX=%O4W_ROOT%/apps/grass/grass-6.4.4 ^
-	-D GRASS_PREFIX7=%O4W_ROOT%/apps/grass/grass-7.0.1RC2 ^
+	-D GRASS_PREFIX7=%O4W_ROOT%/apps/grass/grass-7.0.1 ^
 	-D CMAKE_CXX_FLAGS_RELWITHDEBINFO="/MD /ZI /MP /Od /D NDEBUG /D QGISDEBUG" ^
 	-D CMAKE_PDB_OUTPUT_DIRECTORY_RELWITHDEBINFO=%BUILDDIR%\apps\%PACKAGENAME%\pdb
 goto devenv
