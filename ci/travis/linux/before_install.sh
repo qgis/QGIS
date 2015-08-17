9d8b374fe9f59095b6eb055413d73439c69738ec
Matthias Kuhn <matthias@opengis.ch>
2015-08-17 19:52:26 +0200

    CI for Qt5

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -3,19 +3,33 @@ sudo add-apt-repository ppa:ubuntugis/ppa -y
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y # For postgresql-9.1-postgis-2.1
 sudo add-apt-repository ppa:smspillaz/cmake-3.0.2 -y
 sudo add-apt-repository ppa:kedazo/doxygen-updates-precise -y # For doxygen 1.8.8
+if [${QT} == 5]; then
+  sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+fi
+
 sudo apt-get update -qq
+
 sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
                          bison cmake cmake-data doxygen flex git graphviz \
                          grass-dev libexpat1-dev libfcgi-dev libgdal1-dev \
                          libgeos-dev libgsl0-dev libpq-dev libproj-dev \
-                         libqscintilla2-dev libqt4-dev libqt4-opengl-dev \
-                         libqt4-sql-sqlite libqtwebkit-dev libqwt-dev \
-                         libspatialindex-dev libspatialite-dev libsqlite3-dev \
-                         lighttpd pkg-config poppler-utils pyqt4-dev-tools \
-                         python python-dev python-qt4 python-qt4-dev \
-                         python-sip python-sip-dev spawn-fcgi txt2tags \
-                         xauth xfonts-100dpi xfonts-75dpi xfonts-base \
+                         libqscintilla2-dev libqwt-dev libspatialindex-dev \
+                         libspatialite-dev libsqlite3-dev lighttpd pkg-config \
+                         poppler-utils python python-dev python-sip \
+                         python-sip-dev spawn-fcgi txt2tags xauth \
+                         xfonts-100dpi xfonts-75dpi xfonts-base \
                          xfonts-scalable xvfb \
                          postgresql-9.1-postgis-2.1/precise # postgis one from ubuntugis-unstable, not pgdg
+
+if [${QT} == 5]; then
+  sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
+    qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev qt5-default \
+    qttools5-dev-tools
+else
+  sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
+    libqt4-dev libqt4-opengl-dev libqt4-sql-sqlite libqtwebkit-dev \
+    pyqt4-dev-tools ython-qt4 python-qt4-dev
+fi
+
 cmake --version
 clang --version
