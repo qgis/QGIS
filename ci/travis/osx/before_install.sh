1f48efaecdb67c504c281cd37d66231fdcbd3349
Nyall Dawson <nyall.dawson@gmail.com>
2015-08-19 19:16:24 +1000

    Cull unneeded dependencies from Travis OSX build

--- ci/travis/osx/before_install.sh
+++ ci/travis/osx/before_install.sh
@@ -1,6 +1,6 @@
 brew tap osgeo/osgeo4mac
 brew update
-brew install osgeo/osgeo4mac/qgis-28 --only-dependencies
+brew install osgeo/osgeo4mac/qgis-28 --without-postgis --without-postgresql --without-grass --without-gpsbabel --only-dependencies 
 brew install spawn-fcgi
 brew install lighttpd
 brew install poppler
@@ -12,3 +12,6 @@ brew ln expat --force
 brew ln libxml2 --force
 brew ln gettext --force
 brew ln libffi --force
+
+mkdir -p /Users/travis/Library/Python/2.7/lib/python/site-packages
+echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> /Users/travis/Library/Python/2.7/lib/python/site-packages/homebrew.pth
