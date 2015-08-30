1e6f673a2321eb53269a9c4a5d4e4214801cde62
Matthias Kuhn <matthias@opengis.ch>
2015-08-30 12:59:30 +0200

    Update email address

--- src/gui/qgsexpressionselectiondialog.cpp
+++ src/gui/qgsexpressionselectiondialog.cpp
@@ -3,7 +3,7 @@
      --------------------------------------
     Date                 : 24.1.2013
     Copyright            : (C) 2013 by Matthias kuhn
-    Email                : matthias dot kuhn at gmx dot ch
+    Email                : matthias at opengis dot ch
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
