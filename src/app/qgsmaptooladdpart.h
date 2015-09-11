de75a89a0b405f2b8677f0074ba0bb77ce2a240c
Matthias Kuhn <matthias@opengis.ch>
2015-09-11 11:09:57 +0200

    Cad for add part map tool

--- src/app/qgsmaptooladdpart.h
+++ src/app/qgsmaptooladdpart.h
@@ -23,5 +23,5 @@ class APP_EXPORT QgsMapToolAddPart : public QgsMapToolCapture
   public:
     QgsMapToolAddPart( QgsMapCanvas* canvas );
     virtual ~QgsMapToolAddPart();
-    void canvasReleaseEvent( QgsMapMouseEvent * e ) override;
+    void cadCanvasReleaseEvent( QgsMapMouseEvent * e ) override;
 };
