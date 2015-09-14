0a60bab51288812f148306f3b50294f713b0af33
Matthias Kuhn <matthias@opengis.ch>
2015-09-14 11:06:45 +0200

    Doxymentation update
    Or how to increase documentation coverage with minimal effort

--- src/core/geometry/qgscircularstringv2.h
+++ src/core/geometry/qgscircularstringv2.h
@@ -55,22 +55,38 @@ class CORE_EXPORT QgsCircularStringV2: public QgsCurveV2
     /** Returns the point at index i within the circular string.
      */
     QgsPointV2 pointN( int i ) const;
+
+    /**
+     * @copydoc QgsCurveV2::points()
+     */
     void points( QList<QgsPointV2>& pts ) const override;
 
     /** Sets the circular string's points
      */
     void setPoints( const QList<QgsPointV2>& points );
 
-    //curve interface
+    /**
+     * @copydoc QgsAbstractGeometryV2::length()
+     */
     virtual double length() const override;
+
+    /**
+     * @copydoc QgsCurveV2::startPoint()
+     */
     virtual QgsPointV2 startPoint() const override;
+    /**
+     * @copydoc QgsCurveV2::endPoint()
+     */
     virtual QgsPointV2 endPoint() const override;
+    /**
+     * @copydoc QgsCurveV2::curveToLine()
+     */
     virtual QgsLineStringV2* curveToLine() const override;
 
     void draw( QPainter& p ) const override;
     /** Transforms the geometry using a coordinate transform
      * @param ct coordinate transform
-       @param d transformation direction
+     * @param d transformation direction
      */
     void transform( const QgsCoordinateTransform& ct, QgsCoordinateTransform::TransformDirection d = QgsCoordinateTransform::ForwardTransform ) override;
     void transform( const QTransform& t ) override;
@@ -78,6 +94,10 @@ class CORE_EXPORT QgsCircularStringV2: public QgsCurveV2
     void clip( const QgsRectangle& rect ) override;
 #endif
     void addToPainterPath( QPainterPath& path ) const override;
+
+    /**
+     * @copydoc QgsCurveV2::drawAsPolygon()
+     */
     void drawAsPolygon( QPainter& p ) const override;
 
     virtual bool insertVertex( const QgsVertexId& position, const QgsPointV2& vertex ) override;
@@ -85,10 +105,19 @@ class CORE_EXPORT QgsCircularStringV2: public QgsCurveV2
     virtual bool deleteVertex( const QgsVertexId& position ) override;
 
     double closestSegment( const QgsPointV2& pt, QgsPointV2& segmentPt,  QgsVertexId& vertexAfter, bool* leftOf, double epsilon ) const override;
+    /**
+     * @copydoc QgsCurveV2::pointAt()
+     */
     bool pointAt( int i, QgsPointV2& vertex, QgsVertexId::VertexType& type ) const override;
 
+    /**
+     * @copydoc QgsCurveV2::sumUpArea()
+     */
     void sumUpArea( double& sum ) const override;
 
+    /**
+     * @copydoc QgsAbstractGeometryV2::hasCurvedSegments()
+     */
     bool hasCurvedSegments() const override { return true; }
 
     /** Returns approximate rotation angle for a vertex. Usually average angle between adjacent segments.
