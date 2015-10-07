b680a1be9923049a2b99fd130091009e6802cfaa
Matthias Kuhn <matthias@opengis.ch>
2015-10-07 09:27:35 +0200

    Make travis files easier diff'able

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -5,19 +5,56 @@ sudo add-apt-repository ppa:grass/grass-stable -y
 sudo add-apt-repository ppa:smspillaz/cmake-3.0.2 -y
 sudo add-apt-repository ppa:kedazo/doxygen-updates-precise -y # For doxygen 1.8.8
 sudo apt-get update -qq
-sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
-                         bison cmake cmake-data doxygen flex git graphviz \
-                         grass-dev grass7-dev libexpat1-dev libfcgi-dev \
-                         libgdal1-dev libgeos-dev libgeos++-dev libgsl0-dev libpq-dev \
-                         libproj-dev libqscintilla2-dev libqt4-dev \
-                         libqt4-opengl-dev libqt4-sql-sqlite libqtwebkit-dev \
-                         libqca2-dev libqca2-plugin-ossl \
-                         libqwt-dev libspatialindex-dev libspatialite-dev \
-                         libsqlite3-dev lighttpd pkg-config poppler-utils \
-                         pyqt4-dev-tools python python-dev python-qt4 \
-                         python-qt4-dev python-sip python-sip-dev python-qt4-sql spawn-fcgi \
-                         txt2tags xauth xfonts-100dpi xfonts-75dpi xfonts-base \
-                         xfonts-scalable xvfb \
-                         postgresql-9.1-postgis-2.1/precise # postgis one from ubuntugis-unstable, not pgdg
+sudo \
+apt-get \
+install --force-yes --no-install-recommends --no-install-suggests \
+        bison \
+        cmake \
+        cmake-data \
+        doxygen \
+        flex \
+        git \
+        graphviz \
+        grass-dev \
+        grass7-dev \
+        libexpat1-dev \
+        libfcgi-dev \
+        libgdal1-dev \
+        libgeos++-dev \
+        libgeos-dev \
+        libgsl0-dev \
+        libpq-dev \
+        libproj-dev \
+        libqca2-dev \
+        libqca2-plugin-ossl \
+        libqscintilla2-dev \
+        libqt4-dev \
+        libqt4-opengl-dev \
+        libqt4-sql-sqlite \
+        libqtwebkit-dev \
+        libqwt-dev \
+        libspatialindex-dev \
+        libspatialite-dev \
+        libsqlite3-dev \
+        lighttpd \
+        pkg-config \
+        poppler-utils \
+        pyqt4-dev-tools \
+        python \
+        python-dev \
+        python-qt4 \
+        python-qt4-dev \
+        python-qt4-sql \
+        python-sip \
+        python-sip-dev \
+        spawn-fcgi \
+        txt2tags \
+        xauth \
+        xfonts-100dpi \
+        xfonts-75dpi \
+        xfonts-base \
+        xfonts-scalable \
+        xvfb \
+        postgresql-9.1-postgis-2.1/precise # from ubuntugis-unstable, not pgdg
 cmake --version
 clang --version
