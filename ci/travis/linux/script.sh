80ccb39ae5132420fba0716251297844369878b5
Nyall Dawson <nyall.dawson@gmail.com>
2015-10-10 22:06:06 +1100

    More test mask updates. Also disable qgis_labelingenginev2 on Travis
    qgis_labelingenginev2 needs to be refactored to use QgsRenderChecker.
    Directly comparing the two images is too fragile due and is failing
    on Travis.

--- ci/travis/linux/script.sh
+++ ci/travis/linux/script.sh
@@ -1,2 +1,2 @@
-xvfb-run ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest' -S ./qgis-test-travis.ctest --output-on-failure
+xvfb-run ctest -V -E 'qgis_labelingenginev2|qgis_openstreetmaptest|qgis_wcsprovidertest' -S ./qgis-test-travis.ctest --output-on-failure
 
