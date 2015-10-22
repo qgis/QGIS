83d99b1616d216d5f1c3bdfadb9c791d3ab32f58
Giovanni Manghi <giovanni.manghi@faunalia.pt>
2015-10-22 09:48:45 +0100

    Processing advanced parameters for gdal clip raster

--- python/plugins/processing/algs/gdal/ClipByMask.py
+++ python/plugins/processing/algs/gdal/ClipByMask.py
@@ -33,13 +33,16 @@ from processing.core.parameters import ParameterRaster
 from processing.core.parameters import ParameterVector
 from processing.core.parameters import ParameterBoolean
 from processing.core.parameters import ParameterString
+from processing.core.parameters import ParameterSelection
+from processing.core.parameters import ParameterNumber
 
 from processing.core.outputs import OutputRaster
 
+from processing.algs.gdal.OgrAlgorithm import OgrAlgorithm
 from processing.algs.gdal.GdalUtils import GdalUtils
 
 
-class ClipByMask(GdalAlgorithm):
+class ClipByMask(GdalAlgorithm,OgrAlgorithm):
 
     INPUT = 'INPUT'
     OUTPUT = 'OUTPUT'
@@ -48,7 +51,18 @@ class ClipByMask(GdalAlgorithm):
     ALPHA_BAND = 'ALPHA_BAND'
     KEEP_RESOLUTION = 'KEEP_RESOLUTION'
     EXTRA = 'EXTRA'
-
+    RTYPE = 'RTYPE'
+    TYPE = ['Byte', 'Int16', 'UInt16', 'UInt32', 'Int32', 'Float32', 'Float64']
+    TILED = 'TILED'
+    COMPRESS = 'COMPRESS'
+    JPEGCOMPRESSION = 'JPEGCOMPRESSION'
+    PREDICTOR = 'PREDICTOR'
+    ZLEVEL = 'ZLEVEL'
+    BIGTIFF = 'BIGTIFF'
+    BIGTIFFTYPE = ['', 'YES', 'NO', 'IF_NEEDED', 'IF_SAFER']
+    COMPRESSTYPE = ['NONE', 'JPEG', 'LZW', 'PACKBITS', 'DEFLATE']
+    TFW = 'TFW'
+	
     def defineCharacteristics(self):
         self.name, self.i18n_name = self.trAlgorithm('Clip raster by mask layer')
         self.group, self.i18n_group = self.trAlgorithm('[GDAL] Extraction')
@@ -64,17 +78,55 @@ class ClipByMask(GdalAlgorithm):
                                            self.tr('Keep resolution of output raster'), False))
         self.addParameter(ParameterString(self.EXTRA,
                                           self.tr('Additional creation parameters'), '', optional=True))
+
+        params = []
+        params.append(ParameterSelection(self.RTYPE,
+                                             self.tr('Output raster type'), self.TYPE, 5))
+        params.append(ParameterSelection(self.COMPRESS,
+                                             self.tr('GeoTIFF options. Compression type:'), self.COMPRESSTYPE, 4))
+        params.append(ParameterNumber(self.JPEGCOMPRESSION,
+                                          self.tr('Set the JPEG compression level'),
+                                          1, 100, 75))
+        params.append(ParameterNumber(self.ZLEVEL,
+                                          self.tr('Set the DEFLATE compression level'),
+                                          1, 9, 6))
+        params.append(ParameterNumber(self.PREDICTOR,
+                                          self.tr('Set the predictor for LZW or DEFLATE compression'),
+                                          1, 3, 1))
+        params.append(ParameterBoolean(self.TILED,
+                                           self.tr('Create tiled output (only used for the GTiff format)'), False))
+        params.append(ParameterSelection(self.BIGTIFF,
+                                             self.tr('Control whether the created file is a BigTIFF or a classic TIFF'), self.BIGTIFFTYPE, 0))
+        params.append(ParameterBoolean(self.TFW,
+                                           self.tr('Force the generation of an associated ESRI world file (.tfw))'), False))
+        params.append(ParameterString(self.EXTRA,
+                                          self.tr('Additional creation parameters'), '', optional=True))
+
+        for param in params:
+            param.isAdvanced = True
+            self.addParameter(param)										  
+										  
         self.addOutput(OutputRaster(self.OUTPUT, self.tr('Clipped (mask)')))
 
     def getConsoleCommands(self):
         out = self.getOutputValue(self.OUTPUT)
         mask = self.getParameterValue(self.MASK)
+        ogrMask = self.ogrConnectionString(mask)[1:-1]
         noData = unicode(self.getParameterValue(self.NO_DATA))
         addAlphaBand = self.getParameterValue(self.ALPHA_BAND)
         keepResolution = self.getParameterValue(self.KEEP_RESOLUTION)
         extra = unicode(self.getParameterValue(self.EXTRA))
-
+        jpegcompression = unicode(self.getParameterValue(self.JPEGCOMPRESSION))
+        predictor = unicode(self.getParameterValue(self.PREDICTOR))
+        zlevel = unicode(self.getParameterValue(self.ZLEVEL))
+        tiled = unicode(self.getParameterValue(self.TILED))
+        compress = self.COMPRESSTYPE[self.getParameterValue(self.COMPRESS)]
+        bigtiff = self.BIGTIFFTYPE[self.getParameterValue(self.BIGTIFF)]
+        tfw = unicode(self.getParameterValue(self.TFW))
+		
         arguments = []
+        arguments.append('-ot')
+        arguments.append(self.TYPE[self.getParameterValue(self.RTYPE)])
         arguments.append('-q')
         arguments.append('-of')
         arguments.append(GdalUtils.getFormatShortNameFromFilename(out))
@@ -92,7 +144,7 @@ class ClipByMask(GdalAlgorithm):
             arguments.append('-tap')
 
         arguments.append('-cutline')
-        arguments.append(mask)
+        arguments.append(ogrMask)
         arguments.append('-crop_to_cutline')
 
         if addAlphaBand:
@@ -100,6 +152,20 @@ class ClipByMask(GdalAlgorithm):
 
         if len(extra) > 0:
             arguments.append(extra)
+        if GdalUtils.getFormatShortNameFromFilename(out) == "GTiff":
+            arguments.append("-co COMPRESS=" + compress)
+            if compress == 'JPEG':
+                arguments.append("-co JPEG_QUALITY=" + jpegcompression)
+            elif (compress == 'LZW') or (compress == 'DEFLATE'):
+                arguments.append("-co PREDICTOR=" + predictor)
+            if compress == 'DEFLATE':
+                arguments.append("-co ZLEVEL=" + zlevel)
+            if tiled == "True":
+                arguments.append("-co TILED=YES")
+            if tfw == "True":
+                arguments.append("-co TFW=YES")
+            if len(bigtiff) > 0:
+                arguments.append("-co BIGTIFF=" + bigtiff)
 
         arguments.append(self.getParameterValue(self.INPUT))
         arguments.append(out)
