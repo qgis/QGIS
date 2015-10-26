qgis/QGIS

commit 5be0ee734fa233f2a3f893ddc72e8264d029f2d0

Remove QgsFeatureRequest::FilterRect from providers/iterators

src/providers/wfs/qgswfsfeatureiterator.cpp
@@ -21,20 +21,18 @@
 QgsWFSFeatureIterator::QgsWFSFeatureIterator( QgsWFSFeatureSource* source, bool ownSource, const QgsFeatureRequest& request )
     : QgsAbstractFeatureIteratorFromSource<QgsWFSFeatureSource>( source, ownSource, request )
 {
-  switch ( request.filterType() )
+  if ( !request.filterRect().isNull() && mSource->mSpatialIndex )
   {
-    case QgsFeatureRequest::FilterRect:
-      if ( mSource->mSpatialIndex )
-      {
-        mSelectedFeatures = mSource->mSpatialIndex->intersects( request.filterRect() );
-      }
-      break;
-    case QgsFeatureRequest::FilterFid:
-      mSelectedFeatures.push_back( request.filterFid() );
-      break;
-    case QgsFeatureRequest::FilterNone:
-    default:
-      mSelectedFeatures = mSource->mFeatures.keys();
+    mSelectedFeatures = mSource->mSpatialIndex->intersects( request.filterRect() );
+  }
+
+  if ( request.filterType() == QgsFeatureRequest::FilterFid )
+  {
+    mSelectedFeatures.push_back( request.filterFid() );
+  }
+  else
+  {
+    mSelectedFeatures = mSource->mFeatures.keys();
   }
 
   mFeatureIterator = mSelectedFeatures.constBegin();
@@ -164,7 +162,7 @@ QgsWFSFeatureSource::~QgsWFSFeatureSource()
 
 QgsFeatureIterator QgsWFSFeatureSource::getFeatures( const QgsFeatureRequest& request )
 {
-  if ( request.filterType() == QgsFeatureRequest::FilterRect )
+  if ( !request.filterRect().isNull() )
     emit extentRequested( request.filterRect() );
   return QgsFeatureIterator( new QgsWFSFeatureIterator( this, false, request ) );
 }

python/core/qgsvectorlayer.sip
@@ -801,7 +801,12 @@ class QgsVectorLayer : QgsMapLayer
     /** Return the extent of the layer as a QRect */
     QgsRectangle extent();
 
-    /** Returns field list in the to-be-committed state */
+    /**
+     * Returns the list of fields of this layer.
+     * This also includes fields which have not yet been saved to the provider.
+     *
+     * @return A list of fields
+     */
     const QgsFields &pendingFields() const;
 
     /** Returns list of attributes */

src/core/qgsvectorlayer.cpp
@@ -2220,11 +2220,6 @@ bool QgsVectorLayer::deleteFeature( QgsFeatureId fid )
   return res;
 }
 
-const QgsFields &QgsVectorLayer::pendingFields() const
-{
-  return mUpdatedFields;
-}
-
 QgsAttributeList QgsVectorLayer::pendingAllAttributesList()
 {
   return mUpdatedFields.allAttributesList();

src/core/qgsvectorlayer.h
@@ -1300,8 +1300,13 @@ class CORE_EXPORT QgsVectorLayer : public QgsMapLayer
     /** Return the extent of the layer as a QRect */
     QgsRectangle extent() override;
 
-    /** Returns field list in the to-be-committed state */
-    const QgsFields &pendingFields() const;
+    /**
+     * Returns the list of fields of this layer.
+     * This also includes fields which have not yet been saved to the provider.
+     *
+     * @return A list of fields
+     */
+    const QgsFields &pendingFields() const { return mUpdatedFields; }
 
     /** Returns list of attributes */
     QgsAttributeList pendingAllAttributesList();

src/core/qgsvectorlayerfeatureiterator.cpp
@@ -176,7 +176,7 @@ bool QgsVectorLayerFeatureIterator::fetchFeature( QgsFeature& f )
     return res;
   }
 
-  if ( mRequest.filterType() == QgsFeatureRequest::FilterRect )
+  if ( !mRequest.filterRect().isNull() )
   {
     if ( fetchNextChangedGeomFeature( f ) )
       return true;
@@ -352,6 +352,10 @@ bool QgsVectorLayerFeatureIterator::fetchNextChangedAttributeFeature( QgsFeature
 {
   while ( mChangedFeaturesIterator.nextFeature( f ) )
   {
+    if ( mFetchConsidered.contains( f.id() ) )
+      // skip deleted features and those already handled by the geometry
+      continue;
+
     mFetchConsidered << f.id();
 
     updateChangedAttributes( f );
@@ -359,14 +363,7 @@ bool QgsVectorLayerFeatureIterator::fetchNextChangedAttributeFeature( QgsFeature
     if ( mHasVirtualAttributes )
       addVirtualAttributes( f );
 
-    if ( mRequest.filterType() == QgsFeatureRequest::FilterExpression )
-    {
-      if ( mRequest.filterExpression()->evaluate( &f ).toBool() )
-      {
-        return true;
-      }
-    }
-    else
+    if ( mRequest.filterExpression()->evaluate( &f ).toBool() )
     {
       return true;
     }

src/core/qgsvectorlayerrenderer.cpp
@@ -136,6 +136,7 @@ bool QgsVectorLayerRenderer::render()
 
   QgsRenderOptions opts = mRendererV2->startRender( mContext, mFields );
 
+  QgsDebugMsg( opts.whereClause() );
   QgsRectangle requestExtent = mContext.extent();
   mRendererV2->modifyRequestExtent( requestExtent, mContext );
 

src/providers/delimitedtext/qgsdelimitedtextfeatureiterator.cpp
@@ -44,21 +44,8 @@ QgsDelimitedTextFeatureIterator::QgsDelimitedTextFeatureIterator( QgsDelimitedTe
   mTestGeometry = false;
 
   mMode = FileScan;
-  if ( request.filterType() == QgsFeatureRequest::FilterFid )
-  {
-    QgsDebugMsg( "Configuring for returning single id" );
-    mFeatureIds.append( request.filterFid() );
-    mMode = FeatureIds;
-    mTestSubset = false;
-  }
-  // If have geometry and testing geometry then evaluate options...
-  // If we don't have geometry then all records pass geometry filter.
-  // CC: 2013-05-09
-  // Not sure about intended relationship between filtering on geometry and
-  // requesting no geometry? Have preserved current logic of ignoring spatial filter
-  // if not requesting geometry.
 
-  else if ( request.filterType() == QgsFeatureRequest::FilterRect && hasGeometry )
+  if ( !request.filterRect().isNull() && hasGeometry )
   {
     QgsDebugMsg( "Configuring for rectangle select" );
     mTestGeometry = true;
@@ -98,13 +85,32 @@ QgsDelimitedTextFeatureIterator::QgsDelimitedTextFeatureIterator( QgsDelimitedTe
     }
   }
 
-  // If we have a subset index then use it..
-  if ( mMode == FileScan && mSource->mUseSubsetIndex )
+  if ( request.filterType() == QgsFeatureRequest::FilterFid )
   {
-    QgsDebugMsg( QString( "Layer has subset index - use %1 items from subset index" ).arg( mSource->mSubsetIndex.size() ) );
+    QgsDebugMsg( "Configuring for returning single id" );
+    if ( request.filterRect().isNull() || ( !request.filterRect().isNull() && mFeatureIds.contains( request.filterFid() ) ) )
+    {
+      mFeatureIds = QList<QgsFeatureId>() << request.filterFid();
+    }
+    mMode = FeatureIds;
     mTestSubset = false;
-    mMode = SubsetIndex;
   }
+  // If have geometry and testing geometry then evaluate options...
+  // If we don't have geometry then all records pass geometry filter.
+  // CC: 2013-05-09
+  // Not sure about intended relationship between filtering on geometry and
+  // requesting no geometry? Have preserved current logic of ignoring spatial filter
+  // if not requesting geometry.
+
+  else
+
+    // If we have a subset index then use it..
+    if ( mMode == FileScan && mSource->mUseSubsetIndex )
+    {
+      QgsDebugMsg( QString( "Layer has subset index - use %1 items from subset index" ).arg( mSource->mSubsetIndex.size() ) );
+      mTestSubset = false;
+      mMode = SubsetIndex;
+    }
 
   // Otherwise just have to scan the file
   if ( mMode == FileScan )

src/providers/gpx/qgsgpxfeatureiterator.cpp
@@ -176,7 +176,7 @@ bool QgsGPXFeatureIterator::readFid( QgsFeature& feature )
 
 bool QgsGPXFeatureIterator::readWaypoint( const QgsWaypoint& wpt, QgsFeature& feature )
 {
-  if ( mRequest.filterType() == QgsFeatureRequest::FilterRect )
+  if ( !mRequest.filterRect().isNull() )
   {
     const QgsRectangle& rect = mRequest.filterRect();
     if ( ! rect.contains( QgsPoint( wpt.lon, wpt.lat ) ) )
@@ -206,7 +206,7 @@ bool QgsGPXFeatureIterator::readRoute( const QgsRoute& rte, QgsFeature& feature 
 
   QgsGeometry* theGeometry = readRouteGeometry( rte );
 
-  if ( mRequest.filterType() == QgsFeatureRequest::FilterRect )
+  if ( !mRequest.filterRect().isNull() )
   {
     const QgsRectangle& rect = mRequest.filterRect();
     if (( rte.xMax < rect.xMinimum() ) || ( rte.xMin > rect.xMaximum() ) ||
@@ -248,7 +248,7 @@ bool QgsGPXFeatureIterator::readTrack( const QgsTrack& trk, QgsFeature& feature 
 
   QgsGeometry* theGeometry = readTrackGeometry( trk );
 
-  if ( mRequest.filterType() == QgsFeatureRequest::FilterRect )
+  if ( !mRequest.filterRect().isNull() )
   {
     const QgsRectangle& rect = mRequest.filterRect();
     if (( trk.xMax < rect.xMinimum() ) || ( trk.xMin > rect.xMaximum() ) ||

commit a0a44f2b08394e640f9116c741d2bd70437154d2

Fix critical regression in WFS provider (introduced with 5be0ee734fa233f2a3f893ddc72e8264d029f2d0). Funded by Sourcepole QGIS Enterprise

src/providers/wfs/qgswfsfeatureiterator.cpp
@@ -25,8 +25,7 @@ QgsWFSFeatureIterator::QgsWFSFeatureIterator( QgsWFSFeatureSource* source, bool 
   {
     mSelectedFeatures = mSource->mSpatialIndex->intersects( request.filterRect() );
   }
-
-  if ( request.filterType() == QgsFeatureRequest::FilterFid )
+  else if ( request.filterType() == QgsFeatureRequest::FilterFid )
   {
     mSelectedFeatures.push_back( request.filterFid() );
   }