1bb7aff0678e32ab3652b88fd60312f1ba0ae8f4
Matthias Kuhn <matthias@opengis.ch>
2015-11-10 15:39:40 +0100

    [qt5] Fix QScintilla cmake file

--- cmake/FindQScintilla.cmake
+++ cmake/FindQScintilla.cmake
@@ -27,13 +27,14 @@ ELSE(EXISTS QSCINTILLA_VERSION_STR)
   FIND_PATH(QSCINTILLA_INCLUDE_DIR
     NAMES Qsci/qsciglobal.h
     PATHS
+      ${Qt5Core_INCLUDE_DIRS}
       "${QT_INCLUDE_DIR}"
       /usr/local/include
       /usr/include
     )
 
   if(ENABLE_QT5)
-    set(QSCINTILLA_LIBRARY_NAMES qscintilla2-qt5 libqscintilla2-qt5 libqscintilla2-qt5.dylib)
+    set(QSCINTILLA_LIBRARY_NAMES qscintilla2-qt5 libqscintilla2-qt5 qt5scintilla2 libqscintilla2-qt5.dylib)
   else(ENABLE_QT5)
     set(QSCINTILLA_LIBRARY_NAMES qscintilla2 libqscintilla2 libqscintilla2.dylib)
   endif(ENABLE_QT5)
