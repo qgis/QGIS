77526bd1a31e2f732a106a9ae6f493731eebb1ae
Juergen E. Fischer <jef@norbit.de>
2015-11-25 15:04:51 +0100

    remove references to coding (followup 7b0837f)

--- doc/build.sh
+++ doc/build.sh
@@ -19,8 +19,3 @@ txt2tags -o INSTALL.html -t html INSTALL.t2t
 txt2tags -o INSTALL.tex -t tex INSTALL.t2t
 pdflatex INSTALL.tex
 mv INSTALL.pdf ..
-txt2tags -o ../CODING -t txt CODING.t2t
-txt2tags -o CODING.html -t html CODING.t2t
-txt2tags -o CODING.tex -t tex CODING.t2t
-pdflatex CODING.tex
-mv CODING.pdf ..
