34df72c75b18a5b34ad65dd2ea6894ed0c07b072
radosuav <rmgu@dhi-gras.com>
2015-11-30 14:00:47 +0100

    [Processing] Use raster band scailing factor and offset in Zonal Statistics algorithm

--- python/plugins/processing/algs/qgis/ZonalStatistics.py
+++ python/plugins/processing/algs/qgis/ZonalStatistics.py
@@ -118,6 +118,7 @@ class ZonalStatistics(GeoAlgorithm):
 
             srcOffset = (startColumn, startRow, width, height)
             srcArray = rasterBand.ReadAsArray(*srcOffset)
+            srcArray = srcArray*rasterBand.GetScale()+rasterBand.GetOffset()
 
             newGeoTransform = (
                 geoTransform[0] + srcOffset[0] * geoTransform[1],
@@ -192,6 +193,7 @@ class ZonalStatistics(GeoAlgorithm):
 
                 srcOffset = (startColumn, startRow, width, height)
                 srcArray = rasterBand.ReadAsArray(*srcOffset)
+                srcArray = srcArray*rasterBand.GetScale()+rasterBand.GetOffset()
 
                 newGeoTransform = (
                     geoTransform[0] + srcOffset[0] * geoTransform[1],
