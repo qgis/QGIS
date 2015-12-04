5a7ee20fa9dc7854c51c554cade7a826cf6b7a4f
elpaso <elpaso@itopen.it>
2015-12-04 16:17:30 +0100

    Added test for QgsMapLayerRegistry
    See: PR #2534

---
+++ tests/src/python/test_qgsmaplayerregistry.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""QGIS Unit tests for QgsServer.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+__author__ = 'Alessandro Pasotti'
+__date__ = '04/12/2015'
+__copyright__ = 'Copyright 2015, The QGIS Project'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import unittest
+from qgis.core import QgsMapLayerRegistry
+
+
+class TestQgsMapLayerRegistry(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_removeMapLayer(self):
+        reg = QgsMapLayerRegistry.instance()
+        # Should not segfault
+        reg.removeMapLayers(['not_exists'])
+
+
+if __name__ == '__main__':
+    unittest.main()
