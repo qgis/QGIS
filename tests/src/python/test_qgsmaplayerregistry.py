8b775b034760a634679a36b756a0683bb20e8c54
elpaso <elpaso@itopen.it>
2015-12-04 16:21:03 +0100

    Removed unused imports

--- tests/src/python/test_qgsmaplayerregistry.py
+++ tests/src/python/test_qgsmaplayerregistry.py
@@ -12,12 +12,8 @@ __copyright__ = 'Copyright 2015, The QGIS Project'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
-import os
-import re
 import unittest
-import urllib
-from qgis.core import QgsMapLayerRegistry, QgsVectorLayer
-from utilities import unitTestDataPath
+from qgis.core import QgsMapLayerRegistry
 
 
 class TestQgsMapLayerRegistry(unittest.TestCase):
