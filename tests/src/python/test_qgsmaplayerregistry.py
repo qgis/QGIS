de3ce1560cf64be490b891c127d747a1f7e1d815
Alessandro Pasotti <elpaso@itopen.it>
2015-12-05 09:43:20 +0100

    Changed comment title

--- tests/src/python/test_qgsmaplayerregistry.py
+++ tests/src/python/test_qgsmaplayerregistry.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""QGIS Unit tests for QgsServer.
+"""QGIS Unit tests for QgsMapLayerRegistry.
 
 .. note:: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
