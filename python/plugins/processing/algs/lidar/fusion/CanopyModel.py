584771e90737a01a37672f69f21a12c95cd3b471
Alexander Bruy <alexander.bruy@gmail.com>
2015-12-07 10:12:21 +0200

    fix missed import in the Processing alg

--- python/plugins/processing/algs/lidar/fusion/CanopyModel.py
+++ python/plugins/processing/algs/lidar/fusion/CanopyModel.py
@@ -32,6 +32,7 @@ from processing.core.parameters import ParameterFile
 from processing.core.parameters import ParameterNumber
 from processing.core.parameters import ParameterSelection
 from processing.core.parameters import ParameterString
+from processing.core.parameters import ParameterBoolean
 from processing.core.outputs import OutputFile
 from FusionAlgorithm import FusionAlgorithm
 from FusionUtils import FusionUtils
