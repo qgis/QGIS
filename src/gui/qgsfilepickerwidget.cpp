qgis/QGIS

commit 29752a35974f22bc26d0015fe8d099da1650044f

QgsFilePicker and QgsExternalResource: new file picker widget and editor widget for improved file name widget
* QgsFilePicker is intended to be used across whole QGIS application for file picking
  * right now it supports only single file/folder selection
  * it has advanced support of relative path

* QgsExternalRessourceWidget is the pending editor widget which integrates a photo/web viewer
  * it can be properly accessible from the API and custom widgets
  * new class to avoid duplicating code for legacy support
  * old widgets will be removed from QGIS 3 (users will be invited to use the new widget in the config dialog)

src/gui/qgsfilepickerwidget.cpp
@@ -1,0 +1,265 @@
+/***************************************************************************
+  qgsfilepickerwidget.cpp
+
+ ---------------------
+ begin                : 17.12.2015
+ copyright            : (C) 2015 by Denis Rouzaud
+ email                : denis.rouzaud@gmail.com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsfilepickerwidget.h"
+
+#include <QLineEdit>
+#include <QToolButton>
+#include <QLabel>
+#include <QFileDialog>
+#include <QSettings>
+#include <QGridLayout>
+#include <QUrl>
+
+#include "qgsfilterlineedit.h"
+#include "qgslogger.h"
+#include "qgsproject.h"
+
+QgsFilePickerWidget::QgsFilePickerWidget( QWidget *parent )
+    : QWidget( parent )
+    , mFilePath( QString() )
+    , mButtonVisible( true )
+    , mUseLink( false )
+    , mFullUrl( false )
+    , mDefaultRoot( QString() )
+    , mStorageMode( File )
+{
+  setBackgroundRole( QPalette::Window );
+  setAutoFillBackground( true );
+
+  QGridLayout* layout = new QGridLayout();
+  layout->setMargin( 0 );
+
+  // If displaying a hyperlink, use a QLabel
+  mLinkLabel = new QLabel( this );
+  // Make Qt opens the link with the OS defined viewer
+  mLinkLabel->setOpenExternalLinks( true );
+  // Label should always be enabled to be able to open
+  // the link on read only mode.
+  mLinkLabel->setEnabled( true );
+  mLinkLabel->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Preferred );
+  mLinkLabel->setTextFormat( Qt::RichText );
+  layout->addWidget( mLinkLabel, 0, 0 );
+  mLinkLabel->hide(); // do not show by default
+
+  // otherwise, use the traditional QLineEdit
+  mLineEdit = new QgsFilterLineEdit( this );
+  connect( mLineEdit, SIGNAL( textEdited( QString ) ), this, SLOT( textEdited( QString ) ) );
+  layout->addWidget( mLineEdit, 1, 0 );
+
+  mFilePickerButton = new QToolButton( this );
+  mFilePickerButton->setText( "..." );
+  connect( mFilePickerButton, SIGNAL( clicked() ), this, SLOT( openFileDialog() ) );
+  layout->addWidget( mFilePickerButton, 0, 1, 2, 1 );
+
+  setLayout( layout );
+}
+
+QString QgsFilePickerWidget::filePath()
+{
+  return mFilePath;
+}
+
+void QgsFilePickerWidget::setFilePath( QString path )
+{
+  mFilePath = path;
+  mLineEdit->setText( path );
+  mLinkLabel->setText( toUrl( path ) );
+  emit fileChanged( mFilePath );
+}
+
+void QgsFilePickerWidget::setReadOnly( bool readOnly )
+{
+  mFilePickerButton->setEnabled( !readOnly );
+  mLineEdit->setEnabled( !readOnly );
+}
+
+bool QgsFilePickerWidget::filePickerButtonVisible() const
+{
+  return mButtonVisible;
+}
+
+void QgsFilePickerWidget::setFilePickerButtonVisible( bool visible )
+{
+  mButtonVisible = visible;
+  mFilePickerButton->setVisible( visible );
+}
+
+void QgsFilePickerWidget::textEdited( QString path )
+{
+  mFilePath = path;
+  mLinkLabel->setText( toUrl( path ) );
+  emit fileChanged( mFilePath );
+}
+
+bool QgsFilePickerWidget::useLink() const
+{
+  return mUseLink;
+}
+
+void QgsFilePickerWidget::setUseLink( bool useLink )
+{
+  mUseLink = useLink;
+  mLinkLabel->setVisible( mUseLink );
+  mLineEdit->setVisible( !mUseLink );
+}
+
+bool QgsFilePickerWidget::fullUrl() const
+{
+  return mFullUrl;
+}
+
+void QgsFilePickerWidget::setFullUrl( bool fullUrl )
+{
+  mFullUrl = fullUrl;
+}
+
+QString QgsFilePickerWidget::defaultRoot() const
+{
+  return mDefaultRoot;
+}
+
+void QgsFilePickerWidget::setDefaultRoot( QString defaultRoot )
+{
+  mDefaultRoot = defaultRoot;
+}
+
+QgsFilePickerWidget::StorageMode QgsFilePickerWidget::storageMode() const
+{
+  return mStorageMode;
+}
+
+void QgsFilePickerWidget::setStorageMode( QgsFilePickerWidget::StorageMode storageMode )
+{
+  mStorageMode = storageMode;
+}
+
+QgsFilePickerWidget::RelativeStorage QgsFilePickerWidget::relativeStorage()
+{
+  return mRelativeStorage;
+}
+
+void QgsFilePickerWidget::setRelativeStorage( QgsFilePickerWidget::RelativeStorage relativeStorage )
+{
+  mRelativeStorage = relativeStorage;
+}
+
+void QgsFilePickerWidget::openFileDialog()
+{
+  QSettings settings;
+  QString oldPath;
+
+  // If we use fixed default path
+  if ( !mDefaultRoot.isEmpty() )
+  {
+    oldPath = QDir::cleanPath( mDefaultRoot );
+  }
+  // if we use a relative path option, we need to obtain the full path
+  else if ( !mFilePath.isEmpty() )
+  {
+    oldPath = relativePath( mFilePath, false );
+  }
+
+  // If there is no valid value, find a default path to use
+  QUrl theUrl = QUrl::fromUserInput( oldPath );
+  if ( !theUrl.isValid() )
+  {
+    oldPath = settings.value( "/UI/lastExternalResourceWidgetDir", QDir::cleanPath( QgsProject::instance()->fileInfo().absolutePath() ) ).toString();
+  }
+
+  // Handle Storage
+  QString fileName;
+  if ( mStorageMode == File )
+  {
+    fileName = QFileDialog::getOpenFileName( this, tr( "Select a file" ), QFileInfo( oldPath ).absoluteFilePath() );
+  }
+  else if ( mStorageMode == Directory )
+  {
+    fileName = QFileDialog::getExistingDirectory( this, tr( "Select a directory" ), QFileInfo( oldPath ).absoluteFilePath(),  QFileDialog::ShowDirsOnly );
+  }
+
+  if ( fileName.isEmpty() )
+    return;
+
+
+  fileName = QDir::toNativeSeparators( QDir::cleanPath( QFileInfo( fileName ).absoluteFilePath() ) );
+  // Store the last used path:
+
+  if ( mStorageMode == File )
+    settings.setValue( "/UI/lastFileNameWidgetDir", QFileInfo( fileName ).absolutePath() );
+  else if ( mStorageMode == Directory )
+    settings.setValue( "/UI/lastFileNameWidgetDir", fileName );
+
+  // Handle relative Path storage
+  fileName = relativePath( fileName, true );
+
+  // Keep the new value
+  setFilePath( fileName );
+}
+
+
+QString QgsFilePickerWidget::relativePath( QString filePath, bool removeRelative )
+{
+  QString RelativePath;
+  if ( mRelativeStorage == RelativeProject )
+  {
+    RelativePath = QDir::toNativeSeparators( QDir::cleanPath( QgsProject::instance()->fileInfo().absolutePath() ) );
+  }
+  else if ( mRelativeStorage == RelativeDefaultPath && !mDefaultRoot.isEmpty() )
+  {
+    RelativePath = QDir::toNativeSeparators( QDir::cleanPath( mDefaultRoot ) );
+  }
+  if ( !RelativePath.isEmpty() )
+  {
+    if ( removeRelative )
+      return QDir::cleanPath( QDir( RelativePath ).relativeFilePath( filePath ) );
+    else
+      return QDir::cleanPath( QDir( RelativePath ).filePath( filePath ) );
+  }
+
+  return filePath;
+}
+
+
+QString QgsFilePickerWidget::toUrl( const QString& value )
+{
+  QString rep;
+  if ( value.isEmpty() )
+  {
+    rep =  QSettings().value( "qgis/nullValue", "NULL" ).toString();
+  }
+
+  QString urlStr = relativePath( value, false );
+  QUrl theUrl = QUrl::fromUserInput( urlStr );
+  if ( !theUrl.isValid() or !theUrl.isLocalFile() )
+  {
+    QgsDebugMsg( QString( "URL: %1 is not valid or not a local file !" ).arg( value ) );
+    rep =  value;
+  }
+
+  QString filePath = theUrl.toString();
+  if ( mFullUrl )
+  {
+    rep = QString( "<a href=\"%1\">%2</a>" ).arg( filePath, urlStr );
+  }
+  else
+  {
+    QString fileName = QFileInfo( urlStr ).fileName();
+    rep = QString( "<a href=\"%1\">%2</a>" ).arg( filePath, fileName );
+  }
+
+  return rep;
+}

src/gui/CMakeLists.txt
@@ -103,7 +103,9 @@ SET(QGIS_GUI_SRCS
   editorwidgets/qgsdummyconfigdlg.cpp
   editorwidgets/qgsenumerationwidgetwrapper.cpp
   editorwidgets/qgsenumerationwidgetfactory.cpp
-  editorwidgets/qgsfilenameconfigdlg.cpp
+  editorwidgets/qgsexternalresourceconfigdlg.cpp
+  editorwidgets/qgsexternalresourcewidgetwrapper.cpp
+  editorwidgets/qgsexternalresourcewidgetfactory.cpp
   editorwidgets/qgsfilenamewidgetwrapper.cpp
   editorwidgets/qgsfilenamewidgetfactory.cpp
   editorwidgets/qgshiddenwidgetwrapper.cpp
@@ -190,6 +192,7 @@ SET(QGIS_GUI_SRCS
   qgsexpressionhighlighter.cpp
   qgsexpressionselectiondialog.cpp
   qgsextentgroupbox.cpp
+  qgsexternalresourcewidget.cpp
   qgsfeatureselectiondlg.cpp
   qgsfieldcombobox.cpp
   qgsfieldexpressionwidget.cpp
@@ -197,6 +200,7 @@ SET(QGIS_GUI_SRCS
   qgsfieldvalidator.cpp
   qgsfieldproxymodel.cpp
   qgsfiledropedit.cpp
+  qgsfilepickerwidget.cpp
   qgsfilterlineedit.cpp
   qgsformannotationitem.cpp
   qgsgenericprojectionselector.cpp
@@ -326,6 +330,7 @@ SET(QGIS_GUI_MOC_HDRS
   qgsexpressionhighlighter.h
   qgsexpressionselectiondialog.h
   qgsextentgroupbox.h
+  qgsexternalresourcewidget.h
   qgsfeatureselectiondlg.h
   qgsfieldcombobox.h
   qgsfieldexpressionwidget.h
@@ -333,6 +338,7 @@ SET(QGIS_GUI_MOC_HDRS
   qgsfieldproxymodel.h
   qgsfieldvalidator.h
   qgsfiledropedit.h
+  qgsfilepickerwidget.h
   qgsfilterlineedit.h
   qgsformannotationitem.h
   qgsgenericprojectionselector.h
@@ -496,7 +502,8 @@ SET(QGIS_GUI_MOC_HDRS
   editorwidgets/qgsdoublespinbox.h
   editorwidgets/qgsdummyconfigdlg.h
   editorwidgets/qgsenumerationwidgetwrapper.h
-  editorwidgets/qgsfilenameconfigdlg.h
+  editorwidgets/qgsexternalresourceconfigdlg.h
+  editorwidgets/qgsexternalresourcewidgetwrapper.h
   editorwidgets/qgsfilenamewidgetwrapper.h
   editorwidgets/qgshiddenwidgetwrapper.h
   editorwidgets/qgsphotoconfigdlg.h
@@ -599,6 +606,7 @@ SET(QGIS_GUI_HDRS
   editorwidgets/qgscolorwidgetfactory.h
   editorwidgets/qgsdatetimeeditfactory.h
   editorwidgets/qgsenumerationwidgetfactory.h
+  editorwidgets/qgsexternalresourcewidgetfactory.h
   editorwidgets/qgsfilenamewidgetfactory.h
   editorwidgets/qgshiddenwidgetfactory.h
   editorwidgets/qgsphotowidgetfactory.h

src/gui/editorwidgets/core/qgseditorwidgetregistry.cpp
@@ -24,23 +24,25 @@
 
 // Editors
 #include "qgsclassificationwidgetwrapperfactory.h"
-#include "qgsrangewidgetfactory.h"
-#include "qgsuniquevaluewidgetfactory.h"
-#include "qgsfilenamewidgetfactory.h"
-#include "qgsvaluemapwidgetfactory.h"
-#include "qgsenumerationwidgetfactory.h"
-#include "qgshiddenwidgetfactory.h"
 #include "qgscheckboxwidgetfactory.h"
-#include "qgstexteditwidgetfactory.h"
-#include "qgsvaluerelationwidgetfactory.h"
-#include "qgsuuidwidgetfactory.h"
+#include "qgscolorwidgetfactory.h"
+#include "qgsdatetimeeditfactory.h"
+#include "qgsenumerationwidgetfactory.h"
+#include "qgsexternalresourcewidgetfactory.h"
+#include "qgsfilenamewidgetfactory.h"
+#include "qgshiddenwidgetfactory.h"
 #include "qgsphotowidgetfactory.h"
+#include "qgsrangewidgetfactory.h"
+#include "qgsrelationreferencefactory.h"
+#include "qgstexteditwidgetfactory.h"
+#include "qgsuniquevaluewidgetfactory.h"
+#include "qgsuuidwidgetfactory.h"
+#include "qgsvaluemapwidgetfactory.h"
+#include "qgsvaluerelationwidgetfactory.h"
 #ifdef WITH_QTWEBKIT
 #include "qgswebviewwidgetfactory.h"
 #endif
-#include "qgscolorwidgetfactory.h"
-#include "qgsrelationreferencefactory.h"
-#include "qgsdatetimeeditfactory.h"
+
 
 QgsEditorWidgetRegistry* QgsEditorWidgetRegistry::instance()
 {
@@ -69,6 +71,7 @@ void QgsEditorWidgetRegistry::initEditors( QgsMapCanvas *mapCanvas, QgsMessageBa
   reg->registerWidget( "Color", new QgsColorWidgetFactory( tr( "Color" ) ) );
   reg->registerWidget( "RelationReference", new QgsRelationReferenceFactory( tr( "Relation Reference" ), mapCanvas, messageBar ) );
   reg->registerWidget( "DateTime", new QgsDateTimeEditFactory( tr( "Date/Time" ) ) );
+  reg->registerWidget( "ExternalResource", new QgsExternalResourceWidgetFactory( tr( "External Resource" ) ) );
 }
 
 QgsEditorWidgetRegistry::QgsEditorWidgetRegistry()

src/gui/editorwidgets/qgsexternalresourceconfigdlg.cpp
@@ -1,0 +1,211 @@
+/***************************************************************************
+    qgsexternalresourceconfigdlg.cpp
+     --------------------------------------
+    Date                 : 2015-11-26
+    Copyright            : (C) 2015 Médéric Ribreux
+    Email                : mederic.ribreux at medspx dot fr
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsexternalresourceconfigdlg.h"
+#include "qgsexternalresourcewidget.h"
+#include "qgsproject.h"
+
+#include <QFileDialog>
+#include <QSettings>
+
+class QgsExternalResourceWidgetWrapper;
+
+QgsExternalResourceConfigDlg::QgsExternalResourceConfigDlg( QgsVectorLayer* vl, int fieldIdx, QWidget* parent )
+    : QgsEditorConfigWidget( vl, fieldIdx, parent )
+{
+  setupUi( this );
+
+  // By default, uncheck some options
+  mUseLink->setChecked( false );
+  mFullUrl->setChecked( false );
+  mDocumentViewerGroupBox->setChecked( false );
+  mRootPath->setPlaceholderText( QSettings().value( "/UI/lastExternalResourceWidgetDir", QDir::toNativeSeparators( QDir::cleanPath( QgsProject::instance()->fileInfo().absolutePath() ) ) ).toString() );
+
+  // Add connection to button for choosing default path
+  connect( mRootPathButton, SIGNAL( clicked() ), this, SLOT( chooseDefaultPath() ) );
+
+  // Activate Relative Default Path option only if Default Path is set
+  connect( mRootPath, SIGNAL( textChanged( const QString & ) ), this, SLOT( enableRelativeDefault() ) );
+
+  // Dynamic GroupBox for relative paths option
+  connect( mRelativeGroupBox, SIGNAL( toggled( bool ) ), this, SLOT( enableRelative( bool ) ) );
+
+  // set ids for StorageTypeButtons
+  mStorageButtonGroup->setId( mStoreFilesButton, QgsFilePickerWidget::File );
+  mStorageButtonGroup->setId( mStoreDirsButton, QgsFilePickerWidget::Directory );
+  mStoreFilesButton->setChecked( true );
+
+  // set ids for RelativeButtons
+  mRelativeButtonGroup->setId( mRelativeProject, QgsFilePickerWidget::RelativeProject );
+  mRelativeButtonGroup->setId( mRelativeDefault, QgsFilePickerWidget::RelativeDefaultPath );
+  mRelativeProject->setChecked( true );
+
+  mDocumentViewerContentComboBox->addItem( tr( "Image" ), QgsExternalResourceWidget::Image );
+  mDocumentViewerContentComboBox->addItem( tr( "Web view" ), QgsExternalResourceWidget::Web );
+}
+
+void QgsExternalResourceConfigDlg::chooseDefaultPath()
+{
+  QString dir;
+  if ( !mRootPath->text().isEmpty() )
+    dir = mRootPath->text();
+  else
+    dir = QSettings().value( "/UI/lastExternalResourceWidgetDir", QDir::toNativeSeparators( QDir::cleanPath( QgsProject::instance()->fileInfo().absolutePath() ) ) ).toString();
+
+  QString rootName = QFileDialog::getExistingDirectory( this, tr( "Select a directory" ), dir, QFileDialog::ShowDirsOnly );
+
+  if ( rootName.isNull() )
+    return;
+
+  mRootPath->setText( rootName );
+}
+
+void QgsExternalResourceConfigDlg::enableRelativeDefault()
+{
+  // Activate (or not) the RelativeDefault button if default path
+  if ( mRelativeGroupBox->isChecked() )
+    mRelativeDefault->setEnabled( !mRootPath->text().isEmpty() );
+
+  // If no default path, RelativeProj button enabled by default
+  if ( mRootPath->text().isEmpty() )
+    mRelativeProject->toggle();
+}
+
+void QgsExternalResourceConfigDlg::enableRelative( bool state )
+{
+  if ( state )
+  {
+    mRelativeProject->setEnabled( true );
+    if ( mRootPath->text().isEmpty() )
+      mRelativeDefault->setEnabled( false );
+    else
+      mRelativeDefault->setEnabled( true );
+  }
+  else
+  {
+    mRelativeProject->setEnabled( false );
+    mRelativeDefault->setEnabled( false );
+  }
+}
+
+
+QgsEditorWidgetConfig QgsExternalResourceConfigDlg::config()
+{
+  QgsEditorWidgetConfig cfg;
+
+  cfg.insert( "FilePicker", mFilePickerGroupBox->isChecked() );
+  cfg.insert( "FilePickerButton", mFilePickerButton->isChecked() );
+
+  if ( mUseLink->isChecked() )
+  {
+    cfg.insert( "UseLink", mUseLink->isChecked() );
+    if ( mFullUrl->isChecked() )
+      cfg.insert( "FullUrl", mFullUrl->isChecked() );
+  }
+
+  if ( !mRootPath->text().isEmpty() )
+  {
+    cfg.insert( "DefaultRoot", mRootPath->text() );
+  }
+
+  // Save Storage Mode
+  cfg.insert( "StorageMode", mStorageButtonGroup->checkedId() );
+
+  // Save Relative Paths option
+  if ( mRelativeGroupBox->isChecked() )
+  {
+    cfg.insert( "RelativeStorage", mRelativeButtonGroup->checkedId() );
+  }
+  else
+  {
+    cfg.insert( "RelativeStorage", ( int )QgsFilePickerWidget::Absolute );
+  }
+
+  if ( mDocumentViewerGroupBox->isChecked() )
+  {
+    cfg.insert( "DocumentViewer", mDocumentViewerContentComboBox->itemData( mDocumentViewerContentComboBox->currentIndex() ).toInt() );
+    cfg.insert( "DocumentViewerHeight", mDocumentViewerHeight->value() );
+    cfg.insert( "DocumentViewerWidth", mDocumentViewerWidth->value() );
+  }
+  else
+  {
+    cfg.insert( "DocumentViewer", ( int )QgsExternalResourceWidget::NoContent );
+  }
+
+  return cfg;
+}
+
+
+void QgsExternalResourceConfigDlg::setConfig( const QgsEditorWidgetConfig& config )
+{
+  if ( config.contains( "FilePicker" ) )
+  {
+    mFilePickerGroupBox->setChecked( config.value( "FilePicker" ).toBool() );
+  }
+  if ( config.contains( "FilePicker" ) )
+  {
+    mFilePickerButton->setChecked( config.value( "FilePickerButton" ).toBool() );
+  }
+
+  if ( config.contains( "UseLink" ) )
+  {
+    mUseLink->setChecked( config.value( "UseLink" ).toBool() );
+    if ( config.contains( "FullUrl" ) )
+      mFullUrl->setChecked( true );
+  }
+
+  if ( config.contains( "DefaultRoot" ) )
+  {
+    mRootPath->setText( config.value( "DefaultRoot" ).toString() );
+  }
+
+  // relative storage
+  if ( config.contains( "RelativeStorage" ) )
+  {
+    int relative = config.value( "RelativeStorage" ).toInt();
+    if (( QgsFilePickerWidget::RelativeStorage )relative == QgsFilePickerWidget::Absolute )
+    {
+      mRelativeGroupBox->setChecked( false );
+    }
+    else
+    {
+      mRelativeGroupBox->setChecked( true );
+      mRelativeButtonGroup->button( relative )->setChecked( true );
+    }
+  }
+
+  // set storage mode
+  if ( config.contains( "StorageMode" ) )
+  {
+    int mode = config.value( "StorageMode" ).toInt();
+    mStorageButtonGroup->button( mode )->setChecked( true );
+  }
+
+  // Document viewer
+  if ( config.contains( "DocumentViewer" ) )
+  {
+    QgsExternalResourceWidget::DocumentViewerContent content = ( QgsExternalResourceWidget::DocumentViewerContent )config.value( "DocumentViewer" ).toInt();
+    mDocumentViewerGroupBox->setChecked( content != QgsExternalResourceWidget::NoContent );
+    mDocumentViewerContentComboBox->setCurrentIndex( mDocumentViewerContentComboBox->findData( content ) );
+    if ( config.contains( "DocumentViewerHeight" ) )
+    {
+      mDocumentViewerHeight->setValue( config.value( "DocumentViewerHeight" ).toInt() );
+    }
+    if ( config.contains( "DocumentViewerWidth" ) )
+    {
+      mDocumentViewerWidth->setValue( config.value( "DocumentViewerWidth" ).toInt() );
+    }
+  }
+}

src/gui/editorwidgets/qgsexternalresourceconfigdlg.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-    qgsfilenameconfigdlg.h
+    qgsexternalresourceconfigdlg.h
      --------------------------------------
     Date                 : 2015-11-26
     Copyright            : (C) 2015 Médéric Ribreux
@@ -13,24 +13,23 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifndef QGSFILENAMECONFIGDLG_H
-#define QGSFILENAMECONFIGDLG_H
+#ifndef QGSEXTERNALRESOURCECONFIGDLG_H
+#define QGSEXTERNALRESOURCECONFIGDLG_H
 
-#include "ui_qgsfilenameconfigdlg.h"
+#include "ui_qgsexternalresourceconfigdlg.h"
 
 #include "qgseditorconfigwidget.h"
-//#include "qgsfilenamewidgetwrapper.h"
 
-/** \class QgsFileNameConfigDlg
+/** \class QgsExternalResourceConfigDlg
  * \note not available in Python bindings
  */
 
-class GUI_EXPORT QgsFileNameConfigDlg : public QgsEditorConfigWidget, private Ui::QgsFileNameConfigDlg
+class GUI_EXPORT QgsExternalResourceConfigDlg : public QgsEditorConfigWidget, private Ui::QgsExternalResourceConfigDlg
 {
     Q_OBJECT
 
   public:
-    explicit QgsFileNameConfigDlg( QgsVectorLayer* vl, int fieldIdx, QWidget *parent = 0 );
+    explicit QgsExternalResourceConfigDlg( QgsVectorLayer* vl, int fieldIdx, QWidget *parent = 0 );
 
     // QgsEditorConfigWidget interface
   public:
@@ -38,9 +37,14 @@ class GUI_EXPORT QgsFileNameConfigDlg : public QgsEditorConfigWidget, private Ui
     void setConfig( const QgsEditorWidgetConfig& config ) override;
 
   private slots:
+    //! Choose a base directory for rootPath
     void chooseDefaultPath();
+
+    //! Modify RelativeDefault according to mRootPath content
     void enableRelativeDefault();
+
+    //! Dynamic activation of RelativeGroupBox
     void enableRelative( bool state );
 };
 
-#endif // QGSFILENAMECONFIGDLG_H
+#endif // QGSEXTERNALRESOURCECONFIGDLG_H

src/gui/editorwidgets/qgsexternalresourcewidgetfactory.cpp
@@ -1,0 +1,108 @@
+/***************************************************************************
+   qgsexternalresourcewidgetfactory.cpp
+
+ ---------------------
+ begin                : 16.12.2015
+ copyright            : (C) 2015 by Denis Rouzaud
+ email                : denis.rouzaud@gmail.com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsexternalresourcewidgetfactory.h"
+
+QgsExternalResourceWidgetFactory::QgsExternalResourceWidgetFactory( const QString& name )
+    : QgsEditorWidgetFactory( name )
+{
+}
+
+QgsEditorWidgetWrapper* QgsExternalResourceWidgetFactory::create( QgsVectorLayer* vl, int fieldIdx, QWidget* editor, QWidget* parent ) const
+{
+  return new QgsExternalResourceWidgetWrapper( vl, fieldIdx, editor, parent );
+}
+
+QgsEditorConfigWidget* QgsExternalResourceWidgetFactory::configWidget( QgsVectorLayer* vl, int fieldIdx, QWidget* parent ) const
+{
+  return new QgsExternalResourceConfigDlg( vl, fieldIdx, parent );
+}
+
+void QgsExternalResourceWidgetFactory::writeConfig( const QgsEditorWidgetConfig& config, QDomElement& configElement, QDomDocument& doc, const QgsVectorLayer* layer, int fieldIdx )
+{
+  Q_UNUSED( doc )
+  Q_UNUSED( layer )
+  Q_UNUSED( fieldIdx )
+
+  configElement.setAttribute( "FilePicker", config.value( "FilePicker", true ).toBool() );
+  configElement.setAttribute( "FilePickerButton", config.value( "FilePickerButton", true ).toBool() );
+
+
+  // Non mandatory options are not saved into project file (to save some space).
+  if ( config.contains( "UseLink" ) )
+    configElement.setAttribute( "UseLink", config.value( "UseLink" ).toBool() );
+
+  if ( config.contains( "FullUrl" ) )
+    configElement.setAttribute( "FullUrl", config.value( "FullUrl" ).toBool() );
+
+  if ( config.contains( "DefaultRoot" ) )
+    configElement.setAttribute( "DefaultRoot", config.value( "DefaultRoot" ).toString() );
+
+  if ( config.contains( "RelativeStorage" ) )
+    configElement.setAttribute( "RelativeStorage" , config.value( "RelativeStorage" ).toString() );
+
+  if ( config.contains( "DocumentViewer" ) )
+    configElement.setAttribute( "DocumentViewer", config.value( "DocumentViewer" ).toInt() );
+
+
+  configElement.setAttribute( "StorageMode", config.value( "StorageMode" ).toString() );
+}
+
+QgsEditorWidgetConfig QgsExternalResourceWidgetFactory::readConfig( const QDomElement& configElement, QgsVectorLayer* layer, int fieldIdx )
+{
+  Q_UNUSED( layer )
+  Q_UNUSED( fieldIdx )
+
+  QgsEditorWidgetConfig cfg;
+
+  if ( configElement.hasAttribute( "FilePickerButton" ) )
+    cfg.insert( "FilePickerButton", configElement.attribute( "FilePickerButton" ) == "1" );
+
+  if ( configElement.hasAttribute( "FilePicker" ) )
+    cfg.insert( "FilePicker", configElement.attribute( "FilePicker" ) == "1" );
+
+  if ( configElement.hasAttribute( "UseLink" ) )
+    cfg.insert( "UseLink", configElement.attribute( "UseLink" ) == "1" );
+
+  if ( configElement.hasAttribute( "FullUrl" ) )
+    cfg.insert( "FullUrl", configElement.attribute( "FullUrl" ) == "1" );
+
+  if ( configElement.hasAttribute( "DefaultRoot" ) )
+    cfg.insert( "DefaultRoot", configElement.attribute( "DefaultRoot" ) );
+
+  if ( configElement.hasAttribute( "RelativeStorage" ) )
+  {
+    if (( configElement.attribute( "RelativeStorage" ) == "Default" && configElement.hasAttribute( "DefaultRoot" ) ) ||
+        configElement.attribute( "RelativeStorage" ) == "Project" )
+      cfg.insert( "RelativeStorage" , configElement.attribute( "RelativeStorage" ) );
+  }
+
+  if ( configElement.hasAttribute( "DocumentViewer" ) )
+    cfg.insert( "DocumentViewer", configElement.attribute( "DocumentViewer" ) );
+
+
+  cfg.insert( "StorageMode", configElement.attribute( "StorageMode", "Files" ) );
+
+  return cfg;
+}
+
+bool QgsExternalResourceWidgetFactory::isFieldSupported( QgsVectorLayer* vl, int fieldIdx )
+{
+  if ( vl->fields().at( fieldIdx ).type() == QVariant::String )
+    return true;
+
+  return false;
+}

src/gui/editorwidgets/qgsexternalresourcewidgetfactory.h
@@ -1,9 +1,10 @@
 /***************************************************************************
-    qgsfilenamewidgetfactory.h
-     --------------------------------------
-    Date                 : 5.1.2014
-    Copyright            : (C) 2014 Matthias Kuhn
-    Email                : matthias at opengis dot ch
+   qgsexternalresourcewidgetfactory.h
+
+ ---------------------
+ begin                : 16.12.2015
+ copyright            : (C) 2015 by Denis Rouzaud
+ email                : denis.rouzaud@gmail.com
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -13,24 +14,35 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifndef QGSFILENAMEWIDGETFACTORY_H
-#define QGSFILENAMEWIDGETFACTORY_H
+#ifndef QGSEXTERNALRESOURCEWIDGETFACTORY_H
+#define QGSEXTERNALRESOURCEWIDGETFACTORY_H
 
 #include "qgseditorwidgetfactory.h"
+#include "qgsexternalresourceconfigdlg.h"
+#include "qgsexternalresourcewidgetwrapper.h"
 
-/** \class QgsFileNameWidgetFactory
+
+/** \class QgsExternalResourceWidgetFactory
  * \note not available in Python bindings
  */
 
-class GUI_EXPORT QgsFileNameWidgetFactory : public QgsEditorWidgetFactory
+class GUI_EXPORT QgsExternalResourceWidgetFactory : public QgsEditorWidgetFactory
 {
   public:
-    explicit QgsFileNameWidgetFactory( const QString& name );
+    QgsExternalResourceWidgetFactory( const QString& name );
 
     // QgsEditorWidgetFactory interface
   public:
     QgsEditorWidgetWrapper* create( QgsVectorLayer* vl, int fieldIdx, QWidget* editor, QWidget* parent ) const override;
     QgsEditorConfigWidget* configWidget( QgsVectorLayer* vl, int fieldIdx, QWidget* parent ) const override;
+
+    // QgsEditorWidgetFactory interface
+  public:
+    void writeConfig( const QgsEditorWidgetConfig& config, QDomElement& configElement, QDomDocument& doc, const QgsVectorLayer* layer, int fieldIdx ) override;
+
+  private:
+    QgsEditorWidgetConfig readConfig( const QDomElement& configElement, QgsVectorLayer* layer, int fieldIdx ) override;
+    bool isFieldSupported( QgsVectorLayer* vl, int fieldIdx ) override;
 };
 
-#endif // QGSFILENAMEWIDGETFACTORY_H
+#endif // QGSEXTERNALRESOURCEWIDGETFACTORY_H

commit bb6c24a223e20efab7cd690a4937114459129efd

fix syntax error on Windows Changed 'or' to '||', introduced in 29752a35974f22bc26d0015fe8d099da1650044f

src/gui/qgsfilepickerwidget.cpp
@@ -280,7 +280,7 @@ QString QgsFilePickerWidget::toUrl( const QString& path ) const
 
   QString urlStr = relativePath( path, false );
   QUrl url = QUrl::fromUserInput( urlStr );
-  if ( !url.isValid() or !url.isLocalFile() )
+  if ( !url.isValid() || !url.isLocalFile() )
   {
     QgsDebugMsg( QString( "URL: %1 is not valid or not a local file !" ).arg( path ) );
     rep =  path;