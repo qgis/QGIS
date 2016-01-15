qgis/QGIS

commit ff3200fd6c507429b50c56b8307869d62cce6dcf

make vector joins work within layer-definition-files

src/core/qgslayerdefinition.cpp
@@ -6,6 +6,7 @@
 
 #include "qgslogger.h"
 #include "qgsmaplayer.h"
+#include "qgsvectorlayer.h"
 #include "qgslayertree.h"
 #include "qgsmaplayerregistry.h"
 #include "qgslayerdefinition.h"
@@ -62,6 +63,18 @@ bool QgsLayerDefinition::loadLayerDefinition( QDomDocument doc, QgsLayerTreeGrou
         layerNode.toElement().setAttribute( "id", newid );
       }
     }
+
+    // change layer IDs for vector joins
+    QDomNodeList vectorJoinNodes = doc.elementsByTagName( "join" ); // TODO: Find a better way of searching for vectorjoins, there might be other <join> elements within the project.
+    for ( int j = 0; j < vectorJoinNodes.size(); ++j )
+    {
+      QDomNode joinNode = vectorJoinNodes.at( j );
+      QDomElement joinElement = joinNode.toElement();
+      if ( joinElement.attribute( "joinLayerId" ) == oldid )
+      {
+        joinNode.toElement().setAttribute( "joinLayerId", newid );
+      }
+    }
   }
 
   QDomElement layerTreeElem = doc.documentElement().firstChildElement( "layer-tree-group" );
@@ -74,6 +87,17 @@ bool QgsLayerDefinition::loadLayerDefinition( QDomDocument doc, QgsLayerTreeGrou
 
   QList<QgsMapLayer*> layers = QgsMapLayer::fromLayerDefinition( doc );
   QgsMapLayerRegistry::instance()->addMapLayers( layers, loadInLegend );
+
+  // Now that all layers are loaded, refresh the vectorjoins to get the joined fields
+  Q_FOREACH ( QgsMapLayer* layer, layers )
+  {
+    QgsVectorLayer* vlayer = static_cast< QgsVectorLayer * >( layer );
+    if ( vlayer )
+    {
+      vlayer->createJoinCaches();
+      vlayer->updateFields();
+    }
+  }
 
   QList<QgsLayerTreeNode*> nodes = root->children();
   Q_FOREACH ( QgsLayerTreeNode *node, nodes )

commit 0ca359e4e53127d897096270e069e2c69ff9f256

make QLR files containing non-vector layers work again This was a regression introduced in ff3200fd6c507429b50c56b8307869d62cce6dcf. (fixes #14091)

src/core/qgslayerdefinition.cpp
@@ -131,7 +131,7 @@ bool QgsLayerDefinition::loadLayerDefinition( QDomDocument doc, QgsLayerTreeGrou
   // Now that all layers are loaded, refresh the vectorjoins to get the joined fields
   Q_FOREACH ( QgsMapLayer* layer, layers )
   {
-    QgsVectorLayer* vlayer = static_cast< QgsVectorLayer * >( layer );
+    QgsVectorLayer* vlayer = dynamic_cast< QgsVectorLayer * >( layer );
     if ( vlayer )
     {
       vlayer->createJoinCaches();

tests/src/core/testqgsvectorlayerjoinbuffer.cpp
@@ -384,7 +384,7 @@ void TestVectorLayerJoinBuffer::testJoinLayerDefinitionFile()
   QList<QgsMapLayer*> mapLayers = QgsMapLayerRegistry::instance()->mapLayersByName( "layerB" );
   QCOMPARE( mapLayers.count(), 1 );
 
-  QgsVectorLayer* vLayer = static_cast<QgsVectorLayer*>( mapLayers.value( 0 ) );
+  QgsVectorLayer* vLayer = dynamic_cast<QgsVectorLayer*>( mapLayers.value( 0 ) );
   QVERIFY( vLayer );
 
   // Check for vector join