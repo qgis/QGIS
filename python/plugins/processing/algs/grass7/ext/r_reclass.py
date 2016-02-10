43b39efd73cb907a6abd8e89a43a9a37c5fdd7ca
Médéric Ribreux <medspx@medspx.fr>
2016-02-10 21:01:51 +0100

    [Processing] Add GRASS7 r.reclass inline rules (implements Redmine #5583)

---
+++ python/plugins/processing/algs/grass7/ext/r_reclass.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    r_reclass.py
+    ------------
+    Date                 : February 2016
+    Copyright            : (C) 2016 by Médéric Ribreux
+    Email                : medspx at medspx dot fr
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Médéric Ribreux'
+__date__ = 'February 2016'
+__copyright__ = '(C) 2016, Médéric Ribreux'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+
+def checkParameterValuesBeforeExecuting(alg):
+    """ Verify if we have the right parameters """
+    if alg.getParameterValue(u'rules') and alg.getParameterValue(u'txtrules'):
+        return alg.tr("You need to set either a rules file or write directly the rules !")
+
+    return None
+
+
+def processCommand(alg):
+    """ Handle inline rules """
+    txtRules = alg.getParameterValue(u'txtrules')
+    if txtRules:
+        raster = alg.getParameterValue(u'input')
+        output = alg.getOutputFromName(u'output')
+        alg.exportedLayers[output.value] = output.name + alg.uniqueSufix
+        if raster:
+            raster = alg.exportedLayers[raster]
+        command = u"echo \"{}\" | r.reclass input={} rules=- output={} --overwrite".format(
+            txtRules, raster, output.name + alg.uniqueSufix)
+        alg.commands.append(command)
+    else:
+        alg.processCommand()
