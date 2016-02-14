6d39ad9ccc78e0363496950fdb9bc64b871ff676
Médéric RIBREUX <mederic.ribreux@medspx.homenet.org>
2016-02-14 20:45:40 +0100

    Add r.mask algorithm

---
+++ python/plugins/processing/algs/grass7/ext/r_mask_vect.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    r_mask_vect.py
+    --------------
+    Date                 : February 2016
+    Copyright            : (C) 2016 by Médéric Ribreux
+    Email                : medspx at medspx dot fr
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Médéric Ribreux'
+__date__ = 'February 2016'
+__copyright__ = '(C) 2016, Médéric Ribreux'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+import r_mask
+
+
+def processCommand(alg):
+    r_mask.processCommand(alg)
+
+
+def processOutputs(alg):
+    r_mask.processOutputs(alg)
