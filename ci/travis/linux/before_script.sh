c183248e08dd3a5014c24c3afbfa4ce01bc661f6
Sandro Santilli <strk@keybit.net>
2016-02-17 11:29:49 +0100

    Use pg initialization script from Travis

--- ci/travis/linux/before_script.sh
+++ ci/travis/linux/before_script.sh
@@ -1,5 +1,5 @@
 printf "[qgis_test]\nhost=localhost\ndbname=qgis_test\nuser=postgres" > ~/.pg_service.conf
-psql -c 'CREATE DATABASE qgis_test;' -U postgres
-psql -f $TRAVIS_BUILD_DIR/tests/testdata/provider/testdata_pg.sql -U postgres -d qgis_test
-psql -f $TRAVIS_BUILD_DIR/tests/testdata/provider/testdata_pg_reltests.sql -U postgres -d qgis_test
-psql -f $TRAVIS_BUILD_DIR/tests/testdata/provider/testdata_pg_vectorjoin.sql -U postgres -d qgis_test
+
+export PGUSER=postgres
+psql -c 'CREATE DATABASE qgis_test;' template1
+$TRAVIS_BUILD_DIR/tests/testdata/provider/testdata_pg.sh
