c37e18691ceeca6a4e5d4c76812eeaab5e7c4025
Sandro Santilli <strk@keybit.net>
2016-02-17 12:41:25 +0100

    Script takes also care about creating test db

--- ci/travis/linux/before_script.sh
+++ ci/travis/linux/before_script.sh
@@ -1,5 +1,4 @@
 printf "[qgis_test]\nhost=localhost\ndbname=qgis_test\nuser=postgres" > ~/.pg_service.conf
 
 export PGUSER=postgres
-psql -c 'CREATE DATABASE qgis_test;' template1
 $TRAVIS_BUILD_DIR/tests/testdata/provider/testdata_pg.sh
