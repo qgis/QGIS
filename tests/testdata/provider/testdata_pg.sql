4c0f10a2d84988944a899189f4850139ac9dc12e
Sandro Santilli <strk@keybit.net>
2016-02-17 16:28:20 +0100

    Remove lock_timeout setting, not supported by PostgreSQL 9.1
    See https://travis-ci.org/qgis/QGIS/jobs/109844438#L942

--- tests/testdata/provider/testdata_pg.sql
+++ tests/testdata/provider/testdata_pg.sql
@@ -7,7 +7,7 @@
 -- Started on 2015-05-21 09:37:33 CEST
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
+-- SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
