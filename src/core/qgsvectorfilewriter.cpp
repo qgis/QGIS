a87142a9b4a6c48d0fbbe6b780ad2d7ffb7acc62
aharfoot <aharfoot@users.noreply.github.com>
2016-02-19 14:22:57 +0000

    Missing ;

--- src/core/qgsvectorfilewriter.cpp
+++ src/core/qgsvectorfilewriter.cpp
@@ -284,7 +284,7 @@ void QgsVectorFileWriter::init( QString vectorFileName, QString fileEncoding, co
   OGRwkbGeometryType wkbType = static_cast<OGRwkbGeometryType>( geometryType );
 
   // Remove FEATURE_DATASET layer option (used for ESRI File GDB driver) if its value is not set
-  int optIndex = layOptions.indexOf("FEATURE_DATASET=")
+  int optIndex = layOptions.indexOf("FEATURE_DATASET=");
   if ( optIndex != -1 )
   {
     layOptions.removeAt(optIndex);
