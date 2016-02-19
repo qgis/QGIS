b5afcc69c90f98366054ec0913b189eabbd1e086
aharfoot <aharfoot@users.noreply.github.com>
2016-02-19 14:35:15 +0000

    Disable FEATURE_DATASET layer option if its value is not set

--- src/core/qgsvectorfilewriter.cpp
+++ src/core/qgsvectorfilewriter.cpp
@@ -283,6 +283,13 @@ void QgsVectorFileWriter::init( QString vectorFileName, QString fileEncoding, co
   QString layerName = QFileInfo( vectorFileName ).baseName();
   OGRwkbGeometryType wkbType = static_cast<OGRwkbGeometryType>( geometryType );
 
+  // Remove FEATURE_DATASET layer option (used for ESRI File GDB driver) if its value is not set
+  int optIndex = layOptions.indexOf("FEATURE_DATASET=");
+  if ( optIndex != -1 )
+  {
+    layOptions.removeAt(optIndex);
+  }
+  
   if ( !layerOptions.isEmpty() )
   {
     options = new char *[ layerOptions.size()+1 ];
