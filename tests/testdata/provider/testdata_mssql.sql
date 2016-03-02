527fe9d2344f8d93e2f9d430f5691a678b63c164
Nyall Dawson <nyall.dawson@gmail.com>
2016-03-03 10:31:59 +1100

    Fix mssql provider test data

--- tests/testdata/provider/testdata_mssql.sql
+++ tests/testdata/provider/testdata_mssql.sql
@@ -5,6 +5,7 @@ CREATE TABLE qgis_test.[someData] (
     cnt integer,
     name nvarchar(max) DEFAULT 'qgis',
     name2 nvarchar(max) DEFAULT 'qgis',
+    num_char nvarchar(max),
     geom geometry
 );
 
@@ -20,12 +21,12 @@ CREATE TABLE qgis_test.[date_times] (
        datetime_field datetime
 );
 
-INSERT INTO qgis_test.[someData] (pk, cnt, name, name2, geom) VALUES
-(5, -200, NULL, 'NuLl', geometry::STGeomFromText( 'Point(-71.123 78.23)', 4326 )),
-(3,  300, 'Pear', 'PEaR', NULL),
-(1,  100, 'Orange', 'oranGe', geometry::STGeomFromText( 'Point(-70.332 66.33)', 4326 )),
-(2,  200, 'Apple', 'Apple', geometry::STGeomFromText( 'Point(-68.2 70.8)', 4326 )),
-(4,  400, 'Honey', 'Honey', geometry::STGeomFromText( 'Point(-65.32 78.3)', 4326 ))
+INSERT INTO qgis_test.[someData] (pk, cnt, name, name2, num_char, geom) VALUES
+(5, -200, NULL, 'NuLl', '5', geometry::STGeomFromText( 'Point(-71.123 78.23)', 4326 )),
+(3,  300, 'Pear', 'PEaR', '3', NULL),
+(1,  100, 'Orange', 'oranGe', '1', geometry::STGeomFromText( 'Point(-70.332 66.33)', 4326 )),
+(2,  200, 'Apple', 'Apple', '2', geometry::STGeomFromText( 'Point(-68.2 70.8)', 4326 )),
+(4,  400, 'Honey', 'Honey', '4', geometry::STGeomFromText( 'Point(-65.32 78.3)', 4326 ))
 ;
 
 INSERT INTO qgis_test.[some_poly_data] (pk, geom) VALUES
