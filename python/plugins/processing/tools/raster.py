9c611c98e856e98051299fce9466eef3603dcf4d
radosuav <radoslaw.guzinski@esa.int>
2016-03-07 16:56:35 +0100

    [Processing] Fix TypeError in Zonal Statistics algorithm
    Fixes #14412

--- python/plugins/processing/tools/raster.py
+++ python/plugins/processing/tools/raster.py
@@ -64,8 +64,14 @@ def scanraster(layer, progress):
 
 
 def mapToPixel(mX, mY, geoTransform):
-    (pX, pY) = gdal.ApplyGeoTransform(
-        gdal.InvGeoTransform(geoTransform)[1], mX, mY)
+    try:
+        # GDAL 1.x
+        (pX, pY) = gdal.ApplyGeoTransform(
+            gdal.InvGeoTransform(geoTransform)[1], mX, mY)
+    except TypeError:
+        # GDAL 2.x
+        (pX, pY) = gdal.ApplyGeoTransform(
+            gdal.InvGeoTransform(geoTransform), mX, mY)
     return (int(pX), int(pY))
 
 
