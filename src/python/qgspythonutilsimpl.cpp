72764b29378875836dc8000e79d8424e919e6e12
Matthias Kuhn <matthias@opengis.ch>
2016-03-09 12:35:18 +0100

    Simplfiy some python3 handling

--- src/python/qgspythonutilsimpl.cpp
+++ src/python/qgspythonutilsimpl.cpp
@@ -127,7 +127,7 @@ bool QgsPythonUtilsImpl::checkSystemImports()
       return false;
     }
   }
-#ifdef PYTHON2
+#if (PY_VERSION_HEX < 0x03000000)
   // import Qt bindings
   if ( !runString( "from PyQt4 import QtCore, QtGui",
                    QObject::tr( "Couldn't load PyQt." ) + '\n' + QObject::tr( "Python support will be disabled." ) ) )
@@ -365,7 +365,7 @@ QString QgsPythonUtilsImpl::getTraceback()
   PyErr_Fetch( &type, &value, &traceback );
   PyErr_NormalizeException( &type, &value, &traceback );
 
-#ifdef PYTHON2
+#if (PY_VERSION_HEX < 0x03000000)
   const char* iomod = "cStringIO";
 #else
   const char* iomod = "io";
@@ -403,7 +403,7 @@ QString QgsPythonUtilsImpl::getTraceback()
 
   /* And it should be a string all ready to go - duplicate it. */
   if ( !
-#ifdef PYTHON2
+#if (PY_VERSION_HEX < 0x03000000)
        PyString_Check( obResult )
 #else
        PyUnicode_Check( obResult )
@@ -440,7 +440,7 @@ QString QgsPythonUtilsImpl::getTypeAsString( PyObject* obj )
   if ( !obj )
     return nullptr;
 
-#ifdef PYTHON2
+#if (PY_VERSION_HEX < 0x03000000)
   if ( PyClass_Check( obj ) )
   {
     QgsDebugMsg( "got class" );
@@ -515,20 +515,11 @@ QString QgsPythonUtilsImpl::PyObjectToQString( PyObject* obj )
   // check whether the object is already a unicode string
   if ( PyUnicode_Check( obj ) )
   {
-#ifdef PYTHON2
-    PyObject* utf8 = PyUnicode_AsUTF8String( obj );
-    if ( utf8 )
-      result = QString::fromUtf8( PyString_AS_STRING( utf8 ) );
-    else
-      result = "(qgis error)";
-    Py_XDECREF( utf8 );
-#else
     result = PYOBJ2QSTRING( obj );
-#endif
     return result;
   }
 
-#if PYTHON2
+#if (PY_VERSION_HEX < 0x03000000)
   // check whether the object is a classical (8-bit) string
   if ( PyString_Check( obj ) )
   {
