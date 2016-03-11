d393cdf34fc73570212900e7f9b110cc42e14539
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 15:28:15 +0100

    Fix ccache env var

--- ci/travis/linux/qt4/script.sh
+++ ci/travis/linux/qt4/script.sh
@@ -1,5 +1,4 @@
 ccache -s
-export CCACHE_PATH="${HOME}/ccacheqt4"
-mkdir ${CCACHE_PATH}
+export CCACHE_DIR="${HOME}/.ccache-qt4"
 ccache -s
 xvfb-run ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest' -S ./qgis-test-travis.ctest --output-on-failure
