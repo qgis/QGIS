5bc465afd6b55027aa53967525d6707b6a44b8ca
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 15:07:56 +0100

    ccache debug stats

--- ci/travis/linux/qt4/script.sh
+++ ci/travis/linux/qt4/script.sh
@@ -1,2 +1,5 @@
+ccache -s
 export CCACHE_PATH="${HOME}/ccacheqt4"
+mkdir ${CCACHE_PATH}
+ccache -s
 xvfb-run ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest' -S ./qgis-test-travis.ctest --output-on-failure
