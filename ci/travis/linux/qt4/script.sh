d8a9fc74be6f1e9d89f3e9363d3dc2cba71220bc
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 14:46:34 +0100

    Try with an even simpler name

--- ci/travis/linux/qt4/script.sh
+++ ci/travis/linux/qt4/script.sh
@@ -1,2 +1,2 @@
-export CCACHE_PATH="${HOME}/.ccacheqt4"
+export CCACHE_PATH="${HOME}/ccacheqt4"
 xvfb-run ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest' -S ./qgis-test-travis.ctest --output-on-failure
