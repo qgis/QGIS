52685aedd9d615cb20513ffee202987253206f06
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 15:52:58 +0100

    Use ccache on osx

--- ci/travis/linux/qt4/script.sh
+++ ci/travis/linux/qt4/script.sh
@@ -1,4 +1,3 @@
-ccache -s
 export CCACHE_DIR="${HOME}/.ccache-qt4"
-ccache -s
 xvfb-run ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest' -S ./qgis-test-travis.ctest --output-on-failure
+ccache -s
--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -1,2 +1,5 @@
+export CCACHE_DIR="${HOME}/.ccache-osx"
+ccache -s
 ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest|PyQgsServer' -S ./qgis-test-travis.ctest --output-on-failure
 
+ccache -s
