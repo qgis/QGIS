29d2d28330ff5518acfe816acdf912000e8b0290
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 11:35:21 +0100

    Run more tests in Qt5 setup

--- ci/travis/linux/qt5/script.sh
+++ ci/travis/linux/qt5/script.sh
@@ -1,3 +1,5 @@
 export PYTHONPATH=${HOME}/osgeo4travis/lib/python3/dist-packages/
 
-xvfb-run ctest -V -R 'qgis_applicationtest' -S ./qgis-test-travis.ctest --output-on-failure
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+xvfb-run ctest -V -E "$(cat ${DIR}/blacklist.txt | paste -sd '|' -)" -S ./qgis-test-travis.ctest --output-on-failure
