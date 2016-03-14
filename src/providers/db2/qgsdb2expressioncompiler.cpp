2670dbfdf7bb717656dab2082419725edbe4ab2e
David Adler <dadler@adtechgeospatial.com>
2016-03-14 19:35:03 -0400

    fix missing case in qgsdb2expressioncompiler

--- src/providers/db2/qgsdb2expressioncompiler.cpp
+++ src/providers/db2/qgsdb2expressioncompiler.cpp
@@ -117,6 +117,9 @@ QgsSqlExpressionCompiler::Result QgsDb2ExpressionCompiler::compileNode( const Qg
         result = "NOT " + result;
         QgsDebugMsg( QString( "NOT; result: %1; right: %2" ).arg( resultType( rr ) ).arg( result ) );
         return rr;
+
+      case QgsExpression::uoMinus:
+        break;
     }
   }
 
