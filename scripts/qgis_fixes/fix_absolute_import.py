ded1e045c96af1ff4a9c0a2d1fb37ce0fb7c32a0
Juergen E. Fischer <jef@norbit.de>
2016-03-23 08:06:22 +0100

    indentation fix

--- scripts/qgis_fixes/fix_absolute_import.py
+++ scripts/qgis_fixes/fix_absolute_import.py
@@ -1,5 +1,6 @@
 from libfuturize.fixes.fix_absolute_import import FixAbsoluteImport as FixAbsoluteImportOrig
 
+
 class FixAbsoluteImport(FixAbsoluteImportOrig):
 
     def probably_a_local_import(self, imp_name):
