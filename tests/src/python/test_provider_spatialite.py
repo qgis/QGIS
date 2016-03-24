fb1aa6b0f9c50c418eb1a0f568a329ce68d53cb4
Even Rouault <even.rouault@spatialys.com>
2016-03-24 18:37:17 +0100

    [Spatialite] Fix crash on iterator closing if connection failed.
    If an iterator fails to open the spatialite database (mHandle == nullptr
    in QgsSpatiaLiteFeatureIterator::QgsSpatiaLiteFeatureIterator() ),
    a crash will occur at the destruction of the QgsSpatiaLiteFeatureSource,
    due to the iterator not being removed from the list of active iterators.
    
    Currently QgsSpatiaLiteFeatureIterator::close() does not call
    iteratorClosed() if mHandle is invalid, which later causes
    QgsAbstractFeatureSource::~QgsAbstractFeatureSource() to try calling
    the close() method of a now defunct iterator.
    
    If not applying the patch, the added test case crashes with:
    
    177: src/providers/spatialite/qgsspatialiteconnection.cpp: 736: (openDb) [1ms] New sqlite connection for /tmp/test.sqlite.corrupt
    177: src/providers/spatialite/qgsspatialiteconnection.cpp: 750: (openDb) [1ms] Failure while connecting to: /tmp/test.sqlite.corrupt
    177:
    177: invalid metadata tables
    177: src/core/qgsfeaturerequest.cpp: 259: (~QgsAbstractFeatureSource) [0ms] closing active iterator
    177: CMake Error at PyQgsSpatialiteProvider.cmake:22 (MESSAGE):
    177:   Test failed: Segmentation fault
    (cherry picked from commit 2b15eaa14d9e5067ed32f41572d5e9658fa8b278)

--- tests/src/python/test_provider_spatialite.py
+++ tests/src/python/test_provider_spatialite.py
@@ -14,6 +14,7 @@ __revision__ = '$Format:%H$'
 
 import qgis
 import os
+import shutil
 import tempfile
 
 from qgis.core import QgsVectorLayer, QgsPoint, QgsFeature
@@ -202,6 +203,16 @@ class TestQgsSpatialiteProvider(unittest.TestCase, ProviderTestCase):
         fields = [f.name() for f in l.dataProvider().fields()]
         assert('Geometry' not in fields)
 
+    def test_invalid_iterator(self):
+        """ Test invalid iterator """
+        corrupt_dbname = self.dbname + '.corrupt'
+        shutil.copy(self.dbname, corrupt_dbname)
+        layer = QgsVectorLayer("dbname=%s table=test_pg (geometry)" % corrupt_dbname, "test_pg", "spatialite")
+        # Corrupt the database
+        open(corrupt_dbname, 'wb').write('')
+        layer.getFeatures()
+        layer = None
+        os.unlink(corrupt_dbname)
 
 if __name__ == '__main__':
     unittest.main()
