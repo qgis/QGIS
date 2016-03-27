9fe6a9fe48059ea10a74128a4fbf57cfa39795ce
Even Rouault <even.rouault@spatialys.com>
2016-03-27 17:41:22 +0200

    QgsOgcUtils: add support to generate WFS 1.1 and 2.0 filters
    Fixes #14119: Ensure the OGC filter XML expression contains the GML namespace references

--- src/core/qgsogcutils.h
+++ src/core/qgsogcutils.h
@@ -43,6 +43,16 @@ class CORE_EXPORT QgsOgcUtils
 {
   public:
 
+    /** GML version
+     * @note not available in Python bindings
+     */
+    typedef enum
+    {
+      GML_2_1_2,
+      GML_3_1_0,
+      GML_3_2_1,
+    } GMLVersion;
+
     /** Static method that creates geometry from GML
      @param xmlString xml representation of the geometry. GML elements are expected to be
        in default namespace (\verbatim {<Point>...</Point> \endverbatim) or in
@@ -60,6 +70,17 @@ class CORE_EXPORT QgsOgcUtils
     /** Read rectangle from GML3 Envelope */
     static QgsRectangle rectangleFromGMLEnvelope( const QDomNode& envelopeNode );
 
+    /** Exports the geometry to GML
+        @return QDomElement
+        @note Added in QGIS 2.16
+     */
+    static QDomElement geometryToGML( const QgsGeometry* geometry, QDomDocument& doc,
+                                      GMLVersion gmlVersion,
+                                      const QString& srsName,
+                                      bool invertAxisOrientation,
+                                      const QString& gmlIdBase,
+                                      int precision = 17 );
+
     /** Exports the geometry to GML2 or GML3
         @return QDomElement
      */
@@ -75,10 +96,28 @@ class CORE_EXPORT QgsOgcUtils
      */
     static QDomElement rectangleToGMLBox( QgsRectangle* box, QDomDocument& doc, int precision = 17 );
 
-    /** Exports the rectangle to GML2 Envelope
+    /** Exports the rectangle to GML2 Box
+        @return QDomElement
+        @note Added in QGIS 2.16
+     */
+    static QDomElement rectangleToGMLBox( QgsRectangle* box, QDomDocument& doc,
+                                          const QString& srsName,
+                                          bool invertAxisOrientation,
+                                          int precision = 17 );
+
+    /** Exports the rectangle to GML3 Envelope
         @return QDomElement
      */
     static QDomElement rectangleToGMLEnvelope( QgsRectangle* env, QDomDocument& doc, int precision = 17 );
+
+    /** Exports the rectangle to GML3 Envelope
+        @return QDomElement
+        @note Added in QGIS 2.16
+     */
+    static QDomElement rectangleToGMLEnvelope( QgsRectangle* env, QDomDocument& doc,
+        const QString& srsName,
+        bool invertAxisOrientation,
+        int precision = 17 );
 
 
     /** Parse XML with OGC fill into QColor */
@@ -94,7 +133,34 @@ class CORE_EXPORT QgsOgcUtils
      */
     static QDomElement expressionToOgcFilter( const QgsExpression& exp, QDomDocument& doc, QString* errorMessage = nullptr );
 
+    /** OGC filter version
+     *  @note not available in Python bindings
+     */
+    typedef enum
+    {
+      FILTER_OGC_1_0,
+      FILTER_OGC_1_1,
+      FILTER_FES_2_0
+    } FilterVersion;
+
+    /** Creates OGC filter XML element. Supports minimum standard filter
+     * according to the OGC filter specs (=,!=,<,>,<=,>=,AND,OR,NOT)
+     * @return valid \verbatim <Filter> \endverbatim QDomElement on success,
+     * otherwise null QDomElement
+     * @note Added in QGIS 2.16
+     */
+    static QDomElement expressionToOgcFilter( const QgsExpression& exp,
+        QDomDocument& doc,
+        GMLVersion gmlVersion,
+        FilterVersion filterVersion,
+        const QString& geometryName,
+        const QString& srsName,
+        bool honourAxisOrientation,
+        bool invertAxisOrientation,
+        QString* errorMessage = nullptr );
+
   private:
+
     /** Static method that creates geometry from GML Point */
     static QgsGeometry* geometryFromGMLPoint( const QDomElement& geometryElement );
     /** Static method that creates geometry from GML LineString */
@@ -151,15 +217,51 @@ class CORE_EXPORT QgsOgcUtils
     static QgsExpression::Node* nodeIsBetweenFromOgcFilter( QDomElement& element, QString& errorMessage );
     //! handles \verbatim <PropertyIsNull> \endverbatim tag
     static QgsExpression::NodeBinaryOperator* nodePropertyIsNullFromOgcFilter( QDomElement& element, QString& errorMessage );
+};
 
-    static QDomElement expressionNodeToOgcFilter( const QgsExpression::Node* node, QDomDocument& doc, QString& errorMessage );
-    static QDomElement expressionUnaryOperatorToOgcFilter( const QgsExpression::NodeUnaryOperator* node, QDomDocument& doc, QString& errorMessage );
-    static QDomElement expressionBinaryOperatorToOgcFilter( const QgsExpression::NodeBinaryOperator* node, QDomDocument& doc, QString& errorMessage );
-    static QDomElement expressionLiteralToOgcFilter( const QgsExpression::NodeLiteral* node, QDomDocument& doc, QString& errorMessage );
-    static QDomElement expressionColumnRefToOgcFilter( const QgsExpression::NodeColumnRef* node, QDomDocument& doc, QString& errorMessage );
-    static QDomElement expressionInOperatorToOgcFilter( const QgsExpression::NodeInOperator* node, QDomDocument& doc, QString& errorMessage );
-    static QDomElement expressionFunctionToOgcFilter( const QgsExpression::NodeFunction* node, QDomDocument& doc, QString& errorMessage );
+/** Internal use by QgsOgcUtils
+ * @note not available in Python bindings
+ */
+class QgsOgcUtilsExprToFilter
+{
+  public:
+    /** Constructor */
+    QgsOgcUtilsExprToFilter( QDomDocument& doc,
+                             QgsOgcUtils::GMLVersion gmlVersion,
+                             QgsOgcUtils::FilterVersion filterVersion,
+                             const QString& geometryName,
+                             const QString& srsName,
+                             bool honourAxisOrientation,
+                             bool invertAxisOrientation );
 
+    /** Convert an expression to a OGC filter */
+    QDomElement expressionNodeToOgcFilter( const QgsExpression::Node* node );
+
+    /** Return whether the gml: namespace is used */
+    bool GMLNamespaceUsed() const { return mGMLUsed; }
+
+    /** Return the error message. */
+    const QString& errorMessage() const { return mErrorMessage; }
+
+  private:
+    QDomDocument& mDoc;
+    bool mGMLUsed;
+    QgsOgcUtils::GMLVersion mGMLVersion;
+    QgsOgcUtils::FilterVersion mFilterVersion;
+    const QString& mGeometryName;
+    const QString& mSrsName;
+    bool mInvertAxisOrientation;
+    QString mErrorMessage;
+    QString mFilterPrefix;
+    QString mPropertyName;
+    int mGeomId;
+
+    QDomElement expressionUnaryOperatorToOgcFilter( const QgsExpression::NodeUnaryOperator* node );
+    QDomElement expressionBinaryOperatorToOgcFilter( const QgsExpression::NodeBinaryOperator* node );
+    QDomElement expressionLiteralToOgcFilter( const QgsExpression::NodeLiteral* node );
+    QDomElement expressionColumnRefToOgcFilter( const QgsExpression::NodeColumnRef* node );
+    QDomElement expressionInOperatorToOgcFilter( const QgsExpression::NodeInOperator* node );
+    QDomElement expressionFunctionToOgcFilter( const QgsExpression::NodeFunction* node );
 };
 
 #endif // QGSOGCUTILS_H
