929ab89eba357e8c29e2af2799c460a329857ea5
David Adler <dadler@adtechgeospatial.com>
2016-04-01 11:06:05 -0400

    fix testqgsdatasourceuri.cpp again

--- tests/src/core/testqgsdatasourceuri.cpp
+++ tests/src/core/testqgsdatasourceuri.cpp
@@ -116,7 +116,7 @@ void TestQgsDataSourceUri::checkparser_data()
 
   QTest::newRow( "DB2" )
   << "host=localhost port=50000 user='osuser' password='osuserpw' estimatedmetadata=true srid=4326 key=OBJECTID table=TEST.ZIPPOINT (GEOM) myparam='myvalue' driver='IBM DB2 ODBC DRIVER' sql="
-  << "ZIPPOINT" // table
+  << "TEST.ZIPPOINT" // table
   << "GEOM" // geometrycolumn
   << "OBJECTID" // key
   << true // estimatedmetadata
