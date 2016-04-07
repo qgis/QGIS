92398a68f4f193e600c97cf371e7404d24dc9968
Matthias Kuhn <matthias@opengis.ch>
2016-04-07 20:41:40 +0200

    Container based Qt4 builds

--- ci/travis/linux/qt4/install.sh
+++ ci/travis/linux/qt4/install.sh
@@ -1,15 +1,23 @@
 mkdir build
 cd build
 
-export CXX="clang++-3.8"
-export CC="clang-3.8"
+ln -s ${HOME}/osgeo4travis/bin/ccache ${HOME}/osgeo4travis/bin/clang++-3.6
+ln -s ${HOME}/osgeo4travis/bin/ccache ${HOME}/osgeo4travis/bin/clang-3.6
+
+ccache -s
+
+export CXX="clang++-3.6"
+export CC="clang-3.6"
+export PATH=${HOME}/osgeo4travis/bin:${PATH}
 
 cmake --version
 ${CC} --version
+${CXX} --version
 
 CLANG_WARNINGS="-Wimplicit-fallthrough"
 
 cmake -DWITH_SERVER=ON \
+      -DCMAKE_PREFIX_PATH=/home/travis/osgeo4travis \
       -DWITH_STAGED_PLUGINS=ON \
       -DWITH_GRASS=ON \
       -DSUPPRESS_QT_WARNINGS=ON \
