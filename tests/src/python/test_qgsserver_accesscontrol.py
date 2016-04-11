e1f1f92c7b1963a694051a881d8c3b8953c07d5e
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2016-04-11 16:14:10 +0200

    Fix bugin test and set pixel difference to 250 in test_qgsserver_accesscontrol (tests were failing because of differences in antialiasing)

--- tests/src/python/test_qgsserver_accesscontrol.py
+++ tests/src/python/test_qgsserver_accesscontrol.py
@@ -1424,9 +1424,9 @@ class TestQgsServerAccessControl(unittest.TestCase):
         control.setRenderedImage(temp_image)
         if max_size_diff.isValid():
             control.setSizeTolerance(max_size_diff.width(), max_size_diff.height())
-        return control.compareImages(control_image), control.report()
+        return control.compareImages(control_image, max_diff), control.report()
 
-    def _img_diff_error(self, response, headers, image, max_diff=10, max_size_diff=QSize()):
+    def _img_diff_error(self, response, headers, image, max_diff=250, max_size_diff=QSize()):
         self.assertEquals(
             headers.get("Content-Type"), "image/png",
             "Content type is wrong: %s" % headers.get("Content-Type"))
