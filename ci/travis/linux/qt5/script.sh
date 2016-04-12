ab4e9200387d08417021f6c50d8ffbbaeafde189
Matthias Kuhn <matthias@opengis.ch>
2016-04-12 09:37:38 +0200

    Add libSegFault

--- ci/travis/linux/qt5/script.sh
+++ ci/travis/linux/qt5/script.sh
@@ -1,5 +1,6 @@
 export PYTHONPATH=${HOME}/osgeo4travis/lib/python3.3/site-packages/
 export CTEST_PARALLEL_LEVEL=1
+export LD_PRELOAD=libSegFault.so
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
