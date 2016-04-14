1e9898448a9cf566e7d42e4b619a7bb596213d32
Matthias Kuhn <matthias@opengis.ch>
2016-04-14 12:11:28 +0200

    [travis] Python 3: pip install pyspatialite

--- ci/travis/linux/qt5/before_install.sh
+++ ci/travis/linux/qt5/before_install.sh
@@ -13,4 +13,4 @@ curl -L https://github.com/opengisch/osgeo4travis/raw/binary-py33/osgeo4travis.t
 curl -L https://cmake.org/files/v3.5/cmake-3.5.0-Linux-x86_64.tar.gz | tar --strip-components=1 -zxC /home/travis/osgeo4travis
 popd
 
-pip install psycopg2 numpy nose2 pyyaml mock future
+pip install psycopg2 numpy nose2 pyyaml mock future pyspatialite
