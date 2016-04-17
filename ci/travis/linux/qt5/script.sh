e662bcb90777cb94ae7e4cb4cb8030583eae426a
Matthias Kuhn <matthias@opengis.ch>
2016-04-17 21:16:42 +0200

    Enable all Qt5 tests

--- ci/travis/linux/qt5/script.sh
+++ ci/travis/linux/qt5/script.sh
@@ -3,5 +3,5 @@ export CTEST_PARALLEL_LEVEL=1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-xvfb-run ctest -V -E "$(cat ${DIR}/blacklist.txt | paste -sd '|' -)" -S ./qgis-test-travis.ctest --output-on-failure
-# xvfb-run ctest -V -S ./qgis-test-travis.ctest --output-on-failure
+# xvfb-run ctest -V -E "$(cat ${DIR}/blacklist.txt | paste -sd '|' -)" -S ./qgis-test-travis.ctest --output-on-failure
+xvfb-run ctest -V -S ./qgis-test-travis.ctest --output-on-failure
