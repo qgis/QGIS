b7a43f2cc857e39bf385ac3baa74bfa566e387d9
Nyall Dawson <nyall.dawson@gmail.com>
2016-04-19 13:46:36 +1000

    Disable processing GDAL test on OSX (keeps randomly failing)

--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -2,5 +2,5 @@ echo $PATH
 
 export PATH=/usr/bin:${PATH}
 
-ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest|PyQgsServer' -S ./qgis-test-travis.ctest --output-on-failure
+ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest|PyQgsServer|ProcessingGdalAlgorithmsTest' -S ./qgis-test-travis.ctest --output-on-failure
 
