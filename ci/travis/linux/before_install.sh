fa6d2bb131c056d9950702fa91b4918aa5686822
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-04-21 09:32:07 +0200

    Fix Travis build - take 1

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.8 main' -y
+sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main' -y
 
 sudo add-apt-repository ppa:ubuntugis/ppa -y
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y # For postgresql-9.1-postgis-2.1
@@ -69,5 +69,5 @@ sudo -H pip install autopep8 # TODO when switching to trusty or above: replace p
 sudo -H pip install nose2 pyyaml mock
 
 #update clang
-sudo apt-get install --force-yes llvm-3.8 llvm-3.8-dev clang-3.8 libstdc++-4.9-dev
+sudo apt-get install --force-yes llvm-3.6 llvm-3.6-dev clang-3.6 libstdc++-4.9-dev
 
