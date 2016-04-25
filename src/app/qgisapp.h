a7c3861c162441eadefc49cdf4be3952c9a39e45
Juergen E. Fischer <jef@norbit.de>
2016-04-25 09:32:19 +0200

    fix leading comment

--- src/app/qgisapp.h
+++ src/app/qgisapp.h
@@ -12,7 +12,8 @@
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
- *                                                                         * ***************************************************************************/
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef QGISAPP_H
 #define QGISAPP_H
