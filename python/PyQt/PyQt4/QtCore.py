663328685e6a53a2006d8093769d11c670a923c4
Matthias Kuhn <matthias@opengis.ch>
2016-05-02 10:11:13 +0200

    Improve qgis.PyQt compatibility layer

--- python/PyQt/PyQt4/QtCore.py
+++ python/PyQt/PyQt4/QtCore.py
@@ -23,4 +23,44 @@ __copyright__ = '(C) 2015, Matthias Kuhn'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
+import sip
+for api in ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]:
+    sip.setapi(api, 2)
+
 from PyQt4.QtCore import *
+from PyQt4.QtGui import QItemSelectionModel, QSortFilterProxyModel
+
+# Add a __nonzero__ method onto QPyNullVariant so we can check for null values easier.
+#   >>> value = QPyNullVariant("int")
+#   >>> if value:
+#   >>>       print "Not a null value"
+from types import MethodType
+from PyQt4.QtCore import QPyNullVariant
+
+
+def __nonzero__(self):
+    return False
+
+
+def __repr__(self):
+    return 'NULL'
+
+
+def __eq__(self, other):
+    return isinstance(other, QPyNullVariant) or other is None
+
+
+def __ne__(self, other):
+    return not isinstance(other, QPyNullVariant) and other is not None
+
+
+def __hash__(self):
+    return 2178309
+
+QPyNullVariant.__nonzero__ = MethodType(__nonzero__, None, QPyNullVariant)
+QPyNullVariant.__repr__ = MethodType(__repr__, None, QPyNullVariant)
+QPyNullVariant.__eq__ = MethodType(__eq__, None, QPyNullVariant)
+QPyNullVariant.__ne__ = MethodType(__ne__, None, QPyNullVariant)
+QPyNullVariant.__hash__ = MethodType(__hash__, None, QPyNullVariant)
+
+NULL = QPyNullVariant(int)
--- python/PyQt/PyQt5/QtCore.py
+++ python/PyQt/PyQt5/QtCore.py
@@ -1,1 +1,3 @@
 from PyQt5.QtCore import *
+NULL = QVariant()
+QPyNullVariant = QVariant()
