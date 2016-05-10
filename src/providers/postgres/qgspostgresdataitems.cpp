dd4e32662f0b978611e61cccc0909876e72cec9b
Nyall Dawson <nyall.dawson@gmail.com>
2016-05-10 14:15:53 +1000

    Avoid some detachments

--- src/providers/postgres/qgspostgresdataitems.cpp
+++ src/providers/postgres/qgspostgresdataitems.cpp
@@ -449,9 +449,9 @@ QString QgsPGLayerItem::createUri()
 
   QgsDataSourceURI uri( QgsPostgresConn::connUri( connItem->name() ).connectionInfo( false ) );
   uri.setDataSource( mLayerProperty.schemaName, mLayerProperty.tableName, mLayerProperty.geometryColName, mLayerProperty.sql, pkColName );
-  uri.setWkbType( QGis::fromOldWkbType( mLayerProperty.types[0] ) );
+  uri.setWkbType( QGis::fromOldWkbType( mLayerProperty.types.at( 0 ) ) );
   if ( uri.newWkbType() != QgsWKBTypes::NoGeometry )
-    uri.setSrid( QString::number( mLayerProperty.srids[0] ) );
+    uri.setSrid( QString::number( mLayerProperty.srids.at( 0 ) ) );
   QgsDebugMsg( QString( "layer uri: %1" ).arg( uri.uri( false ) ) );
   return uri.uri( false );
 }
