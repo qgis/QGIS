dc755ed02ddb55e297a0abe16403077e4623a112
Nyall Dawson <nyall.dawson@gmail.com>
2016-05-16 15:55:48 +1000

    Fix datetime fields not shown in QgsFieldComboWidget when set to date filter
    (cherry-picked from b4029dc7f0169b60117c4ece7c15a31a8766cb4f)

--- src/gui/qgsfieldproxymodel.cpp
+++ src/gui/qgsfieldproxymodel.cpp
@@ -52,7 +52,8 @@ bool QgsFieldProxyModel::filterAcceptsRow( int source_row, const QModelIndex &so
       ( mFilters.testFlag( LongLong ) && type == QVariant::LongLong ) ||
       ( mFilters.testFlag( Int ) && type == QVariant::Int ) ||
       ( mFilters.testFlag( Double ) && type == QVariant::Double ) ||
-      ( mFilters.testFlag( Date ) && type == QVariant::Date ) )
+      ( mFilters.testFlag( Date ) && type == QVariant::Date ) ||
+      ( mFilters.testFlag( Date ) && type == QVariant::DateTime ) )
     return true;
 
   return false;
