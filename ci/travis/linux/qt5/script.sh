bbe9664dbae7317519b977b04262452860131948
Matthias Kuhn <matthias@opengis.ch>
2016-05-22 15:27:46 +0200

    [travis] Run Qt5 tests with osgeo4travis in $PATH
    This allows running the gdal processing tests and other subprocesses which have
    been manually built like server applications.

--- ci/travis/linux/qt5/script.sh
+++ ci/travis/linux/qt5/script.sh
@@ -1,10 +1,9 @@
 export PYTHONPATH=${HOME}/osgeo4travis/lib/python3.3/site-packages/
+export PATH=${HOME}/osgeo4travis/bin:${HOME}/osgeo4travis/sbin:${PATH}
+export LD_LIBRARY_PATH=${HOME}/osgeo4travis/lib
 export CTEST_PARALLEL_LEVEL=1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# ccache -o max_size=150M
-# ccache -o run_second_cpp=true
 
 xvfb-run ctest -V -E "$(cat ${DIR}/blacklist.txt | paste -sd '|' -)" -S ./qgis-test-travis.ctest --output-on-failure
 # xvfb-run ctest -V -S ./qgis-test-travis.ctest --output-on-failure
