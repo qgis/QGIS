695883e94f6bc0fcdb48690cd360405d52046014
Nyall Dawson <nyall.dawson@gmail.com>
2016-05-25 16:05:06 +1000

    Fix logic in detecting whether attribute form widgets have changed
    Since two null QVariants can be reported as not equal if they have
    different underlying types we need to ensure that we don't flag
    this situation as a changed value.
    
    (cherry-picked from 2fddc0079f153c6dd7cb312c8de96548d9812094)

--- src/gui/qgsattributeform.cpp
+++ src/gui/qgsattributeform.cpp
@@ -176,9 +176,14 @@ bool QgsAttributeForm::save()
       {
         QVariant dstVar = dst.at( eww->fieldIdx() );
         QVariant srcVar = eww->value();
+
         // need to check dstVar.isNull() != srcVar.isNull()
         // otherwise if dstVar=NULL and scrVar=0, then dstVar = srcVar
-        if (( dstVar != srcVar || dstVar.isNull() != srcVar.isNull() ) && srcVar.isValid() && !mLayer->editFormConfig()->readOnly( eww->fieldIdx() ) )
+        // be careful- sometimes two null qvariants will be reported as not equal!! (eg different types)
+        bool changed = ( dstVar != srcVar && !dstVar.isNull() && !srcVar.isNull() )
+                       || ( dstVar.isNull() != srcVar.isNull() );
+        if ( changed && srcVar.isValid()
+             && !mLayer->editFormConfig()->readOnly( eww->fieldIdx() ) )
         {
           dst[eww->fieldIdx()] = srcVar;
 
