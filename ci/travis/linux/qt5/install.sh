fd00f4f5da1fb771ee5dbf39f38ed8cf113650d7
Matthias Kuhn <matthias@opengis.ch>
2016-06-03 09:31:22 +0200

    Temporarily disable missing llvm apt repo

--- ci/travis/linux/qt5/install.sh
+++ ci/travis/linux/qt5/install.sh
@@ -6,8 +6,10 @@ ln -s ${HOME}/osgeo4travis/bin/ccache ${HOME}/osgeo4travis/bin/clang-${LLVM_VERS
 
 ccache -s
 
-export CXX="clang++-${LLVM_VERSION}"
-export CC="clang-${LLVM_VERSION}"
+#export CXX="clang++-${LLVM_VERSION}"
+#export CC="clang-${LLVM_VERSION}"
+export CXX="g++-6"
+export CC="gcc-6"
 export PATH=${HOME}/osgeo4travis/bin:${PATH}
 export PYTHONPATH=${HOME}/osgeo4travis/lib/python3.3/site-packages/
 
@@ -15,7 +17,8 @@ cmake --version
 ${CC} --version
 ${CXX} --version
 
-CLANG_WARNINGS="-Wimplicit-fallthrough"
+# CLANG_WARNINGS="-Wimplicit-fallthrough"
+CLANG_WARNINGS=""
 
 # Include this line for debug reasons
 #      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
