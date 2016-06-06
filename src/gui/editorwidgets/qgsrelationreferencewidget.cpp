f8dccf87714488491d07f69495f4ed44148daabd
Matthias Kuhn <matthias@opengis.ch>
2016-06-06 14:09:24 +0200

    Fix on map identification on relation reference widget with complex PK
    References #14882

--- src/gui/editorwidgets/qgsrelationreferencewidget.cpp
+++ src/gui/editorwidgets/qgsrelationreferencewidget.cpp
@@ -749,7 +749,7 @@ void QgsRelationReferenceWidget::featureIdentified( const QgsFeature& feature )
   }
   else
   {
-    mComboBox->setCurrentIndex( mComboBox->findData( feature.attribute( mReferencedFieldIdx ), QgsAttributeTableModel::FeatureIdRole ) );
+    mComboBox->setCurrentIndex( mComboBox->findData( feature.id(), QgsAttributeTableModel::FeatureIdRole ) );
     mFeature = feature;
   }
 
