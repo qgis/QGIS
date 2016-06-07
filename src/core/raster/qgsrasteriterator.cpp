d77a33d79b493ca73da4bedacb336399b99a2b39
Nyall Dawson <nyall.dawson@gmail.com>
2016-06-07 13:31:33 +1000

    Partially revert d984148
    Undo the maximum tile size changes as these were reverted from
    downstream due to WMS layer regressions

--- src/core/raster/qgsrasteriterator.cpp
+++ src/core/raster/qgsrasteriterator.cpp
@@ -17,8 +17,10 @@
 #include "qgsrasterprojector.h"
 #include "qgsrasterviewport.h"
 
-QgsRasterIterator::QgsRasterIterator( QgsRasterInterface* input ): mInput( input ),
-    mMaximumTileWidth( 256 ), mMaximumTileHeight( 256 )
+QgsRasterIterator::QgsRasterIterator( QgsRasterInterface* input )
+    : mInput( input )
+    , mMaximumTileWidth( 2000 )
+    , mMaximumTileHeight( 2000 )
 {
 }
 
