qgis/QGIS

commit 3ec3daeb14a047c3f4efdd8646a51b33661c28ce

Fix invalid background/text colors showing in attribute table
(cherry-picked from 38e05026fb7a517cfef7b2b69a5a6e3cd7f0c355)

src/gui/attributetable/qgsattributetablemodel.cpp
@@ -553,7 +553,7 @@ QVariant QgsAttributeTableModel::data( const QModelIndex &index, int role ) cons
   if ( index.column() >= mFieldCount )
     return role == Qt::DisplayRole ? rowId : QVariant();
 
-  int fieldId = mAttributes[index.column()];
+  int fieldId = mAttributes.at( index.column() );
 
   if ( role == FieldIndexRole )
     return fieldId;
@@ -593,46 +593,56 @@ QVariant QgsAttributeTableModel::data( const QModelIndex &index, int role ) cons
     val = mFeat.attribute( fieldId );
   }
 
-  if ( role == Qt::DisplayRole )
+  switch ( role )
   {
-    return mWidgetFactories[index.column()]->representValue( layer(), fieldId, mWidgetConfigs[index.column()], mAttributeWidgetCaches[index.column()], val );
+    case Qt::DisplayRole:
+      return mWidgetFactories.at( index.column() )->representValue( layer(), fieldId, mWidgetConfigs.at( index.column() ),
+             mAttributeWidgetCaches.at( index.column() ), val );
+
+    case Qt::EditRole:
+      return val;
+
+    case Qt::BackgroundColorRole:
+    case Qt::TextColorRole:
+    case Qt::DecorationRole:
+    case Qt::FontRole:
+    {
+      mExpressionContext.setFeature( mFeat );
+      QList<QgsConditionalStyle> styles;
+      if ( mRowStylesMap.contains( index.row() ) )
+      {
+        styles = mRowStylesMap[index.row()];
+      }
+      else
+      {
+        styles = QgsConditionalStyle::matchingConditionalStyles( layer()->conditionalStyles()->rowStyles(), QVariant(),  mExpressionContext );
+        mRowStylesMap.insert( index.row(), styles );
+
+      }
+
+      QgsConditionalStyle rowstyle = QgsConditionalStyle::compressStyles( styles );
+      styles = layer()->conditionalStyles()->fieldStyles( field.name() );
+      styles = QgsConditionalStyle::matchingConditionalStyles( styles , val,  mExpressionContext );
+      styles.insert( 0, rowstyle );
+      QgsConditionalStyle style = QgsConditionalStyle::compressStyles( styles );
+
+      if ( style.isValid() )
+      {
+        if ( role == Qt::BackgroundColorRole && style.validBackgroundColor() )
+          return style.backgroundColor();
+        if ( role == Qt::TextColorRole && style.validTextColor() )
+          return style.textColor();
+        if ( role == Qt::DecorationRole )
+          return style.icon();
+        if ( role == Qt::FontRole )
+          return style.font();
+      }
+
+      return QVariant();
+    }
   }
 
-  if ( role == Qt::BackgroundColorRole || role == Qt::TextColorRole || role == Qt::DecorationRole || role == Qt::FontRole )
-  {
-    mExpressionContext.setFeature( mFeat );
-    QList<QgsConditionalStyle> styles;
-    if ( mRowStylesMap.contains( index.row() ) )
-    {
-      styles = mRowStylesMap[index.row()];
-    }
-    else
-    {
-      styles = QgsConditionalStyle::matchingConditionalStyles( layer()->conditionalStyles()->rowStyles(), QVariant(),  mExpressionContext );
-      mRowStylesMap.insert( index.row(), styles );
-
-    }
-
-    QgsConditionalStyle rowstyle = QgsConditionalStyle::compressStyles( styles );
-    styles = layer()->conditionalStyles()->fieldStyles( field.name() );
-    styles = QgsConditionalStyle::matchingConditionalStyles( styles , val,  mExpressionContext );
-    styles.insert( 0, rowstyle );
-    QgsConditionalStyle style = QgsConditionalStyle::compressStyles( styles );
-
-    if ( style.isValid() )
-    {
-      if ( role == Qt::BackgroundColorRole && style.validBackgroundColor() )
-        return style.backgroundColor();
-      if ( role == Qt::TextColorRole && style.validTextColor() )
-        return style.textColor();
-      if ( role == Qt::DecorationRole )
-        return style.icon();
-      if ( role == Qt::FontRole )
-        return style.font();
-    }
-
-  }
-  return val;
+  return QVariant();
 }
 
 bool QgsAttributeTableModel::setData( const QModelIndex &index, const QVariant &value, int role )

commit 671850f66a580e5849c87a11d448b98dd7f338da

Fix sorting in attribute table
Regression introduced with 3ec3daeb14a047c3f4efdd8646a51b33661c28ce (2.14.3) where
QgsAttributeTableModel::data(index, SortRole) returned an empty variant.

Fixes #14927

src/gui/attributetable/qgsattributetablemodel.cpp
@@ -593,6 +593,11 @@ QVariant QgsAttributeTableModel::data( const QModelIndex &index, int role ) cons
     val = mFeat.attribute( fieldId );
   }
 
+  if ( role == SortRole )
+  {
+    return val;
+  }
+
   switch ( role )
   {
     case Qt::DisplayRole: