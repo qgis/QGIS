f9ff5e25d3eb1a2df52d5460e2ae8944aaa6418f
Nyall Dawson <nyall.dawson@gmail.com>
2016-06-16 14:54:05 +1000

    [composer] Instant feedback for legend when linked map changes

--- src/core/composer/qgscomposerlegend.cpp
+++ src/core/composer/qgscomposerlegend.cpp
@@ -305,7 +305,7 @@ void QgsComposerLegend::updateLegend()
 
 void QgsComposerLegend::updateItem()
 {
-  updateFilterByMap();
+  updateFilterByMap( false );
   QgsComposerItem::updateItem();
 }
 
@@ -604,7 +604,7 @@ void QgsComposerLegend::mapLayerStyleOverridesChanged()
   {
     // legend is being filtered by map, so we need to re run the hit test too
     // as the style overrides may also have affected the visible symbols
-    updateFilterByMap();
+    updateFilterByMap( false );
   }
   else
   {
@@ -618,7 +618,7 @@ void QgsComposerLegend::mapLayerStyleOverridesChanged()
   updateItem();
 }
 
-void QgsComposerLegend::updateFilterByMap()
+void QgsComposerLegend::updateFilterByMap( bool redraw )
 {
   if ( isRemoved() )
     return;
@@ -626,6 +626,9 @@ void QgsComposerLegend::updateFilterByMap()
   // the actual update will take place before the redraw.
   // This is to avoid multiple calls to the filter
   mFilterAskedForUpdate = true;
+
+  if ( redraw )
+    QgsComposerItem::updateItem();
 }
 
 void QgsComposerLegend::doUpdateFilterByMap()
