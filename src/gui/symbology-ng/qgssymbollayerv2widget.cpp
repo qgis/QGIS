91c2d769a1919423d0a4e3749f3c579e76e8bc80
Nyall Dawson <nyall.dawson@gmail.com>
2016-06-27 10:12:18 +1000

    Disable simple marker fill color for outline only shapes

--- src/gui/symbology-ng/qgssymbollayerv2widget.cpp
+++ src/gui/symbology-ng/qgssymbollayerv2widget.cpp
@@ -494,6 +494,7 @@ void QgsSimpleMarkerSymbolLayerV2Widget::setSymbolLayer( QgsSymbolLayerV2* layer
   btnChangeColorBorder->blockSignals( false );
   btnChangeColorFill->blockSignals( true );
   btnChangeColorFill->setColor( mLayer->fillColor() );
+  btnChangeColorFill->setEnabled( QgsSimpleMarkerSymbolLayerBase::shapeIsFilled( mLayer->shape() ) );
   btnChangeColorFill->blockSignals( false );
   spinSize->blockSignals( true );
   spinSize->setValue( mLayer->size() );
@@ -568,6 +569,7 @@ QgsSymbolLayerV2* QgsSimpleMarkerSymbolLayerV2Widget::symbolLayer()
 void QgsSimpleMarkerSymbolLayerV2Widget::setName()
 {
   mLayer->setShape( static_cast< QgsSimpleMarkerSymbolLayerBase::Shape>( lstNames->currentItem()->data( Qt::UserRole ).toInt() ) );
+  btnChangeColorFill->setEnabled( QgsSimpleMarkerSymbolLayerBase::shapeIsFilled( mLayer->shape() ) );
   emit changed();
 }
 
