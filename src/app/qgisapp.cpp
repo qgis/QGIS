59c35dff84c36ea65ff2b46348d4636251db3764
Nyall Dawson <nyall.dawson@gmail.com>
2016-07-01 18:37:47 +1000

    Fix actions are not enabled when loading layer with default style (fix #13910)
    (cherry-picked from 1563526f0dc59ae2b1fdc4a58c07488c5603de3d)

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -3330,6 +3330,7 @@ bool QgisApp::addVectorLayers( const QStringList &theLayerQStringList, const QSt
     bool ok;
     l->loadDefaultStyle( ok );
   }
+  activateDeactivateLayerRelatedActions( activeLayer() );
 
   // Only update the map if we frozen in this method
   // Let the caller do it otherwise
