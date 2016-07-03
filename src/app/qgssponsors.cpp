6103669391ae257b3da7f0bbb63c5a641c1b1bc5
Tim Sutton <tim@kartoza.com>
2016-07-03 23:28:29 +0200

    Added sponsors generated from changelog site (#3264)
    * Added sponsors generated from changelog site
    * Use a single rule for including sponsor images rather. Remove terrelogiche duplication. Remove old sponsor images
    * Fix indentations and remove sponsors from images.qrc

--- src/app/qgssponsors.cpp
+++ src/app/qgssponsors.cpp
@@ -30,6 +30,34 @@ QgsSponsors::QgsSponsors( QWidget *parent )
 {
   setupUi( this );
   init();
+  QString intro = tr( "<p>We work really hard to make this nice software for you. "
+                      "See all the cool features it has? Get a warm fuzzy feeling when you use it? "
+                      "QGIS is a labour of love by a dedicated team of developers. We want you to copy "
+                      "&amp; share it and put it in the hands of as many people as possible. If QGIS "
+                      "is saving you money or you like our work and have the financial ability to "
+                      "help, please consider sponsoring the development of QGIS. We use money from "
+                      "sponsors to pay for travel and costs related to our bi-annual hackfests, and to "
+                      "generally support the goals of our project.</p><p>Please see the <a "
+                      "href='http://qgis.org/en/site/getinvolved/governance/sponsorship/sponsorship.html'>QGIS "
+                      "Sponsorship Web Page</a> for more details. In the <a "
+                      "href='http://qgis.org/en/site/about/sponsorship.html#list-of-sponsors'>Sponsors "
+                      "page</a> you can see the fine people and companies that are helping us "
+                      "financially - a great big 'thank you' to you all!</p>" );
+  txtSponsors->setText( intro );
+  // read the SPONSORS file and populate the text widget
+  QFile sponsorsFile( QgsApplication::pkgDataPath() + QLatin1String( "/doc/release-sponsors.html" ) );
+  if ( sponsorsFile.open( QIODevice::ReadOnly ) )
+  {
+    QString path = "images/";
+    QString newPath = QgsApplication::pkgDataPath() + QLatin1String( "/doc/images/" );
+    QTextStream sponsorsStream( &sponsorsFile );
+    // Always use UTF-8
+    sponsorsStream.setCodec( "UTF-8" );
+    QString sponsors = sponsorsStream.readAll();
+    sponsors.replace( path, newPath );
+    txtSponsors->append( sponsors );
+
+  }
 }
 
 QgsSponsors::~QgsSponsors()
