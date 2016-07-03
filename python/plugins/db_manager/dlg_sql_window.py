5e36de70b026b0986bd51640d6470933783cd4e6
Matthias Kuhn <matthias@opengis.ch>
2016-07-03 14:47:46 +0200

    [dbmanager] Fix error when opening SQL window

--- python/plugins/db_manager/dlg_sql_window.py
+++ python/plugins/db_manager/dlg_sql_window.py
@@ -70,7 +70,6 @@ class DlgSqlWindow(QWidget, Ui_Dialog):
 
         self.editSql.setFocus()
         self.editSql.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
-        self.editSql.setMarginVisible(True)
         self.initCompleter()
 
         # allow copying results
