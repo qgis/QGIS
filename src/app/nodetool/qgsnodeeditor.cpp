102ba2d3c51b2541f3b56301c58224a517323dd1
Nyall Dawson <nyall.dawson@gmail.com>
2016-07-05 22:06:04 +1000

    Much faster selection of large number of nodes using node tool
    Refs #13963

--- src/app/nodetool/qgsnodeeditor.cpp
+++ src/app/nodetool/qgsnodeeditor.cpp
@@ -319,15 +319,17 @@ void QgsNodeEditor::updateTableSelection()
   mTableView->selectionModel()->clearSelection();
   const QList<QgsVertexEntry*>& vertexMap = mSelectedFeature->vertexMap();
   int firstSelectedRow = -1;
+  QItemSelection selection;
   for ( int i = 0, n = vertexMap.size(); i < n; ++i )
   {
     if ( vertexMap[i]->isSelected() )
     {
       if ( firstSelectedRow < 0 )
         firstSelectedRow = i;
-      mTableView->selectionModel()->select( mNodeModel->index( i, 0 ), QItemSelectionModel::Rows | QItemSelectionModel::Select );
+      selection.select( mNodeModel->index( i, 0 ), mNodeModel->index( i, mNodeModel->columnCount() - 1 ) );
     }
   }
+  mTableView->selectionModel()->select( selection, QItemSelectionModel::Select );
 
   if ( firstSelectedRow >= 0 )
     mTableView->scrollTo( mNodeModel->index( firstSelectedRow, 0 ), QAbstractItemView::PositionAtTop );
