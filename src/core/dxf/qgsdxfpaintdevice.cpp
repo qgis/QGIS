41378e177019f2a7632e0911accb161e46702c72
Matthias Kuhn <matthias@opengis.ch>
2016-07-09 14:05:18 +0200

    Shutup build warning with Qt 5.6

--- src/core/dxf/qgsdxfpaintdevice.cpp
+++ src/core/dxf/qgsdxfpaintdevice.cpp
@@ -57,6 +57,10 @@ int QgsDxfPaintDevice::metric( PaintDeviceMetric metric ) const
     case QPaintDevice::PdmDevicePixelRatio:
       return 1;
 #endif
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    case QPaintDevice::PdmDevicePixelRatioScaled:
+      return 1;
+#endif
   }
   return 0;
 }
