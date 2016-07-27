9db46ef16e190428bea6d126dab919757c4deb01
Nyall Dawson <nyall.dawson@gmail.com>
2016-07-27 15:36:27 +1000

    Revert "Default to cartographic label placement for point layers"
    This reverts commit e0609d675af69ea6d90477456ff4561bd95fa9e6.

--- src/core/qgspallabeling.cpp
+++ src/core/qgspallabeling.cpp
@@ -786,9 +786,6 @@ void QgsPalLayerSettings::readFromLayer( QgsVectorLayer* layer )
 {
   if ( layer->customProperty( "labeling" ).toString() != QLatin1String( "pal" ) )
   {
-    if ( layer->geometryType() == Qgis::Point )
-      placement = OrderedPositionsAroundPoint;
-
     // for polygons the "over point" (over centroid) placement is better than the default
     // "around point" (around centroid) which is more suitable for points
     if ( layer->geometryType() == Qgis::Polygon )
