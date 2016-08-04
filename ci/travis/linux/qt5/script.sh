623a33f450240b5e86c9da61b228e16794ae791e
Matthias Kuhn <matthias@opengis.ch>
2016-08-04 06:41:52 +0200

    [travis] pull request caching cannot be disabled
    so at least use it in r/w mode

--- ci/travis/linux/qt5/script.sh
+++ ci/travis/linux/qt5/script.sh
@@ -18,10 +18,6 @@ export PATH=${HOME}/osgeo4travis/bin:${HOME}/osgeo4travis/sbin:${PATH}
 export LD_LIBRARY_PATH=${HOME}/osgeo4travis/lib
 export CTEST_PARALLEL_LEVEL=1
 export CCACHE_TEMPDIR=/tmp
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  export CCACHE_READONLY=yes
-  chmod -R ugo-w ~/.ccache
-fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
