cfcef930a9c30c0e00409b14f927431e5761a14f
Matthias Kuhn <matthias@opengis.ch>
2016-08-16 15:10:03 +0200

    [travis] Use Qt 5.7

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -24,7 +24,7 @@ export CORES=2
 
 pushd ${HOME}
 
-curl -L https://github.com/opengisch/osgeo4travis/archive/qt5bin.tar.gz | tar -xzC /home/travis --strip-components=1
+curl -L https://github.com/opengisch/osgeo4travis/archive/qt57bin.tar.gz | tar -xzC /home/travis --strip-components=1
 curl -L https://cmake.org/files/v3.5/cmake-3.5.0-Linux-x86_64.tar.gz | tar --strip-components=1 -zxC /home/travis/osgeo4travis
 
 # Download OTB package for Processing tests
