a79a5b1fdab022ac308ea1b7ad0229b90408fd05
Nyall Dawson <nyall.dawson@gmail.com>
2016-08-17 14:34:44 +1000

    Make QgsVectorLayer uniqueValues/min/maxValue consider edits
    Previously these methods would inconsistently handle the
    edit buffer, eg uniqueValues would consider changed attributes
    but not added features. Now uniqueValues, minimumValue and
    maximumValue all consider both added features and changed
    attribute values when performing their calculation.
    
    The most noticable effect of this fix is that the unique
    values widget now correctly shows values for features which
    have been added but not yet committed to the provider.
    
    (cherry-picked from 50c35929d86ab01b22c29cd129fd7019a1bf624a)

--- src/core/qgsvectorlayer.cpp
+++ src/core/qgsvectorlayer.cpp
@@ -2955,6 +2955,23 @@ void QgsVectorLayer::uniqueValues( int index, QList<QVariant> &uniqueValues, int
           vals << v.toString();
         }
 
+        QgsFeatureMap added = mEditBuffer->addedFeatures();
+        QMapIterator< QgsFeatureId, QgsFeature > addedIt( added );
+        while ( addedIt.hasNext() && ( limit < 0 || uniqueValues.count() < limit ) )
+        {
+          addedIt.next();
+          QVariant v = addedIt.value().attribute( index );
+          if ( v.isValid() )
+          {
+            QString vs = v.toString();
+            if ( !vals.contains( vs ) )
+            {
+              vals << vs;
+              uniqueValues << v;
+            }
+          }
+        }
+
         QMapIterator< QgsFeatureId, QgsAttributeMap > it( mEditBuffer->changedAttributeValues() );
         while ( it.hasNext() && ( limit < 0 || uniqueValues.count() < limit ) )
         {
@@ -3033,7 +3050,35 @@ QVariant QgsVectorLayer::minimumValue( int index )
       return QVariant();
 
     case QgsFields::OriginProvider: //a provider field
-      return mDataProvider->minimumValue( index );
+    {
+      QVariant min = mDataProvider->minimumValue( index );
+      if ( mEditBuffer )
+      {
+        QgsFeatureMap added = mEditBuffer->addedFeatures();
+        QMapIterator< QgsFeatureId, QgsFeature > addedIt( added );
+        while ( addedIt.hasNext() )
+        {
+          addedIt.next();
+          QVariant v = addedIt.value().attribute( index );
+          if ( v.isValid() && qgsVariantLessThan( v, min ) )
+          {
+            min = v;
+          }
+        }
+
+        QMapIterator< QgsFeatureId, QgsAttributeMap > it( mEditBuffer->changedAttributeValues() );
+        while ( it.hasNext() )
+        {
+          it.next();
+          QVariant v = it.value().value( index );
+          if ( v.isValid() && qgsVariantLessThan( v, min ) )
+          {
+            min = v;
+          }
+        }
+      }
+      return min;
+    }
 
     case QgsFields::OriginEdit:
     {
@@ -3092,7 +3137,35 @@ QVariant QgsVectorLayer::maximumValue( int index )
       return QVariant();
 
     case QgsFields::OriginProvider: //a provider field
-      return mDataProvider->maximumValue( index );
+    {
+      QVariant min = mDataProvider->maximumValue( index );
+      if ( mEditBuffer )
+      {
+        QgsFeatureMap added = mEditBuffer->addedFeatures();
+        QMapIterator< QgsFeatureId, QgsFeature > addedIt( added );
+        while ( addedIt.hasNext() )
+        {
+          addedIt.next();
+          QVariant v = addedIt.value().attribute( index );
+          if ( v.isValid() && qgsVariantGreaterThan( v, min ) )
+          {
+            min = v;
+          }
+        }
+
+        QMapIterator< QgsFeatureId, QgsAttributeMap > it( mEditBuffer->changedAttributeValues() );
+        while ( it.hasNext() )
+        {
+          it.next();
+          QVariant v = it.value().value( index );
+          if ( v.isValid() && qgsVariantGreaterThan( v, min ) )
+          {
+            min = v;
+          }
+        }
+      }
+      return min;
+    }
 
     case QgsFields::OriginEdit:
       // the layer is editable, but in certain cases it can still be avoided going through all features
