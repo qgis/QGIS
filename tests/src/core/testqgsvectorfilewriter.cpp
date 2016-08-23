da599495aac6fb0261f16a86ec5096e7c730e441
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-08-23 14:34:46 +0200

    add missing inclue of qgslogger

--- tests/src/core/testqgsvectorfilewriter.cpp
+++ tests/src/core/testqgsvectorfilewriter.cpp
@@ -18,15 +18,16 @@
 #include <QStringList>
 #include <QApplication>
 
-#include <qgsvectorlayer.h> //defines QgsFieldMap 
-#include <qgsvectorfilewriter.h> //logic for writing shpfiles
-#include <qgsfeature.h> //we will need to pass a bunch of these for each rec
-#include <qgsgeometry.h> //each feature needs a geometry
-#include <qgspoint.h> //we will use point geometry
-#include <qgscoordinatereferencesystem.h> //needed for creating a srs
-#include <qgsapplication.h> //search path for srs.db
-#include <qgsfield.h>
-#include <qgis.h> //defines GEOWkt
+#include "qgsvectorlayer.h" //defines QgsFieldMap
+#include "qgsvectorfilewriter.h" //logic for writing shpfiles
+#include "qgsfeature.h" //we will need to pass a bunch of these for each rec
+#include "qgsgeometry.h" //each feature needs a geometry
+#include "qgspoint.h" //we will use point geometry
+#include "qgscoordinatereferencesystem.h" //needed for creating a srs
+#include "qgsapplication.h" //search path for srs.db
+#include "qgslogger.h"
+#include "qgsfield.h"
+#include "qgis.h" //defines GEOWkt
 
 #if defined(linux)
 #include <langinfo.h>
