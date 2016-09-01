5c3a43e286684c5444e5a10eaea9ae3c8ba4a8c6
Hugo Mercier <hugo.mercier@oslandia.com>
2016-09-01 12:18:45 +0200

    Fix spatialite access with python3

--- python/utils.py
+++ python/utils.py
@@ -585,6 +585,41 @@ def startServerPlugin(packageName):
     return True
 
 
+def spatialite_connect(*args, **kwargs):
+    """returns a dbapi2.Connection to a spatialite db
+either using pyspatialite if it is present
+or using the "mod_spatialite" extension (python3)"""
+    try:
+        from pyspatialite import dbapi2
+    except ImportError:
+        import sqlite3
+        con = sqlite3.dbapi2.connect(*args, **kwargs)
+        con.enable_load_extension(True)
+        cur = con.cursor()
+        libs = [
+            # Spatialite >= 4.2 and Sqlite >= 3.7.17, should work on all platforms
+            ("mod_spatialite", "sqlite3_modspatialite_init"),
+            # Spatialite >= 4.2 and Sqlite < 3.7.17 (Travis)
+            ("mod_spatialite.so", "sqlite3_modspatialite_init"),
+            # Spatialite < 4.2 (linux)
+            ("libspatialite.so", "sqlite3_extension_init")
+        ]
+        found = False
+        for lib, entry_point in libs:
+            try:
+                cur.execute("select load_extension('{}', '{}')".format(lib, entry_point))
+            except sqlite3.OperationalError:
+                continue
+            else:
+                found = True
+                break
+        if not found:
+            raise RuntimeError("Cannot find any suitable spatialite module")
+        cur.close()
+        con.enable_load_extension(False)
+        return con
+    return dbapi2.connect(*args, **kwargs)
+
 #######################
 # IMPORT wrapper
 
