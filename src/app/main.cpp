ffe358179e0586b247cadb4ab4322651b29ed849
Matthias Kuhn <matthias@opengis.ch>
2016-09-01 12:29:44 +0200

    Export main() when building for android

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -417,6 +417,11 @@ void myMessageOutput( QtMsgType type, const char *msg )
   }
 }
 
+#if(ANDROID)
+// On Android, there there is a libqgis.so instead of a qgis executable.
+// The main method symbol of this library needs to be exported so it can be called by java
+APP_EXPORT
+#endif
 int main( int argc, char *argv[] )
 {
 #ifdef Q_OS_MACX
