26f06b75520bf1e86718ccc965748e317101cbbe
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-09-08 19:44:45 +0200

    Fix ParameterGeometryPredicate.getValueAsCommandLineParameter
    (cherry picked from commit d85de7ccd8946ab3ded83abae674554d482dbcf5)

--- python/plugins/processing/core/parameters.py
+++ python/plugins/processing/core/parameters.py
@@ -885,7 +885,7 @@ class ParameterGeometryPredicate(Parameter):
             self.enabledPredicates = self.predicates
 
     def getValueAsCommandLineParameter(self):
-        return '"' + unicode(self.value) + '"'
+        return unicode(self.value)
 
     def setValue(self, value):
         if value is None:
