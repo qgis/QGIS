3d514db61ab8fc3b1675d8ed5384bc11270a3b15
Matthias Kuhn <matthias@opengis.ch>
2016-09-14 08:30:30 +0200

    Export QgsOptional<QgsExpression> to dll/so

--- src/core/qgsoptionalexpression.h
+++ src/core/qgsoptionalexpression.h
@@ -28,6 +28,43 @@
  *
  * Added in QGIS 3.0
  */
-typedef QgsOptional<QgsExpression> QgsOptionalExpression;
+
+class CORE_EXPORT QgsOptionalExpression : public QgsOptional<QgsExpression>
+{
+  public:
+    /**
+     * Construct a default optional expression.
+     * It will be disabled and with an empty expression.
+     */
+    QgsOptionalExpression();
+
+    /**
+     * Construct an optional expression with the provided expression.
+     * It will be enabled.
+     */
+    QgsOptionalExpression( const QgsExpression& expression );
+
+    /**
+     * Construct an optional expression with the provided expression and enabled state.
+     */
+    QgsOptionalExpression( const QgsExpression& expression, bool enabled );
+
+    /**
+     * Save the optional expression to the provided QDomElement.
+     *
+     * The caller is responsible to pass an empty QDomElement and to append it to
+     * a parent element.
+     *
+     * @note Added in QGIS 2.18
+     */
+    void writeXml( QDomElement& element );
+
+    /**
+     * Read the optional expression from the provided QDomElement.
+     *
+     * @note Added in QGIS 2.18
+     */
+    void readXml( const QDomElement& element );
+};
 
 #endif // QGSOPTIONALEXPRESSION_H
