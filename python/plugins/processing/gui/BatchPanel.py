c010a035c31360b6699217cd426dd98fdb8ea9ec
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-09-18 16:11:34 +0200

    Call postInitialise in BatchAlgorithmDialog

--- python/plugins/processing/gui/BatchPanel.py
+++ python/plugins/processing/gui/BatchPanel.py
@@ -235,6 +235,7 @@ class BatchPanel(BASE, WIDGET):
         self.wrappers.append([None] * self.tblParameters.columnCount())
         self.tblParameters.setRowCount(self.tblParameters.rowCount() + 1)
 
+        wrappers = {}
         row = self.tblParameters.rowCount() - 1
         column = 0
         for param in self.alg.parameters:
@@ -242,6 +243,7 @@ class BatchPanel(BASE, WIDGET):
                 continue
 
             wrapper = param.wrapper(self.parent, row, column)
+            wrappers[param.name] = wrapper
             self.setCellWrapper(row, column, wrapper)
             column += 1
 
@@ -260,6 +262,9 @@ class BatchPanel(BASE, WIDGET):
             item.addItem(self.tr('No'))
             item.setCurrentIndex(0)
             self.tblParameters.setCellWidget(row, column, item)
+
+        for wrapper in wrappers.values():
+            wrapper.postInitialize(wrappers.values())
 
     def removeRows(self):
         if self.tblParameters.rowCount() > 2:
