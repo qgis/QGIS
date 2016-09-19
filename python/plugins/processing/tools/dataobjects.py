51efec1967a19364ee3a8344cd8c9d1e75eaa073
volaya <volayaf@gmail.com>
2016-09-19 09:11:43 +0200

    [processing] use original filename when possible when exporting vector layer
    Conflicts:
    	python/plugins/processing/tools/dataobjects.py

--- python/plugins/processing/tools/dataobjects.py
+++ python/plugins/processing/tools/dataobjects.py
@@ -46,6 +46,7 @@ from processing.core.ProcessingConfig import ProcessingConfig
 from processing.algs.gdal.GdalUtils import GdalUtils
 from processing.tools.system import (getTempFilenameInTempFolder,
                                      getTempFilename,
+                                     removeInvalidChars
                                      isWindows)
 
 ALL_TYPES = [-1]
@@ -310,6 +311,13 @@ def exportVectorLayer(layer, supported=None):
     systemEncoding = settings.value('/UI/encoding', 'System')
 
     output = getTempFilename('shp')
+    basename = removeInvalidChars(os.path.basename(layer.source()))
+    if basename:
+        if not basename.endswith("shp"):
+            basename = basename + ".shp"
+        output = getTempFilenameInTempFolder(basename)
+    else:
+        output = getTempFilename("shp")
     provider = layer.dataProvider()
     useSelection = ProcessingConfig.getSetting(ProcessingConfig.USE_SELECTED)
     if useSelection and layer.selectedFeatureCount() != 0:
