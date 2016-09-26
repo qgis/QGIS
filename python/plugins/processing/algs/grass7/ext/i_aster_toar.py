87d778b993bf0289a216703d3bc84edd4805e5a8
volaya <volayaf@gmail.com>
2016-09-27 00:56:37 +0200

    [processing] fixed wrong import in i_aster_toar

--- python/plugins/processing/algs/grass7/ext/i_aster_toar.py
+++ python/plugins/processing/algs/grass7/ext/i_aster_toar.py
@@ -26,7 +26,7 @@ __copyright__ = '(C) 2016, Médéric Ribreux'
 __revision__ = '$Format:%H$'
 
 from i import multipleOutputDir
-from processoing.core.parameters import getParameterFromString
+from processing.core.parameters import getParameterFromString
 
 
 def processCommand(alg):
