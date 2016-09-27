9c32fcc153f2da071aaaa19d5abfab517a548076
Matthias Kuhn <matthias@opengis.ch>
2016-09-27 13:35:50 +0200

    Remove Qt4 travis test scripts

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 ###########################################################################
 #    before_install.sh
 #    ---------------------
-#    Date                 : August 2015
-#    Copyright            : (C) 2015 by Nyall Dawson
-#    Email                : nyall dot dawson at gmail dot com
+#    Date                 : March 2016
+#    Copyright            : (C) 2016 by Matthias Kuhn
+#    Email                : matthias at opengis dot ch
 ###########################################################################
 #                                                                         #
 #   This program is free software; you can redistribute it and/or modify  #
@@ -14,6 +13,19 @@
 #                                                                         #
 ###########################################################################
 
+export DEBIAN_FRONTEND=noninteractive
+export CORES=2
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${DIR}/qt${QT_VERSION}/before_install.sh
+##################################################
+#
+# Get precompiled dependencies
+#
+##################################################
+
+pushd ${HOME}
+
+curl -L https://github.com/opengisch/osgeo4travis/archive/qt5bin.tar.gz | tar -xzC /home/travis --strip-components=1
+curl -L https://cmake.org/files/v3.5/cmake-3.5.0-Linux-x86_64.tar.gz | tar --strip-components=1 -zxC /home/travis/osgeo4travis
+popd
+
+pip install psycopg2 numpy nose2 pyyaml mock future termcolor
