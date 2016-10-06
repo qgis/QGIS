98c0da9cf540c9c2a199e2c94434e746798bded0
Juergen E. Fischer <jef@norbit.de>
2016-10-06 12:09:15 +0200

    grass plugin fix (followup 3b0486c)

--- src/plugins/grass/qgsgrasseditrenderer.cpp
+++ src/plugins/grass/qgsgrasseditrenderer.cpp
@@ -173,7 +173,7 @@ void QgsGrassEditRenderer::stopRender( QgsRenderContext& context )
   mMarkerRenderer->stopRender( context );
 }
 
-QSet<QString> QgsGrassEditRenderer::usedAttributes()
+QSet<QString> QgsGrassEditRenderer::usedAttributes() const
 {
   return mLineRenderer->usedAttributes();
 }
