37b00eb602500950a99a531a4e9c2b6891247786
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-10-05 18:02:46 -0600

    [WFS provider] Fix auth config extra expansion and auth prioritization

--- src/providers/wfs/qgswfsprovider.cpp
+++ src/providers/wfs/qgswfsprovider.cpp
@@ -1104,7 +1104,7 @@ bool QgsWFSProvider::describeFeatureType( QString& geometryAttribute, QgsFields&
 {
   fields.clear();
 
-  QgsWFSDescribeFeatureType describeFeatureType( mShared->mURI.uri( false ) );
+  QgsWFSDescribeFeatureType describeFeatureType( mShared->mURI.uri() );
   if ( !describeFeatureType.requestFeatureType( mShared->mWFSVersion,
        mShared->mURI.typeName() ) )
   {
@@ -1340,7 +1340,7 @@ bool QgsWFSProvider::sendTransactionDocument( const QDomDocument& doc, QDomDocum
     return false;
   }
 
-  QgsWFSTransactionRequest request( mShared->mURI.uri( false ) );
+  QgsWFSTransactionRequest request( mShared->mURI.uri() );
   return request.send( doc, serverResponse );
 }
 
@@ -1443,7 +1443,7 @@ bool QgsWFSProvider::getCapabilities()
 
   if ( mShared->mCaps.version.isEmpty() )
   {
-    QgsWfsCapabilities getCapabilities( mShared->mURI.uri( false ) );
+    QgsWfsCapabilities getCapabilities( mShared->mURI.uri() );
     const bool synchronous = true;
     const bool forceRefresh = false;
     if ( !getCapabilities.requestCapabilities( synchronous, forceRefresh ) )
