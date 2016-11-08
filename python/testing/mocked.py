09ad1dba2fe12406e09e5da1d2cc4517508e0a26
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-11-08 08:08:47 +0100

    add vectorLayerTools to qgis testing

--- python/testing/mocked.py
+++ python/testing/mocked.py
@@ -29,7 +29,7 @@ import os
 import sys
 import mock
 
-from qgis.gui import QgisInterface, QgsMapCanvas
+from qgis.gui import QgisInterface, QgsMapCanvas, QgsGuiVectorLayerTools
 from qgis.core import QgsApplication
 
 from qgis.PyQt.QtWidgets import QMainWindow
@@ -61,7 +61,10 @@ def get_iface():
 
     canvas = QgsMapCanvas(my_iface.mainWindow())
     canvas.resize(QSize(400, 400))
-
     my_iface.mapCanvas.return_value = canvas
+    
+    vectorLayerTools = QgsGuiVectorLayerTools()
+    my_iface.vectorLayerTools.return_value = vectorLayerTools
+      
 
     return my_iface
