63f496c681079e80b69ff254e90af19e83520bfb
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2016-11-29 11:30:29 +0100

    WMS 1.3.0 compliance: throw exception if feature info point is not within the WIDTH/HEIGHT parameters

--- src/server/qgswmsserver.cpp
+++ src/server/qgswmsserver.cpp
@@ -2160,6 +2160,12 @@ bool QgsWmsServer::infoPointToMapCoordinates( int i, int j, QgsPoint* infoPoint,
     return false;
   }
 
+  //check if i, j are in the pixel range of the image
+  if ( i < 0 || i > mapRenderer->width() || j < 0 || j > mapRenderer->height() )
+  {
+    throw QgsMapServiceException( "InvalidPoint", "I/J parameters not within the pixel range" );
+  }
+
   double xRes = mapRenderer->extent().width() / mapRenderer->width();
   double yRes = mapRenderer->extent().height() / mapRenderer->height();
   infoPoint->setX( mapRenderer->extent().xMinimum() + i * xRes + xRes / 2.0 );
