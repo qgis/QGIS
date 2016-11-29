ae64430719b2412be65b5611651f828df0eff460
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2016-11-29 11:45:56 +0100

    WMS 1.3.0 compliance: support BGCOLOR parameter

--- src/server/qgswmsserver.cpp
+++ src/server/qgswmsserver.cpp
@@ -1978,6 +1978,19 @@ QImage* QgsWmsServer::createImage( int width, int height ) const
   //transparent parameter
   bool transparent = mParameters.value( QStringLiteral( "TRANSPARENT" ) ).compare( QLatin1String( "true" ), Qt::CaseInsensitive ) == 0;
 
+  //background  color
+  QString bgColorString = mParameters.value( "BGCOLOR" );
+  if ( bgColorString.startsWith( "0x", Qt::CaseInsensitive ) );
+  {
+    bgColorString.replace( 0, 2, "#" );
+  }
+  QColor backgroundColor;
+  backgroundColor.setNamedColor( bgColorString );
+  if ( !backgroundColor.isValid() )
+  {
+    backgroundColor = QColor( Qt::white );
+  }
+
   //use alpha channel only if necessary because it slows down performance
   if ( transparent && !jpeg )
   {
@@ -1987,7 +2000,7 @@ QImage* QgsWmsServer::createImage( int width, int height ) const
   else
   {
     theImage = new QImage( width, height, QImage::Format_RGB32 );
-    theImage->fill( qRgb( 255, 255, 255 ) );
+    theImage->fill( backgroundColor );
   }
 
   if ( !theImage )
