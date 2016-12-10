2f70f1f75c19325d1e76cc183469a05fdf62b23b
Alexander Bruy <alexander.bruy@gmail.com>
2016-12-10 16:03:45 +0200

    [processing] add algorithm for calculating shortest path to multiple end points defined by vector layer

--- python/plugins/processing/algs/qgis/QGISAlgorithmProvider.py
+++ python/plugins/processing/algs/qgis/QGISAlgorithmProvider.py
@@ -185,6 +185,7 @@ from .BasicStatistics import BasicStatisticsForField
 from .Heatmap import Heatmap
 from .Orthogonalize import Orthogonalize
 from .ShortestPathPointToPoint import ShortestPathPointToPoint
+from .ShortestPathPointToLayer import ShortestPathPointToLayer
 from .ServiceAreaFromPoint import ServiceAreaFromPoint
 from .ServiceAreaFromLayer import ServiceAreaFromLayer
 
@@ -252,8 +253,8 @@ class QGISAlgorithmProvider(AlgorithmProvider):
                         ExtractSpecificNodes(), GeometryByExpression(), SnapGeometriesToLayer(),
                         PoleOfInaccessibility(), CreateAttributeIndex(), DropGeometry(),
                         BasicStatisticsForField(), RasterCalculator(), Heatmap(),
-                        Orthogonalize(), ShortestPathPointToPoint(), ServiceAreaFromPoint(),
-                        ServiceAreaFromLayer()
+                        Orthogonalize(), ShortestPathPointToPoint(), ShortestPathPointToLayer(),
+                        ServiceAreaFromPoint(), ServiceAreaFromLayer()
                         ]
 
         if hasMatplotlib:
