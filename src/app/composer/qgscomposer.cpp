398249806f9cc87d935c5cd8a76c7dee3816d45b
nirvn <nirvn.asia@gmail.com>
2016-12-15 13:31:11 +0700

    [FEATURE] toggle visibility of opened panels in main window Accessible via: - View menu > Toggle Panels Visibility - Ctrl+Tab keyboard shortcut

--- src/app/composer/qgscomposer.cpp
+++ src/app/composer/qgscomposer.cpp
@@ -368,10 +368,10 @@ QgsComposer::QgsComposer( QgisApp *qgis, const QString& title )
   mToolbarMenu = new QMenu( tr( "&Toolbars" ), this );
   mToolbarMenu->setObjectName( QStringLiteral( "mToolbarMenu" ) );
   viewMenu->addSeparator();
-  viewMenu->addAction( mActionToggleFullScreen );
-  viewMenu->addAction( mActionHidePanels );
   viewMenu->addMenu( mPanelMenu );
   viewMenu->addMenu( mToolbarMenu );
+  viewMenu->addAction( mActionToggleFullScreen );
+  viewMenu->addAction( mActionHidePanels );
   // toolBar already exists, add other widgets as they are created
   mToolbarMenu->addAction( mComposerToolbar->toggleViewAction() );
   mToolbarMenu->addAction( mPaperNavToolbar->toggleViewAction() );
@@ -4048,11 +4048,6 @@ void QgsComposer::createComposerView()
 
   //view does not accept focus via tab
   mView->setFocusPolicy( Qt::ClickFocus );
-  //instead, if view is focused and tab is pressed than mActionHidePanels is triggered,
-  //to toggle display of panels
-  QShortcut* tab = new QShortcut( Qt::Key_Tab, mView );
-  tab->setContext( Qt::WidgetWithChildrenShortcut );
-  connect( tab, SIGNAL( activated() ), mActionHidePanels, SLOT( trigger() ) );
 }
 
 void QgsComposer::writeWorldFile( const QString& worldFileName, double a, double b, double c, double d, double e, double f ) const
