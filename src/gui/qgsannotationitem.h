d411b67b5c0905fa85a6a4fc2cebcd15a18d147b
Nyall Dawson <nyall.dawson@gmail.com>
2016-12-31 11:41:39 +1000

    Re-add missing doc

--- src/gui/qgsannotationitem.h
+++ src/gui/qgsannotationitem.h
@@ -74,6 +74,10 @@ class GUI_EXPORT QgsAnnotationItem: public QgsMapCanvasItem, public QgsAnnotatio
     virtual QPointF relativePosition() const override;
     virtual double scaleFactor() const override;
     virtual bool showItem() const override { return isVisible(); }
+
+    /**
+     * Sets the CRS of the map position.
+     */
     virtual void setMapPositionCrs( const QgsCoordinateReferenceSystem& crs );
     QgsCoordinateReferenceSystem mapPositionCrs() const override { return mMapPositionCrs; }
 
