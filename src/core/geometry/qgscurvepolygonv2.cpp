81befa3898031eeec917c0481ff0fcf008e50987
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2017-01-05 09:14:18 +0100

    Exclude degenerated rings from polygon area calculation
    (forward port from 64b0b1)

--- src/core/geometry/qgscurvepolygonv2.cpp
+++ src/core/geometry/qgscurvepolygonv2.cpp
@@ -371,7 +371,7 @@ double QgsCurvePolygonV2::area() const
 
   double totalArea = 0.0;
 
-  if ( mExteriorRing->isClosed() )
+  if ( mExteriorRing->isRing() )
   {
     double area = 0.0;
     mExteriorRing->sumUpArea( area );
@@ -382,7 +382,7 @@ double QgsCurvePolygonV2::area() const
   for ( ; ringIt != mInteriorRings.constEnd(); ++ringIt )
   {
     double area = 0.0;
-    if (( *ringIt )->isClosed() )
+    if (( *ringIt )->isRing() )
     {
       ( *ringIt )->sumUpArea( area );
       totalArea -= qAbs( area );
