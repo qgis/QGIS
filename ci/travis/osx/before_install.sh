9f760d2e8f8dc2d8b816e61f483cf7133d9014e5
Matthias Kuhn <matthias@opengis.ch>
2017-01-08 10:54:16 +0100

    Enable travis tests for osx

--- ci/travis/osx/before_install.sh
+++ ci/travis/osx/before_install.sh
@@ -18,24 +18,13 @@ brew remove gdal || true
 
 brew tap osgeo/osgeo4mac
 brew update
-brew install osgeo/osgeo4mac/qgis-214 --without-postgresql --only-dependencies
+brew install python3
+pip3 install psycopg2 numpy nose2 pyyaml mock future
+
+brew install qgis/qgisdev/qgis3-dev --only-dependencies
 brew install spawn-fcgi
 brew install lighttpd
 brew install poppler
 brew install bison
 brew install expat
-
-brew ln bison --force
-brew ln sqlite --force
-brew ln openssl --force
-brew ln expat --force
-brew ln libxml2 --force
-brew ln gettext --force
-brew ln libffi --force
-
-mkdir -p ${HOME}/Library/Python/2.7/lib/python/site-packages
-echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> ${HOME}/Library/Python/2.7/lib/python/site-packages/homebrew.pth
-echo 'import site; site.addsitedir("/usr/local/opt/gdal-20/lib/python2.7/site-packages")' >> ${HOME}/Library/Python/2.7/lib/python/site-packages/gdal2.pth
-
-# Needed for Processing
-pip install psycopg2 numpy nose2 pyyaml mock future
+brew install gdal2 --with-python3
