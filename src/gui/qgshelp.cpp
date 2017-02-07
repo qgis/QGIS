9bd0d7a7bb1ebf528751f7b58201a4b86a590e7c
Alexander Bruy <alexander.bruy@gmail.com>
2017-02-07 16:22:42 +0200

    strip trailing slash from the help path if necessary (fix #16156)

--- src/gui/qgshelp.cpp
+++ src/gui/qgshelp.cpp
@@ -53,7 +53,15 @@ QUrl QgsHelp::helpUrl( const QString& key )
 
   Q_FOREACH ( const QString& path, paths )
   {
-    fullPath = path;
+    if ( path.endsWith( "\\" ) || path.endsWith( "/" ) )
+    {
+      fullPath = path.left( path.size() - 1 );
+    }
+    else
+    {
+      fullPath = path;
+    }
+
     Q_FOREACH ( const QString& var, scope->variableNames() )
     {
       QRegularExpression rx( QStringLiteral( "(<!\\$\\$)*(\\$%1)" ).arg( var ) );
