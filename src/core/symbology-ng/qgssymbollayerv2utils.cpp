e4d3da2eca0d8bae947b448379f31c86ded80e13
Luigi Pirelli <luipir@gmail.com>
2017-02-08 12:37:42 +0100

    moved node value parsing to ogc filter parsing

--- src/core/symbology-ng/qgssymbollayerv2utils.cpp
+++ src/core/symbology-ng/qgssymbollayerv2utils.cpp
@@ -2593,14 +2593,6 @@ bool QgsSymbolLayerV2Utils::functionFromSldElement( QDomElement &element, QStrin
     return false;
   }
 
-  // check if it is a single string value
-  if ( elem.hasChildNodes() &&
-       elem.firstChild().nodeType() == QDomNode::TextNode )
-  {
-    function = elem.firstChild().nodeValue();
-    return true;
-  }
-
   // parse ogc:Filter
   QgsExpression *expr = QgsOgcUtils::expressionFromOgcFilter( elem );
   if ( !expr )
