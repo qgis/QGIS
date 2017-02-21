4c92689d14782c3fc629051c6a4ab7224d556757
Juergen E. Fischer <jef@norbit.de>
2017-02-21 01:15:42 +0100

    fix windows build

--- src/app/mainwin.cpp
+++ src/app/mainwin.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <memory>
 
 std::string moduleExeBaseName( void )
 {
@@ -104,10 +105,10 @@ int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdL
     }
   }
 
-  HINSTANCE hGetProcIDDLL = LoadLibrary( MAINWIN_DLL );
+  HINSTANCE hGetProcIDDLL = LoadLibrary( "qgis_app.dll" );
   if ( !hGetProcIDDLL )
   {
-    std::cerr << "Could not load the " MAINWIN_DLL << std::endl;
+    std::cerr << "Could not load the qgis_app.dll" << std::endl;
     return EXIT_FAILURE;
   }
 
