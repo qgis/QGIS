402149079171fff09aa568aa5dc1fa548b1a66c5
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-02-22 13:11:31 +0100

    make spell test a proper test and not a standalone script

--- scripts/spell_check/spell_test.sh
+++ scripts/spell_check/spell_test.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 set -e
 
-export PATH=${HOME}/osgeo4travis/bin:${PATH}
-
 
 echo "Spell check"
+cd $(git rev-parse --show-toplevel)
 
 if [[ ! -z  $TRAVIS_PULL_REQUEST_BRANCH  ]]; then
   # if on a PR, just analyse the changed files
   echo "TRAVIS PR BRANCH: $TRAVIS_PULL_REQUEST_BRANCH"
   FILES=$(git diff --diff-filter=AM --name-only $(git merge-base HEAD master) | tr '\n' ' ' )
-else
+  export PATH=${HOME}/osgeo4travis/bin:${PATH}
+elif [[ ! -z  $TRAVIS_COMMIT_RANGE  ]]; then
   echo "TRAVIS COMMIT RANGE: $TRAVIS_COMMIT_RANGE"
   FILES=$(git diff --diff-filter=AM --name-only ${TRAVIS_COMMIT_RANGE/.../..} | tr '\n' ' ' )
+  export PATH=${HOME}/osgeo4travis/bin:${PATH}
+else
+  echo "Runnng spell test on all files (might take a while)"
+  FILES=""
 fi
 
 if [[ ! -z $FILES ]]; then
-  ./scripts/spell_check/check_spelling.sh -r $FILES
+  DIR=$(git rev-parse --show-toplevel)/scripts/spell_check
+  ${DIR}/check_spelling.sh -r $FILES
 fi
