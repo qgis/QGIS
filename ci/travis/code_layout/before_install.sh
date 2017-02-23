ccfbaf82f08f9cb7035405501a518eeefce8ddb6
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-02-23 20:08:34 +0100

    run spelling and indentation test on another Travis build (#4180)

---
+++ ci/travis/code_layout/before_install.sh
@@ -0,0 +1,19 @@
+###########################################################################
+#    before_install.sh
+#    ---------------------
+#    Date                 : February 2017
+#    Copyright            : (C) 2017 by Denis Rouzaud
+#    Email                : denis.rouzaud@gmail.com
+###########################################################################
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+###########################################################################
+
+export DEBIAN_FRONTEND=noninteractive
+export CORES=2
+
+pip install autopep8
