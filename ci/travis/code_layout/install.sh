9b56077248b7215c787586c7356ae5d7a2bcfc88
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-02-23 14:07:46 +0100

    run spelling and indentation test on another Travis build

---
+++ ci/travis/code_layout/install.sh
@@ -0,0 +1,28 @@
+###########################################################################
+#    install.sh
+#    ---------------------
+#    Date                 : February 2017
+#    Copyright            : (C) 2017 by Denis Rouzaud
+#    Email                : denis.rouzaud@gmail.com
+###########################################################################
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+###########################################################################
+
+mkdir build
+cd build
+
+
+export CXX="clang++-${LLVM_VERSION}"
+export CC="clang-${LLVM_VERSION}"
+
+
+cmake --version
+${CC} --version
+${CXX} --version
+
+cmake ../ci/travis/code_layout
