2efb2a38f10a5f6c4f46266dbc86a302f2e5d373
Sandro Mani <manisandro@gmail.com>
2017-03-01 16:12:38 +0100

    Fix build against recent sip/PyQt4:
    qgsfiledownloader.sip:33:0:
    src/gui/qgsfiledownloader.h:94:5: error: overriding non-deleted function 'virtual QgsFileDownloader::~QgsFileDownloader()'
    
    RuntimeError: qgis._core cannot import type 'QList<QVariant>' from PyQt4.QtCore
    (cherry picked from commit 85a0db24f32351f6096cd8282f03ad5c2f4e6ef5)
    
    fixes #16071

--- src/gui/qgsfiledownloader.h
+++ src/gui/qgsfiledownloader.h
@@ -90,8 +90,10 @@ class GUI_EXPORT QgsFileDownloader : public QObject
     void onSslErrors( QNetworkReply *reply, const QList<QSslError> &errors );
 #endif
 
-  private:
+  protected:
     ~QgsFileDownloader();
+
+  private:
     /**
      * Abort current request and show an error if the instance has GUI
      * notifications enabled.
