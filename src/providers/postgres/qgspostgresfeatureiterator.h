2a6bcebd5edb38b081175d4f664546d6e633d06a
Nyall Dawson <nyall.dawson@gmail.com>
2017-03-06 06:02:15 +1000

    Revert "Dynamically adjust postgres feature queue size"
    This reverts commit 37a7a22197056c08c5d759eef15791818d52c759.
    
    Leads to performance regressions in some tests

--- src/providers/postgres/qgspostgresfeatureiterator.h
+++ src/providers/postgres/qgspostgresfeatureiterator.h
@@ -118,6 +118,8 @@ class QgsPostgresFeatureIterator : public QgsAbstractFeatureIteratorFromSource<Q
 
     bool mIsTransactionConnection;
 
+    static const int sFeatureQueueSize;
+
   private:
     //! returns whether the iterator supports simplify geometries on provider side
     virtual bool providerCanSimplify( QgsSimplifyMethod::MethodType methodType ) const override;
