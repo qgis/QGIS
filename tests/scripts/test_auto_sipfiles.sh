9a9805e31e91cdef23b39375cfb2b1440e4b9e94
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-03-29 10:06:41 +0200

    use sip includes rather than listing header to generate all sip files

--- tests/scripts/test_auto_sipfiles.sh
+++ tests/scripts/test_auto_sipfiles.sh
@@ -12,21 +12,26 @@ fi
 
 pushd ${DIR} > /dev/null
 
-EXCLUDE=$(cat ${DIR}/python/auto_sip.blacklist | tr '\n' '|' | ${GP}sed -e 's/|$//')
-FILES=$( find src -iname "*.h" \( -path 'src/core/*' -or -path 'src/gui/*' -or -path 'src/analysis/*' \) -type f | tr -s '[[:blank:]]' '\n' | egrep -iv "$EXCLUDE" | tr ' ' '\n')
-
 code=0
-for header in $FILES; do
-  sipfile=$(sed -E 's/src\/(.*)\.h/python\/\1.sip/' <<< $header)
-  outdiff=$(./scripts/sipify.pl $header | diff $sipfile -)
-  if [[ -n $outdiff ]]; then
-    if [[ $code == 0 ]]; then
-      echo "some sip files are not up to date:"
-      code=1
+while read -r sipfile; do
+  if ! grep -Fxq "$sipfile" python/auto_sip.blacklist; then
+    header=$(sed -E 's/(.*)\.sip/src\/\1.h/' <<< $sipfile)
+    if [ ! -f $header ]; then
+      echo "*** Missing header: $header for sipfile $sipfile"
+    else
+      outdiff=$(./scripts/sipify.pl $header | diff python/$sipfile -)
+      if [[ -n $outdiff ]]; then
+        echo " *** SIP file not up to date: $sipfile"
+        code=1
+      fi
     fi
-    echo "$sipfile"
   fi
-done
+done < <(
+sed -n -r 's/^%Include (.*\.sip)/core\/\1/p' python/core/core.sip
+sed -n -r 's/^%Include (.*\.sip)/gui\/\1/p' python/gui/gui.sip
+sed -n -r 's/^%Include (.*\.sip)/analysis\/\1/p' python/analysis/analysis.sip
+  )
+
 
 popd > /dev/null
 
