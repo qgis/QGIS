60e2b1b83758919ae62765c5de5a603ed60aad33
Matthias Kuhn <matthias@opengis.ch>
2017-04-01 10:57:51 +0200

    mac compatibility fix

--- scripts/sipify_all.sh
+++ scripts/sipify_all.sh
@@ -36,9 +36,9 @@ while read -r sipfile; do
     fi
   fi
 done < <(
-sed -n -r 's/^%Include (.*\.sip)/core\/\1/p' python/core/core.sip
-sed -n -r 's/^%Include (.*\.sip)/gui\/\1/p' python/gui/gui.sip
-sed -n -r 's/^%Include (.*\.sip)/analysis\/\1/p' python/analysis/analysis.sip
+${GP}sed -n -r 's/^%Include (.*\.sip)/core\/\1/p' python/core/core.sip
+${GP}sed -n -r 's/^%Include (.*\.sip)/gui\/\1/p' python/gui/gui.sip
+${GP}sed -n -r 's/^%Include (.*\.sip)/analysis\/\1/p' python/analysis/analysis.sip
   )
 
 popd > /dev/null
