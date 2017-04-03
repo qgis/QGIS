e40389a41c3ab9ceff1c54f094535a0b004e3464
Nyall Dawson <nyall.dawson@gmail.com>
2017-04-03 13:53:47 +1000

    Sipify QgsProcessingAlgorithm

--- src/core/processing/qgsprocessingalgorithm.h
+++ src/core/processing/qgsprocessingalgorithm.h
@@ -27,7 +27,7 @@
  * \class QgsProcessingAlgorithm
  * \ingroup core
  * Abstract base class for processing algorithms.
-  * \note added in QGIS 3.0
+  * \since QGIS 3.0
  */
 class CORE_EXPORT QgsProcessingAlgorithm
 {
@@ -55,16 +55,16 @@ class CORE_EXPORT QgsProcessingAlgorithm
      * should be fixed for the algorithm, and must not be localised. The name should
      * be unique within each provider. Names should contain lowercase alphanumeric characters
      * only and no spaces or other formatting characters.
-     * @see displayName()
-     * @see group()
-     * @see tags()
+     * \see displayName()
+     * \see group()
+     * \see tags()
     */
     virtual QString name() const = 0;
 
     /**
      * Returns the translated algorithm name, which should be used for any user-visible display
      * of the algorithm name.
-     * @see name()
+     * \see name()
      */
     virtual QString displayName() const = 0;
 
@@ -76,20 +76,20 @@ class CORE_EXPORT QgsProcessingAlgorithm
 
     /**
      * Returns an icon for the algorithm.
-     * @see svgIconPath()
+     * \see svgIconPath()
     */
     virtual QIcon icon() const;
 
     /**
      * Returns a path to an SVG version of the algorithm's icon.
-     * @see icon()
+     * \see icon()
      */
     virtual QString svgIconPath() const;
 
     /**
      * Returns the name of the group this algorithm belongs to. This string
      * should be localised.
-     * @see tags()
+     * \see tags()
     */
     virtual QString group() const { return QString(); }
 
