7a12d5fa32402fb8032105dbd2e8c59a5ed2abeb
volaya <volayaf@gmail.com>
2017-04-04 15:09:45 +0200

    Revert "[Processing] prevent error when running escapeAndJoin with empty param string"
    This reverts commit c2a566a3a7bfe369832e5361a0c582bfe2ea5a98.

--- python/plugins/processing/algs/gdal/GdalAlgorithm.py
+++ python/plugins/processing/algs/gdal/GdalAlgorithm.py
@@ -30,7 +30,8 @@ import re
 
 from qgis.PyQt.QtCore import QUrl
 
-from qgis.core import QgsApplication
+from qgis.core import (QgsApplication,
+                       QgsVectorFileWriter)
 
 from processing.core.GeoAlgorithm import GeoAlgorithm
 from processing.algs.gdal.GdalAlgorithmDialog import GdalAlgorithmDialog
@@ -58,7 +59,7 @@ class GdalAlgorithm(GeoAlgorithm):
     def processAlgorithm(self, feedback):
         commands = self.getConsoleCommands()
         layers = dataobjects.getVectorLayers()
-        supported = dataobjects.getSupportedOutputVectorLayerExtensions()
+        supported = QgsVectorFileWriter.supportedFormatExtensions()
         for i, c in enumerate(commands):
             for layer in layers:
                 if layer.source() in c:
