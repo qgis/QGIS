8c5e241be4b228e8be74279bb6754a4085386e5d
Nathan Woodrow <nathan_woodrow@technologyonecorp.com>
2017-04-07 13:56:53 +0100

    Add header to Qt templates

--- editors/QtCreator/templates/wizards/qgis/qgis_test/file.cpp
+++ editors/QtCreator/templates/wizards/qgis/qgis_test/file.cpp
@@ -1,4 +1,24 @@
-%{Cpp:LicenseTemplate}
+/***************************************************************************
+                   %{SrcFileName} - %{CN}
+                         --------------
+    begin                : %{CurrentDate:dd-MM-yyyy}
+@if %{HostOs:isWindows}
+    copyright            : (C) %{CurrentDate:yyyy} by %{Env:USERNAME}
+@else
+    copyright            : (C) %{CurrentDate:yyyy} by %{Env:USER}
+@endif
+    email                : [ YOUR EMAIL]
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgstest.h"
 #include <QObject>
 #include <qgsapplication.h>
@@ -10,9 +30,9 @@ class %{CN}: public QObject
 
 private slots:
   void initTestCase(); // will be called before the first testfunction is executed.
-  void cleanupTestCase() {} // will be called after the last testfunction was executed.
-  void init() {} // will be called before each testfunction is executed.
-  void cleanup() {} // will be called after every testfunction.
+  void cleanupTestCase(); // will be called after the last testfunction was executed.
+  void init(); // will be called before each testfunction is executed.
+  void cleanup(); // will be called after every testfunction.
 
   // Add your test methods here
 };
