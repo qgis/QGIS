d230cf2e4f110a466cdb9b452ce8148ece4528ce
Matthias Kuhn <matthias@opengis.ch>
2017-04-30 15:05:48 +0200

    Cleanup blacklist

---
+++ qgsprocessingoutputs.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+  qgsprocessingoutputs.h - QgsProcessingOutputs
+
+ ---------------------
+ begin                : 30.4.2017
+ copyright            : (C) 2017 by Matthias Kuhn, OPENGIS.ch
+ email                : matthias@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef QGSPROCESSINGOUTPUTS_H
+#define QGSPROCESSINGOUTPUTS_H
+
+
+class QgsProcessingOutputs
+{
+  public:
+    QgsProcessingOutputs();
+};
+
+#endif // QGSPROCESSINGOUTPUTS_H
