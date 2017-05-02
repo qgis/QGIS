dea558b8241fc02c88d34dcc8736b9cbc6acef9d
LOMENEDE Jean-Daniel <jean-daniel.lomenede@eure.gouv.fr>
2017-05-02 13:39:22 +0200

    fix error message in merge algorithm

--- python/plugins/processing/algs/qgis/Merge.py
+++ python/plugins/processing/algs/qgis/Merge.py
@@ -89,7 +89,7 @@ class Merge(GeoAlgorithm):
                         if (dfield.type() != sfield.type()):
                             raise GeoAlgorithmExecutionException(
                                 self.tr('{} field in layer {} has different '
-                                        'data type than in other layers.'))
+                                        'data type than in other layers.'.format(sfield.name(), layerSource)))
 
                 if not found:
                     fields.append(sfield)
