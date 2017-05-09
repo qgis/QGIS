1445505a42a2d5a58e05ee1dc2bef0a7c600f6d4
nirvn <nirvn.asia@gmail.com>
2017-05-09 10:07:35 +0700

    [FEATURE] Color settings for north arrow decoration

--- src/app/qgsdecorationnortharrowdialog.cpp
+++ src/app/qgsdecorationnortharrowdialog.cpp
@@ -35,8 +35,6 @@ QgsDecorationNorthArrowDialog::QgsDecorationNorthArrowDialog( QgsDecorationNorth
   QPushButton *applyButton = buttonBox->button( QDialogButtonBox::Apply );
   connect( applyButton, &QAbstractButton::clicked, this, &QgsDecorationNorthArrowDialog::apply );
 
-  // rotation
-  rotatePixmap( mDeco.mRotationInt );
   // signal/slot connection defined in 'designer' causes the slider to
   // be moved to reflect the change in the spinbox.
   spinAngle->setValue( mDeco.mRotationInt );
@@ -57,6 +55,21 @@ QgsDecorationNorthArrowDialog::QgsDecorationNorthArrowDialog( QgsDecorationNorth
 
   // automatic
   cboxAutomatic->setChecked( mDeco.mAutomatic );
+
+  pbnChangeColor->setAllowAlpha( true );
+  pbnChangeColor->setColor( mDeco.mColor );
+  pbnChangeColor->setContext( QStringLiteral( "gui" ) );
+  pbnChangeColor->setColorDialogTitle( tr( "Select north arrow fill color" ) );
+
+  pbnChangeOutlineColor->setAllowAlpha( true );
+  pbnChangeOutlineColor->setColor( mDeco.mOutlineColor );
+  pbnChangeOutlineColor->setContext( QStringLiteral( "gui" ) );
+  pbnChangeOutlineColor->setColorDialogTitle( tr( "Select north arrow outline color" ) );
+
+  connect( pbnChangeColor, &QgsColorButton::colorChanged, this, [ = ]( QColor ) { drawNorthArrow(); } );
+  connect( pbnChangeOutlineColor, &QgsColorButton::colorChanged, this, [ = ]( QColor ) { drawNorthArrow(); } );
+
+  drawNorthArrow();
 }
 
 QgsDecorationNorthArrowDialog::~QgsDecorationNorthArrowDialog()
@@ -89,11 +102,15 @@ void QgsDecorationNorthArrowDialog::on_spinAngle_valueChanged( int spinAngle )
 
 void QgsDecorationNorthArrowDialog::on_sliderRotation_valueChanged( int rotationValue )
 {
-  rotatePixmap( rotationValue );
+  Q_UNUSED( rotationValue );
+
+  drawNorthArrow();
 }
 
 void QgsDecorationNorthArrowDialog::apply()
 {
+  mDeco.mColor = pbnChangeColor->color();
+  mDeco.mOutlineColor = pbnChangeOutlineColor->color();
   mDeco.mRotationInt = sliderRotation->value();
   mDeco.setPlacement( static_cast< QgsDecorationItem::Placement>( cboPlacement->currentData().toInt() ) );
   mDeco.mMarginUnit = wgtUnitSelection->unit();
@@ -104,12 +121,14 @@ void QgsDecorationNorthArrowDialog::apply()
   mDeco.update();
 }
 
-void QgsDecorationNorthArrowDialog::rotatePixmap( int rotationInt )
+void QgsDecorationNorthArrowDialog::drawNorthArrow()
 {
+  int rotation = spinAngle->value();
+
   QSize size( 64, 64 );
   QSvgRenderer svg;
 
-  const QByteArray &svgContent = QgsApplication::svgCache()->svgContent( QStringLiteral( ":/images/north_arrows/default.svg" ), size.width(), QColor( "#000000" ), QColor( "#FFFFFF" ), 0.2, 1.0 );
+  const QByteArray &svgContent = QgsApplication::svgCache()->svgContent( QStringLiteral( ":/images/north_arrows/default.svg" ), size.width(), pbnChangeColor->color(), pbnChangeOutlineColor->color(), 1.0, 1.0 );
   svg.load( svgContent );
 
   if ( svg.isValid() )
@@ -129,11 +148,11 @@ void QgsDecorationNorthArrowDialog::rotatePixmap( int rotationInt )
     //myQPainter.translate( (int)centerXDouble, (int)centerYDouble );
 
     //rotate the canvas
-    myQPainter.rotate( rotationInt );
+    myQPainter.rotate( rotation );
     //work out how to shift the image so that it appears in the center of the canvas
     //(x cos a + y sin a - x, -x sin a + y cos a - y)
     const double PI = 3.14159265358979323846;
-    double myRadiansDouble = ( PI / 180 ) * rotationInt;
+    double myRadiansDouble = ( PI / 180 ) * rotation;
     int xShift = static_cast<int>( (
                                      ( centerXDouble * cos( myRadiansDouble ) ) +
                                      ( centerYDouble * sin( myRadiansDouble ) )
@@ -175,5 +194,5 @@ void QgsDecorationNorthArrowDialog::rotatePixmap( int rotationInt )
 void QgsDecorationNorthArrowDialog::resizeEvent( QResizeEvent *resizeEvent )
 {
   Q_UNUSED( resizeEvent );
-  rotatePixmap( sliderRotation->value() );
+  drawNorthArrow();
 }
