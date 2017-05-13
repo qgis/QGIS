93c59f4e25cdfea7e703845891a907d6c1363ea8
Mathieu Pellerin <nirvn.asia@gmail.com>
2017-05-13 22:19:35 +0700

    [raster] fix raster blending (fixes #16546)

--- src/core/raster/qgsrasterdrawer.cpp
+++ src/core/raster/qgsrasterdrawer.cpp
@@ -102,7 +102,11 @@ void QgsRasterDrawer::draw( QPainter *p, QgsRasterViewPort *viewPort, const QgsM
 
     delete block;
 
-    p->setCompositionMode( QPainter::CompositionMode_SourceOver );  // go back to the default composition mode
+    if ( feedback && feedback->renderPartialOutput() )
+    {
+      // go back to the default composition mode
+      p->setCompositionMode( QPainter::CompositionMode_SourceOver );
+    }
 
     // ok this does not matter much anyway as the tile size quite big so most of the time
     // there would be just one tile for the whole display area, but it won't hurt...
