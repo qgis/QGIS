af6a63ea727be319c30a10909bc394306507e043
Alexander Bruy <alexander.bruy@gmail.com>
2017-05-19 15:52:58 +0300

    [API] convenience API call to create empty raster file with given number of bands. Will be useful for porting raster analysis code to use QgsRasterBlock instead of GDAL calls.
    Follow up ff8d91214748e0363010e263401212d0b4e0e491

--- src/core/raster/qgsrasterfilewriter.h
+++ src/core/raster/qgsrasterfilewriter.h
@@ -67,6 +67,17 @@ class CORE_EXPORT QgsRasterFileWriter
         const QgsRectangle &extent,
         const QgsCoordinateReferenceSystem &crs ) SIP_FACTORY;
 
+    /** Create a raster file with given number of bands without initializing the pixel data.
+     * Returned provider may be used to initialize the raster using writeBlock() calls.
+     * Ownership of the returned provider is passed to the caller.
+     * \note Does not work with tiled mode enabled.
+     * \returns Instance of data provider in editing mode (on success) or nullptr on error.
+     * \since QGIS 3.0
+     */
+    QgsRasterDataProvider *createRaster( Qgis::DataType dataType,
+                                         int width, int height, const QgsRectangle &extent,
+                                         int nBands, const QgsCoordinateReferenceSystem &crs ) SIP_FACTORY;
+
     /** Write raster file
         \param pipe raster pipe
         \param nCols number of output columns
