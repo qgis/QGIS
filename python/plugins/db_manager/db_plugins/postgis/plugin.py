qgis/QGIS

commit 93624b0865bf19b328fd0e8cc5cc4330bffd8be2

Pass "service" parameter to gdal when opening PostGIS Rasters
Closes #16626 for 2.18

python/plugins/db_manager/db_plugins/postgis/plugin.py
@@ -303,6 +303,7 @@ class PGRasterTable(PGTable, RasterTable):
     def gdalUri(self, uri=None):
         if not uri:
             uri = self.database().uri()
+        service = (u'service=%s' % self.service()) if self.service() else ''
         schema = (u'schema=%s' % self.schemaName()) if self.schemaName() else ''
         dbname = (u'dbname=%s' % uri.database()) if uri.database() else ''
         host = (u'host=%s' % uri.host()) if uri.host() else ''
@@ -317,8 +318,8 @@ class PGRasterTable(PGTable, RasterTable):
                 col = u'column=%s' % fld.name
                 break
 
-        gdalUri = u'PG: %s %s %s %s %s mode=2 %s %s table=%s' % \
-                  (dbname, host, user, passw, port, schema, col, self.name)
+        gdalUri = u'PG: %s %s %s %s %s %s mode=2 %s %s table=%s' % \
+                  (service, dbname, host, user, passw, port, schema, col, self.name)
 
         return gdalUri
 

commit d74864bb92116e6d60639637cf5c99a57c8d974e

Fix typo in URI service extraction (introduced in 93624b08)
Also add a comment about lack of support in GDAL up to 2.2.x for
service specification in datasource uri

Ref #16626

python/plugins/db_manager/db_plugins/postgis/plugin.py
@@ -303,7 +303,8 @@ class PGRasterTable(PGTable, RasterTable):
     def gdalUri(self, uri=None):
         if not uri:
             uri = self.database().uri()
-        service = (u'service=%s' % self.service()) if self.service() else ''
+        # NOTE: service-only URIs won't work with GDAL up to 2.2.x
+        service = (u'service=%s' % uri.service()) if uri.service() else ''
         schema = (u'schema=%s' % self.schemaName()) if self.schemaName() else ''
         dbname = (u'dbname=%s' % uri.database()) if uri.database() else ''
         host = (u'host=%s' % uri.host()) if uri.host() else ''