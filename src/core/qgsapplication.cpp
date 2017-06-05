77072b15ac2998e81158211c724c050f6f5af369
Nyall Dawson <nyall.dawson@gmail.com>
2017-06-06 08:42:48 +1000

    Rename QgsProcessingFeatureSink to QgsProcessingFeatureSinkDefinition
    For consistency with QgsProcessingFeatureSourceDefinition

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -131,7 +131,7 @@ void QgsApplication::init( QString customConfigPath )
 
   qRegisterMetaType<QgsGeometry::Error>( "QgsGeometry::Error" );
   qRegisterMetaType<QgsProcessingFeatureSourceDefinition>( "QgsProcessingFeatureSourceDefinition" );
-  qRegisterMetaType<QgsProcessingFeatureSink>( "QgsProcessingFeatureSink" );
+  qRegisterMetaType<QgsProcessingFeatureSinkDefinition>( "QgsProcessingFeatureSinkDefinition" );
 
   QString prefixPath( getenv( "QGIS_PREFIX_PATH" ) ? getenv( "QGIS_PREFIX_PATH" ) : applicationDirPath() );
   // QgsDebugMsg( QString( "prefixPath(): %1" ).arg( prefixPath ) );
