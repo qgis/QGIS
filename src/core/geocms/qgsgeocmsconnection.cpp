d942a38de352249211c819bff8152a9d7081ed8c
myarjunar <myarjunar@gmail.com>
2017-06-13 12:06:23 +0700

    base class for cms connection

---
+++ src/core/geocms/qgsgeocmsconnection.cpp
@@ -0,0 +1,98 @@
+/***************************************************************************
+    qgsgeocmsconnection.cpp
+    ---------------------
+    begin                : Feb 2017
+    copyright            : (C) 2017 by Rohmat, Ismail Sunni
+    email                : rohmat at kartoza dot com, ismail at kartoza dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsgeocmsconnection.h"
+#include "qgssettings.h"
+#include "qgslogger.h"
+#include "qgsnetworkaccessmanager.h"
+
+#include <QMultiMap>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QByteArray>
+#include <QJsonDocument>
+#include <QDebug>
+#include <QUrl>
+#include <QDomDocument>
+
+
+QgsGeoCMSConnection::QgsGeoCMSConnection( const QString &geoCMSName, const QString &connName )
+    : mConnName( connName )
+    , mGeoCMSName( geoCMSName )
+{
+  QgsDebugMsg( "theConnName = " + connName );
+
+  QgsSettings settings;
+
+  QString key = "qgis/connections-" + mGeoCMSName.toLower() + '/' + mConnName;
+  QString credentialsKey = "qgis/" + mGeoCMSName + '/' + mConnName;
+
+  QStringList connStringParts;
+
+  mUri.setParam( QStringLiteral( "url" ), settings.value( key + "/url" ).toString() );
+
+  // Check for credentials and prepend to the connection info
+  QString username = settings.value( credentialsKey + "/username" ).toString();
+  QString password = settings.value( credentialsKey + "/password" ).toString();
+  if ( !username.isEmpty() )
+  {
+    mUri.setParam( QStringLiteral( "username" ), username );
+    mUri.setParam( QStringLiteral( "password" ), password );
+  }
+
+  QString authcfg = settings.value( credentialsKey + "/authcfg" ).toString();
+  if ( !authcfg.isEmpty() )
+  {
+    mUri.setParam( QStringLiteral( "authcfg" ), authcfg );
+  }
+
+  QgsDebugMsg( QString( "encodedUri: '%1'." ).arg( QString( mUri.encodedUri() ) ) );
+}
+
+QgsGeoCMSConnection::~QgsGeoCMSConnection()
+{
+
+}
+
+QgsDataSourceUri QgsGeoCMSConnection::uri()
+{
+  return mUri;
+}
+
+QStringList QgsGeoCMSConnection::connectionList( const QString &geoCMSName )
+{
+  QgsSettings settings;
+  settings.beginGroup( "qgis/connections-" + geoCMSName.toLower() );
+  return settings.childGroups();
+}
+
+void QgsGeoCMSConnection::deleteConnection( const QString &geoCMSName, const QString &name )
+{
+  QgsSettings settings;
+  settings.remove( "qgis/connections-" + geoCMSName.toLower() + '/' + name );
+  settings.remove( "qgis/" + geoCMSName + '/' + name );
+}
+
+QString QgsGeoCMSConnection::selectedConnection( const QString &geoCMSName )
+{
+  QgsSettings settings;
+  return settings.value( "qgis/connections-" + geoCMSName.toLower() + "/selected" ).toString();
+}
+
+void QgsGeoCMSConnection::setSelectedConnection( const QString &geoCMSName, const QString &name )
+{
+  QgsSettings settings;
+  settings.setValue( "qgis/connections-" + geoCMSName.toLower() + "/selected", name );
+}
---
+++ src/core/geocms/qgsgeocmsconnection.h
@@ -0,0 +1,88 @@
+/***************************************************************************
+    qgsgeocmsconnection.h
+    ---------------------
+    begin                : Feb 2017
+    copyright            : (C) 2017 by Rohmat, Ismail Sunni
+    email                : rohmat at kartoza dot com, ismail at kartoza dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSGEOCMSCONNECTION_H
+#define QGSGEOCMSCONNECTION_H
+
+#include "qgis_core.h"
+#include "qgsdatasourceuri.h"
+#include "qgsmaplayer.h"
+#include "qgsproject.h"
+
+#include <QString>
+#include <QMultiMap>
+#include <QNetworkReply>
+
+
+/** \ingroup core
+ * Base class for all GeoCMS connection.
+ * Parent/children hierarchy is not based on QObject.
+*/
+class CORE_EXPORT QgsGeoCMSConnection : public QObject
+{
+    Q_OBJECT
+
+  public:
+
+    /**
+     * Constructor
+     * \param geoCMSName GeoCMS name eg: geonode, qgiscloud, etc
+     * \param connName connection name
+     */
+    QgsGeoCMSConnection( const QString &geoCMSName, const QString &connName );
+
+    //! Destructor
+    ~QgsGeoCMSConnection();
+
+    //! Returns the list of connections for the specified GeoCMS
+    static QStringList connectionList( const QString &geoCMSName );
+
+    //! Deletes the connection for the specified GeoCMS with the specified name
+    static void deleteConnection( const QString &geoCMSName, const QString &name );
+
+    //! Retrieves the selected connection for the specified GeoCMS
+    static QString selectedConnection( const QString &geoCMSName );
+
+    //! Marks the specified connection for the specified GeoCMS as selected
+    static void setSelectedConnection( const QString &geoCMSName, const QString &name );
+
+    //! Return list of available layers
+    virtual QVariantList getLayers() = 0;
+
+    //! Return list of available layers
+    virtual QVariantList getMaps() = 0;
+
+    //! Return available service urls
+    virtual QString serviceUrl() = 0;
+
+    //! The GeoCMS name
+    QString mGeoCMSName;
+
+    //! The connection name
+    QString mConnName;
+
+    //! Getter for mUri
+    QgsDataSourceUri uri();
+
+    //! Property of mUri
+    QgsDataSourceUri mUri;
+
+    QMultiMap<QString, QString> mLayers;
+    QMultiMap<QString, QString> mMaps;
+    QString mData;
+};
+
+
+#endif //QGSGEOCMSCONNECTION_H
