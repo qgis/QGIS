d8fee1d28c77860a508b67c4727ebf0cfa6bb7f2
Nyall Dawson <nyall.dawson@gmail.com>
2017-06-22 05:16:01 +1000

    Reduce noise in algorithm tests

--- python/plugins/processing/tests/AlgorithmsTestBase.py
+++ python/plugins/processing/tests/AlgorithmsTestBase.py
@@ -50,7 +50,7 @@ from processing.script.ScriptAlgorithm import ScriptAlgorithm  # NOQA
 
 from processing.modeler.ModelerAlgorithmProvider import ModelerAlgorithmProvider  # NOQA
 from processing.algs.qgis.QGISAlgorithmProvider import QGISAlgorithmProvider  # NOQA
-from processing.algs.grass7.Grass7AlgorithmProvider import Grass7AlgorithmProvider  # NOQA
+#from processing.algs.grass7.Grass7AlgorithmProvider import Grass7AlgorithmProvider  # NOQA
 from processing.algs.gdal.GdalAlgorithmProvider import GdalAlgorithmProvider  # NOQA
 from processing.algs.saga.SagaAlgorithmProvider import SagaAlgorithmProvider  # NOQA
 from processing.script.ScriptAlgorithmProvider import ScriptAlgorithmProvider  # NOQA
