450eb7c24a995d5193961f6f8e5408cde3e51844
Harrissou Sant-anna <delazj@gmail.com>
2017-06-30 21:27:36 +0200

    Spelling check: Replace analyse by analyze

--- src/app/qgssnappinglayertreemodel.cpp
+++ src/app/qgssnappinglayertreemodel.cpp
@@ -375,7 +375,7 @@ QVariant QgsSnappingLayerTreeModel::data( const QModelIndex &idx, int role ) con
       }
       else
       {
-        // i.e. this is a group, analyse its children
+        // i.e. this is a group, analyze its children
         bool hasChecked = false, hasUnchecked = false;
         int n;
         for ( n = 0; !hasChecked || !hasUnchecked; n++ )
