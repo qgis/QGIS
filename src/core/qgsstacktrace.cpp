f78157be7dd0eef977df7168359ca600c1d9a88a
Juergen E. Fischer <jef@norbit.de>
2017-07-20 16:16:16 +0200

    fix 67f7b3a2

--- src/core/qgsstacktrace.cpp
+++ src/core/qgsstacktrace.cpp
@@ -54,10 +54,10 @@ QVector<QgsStackTrace::StackLine> QgsStackTrace::trace( _EXCEPTION_POINTERS *Exc
   // StackWalk64() may modify context record passed to it, so we will
   // use a copy.
   CONTEXT context_record;
-  if (ExceptionInfo)
+  if ( ExceptionInfo )
     context_record = *ExceptionInfo->ContextRecord;
   else
-    RtlCaptureContext(&context_record);
+    RtlCaptureContext( &context_record );
 
   // Initialize stack walking.
   STACKFRAME64 stack_frame;
