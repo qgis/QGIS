f8b0c06942f6d7a39e61d6041c79233b56a13839
Alexander Bruy <alexander.bruy@gmail.com>
2017-07-21 14:29:42 +0300

    [processing] port Random points in polygons

--- python/plugins/processing/algs/qgis/QGISAlgorithmProvider.py
+++ python/plugins/processing/algs/qgis/QGISAlgorithmProvider.py
@@ -86,6 +86,7 @@ from .RandomExtract import RandomExtract
 from .RandomExtractWithinSubsets import RandomExtractWithinSubsets
 from .RandomPointsExtent import RandomPointsExtent
 from .RandomPointsLayer import RandomPointsLayer
+from .RandomPointsPolygons import RandomPointsPolygons
 from .RegularPoints import RegularPoints
 from .ReverseLineDirection import ReverseLineDirection
 from .Ruggedness import Ruggedness
@@ -143,8 +144,6 @@ from .ZonalStatistics import ZonalStatistics
 # from .PointsDisplacement import PointsDisplacement
 # from .PointsFromPolygons import PointsFromPolygons
 # from .PointsFromLines import PointsFromLines
-# from .RandomPointsPolygonsFixed import RandomPointsPolygonsFixed
-# from .RandomPointsPolygonsVariable import RandomPointsPolygonsVariable
 # from .RandomPointsAlongLines import RandomPointsAlongLines
 # from .PointsToPaths import PointsToPaths
 # from .SetVectorStyle import SetVectorStyle
@@ -274,6 +273,7 @@ class QGISAlgorithmProvider(QgsProcessingProvider):
                 RandomExtractWithinSubsets(),
                 RandomPointsExtent(),
                 RandomPointsLayer(),
+                RandomPointsPolygons(),
                 RegularPoints(),
                 ReverseLineDirection(),
                 Ruggedness(),
