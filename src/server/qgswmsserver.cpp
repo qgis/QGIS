8ccc84ed229cdadc235f6710576daa34b4c3129a
rldhont <rldhont@gmail.com>
2017-07-26 15:11:19 +0200

    [BUGFIX][SERVER] GetFeatureInfo not evaluated for maptip context when using FILTER param
    fixed #16670

--- src/server/qgswmsserver.cpp
+++ src/server/qgswmsserver.cpp
@@ -2362,15 +2362,14 @@ int QgsWMSServer::featureInfoFromVectorLayer( QgsVectorLayer* layer,
       break;
     }
 
+    renderContext.expressionContext().setFeature( feature );
+
     if ( layer->wkbType() != QGis::WKBNoGeometry && ! searchRect.isEmpty() )
     {
       if ( !r2 )
       {
         continue;
       }
-
-      renderContext.expressionContext().setFeature( feature );
-
 
       //check if feature is rendered at all
       bool render = r2->willRenderFeature( feature, renderContext );
