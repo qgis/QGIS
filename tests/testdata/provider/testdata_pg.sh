531dadf682ab715c079c12592bb794c7b3e44fa9
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-08-08 14:50:50 +0200

    fix psql

--- tests/testdata/provider/testdata_pg.sh
+++ tests/testdata/provider/testdata_pg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 SCRIPTS="
   tests/testdata/provider/testdata_pg.sql
   tests/testdata/provider/testdata_pg_reltests.sql
@@ -12,5 +14,5 @@ SCRIPTS="
 dropdb qgis_test 2> /dev/null || true
 createdb qgis_test || exit 1
 for f in ${SCRIPTS}; do
-  psql -q -f $f qgis_test --set ON_ERROR_STOP=1 || exit 1
+  psql --echo-errors -f $f qgis_test -v ON_ERROR_STOP=1
 done
