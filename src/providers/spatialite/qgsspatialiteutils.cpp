cbb9c1ff823e74ab1f3c200171c4a8d71cc16d8a
mj10777 <mj10777@googlemail.com>
2017-08-17 16:40:49 +0200

    Commented out the 2 sqlite3_db_readonly statements.

--- src/providers/spatialite/qgsspatialiteutils.cpp
+++ src/providers/spatialite/qgsspatialiteutils.cpp
@@ -176,7 +176,8 @@ bool SpatialiteDbInfo::attachQSqliteHandle( QgsSqliteHandle *qSqliteHandle )
         if ( mSqliteHandle != mQSqliteHandle->handle() )
         {
           int i_rc = 0;
-          i_rc = sqlite3_db_readonly( mQSqliteHandle->handle(), "main" );
+          // Note: travis reports: error: use of undeclared identifier 'sqlite3_db_readonly'
+          // i_rc = sqlite3_db_readonly( mQSqliteHandle->handle(), "main" );
           switch ( i_rc )
           {
             case 0:
@@ -1392,6 +1393,7 @@ bool SpatialiteDbInfo::getSniffDatabaseType( )
   {
     sqlite3_stmt *stmt = nullptr;
     int i_rc = 0;
+    // Note: travis reports: error: use of undeclared identifier 'sqlite3_db_readonly'
     i_rc = sqlite3_db_readonly( dbSqliteHandle(), "main" );
     switch ( i_rc )
     {
