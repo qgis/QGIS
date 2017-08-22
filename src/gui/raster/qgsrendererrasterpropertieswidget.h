094174e6c64a9ca25872a11d591f1705ee1a7e8f
Alexandre Neto <senhor.neto@gmail.com>
2017-08-22 17:23:20 +0100

    Explicitly allow override of apply() function

--- src/gui/raster/qgsrendererrasterpropertieswidget.h
+++ src/gui/raster/qgsrendererrasterpropertieswidget.h
@@ -63,7 +63,7 @@ class GUI_EXPORT QgsRendererRasterPropertiesWidget : public QgsMapLayerConfigWid
     void rendererChanged();
 
     //! Apply the changes from the dialog to the layer.
-    void apply();
+    void apply() override;
 
     /**
      * \brief Sync the widget to the given layer.
