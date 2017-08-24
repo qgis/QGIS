df87edf5e63699a70c71e4caff1517c90a33e809
C. Marcel <clement.marcel@nwanda.fr>
2017-08-24 16:38:52 +0200

    Delete layers from project & not through layers tree.

--- src/core/qgsproject.cpp
+++ src/core/qgsproject.cpp
@@ -488,8 +488,6 @@ void QgsProject::clear()
   mMapThemeCollection.reset( new QgsMapThemeCollection( this ) );
   emit mapThemeCollectionChanged();
 
-  mRootGroup->clear();
-
   mLabelingEngineSettings->clear();
   emit labelingEngineSettingsChanged();
 
@@ -504,6 +502,7 @@ void QgsProject::clear()
   writeEntry( QStringLiteral( "Measurement" ), QStringLiteral( "/DistanceUnits" ), s.value( QStringLiteral( "/qgis/measure/displayunits" ) ).toString() );
   writeEntry( QStringLiteral( "Measurement" ), QStringLiteral( "/AreaUnits" ), s.value( QStringLiteral( "/qgis/measure/areaunits" ) ).toString() );
 
+  removeAllMapLayers();
   setDirty( false );
 }
 
