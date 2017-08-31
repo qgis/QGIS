c058f59e66dcc31787bfc7d95f4deb3c9e56da72
lbartoletti <l.bartoletti@free.fr>
2017-09-01 01:07:20 +0200

    init processing

--- python/plugins/processing/algs/qgis/QGISAlgorithmProvider.py
+++ python/plugins/processing/algs/qgis/QGISAlgorithmProvider.py
@@ -100,6 +100,7 @@ from .LinesToPolygons import LinesToPolygons
 from .MeanCoords import MeanCoords
 from .Merge import Merge
 from .MergeLines import MergeLines
+from .MinimalEnclosingCircle import MinimalEnclosingCircle
 from .NearestNeighbourAnalysis import NearestNeighbourAnalysis
 from .OffsetLine import OffsetLine
 from .OrientedMinimumBoundingBox import OrientedMinimumBoundingBox
