2104b273afc2a899c0e17cb330e26a40b7af4984
Alexander Bruy <alexander.bruy@gmail.com>
2017-09-27 11:29:00 +0300

    [processing][API]introduce QgsProcessingOutputFile output and fix gdal2xyz test

--- python/plugins/processing/algs/gdal/gdal2xyz.py
+++ python/plugins/processing/algs/gdal/gdal2xyz.py
@@ -29,7 +29,8 @@ from qgis.core import (QgsProcessing,
                        QgsProcessingParameterRasterLayer,
                        QgsProcessingParameterBand,
                        QgsProcessingParameterBoolean,
-                       QgsProcessingParameterFileDestination)
+                       QgsProcessingParameterFileDestination,
+                       QgsProcessingOutputFile)
 from processing.algs.gdal.GdalAlgorithm import GdalAlgorithm
 from processing.algs.gdal.GdalUtils import GdalUtils
 from processing.tools.system import isWindows
@@ -57,6 +58,7 @@ class gdal2xyz(GdalAlgorithm):
         self.addParameter(QgsProcessingParameterFileDestination(self.OUTPUT,
                                                                 self.tr('XYZ ASCII file'),
                                                                 self.tr('CSV files (*.csv)')))
+        self.addOutput(QgsProcessingOutputFile(self.OUTPUT, self.tr('XYZ ASCII file')))
 
     def name(self):
         return 'gdal2xyz'
