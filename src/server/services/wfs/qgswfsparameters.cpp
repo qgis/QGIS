44754edf16f8a1e8cd33c3309857850eeb868e87
rldhont <rldhont@gmail.com>
2017-09-28 12:17:00 +0200

    [Server][WFS] Update QgsWfsParameters for GetFeature

--- src/server/services/wfs/qgswfsparameters.cpp
+++ src/server/services/wfs/qgswfsparameters.cpp
@@ -34,15 +34,92 @@ namespace QgsWfs
                                     };
     save( pOutputFormat );
 
+    const Parameter pResultType = { ParameterName::RESULTTYPE,
+                                    QVariant::String,
+                                    QVariant( "" ),
+                                    QVariant()
+                                  };
+    save( pResultType );
+
+    const Parameter pPropertyName = { ParameterName::PROPERTYNAME,
+                                      QVariant::String,
+                                      QVariant( "" ),
+                                      QVariant()
+                                    };
+    save( pPropertyName );
+
+    const Parameter pMaxFeatures = { ParameterName::MAXFEATURES,
+                                     QVariant::Int,
+                                     QVariant( -1 ),
+                                     QVariant()
+                                   };
+    save( pMaxFeatures );
+
+    const Parameter pStartIndex = { ParameterName::STARTINDEX,
+                                    QVariant::Int,
+                                    QVariant( 0 ),
+                                    QVariant()
+                                  };
+    save( pStartIndex );
+
     const Parameter pSrsName = { ParameterName::SRSNAME,
                                  QVariant::String,
                                  QVariant( "" ),
                                  QVariant()
                                };
     save( pSrsName );
+
+    const Parameter pTypeName = { ParameterName::TYPENAME,
+                                  QVariant::String,
+                                  QVariant( "" ),
+                                  QVariant()
+                                };
+    save( pTypeName );
+
+    const Parameter pFeatureId = { ParameterName::FEATUREID,
+                                   QVariant::String,
+                                   QVariant( "" ),
+                                   QVariant()
+                                 };
+    save( pFeatureId );
+
+    const Parameter pFilter = { ParameterName::FILTER,
+                                QVariant::String,
+                                QVariant( "" ),
+                                QVariant()
+                              };
+    save( pFilter );
+
+    const Parameter pBbox = { ParameterName::BBOX,
+                              QVariant::String,
+                              QVariant( "" ),
+                              QVariant()
+                            };
+    save( pBbox );
+
+    const Parameter pSortBy = { ParameterName::SORTBY,
+                                QVariant::String,
+                                QVariant( "" ),
+                                QVariant()
+                              };
+    save( pSortBy );
+
+    const Parameter pExpFilter = { ParameterName::EXP_FILTER,
+                                   QVariant::String,
+                                   QVariant( "" ),
+                                   QVariant()
+                                 };
+    save( pExpFilter );
+
+    const Parameter pGeometryName = { ParameterName::GEOMETRYNAME,
+                                      QVariant::String,
+                                      QVariant( "" ),
+                                      QVariant()
+                                    };
+    save( pGeometryName );
   }
 
-  QgsWfsParameters::QgsWfsParameters( const QgsServerRequest::Parameters &parameters )
+  QgsWfsParameters::QgsWfsParameters( const QgsServerRequest::Parameters &parameters ) : QgsWfsParameters()
   {
     load( parameters );
   }
@@ -144,9 +221,79 @@ namespace QgsWfs
     return f;
   }
 
+  QString QgsWfsParameters::resultTypeAsString() const
+  {
+    return value( ParameterName::RESULTTYPE ).toString();
+  }
+
+  QStringList QgsWfsParameters::propertyNames() const
+  {
+    return toStringListWithExp( ParameterName::PROPERTYNAME );
+  }
+
+  QString QgsWfsParameters::maxFeatures() const
+  {
+    return value( ParameterName::MAXFEATURES ).toString();
+  }
+
+  int QgsWfsParameters::maxFeaturesAsInt() const
+  {
+    return toInt( ParameterName::MAXFEATURES );
+  }
+
+  QString QgsWfsParameters::startIndex() const
+  {
+    return value( ParameterName::STARTINDEX ).toString();
+  }
+
+  int QgsWfsParameters::startIndexAsInt() const
+  {
+    return toInt( ParameterName::STARTINDEX );
+  }
+
   QString QgsWfsParameters::srsName() const
   {
     return value( ParameterName::SRSNAME ).toString();
+  }
+
+  QStringList QgsWfsParameters::typeNames() const
+  {
+    return toStringList( ParameterName::TYPENAME );
+  }
+
+  QStringList QgsWfsParameters::featureIds() const
+  {
+    return toStringList( ParameterName::FEATUREID );
+  }
+
+  QStringList QgsWfsParameters::filters() const
+  {
+    return toStringListWithExp( ParameterName::FILTER );
+  }
+
+  QString QgsWfsParameters::bbox() const
+  {
+    return value( ParameterName::BBOX ).toString();
+  }
+
+  QgsRectangle QgsWfsParameters::bboxAsRectangle() const
+  {
+    return toRectangle( ParameterName::BBOX );
+  }
+
+  QStringList QgsWfsParameters::sortBy() const
+  {
+    return toStringListWithExp( ParameterName::SORTBY );
+  }
+
+  QStringList QgsWfsParameters::expFilters() const
+  {
+    return toStringListWithExp( ParameterName::EXP_FILTER );
+  }
+
+  QString QgsWfsParameters::geometryNameAsString() const
+  {
+    return value( ParameterName::GEOMETRYNAME ).toString();
   }
 
   QString QgsWfsParameters::request() const
@@ -184,6 +331,119 @@ namespace QgsWfs
   {
     const QMetaEnum metaEnum( QMetaEnum::fromType<ParameterName>() );
     return metaEnum.valueToKey( name );
+  }
+
+  int QgsWfsParameters::toInt( const QVariant &value, const QVariant &defaultValue, bool *error ) const
+  {
+    int val = defaultValue.toInt();
+    QString valStr = value.toString();
+    bool ok = true;
+
+    if ( !valStr.isEmpty() )
+    {
+      val = value.toInt( &ok );
+    }
+    *error = !ok;
+
+    return val;
+  }
+
+  int QgsWfsParameters::toInt( ParameterName p ) const
+  {
+    bool error;
+    int val = toInt( value( p ), defaultValue( p ), &error );
+    if ( error )
+    {
+      QString n = name( p );
+      QString valStr = value( p ).toString();
+      QString msg = n + " ('" + valStr + "') cannot be converted into int";
+      raiseError( msg );
+    }
+
+    return val;
+  }
+
+  QgsRectangle QgsWfsParameters::toRectangle( const QVariant &value, bool *error ) const
+  {
+    *error = false;
+    QString bbox = value.toString();
+    QgsRectangle extent;
+
+    if ( !bbox.isEmpty() )
+    {
+      QStringList corners = bbox.split( "," );
+
+      if ( corners.size() == 4 )
+      {
+        double d[4];
+        bool ok;
+
+        for ( int i = 0; i < 4; i++ )
+        {
+          corners[i].replace( QLatin1String( " " ), QLatin1String( "+" ) );
+          d[i] = corners[i].toDouble( &ok );
+          if ( !ok )
+          {
+            *error = !ok;
+            return extent;
+          }
+        }
+
+        extent = QgsRectangle( d[0], d[1], d[2], d[3] );
+      }
+      else
+      {
+        *error = true;
+        return extent;
+      }
+    }
+
+    return extent;
+  }
+
+  QgsRectangle QgsWfsParameters::toRectangle( ParameterName p ) const
+  {
+    bool error;
+    QgsRectangle extent = toRectangle( value( p ), &error );
+    if ( error )
+    {
+      QString n = name( p );
+      QString valStr = value( p ).toString();
+      QString msg = n + " ('" + valStr + "') cannot be converted into a rectangle";
+      raiseError( msg );
+    }
+
+    return extent;
+  }
+
+  QStringList QgsWfsParameters::toStringList( ParameterName p, char delimiter ) const
+  {
+    return value( p ).toString().split( delimiter, QString::SkipEmptyParts );
+  }
+
+  QStringList QgsWfsParameters::toStringListWithExp( ParameterName p, const QString &exp ) const
+  {
+    QStringList theList;
+
+    QString val = value( p ).toString();
+    if ( val.isEmpty() )
+      return theList;
+
+    QRegExp rx( exp );
+    if ( rx.indexIn( val, 0 ) == -1 )
+    {
+      theList << val;
+    }
+    else
+    {
+      int pos = 0;
+      while ( ( pos = rx.indexIn( val, pos ) ) != -1 )
+      {
+        theList << rx.cap( 1 );
+        pos += rx.matchedLength();
+      }
+    }
+    return theList;
   }
 
   void QgsWfsParameters::log( const QString &msg ) const
--- src/server/services/wfs/qgswfsparameters.h
+++ src/server/services/wfs/qgswfsparameters.h
@@ -22,6 +22,7 @@
 #include <QObject>
 #include <QMetaEnum>
 
+#include "qgsrectangle.h"
 #include "qgswfsserviceexception.h"
 #include "qgsserverrequest.h"
 #include "qgsprojectversion.h"
@@ -42,7 +43,18 @@ namespace QgsWfs
       enum ParameterName
       {
         OUTPUTFORMAT,
-        SRSNAME
+        RESULTTYPE,
+        PROPERTYNAME,
+        MAXFEATURES,
+        STARTINDEX,
+        SRSNAME,
+        TYPENAME,
+        FEATUREID,
+        FILTER,
+        BBOX,
+        SORTBY,
+        EXP_FILTER,
+        GEOMETRYNAME
       };
       Q_ENUM( ParameterName )
 
@@ -106,7 +118,7 @@ namespace QgsWfs
       QgsProjectVersion versionAsNumber() const;
 
       /** Returns OUTPUTFORMAT parameter as a string.
-       * \returns OUTPUTFORMAT parameter as string
+       * \returns outputFormat parameter as string
        */
       QString outputFormatAsString() const;
 
@@ -117,11 +129,88 @@ namespace QgsWfs
        */
       Format outputFormat() const;
 
+      /** Returns RESULTTYPE parameter as a string.
+       * \returns resultType parameter as string
+       */
+      QString resultTypeAsString() const;
+
+      /** Returns PROPERTYNAME parameter as list.
+       * \returns propertyName parameter as list
+       */
+      QStringList propertyNames() const;
+
+      /** Returns MAXFEATURES parameter as a string.
+       * \returns maxFeatures parameter as string
+       */
+      QString maxFeatures() const;
+
+      /** Returns MAXFEATURES parameter as an int or its default value if not
+       *  defined. An exception is raised if I is defined and cannot be
+       *  converted.
+       * \returns maxFeatures parameter
+       * \throws QgsBadRequestException
+       */
+      int maxFeaturesAsInt() const;
+
+      /** Returns STARTINDEX parameter as a string.
+       * \returns startIndex parameter as string
+       */
+      QString startIndex() const;
+
+      /** Returns STARTINDEX parameter as an int or its default value if not
+       *  defined. An exception is raised if I is defined and cannot be
+       *  converted.
+       * \returns startIndex parameter
+       * \throws QgsBadRequestException
+       */
+      int startIndexAsInt() const;
+
       /** Returns SRSNAME parameter as a string.
-       * \returns SRSNAME parameter as string
+       * \returns srsName parameter as string
        */
       QString srsName() const;
 
+      /** Returns TYPENAME parameter as list.
+       * \returns typeName parameter as list
+       */
+      QStringList typeNames() const;
+
+      /** Returns FEATUREID parameter as list.
+       * \returns featureId parameter as list
+       */
+      QStringList featureIds() const;
+
+      /** Returns FILTER parameter as list.
+       * \returns filter parameter as list
+       */
+      QStringList filters() const;
+
+      /** Returns BBOX if defined or an empty string.
+       * \returns bbox parameter
+       */
+      QString bbox() const;
+
+      /** Returns BBOX as a rectangle if defined and valid. An exception is
+       *  raised if the BBOX string cannot be converted into a rectangle.
+       * \returns bbox as rectangle
+       * \throws QgsBadRequestException
+       */
+      QgsRectangle bboxAsRectangle() const;
+
+      /** Returns SORTBY parameter as list.
+       * \returns sortBy parameter as list
+       */
+      QStringList sortBy() const;
+
+      /** Returns EXP_FILTER parameter as list.
+       * \returns expFilters parameter as list
+       */
+      QStringList expFilters() const;
+
+      /** Returns GEOMETRYNAME parameter as a string.
+       * \returns geometryName parameter as string
+       */
+      QString geometryNameAsString() const;
 
     private:
       QString name( ParameterName name ) const;
@@ -131,6 +220,12 @@ namespace QgsWfs
       QVariant defaultValue( ParameterName name ) const;
       void log( const QString &msg ) const;
       void save( const Parameter &parameter );
+      int toInt( const QVariant &value, const QVariant &defaultValue, bool *error = Q_NULLPTR ) const;
+      int toInt( ParameterName name ) const;
+      QgsRectangle toRectangle( const QVariant &value, bool *error = Q_NULLPTR ) const;
+      QgsRectangle toRectangle( ParameterName p ) const;
+      QStringList toStringList( ParameterName p, char delimiter = ',' ) const;
+      QStringList toStringListWithExp( ParameterName p, const QString &exp = "\\(([^()]+)\\)" ) const;
 
       QgsServerRequest::Parameters mRequestParameters;
       QMap<ParameterName, Parameter> mParameters;
