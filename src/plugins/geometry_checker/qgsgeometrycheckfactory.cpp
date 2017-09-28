db8b53f9ac85e76b807ae921efe83813966a1a69
Sandro Mani <manisandro@gmail.com>
2017-09-28 15:05:43 +0200

    [Geometry checker] Ensure duplicate check ui is enabled also for lines

--- src/plugins/geometry_checker/qgsgeometrycheckfactory.cpp
+++ src/plugins/geometry_checker/qgsgeometrycheckfactory.cpp
@@ -194,9 +194,9 @@ template<> void QgsGeometryCheckFactoryT<QgsGeometryDuplicateCheck>::restorePrev
   ui.checkBoxDuplicates->setChecked( QgsSettings().value( sSettingsGroup + "checkDuplicates" ).toBool() );
 }
 
-template<> bool QgsGeometryCheckFactoryT<QgsGeometryDuplicateCheck>::checkApplicability( Ui::QgsGeometryCheckerSetupTab &ui, int nPoint, int /*nLineString*/, int nPolygon ) const
+template<> bool QgsGeometryCheckFactoryT<QgsGeometryDuplicateCheck>::checkApplicability( Ui::QgsGeometryCheckerSetupTab &ui, int nPoint, int nLineString, int nPolygon ) const
 {
-  ui.checkBoxDuplicates->setEnabled( nPoint > 0 || nPolygon > 0 );
+  ui.checkBoxDuplicates->setEnabled( nPoint > 0 || nLineString > 0 || nPolygon > 0 );
   return ui.checkBoxDuplicates->isEnabled();
 }
 
