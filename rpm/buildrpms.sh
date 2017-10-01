15e5419a74fd8943170ec5384a4b2e20e5ba4eea
Daniele Viganò <daniele@vigano.me>
2017-10-01 23:41:47 +0200

    Initial RPM sources update for QGIS3

--- rpm/buildrpms.sh
+++ rpm/buildrpms.sh
@@ -91,6 +91,8 @@ patch=$(grep -e 'SET(CPACK_PACKAGE_VERSION_PATCH' ../CMakeLists.txt |
 
 version=$(echo $major.$minor.$patch)
 
+timestamp=$(date +'%s')
+
 print_info "Building version $version-$RELVER"
 
 if [ "$build_only" -ne "1" ]
@@ -107,6 +109,7 @@ then
   # Create spec file
   cat qgis.spec.template | sed -e s/%{_version}/$version/g \
     | sed -e s/%{_relver}/$RELVER/g \
+    | sed -e s/%{_timestamp}/$timestamp/g \
     | tee qgis.spec 1>/dev/null
   # Build source package
   mock --buildsrpm --spec qgis.spec --sources ./sources --define "_relver $RELVER" --define "_version $version" --resultdir=$OUTDIR
