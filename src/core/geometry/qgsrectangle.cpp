448f8bb1465f44aa5bfb94ac90d7deced33a8bed
Etienne Trimaille <gustrimaille@yahoo.fr>
2017-10-11 20:17:34 +0200

    add helper to build a QgsRectangle from a WKT string

--- src/core/geometry/qgsrectangle.cpp
+++ src/core/geometry/qgsrectangle.cpp
@@ -24,6 +24,7 @@
 #include <QTransform>
 #include <QRegExp>
 
+#include "qgsgeometry.h"
 #include "qgspointxy.h"
 #include "qgsrectangle.h"
 #include "qgslogger.h"
@@ -57,6 +58,24 @@ QgsRectangle::QgsRectangle( const QgsRectangle &r )
   mYmin = r.yMinimum();
   mXmax = r.xMaximum();
   mYmax = r.yMaximum();
+}
+
+QgsRectangle QgsRectangle::fromWkt( const QString &wkt )
+{
+  QgsGeometry geom = QgsGeometry::fromWkt( wkt );
+  if ( geom.isMultipart() )
+    return QgsRectangle();
+
+  QgsPolygon poly = geom.asPolygon();
+
+  if ( poly.size() != 1 )
+    return QgsRectangle();
+
+  QgsPolyline polyline = geom.asPolygon().at( 0 );
+  if ( polyline.size() == 5 && polyline.at( 0 ) == polyline.at( 4 ) && geom.isGeosValid() )
+    return QgsRectangle( polyline.at( 0 ).x(), polyline.at( 0 ).y(), polyline.at( 2 ).x(), polyline.at( 2 ).y() );
+  else
+    return QgsRectangle();
 }
 
 void QgsRectangle::set( const QgsPointXY &p1, const QgsPointXY &p2 )
