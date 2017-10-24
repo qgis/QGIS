a15c183aeb4fb51e2057bdff0def85d1773169a9
Matthias Kuhn <matthias@opengis.ch>
2017-10-25 01:44:11 +0200

    More doxymentation

--- src/gui/qgsfeaturelistcombobox.h
+++ src/gui/qgsfeaturelistcombobox.h
@@ -27,6 +27,14 @@ class QgsFeatureFilterModel;
 class QgsAnimatedIcon;
 class QgsFilterLineEdit;
 
+/**
+ * This offers a combobox with autocompleter that allows to select features from a layer.
+ *
+ * It will show up to 100 entries at a time. The entries can be chosen based on the displayExpression
+ * and whenever text is typed into the combobox, the completer and popup will adjust to features matching the typed text.
+ *
+ * \since QGIS 3.0
+ */
 class GUI_EXPORT QgsFeatureListComboBox : public QComboBox
 {
     Q_OBJECT
@@ -41,26 +49,87 @@ class GUI_EXPORT QgsFeatureListComboBox : public QComboBox
   public:
     QgsFeatureListComboBox( QWidget *parent = nullptr );
 
+    /**
+     * The layer from which features should be listed.
+     */
     QgsVectorLayer *sourceLayer() const;
+
+    /**
+     * The layer from which features should be listed.
+     */
     void setSourceLayer( QgsVectorLayer *sourceLayer );
 
+    /**
+     * The display expression will be used to display features as well as
+     * the the value to match the typed text against.
+     */
     QString displayExpression() const;
+
+    /**
+     * The display expression will be used to display features as well as
+     * the the value to match the typed text against.
+     */
     void setDisplayExpression( const QString &displayExpression );
 
+    /**
+     * An additional expression to further restrict the available features.
+     * This can be used to integrate additional spatial or other constraints.
+     */
     QString filterExpression() const;
+
+    /**
+     * An additional expression to further restrict the available features.
+     * This can be used to integrate additional spatial or other constraints.
+     *
+     * TODO!
+     */
     void setFilterExpression( const QString &filterExpression );
 
+    /**
+     * The identifier value of the currently selected feature. A value from the
+     * identifierField.
+     */
     QVariant identifierValue() const;
+
+    /**
+     * The identifier value of the currently selected feature. A value from the
+     * identifierField.
+     */
     void setIdentifierValue( const QVariant &identifierValue );
 
+    /**
+     * Shorthand for getting a feature request to query the currently selected
+     * feature.
+     */
     QgsFeatureRequest currentFeatureRequest() const;
 
+    /**
+     * Determines if a NULL value should be available in the list.
+     *
+     * TODO!
+     */
     bool allowNull() const;
+
+    /**
+     * Determines if a NULL value should be available in the list.
+     */
     void setAllowNull( bool allowNull );
 
+    /**
+     * Field name that will be used to uniquely identify the current feature.
+     * Normally the primary key of the layer.
+     */
     QString identifierField() const;
+
+    /**
+     * Field name that will be used to uniquely identify the current feature.
+     * Normally the primary key of the layer.
+     */
     void setIdentifierField( const QString &identifierField );
 
+    /**
+     * The index of the currently selected item.
+     */
     QModelIndex currentModelIndex() const;
 
     virtual void focusOutEvent( QFocusEvent *event ) override;
@@ -68,11 +137,39 @@ class GUI_EXPORT QgsFeatureListComboBox : public QComboBox
     virtual void keyPressEvent( QKeyEvent *event ) override;
 
   signals:
+
+    /**
+     * The layer from which features should be listed.
+     */
     void sourceLayerChanged();
+
+    /**
+     * The display expression will be used to display features as well as
+     * the the value to match the typed text against.
+     */
     void displayExpressionChanged();
+
+    /**
+     * An additional expression to further restrict the available features.
+     * This can be used to integrate additional spatial or other constraints.
+     */
     void filterExpressionChanged();
+
+    /**
+     * The identifier value of the currently selected feature. A value from the
+     * identifierField.
+     */
     void identifierValueChanged();
+
+    /**
+     * Field name that will be used to uniquely identify the current feature.
+     * Normally the primary key of the layer.
+     */
     void identifierFieldChanged();
+
+    /**
+     * Determines if a NULL value should be available in the list.
+     */
     void allowNullChanged();
 
   private slots:
