640b095515162fc5d21045855d773d8e2eac81d5
Juernjakob Dugge <juernjakob@gmail.com>
2017-11-04 13:39:09 +0100

    #5416 Update maptips delay on saving the settings

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -3885,6 +3885,11 @@ void QgisApp::createMapTips()
   mpMaptip = new QgsMapTip();
 }
 
+void QgisApp::setMapTipsDelay( int timerInterval )
+{
+  mpMapTipsTimer->setInterval( timerInterval );
+}
+
 void QgisApp::createDecorations()
 {
   QgsDecorationCopyright *mDecorationCopyright = new QgsDecorationCopyright( this );
--- src/app/qgsoptions.cpp
+++ src/app/qgsoptions.cpp
@@ -1315,6 +1315,8 @@ void QgsOptions::saveOptions()
   mSettings->setValue( QStringLiteral( "/qgis/compileExpressions" ), cbxCompileExpressions->isChecked() );
   mSettings->setValue( QStringLiteral( "/qgis/defaultLegendGraphicResolution" ), mLegendGraphicResolutionSpinBox->value() );
   mSettings->setValue( QStringLiteral( "/qgis/mapTipsDelay" ), mMapTipsDelaySpinBox->value() );
+  QgisApp::instance()->setMapTipsDelay( mMapTipsDelaySpinBox->value() );
+
   bool createRasterLegendIcons = mSettings->value( QStringLiteral( "/qgis/createRasterLegendIcons" ), false ).toBool();
   mSettings->setValue( QStringLiteral( "/qgis/createRasterLegendIcons" ), cbxCreateRasterLegendIcons->isChecked() );
   mSettings->setValue( QStringLiteral( "/qgis/copyFeatureFormat" ), mComboCopyFeatureFormat->currentData().toInt() );
