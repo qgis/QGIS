cd23779e6fb116c55973ba7b8c75d72099e5e4bc
Alessandro Pasotti <apasotti@boundlessgeo.com>
2017-11-06 12:42:20 +0100

    [auth][bugfix] Add a getter for error list on qgsmigrate

--- src/core/qgserror.h
+++ src/core/qgserror.h
@@ -128,6 +128,12 @@ class CORE_EXPORT QgsError
     //! Clear error messages
     void clear() { mMessageList.clear(); }
 
+    /**
+     * \brief messageList return the list of current error messages
+     * \return current list of error messages
+     */
+    const QList<QgsErrorMessage> messageList() { return mMessageList; }
+
   private:
     //! List of messages
     QList<QgsErrorMessage> mMessageList;
