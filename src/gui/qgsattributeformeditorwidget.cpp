53d642cecc3e58fb2993c8ece5ed4b0d9b711b71
David <david@opengis.ch>
2017-11-09 17:48:23 +0100

    Constraint Result Indicator invisible when not in editable mode
    The icon for the constraint result indicator cross/tipp disappears when not in editable mode
    this is made by the new method setconstraintresult.

--- src/gui/qgsattributeformeditorwidget.cpp
+++ src/gui/qgsattributeformeditorwidget.cpp
@@ -159,6 +159,11 @@ void QgsAttributeFormEditorWidget::setConstraintStatus( const QString &constrain
   }
 }
 
+void QgsAttributeFormEditorWidget::setConstraintResultVisibility( bool editable )
+{
+  mConstraintResultLabel->setHidden( !editable );
+}
+
 void QgsAttributeFormEditorWidget::setMode( QgsAttributeFormEditorWidget::Mode mode )
 {
   mMode = mode;
