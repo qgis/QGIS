e6db0dd2203dbcef4ae0d47381487c65943857c7
Alessandro Pasotti <apasotti@boundlessgeo.com>
2017-11-20 08:55:08 +0100

    [bugfix] Do not crash when testing empty virtual layers
    Fixes #17489
    
    Require backporting

--- src/providers/virtual/qgsvirtuallayersourceselect.cpp
+++ src/providers/virtual/qgsvirtuallayersourceselect.cpp
@@ -199,15 +199,20 @@ QgsVirtualLayerDefinition QgsVirtualLayerSourceSelect::getVirtualLayerDef()
 void QgsVirtualLayerSourceSelect::onTestQuery()
 {
   QgsVirtualLayerDefinition def = getVirtualLayerDef();
-
-  std::unique_ptr<QgsVectorLayer> vl( new QgsVectorLayer( def.toString(), QStringLiteral( "test" ), QStringLiteral( "virtual" ) ) );
-  if ( vl->isValid() )
+  // If the definition is empty just do nothing.
+  // TODO: a validation function that can enable/disable the test button
+  //       according to the validity of the active layer definition
+  if ( ! def.toString().isEmpty() )
   {
-    QMessageBox::information( nullptr, tr( "Virtual layer test" ), tr( "No error" ) );
-  }
-  else
-  {
-    QMessageBox::critical( nullptr, tr( "Virtual layer test" ), vl->dataProvider()->error().summary() );
+    std::unique_ptr<QgsVectorLayer> vl( new QgsVectorLayer( def.toString(), QStringLiteral( "test" ), QStringLiteral( "virtual" ) ) );
+    if ( vl->isValid() )
+    {
+      QMessageBox::information( nullptr, tr( "Virtual layer test" ), tr( "No error" ) );
+    }
+    else
+    {
+      QMessageBox::critical( nullptr, tr( "Virtual layer test" ), vl->dataProvider()->error().summary() );
+    }
   }
 }
 
