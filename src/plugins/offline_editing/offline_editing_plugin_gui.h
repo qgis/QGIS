68ddf3b9916a1148b3f645856c13ae0232e8ecca
Alessandro Pasotti <apasotti@boundlessgeo.com>
2017-11-21 14:45:35 +0100

    [offline-editing] Add a warning icon and tooltip to WFS sources
    Fixes #16753 off-line editing synchronization cripples
    the original datasource if this is a WFS-T Layer

--- src/plugins/offline_editing/offline_editing_plugin_gui.h
+++ src/plugins/offline_editing/offline_editing_plugin_gui.h
@@ -30,9 +30,8 @@ class QgsSelectLayerTreeModel : public QgsLayerTreeModel
     Q_OBJECT
   public:
     QgsSelectLayerTreeModel( QgsLayerTree *rootNode, QObject *parent = nullptr );
-
+    int columnCount( const QModelIndex &parent = QModelIndex() ) const override;
     QVariant data( const QModelIndex &index, int role = Qt::DisplayRole ) const override;
-    // bool setData( const QModelIndex &index, const QVariant &value, int role = Qt::EditRole ) override;
 };
 
 class QgsOfflineEditingPluginGui : public QDialog, private Ui::QgsOfflineEditingPluginGuiBase
