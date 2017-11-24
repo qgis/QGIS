1fcadb72ff3f2f9b008e395bd7ff11a9ffe053b2
Mathieu Pellerin <nirvn.asia@gmail.com>
2017-11-24 12:22:19 +0700

    [processing] fix for import into spatialite and execute sql algorithms

--- python/plugins/processing/algs/qgis/ImportIntoSpatialite.py
+++ python/plugins/processing/algs/qgis/ImportIntoSpatialite.py
@@ -84,7 +84,9 @@ class ImportIntoSpatialite(QgisAlgorithm):
         databaseuri = database.dataProvider().dataSourceUri()
         uri = QgsDataSourceUri(databaseuri)
         if uri.database() is '':
-            if '|layerid' in databaseuri:
+            if '|layername' in databaseuri:
+                databaseuri = databaseuri[:databaseuri.find('|layername')]
+            elif '|layerid' in databaseuri:
                 databaseuri = databaseuri[:databaseuri.find('|layerid')]
             uri = QgsDataSourceUri('dbname=\'%s\'' % (databaseuri))
         db = spatialite.GeoDB(uri)
