83b141a21ec76f2dcd8cfb896c01c04e262634bf
Nyall Dawson <nyall.dawson@gmail.com>
2017-11-26 16:11:37 +1000

    Add missing output from package algorithm

--- src/analysis/processing/qgsalgorithmpackage.cpp
+++ src/analysis/processing/qgsalgorithmpackage.cpp
@@ -47,6 +47,7 @@ void QgsPackageAlgorithm::initAlgorithm( const QVariantMap & )
   addParameter( new QgsProcessingParameterMultipleLayers( QStringLiteral( "LAYERS" ), QObject::tr( "Input layers" ), QgsProcessing::TypeVector ) );
   addParameter( new QgsProcessingParameterFileDestination( QStringLiteral( "OUTPUT" ), QObject::tr( "Destination GeoPackage" ), QObject::tr( "GeoPackage files (*.gpkg)" ) ) );
   addParameter( new QgsProcessingParameterBoolean( QStringLiteral( "OVERWRITE" ), QObject::tr( "Overwrite existing GeoPackage" ), false ) );
+  addOutput( new QgsProcessingOutputFile( QStringLiteral( "OUTPUT" ), QObject::tr( "GeoPackage" ) ) );
 }
 
 QString QgsPackageAlgorithm::shortHelpString() const
