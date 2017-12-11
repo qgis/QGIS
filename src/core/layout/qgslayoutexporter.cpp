aa7986f8fc66ce2b0b9169127555276658eb3bcd
Nyall Dawson <nyall.dawson@gmail.com>
2017-12-11 12:07:29 +1000

    Implement methods for exporting layouts as raster, add tests

--- src/core/layout/qgslayoutexporter.cpp
+++ src/core/layout/qgslayoutexporter.cpp
@@ -18,6 +18,8 @@
 #include "qgslayout.h"
 #include "qgslayoutitemmap.h"
 #include "qgslayoutpagecollection.h"
+#include <QImageWriter>
+#include <QSize>
 
 QgsLayoutExporter::QgsLayoutExporter( QgsLayout *layout )
   : mLayout( layout )
@@ -25,7 +27,7 @@ QgsLayoutExporter::QgsLayoutExporter( QgsLayout *layout )
 
 }
 
-void QgsLayoutExporter::renderPage( QPainter *painter, int page )
+void QgsLayoutExporter::renderPage( QPainter *painter, int page ) const
 {
   if ( !mLayout )
     return;
@@ -45,7 +47,27 @@ void QgsLayoutExporter::renderPage( QPainter *painter, int page )
   renderRegion( painter, paperRect );
 }
 
-void QgsLayoutExporter::renderRegion( QPainter *painter, const QRectF &region )
+QImage QgsLayoutExporter::renderPageToImage( int page, QSize imageSize, double dpi ) const
+{
+  if ( !mLayout )
+    return QImage();
+
+  if ( mLayout->pageCollection()->pageCount() <= page || page < 0 )
+  {
+    return QImage();
+  }
+
+  QgsLayoutItemPage *pageItem = mLayout->pageCollection()->page( page );
+  if ( !pageItem )
+  {
+    return QImage();
+  }
+
+  QRectF paperRect = QRectF( pageItem->pos().x(), pageItem->pos().y(), pageItem->rect().width(), pageItem->rect().height() );
+  return renderRegionToImage( paperRect, imageSize, dpi );
+}
+
+void QgsLayoutExporter::renderRegion( QPainter *painter, const QRectF &region ) const
 {
   QPaintDevice *paintDevice = painter->device();
   if ( !paintDevice || !mLayout )
@@ -66,5 +88,207 @@ void QgsLayoutExporter::renderRegion( QPainter *painter, const QRectF &region )
 #endif
 
   mLayout->context().mIsPreviewRender = true;
+}
+
+QImage QgsLayoutExporter::renderRegionToImage( const QRectF &region, QSize imageSize, double dpi ) const
+{
+  double resolution = mLayout->context().dpi();
+  double oneInchInLayoutUnits = mLayout->convertToLayoutUnits( QgsLayoutMeasurement( 1, QgsUnitTypes::LayoutInches ) );
+  if ( imageSize.isValid() )
+  {
+    //output size in pixels specified, calculate resolution using average of
+    //derived x/y dpi
+    resolution = ( imageSize.width() / region.width()
+                   + imageSize.height() / region.height() ) / 2.0 * oneInchInLayoutUnits;
+  }
+  else if ( dpi > 0 )
+  {
+    //dpi overridden by function parameters
+    resolution = dpi;
+  }
+
+  int width = imageSize.isValid() ? imageSize.width()
+              : static_cast< int >( resolution * region.width() / oneInchInLayoutUnits );
+  int height = imageSize.isValid() ? imageSize.height()
+               : static_cast< int >( resolution * region.height() / oneInchInLayoutUnits );
+
+  QImage image( QSize( width, height ), QImage::Format_ARGB32 );
+  if ( !image.isNull() )
+  {
+    image.setDotsPerMeterX( resolution / 25.4 * 1000 );
+    image.setDotsPerMeterY( resolution / 25.4 * 1000 );
+    image.fill( Qt::transparent );
+    QPainter imagePainter( &image );
+    renderRegion( &imagePainter, region );
+    if ( !imagePainter.isActive() )
+      return QImage();
+  }
+
+  return image;
+}
+
+///@cond PRIVATE
+class LayoutDpiRestorer
+{
+  public:
+
+    LayoutDpiRestorer( QgsLayout *layout )
+      : mLayout( layout )
+      , mPreviousSetting( layout->context().dpi() )
+    {
+    }
+
+    ~LayoutDpiRestorer()
+    {
+      mLayout->context().setDpi( mPreviousSetting );
+    }
+
+  private:
+    QgsLayout *mLayout = nullptr;
+    double mPreviousSetting = 0;
+};
+///@endcond PRIVATE
+QgsLayoutExporter::ExportResult QgsLayoutExporter::exportToImage( const QString &filePath, const QgsLayoutExporter::ImageExportSettings &settings )
+{
+  int worldFilePageNo = -1;
+  if ( mLayout->referenceMap() )
+  {
+    worldFilePageNo = mLayout->referenceMap()->page();
+  }
+
+  QFileInfo fi( filePath );
+  QString path = fi.path();
+  QString baseName = fi.baseName();
+  QString extension = fi.completeSuffix();
+
+  LayoutDpiRestorer dpiRestorer( mLayout );
+  ( void )dpiRestorer;
+  mLayout->context().setDpi( settings.dpi );
+
+  QList< int > pages;
+  if ( settings.pages.empty() )
+  {
+    for ( int page = 0; page < mLayout->pageCollection()->pageCount(); ++page )
+      pages << page;
+  }
+  else
+  {
+    for ( int page : settings.pages )
+    {
+      if ( page >= 0 && page < mLayout->pageCollection()->pageCount() )
+        pages << page;
+    }
+  }
+
+  for ( int page : qgis::as_const( pages ) )
+  {
+    if ( !mLayout->pageCollection()->shouldExportPage( page ) )
+    {
+      continue;
+    }
+
+    bool skip = false;
+    QRectF bounds;
+    QImage image = createImage( settings, page, bounds, skip );
+
+    if ( skip )
+      continue; // should skip this page, e.g. null size
+
+    if ( image.isNull() )
+    {
+      return MemoryError;
+    }
+
+    QString outputFilePath = generateFileName( path, baseName, extension, page );
+    if ( !saveImage( image, outputFilePath, extension ) )
+    {
+      return FileError;
+    }
+
+#if 0 //TODO
+    if ( page == worldFilePageNo )
+    {
+      mLayout->georeferenceOutput( outputFilePath, nullptr, bounds, imageDlg.resolution() );
+
+      if ( settings.generateWorldFile )
+      {
+        // should generate world file for this page
+        double a, b, c, d, e, f;
+        if ( bounds.isValid() )
+          mLayout->computeWorldFileParameters( bounds, a, b, c, d, e, f );
+        else
+          mLayout->computeWorldFileParameters( a, b, c, d, e, f );
+
+        QFileInfo fi( outputFilePath );
+        // build the world file name
+        QString outputSuffix = fi.suffix();
+        QString worldFileName = fi.absolutePath() + '/' + fi.baseName() + '.'
+                                + outputSuffix.at( 0 ) + outputSuffix.at( fi.suffix().size() - 1 ) + 'w';
+
+        writeWorldFile( worldFileName, a, b, c, d, e, f );
+      }
+    }
+#endif
+  }
+  return Success;
+}
+
+QImage QgsLayoutExporter::createImage( const QgsLayoutExporter::ImageExportSettings &settings, int page, QRectF &bounds, bool &skipPage ) const
+{
+  bounds = QRectF();
+  skipPage = false;
+
+  if ( settings.cropToContents )
+  {
+    if ( mLayout->pageCollection()->pageCount() == 1 )
+    {
+      // single page, so include everything
+      bounds = mLayout->layoutBounds( true );
+    }
+    else
+    {
+      // multi page, so just clip to items on current page
+      bounds = mLayout->pageItemBounds( page, true );
+    }
+    if ( bounds.width() <= 0 || bounds.height() <= 0 )
+    {
+      //invalid size, skip page
+      skipPage = true;
+      return QImage();
+    }
+
+    double pixelToLayoutUnits = mLayout->convertToLayoutUnits( QgsLayoutMeasurement( 1, QgsUnitTypes::LayoutPixels ) );
+    bounds = bounds.adjusted( -settings.cropMargins.left() * pixelToLayoutUnits,
+                              -settings.cropMargins.top() * pixelToLayoutUnits,
+                              settings.cropMargins.right() * pixelToLayoutUnits,
+                              settings.cropMargins.bottom() * pixelToLayoutUnits );
+    return renderRegionToImage( bounds, QSize(), settings.dpi );
+  }
+  else
+  {
+    return renderPageToImage( page, settings.imageSize, settings.dpi );
+  }
+}
+
+QString QgsLayoutExporter::generateFileName( const QString &path, const QString &baseName, const QString &suffix, int page ) const
+{
+  if ( page == 0 )
+  {
+    return path + '/' + baseName + '.' + suffix;
+  }
+  else
+  {
+    return path + '/' + baseName + '_' + QString::number( page + 1 ) + '.' + suffix;
+  }
+}
+
+bool QgsLayoutExporter::saveImage( const QImage &image, const QString &imageFilename, const QString &imageFormat )
+{
+  QImageWriter w( imageFilename, imageFormat.toLocal8Bit().constData() );
+  if ( imageFormat.compare( QLatin1String( "tiff" ), Qt::CaseInsensitive ) == 0 || imageFormat.compare( QLatin1String( "tif" ), Qt::CaseInsensitive ) == 0 )
+  {
+    w.setCompression( 1 ); //use LZW compression
+  }
+  return w.write( image );
 }
 
---
+++ tests/src/python/test_qgslayoutexporter.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+"""QGIS Unit tests for QgsLayoutExporter
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+__author__ = 'Nyall Dawson'
+__date__ = '11/12/2017'
+__copyright__ = 'Copyright 2017, The QGIS Project'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import qgis  # NOQA
+import sip
+import tempfile
+import shutil
+import os
+
+from qgis.core import (QgsMultiRenderChecker,
+                       QgsLayoutExporter,
+                       QgsLayout,
+                       QgsProject,
+                       QgsMargins,
+                       QgsLayoutItemShape,
+                       QgsLayoutItemPage,
+                       QgsLayoutPoint,
+                       QgsSimpleFillSymbolLayer,
+                       QgsFillSymbol)
+from qgis.PyQt.QtCore import QSize, QSizeF, QDir, QRectF, Qt
+from qgis.PyQt.QtGui import QImage, QPainter
+
+from qgis.testing import start_app, unittest
+
+start_app()
+
+
+class TestQgsLayoutExporter(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """Run before all tests"""
+        cls.basetestpath = tempfile.mkdtemp()
+        cls.dots_per_meter = 96 / 25.4 * 1000
+
+    def setUp(self):
+        self.report = "<h1>Python QgsLayoutExporter Tests</h1>\n"
+
+    def tearDown(self):
+        report_file_path = "%s/qgistest.html" % QDir.tempPath()
+        with open(report_file_path, 'a') as report_file:
+            report_file.write(self.report)
+
+    def checkImage(self, name, reference_image, rendered_image):
+        checker = QgsMultiRenderChecker()
+        checker.setControlPathPrefix("layout_exporter")
+        checker.setControlName("expected_layoutexporter_" + reference_image)
+        checker.setRenderedImage(rendered_image)
+        checker.setColorTolerance(2)
+        result = checker.runTest(name, 20)
+        self.report += checker.report()
+        print((self.report))
+        return result
+
+    def testRenderPage(self):
+        l = QgsLayout(QgsProject.instance())
+        l.initializeDefaults()
+
+        # add some items
+        item1 = QgsLayoutItemShape(l)
+        item1.attemptSetSceneRect(QRectF(10, 20, 100, 150))
+        fill = QgsSimpleFillSymbolLayer()
+        fill_symbol = QgsFillSymbol()
+        fill_symbol.changeSymbolLayer(0, fill)
+        fill.setColor(Qt.green)
+        fill.setStrokeStyle(Qt.NoPen)
+        item1.setSymbol(fill_symbol)
+        l.addItem(item1)
+
+        # get width/height, create image and render the composition to it
+        size = QSize(1122, 794)
+        output_image = QImage(size, QImage.Format_RGB32)
+
+        output_image.setDotsPerMeterX(self.dots_per_meter)
+        output_image.setDotsPerMeterY(self.dots_per_meter)
+        QgsMultiRenderChecker.drawBackground(output_image)
+        painter = QPainter(output_image)
+        exporter = QgsLayoutExporter(l)
+
+        # valid page
+        exporter.renderPage(painter, 0)
+        painter.end()
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_renderpage.png')
+        output_image.save(rendered_file_path, "PNG")
+        self.assertTrue(self.checkImage('renderpage', 'renderpage', rendered_file_path))
+
+    def testRenderPageToImage(self):
+        l = QgsLayout(QgsProject.instance())
+        l.initializeDefaults()
+
+        # add some items
+        item1 = QgsLayoutItemShape(l)
+        item1.attemptSetSceneRect(QRectF(10, 20, 100, 150))
+        fill = QgsSimpleFillSymbolLayer()
+        fill_symbol = QgsFillSymbol()
+        fill_symbol.changeSymbolLayer(0, fill)
+        fill.setColor(Qt.green)
+        fill.setStrokeStyle(Qt.NoPen)
+        item1.setSymbol(fill_symbol)
+        l.addItem(item1)
+
+        exporter = QgsLayoutExporter(l)
+        size = QSize(1122, 794)
+
+        # bad page numbers
+        image = exporter.renderPageToImage(-1, size)
+        self.assertTrue(image.isNull())
+        image = exporter.renderPageToImage(1, size)
+        self.assertTrue(image.isNull())
+
+        # good page
+        image = exporter.renderPageToImage(0, size)
+        self.assertFalse(image.isNull())
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_rendertoimagepage.png')
+        image.save(rendered_file_path, "PNG")
+        self.assertTrue(self.checkImage('rendertoimagepage', 'rendertoimagepage', rendered_file_path))
+
+    def testRenderRegion(self):
+        l = QgsLayout(QgsProject.instance())
+        l.initializeDefaults()
+
+        # add some items
+        item1 = QgsLayoutItemShape(l)
+        item1.attemptSetSceneRect(QRectF(10, 20, 100, 150))
+        fill = QgsSimpleFillSymbolLayer()
+        fill_symbol = QgsFillSymbol()
+        fill_symbol.changeSymbolLayer(0, fill)
+        fill.setColor(Qt.green)
+        fill.setStrokeStyle(Qt.NoPen)
+        item1.setSymbol(fill_symbol)
+        l.addItem(item1)
+
+        # get width/height, create image and render the composition to it
+        size = QSize(560, 509)
+        output_image = QImage(size, QImage.Format_RGB32)
+
+        output_image.setDotsPerMeterX(self.dots_per_meter)
+        output_image.setDotsPerMeterY(self.dots_per_meter)
+        QgsMultiRenderChecker.drawBackground(output_image)
+        painter = QPainter(output_image)
+        exporter = QgsLayoutExporter(l)
+
+        exporter.renderRegion(painter, QRectF(5, 10, 110, 100))
+        painter.end()
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_renderregion.png')
+        output_image.save(rendered_file_path, "PNG")
+        self.assertTrue(self.checkImage('renderregion', 'renderregion', rendered_file_path))
+
+    def testRenderRegionToImage(self):
+        l = QgsLayout(QgsProject.instance())
+        l.initializeDefaults()
+
+        # add some items
+        item1 = QgsLayoutItemShape(l)
+        item1.attemptSetSceneRect(QRectF(10, 20, 100, 150))
+        fill = QgsSimpleFillSymbolLayer()
+        fill_symbol = QgsFillSymbol()
+        fill_symbol.changeSymbolLayer(0, fill)
+        fill.setColor(Qt.green)
+        fill.setStrokeStyle(Qt.NoPen)
+        item1.setSymbol(fill_symbol)
+        l.addItem(item1)
+
+        exporter = QgsLayoutExporter(l)
+        size = QSize(560, 509)
+
+        image = exporter.renderRegionToImage(QRectF(5, 10, 110, 100), size)
+        self.assertFalse(image.isNull())
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_rendertoimageregionsize.png')
+        image.save(rendered_file_path, "PNG")
+        self.assertTrue(self.checkImage('rendertoimageregionsize', 'rendertoimageregionsize', rendered_file_path))
+
+        # using layout dpi
+        l.context().setDpi(40)
+        image = exporter.renderRegionToImage(QRectF(5, 10, 110, 100))
+        self.assertFalse(image.isNull())
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_rendertoimageregiondpi.png')
+        image.save(rendered_file_path, "PNG")
+        self.assertTrue(self.checkImage('rendertoimageregiondpi', 'rendertoimageregiondpi', rendered_file_path))
+
+        # overridding dpi
+        image = exporter.renderRegionToImage(QRectF(5, 10, 110, 100), QSize(), 80)
+        self.assertFalse(image.isNull())
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_rendertoimageregionoverridedpi.png')
+        image.save(rendered_file_path, "PNG")
+        self.assertTrue(self.checkImage('rendertoimageregionoverridedpi', 'rendertoimageregionoverridedpi', rendered_file_path))
+
+    def testExportToImage(self):
+        l = QgsLayout(QgsProject.instance())
+        l.initializeDefaults()
+
+        # add a second page
+        page2 = QgsLayoutItemPage(l)
+        page2.setPageSize('A5')
+        l.pageCollection().addPage(page2)
+
+        # add some items
+        item1 = QgsLayoutItemShape(l)
+        item1.attemptSetSceneRect(QRectF(10, 20, 100, 150))
+        fill = QgsSimpleFillSymbolLayer()
+        fill_symbol = QgsFillSymbol()
+        fill_symbol.changeSymbolLayer(0, fill)
+        fill.setColor(Qt.green)
+        fill.setStrokeStyle(Qt.NoPen)
+        item1.setSymbol(fill_symbol)
+        l.addItem(item1)
+
+        item2 = QgsLayoutItemShape(l)
+        item2.attemptSetSceneRect(QRectF(10, 20, 100, 150))
+        item2.attemptMove(QgsLayoutPoint(10, 20), page=1)
+        fill = QgsSimpleFillSymbolLayer()
+        fill_symbol = QgsFillSymbol()
+        fill_symbol.changeSymbolLayer(0, fill)
+        fill.setColor(Qt.cyan)
+        fill.setStrokeStyle(Qt.NoPen)
+        item2.setSymbol(fill_symbol)
+        l.addItem(item2)
+
+        exporter = QgsLayoutExporter(l)
+        # setup settings
+        settings = QgsLayoutExporter.ImageExportSettings()
+        settings.dpi = 80
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_exporttoimagedpi.png')
+        self.assertEqual(exporter.exportToImage(rendered_file_path, settings), QgsLayoutExporter.Success)
+
+        self.assertTrue(self.checkImage('exporttoimagedpi_page1', 'exporttoimagedpi_page1', rendered_file_path))
+        page2_path = os.path.join(self.basetestpath, 'test_exporttoimagedpi_2.png')
+        self.assertTrue(self.checkImage('exporttoimagedpi_page2', 'exporttoimagedpi_page2', page2_path))
+
+        # crop to contents
+        settings.cropToContents = True
+        settings.cropMargins = QgsMargins(10, 20, 30, 40)
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_exporttoimagecropped.png')
+        self.assertEqual(exporter.exportToImage(rendered_file_path, settings), QgsLayoutExporter.Success)
+
+        self.assertTrue(self.checkImage('exporttoimagecropped_page1', 'exporttoimagecropped_page1', rendered_file_path))
+        page2_path = os.path.join(self.basetestpath, 'test_exporttoimagecropped_2.png')
+        self.assertTrue(self.checkImage('exporttoimagecropped_page2', 'exporttoimagecropped_page2', page2_path))
+
+        # specific pages
+        settings.cropToContents = False
+        settings.pages = [1]
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_exporttoimagepages.png')
+        self.assertEqual(exporter.exportToImage(rendered_file_path, settings), QgsLayoutExporter.Success)
+
+        self.assertFalse(os.path.exists(rendered_file_path))
+        page2_path = os.path.join(self.basetestpath, 'test_exporttoimagepages_2.png')
+        self.assertTrue(self.checkImage('exporttoimagedpi_page2', 'exporttoimagedpi_page2', page2_path))
+
+        # image size
+        settings.imageSize = QSize(600, 851)
+
+        rendered_file_path = os.path.join(self.basetestpath, 'test_exporttoimagesize.png')
+        self.assertEqual(exporter.exportToImage(rendered_file_path, settings), QgsLayoutExporter.Success)
+        self.assertFalse(os.path.exists(rendered_file_path))
+        page2_path = os.path.join(self.basetestpath, 'test_exporttoimagesize_2.png')
+        self.assertTrue(self.checkImage('exporttoimagesize_page2', 'exporttoimagesize_page2', page2_path))
+
+
+if __name__ == '__main__':
+    unittest.main()
