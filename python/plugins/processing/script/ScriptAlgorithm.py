2674022b6cfbaab6754de3517024bc3ae9599b24
Alexander Bruy <alexander.bruy@gmail.com>
2017-12-14 18:07:01 +0200

    [processing] add group id to scripts

--- python/plugins/processing/script/ScriptAlgorithm.py
+++ python/plugins/processing/script/ScriptAlgorithm.py
@@ -62,6 +62,7 @@ class ScriptAlgorithm(QgsProcessingAlgorithm):
         self._name = ''
         self._display_name = ''
         self._group = ''
+        self._groupId = ''
         self._flags = None
 
         self.script = script
@@ -111,6 +112,7 @@ class ScriptAlgorithm(QgsProcessingAlgorithm):
         self._name = filename[:filename.rfind('.')].replace('_', ' ')
         self._display_name = self._name
         self._group = self.tr('User scripts', 'ScriptAlgorithm')
+        self._groupId = 'userscripts'
         with open(self.descriptionFile) as lines:
             line = lines.readline()
             while line != '':
@@ -130,6 +132,7 @@ class ScriptAlgorithm(QgsProcessingAlgorithm):
         self._name = '[Unnamed algorithm]'
         self._display_name = self.tr('[Unnamed algorithm]', 'ScriptAlgorithm')
         self._group = self.tr('User scripts', 'ScriptAlgorithm')
+        self._groupId = 'userscripts'
         for line in lines:
             if line.startswith('##'):
                 try:
@@ -163,6 +166,8 @@ class ScriptAlgorithm(QgsProcessingAlgorithm):
         desc = self.createDescriptiveName(tokens[0])
         if tokens[1].lower().strip() == 'group':
             self._group = tokens[0]
+            validChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:'
+            self._groupId = ''.join(c for c in tokens[0].lower() if c in validChars)
             return
         if tokens[1].lower().strip() == 'name':
             self._name = self._display_name = tokens[0]
