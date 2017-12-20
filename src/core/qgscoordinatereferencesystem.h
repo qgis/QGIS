51873bd1cddbc49cc8cd6a5eb997961ae7f94200
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-12-19 20:44:34 -0400

    fix code descriptor

--- src/core/qgscoordinatereferencesystem.h
+++ src/core/qgscoordinatereferencesystem.h
@@ -90,7 +90,7 @@ typedef void ( *CUSTOM_CRS_VALIDATION )( QgsCoordinateReferenceSystem & ) SIP_SK
  *
  * This will produce the following output:
  *
- * \code{.py}
+ * \code
  * CRS Description: OSGB 1936 / British National Grid
  * CRS PROJ.4 text: +proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 [output trimmed]
  * \endcode
