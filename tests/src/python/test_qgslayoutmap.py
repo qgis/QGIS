29130054184f32ce81b7710fda8e305bdf162627
Nyall Dawson <nyall.dawson@gmail.com>
2018-01-06 16:02:00 +1000

    Drop some python composer tests

--- tests/src/python/test_qgslayoutmap.py
+++ tests/src/python/test_qgslayoutmap.py
@@ -213,29 +213,6 @@ class TestQgsComposerMap(unittest.TestCase, LayoutItemTestCase):
         self.assertEqual(map.crs().authid(), 'EPSG:4326')
         self.assertFalse(map.presetCrs().isValid())
 
-    def testuniqueId(self):
-        return
-        doc = QDomDocument()
-        documentElement = doc.createElement('ComposerItemClipboard')
-        self.layout.writeXml(documentElement, doc)
-        self.layout.addItemsFromXml(documentElement, doc)
-
-        # test if both composer maps have different ids
-        newMap = QgsComposerMap(self.layout, 0, 0, 10, 10)
-        mapList = self.layout.composerMapItems()
-
-        for mapIt in mapList:
-            if mapIt != self.map:
-                newMap = mapIt
-                break
-
-        oldId = self.map.id()
-        newId = newMap.id()
-
-        self.layout.removeComposerItem(newMap)
-        myMessage = 'old: %s new: %s' % (oldId, newId)
-        assert oldId != newId, myMessage
-
     def testContainsAdvancedEffects(self):
         map_settings = QgsMapSettings()
         map_settings.setLayers([self.vector_layer])
@@ -266,22 +243,6 @@ class TestQgsComposerMap(unittest.TestCase, LayoutItemTestCase):
         self.assertTrue(map.requiresRasterization())
 
         self.vector_layer.setBlendMode(QPainter.CompositionMode_SourceOver)
-
-    def testWorldFileGeneration(self):
-        return
-        myRectangle = QgsRectangle(781662.375, 3339523.125, 793062.375, 3345223.125)
-        self.map.setNewExtent(myRectangle)
-        self.map.setMapRotation(30.0)
-
-        self.layout.setGenerateWorldFile(True)
-        self.layout.setReferenceMap(self.map)
-
-        p = self.layout.computeWorldFileParameters()
-        pexpected = (4.180480199790922, 2.4133064516129026, 779443.7612381146,
-                     2.4136013686911886, -4.179969388427311, 3342408.5663611)
-        ptolerance = (0.001, 0.001, 1, 0.001, 0.001, 1e+03)
-        for i in range(0, 6):
-            assert abs(p[i] - pexpected[i]) < ptolerance[i]
 
 
 if __name__ == '__main__':
