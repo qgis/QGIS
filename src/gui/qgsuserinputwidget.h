df90feff007873dd67e392c43e1dd0755ae4942b
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-01-13 13:57:14 -0400

    add missing since and complete API brea doc

--- src/gui/qgsuserinputwidget.h
+++ src/gui/qgsuserinputwidget.h
@@ -35,6 +35,7 @@ class QFrame;
  * Several widgets can be displayed at once, they will be separated by a separator.
  * Widgets will be either layout horizontally or vertically.
  * The widget is automatically hidden if it contains no widget.
+ * \since QGIS 2.10
  */
 class GUI_EXPORT QgsUserInputWidget : public QgsFloatingWidget
 {
