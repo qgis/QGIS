75e613c3adbe7465af29b3eaef3c7b0d612ace5a
Alessandro Pasotti <elpaso@itopen.it>
2018-01-15 14:26:44 +0100

    [bugfix] Transfer focus to canvas when selecting transparency
    This is an attempt to fix #17845 Setting Raster Transparency
    using "Add values from display" doesn't work
    
    Fixes #17845

--- src/app/qgsrasterlayerproperties.h
+++ src/app/qgsrasterlayerproperties.h
@@ -26,13 +26,13 @@
 #include "qgsguiutils.h"
 #include "qgshelp.h"
 #include "qgsmaplayerstylemanager.h"
+#include "qgsmaptoolemitpoint.h"
 #include "qgis_app.h"
 
 class QgsPointXY;
 class QgsMapLayer;
 class QgsMapCanvas;
 class QgsRasterLayer;
-class QgsMapToolEmitPoint;
 class QgsMetadataWidget;
 class QgsRasterRenderer;
 class QgsRasterRendererWidget;
@@ -91,7 +91,7 @@ class APP_EXPORT QgsRasterLayerProperties : public QgsOptionsDialogBase, private
      */
     //void on_btnResetNull_clicked();
 
-    void pixelSelected( const QgsPointXY & );
+    void pixelSelected( const QgsPointXY &, const Qt::MouseButton & );
 
   private slots:
     void mRenderTypeComboBox_currentIndexChanged( int index );
@@ -206,7 +206,7 @@ class APP_EXPORT QgsRasterLayerProperties : public QgsOptionsDialogBase, private
     qreal mGradientWidth;
 
     QgsMapCanvas *mMapCanvas = nullptr;
-    QgsMapToolEmitPoint *mPixelSelectorTool = nullptr;
+    std::unique_ptr<QgsMapToolEmitPoint> mPixelSelectorTool = nullptr;
 
     QgsRasterHistogramWidget *mHistogramWidget = nullptr;
 
