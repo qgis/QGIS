05c71325182fe07675020929f3f3eae94e4c7512
Nyall Dawson <nyall.dawson@gmail.com>
2018-01-16 11:17:05 +1000

    Fix initial positioning of floating widgets

--- src/gui/qgsfloatingwidget.cpp
+++ src/gui/qgsfloatingwidget.cpp
@@ -85,8 +85,8 @@ void QgsFloatingWidget::setAnchorWidgetPoint( QgsFloatingWidget::AnchorPoint poi
 
 void QgsFloatingWidget::showEvent( QShowEvent *e )
 {
-  onAnchorPointChanged();
   QWidget::showEvent( e );
+  onAnchorPointChanged();
 }
 
 void QgsFloatingWidget::paintEvent( QPaintEvent *e )
@@ -96,6 +96,12 @@ void QgsFloatingWidget::paintEvent( QPaintEvent *e )
   opt.init( this );
   QPainter p( this );
   style()->drawPrimitive( QStyle::PE_Widget, &opt, &p, this );
+}
+
+void QgsFloatingWidget::resizeEvent( QResizeEvent *e )
+{
+  QWidget::resizeEvent( e );
+  onAnchorPointChanged();
 }
 
 void QgsFloatingWidget::onAnchorPointChanged()
