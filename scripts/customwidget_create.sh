377c43a2e67bcbeb8591da77182a0c95d7b87b8e
lbartoletti <l.bartoletti@free.fr>
2018-01-17 21:28:10 +0100

    more bash

--- scripts/customwidget_create.sh
+++ scripts/customwidget_create.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script automatically creates custom widget plugin for a given widget class name.
 # Use customwidget_create.sh QgsColorButton to create QgsColorButtonPlugin files.
