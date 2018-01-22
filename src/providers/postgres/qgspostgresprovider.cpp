3f801b5d164f986e33512f35055346c1b94a76fb
Harrissou Sant-anna <delazj@gmail.com>
2018-01-22 14:12:26 +0100

    Switch SpatiaLite and PostgreSQL place in Datasource
    Manager dialog
    Moves up SpatiaLite tab near GeoPackage's as they are both simple file databases and their dialog look alike.

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -4901,7 +4901,7 @@ class QgsPostgresSourceSelectProvider : public QgsSourceSelectProvider  //#spell
 
     QString providerKey() const override { return QStringLiteral( "postgres" ); }
     QString text() const override { return QObject::tr( "PostgreSQL" ); }
-    int ordering() const override { return QgsSourceSelectProvider::OrderDatabaseProvider + 10; }
+    int ordering() const override { return QgsSourceSelectProvider::OrderDatabaseProvider + 20; }
     QIcon icon() const override { return QgsApplication::getThemeIcon( QStringLiteral( "/mActionAddPostgisLayer.svg" ) ); }
     QgsAbstractDataSourceWidget *createDataSourceWidget( QWidget *parent = nullptr, Qt::WindowFlags fl = Qt::Widget, QgsProviderRegistry::WidgetMode widgetMode = QgsProviderRegistry::WidgetMode::Embedded ) const override
     {
--- src/providers/spatialite/qgsspatialiteprovider.cpp
+++ src/providers/spatialite/qgsspatialiteprovider.cpp
@@ -5936,7 +5936,7 @@ class QgsSpatialiteSourceSelectProvider : public QgsSourceSelectProvider
 
     QString providerKey() const override { return QStringLiteral( "spatialite" ); }
     QString text() const override { return QObject::tr( "SpatiaLite" ); }
-    int ordering() const override { return QgsSourceSelectProvider::OrderDatabaseProvider + 20; }
+    int ordering() const override { return QgsSourceSelectProvider::OrderDatabaseProvider + 10; }
     QIcon icon() const override { return QgsApplication::getThemeIcon( QStringLiteral( "/mActionAddSpatiaLiteLayer.svg" ) ); }
     QgsAbstractDataSourceWidget *createDataSourceWidget( QWidget *parent = nullptr, Qt::WindowFlags fl = Qt::Widget, QgsProviderRegistry::WidgetMode widgetMode = QgsProviderRegistry::WidgetMode::Embedded ) const override
     {
