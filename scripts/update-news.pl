df76bfc1a38ac95470d76263bdbd3ac9084c8d47
Juergen E. Fischer <jef@norbit.de>
2018-02-05 02:29:11 +0100

    update INSTALL on release

--- scripts/update-news.pl
+++ scripts/update-news.pl
@@ -59,8 +59,8 @@ close $in;
 
 copy($tempfile, "doc/news.t2t");
 
-system "txt2tags -odoc/news.html -t html doc/news.t2t";
-system "txt2tags -oNEWS -t txt doc/news.t2t";
+system "txt2tags --encoding=utf-8 -odoc/news.html -t html doc/news.t2t";
+system "txt2tags --encoding=utf-8 -oNEWS -t txt doc/news.t2t";
 
 =head1 NAME
 
