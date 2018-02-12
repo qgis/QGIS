03e49f1f6502554b4a65173eeaebffed814c6f75
Alessandro Pasotti <elpaso@itopen.it>
2018-02-12 15:08:34 +0100

    Revert "Try to fix QgsCollapsibleGroupBox crash on Qt 5.10"

--- src/gui/qgscollapsiblegroupbox.h
+++ src/gui/qgscollapsiblegroupbox.h
@@ -157,7 +157,7 @@ class GUI_EXPORT QgsCollapsibleGroupBoxBasic : public QGroupBox
     bool mScrollOnExpand;
     bool mShown;
     QScrollArea *mParentScrollArea = nullptr;
-    std::unique_ptr<QgsGroupBoxCollapseButton> mCollapseButton;
+    QgsGroupBoxCollapseButton *mCollapseButton = nullptr;
     QWidget *mSyncParent = nullptr;
     QString mSyncGroup;
     bool mAltDown;
