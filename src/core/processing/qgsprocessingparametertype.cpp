0b5ccf07096d1b82eaeccf6b725374e94b2cf042
Matthias Kuhn <matthias@opengis.ch>
2018-03-05 22:39:20 -0500

    License headers

--- src/core/processing/qgsprocessingparametertype.cpp
+++ src/core/processing/qgsprocessingparametertype.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgsprocessingparametertype.cpp
+                         ------------------------
+    begin                : March 2018
+    copyright            : (C) 2018 by Matthias Kuhn
+    email                : matthias@opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsprocessingparametertype.h"
 
 bool QgsProcessingParameterType::exposeToModeler() const
