007903494582b1e71273e7b801b928d32eb2f330
Alexander Bruy <alexander.bruy@gmail.com>
2018-03-09 14:57:37 +0200

    [processing] replace QCoreApplication.translate() with tr() in ParametersPanel class

--- python/plugins/processing/gui/ParametersPanel.py
+++ python/plugins/processing/gui/ParametersPanel.py
@@ -91,7 +91,7 @@ class ParametersPanel(BASE, WIDGET):
     def formatParameterTooltip(self, parameter):
         return '<p><b>{}</b></p><p>{}</p>'.format(
             parameter.description(),
-            QCoreApplication.translate('ParametersPanel', 'Python identifier: ‘{}’').format('<i>{}</i>'.format(parameter.name()))
+            self.tr('Python identifier: ‘{}’').format('<i>{}</i>'.format(parameter.name()))
         )
 
     def initWidgets(self):
