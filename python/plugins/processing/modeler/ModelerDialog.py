ad4d0a17d5e5f3a69933077876ec1f75fe6b5d4d
Juergen E. Fischer <jef@norbit.de>
2018-03-09 17:18:37 +0100

    processing: make error messages moved to message bar translatable again (followup 6e2288dcc)

--- python/plugins/processing/modeler/ModelerDialog.py
+++ python/plugins/processing/modeler/ModelerDialog.py
@@ -328,7 +328,7 @@ class ModelerDialog(BASE, WIDGET):
 
     def runModel(self):
         if len(self.model.childAlgorithms()) == 0:
-            self.bar.pushMessage("", "Model doesn't contain any algorithm and/or parameter and can't be executed", level=Qgis.Warning, duration=5)
+            self.bar.pushMessage("", self.tr("Model doesn't contain any algorithm and/or parameter and can't be executed"), level=Qgis.Warning, duration=5)
             return
 
         dlg = AlgorithmDialog(self.model)
@@ -399,7 +399,7 @@ class ModelerDialog(BASE, WIDGET):
 
         img.save(filename)
 
-        self.bar.pushMessage("", "Model was correctly exported as image", level=Qgis.Success, duration=5)
+        self.bar.pushMessage("", self.tr("Model was correctly exported as image"), level=Qgis.Success, duration=5)
         self.repaintModel(controls=True)
 
     def exportAsPdf(self):
@@ -427,7 +427,7 @@ class ModelerDialog(BASE, WIDGET):
         self.scene.render(painter, printerRect, totalRect)
         painter.end()
 
-        self.bar.pushMessage("", "Model was correctly exported as PDF", level=Qgis.Success, duration=5)
+        self.bar.pushMessage("", self.tr("Model was correctly exported as PDF"), level=Qgis.Success, duration=5)
         self.repaintModel(controls=True)
 
     def exportAsSvg(self):
@@ -455,7 +455,7 @@ class ModelerDialog(BASE, WIDGET):
         self.scene.render(painter, svgRect, totalRect)
         painter.end()
 
-        self.bar.pushMessage("", "Model was correctly exported as SVG", level=Qgis.Success, duration=5)
+        self.bar.pushMessage("", self.tr("Model was correctly exported as SVG"), level=Qgis.Success, duration=5)
         self.repaintModel(controls=True)
 
     def exportAsPython(self):
@@ -472,7 +472,7 @@ class ModelerDialog(BASE, WIDGET):
         with codecs.open(filename, 'w', encoding='utf-8') as fout:
             fout.write(text)
 
-        self.bar.pushMessage("", "Model was correctly exported as python script", level=Qgis.Success, duration=5)
+        self.bar.pushMessage("", self.tr("Model was correctly exported as python script"), level=Qgis.Success, duration=5)
 
     def saveModel(self, saveAs):
         if str(self.textGroup.text()).strip() == '' \
@@ -506,7 +506,7 @@ class ModelerDialog(BASE, WIDGET):
                     )
                 return
             self.update_model.emit()
-            self.bar.pushMessage("", "Model was correctly saved", level=Qgis.Success, duration=5)
+            self.bar.pushMessage("", self.tr("Model was correctly saved"), level=Qgis.Success, duration=5)
 
             self.hasChanged = False
 
