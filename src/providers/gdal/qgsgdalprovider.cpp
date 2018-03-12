f95d616d6ad3fcdd2c8c9e80958d923431e55999
Nyall Dawson <nyall.dawson@gmail.com>
2018-03-12 14:43:57 +1000

    Correctly show ers files in browser
    Workaround missing metadata in gdal driver

--- src/providers/gdal/qgsgdalprovider.cpp
+++ src/providers/gdal/qgsgdalprovider.cpp
@@ -2251,7 +2251,6 @@ void buildSupportedRasterFileFilterAndExtensions( QString &fileFiltersString, QS
       }
 
       fileFiltersString += createFileFilter_( myGdalDriverLongName, glob );
-
     }
 
 
@@ -2275,6 +2274,11 @@ void buildSupportedRasterFileFilterAndExtensions( QString &fileFiltersString, QS
         // Fixed in GDAL 2.3
         fileFiltersString += createFileFilter_( myGdalDriverLongName, QStringLiteral( "*.bil" ) );
         extensions << QStringLiteral( "bil" );
+      }
+      else if ( myGdalDriverDescription == QLatin1String( "ERS" ) )
+      {
+        fileFiltersString += createFileFilter_( myGdalDriverLongName, QStringLiteral( "*.ers" ) );
+        extensions << QStringLiteral( "ers" );
       }
       else if ( myGdalDriverDescription.startsWith( QLatin1String( "AIG" ) ) )
       {
