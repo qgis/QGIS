50232884c19c2c01842f0aff8b78621b1c2ce8f9
Nyall Dawson <nyall.dawson@gmail.com>
2018-03-21 07:13:05 +1000

    [processing] Don't try to load algorithms for disabled providers
    Fixes #18488
    
    (cherry-picked from 9ab4d963)

--- tests/src/analysis/testqgsprocessing.cpp
+++ tests/src/analysis/testqgsprocessing.cpp
@@ -322,6 +322,12 @@ class DummyProvider : public QgsProcessingProvider
       return supportsNonFileOutputs;
     }
 
+    bool isActive() const override
+    {
+      return active;
+    }
+
+    bool active = true;
     bool *unloaded = nullptr;
     bool supportsNonFileOutputs = false;
 
@@ -971,6 +977,16 @@ void TestQgsProcessing::algorithm()
     p->refreshAlgorithms();
     QCOMPARE( providerRefreshed.count(), 2 + i );
   }
+
+  // inactive provider, should not load algorithms
+  p->active = false;
+  p->refreshAlgorithms();
+  QCOMPARE( providerRefreshed.count(), 3 );
+  QVERIFY( p->algorithms().empty() );
+  p->active = true;
+  p->refreshAlgorithms();
+  QCOMPARE( providerRefreshed.count(), 4 );
+  QVERIFY( !p->algorithms().empty() );
 
   QgsProcessingRegistry r;
   r.addProvider( p );
