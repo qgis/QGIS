3a5e96a081fd320873daf47f4d1f8cc32594aef2
Matthias Kuhn <matthias@opengis.ch>
2018-04-05 23:40:00 +0200

    Doxygen and license headers

--- src/core/processing/qgsprocessingalgorithmconfigurationwidget.h
+++ src/core/processing/qgsprocessingalgorithmconfigurationwidget.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+                         qgsprocessingalgorithmconfig.h
+                         --------------------------
+    begin                : April 2018
+    copyright            : (C) 2018 by Matthias Kuhn
+    email                : matthias@opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifndef QGSPROCESSINGALGORITHMCONFIGURATIONWIDGET_H
 #define QGSPROCESSINGALGORITHMCONFIGURATIONWIDGET_H
 
@@ -7,14 +25,32 @@
 
 #include "qgis_core.h"
 
+/**
+ * A configuration widget for processing algorithms allows to provide additional
+ * configuration options directly on algorithm level, in addition to parameters.
+ *
+ * \since QGIS 3.2
+ * \ingroup core
+ */
 class CORE_EXPORT QgsProcessingAlgorithmConfigurationWidget : public QWidget
 {
     Q_OBJECT
 
   public:
+    /**
+     * Creates a new QgsProcessingAlgorithmConfigurationWidget
+     */
     QgsProcessingAlgorithmConfigurationWidget( QWidget *parent = nullptr );
     virtual ~QgsProcessingAlgorithmConfigurationWidget() = default;
+
+    /**
+     * Read the current configuration from this widget.
+     */
     virtual QVariantMap configuration() const = 0;
+
+    /**
+     * Set the configuration which this widget should represent.
+     */
     virtual void setConfiguration( const QVariantMap &configuration ) = 0;
 };
 
