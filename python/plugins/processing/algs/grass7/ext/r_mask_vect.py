0d851e95eafd1c7e80216c460d7c869f133362ae
Nyall Dawson <nyall.dawson@gmail.com>
2018-04-08 09:26:44 +1000

    [processing][grass] Add some missing imports
    (cherry-picked from f928e3dd)

--- python/plugins/processing/algs/grass7/ext/r_mask_vect.py
+++ python/plugins/processing/algs/grass7/ext/r_mask_vect.py
@@ -25,6 +25,8 @@ __copyright__ = '(C) 2016, Médéric Ribreux'
 
 __revision__ = '$Format:%H$'
 
+from processing.algs.grass7.Grass7Utils import Grass7Utils
+
 
 def processCommand(alg, parameters, context, feedback):
     # Remove input
