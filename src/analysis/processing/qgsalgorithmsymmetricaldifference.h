5aa02582c618afe78393ac7ebbcc2ff643ab0bb2
Martin Dobias <wonder.sk@gmail.com>
2018-04-24 20:37:53 +0200

    Port Difference & Sym.Diff. to C++, cleanups and tests

---
+++ src/analysis/processing/qgsalgorithmsymmetricaldifference.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+  qgsalgorithmsymmetricaldifference.h
+  ---------------------
+  Date                 : April 2018
+  Copyright            : (C) 2018 by Martin Dobias
+  Email                : wonder dot sk at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSALGORITHMSYMMETRICALDIFFERENCE_H
+#define QGSALGORITHMSYMMETRICALDIFFERENCE_H
+
+#define SIP_NO_FILE
+
+#include "qgsprocessingalgorithm.h"
+
+///@cond PRIVATE
+
+class QgsSymmetricalDifferenceAlgorithm : public QgsProcessingAlgorithm
+{
+  public:
+    QgsSymmetricalDifferenceAlgorithm() = default;
+
+    virtual QString name() const override;
+    virtual QString displayName() const override;
+    virtual QString group() const override;
+    virtual QString groupId() const override;
+    QString shortHelpString() const override;
+
+  protected:
+    virtual QgsProcessingAlgorithm *createInstance() const override;
+    virtual void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) override;
+    virtual QVariantMap processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+
+};
+
+///@endcond PRIVATE
+
+#endif // QGSALGORITHMSYMMETRICALDIFFERENCE_H
