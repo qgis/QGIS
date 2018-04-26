806545b0f820f6afa8e96621a69b8636074e9e31
Matthias Kuhn <matthias@opengis.ch>
2018-04-26 11:40:29 +0200

    Plural disambiguation

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -7327,7 +7327,7 @@ void QgisApp::deleteSelected( QgsMapLayer *layer, QWidget *parent, bool checkFea
   }
 
   //validate selection
-  int numberOfSelectedFeatures = vlayer->selectedFeatureIds().size();
+  const int numberOfSelectedFeatures = vlayer->selectedFeatureCount();
   if ( numberOfSelectedFeatures == 0 )
   {
     messageBar()->pushMessage( tr( "No Features Selected" ),
@@ -7355,7 +7355,7 @@ void QgisApp::deleteSelected( QgsMapLayer *layer, QWidget *parent, bool checkFea
     if ( !allFeaturesInView )
     {
       // for extra safety to make sure we are not removing geometries by accident
-      int res = QMessageBox::warning( mMapCanvas, tr( "Delete %1 features on layer \"%2\"" ).arg( QString::number( vlayer->selectedFeatureCount() ), vlayer->name() ),
+      int res = QMessageBox::warning( mMapCanvas, tr( "Delete %n feature(s) on layer \"%1\"", nullptr, numberOfSelectedFeatures ).arg( vlayer->name() ),
                                       tr( "Some of the selected features are outside of the current map view. Would you still like to continue?" ),
                                       QMessageBox::Yes | QMessageBox::No );
       if ( res != QMessageBox::Yes )
@@ -7368,7 +7368,7 @@ void QgisApp::deleteSelected( QgsMapLayer *layer, QWidget *parent, bool checkFea
   if ( !vlayer->deleteSelectedFeatures( &deletedCount ) )
   {
     messageBar()->pushMessage( tr( "Problem deleting features" ),
-                               tr( "A problem occurred during deletion of %1 feature(s)" ).arg( numberOfSelectedFeatures - deletedCount ),
+                               tr( "A problem occurred during deletion on layer \"%1\". %n feature(s) not deleted.", nullptr, numberOfSelectedFeatures - deletedCount ).arg( vlayer->name() ),
                                Qgis::Warning );
   }
   else
