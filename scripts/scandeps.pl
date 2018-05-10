6b67708166852ad3bc0eb454179662480a8742f5
Juergen E. Fischer <jef@norbit.de>
2018-05-11 01:43:43 +0200

    add libyaml-tiny-perl & python-autopep8 to debian deps

--- scripts/scandeps.pl
+++ scripts/scandeps.pl
@@ -84,7 +84,7 @@ for my $dist (@dists) {
 
 	system("git checkout debian/control" )==0 or die "git checkout failed: $!";
 
-	$deps .= ",cmake-curses-gui,ccache,expect,qt5-default";
+	$deps .= ",cmake-curses-gui,ccache,expect,qt5-default,libyaml-tiny-perl,python-autopep8";
 
 	my @deps;
 	my %deps;
