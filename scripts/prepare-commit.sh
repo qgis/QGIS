395e864b6808467371c49ae907bd06d79dbe0ae8
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-05-14 20:56:53 -0400

    fix prepare-commit after moving sip files

--- scripts/prepare-commit.sh
+++ scripts/prepare-commit.sh
@@ -113,11 +113,11 @@ for f in $MODIFIED; do
   # if cpp header
   if [[ $f =~ ^src\/(core|gui|analysis|server)\/.*\.h$ ]]; then
     # look if corresponding SIP file
-    sip_include=$(${GP}sed -r 's/^src\/(\w+)\/.*$/python\/\1\/\1.sip/' <<< $f )
-    sip_file=$(${GP}sed -r 's/^src\/(core|gui|analysis|server)\///; s/\.h$/.sip/' <<<$f )
-    module=$(${GP}sed -r 's/^src\/(core|gui|analysis|server)\/.*$/\1/' <<<$f )
+    sip_include=$(${GP}sed -r 's@^src/(\w+)/.*$@python/\1/\1.sip@' <<< $f )
+    sip_file=$(${GP}sed -r 's@^src/(core|gui|analysis|server)/@@; s@\.h$@.sip@' <<<$f )
+    module=$(${GP}sed -r 's@src/(core|gui|analysis|server)/.*$@\1@' <<<$f )
     if grep -Fq "$sip_file" ${TOPLEVEL}/python/${module}/${module}_auto.sip; then
-      sip_file=$(${GP}sed -r 's/^src\///; s/\.h$/.sip.in/' <<<$f )
+      sip_file=$(${GP}sed -r 's@^src/(core|gui|analysis|server)@\1/auto_generated@; s@\.h$@.sip.in@' <<<$f )
       m=python/$sip_file.$REV.prepare
       touch python/$sip_file
       cp python/$sip_file $m
