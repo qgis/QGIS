f267adc37ad4431b24befaedd287189560b3ac75
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-05-17 22:54:39 -0400

    run sipify

--- python/gui/__init__.py
+++ python/gui/__init__.py
@@ -25,3 +25,17 @@ __revision__ = '$Format:%H$'
 
 from qgis.PyQt import QtCore
 from qgis._gui import *
+
+# -----------------
+# DO NOT EDIT BELOW
+# These are automatically added by calling sipify.pl script
+QgsAuthSettingsWidget.WarningType.parentClass = lambda: QgsAuthSettingsWidget
+QgsColorButton.Behavior.parentClass = lambda: QgsColorButton
+QgsColorTextWidget.ColorTextFormat.parentClass = lambda: QgsColorTextWidget
+QgsFilterLineEdit.ClearMode.parentClass = lambda: QgsFilterLineEdit
+QgsFloatingWidget.AnchorPoint.parentClass = lambda: QgsFloatingWidget
+QgsFontButton.Mode.parentClass = lambda: QgsFontButton
+QgsMapToolIdentify.IdentifyMode.parentClass = lambda: QgsMapToolIdentify
+QgsAttributeTableFilterModel.FilterMode.parentClass = lambda: QgsAttributeTableFilterModel
+QgsAttributeTableFilterModel.ColumnType.parentClass = lambda: QgsAttributeTableFilterModel
+QgsDualView.ViewMode.parentClass = lambda: QgsDualView
