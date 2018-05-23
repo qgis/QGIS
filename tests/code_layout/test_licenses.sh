d16ec3e2571e83be1976f0b8774c509b58d3d309
Nyall Dawson <nyall.dawson@gmail.com>
2018-05-23 12:49:08 +1000

    Add unit test for license headers

---
+++ tests/code_layout/test_licenses.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This test checks that all source files correctly have license headers
+
+INCLUDE_EXTENSIONS="h|cpp|hpp|py|c"
+EXCLUDE_LIST="(.*\/(qtermwidget)\/|ui_defaults\\.h|CREDITS|TODO|README|URI|^[^.]*$|.*\\.(?!($INCLUDE_EXTENSIONS)$))"
+
+DIR=$(git rev-parse --show-toplevel)
+
+pushd ${DIR} > /dev/null
+missing=$(! { licensecheck -r -i "$EXCLUDE_LIST" src & licensecheck -r -i "$EXCLUDE_LIST" python; } | grep UNKNOWN)
+
+popd > /dev/null
+
+if [[ $missing ]]; then
+  echo " *** Found source files without valid license headers"
+  echo "$missing"
+  exit 1
+fi
+
