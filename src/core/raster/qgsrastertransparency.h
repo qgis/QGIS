b9756275f88f84bac7745df8866ff3930e2fa06d
Nyall Dawson <nyall.dawson@gmail.com>
2018-05-26 14:25:21 +1000

    Add 'mutator for ' to doxygen compliance blacklist

--- src/core/raster/qgsrastertransparency.h
+++ src/core/raster/qgsrastertransparency.h
@@ -76,11 +76,15 @@ class CORE_EXPORT QgsRasterTransparency
      */
     void initializeTransparentPixelList( double redValue, double greenValue, double blueValue );
 
-    //! \brief Mutator for transparentSingleValuePixelList, replaces the whole list
-    void setTransparentSingleValuePixelList( const QList<QgsRasterTransparency::TransparentSingleValuePixel> &newList SIP_TRANSFER );
+    /**
+     * Sets the transparent single value pixel list, replacing the whole existing list.
+     */
+    void setTransparentSingleValuePixelList( const QList<QgsRasterTransparency::TransparentSingleValuePixel> &newList );
 
-    //! \brief Mutator for transparentThreeValuePixelList, replaces the whole list
-    void setTransparentThreeValuePixelList( const QList<QgsRasterTransparency::TransparentThreeValuePixel> &newList SIP_TRANSFER );
+    /**
+     * Sets the transparent three value pixel list, replacing the whole existing list.
+     */
+    void setTransparentThreeValuePixelList( const QList<QgsRasterTransparency::TransparentThreeValuePixel> &newList );
 
     /**
      * Returns the transparency value for a single \a value pixel.
