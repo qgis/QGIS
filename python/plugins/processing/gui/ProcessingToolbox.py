ad394acd476db8b4d3cacf78bec626e7d7e6c941
rldhont <rldhont@gmail.com>
2018-05-30 08:28:17 +0200

    [BUGFIX][Processing] Infinite loop in refresh algorithms tree
    When an algorithm provider is refresh, all algorithm providers are reloaded.
    But after each reload,the providerReloaded event is emitted and the provider
    algorithms tree is refreshed, and all the algorithm providers are reloaded,
    and the infinite loop started.
    
    It fixed #18877 *Maximum recursion depth exceeded after adding a model or
    script to the Processing algorithm list (since 2.18.18!)* by removing
    `updateAlgsList` when refresh provider algorithms tree item.

--- python/plugins/processing/gui/ProcessingToolbox.py
+++ python/plugins/processing/gui/ProcessingToolbox.py
@@ -374,7 +374,6 @@ class TreeProviderItem(QTreeWidgetItem):
 
     def refresh(self):
         self.takeChildren()
-        Processing.updateAlgsList()
         self.populate()
 
     def populate(self):
