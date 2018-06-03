48b117e0db02fc6921eb4dfc54370eb28f2c6840
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-04 08:39:20 +1000

    Fail check if licensecheck missing

--- tests/code_layout/test_licenses.sh
+++ tests/code_layout/test_licenses.sh
@@ -5,6 +5,15 @@
 INCLUDE_EXTENSIONS="h|cpp|hpp|py|c"
 EXCLUDE_LIST="(.*\/(qtermwidget)\/|ui_defaults\\.h|CREDITS|TODO|README|URI|^[^.]*$|.*\\.(?!($INCLUDE_EXTENSIONS)$))"
 
+# check for existance of licensecheck first
+has_licensecheck=$( licensecheck )
+if licensecheck ; then
+  echo "licensecheck installed!"
+else
+  echo "licensecheck not installed!"
+  exit 1
+fi
+
 DIR=$(git rev-parse --show-toplevel)
 
 pushd ${DIR} > /dev/null
