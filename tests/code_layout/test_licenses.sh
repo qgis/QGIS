06e51b2b60fdd03ea42bf688b35cf6b50845117f
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-04 08:54:42 +1000

    Include licensecheck.pl script in code_layout test folder
    Seems to be the only way to get this functional under Travis.
    
    TODO: remove when Travis is upgraded to a reasonably modern
    Ubuntu version

--- tests/code_layout/test_licenses.sh
+++ tests/code_layout/test_licenses.sh
@@ -5,19 +5,10 @@
 INCLUDE_EXTENSIONS="h|cpp|hpp|py|c"
 EXCLUDE_LIST="(.*\/(qtermwidget)\/|ui_defaults\\.h|CREDITS|TODO|README|URI|^[^.]*$|.*\\.(?!($INCLUDE_EXTENSIONS)$))"
 
-# check for existance of licensecheck first
-has_licensecheck=$( licensecheck )
-if licensecheck ; then
-  echo "licensecheck installed!"
-else
-  echo "licensecheck not installed!"
-  exit 1
-fi
-
 DIR=$(git rev-parse --show-toplevel)
 
 pushd ${DIR} > /dev/null
-missing=$(! { licensecheck -r -i "$EXCLUDE_LIST" src & licensecheck -r -i "$EXCLUDE_LIST" python; } | grep UNKNOWN)
+missing=$(! { tests/code_layout/licensecheck.pl -r -i "$EXCLUDE_LIST" src & licensecheck -r -i "$EXCLUDE_LIST" python; } | grep UNKNOWN)
 
 popd > /dev/null
 
