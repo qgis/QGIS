1316dcb21cb9a64d7da9004227542318968caaea
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-04 09:36:05 +1000

    Add missing license headers

--- python/core/additions/qgsfeature.py
+++ python/core/additions/qgsfeature.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    qgsfeature.py
+    ---------------------
+    Date                 : May 2018
+    Copyright            : (C) 2018 by Denis Rouzaud
+    Email                : denis@opengis.ch
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+
 def mapping_feature(feature):
     geom = feature.geometry()
     fields = [field.name() for field in feature.fields()]
--- src/providers/postgres/qgspostgresprojectstoragedialog.cpp
+++ src/providers/postgres/qgspostgresprojectstoragedialog.cpp
@@ -1,3 +1,17 @@
+/***************************************************************************
+    qgspostgresprojectstoragedialog.cpp
+    ---------------------
+    begin                : April 2018
+    copyright            : (C) 2018 by Martin Dobias
+    email                : wonder dot sk at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "qgspostgresprojectstoragedialog.h"
 
 #include "qgspostgresconn.h"
