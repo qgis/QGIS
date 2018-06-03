9651ed5a1585e3c457fc881523f4f08e41c77f9b
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-04 09:09:54 +1000

    Move licensecheck to external folder

--- tests/code_layout/test_licenses.sh
+++ tests/code_layout/test_licenses.sh
@@ -3,14 +3,14 @@
 # This test checks that all source files correctly have license headers
 
 INCLUDE_EXTENSIONS="h|cpp|hpp|py|c"
-EXCLUDE_LIST="(.*\/(qtermwidget)\/|ui_defaults\\.h|CREDITS|TODO|README|URI|^[^.]*$|.*\\.(?!($INCLUDE_EXTENSIONS)$))"
+EXCLUDE_LIST="(.*\\/(qtermwidget)\\/|ui_defaults\\.h|CREDITS|TODO|README|URI|^[^.]*$|.*\\.(?!($INCLUDE_EXTENSIONS)$))"
 
 DIR=$(git rev-parse --show-toplevel)
 
-pushd ${DIR} > /dev/null
-missing=$(! { tests/code_layout/licensecheck.pl -r -i "$EXCLUDE_LIST" src & licensecheck -r -i "$EXCLUDE_LIST" python; } | grep UNKNOWN)
+pushd "${DIR}" || exit > /dev/null
+missing=$(! { external/licensecheck/licensecheck.pl -r -i "$EXCLUDE_LIST" src & licensecheck -r -i "$EXCLUDE_LIST" python; } | grep UNKNOWN)
 
-popd > /dev/null
+popd || exit > /dev/null
 
 if [[ $missing ]]; then
   echo " *** Found source files without valid license headers"
