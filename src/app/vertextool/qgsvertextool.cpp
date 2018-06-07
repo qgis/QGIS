e46f7894569255a1302ae4f5f8976954b66d6fec
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-06-07 13:49:23 -0400

    [vertex tool] create the editor with an initial selection

--- src/app/vertextool/qgsvertextool.cpp
+++ src/app/vertextool/qgsvertextool.cpp
@@ -1036,6 +1036,13 @@ void QgsVertexTool::showVertexEditor()  //#spellok
     return;
 
   mSelectedFeature.reset( new QgsSelectedFeature( m.featureId(), m.layer(), mCanvas ) );
+  for ( int i = 0; i < mSelectedVertices.length(); ++i )
+  {
+    if ( mSelectedVertices.at( i ).layer == m.layer() && mSelectedVertices.at( i ).fid == m.featureId() )
+    {
+      mSelectedFeature->selectVertex( mSelectedVertices.at( i ).vertexId );
+    }
+  }
   if ( !mVertexEditor )
   {
     mVertexEditor.reset( new QgsVertexEditor( m.layer(), mSelectedFeature.get(), mCanvas ) );
