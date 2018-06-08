437a2aa27aa1f246d611a57574d041e8a949bf48
nirvn <nirvn.asia@gmail.com>
2018-06-08 11:33:12 +0700

    [processing] share code between zonal histrogram and zonal stats algs

--- src/analysis/vector/qgszonalstatistics.cpp
+++ src/analysis/vector/qgszonalstatistics.cpp
@@ -16,16 +16,16 @@
  ***************************************************************************/
 
 #include "qgszonalstatistics.h"
+
 #include "qgsfeatureiterator.h"
 #include "qgsfeedback.h"
 #include "qgsgeometry.h"
 #include "qgsvectordataprovider.h"
 #include "qgsvectorlayer.h"
+#include "processing/qgsrasteranalysisutils.h"
 #include "qgsrasterdataprovider.h"
 #include "qgsrasterlayer.h"
-#include "qgsrasterblock.h"
 #include "qgslogger.h"
-#include "qgsgeos.h"
 #include "qgsproject.h"
 
 #include <QFile>
@@ -252,16 +252,18 @@ int QgsZonalStatistics::calculateStatistics( QgsFeedback *feedback )
 
     int nCellsX, nCellsY;
     QgsRectangle rasterBlockExtent;
-    cellInfoForBBox( rasterBBox, featureRect, mCellSizeX, mCellSizeY, nCellsX, nCellsY, nCellsXProvider, nCellsYProvider, rasterBlockExtent );
+    QgsRasterAnalysisUtils::cellInfoForBBox( rasterBBox, featureRect, mCellSizeX, mCellSizeY, nCellsX, nCellsY, nCellsXProvider, nCellsYProvider, rasterBlockExtent );
 
-    statisticsFromMiddlePointTest( featureGeometry, nCellsX, nCellsY, mCellSizeX, mCellSizeY,
-                                   rasterBlockExtent, featureStats );
+    featureStats.reset();
+    QgsRasterAnalysisUtils::statisticsFromMiddlePointTest( mRasterInterface, mRasterBand, featureGeometry, nCellsX, nCellsY, mCellSizeX, mCellSizeY,
+    rasterBlockExtent, [ &featureStats ]( double value ) { featureStats.addValue( value ); } );
 
     if ( featureStats.count <= 1 )
     {
       //the cell resolution is probably larger than the polygon area. We switch to precise pixel - polygon intersection in this case
-      statisticsFromPreciseIntersection( featureGeometry, nCellsX, nCellsY, mCellSizeX, mCellSizeY,
-                                         rasterBlockExtent, featureStats );
+      featureStats.reset();
+      QgsRasterAnalysisUtils::statisticsFromPreciseIntersection( mRasterInterface, mRasterBand, featureGeometry, nCellsX, nCellsY, mCellSizeX, mCellSizeY,
+      rasterBlockExtent, [ &featureStats ]( double value, double weight ) { featureStats.addValue( value, weight ); } );
     }
 
     //write the statistics value to the vector data provider
@@ -352,160 +354,6 @@ int QgsZonalStatistics::calculateStatistics( QgsFeedback *feedback )
   }
 
   return 0;
-}
-
-void QgsZonalStatistics::cellInfoForBBox( const QgsRectangle &rasterBBox, const QgsRectangle &featureBBox, double cellSizeX, double cellSizeY,
-    int &nCellsX, int &nCellsY, int rasterWidth, int rasterHeight, QgsRectangle &rasterBlockExtent ) const
-{
-  //get intersecting bbox
-  QgsRectangle intersectBox = rasterBBox.intersect( &featureBBox );
-  if ( intersectBox.isEmpty() )
-  {
-    nCellsX = 0;
-    nCellsY = 0;
-    rasterBlockExtent = QgsRectangle();
-    return;
-  }
-
-  //get offset in pixels in x- and y- direction
-  int offsetX = static_cast< int >( std::floor( ( intersectBox.xMinimum() - rasterBBox.xMinimum() ) / cellSizeX ) );
-  int offsetY = static_cast< int >( std::floor( ( rasterBBox.yMaximum() - intersectBox.yMaximum() ) / cellSizeY ) );
-
-  int maxColumn = static_cast< int >( std::floor( ( intersectBox.xMaximum() - rasterBBox.xMinimum() ) / cellSizeX ) ) + 1;
-  int maxRow = static_cast< int >( std::floor( ( rasterBBox.yMaximum() - intersectBox.yMinimum() ) / cellSizeY ) ) + 1;
-
-  nCellsX = maxColumn - offsetX;
-  nCellsY = maxRow - offsetY;
-
-  //avoid access to cells outside of the raster (may occur because of rounding)
-  nCellsX = std::min( offsetX + nCellsX, rasterWidth ) - offsetX;
-  nCellsY = std::min( offsetY + nCellsY, rasterHeight ) - offsetY;
-
-  rasterBlockExtent = QgsRectangle( rasterBBox.xMinimum() + offsetX * cellSizeX,
-                                    rasterBBox.yMaximum() - offsetY * cellSizeY,
-                                    rasterBBox.xMinimum() + ( nCellsX + offsetX ) * cellSizeX,
-                                    rasterBBox.yMaximum() - ( nCellsY + offsetY ) * cellSizeY );
-}
-
-void QgsZonalStatistics::statisticsFromMiddlePointTest( const QgsGeometry &poly, int nCellsX, int nCellsY, double cellSizeX, double cellSizeY, const QgsRectangle &rasterBBox, FeatureStats &stats )
-{
-  stats.reset();
-
-  std::unique_ptr< QgsGeometryEngine > polyEngine( QgsGeometry::createGeometryEngine( poly.constGet( ) ) );
-  if ( !polyEngine )
-  {
-    return;
-  }
-  polyEngine->prepareGeometry();
-
-  const int maxWidth = 4000;
-  const int maxHeight = 4000;
-
-  QgsRasterIterator iter( mRasterInterface );
-  iter.setMaximumTileWidth( maxWidth );
-  iter.setMaximumTileHeight( maxHeight );
-  iter.startRasterRead( mRasterBand, nCellsX, nCellsY, rasterBBox );
-
-  QgsRasterBlock *block = nullptr;
-  int iterLeft = 0;
-  int iterTop = 0;
-  int iterCols = 0;
-  int iterRows = 0;
-  while ( iter.readNextRasterPart( mRasterBand, iterCols, iterRows, &block, iterLeft, iterTop ) )
-  {
-    double cellCenterY = rasterBBox.yMinimum() + ( iterTop + iterRows - 0.5 ) * cellSizeY;
-    for ( int row = 0; row < iterRows; ++row )
-    {
-      double cellCenterX = rasterBBox.xMinimum() + ( iterLeft + 0.5 ) * cellSizeX;
-      for ( int col = 0; col < iterCols; ++col )
-      {
-        double pixelValue = block->value( row, col );
-        if ( validPixel( pixelValue ) && !block->isNoData( row, col ) )
-        {
-          QgsPoint cellCenter( cellCenterX, cellCenterY );
-          if ( polyEngine->contains( &cellCenter ) )
-          {
-            stats.addValue( pixelValue );
-          }
-        }
-        cellCenterX += cellSizeX;
-      }
-      cellCenterY -= cellSizeY;
-    }
-    delete block;
-  }
-}
-
-void QgsZonalStatistics::statisticsFromPreciseIntersection( const QgsGeometry &poly, int nCellsX, int nCellsY, double cellSizeX, double cellSizeY, const QgsRectangle &rasterBBox, FeatureStats &stats )
-{
-  stats.reset();
-
-  QgsGeometry pixelRectGeometry;
-
-  double hCellSizeX = cellSizeX / 2.0;
-  double hCellSizeY = cellSizeY / 2.0;
-  double pixelArea = cellSizeX * cellSizeY;
-  double weight = 0;
-
-  std::unique_ptr< QgsGeometryEngine > polyEngine( QgsGeometry::createGeometryEngine( poly.constGet( ) ) );
-  if ( !polyEngine )
-  {
-    return;
-  }
-  polyEngine->prepareGeometry();
-
-  const int maxWidth = 4000;
-  const int maxHeight = 4000;
-
-  QgsRasterIterator iter( mRasterInterface );
-  iter.setMaximumTileWidth( maxWidth );
-  iter.setMaximumTileHeight( maxHeight );
-  iter.startRasterRead( mRasterBand, nCellsX, nCellsY, rasterBBox );
-
-  QgsRasterBlock *block = nullptr;
-  int iterLeft = 0;
-  int iterTop = 0;
-  int iterCols = 0;
-  int iterRows = 0;
-  while ( iter.readNextRasterPart( mRasterBand, iterCols, iterRows, &block, iterLeft, iterTop ) )
-  {
-    double currentY = rasterBBox.yMinimum() + ( iterTop + iterRows - 0.5 ) * cellSizeY;
-    for ( int row = 0; row < iterRows; ++row )
-    {
-      double currentX = rasterBBox.xMinimum() + ( iterLeft + 0.5 ) * cellSizeX;
-      for ( int col = 0; col < iterCols; ++col )
-      {
-        double pixelValue = block->value( row, col );
-        if ( validPixel( pixelValue ) && !block->isNoData( row, col ) )
-        {
-          pixelRectGeometry = QgsGeometry::fromRect( QgsRectangle( currentX - hCellSizeX, currentY - hCellSizeY, currentX + hCellSizeX, currentY + hCellSizeY ) );
-          // GEOS intersects tests on prepared geometry is MAGNITUDES faster than calculating the intersection itself,
-          // so we first test to see if there IS an intersection before doing the actual calculation
-          if ( !pixelRectGeometry.isNull() && polyEngine->intersects( pixelRectGeometry.constGet() ) )
-          {
-            //intersection
-            QgsGeometry intersectGeometry = pixelRectGeometry.intersection( poly );
-            if ( !intersectGeometry.isEmpty() )
-            {
-              double intersectionArea = intersectGeometry.area();
-              if ( intersectionArea > 0.0 )
-              {
-                weight = intersectionArea / pixelArea;
-                stats.addValue( pixelValue, weight );
-              }
-            }
-          }
-        }
-        currentX += cellSizeX;
-      }
-      currentY -= cellSizeY;
-    }
-  }
-}
-
-bool QgsZonalStatistics::validPixel( double value ) const
-{
-  return !std::isnan( value );
 }
 
 QString QgsZonalStatistics::getUniqueFieldName( const QString &fieldName, const QList<QgsField> &newFields )
