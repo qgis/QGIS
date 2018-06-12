82a527417aaf28d114d08aa536a89047f9ac7462
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-13 06:56:48 +1000

    Remove a duplicate test

--- tests/src/python/test_qgsrange.py
+++ tests/src/python/test_qgsrange.py
@@ -341,13 +341,6 @@ class TestQgsDateRange(unittest.TestCase):
         self.assertFalse(QgsDateRange(QDate(2010, 3, 1), QDate(2010, 3, 1), False, False).isInfinite())
         self.assertTrue(QgsDateRange(QDate(), QDate()).isInfinite())
 
-    def testIsEmpty(self):
-        self.assertFalse(QgsDateRange(QDate(2010, 3, 1), QDate(2010, 6, 2)).isEmpty())
-        self.assertFalse(QgsDateRange(QDate(2010, 3, 1), QDate(2010, 3, 1)).isEmpty())
-        self.assertTrue(QgsDateRange(QDate(2010, 3, 1), QDate(2010, 3, 1), False, False).isEmpty())
-        self.assertTrue(QgsDateRange(QDate(2017, 3, 1), QDate(2010, 6, 2)).isEmpty())
-        self.assertFalse(QgsDateRange(QDate(), QDate()).isEmpty())
-
     def testEquality(self):
         range = QgsDateRange(QDate(2010, 3, 1), QDate(2010, 6, 2), False, False)
         self.assertEqual(range, QgsDateRange(QDate(2010, 3, 1), QDate(2010, 6, 2), False, False))
