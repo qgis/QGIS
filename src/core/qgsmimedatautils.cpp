941d1c0677f17e8cfe0c06225d49e70608bc8171
Alessandro Pasotti <elpaso@itopen.it>
2018-06-19 09:05:03 +0200

    Use QRegularExpression and negative look behind

--- src/core/qgsmimedatautils.cpp
+++ src/core/qgsmimedatautils.cpp
@@ -207,12 +207,12 @@ QString QgsMimeDataUtils::encode( const QStringList &items )
 {
   QString encoded;
   // Do not escape colon twice
-  QRegExp re( "([^\\\\]):" );
+  QRegularExpression re( "(?<!\\\\):" );
   Q_FOREACH ( const QString &item, items )
   {
     QString str = item;
     str.replace( '\\', QLatin1String( "\\\\" ) );
-    str.replace( re, QLatin1String( "\\1\\:" ) );
+    str.replace( re, QLatin1String( "\\:" ) );
     encoded += str + ':';
   }
   return encoded.left( encoded.length() - 1 );
