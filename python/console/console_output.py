b437f789dc61c1fdaccd69b5795ce1c4b7bf0f73
nirvn <nirvn.asia@gmail.com>
2018-08-20 13:38:54 +0700

    [ui] add security warning to the python console
    (cherry-picked from 2aebeb01)

--- python/console/console_output.py
+++ python/console/console_output.py
@@ -169,8 +169,9 @@ class ShellOutputScintilla(QsciScintilla):
 
     def insertInitText(self):
         txtInit = QCoreApplication.translate("PythonConsole",
-                                             "Python Console \n"
-                                             "Use iface to access QGIS API interface or Type help(iface) for more info")
+                                             "Python Console\n"
+                                             "Use iface to access QGIS API interface or Type help(iface) for more info\n"
+                                             "Security warning: typing commands from an untrusted source can lead to data loss and/or leak")
 
         # some translation string for the console header ends without '\n'
         # and the first command in console will be appended at the header text.
