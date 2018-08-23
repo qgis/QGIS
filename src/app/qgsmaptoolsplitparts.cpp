391d3f979afa6b92a13afcc2c18bcd6f108a880d
Matthias Kuhn <matthias@opengis.ch>
2018-08-23 12:51:33 +0200

    Disable snap to grid for maptool split parts

--- src/app/qgsmaptoolsplitparts.cpp
+++ src/app/qgsmaptoolsplitparts.cpp
@@ -27,6 +27,7 @@ QgsMapToolSplitParts::QgsMapToolSplitParts( QgsMapCanvas *canvas )
   : QgsMapToolCapture( canvas, QgisApp::instance()->cadDockWidget(), QgsMapToolCapture::CaptureLine )
 {
   mToolName = tr( "Split parts" );
+  setSnapToGridEnabled( false );
 }
 
 void QgsMapToolSplitParts::cadCanvasReleaseEvent( QgsMapMouseEvent *e )
