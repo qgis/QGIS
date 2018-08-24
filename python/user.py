c1ca5a25a1c96ba995859df9d7e01867399a7075
Harrissou Sant-anna <delazj@gmail.com>
2018-08-24 17:58:18 +0200

    Fix typo
    (cherry picked from commit 14f36f201f7fd6fa511a86ae0ed3c8503312762f)

--- python/user.py
+++ python/user.py
@@ -104,7 +104,7 @@ def my_sum(value1, value2, feature, parent):
     <h2>Example usage:</h2>
     <ul>
       <li>my_sum(5, 8) -> 13</li>
-      <li>my_sum(\"fiel1\", \"field2\") -> 42</li>
+      <li>my_sum(\"field1\", \"field2\") -> 42</li>
     </ul>
     \"\"\"
     return value1 + value2
