6ae03afdfac65de20e21b59dfc4c61be0bac072e
Matthias Kuhn <matthias@opengis.ch>
2018-08-26 08:15:57 +0200

    Add license headers

--- src/gui/qgssnaptogridcanvasitem.cpp
+++ src/gui/qgssnaptogridcanvasitem.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+    qgssnaptogridcanvasitem.cpp
+    ----------------------
+    begin                : August 2018
+    copyright            : (C) Matthias Kuhn
+    email                : matthias@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgssnaptogridcanvasitem.h"
 #include "qgsmapcanvas.h"
 
--- src/gui/qgssnaptogridcanvasitem.h
+++ src/gui/qgssnaptogridcanvasitem.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+    qgssnaptogridcanvasitem.h
+    ----------------------
+    begin                : August 2018
+    copyright            : (C) Matthias Kuhn
+    email                : matthias@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSSNAPTOGRIDCANVASITEM_H
 #define QGSSNAPTOGRIDCANVASITEM_H
 
