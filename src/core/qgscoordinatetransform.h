89526a02e023598e85a9f5d8b45ae0e7deb19340
Matthias Kuhn <matthias@opengis.ch>
2018-09-10 12:58:54 +0200

    Add QgsCoordinateTransform::conversionFactor
    directly copied from QgsMapSettings

--- src/core/qgscoordinatetransform.h
+++ src/core/qgscoordinatetransform.h
@@ -405,6 +405,17 @@ class CORE_EXPORT QgsCoordinateTransform
      */
     static void invalidateCache();
 
+    /**
+     * Computes an *estimated* conversion factor between source and destination units:
+     *
+     *   sourceUnits * conversionFactor = destinationUnits
+     *
+     * \param referenceExtent A reference extent based on which to perform the computation
+     *
+     * \since QGIS 3.4
+     */
+    double conversionFactor( const QgsRectangle &referenceExtent ) const;
+
   private:
 
     mutable QExplicitlySharedDataPointer<QgsCoordinateTransformPrivate> d;
