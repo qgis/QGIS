b2c881c02cf88306ca3bf9edcd15e5354ad9eefd
Geoff Kimbell <giskimbell@gmail.com>
2018-09-11 13:10:30 +0100

    Update contour.py

--- python/plugins/processing/algs/gdal/contour.py
+++ python/plugins/processing/algs/gdal/contour.py
@@ -106,12 +106,12 @@ class contour(GdalAlgorithm):
         self.addParameter(offset_param)
 
         options_param = QgsProcessingParameterString(self.OPTIONS,
-                                                 self.tr('Additional creation options'),
-                                                 defaultValue='',
-                                                 optional=True)
+                                                     self.tr('Additional creation options'),
+                                                     defaultValue='',
+                                                     optional=True)
         options_param.setFlags(options_param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(options_param)
-        
+
         self.addParameter(QgsProcessingParameterVectorDestination(
             self.OUTPUT, self.tr('Contours'), QgsProcessing.TypeVectorLine))
 
@@ -170,10 +170,10 @@ class contour(GdalAlgorithm):
 
         if offset:
             arguments.append('-off {}'.format(offset))
-        
+
         options = self.parameterAsString(parameters, self.OPTIONS, context)
         if options:
-             arguments.append(options)
+            arguments.append(options)
 
         if outFormat:
             arguments.append('-f {}'.format(outFormat))
