561586f740fcca2cc33fa0a739e2445899a448d8
Matthias Kuhn <matthias@opengis.ch>
2018-09-12 16:58:54 +0200

    Check layer pointer for validity

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -12054,7 +12054,7 @@ void QgisApp::legendLayerSelectionChanged()
   bool removeEnabled = true;
   for ( QgsLayerTreeLayer *nodeLayer : selectedLayers )
   {
-    if ( !nodeLayer->layer()->flags().testFlag( QgsMapLayer::Removable ) )
+    if ( nodeLayer->layer() && !nodeLayer->layer()->flags().testFlag( QgsMapLayer::Removable ) )
     {
       removeEnabled = false;
       break;
