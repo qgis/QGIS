e972e21a099a4ff3c0789b4fddd45e7e39d10b29
signedav <david@opengis.ch>
2018-09-12 09:41:21 +0200

    test db stuff json

--- tests/testdata/provider/testdata_pg.sh
+++ tests/testdata/provider/testdata_pg.sh
@@ -12,6 +12,7 @@ SCRIPTS="
   tests/testdata/provider/testdata_pg_array.sql
   tests/testdata/provider/testdata_pg_raster.sql
   tests/testdata/provider/testdata_pg_domain.sql
+  tests/testdata/provider/testdata_pg_json.sql
 "
 
 dropdb qgis_test 2> /dev/null || true
