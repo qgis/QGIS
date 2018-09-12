af9d5af4e5bb9ac1906614dd17f5e68e2bd9f8ab
signedav <david@opengis.ch>
2018-09-12 09:41:21 +0200

    test db stuff json

---
+++ tests/testdata/provider/testdata_pg_json.sql
@@ -0,0 +1,15 @@
+
+DROP TABLE IF EXISTS qgis_test.json;
+
+CREATE TABLE qgis_test.json
+(
+  pk SERIAL NOT NULL PRIMARY KEY,
+  jvalue json,
+  jbvalue jsonb
+);
+
+INSERT INTO qgis_test.json(jvalue, jbvalue)
+  VALUES
+    ('[1,2,3]', '[4,5,6]'),
+    ('{"a":1,"b":2}', '{"c":4,"d":5}'),
+    ('123', '456');
