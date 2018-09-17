1bc606ae4eb03c6bec62b309c036be3fd3389407
Alessandro Pasotti <elpaso@itopen.it>
2018-09-17 15:06:29 +0200

    Fix another crash in style categories

--- src/app/qgsmaplayerstylecategoriesmodel.cpp
+++ src/app/qgsmaplayerstylecategoriesmodel.cpp
@@ -47,7 +47,7 @@ void QgsMapLayerStyleCategoriesModel::setShowAllCategories( bool showAll )
 
 QgsMapLayer::StyleCategory QgsMapLayerStyleCategoriesModel::index2category( const QModelIndex &index ) const
 {
-  return mCategoryList.at( index.row() - ( mShowAllCategories ? 1 : 0 ) );
+  return mCategoryList.at( index.row() + ( mShowAllCategories ? 0 : 1 ) );
 }
 
 int QgsMapLayerStyleCategoriesModel::rowCount( const QModelIndex & ) const
