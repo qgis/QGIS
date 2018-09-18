fb8083591159ca1b22a1b388127b70b1750f8f37
nirvn <nirvn.asia@gmail.com>
2018-09-18 10:24:24 +0700

    [processing] fix history dialog alg. execution with CRS params

--- python/plugins/processing/gui/HistoryDialog.py
+++ python/plugins/processing/gui/HistoryDialog.py
@@ -116,7 +116,7 @@ class HistoryDialog(BASE, WIDGET):
         if isinstance(item, TreeLogEntryItem):
             if item.isAlg:
                 script = 'import processing\n'
-                script += 'from qgis.core import QgsProcessingOutputLayerDefinition, QgsProcessingFeatureSourceDefinition, QgsProperty\n'
+                script += 'from qgis.core import QgsProcessingOutputLayerDefinition, QgsProcessingFeatureSourceDefinition, QgsProperty, QgsCoordinateReferenceSystem\n'
                 script += item.entry.text.replace('processing.run(', 'processing.execAlgorithmDialog(')
                 self.close()
                 exec(script)
