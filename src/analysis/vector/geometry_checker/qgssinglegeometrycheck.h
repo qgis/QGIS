0e007d88c9f8184b6df4b442ba800f56f1d93332
Matthias Kuhn <matthias@opengis.ch>
2018-09-19 18:12:08 +0200

    Fix python bindings build error

--- src/analysis/vector/geometry_checker/qgssinglegeometrycheck.h
+++ src/analysis/vector/geometry_checker/qgssinglegeometrycheck.h
@@ -136,7 +136,7 @@ class ANALYSIS_EXPORT QgsSingleGeometryCheck : public QgsGeometryCheck
   public:
     QgsSingleGeometryCheck( CheckType checkType, const QList<QgsWkbTypes::GeometryType> &compatibleGeometryTypes, QgsGeometryCheckerContext *context );
 
-    void collectErrors( QList<QgsGeometryCheckError *> &errors, QStringList &messages, QgsFeedback *feedback = nullptr, const LayerFeatureIds &ids = LayerFeatureIds() ) const final;
+    void collectErrors( QList<QgsGeometryCheckError *> &errors, QStringList &messages, QgsFeedback *feedback = nullptr, const QgsGeometryCheck::LayerFeatureIds &ids = QgsGeometryCheck::LayerFeatureIds() ) const final;
 
     /**
      * Check the \a geometry for errors. It may make use of \a configuration options.
