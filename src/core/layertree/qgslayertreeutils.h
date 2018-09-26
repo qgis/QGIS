6f550b81528a10172ea53d3210156cf0b68ad3ed
Martin Dobias <wonder.sk@gmail.com>
2018-09-26 12:08:26 +0200

    Fix spelling

--- src/core/layertree/qgslayertreeutils.h
+++ src/core/layertree/qgslayertreeutils.h
@@ -96,15 +96,15 @@ class CORE_EXPORT QgsLayerTreeUtils
     static QSet<QgsMapLayer *> collectMapLayersRecursive( const QList<QgsLayerTreeNode *> &nodes );
 
     /**
-     * Returns how many occurences of a map layer are there in a layer tree.
-     * In normal situations there is at most one occurence, but sometimes there
+     * Returns how many occurrences of a map layer are there in a layer tree.
+     * In normal situations there is at most one occurrence, but sometimes there
      * may be temporarily more: for example, during drag&drop, upon drop a new layer
      * node is created while the original dragged node is still in the tree, resulting
-     * in two occurences.
+     * in two occurrences.
      *
      * This is useful when deciding whether to start or stop listening to a signal
      * of a map layer within a layer tree and only connecting/disconnecting when
-     * there is only one occurence of that layer.
+     * there is only one occurrence of that layer.
      * \since QGIS 3.4
      */
     static int countMapLayerInTree( QgsLayerTreeNode *tree, QgsMapLayer *layer );
