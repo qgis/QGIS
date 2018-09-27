3fc3206591f8351a94b2bc2c3c474b6290b4ce86
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-09-27 14:23:46 -0400

    add licence headers

--- src/app/qgsappscreenshots.cpp
+++ src/app/qgsappscreenshots.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+  qgsappscreenshots.cpp
+  --------------------------------------
+  Date                 : September 2018
+  Copyright            : (C) 2018 by Denis Rouzaud
+  Email                : denis@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 
 #include <QMenu>
 #include <QWindow>
--- src/app/qgsappscreenshots.h
+++ src/app/qgsappscreenshots.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+  qgsappscreenshots.h
+  --------------------------------------
+  Date                 : September 2018
+  Copyright            : (C) 2018 by Denis Rouzaud
+  Email                : denis@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifndef QGSAPPSCREENSHOTS_H
 #define QGSAPPSCREENSHOTS_H
 
