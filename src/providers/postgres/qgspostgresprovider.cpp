942a560b3191bea5a26189ca9065ad1c37a51b64
Martin Dobias <wonder.sk@gmail.com>
2018-09-28 18:06:09 +0200

    Fix loading style for non-spatial tables in Postgres+SpatiaLite (fixes #19589)
    OGR is not affected by the bug.
    Oracle and MS SQL are most likely affected, but I don't have them here to test.
    
    (cherry picked from commit bb0c62935562ebe28a0d862e4bdf985865bde595)

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -4228,18 +4228,28 @@ QGISEXTERN QString loadStyle( const QString& uri, QString& errCause )
     dsUri.setDatabase( conn->currentDatabase() );
   }
 
+  QString geomColumnExpr;
+  if ( dsUri.geometryColumn().isEmpty() )
+  {
+    geomColumnExpr = QString( "IS NULL" );
+  }
+  else
+  {
+    geomColumnExpr = QString( "=" ) + QgsPostgresConn::quotedValue( dsUri.geometryColumn() );
+  }
+
   QString selectQmlQuery = QString( "SELECT styleQML"
                                     " FROM layer_styles"
                                     " WHERE f_table_catalog=%1"
                                     " AND f_table_schema=%2"
                                     " AND f_table_name=%3"
-                                    " AND f_geometry_column=%4"
+                                    " AND f_geometry_column %4"
                                     " ORDER BY CASE WHEN useAsDefault THEN 1 ELSE 2 END"
                                     ",update_time DESC LIMIT 1" )
                            .arg( QgsPostgresConn::quotedValue( dsUri.database() ) )
                            .arg( QgsPostgresConn::quotedValue( dsUri.schema() ) )
                            .arg( QgsPostgresConn::quotedValue( dsUri.table() ) )
-                           .arg( QgsPostgresConn::quotedValue( dsUri.geometryColumn() ) );
+                           .arg( geomColumnExpr );
 
   result = conn->PQexec( selectQmlQuery );
 
