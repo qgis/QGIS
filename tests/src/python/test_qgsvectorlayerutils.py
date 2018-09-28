9371ff7a7d6e7d26d00ad4fb55208cde79cb92b0
phborba <philipeborba@gmail.com>
2018-09-28 14:48:10 -0300

    another fix

--- tests/src/python/test_qgsvectorlayerutils.py
+++ tests/src/python/test_qgsvectorlayerutils.py
@@ -258,10 +258,10 @@ class TestQgsVectorLayerUtils(unittest.TestCase):
         # layer with default value expression
         layer.setDefaultValueDefinition(2, QgsDefaultValue('3*4'))
         f = QgsVectorLayerUtils.createFeature(layer)
-        self.assertEqual(f.attributes(), [NULL, NULL, 12.0])
+        self.assertEqual(f.attributes(), ['a', NULL, 6.0])
         # we do not expect the default value expression to take precedence over the attribute map
         f = QgsVectorLayerUtils.createFeature(layer, attributes={0: 'a', 2: 6.0})
-        self.assertEqual(f.attributes(), ['a', NULL, 12.0])
+        self.assertEqual(f.attributes(), ['a', NULL, 6.0])
         # layer with default value expression based on geometry
         layer.setDefaultValueDefinition(2, QgsDefaultValue('3*$x'))
         f = QgsVectorLayerUtils.createFeature(layer, g)
