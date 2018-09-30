f0ddef4beda93901edde11777eb09726bf5c3008
Matthias Kuhn <matthias@opengis.ch>
2018-09-30 10:35:45 +0200

    Dox

--- src/analysis/vector/geometry_checker/qgsgeometrycheckcontext.h
+++ src/analysis/vector/geometry_checker/qgsgeometrycheckcontext.h
@@ -21,14 +21,40 @@
 #include "qgscoordinatetransformcontext.h"
 #include "qgsfeaturepool.h"
 
+/**
+ * Base configuration for geometry checks.
+ *
+ * \since QGIS 3.4
+ */
 struct ANALYSIS_EXPORT QgsGeometryCheckContext
 {
     QgsGeometryCheckContext( int precision,
                              const QgsCoordinateReferenceSystem &mapCrs,
                              const QgsCoordinateTransformContext &transformContext );
+
+    /**
+     * The tolerance to allow for in geometry checks.
+     * Will be calculated as pow(10, -precision) in the constructor.
+     * I.e. if the precision is 4 (decimal digits), this will be 0.0001.
+     */
     const double tolerance;
+
+    /**
+     * The tolerance to allow for in geometry checks.
+     * Will be calculated as pow(10, -precision/2) in the constructor.
+     * I.e. if the precision is 4 (decimal digits), this will be 0.01.
+     * Should be used for areas, where the precision is squared.
+     */
     const double reducedTolerance;
+
+    /**
+     * The coordinate system in which calculations should be done.
+     */
     const QgsCoordinateReferenceSystem mapCrs;
+
+    /**
+     * The coordinate transform context with which transformations will be done.
+     */
     const QgsCoordinateTransformContext transformContext;
 
   private:
