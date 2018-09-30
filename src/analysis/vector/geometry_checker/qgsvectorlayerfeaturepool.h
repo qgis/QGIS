9668eab9e0bd0faa50f14e43d911f85eb58e49f9
Matthias Kuhn <matthias@opengis.ch>
2018-09-30 16:22:30 +0200

    Fix docs

--- src/analysis/vector/geometry_checker/qgsvectorlayerfeaturepool.h
+++ src/analysis/vector/geometry_checker/qgsvectorlayerfeaturepool.h
@@ -23,7 +23,7 @@ email                : matthias@opengis.ch
 
 /**
  * \ingroup analysis
- * A feature pool based on a vector data provider.
+ * A feature pool based on a vector layer.
  *
  * \since QGIS 3.4
  */
