f404751e5bcb2e188ed7d4d8dd5308cf6fa67bfd
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-10-05 15:42:06 -0400

    move definition to source

--- src/app/qgsstatisticalsummarydockwidget.h
+++ src/app/qgsstatisticalsummarydockwidget.h
@@ -23,8 +23,6 @@
 #include "qgsdatetimestatisticalsummary.h"
 #include "qgsdockwidget.h"
 #include "qgsfeatureiterator.h"
-#include "qgsfeedback.h"
-#include "qgsvectorlayerutils.h"
 #include "qgstaskmanager.h"
 
 #include "qgis_app.h"
@@ -47,51 +45,9 @@ class QgsStatisticsValueGatherer : public QgsTask
     Q_OBJECT
 
   public:
-    QgsStatisticsValueGatherer( QgsVectorLayer *layer, const QgsFeatureIterator &fit, long featureCount, const QString &sourceFieldExp )
-      : QgsTask( tr( "Fetching statistic values" ) )
-      , mFeatureIterator( fit )
-      , mFeatureCount( featureCount )
-      , mFieldExpression( sourceFieldExp )
-    {
-      mFieldIndex = layer->fields().lookupField( mFieldExpression );
-      if ( mFieldIndex == -1 )
-      {
-        // use expression, already validated
-        mExpression.reset( new QgsExpression( mFieldExpression ) );
-        mContext.appendScopes( QgsExpressionContextUtils::globalProjectLayerScopes( layer ) );
-      }
-    }
+    QgsStatisticsValueGatherer( QgsVectorLayer *layer, const QgsFeatureIterator &fit, long featureCount, const QString &sourceFieldExp );
 
-    bool run() override
-    {
-      QgsFeature f;
-      int current = 0;
-      while ( mFeatureIterator.nextFeature( f ) )
-      {
-        if ( mExpression )
-        {
-          mContext.setFeature( f );
-          QVariant v = mExpression->evaluate( &mContext );
-          mValues << v;
-        }
-        else
-        {
-          mValues << f.attribute( mFieldIndex );
-        }
-
-        if ( isCanceled() )
-        {
-          return false;
-        }
-
-        current++;
-        if ( mFeatureCount > 0 )
-        {
-          setProgress( 100.0 * static_cast< double >( current ) / mFeatureCount );
-        }
-      }
-      return true;
-    }
+    bool run() override;
 
     QList<QVariant> values() const { return mValues; }
 
