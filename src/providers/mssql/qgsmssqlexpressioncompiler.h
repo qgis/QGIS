ed3925339916d2fdc4b03aee1c3c3def8d875555
Nyall Dawson <nyall.dawson@gmail.com>
2018-10-08 15:43:19 +1000

    Indentation

--- src/providers/mssql/qgsmssqlexpressioncompiler.h
+++ src/providers/mssql/qgsmssqlexpressioncompiler.h
@@ -29,7 +29,7 @@ class QgsMssqlExpressionCompiler : public QgsSqlExpressionCompiler
   protected:
     Result compileNode( const QgsExpressionNode *node, QString &result ) override;
     QString quotedValue( const QVariant &value, bool &ok ) override;
-    QString quotedIdentifier( const QString& identifier ) override;
+    QString quotedIdentifier( const QString &identifier ) override;
     QString castToReal( const QString &value ) const override;
     QString castToInt( const QString &value ) const override;
     QString sqlFunctionFromFunctionName( const QString &fnName ) const override;
