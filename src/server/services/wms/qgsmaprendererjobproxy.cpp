ef4bf1a806224935bee6df2f9f9ae49f8ad71513
Blottiere Paul <blottiere.paul@gmail.com>
2018-10-21 23:35:43 +0200

    Add link to corresponding issue in comment

--- src/server/services/wms/qgsmaprendererjobproxy.cpp
+++ src/server/services/wms/qgsmaprendererjobproxy.cpp
@@ -58,7 +58,7 @@ namespace QgsWms
       renderJob.start();
 
       // Allows the main thread to manage blocking call coming from rendering
-      // threads (see discussion in #18988).
+      // threads (see discussion in https://issues.qgis.org/issues/18988).
       QEventLoop loop;
       QObject::connect( &renderJob, &QgsMapRendererParallelJob::finished, &loop, &QEventLoop::quit );
       loop.exec();
