cd64d31b39e0a21b8e06f2d1170e8b44bac2e116
Nyall Dawson <nyall.dawson@gmail.com>
2018-10-22 16:07:00 +1000

    [mssql] Ensure provider rejects features with incorrect geometry type

--- src/providers/mssql/qgsmssqlprovider.cpp
+++ src/providers/mssql/qgsmssqlprovider.cpp
@@ -855,6 +855,21 @@ bool QgsMssqlProvider::addFeatures( QgsFeatureList &flist, Flags flags )
 {
   for ( QgsFeatureList::iterator it = flist.begin(); it != flist.end(); ++it )
   {
+    if ( it->hasGeometry() && mWkbType == QgsWkbTypes::NoGeometry )
+    {
+      it->clearGeometry();
+    }
+    else if ( it->hasGeometry() && QgsWkbTypes::geometryType( it->geometry().wkbType() ) !=
+              QgsWkbTypes::geometryType( mWkbType ) )
+    {
+      pushError( tr( "Could not add feature with geometry type %1 to layer of type %2" ).arg( QgsWkbTypes::displayString( it->geometry().wkbType() ),
+                 QgsWkbTypes::displayString( mWkbType ) ) );
+      if ( !mSkipFailures )
+        return false;
+
+      continue;
+    }
+
     QString statement;
     QString values;
     statement = QStringLiteral( "INSERT INTO [%1].[%2] (" ).arg( mSchemaName, mTableName );
