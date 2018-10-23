7dde570abf2cc2ae78a0851784c9daf99d99774c
Juergen E. Fischer <jef@norbit.de>
2018-10-23 22:22:46 +0200

    debian/osgeo4w/docker: use public instead of non-existing private PyQt5.sip module

--- ms-windows/osgeo4w/package.cmd
+++ ms-windows/osgeo4w/package.cmd
@@ -161,6 +161,7 @@ cmake -G "%CMAKEGEN%" ^
 	-D SPATIALITE_LIBRARY=%O4W_ROOT%/lib/spatialite_i.lib ^
 	-D PYTHON_EXECUTABLE=%O4W_ROOT%/bin/python3.exe ^
 	-D SIP_BINARY_PATH=%PYTHONHOME:\=/%/sip.exe ^
+	-D SIP_IMPORT=sip ^
 	-D PYTHON_INCLUDE_PATH=%PYTHONHOME:\=/%/include ^
 	-D PYTHON_LIBRARY=%PYTHONHOME:\=/%/libs/%PYVER%.lib ^
 	-D QT_LIBRARY_DIR=%O4W_ROOT%/lib ^
