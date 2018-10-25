cd509de59c94f44ca325d550139231ff12624891
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-10-25 08:35:46 -0400

    fix typo

--- src/native/qgsnative.h
+++ src/native/qgsnative.h
@@ -136,7 +136,8 @@ class NATIVE_EXPORT QgsNative : public QObject
     virtual void setApplicationBadgeCount( int count );
 
     /**
-     * Returns true if the operating system is set to utilise a "dark" theme.
+     * Returns true if the operating system is set to utilize a "dark" theme.
+     * \since QGIS 3.4
      */
     virtual bool hasDarkTheme() {return false;}
 
