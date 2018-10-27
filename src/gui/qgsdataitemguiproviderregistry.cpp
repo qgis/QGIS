f7cece0d07a6d05d1ec602e06725eb614b7b6ab3
Nyall Dawson <nyall.dawson@gmail.com>
2018-10-27 12:00:23 +1000

    Allow provider libraries to create QgsDataItemGuiProviders
    ...by implementing dataItemGuiProviders()

--- src/gui/qgsdataitemguiproviderregistry.cpp
+++ src/gui/qgsdataitemguiproviderregistry.cpp
@@ -15,9 +15,28 @@
 
 #include "qgsdataitemguiproviderregistry.h"
 #include "qgsdataitemguiprovider.h"
+#include "qgsproviderregistry.h"
+
+typedef QList<QgsDataItemGuiProvider *> *dataItemGuiProviders_t();
 
 QgsDataItemGuiProviderRegistry::QgsDataItemGuiProviderRegistry()
 {
+  const QStringList providersList = QgsProviderRegistry::instance()->providerList();
+
+  for ( const QString &key : providersList )
+  {
+    std::unique_ptr< QLibrary > library( QgsProviderRegistry::instance()->createProviderLibrary( key ) );
+    if ( !library )
+      continue;
+
+    if ( dataItemGuiProviders_t *dataItemGuiProvidersFn = reinterpret_cast< dataItemGuiProviders_t * >( cast_to_fptr( library->resolve( "dataItemGuiProviders" ) ) ) )
+    {
+      const QList<QgsDataItemGuiProvider *> *providerList = dataItemGuiProvidersFn();
+      // the function is a factory - we keep ownership of the returned providers
+      mProviders << *providerList;
+      delete providerList;
+    }
+  }
 }
 
 QgsDataItemGuiProviderRegistry::~QgsDataItemGuiProviderRegistry()
