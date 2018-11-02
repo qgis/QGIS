566d5726d76af97c1410bc6a86357d37c14dad92
Håvard Tveite <havard.tveite@nmbu.no>
2018-11-02 13:04:12 +0100

    Update extractprojection.py
    indentation again...

--- python/plugins/processing/algs/gdal/extractprojection.py
+++ python/plugins/processing/algs/gdal/extractprojection.py
@@ -94,8 +94,9 @@ class ExtractProjection(GdalAlgorithm):
         raster = self.parameterAsRasterLayer(parameters, self.INPUT,
                                              context)
         if not raster.dataProvider().name() == 'gdal':
-            raise QgsProcessingException(self.tr('This algorithm can only '
-                                         'be used with GDAL raster layers'))
+            raise QgsProcessingException(self.tr('This algorithm can '
+                                                 'only be used with '
+                                                 'GDAL raster layers'))
         rasterPath = raster.source()
         rasterDS = gdal.Open(rasterPath, gdal.GA_ReadOnly)
         geotransform = rasterDS.GetGeoTransform()
