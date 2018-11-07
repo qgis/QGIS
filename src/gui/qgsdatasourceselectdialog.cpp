02ecb56d951607da1929b56fcac39f3480cf9dd0
Alessandro Pasotti <elpaso@itopen.it>
2018-11-07 11:08:49 +0100

    QgsDataSourceSelectDialog: filter, refresh and scroll to last selected item
    plus:
    - expand children of last selected item
    - save/restore status

--- src/gui/qgsdatasourceselectdialog.cpp
+++ src/gui/qgsdatasourceselectdialog.cpp
@@ -19,8 +19,10 @@
 #include "qgssettings.h"
 #include "qgsgui.h"
 #include "qgis.h"
+#include "qgsbrowsermodel.h"
 
 #include <QPushButton>
+#include <QMenu>
 
 QgsDataSourceSelectDialog::QgsDataSourceSelectDialog(
   QgsBrowserModel *browserModel,
@@ -32,6 +34,7 @@ QgsDataSourceSelectDialog::QgsDataSourceSelectDialog(
   if ( ! browserModel )
   {
     mBrowserModel = qgis::make_unique<QgsBrowserModel>();
+    mBrowserModel->initialize();
     mOwnModel = true;
   }
   else
@@ -44,12 +47,12 @@ QgsDataSourceSelectDialog::QgsDataSourceSelectDialog(
   setWindowTitle( tr( "Select a Data Source" ) );
   QgsGui::enableAutoGeometryRestore( this );
 
-  mBrowserModel->initialize();
   mBrowserProxyModel.setBrowserModel( mBrowserModel.get() );
   mBrowserTreeView->setHeaderHidden( true );
 
   if ( setFilterByLayerType )
   {
+    // This will also set the (proxy) model
     setLayerTypeFilter( layerType );
   }
   else
@@ -57,13 +60,162 @@ QgsDataSourceSelectDialog::QgsDataSourceSelectDialog(
     mBrowserTreeView->setModel( &mBrowserProxyModel );
     buttonBox->button( QDialogButtonBox::StandardButton::Ok )->setEnabled( false );
   }
+
+  mBrowserTreeView->setBrowserModel( mBrowserModel.get() );
+
+  mWidgetFilter->hide();
+  mLeFilter->setPlaceholderText( tr( "Type here to filter visible items…" ) );
+  // icons from http://www.fatcow.com/free-icons License: CC Attribution 3.0
+
+  QMenu *menu = new QMenu( this );
+  menu->setSeparatorsCollapsible( false );
+  mBtnFilterOptions->setMenu( menu );
+  QAction *action = new QAction( tr( "Case Sensitive" ), menu );
+  action->setData( "case" );
+  action->setCheckable( true );
+  action->setChecked( false );
+  connect( action, &QAction::toggled, this, &QgsDataSourceSelectDialog::setCaseSensitive );
+  menu->addAction( action );
+  QActionGroup *group = new QActionGroup( menu );
+  action = new QAction( tr( "Filter Pattern Syntax" ), group );
+  action->setSeparator( true );
+  menu->addAction( action );
+  action = new QAction( tr( "Normal" ), group );
+  action->setData( QgsBrowserProxyModel::Normal );
+  action->setCheckable( true );
+  action->setChecked( true );
+  menu->addAction( action );
+  action = new QAction( tr( "Wildcard(s)" ), group );
+  action->setData( QgsBrowserProxyModel::Wildcards );
+  action->setCheckable( true );
+  menu->addAction( action );
+  action = new QAction( tr( "Regular Expression" ), group );
+  action->setData( QgsBrowserProxyModel::RegularExpression );
+  action->setCheckable( true );
+  menu->addAction( action );
+
+  mBrowserTreeView->setExpandsOnDoubleClick( false );
+
+  connect( mActionRefresh, &QAction::triggered, [ = ] { refreshModel( QModelIndex() ); } );
   connect( mBrowserTreeView, &QgsBrowserTreeView::clicked, this, &QgsDataSourceSelectDialog::onLayerSelected );
+  connect( mActionCollapse, &QAction::triggered, mBrowserTreeView, &QgsBrowserTreeView::collapseAll );
+  connect( mActionShowFilter, &QAction::triggered, this, &QgsDataSourceSelectDialog::showFilterWidget );
+  connect( mLeFilter, &QgsFilterLineEdit::returnPressed, this, &QgsDataSourceSelectDialog::setFilter );
+  connect( mLeFilter, &QgsFilterLineEdit::cleared, this, &QgsDataSourceSelectDialog::setFilter );
+  connect( mLeFilter, &QgsFilterLineEdit::textChanged, this, &QgsDataSourceSelectDialog::setFilter );
+  connect( group, &QActionGroup::triggered, this, &QgsDataSourceSelectDialog::setFilterSyntax );
+
+  if ( QgsSettings().value( QStringLiteral( "datasourceSelectFilterVisible" ), false, QgsSettings::Section::Gui ).toBool() )
+  {
+    mActionShowFilter->trigger();
+  }
 }
 
 QgsDataSourceSelectDialog::~QgsDataSourceSelectDialog()
 {
   if ( ! mOwnModel )
     mBrowserModel.release();
+}
+
+
+void QgsDataSourceSelectDialog::showEvent( QShowEvent *e )
+{
+  QDialog::showEvent( e );
+  QString lastSelectedPath( QgsSettings().value( QStringLiteral( "datasourceSelectLastSelectedItem" ),
+                            QString(), QgsSettings::Section::Gui ).toString() );
+  if ( ! lastSelectedPath.isEmpty() )
+  {
+    QModelIndexList items = mBrowserProxyModel.match(
+                              mBrowserProxyModel.index( 0, 0 ),
+                              QgsBrowserModel::PathRole,
+                              QVariant::fromValue( lastSelectedPath ),
+                              1,
+                              Qt::MatchRecursive );
+    if ( items.count( ) > 0 )
+    {
+      QModelIndex expandIndex = items.at( 0 );
+      if ( expandIndex.isValid() )
+      {
+        mBrowserTreeView->scrollTo( expandIndex, QgsBrowserTreeView::QgsBrowserTreeView::ScrollHint::PositionAtTop );
+        mBrowserTreeView->expand( expandIndex );
+      }
+    }
+  }
+}
+
+void QgsDataSourceSelectDialog::showFilterWidget( bool visible )
+{
+  QgsSettings().setValue( QStringLiteral( "datasourceSelectFilterVisible" ), visible, QgsSettings::Section::Gui );
+  mWidgetFilter->setVisible( visible );
+  if ( ! visible )
+  {
+    mLeFilter->setText( QString() );
+    setFilter();
+  }
+  else
+  {
+    mLeFilter->setFocus();
+  }
+}
+
+void QgsDataSourceSelectDialog::setFilter()
+{
+  QString filter = mLeFilter->text();
+  mBrowserProxyModel.setFilterString( filter );
+}
+
+
+void QgsDataSourceSelectDialog::refreshModel( const QModelIndex &index )
+{
+
+  QgsDataItem *item = mBrowserModel->dataItem( index );
+  if ( item )
+  {
+    QgsDebugMsg( "path = " + item->path() );
+  }
+  else
+  {
+    QgsDebugMsg( QStringLiteral( "invalid item" ) );
+  }
+
+  if ( item && ( item->capabilities2() & QgsDataItem::Fertile ) )
+  {
+    mBrowserModel->refresh( index );
+  }
+
+  for ( int i = 0; i < mBrowserModel->rowCount( index ); i++ )
+  {
+    QModelIndex idx = mBrowserModel->index( i, 0, index );
+    QModelIndex proxyIdx = mBrowserProxyModel.mapFromSource( idx );
+    QgsDataItem *child = mBrowserModel->dataItem( idx );
+
+    // Check also expanded descendants so that the whole expanded path does not get collapsed if one item is collapsed.
+    // Fast items (usually root items) are refreshed so that when collapsed, it is obvious they are if empty (no expand symbol).
+    if ( mBrowserTreeView->isExpanded( proxyIdx ) || mBrowserTreeView->hasExpandedDescendant( proxyIdx ) || ( child && child->capabilities2() & QgsDataItem::Fast ) )
+    {
+      refreshModel( idx );
+    }
+    else
+    {
+      if ( child && ( child->capabilities2() & QgsDataItem::Fertile ) )
+      {
+        child->depopulate();
+      }
+    }
+  }
+}
+
+
+void QgsDataSourceSelectDialog::setFilterSyntax( QAction *action )
+{
+  if ( !action )
+    return;
+  mBrowserProxyModel.setFilterSyntax( static_cast< QgsBrowserProxyModel::FilterSyntax >( action->data().toInt() ) );
+}
+
+void QgsDataSourceSelectDialog::setCaseSensitive( bool caseSensitive )
+{
+  mBrowserProxyModel.setFilterCaseSensitivity( caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive );
 }
 
 void QgsDataSourceSelectDialog::setLayerTypeFilter( QgsMapLayer::LayerType layerType )
@@ -95,6 +247,8 @@ void QgsDataSourceSelectDialog::onLayerSelected( const QModelIndex &index )
       {
         isLayerCompatible = true;
         mUri = layerItem->mimeUri();
+        // Store last viewed item
+        QgsSettings().setValue( QStringLiteral( "datasourceSelectLastSelectedItem" ),  mBrowserProxyModel.data( index, QgsBrowserModel::PathRole ).toString(), QgsSettings::Section::Gui );
       }
     }
   }
