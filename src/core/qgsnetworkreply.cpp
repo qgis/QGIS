e4959a6b9ac922a7ad327dd0d0ad426ad3eff851
Nyall Dawson <nyall.dawson@gmail.com>
2018-11-08 17:11:59 +1000

    New class QgsNetworkReplyContent
    Encapsulates a network reply within a container which
    is inexpensive to copy and safe to pass around between threads.
    The default Qt QNetworkReply class is a QObject, which prevents
    it from being copied and passed between threads. This class
    grabs all the useful information from a QNetworkReply,
    allowing the reply's content to be stored indefinetly without
    concern for the lifetime of the QNetworkReply object itself.

---
+++ src/core/qgsnetworkreply.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************
+    qgsnetworkreply.cpp
+    -------------------
+    begin                : November 2018
+    copyright            : (C) 2018 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsnetworkreply.h"
+#include <QNetworkReply>
+
+QgsNetworkReplyContent::QgsNetworkReplyContent( QNetworkReply *reply )
+  : mContent( reply->readAll() )
+  , mError( reply->error() )
+  , mErrorString( reply->errorString() )
+  , mRawHeaderPairs( reply->rawHeaderPairs() )
+{}
+
+void QgsNetworkReplyContent::clear()
+{
+  *this = QgsNetworkReplyContent();
+}
+
+bool QgsNetworkReplyContent::hasRawHeader( const QByteArray &headerName ) const
+{
+  for ( auto &header : mRawHeaderPairs )
+  {
+    if ( header.first == headerName )
+      return true;
+  }
+  return false;
+}
+
+QList<QByteArray> QgsNetworkReplyContent::rawHeaderList() const
+{
+  QList< QByteArray > res;
+  res.reserve( mRawHeaderPairs.length() );
+  for ( auto &header : mRawHeaderPairs )
+  {
+    res << header.first;
+  }
+  return res;
+}
+
+QByteArray QgsNetworkReplyContent::rawHeader( const QByteArray &headerName ) const
+{
+  for ( auto &header : mRawHeaderPairs )
+  {
+    if ( header.first == headerName )
+      return header.second;
+  }
+  return QByteArray();
+}
