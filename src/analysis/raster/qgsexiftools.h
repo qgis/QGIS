fd383b94dc8159dec65a1cfff0b7f3d95ac478fb
Nyall Dawson <nyall.dawson@gmail.com>
2018-11-09 08:33:12 +1000

    [FEATURE][API] New class QgsExifTools
    Contains utilities for retrieving the geotag from images and
    for setting an image's geotag.
    
    Working with geotags (before this class!) is super-annoying
    and fiddly and relies on either parsing command line tools
    or depending on non-standard Python libraries which are
    not available everywhere, and often very difficult for users
    on certain platforms to get installed and working correctly.
    
    With this class we have stable methods for geotag getting/setting
    which are universally available and can be used safely
    by plugins and scripts.

---
+++ src/analysis/raster/qgsexiftools.h
@@ -0,0 +1,80 @@
+/***************************************************************************
+  qgisexiftools.h
+  ---------------
+  Date                 : November 2018
+  Copyright            : (C) 2018 by Nyall Dawson
+  Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSEXIFTOOLS_H
+#define QGSEXIFTOOLS_H
+
+#include "qgis_analysis.h"
+#include "qgspointxy.h"
+#include <QString>
+#include <QVariant>
+#include <QVariantMap>
+
+/**
+ * Contains utilities for working with EXIF tags in images.
+ * \ingroup analysis
+ * \since QGIS 3.6
+ */
+class ANALYSIS_EXPORT QgsExifTools
+{
+  public:
+
+#if 0
+    static QVariantMap readTags( const QString &imagePath );
+#endif
+
+    /**
+     * Returns the geotagged coordinate stored in the image at \a imagePath.
+     *
+     * If a geotag was found, \a ok will be set to true.
+     *
+     * If the image contains an elevation tag then the returned point will contain
+     * the elevation as a z value.
+     *
+     * \see geoTagImage()
+     */
+    static QgsPoint getGeoTag( const QString &imagePath, bool &ok SIP_OUT );
+
+    /**
+     * Extended image geotag details.
+     */
+    class GeoTagDetails
+    {
+      public:
+
+        GeoTagDetails()
+          : elevation( std::numeric_limits< double >::quiet_NaN() )
+        {
+        }
+
+        /**
+         * GPS elevation, or NaN if elevation is not available.
+         */
+        double elevation = 0;
+    };
+
+    /**
+     * Writes geotags to the image at \a imagePath.
+     *
+     * The \a location argument indicates the GPS location to write to the image, as a WGS84 latitude/longitude coordinate.
+     *
+     * If desired, extended GPS tags (such as elevation) can be specified via the \a details argument.
+     *
+     * Returns true if writing was successful.
+     */
+    static bool geotagImage( const QString &imagePath, const QgsPointXY &location, const GeoTagDetails &details = QgsExifTools::GeoTagDetails() );
+};
+
+#endif // QGSEXIFTOOLS_H
