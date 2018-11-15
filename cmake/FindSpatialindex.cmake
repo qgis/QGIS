0e4120d2568a06f37f60129bccffc7986255b728
Simon Wells <simonrwells@gmail.com>
2018-11-16 10:47:16 +1300

    Remove PATH_SUFFIX as it is no longer needed, as it is optional it is not sufficient to be used in place of spatialindex/

--- cmake/FindSpatialindex.cmake
+++ cmake/FindSpatialindex.cmake
@@ -17,7 +17,6 @@ FIND_PATH(SPATIALINDEX_INCLUDE_DIR spatialindex/SpatialIndex.h PATHS
   "$ENV{LIB_DIR}/include"
   "$ENV{INCLUDE}"
   "$ENV{OSGEO4W_ROOT}/include"
-  PATH_SUFFIXES spatialindex
   )
 
 FIND_LIBRARY(SPATIALINDEX_LIBRARY NAMES spatialindex_i spatialindex spatialindex-64 PATHS
