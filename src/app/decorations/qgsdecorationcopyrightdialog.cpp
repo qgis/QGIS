2a5c5024dce06bab02c78faebea0cd343cce04b7
nirvn <nirvn.asia@gmail.com>
2018-11-15 10:47:34 +0700

    Improve capitalization of decoration dialogs

--- src/app/decorations/qgsdecorationcopyrightdialog.cpp
+++ src/app/decorations/qgsdecorationcopyrightdialog.cpp
@@ -60,12 +60,12 @@ QgsDecorationCopyrightDialog::QgsDecorationCopyrightDialog( QgsDecorationCopyrig
   }
 
   // placement
-  cboPlacement->addItem( tr( "Top left" ), QgsDecorationItem::TopLeft );
-  cboPlacement->addItem( tr( "Top center" ), QgsDecorationItem::TopCenter );
-  cboPlacement->addItem( tr( "Top right" ), QgsDecorationItem::TopRight );
-  cboPlacement->addItem( tr( "Bottom left" ), QgsDecorationItem::BottomLeft );
-  cboPlacement->addItem( tr( "Bottom center" ), QgsDecorationItem::BottomCenter );
-  cboPlacement->addItem( tr( "Bottom right" ), QgsDecorationItem::BottomRight );
+  cboPlacement->addItem( tr( "Top Left" ), QgsDecorationItem::TopLeft );
+  cboPlacement->addItem( tr( "Top Center" ), QgsDecorationItem::TopCenter );
+  cboPlacement->addItem( tr( "Top Right" ), QgsDecorationItem::TopRight );
+  cboPlacement->addItem( tr( "Bottom Left" ), QgsDecorationItem::BottomLeft );
+  cboPlacement->addItem( tr( "Bottom Center" ), QgsDecorationItem::BottomCenter );
+  cboPlacement->addItem( tr( "Bottom Right" ), QgsDecorationItem::BottomRight );
   cboPlacement->setCurrentIndex( cboPlacement->findData( mDeco.placement() ) );
   spnHorizontal->setValue( mDeco.mMarginHorizontal );
   spnVertical->setValue( mDeco.mMarginVertical );
