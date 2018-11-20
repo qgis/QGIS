17e58738b8f151a06af26fea146cd190a9bdbd2a
Nyall Dawson <nyall.dawson@gmail.com>
2018-11-21 06:57:13 +1000

    Capitalization

--- src/app/qgsvectorlayerproperties.cpp
+++ src/app/qgsvectorlayerproperties.cpp
@@ -917,8 +917,8 @@ void QgsVectorLayerProperties::loadDefaultStyle_clicked()
     askToUser.setText( tr( "Load default style from: " ) );
     askToUser.setIcon( QMessageBox::Question );
     askToUser.addButton( tr( "Cancel" ), QMessageBox::RejectRole );
-    askToUser.addButton( tr( "Local database" ), QMessageBox::NoRole );
-    askToUser.addButton( tr( "Datasource database" ), QMessageBox::YesRole );
+    askToUser.addButton( tr( "Local Database" ), QMessageBox::NoRole );
+    askToUser.addButton( tr( "Datasource Database" ), QMessageBox::YesRole );
 
     switch ( askToUser.exec() )
     {
@@ -972,8 +972,8 @@ void QgsVectorLayerProperties::saveDefaultStyle_clicked()
     askToUser.setText( tr( "Save default style to: " ) );
     askToUser.setIcon( QMessageBox::Question );
     askToUser.addButton( tr( "Cancel" ), QMessageBox::RejectRole );
-    askToUser.addButton( tr( "Local database" ), QMessageBox::NoRole );
-    askToUser.addButton( tr( "Datasource database" ), QMessageBox::YesRole );
+    askToUser.addButton( tr( "Local Database" ), QMessageBox::NoRole );
+    askToUser.addButton( tr( "Datasource Database" ), QMessageBox::YesRole );
 
     switch ( askToUser.exec() )
     {
--- src/gui/qgsmaplayerstylemanagerwidget.cpp
+++ src/gui/qgsmaplayerstylemanagerwidget.cpp
@@ -189,8 +189,8 @@ void QgsMapLayerStyleManagerWidget::saveAsDefault()
       askToUser.setText( tr( "Save default style to: " ) );
       askToUser.setIcon( QMessageBox::Question );
       askToUser.addButton( tr( "Cancel" ), QMessageBox::RejectRole );
-      askToUser.addButton( tr( "Local database" ), QMessageBox::NoRole );
-      askToUser.addButton( tr( "Datasource database" ), QMessageBox::YesRole );
+      askToUser.addButton( tr( "Local Database" ), QMessageBox::NoRole );
+      askToUser.addButton( tr( "Datasource Database" ), QMessageBox::YesRole );
 
       switch ( askToUser.exec() )
       {
@@ -232,8 +232,8 @@ void QgsMapLayerStyleManagerWidget::loadDefault()
       askToUser.setText( tr( "Load default style from: " ) );
       askToUser.setIcon( QMessageBox::Question );
       askToUser.addButton( tr( "Cancel" ), QMessageBox::RejectRole );
-      askToUser.addButton( tr( "Local database" ), QMessageBox::NoRole );
-      askToUser.addButton( tr( "Datasource database" ), QMessageBox::YesRole );
+      askToUser.addButton( tr( "Local Database" ), QMessageBox::NoRole );
+      askToUser.addButton( tr( "Datasource Database" ), QMessageBox::YesRole );
 
       switch ( askToUser.exec() )
       {
