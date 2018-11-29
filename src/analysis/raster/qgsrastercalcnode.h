1e5e373641a2faa1760632612ef6380353b5dd80
Alessandro Pasotti <elpaso@itopen.it>
2018-11-29 09:32:24 +0100

    Merge pull request #8564 from elpaso/rastercalculator-fast-track
    Rastercalculator memory efficient track
    
    Cherry-picked from master 0fde410a585c5b1c256b24a6afa8c1dce59ac10e

--- src/analysis/raster/qgsrastercalcnode.h
+++ src/analysis/raster/qgsrastercalcnode.h
@@ -106,6 +106,19 @@ class ANALYSIS_EXPORT QgsRasterCalcNode
      */
     bool calculate( QMap<QString, QgsRasterBlock * > &rasterData, QgsRasterMatrix &result, int row = -1 ) const SIP_SKIP;
 
+    /**
+     * Returns a string representation of the expression
+     * \param cStyle if true operators will follow C syntax
+     * \since QGIS 3.6
+     */
+    QString toString( bool cStyle = false ) const;
+
+    /**
+     * Returns a list of nodes of a specific \a type
+     * \since QGIS 3.6
+     */
+    QList<const QgsRasterCalcNode *> findNodes( const QgsRasterCalcNode::Type type ) const;
+
     static QgsRasterCalcNode *parseRasterCalcString( const QString &str, QString &parserErrorMsg ) SIP_FACTORY;
 
   private:
