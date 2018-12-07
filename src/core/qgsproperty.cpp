7218cf5f593975b96d66aa1df1d2640e675e3f91
Nyall Dawson <nyall.dawson@gmail.com>
2018-12-07 19:27:06 +1000

    Fix help text for color format
    Should be #AARRGGBB, not #RRGGBBAA (see http://doc.qt.io/qt-5/qcolor.html#setNamedColor)
    
    (cherry picked from commit 462ebed0d7902bf08956033b18be56f3d8d840bc)

--- src/core/qgsproperty.cpp
+++ src/core/qgsproperty.cpp
@@ -87,7 +87,7 @@ QgsPropertyDefinition::QgsPropertyDefinition( const QString &name, const QString
 
     case ColorWithAlpha:
       mTypes = DataTypeString;
-      mHelpText = QObject::tr( "string [<b>r,g,b,a</b>] as int 0-255 or #<b>RRGGBBAA</b> as hex or <b>color</b> as color's name" );
+      mHelpText = QObject::tr( "string [<b>r,g,b,a</b>] as int 0-255 or #<b>AARRGGBB</b> as hex or <b>color</b> as color's name" );
       break;
 
     case ColorNoAlpha:
