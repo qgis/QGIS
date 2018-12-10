a49bf9f68f87e8b58fc7bf47c40fe8c246e927fc
Nyall Dawson <nyall.dawson@gmail.com>
2018-12-10 15:32:50 +1000

    Unit tests for item opacity

--- tests/src/python/test_qgslayoutitem.py
+++ tests/src/python/test_qgslayoutitem.py
@@ -199,6 +199,45 @@ class TestQgsLayoutItem(unittest.TestCase):
         label2 = [i for i in items if isinstance(i, QgsLayoutItem) and i.id() == 'label'][0]
         self.assertIsInstance(label2, QgsLayoutItemLabel)
 
+    def testContainsAdvancedEffectsAndRasterization(self):
+        layout = QgsLayout(QgsProject.instance())
+        item = QgsLayoutItemLabel(layout)
+
+        self.assertFalse(item.containsAdvancedEffects())
+
+        # item opacity requires that the individual item be flattened to a raster item
+        item.setItemOpacity(0.5)
+        self.assertTrue(item.containsAdvancedEffects())
+        # but not the WHOLE layout
+        self.assertFalse(item.requiresRasterization())
+        item.dataDefinedProperties().setProperty(QgsLayoutObject.Opacity, QgsProperty.fromExpression('100'))
+        item.refresh()
+        self.assertFalse(item.containsAdvancedEffects())
+        self.assertFalse(item.requiresRasterization())
+        item.dataDefinedProperties().setProperty(QgsLayoutObject.Opacity, QgsProperty())
+        item.refresh()
+        self.assertTrue(item.containsAdvancedEffects())
+        self.assertFalse(item.requiresRasterization())
+        item.setItemOpacity(1.0)
+        self.assertFalse(item.containsAdvancedEffects())
+        self.assertFalse(item.requiresRasterization())
+
+        # item blend mode is NOT an advanced effect -- rather it requires that the WHOLE layout be rasterized to achieve
+        item.setBlendMode(QPainter.CompositionMode_DestinationAtop)
+        self.assertFalse(item.containsAdvancedEffects())
+        self.assertTrue(item.requiresRasterization())
+
+        map = QgsLayoutItemMap(layout)
+        # map items are different -- because they override paint, they don't get the auto-flattening and rasterization
+        map.setItemOpacity(0.5)
+        self.assertFalse(map.containsAdvancedEffects())
+        # rather, a map with opacity requires the WHOLE layout to be rasterized
+        self.assertTrue(map.requiresRasterization())
+        map.dataDefinedProperties().setProperty(QgsLayoutObject.Opacity, QgsProperty.fromExpression('100'))
+        map.refresh()
+        self.assertFalse(map.containsAdvancedEffects())
+        self.assertTrue(map.requiresRasterization())
+
 
 if __name__ == '__main__':
     unittest.main()
