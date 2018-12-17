44856919913f940b490bb117c9b975b9a02c6cd9
Nyall Dawson <nyall.dawson@gmail.com>
2018-12-17 13:31:50 +1000

    [processing] Add API to set default unit to distance parameters
    Otherwise the API forces use of a corresponding CRS parameter in
    order to determine the relevant distance unit, but this is not
    always desirable
    
    (cherry picked from commit 5acd7ffaefff4a0936e14e1be9da5e8445acd82e)

--- src/gui/processing/qgsprocessingwidgetwrapperimpl.cpp
+++ src/gui/processing/qgsprocessingwidgetwrapperimpl.cpp
@@ -661,6 +661,8 @@ QgsAbstractProcessingParameterWidgetWrapper *QgsProcessingDistanceWidgetWrapper:
 
 QWidget *QgsProcessingDistanceWidgetWrapper::createWidget()
 {
+  const QgsProcessingParameterDistance *distanceDef = static_cast< const QgsProcessingParameterDistance * >( parameterDefinition() );
+
   QWidget *spin = QgsProcessingNumericWidgetWrapper::createWidget();
   switch ( type() )
   {
@@ -699,7 +701,7 @@ QWidget *QgsProcessingDistanceWidgetWrapper::createWidget()
       mWarningLabel->setLayout( warningLayout );
       layout->insertWidget( 4, mWarningLabel );
 
-      setUnits( QgsUnitTypes::DistanceUnknownUnit );
+      setUnits( distanceDef->defaultUnit() );
 
       QWidget *w = new QWidget();
       layout->setMargin( 0 );
