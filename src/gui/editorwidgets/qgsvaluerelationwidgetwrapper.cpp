74545e83759ebe5810d4bac7247384c2fd10559b
signedav <david@opengis.ch>
2018-12-20 13:00:47 +0100

    fix for-clause

--- src/gui/editorwidgets/qgsvaluerelationwidgetwrapper.cpp
+++ src/gui/editorwidgets/qgsvaluerelationwidgetwrapper.cpp
@@ -74,7 +74,7 @@ QVariant QgsValueRelationWidgetWrapper::value() const
     {
       QVariantList vl;
       //store as QVariantList because it's json
-      for ( const QString &s, qgis::as_const( selection ) )
+      for ( const QString &s : qgis::as_const( selection ) )
       {
         vl << s;
       }
