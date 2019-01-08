948048cd4d2e369526ac101af2841acffe25ba94
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2019-01-08 14:13:50 +0100

    Do not overwrite renderContext in QgsMapRendererJob::drawLabeling
    Dedicated QgsRenderContext is already created and configured in prepareLabelingJob.

--- src/core/qgsmaprendererjob.h
+++ src/core/qgsmaprendererjob.h
@@ -286,8 +286,14 @@ class CORE_EXPORT QgsMapRendererJob : public QObject
      */
     void cleanupLabelJob( LabelRenderJob &job ) SIP_SKIP;
 
+    /**
+     * \note not available in Python bindings
+     * \deprecated Will be removed in QGIS 4.0
+     */
+    Q_DECL_DEPRECATED static void drawLabeling( const QgsMapSettings &settings, QgsRenderContext &renderContext, QgsLabelingEngine *labelingEngine2, QPainter *painter ) SIP_SKIP;
+
     //! \note not available in Python bindings
-    static void drawLabeling( const QgsMapSettings &settings, QgsRenderContext &renderContext, QgsLabelingEngine *labelingEngine2, QPainter *painter ) SIP_SKIP;
+    static void drawLabeling( QgsRenderContext &renderContext, QgsLabelingEngine *labelingEngine2, QPainter *painter ) SIP_SKIP;
 
   private:
 
