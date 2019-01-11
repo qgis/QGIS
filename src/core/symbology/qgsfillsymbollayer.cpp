8707eee272f164ef7ec8d4add60827b09110036e
Nyall Dawson <nyall.dawson@gmail.com>
2019-01-11 15:41:43 +1000

    Fix incorrect evaluation of data defined end color for shapeburst fills

--- src/core/symbology/qgsfillsymbollayer.cpp
+++ src/core/symbology/qgsfillsymbollayer.cpp
@@ -1073,7 +1073,7 @@ void QgsShapeburstFillSymbolLayer::applyDataDefinedSymbology( QgsSymbolRenderCon
   if ( mDataDefinedProperties.isActive( QgsSymbolLayer::PropertySecondaryColor ) )
   {
     context.setOriginalValueVariable( QgsSymbolLayerUtils::encodeColor( mColor2 ) );
-    color = mDataDefinedProperties.valueAsColor( QgsSymbolLayer::PropertySecondaryColor, context.renderContext().expressionContext(), mColor2 );
+    color2 = mDataDefinedProperties.valueAsColor( QgsSymbolLayer::PropertySecondaryColor, context.renderContext().expressionContext(), mColor2 );
   }
 
   //blur radius
