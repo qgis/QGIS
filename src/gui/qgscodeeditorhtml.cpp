710d568c402a5bb82cc4990a3a42073a88637833
Mathieu Pellerin <nirvn.asia@gmail.com>
2019-01-16 10:47:43 +0700

    Improve code editor default color scheme to fix dark background issues (this also backports night mapping code editor color scheme)

--- src/gui/qgscodeeditorhtml.cpp
+++ src/gui/qgscodeeditorhtml.cpp
@@ -15,6 +15,7 @@
 
 #include "qgsapplication.h"
 #include "qgscodeeditorhtml.h"
+#include "qgssymbollayerutils.h"
 
 #include <QWidget>
 #include <QString>
@@ -37,14 +38,36 @@ QgsCodeEditorHTML::QgsCodeEditorHTML( QWidget *parent )
 
 void QgsCodeEditorHTML::setSciLexerHTML()
 {
+  QHash< QString, QColor > colors;
+  if ( QgsApplication::instance()->themeName() != QStringLiteral( "default" ) )
+  {
+    QSettings ini( QgsApplication::instance()->uiThemes().value( QgsApplication::instance()->themeName() ) + "/qscintilla.ini", QSettings::IniFormat );
+    for ( const auto &key : ini.allKeys() )
+    {
+      colors.insert( key, QgsSymbolLayerUtils::decodeColor( ini.value( key ).toString() ) );
+    }
+  }
+
   QFont font = getMonospaceFont();
 #ifdef Q_OS_MAC
   // The font size gotten from getMonospaceFont() is too small on Mac
   font.setPointSize( QLabel().font().pointSize() );
 #endif
+  QColor defaultColor = colors.value( QStringLiteral( "html/defaultFontColor" ), Qt::black );
 
   QsciLexerHTML *lexer = new QsciLexerHTML( this );
   lexer->setDefaultFont( font );
+  lexer->setDefaultColor( defaultColor );
+  lexer->setDefaultPaper( colors.value( QStringLiteral( "html/paperBackgroundColor" ), Qt::white ) );
   lexer->setFont( font, -1 );
+
+  lexer->setColor( defaultColor, QsciLexerHTML::Default );
+  lexer->setColor( colors.value( QStringLiteral( "html/tagFontColor" ), QColor( 66, 113, 174 ) ), QsciLexerHTML::Tag );
+  lexer->setColor( colors.value( QStringLiteral( "html/unknownTagFontColor" ), QColor( 255, 0, 0 ) ), QsciLexerHTML::UnknownTag );
+  lexer->setColor( colors.value( QStringLiteral( "html/numberFontColor" ), QColor( 200, 40, 41 ) ), QsciLexerHTML::HTMLNumber );
+  lexer->setColor( colors.value( QStringLiteral( "html/commentFontColor" ), QColor( 142, 144, 140 ) ), QsciLexerHTML::HTMLComment );
+  lexer->setColor( colors.value( QStringLiteral( "html/singleQuoteFontColor" ), QColor( 113, 140, 0 ) ), QsciLexerHTML::HTMLSingleQuotedString );
+  lexer->setColor( colors.value( QStringLiteral( "html/doubleQuoteFontColor" ), QColor( 113, 140, 0 ) ), QsciLexerHTML::HTMLDoubleQuotedString );
+
   setLexer( lexer );
 }
