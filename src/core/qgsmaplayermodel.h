5fa5631bbdce5add0c1ad10268805d9d0d9b9936
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-01-17 09:40:13 -0500

    add enum value to key and reverse helpers (#8879)
    * add enum value to key and reverse helpers
    
    * cast cont char* to QString
    
    * use QStringLiteral

--- src/core/qgsmaplayermodel.h
+++ src/core/qgsmaplayermodel.h
@@ -52,6 +52,7 @@ class CORE_EXPORT QgsMapLayerModel : public QAbstractItemModel
       EmptyRole, //!< True if index corresponds to the empty (not set) value
       AdditionalRole, //!< True if index corresponds to an additional (non map layer) item
     };
+    Q_ENUM( ItemDataRole )
 
     /**
      * \brief QgsMapLayerModel creates a model to display layers in widgets.
