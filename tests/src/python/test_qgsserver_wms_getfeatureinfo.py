3b452869ea1f135f1b9f666a932f5def2c994a5b
Blottiere Paul <blottiere.paul@gmail.com>
2019-01-18 16:55:04 +0000

    Add unit test for application/geo+json format

--- tests/src/python/test_qgsserver_wms_getfeatureinfo.py
+++ tests/src/python/test_qgsserver_wms_getfeatureinfo.py
@@ -433,10 +433,19 @@ class TestQgsServerWMSGetFeatureInfo(TestQgsServerWMSTestBase):
                                  'test_project_values.qgz')
 
     def testGetFeatureInfoJSON(self):
-        # simple test without geometry
+        # simple test without geometry and info_format=application/json
         self.wms_request_compare('GetFeatureInfo',
                                  '&layers=testlayer%20%C3%A8%C3%A9&styles=&' +
                                  'info_format=application%2Fjson&transparent=true&' +
+                                 'width=600&height=400&srs=EPSG%3A3857&bbox=913190.6389747962%2C' +
+                                 '5606005.488876367%2C913235.426296057%2C5606035.347090538&' +
+                                 'query_layers=testlayer%20%C3%A8%C3%A9&X=190&Y=320',
+                                 'wms_getfeatureinfo_json')
+
+        # simple test without geometry and info_format=application/geo+json
+        self.wms_request_compare('GetFeatureInfo',
+                                 '&layers=testlayer%20%C3%A8%C3%A9&styles=&' +
+                                 'info_format=application%2Fgeo+json&transparent=true&' +
                                  'width=600&height=400&srs=EPSG%3A3857&bbox=913190.6389747962%2C' +
                                  '5606005.488876367%2C913235.426296057%2C5606035.347090538&' +
                                  'query_layers=testlayer%20%C3%A8%C3%A9&X=190&Y=320',
