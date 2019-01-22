54767928d7fa49edebe211b4f861deb34b9a4afd
Alex <roya0045@users.noreply.github.com>
2019-01-22 10:28:55 -0500

    Whitespaces

--- src/core/expression/qgsexpressionfunction.cpp
+++ src/core/expression/qgsexpressionfunction.cpp
@@ -568,7 +568,7 @@ static QVariant fcnAggregate( const QVariantList &values, const QgsExpressionCon
     if ( context && context->hasCachedValue( cacheKey ) )
       return context->cachedValue( cacheKey );
 
-    if ( context->name() == "Symbol scope")
+    if ( context->name() == "Symbol scope" )
     {
       //QgsFeatureIds *fids = context->variable( "symbol_feature_ids" )
       result = vl->aggregate( aggregate, subExpression, parameters, &Context, &ok, context->variable( "symbol_feature_ids" ) );
@@ -743,7 +743,7 @@ static QVariant fcnAggregateGeneric( QgsAggregateCalculator::Aggregate aggregate
       parameters.filter = node->dump();
   }
 
-  if ( context->name() == "Symbol scope")
+  if ( context->name() == "Symbol scope" )
   {
     //QgsFeatureIds *fids = context->variable( "symbol_feature_ids" )
     result = vl->aggregate( aggregate, subExpression, parameters, &Context, &ok, context->variable( "symbol_feature_ids" ) );
