39f7d9a8e6e91e7abb7a7c11def6428d1003bbb3
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2019-01-23 09:38:55 +0100

    fix include

--- src/quickgui/qgsquickutils.cpp
+++ src/quickgui/qgsquickutils.cpp
@@ -22,6 +22,7 @@
 #include "qgslogger.h"
 #include "qgsvectorlayer.h"
 #include "qgsfeature.h"
+#include "qgsapplication.h"
 
 #include "qgsquickfeaturelayerpair.h"
 #include "qgsquickmapsettings.h"
