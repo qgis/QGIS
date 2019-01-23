bdf77ac75a94ea37feaa3d7d015af57e0d9a7e1f
Julien Cabieces <julien.cabieces@oslandia.com>
2019-01-23 08:24:38 +0100

    fixes #20893 : Update virtual layer when referenced layer update its fields

--- src/providers/virtual/qgsvirtuallayerprovider.h
+++ src/providers/virtual/qgsvirtuallayerprovider.h
@@ -111,11 +111,13 @@ class QgsVirtualLayerProvider: public QgsVectorDataProvider
     bool openIt();
     bool createIt();
     bool loadSourceLayers();
+    void createVirtualTable( QgsVectorLayer *vlayer, const QString &name );
 
     friend class QgsVirtualLayerFeatureSource;
 
   private slots:
     void invalidateStatistics();
+
 };
 
 // clazy:excludeall=qstring-allocations
