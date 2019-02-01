dc2f679c5ced387500a86d30316d42998871306a
Alex <roya0045@users.noreply.github.com>
2019-02-01 15:41:57 -0500

    removing param

--- src/core/layertree/qgslayertreemodellegendnode.h
+++ src/core/layertree/qgslayertreemodellegendnode.h
@@ -260,10 +260,9 @@ class CORE_EXPORT QgsSymbolLegendNode : public QgsLayerTreeModelLegendNode
     /**
     * Evaluate the label with the proper context
     * \param label text to evaluate
-    * \param vl pointer to the vectorlayer
     * \since 3.8
     */
-    QString evaluateLabel( QString label, QgsVectorLayer *vl ) const;
+    QString evaluateLabel( QString label ) const;
 
   public slots:
 
