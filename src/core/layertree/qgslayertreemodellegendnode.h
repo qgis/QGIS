c50869c466809d4344b1483b9479752cd8250132
Alex <roya0045@users.noreply.github.com>
2019-02-01 15:54:17 -0500

    reverting

--- src/core/layertree/qgslayertreemodellegendnode.h
+++ src/core/layertree/qgslayertreemodellegendnode.h
@@ -260,9 +260,10 @@ class CORE_EXPORT QgsSymbolLegendNode : public QgsLayerTreeModelLegendNode
     /**
     * Evaluate the label with the proper context
     * \param label text to evaluate
+    * \param vl pointer to the vectorlayer
     * \since 3.8
     */
-    QString evaluateLabel( QString label ) const;
+    QString evaluateLabel( QString label, QgsVectorLayer *vl ) const;
 
   public slots:
 
