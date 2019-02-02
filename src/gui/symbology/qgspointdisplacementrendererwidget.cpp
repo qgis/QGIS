6fb06738ecfa9960ca673923be1bd42531dcdc75
henrik <2cXaJAyd>
2019-02-02 23:14:34 +0100

    [FEATURE] [needs-docs] Point Displacement label distance factor
    Adds label distance factor spinbox to Point Displacement symbology
    window. Calculates label distance based on the current symbology
    diagonal instead of a general maximum diagonal.
    
    Replaces the hard coded distance "symbolDiagonal/2.0" with
    "symbolDiagonal*mLabelDistanceFactor", where mLabelDistanceFactor is set
    to the spinbox value.
    
     It's an old feature request (Point displacement labels adjustable
    offsets "https://issues.qgis.org/issues/5945").

--- src/gui/symbology/qgspointdisplacementrendererwidget.cpp
+++ src/gui/symbology/qgspointdisplacementrendererwidget.cpp
@@ -58,6 +58,7 @@ QgsPointDisplacementRendererWidget::QgsPointDisplacementRendererWidget( QgsVecto
   connect( mDistanceUnitWidget, &QgsUnitSelectionWidget::changed, this, &QgsPointDisplacementRendererWidget::mDistanceUnitWidget_changed );
   connect( mLabelColorButton, &QgsColorButton::colorChanged, this, &QgsPointDisplacementRendererWidget::mLabelColorButton_colorChanged );
   connect( mCircleModificationSpinBox, static_cast < void ( QDoubleSpinBox::* )( double ) > ( &QDoubleSpinBox::valueChanged ), this, &QgsPointDisplacementRendererWidget::mCircleModificationSpinBox_valueChanged );
+  connect( mLabelDistanceFactorSpinBox, static_cast < void ( QDoubleSpinBox::* )( double ) > ( &QDoubleSpinBox::valueChanged ), this, &QgsPointDisplacementRendererWidget::mLabelDistanceFactorSpinBox_valueChanged );
   connect( mScaleDependentLabelsCheckBox, &QCheckBox::stateChanged, this, &QgsPointDisplacementRendererWidget::mScaleDependentLabelsCheckBox_stateChanged );
   connect( mRendererSettingsButton, &QPushButton::clicked, this, &QgsPointDisplacementRendererWidget::mRendererSettingsButton_clicked );
   this->layout()->setContentsMargins( 0, 0, 0, 0 );
@@ -130,6 +131,8 @@ QgsPointDisplacementRendererWidget::QgsPointDisplacementRendererWidget( QgsVecto
   mLabelFontButton->setCurrentFont( mRenderer->labelFont() );
   mCircleModificationSpinBox->setClearValue( 0.0 );
   mCircleModificationSpinBox->setValue( mRenderer->circleRadiusAddition() );
+  mLabelDistanceFactorSpinBox->setClearValue( 0.5 );
+  mLabelDistanceFactorSpinBox->setValue( mRenderer->labelDistanceFactor() );
   mDistanceSpinBox->setValue( mRenderer->tolerance() );
   mDistanceUnitWidget->setUnit( mRenderer->toleranceUnit() );
   mDistanceUnitWidget->setMapUnitScale( mRenderer->toleranceMapUnitScale() );
@@ -336,6 +339,17 @@ void QgsPointDisplacementRendererWidget::mCircleModificationSpinBox_valueChanged
   emit widgetChanged();
 }
 
+void QgsPointDisplacementRendererWidget::mLabelDistanceFactorSpinBox_valueChanged( double d )
+{
+  if ( !mRenderer )
+  {
+    return;
+  }
+
+  mRenderer->setLabelDistanceFactor( d );
+  emit widgetChanged();
+}
+
 void QgsPointDisplacementRendererWidget::mDistanceSpinBox_valueChanged( double d )
 {
   if ( mRenderer )
@@ -388,6 +402,7 @@ void QgsPointDisplacementRendererWidget::blockAllSignals( bool block )
   mRendererComboBox->blockSignals( block );
   mLabelColorButton->blockSignals( block );
   mCircleModificationSpinBox->blockSignals( block );
+  mLabelDistanceFactorSpinBox->blockSignals( block );
   mScaleDependentLabelsCheckBox->blockSignals( block );
   mMinLabelScaleWidget->blockSignals( block );
   mCenterSymbolToolButton->blockSignals( block );
