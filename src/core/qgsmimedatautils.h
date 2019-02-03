bca829be2bc75986c4431a84b5661fe52f846fbb
Nyall Dawson <nyall.dawson@gmail.com>
2019-02-04 08:18:37 +1000

    Slight improvements to Python docs
    (cherry picked from commit 4b0f6b756c61e02e60322ac30658cadb6cbe5c31)

--- src/core/qgsmimedatautils.h
+++ src/core/qgsmimedatautils.h
@@ -52,21 +52,21 @@ class CORE_EXPORT QgsMimeDataUtils
       QString data() const;
 
       /**
-       * Gets vector layer from uri if possible, otherwise returns 0 and error is set
+       * Gets vector layer from uri if possible, otherwise returns nullptr and error is set
        * \param owner set to true if caller becomes owner
        * \param error set to error message if cannot get vector
        */
       QgsVectorLayer *vectorLayer( bool &owner, QString &error ) const;
 
       /**
-       * Gets raster layer from uri if possible, otherwise returns 0 and error is set
+       * Gets raster layer from uri if possible, otherwise returns nullptr and error is set
        * \param owner set to true if caller becomes owner
        * \param error set to error message if cannot get raster
        */
       QgsRasterLayer *rasterLayer( bool &owner, QString &error ) const;
 
       /**
-       * Gets mesh layer from uri if possible, otherwise returns 0 and error is set
+       * Gets mesh layer from uri if possible, otherwise returns nullptr and error is set
        * \param owner set to true if caller becomes owner
        * \param error set to error message if cannot get raster
        */
