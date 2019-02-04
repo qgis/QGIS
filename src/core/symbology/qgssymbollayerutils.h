fef517663749a65afd6004369b5a31d330608418
Mathieu Pellerin <nirvn.asia@gmail.com>
2019-02-04 16:43:04 +0700

    [ui] Fix tiny vertex marker on hidpi

--- src/core/symbology/qgssymbollayerutils.h
+++ src/core/symbology/qgssymbollayerutils.h
@@ -55,6 +55,14 @@ class CORE_EXPORT QgsSymbolLayerUtils
 {
   public:
 
+    //! Editing vertex markers
+    enum VertexMarkerType
+    {
+      SemiTransparentCircle,
+      Cross,
+      NoMarker
+    };
+
     static QString encodeColor( const QColor &color );
     static QColor decodeColor( const QString &str );
 
@@ -233,6 +241,12 @@ class CORE_EXPORT QgsSymbolLayerUtils
     static QPixmap colorRampPreviewPixmap( QgsColorRamp *ramp, QSize size, int padding = 0 );
 
     static void drawStippledBackground( QPainter *painter, QRect rect );
+
+    /**
+     * Draws a vertex symbol at (painter) coordinates x, y. (Useful to assist vertex editing.)
+     * \since QGIS 3.4.5
+     */
+    static void drawVertexMarker( double x, double y, QPainter &p, QgsSymbolLayerUtils::VertexMarkerType type, int markerSize );
 
     //! Returns the maximum estimated bleed for the symbol
     static double estimateMaxSymbolBleed( QgsSymbol *symbol, const QgsRenderContext &context );
