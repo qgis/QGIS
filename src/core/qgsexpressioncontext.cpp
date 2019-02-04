42a295faafa9704a856f577881dbbb1dc5f9c1d0
Alex <roya0045@users.noreply.github.com>
2019-02-04 11:57:20 -0500

    reverting

--- src/core/qgsexpressioncontext.cpp
+++ src/core/qgsexpressioncontext.cpp
@@ -299,20 +299,6 @@ QgsExpressionContext &QgsExpressionContext::operator=( const QgsExpressionContex
   return *this;
 }
 
-QgsExpressionContext QgsExpressionContext::overwriteWith( QgsExpressionContext other )
-{
-  qDeleteAll( mStack );
-  mStack.clear();
-  Q_FOREACH ( const QgsExpressionContextScope *scope, other.mStack )
-  {
-    mStack << new QgsExpressionContextScope( *scope );
-  }
-  mHighlightedVariables = other.mHighlightedVariables;
-  mHighlightedFunctions = other.mHighlightedFunctions;
-  mCachedValues = other.mCachedValues;
-  return this;
-}
-
 QgsExpressionContext::~QgsExpressionContext()
 {
   qDeleteAll( mStack );
