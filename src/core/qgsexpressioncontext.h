22782cb3676633e13c640dad86832bbabdc9e992
Alex <roya0045@users.noreply.github.com>
2019-02-06 08:12:16 -0500

    Update qgsexpressioncontext.h

--- src/core/qgsexpressioncontext.h
+++ src/core/qgsexpressioncontext.h
@@ -583,7 +583,7 @@ class CORE_EXPORT QgsExpressionContext
      */
     QStringList functionNames() const;
 
-      /**
+    /**
      * Fetches a matching function from the context. The function will be fetched
      * from the last scope contained within the context which has a matching
      * function set.
