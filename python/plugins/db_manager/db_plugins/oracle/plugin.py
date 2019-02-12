ed62c29b78d51334321308fe2d13545861935419
Nyall Dawson <nyall.dawson@gmail.com>
2019-02-13 07:21:16 +1000

    Astyle

--- python/plugins/db_manager/db_plugins/oracle/plugin.py
+++ python/plugins/db_manager/db_plugins/oracle/plugin.py
@@ -80,7 +80,7 @@ class OracleDBPlugin(DBPlugin):
         conn_name = self.connectionName()
         settings = QgsSettings()
         settings.beginGroup(u"/{0}/{1}".format(
-                            self.connectionSettingsKey(), conn_name))
+            self.connectionSettingsKey(), conn_name))
 
         if not settings.contains("database"):  # non-existent entry?
             raise InvalidDataException(
@@ -408,18 +408,12 @@ class ORTable(Table):
             for idx in indexes:
                 if idx.isUnique and len(idx.columns) == 1:
                     fld = idx.fields()[idx.columns[0]]
-                    if (fld.dataType == u"NUMBER"
-                            and not fld.modifier
-                            and fld.notNull
-                            and fld not in ret):
+                    if (fld.dataType == u"NUMBER" and not fld.modifier and fld.notNull and fld not in ret):
                         ret.append(fld)
 
         # and finally append the other suitable fields
         for fld in self.fields():
-            if (fld.dataType == u"NUMBER"
-                    and not fld.modifier
-                    and fld.notNull
-                    and fld not in ret):
+            if (fld.dataType == u"NUMBER" and not fld.modifier and fld.notNull and fld not in ret):
                 ret.append(fld)
 
         if onlyOne:
@@ -519,15 +513,12 @@ class ORTableField(TableField):
 
         # find out whether fields are part of primary key
         for con in self.table().constraints():
-            if (con.type == ORTableConstraint.TypePrimaryKey
-                    and self.name == con.column):
+            if con.type == ORTableConstraint.TypePrimaryKey and self.name == con.column:
                 self.primaryKey = True
                 break
 
     def type2String(self):
-        if (u"TIMESTAMP" in self.dataType
-            or self.dataType in [u"DATE", u"SDO_GEOMETRY",
-                                 u"BINARY_FLOAT", u"BINARY_DOUBLE"]):
+        if (u"TIMESTAMP" in self.dataType or self.dataType in [u"DATE", u"SDO_GEOMETRY", u"BINARY_FLOAT", u"BINARY_DOUBLE"]):
             return u"{}".format(self.dataType)
         if self.charMaxLen in [None, -1]:
             return u"{}".format(self.dataType)
@@ -564,7 +555,6 @@ class ORTableField(TableField):
 
 
 class ORTableConstraint(TableConstraint):
-
     TypeCheck, TypeForeignKey, TypePrimaryKey, \
         TypeUnique, TypeUnknown = list(range(5))
 
