3e041b3a30cb7703cbe37bfe7e2a1df65a2ef1d6
Alessandro Pasotti <elpaso@itopen.it>
2019-02-13 15:09:20 +0100

    [ogr] Show style description instead of name in other styles
    Fixes #21254

--- src/providers/ogr/qgsogrprovider.cpp
+++ src/providers/ogr/qgsogrprovider.cpp
@@ -6317,7 +6317,7 @@ QGISEXTERN int listStyles( const QString &uri, QStringList &ids, QStringList &na
 
       listTimestamp.append( ts );
       mapIdToStyleName[fid] = styleName;
-      mapIdToDescription[fid] = styleName;
+      mapIdToDescription[fid] = description;
       mapTimestampToId[ts].append( fid );
     }
   }
