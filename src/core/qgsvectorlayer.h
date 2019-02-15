b33cb913a16db13c05115ee6cb53590b2488f6f6
Alex <roya0045@users.noreply.github.com>
2019-02-15 17:51:57 -0500

    Update qgsvectorlayer.h

--- src/core/qgsvectorlayer.h
+++ src/core/qgsvectorlayer.h
@@ -995,14 +995,14 @@ class CORE_EXPORT QgsVectorLayer : public QgsMapLayer, public QgsExpressionConte
      * The method will return the feature counter task. You will need to
      * connect to the symbolFeatureCountMapChanged() signal to be
      * notified when the freshly updated feature counts are ready.
-     *
+     * \param wait wait for the features to be counter or put the action on the stack
      * \note If the count features for symbols has been already done a
      *       nullptr is returned. If you need to wait for the results,
      *       you can call waitForFinished() on the feature counter.
      *
      * \since This is asynchronous since QGIS 3.0
      */
-    QgsVectorLayerFeatureCounter *countSymbolFeatures();
+    QgsVectorLayerFeatureCounter *countSymbolFeatures( bool wait = false);
 
     /**
      * Set the string (typically sql) used to define a subset of the layer
@@ -1409,8 +1409,9 @@ class CORE_EXPORT QgsVectorLayer : public QgsMapLayer, public QgsExpressionConte
 
     /**
      * Return true if the symbols were counted
-     * \Since: 3.8
      * \returns true if features are counted
+     * 
+     *  \Since: 3.8
      */
     bool featureCounted() const { return mSymbolFeatureCounted ;}
 
