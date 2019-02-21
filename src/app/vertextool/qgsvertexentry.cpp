dfac826f35316f176db2330459358ee8c9d54935
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-02-21 15:19:39 -0500

    remove vertex highlighting from editor

--- src/app/vertextool/qgsvertexentry.cpp
+++ src/app/vertextool/qgsvertexentry.cpp
@@ -19,65 +19,19 @@
 #include "qgsmaplayer.h"
 #include "qgsmapcanvas.h"
 
-QgsVertexEntry::QgsVertexEntry( QgsMapCanvas *canvas, QgsMapLayer *layer, const QgsPoint &p, QgsVertexId vertexId, const QString &tooltip, QgsVertexMarker::IconType type, int penWidth )
+QgsVertexEntry::QgsVertexEntry( const QgsPoint &p, QgsVertexId vertexId )
   : mSelected( false )
   , mPoint( p )
   , mVertexId( vertexId )
-  , mPenWidth( penWidth )
-  , mToolTip( tooltip )
-  , mType( type )
-  , mCanvas( canvas )
-  , mLayer( layer )
 {
-  placeMarker();
 }
 
 QgsVertexEntry::~QgsVertexEntry()
 {
-  delete mMarker;
 }
 
-void QgsVertexEntry::placeMarker()
-{
-  QgsPointXY pm = mCanvas->mapSettings().layerToMapCoordinates( mLayer, pointV1() );
-
-  if ( mCanvas->extent().contains( pm ) )
-  {
-    if ( !mMarker )
-    {
-      mMarker = new QgsVertexMarker( mCanvas );
-      QColor c = mSelected ? QColor( Qt::blue ) : QColor( Qt::red );
-      if ( mVertexId.type == QgsVertexId::CurveVertex )
-      {
-        mMarker->setIconType( QgsVertexMarker::ICON_CIRCLE );
-      }
-      else
-      {
-        mMarker->setIconType( mType );
-      }
-      mMarker->setColor( c );
-      mMarker->setIconSize( QgsGuiUtils::scaleIconSize( 10 ) );
-      mMarker->setPenWidth( QgsGuiUtils::scaleIconSize( mPenWidth ) );
-      mMarker->setToolTip( mToolTip );
-    }
-
-    mMarker->setCenter( pm );
-    mMarker->update();
-  }
-  else if ( mMarker )
-  {
-    delete mMarker;
-    mMarker = nullptr;
-  }
-}
 
 void QgsVertexEntry::setSelected( bool selected )
 {
   mSelected = selected;
-  if ( mMarker )
-  {
-    QColor c = mSelected ? QColor( Qt::blue ) : QColor( Qt::red );
-    mMarker->setColor( c );
-    mMarker->update();
-  }
 }
