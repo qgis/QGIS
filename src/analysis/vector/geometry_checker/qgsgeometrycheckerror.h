d99c1f1f32bbd3438fb0ef9ad6a3c0838786d2c7
Matthias Kuhn <matthias@opengis.ch>
2019-02-27 17:53:07 +0100

    Fixes

--- src/analysis/vector/geometry_checker/qgsgeometrycheckerror.h
+++ src/analysis/vector/geometry_checker/qgsgeometrycheckerror.h
@@ -180,13 +180,13 @@ class ANALYSIS_EXPORT QgsGeometryCheckError
     virtual bool handleChanges( const QgsGeometryCheck::Changes &changes ) SIP_SKIP;
 
     /**
-     * Return a list of involved features.
+     * Returns a list of involved features.
      * By default returns an empty map.
      * The map keys are layer ids, the map value is a set of feature ids.
      *
      * \since QGIS 3.8
      */
-    virtual QMap<QString, QgsFeatureIds> involvedFeatures() const;
+    virtual QMap<QString, QgsFeatureIds > involvedFeatures() const SIP_SKIP;
 
   protected:
 
