c928add144814bb86bfad065aa5212845dd54f06
Matthias Kuhn <matthias@opengis.ch>
2019-03-02 09:56:04 +0100

    Some tests rely on the current dinitialization order

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -1169,6 +1169,8 @@ QgsAuthManager *QgsApplication::authManager()
 
 void QgsApplication::exitQgis()
 {
+  delete QgsApplication::authManager();
+  
   //Ensure that all remaining deleteLater QObjects are actually deleted before we exit.
   //This isn't strictly necessary (since we're exiting anyway) but doing so prevents a lot of
   //LeakSanitiser noise which hides real issues
@@ -1180,8 +1182,6 @@ void QgsApplication::exitQgis()
   delete QgsProject::instance();
 
   delete QgsProviderRegistry::instance();
-
-  delete QgsApplication::authManager();
 
   // invalidate coordinate cache while the PROJ context held by the thread-locale
   // QgsProjContextStore object is still alive. Otherwise if this later object
