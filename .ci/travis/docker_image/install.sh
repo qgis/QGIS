e597803c8592bd217b942af1633a38f935e15702
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-05 16:22:30 +0100

    update headers

--- .ci/travis/docker_image/install.sh
+++ .ci/travis/docker_image/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ###########################################################################
-#    install.sh
+#    before_install.sh
 #    ---------------------
-#    Date                 : March 2016
-#    Copyright            : (C) 2016 by Matthias Kuhn
-#    Email                : matthias at opengis dot ch
+#    Date                 : March 2019
+#    Copyright            : (C) 2019 by Denis Rouzaud
+#    Email                : denis@opengis.ch
 ###########################################################################
 #                                                                         #
 #   This program is free software; you can redistribute it and/or modify  #
@@ -13,4 +13,5 @@
 #   (at your option) any later version.                                   #
 #                                                                         #
 ###########################################################################
+
 
