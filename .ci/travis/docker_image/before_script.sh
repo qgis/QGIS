5f4f2f96e55822bc343828d415297d6c30d37c0a
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-05 17:38:04 +0100

    simplify CI scripts (#9387)
    by separating the testing and docker image building scripts
    there was almost no redundany in the scripts, it is much clearer now

--- .ci/travis/docker_image/before_script.sh
+++ .ci/travis/docker_image/before_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ###########################################################################
-#    script.sh
+#    before_install.sh
 #    ---------------------
-#    Date                 : March 2016
-#    Copyright            : (C) 2016 by Matthias Kuhn
-#    Email                : matthias at opengis dot ch
+#    Date                 : March 2019
+#    Copyright            : (C) 2019 by Denis Rouzaud
+#    Email                : denis@opengis.ch
 ###########################################################################
 #                                                                         #
 #   This program is free software; you can redistribute it and/or modify  #
@@ -14,8 +14,23 @@
 #                                                                         #
 ###########################################################################
 
+
 set -e
 
-# running QGIS tests
-docker-compose -f ${TRAVIS_BUILD_DIR}/.docker/docker-compose.travis.yml run --rm qgis-deps
+.ci/travis/scripts/echo_travis_var.sh
 
+pushd .docker
+
+
+
+echo "travis_fold:start:docker_build"
+echo "${bold}Docker build deps${endbold}"
+docker --version
+
+docker pull "qgis/qgis3-build-deps:${DOCKER_TAG}" || true
+docker build --cache-from "qgis/qgis3-build-deps:${DOCKER_TAG}" -t "qgis/qgis3-build-deps:${DOCKER_TAG}" -f ${DOCKER_BUILD_DEPS_FILE} .
+
+echo "travis_fold:end:docker_build"
+
+
+popd
