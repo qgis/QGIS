aee9ffee15b93aec00e8db0b50aadb3eaec7ccb1
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-05 15:22:30 +0000

    update headers

--- .ci/travis/docker_image/before_script.sh
+++ .ci/travis/docker_image/before_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ###########################################################################
-#    before_script.sh
+#    before_install.sh
 #    ---------------------
-#    Date                 : August 2015
-#    Copyright            : (C) 2015 by Nyall Dawson
-#    Email                : nyall dot dawson at gmail dot com
+#    Date                 : March 2019
+#    Copyright            : (C) 2019 by Denis Rouzaud
+#    Email                : denis@opengis.ch
 ###########################################################################
 #                                                                         #
 #   This program is free software; you can redistribute it and/or modify  #
@@ -13,6 +13,7 @@
 #   (at your option) any later version.                                   #
 #                                                                         #
 ###########################################################################
+
 
 set -e
 
