1e11e1157a8f5ec645c2e5267d6f31ab810ad110
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-05 17:38:04 +0100

    simplify CI scripts (#9387)
    by separating the testing and docker image building scripts
    there was almost no redundany in the scripts, it is much clearer now

--- .ci/travis/docker_image/before_script.sh
+++ .ci/travis/docker_image/before_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ###########################################################################
-#    before_script.sh
+#    before_install.sh
 #    ---------------------
-#    Date                 : August 2015
-#    Copyright            : (C) 2015 by Nyall Dawson
-#    Email                : nyall dot dawson at gmail dot com
+#    Date                 : March 2019
+#    Copyright            : (C) 2019 by Denis Rouzaud
+#    Email                : denis@opengis.ch
 ###########################################################################
 #                                                                         #
 #   This program is free software; you can redistribute it and/or modify  #
@@ -14,33 +14,23 @@
 #                                                                         #
 ###########################################################################
 
-set -e
 
-DOCKER_DEPS_PUSH=$( [[ $TRAVIS_REPO_SLUG =~ qgis/QGIS ]] && [[ "${TRAVIS_EVENT_TYPE}" != "pull_request" ]] && echo "true" || echo "false" )
+set -e
 
 .ci/travis/scripts/echo_travis_var.sh
 
 pushd .docker
 
+
+
 echo "travis_fold:start:docker_build"
 echo "${bold}Docker build deps${endbold}"
 docker --version
-docker-compose --version
-docker-compose -f ${TRAVIS_BUILD_DIR}/.docker/docker-compose.travis.yml config
+
 docker pull "qgis/qgis3-build-deps:${DOCKER_TAG}" || true
 docker build --cache-from "qgis/qgis3-build-deps:${DOCKER_TAG}" -t "qgis/qgis3-build-deps:${DOCKER_TAG}" -f ${DOCKER_BUILD_DEPS_FILE} .
+
 echo "travis_fold:end:docker_build"
 
-echo "travis_fold:start:docker_push"
-echo "${bold}Docker push deps${endbold}"
-# image should be pushed even if QGIS build fails
-# but push is achieved only on branches (not for PRs)
-if [[ $DOCKER_DEPS_PUSH =~ true ]]; then
-  echo "push to qgis/qgis3-build-deps:${DOCKER_TAG}"
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  #docker tag "qgis/qgis3-build-deps:${DOCKER_TAG}" "qgis/qgis3-build-deps:latest"
-  docker push "qgis/qgis3-build-deps:${DOCKER_TAG}"
-fi
-echo "travis_fold:end:docker_push"
 
 popd
