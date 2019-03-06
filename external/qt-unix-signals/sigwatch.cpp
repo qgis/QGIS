3323694f41b20f2a64658034ac4f59c4997d213e
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-06 12:55:53 +0100

    fix building on Mac
    not sure if this is the right way to fix this 
    seen here https://github.com/boostorg/process/issues/55#issuecomment-435853519 and mostly here https://github.com/boostorg/process/issues/55#issuecomment-450444271

--- external/qt-unix-signals/sigwatch.cpp
+++ external/qt-unix-signals/sigwatch.cpp
@@ -108,7 +108,7 @@ void UnixSignalWatcherPrivate::watchForSignal(int signal)
     struct sigaction sigact;
     sigact.sa_handler = UnixSignalWatcherPrivate::signalHandler;
     sigact.sa_flags = 0;
-    ::sigemptyset(&sigact.sa_mask);
+    sigemptyset(&sigact.sa_mask);
     sigact.sa_flags |= SA_RESTART;
     if (::sigaction(signal, &sigact, nullptr)) {
         qDebug() << "UnixSignalWatcher: sigaction: " << ::strerror(errno);
