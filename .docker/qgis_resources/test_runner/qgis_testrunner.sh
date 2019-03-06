9b01e6f9544dd78a18f68e10d354ca1a48e5d97f
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-06 14:48:42 +0100

    run test without tty

--- .docker/qgis_resources/test_runner/qgis_testrunner.sh
+++ .docker/qgis_resources/test_runner/qgis_testrunner.sh
@@ -11,7 +11,7 @@
 TEST_NAME=$1
 
 echo "Running test $1 ..."
-OUTPUT=$(QGIS_TEST_MODULE=${TEST_NAME} unbuffer qgis --version-migration --nologo --code /usr/bin/qgis_testrunner.py "$TEST_NAME"  2>/dev/null | tee /dev/tty)
+OUTPUT=$(QGIS_TEST_MODULE=${TEST_NAME} unbuffer qgis --version-migration --nologo --code /usr/bin/qgis_testrunner.py "$TEST_NAME"  2>/dev/null)
 EXIT_CODE="$?"
 if [ -z "$OUTPUT" ]; then
     echo "ERROR: no output from the test runner! (exit code: ${EXIT_CODE})"
