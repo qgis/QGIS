44847ca8246bf614add82f18ec63ee5274c964ca
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-03-06 17:43:59 +0100

    avoid triggering twice build of PyQGIS docs (#9406)

--- .ci/travis/docker_image/script.sh
+++ .ci/travis/docker_image/script.sh
@@ -39,21 +39,25 @@ docker cp qgis_container:/usr/src/.ccache_image_build ${CCACHE_DIR}
 docker rm qgis_container
 popd
 echo "Trigger build of PyQGIS Documentation"
-body='{
-  "request": {
-    "branch":"master",
-    "message": "Trigger PyQGIS doc build after release of new Docker image as __DOCKER_TAG__",
-    "config": {
-      "merge_mode": "deep_merge",
-      "matrix": {
-        "include": {
-          "env": ["QGIS_VERSION_BRANCH=__QGIS_VERSION_BRANCH__"]
+if [[ ${TRIGGER_PYQGIS_DOC} =~ ^TRUE$ ]]; then
+  body='{
+    "request": {
+      "branch":"master",
+      "message": "Trigger PyQGIS doc build after release of new Docker image as __DOCKER_TAG__",
+      "config": {
+        "merge_mode": "deep_merge",
+        "matrix": {
+          "include": {
+            "env": ["QGIS_VERSION_BRANCH=__QGIS_VERSION_BRANCH__"]
+          }
         }
       }
     }
-  }
-}'
-body=$(sed "s/__QGIS_VERSION_BRANCH__/${TRAVIS_BRANCH}/; s/__DOCKER_TAG__/${DOCKER_TAG}/" <<< $body)
-curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" \
-  -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$body" \
-  https://api.travis-ci.org/repo/qgis%2Fpyqgis/requests
+  }'
+  body=$(sed "s/__QGIS_VERSION_BRANCH__/${TRAVIS_BRANCH}/; s/__DOCKER_TAG__/${DOCKER_TAG}/" <<< $body)
+  curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$body" \
+    https://api.travis-ci.org/repo/qgis%2Fpyqgis/requests
+else
+  echo "skipped from configuration"
+fi
