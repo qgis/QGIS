29ba7d70734a3fb75519b78d33618b4d2f722912
Alex <roya0045@users.noreply.github.com>
2019-03-07 14:57:52 -0500

    Update test_qgslayoutlegend.py

--- tests/src/python/test_qgslayoutlegend.py
+++ tests/src/python/test_qgslayoutlegend.py
@@ -408,9 +408,9 @@ class TestQgsLayoutItemLegend(unittest.TestCase, LayoutItemTestCase):
         label2 = legendnodes[1].evaluateLabel()
         label3 = legendnodes[2].evaluateLabel()
 
-        self.assertEqual(label1, '0 0')
-        self.assertEqual(label2, '5 1')
-        self.assertEqual(label3, '12 2')
+        self.assertEqual(label1, '@symbol_id 0')
+        self.assertEqual(label2, '@symbol_count 1')
+        self.assertEqual(label3, 'sum("Pilots") 2')
 
         QgsProject.instance().clear()
 
