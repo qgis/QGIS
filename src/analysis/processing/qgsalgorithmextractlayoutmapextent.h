92c8fddac2bca80c60ff0978d34dc01e9db5bb79
Nyall Dawson <nyall.dawson@gmail.com>
2019-03-12 16:06:50 +1000

    [FEATURE][processing] Add new algorithm "Print layout map extent to layer"
    This algorithm creates a polygon layer containing the extent
    of a print layout map item, with attributes specifying the map
    size (in layout units), scale and rotatation.
    
    The main use case is when you want to create an advanced overview
    indicator and the inbuilt layout tools to do this don't suffice.

---
+++ src/analysis/processing/qgsalgorithmextractlayoutmapextent.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+                         qgsalgorithmextractlayoutmapextent.h
+                         ---------------------
+    begin                : March 2019
+    copyright            : (C) 2019 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSALGORITHMEXTRACTLAYOUTMAPEXTENT_H
+#define QGSALGORITHMEXTRACTLAYOUTMAPEXTENT_H
+
+#define SIP_NO_FILE
+
+#include "qgis_sip.h"
+#include "qgsprocessingalgorithm.h"
+
+///@cond PRIVATE
+
+/**
+ * Native layout map extent to layer algorithm.
+ */
+class QgsLayoutMapExtentToLayerAlgorithm : public QgsProcessingAlgorithm
+{
+
+  public:
+
+    QgsLayoutMapExtentToLayerAlgorithm() = default;
+    void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) override;
+    QString name() const override;
+    QString displayName() const override;
+    QStringList tags() const override;
+    QString group() const override;
+    QString groupId() const override;
+    QString shortDescription() const override;
+    QString shortHelpString() const override;
+    QgsLayoutMapExtentToLayerAlgorithm *createInstance() const override SIP_FACTORY;
+
+  protected:
+    bool prepareAlgorithm( const QVariantMap &parameters,
+                           QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+    QVariantMap processAlgorithm( const QVariantMap &parameters,
+                                  QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+
+  private:
+    QgsFeatureList mFeatures;
+    double mWidth = 0;
+    double mHeight = 0;
+    double mScale = 0;
+    double mRotation = 0;
+    QgsCoordinateReferenceSystem mCrs;
+
+};
+
+///@endcond PRIVATE
+
+#endif // QGSALGORITHMEXTRACTLAYOUTMAPEXTENT_H
+
+
