e1847725e47800ae65ff9b0a8cbeb55f20a81208
David Signer <david@opengis.ch>
2019-03-18 12:23:15 +0100

    use of SRCHEIGHT SRCWIDTH intead of HEIGHT WIDTH

--- tests/src/python/test_qgsserver_wms_getlegendgraphic.py
+++ tests/src/python/test_qgsserver_wms_getlegendgraphic.py
@@ -468,8 +468,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "LAYER": "Country,Hello,db_point",
             "LAYERTITLE": "FALSE",
             "FORMAT": "image/png",
-            "HEIGHT": "500",
-            "WIDTH": "500",
+            "SRCHEIGHT": "500",
+            "SRCWIDTH": "500",
             "BBOX": "-151.7,-38.9,51.0,78.0",
             "CRS": "EPSG:4326"
         }.items())])
@@ -486,8 +486,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "LAYER": "Country,Hello,db_point",
             "LAYERTITLE": "FALSE",
             "FORMAT": "image/png",
-            "HEIGHT": "500",
-            "WIDTH": "500",
+            "SRCHEIGHT": "500",
+            "SRCWIDTH": "500",
             "BBOX": "-76.08,-6.4,-19.38,38.04",
             "SRS": "EPSG:4326"
         }.items())])
@@ -503,8 +503,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "QGIS%20Server%20Hello%20World",
             "FORMAT": "image/png",
-            "HEIGHT": "840",
-            "WIDTH": "1226",
+            "SRCHEIGHT": "840",
+            "SRCWIDTH": "1226",
             "BBOX": "10.38450,-49.6370,73.8183,42.9461",
             "SRS": "EPSG:4326",
             "SCALE": "15466642"
@@ -525,8 +525,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "QGIS%20Server%20-%20Grouped%20Layer",
             "FORMAT": "image/png",
-            "HEIGHT": "840",
-            "WIDTH": "1226",
+            "SRCHEIGHT": "840",
+            "SRCWIDTH": "1226",
             "BBOX": "609152,5808188,625492,5814318",
             "SRS": "EPSG:25832",
             "SCALE": "38976"
@@ -544,8 +544,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "All_grouped_layers",
             "FORMAT": "image/png",
-            "HEIGHT": "840",
-            "WIDTH": "1226",
+            "SRCHEIGHT": "840",
+            "SRCWIDTH": "1226",
             "BBOX": "609152,5808188,625492,5814318",
             "SRS": "EPSG:25832",
             "SCALE": "38976"
@@ -563,8 +563,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "testlayer",
             "FORMAT": "image/png",
-            "HEIGHT": "550",
-            "WIDTH": "850",
+            "SRCHEIGHT": "550",
+            "SRCWIDTH": "850",
             "BBOX": "-608.4,-1002.6,698.2,1019.0",
             "CRS": "EPSG:4326"
         }.items())])
@@ -579,8 +579,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "testlayer",
             "FORMAT": "image/png",
-            "HEIGHT": "550",
-            "WIDTH": "850",
+            "SRCHEIGHT": "550",
+            "SRCWIDTH": "850",
             "BBOX": "-1261.7,-2013.5,1351.5,2029.9",
             "CRS": "EPSG:4326"
         }.items())])
@@ -596,8 +596,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "testlayer",
             "FORMAT": "image/png",
-            "HEIGHT": "550",
-            "WIDTH": "850",
+            "SRCHEIGHT": "550",
+            "SRCWIDTH": "850",
             "BBOX": "31.8,-12.0,58.0,28.4",
             "CRS": "EPSG:4326"
         }.items())])
@@ -613,8 +613,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "testlayer",
             "FORMAT": "image/png",
-            "HEIGHT": "550",
-            "WIDTH": "850",
+            "SRCHEIGHT": "550",
+            "SRCWIDTH": "850",
             "BBOX": "25.3,-22.1,64.5,38.5",
             "CRS": "EPSG:4326"
         }.items())])
@@ -630,8 +630,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "testlayer",
             "FORMAT": "image/png",
-            "HEIGHT": "550",
-            "WIDTH": "850",
+            "SRCHEIGHT": "550",
+            "SRCWIDTH": "850",
             "BBOX": "44.8,8.0,45.0,8.4",
             "CRS": "EPSG:4326"
         }.items())])
@@ -646,8 +646,8 @@ class TestQgsServerWMSGetLegendGraphic(QgsServerTestBase):
             "REQUEST": "GetLegendGraphic",
             "LAYER": "testlayer",
             "FORMAT": "image/png",
-            "HEIGHT": "550",
-            "WIDTH": "850",
+            "SRCHEIGHT": "550",
+            "SRCWIDTH": "850",
             "BBOX": "43.6,6.2,46.2,10.2",
             "CRS": "EPSG:4326"
         }.items())])
