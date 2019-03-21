71698bf70751bb65a0d474c85262481a2d04bdc3
Alessandro Pasotti <elpaso@itopen.it>
2019-03-21 11:49:54 +0100

    Remove all deprecated usages of QgsCoordinateTransform without context

--- src/providers/wfs/qgswfsprovider.cpp
+++ src/providers/wfs/qgswfsprovider.cpp
@@ -1819,10 +1819,7 @@ bool QgsWFSProvider::getCapabilities()
         if ( mShared->mCaps.featureTypes[i].bboxSRSIsWGS84 )
         {
           QgsCoordinateReferenceSystem src = QgsCoordinateReferenceSystem::fromOgcWmsCrs( QStringLiteral( "CRS:84" ) );
-          Q_NOWARN_DEPRECATED_PUSH
-          QgsCoordinateTransform ct( src, mShared->mSourceCRS );
-          Q_NOWARN_DEPRECATED_POP
-
+          QgsCoordinateTransform ct( src, mShared->mSourceCRS, options().coordinateTransformContext );
           QgsDebugMsgLevel( "latlon ext:" + r.toString(), 4 );
           QgsDebugMsgLevel( "src:" + src.authid(), 4 );
           QgsDebugMsgLevel( "dst:" + mShared->mSourceCRS.authid(), 4 );
