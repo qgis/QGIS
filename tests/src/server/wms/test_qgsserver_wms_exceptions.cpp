c5817cb4131be88e1c8b31dbbb0fd666b8bbc2bf
Blottiere Paul <blottiere.paul@gmail.com>
2019-03-27 16:14:10 +0100

    Add unit tests

---
+++ tests/src/server/wms/test_qgsserver_wms_exceptions.cpp
@@ -0,0 +1,57 @@
+/***************************************************************************
+     test_qgsserver_wms_exceptions.cpp
+     ---------------------------------
+    Date                 : 27 Mar 2019
+    Copyright            : (C) 2019 by Paul Blottiere
+    Email                : paul dot blottiere @ oslandia.com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgstest.h"
+#include "qgswmsserviceexception.h"
+
+/**
+ * \ingroup UnitTests
+ * This is a unit test for the WMS Exceptions
+ */
+class TestQgsServerWmsExceptions : public QObject
+{
+    Q_OBJECT
+
+  private slots:
+    void initTestCase();
+    void cleanupTestCase();
+
+    void exception_code();
+};
+
+void TestQgsServerWmsExceptions::initTestCase()
+{
+  QgsApplication::init();
+  QgsApplication::initQgis();
+}
+
+void TestQgsServerWmsExceptions::cleanupTestCase()
+{
+  QgsApplication::exitQgis();
+}
+
+void TestQgsServerWmsExceptions::exception_code()
+{
+  QgsWms::QgsServiceException::ExceptionCode code = QgsWms::QgsServiceException::OGC_INVALID_FORMAT;
+  QgsWms::QgsServiceException exception0( code, QString(), 400 );
+  QCOMPARE( exception0.code(), QString( "InvalidFormat" ) );
+
+  code = QgsWms::QgsServiceException::QGIS_ERROR;
+  QgsWms::QgsServiceException exception1( code, QString(), 400 );
+  QCOMPARE( exception1.code(), QString( "Error" ) );
+}
+
+QGSTEST_MAIN( TestQgsServerWmsExceptions )
+#include "test_qgsserver_wms_exceptions.moc"
