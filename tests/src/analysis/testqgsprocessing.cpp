fed56d4bd3dfab1ee59aacb49172737ccacf912a
lbartoletti <loic.bartoletti@oslandia.com>
2019-04-03 06:57:44 +0200

    fix TestQgsProcessing::parameterPoint

--- tests/src/analysis/testqgsprocessing.cpp
+++ tests/src/analysis/testqgsprocessing.cpp
@@ -2862,13 +2862,13 @@ void TestQgsProcessing::parameterPoint()
   // nonsense string
   params.insert( "non_optional", QString( "i'm not a crs, and nothing you can do will make me one" ) );
   point = QgsProcessingParameters::parameterAsPoint( def.get(), params, context );
-  QCOMPARE( point.x(), 0.0 );
-  QCOMPARE( point.y(), 0.0 );
+  QVERIFY( std::isnan( point.x() ) );
+  QVERIFY( std::isnan( point.y() ) );
 
   params.insert( "non_optional", QString( "   (   )  " ) );
   point = QgsProcessingParameters::parameterAsPoint( def.get(), params, context );
-  QCOMPARE( point.x(), 0.0 );
-  QCOMPARE( point.y(), 0.0 );
+  QVERIFY( std::isnan( point.x() ) );
+  QVERIFY( std::isnan( point.y() ) );
 
   // QgsPointXY
   params.insert( "non_optional", QgsPointXY( 11.1, 12.2 ) );
