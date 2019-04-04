29a15ad890e69b9a9f05443b0e4e4a8b41e9f415
Marco Bernasocchi <marco@opengis.ch>
2019-04-04 13:26:31 +0200

    Move invalid size test to its own class so it can reread os.env
    Signed-off-by: Marco Bernasocchi <marco@opengis.ch>
    
    add init_server method
    
    Signed-off-by: Marco Bernasocchi <marco@opengis.ch>
    
    Use own qgs application so we can change the env settings
    
    Signed-off-by: Marco Bernasocchi <marco@opengis.ch>

--- tests/src/python/test_qgsserver_wms_getmap.py
+++ tests/src/python/test_qgsserver_wms_getmap.py
@@ -615,44 +615,6 @@ class TestQgsServerWMSGetMap(QgsServerTestBase):
         r, h = self._result(self._execute_request(qs))
         self._img_diff_error(r, h, "WMS_GetMap_Background_Hex")
 
-    def test_wms_getmap_invalid_size(self):
-        project = os.path.join(self.testdata_path, "test_project_with_size.qgs")
-        expected = self.strip_version_xmlns(b'<ServiceExceptionReport version="1.3.0" xmlns="http://www.opengis.net/ogc">\n <ServiceException code="InvalidParameterValue">The requested map size is too large</ServiceException>\n</ServiceExceptionReport>\n')
-
-        os.environ['QGIS_SERVER_WMS_MAX_WIDTH'] = '6000'
-        os.environ['QGIS_SERVER_WMS_MAX_HEIGHT'] = '6000'
-        # test the 6000 limit from server is overridden by the more conservative 5000 in the project
-        qs = "?" + "&".join(["%s=%s" % i for i in list({
-            "MAP": urllib.parse.quote(project),
-            "SERVICE": "WMS",
-            "VERSION": "1.3.0",
-            "REQUEST": "GetMap",
-            "LAYERS": "",
-            "STYLES": "",
-            "FORMAT": "image/png",
-            "HEIGHT": "5001",
-            "WIDTH": "5000"
-        }.items())])
-        r, h = self._result(self._execute_request(qs))
-        self.assertEqual(self.strip_version_xmlns(r), expected)
-
-        # test the QGIS_SERVER_WMS_MAX_XXX env vars
-        os.environ['QGIS_SERVER_WMS_MAX_WIDTH'] = '3000'
-        os.environ['QGIS_SERVER_WMS_MAX_HEIGHT'] = '3000'
-        qs = "?" + "&".join(["%s=%s" % i for i in list({
-            "MAP": urllib.parse.quote(project),
-            "SERVICE": "WMS",
-            "VERSION": "1.3.0",
-            "REQUEST": "GetMap",
-            "LAYERS": "",
-            "STYLES": "",
-            "FORMAT": "image/png",
-            "HEIGHT": "3000",
-            "WIDTH": "3001"
-        }.items())])
-        r, h = self._result(self._execute_request(qs))
-        self.assertEqual(self.strip_version_xmlns(r), expected)
-
     def test_wms_getmap_order(self):
         qs = "?" + "&".join(["%s=%s" % i for i in list({
             "MAP": urllib.parse.quote(self.projectPath),
