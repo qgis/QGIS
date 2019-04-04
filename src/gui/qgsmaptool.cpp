9ad45b3721a699484da6fff7973e21b1844ca04f
Sandro Mani <manisandro@gmail.com>
2019-04-04 11:29:50 +0200

    Add missing consts

--- src/gui/qgsmaptool.cpp
+++ src/gui/qgsmaptool.cpp
@@ -72,7 +72,7 @@ QgsRectangle QgsMapTool::toLayerCoordinates( const QgsMapLayer *layer, const Qgs
   return mCanvas->mapSettings().mapToLayerCoordinates( layer, rect );
 }
 
-QPoint QgsMapTool::toCanvasCoordinates( const QgsPointXY &point )
+QPoint QgsMapTool::toCanvasCoordinates( const QgsPointXY &point ) const
 {
   qreal x = point.x(), y = point.y();
   mCanvas->getCoordinateTransform()->transformInPlace( x, y );
@@ -195,7 +195,7 @@ bool QgsMapTool::gestureEvent( QGestureEvent *e )
   return true;
 }
 
-QgsMapCanvas *QgsMapTool::canvas()
+QgsMapCanvas *QgsMapTool::canvas() const
 {
   return mCanvas;
 }
