adb646fc6f20ff5e5b0ead8b5d560ca0e1fb7ab0
Alessandro Pasotti <elpaso@itopen.it>
2019-04-16 11:27:47 +0000

    Guard vector file writer against invalid layers
    instead of crashing.
    
    Fixes #21856
    
    Funded by: QCooperative.net

--- src/core/qgsvectorfilewriter.cpp
+++ src/core/qgsvectorfilewriter.cpp
@@ -2453,7 +2453,7 @@ QgsVectorFileWriter::SaveVectorOptions::SaveVectorOptions()
 
 QgsVectorFileWriter::WriterError QgsVectorFileWriter::prepareWriteAsVectorFormat( QgsVectorLayer *layer, const QgsVectorFileWriter::SaveVectorOptions &options, QgsVectorFileWriter::PreparedWriterDetails &details )
 {
-  if ( !layer )
+  if ( !layer || !layer->isValid() )
   {
     return ErrInvalidLayer;
   }
