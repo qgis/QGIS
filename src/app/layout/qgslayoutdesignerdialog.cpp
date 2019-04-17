442138ca70b7867ece30d0389fb8247f5c9aef95
nirvn <nirvn.asia@gmail.com>
2019-04-17 10:10:53 +0700

    [ui] Fix layout designer panel icon (re)sizing

--- src/app/layout/qgslayoutdesignerdialog.cpp
+++ src/app/layout/qgslayoutdesignerdialog.cpp
@@ -1065,14 +1065,25 @@ void QgsLayoutDesignerDialog::setCurrentLayout( QgsLayout *layout )
 
 void QgsLayoutDesignerDialog::setIconSizes( int size )
 {
+  QSize iconSize = QSize( size, size );
+  QSize panelIconSize = QgsGuiUtils::panelIconSize( iconSize );
+
   //Set the icon size of for all the toolbars created in the future.
-  setIconSize( QSize( size, size ) );
+  setIconSize( iconSize );
 
   //Change all current icon sizes.
   QList<QToolBar *> toolbars = findChildren<QToolBar *>();
   Q_FOREACH ( QToolBar *toolbar, toolbars )
   {
-    toolbar->setIconSize( QSize( size, size ) );
+    QString className = toolbar->parent()->metaObject()->className();
+    if ( className == QLatin1String( "QgsLayoutDesignerDialog" ) )
+    {
+      toolbar->setIconSize( iconSize );
+    }
+    else
+    {
+      toolbar->setIconSize( panelIconSize );
+    }
   }
 }
 
