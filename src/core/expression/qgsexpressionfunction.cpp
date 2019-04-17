a72f4621434489a91f72f1249e9afc17629d3a93
Alex <roya0045@users.noreply.github.com>
2019-04-17 18:40:50 -0400

    pointer to feature ids

--- src/core/expression/qgsexpressionfunction.cpp
+++ src/core/expression/qgsexpressionfunction.cpp
@@ -580,7 +580,7 @@ static QVariant fcnAggregate( const QVariantList &values, const QgsExpressionCon
       //QgsFeatureIds *fids = context->variable( "symbol_feature_ids" )
       QgsExpressionContext context_copy = *context;
       const QgsFeatureIds fids = vl->countSymbolFeatures()->featureIds( context_copy.variable( "symbol_id" ).toString() );
-      result = vl->aggregate( aggregate, subExpression, parameters, &context_copy, &ok, fids );
+      result = vl->aggregate( aggregate, subExpression, parameters, &context_copy, &ok, &fids );
     }
     else
     {
