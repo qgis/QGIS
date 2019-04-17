f0f524a2ab1c238ac0df7e3e0d726725dfb87f38
Alex <roya0045@users.noreply.github.com>
2019-04-17 14:51:21 -0400

    verbose sip test

--- tests/code_layout/test_sipfiles_uptodate.sh
+++ tests/code_layout/test_sipfiles_uptodate.sh
@@ -25,12 +25,14 @@ for module in "${modules[@]}"; do
         outdiff=$(./scripts/sipify.pl -p python/${module}/auto_additions/${pyfile}.temp $header | diff python/$sipfile.in -)
         if [[ -n "$outdiff" ]]; then
           echo " *** SIP file not up to date: $sipfile"
+          echo " $outdiff "
           code=1
         fi
         if [[ -f python/${module}/auto_additions/${pyfile}.temp ]]; then
           outdiff2=$(diff python/${module}/auto_additions/${pyfile} python/${module}/auto_additions/${pyfile}.temp)
           if [[ -n "$outdiff2" ]]; then
             echo " *** Python addition file not up to date: $sipfile"
+            echo " $outdiff2 "
             code=1
           fi
         fi
