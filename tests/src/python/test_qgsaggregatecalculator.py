1e8923a4fbd30911dd3888a239f3472d97834d81
Alex <roya0045@users.noreply.github.com>
2019-04-18 09:06:38 -0400

    trying to fix test
    How does 4+2 = 4, unless the index starts at 1..

--- tests/src/python/test_qgsaggregatecalculator.py
+++ tests/src/python/test_qgsaggregatecalculator.py
@@ -392,9 +392,9 @@ class TestQgsAggregateCalculator(unittest.TestCase):
         self.assertEqual(val, 5)
 
         # test with subset
-        val, ok = agg.calculate(QgsAggregateCalculator.Sum, 'fldint', ids=[0, 1])
+        val, ok = agg.calculate(QgsAggregateCalculator.Sum, 'fldint', ids=[1, 2])
         self.assertTrue(ok)
-        self.assertEqual(val, 6)
+        self.assertEqual(val, 6.0)
 
     def testExpressionNoMatch(self):
         """ test aggregate calculation using an expression with no features """
