68c94dd844a4a117a983302439d27092f9c1567d
Sandro Mani <manisandro@gmail.com>
2019-04-29 17:18:11 +0200

    Move QgsReadWriteContext::DefaultTranslator to cpp file
    The nested class appears to cause sip to stumble with undefined references to vtable (observed cross-compiling from Fedora to MinGW)

--- src/core/qgsreadwritecontext.cpp
+++ src/core/qgsreadwritecontext.cpp
@@ -14,8 +14,23 @@
  ***************************************************************************/
 #include "qgsreadwritecontext.h"
 
+class DefaultTranslator : public QgsProjectTranslator
+{
+    // QgsProjectTranslator interface
+  public:
+    QString translate( const QString &context, const QString &sourceText, const char *disambiguation, int n ) const override
+    {
+      Q_UNUSED( context );
+      Q_UNUSED( disambiguation );
+      Q_UNUSED( n );
+      return sourceText;
+    }
+};
+
+static DefaultTranslator sDefaultTranslator;
+
 QgsReadWriteContext::QgsReadWriteContext()
-  : mProjectTranslator( &mDefaultTranslator )
+  : mProjectTranslator( &sDefaultTranslator )
 {
 
 }
@@ -77,12 +92,4 @@ QList<QgsReadWriteContext::ReadWriteMessage > QgsReadWriteContext::takeMessages(
   QList<QgsReadWriteContext::ReadWriteMessage > messages = mMessages;
   mMessages.clear();
   return messages;
-}
-
-QString QgsReadWriteContext::DefaultTranslator::translate( const QString &context, const QString &sourceText, const char *disambiguation, int n ) const
-{
-  Q_UNUSED( context )
-  Q_UNUSED( disambiguation )
-  Q_UNUSED( n )
-  return sourceText;
 }
