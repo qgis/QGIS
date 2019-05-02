8bf97f5d719e75026702967fb990b24f7672c721
Sandro Mani <manisandro@gmail.com>
2019-05-02 16:43:24 +0200

    Clear QgsMapCanvas::mMapTool before emitting mapToolSet in QgsMapCanvas::unsetMapTool
    Otherwise, third-parties setting another tool based on the emitted signal will have their tool cleared again immediately

--- src/gui/qgsmapcanvas.cpp
+++ src/gui/qgsmapcanvas.cpp
@@ -1783,8 +1783,9 @@ void QgsMapCanvas::unsetMapTool( QgsMapTool *tool )
   {
     disconnect( mMapTool, &QObject::destroyed, this, &QgsMapCanvas::mapToolDestroyed );
     mMapTool->deactivate();
+    QgsMapTool *oldTool = mMapTool;
     mMapTool = nullptr;
-    emit mapToolSet( nullptr, mMapTool );
+    emit mapToolSet( nullptr, oldTool );
     setCursor( Qt::ArrowCursor );
   }
 
