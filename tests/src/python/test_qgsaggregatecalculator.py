7983a5d2530747718fcf59aecfcdd4e9c895f25a
Alex <roya0045@users.noreply.github.com>
2019-05-07 12:01:40 -0400

    removing useless assignment in test

--- tests/src/python/test_qgsaggregatecalculator.py
+++ tests/src/python/test_qgsaggregatecalculator.py
@@ -394,13 +394,13 @@ class TestQgsAggregateCalculator(unittest.TestCase):
 
         # test with subset
         agg = QgsAggregateCalculator(layer)  # reset to remove expression filter
-        request = agg.setFidsFilter([1, 2])
+        agg.setFidsFilter([1, 2])
         val, ok = agg.calculate(QgsAggregateCalculator.Sum, 'fldint')
         self.assertTrue(ok)
         self.assertEqual(val, 6.0)
 
         # test with empty subset
-        request = agg.setFidsFilter(list())
+        agg.setFidsFilter(list())
         val, ok = agg.calculate(QgsAggregateCalculator.Sum, 'fldint')
         self.assertTrue(ok)
         self.assertEqual(val, 0.0)
