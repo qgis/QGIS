49457b0efe34cd34302ecf07b7018245ae631ff4
Nyall Dawson <nyall.dawson@gmail.com>
2019-05-09 09:19:03 +1000

    Fix constraint expression and description are flipped
    Fixes #22016

--- src/app/qgsattributesformproperties.cpp
+++ src/app/qgsattributesformproperties.cpp
@@ -321,7 +321,7 @@ void QgsAttributesFormProperties::storeAttributeTypeDialog()
     constraints.setConstraint( QgsFieldConstraints::ConstraintExpression );
   }
 
-  constraints.setConstraintExpression( mAttributeTypeDialog->constraintExpressionDescription(), mAttributeTypeDialog->constraintExpression() );
+  constraints.setConstraintExpression( mAttributeTypeDialog->constraintExpression(), mAttributeTypeDialog->constraintExpressionDescription() );
 
   constraints.setConstraintStrength( QgsFieldConstraints::ConstraintNotNull, mAttributeTypeDialog->notNullEnforced() ?
                                      QgsFieldConstraints::ConstraintStrengthHard : QgsFieldConstraints::ConstraintStrengthSoft );
