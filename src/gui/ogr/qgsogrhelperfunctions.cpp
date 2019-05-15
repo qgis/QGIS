a3d4055941506e9434d3fc755498c370667fe11c
Alessandro Pasotti <elpaso@itopen.it>
2019-05-15 17:30:54 +0200

    Fix WFS3 OGR build URI

--- src/gui/ogr/qgsogrhelperfunctions.cpp
+++ src/gui/ogr/qgsogrhelperfunctions.cpp
@@ -269,7 +269,8 @@ QString createProtocolURI( const QString &type, const QString &url,  const QStri
   {
     uri = QStringLiteral( "DODS:%1" ).arg( url );
   }
-  else if ( type == QLatin1String( "WFS3" ) )
+  // Check beginning because of "experimental"
+  else if ( type.startsWith( QLatin1String( "WFS3" ) ) )
   {
     uri = QStringLiteral( "WFS3:%1" ).arg( url );
   }
