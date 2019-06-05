5205066e78b1b80ae8161469a1bb9d7184d6b06d
Alessandro Pasotti <elpaso@itopen.it>
2019-06-05 08:35:06 +0200

    Update browser project home path when saved as

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -1061,8 +1061,6 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, bool skipVersionCh
 
   addDockWidget( Qt::LeftDockWidgetArea, mBrowserWidget );
   mBrowserWidget->hide();
-  connect( this, &QgisApp::newProject, mBrowserWidget, &QgsBrowserDockWidget::updateProjectHome );
-  connect( this, &QgisApp::projectSavedAs, [ = ]( const QString & ) { mBrowserWidget->updateProjectHome(); } );
   // Only connect the first widget: the model is shared, there is no need to refresh multiple times.
   connect( this, &QgisApp::connectionsChanged, mBrowserWidget, &QgsBrowserDockWidget::refresh );
   connect( mBrowserWidget, &QgsBrowserDockWidget::connectionsChanged, this, &QgisApp::connectionsChanged );
@@ -1073,8 +1071,6 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, bool skipVersionCh
   mBrowserWidget2->setObjectName( QStringLiteral( "Browser2" ) );
   addDockWidget( Qt::LeftDockWidgetArea, mBrowserWidget2 );
   mBrowserWidget2->hide();
-  connect( this, &QgisApp::newProject, mBrowserWidget2, &QgsBrowserDockWidget::updateProjectHome );
-  connect( this, &QgisApp::projectSavedAs, [ = ]( const QString & ) { mBrowserWidget->updateProjectHome(); } );
   connect( mBrowserWidget2, &QgsBrowserDockWidget::connectionsChanged, this, &QgisApp::connectionsChanged );
   connect( mBrowserWidget2, &QgsBrowserDockWidget::openFile, this, &QgisApp::openFile );
   connect( mBrowserWidget2, &QgsBrowserDockWidget::handleDropUriList, this, &QgisApp::handleDropUriList );
@@ -6279,7 +6275,6 @@ void QgisApp::fileSaveAs()
     // add this to the list of recently used project files
     saveRecentProjectPath();
     mProjectLastModified = fullPath.lastModified();
-    emit projectSavedAs( fullPath.filePath() );
   }
   else
   {
