12094db813f8d29e1addfcf2717fbf5e7c6f1531
Alessandro Pasotti <elpaso@itopen.it>
2019-06-05 12:50:02 +0200

    createFeatures check unique constraints on unfiltered layer
    Fixes #30062

--- tests/src/python/test_qgsvectorlayerutils.py
+++ tests/src/python/test_qgsvectorlayerutils.py
@@ -12,6 +12,8 @@ __copyright__ = 'Copyright 2016, The QGIS Project'
 
 import os
 import qgis  # NOQA
+import shutil
+import tempfile
 
 from qgis.PyQt.QtCore import QVariant
 from qgis.core import (QgsProject,
@@ -32,7 +34,7 @@ from qgis.core import (QgsProject,
                        NULL
                        )
 from qgis.testing import start_app, unittest
-
+from utilities import unitTestDataPath
 
 start_app()
 
@@ -613,6 +615,28 @@ class TestQgsVectorLayerUtils(unittest.TestCase):
 
         for f in features:
             self.assertEqual(f.attributes()[0], 'my_default', f.id())
+
+    def test_unique_pk_when_subset(self):
+        """Test unique values on filtered layer GH #30062"""
+
+        src = unitTestDataPath('points_gpkg.gpkg')
+        dest = tempfile.mktemp() + '.gpkg'
+        shutil.copy(src, dest)
+        vl = QgsVectorLayer(dest, 'vl', 'ogr')
+        self.assertTrue(vl.isValid())
+        features_data = []
+        it = vl.getFeatures()
+        for _ in range(3):
+            f = next(it)
+            features_data.append(QgsVectorLayerUtils.QgsFeatureData(f.geometry(), dict(zip(range(f.fields().count()), f.attributes()))))
+        # Set a filter
+        vl.setSubsetString('"fid" in (4,5,6)')
+        self.assertTrue(vl.isValid())
+        context = vl.createExpressionContext()
+        features = QgsVectorLayerUtils.createFeatures(vl, features_data, context)
+        self.assertTrue(vl.startEditing())
+        vl.addFeatures(features)
+        self.assertTrue(vl.commitChanges())
 
 
 if __name__ == '__main__':
