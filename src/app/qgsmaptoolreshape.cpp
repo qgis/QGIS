896751de1ffe8b030ae003a2923d531b268e9d3f
lbartoletti <loic.bartoletti@oslandia.com>
2019-06-06 11:50:32 +0200

    Fix reshape for snapped point with Z and add test

--- src/app/qgsmaptoolreshape.cpp
+++ src/app/qgsmaptoolreshape.cpp
@@ -121,9 +121,11 @@ void QgsMapToolReshape::reshape( QgsVectorLayer *vlayer )
     bbox.combineExtentWith( points().at( i ).x(), points().at( i ).y() );
   }
 
-  QgsLineString reshapeLineString( points() );
-  if ( QgsWkbTypes::hasZ( vlayer->wkbType() ) )
-    reshapeLineString.addZValue( defaultZValue() );
+
+  QgsPointSequence pts;
+  QVector<QgsPoint> points;
+  captureCurve()->points( pts );
+  QgsLineString reshapeLineString( pts );
 
   //query all the features that intersect bounding box of capture line
   QgsFeatureIterator fit = vlayer->getFeatures( QgsFeatureRequest().setFilterRect( bbox ).setNoAttributes() );
@@ -143,7 +145,7 @@ void QgsMapToolReshape::reshape( QgsVectorLayer *vlayer )
     {
       // in case of a binding line, we just want to update the line from
       // the starting point and not both side
-      if ( isBinding && !geom.asPolyline().contains( points().first() ) )
+      if ( isBinding && !geom.asPolyline().contains( pts.constFirst() ) )
         continue;
 
       reshapeReturn = geom.reshapeGeometry( reshapeLineString );
