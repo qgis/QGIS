43904df6c4899ced6e64fbfa826b0593356b4efa
Nyall Dawson <nyall.dawson@gmail.com>
2019-06-11 09:28:20 +1000

    [processing] Fix exception in select by expression using not equal to operator
    Fixes #30135

--- python/plugins/processing/algs/qgis/SelectByAttribute.py
+++ python/plugins/processing/algs/qgis/SelectByAttribute.py
@@ -44,7 +44,7 @@ class SelectByAttribute(QgisAlgorithm):
     OUTPUT = 'OUTPUT'
 
     OPERATORS = ['=',
-                 '≠',
+                 '<>',
                  '>',
                  '>=',
                  '<',
