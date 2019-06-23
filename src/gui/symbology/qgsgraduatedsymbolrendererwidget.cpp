3b6e672f266e4f1e05d22330b63b5e7d8a69a5f9
Alexander Bruy <alexander.bruy@gmail.com>
2019-06-23 11:20:12 +0000

    update number of classes in the graduated renderer after adding new classes with the histogram (fix #29796)

--- src/gui/symbology/qgsgraduatedsymbolrendererwidget.cpp
+++ src/gui/symbology/qgsgraduatedsymbolrendererwidget.cpp
@@ -818,6 +818,11 @@ void QgsGraduatedSymbolRendererWidget::refreshRanges( bool reset )
     return;
 
   mModel->updateSymbology( reset );
+
+  disconnectUpdateHandlers();
+  spinGraduatedClasses->setValue( mRenderer->ranges().count() );
+  connectUpdateHandlers();
+
   emit widgetChanged();
 }
 
