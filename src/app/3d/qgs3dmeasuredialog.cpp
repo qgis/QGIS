78d59b0701f7d8e13ef80784be01af172da41288
Ismail Sunni <imajimatika@gmail.com>
2019-06-23 21:06:51 +0200

    Add header.

--- src/app/3d/qgs3dmeasuredialog.cpp
+++ src/app/3d/qgs3dmeasuredialog.cpp
@@ -1,4 +1,18 @@
-#include <QtMath>
+/***************************************************************************
+  qgs3dmeasuredialog.cpp
+  --------------------------------------
+  Date                 : Jun 2019
+  Copyright            : (C) 2019 by Ismail Sunni
+  Email                : imajimatika at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QCloseEvent>
 #include <QPushButton>
 
--- src/app/3d/qgs3dmeasuredialog.h
+++ src/app/3d/qgs3dmeasuredialog.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+  qgs3dmeasuredialog.h
+  --------------------------------------
+  Date                 : Jun 2019
+  Copyright            : (C) 2019 by Ismail Sunni
+  Email                : imajimatika at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifndef QGS3DMEASUREDIALOG_H
 #define QGS3DMEASUREDIALOG_H
 
