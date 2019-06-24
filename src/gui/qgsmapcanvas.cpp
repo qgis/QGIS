eacd3c9c554610c309d67f256bd872301d0f8acb
nirvn <nirvn.asia@gmail.com>
2019-06-24 09:55:41 +0000

    [map canvas] Fix scale change when resizing in locked scale mode

--- src/gui/qgsmapcanvas.cpp
+++ src/gui/qgsmapcanvas.cpp
@@ -1538,19 +1538,28 @@ void QgsMapCanvas::mouseReleaseEvent( QMouseEvent *e )
 void QgsMapCanvas::resizeEvent( QResizeEvent *e )
 {
   QGraphicsView::resizeEvent( e );
-  mResizeTimer->start( 500 );
+  mResizeTimer->start( 500 ); // in charge of refreshing canvas
 
+  double oldScale = mSettings.scale();
   QSize lastSize = viewport()->size();
-
   mSettings.setOutputSize( lastSize );
 
   mScene->setSceneRect( QRectF( 0, 0, lastSize.width(), lastSize.height() ) );
 
   moveCanvasContents( true );
 
-  updateScale();
-
-  //refresh();
+  if ( mScaleLocked )
+  {
+    double scaleFactor = oldScale / mSettings.scale();
+    QgsRectangle r = mSettings.extent();
+    QgsPointXY center = r.center();
+    r.scale( scaleFactor, &center );
+    mSettings.setExtent( r );
+  }
+  else
+  {
+    updateScale();
+  }
 
   emit extentsChanged();
 }
