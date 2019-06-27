caf19aa3de15cad91df916f0ae1fb4e9159aa862
nirvn <nirvn.asia@gmail.com>
2019-06-27 11:03:37 +0000

    [arcgisrest] Only show an 'all layers' mapserver item when multiple layers offered

--- src/providers/arcgisrest/qgsarcgisrestutils.cpp
+++ src/providers/arcgisrest/qgsarcgisrestutils.cpp
@@ -1381,8 +1381,8 @@ void QgsArcGisRestUtils::addLayerItems( const std::function< void( const QString
     }
   }
 
-  // Add root MapServer as layer
-  if ( serviceData.contains( QStringLiteral( "supportedImageFormatTypes" ) ) )
+  // Add root MapServer as layer when multiple layers are listed
+  if ( layerInfoList.count() > 1 && serviceData.contains( QStringLiteral( "supportedImageFormatTypes" ) ) )
   {
     const QString name = QStringLiteral( "(%1)" ).arg( QObject::tr( "All layers" ) );
     const QString description = serviceData.value( QStringLiteral( "Comments" ) ).toString();
