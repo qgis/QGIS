240f573b012fa5bb471fc1e0c7cdb23ea9ce4463
Nyall Dawson <nyall.dawson@gmail.com>
2019-06-27 03:10:49 +0000

    Fix potential crash on exit
    Fixes #30399

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -1586,6 +1586,8 @@ QgisApp::~QgisApp()
   mBrowserModel = nullptr;
   delete mGeometryValidationDock;
   mGeometryValidationDock = nullptr;
+  delete mSnappingUtils;
+  mSnappingUtils = nullptr;
 
   QgsGui::instance()->nativePlatformInterface()->cleanup();
 
