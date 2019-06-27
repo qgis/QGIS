de95065e8b824a790703d984ef6c27072039aec1
lbartoletti <loic.bartoletti@oslandia.com>
2019-06-27 14:38:04 +0200

    fix geometrychecks

--- tests/src/geometry_checker/testqgsgeometrychecks.cpp
+++ tests/src/geometry_checker/testqgsgeometrychecks.cpp
@@ -1163,7 +1163,7 @@ QList<QgsGeometryCheckError *> TestQgsGeometryChecks::searchCheckErrors( const Q
     {
       continue;
     }
-    if ( pos != QgsPointXY() && ( !qgsDoubleNear( error->location().x(), pos.x(), tol ) || !qgsDoubleNear( error->location().y(), pos.y(), tol ) ) )
+    if ( !pos.isEmpty() && ( !qgsDoubleNear( error->location().x(), pos.x(), tol ) || !qgsDoubleNear( error->location().y(), pos.y(), tol ) ) )
     {
       continue;
     }
