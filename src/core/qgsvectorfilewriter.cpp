9191c05ecb543e5b41686a5527273e28bfb5b120
Richard Duivenvoorde <richard@duif.net>
2019-07-03 15:19:56 +0200

    Change tooltip and hint to documentation

--- src/core/qgsvectorfilewriter.cpp
+++ src/core/qgsvectorfilewriter.cpp
@@ -1081,8 +1081,8 @@ class QgsVectorFileWriterMetadataContainer
 
       layerOptions.insert( QStringLiteral( "RFC7946" ), new QgsVectorFileWriter::BoolOption(
                              QObject::tr( "Whether to use RFC 7946 standard. "
-                                          "Otherwise GeoJSON 2008 initial version will be used. "
-                                          "Default is NO (thus GeoJSON 2008)." ),
+                                          "If disabled GeoJSON 2008 initial version will be used. "
+                                          "Default is NO (thus GeoJSON 2008). See also Documentation (via Help button)" ),
                              false // Default value
                            ) );
 
