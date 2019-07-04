87f10e140e494ce8ebd41edfce6d8d400e92989c
Nyall Dawson <nyall.dawson@gmail.com>
2019-07-04 10:49:24 +1000

    Fix generation of enum class Pydocs

---
+++ python/core/auto_additions/qgsprovidermetadata.py
@@ -0,0 +1,8 @@
+# The following has been generated automatically from src/core/qgsprovidermetadata.h
+# monkey patching scoped based enum
+QgsProviderMetadata.FilterType.FilterVector.__doc__ = ""
+QgsProviderMetadata.FilterType.FilterRaster.__doc__ = ""
+QgsProviderMetadata.FilterType.FilterMesh.__doc__ = ""
+QgsProviderMetadata.FilterType.FilterMeshDataset.__doc__ = ""
+QgsProviderMetadata.FilterType.__doc__ = 'Type of file filters\n\n.. versionadded:: 3.10\n\n' + '* ``FilterVector``: ' + QgsProviderMetadata.FilterType.FilterVector.__doc__ + '\n' + '* ``FilterRaster``: ' + QgsProviderMetadata.FilterType.FilterRaster.__doc__ + '\n' + '* ``FilterMesh``: ' + QgsProviderMetadata.FilterType.FilterMesh.__doc__ + '\n' + '* ``FilterMeshDataset``: ' + QgsProviderMetadata.FilterType.FilterMeshDataset.__doc__
+# --
