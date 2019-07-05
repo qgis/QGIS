8f88a5ba5236f16194ebd147dc763d0052a51fef
Nyall Dawson <nyall.dawson@gmail.com>
2019-07-05 23:46:33 +0000

    Warn users when min zoom > max zoom in xyz connection properties
    This results in a situation where no tiles can be loaded

--- src/providers/wms/qgsxyzconnectiondialog.cpp
+++ src/providers/wms/qgsxyzconnectiondialog.cpp
@@ -16,6 +16,7 @@
 #include "qgsxyzconnectiondialog.h"
 #include "qgsxyzconnection.h"
 #include "qgsgui.h"
+#include <QMessageBox>
 
 QgsXyzConnectionDialog::QgsXyzConnectionDialog( QWidget *parent )
   : QDialog( parent )
@@ -68,4 +69,14 @@ QgsXyzConnection QgsXyzConnectionDialog::connection() const
     conn.tilePixelRatio = 0;  // unknown
   conn.authCfg = mAuthSettings->configId( );
   return conn;
+}
+
+void QgsXyzConnectionDialog::accept()
+{
+  if ( mCheckBoxZMin->isChecked() && mCheckBoxZMax->isChecked() && mSpinZMax->value() < mSpinZMin->value() )
+  {
+    QMessageBox::warning( this, tr( "Connection Properties" ), tr( "The maximum zoom level (%1) cannot be lower than the minimum zoom level (%2)." ).arg( mSpinZMax->value() ).arg( mSpinZMin->value() ) );
+    return;
+  }
+  QDialog::accept();
 }
