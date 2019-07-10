d7c5f6e70f3acbf8965774226f6aa895f796a976
Nyall Dawson <nyall.dawson@gmail.com>
2019-07-10 22:55:35 +0000

    [processing] Fix occasional crash in Processing gui unit test cleanup

--- src/gui/processing/qgsprocessingmaplayercombobox.cpp
+++ src/gui/processing/qgsprocessingmaplayercombobox.cpp
@@ -28,9 +28,9 @@
 
 ///@cond PRIVATE
 
-QgsProcessingMapLayerComboBox::QgsProcessingMapLayerComboBox( QgsProcessingParameterDefinition *parameter, QWidget *parent )
+QgsProcessingMapLayerComboBox::QgsProcessingMapLayerComboBox( const QgsProcessingParameterDefinition *parameter, QWidget *parent )
   : QWidget( parent )
-  , mParameter( parameter )
+  , mParameter( parameter->clone() )
 {
   QHBoxLayout *layout = new QHBoxLayout();
   layout->setMargin( 0 );
@@ -68,9 +68,9 @@ QgsProcessingMapLayerComboBox::QgsProcessingMapLayerComboBox( QgsProcessingParam
   {
     QList<int> dataTypes;
     if ( mParameter->type() == QgsProcessingParameterFeatureSource::typeName() )
-      dataTypes = static_cast< QgsProcessingParameterFeatureSource *>( mParameter )->dataTypes();
+      dataTypes = static_cast< QgsProcessingParameterFeatureSource *>( mParameter.get() )->dataTypes();
     else if ( mParameter->type() == QgsProcessingParameterVectorLayer::typeName() )
-      dataTypes = static_cast< QgsProcessingParameterVectorLayer *>( mParameter )->dataTypes();
+      dataTypes = static_cast< QgsProcessingParameterVectorLayer *>( mParameter.get() )->dataTypes();
 
     if ( dataTypes.contains( QgsProcessing::TypeVectorAnyGeometry ) || dataTypes.isEmpty() )
       filters = QgsMapLayerProxyModel::HasGeometry;
@@ -118,6 +118,8 @@ QgsProcessingMapLayerComboBox::QgsProcessingMapLayerComboBox( QgsProcessingParam
 
   setAcceptDrops( true );
 }
+
+QgsProcessingMapLayerComboBox::~QgsProcessingMapLayerComboBox() = default;
 
 void QgsProcessingMapLayerComboBox::setLayer( QgsMapLayer *layer )
 {
@@ -276,8 +278,8 @@ QString QgsProcessingMapLayerComboBox::compatibleUriFromMimeData( const QMimeDat
            || mParameter->type() == QgsProcessingParameterMapLayer::typeName() )
          && u.layerType == QLatin1String( "vector" ) && u.providerKey == QLatin1String( "ogr" ) )
     {
-      QList< int > dataTypes =  mParameter->type() == QgsProcessingParameterFeatureSource::typeName() ? static_cast< QgsProcessingParameterFeatureSource * >( mParameter )->dataTypes()
-                                : ( mParameter->type() == QgsProcessingParameterVectorLayer::typeName() ? static_cast<QgsProcessingParameterVectorLayer *>( mParameter )->dataTypes()
+      QList< int > dataTypes =  mParameter->type() == QgsProcessingParameterFeatureSource::typeName() ? static_cast< QgsProcessingParameterFeatureSource * >( mParameter.get() )->dataTypes()
+                                : ( mParameter->type() == QgsProcessingParameterVectorLayer::typeName() ? static_cast<QgsProcessingParameterVectorLayer *>( mParameter.get() )->dataTypes()
                                     : QList< int >() );
       switch ( QgsWkbTypes::geometryType( u.wkbType ) )
       {
