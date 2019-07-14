2b89cf7f8ae8a43d8c271b2485116a2ebda88d25
Alex <roya0045@users.noreply.github.com>
2019-07-14 06:46:49 -0400

    doc and connection type

--- src/core/layout/qgslayoutitemlegend.cpp
+++ src/core/layout/qgslayoutitemlegend.cpp
@@ -922,8 +922,9 @@ QVariant QgsLegendModel::data( const QModelIndex &index, int role ) const
       QgsExpressionContext expressionContext;
       if ( vlayer )
       {
-        connect( vlayer, &QgsVectorLayer::symbolFeatureCountMapChanged, this, &QgsLegendModel::forceRefresh );
-        vlayer->countSymbolFeatures();
+        connect( vlayer, &QgsVectorLayer::symbolFeatureCountMapChanged, this, &QgsLegendModel::forceRefresh, Qt::UniqueConnection );
+        // counting is done here to ensure that a valid vector layer needs to be evaluated, count is used to validate previous count or update the count if invalidated
+        vlayer->countSymbolFeatures(); 
       }
 
       if ( mLayoutLegend )
