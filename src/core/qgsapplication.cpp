d7c8c0c96b6ab83f059961d4024cfe5f75eadad5
Nyall Dawson <nyall.dawson@gmail.com>
2019-07-15 12:28:57 +1000

    Ship a seperate srs.db file for proj >= 6 builds
    It's too dangerous to reuse the existing one, as it breaks the
    ability to run multiple versions of qgis (i.e. proj < 6 and proj >= 6)
    on the same machine
    
    Fixes #30569

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -953,6 +953,27 @@ QString QgsApplication::iconsPath()
 
 QString QgsApplication::srsDatabaseFilePath()
 {
+#if PROJ_VERSION_MAJOR>=6
+  if ( ABISYM( mRunningFromBuildDir ) )
+  {
+    QString tempCopy = QDir::tempPath() + "/srs6.db";
+
+    if ( !QFile( tempCopy ).exists() )
+    {
+      QFile f( pkgDataPath() + "/resources/srs6.db" );
+      if ( !f.copy( tempCopy ) )
+      {
+        qFatal( "Could not create temporary copy" );
+      }
+    }
+
+    return tempCopy;
+  }
+  else
+  {
+    return pkgDataPath() + QStringLiteral( "/resources/srs6.db" );
+  }
+#else
   if ( ABISYM( mRunningFromBuildDir ) )
   {
     QString tempCopy = QDir::tempPath() + "/srs.db";
@@ -972,6 +993,7 @@ QString QgsApplication::srsDatabaseFilePath()
   {
     return pkgDataPath() + QStringLiteral( "/resources/srs.db" );
   }
+#endif
 }
 
 QStringList QgsApplication::svgPaths()
