224216253fb6cb7306b5ceffa14dbefed2a7583c
Ismail Sunni <imajimatika@gmail.com>
2019-07-17 18:53:34 +0200

    Add billboard material class.

---
+++ src/3d/symbols/qgspoint3dbillboardmaterial.cpp
@@ -0,0 +1,123 @@
+/***************************************************************************
+  qgspoint3dbillboardmaterial.h
+  --------------------------------------
+  Date                 : Jul 2019
+  Copyright            : (C) 2019 by Ismail Sunni
+  Email                : imajimatika at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include <Qt3DRender/QParameter>
+#include <Qt3DRender/QShaderProgram>
+#include <Qt3DRender/QRenderPass>
+#include <Qt3DRender/QTechnique>
+#include <Qt3DRender/QGraphicsApiFilter>
+#include <Qt3DRender/QEffect>
+
+#include "qgspoint3dbillboardmaterial.h"
+
+QgsPoint3DBillboardMaterial::QgsPoint3DBillboardMaterial()
+  : mSize( new Qt3DRender::QParameter( "BB_SIZE", QSizeF( 100, 100 ), this ) )
+  , mWindowSize( new Qt3DRender::QParameter( "WIN_SCALE", QSizeF( 800, 600 ), this ) )
+{
+  addParameter( mSize );
+  addParameter( mWindowSize );
+
+  Qt3DRender::QTextureImage *image = new Qt3DRender::QTextureImage;
+  image->setSource( QUrl( QStringLiteral( "qrc:/shaders/success-kid.png" ) ) );
+
+  // Texture2D
+  Qt3DRender::QTexture2D *texture2D = new Qt3DRender::QTexture2D;
+  texture2D->setGenerateMipMaps( false );
+  texture2D->setMagnificationFilter( Qt3DRender::QTexture2D::Linear );
+  texture2D->setMinificationFilter( Qt3DRender::QTexture2D::Linear );
+
+  texture2D->addTextureImage( image );
+
+  mTexture2D = new Qt3DRender::QParameter( "tex0", texture2D, this );
+
+  addParameter( mTexture2D );
+
+  // Shader program
+  Qt3DRender::QShaderProgram *shaderProgram = new Qt3DRender::QShaderProgram( this );
+  shaderProgram->setVertexShaderCode( Qt3DRender::QShaderProgram::loadSource( QUrl( QStringLiteral( "qrc:/shaders/billboards.vert" ) ) ) );
+  shaderProgram->setFragmentShaderCode( Qt3DRender::QShaderProgram::loadSource( QUrl( QStringLiteral( "qrc:/shaders/billboards.frag" ) ) ) );
+  shaderProgram->setGeometryShaderCode( Qt3DRender::QShaderProgram::loadSource( QUrl( QStringLiteral( "qrc:/shaders/billboards.geom" ) ) ) );
+
+  // Render Pass
+  Qt3DRender::QRenderPass *renderPass = new Qt3DRender::QRenderPass( this );
+  renderPass->setShaderProgram( shaderProgram );
+
+  // without this filter the default forward renderer would not render this
+  Qt3DRender::QFilterKey *filterKey = new Qt3DRender::QFilterKey;
+  filterKey->setName( QStringLiteral( "renderingStyle" ) );
+  filterKey->setValue( "forward" );
+
+  // Technique
+  Qt3DRender::QTechnique *technique = new Qt3DRender::QTechnique;
+  technique->addRenderPass( renderPass );
+  technique->addFilterKey( filterKey );
+  technique->graphicsApiFilter()->setApi( Qt3DRender::QGraphicsApiFilter::OpenGL );
+  technique->graphicsApiFilter()->setProfile( Qt3DRender::QGraphicsApiFilter::CoreProfile );
+  technique->graphicsApiFilter()->setMajorVersion( 3 );
+  technique->graphicsApiFilter()->setMinorVersion( 1 );
+
+  // Effect
+  Qt3DRender::QEffect *effect = new Qt3DRender::QEffect( this );
+  effect->addTechnique( technique );
+
+  setEffect( effect );
+}
+
+void QgsPoint3DBillboardMaterial::setSize( const QSizeF size )
+{
+  mSize->setValue( size );
+}
+
+QSizeF QgsPoint3DBillboardMaterial::size() const
+{
+  return mSize->value().value<QSizeF>();
+}
+
+void QgsPoint3DBillboardMaterial::setWindowSize( const QSizeF size )
+{
+  mWindowSize->setValue( size );
+}
+
+QSizeF QgsPoint3DBillboardMaterial::windowSize() const
+{
+  return mWindowSize->value().value<QSizeF>();
+}
+
+void QgsPoint3DBillboardMaterial::setTexture2D( Qt3DRender::QTexture2D *texture2D )
+{
+  mTexture2D->setValue( QVariant::fromValue( texture2D ) );
+}
+
+Qt3DRender::QTexture2D *QgsPoint3DBillboardMaterial::texture2D()
+{
+  QVariant variant = mTexture2D->value();
+  return qvariant_cast<Qt3DRender::QTexture2D *>( variant );
+}
+
+void QgsPoint3DBillboardMaterial::setTexture2DFromImagePath( QString imagePath )
+{
+  // Texture Image
+  Qt3DRender::QTextureImage *image = new Qt3DRender::QTextureImage;
+  image->setSource( QUrl( imagePath ) );
+
+  // Texture2D
+  Qt3DRender::QTexture2D *texture2D = new Qt3DRender::QTexture2D;
+  texture2D->setGenerateMipMaps( false );
+  texture2D->setMagnificationFilter( Qt3DRender::QTexture2D::Linear );
+  texture2D->setMinificationFilter( Qt3DRender::QTexture2D::Linear );
+
+  texture2D->addTextureImage( image );
+
+  setTexture2D( texture2D );
+}
---
+++ src/3d/symbols/qgspoint3dbillboardmaterial.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+  qgspoint3dbillboardmaterial.h
+  --------------------------------------
+  Date                 : Jul 2019
+  Copyright            : (C) 2019 by Ismail Sunni
+  Email                : imajimatika at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef QGSPOINT3DBILLBOARDMATERIAL_H
+#define QGSPOINT3DBILLBOARDMATERIAL_H
+
+#include <QObject>
+#include <Qt3DRender/QParameter>
+#include <Qt3DRender/QTexture>
+#include <Qt3DRender/QMaterial>
+
+class QgsPoint3DBillboardMaterial : public Qt3DRender::QMaterial
+{
+    Q_OBJECT
+
+  public:
+    QgsPoint3DBillboardMaterial();
+
+    void setSize( const QSizeF size );
+    QSizeF size() const;
+
+    void setWindowSize( const QSizeF size );
+    QSizeF windowSize() const;
+
+    void setTexture2D( Qt3DRender::QTexture2D *texture2D );
+    Qt3DRender::QTexture2D *texture2D();
+
+    void setTexture2DFromImagePath( QString imagePath );
+
+  private:
+    Qt3DRender::QParameter *mSize = nullptr;
+    Qt3DRender::QParameter *mWindowSize = nullptr;
+    Qt3DRender::QParameter *mTexture2D = nullptr;
+};
+
+#endif // QGSPOINT3DBILLBOARDMATERIAL_H
