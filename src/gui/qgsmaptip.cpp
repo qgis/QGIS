b2ee0182e20ef433b8325822ce81578027a915c1
Alexandre Neto <senhor.neto@gmail.com>
2019-07-23 14:34:32 +0000

    Better Support to hidpi

--- src/gui/qgsmaptip.cpp
+++ src/gui/qgsmaptip.cpp
@@ -144,7 +144,7 @@ void QgsMapTip::showMapTip( QgsMapLayer *pLayer,
 
   QgsDebugMsg( tipHtml );
 
-  mWidget->move( pixelPosition.x() + 20,
+  mWidget->move( pixelPosition.x() + Qgis::UI_SCALE_FACTOR * 15,
                  pixelPosition.y() );
 
   mWebView->setHtml( tipHtml );
