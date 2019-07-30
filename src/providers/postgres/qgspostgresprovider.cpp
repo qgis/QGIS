d2dbe01a1eb70b379a07b3ca7686daa26c50038c
Alban Kraus <alban.kraus@tulleagglo.fr>
2019-07-30 14:37:18 +0200

    PostgreSQL foreign tables: handle them like views (fixes #30990) [needs-docs]
    PostgreSQL foreign tables are not explicitely supported by the PosgreSQL provider.
    This commit removes the error message, and handle foreign tables like views.
    
    A foreign table cannot (in PostgreSQL <10) state what column
    is the primary key, so we let the user tell QGIS.

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -1420,7 +1420,7 @@ bool QgsPostgresProvider::determinePrimaryKey()
           QgsMessageLog::logMessage( tr( "The table has no column suitable for use as a key. QGIS requires a primary key, a PostgreSQL oid column or a ctid for tables." ), tr( "PostGIS" ) );
         }
       }
-      else if ( type == Relkind::View || type == Relkind::MaterializedView )
+      else if ( type == Relkind::View || type == Relkind::MaterializedView || type == Relkind::ForeignTable )
       {
         determinePrimaryKeyFromUriKeyColumn();
       }
