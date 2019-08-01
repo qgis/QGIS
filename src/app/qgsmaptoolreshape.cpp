d92480c2d6160924113bb68808d7fd2bd7fbcedc
lbartoletti <loic.bartoletti@oslandia.com>
2019-08-01 07:45:42 +0000

    topological editing: fix topological editing for reshape map tool

--- src/app/qgsmaptoolreshape.cpp
+++ src/app/qgsmaptoolreshape.cpp
@@ -182,6 +182,21 @@ void QgsMapToolReshape::reshape( QgsVectorLayer *vlayer )
 
   if ( reshapeDone )
   {
+    // Add topological points
+    if ( QgsProject::instance()->topologicalEditing() )
+    {
+      QList<QgsPointLocator::Match> sm = snappingMatches();
+      if ( pts.size() == sm.size() ) // should be always true
+      {
+        for ( int i = 0; i < sm.size() ; ++i )
+        {
+          if ( sm.at( i ).layer() )
+          {
+            sm.at( i ).layer()->addTopologicalPoints( pts.at( i ) );
+          }
+        }
+      }
+    }
     vlayer->endEditCommand();
   }
   else
