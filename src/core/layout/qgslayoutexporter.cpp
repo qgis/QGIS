a8d777d0a527ec27608ef673da8f636237019a9d
Nyall Dawson <nyall.dawson@gmail.com>
2019-08-07 11:27:54 +1000

    Use a more appropriate, global snap to grid algorithm when simplifying geometries for layout exports
    This avoids all chance of slivers or overlaps between previously
    adjacent features.

--- src/core/layout/qgslayoutexporter.cpp
+++ src/core/layout/qgslayoutexporter.cpp
@@ -1471,8 +1471,9 @@ QgsVectorSimplifyMethod QgsLayoutExporter::createExportSimplifyMethod()
   QgsVectorSimplifyMethod simplifyMethod;
   simplifyMethod.setSimplifyHints( QgsVectorSimplifyMethod::GeometrySimplification );
   simplifyMethod.setForceLocalOptimization( true );
-  simplifyMethod.setSimplifyAlgorithm( QgsVectorSimplifyMethod::SnapToGrid );
-  simplifyMethod.setThreshold( 0.5 ); // pixels
+  // we use SnappedToGridGlobal, because it avoids gaps and slivers between previously adjacent polygons
+  simplifyMethod.setSimplifyAlgorithm( QgsVectorSimplifyMethod::SnappedToGridGlobal );
+  simplifyMethod.setThreshold( 0.1f ); // (pixels). We are quite conservative here. This could possibly be bumped all the way up to 1. But let's play it safe.
   return simplifyMethod;
 }
 
