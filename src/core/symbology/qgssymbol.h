d422db84cead74d325bd9624064c930c3f9b3ff3
Ismail Sunni <imajimatika@gmail.com>
2019-08-24 09:31:20 +0300

    Remove documentation warning.

--- src/core/symbology/qgssymbol.h
+++ src/core/symbology/qgssymbol.h
@@ -322,6 +322,9 @@ class CORE_EXPORT QgsSymbol
      *
      * Optionally a custom render context may be given in order to ensure that the preview icon exactly
      * matches the settings from that context.
+     * \param painter destination painter
+     * \param size size of the icon
+     * \param customContext the context in which the rendering happens
      * \param selected set to TRUE to render the symbol in a selected state
      *
      * \see exportImage()
