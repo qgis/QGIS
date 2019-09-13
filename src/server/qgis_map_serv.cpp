61eb9a231a2e532e857d2c98781d71f3ccda6ad5
Stefan Pascal <13947175+stefan-pascal@users.noreply.github.com>
2019-09-13 13:45:34 +0000

    Fix QT font database initializing (FCGI with IIS)
    When using FCGI with IIS, environment variables (QT_QPA_FONTDIR in this case) are lost after fcgi_accept().

--- src/server/qgis_map_serv.cpp
+++ src/server/qgis_map_serv.cpp
@@ -65,6 +65,13 @@ int main( int argc, char *argv[] )
 #ifdef HAVE_SERVER_PYTHON_PLUGINS
   server.initPython();
 #endif
+
+#ifdef Q_OS_WIN
+  // Initialize font database before fcgi_accept.
+  // When using FCGI with IIS, environment variables (QT_QPA_FONTDIR in this case) are lost after fcgi_accept().
+  QFontDatabase fontDB;
+#endif
+  
   // Starts FCGI loop
   while ( fcgi_accept() >= 0 )
   {
