979237515ce2921b9552a99eda071e9e9a14297a
Nyall Dawson <nyall.dawson@gmail.com>
2019-09-17 15:52:58 +1000

    [properties] Do not return nan values when an assistant is used with min val == max val
    Fixes #31242

--- src/core/qgspropertytransformer.cpp
+++ src/core/qgspropertytransformer.cpp
@@ -175,6 +175,9 @@ bool QgsGenericNumericTransformer::loadVariant( const QVariant &transformer )
 
 double QgsGenericNumericTransformer::value( double input ) const
 {
+  if ( qgsDoubleNear( mMaxValue, mMinValue ) )
+    return qBound( mMinOutput, input, mMaxOutput );
+
   input = transformNumeric( input );
   if ( qgsDoubleNear( mExponent, 1.0 ) )
     return mMinOutput + ( qBound( mMinValue, input, mMaxValue ) - mMinValue ) * ( mMaxOutput - mMinOutput ) / ( mMaxValue - mMinValue );
