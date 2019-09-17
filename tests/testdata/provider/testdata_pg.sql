f7d1971d01a58f2cd2cb52681a5adb5c11dd7e47
Alessandro Pasotti <elpaso@itopen.it>
2019-09-17 14:30:33 +0200

    PG views: be tolerant for unsupported field type ...
    ... if it's in URI key
    
    Fixes #31799

--- tests/testdata/provider/testdata_pg.sql
+++ tests/testdata/provider/testdata_pg.sql
@@ -653,3 +653,12 @@ CREATE VIEW qgis_test.some_poly_data_shift_bbox AS
           0.0
         ) AS shiftbox
    FROM qgis_test.some_poly_data;
+
+
+---------------------------------------------
+--
+-- View with tid PK field
+--
+
+CREATE TABLE qgis_test.b31799_test_table AS (SELECT (ST_DumpPoints(ST_GeneratePoints(ST_Expand('SRID=4326;POINT(0 0)'::geometry,90),10))).geom, random());
+CREATE VIEW qgis_test.b31799_test_view_ctid AS (SELECT ctid, geom, random() FROM qgis_test.b31799_test_table, pg_sleep(0.1));
