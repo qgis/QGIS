186581cfba08e69501d465ad38964fb690c25191
BogDan Vatra <bogdan@kdab.com>
2019-09-20 11:34:29 +0000

    Block the events while showing a dialog
    When showing a file dialog, Qt can choose to use the "system" file
    dialog, which will make QgsExternalResourceWidget to loose the focus.
    This patch blocks all the events that are sent to
    QgsExternalResourceWidget while a dialog is shown, this way it will keep
    the focus until the dialog is closed.
    
    Sponsored by the QGIS project (qgis.org)
    
    Fixes: #26948

--- src/gui/qgsfilewidget.cpp
+++ src/gui/qgsfilewidget.cpp
@@ -253,6 +253,7 @@ void QgsFileWidget::openFileDialog()
   QStringList fileNames;
   QString title;
 
+  emit blockEvents( true );
   switch ( mStorageMode )
   {
     case GetFile:
@@ -285,6 +286,7 @@ void QgsFileWidget::openFileDialog()
     }
     break;
   }
+  emit blockEvents( false );
 
   if ( fileName.isEmpty() && fileNames.isEmpty( ) )
     return;
