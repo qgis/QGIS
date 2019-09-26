09bb5ad29a42219b166d4cb8e3d53a5fb650abaf
Nyall Dawson <nyall.dawson@gmail.com>
2019-09-26 13:00:55 +1000

    [layouts] Fix crash when exporting layout with no pages

--- src/core/layout/qgslayoutexporter.cpp
+++ src/core/layout/qgslayoutexporter.cpp
@@ -506,7 +506,7 @@ QgsLayoutExporter::ExportResult QgsLayoutExporter::exportToImage( QgsAbstractLay
 
 QgsLayoutExporter::ExportResult QgsLayoutExporter::exportToPdf( const QString &filePath, const QgsLayoutExporter::PdfExportSettings &s )
 {
-  if ( !mLayout )
+  if ( !mLayout || mLayout->pageCollection()->pageCount() == 0 )
     return PrintError;
 
   PdfExportSettings settings = s;
@@ -1186,6 +1186,9 @@ void QgsLayoutExporter::preparePrint( QgsLayout *layout, QPrinter &printer, bool
 
 QgsLayoutExporter::ExportResult QgsLayoutExporter::print( QPrinter &printer )
 {
+  if ( mLayout->pageCollection()->pageCount() == 0 )
+    return PrintError;
+
   preparePrint( mLayout, printer, true );
   QPainter p;
   if ( !p.begin( &printer ) )
