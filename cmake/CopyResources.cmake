55660d6bae682009b1f4395a629d3143d01b363e
Matthias Kuhn <matthias@opengis.ch>
2019-10-06 22:18:43 +0200

    Add the cmake docs while I still remember how things are supposed to work

--- cmake/CopyResources.cmake
+++ cmake/CopyResources.cmake
@@ -1,3 +1,14 @@
+##
+# The ADD_QGIS_RESOURCES macro handles resource file installation
+#
+# - at build time it copies files to buildPath/output/data
+# - at install time it installs the files into the qgis data folder (typically INSTALL_PREFIX/share/qgis)
+#
+# The SOURCE_PREFIX is the base path of all the files in the source folder (typically ${CMAKE_CURRENT_SOURCE_DIR})
+# The TARGET_PREFIX is the subdirectory inside the data folder where files should be placed
+# DEST_FILES is an output variable where a list of all generated files in the build folder is written to. This can be used to define a custom target
+# The SOURCE_FILE_PATHS takes a list of files relative to SOURCE_PREFIX
+##
 MACRO(ADD_QGIS_RESOURCES SOURCE_PREFIX TARGET_PREFIX DEST_FILES SOURCE_FILE_PATHS)
 
 # On build copy all resource files to build folder
