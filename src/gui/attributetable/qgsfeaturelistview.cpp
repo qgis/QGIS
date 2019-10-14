0a5ae196187c532dafbfa2e04e685721e6ca4feb
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-10-14 09:24:06 +0200

    Update qgsfeaturelistview.cpp

--- src/gui/attributetable/qgsfeaturelistview.cpp
+++ src/gui/attributetable/qgsfeaturelistview.cpp
@@ -204,14 +204,20 @@ void QgsFeatureListView::setEditSelection( const QModelIndex &index, QItemSelect
 {
   bool ok = true;
   emit aboutToChangeEditSelection( ok );
-
-#ifdef QGISDEBUG
+  
   if ( index.isValid() && index.model() != mModel->masterModel() )
+  {
+#ifdef QGISDEBUG 
     qWarning() << "Index from wrong model passed in";
 #endif
-
-  if ( ok )
-    mCurrentEditSelectionModel->select( index, command );
+    if ( ok )
+      mCurrentEditSelectionModel->select( QModelIndex(), command );
+  }
+  else
+  {
+    if ( ok )
+      mCurrentEditSelectionModel->select( index, command );
+  }
 }
 
 void QgsFeatureListView::repaintRequested( const QModelIndexList &indexes )
