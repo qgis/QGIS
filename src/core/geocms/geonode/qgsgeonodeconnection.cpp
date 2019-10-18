f225ee06b76e486a2c035516eddfa30105886e13
myarjunar <myarjunar@gmail.com>
2019-10-18 16:35:14 +0700

    [geonode] fix data item context menu

--- src/core/geocms/geonode/qgsgeonodeconnection.cpp
+++ src/core/geocms/geonode/qgsgeonodeconnection.cpp
@@ -102,10 +102,7 @@ QStringList QgsGeoNodeConnectionUtils::connectionList()
 
 void QgsGeoNodeConnectionUtils::deleteConnection( const QString &name )
 {
-  QgsSettings settings;
-  // Add Section manually
-  settings.remove( QStringLiteral( "qgis/connections-geonode/" ) + name );
-  settings.remove( QStringLiteral( "qgis/geonode/" ) + name );
+  QgsOwsConnection::deleteConnection( QStringLiteral( "GEONODE" ), name );
 }
 
 QString QgsGeoNodeConnectionUtils::pathGeoNodeConnection()
