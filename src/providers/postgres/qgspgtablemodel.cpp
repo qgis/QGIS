312ac93b58a7ecd893aff7214ce5c3a3300d7942
Alessandro Pasotti <elpaso@itopen.it>
2019-10-23 15:06:58 +0200

    PG source select: remove initial newlines from table comments

--- src/providers/postgres/qgspgtablemodel.cpp
+++ src/providers/postgres/qgspgtablemodel.cpp
@@ -102,7 +102,10 @@ void QgsPgTableModel::addTableEntry( const QgsPostgresLayerProperty &layerProper
     if ( ! layerProperty.tableComment.isEmpty() )
     {
       // word wrap
-      commentItem->setToolTip( QStringLiteral( "<span>%1</span>" ).arg( layerProperty.tableComment ).replace( '\n', QStringLiteral( "<br/>" ) ) );
+      QString commentText { layerProperty.tableComment };
+      commentText.replace( QRegularExpression( QStringLiteral( "^\n*" ) ), QString() );
+      commentItem->setText( commentText );
+      commentItem->setToolTip( QStringLiteral( "<span>%1</span>" ).arg( commentText.replace( '\n', QStringLiteral( "<br/>" ) ) ) );
       commentItem->setTextAlignment( Qt::AlignTop );
     }
     QStandardItem *geomItem  = new QStandardItem( layerProperty.geometryColName );
