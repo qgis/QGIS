1fd26732e8001e317735bb6ae439d482920d29d1
Juergen E. Fischer <jef@norbit.de>
2019-10-27 13:29:02 +0100

    fix python constructor for QgsPoint (does not accept QgsPointXY &  QPointF anymore)
    fixes #32443

--- tests/src/python/test_qgspoint.py
+++ tests/src/python/test_qgspoint.py
@@ -12,7 +12,8 @@ __copyright__ = 'Copyright 2012, The QGIS Project'
 
 import qgis  # NOQA
 
-from qgis.core import QgsPointXY
+from qgis.core import QgsPointXY, QgsPoint, QgsWkbTypes
+from qgis.PyQt.QtCore import QPointF
 
 from qgis.testing import start_app, unittest
 
@@ -51,6 +52,39 @@ class TestQgsPointXY(unittest.TestCase):
 
         mySet = set([a, b, c, d, e])
         assert len(mySet) == 4
+
+    def test_issue_32443(self):
+        p = QgsPoint()
+        assert p.wkbType() == QgsWkbTypes.Point and p.x() != p.x() and p.y() != p.y()
+
+        # ctor from QgsPointXY should be available
+        p = QgsPoint(QgsPointXY(1, 2))
+        assert p.wkbType() == QgsWkbTypes.Point and p.x() == 1 and p.y() == 2
+
+        # ctor from QPointF should be available
+        p = QgsPoint(QPointF(1, 2))
+        assert p.wkbType() == QgsWkbTypes.Point and p.x() == 1 and p.y() == 2
+
+        p = QgsPoint(1, 2)
+        assert p.wkbType() == QgsWkbTypes.Point and p.x() == 1 and p.y() == 2
+
+        p = QgsPoint(1, 2, 3)
+        assert p.wkbType() == QgsWkbTypes.PointZ and p.x() == 1 and p.y() == 2 and p.z() == 3
+
+        p = QgsPoint(1, 2, z=3)
+        assert p.wkbType() == QgsWkbTypes.PointZ and p.x() == 1 and p.y() == 2 and p.z() == 3
+
+        p = QgsPoint(1, 2, m=3)
+        assert p.wkbType() == QgsWkbTypes.PointM and p.x() == 1 and p.y() == 2 and p.m() == 3
+
+        p = QgsPoint(1, 2, wkbType=QgsWkbTypes.PointM)
+        assert p.wkbType() == QgsWkbTypes.PointM and p.x() == 1 and p.y() == 2 and p.m() != p.m()
+
+        p = QgsPoint(1, 2, 3, 4)
+        assert p.wkbType() == QgsWkbTypes.PointZM and p.x() == 1 and p.y() == 2 and p.z() == 3 and p.m() == 4
+
+        p = QgsPoint(1, 2, m=4, z=3)
+        assert p.wkbType() == QgsWkbTypes.PointZM and p.x() == 1 and p.y() == 2 and p.z() == 3 and p.m() == 4
 
 
 if __name__ == '__main__':
