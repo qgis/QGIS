960b1fdc2af74fb7e912f6a0a3ec7cec3ab3de43
Matthias Kuhn <matthias@opengis.ch>
2019-10-30 18:10:19 +0000

    Typo

--- python/core/auto_additions/qgsdxfexport.py
+++ python/core/auto_additions/qgsdxfexport.py
@@ -4,5 +4,5 @@ QgsDxfExport.ExportResult.Success.__doc__ = "Successful export"
 QgsDxfExport.ExportResult.InvalidDeviceError.__doc__ = "Invalid device error"
 QgsDxfExport.ExportResult.DeviceNotWritableError.__doc__ = "Device not writable error"
 QgsDxfExport.ExportResult.EmptyExtentError.__doc__ = "Empty extent, no extent given and no extent could be derived from layers"
-QgsDxfExport.ExportResult.__doc__ = 'The result of an export as dxf operaion\n\n.. versionadded:: 3.12\n\n' + '* ``Success``: ' + QgsDxfExport.ExportResult.Success.__doc__ + '\n' + '* ``InvalidDeviceError``: ' + QgsDxfExport.ExportResult.InvalidDeviceError.__doc__ + '\n' + '* ``DeviceNotWritableError``: ' + QgsDxfExport.ExportResult.DeviceNotWritableError.__doc__ + '\n' + '* ``EmptyExtentError``: ' + QgsDxfExport.ExportResult.EmptyExtentError.__doc__
+QgsDxfExport.ExportResult.__doc__ = 'The result of an export as dxf operation\n\n.. versionadded:: 3.12\n\n' + '* ``Success``: ' + QgsDxfExport.ExportResult.Success.__doc__ + '\n' + '* ``InvalidDeviceError``: ' + QgsDxfExport.ExportResult.InvalidDeviceError.__doc__ + '\n' + '* ``DeviceNotWritableError``: ' + QgsDxfExport.ExportResult.DeviceNotWritableError.__doc__ + '\n' + '* ``EmptyExtentError``: ' + QgsDxfExport.ExportResult.EmptyExtentError.__doc__
 # --
