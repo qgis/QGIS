b708e2985ded2c182b56abc0fb112c20638b6ee2
Matthias Kuhn <matthias@opengis.ch>
2019-10-30 15:23:10 +0000

    Use class enum

---
+++ python/core/auto_additions/qgsdxfexport.py
@@ -0,0 +1,8 @@
+# The following has been generated automatically from src/core/dxf/qgsdxfexport.h
+# monkey patching scoped based enum
+QgsDxfExport.ExportResult.Success.__doc__ = "Successful export"
+QgsDxfExport.ExportResult.InvalidDeviceError.__doc__ = "Invalid device error"
+QgsDxfExport.ExportResult.DeviceNotWritableError.__doc__ = "Device not writable error"
+QgsDxfExport.ExportResult.EmptyExtentError.__doc__ = "Empty extent, no extent given and no extent could be derived from layers"
+QgsDxfExport.ExportResult.__doc__ = 'The result of an export as dxf operaion\n\n.. versionadded:: 3.12\n\n' + '* ``Success``: ' + QgsDxfExport.ExportResult.Success.__doc__ + '\n' + '* ``InvalidDeviceError``: ' + QgsDxfExport.ExportResult.InvalidDeviceError.__doc__ + '\n' + '* ``DeviceNotWritableError``: ' + QgsDxfExport.ExportResult.DeviceNotWritableError.__doc__ + '\n' + '* ``EmptyExtentError``: ' + QgsDxfExport.ExportResult.EmptyExtentError.__doc__
+# --
