14af562b91650a7427113c4a64d98d82820552fc
Nyall Dawson <nyall.dawson@gmail.com>
2019-11-01 16:26:33 +1000

    [processing] Allow enum input values to be used in expressions for model inputs
    Fixes #32452
    
    (cherry picked from commit c8255a99ea49cce7c3d497e430934e8bf981f221)

--- src/core/processing/models/qgsprocessingmodelalgorithm.cpp
+++ src/core/processing/models/qgsprocessingmodelalgorithm.cpp
@@ -721,6 +721,7 @@ QMap<QString, QgsProcessingModelAlgorithm::VariableDefinition> QgsProcessingMode
       << QgsProcessingParameterDistance::typeName()
       << QgsProcessingParameterScale::typeName()
       << QgsProcessingParameterBoolean::typeName()
+      << QgsProcessingParameterEnum::typeName()
       << QgsProcessingParameterExpression::typeName()
       << QgsProcessingParameterField::typeName()
       << QgsProcessingParameterString::typeName()
