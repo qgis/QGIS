04fd7f6a91cb88c247bba62fc699025fa92bea42
Matthias Kuhn <matthias@opengis.ch>
2019-11-02 16:48:33 +0100

    License header

--- src/core/dxf/qgsdxfexport_p.h
+++ src/core/dxf/qgsdxfexport_p.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                         qgsdxfexport_p.h
+                         --------------
+    begin                : November 2019
+    copyright            : (C) 2019 by Matthias Kuhn
+    email                : matthias@opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 // dxf color palette
 static int sDxfColors[][3] =
