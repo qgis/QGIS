198b048b14084ff2a30f93dd5e1c6c5958757ee5
Juergen E. Fischer <jef@norbit.de>
2019-11-02 10:02:56 +0100

    osgeo4w: detect grass78

--- ms-windows/osgeo4w/msvc-env.bat
+++ ms-windows/osgeo4w/msvc-env.bat
@@ -45,6 +45,7 @@ path %path%;%PF86%\Microsoft Visual Studio 14.0\VC\bin
 set GRASS7=
 if exist %OSGEO4W_ROOT%\bin\grass74.bat set GRASS7=%OSGEO4W_ROOT%\bin\grass74.bat
 if exist %OSGEO4W_ROOT%\bin\grass76.bat set GRASS7=%OSGEO4W_ROOT%\bin\grass76.bat
+if exist %OSGEO4W_ROOT%\bin\grass78.bat set GRASS7=%OSGEO4W_ROOT%\bin\grass78.bat
 if "%GRASS7%"=="" (echo GRASS7 not found & goto error)
 for /f "usebackq tokens=1" %%a in (`%GRASS7% --config path`) do set GRASS_PREFIX=%%a
 
