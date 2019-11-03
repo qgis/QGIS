50aa7e9ac96d1cf826c39861a209791a394a9051
Alessandro Pasotti <elpaso@itopen.it>
2019-11-03 17:10:18 +0100

    Trick the json exporter to not transform geoms
    This is required because of a missing API
    that was added with
    
    https://github.com/qgis/QGIS/pull/32386

--- src/server/services/wfs3/qgswfs3handlers.cpp
+++ src/server/services/wfs3/qgswfs3handlers.cpp
@@ -1077,8 +1077,8 @@ void QgsWfs3CollectionsItemsHandler::handleRequest( const QgsServerApiContext &c
     QgsJsonExporter exporter { mapLayer };
     exporter.setAttributes( featureRequest.subsetOfAttributes() );
     exporter.setAttributeDisplayName( true );
-    exporter.setSourceCrs( mapLayer->crs() );
-    exporter.setTransformGeometries( false );
+    // Trick! See: https://github.com/qgis/QGIS/commit/7d2000bc30b2160692baf43c22d7ef4b47e52cff
+    exporter.setSourceCrs( QgsCoordinateReferenceSystem( 4326, QgsCoordinateReferenceSystem::EpsgCrsId ) );
     QgsFeatureList featureList;
     QgsFeatureIterator features { mapLayer->getFeatures( featureRequest ) };
     QgsFeature feat;
