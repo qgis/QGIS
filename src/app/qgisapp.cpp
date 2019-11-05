e5eb23e30ce7ead00ffa8cc3fedf2637ecbbd61b
Blottiere Paul <blottiere.paul@gmail.com>
2019-11-06 00:24:20 +0100

    Read qgz project when embedding layers. Fixes #28280

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -11597,6 +11597,13 @@ void QgisApp::embedLayers()
 
     QString projectFile = d.selectedProjectFile();
 
+    QgsProjectArchive archive;
+    if ( projectFile.endsWith( QLatin1String( ".qgz" ), Qt::CaseInsensitive ) )
+    {
+      archive.unzip( projectFile );
+      projectFile = archive.projectFile();
+    }
+
     //groups
     QStringList groups = d.selectedGroups();
     QStringList::const_iterator groupIt = groups.constBegin();
