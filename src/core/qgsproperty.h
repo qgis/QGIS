2bcd3b02a64a78bac0cd3a1aa7d8217036ccea31
Matthias Kuhn <matthias@opengis.ch>
2019-11-05 10:20:51 +0100

    Fix more private includes

--- src/core/qgsproperty.h
+++ src/core/qgsproperty.h
@@ -17,7 +17,6 @@
 
 #include "qgis_core.h"
 #include "qgis_sip.h"
-#include "qgsproperty_p.h"
 #include "qgsexpression.h"
 #include "qgsexpressioncontext.h"
 #include "qgscolorramp.h"
@@ -31,6 +30,7 @@
 #include <QColor>
 
 class QgsPropertyTransformer;
+class QgsPropertyPrivate;
 
 /**
  * \ingroup core
