129165a1001deb4b071c0b1d037e09d10f042ffc
Nyall Dawson <nyall.dawson@gmail.com>
2019-11-10 06:13:43 +1000

    Fix warning

--- src/server/services/wms/qgswmsgetschemaextension.cpp
+++ src/server/services/wms/qgswmsgetschemaextension.cpp
@@ -37,7 +37,7 @@ namespace QgsWms
   }
 
   QDomDocument getSchemaExtension( QgsServerInterface *serverIface, const QString &version,
-                                   const QgsServerRequest &request )
+                                   const QgsServerRequest & )
   {
     Q_UNUSED( version )
     Q_UNUSED( serverIface )
