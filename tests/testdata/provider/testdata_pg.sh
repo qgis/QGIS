c20ba1e5f35299ccd3872a821df824fd169cb4c3
Sandro Santilli <strk@kbt.io>
2019-11-14 11:54:01 +0100

    Have db setup script check for pg_service.conf functionality
    ... and hint about how to set it up

--- tests/testdata/provider/testdata_pg.sh
+++ tests/testdata/provider/testdata_pg.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 DB=${DB:-qgis_test}
 
 SCRIPTS="
@@ -16,7 +14,31 @@ SCRIPTS="
 "
 
 dropdb --if-exists $DB
-createdb $DB -E UTF8 -T template0
+createdb $DB -E UTF8 -T template0 || exit 1
 for f in ${SCRIPTS}; do
-  psql -q --echo-errors -c "SET client_min_messages TO WARNING;" -f $f $DB -v ON_ERROR_STOP=1
+  psql -q --echo-errors -c "SET client_min_messages TO WARNING;" -f $f $DB -v ON_ERROR_STOP=1 || exit 1
 done
+
+# Test existence of qgis_test service, and recommend how to set it up
+# otherwise
+TESTDB=$(psql -XtA 'service=qgis_test' -c "select current_database()")
+if test "${TESTDB}" != "${DB}"; then
+  exec >&2
+  if test -n "${TESTDB}"; then
+    echo "WARNING: [qgis_test] service section points to db '${TESTDB}'" \
+         "but we populated db '${DB}' instead"
+  else
+    echo "ERROR: [qgis_test] service not found in ~/.pg_service.conf"
+    echo "HINT: create a section like the following:"
+    cat <<EOF
+  [qgis_test]
+  host=localhost
+  port=5432
+  dbname=${DB}
+  user=USERNAME
+  password=PASSWORD
+EOF
+  fi
+else
+  echo "Database ${DB} populated and ready for use with 'service=qgis_test'"
+fi
