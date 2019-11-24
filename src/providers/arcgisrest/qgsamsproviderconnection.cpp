12ea84b56fc7bedf22550cd99b6c4a9904f00212
Alessandro Pasotti <elpaso@itopen.it>
2019-11-24 16:39:45 +0100

    Add file headers

--- src/providers/arcgisrest/qgsamsproviderconnection.cpp
+++ src/providers/arcgisrest/qgsamsproviderconnection.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+  qgsamsproviderconnection.cpp - QgsAmsProviderConnection
+
+ ---------------------
+ begin                : 24.11.2019
+ copyright            : (C) 2019 by Alessandro Pasotti
+ email                : elpaso at itopen dot it
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsamsproviderconnection.h"
 #include "qgsowsconnection.h"
 #include "qgssettings.h"
--- src/providers/arcgisrest/qgsamsproviderconnection.h
+++ src/providers/arcgisrest/qgsamsproviderconnection.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+  qgsamsproviderconnection.h - QgsAmsProviderConnection
+
+ ---------------------
+ begin                : 24.11.2019
+ copyright            : (C) 2019 by Alessandro Pasotti
+ email                : elpaso at itopen dot it
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSAMSPROVIDERCONNECTION_H
 #define QGSAMSPROVIDERCONNECTION_H
 
