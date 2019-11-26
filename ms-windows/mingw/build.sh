0879e72fa6160bcbef651fec5338a7def9bc900a
Alessandro Pasotti <elpaso@itopen.it>
2019-11-26 14:20:52 +0100

    MingW test 2

--- ms-windows/mingw/build.sh
+++ ms-windows/mingw/build.sh
@@ -5,6 +5,8 @@ debug=false
 if [ "$2" == "debug" ]; then
   debug=true
 fi
+njobs=${3:-$(($(grep -c ^processor /proc/cpuinfo) * 3 / 2))}
+
 
 if [ "$arch" == "i686" ]; then
     bits=32
@@ -18,7 +20,7 @@ fi
 # Do copies instead of links if building inside container
 if [ -f /.dockerenv ]; then
     lnk() {
-        cp -a "$1" "$2"
+        cp -aL "$1" "$2"
     }
 else
     lnk() {
@@ -101,7 +103,6 @@ mkdir -p $builddir
 # Xvfb :99 &
 # export DISPLAY=:99
 
-njobs=$(($(grep -c ^processor /proc/cpuinfo) * 3 / 2))
 mingw$bits-make -C$builddir -j$njobs DESTDIR="${installroot}" install VERBOSE=1
 
 binaries=$(find $installprefix -name '*.exe' -or -name '*.dll' -or -name '*.pyd')
