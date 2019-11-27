7de670538079b543c3699652fcbb0d0a4808adf4
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-11-27 13:40:08 +0100

    [field calc] do not propose to update field for read-only layers

--- src/app/qgsfieldcalculator.h
+++ src/app/qgsfieldcalculator.h
@@ -57,6 +57,9 @@ class APP_EXPORT QgsFieldCalculator: public QDialog, private Ui::QgsFieldCalcula
 
     QgsVectorLayer *mVectorLayer = nullptr;
 
+    bool mCanAddAttribute;
+    bool mCanChangeAttributeValue;
+
     //! Create a field based on the definitions
     QgsField fieldDefinition();
 
