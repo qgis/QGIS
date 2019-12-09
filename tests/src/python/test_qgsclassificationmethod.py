0391c8536074419d688ef0a087c7a3fd2c6827ed
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-12-09 22:29:48 +0100

    fix test

--- tests/src/python/test_qgsclassificationmethod.py
+++ tests/src/python/test_qgsclassificationmethod.py
@@ -74,13 +74,13 @@ class TestQgsClassificationMethods(unittest.TestCase):
         vl = createMemoryLayer(values)
         m = QgsClassificationLogarithmic()
 
-        m.setParameterValues({'ZERO_NEG_VALUES_HANDLE': QgsClassificationLogarithmic.NegativeValueHandling.Discard})
+        m.setParameterValues({'ZERO_NEG_VALUES_HANDLE': QgsClassificationLogarithmic.Discard})
         r = m.classes(vl, 'value', 4)
         self.assertEqual(len(r), 4)
         self.assertEqual(r[0].label(), '1 - 10^1')
         self.assertEqual(QgsClassificationMethod.rangesToBreaks(r), [10.0, 100.0, 1000.0, 10000.0])
 
-        m.setParameterValues({'ZERO_NEG_VALUES_HANDLE': QgsClassificationLogarithmic.NegativeValueHandling.PrependBreak})
+        m.setParameterValues({'ZERO_NEG_VALUES_HANDLE': QgsClassificationLogarithmic.PrependBreak})
         r = m.classes(vl, 'value', 4)
         self.assertEqual(r[0].label(), '-2 - 10^0')
         self.assertEqual(QgsClassificationMethod.rangesToBreaks(r), [1.0, 10.0, 100.0, 1000.0, 10000.0])
