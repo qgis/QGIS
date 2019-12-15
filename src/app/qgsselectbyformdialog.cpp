5190aa653c4d5d4bb2977e4b36bd4b6ce69ac99a
Alessandro Pasotti <elpaso@itopen.it>
2019-12-15 21:43:28 +0100

    Select features by value: show layer name in window title
    Fixes #33210

--- src/app/qgsselectbyformdialog.cpp
+++ src/app/qgsselectbyformdialog.cpp
@@ -47,7 +47,7 @@ QgsSelectByFormDialog::QgsSelectByFormDialog( QgsVectorLayer *layer, const QgsAt
 
   QgsGui::enableAutoGeometryRestore( this );
 
-  setWindowTitle( tr( "Select Features by Value" ) );
+  setWindowTitle( tr( "%1 - Select Features by Value" ).arg( layer->name() ) );
 }
 
 void QgsSelectByFormDialog::setMessageBar( QgsMessageBar *messageBar )
