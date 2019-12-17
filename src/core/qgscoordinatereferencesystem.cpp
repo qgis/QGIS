2c6f49c5de923dc6c73beff6a16398d6fc906aae
Nyall Dawson <nyall.dawson@gmail.com>
2019-12-17 11:19:48 +1000

    Don't incorrectly flag unknown CRS created from proj strings as invalid on proj 6+ builds
    (cherry picked from commit 5cfa6627b47cacc92fc4b6d46539f8cbab5061f6)

--- src/core/qgscoordinatereferencesystem.cpp
+++ src/core/qgscoordinatereferencesystem.cpp
@@ -1008,6 +1008,7 @@ bool QgsCoordinateReferenceSystem::createFromProj( const QString &projString )
   {
     // Last ditch attempt to piece together what we know of the projection to find a match...
     setProjString( myProj4String );
+#if PROJ_VERSION_MAJOR<6
     Q_NOWARN_DEPRECATED_PUSH
     mySrsId = findMatchingProj();
     Q_NOWARN_DEPRECATED_POP
@@ -1019,6 +1020,7 @@ bool QgsCoordinateReferenceSystem::createFromProj( const QString &projString )
     {
       d->mIsValid = false;
     }
+#endif
   }
 #endif
 
