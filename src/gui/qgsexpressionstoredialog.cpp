6b232414f21397539104f60a9924d1d71991e7ab
Alessandro Pasotti <elpaso@itopen.it>
2019-12-17 15:00:48 +0100

    Added new files header

--- src/gui/qgsexpressionstoredialog.cpp
+++ src/gui/qgsexpressionstoredialog.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+    qgsexpressionstoredialog.cpp
+    ---------------------
+    begin                : December 2019
+    copyright            : (C) 2019 by Alessandro Pasotti
+    email                : elpaso at itopen dot it
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsexpressionstoredialog.h"
 #include <QPushButton>
 #include <QStyle>
--- src/gui/qgsexpressionstoredialog.h
+++ src/gui/qgsexpressionstoredialog.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+    qgsexpressionstoredialog.h
+    ---------------------
+    begin                : December 2019
+    copyright            : (C) 2019 by Alessandro Pasotti
+    email                : elpaso at itopen dot it
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSEXPRESSIONSTOREDIALOG_H
 #define QGSEXPRESSIONSTOREDIALOG_H
 
