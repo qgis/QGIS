7523d9305307146d4ae12fa0640526f684711301
Nyall Dawson <nyall.dawson@gmail.com>
2019-12-17 10:41:26 +1000

    Don't use a boundCRS definition as the WKT definition of EPSG:4326 on proj 6 builds
    See https://github.com/OSGeo/PROJ/issues/1804
    
    (cherry picked from commit 550dede70e233b2f6a036e6402f0081c98e603c5)

--- src/core/qgis.cpp
+++ src/core/qgis.cpp
@@ -50,6 +50,9 @@ const QString Qgis::QGIS_RELEASE_NAME( QString::fromUtf8( RELEASE_NAME ) );
 
 const QString GEOPROJ4 = QStringLiteral( "+proj=longlat +datum=WGS84 +no_defs" );
 
+#if PROJ_VERSION_MAJOR>=6
+const QString GEOWKT = R"""(GEOGCRS["WGS 84",DATUM["World Geodetic System 1984",ELLIPSOID["WGS 84",6378137,298.257223563,LENGTHUNIT["metre",1]]],PRIMEM["Greenwich",0,ANGLEUNIT["degree",0.0174532925199433]],CS[ellipsoidal,2],AXIS["geodetic latitude (Lat)",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433]],AXIS["geodetic longitude (Lon)",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433]],USAGE[SCOPE["unknown"],AREA["World"],BBOX[-90,-180,90,180]],ID["EPSG",4326]] )""";
+#else
 const QString GEOWKT =
   "GEOGCS[\"WGS 84\", "
   "  DATUM[\"WGS_1984\", "
@@ -62,6 +65,7 @@ const QString GEOWKT =
   "  AXIS[\"Lat\",NORTH], "
   "  AXIS[\"Long\",EAST], "
   "  AUTHORITY[\"EPSG\",\"4326\"]]";
+#endif
 
 const QString PROJECT_SCALES =
   "1:1000000,1:500000,1:250000,1:100000,1:50000,1:25000,"
