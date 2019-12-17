bc3f6f816de055328049db0ab91270ac0a681030
Nyall Dawson <nyall.dawson@gmail.com>
2019-12-17 14:51:50 +1000

    Expose control over whether custom projections are stored using proj or WKT strings
    (cherry picked from commit 8bfca7f2c8467d17d64cea8d8b961f4002461d7f)

--- src/core/qgscoordinatereferencesystem.cpp
+++ src/core/qgscoordinatereferencesystem.cpp
@@ -2114,7 +2114,7 @@ QString QgsCoordinateReferenceSystem::validationHint()
 /// Copied from QgsCustomProjectionDialog ///
 /// Please refactor into SQL handler !!!  ///
 
-long QgsCoordinateReferenceSystem::saveAsUserCrs( const QString &name )
+long QgsCoordinateReferenceSystem::saveAsUserCrs( const QString &name, bool storeWkt )
 {
   if ( !d->mIsValid )
   {
@@ -2150,9 +2150,9 @@ long QgsCoordinateReferenceSystem::saveAsUserCrs( const QString &name )
             + ',' + QgsSqliteUtils::quotedString( name )
             + ',' + ( !d->mProjectionAcronym.isEmpty() ? QgsSqliteUtils::quotedString( d->mProjectionAcronym ) : QStringLiteral( "''" ) )
             + ',' + quotedEllipsoidString
-            + ',' + QgsSqliteUtils::quotedString( toProj4() )
+            + ',' + ( !proj4String.isEmpty() ? QgsSqliteUtils::quotedString( proj4String ) : QStringLiteral( "''" ) )
             + ",0,"  // <-- is_geo shamelessly hard coded for now
-            + QgsSqliteUtils::quotedString( wktString )
+            + ( storeWkt ? QgsSqliteUtils::quotedString( wktString ) : QStringLiteral( "''" ) )
             + ')';
   }
   else
@@ -2161,9 +2161,9 @@ long QgsCoordinateReferenceSystem::saveAsUserCrs( const QString &name )
             + QgsSqliteUtils::quotedString( name )
             + ',' + ( !d->mProjectionAcronym.isEmpty() ? QgsSqliteUtils::quotedString( d->mProjectionAcronym ) : QStringLiteral( "''" ) )
             + ',' + quotedEllipsoidString
-            + ',' + QgsSqliteUtils::quotedString( toProj4() )
+            + ',' + ( !proj4String.isEmpty() ? QgsSqliteUtils::quotedString( proj4String ) : QStringLiteral( "''" ) )
             + ",0,"  // <-- is_geo shamelessly hard coded for now
-            + QgsSqliteUtils::quotedString( wktString )
+            + ( storeWkt ? QgsSqliteUtils::quotedString( wktString ) : QStringLiteral( "''" ) )
             + ')';
   }
   sqlite3_database_unique_ptr database;
