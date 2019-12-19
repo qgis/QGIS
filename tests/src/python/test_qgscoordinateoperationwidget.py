0e94157aa4d4af8e1c4b081ceb29413f9e7c68b8
Nyall Dawson <nyall.dawson@gmail.com>
2019-12-19 13:12:23 +1000

    Use QgsCoordinateOperationWidget in QgsDatumTransformDialog

--- tests/src/python/test_qgscoordinateoperationwidget.py
+++ tests/src/python/test_qgscoordinateoperationwidget.py
@@ -38,6 +38,8 @@ class TestQgsCoordinateOperationWidget(unittest.TestCase):
         w = QgsCoordinateOperationWidget()
         self.assertFalse(w.sourceCrs().isValid())
         self.assertFalse(w.destinationCrs().isValid())
+        self.assertFalse(w.hasSelection())
+        self.assertFalse(w.availableOperations())
 
         w.setSourceCrs(QgsCoordinateReferenceSystem('EPSG:28355'))
         self.assertEqual(w.sourceCrs().authid(), 'EPSG:28355')
@@ -50,15 +52,19 @@ class TestQgsCoordinateOperationWidget(unittest.TestCase):
     @unittest.skipIf(QgsProjUtils.projVersionMajor() < 6, 'Not a proj6 build')
     def testOperations(self):
         w = QgsCoordinateOperationWidget()
+        self.assertFalse(w.hasSelection())
         spy = QSignalSpy(w.operationChanged)
         w.setSourceCrs(QgsCoordinateReferenceSystem('EPSG:28355'))
         self.assertEqual(len(spy), 0)
         w.setDestinationCrs(QgsCoordinateReferenceSystem('EPSG:7855'))
         self.assertEqual(len(spy), 1)
+        self.assertTrue(w.hasSelection())
+        self.assertEqual(len(w.availableOperations()), 3)
 
         self.assertEqual(w.defaultOperation().proj, '+proj=pipeline +step +inv +proj=utm +zone=55 +south +ellps=GRS80 +step +proj=push +v_3 +step +proj=cart +ellps=GRS80 +step +proj=helmert +x=0.06155 +y=-0.01087 +z=-0.04019 +rx=-0.0394924 +ry=-0.0327221 +rz=-0.0328979 +s=-0.009994 +convention=coordinate_frame +step +inv +proj=cart +ellps=GRS80 +step +proj=pop +v_3 +step +proj=utm +zone=55 +south +ellps=GRS80')
         self.assertEqual(w.selectedOperation().proj,
                          '+proj=pipeline +step +inv +proj=utm +zone=55 +south +ellps=GRS80 +step +proj=push +v_3 +step +proj=cart +ellps=GRS80 +step +proj=helmert +x=0.06155 +y=-0.01087 +z=-0.04019 +rx=-0.0394924 +ry=-0.0327221 +rz=-0.0328979 +s=-0.009994 +convention=coordinate_frame +step +inv +proj=cart +ellps=GRS80 +step +proj=pop +v_3 +step +proj=utm +zone=55 +south +ellps=GRS80')
+        self.assertTrue(w.selectedOperation().isAvailable)
 
         op = QgsCoordinateOperationWidget.OperationDetails()
         op.proj = '+proj=pipeline +step +inv +proj=utm +zone=55 +south +ellps=GRS80 +step +proj=hgridshift +grids=GDA94_GDA2020_conformal_and_distortion.gsb +step +proj=utm +zone=55 +south +ellps=GRS80'
