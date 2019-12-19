dbc175f131145dd09f3cca21ea6007a7c7d60f58
Nyall Dawson <nyall.dawson@gmail.com>
2019-12-20 09:27:36 +1000

    Add test for proj < 6 logic

--- tests/src/python/test_qgscoordinateoperationwidget.py
+++ tests/src/python/test_qgscoordinateoperationwidget.py
@@ -15,7 +15,7 @@ import os
 
 from qgis.core import (
     QgsProjUtils,
-    QgsRectangle,
+    QgsDatumTransform,
     QgsCoordinateReferenceSystem,
     QgsVectorLayer,
     QgsProject,
@@ -82,6 +82,57 @@ class TestQgsCoordinateOperationWidget(unittest.TestCase):
         self.assertEqual(w.selectedOperation().proj,
                          '+proj=pipeline +step +inv +proj=utm +zone=55 +south +ellps=GRS80 +step +proj=push +v_3 +step +proj=cart +ellps=GRS80 +step +proj=helmert +x=0.06155 +y=-0.01087 +z=-0.04019 +rx=-0.0394924 +ry=-0.0327221 +rz=-0.0328979 +s=-0.009994 +convention=coordinate_frame +step +inv +proj=cart +ellps=GRS80 +step +proj=pop +v_3 +step +proj=utm +zone=55 +south +ellps=GRS80')
         self.assertEqual(len(spy), 3)
+
+    @unittest.skipIf(QgsProjUtils.projVersionMajor() >= 6, 'Not a crufty proj build')
+    def testOperationsCruftyProj(self):
+        w = QgsCoordinateOperationWidget()
+        self.assertFalse(w.hasSelection())
+        spy = QSignalSpy(w.operationChanged)
+        w.setSourceCrs(QgsCoordinateReferenceSystem('EPSG:4283'))
+        self.assertEqual(len(spy), 0)
+        w.setDestinationCrs(QgsCoordinateReferenceSystem('EPSG:7844'))
+        self.assertEqual(len(spy), 1)
+        self.assertTrue(w.hasSelection())
+        self.assertEqual(len(w.availableOperations()), 2)
+
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.defaultOperation().sourceTransformId), '+nadgrids=GDA94_GDA2020_conformal_and_distortion.gsb')
+        self.assertEqual(w.defaultOperation().destinationTransformId, -1)
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.selectedOperation().sourceTransformId), '+nadgrids=GDA94_GDA2020_conformal_and_distortion.gsb')
+        self.assertEqual(w.selectedOperation().destinationTransformId, -1)
+
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.availableOperations()[1].sourceTransformId),
+                         '+nadgrids=GDA94_GDA2020_conformal.gsb')
+        self.assertEqual(w.availableOperations()[1].destinationTransformId, -1)
+
+        op = QgsCoordinateOperationWidget.OperationDetails()
+        op.sourceTransformId = w.availableOperations()[1].sourceTransformId
+        w.setSelectedOperation(op)
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.selectedOperation().sourceTransformId), '+nadgrids=GDA94_GDA2020_conformal.gsb')
+        self.assertEqual(len(spy), 2)
+        w.setSelectedOperation(op)
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.selectedOperation().sourceTransformId), '+nadgrids=GDA94_GDA2020_conformal.gsb')
+        self.assertEqual(len(spy), 2)
+
+        op.sourceTransformId = w.availableOperations()[0].sourceTransformId
+        op.destinationTransformId = -1
+        w.setSelectedOperation(op)
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.selectedOperation().sourceTransformId),
+                         '+nadgrids=GDA94_GDA2020_conformal_and_distortion.gsb')
+        self.assertEqual(len(spy), 3)
+
+        op.destinationTransformId = w.availableOperations()[1].sourceTransformId
+        op.sourceTransformId = -1
+        w.setSelectedOperation(op)
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.selectedOperation().sourceTransformId),
+                         '+nadgrids=GDA94_GDA2020_conformal.gsb')
+        self.assertEqual(len(spy), 4)
+
+        op.destinationTransformId = w.availableOperations()[0].sourceTransformId
+        op.sourceTransformId = -1
+        w.setSelectedOperation(op)
+        self.assertEqual(QgsDatumTransform.datumTransformToProj(w.selectedOperation().sourceTransformId),
+                         '+nadgrids=GDA94_GDA2020_conformal_and_distortion.gsb')
+        self.assertEqual(len(spy), 5)
 
 
 if __name__ == '__main__':
