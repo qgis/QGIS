b54881e41d75efcc1f152e224aa5e978d8500c26
Nyall Dawson <nyall.dawson@gmail.com>
2019-12-27 16:06:46 +1000

    Fix crash when no candidates remain

--- src/core/pal/pal.cpp
+++ src/core/pal/pal.cpp
@@ -349,6 +349,9 @@ std::unique_ptr<Problem> Pal::extract( const QgsRectangle &extent, const QgsGeom
         }
       }
 
+      if ( feat->candidates.empty() )
+        continue;
+
       // calculate final costs
       CostCalculator::finalizeCandidatesCosts( feat.get(), &obstacles, bbx, bby );
 
