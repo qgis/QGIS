8e497ff852b05f4c240b7c77bb371706ea61b7f1
Samweli <smwltwesa6@gmail.com>
2020-01-07 20:33:20 +0300

    added documentation on new wms layer class

--- src/providers/wms/qgswmsdataitems.cpp
+++ src/providers/wms/qgswmsdataitems.cpp
@@ -251,8 +251,12 @@ QgsWMSLayerCollectionItem::QgsWMSLayerCollectionItem( QgsDataItem *parent, QStri
     // Attention, the name may be empty
     QgsDebugMsgLevel( QString::number( layerProperty.orderId ) + ' ' + layerProperty.name + ' ' + layerProperty.title, 2 );
     QString pathName = layerProperty.name.isEmpty() ? QString::number( layerProperty.orderId ) : layerProperty.name;
-    QgsWMSLayerItem *layer = new QgsWMSLayerItem( this, layerProperty.title, mPath + '/' + pathName, mCapabilitiesProperty, dataSourceUri, layerProperty );
-    //mChildren.append( layer );
+
+    if ( layerProperty.name.isEmpty() )
+      layer = new QgsWMSLayerCollectionItem( this, layerProperty.title, mPath + '/' + pathName, capabilitiesProperty, dataSourceUri, layerProperty );
+    else
+      layer = new QgsWMSLayerItem( this, layerProperty.title, mPath + '/' + pathName, mCapabilitiesProperty, dataSourceUri, layerProperty );
+
     addChildItem( layer );
   }
 
