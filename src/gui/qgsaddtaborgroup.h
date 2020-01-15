84bd5797f784b7758ae66e42010abb27e464987b
Sandro Mani <manisandro@gmail.com>
2020-01-15 23:53:18 +0100

    Move QgsVectorLayerProperties and dependencies to GUI

---
+++ src/gui/qgsaddtaborgroup.h
@@ -0,0 +1,63 @@
+/***************************************************************************
+                          qgsaddtaborgroup.h
+        Add a tab or a group for the tab and group display of fields
+                             -------------------
+    begin                : 2012-07-30
+    copyright            : (C) 2012 by Denis Rouzaud
+    email                : denis dot rouzaud at gmail dot com
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSADDTABORGROUP
+#define QGSADDTABORGROUP
+
+// We don't want to expose this in the public API
+#define SIP_NO_FILE
+
+#include "ui_qgsaddtaborgroupbase.h"
+#include "qgsguiutils.h"
+#include "qgis_gui.h"
+#include "qgshelp.h"
+
+class QTreeWidgetItem;
+class QgsVectorLayer;
+
+class GUI_EXPORT QgsAddTabOrGroup : public QDialog, private Ui::QgsAddTabOrGroupBase
+{
+    Q_OBJECT
+
+  public:
+    typedef QPair<QString, QTreeWidgetItem *> TabPair;
+
+  public:
+    QgsAddTabOrGroup( QgsVectorLayer *lyr, const QList<TabPair> &tabList, QWidget *parent = nullptr );
+
+    QString name();
+
+    QTreeWidgetItem *tab();
+
+    int columnCount() const;
+
+    bool tabButtonIsChecked();
+
+    void accept() override;
+
+  private slots:
+    void mGroupButton_toggled( bool checked );
+    void mTabButton_toggled( bool checked );
+    void showHelp();
+
+  protected:
+    QgsVectorLayer *mLayer = nullptr;
+    QList< TabPair > mTabs;
+};
+
+#endif
