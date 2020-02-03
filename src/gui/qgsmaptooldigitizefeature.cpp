6fe51c42070bdf2c900435a0c7ab637e3bb8a5e4
lbartoletti <loic.bartoletti@oslandia.com>
2020-02-03 12:06:19 +0100

    Close vertex for line when editing. fixes #32359 [needs-docs]

--- src/gui/qgsmaptooldigitizefeature.cpp
+++ src/gui/qgsmaptooldigitizefeature.cpp
@@ -92,6 +92,40 @@ void QgsMapToolDigitizeFeature::setCheckGeometryType( bool checkGeometryType )
   mCheckGeometryType = checkGeometryType;
 }
 
+void QgsMapToolDigitizeFeature::keyPressEvent( QKeyEvent *e )
+{
+  QgsVectorLayer *vlayer = qobject_cast<QgsVectorLayer *>( mLayer );
+  if ( !vlayer )
+    //if no given layer take the current from canvas
+    vlayer = currentVectorLayer();
+
+  if ( !vlayer )
+  {
+    notifyNotVectorLayer();
+    return;
+  }
+
+  QgsVectorDataProvider *provider = vlayer->dataProvider();
+
+  if ( !( provider->capabilities() & QgsVectorDataProvider::AddFeatures ) )
+  {
+    emit messageEmitted( tr( "The data provider for this layer does not support the addition of features." ), Qgis::Warning );
+    return;
+  }
+
+  if ( !vlayer->isEditable() )
+  {
+    notifyNotEditableLayer();
+    return;
+  }
+
+  if ( e && e->key() == Qt::Key_C && mode() == CaptureLine && vlayer->geometryType() == QgsWkbTypes::LineGeometry && mCheckGeometryType )
+  {
+    closePolygon();
+    QgsMapMouseEvent e2( mCanvas, QEvent::MouseButtonRelease, QPoint( ), Qt::RightButton );
+    cadCanvasReleaseEvent( &e2 );
+  }
+}
 void QgsMapToolDigitizeFeature::cadCanvasReleaseEvent( QgsMapMouseEvent *e )
 {
   QgsVectorLayer *vlayer = qobject_cast<QgsVectorLayer *>( mLayer );
