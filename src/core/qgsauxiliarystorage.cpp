1cbcbd7622d8e4ec5340a5f3f309cdce3328e669
Blottiere Paul <blottiere.paul@gmail.com>
2020-02-03 15:32:26 +0100

    Clean

--- src/core/qgsauxiliarystorage.cpp
+++ src/core/qgsauxiliarystorage.cpp
@@ -245,14 +245,6 @@ int QgsAuxiliaryLayer::createProperty( QgsPalLayerSettings::Property property, Q
         c.setProperty( property, prop );
         settings->setDataDefinedProperties( c );
 
-        if ( property == QgsPalLayerSettings::BufferColor
-             || property == QgsPalLayerSettings::BufferSize )
-        {
-          QgsTextFormat format = settings->format();
-          format.buffer().setEnabled( true );
-          settings->setFormat( format );
-        }
-
         layer->labeling()->setSettings( settings, providerId );
       }
     }
