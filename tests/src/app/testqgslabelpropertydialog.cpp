4ba55c37c9f309f207fe96ee0c3014df7a27fc83
Blottiere Paul <blottiere.paul@gmail.com>
2020-02-07 19:41:55 +0100

    Add unit test

--- tests/src/app/testqgslabelpropertydialog.cpp
+++ tests/src/app/testqgslabelpropertydialog.cpp
@@ -16,8 +16,14 @@
 #include "qgstest.h"
 #include <QObject>
 
+#include "qgisapp.h"
+#include "qgsproject.h"
 #include "qgsapplication.h"
+#include "qgsvectorlayer.h"
+#include "qgslabelingengine.h"
+#include "qgsauxiliarystorage.h"
 #include "qgslabelpropertydialog.h"
+#include "qgsvectorlayerlabeling.h"
 
 class TestQgsLabelPropertyDialog : public QObject
 {
@@ -27,6 +33,8 @@ class TestQgsLabelPropertyDialog : public QObject
     TestQgsLabelPropertyDialog() = default;
 
   private:
+    QString mTestDataDir;
+    QgisApp *mQgisApp = nullptr;
 
   private slots:
 
@@ -34,7 +42,10 @@ class TestQgsLabelPropertyDialog : public QObject
     {
       QgsApplication::init();
       QgsApplication::initQgis();
+      mQgisApp = new QgisApp();
 
+      QString myDataDir( TEST_DATA_DIR );
+      mTestDataDir = myDataDir + '/';
     }
 
     void cleanupTestCase()
@@ -44,7 +55,47 @@ class TestQgsLabelPropertyDialog : public QObject
 
     void test()
     {
-      // TODO
+      // init vector layer
+      QString pointFileName = mTestDataDir + "points.shp";
+      QFileInfo pointFileInfo( pointFileName );
+      QgsVectorLayer *vl = new QgsVectorLayer( pointFileInfo.filePath(),
+          pointFileInfo.completeBaseName(), QStringLiteral( "ogr" ) );
+      QgsProject::instance()->addMapLayer( vl );
+
+      // activate labeling
+      QgsPalLayerSettings settings;
+      vl->setLabeling( new QgsVectorLayerSimpleLabeling( settings ) );
+
+      // create auxiliary layer
+      QgsField field = vl->fields().field( 1 );
+      QgsAuxiliaryLayer *al = QgsProject::instance()->auxiliaryStorage()->createAuxiliaryLayer( field, vl );
+      vl->setAuxiliaryLayer( al );
+
+      // create auxiliary field for BufferDraw
+      QgsAuxiliaryLayer::createProperty( QgsPalLayerSettings::BufferDraw, vl );
+      QgsPropertyDefinition def = QgsPalLayerSettings::propertyDefinitions()[QgsPalLayerSettings::BufferDraw];
+      QString propName = QgsAuxiliaryLayer::nameFromProperty( def, true );
+      QCOMPARE( al->featureCount(), 0 );
+
+      QgsFeatureId fid = 0;
+      QVariant val = vl->getFeature( fid ).attribute( propName );
+
+      // init label property dialog and togle buffer draw
+      QgsLabelPropertyDialog dialog( vl->id(), QString(), fid, QFont(), QString(), false, settings );
+      dialog.bufferDrawToggled( true );
+
+      // apply changes
+      QgsAttributeMap changes = dialog.changedProperties();
+      QgsAttributeMap::const_iterator changeIt = changes.constBegin();
+      for ( ; changeIt != changes.constEnd(); ++changeIt )
+      {
+        vl->changeAttributeValue( fid, changeIt.key(), changeIt.value() );
+      }
+
+      // check auxiliary values
+      QCOMPARE( al->featureCount(), 1 );
+      val = vl->getFeature( fid ).attribute( propName );
+      QCOMPARE( val.toInt(), 1 );
     }
 };
 
