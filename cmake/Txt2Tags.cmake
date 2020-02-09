c23d37cd965aca8b99ee1b82b8e34241fa39950e
lbartoletti <loic.bartoletti@oslandia.com>
2020-02-09 09:14:30 +0100

    Fix build with txt2tags >= 3.5

--- cmake/Txt2Tags.cmake
+++ cmake/Txt2Tags.cmake
@@ -42,7 +42,7 @@ MACRO(ADD_TXT2TAGS_FILES _sources)
     ADD_CUSTOM_COMMAND(
       OUTPUT ${_out}
       COMMAND ${TXT2TAGS_EXECUTABLE}
-      ARGS --encoding=utf-8 -o${_out} -t txt ${_in}
+      ARGS -o${_out} -t txt ${_in}.tmp
       DEPENDS ${_in}
       COMMENT "Building ${_out} from ${_in}"
       )
@@ -50,7 +50,7 @@ MACRO(ADD_TXT2TAGS_FILES _sources)
     ADD_CUSTOM_COMMAND(
       OUTPUT ${_out}.html
       COMMAND ${TXT2TAGS_EXECUTABLE}
-      ARGS --encoding=utf-8 -o${_out}.html -t html ${_in}
+      ARGS -o${_out}.html -t html ${_in}.tmp
       DEPENDS ${_in}
       COMMENT "Building ${_out}.html from ${_in}"
       )
--- scripts/update-news.pl
+++ scripts/update-news.pl
@@ -61,8 +61,8 @@ close $in;
 
 copy($tempfile, "doc/news.t2t");
 
-system "txt2tags --encoding=utf-8 -odoc/news.html -t html doc/news.t2t";
-system "txt2tags --encoding=utf-8 -oNEWS -t txt doc/news.t2t";
+system "txt2tags -odoc/NEWS.html -t html doc/NEWS.t2t";
+system "txt2tags -oNEWS -t txt doc/NEWS.t2t";
 
 =head1 NAME
 
