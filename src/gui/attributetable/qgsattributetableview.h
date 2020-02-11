181bfd4fb886c5ac975f5c1a37242c491cf3709e
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-02-11 20:49:19 +0100

    fix ownership issues in dualview/attributetable (#34403)
    * fix ownership issues in dualview/attributetable
    
    correctly fix #33120
    
    * update dox + bindings

--- src/gui/attributetable/qgsattributetableview.h
+++ src/gui/attributetable/qgsattributetableview.h
@@ -58,9 +58,9 @@ class GUI_EXPORT QgsAttributeTableView : public QTableView
 
     /**
      * \brief setFeatureSelectionManager
-     * \param featureSelectionManager We will take ownership
+     * \param featureSelectionManager
      */
-    void setFeatureSelectionManager( QgsIFeatureSelectionManager *featureSelectionManager SIP_TRANSFER );
+    void setFeatureSelectionManager( QgsIFeatureSelectionManager *featureSelectionManager );
 
     /**
      * This event filter is installed on the verticalHeader to intercept mouse press and release
@@ -181,6 +181,7 @@ class GUI_EXPORT QgsAttributeTableView : public QTableView
     void selectRow( int row, bool anchor );
     QgsAttributeTableFilterModel *mFilterModel = nullptr;
     QgsFeatureSelectionModel *mFeatureSelectionModel = nullptr;
+    QgsIFeatureSelectionManager *mOwnedFeatureSelectionManager = nullptr;
     QgsIFeatureSelectionManager *mFeatureSelectionManager = nullptr;
     QgsAttributeTableDelegate *mTableDelegate = nullptr;
     QMenu *mActionPopup = nullptr;
