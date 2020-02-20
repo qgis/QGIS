8115be4a914d87914956ac7f8ef5759b6f92a777
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2020-02-20 10:37:32 +0100

    astyle

--- tests/src/core/testqgsdxfexport.cpp
+++ tests/src/core/testqgsdxfexport.cpp
@@ -401,44 +401,44 @@ void TestQgsDxfExport::testMTextEscapeSpaces()
 
 void TestQgsDxfExport::testMTextEscapeLineBreaks()
 {
-    int field = mPointLayerNoSymbols->addExpressionField( QStringLiteral("'A text with ' || char(13) || char(10) || 'line break'"), QgsField( QStringLiteral( "linebreaktest" ), QVariant::String ) );
+  int field = mPointLayerNoSymbols->addExpressionField( QStringLiteral( "'A text with ' || char(13) || char(10) || 'line break'" ), QgsField( QStringLiteral( "linebreaktest" ), QVariant::String ) );
 
-    QgsPalLayerSettings settings;
-    settings.fieldName = QStringLiteral( "linebreaktest" );
-    QgsTextFormat format;
-    format.setFont( QgsFontUtils::getStandardTestFont( QStringLiteral( "Bold" ) ).family() );
-    format.setSize( 12 );
-    format.setNamedStyle( QStringLiteral( "Bold" ) );
-    format.setColor( QColor( 200, 0, 200 ) );
-    settings.setFormat( format );
-    mPointLayerNoSymbols->setLabeling( new QgsVectorLayerSimpleLabeling( settings ) );
-    mPointLayerNoSymbols->setLabelsEnabled( true );
+  QgsPalLayerSettings settings;
+  settings.fieldName = QStringLiteral( "linebreaktest" );
+  QgsTextFormat format;
+  format.setFont( QgsFontUtils::getStandardTestFont( QStringLiteral( "Bold" ) ).family() );
+  format.setSize( 12 );
+  format.setNamedStyle( QStringLiteral( "Bold" ) );
+  format.setColor( QColor( 200, 0, 200 ) );
+  settings.setFormat( format );
+  mPointLayerNoSymbols->setLabeling( new QgsVectorLayerSimpleLabeling( settings ) );
+  mPointLayerNoSymbols->setLabelsEnabled( true );
 
-    QgsDxfExport d;
-    d.addLayers( QList< QgsDxfExport::DxfLayer >() << QgsDxfExport::DxfLayer( mPointLayerNoSymbols ) );
+  QgsDxfExport d;
+  d.addLayers( QList< QgsDxfExport::DxfLayer >() << QgsDxfExport::DxfLayer( mPointLayerNoSymbols ) );
 
-    QgsMapSettings mapSettings;
-    QSize size( 640, 480 );
-    mapSettings.setOutputSize( size );
-    mapSettings.setExtent( mPointLayerNoSymbols->extent() );
-    mapSettings.setLayers( QList<QgsMapLayer *>() << mPointLayerNoSymbols );
-    mapSettings.setOutputDpi( 96 );
-    mapSettings.setDestinationCrs( mPointLayerNoSymbols->crs() );
+  QgsMapSettings mapSettings;
+  QSize size( 640, 480 );
+  mapSettings.setOutputSize( size );
+  mapSettings.setExtent( mPointLayerNoSymbols->extent() );
+  mapSettings.setLayers( QList<QgsMapLayer *>() << mPointLayerNoSymbols );
+  mapSettings.setOutputDpi( 96 );
+  mapSettings.setDestinationCrs( mPointLayerNoSymbols->crs() );
 
-    d.setMapSettings( mapSettings );
-    d.setSymbologyScale( 1000 );
-    d.setSymbologyExport( QgsDxfExport::FeatureSymbology );
+  d.setMapSettings( mapSettings );
+  d.setSymbologyScale( 1000 );
+  d.setSymbologyExport( QgsDxfExport::FeatureSymbology );
 
-    QString file = getTempFileName( "mtext_escape_linebreaks" );
-    QFile dxfFile( file );
-    QCOMPARE( d.writeToFile( &dxfFile, QStringLiteral( "CP1252" ) ), QgsDxfExport::ExportResult::Success );
-    dxfFile.close();
+  QString file = getTempFileName( "mtext_escape_linebreaks" );
+  QFile dxfFile( file );
+  QCOMPARE( d.writeToFile( &dxfFile, QStringLiteral( "CP1252" ) ), QgsDxfExport::ExportResult::Success );
+  dxfFile.close();
 
-    dxfFile.open( QIODevice::ReadOnly );
-    QString fileContent = QTextStream( &dxfFile ).readAll();
-    dxfFile.close();
-    QVERIFY( fileContent.contains( "A\\~text\\~with\\~\\Pline\\~break" ) );
-    mPointLayerNoSymbols->removeExpressionField( field );
+  dxfFile.open( QIODevice::ReadOnly );
+  QString fileContent = QTextStream( &dxfFile ).readAll();
+  dxfFile.close();
+  QVERIFY( fileContent.contains( "A\\~text\\~with\\~\\Pline\\~break" ) );
+  mPointLayerNoSymbols->removeExpressionField( field );
 }
 
 void TestQgsDxfExport::testText()
