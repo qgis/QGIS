7ee193e184d8781a3040a305516d662247ee7cbe
José de Paula Rodrigues Neto Assis <espinafre@gmail.com>
2020-02-28 13:29:45 -0300

    Fixes debug message for Postgis/Proj version.
    Returns the correct version for the Proj library linked against the
    running PostGIS.

--- src/providers/postgres/qgspostgresconn.h
+++ src/providers/postgres/qgspostgresconn.h
@@ -392,6 +392,9 @@ class QgsPostgresConn : public QObject
     //! GEOS capability
     mutable bool mGeosAvailable;
 
+    //! PROJ capability
+    mutable bool mProjAvailable;
+
     //! Topology capability
     mutable bool mTopologyAvailable;
 
