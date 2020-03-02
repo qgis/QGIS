0735faa373b95c141f3f382a1f1f9f467d5a9f21
Nyall Dawson <nyall.dawson@gmail.com>
2020-03-03 09:36:44 +1000

    Move logic for calculation of linkage points for model items to C++

--- src/gui/processing/models/qgsmodelcomponentgraphicitem.cpp
+++ src/gui/processing/models/qgsmodelcomponentgraphicitem.cpp
@@ -145,6 +145,20 @@ QVariant QgsModelComponentGraphicItem::itemChange( QGraphicsItem::GraphicsItemCh
   return QGraphicsObject::itemChange( change, value );
 }
 
+QRectF QgsModelComponentGraphicItem::boundingRect() const
+{
+  QFontMetricsF fm( mFont );
+  const int linksAbove = mComponent->linksCollapsed( Qt::TopEdge ) ? 0 : linkPointCount( Qt::TopEdge );
+  const int linksBelow = mComponent->linksCollapsed( Qt::BottomEdge ) ? 0 : linkPointCount( Qt::BottomEdge );
+
+  const double hUp = fm.height() * 1.2 * ( linksAbove + 2 );
+  const double hDown = fm.height() * 1.2 * ( linksBelow + 2 );
+  return QRectF( -( mComponent->size().width() + 2 ) / 2,
+                 -( mComponent->size().height() + 2 ) / 2 - hUp,
+                 mComponent->size().width() + 2,
+                 mComponent->size().height() + hDown + hUp );
+}
+
 QRectF QgsModelComponentGraphicItem::itemRect() const
 {
   return QRectF( -( mComponent->size().width() + 2 ) / 2.0,
@@ -212,6 +226,138 @@ QgsModelComponentGraphicItem::State QgsModelComponentGraphicItem::state() const
     return Normal;
 }
 
+int QgsModelComponentGraphicItem::linkPointCount( Qt::Edge ) const
+{
+  return 0;
+}
+
+QString QgsModelComponentGraphicItem::linkPointText( Qt::Edge, int ) const
+{
+  return QString();
+}
+
+QPointF QgsModelComponentGraphicItem::linkPoint( Qt::Edge edge, int index ) const
+{
+  switch ( edge )
+  {
+    case Qt::BottomEdge:
+    {
+      if ( linkPointCount( Qt::BottomEdge ) )
+      {
+        const int pointIndex = !mComponent->linksCollapsed( Qt::BottomEdge ) ? index : -1;
+        const QString text = truncatedTextForItem( linkPointText( Qt::BottomEdge, index ) );
+        QFontMetricsF fm( mFont );
+        const double w = fm.boundingRect( text ).width();
+        const double h = fm.height() * 1.2 * ( pointIndex + 1 ) + fm.height() / 2.0;
+        const double y = h + mComponent->size().height() / 2.0 + 5;
+        const double x = !mComponent->linksCollapsed( Qt::BottomEdge ) ? ( -mComponent->size().width() / 2 + 33 + w + 5 ) : 10;
+        return QPointF( x, y );
+      }
+      break;
+    }
+
+    case Qt::TopEdge:
+    {
+      if ( linkPointCount( Qt::TopEdge ) )
+      {
+        double offsetX = 25;
+        int paramIndex = index;
+        if ( mComponent->linksCollapsed( Qt::TopEdge ) )
+        {
+          paramIndex = -1;
+          offsetX = 17;
+        }
+        QFontMetricsF fm( mFont );
+        double h = -( fm.height() * 1.2 ) * ( paramIndex + 2 ) - fm.height() / 2.0 + 8;
+        h = h - mComponent->size().height() / 2.0;
+        return QPointF( -mComponent->size().width() / 2 + offsetX, h );
+      }
+      break;
+    }
+    case Qt::LeftEdge:
+    case Qt::RightEdge:
+      break;
+  }
+
+  return QPointF();
+}
+
+QPointF QgsModelComponentGraphicItem::calculateAutomaticLinkPoint( QgsModelComponentGraphicItem *other, Qt::Edge &edge ) const
+{
+  // find closest edge to other item
+  const QgsRectangle otherRect( other->itemRect().translated( other->pos() ) );
+
+  const QPointF leftPoint = pos() + QPointF( -mComponent->size().width() / 2.0, 0 );
+  const double distLeft = otherRect.distance( QgsPointXY( leftPoint ) );
+
+  const QPointF rightPoint = pos() + QPointF( mComponent->size().width() / 2.0, 0 );
+  const double distRight = otherRect.distance( QgsPointXY( rightPoint ) );
+
+  const QPointF topPoint = pos() + QPointF( 0, -mComponent->size().height() / 2.0 );
+  const double distTop = otherRect.distance( QgsPointXY( topPoint ) );
+
+  const QPointF bottomPoint = pos() + QPointF( 0, mComponent->size().height() / 2.0 );
+  const double distBottom = otherRect.distance( QgsPointXY( bottomPoint ) );
+
+  if ( distLeft <= distRight && distLeft <= distTop && distLeft <= distBottom )
+  {
+    edge = Qt::LeftEdge;
+    return leftPoint;
+  }
+  else if ( distRight <= distTop && distRight <= distBottom )
+  {
+    edge = Qt::RightEdge;
+    return rightPoint;
+  }
+  else if ( distBottom <= distTop )
+  {
+    edge = Qt::BottomEdge;
+    return bottomPoint;
+  }
+  else
+  {
+    edge = Qt::TopEdge;
+    return topPoint;
+  }
+}
+
+QPointF QgsModelComponentGraphicItem::calculateAutomaticLinkPoint( const QPointF &point, Qt::Edge &edge ) const
+{
+  // find closest edge to other point
+  const QgsPointXY otherPt( point );
+  const QPointF leftPoint = pos() + QPointF( -mComponent->size().width() / 2.0, 0 );
+  const double distLeft = otherPt.distance( QgsPointXY( leftPoint ) );
+
+  const QPointF rightPoint = pos() + QPointF( mComponent->size().width() / 2.0, 0 );
+  const double distRight = otherPt.distance( QgsPointXY( rightPoint ) );
+
+  const QPointF topPoint = pos() + QPointF( 0, -mComponent->size().height() / 2.0 );
+  const double distTop = otherPt.distance( QgsPointXY( topPoint ) );
+
+  const QPointF bottomPoint = pos() + QPointF( 0, mComponent->size().height() / 2.0 );
+  const double distBottom = otherPt.distance( QgsPointXY( bottomPoint ) );
+
+  if ( distLeft <= distRight && distLeft <= distTop && distLeft <= distBottom )
+  {
+    edge = Qt::LeftEdge;
+    return leftPoint;
+  }
+  else if ( distRight <= distTop && distRight <= distBottom )
+  {
+    edge = Qt::RightEdge;
+    return rightPoint;
+  }
+  else if ( distBottom <= distTop )
+  {
+    edge = Qt::BottomEdge;
+    return bottomPoint;
+  }
+  else
+  {
+    edge = Qt::TopEdge;
+    return topPoint;
+  }
+}
 
 QgsModelParameterGraphicItem::QgsModelParameterGraphicItem( QgsProcessingModelParameter *parameter, QgsProcessingModelAlgorithm *model, QGraphicsItem *parent )
   : QgsModelComponentGraphicItem( parameter, model, parent )
