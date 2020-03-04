00f2eab0bb9f637008b68dc39e61c6b176f586b9
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-03-04 08:23:44 +0100

    make QgsVectorLayer::wkbType and geometryType invokable (#34855)

--- src/core/qgsvectorlayer.h
+++ src/core/qgsvectorlayer.h
@@ -869,10 +869,10 @@ class CORE_EXPORT QgsVectorLayer : public QgsMapLayer, public QgsExpressionConte
     void setRenderer( QgsFeatureRenderer *r SIP_TRANSFER );
 
     //! Returns point, line or polygon
-    QgsWkbTypes::GeometryType geometryType() const;
+    Q_INVOKABLE QgsWkbTypes::GeometryType geometryType() const;
 
     //! Returns the WKBType or WKBUnknown in case of error
-    QgsWkbTypes::Type wkbType() const FINAL;
+    Q_INVOKABLE QgsWkbTypes::Type wkbType() const FINAL;
 
     QgsCoordinateReferenceSystem sourceCrs() const FINAL;
     QString sourceName() const FINAL;
