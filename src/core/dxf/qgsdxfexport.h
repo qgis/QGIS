84ff115b19c0c4a49d4f643bedde6121d17de30c
SergeyChern <42201235+SergeyChern@users.noreply.github.com>
2020-03-05 16:09:20 +0300

    Update qgsdxfexport.h

--- src/core/dxf/qgsdxfexport.h
+++ src/core/dxf/qgsdxfexport.h
@@ -360,7 +360,7 @@ class CORE_EXPORT QgsDxfExport
     void writeGroup( const QColor &color, int exactMatch = 62, int rgbCode = 420, int transparencyCode = 440 );
 
     /**
-     * Write a group code with color value
+     * Write a group code with color and transparency value
      * \param color color
      * \param withTransparency must be true for polygon fill color only
      * \param exactMatch group code to use if the color has an exact match in the dxf palette
