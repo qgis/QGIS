f024d7c779dcc5f8db88819f3af085acdd165e9f
Samweli <smwltwesa6@gmail.com>
2020-03-06 08:57:55 +0100

    added temporal layer indicators

---
+++ src/app/qgslayertreeviewtemporalindicator.cpp
@@ -0,0 +1,89 @@
+/***************************************************************************
+                         qgslayertreeviewtemporalindicator.cpp
+                         ---------------
+    begin                : February 2020
+    copyright            : (C) 2020 by Samweli Mwakisambwe
+    email                : samweli at kartoza dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgslayertreeviewtemporalindicator.h"
+#include "qgslayertreeview.h"
+#include "qgslayertree.h"
+#include "qgslayertreemodel.h"
+#include "qgslayertreeutils.h"
+#include "qgsrasterlayer.h"
+#include "qgisapp.h"
+
+QgsLayerTreeViewTemporalIndicatorProvider::QgsLayerTreeViewTemporalIndicatorProvider( QgsLayerTreeView *view )
+  : QgsLayerTreeViewIndicatorProvider( view )
+{
+}
+
+void QgsLayerTreeViewTemporalIndicatorProvider::connectSignals( QgsMapLayer *layer )
+{
+  if ( !( qobject_cast<QgsVectorLayer *>( layer ) || qobject_cast<QgsRasterLayer *>( layer ) ) )
+    return;
+  QgsMapLayer *mapLayer = layer;
+  connect( mapLayer, &QgsMapLayer::dataSourceChanged, this, &QgsLayerTreeViewTemporalIndicatorProvider::onLayerChanged );
+
+  if ( mapLayer && mapLayer->dataProvider() )
+  {
+    mLayer = mapLayer;
+    QgsRasterDataProvider *provider = qobject_cast<QgsRasterDataProvider *>( mapLayer->dataProvider() );
+    connect( provider, &QgsRasterDataProvider::statusChanged,
+             this, &QgsLayerTreeViewTemporalIndicatorProvider::onLayerChanged );
+  }
+}
+
+void QgsLayerTreeViewTemporalIndicatorProvider::onIndicatorClicked( const QModelIndex &index )
+{
+  Q_UNUSED( index )
+}
+
+bool QgsLayerTreeViewTemporalIndicatorProvider::acceptLayer( QgsMapLayer *layer )
+{
+  if ( !layer )
+    return false;
+  if ( layer->temporalProperties() &&
+       layer->temporalProperties()->temporalSource() ==
+       QgsMapLayerTemporalProperties::TemporalSource::Layer )
+    return true;
+  return false;
+}
+
+QString QgsLayerTreeViewTemporalIndicatorProvider::iconName( QgsMapLayer *layer )
+{
+  Q_UNUSED( layer );
+  return QStringLiteral( "/mIndicatorTemporal.svg" );
+}
+
+QString QgsLayerTreeViewTemporalIndicatorProvider::tooltipText( QgsMapLayer *layer )
+{
+  Q_UNUSED( layer );
+  return tr( "<b>Temporal layer</b>" );
+}
+
+void QgsLayerTreeViewTemporalIndicatorProvider::onLayerChanged()
+{
+  QgsMapLayer *mapLayer = qobject_cast<QgsMapLayer *>( sender() );
+  QgsMapLayer *mapLayerFromIndicator = qobject_cast<QgsMapLayer *>( mLayer );
+
+  if ( !mapLayer )
+  {
+    if ( !mapLayerFromIndicator )
+      return;
+    else
+      updateLayerIndicator( mapLayerFromIndicator );
+  }
+  else
+    updateLayerIndicator( mapLayer );
+}
