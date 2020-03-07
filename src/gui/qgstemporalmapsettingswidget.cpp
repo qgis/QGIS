3e06d0a696e1c6847d88ef2fafb7f47d89133663
Samweli <smwltwesa6@gmail.com>
2020-03-07 17:36:25 +0100

    new temporal map settings dialog

---
+++ src/gui/qgstemporalmapsettingswidget.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+                         qgstemporalmapsettingswidget.cpp
+                         ---------------
+    begin                : March 2020
+    copyright            : (C) 2020 by Samweli Mwakisambwe
+    email                : samweli at kartoza dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgstemporalmapsettingswidget.h"
+#include "qgsgui.h"
+
+QgsTemporalMapSettingsWidget::QgsTemporalMapSettingsWidget( QWidget *parent )
+  : QWidget( parent )
+{
+  setupUi( this );
+}
+
+QgsTemporalMapSettingsWidget::~QgsTemporalMapSettingsWidget()
+{
+}
+
+double QgsTemporalMapSettingsWidget::frameRateValue()
+{
+  return mFrameSpinBox->value();
+}
+
+void QgsTemporalMapSettingsWidget::setFrameRateValue( double value )
+{
+  mFrameSpinBox->setValue( value );
+}
