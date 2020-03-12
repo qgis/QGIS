4bad25444192eac71c9c10535fb092e5cee495f2
Nyall Dawson <nyall.dawson@gmail.com>
2020-03-12 14:23:16 +1000

    [FEATURE][processing] Add option to enable automatic snapping to grid while designer models
    If enabled, model component moving or resizing will be automatically
    snapped to a grid

--- src/gui/processing/models/qgsmodeldesignerdialog.cpp
+++ src/gui/processing/models/qgsmodeldesignerdialog.cpp
@@ -118,6 +118,7 @@ QgsModelDesignerDialog::QgsModelDesignerDialog( QWidget *parent, Qt::WindowFlags
   mainLayout->insertWidget( 0, mMessageBar );
 
   mView->setAcceptDrops( true );
+  QgsSettings settings;
 
   connect( mActionClose, &QAction::triggered, this, &QWidget::close );
   connect( mActionZoomIn, &QAction::triggered, this, &QgsModelDesignerDialog::zoomIn );
@@ -131,6 +132,14 @@ QgsModelDesignerDialog::QgsModelDesignerDialog( QWidget *parent, Qt::WindowFlags
   connect( mActionSave, &QAction::triggered, this, [ = ] { saveModel( false ); } );
   connect( mActionSaveAs, &QAction::triggered, this, [ = ] { saveModel( true ); } );
   connect( mActionDeleteComponents, &QAction::triggered, this, &QgsModelDesignerDialog::deleteSelected );
+
+  mActionSnappingEnabled->setChecked( settings.value( QStringLiteral( "/Processing/modelDesignerEnableSnap" ), true ).toBool() );
+  connect( mActionSnappingEnabled, &QAction::toggled, this, [ = ]( bool enabled )
+  {
+    mView->snapper()->setSnapToGrid( enabled );
+    QgsSettings().setValue( QStringLiteral( "/Processing/modelDesignerEnableSnap" ), enabled );
+  } );
+  mView->snapper()->setSnapToGrid( mActionSnappingEnabled->isChecked() );
 
   mUndoAction = mUndoStack->createUndoAction( this );
   mUndoAction->setIcon( QgsApplication::getThemeIcon( QStringLiteral( "/mActionUndo.svg" ) ) );
@@ -146,7 +155,7 @@ QgsModelDesignerDialog::QgsModelDesignerDialog( QWidget *parent, Qt::WindowFlags
   mToolbar->insertAction( mActionZoomIn, mRedoAction );
   mToolbar->insertSeparator( mActionZoomIn );
 
-  QgsSettings settings;
+
   QgsProcessingToolboxProxyModel::Filters filters = QgsProcessingToolboxProxyModel::FilterModeler;
   if ( settings.value( QStringLiteral( "Processing/Configuration/SHOW_ALGORITHMS_KNOWN_ISSUES" ), false ).toBool() )
   {
