4944c8aa1f6fbe13eab1bb346c763c21c6611018
Alexander Bruy <alexander.bruy@gmail.com>
2020-03-13 08:49:31 +0200

    add "Organize Columns" button to the attribute table toolbar to allow unhiding columns if all of them were hidden by accident (fix #23397)

--- src/app/qgsattributetabledialog.h
+++ src/app/qgsattributetabledialog.h
@@ -154,6 +154,11 @@ class APP_EXPORT QgsAttributeTableDialog : public QDialog, private Ui::QgsAttrib
     void mActionDeleteSelected_triggered();
 
     /**
+     * Opens organize columns dialog
+     */
+    void mActionOrganizeColumns_triggered();
+
+    /**
      * Called when the current index changes in the main view
      * i.e. when the view mode is switched from table to form view
      * or vice versa.
