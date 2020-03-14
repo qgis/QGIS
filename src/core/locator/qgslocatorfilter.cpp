5241d64dcbacd0fa99978b90dae5f95074b28043
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-03-14 22:27:57 +0200

    code formatting

--- src/core/locator/qgslocatorfilter.cpp
+++ src/core/locator/qgslocatorfilter.cpp
@@ -23,6 +23,11 @@
 #include "qgsmessagelog.h"
 
 
+#define FUZZY_SCORE_WORD_MATCH 5
+#define FUZZY_SCORE_NEW_MATCH 3
+#define FUZZY_SCORE_CONSECUTIVE_MATCH 4
+
+
 QgsLocatorFilter::QgsLocatorFilter( QObject *parent )
   : QObject( parent )
 {
@@ -42,6 +47,92 @@ void QgsLocatorFilter::triggerResultFromAction( const QgsLocatorResult &result, 
 bool QgsLocatorFilter::stringMatches( const QString &candidate, const QString &search )
 {
   return !search.isEmpty() && candidate.contains( search, Qt::CaseInsensitive );
+}
+
+double QgsLocatorFilter::fuzzyScore( const QString &candidate, const QString &search )
+{
+  QString candidateNormalized = candidate.simplified().normalized( QString:: NormalizationForm_C ).toLower();
+  QString searchNormalized = search.simplified().normalized( QString:: NormalizationForm_C ).toLower();
+
+  int candidateLength = candidateNormalized.length();
+  int searchLength = searchNormalized.length();
+  int score = 0;
+
+  // if the candidate and the search term are empty, no other option than 0 score
+  if ( candidateLength == 0 || searchLength == 0 )
+    return score;
+
+  int candidateIdx = 0;
+  int searchIdx = 0;
+  int maxScore = 0;
+
+  bool isPreviousIndexMatching = false;
+  bool isWordOpen = true;
+
+  // loop throught each candidate char and calculate the potential max score
+  while ( candidateIdx < candidateLength )
+  {
+    QChar candidateChar = candidateNormalized[ candidateIdx++ ];
+
+    // the first char is always the default score
+    if ( candidateIdx == 1 )
+      maxScore += FUZZY_SCORE_NEW_MATCH;
+    // every space character or end of string is a opportunity for a new word
+    else if ( candidateChar.isSpace() || candidateIdx == candidateLength )
+      maxScore += FUZZY_SCORE_WORD_MATCH;
+    // potentially we can match every other character
+    else
+      maxScore += FUZZY_SCORE_CONSECUTIVE_MATCH;
+
+    // we looped through all the characters
+    if ( searchIdx >= searchLength )
+      continue;
+
+    QChar searchChar = searchNormalized[ searchIdx ];
+
+    // match!
+    if ( candidateChar == searchChar )
+    {
+      searchIdx++;
+
+      // if we have just successfully finished a word, give higher score
+      if ( candidateChar.isSpace() || searchIdx == searchLength )
+      {
+        if ( isWordOpen )
+          score += FUZZY_SCORE_WORD_MATCH;
+        else if ( isPreviousIndexMatching )
+          score += FUZZY_SCORE_CONSECUTIVE_MATCH;
+        else
+          score += FUZZY_SCORE_NEW_MATCH;
+
+        isWordOpen = true;
+      }
+      // if we have consecutive characters matching, give higher score
+      else if ( isPreviousIndexMatching )
+      {
+        score += FUZZY_SCORE_CONSECUTIVE_MATCH;
+      }
+      // normal score for new independent character that matches
+      else
+      {
+        score += FUZZY_SCORE_NEW_MATCH;
+      }
+
+      isPreviousIndexMatching = true;
+    }
+    // if the current character does NOT match, we are sure we cannot build a word for now
+    else
+    {
+      isPreviousIndexMatching = false;
+      isWordOpen = false;
+    }
+  }
+
+  // we didn't loop through all the search chars, it means, that they are not present in the current candidate
+  if ( searchIdx != searchLength )
+    score = 0;
+
+  return static_cast<float>( std::max( score, 0 ) ) / std::max( maxScore, 1 );
 }
 
 bool QgsLocatorFilter::enabled() const
