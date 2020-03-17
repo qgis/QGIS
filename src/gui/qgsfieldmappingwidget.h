f770ca0a6b7d5d11c7f632222a4a8df35db2b06c
Alessandro Pasotti <elpaso@itopen.it>
2020-03-17 17:47:30 +0100

    Code layout

--- src/gui/qgsfieldmappingwidget.h
+++ src/gui/qgsfieldmappingwidget.h
@@ -26,7 +26,7 @@
 
 /**
  * \ingroup gui
- * The QgsFieldMappingWidget class allows to define a map from one set of QgsFields to another,
+ * The QgsFieldMappingWidget class creates a mapping from one set of QgsFields to another,
  * for each set of "destination" fields an expression defines how to obtain the values of the
  * "destination" fields.
  * \since QGIS 3.14
