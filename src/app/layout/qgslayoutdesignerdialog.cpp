05ffd2db8d43c4aacb2510a46e55d36a002661d6
Alex <roya0045@users.noreply.github.com>
2020-03-22 08:25:48 -0400

    Add coverage check on atlas exports
    Fixes #26144

--- src/app/layout/qgslayoutdesignerdialog.cpp
+++ src/app/layout/qgslayoutdesignerdialog.cpp
@@ -2651,6 +2651,15 @@ void QgsLayoutDesignerDialog::exportAtlasToRaster()
   if ( !printAtlas || !printAtlas->enabled() )
     return;
 
+  if ( printAtlas->coverageLayer() )
+  {
+    QMessageBox::warning( this, tr( "Export Atlas as Image" ),
+                          tr( "Error: No coverage layer is set." ),
+                          QMessageBox::Ok,
+                          QMessageBox::Ok );
+    return;
+  }
+
   // else, it has an atlas to render, so a directory must first be selected
   if ( printAtlas->filenameExpression().isEmpty() )
   {
@@ -2823,6 +2832,15 @@ void QgsLayoutDesignerDialog::exportAtlasToSvg()
   if ( !printAtlas || !printAtlas->enabled() )
     return;
 
+  if ( printAtlas->coverageLayer() )
+  {
+    QMessageBox::warning( this, tr( "Export Atlas as Image" ),
+                          tr( "Error: No coverage layer is set." ),
+                          QMessageBox::Ok,
+                          QMessageBox::Ok );
+    return;
+  }
+
   if ( containsWmsLayers() )
   {
     showWmsPrintingWarning();
@@ -2990,6 +3008,15 @@ void QgsLayoutDesignerDialog::exportAtlasToPdf()
   QgsLayoutAtlas *printAtlas = atlas();
   if ( !printAtlas || !printAtlas->enabled() )
     return;
+
+  if ( printAtlas->coverageLayer() )
+  {
+    QMessageBox::warning( this, tr( "Export Atlas as Image" ),
+                          tr( "Error: No coverage layer is set." ),
+                          QMessageBox::Ok,
+                          QMessageBox::Ok );
+    return;
+  }
 
   if ( containsWmsLayers() )
   {
