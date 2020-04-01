c7f7c9829d573b31b27ff9244c22f6eea4a78293
obrix <bertrand.rix@oslandia.com>
2020-04-01 17:14:42 +0200

    Regenerate qgssnappingconfig.py

--- python/core/auto_additions/qgssnappingconfig.py
+++ python/core/auto_additions/qgssnappingconfig.py
@@ -3,3 +3,4 @@ QgsSnappingConfig.SnappingMode.baseClass = QgsSnappingConfig
 QgsSnappingConfig.SnappingTypes.baseClass = QgsSnappingConfig
 QgsSnappingConfig.SnappingTypeFlag.baseClass = QgsSnappingConfig
 SnappingTypeFlag = QgsSnappingConfig  # dirty hack since SIP seems to introduce the flags in module
+QgsSnappingConfig.ScaleDependencyMode.baseClass = QgsSnappingConfig
