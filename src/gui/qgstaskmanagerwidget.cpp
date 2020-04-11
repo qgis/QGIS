839a1b85e15602d7652ec8d42c0756de42f83d6d
Nyall Dawson <nyall.dawson@gmail.com>
2020-04-11 10:24:23 +1000

    Fix tasks don't show in status bar if they start before the widget is created

--- src/gui/qgstaskmanagerwidget.cpp
+++ src/gui/qgstaskmanagerwidget.cpp
@@ -621,6 +621,9 @@ QgsTaskManagerStatusBarWidget::QgsTaskManagerStatusBarWidget( QgsTaskManager *ma
   connect( manager, &QgsTaskManager::allTasksFinished, this, &QgsTaskManagerStatusBarWidget::allFinished );
   connect( manager, &QgsTaskManager::finalTaskProgressChanged, this, &QgsTaskManagerStatusBarWidget::overallProgressChanged );
   connect( manager, &QgsTaskManager::countActiveTasksChanged, this, &QgsTaskManagerStatusBarWidget::countActiveTasksChanged );
+
+  if ( manager->countActiveTasks() )
+    showButton();
 }
 
 QSize QgsTaskManagerStatusBarWidget::sizeHint() const
