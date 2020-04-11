d563218ff1e921d81b1274d7986d86fa0351f42b
Samweli <samweli@kartoza.com>
2020-04-11 16:31:54 +0200

    only show the static temporal range options when they are selected

--- src/gui/raster/qgsrasterlayerproperties.cpp
+++ src/gui/raster/qgsrasterlayerproperties.cpp
@@ -1293,9 +1293,9 @@ void QgsRasterLayerProperties::setSourceStaticTimeState()
     const QString enableTime = uri.param( QStringLiteral( "enableTime" ) );
 
     if ( temporalSource == QLatin1String( "provider" ) )
-      mStaticTemporalRange->setChecked( true );
+      mStaticTemporalRange->setChecked( !time.isEmpty() );
     else if ( temporalSource == QLatin1String( "project" ) )
-      mProjectTemporalRange->setChecked( true );
+      mProjectTemporalRange->setChecked( !time.isEmpty() );
 
     mDisableTime->setChecked( enableTime == QLatin1String( "no" ) );
   }
