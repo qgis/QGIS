28e7b0853659ac29a2d385279ff92085c4bd436a
root676 <clemens.raffler@gmail.com>
2020-04-12 15:23:44 +0200

    add algorithm icon

--- src/analysis/processing/qgsalgorithmconstantraster.h
+++ src/analysis/processing/qgsalgorithmconstantraster.h
@@ -35,6 +35,8 @@ class QgsConstantRasterAlgorithm : public QgsProcessingAlgorithm
 
     QgsConstantRasterAlgorithm() = default;
     void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) override;
+    QIcon icon() const override { return QgsApplication::getThemeIcon( QStringLiteral( "/algorithms/mAlgorithmConstantRaster.svg" ) ); }
+    QString svgIconPath() const override { return QgsApplication::iconPath( QStringLiteral( "/algorithms/mAlgorithmConstantRaster.svg" ) ); }
     QString name() const override;
     QString displayName() const override;
     QStringList tags() const override;
