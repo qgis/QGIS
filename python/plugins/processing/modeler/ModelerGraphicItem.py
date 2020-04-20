c3680829e5275f6aea8c208be646d5057aac7c8a
Juergen E. Fischer <jef@norbit.de>
2020-04-21 01:22:48 +0200

    translation string fixes

--- python/plugins/processing/modeler/ModelerGraphicItem.py
+++ python/plugins/processing/modeler/ModelerGraphicItem.py
@@ -21,6 +21,8 @@ __author__ = 'Victor Olaya'
 __date__ = 'August 2012'
 __copyright__ = '(C) 2012, Victor Olaya'
 
+from qgis.PyQt.QtCore import QCoreApplication
+
 from qgis.core import (QgsProcessingParameterDefinition,
                        QgsProject,
                        Qgis)
@@ -146,8 +148,12 @@ class ModelerChildAlgorithmGraphicItem(QgsModelChildAlgorithmGraphicItem):
 
             res, errors = self.model().validateChildAlgorithm(alg.childId())
             if not res:
-                self.scene().showWarning(self.tr('Algorithm “{}” is invalid').format(alg.description()), self.tr('Algorithm is Invalid'), self.tr(
-                    "<p>The “{}” algorithm is invalid, because:</p><ul><li>{}</li></ul>").format(alg.description(), '</li><li>'.join(errors)), level=Qgis.Warning)
+                self.scene().showWarning(
+                    QCoreApplication.translate('ModelerGraphicItem', 'Algorithm “{}” is invalid').format(alg.description()),
+                    self.tr('Algorithm is Invalid'),
+                    QCoreApplication.translate('ModelerGraphicItem', "<p>The “{}” algorithm is invalid, because:</p><ul><li>{}</li></ul>").format(alg.description(), '</li><li>'.join(errors)),
+                    level=Qgis.Warning
+                )
             else:
                 self.scene().messageBar().clearWidgets()
 
