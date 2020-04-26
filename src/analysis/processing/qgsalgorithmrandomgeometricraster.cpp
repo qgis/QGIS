6267bfad09edebf861f5d833f823ffd2c045d32c
root676 <clemens.raffler@gmx.at>
2020-04-26 23:36:16 +0200

    add Create geometrically distributed random raster layer algorithm

---
+++ src/analysis/processing/qgsalgorithmrandomgeometricraster.cpp
@@ -0,0 +1,247 @@
+/***************************************************************************
+                         qgsalgorithmrandomgeometricraster.cpp
+                         ---------------------
+    begin                : April 2020
+    copyright            : (C) 2020 by Clemens Raffler
+    email                : clemens dot raffler at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsalgorithmrandomgeometricraster.h"
+#include "qgsrasterfilewriter.h"
+#include "qgsstringutils.h"
+#include "random"
+
+///@cond PRIVATE
+
+//
+// QgsRandomGeometricRasterAlgorithm
+//
+QString QgsRandomGeometricRasterAlgorithm::name() const
+{
+  return QStringLiteral( "createrandomgeometricrasterlayer" );
+}
+
+QString QgsRandomGeometricRasterAlgorithm::group() const
+{
+  return QObject::tr( "Raster creation" );
+}
+
+QString QgsRandomGeometricRasterAlgorithm::groupId() const
+{
+  return QStringLiteral( "rastercreation" );
+}
+
+QString QgsRandomGeometricRasterAlgorithm::displayName() const
+{
+  return QObject::tr( "Create random raster layer (geometric distribution)" );
+}
+
+QStringList QgsRandomGeometricRasterAlgorithm::tags() const
+{
+  return QObject::tr( "raster,create,random,geometric" ).split( ',' );
+}
+
+QString QgsRandomGeometricRasterAlgorithm::shortHelpString() const
+{
+  return QObject::tr( "Generates a raster layer for given extent and cell size "
+                      "filled with geometrically distributed random values.\n"
+                      "By default, the values will be chosen given a probability of 0.5."
+                      "This can be overridden by using the advanced parameter for mean "
+                      "value. The raster data type is set to Integer types (Integer16 by default). "
+                      "The geometric distribution random values are defined as positive integer numbers. "
+                      "A floating point raster will represent a cast of "
+                      "integer values to floating point." );
+}
+
+QgsRandomGeometricRasterAlgorithm *QgsRandomGeometricRasterAlgorithm::createInstance() const
+{
+  return new QgsRandomGeometricRasterAlgorithm();
+}
+
+
+void QgsRandomGeometricRasterAlgorithm::initAlgorithm( const QVariantMap & )
+{
+  addParameter( new QgsProcessingParameterExtent( QStringLiteral( "EXTENT" ), QObject::tr( "Desired extent" ) ) );
+  addParameter( new QgsProcessingParameterCrs( QStringLiteral( "TARGET_CRS" ), QObject::tr( "Target CRS" ), QStringLiteral( "ProjectCrs" ) ) );
+  addParameter( new QgsProcessingParameterNumber( QStringLiteral( "PIXEL_SIZE" ), QObject::tr( "Pixel size" ),
+                QgsProcessingParameterNumber::Double, 1, false, 0.01 ) );
+
+  QStringList rasterDataTypes = QStringList();
+  rasterDataTypes << QStringLiteral( "Integer16" )
+                  << QStringLiteral( "Unsigned Integer16" )
+                  << QStringLiteral( "Integer32" )
+                  << QStringLiteral( "Unsigned Integer32" )
+                  << QStringLiteral( "Float32" )
+                  << QStringLiteral( "Float64" );
+
+  std::unique_ptr< QgsProcessingParameterDefinition > rasterTypeParameter = qgis::make_unique< QgsProcessingParameterEnum >( QStringLiteral( "OUTPUT_TYPE" ), QObject::tr( "Output raster data type" ),  rasterDataTypes, false, 0, false );
+  rasterTypeParameter->setFlags( QgsProcessingParameterDefinition::FlagAdvanced );
+  addParameter( rasterTypeParameter.release() );
+
+  std::unique_ptr< QgsProcessingParameterNumber > probabilityParameter = qgis::make_unique< QgsProcessingParameterNumber >( QStringLiteral( "PROBABILITY" ), QStringLiteral( "Probability" ), QgsProcessingParameterNumber::Double, 0.5, true, 0.00001 );
+  probabilityParameter->setFlags( QgsProcessingParameterDefinition::FlagAdvanced );
+  addParameter( probabilityParameter.release() );
+
+  addParameter( new QgsProcessingParameterRasterDestination( QStringLiteral( "OUTPUT" ), QObject::tr( "Output raster" ) ) );
+}
+
+bool QgsRandomGeometricRasterAlgorithm::prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback )
+{
+  Q_UNUSED( feedback );
+  mCrs = parameterAsCrs( parameters, QStringLiteral( "TARGET_CRS" ), context );
+  mExtent = parameterAsExtent( parameters, QStringLiteral( "EXTENT" ), context, mCrs );
+  mPixelSize = parameterAsDouble( parameters, QStringLiteral( "PIXEL_SIZE" ), context );
+
+  mTypeId = parameterAsInt( parameters, QStringLiteral( "OUTPUT_TYPE" ), context );
+  mProbability = parameterAsDouble( parameters, QStringLiteral( "PROBABILITY" ), context );
+
+
+  switch ( mTypeId )
+  {
+    case 0:
+      mRasterDataType = Qgis::Int16;
+      break;
+    case 1:
+      mRasterDataType = Qgis::UInt16;
+      break;
+    case 2:
+      mRasterDataType = Qgis::Int32;
+      break;
+    case 3:
+      mRasterDataType = Qgis::UInt32;
+      break;
+    case 4:
+      mRasterDataType = Qgis::Float32;
+      break;
+    case 5:
+      mRasterDataType = Qgis::Float64;
+      break;
+    default:
+      mRasterDataType = Qgis::Int16; //standard output type
+      break;
+  }
+  return true;
+}
+
+
+QVariantMap QgsRandomGeometricRasterAlgorithm::processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback )
+{
+  std::random_device rd {};
+  std::mt19937 mersenneTwister{rd()};
+
+  std::geometric_distribution<long> randomGeometricDistribution( mProbability );
+
+  const QString outputFile = parameterAsOutputLayer( parameters, QStringLiteral( "OUTPUT" ), context );
+  QFileInfo fi( outputFile );
+  const QString outputFormat = QgsRasterFileWriter::driverForExtension( fi.suffix() );
+
+  int rows = std::max( std::ceil( mExtent.height() / mPixelSize ), 1.0 );
+  int cols = std::max( std::ceil( mExtent.width() / mPixelSize ), 1.0 );
+
+  //build new raster extent based on number of columns and cellsize
+  //this prevents output cellsize being calculated too small
+  QgsRectangle rasterExtent = QgsRectangle( mExtent.xMinimum(), mExtent.yMaximum() - ( rows * mPixelSize ), mExtent.xMinimum() + ( cols * mPixelSize ), mExtent.yMaximum() );
+
+  std::unique_ptr< QgsRasterFileWriter > writer = qgis::make_unique< QgsRasterFileWriter >( outputFile );
+  writer->setOutputProviderKey( QStringLiteral( "gdal" ) );
+  writer->setOutputFormat( outputFormat );
+  std::unique_ptr<QgsRasterDataProvider > provider( writer->createOneBandRaster( mRasterDataType, cols, rows, rasterExtent, mCrs ) );
+  if ( !provider )
+    throw QgsProcessingException( QObject::tr( "Could not create raster output: %1" ).arg( outputFile ) );
+  if ( !provider->isValid() )
+    throw QgsProcessingException( QObject::tr( "Could not create raster output %1: %2" ).arg( outputFile, provider->error().message( QgsErrorMessage::Text ) ) );
+
+  double step = rows > 0 ? 100.0 / rows : 1;
+
+  for ( int row = 0; row < rows ; row++ )
+  {
+    if ( feedback->isCanceled() )
+    {
+      break;
+    }
+    //prepare raw data depending on raster data type
+    QgsRasterBlock block( mRasterDataType, cols, 1 );
+    switch ( mTypeId )
+    {
+      case 0:
+      {
+        std::vector<qint16> int16Row( cols );
+        for ( int col = 0; col < cols; col++ )
+        {
+          int16Row[col] = static_cast<qint16>( randomGeometricDistribution( mersenneTwister ) );
+        }
+        block.setData( QByteArray( reinterpret_cast<const char *>( int16Row.data() ), QgsRasterBlock::typeSize( Qgis::Int16 ) * cols ) );
+        break;
+      }
+      case 1:
+      {
+        std::vector<quint16> uInt16Row( cols );
+        for ( int col = 0; col < cols; col++ )
+        {
+          uInt16Row[col] = static_cast<quint16>( randomGeometricDistribution( mersenneTwister ) );
+        }
+        block.setData( QByteArray( reinterpret_cast<const char *>( uInt16Row.data() ), QgsRasterBlock::typeSize( Qgis::UInt16 ) * cols ) );
+        break;
+      }
+      case 2:
+      {
+        std::vector<qint32> int32Row( cols );
+        for ( int col = 0; col < cols; col++ )
+        {
+          int32Row[col] = static_cast<qint32>( randomGeometricDistribution( mersenneTwister ) );
+        }
+        block.setData( QByteArray( reinterpret_cast<const char *>( int32Row.data() ), QgsRasterBlock::typeSize( Qgis::Int32 ) * cols ) );
+        break;
+      }
+      case 3:
+      {
+        std::vector<quint32> uInt32Row( cols );
+        for ( int col = 0; col < cols; col++ )
+        {
+          uInt32Row[col] = static_cast<quint32>( randomGeometricDistribution( mersenneTwister ) );
+        }
+        block.setData( QByteArray( reinterpret_cast<const char *>( uInt32Row.data() ), QgsRasterBlock::typeSize( Qgis::UInt32 ) * cols ) );
+        break;
+      }
+      case 4:
+      {
+        std::vector<float> float32Row( cols );
+        for ( int col = 0; col < cols; col++ )
+        {
+          float32Row[col] = static_cast<float>( randomGeometricDistribution( mersenneTwister ) );
+        }
+        block.setData( QByteArray( reinterpret_cast<const char *>( float32Row.data() ), QgsRasterBlock::typeSize( Qgis::Float32 ) * cols ) );
+        break;
+      }
+      case 5:
+      {
+        std::vector<double> float64Row( cols );
+        for ( int col = 0; col < cols; col++ )
+        {
+          float64Row[col] = static_cast<double>( randomGeometricDistribution( mersenneTwister ) );
+        }
+        block.setData( QByteArray( reinterpret_cast<const char *>( float64Row.data() ), QgsRasterBlock::typeSize( Qgis::Float64 ) * cols ) );
+        break;
+      }
+      default:
+        break;
+    }
+    provider->writeBlock( &block, 1, 0, row );
+    feedback->setProgress( row * step );
+  }
+
+  QVariantMap outputs;
+  outputs.insert( QStringLiteral( "OUTPUT" ), outputFile );
+  return outputs;
+}
+
+///@endcond
---
+++ src/analysis/processing/qgsalgorithmrandomgeometricraster.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+                         qgsalgorithmrandomgeometricraster.h
+                         ---------------------
+    begin                : April 2020
+    copyright            : (C) 2020 by Clemens Raffler
+    email                : clemens dot raffler at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSRANDOMGEOMETRICRASTERALGORITHM_H
+#define QGSRANDOMGEOMETRICRASTERALGORITHM_H
+
+#define SIP_NO_FILE
+
+#include "qgis_sip.h"
+#include "qgsprocessingalgorithm.h"
+#include "qgsapplication.h"
+
+///@cond PRIVATE
+
+class QgsRandomGeometricRasterAlgorithm : public QgsProcessingAlgorithm
+{
+  public:
+    QgsRandomGeometricRasterAlgorithm() = default;
+    void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) final;
+    QIcon icon() const override { return QgsApplication::getThemeIcon( QStringLiteral( "/algorithms/mAlgorithmRandomExponentialRaster.svg" ) ); }
+    QString svgIconPath() const override { return QgsApplication::iconPath( QStringLiteral( "/algorithms/mAlgorithmRandomExponentialRaster.svg" ) ); }
+    QString name() const override;
+    QString group() const override;
+    QString groupId() const override;
+    QString displayName() const override;
+    QStringList tags() const override;
+    QString shortHelpString() const override;
+    QgsRandomGeometricRasterAlgorithm *createInstance() const override SIP_FACTORY;
+
+  protected:
+    bool prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) final;
+    QVariantMap processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) final;
+
+  private:
+    QgsRectangle mExtent;
+    QgsCoordinateReferenceSystem mCrs;
+    double mPixelSize;
+    Qgis::DataType mRasterDataType;
+    int mTypeId;
+
+    double mProbability;
+};
+
+///@endcond PRIVATE
+
+#endif // QGSRANDOMGEOMETRICRASTERALGORITHM_H
