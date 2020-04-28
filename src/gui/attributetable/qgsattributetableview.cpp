83e2e63493d9d714373340980145145cd42fa833
Alex <roya0045@users.noreply.github.com>
2020-04-28 08:08:03 -0400

    Fix Duplicate and Digitize action duplicates wrong feature when table is sorted
    Fixes #33665
    
    (cherry picked from commit d216c14adff1b656109c89c8f1e5c1e2cd2fe44d)

--- src/gui/attributetable/qgsattributetableview.cpp
+++ src/gui/attributetable/qgsattributetableview.cpp
@@ -356,7 +356,7 @@ void QgsAttributeTableView::contextMenuEvent( QContextMenuEvent *event )
   delete mActionPopup;
   mActionPopup = nullptr;
 
-  QModelIndex idx = indexAt( event->pos() );
+  QModelIndex idx = mFilterModel->mapToMaster( indexAt( event->pos() ) );
   if ( !idx.isValid() )
   {
     return;
