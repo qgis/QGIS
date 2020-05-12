0937ad3724aecb76e586a451ad171553dbad8fef
David Signer <david@opengis.ch>
2020-05-12 13:15:00 +0200

    links to another sip doc host

--- src/core/qgis_sip.h
+++ src/core/qgis_sip.h
@@ -29,7 +29,7 @@
 
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=keepreference#function-annotation-Transfer
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=keepreference#function-annotation-Transfer
  *
  * Example QgsVectorLayer::setDiagramRenderer
  */
@@ -43,80 +43,80 @@
 #define SIP_GETWRAPPER
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=keepreference#function-annotation-TransferBack
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=keepreference#function-annotation-TransferBack
  */
 #define SIP_TRANSFERBACK
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=keepreference#function-annotation-TransferThis
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=keepreference#function-annotation-TransferThis
  */
 #define SIP_TRANSFERTHIS
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-Out
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-Out
  */
 #define SIP_OUT
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-In
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-In
  */
 #define SIP_IN
 
 /*
  * Combination of
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-In
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-In
  * and
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-Out
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-Out
  */
 #define SIP_INOUT
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#function-annotation-Factory
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#function-annotation-Factory
  */
 #define SIP_FACTORY
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#class-annotation-PyName
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#class-annotation-PyName
  */
 #define SIP_PYNAME(name)
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-KeepReference
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-KeepReference
  */
 #define SIP_KEEPREFERENCE
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-Array
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-Array
  */
 #define SIP_ARRAY
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#argument-annotation-ArraySize
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#argument-annotation-ArraySize
  */
 #define SIP_ARRAYSIZE
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html#class-annotation-NoDefaultCtors
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html#class-annotation-NoDefaultCtors
  */
 #define SIP_NODEFAULTCTORS
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=deprecated#function-annotation-Deprecated
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=deprecated#function-annotation-Deprecated
  */
 #define SIP_DEPRECATED
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=constrained#argument-annotation-Constrained
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=constrained#argument-annotation-Constrained
  */
 #define SIP_CONSTRAINED
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=external#class-annotation-External
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=external#class-annotation-External
  */
 #define SIP_EXTERNAL
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=allownone
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=allownone
  */
 #define SIP_ALLOWNONE
 
@@ -147,7 +147,7 @@
 
 
 /*
- * http://pyqt.sourceforge.net/Docs/sip4/annotations.html?highlight=keepreference#function-annotation-ReleaseGIL
+ * http://docs.huihoo.com/pyqt/sip4/annotations.html?highlight=keepreference#function-annotation-ReleaseGIL
  */
 #define SIP_RELEASEGIL
 
