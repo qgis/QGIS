6bfc7194e3ad1e879c992f34ea4933a8dd2b92de
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-05-14 08:24:17 +0200

    handle basemaps in QgsPathResolver

--- src/core/qgspathresolver.cpp
+++ src/core/qgspathresolver.cpp
@@ -14,12 +14,14 @@
  ***************************************************************************/
 
 #include "qgspathresolver.h"
+#include "qgsbasemappathregistry.h"
 
 #include "qgis.h"
 #include "qgsapplication.h"
 #include <QFileInfo>
 #include <QUrl>
 #include <QUuid>
+
 
 typedef std::vector< std::pair< QString, std::function< QString( const QString & ) > > > CustomResolvers;
 Q_GLOBAL_STATIC( CustomResolvers, sCustomResolvers )
@@ -46,6 +48,12 @@ QString QgsPathResolver::readPath( const QString &f ) const
   {
     // strip away "inbuilt:" prefix, replace with actual  inbuilt data folder path
     return QgsApplication::pkgDataPath() + QStringLiteral( "/resources" ) + src.mid( 8 );
+  }
+
+  if ( src.startsWith( QLatin1String( "basemap:" ) ) )
+  {
+    // strip away "inbuilt:" prefix, replace with actual  inbuilt data folder path
+    return QgsApplication::basemapPathRegistry()->fullPath( src.mid( 8 ) ) ;
   }
 
   if ( mBaseFileName.isNull() )
@@ -182,6 +190,10 @@ QString QgsPathResolver::writePath( const QString &src ) const
   {
     return src;
   }
+
+  QString basemapPath = QgsApplication::basemapPathRegistry()->relativePath( src );
+  if ( !basemapPath.isEmpty() )
+    return QStringLiteral( "basemap:" ) + basemapPath;
 
   if ( src.startsWith( QgsApplication::pkgDataPath() + QStringLiteral( "/resources" ) ) )
   {
