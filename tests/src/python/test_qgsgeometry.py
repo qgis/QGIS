156c41782c86bdbba5c064c5eb433e1d610598f9
uclaros <uclaros@gmail.com>
2020-05-19 02:15:04 +0300

    test added

--- tests/src/python/test_qgsgeometry.py
+++ tests/src/python/test_qgsgeometry.py
@@ -1193,6 +1193,9 @@ class TestQgsGeometry(unittest.TestCase):
         assert abs(dist - exp) < 0.00001, "Expected: %f; Got:%f" % (exp, dist)
         self.assertEqual(leftOf, -1)
 
+        (point, atVertex, beforeVertex, afterVertex, dist) = QgsGeometry().closestVertex(QgsPointXY(42, 42))
+        self.assertTrue(point.isEmpty())
+
     def testAdjacentVertex(self):
         # 2-+-+-+-+-3
         # |         |
