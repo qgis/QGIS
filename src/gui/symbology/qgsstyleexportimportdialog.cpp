c8ffe9a1d1698e3b6879154a9a955058ad4edfcc
Nyall Dawson <nyall.dawson@gmail.com>
2020-05-23 11:36:29 +1000

    Fix random sort order of symbols in Symbol Export dialog
    Fixes #20572

--- src/gui/symbology/qgsstyleexportimportdialog.cpp
+++ src/gui/symbology/qgsstyleexportimportdialog.cpp
@@ -66,6 +66,7 @@ QgsStyleExportImportDialog::QgsStyleExportImportDialog( QgsStyle *style, QWidget
   mGroupSelectionDlg = nullptr;
   mTempFile = nullptr;
 
+  QgsStyle *dialogStyle = nullptr;
   if ( mDialogMode == Import )
   {
     setWindowTitle( tr( "Import Item(s)" ) );
@@ -91,8 +92,7 @@ QgsStyleExportImportDialog::QgsStyleExportImportDialog( QgsStyle *style, QWidget
     label->setText( tr( "Select items to import" ) );
     buttonBox->button( QDialogButtonBox::Ok )->setText( tr( "Import" ) );
 
-    mModel = new QgsStyleModel( mTempStyle.get(), this );
-    listItems->setModel( mModel );
+    dialogStyle = mTempStyle.get();
   }
   else
   {
@@ -115,7 +115,7 @@ QgsStyleExportImportDialog::QgsStyleExportImportDialog( QgsStyle *style, QWidget
 
     buttonBox->button( QDialogButtonBox::Ok )->setText( tr( "Export" ) );
 
-    mModel = new QgsStyleModel( mStyle, this );
+    dialogStyle = mStyle;
   }
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
@@ -125,7 +125,10 @@ QgsStyleExportImportDialog::QgsStyleExportImportDialog( QgsStyle *style, QWidget
 #endif
   listItems->setIconSize( QSize( static_cast< int >( iconSize ), static_cast< int >( iconSize * 0.9 ) ) );  // ~100, 90 on low dpi
 
+  mModel = new QgsStyleProxyModel( dialogStyle, this );
+
   mModel->addDesiredIconSize( listItems->iconSize() );
+
   listItems->setModel( mModel );
 
   connect( listItems->selectionModel(), &QItemSelectionModel::selectionChanged,
