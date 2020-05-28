e2955911a0cc8977ca4169d2115ff222baabc802
Even Rouault <even.rouault@spatialys.com>
2020-05-28 23:47:30 +0200

    qgsdemterraintileloader_p.h: remove unused member for nested structure which caused cppcheck warning

--- src/3d/terrain/qgsdemterraintileloader_p.h
+++ src/3d/terrain/qgsdemterraintileloader_p.h
@@ -125,7 +125,6 @@ class QgsDemHeightMapGenerator : public QObject
       QgsChunkNodeId tileId;
       QgsRectangle extent;
       QFuture<QByteArray> future;
-      QFutureWatcher<QByteArray> *fw;
       QElapsedTimer timer;
     };
 
