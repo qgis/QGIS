32ffd57eacbd8b09c62cdfdfc0911425d394bf43
Luigi Pirelli <luipir@gmail.com>
2020-06-01 18:17:02 +0200

    again style fix

--- tests/src/analysis/testqgsprocessing.cpp
+++ tests/src/analysis/testqgsprocessing.cpp
@@ -9455,7 +9455,7 @@ void TestQgsProcessing::modelExecution()
   // name of the variable is get from childDescription or childId. To avoid using grass provider
   // is simpler to use native algorithm and change it's description.
   // test to check fix https://github.com/qgis/QGIS/issues/36377
-  QgsProcessingModelChildAlgorithm& cx1 = model2.childAlgorithm( "cx1" );
+  QgsProcessingModelChildAlgorithm &cx1 = model2.childAlgorithm( "cx1" );
   QString oldDescription = cx1.description();
   cx1.setDescription( "cx '():.1" );
   variables = model2.variablesForChildAlgorithm( "cx3", context );
