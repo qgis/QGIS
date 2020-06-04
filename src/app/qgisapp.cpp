10f71d6b263e475a7998f446ad8bdd795c31a205
Nyall Dawson <nyall.dawson@gmail.com>
2020-06-04 14:45:45 +1000

    Avoid corrupted project thumbnail images in some circumstances

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -14975,6 +14975,7 @@ void QgisApp::createPreviewImage( const QString &path, const QIcon &icon )
                      , previewSize );
 
   QPixmap previewImage( previewSize );
+  previewImage.fill();
   QPainter previewPainter( &previewImage );
   mMapCanvas->render( &previewPainter, QRect( QPoint(), previewSize ), previewRect );
 
