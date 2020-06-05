0e6a19707e2a0c86c7d2da2c3171b6efdbf7efca
Alessandro Pasotti <elpaso@itopen.it>
2020-06-05 17:57:10 +0200

    Fix labeling aligment DD activation signals
    Fixes #37003

--- src/gui/qgstextformatwidget.h
+++ src/gui/qgstextformatwidget.h
@@ -280,8 +280,8 @@ class GUI_EXPORT QgsTextFormatWidget : public QWidget, public QgsExpressionConte
     void mFontMaxPixelSpinBox_valueChanged( int px );
     void mBufferUnitWidget_changed();
     void mMaskBufferUnitWidget_changed();
-    void mCoordXDDBtn_activated( bool active );
-    void mCoordYDDBtn_activated( bool active );
+    void mCoordXDDBtn_changed( );
+    void mCoordYDDBtn_changed( );
     void mShapeTypeCmbBx_currentIndexChanged( int index );
     void mShapeRotationCmbBx_currentIndexChanged( int index );
     void mShapeSVGParamsBtn_clicked();
