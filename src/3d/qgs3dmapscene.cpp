5bf1851a680bb4b369bafd9ec35e58b9b6cd0886
nedjima <gb_nedjima@esi.dz>
2020-06-07 00:24:46 +0100

    [Feature] directional lights support for QGIS 3D

--- src/3d/qgs3dmapscene.cpp
+++ src/3d/qgs3dmapscene.cpp
@@ -22,6 +22,7 @@
 #include <Qt3DRender/QPickingSettings>
 #include <Qt3DRender/QPickTriangleEvent>
 #include <Qt3DRender/QPointLight>
+#include <Qt3DRender/QDirectionalLight>
 #include <Qt3DRender/QRenderSettings>
 #include <Qt3DRender/QSceneLoader>
 #include <Qt3DExtras/QForwardRenderer>
@@ -103,6 +104,7 @@ Qgs3DMapScene::Qgs3DMapScene( const Qgs3DMapSettings &map, QgsAbstract3DEngine *
   connect( &map, &Qgs3DMapSettings::maxTerrainGroundErrorChanged, this, &Qgs3DMapScene::createTerrain );
   connect( &map, &Qgs3DMapSettings::terrainShadingChanged, this, &Qgs3DMapScene::createTerrain );
   connect( &map, &Qgs3DMapSettings::pointLightsChanged, this, &Qgs3DMapScene::updateLights );
+  connect( &map, &Qgs3DMapSettings::directionalLightsChanged, this, &Qgs3DMapScene::updateLights );
   connect( &map, &Qgs3DMapSettings::fieldOfViewChanged, this, &Qgs3DMapScene::updateCameraLens );
   connect( &map, &Qgs3DMapSettings::renderersChanged, this, &Qgs3DMapScene::onRenderersChanged );
 
@@ -465,6 +467,24 @@ void Qgs3DMapScene::updateLights()
     light->setConstantAttenuation( pointLightSettings.constantAttenuation() );
     light->setLinearAttenuation( pointLightSettings.linearAttenuation() );
     light->setQuadraticAttenuation( pointLightSettings.quadraticAttenuation() );
+
+    lightEntity->addComponent( light );
+    lightEntity->addComponent( lightTransform );
+    lightEntity->setParent( this );
+    mLightEntities << lightEntity;
+  }
+
+  const auto newDirectionalLights = mMap.directionalLights();
+  for ( const QgsDirectionalLightSettings &pointLightSettings : newDirectionalLights )
+  {
+    Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity;
+    Qt3DCore::QTransform *lightTransform = new Qt3DCore::QTransform;
+
+    Qt3DRender::QDirectionalLight *light = new Qt3DRender::QDirectionalLight;
+    light->setColor( pointLightSettings.color() );
+    light->setIntensity( pointLightSettings.intensity() );
+    QgsVector3D direction = pointLightSettings.direction();
+    light->setWorldDirection( QVector3D( direction.x(), direction.y(), direction.z() ) );
 
     lightEntity->addComponent( light );
     lightEntity->addComponent( lightTransform );
