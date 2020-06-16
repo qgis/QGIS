75c86786c160605c8b9937f3c35680db326eca5a
Nyall Dawson <nyall.dawson@gmail.com>
2020-06-16 14:06:43 +1000

    Add QgsCodeEditorJavascript subclass of QgsCodeEditor
    Allows plugins to create a nice editor for JSON content -- not easy
    to do if we don't expose this through the QGIS api!

---
+++ src/gui/qgscodeeditorjs.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+    qgscodeeditorjs.h - A Javascript editor based on QScintilla
+     --------------------------------------
+    Date                 : June 2020
+    Copyright            : (C) 2020 by Nyall Dawson
+    Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSCODEEDITORJS_H
+#define QGSCODEEDITORJS_H
+
+#include "qgscodeeditor.h"
+#include "qgis_sip.h"
+#include "qgis_gui.h"
+
+SIP_IF_MODULE( HAVE_QSCI_SIP )
+
+
+/**
+ * \ingroup gui
+ * A Javascript editor based on QScintilla2. Adds syntax highlighting and
+ * code autocompletion.
+ * \since QGIS 3.14
+ */
+class GUI_EXPORT QgsCodeEditorJavascript : public QgsCodeEditor
+{
+    Q_OBJECT
+
+  public:
+
+    //! Constructor for QgsCodeEditorJavascript
+    QgsCodeEditorJavascript( QWidget *parent SIP_TRANSFERTHIS = nullptr );
+
+  private:
+    void setSciLexerJs();
+};
+
+#endif // QGSCODEEDITORJS_H
