13b772d32a68679708820a764edbb91ca86f169e
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-06-29 12:04:44 +0300

    Reload the data source when any of the dependent layers is changed

--- src/core/qgsvectorlayer.cpp
+++ src/core/qgsvectorlayer.cpp
@@ -3354,6 +3354,7 @@ bool QgsVectorLayer::commitChanges()
     delete mEditBuffer;
     mEditBuffer = nullptr;
     undoStack()->clear();
+    emit afterCommitChanges();
     emit editingStopped();
   }
   else
@@ -5322,6 +5323,11 @@ void QgsVectorLayer::emitDataChanged()
   mDataChangedFired = false;
 }
 
+void QgsVectorLayer::onAfterCommitDependency()
+{
+  mDataProvider->reloadData();
+}
+
 bool QgsVectorLayer::setDependencies( const QSet<QgsMapLayerDependency> &oDeps )
 {
   QSet<QgsMapLayerDependency> deps;
@@ -5345,6 +5351,7 @@ bool QgsVectorLayer::setDependencies( const QSet<QgsMapLayerDependency> &oDeps )
     disconnect( lyr, &QgsVectorLayer::geometryChanged, this, &QgsVectorLayer::emitDataChanged );
     disconnect( lyr, &QgsVectorLayer::dataChanged, this, &QgsVectorLayer::emitDataChanged );
     disconnect( lyr, &QgsVectorLayer::repaintRequested, this, &QgsVectorLayer::triggerRepaint );
+    disconnect( lyr, &QgsVectorLayer::afterCommitChanges, this, &QgsVectorLayer::onAfterCommitDependency );
   }
 
   // assign new dependencies
@@ -5365,6 +5372,7 @@ bool QgsVectorLayer::setDependencies( const QSet<QgsMapLayerDependency> &oDeps )
     connect( lyr, &QgsVectorLayer::geometryChanged, this, &QgsVectorLayer::emitDataChanged );
     connect( lyr, &QgsVectorLayer::dataChanged, this, &QgsVectorLayer::emitDataChanged );
     connect( lyr, &QgsVectorLayer::repaintRequested, this, &QgsVectorLayer::triggerRepaint );
+    connect( lyr, &QgsVectorLayer::afterCommitChanges, this, &QgsVectorLayer::onAfterCommitDependency );
   }
 
   // if new layers are present, emit a data change
