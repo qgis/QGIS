4c80aa5c21bcec70f1680821c540d75c8daa6b35
Nyall Dawson <nyall.dawson@gmail.com>
2020-07-08 14:22:05 +1000

    [processing] Collect features algorithm doesn't care about geometry validity

--- src/analysis/processing/qgsalgorithmdissolve.h
+++ src/analysis/processing/qgsalgorithmdissolve.h
@@ -34,7 +34,7 @@ class QgsCollectorAlgorithm : public QgsProcessingAlgorithm
   protected:
 
     QVariantMap processCollection( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback,
-                                   const std::function<QgsGeometry( const QVector<QgsGeometry>& )> &collector, int maxQueueLength = 0 );
+                                   const std::function<QgsGeometry( const QVector<QgsGeometry>& )> &collector, int maxQueueLength = 0, QgsProcessingFeatureSource::Flags sourceFlags = nullptr );
 };
 
 /**
