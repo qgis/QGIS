053d504c3a5b3f229124dc6efb463c72318ea7e4
Nyall Dawson <nyall.dawson@gmail.com>
2020-07-11 10:12:42 +1000

    Only set texture origin for non point geometries

--- src/core/symbology/qgssymbol.cpp
+++ src/core/symbology/qgssymbol.cpp
@@ -832,7 +832,7 @@ void QgsSymbol::renderFeature( const QgsFeature &feature, QgsRenderContext &cont
   bool usingSegmentizedGeometry = false;
   context.setGeometry( geom.constGet() );
 
-  if ( !geom.boundingBox().isNull() )
+  if ( geom.type() != QgsWkbTypes::PointGeometry && !geom.boundingBox().isNull() )
   {
     try
     {
