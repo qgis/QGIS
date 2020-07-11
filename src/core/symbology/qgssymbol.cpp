da05927f4eba97de06f05da3bacbdc3d85636959
Nyall Dawson <nyall.dawson@gmail.com>
2020-07-11 10:12:06 +1000

    Fix rendering when transform errors occur

--- src/core/symbology/qgssymbol.cpp
+++ src/core/symbology/qgssymbol.cpp
@@ -834,8 +834,16 @@ void QgsSymbol::renderFeature( const QgsFeature &feature, QgsRenderContext &cont
 
   if ( !geom.boundingBox().isNull() )
   {
-    const QPointF boundsOrigin = _getPoint( context, QgsPoint( geom.boundingBox().xMinimum(), geom.boundingBox().yMinimum() ) );
-    context.setTextureOrigin( boundsOrigin );
+    try
+    {
+      const QPointF boundsOrigin = _getPoint( context, QgsPoint( geom.boundingBox().xMinimum(), geom.boundingBox().yMinimum() ) );
+      if ( std::isfinite( boundsOrigin.x() ) && std::isfinite( boundsOrigin.y() ) )
+        context.setTextureOrigin( boundsOrigin );
+    }
+    catch ( QgsCsException & )
+    {
+
+    }
   }
 
   bool tileMapRendering = context.testFlag( QgsRenderContext::RenderMapTile );
