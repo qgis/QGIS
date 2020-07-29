e51994f9262f7f18c17ee3478f7255f46325899f
Alessandro Pasotti <elpaso@itopen.it>
2020-07-29 17:00:27 +0200

    PG style storage: replace forbidden XML unicode chars
    Fixes #3800

--- src/core/qgsdatasourceuri.cpp
+++ src/core/qgsdatasourceuri.cpp
@@ -159,7 +159,7 @@ QgsDataSourceUri::QgsDataSourceUri( const QString &u )
       }
       else if ( pname == QLatin1String( "connect_timeout" ) )
       {
-        QgsDebugMsg( QStringLiteral( "connection timeout ignored" ) );
+        QgsDebugMsgLevel( QStringLiteral( "connection timeout ignored" ), 3 );
       }
       else if ( pname == QLatin1String( "dbname" ) )
       {
