0b7b74879e0b95a9b1e12eac1cfc99bf91fb9e49
Nyall Dawson <nyall.dawson@gmail.com>
2020-07-30 13:16:27 +1000

    Fix sipifaction of enum class

--- python/3d/auto_additions/qgsabstractmaterialsettings.py
+++ python/3d/auto_additions/qgsabstractmaterialsettings.py
@@ -1,8 +1,13 @@
 # The following has been generated automatically from src/3d/qgsabstractmaterialsettings.h
+QgsAbstractMaterialSettings.RenderingTechnique = QgsMaterialSettingsRenderingTechnique
 # monkey patching scoped based enum
-.QgsMaterialSettingsRenderingTechnique.Triangles.__doc__ = "Triangle based rendering (default)"
-.QgsMaterialSettingsRenderingTechnique.Lines.__doc__ = "Line based rendering, requires line data"
-.QgsMaterialSettingsRenderingTechnique.InstancedPoints.__doc__ = "Instanced based rendering, requiring triangles and point data"
-.QgsMaterialSettingsRenderingTechnique.Points.__doc__ = "Point based rendering, requires point data"
-QgsMaterialSettingsRenderingTechnique.__doc__ = 'Material rendering techniques\n\n.. versionadded:: 3.16\n\n' + '* ``Triangles``: ' + .QgsMaterialSettingsRenderingTechnique.Triangles.__doc__ + '\n' + '* ``Lines``: ' + .QgsMaterialSettingsRenderingTechnique.Lines.__doc__ + '\n' + '* ``InstancedPoints``: ' + .QgsMaterialSettingsRenderingTechnique.InstancedPoints.__doc__ + '\n' + '* ``Points``: ' + .QgsMaterialSettingsRenderingTechnique.Points.__doc__
+QgsAbstractMaterialSettings.Triangles = QgsMaterialSettingsRenderingTechnique.Triangles
+QgsAbstractMaterialSettings.Triangles.__doc__ = "Triangle based rendering (default)"
+QgsAbstractMaterialSettings.Lines = QgsMaterialSettingsRenderingTechnique.Lines
+QgsAbstractMaterialSettings.Lines.__doc__ = "Line based rendering, requires line data"
+QgsAbstractMaterialSettings.InstancedPoints = QgsMaterialSettingsRenderingTechnique.InstancedPoints
+QgsAbstractMaterialSettings.InstancedPoints.__doc__ = "Instanced based rendering, requiring triangles and point data"
+QgsAbstractMaterialSettings.Points = QgsMaterialSettingsRenderingTechnique.Points
+QgsAbstractMaterialSettings.Points.__doc__ = "Point based rendering, requires point data"
+QgsMaterialSettingsRenderingTechnique.__doc__ = 'Material rendering techniques\n\n.. versionadded:: 3.16\n\n' + '* ``Triangles``: ' + QgsMaterialSettingsRenderingTechnique.Triangles.__doc__ + '\n' + '* ``Lines``: ' + QgsMaterialSettingsRenderingTechnique.Lines.__doc__ + '\n' + '* ``InstancedPoints``: ' + QgsMaterialSettingsRenderingTechnique.InstancedPoints.__doc__ + '\n' + '* ``Points``: ' + QgsMaterialSettingsRenderingTechnique.Points.__doc__
 # --
