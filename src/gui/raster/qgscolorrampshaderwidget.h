41ec02e0a605fa0705b7b17983544915f36523ab
iona5 <iona5@ag99.de>
2020-08-01 12:49:09 +0200

    add documentation for new protected method

--- src/gui/raster/qgscolorrampshaderwidget.h
+++ src/gui/raster/qgscolorrampshaderwidget.h
@@ -102,6 +102,7 @@ class GUI_EXPORT QgsColorRampShaderWidget: public QWidget, protected Ui::QgsColo
   protected:
     //! Populates color ramp tree from ramp items
     void populateColormapTreeWidget( const QList<QgsColorRampShader::ColorRampItem> &colorRampItems );
+    //! Extracts the minimal and maximal value from the colormapTreeWidget
     void colormapMinMax( double &min, double &max ) SIP_SKIP;
 
   private:
