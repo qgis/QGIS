989397585787ab69e92e616f82c2670396cd54ae
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-08-02 01:56:54 +0100

    Add licence

--- src/app/3d/qgsskyboxrenderingsettingswidget.h
+++ src/app/3d/qgsskyboxrenderingsettingswidget.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+  qgsskyboxrenderingsettingswidget.h
+  --------------------------------------
+  Date                 : August 2020
+  Copyright            : (C) 2020 by Belgacem Nedjima
+  Email                : gb uderscore nedjima at esi dot dz
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef SKYBOXRENDERINGSETTINGSWIDGET_H
 #define SKYBOXRENDERINGSETTINGSWIDGET_H
 
