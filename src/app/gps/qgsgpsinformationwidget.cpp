182beada104015307238f24bcb9b7c423adbb49a
Jürgen Fredriksson <jiargei@users.noreply.github.com>
2020-08-15 00:28:07 +0200

    Update src/app/gps/qgsgpsinformationwidget.cpp
    Co-authored-by: Matthias Kuhn <matthias@opengis.ch>

--- src/app/gps/qgsgpsinformationwidget.cpp
+++ src/app/gps/qgsgpsinformationwidget.cpp
@@ -949,7 +949,7 @@ void QgsGpsInformationWidget::displayGPSInformation( const QgsGpsInformation &in
     if ( std::isfinite( info.vacc ) )
     {
       mTxtVacc->setEnabled( true );
-      mTxtVacc->setText( QString::number( info.vacc, 'f', 2 ) + "m" );
+      mTxtVacc->setText( tr( "%1 m" ), QLocale().toString( info.vacc, 'f', 2 ) ) );
     }
     else
     {
