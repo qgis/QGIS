3710dbefa0280458ac6490d2ff7266f90648f623
Nyall Dawson <nyall.dawson@gmail.com>
2020-08-31 13:37:38 +1000

    Update a test for proj 6+

--- tests/src/core/testqgslayoutmap.cpp
+++ tests/src/core/testqgslayoutmap.cpp
@@ -488,8 +488,11 @@ void TestQgsLayoutMap::dataDefinedCrs()
   //test proj string variable
   map->dataDefinedProperties().setProperty( QgsLayoutObject::MapCrs, QgsProperty::fromValue( QStringLiteral( "PROJ4: +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs" ) ) );
   map->refreshDataDefinedProperty( QgsLayoutObject::MapCrs );
+#if PROJ_VERSION_MAJOR>=6
+  QCOMPARE( map->crs().toProj(), QStringLiteral( "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs" ) );
+#else
   QCOMPARE( map->crs().toProj(), QStringLiteral( "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs" ) );
-
+#endif
 }
 
 void TestQgsLayoutMap::dataDefinedTemporalRange()
