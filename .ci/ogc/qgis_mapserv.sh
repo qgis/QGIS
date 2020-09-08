a806023a55ddc9c727e7b694fbd9801f5f000db4
Blottiere Paul <blottiere.paul@gmail.com>
2020-09-08 15:40:53 +0200

    Add docker-compose scripts

--- .ci/ogc/qgis_mapserv.sh
+++ .ci/ogc/qgis_mapserv.sh
@@ -4,4 +4,4 @@ export QGIS_SERVER_LOG_FILE=/var/log/qgisserver.log
 export QGIS_SERVER_LOG_LEVEL=0
 export QGIS_PREFIX_PATH=/usr/local/
 
-exec /usr/bin/spawn-fcgi -n -p 5555 /usr/local/bin/qgis_mapserv.fcgi
+exec /usr/bin/spawn-fcgi -n -p 5555 /usr/src/qgis/build/output/bin/qgis_mapserv.fcgi
