cf834cb3fe962e11bd8b44324d47aefd6d77f8e8
Blottiere Paul <blottiere.paul@gmail.com>
2020-09-09 11:53:15 +0200

    Update qgis server prefix path

--- .ci/ogc/qgis_mapserv.sh
+++ .ci/ogc/qgis_mapserv.sh
@@ -2,6 +2,6 @@
 
 export QGIS_SERVER_LOG_FILE=/var/log/qgisserver.log
 export QGIS_SERVER_LOG_LEVEL=0
-export QGIS_PREFIX_PATH=/usr/local/
+export QGIS_PREFIX_PATH=/usr/src/qgis/build/output
 
 exec /usr/bin/spawn-fcgi -n -p 5555 /usr/src/qgis/build/output/bin/qgis_mapserv.fcgi
