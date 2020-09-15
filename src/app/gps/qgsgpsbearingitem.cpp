251c5906dba9348d134ffa565402664cea3abc2f
Nyall Dawson <nyall.dawson@gmail.com>
2020-09-15 13:54:53 +1000

    [gps] Make GPS bearing line ellipsoid aware, instead of just a straight line drawn from the GPS point

--- src/app/gps/qgsgpsbearingitem.cpp
+++ src/app/gps/qgsgpsbearingitem.cpp
@@ -24,7 +24,7 @@
 #include "qgsproject.h"
 #include "qgsmessagelog.h"
 #include "qgssymbol.h"
-
+#include "qgslogger.h"
 
 QgsGpsBearingItem::QgsGpsBearingItem( QgsMapCanvas *mapCanvas )
   : QgsMapCanvasLineSymbolItem( mapCanvas )
@@ -75,9 +75,37 @@ void QgsGpsBearingItem::updatePosition()
 
 void QgsGpsBearingItem::updateLine()
 {
-  QLineF bearingLine;
-  bearingLine.setP1( toCanvasCoordinates( mCenter ) );
-  bearingLine.setLength( 5 * std::max( mMapCanvas->width(), mMapCanvas->height() ) );
-  bearingLine.setAngle( 90 - mBearing  - mMapCanvas->rotation() );
+  QPolygonF bearingLine;
+
+  QgsCoordinateTransform wgs84ToCanvas( mWgs84CRS, mMapCanvas->mapSettings().destinationCrs(), QgsProject::instance()->transformContext() );
+
+  try
+  {
+    bearingLine << mMapCanvas->getCoordinateTransform()->transform( wgs84ToCanvas.transform( mCenterWGS84 ) ).toQPointF();
+
+    // project out the bearing line by roughly twice the size of the canvas
+    QgsDistanceArea da1;
+    da1.setSourceCrs( mMapCanvas->mapSettings().destinationCrs(), QgsProject::instance()->transformContext() );
+    da1.setEllipsoid( QgsProject::instance()->ellipsoid() );
+    const double totalLength = 2 * da1.measureLine( mMapCanvas->mapSettings().extent().center(), QgsPointXY( mMapCanvas->mapSettings().extent().xMaximum(),
+                               mMapCanvas->mapSettings().extent().yMaximum() ) );
+
+    QgsDistanceArea da;
+    da.setSourceCrs( mWgs84CRS, QgsProject::instance()->transformContext() );
+    da.setEllipsoid( QgsProject::instance()->ellipsoid() );
+    // use 20 segments, projected from GPS position, in order to render a nice ellipsoid aware bearing line
+    for ( int i = 1; i < 20; i++ )
+    {
+      const double distance = totalLength * i / 20;
+      const QgsPointXY res = da.computeSpheroidProject( mCenterWGS84, distance, mBearing * M_PI / 180.0 );
+      bearingLine << mMapCanvas->getCoordinateTransform()->transform( wgs84ToCanvas.transform( res ) ).toQPointF();
+    }
+  }
+  catch ( QgsCsException & )
+  {
+    QgsDebugMsg( QStringLiteral( "Coordinate exception encountered while drawing GPS bearing line" ) );
+    bearingLine.clear();
+  }
+
   setLine( bearingLine );
 }
