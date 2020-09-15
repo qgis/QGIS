17bf41e4002159a8d34c86354a946ae952c6e5d7
Nyall Dawson <nyall.dawson@gmail.com>
2020-09-15 14:29:39 +1000

    Extract utility functions for saving and reading raster color map definitions to file to new QgsRasterRendererUtils class

---
+++ src/core/raster/qgsrasterrendererutils.cpp
@@ -0,0 +1,142 @@
+/***************************************************************************
+                         qgsrasterrendererutils.cpp
+                         -------------------
+    begin                : September 2020
+    copyright            : (C) 2020 by Nyall Dawson
+    email                : nyall dawson dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsrasterrendererutils.h"
+#include "qgis.h"
+#include <QFile>
+#include <QTextStream>
+
+bool QgsRasterRendererUtils::parseColorMapFile( const QString &path, QList<QgsColorRampShader::ColorRampItem> &items, QgsColorRampShader::Type &type, QStringList &errors )
+{
+  QFile inputFile( path );
+  if ( !inputFile.open( QFile::ReadOnly ) )
+  {
+    errors.append( QObject::tr( "Read access denied. Adjust the file permissions and try again.\n\n" ) );
+    return false;
+  }
+
+  bool res = true;
+
+  QTextStream inputStream( &inputFile );
+  QString inputLine;
+  QStringList inputStringComponents;
+  int lineCounter = 0;
+
+  //read through the input looking for valid data
+  while ( !inputStream.atEnd() )
+  {
+    lineCounter++;
+    inputLine = inputStream.readLine();
+    if ( !inputLine.isEmpty() )
+    {
+      if ( !inputLine.simplified().startsWith( '#' ) )
+      {
+        if ( inputLine.contains( QLatin1String( "INTERPOLATION" ), Qt::CaseInsensitive ) )
+        {
+          inputStringComponents = inputLine.split( ':' );
+          if ( inputStringComponents.size() == 2 )
+          {
+            if ( inputStringComponents[1].trimmed().toUpper().compare( QLatin1String( "INTERPOLATED" ), Qt::CaseInsensitive ) == 0 )
+            {
+              type = QgsColorRampShader::Interpolated;
+            }
+            else if ( inputStringComponents[1].trimmed().toUpper().compare( QLatin1String( "DISCRETE" ), Qt::CaseInsensitive ) == 0 )
+            {
+              type = QgsColorRampShader::Discrete;
+            }
+            else
+            {
+              type = QgsColorRampShader::Exact;
+            }
+          }
+          else
+          {
+            res = false;
+            errors << QObject::tr( "Unknown interpolation type at line %1: %2" ).arg( lineCounter ).arg( inputLine );
+          }
+        }
+        else
+        {
+          inputStringComponents = inputLine.split( ',' );
+          if ( inputStringComponents.size() == 6 )
+          {
+            QgsColorRampShader::ColorRampItem currentItem( QLocale().toDouble( inputStringComponents[0] ),
+                QColor::fromRgb( inputStringComponents[1].toInt(), inputStringComponents[2].toInt(),
+                                 inputStringComponents[3].toInt(), inputStringComponents[4].toInt() ),
+                inputStringComponents[5] );
+            items.push_back( currentItem );
+          }
+          else
+          {
+            res = false;
+            errors << QObject::tr( "Invalid entry at line %1: %2" ).arg( lineCounter ).arg( inputLine );
+          }
+        }
+      }
+    }
+    lineCounter++;
+  }
+
+  return res;
+}
+
+bool QgsRasterRendererUtils::saveColorMapFile( const QString &path, const QList<QgsColorRampShader::ColorRampItem> &items, QgsColorRampShader::Type type )
+{
+  QFile outputFile( path );
+  if ( outputFile.open( QFile::WriteOnly | QIODevice::Truncate ) )
+  {
+    QTextStream outputStream( &outputFile );
+    outputStream << "# " << QObject::tr( "QGIS Generated Color Map Export File" ) << '\n';
+    outputStream << "INTERPOLATION:";
+    switch ( type )
+    {
+      case QgsColorRampShader::Interpolated:
+        outputStream << "INTERPOLATED\n";
+        break;
+      case QgsColorRampShader::Discrete:
+        outputStream << "DISCRETE\n";
+        break;
+      case QgsColorRampShader::Exact:
+        outputStream << "EXACT\n";
+        break;
+    }
+
+    int i = 0;
+    for ( const QgsColorRampShader::ColorRampItem &item : items )
+    {
+      outputStream << qgsDoubleToString( item.value ) << ',';
+      outputStream << item.color.red() << ',' << item.color.green() << ',' << item.color.blue() << ',' << item.color.alpha() << ',';
+      if ( item.label.isEmpty() )
+      {
+        outputStream << "Color entry " << i + 1 << '\n';
+      }
+      else
+      {
+        outputStream << item.label << '\n';
+      }
+      i++;
+    }
+    outputStream.flush();
+    outputFile.close();
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
