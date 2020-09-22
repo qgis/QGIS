f87fb04af812e1cd1c80195d8e6c45096ee4e4c3
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-09-22 13:30:32 +0200

    fix typo

--- src/app/locator/qgsinbuiltlocatorfilters.cpp
+++ src/app/locator/qgsinbuiltlocatorfilters.cpp
@@ -328,7 +328,7 @@ QStringList QgsActiveLayerFeaturesLocatorFilter::prepare( const QString &string,
       mFieldsCompletion << field.name();
     }
 
-    // the completion list (returned by the current method) is used by the locator line edit directy
+    // the completion list (returned by the current method) is used by the locator line edit directly
     completionList.append( QStringLiteral( "@%1 " ).arg( field.name() ) );
 
     if ( field.type() == QVariant::String )
