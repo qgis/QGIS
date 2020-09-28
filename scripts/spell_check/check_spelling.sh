12cdf632d533edacdb1bfa97b3cb77ccc400a0f3
Alessandro Pasotti <elpaso@itopen.it>
2020-09-28 09:23:33 +0200

    Spell ignore

--- scripts/spell_check/check_spelling.sh
+++ scripts/spell_check/check_spelling.sh
@@ -23,7 +23,7 @@
 # fi
 
 # extensions or files that should be excluded from file list if :% is appended in the spelling.dat file
-EXCLUDE_SCRIPT_LIST='(\.(sql|xml|svg|sip|pl|sh|qgs|badquote|cmake(\.in)?)|^(debian/copyright|cmake_templates/.*|tests/testdata/labeling/README.rst|tests/testdata/font/QGIS-Vera/COPYRIGHT.TXT|doc/NEWS\.html|debian/build/))$'
+EXCLUDE_SCRIPT_LIST='(\.(xml|svg|sip|pl|sh|qgs|badquote|cmake(\.in)?)|^(debian/copyright|cmake_templates/.*|tests/testdata/labeling/README.rst|tests/testdata/font/QGIS-Vera/COPYRIGHT.TXT|doc/NEWS\.html|debian/build/))$'
 
 # always exclude these external files
 EXCLUDE_EXTERNAL_LIST='(resources/server/api/ogc/static/landingpage/js/.*)$'
