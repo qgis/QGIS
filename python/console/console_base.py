8533d9a89b3cbc8d8ecc5771b7f0bf596e85210d
Salvatore Larosa <lrssvtml@gmail.com>
2020-10-03 16:50:19 +0200

    [pyqgis-console] restore the correct prompt

--- python/console/console_base.py
+++ python/console/console_base.py
@@ -31,6 +31,8 @@ class QgsPythonConsoleBase(QgsCodeEditorPython):
 
     MARKER_NUM = 6
 
+    HANDY_COMMANDS = ['_pyqgis', '_api', '_cookbook']
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -136,6 +138,16 @@ class QgsPythonConsoleBase(QgsCodeEditorPython):
             text = text.replace('>>> ', '').replace('... ', '').strip()  # removing prompts
             version = '.'.join(Qgis.QGIS_VERSION.split('.')[0:2])
             QDesktopServices.openUrl(QUrl('https://qgis.org/pyqgis/' + version + '/search.html?q=' + text))
+
+    def handyCommands(self, hcmd):
+        version = 'master' if 'master' in Qgis.QGIS_VERSION.lower() else re.findall(r'^\d.[0-9]*', Qgis.QGIS_VERSION)[0]
+        if hcmd == '_pyqgis':
+            QDesktopServices.openUrl(QUrl("https://qgis.org/pyqgis/{}".format(version)))
+        elif hcmd == '_api':
+            QDesktopServices.openUrl(QUrl("https://qgis.org/api/{}".format('' if version == 'master' else version)))
+        elif hcmd == '_cookbook':
+            QDesktopServices.openUrl(QUrl("https://docs.qgis.org/{}/en/docs/pyqgis_developer_cookbook/".format(
+                'testing' if version == 'master' else version)))
 
 
 if __name__ == "__main__":
