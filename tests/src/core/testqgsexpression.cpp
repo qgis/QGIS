e1319a17095309e6a3b958b4372b77fca7b0c4f9
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-10-15 15:10:09 +0200

    fix test with old Qt

--- tests/src/core/testqgsexpression.cpp
+++ tests/src/core/testqgsexpression.cpp
@@ -2044,7 +2044,7 @@ class TestQgsExpression: public QObject
         context.setFeature( f );
         QgsExpression exp( QString( "with_variable('my_var',\"col1\", aggregate(layer:='aggregate_layer', aggregate:='concatenate_unique', expression:=\"col2\", filter:=\"col1\"=@my_var))" ) );
         QString res = exp.evaluate( &context ).toString();
-        QCOMPARE( res, f.attribute( "col2" ) );
+        QCOMPARE( res, f.attribute( "col2" ).toString() );
 
         // also test for generic aggregates
         exp = QString( "with_variable('my_var',\"col1\", sum(expression:=\"col1\", filter:=\"col1\"=@my_var))" );
