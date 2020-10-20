cdff8ba5d0e7d100497ef1f278553b41a39e2f24
signedav <david@opengis.ch>
2020-10-20 09:13:48 +0200

    indicator of offline layers in the layertree according to the customProperty isOfflineEditable

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -248,6 +248,7 @@ Q_GUI_EXPORT extern int qt_defaultDpiX();
 #include "qgslayertreeviewnonremovableindicator.h"
 #include "qgslayertreeviewnocrsindicator.h"
 #include "qgslayertreeviewtemporalindicator.h"
+#include "qgslayertreeviewofflineindicator.h"
 #include "qgslayout.h"
 #include "qgslayoutatlas.h"
 #include "qgslayoutcustomdrophandler.h"
@@ -4735,6 +4736,7 @@ void QgisApp::initLayerTreeView()
   new QgsLayerTreeViewMemoryIndicatorProvider( mLayerTreeView );  // gets parented to the layer view
   new QgsLayerTreeViewTemporalIndicatorProvider( mLayerTreeView ); // gets parented to the layer view
   new QgsLayerTreeViewNoCrsIndicatorProvider( mLayerTreeView );  // gets parented to the layer view
+  new QgsLayerTreeViewOfflineIndicatorProvider( mLayerTreeView );  // gets parented to the layer view
   QgsLayerTreeViewBadLayerIndicatorProvider *badLayerIndicatorProvider = new QgsLayerTreeViewBadLayerIndicatorProvider( mLayerTreeView );  // gets parented to the layer view
   connect( badLayerIndicatorProvider, &QgsLayerTreeViewBadLayerIndicatorProvider::requestChangeDataSource, this, &QgisApp::changeDataSource );
   new QgsLayerTreeViewNonRemovableIndicatorProvider( mLayerTreeView );  // gets parented to the layer view
---
+++ src/app/qgslayertreeviewofflineindicator.cpp
@@ -0,0 +1,59 @@
+/***************************************************************************
+  qgslayertreeviewofflineindicator.cpp
+  --------------------------------------
+  Date                 : October 2020
+  Copyright            : (C) 2020 by David Signer
+  Email                : david at opengis dot ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgslayertreeviewofflineindicator.h"
+#include "qgslayertreeview.h"
+#include "qgslayertree.h"
+#include "qgslayertreemodel.h"
+#include "qgisapp.h"
+
+QgsLayerTreeViewOfflineIndicatorProvider::QgsLayerTreeViewOfflineIndicatorProvider( QgsLayerTreeView *view )
+  : QgsLayerTreeViewIndicatorProvider( view )
+{
+
+}
+
+void QgsLayerTreeViewOfflineIndicatorProvider::connectSignals( QgsMapLayer *layer )
+{
+  if ( !layer )
+    return;
+
+  //this has to be solved different
+  connect( mLayerTreeView->layerTreeModel()->rootGroup(), &QgsLayerTreeNode::customPropertyChanged, this, [ this, layer ]( ) { this->onLayerChanged( layer ); } );
+}
+
+bool QgsLayerTreeViewOfflineIndicatorProvider::acceptLayer( QgsMapLayer *layer )
+{
+  return layer->customProperty( QStringLiteral( "isOfflineEditable" ), false ).toBool();
+}
+
+QString QgsLayerTreeViewOfflineIndicatorProvider::iconName( QgsMapLayer *layer )
+{
+  Q_UNUSED( layer )
+  return QStringLiteral( "/mIndicatorOffline.svg" );
+}
+
+QString QgsLayerTreeViewOfflineIndicatorProvider::tooltipText( QgsMapLayer *layer )
+{
+  Q_UNUSED( layer )
+  return tr( "<b>Offline layer</b>" );
+}
+
+void QgsLayerTreeViewOfflineIndicatorProvider::onLayerChanged( QgsMapLayer *layer )
+{
+  if ( !layer )
+    return;
+  updateLayerIndicator( layer );
+}
