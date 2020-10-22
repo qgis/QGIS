3f522e86c90691728db23e8d926271f7ce29411f
Nyall Dawson <nyall.dawson@gmail.com>
2020-10-22 12:56:00 +1000

    Newly created directional lights default to slightly angled lights, instead of directly top-down directions
    This is more friendly behavior, because it allows visibility of
    vertical features with a default light (instead of "black" walls
    and lit roofs you get shaded walls + roofs)

--- src/3d/qgsdirectionallightsettings.h
+++ src/3d/qgsdirectionallightsettings.h
@@ -58,7 +58,7 @@ class _3D_EXPORT QgsDirectionalLightSettings
     bool operator==( const QgsDirectionalLightSettings &other );
 
   private:
-    QgsVector3D mDirection = QgsVector3D( 0.0, -1.0, 0.0 );
+    QgsVector3D mDirection { -0.32, -0.91, -0.27 };
     QColor mColor = Qt::white;
     float mIntensity = 0.5;
 };
