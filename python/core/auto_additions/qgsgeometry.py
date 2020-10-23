8ea34de8a2cf1e7ad89562412d61a348f5490a50
Peter Petrik <zilolv@gmail.com>
2020-10-23 19:18:12 +0200

    restore accidentally deleted files

---
+++ python/core/auto_additions/qgsgeometry.py
@@ -0,0 +1,5 @@
+# The following has been generated automatically from src/core/geometry/qgsgeometry.h
+QgsGeometry.OperationResult.baseClass = QgsGeometry
+QgsGeometry.BufferSide.baseClass = QgsGeometry
+QgsGeometry.EndCapStyle.baseClass = QgsGeometry
+QgsGeometry.JoinStyle.baseClass = QgsGeometry
---
+++ python/core/auto_additions/qgstextcharacterformat.py
@@ -0,0 +1,7 @@
+# The following has been generated automatically from src/core/textrenderer/qgstextcharacterformat.h
+# monkey patching scoped based enum
+QgsTextCharacterFormat.BooleanValue.NotSet.__doc__ = "Property is not set"
+QgsTextCharacterFormat.BooleanValue.SetTrue.__doc__ = "Property is set and ``True``"
+QgsTextCharacterFormat.BooleanValue.SetFalse.__doc__ = "Property is set and ``False``"
+QgsTextCharacterFormat.BooleanValue.__doc__ = 'Status values for boolean format properties\n\n' + '* ``NotSet``: ' + QgsTextCharacterFormat.BooleanValue.NotSet.__doc__ + '\n' + '* ``SetTrue``: ' + QgsTextCharacterFormat.BooleanValue.SetTrue.__doc__ + '\n' + '* ``SetFalse``: ' + QgsTextCharacterFormat.BooleanValue.SetFalse.__doc__
+# --
