8b3aa2c2cae4b232f82050201fe28f8fa8141e2c
Alex <roya0045@users.noreply.github.com>
2020-10-25 09:23:46 -0400

    Update qgsgdalcompress.py

--- python/plugins/processing/algs/gdal/qgsgdalcompress.py
+++ python/plugins/processing/algs/gdal/qgsgdalcompress.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 """
@@ -58,71 +59,90 @@ class translate(GdalAlgorithm):
 
     def initAlgorithm(self, config=None):
 
-        self.TYPES = [self.tr('Use Input Layer Data Type'), 'Byte', 'Int16', 'UInt16', 'UInt32', 'Int32', 'Float32', 'Float64', 'CInt16', 'CInt32', 'CFloat32', 'CFloat64']
-		self.COMPRESSOR = ["JPEG","LZW","PACKBITS","DEFLATE","LZMA","ZSTD","LERC","LERC_DEFLATE","LERC_ZSTD","WEBP","CCITTRLE","CCITTFAX3","CCITTFAX4","NONE"]
-		
-        self.addParameter(QgsProcessingParameterRasterLayer(self.INPUT, self.tr('Input layer')))
+        self.TYPES = [self.tr('Use Input Layer Data Type'), 'Byte', 'Int16', 'UInt16', 'UInt32',
+                      'Int32', 'Float32', 'Float64', 'CInt16', 'CInt32', 'CFloat32', 'CFloat64']
+        self.COMPRESSOR = ["JPEG", "LZW", "PACKBITS", "DEFLATE", "LZMA", "ZSTD", "LERC",
+                           "LERC_DEFLATE", "LERC_ZSTD", "WEBP", "CCITTRLE", "CCITTFAX3", "CCITTFAX4", "NONE"]
+
+        self.addParameter(QgsProcessingParameterRasterLayer(
+            self.INPUT, self.tr('Input layer')))
         self.addParameter(QgsPRocessingParameterEnum(self.COMPRESSOR,
-                                                     self.tr('Compression method to use'),
+                                                     self.tr(
+                                                         'Compression method to use'),
                                                      self.COMPRESSION,
                                                      allowMultiple=False,
                                                      defaultValue=3))
 
         self.addParameter(QgsProcessingParameterNumber(self.NUM_THREADS,
-                                                       self.tr('Number of thread to use for compression'),
+                                                       self.tr(
+                                                           'Number of thread to use for compression'),
                                                        type=QgsProcessingParameterNumber.Integer,
-                                                       defaultValue=-1,minValue=-1,maxValue = (int)(os.popen('grep -c cores /proc/cpuinfo').read()),
+                                                       defaultValue=-1, minValue=-1,
+                                                       maxValue=(int)(
+                                                           os.popen('grep -c cores /proc/cpuinfo').read()),
                                                        optional=True))
 
         self.addParameter(QgsProcessingParameterNumber(self.PREDICTOR,
-                                                       self.tr('Sets the LZW, DEFLATE and ZSTD compression.'),
+                                                       self.tr(
+                                                           'Sets the LZW, DEFLATE and ZSTD compression.'),
                                                        type=QgsProcessingParameterNumber.Integer,
-                                                       defaultValue=1,minValue=1,maxValue =3,
+                                                       defaultValue=1, minValue=1, maxValue=3,
                                                        optional=True))
 
-        sparse=QgsProcessingParameterBoolean(self.SPARSE,
-                                             self.tr('Make output sparse (save space, possibly not supported by other reader)'),
-                                             defaultValue=False)
-        sparse.setFlags(sparse.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        sparse = QgsProcessingParameterBoolean(self.SPARSE,
+                                               self.tr(
+                                                   'Make output sparse (save space, possibly not supported by other reader)'),
+                                               defaultValue=False)
+        sparse.setFlags(sparse.flags() |
+                        QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(sparse)
 
         self.addParameter(QgsProcessingParameterNumber(self.QUALITY,
-                                                       self.tr('Control Quality/compression, 100 is best quality, 1 is most compression, default is 75'),
+                                                       self.tr(
+                                                           'Control Quality/compression, 100 is best quality, 1 is most compression, default is 75'),
                                                        type=QgsProcessingParameterNumber.Integer,
-                                                       defaultValue=75,minValue=1,maxValue = 100,
+                                                       defaultValue=75, minValue=1, maxValue=100,
                                                        optional=True))
 
-        tables=QgsProcessingParameterNumber(self.JPEGTABLESMODE,
-                                                       self.tr('Configure JPEG quantisation'),
-                                                       type=QgsProcessingParameterNumber.Integer,
-                                                       defaultValue=0,minValue=0,maxValue = 3,
-                                                       optional=True)
-        tables.setFlags(tables.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        tables = QgsProcessingParameterNumber(self.JPEGTABLESMODE,
+                                              self.tr(
+                                                  'Configure JPEG quantisation'),
+                                              type=QgsProcessingParameterNumber.Integer,
+                                              defaultValue=0, minValue=0, maxValue=3,
+                                              optional=True)
+        tables.setFlags(tables.flags() |
+                        QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(tables)
 
         dataType_param = QgsProcessingParameterEnum(self.DATA_TYPE,
-                                                    self.tr('Output data type'),
+                                                    self.tr(
+                                                        'Output data type'),
                                                     self.TYPES,
                                                     allowMultiple=False,
                                                     defaultValue=0)
-        dataType_param.setFlags(dataType_param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        dataType_param.setFlags(dataType_param.flags(
+        ) | QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(dataType_param)
 
         options_param = QgsProcessingParameterString(self.OPTIONS,
-                                                     self.tr('Additional creation options'),
+                                                     self.tr(
+                                                         'Additional creation options'),
                                                      defaultValue='',
                                                      optional=True)
-        options_param.setFlags(options_param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        options_param.setFlags(options_param.flags() |
+                               QgsProcessingParameterDefinition.FlagAdvanced)
         options_param.setMetadata({
             'widget_wrapper': {
                 'class': 'processing.algs.gdal.ui.RasterOptionsWidget.RasterOptionsWidgetWrapper'}})
         self.addParameter(options_param)
 
         extra_param = QgsProcessingParameterString(self.EXTRA,
-                                                   self.tr('Additional command-line parameters'),
+                                                   self.tr(
+                                                       'Additional command-line parameters'),
                                                    defaultValue=None,
                                                    optional=True)
-        extra_param.setFlags(extra_param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        extra_param.setFlags(extra_param.flags() |
+                             QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(extra_param)
 
         self.addParameter(QgsProcessingParameterRasterDestination(self.OUTPUT,
@@ -148,11 +168,12 @@ class translate(GdalAlgorithm):
 
     def getConsoleCommands(self, parameters, context, feedback, executing=True):
         qualArg = None
-        pred=False
+        pred = False
 
         inLayer = self.parameterAsRasterLayer(parameters, self.INPUT, context)
         if inLayer is None:
-            raise QgsProcessingException(self.invalidRasterError(parameters, self.INPUT))
+            raise QgsProcessingException(
+                self.invalidRasterError(parameters, self.INPUT))
 
         out = self.parameterAsOutputLayer(parameters, self.OUTPUT, context)
         self.setOutputValue(self.OUTPUT, out)
@@ -166,50 +187,54 @@ class translate(GdalAlgorithm):
 
         arguments.append('-strict')
 
+        predictor = self.parameterAsInteger(
+            parameters, self.PREDICTOR, context)
 
-        predictor = self.parameterAsInteger( parameters, self.PREDICTOR,context)    
+        compression = self.COMPRESSOR[self.parameterAsEnum(
+            parameters, self.COMPRESSION, context)]
+        arguments.append('-co '+'{}={}'.format('COMPRESS', compression))
 
-        compression = self.COMPRESSOR[self.parameterAsEnum(parameters, self.COMPRESSION,context)]
-        arguments.append('-co '+'{}={}'.format('COMPRESS',compression))
+        quality = self.parameterAsInteger(
+            parameters, self.QUALITY, context)/100.0
 
-        quality = self.parameterAsInteger( parameters, self.QUALITY,context)/100.0
-
-		if 'DEFLATE' in compression :
-			qualArg='-ZLEVEL'
+        if 'DEFLATE' in compression:
+            qualArg = '-ZLEVEL'
             if compression == 'LERC_DEFLATE':
                 quality = int(quality*12)
             else:
                 quality = int(quality*9)
             pred = True
-		elif compression =='JPG':
-			quality=int(quality*100)
-            qualArg='-JPEG_QUALITY'
-            jpgtables=self.parameterAsInteger(parameters,self.JPEGTABLESMODE,context)
-            arguments.append('-co '+'{}={}'.format('JPEGTABLESMODE',jpgtables))
-		elif compression =='WEBP':
-			qualArg = '-WEBP_LEVEL'
-            quality=int(quality*100)
-		elif 'ZSTD' in compression:
-			qualArg='-ZSTD_LEVEL'
+        elif compression == 'JPG':
+            quality = int(quality*100)
+            qualArg = '-JPEG_QUALITY'
+            jpgtables = self.parameterAsInteger(
+                parameters, self.JPEGTABLESMODE, context)
+            arguments.append(
+                '-co '+'{}={}'.format('JPEGTABLESMODE', jpgtables))
+        elif compression == 'WEBP':
+            qualArg = '-WEBP_LEVEL'
+            quality = int(quality*100)
+        elif 'ZSTD' in compression:
+            qualArg = '-ZSTD_LEVEL'
             quality = int(quality*22)
-            pred=True
-		elif compression = 'LZW':
-			pred=True
+            pred = True
+        elif compression == 'LZW':
+            pred = True
 
         if qualArg:
-            arguments.append('-co '+'{}={}'.format(qualArg,max(quality,1))
-        
-        if pred:
-            arguments.append('-co '+'{}={}'.format('PREDICTOR',predictor)
+            arguments.append('-co '+'{}={}'.format(qualArg, max(quality, 1)))
 
-        threads=self.parameterAsInteger(parameters,self.NUM_THREADS,context)
-        arguments.append('-co '+'{}={}'.format('NUM_THREADS',threads))
+        if pred:
+            arguments.append('-co '+'{}={}'.format('PREDICTOR', predictor))
+
+        threads = self.parameterAsInteger(
+            parameters, self.NUM_THREADS, context)
+        arguments.append('-co '+'{}={}'.format('NUM_THREADS', threads))
 
         if self.parameterAsBoolean(parameters, self.SPARSE, context):
-            arguments.append('-co '+'{}={}'.format('SPARSE_OK','TRUE'))
+            arguments.append('-co '+'{}={}'.format('SPARSE_OK', 'TRUE'))
 
-
-        arguments.append('-ot ' +'BIGTIFF=IF_SAFER')
+        arguments.append('-ot ' + 'BIGTIFF=IF_SAFER')
 
         options = self.parameterAsString(parameters, self.OPTIONS, context)
         if options:
