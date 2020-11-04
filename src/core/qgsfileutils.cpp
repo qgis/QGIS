d34d61e36f9313a300790dbea4f01d17907dfefc
Nyall Dawson <nyall.dawson@gmail.com>
2020-11-04 11:08:16 +1000

    Fix build on older qt versions

--- src/core/qgsfileutils.cpp
+++ src/core/qgsfileutils.cpp
@@ -70,10 +70,18 @@ bool QgsFileUtils::fileMatchesFilter( const QString &fileName, const QString &fi
       const QStringList globPatterns = matches.captured( 1 ).split( ' ' );
       for ( const QString &glob : globPatterns )
       {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         const QString re = QRegularExpression::wildcardToRegularExpression( glob );
+
         const QRegularExpression globRx( re );
         if ( globRx.match( name ).hasMatch() )
           return true;
+#else
+        QRegExp rx( glob );
+        rx.setPatternSyntax( QRegExp::Wildcard );
+        if ( rx.indexIn( name ) != -1 )
+          return true;
+#endif
       }
     }
   }
