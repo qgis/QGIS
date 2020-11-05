72fe36354a4955ba22f794a66813768faf16a6fc
Alessandro Pasotti <elpaso@itopen.it>
2020-11-05 12:08:48 +0100

    Fix min/lowest and add decimal constructor

---
+++ src/gui/qgslocaleawarenumericlineeditdelegate.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+  qgslocaleawarenumericlineeditdelegate.h - QgsLocaleAwareNumericLineEditDelegate
+
+ ---------------------
+ begin                : 5.11.2020
+ copyright            : (C) 2020 by Alessandro Pasotti
+ email                : elpaso at itopen dot it
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef QGSLOCALEAWARENUMERICLINEEDITDELEGATE_H
+#define QGSLOCALEAWARENUMERICLINEEDITDELEGATE_H
+
+
+class QgsLocaleAwareNumericLineEditDelegate
+{
+  public:
+    QgsLocaleAwareNumericLineEditDelegate();
+};
+
+#endif // QGSLOCALEAWARENUMERICLINEEDITDELEGATE_H
