68ceaf22f015db19e16323b8fccb7c562a35e5c2
Nyall Dawson <nyall.dawson@gmail.com>
2020-11-09 09:16:55 +1000

    Fix capitalisation, spin box clear values in eye dome lighting widgets

--- src/app/3d/qgs3dmapconfigwidget.cpp
+++ src/app/3d/qgs3dmapconfigwidget.cpp
@@ -75,6 +75,10 @@ Qgs3DMapConfigWidget::Qgs3DMapConfigWidget( Qgs3DMapSettings *map, QgsMapCanvas 
   spinMapResolution->setClearValue( 512 );
   spinScreenError->setClearValue( 3 );
   spinGroundError->setClearValue( 1 );
+  edlStrengthSpinBox->setClearValue( 1000 );
+  edlDistanceSpinBox->setClearValue( 1 );
+  mDebugShadowMapSizeSpinBox->setClearValue( 0.1 );
+  mDebugDepthMapSizeSpinBox->setClearValue( 0.1 );
 
   cboTerrainLayer->setAllowEmptyLayer( true );
   cboTerrainLayer->setFilters( QgsMapLayerProxyModel::RasterLayer );
