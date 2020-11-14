2688389332b3e059554abb87444319e4083405b4
Alex <roya0045@users.noreply.github.com>
2020-11-13 19:43:11 -0500

    elif is not a thing in c++

--- src/core/expression/qgsexpressionfunction.cpp
+++ src/core/expression/qgsexpressionfunction.cpp
@@ -5325,7 +5325,7 @@ static QVariant fcnArrayGet( const QVariantList &values, const QgsExpressionCont
   const QVariantList list = QgsExpressionUtils::getListValue( values.at( 0 ), parent );
   const int pos = QgsExpressionUtils::getNativeIntValue( values.at( 1 ), parent );
   if ( pos < list.length() && pos >=0 ) return list.at( pos );
-  elif ( pos < 0 && ( list.length() + pos ) >= 0 )
+  else if ( pos < 0 && ( list.length() + pos ) >= 0 )
     return list.at( list.length() + pos );
   return QVariant();
 }
