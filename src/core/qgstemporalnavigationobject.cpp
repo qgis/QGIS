ef049e3d35e3d5d89e0f090cda6cf2824f30109f
Nyall Dawson <nyall.dawson@gmail.com>
2020-11-17 08:15:17 +1000

    Update src/core/qgstemporalnavigationobject.cpp

--- src/core/qgstemporalnavigationobject.cpp
+++ src/core/qgstemporalnavigationobject.cpp
@@ -195,7 +195,6 @@ void QgsTemporalNavigationObject::setFrameDuration( QgsInterval frameDuration )
   emit temporalFrameDurationChanged( mFrameDuration );
 
   // temporarily disable the updateTemporalRange signal, as we'll emit it ourselves at the end of this function...
-  mBlockUpdateTemporalRangeSignal--;
 
   // forcing an update of our views
   QgsDateTimeRange range = dateTimeRangeForFrameNumber( mCurrentFrameNumber );
