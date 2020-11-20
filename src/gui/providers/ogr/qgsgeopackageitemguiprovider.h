5e7b5003db3656cde9735ecb305e1395f769d3ba
Nyall Dawson <nyall.dawson@gmail.com>
2020-11-21 07:55:31 +1000

    Use message bar for messages when performing actions to geopackages through browser instead of message boxes

--- src/gui/providers/ogr/qgsgeopackageitemguiprovider.h
+++ src/gui/providers/ogr/qgsgeopackageitemguiprovider.h
@@ -26,6 +26,7 @@
 #define SIP_NO_FILE
 
 class QgsGeoPackageCollectionItem;
+class QgsGeoPackageRootItem;
 
 class QgsGeoPackageItemGuiProvider : public QObject, public QgsDataItemGuiProvider
 {
@@ -49,15 +50,16 @@ class QgsGeoPackageItemGuiProvider : public QObject, public QgsDataItemGuiProvid
                      const QMimeData *data,
                      Qt::DropAction action ) override;
   private:
-    bool handleDropGeopackage( QgsGeoPackageCollectionItem *item, const QMimeData *data );
+    bool handleDropGeopackage( QgsGeoPackageCollectionItem *item, const QMimeData *data, const QgsDataItemGuiContext &context );
     //! Compacts (VACUUM) a geopackage database
-    void vacuumGeoPackageDbAction( const QString &path, const QString &name );
-    void createDatabase( );
+    void vacuumGeoPackageDbAction( const QString &path, const QString &name, const QgsDataItemGuiContext &context );
+    void createDatabase( QPointer< QgsGeoPackageRootItem > item );
 
   protected slots:
-    void vacuum();
-    void renameVectorLayer();
-    void deleteGpkg();
+    void vacuum( const QString &path, const QString &name, const QgsDataItemGuiContext &context );
+    void renameVectorLayer( const QString &uri, const QString &key, const QStringList &tableNames,
+                            QPointer< QgsDataItem > item, const QgsDataItemGuiContext &context );
+    void deleteGpkg( const QString &itemPath, const QString &name, QPointer<QgsDataItem> parent, const QgsDataItemGuiContext &context );
 };
 
 ///@endcond
