877c7b5cbb6febe30cd11324052024f2251c59d4
Peter Petrik <zilolv@gmail.com>
2020-11-23 20:04:29 +0100

    modernize? cmake

--- cmake/FindPoly2Tri.cmake
+++ cmake/FindPoly2Tri.cmake
@@ -10,14 +10,15 @@
 #  Poly2Tri_INCLUDE_DIR - The Poly2Tri include directory
 #  Poly2Tri_LIBRARY - The library needed to use Poly2Tri
 
-FIND_PATH(Poly2Tri_INCLUDE_DIR poly2tri.h
+find_path(Poly2Tri_INCLUDE_DIR poly2tri.h
           HINTS $ENV{LIB_DIR}/include)
 
-FIND_LIBRARY(Poly2Tri_LIBRARY NAMES poly2tri libpoly2tri
+find_library(Poly2Tri_LIBRARY NAMES poly2tri libpoly2tri
              HINTS $ENV{LIB_DIR}/lib)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Poly2Tri DEFAULT_MSG
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(Poly2Tri DEFAULT_MSG
                                   Poly2Tri_LIBRARY Poly2Tri_INCLUDE_DIR)
 
-MARK_AS_ADVANCED( Poly2Tri_INCLUDE_DIR Poly2Tri_LIBRARY )
+mark_as_advanced( Poly2Tri_INCLUDE_DIR Poly2Tri_LIBRARY )
