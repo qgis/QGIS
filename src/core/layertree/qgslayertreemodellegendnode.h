762ee09e81d5bdbc02f8b33f9adb84f497b3db8d
Nyall Dawson <nyall.dawson@gmail.com>
2020-12-03 11:53:11 +1000

    Spelling, sip

--- src/core/layertree/qgslayertreemodellegendnode.h
+++ src/core/layertree/qgslayertreemodellegendnode.h
@@ -597,7 +597,7 @@ class CORE_EXPORT QgsRasterSymbolLegendNode : public QgsLayerTreeModelLegendNode
      * \param color color
      * \param label label
      * \param parent attach a parent QObject to the legend node.
-     * \param isCheckable set to TRUE to enable the check box for the node (since QGIS 3.18)
+     * \param isCheckable set to TRUE to enable the checkbox for the node (since QGIS 3.18)
      * \param ruleKey optional identifier to allow a unique ID to be assigned to the node by a renderer (since QGIS 3.18)
      */
     QgsRasterSymbolLegendNode( QgsLayerTreeLayer *nodeLayer, const QColor &color, const QString &label, QObject *parent SIP_TRANSFERTHIS = nullptr, bool isCheckable = false, const QString &ruleKey = QString() );
