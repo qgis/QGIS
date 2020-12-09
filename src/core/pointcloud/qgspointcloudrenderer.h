767d8a3835b4d5c3c1dcbcfdced20bd54a6a4cdf
Nyall Dawson <nyall.dawson@gmail.com>
2020-12-09 11:21:51 +1000

    Drop default 2d max error even further (0.3 now)

--- src/core/pointcloud/qgspointcloudrenderer.h
+++ src/core/pointcloud/qgspointcloudrenderer.h
@@ -511,7 +511,7 @@ class CORE_EXPORT QgsPointCloudRenderer
     QThread *mThread = nullptr;
 #endif
 
-    double mMaximumScreenError = 0.4;
+    double mMaximumScreenError = 0.3;
     QgsUnitTypes::RenderUnit mMaximumScreenErrorUnit = QgsUnitTypes::RenderMillimeters;
 
     double mPointSize = 1;
