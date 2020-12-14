578813ac78de84bedf91a8cf2b57a3966ebe5d63
Nyall Dawson <nyall.dawson@gmail.com>
2020-12-14 11:08:11 +1000

    Geocoder algorithms can be run in in-place mode too

--- src/analysis/processing/qgsalgorithmbatchgeocode.cpp
+++ src/analysis/processing/qgsalgorithmbatchgeocode.cpp
@@ -19,6 +19,7 @@
 #include "qgsgeocoder.h"
 #include "qgsgeocoderresult.h"
 #include "qgsgeocodercontext.h"
+#include "qgsvectorlayer.h"
 
 QgsBatchGeocodeAlgorithm::QgsBatchGeocodeAlgorithm( QgsGeocoderInterface *geocoder )
   : QgsProcessingFeatureBasedAlgorithm()
@@ -42,14 +43,32 @@ QString QgsBatchGeocodeAlgorithm::groupId() const
   return QStringLiteral( "vectorgeneral" );
 }
 
-void QgsBatchGeocodeAlgorithm::initParameters( const QVariantMap & )
+void QgsBatchGeocodeAlgorithm::initParameters( const QVariantMap &configuration )
 {
+  mIsInPlace = configuration.value( QStringLiteral( "IN_PLACE" ) ).toBool();
+
   addParameter( new QgsProcessingParameterField( QStringLiteral( "FIELD" ), QObject::tr( "Address field" ), QVariant(), QStringLiteral( "INPUT" ), QgsProcessingParameterField::String ) );
+
+  if ( mIsInPlace )
+  {
+    const QgsFields newFields = mGeocoder->appendedFields();
+    for ( const QgsField &newField : newFields )
+      addParameter( new QgsProcessingParameterField( newField.name(), QObject::tr( "%1 field" ).arg( newField.name() ), newField.name(), QStringLiteral( "INPUT" ), QgsProcessingParameterField::Any, false, true ) );
+  }
 }
 
 QList<int> QgsBatchGeocodeAlgorithm::inputLayerTypes() const
 {
   return QList<int>() << QgsProcessing::TypeVector;
+}
+
+bool QgsBatchGeocodeAlgorithm::supportInPlaceEdit( const QgsMapLayer *layer ) const
+{
+  if ( const QgsVectorLayer *vl = qobject_cast< const QgsVectorLayer * >( layer ) )
+  {
+    return vl->geometryType() == QgsWkbTypes::PointGeometry;
+  }
+  return false;
 }
 
 QString QgsBatchGeocodeAlgorithm::outputName() const
@@ -60,6 +79,14 @@ QString QgsBatchGeocodeAlgorithm::outputName() const
 bool QgsBatchGeocodeAlgorithm::prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback * )
 {
   mAddressField = parameterAsString( parameters, QStringLiteral( "FIELD" ), context );
+
+  if ( mIsInPlace )
+  {
+    const QgsFields newFields = mGeocoder->appendedFields();
+    for ( const QgsField &newField : newFields )
+      mInPlaceFieldMap.insert( newField.name(), parameterAsString( parameters, newField.name(), context ) );
+  }
+
   return true;
 }
 
@@ -76,12 +103,18 @@ QgsCoordinateReferenceSystem QgsBatchGeocodeAlgorithm::outputCrs( const QgsCoord
 
 QgsFields QgsBatchGeocodeAlgorithm::outputFields( const QgsFields &inputFields ) const
 {
-  // append any additional fields created by the geocoder
-  QgsFields res = inputFields;
-  const QgsFields newFields = mGeocoder->appendedFields();
-  mAdditionalFields = newFields.names();
-  res.extend( newFields );
-  return res;
+  if ( !mIsInPlace )
+  {
+    // append any additional fields created by the geocoder
+    const QgsFields newFields = mGeocoder->appendedFields();
+    mAdditionalFields = newFields.names();
+
+    return QgsProcessingUtils::combineFields( inputFields, newFields );
+  }
+  else
+  {
+    return inputFields;
+  }
 }
 
 QgsFeatureList QgsBatchGeocodeAlgorithm::processFeature( const QgsFeature &feature, QgsProcessingContext &context, QgsProcessingFeedback *feedback )
@@ -125,11 +158,24 @@ QgsFeatureList QgsBatchGeocodeAlgorithm::processFeature( const QgsFeature &featu
   }
 
   const QVariantMap additionalAttributes = results.at( 0 ).additionalAttributes();
-  for ( const QString &additionalField : mAdditionalFields )
+  if ( !mIsInPlace )
   {
-    attr.append( additionalAttributes.value( additionalField ) );
+    for ( const QString &additionalField : mAdditionalFields )
+    {
+      attr.append( additionalAttributes.value( additionalField ) );
+    }
+    f.setAttributes( attr );
   }
-  f.setAttributes( attr );
+  else
+  {
+    for ( auto it = mInPlaceFieldMap.constBegin(); it != mInPlaceFieldMap.constEnd(); ++it )
+    {
+      if ( !it.value().isEmpty() )
+      {
+        f.setAttribute( it.value(), additionalAttributes.value( it.key() ) );
+      }
+    }
+  }
 
   QgsCoordinateTransform transform = QgsCoordinateTransform( results.at( 0 ).crs(), mOutputCrs, context.transformContext() );
   QgsGeometry g = results.at( 0 ).geometry();
