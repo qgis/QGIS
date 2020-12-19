015c6588ee50a6c3aec798bbceb8acae608b5f1e
Mathieu Pellerin <nirvn.asia@gmail.com>
2020-12-19 18:19:08 +0700

    [wms] When decoding uri strings, insure local files are mapped to a 'path' key

--- src/providers/wms/qgswmsprovider.cpp
+++ src/providers/wms/qgswmsprovider.cpp
@@ -4640,7 +4640,22 @@ QVariantMap QgsWmsProviderMetadata::decodeUri( const QString &uri )
   QVariantMap decoded;
   for ( const auto &item : constItems )
   {
-    decoded[ item.first ] = item.second;
+    if ( item.first == QStringLiteral( "url" ) )
+    {
+      const QUrl url( item.second );
+      if ( url.isLocalFile() )
+      {
+        decoded[ QStringLiteral( "path" ) ] = url.toLocalFile();
+      }
+      else
+      {
+        decoded[ item.first ] = item.second;
+      }
+    }
+    else
+    {
+      decoded[ item.first ] = item.second;
+    }
   }
   return decoded;
 }
@@ -4651,7 +4666,14 @@ QString QgsWmsProviderMetadata::encodeUri( const QVariantMap &parts )
   QList<QPair<QString, QString> > items;
   for ( auto it = parts.constBegin(); it != parts.constEnd(); ++it )
   {
-    items.push_back( {it.key(), it.value().toString() } );
+    if ( it.key() == QStringLiteral( "path" ) )
+    {
+      items.push_back( { QStringLiteral( "url" ), QUrl::fromLocalFile( it.value().toString() ).toString() } );
+    }
+    else
+    {
+      items.push_back( { it.key(), it.value().toString() } );
+    }
   }
   query.setQueryItems( items );
   return query.toString();
