f0e638cf66d6b60ccad0fab5f63fec2563b5e547
Alex <roya0045@users.noreply.github.com>
2020-12-22 18:28:32 -0500

    Update testqgspdalprovider.cpp

--- tests/src/providers/testqgspdalprovider.cpp
+++ tests/src/providers/testqgspdalprovider.cpp
@@ -206,7 +206,7 @@ void TestQgsPdalProvider::testEptGenerationNonASCII()
 {
   QTemporaryDir dir;
   QVERIFY( dir.isValid() );
-  QgsPdalEptGenerationTask task( mTestDataDir + QStringLiteral( "point_clouds/las/cloud_copy.las" ), dir.path() );
+  QgsPdalEptGenerationTask task( mTestDataDir + QStringLiteral( "point_clouds/las/cloud%! _čopy.las" ), dir.path() );
   QVERIFY( task.run() );
   QFileInfo fi( dir.path() + "/ept.json" );
   QVERIFY( fi.exists() );
