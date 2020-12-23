2f7d6defeeb9274320cc3a795a0d5bb1c8a0e878
Luigi Pirelli <luipir@gmail.com>
2020-12-23 15:33:00 +0100

    Added sanity check of links between graphicitems showing error in dialog. Fixes #40724 #39260

--- python/plugins/processing/modeler/ModelerDialog.py
+++ python/plugins/processing/modeler/ModelerDialog.py
@@ -235,8 +235,9 @@ class ModelerDialog(QgsModelDesignerDialog):
             scene.setFlag(QgsModelGraphicsScene.FlagHideComments)
 
         context = createContext()
-        scene.createItems(self.model(), context)
         self.setModelScene(scene)
+        # create items later that setModelScene to setup link to messageBar to the scene
+        scene.createItems(self.model(), context)
 
     def create_widget_context(self):
         """
