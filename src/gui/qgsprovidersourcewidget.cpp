d5c3b4b8a5d15682a20b0e2c0e6428e2e0a3c9c9
Nyall Dawson <nyall.dawson@gmail.com>
2020-12-23 10:09:39 +1000

    Add API to allow creation of widgets for customising a layer's source
    This adds api in the provider gui to allow a provider to create a custom
    widget which exposes options to customise and change the layer's source.
    
    E.g. a file based layer provider could expose a widget for selecting
    a new file path, and an online one could expose widgets for changing
    the host, authentication, etc.

---
+++ src/gui/qgsprovidersourcewidget.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+                             qgsprovidersourcewidget.cpp
+                             -----------------
+    begin                : August 2018
+    copyright            : (C) 2018 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "qgsprovidersourcewidget.h"
+
+QgsProviderSourceWidget::QgsProviderSourceWidget( QWidget *parent )
+  : QWidget( parent )
+{
+
+}
