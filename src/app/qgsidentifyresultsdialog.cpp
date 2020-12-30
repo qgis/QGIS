c0576d88ebe4f154bbdd1874d01b499a60f880cc
Nyall Dawson <nyall.dawson@gmail.com>
2020-12-30 12:48:29 +1000

    Make identify results indentation more hidpi friendly, also lower browser tree idendation

--- src/app/qgsidentifyresultsdialog.cpp
+++ src/app/qgsidentifyresultsdialog.cpp
@@ -57,6 +57,7 @@
 #include "qgsfeatureiterator.h"
 #include "qgsfeaturestore.h"
 #include "qgsgeometry.h"
+#include "qgsguiutils.h"
 #include "qgshighlight.h"
 #include "qgsmaptoolidentifyaction.h"
 #include "qgsidentifyresultsdialog.h"
@@ -367,7 +368,7 @@ QgsIdentifyResultsDialog::QgsIdentifyResultsDialog( QgsMapCanvas *canvas, QWidge
   mActionCopy->setEnabled( false );
   lstResults->setColumnCount( 2 );
   lstResults->sortByColumn( -1, Qt::AscendingOrder );
-  lstResults->setIndentation( 10 );
+  lstResults->setIndentation( QgsGuiUtils::scaleIconSize( 12 ) );
   setColumnText( 0, tr( "Feature" ) );
   setColumnText( 1, tr( "Value" ) );
 
