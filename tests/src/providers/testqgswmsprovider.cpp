be97dc0929d364d6aa9198426ea1bfd16a3a6cf1
Nyall Dawson <nyall.dawson@gmail.com>
2021-01-09 09:24:09 +1000

    Automatically populate appropriate layer metadata when adding OpenStreetMap tile layers
    (cherry picked from commit ed4bb5069b1991e296265ac248a985ff242bb8a2)

--- tests/src/providers/testqgswmsprovider.cpp
+++ tests/src/providers/testqgswmsprovider.cpp
@@ -179,6 +179,20 @@ class TestQgsWmsProvider: public QObject
       QCOMPARE( encodedUri, uriString );
     }
 
+    void testOsmMetadata()
+    {
+      // test that we auto-populate openstreetmap tile metadata
+
+      // don't actually hit the osm server -- the url below uses "file" instead of "http"!
+      QgsWmsProvider provider( QStringLiteral( "type=xyz&url=file://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png&zmax=19&zmin=0" ), QgsDataProvider::ProviderOptions(), mCapabilities );
+      QCOMPARE( provider.layerMetadata().identifier(), QStringLiteral( "OpenStreetMap tiles" ) );
+      QCOMPARE( provider.layerMetadata().title(), QStringLiteral( "OpenStreetMap tiles" ) );
+      QVERIFY( !provider.layerMetadata().abstract().isEmpty() );
+      QCOMPARE( provider.layerMetadata().licenses().at( 0 ), QStringLiteral( "Open Data Commons Open Database License (ODbL)" ) );
+      QCOMPARE( provider.layerMetadata().licenses().at( 1 ), QStringLiteral( "Creative Commons Attribution-ShareAlike (CC-BY-SA)" ) );
+      QVERIFY( provider.layerMetadata().rights().at( 0 ).startsWith( "Base map and data from OpenStreetMap and OpenStreetMap Foundation" ) );
+    }
+
     bool imageCheck( const QString &testType, QgsMapLayer *layer, const QgsRectangle &extent )
     {
       //use the QgsRenderChecker test utility class to
