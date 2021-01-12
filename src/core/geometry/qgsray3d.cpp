6caf1bfe32a5a9a8cb9cd4585da4087544b36f2a
Belgacem <gb_nedjima@esi.dz>
2021-01-12 02:50:14 +0100

    fix code_layout

--- src/core/geometry/qgsray3d.cpp
+++ src/core/geometry/qgsray3d.cpp
@@ -1,3 +1,17 @@
+/***************************************************************************
+  qgsray3d.cpp
+  --------------------------------------
+  Date                 : January 2021
+  Copyright            : (C) 2021 by Belgacem Nedjima
+  Email                : belgacem dot nedjima at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "qgsray3d.h"
 
 #include "qgsrange.h"
--- src/core/geometry/qgsray3d.h
+++ src/core/geometry/qgsray3d.h
@@ -1,3 +1,17 @@
+/***************************************************************************
+  qgsray3d.h
+  --------------------------------------
+  Date                 : January 2021
+  Copyright            : (C) 2021 by Belgacem Nedjima
+  Email                : belgacem dot nedjima at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef QGSRAY3D_H
 #define QGSRAY3D_H
 
