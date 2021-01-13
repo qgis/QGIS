0167d07eaa5d32569e8c3d219fa0b80b5c5a5d5a
Belgacem <gb_nedjima@esi.dz>
2021-01-13 03:06:34 +0100

    fix tests

--- src/core/geometry/qgsray3d.cpp
+++ src/core/geometry/qgsray3d.cpp
@@ -14,8 +14,6 @@
  ***************************************************************************/
 #include "qgsray3d.h"
 
-#include "qgsrange.h"
-
 #include <QtMath>
 
 QgsRay3D::QgsRay3D( const QVector3D &origin, const QVector3D &direction )
@@ -35,56 +33,9 @@ void QgsRay3D::setDirection( const QVector3D direction )
   mDirection = direction.normalized();
 }
 
-bool QgsRay3D::operator==( const QgsRay3D &r )
-{
-  return this->mOrigin == r.origin() && this->mDirection == r.direction();
-}
-
 QVector3D QgsRay3D::projectedPoint( const QVector3D &point ) const
 {
   return mOrigin + QVector3D::dotProduct( point - mOrigin, mDirection ) * mDirection;
-}
-
-bool QgsRay3D::intersectsWith( const QgsBox3d &box ) const
-{
-  double tminX = box.xMinimum() - mOrigin.x(), tmaxX = box.xMaximum() - mOrigin.x();
-  double tminY = box.yMinimum() - mOrigin.y(), tmaxY = box.yMaximum() - mOrigin.y();
-  double tminZ = box.zMinimum() - mOrigin.z(), tmaxZ = box.zMaximum() - mOrigin.z();
-  if ( mDirection.x() < 0 ) std::swap( tminX, tmaxX );
-  if ( mDirection.y() < 0 ) std::swap( tminY, tmaxY );
-  if ( mDirection.z() < 0 ) std::swap( tminZ, tmaxZ );
-  if ( mDirection.x() != 0 )
-  {
-    tminX /= mDirection.x();
-    tmaxX /= mDirection.x();
-  }
-  else
-  {
-    tminX = std::numeric_limits<double>::lowest();
-    tmaxX = std::numeric_limits<double>::max();
-  }
-  if ( mDirection.y() != 0 )
-  {
-    tminY /= mDirection.y();
-    tmaxY /= mDirection.y();
-  }
-  else
-  {
-    tminY = std::numeric_limits<double>::lowest();
-    tmaxY = std::numeric_limits<double>::max();
-  }
-  if ( mDirection.z() != 0 )
-  {
-    tminZ /= mDirection.z();
-    tmaxZ /= mDirection.z();
-  }
-  else
-  {
-    tminZ = std::numeric_limits<double>::lowest();
-    tmaxZ = std::numeric_limits<double>::max();
-  }
-  QgsDoubleRange tRange( std::max( std::max( tminX, tminY ), tminZ ), std::min( std::min( tmaxX, tmaxY ), tmaxZ ) );
-  return !tRange.isEmpty();
 }
 
 bool QgsRay3D::isInFront( const QVector3D &point ) const
