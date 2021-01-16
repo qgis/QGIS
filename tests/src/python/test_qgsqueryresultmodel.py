0a5bef3a0b1c2eac9467756f030b51ce3518b87e
Alessandro Pasotti <elpaso@itopen.it>
2021-01-16 08:36:17 +0100

    Fix test

--- tests/src/python/test_qgsqueryresultmodel.py
+++ tests/src/python/test_qgsqueryresultmodel.py
@@ -45,7 +45,6 @@ class TestPyQgsQgsQueryResultModel(unittest.TestCase):
         # Prepare data for threaded test
         cls._deleteBigData()
 
-        return
         md = QgsProviderRegistry.instance().providerMetadata('postgres')
         conn = md.createConnection(cls.uri, {})
         conn.executeSql('SELECT * INTO qgis_test.random_big_data FROM generate_series(1,%s) AS id, md5(random()::text) AS descr' % cls.NUM_RECORDS)
