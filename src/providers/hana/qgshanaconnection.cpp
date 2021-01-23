23bfee444b4d311620a9a7d3fdbbed494f9a958d
Stefan Uhrig <stefan.uhrig@sap.com>
2021-01-23 12:54:22 +0100

    Update src/providers/hana/qgshanaconnection.cpp
    Co-authored-by: Matthias Kuhn <matthias@opengis.ch>

--- src/providers/hana/qgshanaconnection.cpp
+++ src/providers/hana/qgshanaconnection.cpp
@@ -762,7 +762,7 @@ QgsHanaResultSetRef QgsHanaConnection::getColumns( const QString &schemaName, co
 
 bool  QgsHanaConnection::isTable( const QString &schemaName, const QString &tableName )
 {
-  QString sql = "SELECT COUNT(*) FROM SYS.TABLES WHERE SCHEMA_NAME = ? AND TABLE_NAME = ?";
+  QString sql = QStringLiteral( "SELECT COUNT(*) FROM SYS.TABLES WHERE SCHEMA_NAME = ? AND TABLE_NAME = ?" );
   return executeCountQuery( sql, {schemaName, tableName } ) == 1;
 }
 
