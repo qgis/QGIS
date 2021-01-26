9d18fe14f7e96481193b5ff2e35199fdd0a80ef8
Alessandro Pasotti <elpaso@itopen.it>
2021-01-26 09:58:53 +0100

    Fix basicstatisticsforfields tests

--- src/core/qgsdatetimestatisticalsummary.cpp
+++ src/core/qgsdatetimestatisticalsummary.cpp
@@ -58,36 +58,30 @@ void QgsDateTimeStatisticalSummary::calculate( const QVariantList &values )
 
 void QgsDateTimeStatisticalSummary::addValue( const QVariant &value )
 {
-  if ( value.isNull() )
+
+  if ( value.type() == QVariant::DateTime )
+  {
+    testDateTime( value.toDateTime(), value.isNull() );
+  }
+  else if ( value.type() == QVariant::Date )
+  {
+    QDate date = value.toDate();
+    testDateTime( date.isValid() ? QDateTime( date, QTime( 0, 0, 0 ) )
+                  : QDateTime(), value.isNull() );
+  }
+  else if ( value.type() == QVariant::Time )
+  {
+    mIsTimes = true;
+    QTime time = value.toTime();
+    testDateTime( time.isValid() ? QDateTime( QDate::fromJulianDay( 0 ), time )
+                  : QDateTime(), value.isNull() );
+  }
+  else //not a date
   {
     mCountMissing++;
     mCount++;
   }
-  else
-  {
-    if ( value.type() == QVariant::DateTime )
-    {
-      testDateTime( value.toDateTime() );
-    }
-    else if ( value.type() == QVariant::Date )
-    {
-      QDate date = value.toDate();
-      testDateTime( date.isValid() ? QDateTime( date, QTime( 0, 0, 0 ) )
-                    : QDateTime() );
-    }
-    else if ( value.type() == QVariant::Time )
-    {
-      mIsTimes = true;
-      QTime time = value.toTime();
-      testDateTime( time.isValid() ? QDateTime( QDate::fromJulianDay( 0 ), time )
-                    : QDateTime() );
-    }
-    else //not a date
-    {
-      mCountMissing++;
-      mCount++;
-    }
-  }
+
   // QTime?
 }
 
@@ -97,11 +91,11 @@ void QgsDateTimeStatisticalSummary::finalize()
   //if statistics are implemented which require a post-calculation step
 }
 
-void QgsDateTimeStatisticalSummary::testDateTime( const QDateTime &dateTime )
+void QgsDateTimeStatisticalSummary::testDateTime( const QDateTime &dateTime, bool isNull )
 {
   mCount++;
 
-  if ( !dateTime.isValid() )
+  if ( !dateTime.isValid() || isNull )
     mCountMissing++;
 
   if ( mStatistics & CountDistinct )
