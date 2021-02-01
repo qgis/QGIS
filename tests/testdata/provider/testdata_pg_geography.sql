b936829b3e4ba8e952597b9483f950ed5a798385
Even Rouault <even.rouault@spatialys.com>
2021-02-01 16:25:35 +0100

    [Postgres] Use spatial index for tables using geography type (fixes #39453)

---
+++ tests/testdata/provider/testdata_pg_geography.sql
@@ -0,0 +1,14 @@
+
+DROP TABLE IF EXISTS qgis_test.testgeog CASCADE;
+
+CREATE TABLE qgis_test.testgeog
+(
+  pk SERIAL NOT NULL PRIMARY KEY,
+  geog GEOGRAPHY
+);
+
+INSERT INTO qgis_test.testgeog(geog) VALUES ('POINT(40 0)'::geography);
+INSERT INTO qgis_test.testgeog(geog) VALUES ('POINT(40 60)'::geography);
+INSERT INTO qgis_test.testgeog(geog) VALUES ('POINT(40 -60)'::geography);
+
+CREATE INDEX testgeog_geog_idx ON qgis_test.testgeog USING GIST ( geog );
