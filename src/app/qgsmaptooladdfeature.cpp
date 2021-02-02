f0c37f96a2ec935d0e9de72f8202d476455a02b0
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2021-02-02 08:35:42 +0100

    catch exception

--- src/app/qgsmaptooladdfeature.cpp
+++ src/app/qgsmaptooladdfeature.cpp
@@ -100,8 +100,17 @@ void QgsMapToolAddFeature::digitized( const QgsFeature &f )
         {
           // transform geometry to vlayer crs and add topological point
           QgsGeometry geom( f.geometry() );
-          geom.transform( QgsCoordinateTransform( vlayer->crs(), sm.at( i ).layer()->crs(), sm.at( i ).layer()->transformContext() ) );
-          sm.at( i ).layer()->addTopologicalPoints( geom.vertexAt( i ) );
+          try
+          {
+            geom.transform( QgsCoordinateTransform( vlayer->crs(), sm.at( i ).layer()->crs(), sm.at( i ).layer()->transformContext() ) );
+            sm.at( i ).layer()->addTopologicalPoints( geom.vertexAt( i ) );
+          }
+          catch ( QgsCsException &cse )
+          {
+            Q_UNUSED( cse )
+            QgsDebugMsg( QStringLiteral( "transformation to layer coordinate failed" ) );
+          }
+
         }
       }
       vlayer->addTopologicalPoints( f.geometry() );
