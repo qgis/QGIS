618734a34bfc1aa9a84cf679289441c0ec8c9669
Julien Cabieces <julien.cabieces@oslandia.com>
2021-02-04 12:11:15 +0100

    Fixes #40720 : Refresh snapping index on vector data provider notify

--- src/core/qgsmaplayer.cpp
+++ src/core/qgsmaplayer.cpp
@@ -1930,18 +1930,21 @@ void QgsMapLayer::setRefreshOnNotifyEnabled( bool enabled )
   if ( enabled && !isRefreshOnNotifyEnabled() )
   {
     lDataProvider->setListening( enabled );
-    connect( lDataProvider, &QgsVectorDataProvider::notify, this, &QgsMapLayer::onNotifiedTriggerRepaint );
+    connect( lDataProvider, &QgsVectorDataProvider::notify, this, &QgsMapLayer::onNotified );
   }
   else if ( !enabled && isRefreshOnNotifyEnabled() )
   {
     // we don't want to disable provider listening because someone else could need it (e.g. actions)
-    disconnect( lDataProvider, &QgsVectorDataProvider::notify, this, &QgsMapLayer::onNotifiedTriggerRepaint );
+    disconnect( lDataProvider, &QgsVectorDataProvider::notify, this, &QgsMapLayer::onNotified );
   }
   mIsRefreshOnNofifyEnabled = enabled;
 }
 
-void QgsMapLayer::onNotifiedTriggerRepaint( const QString &message )
+void QgsMapLayer::onNotified( const QString &message )
 {
   if ( refreshOnNotifyMessage().isEmpty() || refreshOnNotifyMessage() == message )
+  {
     triggerRepaint();
+    emit dataChanged();
+  }
 }
