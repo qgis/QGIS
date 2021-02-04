a89559879743242d2b7138c2d3d73179c380af97
Nyall Dawson <nyall.dawson@gmail.com>
2021-02-04 14:31:30 +1000

    Fix incorrect feature request filter expression generated when rule based renderer uses a NOT IN rule
    Followup 40939

--- src/core/expression/qgsexpression.cpp
+++ src/core/expression/qgsexpression.cpp
@@ -1146,6 +1146,9 @@ bool QgsExpression::attemptReduceToInClause( const QStringList &expressions, QSt
 
       if ( const QgsExpressionNodeInOperator *inOp = dynamic_cast<const QgsExpressionNodeInOperator *>( e.rootNode() ) )
       {
+        if ( inOp->isNotIn() )
+          return false;
+
         const QgsExpressionNodeColumnRef *columnRef = dynamic_cast<const QgsExpressionNodeColumnRef *>( inOp->node() );
         if ( !columnRef )
           return false;
