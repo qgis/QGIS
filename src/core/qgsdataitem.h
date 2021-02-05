7de119d625b6f7e8dbe54f05585462f36a52364a
Alessandro Pasotti <elpaso@itopen.it>
2021-02-05 12:15:20 +0100

    Add DB connections factory to data items
    (cherry picked from commit 5733970f84f0d4f670b0a26046d4c020c3065337)

--- src/core/qgsdataitem.h
+++ src/core/qgsdataitem.h
@@ -416,6 +416,17 @@ class CORE_EXPORT QgsDataItem : public QObject
     //! Move object and all its descendants to thread
     void moveToThread( QThread *targetThread );
 
+    /**
+     * For data items that represent a DB connection or one of its children,
+     * this method returns a connection.
+     * All other data items will return NULL.
+     *
+     * Ownership of the returned objects is transferred to the caller.
+     *
+     * \since QGIS 3.16
+     */
+    virtual QgsAbstractDatabaseProviderConnection *databaseConnection() const SIP_FACTORY;
+
   protected:
     virtual void populate( const QVector<QgsDataItem *> &children );
 
@@ -640,6 +651,7 @@ class CORE_EXPORT QgsLayerItem : public QgsDataItem
 
     //! \returns the layer name
     virtual QString layerName() const { return name(); }
+
 };
 
 
@@ -681,6 +693,8 @@ class CORE_EXPORT QgsDataCollectionItem : public QgsDataItem
      */
     static QIcon iconDataCollection();
 
+    QgsAbstractDatabaseProviderConnection *databaseConnection() const override;
+
   protected:
 
     /**
@@ -694,6 +708,7 @@ class CORE_EXPORT QgsDataCollectionItem : public QgsDataItem
      * \since QGIS 3.4
      */
     static QIcon homeDirIcon();
+
 };
 
 
@@ -722,6 +737,7 @@ class CORE_EXPORT QgsDatabaseSchemaItem : public QgsDataCollectionItem
 
     ~QgsDatabaseSchemaItem() override;
 
+    QgsAbstractDatabaseProviderConnection *databaseConnection() const override;
 
     /**
      * Returns the standard browser data collection icon.
