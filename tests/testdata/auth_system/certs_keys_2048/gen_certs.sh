721c3c57d751caf4fa4f90ee14065df2e0f04b03
Nyall Dawson <nyall.dawson@gmail.com>
2021-02-08 16:30:50 +1000

    Add header to gen_certs.sh

--- tests/testdata/auth_system/certs_keys_2048/gen_certs.sh
+++ tests/testdata/auth_system/certs_keys_2048/gen_certs.sh
@@ -1,3 +1,18 @@
+#!/usr/bin/env bash
+###########################################################################
+#    gen_certs.sh
+#    ---------------------
+#    Date                 : February 2021
+#    Copyright            : (C) 2021 by Alessandro Pasotti
+#    Email                : elpaso@itopen.it
+###########################################################################
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+###########################################################################
 
 rm -rf ./*.pem ./*.cer ./*.key ./*.crt ./*.csr ./*.srl
 # Generate the CA/root private key
