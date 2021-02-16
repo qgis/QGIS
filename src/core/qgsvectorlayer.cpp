ad984ab8618054dc222d5b0611a4da0f7bada6b9
Nyall Dawson <nyall.dawson@gmail.com>
2021-02-16 10:23:03 +1000

    Fix logic error in QgsVectorLayer::changeAttributeValues which results in an incorrect failure status when editing a layer which contains joins
    This causes the attribute form to incorrectly report that changes
    cannot be saved whenever attempting to edit a layer which contains
    a join and a mix of joined/not-joined attributes are edited.
    
    (cherry picked from commit 01889d96b22757028e62c156a945ae8b86e69a68)

--- src/core/qgsvectorlayer.cpp
+++ src/core/qgsvectorlayer.cpp
@@ -3043,7 +3043,7 @@ bool QgsVectorLayer::changeAttributeValues( QgsFeatureId fid, const QgsAttribute
 
   if ( ! newValuesNotJoin.isEmpty() && mEditBuffer && mDataProvider )
   {
-    result &= mEditBuffer->changeAttributeValues( fid, newValues, oldValues );
+    result &= mEditBuffer->changeAttributeValues( fid, newValuesNotJoin, oldValues );
   }
 
   if ( result && !skipDefaultValues && !mDefaultValueOnUpdateFields.isEmpty() )
