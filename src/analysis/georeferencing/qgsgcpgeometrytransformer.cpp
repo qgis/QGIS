fd7149ac97a67447113400623af2199cecfa72f0
Nyall Dawson <nyall.dawson@gmail.com>
2021-02-17 12:55:01 +1000

    Create class QgsGcpGeometryTransformer, which allows transformation of geometry objects via a ground control points based transformation

---
+++ src/analysis/georeferencing/qgsgcpgeometrytransformer.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+                             qgsgcpgeometrytransformer.cpp
+                             ----------------------
+    begin                : February 2021
+    copyright            : (C) 2021 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsgcpgeometrytransformer.h"
+
+
+QgsGcpGeometryTransformer::QgsGcpGeometryTransformer( QgsGcpTransformerInterface *gcpTransformer )
+  : mGcpTransformer( gcpTransformer )
+{
+
+}
+
+QgsGcpGeometryTransformer::QgsGcpGeometryTransformer( QgsGcpTransformerInterface::TransformMethod method, const QVector<QgsPointXY> &sourceCoordinates, const QVector<QgsPointXY> &destinationCoordinates )
+  : mGcpTransformer( QgsGcpTransformerInterface::createFromParameters( method, sourceCoordinates, destinationCoordinates ) )
+{
+
+}
+
+QgsGcpGeometryTransformer::~QgsGcpGeometryTransformer() = default;
+
+bool QgsGcpGeometryTransformer::transformPoint( double &x, double &y, double &, double & )
+{
+  return mGcpTransformer->transform( x, y );
+}
+
+QgsGcpTransformerInterface *QgsGcpGeometryTransformer::gcpTransformer() const
+{
+  return mGcpTransformer.get();
+}
+
+void QgsGcpGeometryTransformer::setGcpTransformer( QgsGcpTransformerInterface *gcpTransformer )
+{
+  mGcpTransformer.reset( gcpTransformer );
+}
