22bad6e90fbbdee56e26495e633a1febe67ed533
Nyall Dawson <nyall.dawson@gmail.com>
2021-03-01 14:06:08 +1000

    Revert "catch exception"
    This reverts commit 691a0b45a72069094561e777e846c42357ebaf9e.

--- src/app/qgsmaptooladdfeature.cpp
+++ src/app/qgsmaptooladdfeature.cpp
@@ -100,17 +100,8 @@ void QgsMapToolAddFeature::digitized( const QgsFeature &f )
         {
           // transform geometry to vlayer crs and add topological point
           QgsGeometry geom( f.geometry() );
-          try
-          {
-            geom.transform( QgsCoordinateTransform( vlayer->crs(), sm.at( i ).layer()->crs(), sm.at( i ).layer()->transformContext() ) );
-            sm.at( i ).layer()->addTopologicalPoints( geom.vertexAt( i ) );
-          }
-          catch ( QgsCsException &cse )
-          {
-            Q_UNUSED( cse )
-            QgsDebugMsg( QStringLiteral( "transformation to layer coordinate failed" ) );
-          }
-
+          geom.transform( QgsCoordinateTransform( vlayer->crs(), sm.at( i ).layer()->crs(), sm.at( i ).layer()->transformContext() ) );
+          sm.at( i ).layer()->addTopologicalPoints( geom.vertexAt( i ) );
         }
       }
       vlayer->addTopologicalPoints( f.geometry() );
