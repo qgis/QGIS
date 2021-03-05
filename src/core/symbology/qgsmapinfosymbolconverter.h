1a4d7b02889406f48a5290bf5be547fc19f61667
Nyall Dawson <nyall.dawson@gmail.com>
2021-03-05 15:05:24 +1000

    Improved conversion of MapInfo line symbols
    Many MapInfo line symbols don't translate well into OGR styles, so
    in order to improve the quality of the conversion this commit introduces
    a new QgsMapInfoSymbolConverter class which contains utility functions
    for converting MapInfo symbols by ID to their QGIS equivalent.
    
    Implemented for line symbols only for now.

---
+++ src/core/symbology/qgsmapinfosymbolconverter.h
@@ -0,0 +1,78 @@
+/***************************************************************************
+  qgsmapinfosymbolconverter.h
+  --------------------------------------
+  Date                 : March 2021
+  Copyright            : (C) 2021 by Nyall Dawson
+  Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSMAPINFOSYMBOLCONVERTER_H
+#define QGSMAPINFOSYMBOLCONVERTER_H
+
+#include "qgis_core.h"
+#include "qgis_sip.h"
+#include "qgsunittypes.h"
+#include <QStringList>
+
+class QgsLineSymbol;
+
+/**
+ * Context for a MapInfo symbol conversion operation.
+ * \warning This is private API only, and may change in future QGIS versions
+ * \ingroup core
+ * \since QGIS 3.20
+ */
+class CORE_EXPORT QgsMapInfoSymbolConversionContext
+{
+  public:
+
+    /**
+     * Pushes a \a warning message generated during the conversion.
+     */
+    void pushWarning( const QString &warning );
+
+    /**
+     * Returns a list of warning messages generated during the conversion.
+     */
+    QStringList warnings() const { return mWarnings; }
+
+    /**
+     * Clears the list of warning messages.
+     */
+    void clearWarnings() { mWarnings.clear(); }
+
+  private:
+
+    QStringList mWarnings;
+    QgsUnitTypes::RenderUnit mTargetUnit = QgsUnitTypes::RenderPixels;
+
+    double mSizeConversionFactor = 1.0;
+};
+
+/**
+ * \ingroup core
+ * \brief Handles conversion of MapInfo symbols to QGIS symbology.
+ *
+ * \since QGIS 3.20
+ */
+class CORE_EXPORT QgsMapInfoSymbolConverter
+{
+  public:
+
+    /**
+     * Converts the MapInfo line symbol with the specified \a identifier to a QgsLineSymbol.
+     *
+     * The caller takes ownership of the returned symbol.
+     */
+    static QgsLineSymbol *convertLineSymbol( int identifier, QgsMapInfoSymbolConversionContext &context, const QColor &foreColor, double size, QgsUnitTypes::RenderUnit sizeUnit, bool interleaved = false ) SIP_FACTORY;
+
+};
+
+#endif // QGSMAPINFOSYMBOLCONVERTER_H
