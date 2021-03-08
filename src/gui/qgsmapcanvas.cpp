f4028405544b4a466d3b57c67fd5b1a381cf578d
Julien Cabieces <julien.cabieces@oslandia.com>
2021-03-08 15:19:21 +0100

    Update widget state when scale locked state is changed from API

--- src/gui/qgsmapcanvas.cpp
+++ src/gui/qgsmapcanvas.cpp
@@ -2123,7 +2123,11 @@ void QgsMapCanvas::zoomWithCenter( int x, int y, bool zoomIn )
 
 void QgsMapCanvas::setScaleLocked( bool isLocked )
 {
-  mScaleLocked = isLocked;
+  if ( mScaleLocked != isLocked )
+  {
+    mScaleLocked = isLocked;
+    emit scaleLockChanged( mScaleLocked );
+  }
 }
 
 void QgsMapCanvas::mouseMoveEvent( QMouseEvent *e )
