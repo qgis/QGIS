4c52f404adf2a9823412c8c0433ef4c3ad97ad0d
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-03-18 18:38:31 +0100

    fix spelling

--- src/core/qgsfeaturepickermodelbase.h
+++ src/core/qgsfeaturepickermodelbase.h
@@ -181,7 +181,7 @@ class CORE_EXPORT QgsFeaturePickerModelBase : public QAbstractItemModel SIP_ABST
     /**
      * Emitted when the current feature in the model has changed
      * This emitted both when the extra value changes and when the extra value status changes.
-     * It allows to be aware when the feature is fetched after the extra value has been set.
+     * It allows being notified when the feature is fetched after the extra value has been set.
      * \since QGIS 3.16.5
      */
     void currentFeatureChanged();
