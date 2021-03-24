5b62c63d8154d1b32fa635f96fd172c243e45c7c
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-03-24 01:53:04 +0100

    add license

--- src/core/pointcloud/qgspointcloudblockhandle.cpp
+++ src/core/pointcloud/qgspointcloudblockhandle.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgspointcloudblockhandle.cpp
+                         --------------------
+    begin                : March 2021
+    copyright            : (C) 2021 by Belgacem Nedjima
+    email                : belgacem dot nedjima at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgspointcloudblockhandle.h"
 
 #include "qgstiledownloadmanager.h"
--- src/core/pointcloud/qgsremoteeptpointcloudindex.cpp
+++ src/core/pointcloud/qgsremoteeptpointcloudindex.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgsremoteeptpointcloudindex.cpp
+                         --------------------
+    begin                : March 2021
+    copyright            : (C) 2021 by Belgacem Nedjima
+    email                : belgacem dot nedjima at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgsremoteeptpointcloudindex.h"
 
 #include <QFile>
