1dfccaca0029944c6852422863734c3cf75a27b4
nicogodet <nicolas.godet@outlook.fr>
2021-03-24 16:56:37 +0100

    Fix indentation consistence

--- python/plugins/processing/algs/gdal/gdalcalc.py
+++ python/plugins/processing/algs/gdal/gdalcalc.py
@@ -87,10 +87,11 @@ class gdalcalc(GdalAlgorithm):
                 self.tr('Input layer C'),
                 optional=True))
         self.addParameter(
-            QgsProcessingParameterBand(self.BAND_C,
-                                       self.tr('Number of raster band for C'),
-                                       parentLayerParameterName=self.INPUT_C,
-                                       optional=True))
+            QgsProcessingParameterBand(
+                self.BAND_C,
+                self.tr('Number of raster band for C'),
+                parentLayerParameterName=self.INPUT_C,
+                optional=True))
         self.addParameter(
             QgsProcessingParameterRasterLayer(
                 self.INPUT_D,
