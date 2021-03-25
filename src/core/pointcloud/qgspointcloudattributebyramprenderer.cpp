ebe9ce48f8b7166e8e75351ab655e0eec01dd201
Nyall Dawson <nyall.dawson@gmail.com>
2021-03-26 09:02:35 +1000

    Fix crash when changing point cloud to Attribute by Ramp renderer
    Fixes #42486

--- src/core/pointcloud/qgspointcloudattributebyramprenderer.cpp
+++ src/core/pointcloud/qgspointcloudattributebyramprenderer.cpp
@@ -181,7 +181,7 @@ QList<QgsLayerTreeModelLegendNode *> QgsPointCloudAttributeByRampRenderer::creat
     case QgsColorRampShader::Interpolated:
       // for interpolated shaders we use a ramp legend node unless the settings flag
       // to use the continuous legend is not set, in that case we fall through
-      if ( ! mColorRampShader.legendSettings() || mColorRampShader.legendSettings()->useContinuousLegend() )
+      if ( mColorRampShader.sourceColorRamp() && ( ! mColorRampShader.legendSettings() || mColorRampShader.legendSettings()->useContinuousLegend() ) )
       {
         res << new QgsColorRampLegendNode( nodeLayer, mColorRampShader.sourceColorRamp()->clone(),
                                            mColorRampShader.legendSettings() ? *mColorRampShader.legendSettings() : QgsColorRampLegendNodeSettings(),
