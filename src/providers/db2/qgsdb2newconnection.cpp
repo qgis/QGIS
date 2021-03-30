f3ada045c10343a803435835643c4631850c286e
Nyall Dawson <nyall.dawson@gmail.com>
2021-03-31 08:55:02 +1000

    Some provider QRegularExpression ports

--- src/providers/db2/qgsdb2newconnection.cpp
+++ src/providers/db2/qgsdb2newconnection.cpp
@@ -19,7 +19,8 @@
 #include <QMessageBox>
 #include <QSqlDatabase>
 #include <QSqlError>
-#include <QRegExpValidator>
+#include <QRegularExpression>
+#include <QRegularExpressionValidator>
 
 #include "qgssettings.h"
 #include "qgslogger.h"
@@ -80,7 +81,7 @@ QgsDb2NewConnection::QgsDb2NewConnection( QWidget *parent, const QString &connNa
 
     txtName->setText( connName );
   }
-  txtName->setValidator( new QRegExpValidator( QRegExp( "[^\\/]+" ), txtName ) );
+  txtName->setValidator( new QRegularExpressionValidator( QRegularExpression( QStringLiteral( "[^\\/]+" ) ), txtName ) );
 }
 
 //! Autoconnected SLOTS
