sbrunner/Quantum-GIS

[Server][Feature][needs-docs] Make service URL configurable
The goals are:
* To have better result in the GetCapabilities URL without having to fill it in the Project configuration
* Be able to be set by an integrator with environment variable or by providing headers from a proxy

The server resolution will use the following order:
 - Value defined in the project per service.
 - The ``<service>_SERVICE_URL`` environment variable.
 - The ``SERVICE_URL`` environment variable.
 - The custom ``X-Qgis-<service>-Servcie-Url`` header.
 - The custom ``X-Qgis-Service-Url`` header.
 - Build form the standard ``Forwarded`` header.
 - Build form the pseudo standard ``X-Forwarded-Host`` and ``X-Forwarded-Proto`` headers.
 - Build form the standard ``Host`` header and the server protocol.
 - Build form the server name and the server protocol.

src/server/services/wms/qgswmsgetcapabilities.h
@@ -62,7 +62,7 @@ namespace QgsWms
    * Create Service element for get capabilities document
    */
   QDomElement getServiceElement( QDomDocument &doc, const QgsProject *project, const QString &version,
-                                 const QgsServerRequest &request );
+                                 const QgsServerRequest &request, const QgsServerSettings *serverSettings );
 
   /**
    * Output GetCapabilities response

qgis/QGIS

src/server/services/wms/qgswmsgetcapabilities.h
@@ -66,7 +66,7 @@ namespace QgsWms
    * Create Service element for get capabilities document
    */
   QDomElement getServiceElement( QDomDocument &doc, const QgsProject *project,
-                                 const QgsWmsRequest &request );
+                                 const QgsWmsRequest &request, const QgsServerSettings *serverSettings );
 
   /**
    * Output GetCapabilities response