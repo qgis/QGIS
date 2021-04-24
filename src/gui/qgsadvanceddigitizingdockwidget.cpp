4241cc597cee6ef81f7bd7555c9fce74f032394f
uclaros <uclaros@gmail.com>
2021-04-24 12:00:17 +0300

    Make "Snap to common angles" checkable to define its state

--- src/gui/qgsadvanceddigitizingdockwidget.cpp
+++ src/gui/qgsadvanceddigitizingdockwidget.cpp
@@ -123,6 +123,9 @@ QgsAdvancedDigitizingDockWidget::QgsAdvancedDigitizingDockWidget( QgsMapCanvas *
 
   qobject_cast< QToolButton *>( mToolbar->widgetForAction( mSettingsAction ) )->setPopupMode( QToolButton::InstantPopup );
   mSettingsAction->setMenu( menu );
+  mSettingsAction->setCheckable( true );
+  mSettingsAction->setToolTip( tr( "Snap to common angles" ) );
+  mSettingsAction->setChecked( mCommonAngleConstraint != 0 );
   connect( menu, &QMenu::triggered, this, &QgsAdvancedDigitizingDockWidget::settingsButtonTriggered );
 
   // set tooltips
@@ -334,6 +337,7 @@ void QgsAdvancedDigitizingDockWidget::settingsButtonTriggered( QAction *action )
     ica.key()->setChecked( true );
     mCommonAngleConstraint = ica.value();
     QgsSettings().setValue( QStringLiteral( "/Cad/CommonAngle" ), ica.value() );
+    mSettingsAction->setChecked( mCommonAngleConstraint != 0 );
     return;
   }
 }
