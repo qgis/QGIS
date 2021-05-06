d295e8f815329782c6e1bc976b4bb674c2d5fd80
Andrea Giudiceandrea <andreaerdna@libero.it>
2021-05-07 00:24:32 +0200

    Release datasource when cancelling loading of OGR sublayers

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -6099,7 +6099,11 @@ QList<QgsMapLayer *> QgisApp::askUserForOGRSublayers( QgsVectorLayer *&parentLay
   chooseSublayersDialog.populateLayerTable( list );
 
   if ( !chooseSublayersDialog.exec() )
+  {
+    delete parentLayer;
+    parentLayer = nullptr;
     return result;
+  }
 
   const bool addToGroup = chooseSublayersDialog.addToGroupCheckbox();
 
