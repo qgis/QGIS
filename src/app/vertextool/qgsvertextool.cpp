5405aa33b1fd13504fe930e4cb2860cdf00bf9b1
Olivier Dalang <olivier.dalang@gmail.com>
2021-05-12 10:32:24 +0200

    [convert to curve] PR review
    Co-authored-by: Matthias Kuhn <matthias@opengis.ch>

--- src/app/vertextool/qgsvertextool.cpp
+++ src/app/vertextool/qgsvertextool.cpp
@@ -2579,7 +2579,7 @@ void QgsVertexTool::toggleVertexCurve()
     // TODO support more than just 1 vertex    
     QgisApp::instance()->messageBar()->pushMessage(
       tr( "Could not convert vertex" ),
-      tr( "Currently, conversion can only be done on exactly one vertex." ),
+      tr( "Conversion can only be done on exactly one vertex." ),
       Qgis::Info );
     return;
   }
