c5af966176295d576163c0e9afd8a29d9423614f
rldhont <rldhont@gmail.com>
2021-05-17 11:37:32 +0200

    [Tests][Server] Remove print('CTEST_FULL_OUTPUT') and duplicated line

--- tests/src/python/test_qgsserver_accesscontrol_wfs_transactional.py
+++ tests/src/python/test_qgsserver_accesscontrol_wfs_transactional.py
@@ -12,8 +12,6 @@ __author__ = 'Stephane Brunner'
 __date__ = '28/08/2015'
 __copyright__ = 'Copyright 2015, The QGIS Project'
 
-print('CTEST_FULL_OUTPUT')
-
 from qgis.testing import unittest
 from test_qgsserver_accesscontrol import TestQgsServerAccessControl, XML_NS
 
