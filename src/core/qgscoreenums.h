af6c08bb26511e0fc998b2e53a513dcec5a57f41
Nyall Dawson <nyall.dawson@gmail.com>
2021-05-17 11:33:16 +1000

    Move QgsMapLayerType to separate generic header for core enums
    Avoids the need to include the whole qgsmaplayer.h header and
    all its dependancies when only the QgsMapLayerType enum is required.

---
+++ src/core/qgscoreenums.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+                             qgscoreenums.h
+                             ----------
+    begin                : May 2021
+    copyright            : (C) 2021 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSCOREENUMS_H
+#define QGSCOREENUMS_H
+
+#include "qgis_core.h"
+#include "qgis_sip.h"
+
+/**
+ * \ingroup core
+ * \brief Types of layers that can be added to a map
+ * \since QGIS 3.8
+ */
+enum class QgsMapLayerType SIP_MONKEYPATCH_SCOPEENUM_UNNEST( QgsMapLayer, LayerType ) : int
+  {
+  VectorLayer,
+  RasterLayer,
+  PluginLayer,
+  MeshLayer,      //!< Added in 3.2
+  VectorTileLayer, //!< Added in 3.14
+  AnnotationLayer, //!< Contains freeform, georeferenced annotations. Added in QGIS 3.16
+  PointCloudLayer, //!< Added in 3.18
+};
+
+#endif // QGSCOREENUMS_H
