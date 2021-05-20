3005d24aff5f6c4dfae12f6ba8e2193b7e9c2d5f
vcloarec <vcloarec@gmail.com>
2021-05-20 11:53:02 -0400

    force UTC at mesh layer level

--- src/core/layout/qgslayoutitemmap.cpp
+++ src/core/layout/qgslayoutitemmap.cpp
@@ -1922,15 +1922,9 @@ void QgsLayoutItemMap::refreshDataDefinedProperty( const QgsLayoutObject::DataDe
     QDateTime end = temporalRange().end();
 
     if ( property == QgsLayoutObject::StartDateTime || property == QgsLayoutObject::AllProperties )
-    {
       begin = mDataDefinedProperties.valueAsDateTime( QgsLayoutObject::StartDateTime, context, temporalRange().begin() );
-      begin.setTimeSpec( Qt::UTC );
-    }
     if ( property == QgsLayoutObject::EndDateTime || property == QgsLayoutObject::AllProperties )
-    {
       end = mDataDefinedProperties.valueAsDateTime( QgsLayoutObject::EndDateTime, context, temporalRange().end() );
-      end.setTimeSpec( Qt::UTC );
-    }
 
     setTemporalRange( QgsDateTimeRange( begin, end, true, begin == end ) );
   }
