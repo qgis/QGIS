26f8c28812cb1c182c04a0ea15a011a73f2707d7
wanpeng <wangpeng187@126.com>
2021-05-24 23:04:06 +0800

    model

--- src/app/qgsprofilewindowdockwidget.h
+++ src/app/qgsprofilewindowdockwidget.h
@@ -38,7 +38,6 @@ class QRadioButton;
 typedef View3D QgsProfileWinow;
 typedef Imath::V3d Point3D;
 
-
 enum PointType
 {
   Start = 0,
@@ -54,49 +53,35 @@ struct ModelItem
   double error;
 };
 
-
-
 class  QgsDLAttributeTableModel : public QAbstractTableModel
 {
   Q_OBJECT
-
 public:
 
-  /**
-   * Constructor
-   * \param layerCache  A layer cache to use as backend
-   * \param parent      The parent QObject (owner)
-   */
   QgsDLAttributeTableModel(QgsProfileWinow *mapCanvas, QObject *parent = nullptr)
     : QAbstractTableModel(parent)
     , mMapCanvas(mapCanvas)
   {
-
+      m_header.push_back("类型");
+      m_header.push_back("X");
+      m_header.push_back("Y");
+      m_header.push_back("Z");
+      modelData.clear();
   }
 
   //自定义导入导出数据的接口
-  void setModelData(const QList<ModelItem> &datas)
+  void setModelData(const std::vector<ModelItem> &datas)
   {
-    beginResetModel();
-    modelData = datas;
-    endResetModel();
+      beginResetModel();
+      modelData = datas;
+      endResetModel();
   };
 
-  QList<ModelItem> getModelData() const
+  std::vector<ModelItem> getModelData() const
   {
-    return modelData;
+     return modelData;
   };
 
-
-  void SetHeaderData(const QStringList& list)
-  {
-   
-  }
-
-  void SetData(const QMap<int, QVector<QString>>& map)
-  {
-
-  }
   /**
  * Returns the number of rows
  * \param parent parent index
@@ -105,7 +90,7 @@ public:
   {
     if (parent.isValid())
       return 0;
-    return modelData.count();
+    return modelData.size();
   };
 
   /**
@@ -120,12 +105,6 @@ public:
     return  m_header.size();
   };
 
-  /**
- * Returns header data
- * \param section required section
- * \param orientation horizontal or vertical orientation
- * \param role data role
- */
   QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override
   {
     if (role == Qt::DisplayRole&&orientation == Qt::Horizontal)
@@ -135,36 +114,32 @@ public:
 
   bool insertModelData(int row, const ModelItem &datas)
   {
-    //row为0就是开始，为rowcount就在尾巴
-    if (row<0 || row>rowCount())
-      return false;
-    //需要将操作放到beginInsertRows和endInsertRows两个函数调用之间
-    beginInsertRows(QModelIndex(), row, row);
-    //在接口对应行插入空数据
-    modelData.insert(row, datas);
-    endInsertRows();
-    return true;
+      //row为0就是开始，为rowcount就在尾巴
+      if (row<0 || row>rowCount())
+        return false;
+      //需要将操作放到beginInsertRows和endInsertRows两个函数调用之间
+      beginInsertRows(QModelIndex(), row, row);
+      //在接口对应行插入空数据
+      std::vector<ModelItem>::iterator  it = modelData.begin() + row;
+      modelData.insert(it, datas);
+      endInsertRows();
+      return true;
   }
 
-  /**
- * Returns data on the given index
- * \param index model index
- * \param role data role
- */
   QVariant data(const QModelIndex &index, int role) const override
   {
     if (!index.isValid())
       return QVariant();
     if (role == Qt::DisplayRole || role == Qt::EditRole)
     {
-      //DisplayRole返回显示的文本值
       const int row = index.row();
       switch (index.column())
       {
-     /* case 0: return modelData.at(row).name;
-      case 1: return modelData.at(row).age;
-      case 2: return modelData.at(row).info;
-      */
+          case 0: return modelData.at(row).type;
+          case 1: return modelData.at(row).XYZ.x;
+          case 2: return  modelData.at(row).XYZ.y;
+          case 3: return  modelData.at(row).XYZ.z;
+          case 4: return  modelData.at(row).error;
       }
     }
     return QVariant();
@@ -179,24 +154,20 @@ public:
     return flags;
   };
 
-  /**
- * Updates data on given index
- * \param index model index
- * \param value new data value
- * \param role data role
- */
+
   bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override
   {
     //将界面修改的值进行保存
-    if (index.isValid() && role == Qt::EditRole) {
+    if (index.isValid() && role == Qt::EditRole)
+    {
       const int row = index.row();
       switch (index.column())
       {
-        /*
-      case 0: modelData[row].name = value.toString(); break;
-      case 1: modelData[row].age = value.toInt(); break;
-      case 2: modelData[row].info = value.toString(); break;
-        */
+          case 0:  modelData.at(row).type = PointType(value.toInt());
+          case 1:  modelData.at(row).XYZ.x = value.toDouble() ;
+          case 2:  modelData.at(row).XYZ.y = value.toDouble(); 
+          case 3:  modelData.at(row).XYZ.z = value.toDouble(); 
+          case 4:  modelData.at(row).error =  value.toDouble();
       }
       //发送信号触发刷新
       emit dataChanged(index, index, QVector<int>() << role);
@@ -205,49 +176,11 @@ public:
     return false;
   };
 
-
-  /**
- * Returns item flags for the index
- * \param index model index
- */
-  Qt::ItemFlags flags(const QModelIndex &index) const override
-  {
-    if (!index.isValid())
-      return Qt::NoItemFlags;
-    //单元格允许的操作，至少得是ItemIsEnabled的才能进行其他操作
-    return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
-  };
-
-  //添加行列
-  bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) override
-  {
-
-  };
-  /**
-   * Remove rows
-   */
-  bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override
-  {
-
-  };
-
-  /**
- * Gets field index from column
- */
-  int fieldIdx(int col) const;
-
-  /**
- * Gets column from field index
- */
-
-  int fieldCol(int idx) const;
-
 private:
   QgsProfileWinow * mMapCanvas = nullptr;
 
   QStringList m_header;
-
-  QList <ModelItem> modelData;
+  std::vector<ModelItem> modelData;
 };
 
 class  APP_EXPORT  QgsPcdpickeddlgWindowDockWidget : public QgsDockWidget, private Ui::pcdpickeddlg
@@ -263,6 +196,7 @@ public:
     this->setWindowTitle(name);
     this->alignedPointsTableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);// 自适应列宽
     this->alignedPointsTableWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch); //自适应行高
+    this->pushButton->setEnabled(false);
   };
 private :
   bool insert_pt_table_(Point3D xyz , PointType type)
