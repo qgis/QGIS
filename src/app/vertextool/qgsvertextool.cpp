56b7cb98094cdfc2d0bbf46d69e0ab0ab941ad4b
Nyall Dawson <nyall.dawson@gmail.com>
2021-05-24 13:19:06 +1000

    Ensure correct expression context is used in map select tools, identify tool, map tips and vertex tool
    Fixes #42006
    
    (cherry picked from commit 01d971089a5c9f25d6302a05dba0d352dca6375b)

--- src/app/vertextool/qgsvertextool.cpp
+++ src/app/vertextool/qgsvertextool.cpp
@@ -564,6 +564,7 @@ void QgsVertexTool::cadCanvasReleaseEvent( QgsMapMouseEvent *e )
       }
 
       QgsRenderContext context = QgsRenderContext::fromMapSettings( mCanvas->mapSettings() );
+      context.setExpressionContext( mCanvas->createExpressionContext() );
       context.expressionContext() << QgsExpressionContextUtils::layerScope( vlayer );
       std::unique_ptr< QgsFeatureRenderer > r;
       if ( vlayer->renderer() )
