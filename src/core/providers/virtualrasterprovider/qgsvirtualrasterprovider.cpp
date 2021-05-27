584f7c5cc8c8d225cee46d8e6cb185acc200e98d
franc <Franc-Brs>
2021-05-27 19:28:43 +0200

    updating VRP, no good results

--- src/core/providers/virtualrasterprovider/qgsvirtualrasterprovider.cpp
+++ src/core/providers/virtualrasterprovider/qgsvirtualrasterprovider.cpp
@@ -6,31 +6,59 @@
 const QString QgsVirtualRasterProvider::VR_RASTER_PROVIDER_KEY = QStringLiteral( "virtualrasterprovider" );
 const QString QgsVirtualRasterProvider::VR_RASTER_PROVIDER_DESCRIPTION =  QStringLiteral( "Virtual Raster data provider" );
 
-QgsVirtualRasterProvider::QgsVirtualRasterProvider( const QString &uri, const QgsDataProvider::ProviderOptions &providerOptions, QgsDataProvider::ReadFlags flags )
-    : QgsRasterDataProvider( uri, providerOptions, flags )
+QgsVirtualRasterProvider::QgsVirtualRasterProvider( const QString &uri, const QgsDataProvider::ProviderOptions &providerOptions)
+    : QgsRasterDataProvider( uri, providerOptions)
 {
 
 }
 
-QgsVirtualRasterProvider::QgsVirtualRasterProvider( const QgsVirtualRasterProvider &other, const QgsDataProvider::ProviderOptions &providerOptions, QgsDataProvider::ReadFlags flags )
-  : QgsRasterDataProvider( other.dataSourceUri(), providerOptions, flags )
+QgsVirtualRasterProvider::QgsVirtualRasterProvider(const QgsVirtualRasterProvider &other)
+    : QgsRasterDataProvider (other.dataSourceUri(), QgsDataProvider::ProviderOptions() )
+{
+    mExtent = other.mExtent;
+    mWidth = other.mWidth;
+    mHeight = other.mHeight;
+    mBandCount = other.mBandCount;
+    mXBlockSize = other.mXBlockSize;
+    mYBlockSize = other.mYBlockSize;
+
+}
+
+
+/*
+//QgsRasterBlock *QgsVirtualRasterProvider::block( Qgis::DataType dataType, int width, int height )
+QgsRasterBlock *QgsVirtualRasterProvider::block( int bandNo, const QgsRectangle &extent, int width, int height, QgsRasterBlockFeedback *feedback )
+{
+    std::unique_ptr< QgsRasterBlock > block = std::make_unique< QgsRasterBlock >( dataType( bandNo ), width, height );
+}
+*/
+
+
+QgsRasterBlock *QgsVirtualRasterProvider::block( Qgis::DataType dataType, int width, int height  )
 {
 
 }
 
-QgsCoordinateReferenceSystem QgsVirtualRasterProvider::crs() const
-{
-  return mCrs;
-}
 
 QgsRectangle QgsVirtualRasterProvider::extent() const
 {
   return mExtent;
 }
 
-bool QgsVirtualRasterProvider::isValid() const
+int QgsVirtualRasterProvider::xSize() const
 {
-  return mValid;
+    return mWidth;
+}
+
+int QgsVirtualRasterProvider::ySize() const
+{
+    return mHeight;
+}
+
+
+int QgsVirtualRasterProvider::bandCount() const
+{
+  return mBandCount;
 }
 
 QString QgsVirtualRasterProvider::name() const
@@ -43,164 +71,14 @@ QString QgsVirtualRasterProvider::description() const
   return QgsVirtualRasterProvider::VR_RASTER_PROVIDER_DESCRIPTION;
 }
 
-
-bool QgsVirtualRasterProvider::readBlock( int bandNo, const QgsRectangle &viewExtent, int width, int height, void *data, QgsRasterBlockFeedback * )
-{
-    return true;
-}
-
-///*
-//QImage *QgsVirtualRasterProvider::draw( QgsRectangle const &viewExtent, int pixelWidth, int pixelHeight, QgsRasterBlockFeedback *feedback )
-//QImage *QgsVirtualRasterProvider::draw( QgsRectangle const &viewExtent, int width, int height, QgsRasterBlockFeedback *)
-QImage *QgsVirtualRasterProvider::draw( int width, int height)
-{
-  //QImage *image = new QImage( pixelWidth, pixelHeight, QImage::Format_ARGB32 );
-  QImage *image = new QImage( width, height, QImage::Format_ARGB32 );
-  image->fill( 0 );
-
-  return image;
-}
-//*/
-
-
-QgsVirtualRasterProviderMetadata::QgsVirtualRasterProviderMetadata()
-  : QgsProviderMetadata( QgsVirtualRasterProvider::VR_RASTER_PROVIDER_KEY, QgsVirtualRasterProvider::VR_RASTER_PROVIDER_DESCRIPTION )
-{
-
-}
-
-
-QgsVirtualRasterProvider *QgsVirtualRasterProviderMetadata::createProvider( const QString &uri, const QgsDataProvider::ProviderOptions &options, QgsDataProvider::ReadFlags flags )
-
-{
-  return new QgsVirtualRasterProvider( uri, options, flags );
-
-}
-
-
-Qgis::DataType QgsVirtualRasterProvider::dataType( int bandNo ) const
-{
-  if ( mDataTypes.size() < static_cast<unsigned long>( bandNo ) )
-  {
-    QgsMessageLog::logMessage( tr( "Data type size for band %1 could not be found: num bands is: %2 and the type size map for bands contains: %3 items" )
-                               .arg( bandNo )
-                               .arg( mBandCount )
-                               .arg( mDataSizes.size() ),
-                               QStringLiteral( "Virtual Raster Provider" ), Qgis::Warning );
-    return Qgis::DataType::UnknownDataType;
-  }
-  // Band is 1-based
-  return mDataTypes[ static_cast<unsigned long>( bandNo ) - 1 ];
-}
-
-int QgsVirtualRasterProvider::bandCount() const
-{
-  return mBandCount;
-}
-
-///*
-QgsVirtualRasterProvider *QgsVirtualRasterProvider::clone() const
-{
-  QgsDataProvider::ProviderOptions options;
-  options.transformContext = transformContext();
-  QgsVirtualRasterProvider *provider = new QgsVirtualRasterProvider( *this, options );
-  provider->copyBaseSettings( *this );
-  return provider;
-}
-//*/
-
-
-QString QgsVirtualRasterProvider::htmlMetadata()
-{
-  /*
-  // This must return the content of a HTML table starting by tr and ending by tr
-  QVariantMap overviews;
-  for ( auto it = mOverViews.constBegin(); it != mOverViews.constEnd(); ++it )
-  {
-    overviews.insert( QString::number( it.key() ), it.value() );
-  }
-
-  const QVariantMap additionalInformation
-  {
-    { tr( "Is Tiled" ), mIsTiled },
-    //{ tr( "Where Clause SQL" ), subsetString() },
-    { tr( "Pixel Size" ), QStringLiteral( "%1, %2" ).arg( mScaleX ).arg( mScaleY ) },
-    { tr( "Overviews" ),  overviews },
-    //{ tr( "Primary Keys SQL" ),  pkSql() },
-    //{ tr( "Temporal Column" ),  mTemporalFieldIndex >= 0 && mAttributeFields.exists( mTemporalFieldIndex ) ?  mAttributeFields.field( mTemporalFieldIndex ).name() : QString() },
-  };
-  return  dumpVariantMap( additionalInformation, tr( "Additional information" ) );
-  */
-  //only test
-  return "Virtual Raster data provider";
-}
-
-QString QgsVirtualRasterProvider::lastErrorTitle()
-{
-  return mErrorTitle;
-}
-
-QString QgsVirtualRasterProvider::lastError()
-{
-  return mError;
-}
-
-int QgsVirtualRasterProvider::capabilities() const
-{
-  const int capability = QgsRasterDataProvider::Identify
-                         | QgsRasterDataProvider::IdentifyValue
-                         | QgsRasterDataProvider::Size
-                         // TODO:| QgsRasterDataProvider::BuildPyramids
-                         | QgsRasterDataProvider::Create
-                         | QgsRasterDataProvider::Remove
-                         | QgsRasterDataProvider::Prefetch;
-  return capability;
-}
-
-
-int QgsVirtualRasterProvider::xSize() const
-{
-  return static_cast<int>( mWidth );
-}
-
-int QgsVirtualRasterProvider::ySize() const
-{
-  return static_cast<int>( mHeight );
-}
-
-Qgis::DataType QgsVirtualRasterProvider::sourceDataType( int bandNo ) const
-{
-  if ( bandNo <= mBandCount && static_cast<unsigned long>( bandNo ) <= mDataTypes.size() )
-  {
-    return mDataTypes[ static_cast<unsigned long>( bandNo - 1 ) ];
-  }
-  else
-  {
-    QgsMessageLog::logMessage( tr( "Data type is unknown" ), QStringLiteral( "PostGIS" ), Qgis::Warning );
-    return Qgis::DataType::UnknownDataType;
-  }
-}
-
 int QgsVirtualRasterProvider::xBlockSize() const
 {
-  if ( mInput )
-  {
-    return mInput->xBlockSize();
-  }
-  else
-  {
-    return static_cast<int>( mWidth );
-  }
+  return mXBlockSize;
 }
 
 int QgsVirtualRasterProvider::yBlockSize() const
 {
-  if ( mInput )
-  {
-    return mInput->yBlockSize();
-  }
-  else
-  {
-    return static_cast<int>( mHeight );
-  }
+  return mYBlockSize;
 }
+
+
--- src/core/providers/virtualrasterprovider/qgsvirtualrasterprovider.h
+++ src/core/providers/virtualrasterprovider/qgsvirtualrasterprovider.h
@@ -1,3 +1,4 @@
+
 #ifndef QGSVIRTUALRASTERPROVIDER_H
 #define QGSVIRTUALRASTERPROVIDER_H
 
@@ -7,103 +8,49 @@
 #include "qgscoordinatereferencesystem.h"
 #include "qgsprovidermetadata.h"
 
-#include "qgis_sip.h"
 
-///@cond PRIVATE
-#define SIP_NO_FILE
 class QgsVirtualRasterProvider : public QgsRasterDataProvider
 {
     Q_OBJECT
 public:
-    //QgsVirtualRasterProvider ( const QString &uri);
-    QgsVirtualRasterProvider ( const QString &uri, const QgsDataProvider::ProviderOptions &providerOptions, QgsDataProvider::ReadFlags flags = QgsDataProvider::ReadFlags() );
 
-    //explicit QgsVirtualRasterProvider(const QgsVirtualRasterProvider &other);
-    explicit QgsVirtualRasterProvider( const QgsVirtualRasterProvider &other, const QgsDataProvider::ProviderOptions &providerOptions, QgsDataProvider::ReadFlags flags = QgsDataProvider::ReadFlags() );
+    QgsVirtualRasterProvider ( const QString &uri, const QgsDataProvider::ProviderOptions &providerOptions);
     virtual ~QgsVirtualRasterProvider() override = default;
 
-public:
-    // QgsDataProvider interface
-    virtual QgsCoordinateReferenceSystem crs() const override;
-    virtual QgsRectangle extent() const override;
-    virtual bool isValid() const override;
-    virtual QString name() const override;
-    virtual QString description() const override;
+    QgsRasterBlock *block(Qgis::DataType dataType, int width, int height );
+    //QgsRasterBlock *block( int bandNo, const QgsRectangle &extent, int width, int height, QgsRasterBlockFeedback *feedback = nullptr ) override;
 
-    bool readBlock( int bandNo, QgsRectangle const &viewExtent, int width, int height, void *data, QgsRasterBlockFeedback *feedback = nullptr ) override;
+    //bool readBlock( int bandNo, int xBlock, int yBlock, void *data ) override;
+    //bool readBlock( int bandNo, QgsRectangle  const &viewExtent, int width, int height, void *data, QgsRasterBlockFeedback *feedback = nullptr ) override;
+    bool readBlock( int bandNo, int xBlock, int yBlock, void *data ) override
+        { Q_UNUSED( bandNo ) Q_UNUSED( xBlock ); Q_UNUSED( yBlock ); Q_UNUSED( data ); return true; }
+    bool readBlock( int bandNo, QgsRectangle  const &viewExtent, int width, int height, void *data, QgsRasterBlockFeedback *feedback = nullptr ) override
+        { Q_UNUSED( bandNo ) Q_UNUSED( viewExtent ); Q_UNUSED( width ); Q_UNUSED( height ); Q_UNUSED( data ); Q_UNUSED( feedback ); true; }
 
-    // QgsRasterInterface interface
-    virtual Qgis::DataType dataType( int bandNo ) const override;
-    virtual int bandCount() const override;
-    virtual QgsVirtualRasterProvider *clone() const override;
-    virtual Qgis::DataType sourceDataType( int bandNo ) const override;
-    virtual int xBlockSize() const override;
-    virtual int yBlockSize() const override;
-    //virtual QgsRasterBandStats bandStatistics( int bandNo, int stats, const QgsRectangle &extent, int sampleSize, QgsRasterBlockFeedback *feedback ) override;
 
-    // QgsRasterInterface interface
+    QgsRectangle extent() const override;
+    int xBlockSize() const override;
+    int yBlockSize() const override;
     int xSize() const override;
     int ySize() const override;
-
-    // QgsRasterDataProvider interface
-    virtual QString htmlMetadata() override;
-    virtual QString lastErrorTitle() override; //not useful at the mom
-    virtual QString lastError() override; //not useful at the mom
-    int capabilities() const override;
-    //QgsFields fields() const override;
+    virtual QString name() const override;
+    virtual QString description() const override;
+    int bandCount() const override;
 
     static const QString VR_RASTER_PROVIDER_KEY;
     static const QString VR_RASTER_PROVIDER_DESCRIPTION;
 
-signals:
-
 private:
+    //I don't really understand it now
+    QgsVirtualRasterProvider( const QgsVirtualRasterProvider &other);
 
-    //QImage draw( const QgsRectangle &viewExtent, int width, int height, QgsRasterBlockFeedback *feedback = nullptr );
-    QImage *draw(int width, int height);
-
-    QgsCoordinateReferenceSystem mCrs;
-    //! Data source URI struct for this layer
-    QgsDataSourceUri mUri;
-    bool mValid = false;
-    //! Error information
-    QString mError;
-    //! Error title
-    QString mErrorTitle;
-    //! Data type for each band
-    std::vector<Qgis::DataType> mDataTypes;
-    //! Data size in bytes for each band
-    std::vector<int> mDataSizes;
-    //! Store overviews
-    QMap<unsigned int, QString> mOverViews;
-    //! Band count
-    int mBandCount = 0;
-    //! If is tiled
-    bool mIsTiled = false;
-    //! Raster size x
-    long mWidth = 0;
-    //! Raster size y
-    long mHeight = 0;
-    //! Raster tile size x
-     int mTileWidth = 0;
-     //! Raster tile size y
-     int mTileHeight = 0;
-     //! Scale x
-     double mScaleX = 0;
-     //! Scale y
-     double mScaleY = 0;
-
+    QgsRectangle mExtent;
+    int mWidth = 0;
+    int mHeight = 0;
+    int mBandCount = 1;
+    int mXBlockSize = 0;
+    int mYBlockSize = 0;
 };
-
-class QgsVirtualRasterProviderMetadata: public QgsProviderMetadata
-{
-  public:
-    QgsVirtualRasterProviderMetadata();
-    //QVariantMap decodeUri( const QString &uri ) const override;
-    QgsVirtualRasterProvider *createProvider( const QString &uri, const QgsDataProvider::ProviderOptions &options, QgsDataProvider::ReadFlags flags = QgsDataProvider::ReadFlags() ) override;
-
-    //QString encodeUri( const QVariantMap &parts ) const override;
-};
-
 
 #endif // QGSVIRTUALRASTERPROVIDER_H
+
