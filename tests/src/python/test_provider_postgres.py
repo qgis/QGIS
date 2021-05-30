9194c07059e913d0c15d521e0a96cb0e4799d432
MorriganR <d_daryna@knu.ua>
2021-05-31 00:14:27 +0300

    add test for NULL in multi-column PK

--- tests/src/python/test_provider_postgres.py
+++ tests/src/python/test_provider_postgres.py
@@ -2639,7 +2639,8 @@ class TestPyQgsPostgresProvider(unittest.TestCase, ProviderTestCase):
         """Test Multi Column PK & `Small` Data"""
         from itertools import combinations
 
-        def test_for_pk_combinations(pk_column_name, fids_get_count):
+        def test_for_pk_combinations(test_type_list, pk_column_name_list, fids_get_count):
+            pk_column_name = ','.join(pk_column_name_list)
             set_new_pk = '''
                 ALTER TABLE qgis_test.multi_column_pk_small_data_table DROP CONSTRAINT multi_column_pk_small_data_pk;
                 ALTER TABLE qgis_test.multi_column_pk_small_data_table
@@ -2647,8 +2648,9 @@ class TestPyQgsPostgresProvider(unittest.TestCase, ProviderTestCase):
             set_new_layer = ' sslmode=disable key=\'{}\' srid=3857 type=POLYGON table="qgis_test"."multi_column_pk_small_data_{}" (geom) sql='
             error_string = 'from {} with PK - {} : expected {}, got {}'
 
-            self.execSQLCommand(set_new_pk.format(pk_column_name))
-            for test_type in ['table', 'view', 'mat_view']:
+            if 'table' in test_type_list:
+                self.execSQLCommand(set_new_pk.format(pk_column_name))
+            for test_type in test_type_list:
                 vl = QgsVectorLayer(self.dbconn + set_new_layer.format(pk_column_name, test_type), 'test_multi_column_pk_small_data', 'postgres')
                 fids = [f.id() for f in vl.getFeatures(QgsFeatureRequest().setLimit(fids_get_count))]
                 fids2 = [f.id() for f in vl.getFeatures(fids)]
@@ -2672,7 +2674,8 @@ class TestPyQgsPostgresProvider(unittest.TestCase, ProviderTestCase):
           id_macaddr macaddr NOT NULL,
           id_macaddr8 macaddr8 NOT NULL,
           id_timestamp timestamp with time zone NOT NULL,
-          --id_  NOT NULL,
+          id_half_null_uuid uuid,
+          id_all_null_uuid uuid,
           geom geometry(Polygon,3857),
           CONSTRAINT multi_column_pk_small_data_pk
             PRIMARY KEY (id_serial, id_uuid, id_int, id_bigint, id_str) );''')
@@ -2686,7 +2689,7 @@ class TestPyQgsPostgresProvider(unittest.TestCase, ProviderTestCase):
         TRUNCATE qgis_test.multi_column_pk_small_data_table;
         INSERT INTO qgis_test.multi_column_pk_small_data_table(
           id_uuid, id_int, id_bigint, id_str, id_inet4, id_inet6, id_cidr4, id_cidr6,
-            id_macaddr, id_macaddr8, id_timestamp, geom)
+            id_macaddr, id_macaddr8, id_timestamp, id_half_null_uuid, id_all_null_uuid, geom)
           SELECT
             ( (10000000)::text || (100000000000 + dy)::text || (100000000000 + dx)::text )::uuid,
             dx + 1000000 * dy,                                                    --id_int
@@ -2699,20 +2702,27 @@ class TestPyQgsPostgresProvider(unittest.TestCase, ProviderTestCase):
             ((112233445566 + dx + 100 * dy)::text)::macaddr,                      --id_macaddr
             ((1122334455667788 + dx + 100 * dy)::text)::macaddr8,                 --id_macaddr8
             now() - ((dx||\' hour\')::text)::interval - ((dy||\' day\')::text)::interval,
+            NULLIF( ( (10000000)::text || (100000000000 + dy)::text || (100000000000 + dx)::text )::uuid,
+              ( (10000000)::text || (100000000000 + dy + dy%2)::text || (100000000000 + dx)::text )::uuid ),
+            NULL,
             ST_Translate(
               ST_GeomFromText(\'POLYGON((3396900.0 6521800.0,3396900.0 6521870.0,
                   3396830.0 6521870.0,3396830.0 6521800.0,3396900.0 6521800.0))\', 3857 ),
               100.0 * dx,
               100.0 * dy )
-          FROM generate_series(1,10) dx, generate_series(1,10) dy;
+          FROM generate_series(1,8) dx, generate_series(1,8) dy;
         REFRESH MATERIALIZED VIEW qgis_test.multi_column_pk_small_data_mat_view;''')
 
         pk_col_list = ("id_serial", "id_uuid", "id_int", "id_bigint", "id_str", "id_inet4", "id_inet6", "id_cidr4", "id_cidr6", "id_macaddr", "id_macaddr8")
+        test_type_list = ["table", "view", "mat_view"]
         for n in [1, 2, len(pk_col_list)]:
             pk_col_set_list = list(combinations(pk_col_list, n))
             for pk_col_set in pk_col_set_list:
-                pk_column_name = ','.join(pk_col_set)
-                test_for_pk_combinations(pk_column_name, 42)
+                test_for_pk_combinations(test_type_list, pk_col_set, 42)
+
+        for col_name in ["id_serial", "id_uuid", "id_int", "id_bigint", "id_str", "id_inet4"]:
+            test_for_pk_combinations(["view", "mat_view"], ["id_half_null_uuid", col_name], 42)
+            test_for_pk_combinations(["view", "mat_view"], ["id_all_null_uuid", col_name], 42)
 
     def testMultiColumnPkBigData(self):
         """Test Multi Column PK & `Big` Data"""
