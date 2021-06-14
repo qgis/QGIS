beef71d9561d1932dfe20e05c21f3ac5af9c6b98
Nyall Dawson <nyall.dawson@gmail.com>
2021-06-14 10:43:05 +1000

    Update internal srsdb and legacy mappings

--- src/core/proj/qgscoordinatereferencesystem.cpp
+++ src/core/proj/qgscoordinatereferencesystem.cpp
@@ -2354,8 +2354,8 @@ int QgsCoordinateReferenceSystem::syncDatabase()
 
   PROJ_STRING_LIST authorities = proj_get_authorities_from_database( pjContext );
 
-  int nextSrsId = 63321;
-  int nextSrId = 520003321;
+  int nextSrsId = 63560;
+  int nextSrId = 520003560;
   for ( auto authIter = authorities; authIter && *authIter; ++authIter )
   {
     const QString authority( *authIter );
