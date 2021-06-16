79786343cd87626e315f2585ca2c8b04833cf10e
Nyall Dawson <nyall.dawson@gmail.com>
2021-06-16 14:13:06 +1000

    [layouts] Fix bounding rectangle for shapes doesn't include stroke width after loading from xml
    Fixes #43748

--- src/core/layout/qgslayoutitemshape.cpp
+++ src/core/layout/qgslayoutitemshape.cpp
@@ -37,7 +37,7 @@ QgsLayoutItemShape::QgsLayoutItemShape( QgsLayout *layout )
   properties.insert( QStringLiteral( "width_border" ), QStringLiteral( "0.3" ) );
   properties.insert( QStringLiteral( "joinstyle" ), QStringLiteral( "miter" ) );
   mShapeStyleSymbol.reset( QgsFillSymbol::createSimple( properties ) );
-  refreshSymbol();
+  refreshSymbol( false );
 
   connect( this, &QgsLayoutItemShape::sizePositionChanged, this, [ = ]
   {
@@ -117,7 +117,7 @@ void QgsLayoutItemShape::setShapeType( QgsLayoutItemShape::Shape type )
   emit clipPathChanged();
 }
 
-void QgsLayoutItemShape::refreshSymbol()
+void QgsLayoutItemShape::refreshSymbol( bool redraw )
 {
   if ( auto *lLayout = layout() )
   {
@@ -127,7 +127,9 @@ void QgsLayoutItemShape::refreshSymbol()
 
   updateBoundingRect();
 
-  update();
+  if ( redraw )
+    update();
+
   emit frameChanged();
 }
 
@@ -148,7 +150,7 @@ void QgsLayoutItemShape::setSymbol( QgsFillSymbol *symbol )
     return;
 
   mShapeStyleSymbol.reset( symbol->clone() );
-  refreshSymbol();
+  refreshSymbol( true );
 }
 
 void QgsLayoutItemShape::setCornerRadius( QgsLayoutMeasurement radius )
@@ -276,6 +278,7 @@ bool QgsLayoutItemShape::readPropertiesFromElement( const QDomElement &element, 
   if ( !shapeStyleSymbolElem.isNull() )
   {
     mShapeStyleSymbol.reset( QgsSymbolLayerUtils::loadSymbol<QgsFillSymbol>( shapeStyleSymbolElem, context ) );
+    refreshSymbol( false );
   }
 
   return true;
