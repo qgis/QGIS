ccc601b31fe51608f512187f54268fc219799ed2
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2021-07-02 09:45:34 +0200

    Fix painter scaling and add unit test

--- src/core/layertree/qgslayertreemodellegendnode.cpp
+++ src/core/layertree/qgslayertreemodellegendnode.cpp
@@ -946,18 +946,23 @@ QSizeF QgsImageLegendNode::drawSymbol( const QgsLegendSettings &settings, ItemCo
 {
   Q_UNUSED( itemHeight )
 
-  if ( ctx && ctx->painter )
+  if ( ctx && ctx->painter && ctx->context )
   {
-    QImage scaledImg = mImage.scaled( settings.wmsLegendSize().toSize(), Qt::KeepAspectRatio, Qt::FastTransformation );
+    QgsScopedRenderContextScaleToPixels scopedScaleToPixels( *( ctx->context ) );
+    double scaleFactor = ctx->context->scaleFactor();
+    double imgWidth = settings.wmsLegendSize().width() * scaleFactor;
+    double imgHeight = settings.wmsLegendSize().height() * scaleFactor;
+
+    QImage scaledImg = mImage.scaled( QSizeF( imgWidth, imgHeight ).toSize(), Qt::KeepAspectRatio, Qt::SmoothTransformation );
     switch ( settings.symbolAlignment() )
     {
       case Qt::AlignLeft:
       default:
-        ctx->painter->drawImage( QPointF( ctx->columnLeft, ctx->top ), scaledImg );
+        ctx->painter->drawImage( QPointF( ctx->columnLeft * scaleFactor, ctx->top * scaleFactor ), scaledImg );
         break;
 
       case Qt::AlignRight:
-        ctx->painter->drawImage( QPointF( ctx->columnRight - settings.wmsLegendSize().width(), ctx->top ), scaledImg );
+        ctx->painter->drawImage( QPointF( ctx->columnRight * scaleFactor - imgWidth, ctx->top * scaleFactor ), scaledImg );
         break;
     }
   }
