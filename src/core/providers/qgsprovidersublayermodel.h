c9c0fa01b99db5fe04a088041832c1a1a4dd7503
Nyall Dawson <nyall.dawson@gmail.com>
2021-07-05 10:35:17 +1000

    Allow non sublayer items to be added to QgsProviderSublayerModel
    E.g. embedded project items

--- src/core/providers/qgsprovidersublayermodel.h
+++ src/core/providers/qgsprovidersublayermodel.h
@@ -49,6 +49,8 @@ class CORE_EXPORT QgsProviderSublayerModel: public QAbstractItemModel
       WkbType, //!< WKB geometry type (for vector sublayers)
       GeometryColumnName, //!< Geometry column name (for vector sublayers)
       LayerNumber, //!< Layer number
+      IsNonLayerItem, //!< TRUE if item is a non-sublayer item (e.g. an embedded project)
+      NonLayerItemType, //!< Item type (for non-sublayer items)
     };
 
     //! Model columns
@@ -56,6 +58,87 @@ class CORE_EXPORT QgsProviderSublayerModel: public QAbstractItemModel
     {
       Name = 0, //!< Layer name
       Description = 1, //!< Layer description
+    };
+
+    /**
+     * \ingroup core
+     *
+     * \brief
+
+     * \since QGIS 3.22
+     */
+    class NonLayerItem
+    {
+      public:
+
+        /**
+         * Returns the item's type.
+         * \see setType()
+         */
+        QString type() const;
+
+        /**
+         * Sets the item's \a type.
+         * \see type()
+         */
+        void setType( const QString &type );
+
+        /**
+         * Returns the item's name.
+         * \see setName()
+         */
+        QString name() const;
+
+        /**
+         * Sets the item's \a name.
+         * \see setName()
+         */
+        void setName( const QString &name );
+
+        /**
+         * Returns the item's description.
+         * \see setDescription()
+         */
+        QString description() const;
+
+        /**
+         * Sets the item's \a description.
+         * \see setDescription()
+         */
+        void setDescription( const QString &description );
+
+        /**
+         * Returns the item's URI.
+         * \see setUri()
+         */
+        QString uri() const;
+
+        /**
+         * Set the item's \a uri.
+         * \see setUri()
+         */
+        void setUri( const QString &uri );
+
+        /**
+         * Returns the item's icon.
+         * \see setIcon()
+         */
+        QIcon icon() const;
+
+        /**
+         * Sets the item's \a icon.
+         * \see setIcon()
+         */
+        void setIcon( const QIcon &icon );
+
+      private:
+
+        QString mType;
+        QString mName;
+        QString mDescription;
+        QString mUri;
+        QIcon mIcon;
+
     };
 
     /**
@@ -76,6 +159,12 @@ class CORE_EXPORT QgsProviderSublayerModel: public QAbstractItemModel
      * \see setSublayerDetails()
      */
     QList< QgsProviderSublayerDetails > sublayerDetails() const;
+
+    /**
+     * Adds a non-layer item (e.g. an embedded QGIS project item) to the model.
+     */
+    void addNonLayerItem( const QgsProviderSublayerModel::NonLayerItem &item );
+
     QModelIndex index( int row, int column, const QModelIndex &parent = QModelIndex() ) const override;
     QModelIndex parent( const QModelIndex &index ) const override;
     int columnCount( const QModelIndex &parent = QModelIndex() ) const override;
@@ -86,6 +175,7 @@ class CORE_EXPORT QgsProviderSublayerModel: public QAbstractItemModel
   private:
 
     QList<QgsProviderSublayerDetails> mSublayers;
+    QList<NonLayerItem> mNonLayerItems;
 
 };
 
