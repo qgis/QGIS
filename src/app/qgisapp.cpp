9f2965b61703378bde82faee76289c8cb7854896
Harrissou Sant-anna <delazj@gmail.com>
2021-07-15 11:40:12 +0200

    Fix indentation

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -3506,7 +3506,7 @@ void QgisApp::createToolBars()
     bt->addAction( mActionAddPgLayer );
   if ( mActionAddMssqlLayer )
     bt->addAction( mActionAddMssqlLayer );
-   if ( mActionAddOracleLayer )
+  if ( mActionAddOracleLayer )
     bt->addAction( mActionAddOracleLayer );
   if ( mActionAddHanaLayer )
     bt->addAction( mActionAddHanaLayer );
