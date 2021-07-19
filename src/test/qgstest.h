db79be58fd274e37d306ad95205cf05ff81ec3fb
nirvn <nirvn.asia@gmail.com>
2021-07-19 12:50:36 +0700

    Remove deprecated QTest flag to unlock compilation against qt6

--- src/test/qgstest.h
+++ src/test/qgstest.h
@@ -22,7 +22,6 @@
 
 #define QGSTEST_MAIN(TestObject) \
   QT_BEGIN_NAMESPACE \
-  QTEST_ADD_GPU_BLACKLIST_SUPPORT_DEFS \
   QT_END_NAMESPACE \
   int main(int argc, char *argv[]) \
   { \
@@ -30,7 +29,6 @@
     app.init(); \
     app.setAttribute(Qt::AA_Use96Dpi, true); \
     QTEST_DISABLE_KEYPAD_NAVIGATION \
-    QTEST_ADD_GPU_BLACKLIST_SUPPORT \
     TestObject tc; \
     QTEST_SET_MAIN_SOURCE_PATH \
     return QTest::qExec(&tc, argc, argv); \
