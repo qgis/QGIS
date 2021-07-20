b3283f63c7f75d69634723f915a6d5ef4797e883
Nyall Dawson <nyall.dawson@gmail.com>
2021-07-20 11:27:53 +1000

    Misc qt6 build fixes

--- src/gui/auth/qgsauthsslimportdialog.cpp
+++ src/gui/auth/qgsauthsslimportdialog.cpp
@@ -186,8 +186,12 @@ void QgsAuthSslImportDialog::secureConnect()
              this, &QgsAuthSslImportDialog::socketDisconnected );
     connect( mSocket, &QSslSocket::encrypted,
              this, &QgsAuthSslImportDialog::socketEncrypted );
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     connect( mSocket, static_cast<void ( QAbstractSocket::* )( QAbstractSocket::SocketError )>( &QAbstractSocket::error ),
              this, &QgsAuthSslImportDialog::socketError );
+#else
+    connect( mSocket, &QAbstractSocket::errorOccurred, this, &QgsAuthSslImportDialog::socketError );
+#endif
     connect( mSocket, static_cast<void ( QSslSocket::* )( const QList<QSslError> & )>( &QSslSocket::sslErrors ),
              this, &QgsAuthSslImportDialog::sslErrors );
     connect( mSocket, &QIODevice::readyRead,
