274584553f4e0842aaa01c9ec3405f4fef205f36
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2021-07-22 10:08:34 +0200

    Fix handling of cached geometries in QgsPostgresExpressionCompiler

--- src/providers/postgres/qgspostgresexpressioncompiler.cpp
+++ src/providers/postgres/qgspostgresexpressioncompiler.cpp
@@ -44,6 +44,10 @@ QString QgsPostgresExpressionCompiler::quotedValue( const QVariant &value, bool 
     case QVariant::Double:
       return value.toString();
 
+    //most likely a geometry
+    case QVariant::UserType:
+      ok = false;
+
     default:
       break;
   }
