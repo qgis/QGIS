0601381a158a641e2289610e7d338ec50bef480c
Nyall Dawson <nyall.dawson@gmail.com>
2021-07-23 16:19:48 +1000

    Fix dox warning

--- src/core/browser/qgsfilebaseddataitemprovider.h
+++ src/core/browser/qgsfilebaseddataitemprovider.h
@@ -48,6 +48,7 @@ class CORE_EXPORT QgsProviderSublayerItem final: public QgsLayerItem
 
     /**
      * Constructor for QgsProviderSublayerItem.
+     * \param parent parent item
      * \param name data item name (this should match either the layer's name or the filename of a single-layer file)
      * \param details sublayer details
      * \param isFile set to TRUE if the item represents a single-layer file, or FALSE if the item is a sublayer from a
