7c8935b94199ec2ecf7f4708a42eebe352d890b7
Nyall Dawson <nyall.dawson@gmail.com>
2021-07-29 11:56:47 +1000

    Show search hits for tables and tree/table headers

--- src/gui/qgsoptionsdialoghighlightwidgetsimpl.cpp
+++ src/gui/qgsoptionsdialoghighlightwidgetsimpl.cpp
@@ -22,6 +22,7 @@
 #include <QTreeView>
 #include <QTreeWidget>
 #include <QAbstractItemModel>
+#include <QTableView>
 
 #include "qgsoptionsdialoghighlightwidget.h"
 #include "qgsmessagebaritem.h"
@@ -212,6 +213,15 @@ bool QgsOptionsDialogHighlightTree::searchText( const QString &text )
 {
   if ( !mTreeView || !mTreeView->model() )
     return false;
+
+  // search headers too!
+  for ( int col = 0; col < mTreeView->model()->columnCount(); ++col )
+  {
+    const QString headerText = mTreeView->model()->headerData( col, Qt::Horizontal ).toString();
+    if ( headerText.contains( text, Qt::CaseInsensitive ) )
+      return true;
+  }
+
   QModelIndexList hits = mTreeView->model()->match( mTreeView->model()->index( 0, 0 ), Qt::DisplayRole, text, 1, Qt::MatchContains | Qt::MatchRecursive );
   return !hits.isEmpty();
 }
@@ -283,4 +293,39 @@ void QgsOptionsDialogHighlightTree::reset()
     }
     mTreeInitialExpand.clear();
   }
+}
+
+
+// ****************
+// QTableView
+QgsOptionsDialogHighlightTable::QgsOptionsDialogHighlightTable( QTableView *tableView )
+  : QgsOptionsDialogHighlightWidget( tableView )
+  , mTableView( tableView )
+{
+}
+
+bool QgsOptionsDialogHighlightTable::searchText( const QString &text )
+{
+  if ( !mTableView || !mTableView->model() )
+    return false;
+
+  // search headers too!
+  for ( int col = 0; col < mTableView->model()->columnCount(); ++col )
+  {
+    const QString headerText = mTableView->model()->headerData( col, Qt::Horizontal ).toString();
+    if ( headerText.contains( text, Qt::CaseInsensitive ) )
+      return true;
+  }
+
+  QModelIndexList hits = mTableView->model()->match( mTableView->model()->index( 0, 0 ), Qt::DisplayRole, text, 1, Qt::MatchContains | Qt::MatchRecursive );
+  return !hits.isEmpty();
+}
+
+bool QgsOptionsDialogHighlightTable::highlightText( const QString & )
+{
+  return false;
+}
+
+void QgsOptionsDialogHighlightTable::reset()
+{
 }
