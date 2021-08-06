a0f359e7ae7e694bb6a4ed22bc7a5cc0c2002fd2
Samweli <samweli@kartoza.com>
2021-08-06 07:38:53 +0300

    set singleFile property inside the QgsLayoutAtlas constructor

--- src/gui/layout/qgslayoutatlaswidget.cpp
+++ src/gui/layout/qgslayoutatlaswidget.cpp
@@ -392,9 +392,6 @@ void QgsLayoutAtlasWidget::updateGuiElements()
   mAtlasFilenamePatternEdit->setText( mAtlas->filenameExpression() );
   mAtlasHideCoverageCheckBox->setCheckState( mAtlas->hideCoverage() ? Qt::Checked : Qt::Unchecked );
 
-  if ( mLayout->customProperty( QStringLiteral( "singleFile" ) ).isNull() )
-    mLayout->setCustomProperty( QStringLiteral( "singleFile" ), true );
-
   bool singleFile = mLayout->customProperty( QStringLiteral( "singleFile" ) ).toBool();
   mAtlasSingleFileCheckBox->setCheckState( singleFile ? Qt::Checked : Qt::Unchecked );
   mAtlasFilenamePatternEdit->setEnabled( !singleFile );
