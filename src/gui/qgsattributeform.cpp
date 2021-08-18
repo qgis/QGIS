0eaf6296b5fd874d701d47ab082fba91c87677ba
Nyall Dawson <nyall.dawson@gmail.com>
2021-08-18 12:21:09 +1000

    No setCodec on qt6

--- src/gui/qgsattributeform.cpp
+++ src/gui/qgsattributeform.cpp
@@ -1911,7 +1911,9 @@ void QgsAttributeForm::initPython()
           {
             // Read it into a string
             QTextStream inf( inputFile );
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
             inf.setCodec( "UTF-8" );
+#endif
             initCode = inf.readAll();
             inputFile->close();
           }
