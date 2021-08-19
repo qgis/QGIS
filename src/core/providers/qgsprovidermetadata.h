15e1e275e4053b100832a913398ebca46e080df2
vcloarec <vcloarec@gmail.com>
2021-08-18 20:57:45 -0400

    typo

--- src/core/providers/qgsprovidermetadata.h
+++ src/core/providers/qgsprovidermetadata.h
@@ -456,7 +456,7 @@ class CORE_EXPORT QgsProviderMetadata : public QObject
       const QStringList &createOptions = QStringList() ) SIP_FACTORY;
 
     /**
-     * Creates mesh data source from a file name \fileName and a driver \driverName, that is the mesh frame stored in file, memory or with other way (depending of the provider)
+     * Creates mesh data source from a file name \a fileName and a driver \a driverName, that is the mesh frame stored in file, memory or with other way (depending of the provider)
      * \since QGIS 3.16
      */
     virtual bool createMeshData(
