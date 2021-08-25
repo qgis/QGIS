ab0073955026f355f78574e236bed79d3b9e7299
Damiano Lombardi <damiano@opengis.ch>
2021-08-25 20:25:14 +0200

    Backport #44798 Fix attachment widget drag&drop relative path

--- src/gui/qgsfilewidget.cpp
+++ src/gui/qgsfilewidget.cpp
@@ -181,7 +181,7 @@ void QgsFileWidget::editLink()
 
 void QgsFileWidget::fileDropped( const QString &filePath )
 {
-  setSelectedFileNames( QStringList() << filePath );
+  setFilePath( relativePath( filePath, true ) );
   mLineEdit->selectAll();
   mLineEdit->setFocus( Qt::MouseFocusReason );
 }
