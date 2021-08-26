4cf3c97fbaff7b35b94a1c4fa57865273132bbaf
MorriganR <d_daryna@knu.ua>
2021-08-26 13:30:44 +0300

    set play env

--- .docker/docker-qgis-test.sh
+++ .docker/docker-qgis-test.sh
@@ -192,7 +192,26 @@ echo "List of skipped tests: $EXCLUDE_TESTS"
 echo "Print disk space"
 df -h
 
-python3 /root/QGIS/.ci/ctest2ci.py xvfb-run ctest -V $CTEST_OPTIONS -E "${EXCLUDE_TESTS}" -S /root/QGIS/.ci/config_test.ctest --output-on-failure
+python3 /root/QGIS/.ci/ctest2ci.py xvfb-run ctest -V $CTEST_OPTIONS -E "${EXCLUDE_TESTS}" -S /root/QGIS/.ci/config_test.ctest --output-on-failure || :
+
+# cd /root/QGIS || :
+# pwd
+# ls -la
+# cd build || :
+# tc qdisc add dev eth0 root netem delay 5ms || :
+#ping -c 2 httpbin || :
+TESTCOUNT=0
+TESTCOUNTFAIL=0
+while :
+do
+  echo "Test # $TESTCOUNT"
+  python3 /root/QGIS/.ci/ctest2ci.py xvfb-run ctest -V -R test_core_networkaccessmanager -S /root/QGIS/.ci/config_test.ctest --output-on-failure \
+    || echo $(( TESTCOUNTFAIL++ ))
+  if [[ $(( TESTCOUNT++ )) -eq 100 ]]; then
+    break
+  fi
+done
+echo "TESTCOUNTFAIL = $TESTCOUNTFAIL"
 
 echo "Print disk space"
 df -h
