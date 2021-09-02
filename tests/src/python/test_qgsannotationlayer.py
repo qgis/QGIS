d93933b8042d73dee13a05be17ca19dcfbec3167
Nyall Dawson <nyall.dawson@gmail.com>
2021-09-02 12:27:24 +1000

    Update tests/src/python/test_qgsannotationlayer.py

--- tests/src/python/test_qgsannotationlayer.py
+++ tests/src/python/test_qgsannotationlayer.py
@@ -466,7 +466,7 @@ class TestQgsAnnotationLayer(unittest.TestCase):
         expected = 'Polygon ((1325786 1449684, 1345882 1449684, 1345882 1469780, 1325786 1469780, 1325786 1449684))'
         result = [QgsGeometry.fromRect(i.boundingBox()).asWkt(0) for i in item_details if i.itemId() == i3_id][0]
         self.assertTrue(compareWkt(result, expected, tol=1000), "mismatch Expected:\n{}\nGot:\n{}\n".format(expected,
-                                                                                                           result))
+                                                                                                            result))
 
     def imageCheck(self, name, reference_image, image):
         TestQgsAnnotationLayer.report += "<h2>Render {}</h2>\n".format(name)
