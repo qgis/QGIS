89409421165bc2e43fca35168a1b0824a5d8385b
Antoine Facchini <antoine.facchini@oslandia.com>
2021-09-06 09:47:12 +0200

    add conditional layer merge processing

---
+++ src/analysis/processing/qgsalgorithmconditionalmerge.cpp
@@ -0,0 +1,126 @@
+/***************************************************************************
+                         qgsalgorithmconditionalbranch.cpp
+                         ---------------------
+    begin                : March 2020
+    copyright            : (C) 2020 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsalgorithmconditionalmerge.h"
+#include "qgsapplication.h"
+#include "qgsvectorlayer.h"
+
+///@cond PRIVATE
+
+QString QgsConditionalMergeAlgorithm::name() const
+{
+  return QStringLiteral( "conditionmerge" );
+}
+
+QString QgsConditionalMergeAlgorithm::displayName() const
+{
+  return QObject::tr( "Conditional merge" );
+}
+
+QStringList QgsConditionalMergeAlgorithm::tags() const
+{
+  return QObject::tr( "if,logic,test,merge" ).split( ',' );
+}
+
+QString QgsConditionalMergeAlgorithm::group() const
+{
+  return QObject::tr( "Modeler tools" );
+}
+
+QString QgsConditionalMergeAlgorithm::groupId() const
+{
+  return QStringLiteral( "modelertools" );
+}
+
+QString QgsConditionalMergeAlgorithm::shortHelpString() const
+{
+  return QObject::tr( "TODO" );
+}
+
+QgsConditionalMergeAlgorithm *QgsConditionalMergeAlgorithm::createInstance() const
+{
+  return new QgsConditionalMergeAlgorithm();
+}
+
+QgsProcessingAlgorithm::Flags QgsConditionalMergeAlgorithm::flags() const
+{
+  return FlagHideFromToolbox;
+}
+
+void QgsConditionalMergeAlgorithm::initAlgorithm( const QVariantMap & )
+{
+  addParameter( new QgsProcessingParameterExpression( QStringLiteral( "EXPRESSION" ), QObject::tr( "Routing expression" ),
+                QStringLiteral( "" ), QStringLiteral( "INPUT" ) ) );
+  addParameter( new QgsProcessingParameterVectorLayer( QStringLiteral( "LAYER_IF" ), QObject::tr( "Layer to transfer if condition" ), QList< int >(), QVariant(), true ) );
+  addParameter( new QgsProcessingParameterVectorLayer( QStringLiteral( "LAYER_ELSE" ), QObject::tr( "Layer to transfer else" ), QList< int >(), QVariant(), true ) );
+
+  addParameter( new QgsProcessingParameterFeatureSink( QStringLiteral( "OUTPUT" ), QObject::tr( "Layer" ) ) );
+}
+
+bool QgsConditionalMergeAlgorithm::prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback )
+{
+  mExpression = QgsExpression( parameterAsString( parameters, QStringLiteral( "EXPRESSION" ), context ) );
+  if ( mExpression.hasParserError() )
+  {
+    feedback->reportError( mExpression.parserErrorString() );
+    return false;
+  }
+
+  mExpressionContext = createExpressionContext( parameters, context );
+  mExpression.prepare( &mExpressionContext );
+
+  mLayerIf = parameterAsVectorLayer( parameters, QStringLiteral( "LAYER_IF" ), context );
+  mLayerElse = parameterAsVectorLayer( parameters, QStringLiteral( "LAYER_ELSE" ), context );
+
+  return true;
+}
+
+
+QVariantMap QgsConditionalMergeAlgorithm::processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback )
+{
+  QgsVectorLayer *selectedLayer = nullptr;
+
+  const bool res = mExpression.evaluate( &mExpressionContext ).toBool();
+  if ( mLayerIf && res )
+  {
+    selectedLayer = mLayerIf;
+  }
+  else if ( mLayerElse )
+  {
+    selectedLayer = mLayerElse;
+  }
+  else
+  {
+    throw QgsProcessingException( QStringLiteral( "No valid layer" ) );
+  }
+
+  QString sinkId;
+  std::unique_ptr< QgsFeatureSink > sink( parameterAsSink( parameters, QStringLiteral( "OUTPUT" ), context, sinkId, selectedLayer->fields(),
+                                          selectedLayer->wkbType(), selectedLayer->sourceCrs() ) );
+  if ( !sink )
+    throw QgsProcessingException( invalidSinkError( parameters, QStringLiteral( "OUTPUT" ) ) );
+
+  QgsFeatureIterator featureIt = selectedLayer->getFeatures();
+  sink->addFeatures( featureIt, QgsFeatureSink::FastInsert );
+
+  QVariantMap outputs;
+  outputs.insert( QStringLiteral( "OUTPUT" ), sinkId );
+  return outputs;
+}
+
+
+///@endcond PRIVATE
---
+++ src/analysis/processing/qgsalgorithmconditionalmerge.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+                         qgsalgorithmconditionalbranch.h
+                         ---------------------
+    begin                : September 2021
+    copyright            : (C) 2021 by Antoine Facchini
+    email                : zntoine dot facchini at oslandia dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSALGORITHMCONDITIONALMERGE_H
+#define QGSALGORITHMCONDITIONALMERGE_H
+
+#define SIP_NO_FILE
+
+#include "qgis_sip.h"
+#include "qgsprocessingalgorithm.h"
+
+class QgsProcessingModelAlgorithm;
+class QTableWidget;
+
+///@cond PRIVATE
+
+/**
+ * Feature filter algorithm for modeler.
+ * Accepts a list of expressions and names and creates outputs where
+ * matching features are sent to.
+ *
+ * \since QGIS 3.2
+ */
+class QgsConditionalMergeAlgorithm : public QgsProcessingAlgorithm
+{
+  public:
+    QgsConditionalMergeAlgorithm() = default;
+
+    void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) override;
+    QString name() const override;
+    QString displayName() const override;
+    QStringList tags() const override;
+    QString group() const override;
+    QString groupId() const override;
+    QString shortHelpString() const override;
+    QgsConditionalMergeAlgorithm *createInstance() const override SIP_FACTORY;
+
+
+  protected:
+    Flags flags() const override;
+    bool prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+    QVariantMap processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+
+  private:
+    QgsExpression mExpression;
+    QgsExpressionContext mExpressionContext;
+
+    QgsVectorLayer *mLayerIf;
+    QgsVectorLayer *mLayerElse;
+};
+
+///@endcond PRIVATE
+
+#endif // QGSALGORITHMCONDITIONALMERGE_H
