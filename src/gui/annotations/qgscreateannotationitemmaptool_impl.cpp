8f882912c7614f27a73ec82e23a6c512d905ba74
Nyall Dawson <nyall.dawson@gmail.com>
2021-09-08 14:50:27 +1000

    Add map tools for creating line/polygon annotation items
    These use the same base class as the vector feature capture tools,
    so support the same interaction (snapping, tracing, cad dock, backspace
    to remove vertices, etc) as drawing vector line/polygon features

--- src/gui/annotations/qgscreateannotationitemmaptool_impl.cpp
+++ src/gui/annotations/qgscreateannotationitemmaptool_impl.cpp
@@ -17,10 +17,14 @@
 #include "qgsmapmouseevent.h"
 #include "qgsannotationpointtextitem.h"
 #include "qgsannotationmarkeritem.h"
+#include "qgsannotationlineitem.h"
+#include "qgsannotationpolygonitem.h"
 #include "qgsannotationlayer.h"
 #include "qgsstyle.h"
 #include "qgsmapcanvas.h"
 #include "qgsmarkersymbol.h"
+#include "qgslinesymbol.h"
+#include "qgsfillsymbol.h"
 
 ///@cond PRIVATE
 
@@ -101,6 +105,166 @@ QgsCreateAnnotationItemMapToolHandler *QgsCreateMarkerItemMapTool::handler()
 QgsMapTool *QgsCreateMarkerItemMapTool::mapTool()
 {
   return this;
+}
+
+//
+// QgsMapToolCaptureAnnotationItem
+//
+
+QgsMapToolCaptureAnnotationItem::QgsMapToolCaptureAnnotationItem( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget, CaptureMode mode )
+  : QgsMapToolCapture( canvas, cadDockWidget, mode )
+{
+
+}
+
+QgsMapToolCapture::Capabilities QgsMapToolCaptureAnnotationItem::capabilities() const
+{
+  // no geometry validation!
+  return SupportsCurves;
+}
+
+
+//
+// QgsCreateLineMapTool
+//
+
+QgsCreateLineItemMapTool::QgsCreateLineItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget )
+  : QgsMapToolCaptureAnnotationItem( canvas, cadDockWidget, CaptureLine )
+  , mHandler( new QgsCreateAnnotationItemMapToolHandler( canvas, cadDockWidget ) )
+{
+
+}
+
+void QgsCreateLineItemMapTool::cadCanvasReleaseEvent( QgsMapMouseEvent *e )
+{
+  //add point to list and to rubber band
+  if ( e->button() == Qt::LeftButton )
+  {
+    const int error = addVertex( e->mapPoint(), e->mapPointMatch() );
+    if ( error == 2 )
+    {
+      //problem with coordinate transformation
+      emit messageEmitted( tr( "Cannot transform the point to the layers coordinate system" ), Qgis::MessageLevel::Warning );
+      return;
+    }
+
+    startCapturing();
+  }
+  else if ( e->button() == Qt::RightButton )
+  {
+    deleteTempRubberBand();
+
+    //find out bounding box of mCaptureList
+    if ( size() < 1 )
+    {
+      stopCapturing();
+      return;
+    }
+
+    // do it!
+    std::unique_ptr< QgsAbstractGeometry > geometry( captureCurve()->simplifiedTypeRef()->clone() );
+    if ( qgsgeometry_cast< QgsCurve * >( geometry.get() ) )
+    {
+      std::unique_ptr< QgsAnnotationLineItem > createdItem = std::make_unique< QgsAnnotationLineItem >( qgsgeometry_cast< QgsCurve * >( geometry.release() ) );
+      createdItem->setSymbol( qgis::down_cast< QgsLineSymbol * >( QgsSymbol::defaultSymbol( QgsWkbTypes::LineGeometry ) ) );
+      // set reference scale to match canvas scale, but don't enable it by default for marker items
+      createdItem->setSymbologyReferenceScale( canvas()->scale() );
+
+      mHandler->pushCreatedItem( createdItem.release() );
+    }
+    stopCapturing();
+  }
+}
+
+QgsCreateLineItemMapTool::~QgsCreateLineItemMapTool() = default;
+
+QgsCreateAnnotationItemMapToolHandler *QgsCreateLineItemMapTool::handler()
+{
+  return mHandler;
+}
+
+QgsMapTool *QgsCreateLineItemMapTool::mapTool()
+{
+  return this;
+}
+
+QgsMapLayer *QgsCreateLineItemMapTool::layer() const
+{
+  return mHandler->targetLayer();
+}
+
+
+
+//
+// QgsCreatePolygonItemMapTool
+//
+
+QgsCreatePolygonItemMapTool::QgsCreatePolygonItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget )
+  : QgsMapToolCaptureAnnotationItem( canvas, cadDockWidget, CapturePolygon )
+  , mHandler( new QgsCreateAnnotationItemMapToolHandler( canvas, cadDockWidget ) )
+{
+
+}
+
+void QgsCreatePolygonItemMapTool::cadCanvasReleaseEvent( QgsMapMouseEvent *e )
+{
+  //add point to list and to rubber band
+  if ( e->button() == Qt::LeftButton )
+  {
+    const int error = addVertex( e->mapPoint(), e->mapPointMatch() );
+    if ( error == 2 )
+    {
+      //problem with coordinate transformation
+      emit messageEmitted( tr( "Cannot transform the point to the layers coordinate system" ), Qgis::MessageLevel::Warning );
+      return;
+    }
+
+    startCapturing();
+  }
+  else if ( e->button() == Qt::RightButton )
+  {
+    deleteTempRubberBand();
+
+    //find out bounding box of mCaptureList
+    if ( size() < 1 )
+    {
+      stopCapturing();
+      return;
+    }
+
+    closePolygon();
+
+    std::unique_ptr< QgsAbstractGeometry > geometry( captureCurve()->simplifiedTypeRef()->clone() );
+    if ( qgsgeometry_cast< QgsCurve * >( geometry.get() ) )
+    {
+      std::unique_ptr< QgsCurvePolygon > newPolygon = std::make_unique< QgsCurvePolygon >();
+      newPolygon->setExteriorRing( qgsgeometry_cast< QgsCurve * >( geometry.release() ) );
+      std::unique_ptr< QgsAnnotationPolygonItem > createdItem = std::make_unique< QgsAnnotationPolygonItem >( newPolygon.release() );
+      createdItem->setSymbol( qgis::down_cast< QgsFillSymbol * >( QgsSymbol::defaultSymbol( QgsWkbTypes::PolygonGeometry ) ) );
+      // set reference scale to match canvas scale, but don't enable it by default for marker items
+      createdItem->setSymbologyReferenceScale( canvas()->scale() );
+
+      mHandler->pushCreatedItem( createdItem.release() );
+    }
+    stopCapturing();
+  }
+}
+
+QgsCreatePolygonItemMapTool::~QgsCreatePolygonItemMapTool() = default;
+
+QgsCreateAnnotationItemMapToolHandler *QgsCreatePolygonItemMapTool::handler()
+{
+  return mHandler;
+}
+
+QgsMapTool *QgsCreatePolygonItemMapTool::mapTool()
+{
+  return this;
+}
+
+QgsMapLayer *QgsCreatePolygonItemMapTool::layer() const
+{
+  return mHandler->targetLayer();
 }
 
 ///@endcond PRIVATE
