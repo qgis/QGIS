453e2ebfbd0550f372e73df66352701d7f150321
Nyall Dawson <nyall.dawson@gmail.com>
2021-09-13 12:29:04 +1000

    Fix qt < 5.14 build

--- src/core/qgis.h
+++ src/core/qgis.h
@@ -1225,6 +1225,20 @@ namespace qgis
 #endif
   }
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+namespace std
+{
+  template<> struct hash<QString>
+  {
+    std::size_t operator()( const QString &s ) const noexcept
+    {
+      return ( size_t ) qHash( s );
+    }
+  };
+}
+#endif
+
 ///@endcond
 #endif
 
