27ae2c9516f4b08fd798263750e8f4772f5c80ac
Sandro Santilli <strk@kbt.io>
2021-09-22 11:38:54 +0200

    Avoid re-preparing geos geometry from QgsVectorLayerFeatureIterator
    Does so by exposing a referenceGeometryEngine method to
    QgsFeatureRequest and copying the engine rather than creating
    a new one on each iteration.

--- src/core/qgsfeaturerequest.h
+++ src/core/qgsfeaturerequest.h
@@ -408,6 +408,16 @@ class CORE_EXPORT QgsFeatureRequest
     QgsGeometry referenceGeometry() const { return mReferenceGeometry; }
 
     /**
+     * Returns the reference geometry engine used for spatial filtering of features.
+     *
+     * This is to avoid re-creating the engine.
+     *
+     * \see referenceGeometry()
+     * \since QGIS 3.22
+     */
+    std::shared_ptr< QgsGeometryEngine > referenceGeometryEngine() const SIP_SKIP { return mReferenceGeometryEngine; }
+
+    /**
      * Returns the maximum distance from the referenceGeometry() of fetched
      * features, if spatialFilterType() is Qgis::SpatialFilterType::DistanceWithin.
      *
