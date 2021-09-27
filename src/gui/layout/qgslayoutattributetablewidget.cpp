0f73991474d646cc7f1fd86aaf41fdc09556b964
Alessandro Pasotti <elpaso@itopen.it>
2021-09-27 13:52:07 +0200

    Table layout item: set source layer in font tool btn
    Fixes #45098

--- src/gui/layout/qgslayoutattributetablewidget.cpp
+++ src/gui/layout/qgslayoutattributetablewidget.cpp
@@ -74,7 +74,9 @@ QgsLayoutAttributeTableWidget::QgsLayoutAttributeTableWidget( QgsLayoutFrame *fr
   mHeaderFontToolButton->setMode( QgsFontButton::ModeTextRenderer );
 
   mContentFontToolButton->registerExpressionContextGenerator( this );
+  mContentFontToolButton->setLayer( mTable->sourceLayer() );
   mHeaderFontToolButton->registerExpressionContextGenerator( this );
+  mHeaderFontToolButton->setLayer( mTable->sourceLayer() );
 
   blockAllSignals( true );
 
