1bcec6ac3884e029159025113111746bc2b3ed4a
Vincent Cloarec <vcloarec@gmail.com>
2021-09-30 09:51:24 -0400

    Update src/core/qgspointlocator.h

--- src/core/qgspointlocator.h
+++ src/core/qgspointlocator.h
@@ -296,7 +296,7 @@ class CORE_EXPORT QgsPointLocator : public QObject
             {
               try
               {
-                point.transform( transform, Qgis::TransformDirection::ReverseTransform );
+                point.transform( transform, Qgis::TransformDirection::Reverse );
               }
               catch ( QgsCsException & )
               {
