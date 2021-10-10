033bcd1c10759a53cc37833c6d6baa6857a1ac39
Tom Cummins <thomas.cummins@metoffice.gov.uk>
2021-10-10 03:31:00 +0100

    Added ability to add additional headers to the API Header authentication method

--- src/auth/apiheader/gui/qgsauthapiheaderedit.cpp
+++ src/auth/apiheader/gui/qgsauthapiheaderedit.cpp
@@ -17,17 +17,24 @@
 #include "qgsauthapiheaderedit.h"
 #include "ui_qgsauthapiheaderedit.h"
 
+#include "qgsapplication.h"
 
 QgsAuthApiHeaderEdit::QgsAuthApiHeaderEdit( QWidget *parent )
   : QgsAuthMethodEdit( parent )
 {
   setupUi( this );
-  connect( headerKeyEdit, &QLineEdit::textChanged, this, &QgsAuthApiHeaderEdit::textChanged );
+
+  connect( tblwdgHeaderPairs, &QTableWidget::itemSelectionChanged, this, &QgsAuthApiHeaderEdit::headerTableSelectionChanged );
+  connect( tblwdgHeaderPairs, &QTableWidget::cellChanged, this, &QgsAuthApiHeaderEdit::headerTableCellChanged );
+
+  connect( btnAddHeaderPair, &QToolButton::clicked, this, &QgsAuthApiHeaderEdit::addHeaderPair );
+  connect( btnRemoveHeaderPair, &QToolButton::clicked, this, &QgsAuthApiHeaderEdit::removeHeaderPair );
 }
+
 
 bool QgsAuthApiHeaderEdit::validateConfig()
 {
-  const bool curvalid = !headerKeyEdit->text().isEmpty();
+  const bool curvalid = !emptyHeadersKeysPresent() && tblwdgHeaderPairs->rowCount() != 0;
   if ( mValid != curvalid )
   {
     mValid = curvalid;
@@ -36,39 +43,144 @@ bool QgsAuthApiHeaderEdit::validateConfig()
   return curvalid;
 }
 
+
 QgsStringMap QgsAuthApiHeaderEdit::configMap() const
 {
-  QgsStringMap config;
-  config.insert( QStringLiteral( "headerKey" ), headerKeyEdit->text() );
-  config.insert( QStringLiteral( "headerValue" ), headerValueEdit->toPlainText() );
-
-  return config;
+  return headerPairs();
 }
+
 
 void QgsAuthApiHeaderEdit::loadConfig( const QgsStringMap &configmap )
 {
   clearConfig();
 
   mConfigMap = configmap;
-  headerKeyEdit->setText( configmap.value( QStringLiteral( "headerKey" ) ) );
-  headerValueEdit->setPlainText( configmap.value( QStringLiteral( "headerValue" ) ) );
+
+  populateHeaderPairs( configmap );
 
   validateConfig();
 }
+
 
 void QgsAuthApiHeaderEdit::resetConfig()
 {
   loadConfig( mConfigMap );
 }
 
+
 void QgsAuthApiHeaderEdit::clearConfig()
 {
-  headerKeyEdit->clear();
-  headerValueEdit->clear();
+  clearHeaderPairs();
 }
 
-void QgsAuthApiHeaderEdit::textChanged( const QString &txt )
+
+void QgsAuthApiHeaderEdit::addHeaderPair()
 {
-  Q_UNUSED( txt )
+  addHeaderPairRow( QString(), QString() );
+  tblwdgHeaderPairs->setFocus();
+  tblwdgHeaderPairs->setCurrentCell( tblwdgHeaderPairs->rowCount() - 1, 0 );
+  tblwdgHeaderPairs->edit( tblwdgHeaderPairs->currentIndex() );
+}
+
+
+void QgsAuthApiHeaderEdit::removeHeaderPair()
+{
+  tblwdgHeaderPairs->removeRow( tblwdgHeaderPairs->currentRow() );
   validateConfig();
+}
+
+
+void QgsAuthApiHeaderEdit::clearHeaderPairs()
+{
+  for ( int i = tblwdgHeaderPairs->rowCount(); i > 0 ; --i )
+  {
+    tblwdgHeaderPairs->removeRow( i - 1 );
+  }
+  validateConfig();
+}
+
+
+void QgsAuthApiHeaderEdit::populateHeaderPairs( const QgsStringMap &headerpairs, bool append )
+{
+  if ( !append )
+  {
+    clearHeaderPairs();
+  }
+
+  QgsStringMap::const_iterator i = headerpairs.constBegin();
+  while ( i != headerpairs.constEnd() )
+  {
+    addHeaderPairRow( i.key(), i.value() );
+    ++i;
+  }
+}
+
+
+void QgsAuthApiHeaderEdit::headerTableSelectionChanged()
+{
+  const bool hasPair = tblwdgHeaderPairs->selectedItems().count() > 0;
+  btnRemoveHeaderPair->setEnabled( hasPair );
+  validateConfig();
+}
+
+
+void QgsAuthApiHeaderEdit::headerTableCellChanged( const int row, const int column )
+{
+  QgsDebugMsg( QStringLiteral( "Cell updated: row %1 column %2" ).arg( row ).arg( column ) );
+  validateConfig();
+}
+
+
+bool QgsAuthApiHeaderEdit::emptyHeadersKeysPresent()
+{
+  const int rowCount = tblwdgHeaderPairs->rowCount();
+  QgsDebugMsg( QStringLiteral( "Validate header table contains valid header keys for %1 rows" ).arg( rowCount ) );
+
+  for ( int i = 0; i < rowCount; ++i )
+  {
+    if ( tblwdgHeaderPairs->item( i, 0 )->text().isEmpty() )
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+void QgsAuthApiHeaderEdit::addHeaderPairRow( const QString &key, const QString &val )
+{
+  const int rowCnt = tblwdgHeaderPairs->rowCount();
+  tblwdgHeaderPairs->insertRow( rowCnt );
+
+  const Qt::ItemFlags itmFlags = Qt::ItemIsEnabled | Qt::ItemIsSelectable
+                                 | Qt::ItemIsEditable | Qt::ItemIsDropEnabled;
+
+  QTableWidgetItem *keyItm = new QTableWidgetItem( key );
+  keyItm->setFlags( itmFlags );
+  tblwdgHeaderPairs->setItem( rowCnt, 0, keyItm );
+
+  QTableWidgetItem *valItm = new QTableWidgetItem( val );
+  keyItm->setFlags( itmFlags );
+  tblwdgHeaderPairs->setItem( rowCnt, 1, valItm );
+
+  validateConfig();
+}
+
+
+QgsStringMap QgsAuthApiHeaderEdit::headerPairs() const
+{
+  QgsStringMap headerPairs;
+  for ( int i = 0; i < tblwdgHeaderPairs->rowCount(); ++i )
+  {
+    if ( tblwdgHeaderPairs->item( i, 0 )->text().isEmpty() )
+    {
+      continue;
+    }
+
+    const QString headerKey = tblwdgHeaderPairs->item( i, 0 )->text();
+    const QString headerValue = tblwdgHeaderPairs->item( i, 1 )->text();
+    headerPairs.insert( headerKey, headerValue );
+  }
+  return headerPairs;
 }
