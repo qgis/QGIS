f5fddbf86e820d892b6c61797e03c0d1958aefa5
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-10-11 11:11:11 +0200

    do not check spelling in .sip.in files
    since they are auto-generated

--- scripts/spell_check/check_spelling.sh
+++ scripts/spell_check/check_spelling.sh
@@ -26,7 +26,7 @@
 EXCLUDE_SCRIPT_LIST='(\.(xml|sip|pl|sh|badquote|cmake(\.in)?)|^(debian/copyright|cmake_templates/.*|tests/testdata/labeling/README.rst|tests/testdata/font/QGIS-Vera/COPYRIGHT.TXT|doc/NEWS\.html|debian/build/))$'
 
 # always exclude these external files
-EXCLUDE_EXTERNAL_LIST='((\.(svg|qgs|laz|las|png|lock))|resources/server/src/.*|resources/server/api/ogc/static/landingpage/js/.*|tests/testdata/.*|doc/api_break.dox)$'
+EXCLUDE_EXTERNAL_LIST='((\.(svg|qgs|laz|las|png|lock|sip\.in))|resources/server/src/.*|resources/server/api/ogc/static/landingpage/js/.*|tests/testdata/.*|doc/api_break.dox)$'
 
 DIR=$(git rev-parse --show-toplevel)/scripts/spell_check
 
