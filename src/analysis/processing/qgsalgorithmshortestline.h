72d931e85147b7595f69025b8b6711c467ced348
Clemens Raffler <clemens.raffler@gmail.com>
2021-10-11 23:10:00 +0200

    port shortest line algorithm to c++

---
+++ src/analysis/processing/qgsalgorithmshortestline.h
@@ -0,0 +1,11 @@
+#ifndef QGSALGORITHMSHORTESTLINE_H
+#define QGSALGORITHMSHORTESTLINE_H
+
+
+class qgsalgorithmshortestline
+{
+public:
+  qgsalgorithmshortestline();
+};
+
+#endif // QGSALGORITHMSHORTESTLINE_H
