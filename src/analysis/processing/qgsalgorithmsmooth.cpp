b8db06e5d20c77e93e02bb2db38f950dea31873a
Nyall Dawson <nyall.dawson@gmail.com>
2021-10-14 10:45:03 +1000

    Don't restrict smooth algorithm outputs for use as inputs to line parameters
    Fixes #45503

--- src/analysis/processing/qgsalgorithmsmooth.cpp
+++ src/analysis/processing/qgsalgorithmsmooth.cpp
@@ -49,11 +49,6 @@ QString QgsSmoothAlgorithm::outputName() const
   return QObject::tr( "Smoothed" );
 }
 
-QgsProcessing::SourceType QgsSmoothAlgorithm::outputLayerType() const
-{
-  return QgsProcessing::TypeVectorLine;
-}
-
 QString QgsSmoothAlgorithm::shortHelpString() const
 {
   return QObject::tr( "This algorithm smooths the geometries in a line or polygon layer. It creates a new layer with the "
