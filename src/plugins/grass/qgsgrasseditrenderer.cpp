cda03d80d62a069cfc45e857830b9ad15e725c4e
Nyall Dawson <nyall.dawson@gmail.com>
2021-10-21 14:51:54 +1000

    Fix grass build

--- src/plugins/grass/qgsgrasseditrenderer.cpp
+++ src/plugins/grass/qgsgrasseditrenderer.cpp
@@ -64,9 +64,9 @@ QgsGrassEditRenderer::QgsGrassEditRenderer()
   markerLayers << markerSymbolLayer;
   QgsMarkerSymbol *markerSymbol = new QgsMarkerSymbol( markerLayers );
   firstVertexMarkerLine->setSubSymbol( markerSymbol );
-  firstVertexMarkerLine->setPlacement( QgsTemplatedLineSymbolLayerBase::FirstVertex );
+  firstVertexMarkerLine->setPlacement( Qgis::MarkerLinePlacement::FirstVertex );
   QgsMarkerLineSymbolLayer *lastVertexMarkerLine = static_cast<QgsMarkerLineSymbolLayer *>( firstVertexMarkerLine->clone() );
-  lastVertexMarkerLine->setPlacement( QgsTemplatedLineSymbolLayerBase::LastVertex );
+  lastVertexMarkerLine->setPlacement( Qgis::MarkerLinePlacement::LastVertex );
   for ( int value : colors.keys() )
   {
     QgsSymbol *symbol = QgsSymbol::defaultSymbol( QgsWkbTypes::LineGeometry );
