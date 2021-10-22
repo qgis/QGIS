e4b611db7dcc81a9f98a465edce78db386dd6271
Jochen Topf <jochen@topf.org>
2021-10-22 16:22:32 +0200

    Make some member functions const, because they can be
    Also removes the getDefaultInterval() member function from the .h file
    because it is not used and doesn't have an implementation.
    
    And makes an argument a const ptr instead of a non-const ptr, because it
    is only used in const context.

--- src/app/decorations/qgsdecorationgrid.h
+++ src/app/decorations/qgsdecorationgrid.h
@@ -126,18 +126,16 @@ class APP_EXPORT QgsDecorationGrid: public QgsDecorationItem
 
     //! Sets map unit type
     void setMapUnits( QgsUnitTypes::DistanceUnit t ) { mMapUnits = t; }
-    QgsUnitTypes::DistanceUnit mapUnits() { return mMapUnits; }
+    QgsUnitTypes::DistanceUnit mapUnits() const { return mMapUnits; }
 
     //! Sets mapUnits value
     void setDirty( bool dirty = true );
     bool isDirty();
 
     //! Computes interval that is approx. 1/5 of canvas extent
-    bool getIntervalFromExtent( double *values, bool useXAxis = true );
+    bool getIntervalFromExtent( double *values, bool useXAxis = true ) const;
     //! Computes interval from current raster layer
-    bool getIntervalFromCurrentLayer( double *values );
-
-    double getDefaultInterval( bool useXAxis = true );
+    bool getIntervalFromCurrentLayer( double *values ) const;
 
   public slots:
     //! Sets values on the gui when a project is read or the gui first loaded
@@ -214,7 +212,7 @@ class APP_EXPORT QgsDecorationGrid: public QgsDecorationItem
     int yGridLines( const QgsMapSettings &mapSettings, QList< QPair< qreal, QLineF > > &lines ) const;
 
     //! Returns the item border of a point (in item coordinates)
-    Border borderForLineCoord( QPointF point, QPainter *p ) const;
+    Border borderForLineCoord( QPointF point, const QPainter *p ) const;
 
     QgsTextFormat mTextFormat;
 };
