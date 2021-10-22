0a374a9c27dc600e87e7afd1623dced075cc7c12
Sandro Santilli <strk@kbt.io>
2021-10-22 11:03:38 +0200

    Do not pass -projwin to gdal_translate when given rectangle is Null

--- python/plugins/processing/algs/gdal/ClipRasterByExtent.py
+++ python/plugins/processing/algs/gdal/ClipRasterByExtent.py
@@ -132,13 +132,14 @@ class ClipRasterByExtent(GdalAlgorithm):
         out = self.parameterAsOutputLayer(parameters, self.OUTPUT, context)
         self.setOutputValue(self.OUTPUT, out)
 
-        arguments = [
-            '-projwin',
-            str(bbox.xMinimum()),
-            str(bbox.yMaximum()),
-            str(bbox.xMaximum()),
-            str(bbox.yMinimum()),
-        ]
+        arguments = []
+
+        if not bbox.isNull():
+            arguments.append('-projwin')
+            arguments.append(str(bbox.xMinimum()))
+            arguments.append(str(bbox.yMaximum()))
+            arguments.append(str(bbox.xMaximum()))
+            arguments.append(str(bbox.yMinimum()))
 
         crs = inLayer.crs()
         if override_crs and crs.isValid():
