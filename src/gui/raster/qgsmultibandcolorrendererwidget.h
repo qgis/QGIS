ac1957a699d5a8655ed5b094d64ddb1eb16cbfc6
Nyall Dawson <nyall.dawson@gmail.com>
2021-10-29 09:33:11 +1000

    Add some missing /Factory/ annotations

--- src/gui/raster/qgsmultibandcolorrendererwidget.h
+++ src/gui/raster/qgsmultibandcolorrendererwidget.h
@@ -41,7 +41,7 @@ class GUI_EXPORT QgsMultiBandColorRendererWidget: public QgsRasterRendererWidget
     QgsMultiBandColorRendererWidget( QgsRasterLayer *layer, const QgsRectangle &extent = QgsRectangle() );
     static QgsRasterRendererWidget *create( QgsRasterLayer *layer, const QgsRectangle &extent ) { return new QgsMultiBandColorRendererWidget( layer, extent ); }
 
-    QgsRasterRenderer *renderer() override;
+    QgsRasterRenderer *renderer() override SIP_FACTORY;
     void setMapCanvas( QgsMapCanvas *canvas ) override;
 
     /**
