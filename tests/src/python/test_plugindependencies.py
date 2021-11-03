78ad0dbb9abc86368222d0379c48b1e88bfebc42
Germán Carrillo <gcarrillo@linuxmail.org>
2021-11-03 17:00:40 -0500

    Add tests for experimental plugin dependencies

--- tests/src/python/test_plugindependencies.py
+++ tests/src/python/test_plugindependencies.py
@@ -71,12 +71,18 @@ class PluginDependenciesTest(unittest.TestCase):
 
     def test_find_dependencies(self):
 
-        to_install, to_upgrade, not_found = find_dependencies('qgisce', self.plugin_data, plugin_deps={'InaSAFE': None}, installed_plugins=self.installed_plugins)
+        to_install, to_upgrade, not_found = find_dependencies('qgisce',
+                                                              self.plugin_data,
+                                                              plugin_deps={'InaSAFE': None},
+                                                              installed_plugins=self.installed_plugins)
         self.assertEqual(to_install, {})
         self.assertEqual(to_upgrade, {})
         self.assertEqual(not_found, {})
 
-        to_install, to_upgrade, not_found = find_dependencies('qgisce', self.plugin_data, plugin_deps={'InaSAFE': '110.1'}, installed_plugins=self.installed_plugins)
+        to_install, to_upgrade, not_found = find_dependencies('qgisce',
+                                                              self.plugin_data,
+                                                              plugin_deps={'InaSAFE': '110.1'},
+                                                              installed_plugins=self.installed_plugins)
         self.assertEqual(to_install, {})
         self.assertEqual(to_upgrade, {})
         self.assertEqual(not_found['InaSAFE']['version_installed'], '5.0.0')
@@ -84,19 +90,33 @@ class PluginDependenciesTest(unittest.TestCase):
         # QuickWkt is installed, version is not specified: ignore
         installed_plugins = self.installed_plugins
         installed_plugins['QuickWKT'] = '2.1'
-        to_install, to_upgrade, not_found = find_dependencies('qgisce', self.plugin_data, plugin_deps={'QuickMapServices': '0.19.10.1', 'QuickWKT': None}, installed_plugins=self.installed_plugins)
+        to_install, to_upgrade, not_found = find_dependencies('qgisce',
+                                                              self.plugin_data,
+                                                              plugin_deps={'QuickMapServices': '0.19.10.1',
+                                                                           'QuickWKT': None},
+                                                              installed_plugins=self.installed_plugins)
         self.assertEqual(to_install['QuickMapServices']['version_required'], '0.19.10.1')
+        self.assertEqual(to_install['QuickMapServices']['version_available'], '0.19.10.1')
+        self.assertEqual(to_install['QuickMapServices']['use_stable_version'], True)
         self.assertEqual(to_upgrade, {})
         self.assertEqual(not_found, {})
 
         # QuickWkt is installed, version requires upgrade and it's in the repo: upgrade
-        to_install, to_upgrade, not_found = find_dependencies('qgisce', self.plugin_data, plugin_deps={'QuickWKT': '3.1'}, installed_plugins=installed_plugins)
+        to_install, to_upgrade, not_found = find_dependencies('qgisce',
+                                                              self.plugin_data,
+                                                              plugin_deps={'QuickWKT': '3.1'},
+                                                              installed_plugins=installed_plugins)
         self.assertEqual(to_install, {})
         self.assertEqual(to_upgrade['QuickWKT']['version_required'], '3.1')
+        self.assertEqual(to_upgrade['QuickWKT']['version_available'], '3.1')
+        self.assertEqual(to_upgrade['QuickWKT']['use_stable_version'], True)
         self.assertEqual(not_found, {})
 
         # QuickWkt is installed, version requires upgrade and it's NOT in the repo: not found
-        to_install, to_upgrade, not_found = find_dependencies('qgisce', self.plugin_data, plugin_deps={'QuickWKT': '300.11234'}, installed_plugins=installed_plugins)
+        to_install, to_upgrade, not_found = find_dependencies('qgisce',
+                                                              self.plugin_data,
+                                                              plugin_deps={'QuickWKT': '300.11234'},
+                                                              installed_plugins=installed_plugins)
         self.assertEqual(to_install, {})
         self.assertEqual(to_upgrade, {})
         self.assertEqual(not_found['QuickWKT']['version_required'], '300.11234')
@@ -105,8 +125,66 @@ class PluginDependenciesTest(unittest.TestCase):
 
         # Installed version is > than required: ignore (no downgrade is currently possible)
         installed_plugins['QuickWKT'] = '300.1'
-        to_install, to_upgrade, not_found = find_dependencies('qgisce', self.plugin_data, plugin_deps={'QuickWKT': '1.2'}, installed_plugins=installed_plugins)
+        to_install, to_upgrade, not_found = find_dependencies('qgisce',
+                                                              self.plugin_data,
+                                                              plugin_deps={'QuickWKT': '1.2'},
+                                                              installed_plugins=installed_plugins)
         self.assertEqual(to_install, {})
+        self.assertEqual(to_upgrade, {})
+        self.assertEqual(not_found, {})
+
+        # A plugin offers both stable and experimental versions. A dependent plugin requires the experimental one.
+        to_install, to_upgrade, not_found = find_dependencies('LADM-COL-Add-on-Ambiente',
+                                                              self.plugin_data,
+                                                              plugin_deps={'Asistente LADM-COL': '3.2.0-beta-1'},
+                                                              installed_plugins=self.installed_plugins)
+        self.assertEqual(to_install['Asistente LADM-COL']['version_required'], '3.2.0-beta-1')
+        self.assertEqual(to_install['Asistente LADM-COL']['version_available'], '3.2.0-beta-1')
+        self.assertEqual(to_install['Asistente LADM-COL']['use_stable_version'], False)
+        self.assertEqual(to_upgrade, {})
+        self.assertEqual(not_found, {})
+
+        # A plugin offers both stable and experimental versions. A dependent plugin requires the stable one.
+        to_install, to_upgrade, not_found = find_dependencies('LADM-COL-Add-on-Ambiente',
+                                                              self.plugin_data,
+                                                              plugin_deps={'Asistente LADM-COL': '3.1.9'},
+                                                              installed_plugins=self.installed_plugins)
+        self.assertEqual(to_install['Asistente LADM-COL']['version_required'], '3.1.9')
+        self.assertEqual(to_install['Asistente LADM-COL']['version_available'], '3.1.9')
+        self.assertEqual(to_install['Asistente LADM-COL']['use_stable_version'], True)
+        self.assertEqual(to_upgrade, {})
+        self.assertEqual(not_found, {})
+
+        # A plugin offers both stable and experimental versions. If no version is required, choose the stable one.
+        to_install, to_upgrade, not_found = find_dependencies('LADM-COL-Add-on-Ambiente',
+                                                              self.plugin_data,
+                                                              plugin_deps={'Asistente LADM-COL': None},
+                                                              installed_plugins=self.installed_plugins)
+        self.assertEqual(to_install['Asistente LADM-COL']['version_required'], None)
+        self.assertEqual(to_install['Asistente LADM-COL']['version_available'], '3.1.9')
+        self.assertEqual(to_install['Asistente LADM-COL']['use_stable_version'], True)
+        self.assertEqual(to_upgrade, {})
+        self.assertEqual(not_found, {})
+
+        # A plugin only offers experimental version. If the experimental version is required, give it to him.
+        to_install, to_upgrade, not_found = find_dependencies('dependent-on-unique_values_viewer',
+                                                              self.plugin_data,
+                                                              plugin_deps={'UniqueValuesViewer': '0.2'},
+                                                              installed_plugins=self.installed_plugins)
+        self.assertEqual(to_install['UniqueValuesViewer']['version_required'], '0.2')
+        self.assertEqual(to_install['UniqueValuesViewer']['version_available'], '0.2')
+        self.assertEqual(to_install['UniqueValuesViewer']['use_stable_version'], False)
+        self.assertEqual(to_upgrade, {})
+        self.assertEqual(not_found, {})
+
+        # A plugin only offers experimental version. If no version is required, choose the experimental one.
+        to_install, to_upgrade, not_found = find_dependencies('dependent-on-unique_values_viewer',
+                                                              self.plugin_data,
+                                                              plugin_deps={'UniqueValuesViewer': None},
+                                                              installed_plugins=self.installed_plugins)
+        self.assertEqual(to_install['UniqueValuesViewer']['version_required'], None)
+        self.assertEqual(to_install['UniqueValuesViewer']['version_available'], '0.2')
+        self.assertEqual(to_install['UniqueValuesViewer']['use_stable_version'], False)
         self.assertEqual(to_upgrade, {})
         self.assertEqual(not_found, {})
 
