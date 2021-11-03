c87d9ff3929dcedf9de00dfc61c075cd6e828d14
Alessandro Pasotti <elpaso@itopen.it>
2021-11-03 11:30:08 +0100

    Wrong indentation

--- tests/src/python/test_qgsvectorlayer.py
+++ tests/src/python/test_qgsvectorlayer.py
@@ -3660,31 +3660,31 @@ class TestQgsVectorLayerTransformContext(unittest.TestCase):
         self.assertEqual(layer.featureCount(), 0)
         self.assertEqual(layer.selectedFeatureIds(), [])
 
-        def testCommitChangesReportsDeletedFeatureIDs(self):
-            """
-            Tests if commitChanges emits "featuresDeleted" with all deleted feature IDs,
-            e.g. in case (negative) temporary FIDs are converted into (positive) persistent FIDs.
-            """
-            temp_fids = []
+    def testCommitChangesReportsDeletedFeatureIDs(self):
+        """
+        Tests if commitChanges emits "featuresDeleted" with all deleted feature IDs,
+        e.g. in case (negative) temporary FIDs are converted into (positive) persistent FIDs.
+        """
+        temp_fids = []
 
-            def onFeaturesDeleted(deleted_fids):
-                self.assertEqual(len(deleted_fids), len(temp_fids),
-                                 msg=f'featuresDeleted returned {len(deleted_fids)} instead of 2 deleted feature IDs: '
-                                     f'{deleted_fids}')
-                for d in deleted_fids:
-                    self.assertTrue(d in temp_fids)
+        def onFeaturesDeleted(deleted_fids):
+            self.assertEqual(len(deleted_fids), len(temp_fids),
+                             msg=f'featuresDeleted returned {len(deleted_fids)} instead of 2 deleted feature IDs: '
+                             f'{deleted_fids}')
+            for d in deleted_fids:
+                self.assertTrue(d in temp_fids)
 
-            layer = QgsVectorLayer("point?crs=epsg:4326&field=name:string", "Scratch point layer", "memory")
-            layer.featuresDeleted.connect(onFeaturesDeleted)
+        layer = QgsVectorLayer("point?crs=epsg:4326&field=name:string", "Scratch point layer", "memory")
+        layer.featuresDeleted.connect(onFeaturesDeleted)
 
-            layer.startEditing()
-            layer.beginEditCommand('add 2 features')
-            layer.addFeature(QgsFeature(layer.fields()))
-            layer.addFeature(QgsFeature(layer.fields()))
-            layer.endEditCommand()
-            temp_fids.extend(layer.allFeatureIds())
+        layer.startEditing()
+        layer.beginEditCommand('add 2 features')
+        layer.addFeature(QgsFeature(layer.fields()))
+        layer.addFeature(QgsFeature(layer.fields()))
+        layer.endEditCommand()
+        temp_fids.extend(layer.allFeatureIds())
 
-            layer.commitChanges()
+        layer.commitChanges()
 
     def testSubsetStringInvalidLayer(self):
         """
