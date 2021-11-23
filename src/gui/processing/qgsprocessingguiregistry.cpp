438dc72fa90cb9c9e03690651b1c89375d52e7e2
Alexander Bruy <alexander.bruy@gmail.com>
2021-11-23 14:31:18 +0200

    [processing] widget wrapper for point cloud destination parameter

--- src/gui/processing/qgsprocessingguiregistry.cpp
+++ src/gui/processing/qgsprocessingguiregistry.cpp
@@ -81,6 +81,8 @@ QgsProcessingGuiRegistry::QgsProcessingGuiRegistry()
   addParameterWidgetFactory( new QgsProcessingMeshDatasetTimeWidgetWrapper() );
   addParameterWidgetFactory( new QgsProcessingPointCloudLayerWidgetWrapper() );
   addParameterWidgetFactory( new QgsProcessingAnnotationLayerWidgetWrapper() );
+  addParameterWidgetFactory( new QgsProcessingAnnotationLayerWidgetWrapper() );
+  addParameterWidgetFactory( new QgsProcessingPointCloudDestinationWidgetWrapper() );
 }
 
 QgsProcessingGuiRegistry::~QgsProcessingGuiRegistry()
