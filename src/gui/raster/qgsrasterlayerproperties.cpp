6f23c45623314825026b9f6fed7a5b7d149d44f9
Nyall Dawson <nyall.dawson@gmail.com>
2021-11-24 14:19:01 +1000

    Expose clipping blend modes for layers which are contained within group layers

--- src/gui/raster/qgsrasterlayerproperties.cpp
+++ src/gui/raster/qgsrasterlayerproperties.cpp
@@ -67,6 +67,7 @@
 #include "qgsrasterlayertemporalproperties.h"
 #include "qgsdoublevalidator.h"
 #include "qgsmaplayerconfigwidgetfactory.h"
+#include "qgsprojectutils.h"
 
 #include "qgsrasterlayertemporalpropertieswidget.h"
 #include "qgsprojecttimesettings.h"
@@ -385,6 +386,7 @@ QgsRasterLayerProperties::QgsRasterLayerProperties( QgsMapLayer *lyr, QgsMapCanv
   }
 
   //blend mode
+  mBlendModeComboBox->setShowClippingModes( QgsProjectUtils::layerIsContainedInGroupLayer( QgsProject::instance(), mRasterLayer ) );
   mBlendModeComboBox->setBlendMode( mRasterLayer->blendMode() );
 
   //transparency band
