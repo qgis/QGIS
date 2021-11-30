67bdd431cd61b330c39f6cee28a6d4e6f6e0ba44
Nyall Dawson <nyall.dawson@gmail.com>
2021-11-30 14:47:22 +1000

    Fix rendering of shadows when justify align is used
    (cherry picked from commit 7ee49f1a78be7941d77abe4ba4151f3fbcfab2c7)

--- src/core/textrenderer/qgstextrenderer.cpp
+++ src/core/textrenderer/qgstextrenderer.cpp
@@ -1523,6 +1523,7 @@ void QgsTextRenderer::drawTextInternalHorizontal( QgsRenderContext &context, con
           if ( !isFinalLineInParagraph && labelWidest > labelWidth )
           {
             calculateExtraSpacingForLineJustification( labelWidest - labelWidth, block, extraWordSpace, extraLetterSpace );
+            labelWidth = labelWidest;
           }
           break;
 
