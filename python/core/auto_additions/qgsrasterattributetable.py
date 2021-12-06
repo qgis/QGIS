7debc9148093d0620e305599de3749a100e8dd2c
Alessandro Pasotti <elpaso@itopen.it>
2021-12-06 19:03:46 +0100

    sipify

---
+++ python/core/auto_additions/qgsrasterattributetable.py
@@ -0,0 +1,28 @@
+# The following has been generated automatically from src/core/raster/qgsrasterattributetable.h
+# monkey patching scoped based enum
+QgsRasterAttributeTable.FieldUsage.Generic.__doc__ = "Field usage Generic"
+QgsRasterAttributeTable.FieldUsage.PixelCount.__doc__ = "Field usage PixelCount"
+QgsRasterAttributeTable.FieldUsage.Name.__doc__ = "Field usage Name"
+QgsRasterAttributeTable.FieldUsage.Min.__doc__ = "Field usage Min"
+QgsRasterAttributeTable.FieldUsage.Max.__doc__ = "Field usage Max"
+QgsRasterAttributeTable.FieldUsage.MinMax.__doc__ = "Field usage MinMax"
+QgsRasterAttributeTable.FieldUsage.Red.__doc__ = "Field usage Red"
+QgsRasterAttributeTable.FieldUsage.Green.__doc__ = "Field usage Green"
+QgsRasterAttributeTable.FieldUsage.Blue.__doc__ = "Field usage Blue"
+QgsRasterAttributeTable.FieldUsage.Alpha.__doc__ = "Field usage Alpha"
+QgsRasterAttributeTable.FieldUsage.RedMin.__doc__ = "Field usage RedMin"
+QgsRasterAttributeTable.FieldUsage.GreenMin.__doc__ = "Field usage GreenMin"
+QgsRasterAttributeTable.FieldUsage.BlueMin.__doc__ = "Field usage BlueMin"
+QgsRasterAttributeTable.FieldUsage.AlphaMin.__doc__ = "Field usage AlphaMin"
+QgsRasterAttributeTable.FieldUsage.RedMax.__doc__ = "Field usage RedMax"
+QgsRasterAttributeTable.FieldUsage.GreenMax.__doc__ = "Field usage GreenMax"
+QgsRasterAttributeTable.FieldUsage.BlueMax.__doc__ = "Field usage BlueMax"
+QgsRasterAttributeTable.FieldUsage.AlphaMax.__doc__ = "Field usage AlphaMax"
+QgsRasterAttributeTable.FieldUsage.MaxCount.__doc__ = "Field usage MaxCount"
+QgsRasterAttributeTable.FieldUsage.__doc__ = '\n\n' + '* ``Generic``: ' + QgsRasterAttributeTable.FieldUsage.Generic.__doc__ + '\n' + '* ``PixelCount``: ' + QgsRasterAttributeTable.FieldUsage.PixelCount.__doc__ + '\n' + '* ``Name``: ' + QgsRasterAttributeTable.FieldUsage.Name.__doc__ + '\n' + '* ``Min``: ' + QgsRasterAttributeTable.FieldUsage.Min.__doc__ + '\n' + '* ``Max``: ' + QgsRasterAttributeTable.FieldUsage.Max.__doc__ + '\n' + '* ``MinMax``: ' + QgsRasterAttributeTable.FieldUsage.MinMax.__doc__ + '\n' + '* ``Red``: ' + QgsRasterAttributeTable.FieldUsage.Red.__doc__ + '\n' + '* ``Green``: ' + QgsRasterAttributeTable.FieldUsage.Green.__doc__ + '\n' + '* ``Blue``: ' + QgsRasterAttributeTable.FieldUsage.Blue.__doc__ + '\n' + '* ``Alpha``: ' + QgsRasterAttributeTable.FieldUsage.Alpha.__doc__ + '\n' + '* ``RedMin``: ' + QgsRasterAttributeTable.FieldUsage.RedMin.__doc__ + '\n' + '* ``GreenMin``: ' + QgsRasterAttributeTable.FieldUsage.GreenMin.__doc__ + '\n' + '* ``BlueMin``: ' + QgsRasterAttributeTable.FieldUsage.BlueMin.__doc__ + '\n' + '* ``AlphaMin``: ' + QgsRasterAttributeTable.FieldUsage.AlphaMin.__doc__ + '\n' + '* ``RedMax``: ' + QgsRasterAttributeTable.FieldUsage.RedMax.__doc__ + '\n' + '* ``GreenMax``: ' + QgsRasterAttributeTable.FieldUsage.GreenMax.__doc__ + '\n' + '* ``BlueMax``: ' + QgsRasterAttributeTable.FieldUsage.BlueMax.__doc__ + '\n' + '* ``AlphaMax``: ' + QgsRasterAttributeTable.FieldUsage.AlphaMax.__doc__ + '\n' + '* ``MaxCount``: ' + QgsRasterAttributeTable.FieldUsage.MaxCount.__doc__
+# --
+# monkey patching scoped based enum
+QgsRasterAttributeTable.RatType.Thematic.__doc__ = ""
+QgsRasterAttributeTable.RatType.Athematic.__doc__ = ""
+QgsRasterAttributeTable.RatType.__doc__ = '\n\n' + '* ``Thematic``: ' + QgsRasterAttributeTable.RatType.Thematic.__doc__ + '\n' + '* ``Athematic``: ' + QgsRasterAttributeTable.RatType.Athematic.__doc__
+# --
