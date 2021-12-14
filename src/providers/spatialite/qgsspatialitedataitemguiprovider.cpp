be4516917525386cd1854733549960e8d7769485
Nyall Dawson <nyall.dawson@gmail.com>
2021-12-14 10:33:05 +1000

    [browser] Fix creation of new spatialite databases through browser
    Fixes #45725

--- src/providers/spatialite/qgsspatialitedataitemguiprovider.cpp
+++ src/providers/spatialite/qgsspatialitedataitemguiprovider.cpp
@@ -27,6 +27,7 @@
 #include "qgssettings.h"
 #include "qgsvectorlayer.h"
 #include "qgsvectorlayerexporter.h"
+#include "qgsfileutils.h"
 
 #include <QFileDialog>
 #include <QMessageBox>
@@ -110,18 +111,25 @@ void QgsSpatiaLiteDataItemGuiProvider::createDatabase( QgsDataItem *item )
   const QgsSettings settings;
   const QString lastUsedDir = settings.value( QStringLiteral( "UI/lastSpatiaLiteDir" ), QDir::homePath() ).toString();
 
-  const QString filename = QFileDialog::getSaveFileName( nullptr, tr( "New SpatiaLite Database File" ),
-                           lastUsedDir,
-                           tr( "SpatiaLite" ) + " (*.sqlite *.db *.sqlite3 *.db3 *.s3db)" );
+  QString filename = QFileDialog::getSaveFileName( nullptr, tr( "New SpatiaLite Database File" ),
+                     lastUsedDir,
+                     tr( "SpatiaLite" ) + " (*.sqlite *.db *.sqlite3 *.db3 *.s3db)" );
   if ( filename.isEmpty() )
     return;
+
+  filename = QgsFileUtils::ensureFileNameHasExtension( filename, QStringList() << QStringLiteral( "sqlite" ) << QStringLiteral( "db" ) << QStringLiteral( "sqlite3" )
+             << QStringLiteral( "db3" ) << QStringLiteral( "s3db" ) );
 
   QString errCause;
   if ( SpatiaLiteUtils::createDb( filename, errCause ) )
   {
     QgsProviderMetadata *providerMetadata = QgsProviderRegistry::instance()->providerMetadata( QStringLiteral( "spatialite" ) );
-    QgsSpatiaLiteProviderConnection *providerConnection =  static_cast<QgsSpatiaLiteProviderConnection *>( providerMetadata->createConnection( filename ) );
-    providerMetadata->saveConnection( providerConnection, filename );
+    std::unique_ptr< QgsSpatiaLiteProviderConnection > providerConnection( qgis::down_cast<QgsSpatiaLiteProviderConnection *>( providerMetadata->createConnection( QStringLiteral( "dbname='%1'" ).arg( filename ), QVariantMap() ) ) );
+    if ( providerConnection )
+    {
+      const QFileInfo fi( filename );
+      providerMetadata->saveConnection( providerConnection.get(), fi.fileName() );
+    }
 
     item->refresh();
   }
