b8fa11d1544b06c13e0c419b4bf218db62a4013a
Nyall Dawson <nyall.dawson@gmail.com>
2021-12-17 16:50:03 +1000

    Fix cannot set width unit scale for raster image fill

--- src/gui/symbology/qgssymbollayerwidget.cpp
+++ src/gui/symbology/qgssymbollayerwidget.cpp
@@ -4225,7 +4225,7 @@ void QgsRasterFillSymbolLayerWidget::mWidthUnitWidget_changed()
     return;
   }
   mLayer->setWidthUnit( mWidthUnitWidget->unit() );
-  mLayer->setWidthMapUnitScale( mOffsetUnitWidget->getMapUnitScale() );
+  mLayer->setWidthMapUnitScale( mWidthUnitWidget->getMapUnitScale() );
   emit changed();
 }
 
