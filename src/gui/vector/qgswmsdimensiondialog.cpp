6b3225e44db0507526470c9d8bda76ff48f1ddae
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-12-17 10:07:14 +0100

    UX - Review tooltip about dimensions in QGIS server dialog

--- src/gui/vector/qgswmsdimensiondialog.cpp
+++ src/gui/vector/qgswmsdimensiondialog.cpp
@@ -33,6 +33,10 @@ QgsWmsDimensionDialog::QgsWmsDimensionDialog( QgsVectorLayer *layer, QStringList
     return;
   }
 
+  const QString nameToolTip = tr( "OAPIF supports \"Name\" and \"Date\" only. For \"WMS\", \"Time\" and \"Elevation\" are predefined values according to OGC specifications but it can be user-defined." );
+  mNameLabel->setToolTip( nameToolTip );
+  mNameComboBox->setToolTip( nameToolTip );
+
   // Set field combobox
   mFieldComboBox->setLayer( mLayer );
   mEndFieldComboBox->setLayer( mLayer );
@@ -68,6 +72,8 @@ QgsWmsDimensionDialog::QgsWmsDimensionDialog( QgsVectorLayer *layer, QStringList
 
   mReferenceValueLabel->setEnabled( false );
   mReferenceValueComboBox->setEnabled( false );
+
+  fieldChanged();
 }
 
 void QgsWmsDimensionDialog::setInfo( const QgsMapLayerServerProperties::WmsDimensionInfo &info )
@@ -140,6 +146,10 @@ void QgsWmsDimensionDialog::nameChanged( const QString &name )
   mUnitSymbolLabel->setEnabled( true );
   mUnitSymbolLineEdit->setEnabled( true );
   mUnitSymbolLineEdit->clear();
+  mFieldLabel->setToolTip( QString() );
+  mEndFieldLabel->setToolTip( QString() );
+  mFieldComboBox->setToolTip( QString() );
+  mEndFieldComboBox->setToolTip( QString() );
 
   // Is the name a predefined value?
   if ( mNameComboBox->findText( name ) != -1 )
@@ -159,6 +169,12 @@ void QgsWmsDimensionDialog::nameChanged( const QString &name )
       mUnitsLineEdit->setEnabled( false );
       mUnitSymbolLabel->setEnabled( false );
       mUnitSymbolLineEdit->setEnabled( false );
+
+      const QString fieldToolTip = tr( "If a string is used, it must be formatted according to <a href=\"https://www.iso.org/iso-8601-date-and-time-format.html\">ISO8601</a>." );
+      mFieldLabel->setToolTip( fieldToolTip );
+      mEndFieldLabel->setToolTip( fieldToolTip );
+      mFieldComboBox->setToolTip( fieldToolTip );
+      mEndFieldComboBox->setToolTip( fieldToolTip );
     }
     if ( data == QgsMapLayerServerProperties::DATE )
     {
