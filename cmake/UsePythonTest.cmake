720e26c29686ca7b88341a0610d11d89a62d2fa3
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-12-27 14:53:31 +0100

    restore additional args

--- cmake/UsePythonTest.cmake
+++ cmake/UsePythonTest.cmake
@@ -1,21 +1,22 @@
 # Add a python test from a python file
-# One cannot simply do:
-# set(ENV{PYTHONPATH} ${LIBRARY_OUTPUT_PATH})
-# set(my_test "from test_mymodule import *\;test_mymodule()")
-# add_test(PYTHON-TEST-MYMODULE  python -c ${my_test})
+#
 # Since cmake is only transmitting the ADD_TEST line to ctest thus you are losing
 # the env var. The only way to store the env var is to physically write in the cmake script
-# whatever PYTHONPATH you want and then add the test as 'cmake -P python_test.cmake'
+# whatever PYTHONPATH or variable you want and then add the test as 'cmake -P python_test.cmake'
 #
 # Usage:
 # ADD_PYTHON_TEST(PYTHON-TEST test.py)
+#
+# Optionally pass environment variables to your test
+# ADD_PYTHON_TEST(PYTHON-TEST test.py FOO="bar" BAZ="quux")
+#
+# Timeout can be set by doing ADD_PYTHON_TEST(PYTHON-TEST test.py TIMEOUT=10)
 #
 #
 #  Copyright (c) 2006-2010 Mathieu Malaterre <mathieu.malaterre@gmail.com>
 #
 #  Redistribution and use is allowed according to the terms of the New
 #  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS
 #
 
 
@@ -31,32 +32,36 @@ macro(ADD_PYTHON_TEST TESTNAME FILENAME)
   endif()
 
   set(_QGIS_PYTEST_PYTHONPATHS "${PYTHON_OUTPUT_DIRECTORY}" "${PYTHON_OUTPUT_DIRECTORY}/plugins" "${CMAKE_SOURCE_DIR}/tests/src/python")
+  cmake_path(CONVERT "${_QGIS_PYTEST_PYTHONPATHS}" TO_NATIVE_PATH_LIST QGIS_PYTEST_PYTHONPATH)
 
   if(WIN32)
-    set(QGIS_PYTEST_SEPARATOR ";")
-    set(PATH_VAR_NAME "PATH")
-    if(USING_NINJA OR USING_NMAKE)
+    set(QGIS_PYTEST_PATH_VAR_NAME "PATH")
+    if(NOT CMAKE_CONFIGURATION_TYPES)
       set(QGIS_PYTEST_PREFIX_PATH "${QGIS_OUTPUT_DIRECTORY}/bin")
       set(QGIS_PYTEST_LIBRARY_PATH "${QGIS_OUTPUT_DIRECTORY}/bin;")
-      set(QGIS_PYTEST_PYTHONPATH "${_QGIS_PYTEST_PYTHONPATHS};")
     else()
-      set(QGIS_PYTEST_PREFIX_PATH "${QGIS_OUTPUT_DIRECTORY}/bin/${CMAKE_BUILD_TYPE}")
-      set(QGIS_PYTEST_LIBRARY_PATH "${QGIS_OUTPUT_DIRECTORY}/bin/${CMAKE_BUILD_TYPE};")
-      set(QGIS_PYTEST_PYTHONPATH "${_QGIS_PYTEST_PYTHONPATHS};")
+      set(QGIS_PYTEST_PREFIX_PATH "${QGIS_OUTPUT_DIRECTORY}/bin/$<CONFIG>")
+      set(QGIS_PYTEST_LIBRARY_PATH "${QGIS_OUTPUT_DIRECTORY}/bin/$<CONFIG>;")
     endif()
   else()
-    set(QGIS_PYTEST_SEPARATOR ":")
     set(QGIS_PYTEST_PATH_VAR_NAME "LD_LIBRARY_PATH")
     set(QGIS_PYTEST_PREFIX_PATH "${QGIS_OUTPUT_DIRECTORY}")
-    set(QGIS_PYTEST_LIBRARY_PATH "${QGIS_PYTEST_PYENV}${QGIS_OUTPUT_DIRECTORY}/lib")
-    string(REPLACE ";" ":" QGIS_PYTEST_PYTHONPATH "${_QGIS_PYTEST_PYTHONPATHS};")
-
+    set(QGIS_PYTEST_LIBRARY_PATH "${QGIS_PYTEST_PYENV}${QGIS_OUTPUT_DIRECTORY}/lib:")
   endif()
 
-  configure_file(${CMAKE_SOURCE_DIR}/cmake_templates/PyQgsTest.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/${TESTNAME}.cmake @ONLY)
-
-
   set(TEST_TIMEOUT 0)
+  set(QGIS_PYTEST_ADDITIONAL_ENV_VARS "")
+  foreach(_in ${ARGN})
+    string(REGEX MATCH "^([^=]+)=(.*)$" _out ${_in})
+    if("${CMAKE_MATCH_1}" STREQUAL "TEST_TIMEOUT")
+      set(TEST_TIMEOUT ${CMAKE_MATCH_2})
+    else()
+      message(STATUS "ENV: set(ENV{${CMAKE_MATCH_1}} \"${CMAKE_MATCH_2}\")")
+      set(QGIS_PYTEST_ADDITIONAL_ENV_VARS "${QGIS_PYTEST_ADDITIONAL_ENV_VARS}\nset(ENV{${CMAKE_MATCH_1}} \"${CMAKE_MATCH_2}\")")
+    endif()
+  endforeach(_in)
+
+  configure_file(${CMAKE_SOURCE_DIR}/cmake_templates/PyQgsTest.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/${TESTNAME}.cmake @ONLY NEWLINE_STYLE "LF")
 
   set (PYTHON_TEST_WRAPPER "" CACHE STRING "Wrapper command for python tests (e.g. `timeout -sSIGSEGV 55s` to segfault after 55 seconds)")
   set (PYTHON_TEST_WRAPPER_PROCESSED ${PYTHON_TEST_WRAPPER})
