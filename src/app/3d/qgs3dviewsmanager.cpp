b77f9f8ab60aa4c4c3895879e2e11f1bc1f12317
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-12-30 12:53:46 +0100

    add license

--- src/app/3d/qgs3dviewsmanager.cpp
+++ src/app/3d/qgs3dviewsmanager.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+  qgs3dviewsmanager.cpp
+  --------------------------------------
+  Date                 : December 2021
+  Copyright            : (C) 2021 by Belgacem Nedjima
+  Email                : gb underscore nedjima at esi dot dz
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgs3dviewsmanager.h"
 
 #include "qgs3dmapcanvasdockwidget.h"
