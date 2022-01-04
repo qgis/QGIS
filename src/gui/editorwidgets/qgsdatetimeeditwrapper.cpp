40f2f71330364157a8764e2f5a6028fcd0f70504
Alessandro Pasotti <elpaso@itopen.it>
2022-01-04 16:39:48 +0100

    Fix datetime default with text fields
    Fixes #46542
    
    Funded by: ARPA Piemonte

--- src/gui/editorwidgets/qgsdatetimeeditwrapper.cpp
+++ src/gui/editorwidgets/qgsdatetimeeditwrapper.cpp
@@ -227,15 +227,41 @@ void QgsDateTimeEditWrapper::updateValues( const QVariant &value, const QVariant
       dateTime.setTime( value.toTime() );
       break;
     default:
-      const bool fieldIsoFormat = config( QStringLiteral( "field_iso_format" ), false ).toBool();
-      const QString fieldFormat = config( QStringLiteral( "field_format" ), QgsDateTimeFieldFormatter::defaultFormat( field().type() ) ).toString();
-      if ( fieldIsoFormat )
+      // Field type is not a date/time but we might already have a date/time variant
+      // value coming from a default: no need for string parsing in that case
+      switch ( value.type() )
       {
-        dateTime = QDateTime::fromString( value.toString(), Qt::ISODate );
-      }
-      else
-      {
-        dateTime = QDateTime::fromString( value.toString(), fieldFormat );
+        case QVariant::DateTime:
+        {
+          dateTime = value.toDateTime();
+          break;
+        }
+        case QVariant::Date:
+        {
+          dateTime.setDate( value.toDate() );
+          dateTime.setTime( QTime( 0, 0, 0 ) );
+          break;
+        }
+        case QVariant::Time:
+        {
+          dateTime.setDate( QDate::currentDate() );
+          dateTime.setTime( value.toTime() );
+          break;
+        }
+        default:
+        {
+          const bool fieldIsoFormat = config( QStringLiteral( "field_iso_format" ), false ).toBool();
+          const QString fieldFormat = config( QStringLiteral( "field_format" ), QgsDateTimeFieldFormatter::defaultFormat( field().type() ) ).toString();
+          if ( fieldIsoFormat )
+          {
+            dateTime = QDateTime::fromString( value.toString(), Qt::ISODate );
+          }
+          else
+          {
+            dateTime = QDateTime::fromString( value.toString(), fieldFormat );
+          }
+          break;
+        }
       }
       break;
   }
