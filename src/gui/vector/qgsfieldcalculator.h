d7bf98881798545f359ecb65d40c130cf7690ce0
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2022-01-08 16:28:27 +0100

    Expose QgsFieldCalculator to public API (#46748)
    * shows QgsFieldCalculator in public API
    
    * removed unnecessary comment
    
    * QgsFieldCalculator:
    made public slots private and added code documentation
    
    * fixed doc string
    
    * Apply suggestions from code review
    
    Co-authored-by: Nyall Dawson <nyall.dawson@gmail.com>
    
    * updated sip file
    
    Co-authored-by: Nyall Dawson <nyall.dawson@gmail.com>

--- src/gui/vector/qgsfieldcalculator.h
+++ src/gui/vector/qgsfieldcalculator.h
@@ -16,9 +16,6 @@
 #ifndef QGSFIELDCALCULATOR_H
 #define QGSFIELDCALCULATOR_H
 
-// We don't want to expose this in the public API
-#define SIP_NO_FILE
-
 #include "ui_qgsfieldcalculatorbase.h"
 #include "qgshelp.h"
 #include "qgsfields.h"
@@ -31,6 +28,16 @@ class QgsVectorLayer;
  * \class QgsFieldCalculator
  *
  * \brief A dialog class that provides calculation of new fields using existing fields, values and a set of operators
+ *
+ * Sample usage of the QgsFieldCalculator class:
+ *
+ * \code{.py}
+ *     uri = "point?crs=epsg:4326&field=id:integer"
+ *     layer = QgsVectorLayer(uri, "Scratch point layer",  "memory")
+ *     layer.startEditing()
+ *     dialog = QgsFieldCalculator(layer)
+ *     dialog.exec_()
+ * \endcode
  */
 class GUI_EXPORT QgsFieldCalculator: public QDialog, private Ui::QgsFieldCalculatorBase
 {
@@ -38,18 +45,23 @@ class GUI_EXPORT QgsFieldCalculator: public QDialog, private Ui::QgsFieldCalcula
   public:
     QgsFieldCalculator( QgsVectorLayer *vl, QWidget *parent = nullptr );
 
+    /**
+     * \brief Returns the field index of the field for which new attribute values were calculated.
+     *
+     * \returns The field index if attribute values were calculated or -1, e.g. in case of geometry changes.
+     */
     int changedAttributeId() const { return mAttributeId; }
 
   public slots:
     void accept() override;
 
+  private slots:
     void mNewFieldGroupBox_toggled( bool on );
     void mUpdateExistingGroupBox_toggled( bool on );
     void mCreateVirtualFieldCheckbox_stateChanged( int state );
     void mOutputFieldNameLineEdit_textChanged( const QString &text );
     void mOutputFieldTypeComboBox_activated( int index );
 
-  private slots:
     //! Sets the OK button enabled / disabled
     void setOkButtonState();
     void setPrecisionMinMax();
