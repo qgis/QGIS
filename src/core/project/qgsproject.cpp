b9b635f205da4d574f0a52f347f266d1d92f7216
Damiano Lombardi <damiano@opengis.ch>
2022-01-09 20:07:43 +0100

    Change transaction mode only if all layers are not in edit mode

--- src/core/project/qgsproject.cpp
+++ src/core/project/qgsproject.cpp
@@ -3332,10 +3332,21 @@ Qgis::TransactionMode QgsProject::transactionMode() const
   return mTransactionMode;
 }
 
-void QgsProject::setTransactionMode( Qgis::TransactionMode transactionMode )
+bool QgsProject::setTransactionMode( Qgis::TransactionMode transactionMode )
 {
   if ( transactionMode == mTransactionMode )
-    return;
+    return true;
+
+  // Check that all layer are not in edit mode
+  const auto constLayers = mapLayers().values();
+  for ( QgsMapLayer *layer : constLayers )
+  {
+    if ( layer->isEditable() )
+    {
+      QgsLogger::warning( tr( "Transaction mode can be changed only if all layers are not editable." ) );
+      return false;
+    }
+  }
 
   mTransactionMode = transactionMode;
 
@@ -3349,6 +3360,8 @@ void QgsProject::setTransactionMode( Qgis::TransactionMode transactionMode )
       onMapLayersAdded( mapLayers().values() );
       break;
   }
+
+  return true;
 }
 
 QMap<QPair<QString, QString>, QgsTransactionGroup *> QgsProject::transactionGroups()
