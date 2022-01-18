c7b42fda47136101c304ebdcd875b565ea0fcae1
Nyall Dawson <nyall.dawson@gmail.com>
2022-01-18 10:39:36 +1000

    Fix scroll area child widgets wheel event lock lasts too long after scrolling stops in some circumstances
    Basically, if the user moves the mouse after scrolling the area
    with the wheel, we should release the time based lock we use
    to avoid unwanted widget changes mid-scroll immediately. Otherwise
    it's impossible to scroll wheel->move mouse onto widget->wheel to change
    widget value quickly.
    
    (cherry picked from commit 06758606ae3a44d60f03ff0a6a35c5ef8a219544)

--- src/gui/qgsscrollarea.cpp
+++ src/gui/qgsscrollarea.cpp
@@ -17,6 +17,7 @@
 #include <QMouseEvent>
 #include "qgsscrollarea.h"
 #include <QScrollBar>
+#include "qgslogger.h"
 
 // milliseconds to swallow child wheel events for after a scroll occurs
 #define TIMEOUT 1000
@@ -26,6 +27,7 @@ QgsScrollArea::QgsScrollArea( QWidget *parent )
   , mFilter( new ScrollAreaFilter( this, viewport() ) )
 {
   viewport()->installEventFilter( mFilter );
+  setMouseTracking( true );
 }
 
 void QgsScrollArea::wheelEvent( QWheelEvent *e )
@@ -53,6 +55,11 @@ bool QgsScrollArea::hasScrolled() const
   return mTimer.isActive();
 }
 
+void QgsScrollArea::resetHasScrolled()
+{
+  mTimer.stop();
+}
+
 void QgsScrollArea::setVerticalOnly( bool verticalOnly )
 {
   mVerticalOnly = verticalOnly;
@@ -69,7 +76,10 @@ ScrollAreaFilter::ScrollAreaFilter( QgsScrollArea *parent, QWidget *viewPort )
   : QObject( parent )
   , mScrollAreaWidget( parent )
   , mViewPort( viewPort )
-{}
+{
+  QFontMetrics fm( parent->font() );
+  mMoveDistanceThreshold = fm.horizontalAdvance( 'X' );
+}
 
 bool ScrollAreaFilter::eventFilter( QObject *obj, QEvent *event )
 {
@@ -87,6 +97,22 @@ bool ScrollAreaFilter::eventFilter( QObject *obj, QEvent *event )
     {
       QChildEvent *ce = static_cast<QChildEvent *>( event );
       removeChild( ce->child() );
+      break;
+    }
+
+    case QEvent::MouseMove:
+    {
+      if ( obj == mViewPort )
+      {
+        const QPoint mouseDelta = QCursor::pos() - mPreviousViewportCursorPos;
+        if ( mouseDelta.manhattanLength() > mMoveDistanceThreshold )
+        {
+          // release time based child widget constraint -- user moved the mouse over the viewport (and not just an accidental "wiggle")
+          // so we no longer are in the 'possible unwanted mouse wheel event going to child widget mid-scroll' state
+          mScrollAreaWidget->resetHasScrolled();
+        }
+        mPreviousViewportCursorPos = QCursor::pos();
+      }
       break;
     }
 
@@ -119,6 +145,8 @@ void ScrollAreaFilter::addChild( QObject *child )
   if ( child && child->isWidgetType() )
   {
     child->installEventFilter( this );
+    if ( QWidget *w = qobject_cast< QWidget * >( child ) )
+      w->setMouseTracking( true );
 
     // also install filter on existing children
     const auto constChildren = child->children();
