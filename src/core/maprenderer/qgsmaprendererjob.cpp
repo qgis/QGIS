6f66b0a27753312e73261d0e4fd85ce2a3c93ffa
Alessandro Pasotti <elpaso@itopen.it>
2022-01-21 11:50:07 +0100

    Fix a regression followup #46515
    See comment : https://github.com/qgis/QGIS/pull/46515#issuecomment-1018365479

--- src/core/maprenderer/qgsmaprendererjob.cpp
+++ src/core/maprenderer/qgsmaprendererjob.cpp
@@ -380,8 +380,8 @@ QImage *QgsMapRendererJob::allocateImage( QString layerId )
   QImage *image = new QImage( mSettings.deviceOutputSize(),
                               mSettings.outputImageFormat() );
   image->setDevicePixelRatio( static_cast<qreal>( mSettings.devicePixelRatio() ) );
-  image->setDotsPerMeterX( mSettings.devicePixelRatio() * 1000 * mSettings.outputDpi() / 25.4 );
-  image->setDotsPerMeterY( mSettings.devicePixelRatio() * 1000 * mSettings.outputDpi() / 25.4 );
+  image->setDotsPerMeterX( 1000 * mSettings.outputDpi() / 25.4 );
+  image->setDotsPerMeterY( 1000 * mSettings.outputDpi() / 25.4 );
   if ( image->isNull() )
   {
     mErrors.append( Error( layerId, tr( "Insufficient memory for image %1x%2" ).arg( mSettings.outputSize().width() ).arg( mSettings.outputSize().height() ) ) );
