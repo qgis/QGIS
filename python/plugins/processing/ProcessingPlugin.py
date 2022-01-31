a622004e21789e6fa82670140b65b0cf959b744f
Nyall Dawson <nyall.dawson@gmail.com>
2022-02-01 09:29:48 +1000

    [processing] Fix python exception raised after running in-place algorithm which requires no parameters

--- python/plugins/processing/ProcessingPlugin.py
+++ python/plugins/processing/ProcessingPlugin.py
@@ -488,8 +488,8 @@ class ProcessingPlugin(QObject):
         dlg = HistoryDialog()
         dlg.exec_()
 
-    def tr(self, message):
-        return QCoreApplication.translate('ProcessingPlugin', message)
+    def tr(self, message, disambiguation=None, n=-1):
+        return QCoreApplication.translate('ProcessingPlugin', message, disambiguation=disambiguation, n=n)
 
     def editSelected(self, enabled):
         self.toolbox.set_in_place_edit_mode(enabled)
