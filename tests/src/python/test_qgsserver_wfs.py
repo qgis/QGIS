3d6c6cb8dc8bc84708d75bca1d63a64b9cdb2bf0
rldhont <rldhont@gmail.com>
2022-02-10 12:03:56 +0100

    [Server][Tests] WFS: multiple FEATUREID values

--- tests/src/python/test_qgsserver_wfs.py
+++ tests/src/python/test_qgsserver_wfs.py
@@ -538,6 +538,10 @@ class TestQgsServerWFS(QgsServerTestBase):
         self.wfs_request_compare(
             "GetFeature", '1.0.0', "SRSNAME=EPSG:4326&TYPENAME=testlayer&FEATUREID=testlayer.0", 'wfs_getFeature_1_0_0_featureid_0')
 
+        # with multiple feature ids
+        self.wfs_request_compare(
+            "GetFeature", '1.0.0', "SRSNAME=EPSG:4326&TYPENAME=testlayer&FEATUREID=testlayer.0,testlayer.2", 'wfs_getFeature_1_0_0_featureid_02')
+
         # with layer name Testing Layer (copy)
         self.wfs_request_compare(
             "GetFeature", '1.0.0', "SRSNAME=EPSG:4326&TYPENAME=Testing_Layer_(copy)&FEATUREID=Testing_Layer_(copy).0", 'wfs_getFeature_1_0_0_featureid_0_testing')
