bcdaef83c01518ced91765a0a8fead5fce48d191
Ismail Sunni <imajimatika@gmail.com>
2022-02-22 15:54:18 +0100

    Handle NULL.

--- src/app/qgsidentifyresultsdialog.cpp
+++ src/app/qgsidentifyresultsdialog.cpp
@@ -2328,22 +2328,31 @@ void QgsIdentifyResultsDialog::copyFeatureAttributes()
 
 void QgsIdentifyResultsDialog::selectFeatureByAttribute()
 {
-  const QString attribute = lstResults->currentItem()->data( 0, Qt::DisplayRole ).toString();
-  const QString value = lstResults->currentItem()->data( 1, Qt::DisplayRole ).toString();
-
-  QgsIdentifyResultsFeatureItem *item = dynamic_cast<QgsIdentifyResultsFeatureItem *>( featureItem( lstResults->selectedItems().value( 0 ) ) );
-
+  QTreeWidgetItem *item = lstResults->currentItem();
   if ( !item ) // should not happen
   {
     QgsDebugMsg( QStringLiteral( "Selected item is not feature" ) );
     return;
   }
 
-  QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( layer( item ) );
-  if ( !vl )
+  const QString attribute = item->data( 0, Qt::DisplayRole ).toString();
+  const QString value = item->data( 1, Qt::DisplayRole ).toString();
+
+  QgsVectorLayer *vlayer = vectorLayer( item );
+
+  if ( !vlayer )
     return;
 
-  vl->selectByExpression( QString( "\"%1\"='%2'" ).arg( attribute, value ) );
+  if ( value != QString( "NULL" ) )
+  {
+    vlayer->selectByExpression( QString( "\"%1\"='%2'" ).arg( attribute, value ) );
+  }
+  else
+  {
+    vlayer->selectByExpression( QString( "\"%1\" IS %2" ).arg( attribute, value ) );
+  }
+
+
 }
 
 void QgsIdentifyResultsDialog::copyGetFeatureInfoUrl()
