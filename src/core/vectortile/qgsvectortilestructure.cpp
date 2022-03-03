0d0abc707e2cfc9d4b3cb3fa1974cee4b1303de0
Nyall Dawson <nyall.dawson@gmail.com>
2022-03-03 15:28:58 +1000

    Correctly load arcgis vector tiles in non-EPSG:3857 crs

--- src/core/vectortile/qgsvectortilestructure.cpp
+++ src/core/vectortile/qgsvectortilestructure.cpp
@@ -16,6 +16,7 @@
 #include "qgsvectortilestructure.h"
 #include "qgstiles.h"
 #include "qgsvectortileutils.h"
+#include "qgsarcgisrestutils.h"
 
 QgsVectorTileStructure QgsVectorTileStructure::fromWebMercator()
 {
@@ -43,4 +44,47 @@ double QgsVectorTileStructure::scaleToZoom( double scale ) const
 int QgsVectorTileStructure::scaleToZoomLevel( double scale ) const
 {
   return QgsVectorTileUtils::scaleToZoomLevel( scale, mMinZoom, mMaxZoom, mZ0Scale );
+}
+
+bool QgsVectorTileStructure::fromEsriJson( const QVariantMap &json )
+{
+  const QVariantMap tileInfo = json.value( QStringLiteral( "tileInfo" ) ).toMap();
+
+  const QVariantMap origin = tileInfo.value( QStringLiteral( "origin" ) ).toMap();
+  const double originX = origin.value( QStringLiteral( "x" ) ).toDouble();
+  const double originY = origin.value( QStringLiteral( "y" ) ).toDouble();
+
+  const QVariantList lodList = tileInfo.value( QStringLiteral( "lods" ) ).toList();
+  bool foundLevel0 = false;
+  double z0Dimension = 0;
+  double z0Scale = 0;
+  int minLOD = -1;
+  int maxLOD = -1;
+  for ( const QVariant &lod : lodList )
+  {
+    const QVariantMap lodMap = lod.toMap();
+    const int level = lodMap.value( QStringLiteral( "level" ) ).toInt();
+    minLOD = minLOD == -1 ? level : std::min( level, minLOD );
+    maxLOD = maxLOD == -1 ? level : std::max( level, maxLOD );
+    if ( level == 0 )
+    {
+      z0Dimension = lodMap.value( QStringLiteral( "resolution" ) ).toDouble() * 512;
+      z0Scale = lodMap.value( QStringLiteral( "scale" ) ).toDouble();
+      foundLevel0 = true;
+    }
+  }
+
+  if ( !foundLevel0 )
+    return false;
+
+  mZ0xMin = originX;
+  mZ0xMax = originX + z0Dimension;
+  mZ0yMin = originY - z0Dimension;
+  mZ0yMax = originY;
+  mZ0Dimension = z0Dimension;
+  mZ0Scale = z0Scale;
+  mCrs = QgsArcGisRestUtils::convertSpatialReference( tileInfo.value( QStringLiteral( "spatialReference" ) ).toMap() );
+  mMinZoom = minLOD;
+  mMaxZoom = maxLOD;
+  return true;
 }
