5ffc68709c6fccea8d22a8315704863a36328c70
uclaros <uclaros@gmail.com>
2022-03-04 23:30:57 +0200

    avoid unnecessary camera update on mouse move

--- src/3d/qgscameracontroller.cpp
+++ src/3d/qgscameracontroller.cpp
@@ -480,6 +480,7 @@ void QgsCameraController::onPositionChangedTerrainNavigation( Qt3DInput::QMouseE
     const float diffPitch = 0.2f * dy;
     const float diffYaw = - 0.2f * dx;
     rotateCamera( diffPitch, diffYaw );
+    updateCameraFromPose();
   }
   else if ( hasLeftButton && !hasShift && !hasCtrl )
   {
@@ -587,7 +588,6 @@ void QgsCameraController::onPositionChangedTerrainNavigation( Qt3DInput::QMouseE
   }
 
   mMousePos = QPoint( mouse->x(), mouse->y() );
-  updateCameraFromPose();
 }
 
 void QgsCameraController::zoom( float factor )
