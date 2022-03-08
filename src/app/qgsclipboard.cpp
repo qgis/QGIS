ad1c530c315eeb79d6033bcee1ab86001a70876c
Ismail Sunni <imajimatika@gmail.com>
2022-03-08 16:28:56 +0100

    Fix #47569. Unable to copy data stored in JSON and list fields

--- src/app/qgsclipboard.cpp
+++ src/app/qgsclipboard.cpp
@@ -41,6 +41,8 @@
 #include "qgsproject.h"
 #include "qgsapplication.h"
 
+#include <nlohmann/json.hpp>
+
 QgsClipboard::QgsClipboard()
 {
   connect( QApplication::clipboard(), &QClipboard::dataChanged, this, &QgsClipboard::systemClipboardChanged );
@@ -131,14 +133,15 @@ void QgsClipboard::generateClipboardText( QString &textContent, QString &htmlCon
 
         for ( int idx = 0; idx < attributes.count(); ++idx )
         {
-          QString value = attributes.at( idx ).toString();
+          QString value = QString::fromStdString( QgsJsonUtils::jsonFromVariant( attributes.at( idx ) ).dump() );
+
           if ( value.contains( '\n' ) || value.contains( '\t' ) )
             textFields += '"' + value.replace( '"', QLatin1String( "\"\"" ) ) + '\"';
           else
           {
             textFields += value;
           }
-          value = attributes.at( idx ).toString();
+          value = QString::fromStdString( QgsJsonUtils::jsonFromVariant( attributes.at( idx ) ).dump() );
           value.replace( '\n', QLatin1String( "<br>" ) ).replace( '\t', QLatin1String( "&emsp;" ) );
           htmlFields += QStringLiteral( "<td>%1</td>" ).arg( value );
         }
