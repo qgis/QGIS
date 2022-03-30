499c374dba564617985219156eed0c38fdc2dddc
Nyall Dawson <nyall.dawson@gmail.com>
2022-03-30 17:18:19 +1000

    Split out axis properties into separate class

--- tests/src/python/test_qgsplot.py
+++ tests/src/python/test_qgsplot.py
@@ -67,35 +67,35 @@ class TestQgsPlot(unittest.TestCase):
         plot.setChartBorderSymbol(sym2)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#00ffff', 'outline_width': 1})
-        plot.setXGridMajorSymbol(sym3)
+        plot.xAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff00ff', 'outline_width': 0.5})
-        plot.setXGridMinorSymbol(sym4)
+        plot.xAxis().setGridMinorSymbol(sym4)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#0066ff', 'outline_width': 1})
-        plot.setYGridMajorSymbol(sym3)
+        plot.yAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff4433', 'outline_width': 0.5})
-        plot.setYGridMinorSymbol(sym4)
+        plot.yAxis().setGridMinorSymbol(sym4)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 16)
         x_axis_format = QgsTextFormat.fromQFont(font)
         x_axis_format.setColor(QColor(255, 0, 0))
-        plot.setXAxisTextFormat(x_axis_format)
+        plot.xAxis().setTextFormat(x_axis_format)
 
         x_axis_number_format = QgsBasicNumericFormat()
         x_axis_number_format.setNumberDecimalPlaces(1)
         x_axis_number_format.setShowTrailingZeros(True)
-        plot.setXAxisNumericFormat(x_axis_number_format)
+        plot.xAxis().setNumericFormat(x_axis_number_format)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 18)
         y_axis_format = QgsTextFormat.fromQFont(font)
         y_axis_format.setColor(QColor(0, 255, 0))
-        plot.setYAxisTextFormat(y_axis_format)
+        plot.yAxis().setTextFormat(y_axis_format)
 
         y_axis_number_format = QgsBasicNumericFormat()
         y_axis_number_format.setShowPlusSign(True)
-        plot.setYAxisNumericFormat(y_axis_number_format)
+        plot.yAxis().setNumericFormat(y_axis_number_format)
 
         plot.setXMinimum(3)
         plot.setXMaximum(9)
@@ -133,24 +133,24 @@ class TestQgsPlot(unittest.TestCase):
         plot.setChartBorderSymbol(sym2)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#00ffff', 'outline_width': 1})
-        plot.setXGridMajorSymbol(sym3)
+        plot.xAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff00ff', 'outline_width': 0.5})
-        plot.setXGridMinorSymbol(sym4)
+        plot.xAxis().setGridMinorSymbol(sym4)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#0066ff', 'outline_width': 1})
-        plot.setYGridMajorSymbol(sym3)
+        plot.yAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff4433', 'outline_width': 0.5})
-        plot.setYGridMinorSymbol(sym4)
+        plot.yAxis().setGridMinorSymbol(sym4)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 16)
         x_axis_format = QgsTextFormat.fromQFont(font)
-        plot.setXAxisTextFormat(x_axis_format)
+        plot.xAxis().setTextFormat(x_axis_format)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 18)
         y_axis_format = QgsTextFormat.fromQFont(font)
-        plot.setYAxisTextFormat(y_axis_format)
+        plot.yAxis().setTextFormat(y_axis_format)
 
         plot.setXMinimum(1)
         plot.setXMaximum(21)
@@ -158,14 +158,14 @@ class TestQgsPlot(unittest.TestCase):
         plot.setYMinimum(27)
         plot.setYMaximum(327)
 
-        plot.setGridIntervalMajorX(10)
-        plot.setGridIntervalMinorX(2)
+        plot.xAxis().setGridIntervalMajor(10)
+        plot.xAxis().setGridIntervalMinor(2)
 
-        plot.setGridIntervalMajorY(100)
-        plot.setGridIntervalMinorY(50)
+        plot.yAxis().setGridIntervalMajor(100)
+        plot.yAxis().setGridIntervalMinor(50)
 
-        plot.setLabelIntervalX(5)
-        plot.setLabelIntervalY(70)
+        plot.xAxis().setLabelInterval(5)
+        plot.yAxis().setLabelInterval(70)
 
         im = QImage(600, 500, QImage.Format_ARGB32)
         im.fill(Qt.white)
@@ -192,29 +192,29 @@ class TestQgsPlot(unittest.TestCase):
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#00ffff', 'outline_width': 1, 'capstyle': 'flat'})
         sym3[0].setDataDefinedProperty(QgsSymbolLayer.PropertyStrokeWidth, QgsProperty.fromExpression('case when @plot_axis_value = 10 then 3 else 1 end'))
-        plot.setXGridMajorSymbol(sym3)
+        plot.xAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff00ff', 'outline_width': 0.5, 'capstyle': 'flat'})
         sym4[0].setDataDefinedProperty(QgsSymbolLayer.PropertyStrokeWidth, QgsProperty.fromExpression('case when @plot_axis_value = 6 then 3 else 0.5 end'))
-        plot.setXGridMinorSymbol(sym4)
+        plot.xAxis().setGridMinorSymbol(sym4)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#0066ff', 'outline_width': 1, 'capstyle': 'flat'})
         sym3[0].setDataDefinedProperty(QgsSymbolLayer.PropertyStrokeWidth, QgsProperty.fromExpression('case when @plot_axis_value = 5 then 3 else 0.5 end'))
-        plot.setYGridMajorSymbol(sym3)
+        plot.yAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff4433', 'outline_width': 0.5, 'capstyle': 'flat'})
         sym4[0].setDataDefinedProperty(QgsSymbolLayer.PropertyStrokeWidth, QgsProperty.fromExpression('case when @plot_axis_value = 9 then 3 else 0.5 end'))
-        plot.setYGridMinorSymbol(sym4)
+        plot.yAxis().setGridMinorSymbol(sym4)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 16)
         x_axis_format = QgsTextFormat.fromQFont(font)
         x_axis_format.dataDefinedProperties().setProperty(QgsPalLayerSettings.Color, QgsProperty.fromExpression('case when @plot_axis_value %3 = 0 then \'#ff0000\' else \'#000000\' end'))
-        plot.setXAxisTextFormat(x_axis_format)
+        plot.xAxis().setTextFormat(x_axis_format)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 18)
         y_axis_format = QgsTextFormat.fromQFont(font)
         y_axis_format.dataDefinedProperties().setProperty(QgsPalLayerSettings.Color, QgsProperty.fromExpression('case when @plot_axis_value %4 = 0 then \'#0000ff\' else \'#000000\' end'))
-        plot.setYAxisTextFormat(y_axis_format)
+        plot.yAxis().setTextFormat(y_axis_format)
 
         plot.setXMinimum(3)
         plot.setXMaximum(13)
@@ -246,11 +246,11 @@ class TestQgsPlot(unittest.TestCase):
 
         font = QgsFontUtils.getStandardTestFont('Bold', 16)
         x_axis_format = QgsTextFormat.fromQFont(font)
-        plot.setXAxisTextFormat(x_axis_format)
+        plot.xAxis().setTextFormat(x_axis_format)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 18)
         y_axis_format = QgsTextFormat.fromQFont(font)
-        plot.setYAxisTextFormat(y_axis_format)
+        plot.yAxis().setTextFormat(y_axis_format)
 
         plot.setXMinimum(3)
         plot.setXMaximum(13)
@@ -267,12 +267,12 @@ class TestQgsPlot(unittest.TestCase):
         painter.end()
 
         plot.calculateOptimisedIntervals(rc)
-        self.assertEqual(plot.labelIntervalX(), 1)
-        self.assertEqual(plot.labelIntervalY(), 2)
-        self.assertEqual(plot.gridIntervalMinorX(), 1)
-        self.assertEqual(plot.gridIntervalMinorY(), 1)
-        self.assertEqual(plot.gridIntervalMajorX(), 5)
-        self.assertEqual(plot.gridIntervalMajorY(), 4)
+        self.assertEqual(plot.xAxis().labelInterval(), 1)
+        self.assertEqual(plot.yAxis().labelInterval(), 2)
+        self.assertEqual(plot.xAxis().gridIntervalMinor(), 1)
+        self.assertEqual(plot.yAxis().gridIntervalMinor(), 1)
+        self.assertEqual(plot.xAxis().gridIntervalMajor(), 5)
+        self.assertEqual(plot.yAxis().gridIntervalMajor(), 4)
 
         plot.setXMinimum(3)
         plot.setXMaximum(113)
@@ -280,12 +280,12 @@ class TestQgsPlot(unittest.TestCase):
         plot.setYMaximum(112)
 
         plot.calculateOptimisedIntervals(rc)
-        self.assertEqual(plot.labelIntervalX(), 20)
-        self.assertEqual(plot.labelIntervalY(), 20)
-        self.assertEqual(plot.gridIntervalMinorX(), 10)
-        self.assertEqual(plot.gridIntervalMinorY(), 10)
-        self.assertEqual(plot.gridIntervalMajorX(), 40)
-        self.assertEqual(plot.gridIntervalMajorY(), 40)
+        self.assertEqual(plot.xAxis().labelInterval(), 20)
+        self.assertEqual(plot.yAxis().labelInterval(), 20)
+        self.assertEqual(plot.xAxis().gridIntervalMinor(), 10)
+        self.assertEqual(plot.yAxis().gridIntervalMinor(), 10)
+        self.assertEqual(plot.xAxis().gridIntervalMajor(), 40)
+        self.assertEqual(plot.yAxis().gridIntervalMajor(), 40)
 
         plot.setXMinimum(0.3)
         plot.setXMaximum(0.5)
@@ -293,12 +293,12 @@ class TestQgsPlot(unittest.TestCase):
         plot.setYMaximum(2)
 
         plot.calculateOptimisedIntervals(rc)
-        self.assertEqual(plot.labelIntervalX(), 0.1)
-        self.assertEqual(plot.labelIntervalY(), 0.2)
-        self.assertEqual(plot.gridIntervalMinorX(), 0.05)
-        self.assertEqual(plot.gridIntervalMinorY(), 0.1)
-        self.assertEqual(plot.gridIntervalMajorX(), 0.2)
-        self.assertEqual(plot.gridIntervalMajorY(), 0.4)
+        self.assertEqual(plot.xAxis().labelInterval(), 0.1)
+        self.assertEqual(plot.yAxis().labelInterval(), 0.2)
+        self.assertEqual(plot.xAxis().gridIntervalMinor(), 0.05)
+        self.assertEqual(plot.yAxis().gridIntervalMinor(), 0.1)
+        self.assertEqual(plot.xAxis().gridIntervalMajor(), 0.2)
+        self.assertEqual(plot.yAxis().gridIntervalMajor(), 0.4)
 
         plot.setXMinimum(-10)
         plot.setXMaximum(0)
@@ -306,20 +306,20 @@ class TestQgsPlot(unittest.TestCase):
         plot.setYMaximum(-500)
 
         plot.calculateOptimisedIntervals(rc)
-        self.assertEqual(plot.labelIntervalX(), 2)
-        self.assertEqual(plot.labelIntervalY(), 2000)
-        self.assertEqual(plot.gridIntervalMinorX(), 1)
-        self.assertEqual(plot.gridIntervalMinorY(), 1000)
-        self.assertEqual(plot.gridIntervalMajorX(), 4)
-        self.assertEqual(plot.gridIntervalMajorY(), 4000)
+        self.assertEqual(plot.xAxis().labelInterval(), 2)
+        self.assertEqual(plot.yAxis().labelInterval(), 2000)
+        self.assertEqual(plot.xAxis().gridIntervalMinor(), 1)
+        self.assertEqual(plot.yAxis().gridIntervalMinor(), 1000)
+        self.assertEqual(plot.xAxis().gridIntervalMajor(), 4)
+        self.assertEqual(plot.yAxis().gridIntervalMajor(), 4000)
 
         plot.setXMinimum(100000)
         plot.setXMaximum(200000)
 
         plot.calculateOptimisedIntervals(rc)
-        self.assertEqual(plot.labelIntervalX(), 100000)
-        self.assertEqual(plot.gridIntervalMinorX(), 50000)
-        self.assertEqual(plot.gridIntervalMajorX(), 200000)
+        self.assertEqual(plot.xAxis().labelInterval(), 100000)
+        self.assertEqual(plot.xAxis().gridIntervalMinor(), 50000)
+        self.assertEqual(plot.xAxis().gridIntervalMajor(), 200000)
 
     def test_read_write(self):
         plot = Qgs2DPlot()
@@ -333,35 +333,35 @@ class TestQgsPlot(unittest.TestCase):
         plot.setChartBorderSymbol(sym2)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#00ffff', 'outline_width': 1})
-        plot.setXGridMajorSymbol(sym3)
+        plot.xAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff00ff', 'outline_width': 0.5})
-        plot.setXGridMinorSymbol(sym4)
+        plot.xAxis().setGridMinorSymbol(sym4)
 
         sym3 = QgsLineSymbol.createSimple({'outline_color': '#0066ff', 'outline_width': 1})
-        plot.setYGridMajorSymbol(sym3)
+        plot.yAxis().setGridMajorSymbol(sym3)
 
         sym4 = QgsLineSymbol.createSimple({'outline_color': '#ff4433', 'outline_width': 0.5})
-        plot.setYGridMinorSymbol(sym4)
+        plot.yAxis().setGridMinorSymbol(sym4)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 16)
         x_axis_format = QgsTextFormat.fromQFont(font)
         x_axis_format.setColor(QColor(255, 0, 0))
-        plot.setXAxisTextFormat(x_axis_format)
+        plot.xAxis().setTextFormat(x_axis_format)
 
         x_axis_number_format = QgsBasicNumericFormat()
         x_axis_number_format.setNumberDecimalPlaces(1)
         x_axis_number_format.setShowTrailingZeros(True)
-        plot.setXAxisNumericFormat(x_axis_number_format)
+        plot.xAxis().setNumericFormat(x_axis_number_format)
 
         font = QgsFontUtils.getStandardTestFont('Bold', 18)
         y_axis_format = QgsTextFormat.fromQFont(font)
         y_axis_format.setColor(QColor(0, 255, 0))
-        plot.setYAxisTextFormat(y_axis_format)
+        plot.yAxis().setTextFormat(y_axis_format)
 
         y_axis_number_format = QgsBasicNumericFormat()
         y_axis_number_format.setShowPlusSign(True)
-        plot.setYAxisNumericFormat(y_axis_number_format)
+        plot.yAxis().setNumericFormat(y_axis_number_format)
 
         plot.setXMinimum(3)
         plot.setXMaximum(9)
@@ -369,13 +369,13 @@ class TestQgsPlot(unittest.TestCase):
         plot.setYMinimum(2)
         plot.setYMaximum(12)
 
-        plot.setGridIntervalMinorX(0.5)
-        plot.setGridIntervalMajorX(1.5)
-        plot.setGridIntervalMinorY(0.3)
-        plot.setGridIntervalMajorY(1.3)
+        plot.xAxis().setGridIntervalMinor(0.5)
+        plot.xAxis().setGridIntervalMajor(1.5)
+        plot.yAxis().setGridIntervalMinor(0.3)
+        plot.yAxis().setGridIntervalMajor(1.3)
 
-        plot.setLabelIntervalX(32)
-        plot.setLabelIntervalY(23)
+        plot.xAxis().setLabelInterval(32)
+        plot.yAxis().setLabelInterval(23)
 
         doc = QDomDocument()
         elem = doc.createElement('test')
@@ -389,27 +389,27 @@ class TestQgsPlot(unittest.TestCase):
         self.assertEqual(res.yMinimum(), 2)
         self.assertEqual(res.yMaximum(), 12)
 
-        self.assertEqual(res.gridIntervalMinorX(), 0.5)
-        self.assertEqual(res.gridIntervalMajorX(), 1.5)
-        self.assertEqual(res.gridIntervalMinorY(), 0.3)
-        self.assertEqual(res.gridIntervalMajorY(), 1.3)
+        self.assertEqual(res.xAxis().gridIntervalMinor(), 0.5)
+        self.assertEqual(res.xAxis().gridIntervalMajor(), 1.5)
+        self.assertEqual(res.yAxis().gridIntervalMinor(), 0.3)
+        self.assertEqual(res.yAxis().gridIntervalMajor(), 1.3)
 
-        self.assertEqual(res.labelIntervalX(), 32)
-        self.assertEqual(res.labelIntervalY(), 23)
+        self.assertEqual(res.xAxis().labelInterval(), 32)
+        self.assertEqual(res.yAxis().labelInterval(), 23)
 
-        self.assertEqual(res.xAxisNumericFormat().numberDecimalPlaces(), 1)
-        self.assertTrue(res.yAxisNumericFormat().showPlusSign())
+        self.assertEqual(res.xAxis().numericFormat().numberDecimalPlaces(), 1)
+        self.assertTrue(res.yAxis().numericFormat().showPlusSign())
 
-        self.assertEqual(res.xAxisTextFormat().color().name(), '#ff0000')
-        self.assertEqual(res.yAxisTextFormat().color().name(), '#00ff00')
+        self.assertEqual(res.xAxis().textFormat().color().name(), '#ff0000')
+        self.assertEqual(res.yAxis().textFormat().color().name(), '#00ff00')
 
         self.assertEqual(res.chartBackgroundSymbol().color().name(), '#fdbf6f')
         self.assertEqual(res.chartBorderSymbol().color().name(), '#0000ff')
 
-        self.assertEqual(res.xGridMinorSymbol().color().name(), '#ff00ff')
-        self.assertEqual(res.xGridMajorSymbol().color().name(), '#00ffff')
-        self.assertEqual(res.yGridMinorSymbol().color().name(), '#ff4433')
-        self.assertEqual(res.yGridMajorSymbol().color().name(), '#0066ff')
+        self.assertEqual(res.xAxis().gridMinorSymbol().color().name(), '#ff00ff')
+        self.assertEqual(res.xAxis().gridMajorSymbol().color().name(), '#00ffff')
+        self.assertEqual(res.yAxis().gridMinorSymbol().color().name(), '#ff4433')
+        self.assertEqual(res.yAxis().gridMajorSymbol().color().name(), '#0066ff')
 
     def imageCheck(self, name, reference_image, image):
         self.report += "<h2>Render {}</h2>\n".format(name)
