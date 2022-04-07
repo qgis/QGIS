abf99f995114ec261b32394e06cb29bb0260af06
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2022-04-07 05:48:54 +0100

    fix spelling

--- src/core/pointcloud/qgslazinfo.h
+++ src/core/pointcloud/qgslazinfo.h
@@ -30,7 +30,7 @@
 /**
  * \ingroup core
  *
- * Class for extracting informations contained in LAZ file such as the public header block
+ * Class for extracting information contained in LAZ file such as the public header block
  * and variable length records
  *
  * \since QGIS 3.26
