7b03f9b904e15de848151a97d48d2b5ca749a9e2
Damiano Lombardi <damiano@opengis.ch>
2022-04-19 15:32:14 +0200

    fix attribute table incorrect form for default values with geometry

--- src/gui/qgsattributeform.cpp
+++ src/gui/qgsattributeform.cpp
@@ -2789,6 +2789,10 @@ void QgsAttributeForm::updateFieldDependencies()
 void QgsAttributeForm::updateFieldDependenciesDefaultValue( QgsEditorWidgetWrapper *eww )
 {
   QgsExpression exp( eww->field().defaultValueDefinition().expression() );
+
+  if ( exp.needsGeometry() )
+    mNeedsGeometry = true;
+
   const QSet<QString> referencedColumns = exp.referencedColumns();
   for ( const QString &referencedColumn : referencedColumns )
   {
@@ -2815,6 +2819,10 @@ void QgsAttributeForm::updateFieldDependenciesVirtualFields( QgsEditorWidgetWrap
     return;
 
   QgsExpression exp( expressionField );
+
+  if ( exp.needsGeometry() )
+    mNeedsGeometry = true;
+
   const QSet<QString> referencedColumns = exp.referencedColumns();
   for ( const QString &referencedColumn : referencedColumns )
   {
