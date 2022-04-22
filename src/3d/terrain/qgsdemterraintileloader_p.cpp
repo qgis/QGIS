b9bc6cc8650621ce81a4f2939164da228f92c23d
Nyall Dawson <nyall.dawson@gmail.com>
2022-04-23 06:37:18 +1000

    Also update parent bounding boxes after determining exact DEM terrain tile box

--- src/3d/terrain/qgsdemterraintileloader_p.cpp
+++ src/3d/terrain/qgsdemterraintileloader_p.cpp
@@ -124,6 +124,7 @@ Qt3DCore::QEntity *QgsDemTerrainTileLoader::createEntity( Qt3DCore::QEntity *par
   transform->setTranslation( QVector3D( x0 + half, 0, - ( y0 + half ) ) );
 
   mNode->setExactBbox( QgsAABB( x0, zMin * map.terrainVerticalScale(), -y0, x0 + side, zMax * map.terrainVerticalScale(), -( y0 + side ) ) );
+  mNode->updateParentBoundingBoxesRecursively();
 
   entity->setEnabled( false );
   entity->setParent( parent );
