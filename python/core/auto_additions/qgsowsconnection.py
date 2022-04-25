db2b09de23bd8514f62266f71d80cc98c312f963
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-04-25 10:33:03 +0200

    sipify

---
+++ python/core/auto_additions/qgsowsconnection.py
@@ -0,0 +1,10 @@
+# The following has been generated automatically from src/core/qgsowsconnection.h
+# monkey patching scoped based enum
+QgsOwsConnection.DpiMode.All.__doc__ = ""
+QgsOwsConnection.DpiMode.Off.__doc__ = ""
+QgsOwsConnection.DpiMode.QGIS.__doc__ = ""
+QgsOwsConnection.DpiMode.UMN.__doc__ = ""
+QgsOwsConnection.DpiMode.GeoServer.__doc__ = ""
+QgsOwsConnection.DpiMode.__doc__ = 'DpiMode enum\n\n.. versionadded:: 3.26\n\n' + '* ``All``: ' + QgsOwsConnection.DpiMode.All.__doc__ + '\n' + '* ``Off``: ' + QgsOwsConnection.DpiMode.Off.__doc__ + '\n' + '* ``QGIS``: ' + QgsOwsConnection.DpiMode.QGIS.__doc__ + '\n' + '* ``UMN``: ' + QgsOwsConnection.DpiMode.UMN.__doc__ + '\n' + '* ``GeoServer``: ' + QgsOwsConnection.DpiMode.GeoServer.__doc__
+# --
+QgsOwsConnection.DpiMode.baseClass = QgsOwsConnection
