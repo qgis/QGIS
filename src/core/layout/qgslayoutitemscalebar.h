5af51816a1e43fcaf3f250c5e19bf85a11a0ba1f
Ethan Snyder <ethan.snyder@rve.com>
2022-04-26 12:16:56 -0400

    Fixed parameter names in doxygen comments

--- src/core/layout/qgslayoutitemscalebar.h
+++ src/core/layout/qgslayoutitemscalebar.h
@@ -132,31 +132,31 @@ class CORE_EXPORT QgsLayoutItemScaleBar: public QgsLayoutItem
 
     /**
      * Recalculates the number of scalebar units per segment
-     * \param expression context for evaluating data defined units per segment
+     * \param context for evaluating data defined units per segment
      */
     void refreshUnitsPerSegment( const QgsExpressionContext *context = nullptr );
 
     /**
      * Recalculates the number of segments to the left of 0.
-     * \param expression context for evaluating data defined number of segments to the left.
+     * \param context for evaluating data defined number of segments to the left.
      */
     void refreshNumberOfSegmentsLeft( const QgsExpressionContext *context = nullptr );
 
     /**
      * Recalculates the number of segments to the right of 0.
-     * \param expression context for evaluating data defined number of segments to the left.
+     * \param context for evaluating data defined number of segments to the left.
      */
     void refreshNumberOfSegmentsRight( const QgsExpressionContext *context = nullptr );
 
     /**
      * Recalculates the minimum size of a bar segment in mm.
-     * \param expression context for evaluating data defined minimum width of a segment.
+     * \param context for evaluating data defined minimum width of a segment.
      */
     void refreshMinimumBarWidth( const QgsExpressionContext *context = nullptr );
 
     /**
      * Recalculates the maximum size of a bar segment in mm.
-     * \param expression context for evaluating data defined maximum width of a segment.
+     * \param context for evaluating data defined maximum width of a segment.
      */
     void refreshMaximumBarWidth( const QgsExpressionContext *context = nullptr );
 
