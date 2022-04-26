e59c5d98812eed231d48c61f21ff73dc56c39355
Nyall Dawson <nyall.dawson@gmail.com>
2022-04-27 09:17:08 +1000

    Add dox note about source ID and layer IDs

--- src/core/elevation/qgsabstractprofilegenerator.h
+++ src/core/elevation/qgsabstractprofilegenerator.h
@@ -211,6 +211,9 @@ class CORE_EXPORT QgsAbstractProfileGenerator
 
     /**
      * Returns a unique identifier representing the source of the profile.
+     *
+     * For generators associated with a map layer the source ID will match the layer's QgsMapLayer::id(). Other (non-map-layer) sources
+     * will have a different unique ID with its own custom interpretation.
      */
     virtual QString sourceId() const = 0;
 
