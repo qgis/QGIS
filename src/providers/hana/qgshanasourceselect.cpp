8b62a0e74a00fe3989d6d50f61b7ef7851280dd5
Maxim Rylov <maxim.rylov@sap.com>
2022-05-04 14:18:17 +0200

    Fix #48329 for HANA

--- src/providers/hana/qgshanasourceselect.cpp
+++ src/providers/hana/qgshanasourceselect.cpp
@@ -506,15 +506,14 @@ void QgsHanaSourceSelect::setSql( const QModelIndex &index )
     return;
   }
 
-  const QModelIndex idx = proxyModel()->mapToSource( index );
-  const QString uri = mTableModel->layerURI( idx, mConnectionName, mConnectionInfo );
+  const QString uri = mTableModel->layerURI( index, mConnectionName, mConnectionInfo );
   if ( uri.isNull() )
   {
     QgsDebugMsg( "no uri" );
     return;
   }
 
-  const QString tableName = mTableModel->itemFromIndex( idx.sibling( idx.row(), QgsHanaTableModel::DbtmTable ) )->text();
+  const QString tableName = mTableModel->itemFromIndex( index.sibling( index.row(), QgsHanaTableModel::DbtmTable ) )->text();
 
   QgsVectorLayer vlayer( uri, tableName, QStringLiteral( "hana" ) );
   if ( !vlayer.isValid() )
@@ -522,7 +521,7 @@ void QgsHanaSourceSelect::setSql( const QModelIndex &index )
 
   QgsQueryBuilder gb( &vlayer, this );
   if ( gb.exec() )
-    mTableModel->setSql( proxyModel()->mapToSource( index ), gb.sql() );
+    mTableModel->setSql( index, gb.sql() );
 }
 
 QString QgsHanaSourceSelect::fullDescription(
