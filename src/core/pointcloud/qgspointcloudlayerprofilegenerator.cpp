cb973e92618ff118fef63013e89791ec222bca93
Nyall Dawson <nyall.dawson@gmail.com>
2022-05-05 13:40:05 +1000

    Ensure that points are visible on elevation profile even when tolerance is very small compared to profile line length

--- src/core/pointcloud/qgspointcloudlayerprofilegenerator.cpp
+++ src/core/pointcloud/qgspointcloudlayerprofilegenerator.cpp
@@ -395,7 +395,13 @@ bool QgsPointCloudLayerProfileGenerator::generateProfile( const QgsProfileGenera
 
   const IndexedPointCloudNode root = pc->root();
 
-  const double maximumErrorPixels = context.convertDistanceToPixels( mMaximumScreenError, mMaximumScreenErrorUnit );
+  double maximumErrorPixels = context.convertDistanceToPixels( mMaximumScreenError, mMaximumScreenErrorUnit );
+  const double toleranceInPixels = context.convertDistanceToPixels( mTolerance, QgsUnitTypes::RenderMapUnits );
+  // ensure that the maximum error is compatible with the tolerance size -- otherwise if the tolerance size
+  // is much smaller than the maximum error, we don't dig deep enough into the point cloud nodes to find
+  // points which are inside the tolerance.
+  if ( toleranceInPixels / 4 < maximumErrorPixels )
+    maximumErrorPixels = toleranceInPixels / 4;
 
   const QgsRectangle rootNodeExtentLayerCoords = pc->nodeMapExtent( root );
   QgsRectangle rootNodeExtentInCurveCrs;
