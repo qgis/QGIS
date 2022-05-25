bbdc4bf195f181520566321c4301896880d70713
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2022-05-25 21:14:34 +0200

    add test for epsg:4978. See PR 48738

--- tests/src/core/testqgsclipper.cpp
+++ tests/src/core/testqgsclipper.cpp
@@ -23,6 +23,10 @@
 #include <qgsclipper.h>
 #include <qgspoint.h>
 #include "qgslogger.h"
+#include "qgsvectorlayer.h"
+#include "qgslinesymbol.h"
+#include "qgssinglesymbolrenderer.h"
+#include "qgsmultirenderchecker.h"
 
 class TestQgsClipper: public QObject
 {
@@ -30,20 +34,37 @@ class TestQgsClipper: public QObject
     Q_OBJECT
   private slots:
     void initTestCase(); // will be called before the first testfunction is executed.
-    void cleanupTestCase() {} // will be called after the last testfunction was executed.
+    void cleanupTestCase(); // will be called after the last testfunction was executed.
     void init() {} // will be called before each testfunction is executed.
     void cleanup() {} // will be called after every testfunction.
     void basic();
     void basicWithZ();
     void basicWithZInf();
+    void epsg4978LineRendering();
 
   private:
+    QString mReport;
+
     bool checkBoundingBox( const QPolygonF &polygon, const QgsRectangle &clipRect );
     bool checkBoundingBox( const QgsLineString &polygon, const QgsBox3d &clipRect );
+    bool render2dCheck( const QString &testName, QgsVectorLayer *layer, QgsRectangle extent );
 };
+
+void TestQgsClipper::cleanupTestCase()
+{
+  const QString myReportFile = QDir::tempPath() + "/qgistest.html";
+  QFile myFile( myReportFile );
+  if ( myFile.open( QIODevice::WriteOnly | QIODevice::Append ) )
+  {
+    QTextStream myQTextStream( &myFile );
+    myQTextStream << mReport;
+    myFile.close();
+  }
+}
 
 void TestQgsClipper::initTestCase()
 {
+  mReport = QStringLiteral( "<h1>Clipper Rendering Tests</h1>\n" );
 
 }
 
@@ -168,6 +189,46 @@ bool TestQgsClipper::checkBoundingBox( const QPolygonF &polygon, const QgsRectan
   const QgsRectangle bBox( polygon.boundingRect() );
 
   return clipRect.contains( bBox );
+}
+
+void TestQgsClipper::epsg4978LineRendering()
+{
+  QgsVectorLayer *layerLines = new QgsVectorLayer( QString( TEST_DATA_DIR ) + "/3d/earth_size_sphere_4978.gpkg", "lines", "ogr" );
+
+  QgsLineSymbol *fillSymbol = new QgsLineSymbol();
+  fillSymbol->setWidth( 0.5 );
+  fillSymbol->setColor( QColor( 255, 0, 0 ) );
+  layerLines->setRenderer( new QgsSingleSymbolRenderer( fillSymbol ) );
+
+  QVERIFY( render2dCheck( "4978_2D_line_rendering", layerLines, QgsRectangle( -2.5e7, -2.5e7, 2.5e7, 2.5e7 ) ) );
+
+  delete layerLines;
+}
+
+bool TestQgsClipper::render2dCheck( const QString &testName, QgsVectorLayer *layer, QgsRectangle extent )
+{
+  mReport += "<h2>" + testName + "</h2>\n";
+
+  const QString myTmpDir = QDir::tempPath() + '/';
+  const QString myFileName = myTmpDir + testName + ".png";
+
+  auto mMapSettings = new QgsMapSettings();
+  mMapSettings->setLayers( QList<QgsMapLayer *>() << layer );
+  mMapSettings->setExtent( extent );
+  mMapSettings->setOutputDpi( 96 );
+  QgsCoordinateReferenceSystem newCrs;
+  newCrs.createFromString( "EPSG:3857" );
+  mMapSettings->setDestinationCrs( newCrs );
+
+  QgsRenderChecker myChecker;
+  myChecker.setControlPathPrefix( QStringLiteral( "3d" ) );
+  myChecker.setControlName( "expected_" + testName );
+  myChecker.setMapSettings( *mMapSettings );
+  myChecker.setRenderedImage( myFileName );
+  myChecker.setColorTolerance( 50 );
+  const bool myResultFlag = myChecker.runTest( testName, 0 );
+  mReport += myChecker.report();
+  return myResultFlag;
 }
 
 QGSTEST_MAIN( TestQgsClipper )
