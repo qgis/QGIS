4fb2620e48019f4546c30b9a04709288776b2f97
Sandro Santilli <strk@kbt.io>
2022-06-06 15:35:15 +0200

    SOURCETREE fixture using QGIS_TEST_ACCEPT_GITSTATUS_CHECK_FAILURE
    When the env variable is set to value of 1 the fixture cleanup
    failure is not considered a failure but a skip.
    
    This is because currently the testsuite is still unable to leave
    the source tree in an untouched state.

--- scripts/chkgitstatus.sh
+++ scripts/chkgitstatus.sh
@@ -4,7 +4,7 @@ srcdir=$(dirname $0)/..
 
 command -v git > /dev/null || {
   echo "git is not found in PATH" >&2
-  exit 2
+  exit 2 # considered a "skip"
 }
 
 git -C ${srcdir} status -uno > .gitstatus-full || exit 1
@@ -18,6 +18,9 @@ elif test "$1" = "check"; then
   else
     echo "Source files (printed above) were modified. Diff follows:"
     git -C ${srcdir} diff
+    if test "${QGIS_TEST_ACCEPT_GITSTATUS_CHECK_FAILURE}" = "1"; then
+      exit 2 # considered a "skip" by CMake
+    fi
     exit 1
   fi
 else
