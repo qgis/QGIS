d99e0ed1fdacb74b6311d2c6da3d0c595a96eb27
Nyall Dawson <nyall.dawson@gmail.com>
2022-06-07 11:02:00 +1000

    Fix loss of precision when an extent is manually entered
    Fixes #48887

--- src/gui/qgsextentwidget.h
+++ src/gui/qgsextentwidget.h
@@ -289,6 +289,8 @@ class GUI_EXPORT QgsExtentWidget : public QWidget, private Ui::QgsExtentGroupBox
 
     QgsMapLayer *mapLayerFromMimeData( const QMimeData *data ) const;
 
+    friend class TestProcessingGui;
+
 
 };
 
