92ff79a0f1f77a3157a19251bafb11003f0b8f1d
Matthias Kuhn <matthias@opengis.ch>
2022-06-11 20:38:45 +0200

    Create a deep copy of wfs shared data on setSubsetString

--- src/providers/wfs/qgswfsdatasourceuri.h
+++ src/providers/wfs/qgswfsdatasourceuri.h
@@ -42,8 +42,8 @@ class QgsWFSDataSourceURI
 
     explicit QgsWFSDataSourceURI( const QString &uri );
 
-    //! Returns the URI, avoiding expansion of authentication configuration, which is handled during network access
-    const QString uri() const;
+    //! Returns the URI, optionally with the authentication configuration expanded
+    QString uri( bool expandAuthConfig = false ) const;
 
     //! Returns base URL (with SERVICE=WFS parameter if bIncludeServiceWFS=true)
     QUrl baseURL( bool bIncludeServiceWFS = true ) const;
