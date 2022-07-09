3afcb1098efd0857c3351d089494ce211f210255
Andrea Giudiceandrea <andreaerdna@libero.it>
2022-07-09 14:57:30 +0200

    QgsGeos: return null point instead of null geometry

--- src/core/geometry/qgsgeos.cpp
+++ src/core/geometry/qgsgeos.cpp
@@ -1363,12 +1363,12 @@ std::unique_ptr<QgsAbstractGeometry> QgsGeos::fromGeos( const GEOSGeometry *geos
     case GEOS_POINT:                 // a point
     {
       if ( GEOSisEmpty_r( geosinit()->ctxt, geos ) )
-        return nullptr;
+        return std::make_unique< QgsPoint >();
 
       const GEOSCoordSequence *cs = GEOSGeom_getCoordSeq_r( geosinit()->ctxt, geos );
       unsigned int nPoints = 0;
       GEOSCoordSeq_getSize_r( geosinit()->ctxt, cs, &nPoints );
-      return  nPoints > 0 ? std::unique_ptr<QgsAbstractGeometry>( coordSeqPoint( cs, 0, hasZ, hasM ).clone() ) : std::make_unique< QgsPoint >();
+      return nPoints > 0 ? std::unique_ptr<QgsAbstractGeometry>( coordSeqPoint( cs, 0, hasZ, hasM ).clone() ) : std::make_unique< QgsPoint >();
     }
     case GEOS_LINESTRING:
     {
