f43ba7993e18d16d3ee7188b7fe8768cb9f97903
Nyall Dawson <nyall.dawson@gmail.com>
2022-07-28 10:17:52 +1000

    Expose QgsWeakRelation to PyQGIS

--- src/core/qgsweakrelation.h
+++ src/core/qgsweakrelation.h
@@ -16,8 +16,6 @@
 #ifndef QGSWEAKRELATION_H
 #define QGSWEAKRELATION_H
 
-#define SIP_NO_FILE
-
 #include "qgis_core.h"
 #include "qgsrelation.h"
 #include "qgsvectorlayerref.h"
@@ -31,7 +29,6 @@
  * implemented and can be used to create a QgsRelation after the
  * dependent layers are loaded and available.
  *
- * \note not available in Python bindings
  * \ingroup core
  * \since QGIS 3.12
  */
@@ -45,13 +42,21 @@ class CORE_EXPORT QgsWeakRelation
      */
     enum WeakRelationType
     {
-      Referencing, //!< The layer is referencing
-      Referenced //!< The layer is referenced
+      Referencing, //!< The layer is referencing (or the "child" / "right" layer in the relationship)
+      Referenced //!< The layer is referenced (or the "parent" / "left" left in the relationship)
     };
 
+    /**
+     * Default constructor for an invalid relation.
+     */
+    QgsWeakRelation();
+
+#ifndef SIP_RUN
 
     /**
-     * Creates a QgsWeakRelation
+     * Creates a QgsWeakRelation.
+     *
+     * \note Not available in Python bindings.
      */
     QgsWeakRelation( const QString &relationId,
                      const QString &relationName,
@@ -66,6 +71,7 @@ class CORE_EXPORT QgsWeakRelation
                      const QString &referencedLayerProviderKey,
                      const QList<QgsRelation::FieldPair> &fieldPairs
                    );
+#endif
 
     /**
      * Resolves a weak relation in the given \a project returning a possibly invalid QgsRelation
@@ -74,30 +80,65 @@ class CORE_EXPORT QgsWeakRelation
      * \note Client code should never assume that the returned relation is valid and the
      * layer components are not NULL.
      */
+#ifndef SIP_RUN
     QgsRelation resolvedRelation( const QgsProject *project, QgsVectorLayerRef::MatchType matchType = QgsVectorLayerRef::MatchType::All ) const;
+#else
+    QgsRelation resolvedRelation( const QgsProject *project ) const;
+#endif
 
     /**
-     * Returns a weak reference to the referencing layer
+     * Returns the relationship's ID.
+     *
+     * \since QGIS 3.28
      */
-    QgsVectorLayerRef referencingLayer() const;
+    QString id() const { return mRelationId; }
 
     /**
-     * Returns a weak reference to the referenced layer
+     * Returns the relationship's name.
+     *
+     * \since QGIS 3.28
      */
-    QgsVectorLayerRef referencedLayer() const;
+    QString name() const { return mRelationName; }
 
     /**
-     * Returns the strength of the relation
+     * Returns a weak reference to the referencing (or "child" / "right") layer.
+     *
+     * \note Not available in Python bindings.
+     */
+    QgsVectorLayerRef referencingLayer() const SIP_SKIP;
+
+    /**
+     * Returns a weak reference to the referenced (or "parent" / "left") layer.
+     *
+     * \note Not available in Python bindings.
+     */
+    QgsVectorLayerRef referencedLayer() const SIP_SKIP;
+
+    /**
+     * Returns the strength of the relation.
      */
     Qgis::RelationshipStrength strength() const;
 
     /**
-     * Returns the list of field pairs
+     * Returns the list of field pairs.
      */
+#ifndef SIP_RUN
     QList<QgsRelation::FieldPair> fieldPairs() const;
+#else
+    QMap< QString, QString > fieldPairs() const;
+    % MethodCode
+    const QList< QgsRelation::FieldPair > &pairs = sipCpp->fieldPairs();
+    sipRes = new QMap< QString, QString >();
+    for ( const QgsRelation::FieldPair &pair : pairs )
+    {
+      sipRes->insert( pair.first, pair.second );
+    }
+    % End
+#endif
 
     /**
-     * Returns a weak relation for the given layer
+     * Returns a weak relation for the given layer.
+     *
      * \param layer the layer of the weak relation
      * \param type determines if the layer is referencing or referenced
      * \param node the QDomNode
@@ -117,6 +158,28 @@ class CORE_EXPORT QgsWeakRelation
      * \since QGIS 3.16
      */
     static void writeXml( const QgsVectorLayer *layer, WeakRelationType type, const QgsRelation &relation, QDomNode &node, QDomDocument &doc );
+
+#ifdef SIP_RUN
+    SIP_PYOBJECT __repr__();
+    % MethodCode
+
+    QString leftIdentifier;
+    if ( !sipCpp->referencedLayer().source.isEmpty() )
+      leftIdentifier = sipCpp->referencedLayer().source;
+
+    QString rightIdentifier;
+    if ( !sipCpp->referencingLayer().source.isEmpty() )
+      rightIdentifier = sipCpp->referencingLayer().source;
+
+    QString str;
+    if ( leftIdentifier.isEmpty() && rightIdentifier.isEmpty() )
+      str = QStringLiteral( "<QgsWeakRelation: %1>" ).arg( sipCpp->id() );
+    else
+      str = QStringLiteral( "<QgsWeakRelation: %1 - %2 -> %3>" ).arg( sipCpp->id(), leftIdentifier, rightIdentifier );
+
+    sipRes = PyUnicode_FromString( str.toUtf8().constData() );
+    % End
+#endif
 
   private:
 
