31cc22153c9e53e1ce2156f385e2ef3163cf9b61
Julien Cabieces <julien.cabieces@oslandia.com>
2022-08-09 07:45:29 +0200

    check size equals 0

--- src/core/geometry/qgslinestring.cpp
+++ src/core/geometry/qgslinestring.cpp
@@ -1093,6 +1093,12 @@ void QgsLineString::setPoints( size_t size, const double *x, const double *y, co
 {
   clearCache(); //set bounding box invalid
 
+  if ( size == 0 )
+  {
+    clear();
+    return;
+  }
+
   const bool hasZ = static_cast< bool >( z );
   const bool hasM = static_cast< bool >( m );
 
