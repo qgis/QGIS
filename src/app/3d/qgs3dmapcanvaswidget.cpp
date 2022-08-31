qgis/QGIS

commit 9e0074c5b4fea78bab72469f35a672045acf9423

qgs3dmapcanvaswidget: Use close button for the configuration dialog
With this dialog being non-modal (see previous commit), it is more
appropriate to have just the Close button instead of OK/Cancel.

src/app/3d/qgs3dmapcanvaswidget.cpp
@@ -394,7 +394,7 @@ void Qgs3DMapCanvasWidget::configure()
 
   Qgs3DMapSettings *map = mCanvas->map();
   Qgs3DMapConfigWidget *w = new Qgs3DMapConfigWidget( map, mMainCanvas, mCanvas, dlg );
-  QDialogButtonBox *buttons = new QDialogButtonBox( QDialogButtonBox::Ok | QDialogButtonBox::Apply | QDialogButtonBox::Cancel | QDialogButtonBox::Help, dlg );
+  QDialogButtonBox *buttons = new QDialogButtonBox( QDialogButtonBox::Apply | QDialogButtonBox::Close | QDialogButtonBox::Help, dlg );
 
   auto applyConfig = [ = ]()
   {

commit 30eeef35dd477b5c5a6166d7bbd7b1033da06d56

qgs3dmapcanvaswidget: Update state of the correct button on change
This is fix for a regression introduced by commit
9e0074c5b4fea78bab72469f35a672045acf9423.

With the configuration window being non modal, the `OK` and `Cancel`
buttons were removed to keep only the `Apply` button. However, the
state of the removed `Ok` button was still updated on every change,
hence creating a crash.

This issue is fixed by changing the state of the `Apply` button
instead.

src/app/3d/qgs3dmapcanvaswidget.cpp
@@ -436,7 +436,7 @@ void Qgs3DMapCanvasWidget::configure()
 
   connect( w, &Qgs3DMapConfigWidget::isValidChanged, this, [ = ]( bool valid )
   {
-    buttons->button( QDialogButtonBox::Ok )->setEnabled( valid );
+    buttons->button( QDialogButtonBox::Apply )->setEnabled( valid );
   } );
 
   QVBoxLayout *layout = new QVBoxLayout( dlg );