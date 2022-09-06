700042798cd1432e4e8f8e990075eccdad7cd4d5
Damiano Lombardi <damiano@opengis.ch>
2022-09-06 14:11:27 +0200

    Set sys.argv even if --code is empty

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1547,15 +1547,26 @@ int main( int argc, char *argv[] )
     }
   }
 
+  if ( !pythonArgs.isEmpty() )
+  {
+    if ( !pythonfile.isEmpty() )
+    {
+#ifdef Q_OS_WIN
+      //replace backslashes with forward slashes
+      pythonfile.replace( '\\', '/' );
+#endif
+      pythonArgs.prepend( pythonfile );
+    }
+    QgsPythonRunner::run( QStringLiteral( "sys.argv = ['%1']" ).arg( pythonArgs.join( "','" ) ) );
+  }
+
   if ( !pythonfile.isEmpty() )
   {
 #ifdef Q_OS_WIN
     //replace backslashes with forward slashes
     pythonfile.replace( '\\', '/' );
 #endif
-    pythonArgs.prepend( pythonfile );
-    QgsPythonRunner::run( QStringLiteral( "sys.argv = ['%1']\n"
-                                          "with open('%2','r') as f: exec(f.read())" ).arg( pythonArgs.join( "','" ), pythonfile ) );
+    QgsPythonRunner::run( QStringLiteral( "with open('%1','r') as f: exec(f.read())" ).arg( pythonfile ) );
   }
 
   /////////////////////////////////`////////////////////////////////////
