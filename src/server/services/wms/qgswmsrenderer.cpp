c780ea65c0a9339a7da840054e34ef92e14d32cf
Alessandro Pasotti <elpaso@itopen.it>
2022-09-15 11:33:15 +0200

    Server WMS gfi, fix maptips layer scope
    Fixes #30719

--- src/server/services/wms/qgswmsrenderer.cpp
+++ src/server/services/wms/qgswmsrenderer.cpp
@@ -1828,7 +1828,9 @@ namespace QgsWms
         {
           QDomElement maptipElem = infoDocument.createElement( QStringLiteral( "Attribute" ) );
           maptipElem.setAttribute( QStringLiteral( "name" ), QStringLiteral( "maptip" ) );
-          maptipElem.setAttribute( QStringLiteral( "value" ),  QgsExpression::replaceExpressionText( mapTip, &renderContext.expressionContext() ) );
+          QgsExpressionContext context { renderContext.expressionContext() };
+          context.appendScope( QgsExpressionContextUtils::layerScope( layer ) );
+          maptipElem.setAttribute( QStringLiteral( "value" ),  QgsExpression::replaceExpressionText( mapTip, &context ) );
           featureElement.appendChild( maptipElem );
         }
 
