7b8cbc457affce3532e41309799540967ecf6c9f
uclaros <uclaros@gmail.com>
2022-09-22 13:30:52 +0300

    ignore stderr when checking astyle version

--- scripts/astyle.sh
+++ scripts/astyle.sh
@@ -22,7 +22,7 @@ fi
 
 min_version="3"
 astyle_version_check() {
-	[ $(printf "$($1 --version 2>&1 | cut -d ' ' -f4)\\n$min_version" | sort -${SV} | head -n1) = "$min_version" ]
+	[ $(printf "$($1 --version 2>/dev/null | cut -d ' ' -f4)\\n$min_version" | sort -${SV} | head -n1) = "$min_version" ]
 }
 
 for ASTYLE in ${QGISSTYLE} $(dirname "$0")/qgisstyle $(dirname "$0")/RelWithDebInfo/qgisstyle astyle
