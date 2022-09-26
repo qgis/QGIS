fbef581f844d58ca7147a6d830600b7fec3e50f0
Alessandro Pasotti <elpaso@itopen.it>
2022-09-26 12:02:29 +0200

    Fix SLD rule text fields quoting
    Fixes #42348

--- src/core/symbology/qgscategorizedsymbolrenderer.cpp
+++ src/core/symbology/qgscategorizedsymbolrenderer.cpp
@@ -147,13 +147,13 @@ void QgsRendererCategory::toSld( QDomDocument &doc, QDomElement &element, QVaria
   if ( mValue.isNull() || mValue.toString().isEmpty() )
   {
     filterFunc = QStringLiteral( "%1 = '%2' or %1 is null" )
-                 .arg( attrName.replace( '\"', QLatin1String( "\"\"" ) ),
+                 .arg( attrName.replace( '\"', QLatin1String( "\"\"" ) ).append( '"' ).prepend( '"' ),
                        mValue.toString().replace( '\'', QLatin1String( "''" ) ) );
   }
   else
   {
     filterFunc = QStringLiteral( "%1 = '%2'" )
-                 .arg( attrName.replace( '\"', QLatin1String( "\"\"" ) ),
+                 .arg( attrName.replace( '\"', QLatin1String( "\"\"" ) ).append( '"' ).prepend( '"' ),
                        mValue.toString().replace( '\'', QLatin1String( "''" ) ) );
   }
 
