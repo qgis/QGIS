170a8895ff2fb3f1d9c623c1c39f9ce24aa9677d
Maxim Rylov <maxim.rylov@sap.com>
2022-09-29 16:55:56 +0200

    HANA: properly handle special characters in connection string

--- src/providers/hana/qgshanaconnectionstringbuilder.cpp
+++ src/providers/hana/qgshanaconnectionstringbuilder.cpp
@@ -21,6 +21,7 @@ QgsHanaConnectionStringBuilder::QgsHanaConnectionStringBuilder( const QgsDataSou
   , mHost( uri.host() )
   , mPort( uri.port() )
   , mDatabase( uri.database() )
+  , mSchema( uri.schema() )
   , mUserName( uri.username() )
   , mPassword( uri.password() )
 {
@@ -52,42 +53,75 @@ QgsHanaConnectionStringBuilder::QgsHanaConnectionStringBuilder( const QgsDataSou
 
 QString QgsHanaConnectionStringBuilder::toString() const
 {
-  // For more details on how to escape special characters in passwords,
-  // see https://stackoverflow.com/questions/55150362/maybe-illegal-character-in-odbc-sql-server-connection-string-pwd
-  const QString pwd = QString( mPassword ).replace( '}', QLatin1String( "}}" ) );
-  QString ret = !mDsn.isEmpty()
-                ? QStringLiteral( "DSN=%1;UID={%2};PWD={%3};CHAR_AS_UTF8=1" ).arg(
-                  mDsn, mUserName, pwd )
-                : QStringLiteral( "DRIVER={%1};SERVERNODE=%2:%3;DATABASENAME=%4;UID={%5};PWD={%6};CHAR_AS_UTF8=1" ).arg(
-                  mDriver, mHost, mPort, mDatabase, mUserName, pwd );
-  if ( !mSchema.isEmpty() )
-    ret += QStringLiteral( ";CURRENTSCHEMA=" ) + mSchema;
+  QStringList props;
+  auto addProperty = [&props]( const QString & name, const QString & value )
+  {
+    // See notes for constructing connection string for HANA
+    // https://help.sap.com/docs/SAP_HANA_CLIENT/f1b440ded6144a54ada97ff95dac7adf/7cab593774474f2f8db335710b2f5c50.html
+
+    if ( value.isEmpty() )
+      return;
+
+    bool hasSpecialChars = false;
+    for ( const QChar ch : value )
+    {
+      if ( QStringLiteral( "[]{}(),;?*=!@" ).indexOf( ch ) != -1 )
+      {
+        hasSpecialChars = true;
+        break;
+      }
+    }
+
+    if ( hasSpecialChars )
+    {
+      QString newValue = QString( value ).replace( '}', QLatin1String( "}}" ) );
+      props.append( name + "={" + newValue + "}" );
+    }
+    else
+    {
+      props.append( name + "=" + value );
+    }
+  };
+
+  if ( !mDsn.isEmpty() )
+  {
+    addProperty( QStringLiteral( "DSN" ), mDsn );
+  }
+  else
+  {
+    addProperty( QStringLiteral( "DRIVER" ), mDriver );
+    addProperty( QStringLiteral( "SERVERNODE" ), QStringLiteral( "%1:%2" ).arg( mHost, mPort ) );
+    addProperty( QStringLiteral( "DATABASENAME" ), mDatabase );
+  }
+
+  addProperty( QStringLiteral( "UID" ), mUserName );
+  addProperty( QStringLiteral( "PWD" ), mPassword );
+  addProperty( QStringLiteral( "CURRENTSCHEMA" ), mSchema );
+
   if ( mSslEnabled )
   {
-    ret += QLatin1String( ";encrypt=true" );
-    if ( !mSslCryptoProvider.isEmpty() )
-      ret += QStringLiteral( ";sslCryptoProvider=" ) + mSslCryptoProvider;
-    ret += QStringLiteral( ";sslValidateCertificate=" ) + QString( mSslValidateCertificate ? QStringLiteral( "true" ) : QStringLiteral( "false" ) );
-    if ( !mSslHostNameInCertificate.isEmpty() )
-      ret += QStringLiteral( ";sslHostNameInCertificate=" ) + mSslHostNameInCertificate;
-    if ( !mSslKeyStore.isEmpty() )
-      ret += QStringLiteral( ";sslKeyStore=" ) + mSslKeyStore;
-    if ( !mSslKeyStore.isEmpty() )
-      ret += QStringLiteral( ";sslTrustStore=" ) + mSslTrustStore;
+    addProperty( QStringLiteral( "encrypt" ), QStringLiteral( "true" ) );
+    addProperty( QStringLiteral( "sslCryptoProvider" ), mSslCryptoProvider );
+    addProperty( QStringLiteral( "sslValidateCertificate" ), mSslValidateCertificate ? QStringLiteral( "true" ) : QStringLiteral( "false" ) );
+    addProperty( QStringLiteral( "sslHostNameInCertificate" ), mSslHostNameInCertificate );
+    addProperty( QStringLiteral( "sslKeyStore" ), mSslKeyStore );
+    addProperty( QStringLiteral( "sslTrustStore" ), mSslTrustStore );
   }
 
   if ( mProxyEnabled )
   {
     if ( mProxyHttp )
-      ret += QStringLiteral( ";proxyHttp=TRUE" );
-    ret += QStringLiteral( ";proxyHostname=" ) + mProxyHost;
-    ret += QStringLiteral( ";proxyPort=" ) + QString::number( mProxyPort );
+      addProperty( QStringLiteral( "proxyHttp" ), QStringLiteral( "TRUE" ) );
+    addProperty( QStringLiteral( "proxyHostname" ), mProxyHost );
+    addProperty( QStringLiteral( "proxyPort" ), QString::number( mProxyPort ) );
     if ( !mProxyUsername.isEmpty() )
     {
-      ret += QStringLiteral( ";proxyUserName=" ) + mProxyUsername;
-      ret += QStringLiteral( ";proxyPassword=" ) + mProxyPassword;
+      addProperty( QStringLiteral( "proxyUserName" ), mProxyUsername );
+      addProperty( QStringLiteral( "proxyPassword" ), mProxyPassword );
     }
   }
 
-  return ret;
+  addProperty( QStringLiteral( "CHAR_AS_UTF8" ), QStringLiteral( "1" ) );
+
+  return props.join( QStringLiteral( ";" ) );
 }
