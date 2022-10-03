dfc8ab612be3edf835c4752026c9073b296c89e5
Andrea Giudiceandrea <andreaerdna@libero.it>
2022-10-03 15:27:49 +0200

    Make some strings translatable in QgsLeastSquares

--- src/analysis/georeferencing/qgsleastsquares.cpp
+++ src/analysis/georeferencing/qgsleastsquares.cpp
@@ -77,7 +77,7 @@ void QgsLeastSquares::helmert( const QVector<QgsPointXY> &sourceCoordinates,
   ( void )origin;
   ( void )pixelSize;
   ( void )rotation;
-  throw QgsNotSupportedException( QStringLiteral( "Calculating a helmert transformation requires a QGIS build based GSL" ) );
+  throw QgsNotSupportedException( QObject::tr( "Calculating a helmert transformation requires a QGIS build based GSL" ) );
 #else
   const int n = destinationCoordinates.size();
   if ( n < 2 )
@@ -262,7 +262,7 @@ void QgsLeastSquares::projective( const QVector<QgsPointXY> &sourceCoordinates,
   ( void )sourceCoordinates;
   ( void )destinationCoordinates;
   ( void )H;
-  throw QgsNotSupportedException( QStringLiteral( "Calculating a projective transformation requires a QGIS build based GSL" ) );
+  throw QgsNotSupportedException( QObject::tr( "Calculating a projective transformation requires a QGIS build based GSL" ) );
 #else
   Q_ASSERT( sourceCoordinates.size() == destinationCoordinates.size() );
 
