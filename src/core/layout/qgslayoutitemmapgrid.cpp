283bfcaa7ae42b8cc18f212bfcc918cc30d0a73b
Alessandro Pasotti <elpaso@itopen.it>
2022-10-04 11:51:46 +0200

    Manual backport of #50295 grid multiline labels

--- src/core/layout/qgslayoutitemmapgrid.cpp
+++ src/core/layout/qgslayoutitemmapgrid.cpp
@@ -1392,7 +1392,7 @@ void QgsLayoutItemMapGrid::drawCoordinateAnnotation( QgsRenderContext &context, 
   context.painter()->rotate( rotation );
   context.painter()->translate( -anchor );
   const QgsScopedRenderContextScaleToPixels scale( context );
-  QgsTextRenderer::drawText( QPointF( 0, 0 ), 0, QgsTextRenderer::AlignLeft, QStringList() << annotationString, context, mAnnotationFormat );
+  QgsTextRenderer::drawText( QPointF( 0, 0 ), 0, QgsTextRenderer::AlignLeft, annotationString.split( '\n' ), context, mAnnotationFormat );
 }
 
 QString QgsLayoutItemMapGrid::gridAnnotationString( double value, QgsLayoutItemMapGrid::AnnotationCoordinate coord, QgsExpressionContext &expressionContext ) const
