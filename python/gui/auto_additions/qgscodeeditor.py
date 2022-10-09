39a4a6389072968ba582e9e9367380726643b090
Nyall Dawson <nyall.dawson@gmail.com>
2022-10-10 08:28:19 +1000

    Promote to enum class

---
+++ python/gui/auto_additions/qgscodeeditor.py
@@ -0,0 +1,14 @@
+# The following has been generated automatically from src/gui/codeeditors/qgscodeeditor.h
+# monkey patching scoped based enum
+QgsCodeEditor.LineNumbers = QgsCodeEditor.MarginRole.LineNumbers
+QgsCodeEditor.LineNumbers.is_monkey_patched = True
+QgsCodeEditor.LineNumbers.__doc__ = "Line numbers"
+QgsCodeEditor.ErrorIndicators = QgsCodeEditor.MarginRole.ErrorIndicators
+QgsCodeEditor.ErrorIndicators.is_monkey_patched = True
+QgsCodeEditor.ErrorIndicators.__doc__ = "Error indicators"
+QgsCodeEditor.FoldingControls = QgsCodeEditor.MarginRole.FoldingControls
+QgsCodeEditor.FoldingControls.is_monkey_patched = True
+QgsCodeEditor.FoldingControls.__doc__ = "Folding controls"
+QgsCodeEditor.MarginRole.__doc__ = 'Margin roles.\n\nThis enum contains the roles which the different numbered margins are used for.\n\n.. versionadded:: 3.16\n\n' + '* ``LineNumbers``: ' + QgsCodeEditor.MarginRole.LineNumbers.__doc__ + '\n' + '* ``ErrorIndicators``: ' + QgsCodeEditor.MarginRole.ErrorIndicators.__doc__ + '\n' + '* ``FoldingControls``: ' + QgsCodeEditor.MarginRole.FoldingControls.__doc__
+# --
+QgsCodeEditor.MarginRole.baseClass = QgsCodeEditor
