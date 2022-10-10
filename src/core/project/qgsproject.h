bc4d27518c4dbe267fc86ebd72537b3a0ab83e4c
Blottiere Paul <blottiere.paul@gmail.com>
2022-10-10 09:02:17 +0200

    Fix doc

--- src/core/project/qgsproject.h
+++ src/core/project/qgsproject.h
@@ -2174,7 +2174,7 @@ class CORE_EXPORT QgsProject : public QObject, public QgsExpressionContextGenera
     bool addLayer( const QDomElement &layerElem, QList<QDomNode> &brokenNodes, QgsReadWriteContext &context, Qgis::ProjectReadFlags flags = Qgis::ProjectReadFlags() ) SIP_SKIP;
 
     /**
-     * Remove auxiliary layer thanks to a vector later id.
+     * Remove auxiliary layer of the corresponding layer.
      */
     void removeAuxiliaryLayer( const QgsMapLayer *ml );
 
