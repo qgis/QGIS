91e331d3f3f0ee0b9b4bfeb62ec8c64a502f06d8
Julien Cabieces <julien.cabieces@oslandia.com>
2022-10-12 19:17:56 +0200

    Fixes #49589 : avoid endless recursion when evaluating aggregate expressions

--- src/core/expression/qgsexpressionutils.cpp
+++ src/core/expression/qgsexpressionutils.cpp
@@ -46,6 +46,17 @@ std::tuple<QVariant::Type, int> QgsExpressionUtils::determineResultType( const Q
   request.setLimit( 10 );
   request.setExpressionContext( context );
 
+  // avoid endless recursion by removing virtual fields while going through features
+  // to determine result type
+  QgsAttributeList attributes;
+  const QgsFields fields = layer->fields();
+  for ( int i = 0; i < fields.count(); i++ )
+  {
+    if ( fields.fieldOrigin( i ) != QgsFields::OriginExpression )
+      attributes << i;
+  }
+  request.setSubsetOfAttributes( attributes );
+
   QVariant value;
   QgsFeature f;
   QgsFeatureIterator it = layer->getFeatures( request );
@@ -65,5 +76,3 @@ std::tuple<QVariant::Type, int> QgsExpressionUtils::determineResultType( const Q
   value = QVariant();
   return std::make_tuple( value.type(), value.userType() );
 }
-
-
