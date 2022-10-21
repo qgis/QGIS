qgis/QGIS

commit 3d168eddee9da880c71688603340b3697eda9116

Fix raster contour renderer ignores opacity setting
Fixes #41663

src/core/raster/qgsrasterlayerrenderer.cpp
@@ -22,7 +22,7 @@
 #include "qgsrasterlayer.h"
 #include "qgsrasterprojector.h"
 #include "qgsrendercontext.h"
-#include "qgsproject.h"
+#include "qgsrasterrenderer.h"
 #include "qgsexception.h"
 #include "qgsrasterlayertemporalproperties.h"
 #include "qgsmapclippingutils.h"
@@ -70,6 +70,7 @@ void QgsRasterLayerRendererFeedback::onNewData()
 ///
 QgsRasterLayerRenderer::QgsRasterLayerRenderer( QgsRasterLayer *layer, QgsRenderContext &rendererContext )
   : QgsMapLayerRenderer( layer->id(), &rendererContext )
+  , mLayerOpacity( layer->opacity() )
   , mProviderCapabilities( static_cast<QgsRasterDataProvider::Capability>( layer->dataProvider()->capabilities() ) )
   , mFeedback( new QgsRasterLayerRendererFeedback( this ) )
 {
@@ -374,5 +375,15 @@ QgsFeedback *QgsRasterLayerRenderer::feedback() const
 bool QgsRasterLayerRenderer::forceRasterRender() const
 {
   // preview of intermediate raster rendering results requires a temporary output image
-  return renderContext()->testFlag( Qgis::RenderContextFlag::RenderPartialOutput );
+  if ( renderContext()->testFlag( Qgis::RenderContextFlag::RenderPartialOutput ) )
+    return true;
+
+  if ( QgsRasterRenderer *renderer = mPipe->renderer() )
+  {
+    if ( !( renderer->flags() & Qgis::RasterRendererFlag::InternalLayerOpacityHandling )
+         && renderContext()->testFlag( Qgis::RenderContextFlag::UseAdvancedEffects ) && ( !qgsDoubleNear( mLayerOpacity, 1.0 ) ) )
+      return true;
+  }
+
+  return false;
 }

python/core/auto_additions/qgis.py
@@ -708,6 +708,13 @@ Qgis.RasterResamplingStage.__doc__ = 'Stage at which raster resampling occurs.\n
 # --
 Qgis.RasterResamplingStage.baseClass = Qgis
 # monkey patching scoped based enum
+Qgis.RasterRendererFlag.InternalLayerOpacityHandling.__doc__ = "The renderer internally handles the raster layer's opacity, so the default layer level opacity handling should not be applied."
+Qgis.RasterRendererFlag.__doc__ = 'Flags which control behavior of raster renderers.\n\n.. versionadded:: 3.28\n\n' + '* ``InternalLayerOpacityHandling``: ' + Qgis.RasterRendererFlag.InternalLayerOpacityHandling.__doc__
+# --
+Qgis.RasterRendererFlag.baseClass = Qgis
+Qgis.RasterRendererFlags.baseClass = Qgis
+RasterRendererFlags = Qgis  # dirty hack since SIP seems to introduce the flags in module
+# monkey patching scoped based enum
 Qgis.MeshEditingErrorType.NoError.__doc__ = "No type"
 Qgis.MeshEditingErrorType.InvalidFace.__doc__ = "An error occurs due to an invalid face (for example, vertex indexes are unordered)"
 Qgis.MeshEditingErrorType.TooManyVerticesInFace.__doc__ = "A face has more vertices than the maximum number supported per face"

python/core/auto_generated/qgis.sip.in
@@ -452,6 +452,15 @@ The development version
       Provider
     };
 
+    enum class RasterRendererFlag
+    {
+      InternalLayerOpacityHandling,
+    };
+
+    typedef QFlags<Qgis::RasterRendererFlag> RasterRendererFlags;
+
+
+
     enum class MeshEditingErrorType
     {
       NoError,
@@ -1415,6 +1424,7 @@ QFlags<Qgis::RasterTemporalCapabilityFlag> operator|(Qgis::RasterTemporalCapabil
 
 QFlags<Qgis::SelectionFlag> operator|(Qgis::SelectionFlag f1, QFlags<Qgis::SelectionFlag> f2);
 
+QFlags<Qgis::RasterRendererFlag> operator|(Qgis::RasterRendererFlag f1, QFlags<Qgis::RasterRendererFlag> f2);
 
 
 

python/core/auto_generated/raster/qgshillshaderenderer.sip.in
@@ -37,6 +37,8 @@ A renderer for generating live hillshade models.
 
     virtual QgsHillshadeRenderer *clone() const /Factory/;
 
+    virtual Qgis::RasterRendererFlags flags() const;
+
 
     static QgsRasterRenderer *create( const QDomElement &elem, QgsRasterInterface *input ) /Factory/;
 %Docstring

python/core/auto_generated/raster/qgsmultibandcolorrenderer.sip.in
@@ -31,6 +31,8 @@ Renderer for multiband images with the color components
 %Docstring
 QgsMultiBandColorRenderer cannot be copied. Use :py:func:`~QgsMultiBandColorRenderer.clone` instead.
 %End
+    virtual Qgis::RasterRendererFlags flags() const;
+
 
     static QgsRasterRenderer *create( const QDomElement &elem, QgsRasterInterface *input ) /Factory/;
 

python/core/auto_generated/raster/qgspalettedrasterrenderer.sip.in
@@ -48,6 +48,9 @@ Constructor for QgsPalettedRasterRenderer.
 %Docstring
 QgsPalettedRasterRenderer cannot be copied. Use :py:func:`~QgsPalettedRasterRenderer.clone` instead.
 %End
+    virtual Qgis::RasterRendererFlags flags() const;
+
+
     static QgsRasterRenderer *create( const QDomElement &elem, QgsRasterInterface *input ) /Factory/;
 
     virtual QgsRasterBlock *block( int bandNo, const QgsRectangle &extent, int width, int height, QgsRasterBlockFeedback *feedback = 0 ) /Factory/;

python/core/auto_generated/raster/qgsrasterrenderer.sip.in
@@ -45,6 +45,16 @@ QgsRasterRenderer cannot be copied. Use :py:func:`~QgsRasterRenderer.clone` inst
 
 
     virtual QString type() const;
+%Docstring
+Returns a unique string representation of the renderer type.
+%End
+
+    virtual Qgis::RasterRendererFlags flags() const;
+%Docstring
+Returns flags which dictate renderer behavior.
+
+.. versionadded:: 3.28
+%End
 
     virtual bool setInput( QgsRasterInterface *input );
 

python/core/auto_generated/raster/qgssinglebandcolordatarenderer.sip.in
@@ -28,6 +28,8 @@ Raster renderer pipe for single band color.
 %Docstring
 QgsSingleBandColorDataRenderer cannot be copied. Use :py:func:`~QgsSingleBandColorDataRenderer.clone` instead.
 %End
+    virtual Qgis::RasterRendererFlags flags() const;
+
 
     static QgsRasterRenderer *create( const QDomElement &elem, QgsRasterInterface *input ) /Factory/;
 

commit c3d26cf631ce4db61987babab4b5d0f89794b525

Fix a crash when rendering a raster outside of its extent
If QgsRasterLayerRenderer constructor exists prematurely because there
is nothing to render, its internal pointers end up null, but forceRasterRender()
was expecting that mPipe was always allocated.

The crash was introduced recently in #50382

src/core/raster/qgsrasterlayerrenderer.cpp
@@ -374,6 +374,9 @@ QgsFeedback *QgsRasterLayerRenderer::feedback() const
 
 bool QgsRasterLayerRenderer::forceRasterRender() const
 {
+  if ( !mRasterViewPort || !mPipe )
+    return false;  // this layer is not going to get rendered
+
   // preview of intermediate raster rendering results requires a temporary output image
   if ( renderContext()->testFlag( Qgis::RenderContextFlag::RenderPartialOutput ) )
     return true;