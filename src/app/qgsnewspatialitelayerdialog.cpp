6b7557148aadf6e08e7d0ddfa7aeb728eeb8a869
Nyall Dawson <nyall.dawson@gmail.com>
2022-11-19 12:46:27 +1000

    In various "new *** layer" dialogs, show a warning when the user accepts the dialog but has entered a field name in the new field box without adding it to the fields list
    Avoids a papercut where a user thinks that just entering
    the name of a field alone is sufficient to create that
    field in the resultant table

--- src/app/qgsnewspatialitelayerdialog.cpp
+++ src/app/qgsnewspatialitelayerdialog.cpp
@@ -336,6 +336,33 @@ void QgsNewSpatialiteLayerDialog::buttonBox_rejected()
 
 bool QgsNewSpatialiteLayerDialog::apply()
 {
+  if ( !mNameEdit->text().trimmed().isEmpty() )
+  {
+    const QString currentFieldName = mNameEdit->text();
+    bool currentFound = false;
+    QTreeWidgetItemIterator it( mAttributeView );
+    while ( *it )
+    {
+      QTreeWidgetItem *item = *it;
+      if ( item->text( 0 ) == currentFieldName )
+      {
+        currentFound = true;
+        break;
+      }
+      ++it;
+    }
+
+    if ( !currentFound )
+    {
+      if ( QMessageBox::question( this, windowTitle(),
+                                  tr( "The field “%1” has not been added to the fields list. Are you sure you want to proceed and discard this field?" ).arg( currentFieldName ),
+                                  QMessageBox::Ok | QMessageBox::Cancel ) != QMessageBox::Ok )
+      {
+        return false;
+      }
+    }
+  }
+
   const QgsDataSourceUri dbUri = mDatabaseComboBox->currentConnectionUri();
   const QString dbPath = dbUri.database();
 
