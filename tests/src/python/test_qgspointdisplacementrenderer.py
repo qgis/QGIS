3261df495b5698c5f5d1c60e207074e79f2432f9
Alex <roya0045@users.noreply.github.com>
2022-12-23 10:05:37 -0500

    Update test_qgspointdisplacementrenderer.py

--- tests/src/python/test_qgspointdisplacementrenderer.py
+++ tests/src/python/test_qgspointdisplacementrenderer.py
@@ -132,7 +132,7 @@ class TestQgsPointDisplacementRenderer(unittest.TestCase):
         f = QgsFontUtils.getStandardTestFont('Bold Oblique', 14)
         stylename = r.labelFormat().namedStyle()
         if stylename == "":
-          stylename = QgsFontUtils.resolveFontStyleName(r.labelFormat().font()) #style name may be lost along the way
+            stylename = QgsFontUtils.resolveFontStyleName(r.labelFormat().font())
         self.assertEqual(stylename, f.styleName())
         self.assertEqual(r.minimumLabelScale(), 50000)
         self.assertEqual(r.labelColor(), QColor(255, 0, 0))
