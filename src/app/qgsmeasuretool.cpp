dce8b4309ac48c8bf6b5f7dcd036524a05a43be0
Yoann Quenach de Quivillic <yoann.quenach@gmail.com>
2023-01-16 22:07:12 +0100

    Fixes #46204 QgsMeasureTool create invalid point when measurement is resumed

--- src/app/qgsmeasuretool.cpp
+++ src/app/qgsmeasuretool.cpp
@@ -102,6 +102,17 @@ void QgsMeasureTool::deactivate()
   mDialog->hide();
   mRubberBand->hide();
   mRubberBandPoints->hide();
+
+  // Deactivating the tool does not reset the measure.
+  // Remove the last temporary point as to not duplicate it when
+  // the tool is re activated
+  int nbTempVertices = mRubberBand->numberOfVertices();
+  int nbVertices = mRubberBandPoints->numberOfVertices();
+  if ( nbTempVertices > nbVertices )
+  {
+    mRubberBand->removeLastPoint();
+  }
+
   QgsMapTool::deactivate();
 }
 
