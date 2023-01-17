7fa484dbf65463b6ee2d7c65014434e295631ef1
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-01-17 08:39:30 +0100

    sipify

---
+++ python/core/auto_additions/qgssettingstreenode.py
@@ -0,0 +1,15 @@
+# The following has been generated automatically from src/core/settings/qgssettingstreenode.h
+# monkey patching scoped based enum
+QgsSettingsTreeNode.Type.Root.__doc__ = "Root Element"
+QgsSettingsTreeNode.Type.Standard.__doc__ = "Normal Element"
+QgsSettingsTreeNode.Type.NamedList.__doc__ = ""
+QgsSettingsTreeNode.Type.__doc__ = 'Type of tree element\n\n' + '* ``Root``: ' + QgsSettingsTreeNode.Type.Root.__doc__ + '\n' + '* ``Standard``: ' + QgsSettingsTreeNode.Type.Standard.__doc__ + '\n' + '* ``NamedList``: ' + QgsSettingsTreeNode.Type.NamedList.__doc__
+# --
+QgsSettingsTreeNode.Type.baseClass = QgsSettingsTreeNode
+# monkey patching scoped based enum
+QgsSettingsTreeNode.Option.NamedListSelectedItemSetting.__doc__ = "Creates a setting to store which is the current item"
+QgsSettingsTreeNode.Option.__doc__ = 'Options for named list elements\n\n' + '* ``NamedListSelectedItemSetting``: ' + QgsSettingsTreeNode.Option.NamedListSelectedItemSetting.__doc__
+# --
+QgsSettingsTreeNode.Option.baseClass = QgsSettingsTreeNode
+QgsSettingsTreeNode.Options.baseClass = QgsSettingsTreeNode
+Options = QgsSettingsTreeNode  # dirty hack since SIP seems to introduce the flags in module
