c82f35f5a21883fc00815be79b9d4ffa4a0dfa1e
Nyall Dawson <nyall.dawson@gmail.com>
2023-01-31 14:03:12 +1000

    Ensure using @geometry/@feature variables trigger geometry fetching in expression calculation
    Fixes #50791

--- tests/src/core/testqgsexpression.cpp
+++ tests/src/core/testqgsexpression.cpp
@@ -2376,6 +2376,7 @@ class TestQgsExpression: public QObject
 
       // newer form
       QgsExpression exp2( QStringLiteral( "@feature" ) );
+      QVERIFY( exp2.needsGeometry() );
       v = exp2.evaluate( &context );
       evalFeature = v.value<QgsFeature>();
       QCOMPARE( evalFeature.id(), f.id() );
@@ -2402,6 +2403,7 @@ class TestQgsExpression: public QObject
 
       // newer form
       QgsExpression exp2( QStringLiteral( "geom_to_wkt(@geometry)" ) );
+      QVERIFY( exp2.needsGeometry() );
       v = exp2.evaluate( &contextWithGeometry );
       QCOMPARE( v.toString(), QStringLiteral( "Point (1 2)" ) );
       v = exp2.evaluate( &contextWithNoGeometry );
