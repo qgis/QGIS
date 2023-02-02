5988fc6ea2f31de474869a3f53d233a4a52a2855
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-02-02 14:48:09 +0100

    add licence

--- src/core/settings/qgssettingstree.cpp
+++ src/core/settings/qgssettingstree.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+  qgssettingstree.h
+  --------------------------------------
+  Date                 : February 2023
+  Copyright            : (C) 2023 by Denis Rouzaud
+  Email                : denis@opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "qgssettingstree.h"
 
 QgsSettingsTreeNode *QgsSettingsTree::treeRoot()
