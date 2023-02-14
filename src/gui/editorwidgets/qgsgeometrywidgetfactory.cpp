cfe6af3d04e89114b5fafdedb679a97f53d38971
Nyall Dawson <nyall.dawson@gmail.com>
2023-02-15 09:44:48 +1000

    Create an editor widget wrapper for geometry fields
    This will show a dedicated widget for geometry fields, allowing
    for proper lossless use of these fields in the attribute form
    
    Refs #49380

---
+++ src/gui/editorwidgets/qgsgeometrywidgetfactory.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+    qgsgeometrywidgetfactory.cpp
+     -----------------------
+    Date                 : February 2023
+    Copyright            : (C) 2023 Nyall Dawson
+    Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsgeometrywidgetfactory.h"
+
+#include "qgsgeometrywidgetwrapper.h"
+#include "qgsdummyconfigdlg.h"
+#include "qgsvectorlayer.h"
+
+QgsGeometryWidgetFactory::QgsGeometryWidgetFactory( const QString &name, QgsMessageBar *messageBar )
+  : QgsEditorWidgetFactory( name )
+  , mMessageBar( messageBar )
+{
+}
+
+QgsEditorWidgetWrapper *QgsGeometryWidgetFactory::create( QgsVectorLayer *vl, int fieldIdx, QWidget *editor, QWidget *parent ) const
+{
+  return new QgsGeometryWidgetWrapper( vl, fieldIdx, editor, parent, mMessageBar );
+}
+
+QgsEditorConfigWidget *QgsGeometryWidgetFactory::configWidget( QgsVectorLayer *vl, int fieldIdx, QWidget *parent ) const
+{
+  return new QgsDummyConfigDlg( vl, fieldIdx, parent, QObject::tr( "A widget for interacting with geometry fields." ) );
+}
+
+unsigned int QgsGeometryWidgetFactory::fieldScore( const QgsVectorLayer *vl, int fieldIdx ) const
+{
+  const QgsField field = vl->fields().field( fieldIdx );
+  const QVariant::Type type = field.type();
+  // Geometry fields only
+  return ( type == QVariant::UserType && field.typeName().compare( QLatin1String( "geometry" ), Qt::CaseInsensitive ) == 0 ) ? 20 : 0;
+}
---
+++ src/gui/editorwidgets/qgsgeometrywidgetwrapper.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+    qgsgeometrywidgetwrapper.cpp
+     -----------------------
+    Date                 : February 2023
+    Copyright            : (C) 2023 Nyall Dawson
+    Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsgeometrywidgetwrapper.h"
+#include "qgsvectorlayer.h"
+#include "qgsmessagebar.h"
+#include "qgsgeometrywidget.h"
+#include <QHBoxLayout>
+#include <QFileDialog>
+#include <QLabel>
+#include <QToolButton>
+#include <QAction>
+#include <QMenu>
+#include <QMessageBox>
+#include <QUrl>
+
+QgsGeometryWidgetWrapper::QgsGeometryWidgetWrapper( QgsVectorLayer *layer, int fieldIdx, QWidget *editor, QWidget *parent, QgsMessageBar *messageBar )
+  : QgsEditorWidgetWrapper( layer, fieldIdx, editor, parent )
+  , mMessageBar( messageBar )
+{
+}
+
+
+QVariant QgsGeometryWidgetWrapper::value() const
+{
+  if ( !mWidget )
+    return QVariant();
+
+  const QgsReferencedGeometry geomValue = mWidget->geometryValue();
+  if ( geomValue.isNull() )
+    return QVariant();
+
+  return QVariant::fromValue( geomValue );
+}
+
+void QgsGeometryWidgetWrapper::setEnabled( bool enabled )
+{
+  if ( mWidget )
+    mWidget->setReadOnly( !enabled );
+}
+
+QWidget *QgsGeometryWidgetWrapper::createWidget( QWidget *parent )
+{
+  QgsGeometryWidget *widget = new QgsGeometryWidget( parent );
+  widget->setSizePolicy( QSizePolicy::Preferred, QSizePolicy::Minimum );
+  return widget;
+}
+
+void QgsGeometryWidgetWrapper::initWidget( QWidget *editor )
+{
+  mWidget = qobject_cast<QgsGeometryWidget *>( editor );
+
+  if ( mWidget )
+  {
+    connect( mWidget, &QgsGeometryWidget::geometryValueChanged, this, &QgsEditorWidgetWrapper::emitValueChanged );
+  }
+}
+
+bool QgsGeometryWidgetWrapper::valid() const
+{
+  return mWidget;
+}
+
+void QgsGeometryWidgetWrapper::updateValues( const QVariant &value, const QVariantList & )
+{
+  QgsReferencedGeometry geom;
+  if ( !QgsVariantUtils::isNull( value ) )
+  {
+    if ( value.userType() == QMetaType::type( "QgsReferencedGeometry" ) )
+    {
+      geom = value.value< QgsReferencedGeometry >();
+    }
+    else if ( value.userType() == QMetaType::type( "QgsGeometry" ) )
+    {
+      geom = QgsReferencedGeometry( value.value< QgsGeometry >(), QgsCoordinateReferenceSystem() );
+    }
+  }
+
+  if ( mWidget )
+  {
+    mWidget->setGeometryValue( geom );
+  }
+}
+
