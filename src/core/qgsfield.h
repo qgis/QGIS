5d54484897102352cf4b4ce4ea15413e17d98af4
Nyall Dawson <nyall.dawson@gmail.com>
2023-02-16 10:01:25 +1000

    Add capacity for storing additional metadata/properties in QgsField
    This adds the API framework for storing additional, semi-structured
    properties inside QgsField objects. The intention is that strong
    field-type specific properties can be stored.
    
    Eg for a geometry field type coming from the postgres provider
    the metadata can be used to store the associated CRS and WKB types
    so that clients can be aware of the correct format required for
    geometries stored in that field. Instead of cluttering the QgsField
    API with specific getters/setters for properties like crs(), wkbType(),
    etc which only apply for a certain field type, the metadata map
    approach helps us keep the API nice and slim.
    
    The API has been designed to follow the approach used by various
    Qt objects (such as QTextFormat) where a preset set of keys
    are exposed as an enum, but additional ones can be used for
    custom property storage. This allows for a more structured
    use of properties with conventions which apply across different
    providers (as opposed to a free-form string key approach).
    
    Refs #49380

--- src/core/qgsfield.h
+++ src/core/qgsfield.h
@@ -22,6 +22,7 @@
 #include <QSharedDataPointer>
 #include "qgis_core.h"
 #include "qgis_sip.h"
+#include "qgis.h"
 
 typedef QList<int> QgsAttributeList SIP_SKIP;
 
@@ -215,6 +216,42 @@ class CORE_EXPORT QgsField
      * Returns the field comment
      */
     QString comment() const;
+
+    /**
+     * Returns the map of field metadata.
+     *
+     * Map keys should match values from the Qgis::FieldMetadataProperty enum.
+     *
+     * \see setMetadata()
+     * \since QGIS 3.30
+     */
+    QMap< int, QVariant > metadata() const;
+
+    /**
+     * Returns a specific metadata \a property.
+     *
+     * \see setMetadata()
+     * \since QGIS 3.30
+     */
+    QVariant metadata( Qgis::FieldMetadataProperty property ) const;
+
+    /**
+     * Sets the map of field \a metadata.
+     *
+     * Map keys should match values from the Qgis::FieldMetadataProperty enum.
+     *
+     * \see metadata()
+     * \since QGIS 3.30
+     */
+    void setMetadata( const QMap< int, QVariant > metadata );
+
+    /**
+     * Sets a metadata \a property to \a value.
+     *
+     * \see metadata()
+     * \since QGIS 3.30
+     */
+    void setMetadata( Qgis::FieldMetadataProperty property, const QVariant &value );
 
     /**
      * Returns if this field is numeric. Any integer or floating point type
