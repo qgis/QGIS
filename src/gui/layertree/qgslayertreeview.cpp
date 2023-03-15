2f48b1e73201e4e6adf17d4baf80bd51b435ca60
Nyall Dawson <nyall.dawson@gmail.com>
2023-03-15 10:01:12 +1000

    Add a "contextMenuAboutToShow" signal to QgsLayerTreeView
    Allows plugins to hook into this menu and add custom entries
    and customise the standard entries
    
    (cherry picked from commit a79adfc8dc677e087820ce0fb38f82b6f6d9c7ae)

--- src/gui/layertree/qgslayertreeview.cpp
+++ src/gui/layertree/qgslayertreeview.cpp
@@ -178,9 +178,14 @@ void QgsLayerTreeView::contextMenuEvent( QContextMenuEvent *event )
     setCurrentIndex( QModelIndex() );
 
   QMenu *menu = mMenuProvider->createContextMenu();
-  if ( menu && menu->actions().count() != 0 )
-    menu->exec( mapToGlobal( event->pos() ) );
-  delete menu;
+  if ( menu )
+  {
+    emit contextMenuAboutToShow( menu );
+
+    if ( menu->actions().count() != 0 )
+      menu->exec( mapToGlobal( event->pos() ) );
+    delete menu;
+  }
 }
 
 
