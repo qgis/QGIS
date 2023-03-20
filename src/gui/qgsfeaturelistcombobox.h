4eaeb3ec2b8d10773bbe1bde9fb8da45c37b98ea
signedav <david@opengis.ch>
2023-03-20 13:04:33 +0100

    add since note

--- src/gui/qgsfeaturelistcombobox.h
+++ src/gui/qgsfeaturelistcombobox.h
@@ -156,12 +156,14 @@ class GUI_EXPORT QgsFeatureListComboBox : public QComboBox
 
     /**
      * Returns the feature request fetch limit
+     * \since QGIS 3.32
      */
     int fetchLimit() const;
 
     /**
      * Defines the feature request fetch limit
      * If set to 0, no limit is applied when fetching
+     * \since QGIS 3.32
      */
     void setFetchLimit( int fetchLimit );
 
