a2f89a931853276c33e36804f0dd9611128c915c
Nyall Dawson <nyall.dawson@gmail.com>
2023-03-23 10:42:03 +1000

    Cleanup mbtiles uri handling

--- src/core/vectortile/qgsmbtilesvectortiledataprovider.cpp
+++ src/core/vectortile/qgsmbtilesvectortiledataprovider.cpp
@@ -236,94 +236,42 @@ QgsProviderMetadata::ProviderCapabilities QgsMbTilesVectorTileDataProviderMetada
 
 QVariantMap QgsMbTilesVectorTileDataProviderMetadata::decodeUri( const QString &uri ) const
 {
-  // TODO -- carefully thin out options which don't apply to mbtiles
-
   QgsDataSourceUri dsUri;
   dsUri.setEncodedUri( uri );
 
   QVariantMap uriComponents;
   uriComponents.insert( QStringLiteral( "type" ), dsUri.param( QStringLiteral( "type" ) ) );
-  if ( dsUri.hasParam( QStringLiteral( "serviceType" ) ) )
-    uriComponents.insert( QStringLiteral( "serviceType" ), dsUri.param( QStringLiteral( "serviceType" ) ) );
-
-  if ( uriComponents[ QStringLiteral( "type" ) ] == QLatin1String( "mbtiles" ) ||
-       ( uriComponents[ QStringLiteral( "type" ) ] == QLatin1String( "xyz" ) &&
-         !dsUri.param( QStringLiteral( "url" ) ).startsWith( QLatin1String( "http" ) ) ) )
-  {
-    uriComponents.insert( QStringLiteral( "path" ), dsUri.param( QStringLiteral( "url" ) ) );
-  }
-  else
-  {
-    uriComponents.insert( QStringLiteral( "url" ), dsUri.param( QStringLiteral( "url" ) ) );
-  }
-
-  if ( dsUri.hasParam( QStringLiteral( "zmin" ) ) )
-    uriComponents.insert( QStringLiteral( "zmin" ), dsUri.param( QStringLiteral( "zmin" ) ) );
-  if ( dsUri.hasParam( QStringLiteral( "zmax" ) ) )
-    uriComponents.insert( QStringLiteral( "zmax" ), dsUri.param( QStringLiteral( "zmax" ) ) );
-
-  dsUri.httpHeaders().updateMap( uriComponents );
-
-  if ( dsUri.hasParam( QStringLiteral( "styleUrl" ) ) )
-    uriComponents.insert( QStringLiteral( "styleUrl" ), dsUri.param( QStringLiteral( "styleUrl" ) ) );
-
-  const QString authcfg = dsUri.authConfigId();
-  if ( !authcfg.isEmpty() )
-    uriComponents.insert( QStringLiteral( "authcfg" ), authcfg );
+  uriComponents.insert( QStringLiteral( "path" ), dsUri.param( QStringLiteral( "url" ) ) );
 
   return uriComponents;
 }
 
 QString QgsMbTilesVectorTileDataProviderMetadata::encodeUri( const QVariantMap &parts ) const
 {
-  // TODO -- carefully thin out options which don't apply to mbtiles
-
   QgsDataSourceUri dsUri;
   dsUri.setParam( QStringLiteral( "type" ), parts.value( QStringLiteral( "type" ) ).toString() );
-  if ( parts.contains( QStringLiteral( "serviceType" ) ) )
-    dsUri.setParam( QStringLiteral( "serviceType" ), parts[ QStringLiteral( "serviceType" ) ].toString() );
   dsUri.setParam( QStringLiteral( "url" ), parts.value( parts.contains( QStringLiteral( "path" ) ) ? QStringLiteral( "path" ) : QStringLiteral( "url" ) ).toString() );
-
-  if ( parts.contains( QStringLiteral( "zmin" ) ) )
-    dsUri.setParam( QStringLiteral( "zmin" ), parts[ QStringLiteral( "zmin" ) ].toString() );
-  if ( parts.contains( QStringLiteral( "zmax" ) ) )
-    dsUri.setParam( QStringLiteral( "zmax" ), parts[ QStringLiteral( "zmax" ) ].toString() );
-
-  dsUri.httpHeaders().setFromMap( parts );
-
-  if ( parts.contains( QStringLiteral( "styleUrl" ) ) )
-    dsUri.setParam( QStringLiteral( "styleUrl" ), parts[ QStringLiteral( "styleUrl" ) ].toString() );
-
-  if ( parts.contains( QStringLiteral( "authcfg" ) ) )
-    dsUri.setAuthConfigId( parts[ QStringLiteral( "authcfg" ) ].toString() );
-
   return dsUri.encodedUri();
 }
 
 QString QgsMbTilesVectorTileDataProviderMetadata::absoluteToRelativeUri( const QString &uri, const QgsReadWriteContext &context ) const
 {
-  QgsDataSourceUri dsUri;
-  dsUri.setEncodedUri( uri );
+  QVariantMap parts = decodeUri( uri );
 
-  QString sourcePath = dsUri.param( QStringLiteral( "url" ) );
+  const QString originalPath = parts.value( QStringLiteral( "path" ) ).toString();
+  parts.insert( QStringLiteral( "path" ), context.pathResolver().writePath( originalPath ) );
 
-  sourcePath = context.pathResolver().writePath( sourcePath );
-  dsUri.removeParam( QStringLiteral( "url" ) );  // needed because setParam() would insert second "url" key
-  dsUri.setParam( QStringLiteral( "url" ), sourcePath );
-  return dsUri.encodedUri();
+  return encodeUri( parts );
 }
 
 QString QgsMbTilesVectorTileDataProviderMetadata::relativeToAbsoluteUri( const QString &uri, const QgsReadWriteContext &context ) const
 {
-  QgsDataSourceUri dsUri;
-  dsUri.setEncodedUri( uri );
+  QVariantMap parts = decodeUri( uri );
 
-  QString sourcePath = dsUri.param( QStringLiteral( "url" ) );
+  const QString originalPath = parts.value( QStringLiteral( "path" ) ).toString();
+  parts.insert( QStringLiteral( "path" ), context.pathResolver().readPath( originalPath ) );
 
-  sourcePath = context.pathResolver().readPath( sourcePath );
-  dsUri.removeParam( QStringLiteral( "url" ) );  // needed because setParam() would insert second "url" key
-  dsUri.setParam( QStringLiteral( "url" ), sourcePath );
-  return dsUri.encodedUri();
+  return encodeUri( parts );
 }
 
 QList<Qgis::LayerType> QgsMbTilesVectorTileDataProviderMetadata::supportedLayerTypes() const
