1917362dffe6227e674e7ecc406660706df45997
Alexander Bruy <alexander.bruy@gmail.com>
2023-03-29 11:07:33 +0300

    enable build virtual point cloud algorithm
    Virtual point cloud (VPC) is similar to GDAL's VRT - it is a single file
    referring to other files that contain actual data.

--- tests/src/analysis/testqgsprocessingpdalalgs.cpp
+++ tests/src/analysis/testqgsprocessingpdalalgs.cpp
@@ -35,7 +35,7 @@ class TestQgsProcessingPdalAlgs: public QObject
     void cleanup() {} // will be called after every testfunction.
 
     void boundary();
-    //void buildVpc();
+    void buildVpc();
     void clip();
     void convertFormat();
     void density();
@@ -716,7 +716,6 @@ void TestQgsProcessingPdalAlgs::merge()
           );
 }
 
-/*
 void TestQgsProcessingPdalAlgs::buildVpc()
 {
   QgsPdalAlgorithmBase *alg = const_cast<QgsPdalAlgorithmBase *>( static_cast< const QgsPdalAlgorithmBase * >( QgsApplication::processingRegistry()->algorithmById( QStringLiteral( "pdal:virtualpointcloud" ) ) ) );
@@ -760,7 +759,6 @@ void TestQgsProcessingPdalAlgs::buildVpc()
             << pointCloud2
           );
 }
-*/
 
 void TestQgsProcessingPdalAlgs::clip()
 {
