b9c0fc46b5fa0ed0f8d99ec2c488f468789ed3fc
Nyall Dawson <nyall.dawson@gmail.com>
2023-03-31 10:46:52 +1000

    Checkin missed file

---
+++ python/core/auto_additions/qgssensormodel.py
@@ -0,0 +1,6 @@
+# The following has been generated automatically from src/core/sensor/qgssensormodel.h
+# monkey patching scoped based enum
+QgsSensorModel.Column.Name.__doc__ = "Name"
+QgsSensorModel.Column.LastValue.__doc__ = "Last value"
+QgsSensorModel.Column.__doc__ = 'Model columns\n\n' + '* ``Name``: ' + QgsSensorModel.Column.Name.__doc__ + '\n' + '* ``LastValue``: ' + QgsSensorModel.Column.LastValue.__doc__
+# --
