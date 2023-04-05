996a73ae0f4bffc981652a8c3b040b0ee07fb5b8
Alexander Bruy <alexander.bruy@gmail.com>
2023-04-05 19:31:30 +0300

    [processing] add filter by rectangle (extent) option to the point cloud filter algorithm

--- src/analysis/processing/pdal/qgsalgorithmpdalfilter.cpp
+++ src/analysis/processing/pdal/qgsalgorithmpdalfilter.cpp
@@ -44,12 +44,12 @@ QString QgsPdalFilterAlgorithm::groupId() const
 
 QStringList QgsPdalFilterAlgorithm::tags() const
 {
-  return QObject::tr( "filter,subset,extract,dimension,attribute" ).split( ',' );
+  return QObject::tr( "filter,subset,extract,dimension,attribute,extent,bounds,rectangle" ).split( ',' );
 }
 
 QString QgsPdalFilterAlgorithm::shortHelpString() const
 {
-  return QObject::tr( "This algorithm extracts point from the input point cloud which match PDAL expression." );
+  return QObject::tr( "This algorithm extracts point from the input point cloud which match PDAL expression and/or are inside of a cropping rectangle." );
 }
 
 QgsPdalFilterAlgorithm *QgsPdalFilterAlgorithm::createInstance() const
@@ -60,7 +60,8 @@ QgsPdalFilterAlgorithm *QgsPdalFilterAlgorithm::createInstance() const
 void QgsPdalFilterAlgorithm::initAlgorithm( const QVariantMap & )
 {
   addParameter( new QgsProcessingParameterPointCloudLayer( QStringLiteral( "INPUT" ), QObject::tr( "Input layer" ) ) );
-  addParameter( new QgsProcessingParameterString( QStringLiteral( "FILTER" ), QObject::tr( "Filter expression" ) ) );
+  addParameter( new QgsProcessingParameterString( QStringLiteral( "FILTER_EXPRESSION" ), QObject::tr( "Filter expression" ), QVariant(), false, true ) );
+  addParameter( new QgsProcessingParameterExtent( QStringLiteral( "FILTER_EXTENT" ), QObject::tr( "Cropping extent" ), QVariant(), true ) );
   addParameter( new QgsProcessingParameterPointCloudDestination( QStringLiteral( "OUTPUT" ), QObject::tr( "Output" ) ) );
 }
 
@@ -77,9 +78,38 @@ QStringList QgsPdalFilterAlgorithm::createArgumentLists( const QVariantMap &para
 
   QStringList args = { QStringLiteral( "translate" ),
                        QStringLiteral( "--input=%1" ).arg( layer->source() ),
-                       QStringLiteral( "--filter=%1" ).arg( parameterAsString( parameters, QStringLiteral( "FILTER" ), context ) ),
                        QStringLiteral( "--output=%1" ).arg( outputFile )
                      };
+
+
+  const QString filterExpression = parameterAsString( parameters, QStringLiteral( "FILTER_EXPRESSION" ), context ).trimmed();
+  if ( !filterExpression.isEmpty() )
+  {
+    args << QStringLiteral( "--filter=%1" ).arg( filterExpression );
+  }
+
+  if ( parameters.value( QStringLiteral( "FILTER_EXTENT" ) ).isValid() )
+  {
+    if ( layer->crs().isValid() )
+    {
+      const QgsRectangle extent = parameterAsExtent( parameters, QStringLiteral( "FILTER_EXTENT" ), context, layer->crs() );
+      args << QStringLiteral( "--bounds=([%1, %2], [%3, %4])" )
+           .arg( extent.xMinimum() )
+           .arg( extent.xMaximum() )
+           .arg( extent.yMinimum() )
+           .arg( extent.yMaximum() );
+
+    }
+    else
+    {
+      const QgsRectangle extent = parameterAsExtent( parameters, QStringLiteral( "FILTER_EXTENT" ), context );
+      args << QStringLiteral( "--bounds=([%1, %2], [%3, %4])" )
+           .arg( extent.xMinimum() )
+           .arg( extent.xMaximum() )
+           .arg( extent.yMinimum() )
+           .arg( extent.yMaximum() );
+    }
+  }
 
   applyThreadsParameter( args );
   return args;
