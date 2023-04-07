e348da206f22b4d79d74d6671f29f32efe9f9ecf
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2023-04-07 18:38:41 +0200

    Move loading MVT sprites to utils class and use it from QgsVectorTileLayerProperties and QgsVectorTileLayer

--- src/gui/vectortile/qgsvectortilelayerproperties.cpp
+++ src/gui/vectortile/qgsvectortilelayerproperties.cpp
@@ -23,9 +23,11 @@
 #include "qgsvectortilebasicrendererwidget.h"
 #include "qgsvectortilebasiclabelingwidget.h"
 #include "qgsvectortilelayer.h"
+#include "qgsvectortileutils.h"
 #include "qgsgui.h"
 #include "qgsnative.h"
 #include "qgsapplication.h"
+#include "qgsjsonutils.h"
 #include "qgsmetadatawidget.h"
 #include "qgsmaplayerloadstyledialog.h"
 #include "qgsmapboxglstyleconverter.h"
@@ -323,6 +325,10 @@ void QgsVectorTileLayerProperties::loadStyle()
         context.setTargetUnit( Qgis::RenderUnit::Millimeters );
         //assume source uses 96 dpi
         context.setPixelSizeConversionFactor( 25.4 / 96.0 );
+
+        //load sprites
+        QVariantMap styleDefinition = QgsJsonUtils::parseJson( content ).toMap();
+        QgsVectorTileUtils::loadSprites( styleDefinition, context );
 
         QgsMapBoxGlStyleConverter converter;
 
