e3af648c6212d18515e6e17c6e78f848359ed49b
Nyall Dawson <nyall.dawson@gmail.com>
2023-04-14 11:25:34 +1000

    Cleanup another api to avoid dangling pointers

--- src/app/devtools/qgsdevtoolsmodelnode.cpp
+++ src/app/devtools/qgsdevtoolsmodelnode.cpp
@@ -52,15 +52,15 @@ QgsDevToolsModelGroup::QgsDevToolsModelGroup( const QString &title )
 {
 }
 
-void QgsDevToolsModelGroup::addChild( std::unique_ptr<QgsDevToolsModelNode> child )
+QgsDevToolsModelNode *QgsDevToolsModelGroup::addChild( std::unique_ptr<QgsDevToolsModelNode> child )
 {
   if ( !child )
-    return;
+    return nullptr;
 
   Q_ASSERT( !child->mParent );
   child->mParent = this;
 
-  mChildren.emplace_back( std::move( child ) );
+  return mChildren.emplace_back( std::move( child ) ).get();
 }
 
 int QgsDevToolsModelGroup::indexOf( QgsDevToolsModelNode *child ) const
