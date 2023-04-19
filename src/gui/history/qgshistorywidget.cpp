34ce4d76f5103b2461fd21f2c1e6a0b278f7f7f8
Nyall Dawson <nyall.dawson@gmail.com>
2023-04-19 13:42:08 +1000

    Create QgsHistoryWidget resuable widget for showing history entries

---
+++ src/gui/history/qgshistorywidget.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+                             qgshistorywidget.cpp
+                             ------------------
+    Date                 : April 2023
+    Copyright            : (C) 2023 Nyall Dawson
+    Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgshistorywidget.h"
+#include "qgsgui.h"
+#include "qgshistoryentrymodel.h"
+
+QgsHistoryWidget::QgsHistoryWidget( const QString &providerId, Qgis::HistoryProviderBackends backends, QgsHistoryProviderRegistry *registry, QWidget *parent )
+  : QgsPanelWidget( parent )
+{
+  setupUi( this );
+
+  mModel = new QgsHistoryEntryModel( providerId, backends, registry, this );
+  mTreeView->setModel( mModel );
+
+}
---
+++ src/gui/history/qgshistorywidget.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+                             qgshistorywidget.h
+                             ------------------
+    Date                 : April 2023
+    Copyright            : (C) 2023 Nyall Dawson
+    Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSHISTORYWIDGET_H
+#define QGSHISTORYWIDGET_H
+
+#include "qgis.h"
+#include "qgis_gui.h"
+#include "ui_qgshistorywidgetbase.h"
+#include "qgspanelwidget.h"
+
+class QgsHistoryProviderRegistry;
+class QgsHistoryEntryModel;
+
+/**
+ * A widget showing entries from a QgsHistoryProviderRegistry.
+ * \ingroup gui
+ * \since QGIS 3.32
+ */
+class GUI_EXPORT QgsHistoryWidget : public QgsPanelWidget, private Ui::QgsHistoryWidgetBase
+{
+    Q_OBJECT
+
+  public:
+
+    /**
+     * Constructor for QgsHistoryWidget, with the specified \a parent widget.
+     *
+     * If \a providerId is specified then the widget will contain only items from the matching
+     * history provider.
+     * If \a backends is specified then the widget will be filtered to only matching backends.
+     *
+     * If no \a registry is specified then the singleton QgsHistoryProviderRegistry from QgsGui::historyProviderRegistry()
+     * will be used.
+     */
+    QgsHistoryWidget( const QString &providerId = QString(),
+                      Qgis::HistoryProviderBackends backends = Qgis::HistoryProviderBackend::LocalProfile,
+                      QgsHistoryProviderRegistry *registry = nullptr,
+                      QWidget *parent = nullptr );
+
+
+  private:
+
+    QgsHistoryEntryModel *mModel = nullptr;
+
+};
+
+#endif // QGSHISTORYWIDGET_H
