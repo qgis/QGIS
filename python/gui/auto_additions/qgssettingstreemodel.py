9f3ac79b8ba165fc462497f8b792fe8c9e8a3989
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-04-23 07:19:47 +0200

    sipify + CMakeLists

---
+++ python/gui/auto_additions/qgssettingstreemodel.py
@@ -0,0 +1,7 @@
+# The following has been generated automatically from src/gui/settings/qgssettingstreemodel.h
+# monkey patching scoped based enum
+QgsSettingsTreeModel.Column.Name.__doc__ = "Name"
+QgsSettingsTreeModel.Column.Value.__doc__ = "Value"
+QgsSettingsTreeModel.Column.Description.__doc__ = "Description"
+QgsSettingsTreeModel.Column.__doc__ = 'Columns\n\n' + '* ``Name``: ' + QgsSettingsTreeModel.Column.Name.__doc__ + '\n' + '* ``Value``: ' + QgsSettingsTreeModel.Column.Value.__doc__ + '\n' + '* ``Description``: ' + QgsSettingsTreeModel.Column.Description.__doc__
+# --
