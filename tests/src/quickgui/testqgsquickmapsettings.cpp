14c66562093b67af6dd834a87dc80c37af22afa5
Mathieu Pellerin <nirvn.asia@gmail.com>
2023-04-23 12:45:49 +0700

    [qgsquick] New map to screen class to tie QML items to the map canvas

--- tests/src/quickgui/testqgsquickmapsettings.cpp
+++ tests/src/quickgui/testqgsquickmapsettings.cpp
@@ -15,12 +15,14 @@
 #include <QObject>
 #include <QApplication>
 
+#include "qgis.h"
 #include "qgsapplication.h"
 #include "qgstest.h"
-#include "qgis.h"
+#include "qgspoint.h"
 #include "qgsunittypes.h"
 
 #include "qgsquickmapsettings.h"
+#include "qgsquickmaptoscreen.h"
 
 class TestQgsQuickMapSettings: public QObject
 {
@@ -30,6 +32,7 @@ class TestQgsQuickMapSettings: public QObject
     void cleanup() {} // will be called after every testfunction.
 
     void test_project_existency();
+    void test_map_to_screen();
 };
 
 void TestQgsQuickMapSettings::test_project_existency()
@@ -37,6 +40,21 @@ void TestQgsQuickMapSettings::test_project_existency()
   QgsQuickMapSettings *settings = new QgsQuickMapSettings();
   QVERIFY( !settings->project() );
   delete settings;
+}
+
+void TestQgsQuickMapSettings::test_map_to_screen()
+{
+  QgsQuickMapSettings *settings = new QgsQuickMapSettings();
+  settings->setOutputSize( QSize( 10, 10 ) );
+  settings->setExtent( QgsRectangle( 0, 0, 10, 10 ) );
+
+  QgsPoint point( 5, 5 );
+
+  QgsQuickMapToScreen *mapToScreen = new QgsQuickMapToScreen();
+  mapToScreen->setMapSettings( settings );
+  mapToScreen->setMapPoint( point );
+
+  QCOMPARE( mapToScreen->screenPoint(), QPointF( 5, 5 ) );
 }
 
 QGSTEST_MAIN( TestQgsQuickMapSettings )
