21f3e587ba9653e3cac8bc4923795abe4051bd48
Yoann Quenach de Quivillic <yoann.quenach@gmail.com>
2023-04-24 09:22:53 +0200

    Allow setting the profile icon

--- src/core/qgsuserprofile.cpp
+++ src/core/qgsuserprofile.cpp
@@ -127,12 +127,16 @@ QgsError QgsUserProfile::setAlias( const QString &alias ) const
 
 const QIcon QgsUserProfile::icon() const
 {
-  const QString path = mProfileFolder + QDir::separator() + "icon.svg";
-  if ( !QFileInfo::exists( path ) )
+  const QStringList extensions = {".svg", ".png", ".jpg", ".jpeg", ".gif", ".bmp"};
+  const QString basename = mProfileFolder + QDir::separator() + "icon";
+
+  for ( const QString &extension : extensions )
   {
-    return QgsApplication::getThemeIcon( "user.svg" );
+    const QString path = basename + extension;
+    if ( QFileInfo::exists( path ) )
+      return QIcon( path );
   }
-  return QIcon( path );
+  return QgsApplication::getThemeIcon( "user.svg" );
 }
 
 QString QgsUserProfile::qgisDB() const
