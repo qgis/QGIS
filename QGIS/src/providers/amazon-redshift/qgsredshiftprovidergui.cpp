da597ef716c07f35be69f8128d4d36515977cd76
Alexey Karandashev <reflectored@pm.me>
2023-04-25 11:58:24 +0200

    Add Redshift Provider factory classes which are used by QGIS core to access GUI classes.

---
+++ QGIS/src/providers/amazon-redshift/qgsredshiftprovidergui.cpp
@@ -0,0 +1,121 @@
+/***************************************************************************
+  qgspostgresprovidergui.cpp
+  --------------------------------------
+  Date                 : June 2019
+  Copyright            : (C) 2019 by Martin Dobias
+  Email                : wonder dot sk at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsredshiftprovidergui.h"
+
+#include "qgsapplication.h"
+#include "qgsprojectstorageguiprovider.h"
+#include "qgsproviderguimetadata.h"
+#include "qgsredshiftdataitemguiprovider.h"
+#include "qgsredshiftdataitems.h"
+#include "qgsredshiftprojectstoragedialog.h"
+#include "qgsredshiftprovider.h"
+#include "qgsredshiftsourceselect.h"
+#include "qgssourceselectprovider.h"
+
+//! Provider for redshift source select
+class QgsRedshiftSourceSelectProvider : public QgsSourceSelectProvider //#spellok
+{
+  public:
+    QString providerKey() const override
+    {
+      return QStringLiteral( "redshift" );
+    }
+    QString text() const override
+    {
+      return QObject::tr( "Amazon Redshift" );
+    }
+    int ordering() const override
+    {
+      return QgsSourceSelectProvider::OrderDatabaseProvider + 20;
+    }
+    QIcon icon() const override
+    {
+      return QgsApplication::getThemeIcon( QStringLiteral( "/mActionAddRedshiftLayer.svg" ) );
+    }
+    QgsAbstractDataSourceWidget *createDataSourceWidget(
+      QWidget *parent = nullptr, Qt::WindowFlags fl = Qt::Widget,
+      QgsProviderRegistry::WidgetMode widgetMode = QgsProviderRegistry::WidgetMode::Embedded ) const override
+    {
+      return new QgsRedshiftSourceSelect( parent, fl, widgetMode );
+    }
+};
+
+class QgsRedshiftProjectStorageGuiProvider : public QgsProjectStorageGuiProvider
+{
+  public:
+    QString type() override
+    {
+      return QStringLiteral( "redshift" );
+    }
+    QString visibleName() override
+    {
+      return QObject::tr( "Amazon Redshift" );
+    }
+
+    QString showLoadGui() override
+    {
+      QgsRedshiftProjectStorageDialog dlg( false );
+      if ( !dlg.exec() )
+        return QString();
+
+      return dlg.currentProjectUri();
+    }
+
+    QString showSaveGui() override
+    {
+      QgsRedshiftProjectStorageDialog dlg( true );
+      if ( !dlg.exec() )
+        return QString();
+
+      return dlg.currentProjectUri();
+    }
+};
+
+QgsRedshiftProviderGuiMetadata::QgsRedshiftProviderGuiMetadata()
+  : QgsProviderGuiMetadata( QgsRedshiftProvider::REDSHIFT_KEY )
+{
+}
+
+QList<QgsSourceSelectProvider *> QgsRedshiftProviderGuiMetadata::sourceSelectProviders()
+{
+  QList<QgsSourceSelectProvider *> providers;
+  providers << new QgsRedshiftSourceSelectProvider; //#spellok
+  return providers;
+}
+
+QList<QgsDataItemGuiProvider *> QgsRedshiftProviderGuiMetadata::dataItemGuiProviders()
+{
+  return QList<QgsDataItemGuiProvider *>() << new QgsRedshiftDataItemGuiProvider;
+}
+
+QList<QgsProjectStorageGuiProvider *> QgsRedshiftProviderGuiMetadata::projectStorageGuiProviders()
+{
+  QList<QgsProjectStorageGuiProvider *> providers;
+  providers << new QgsRedshiftProjectStorageGuiProvider;
+  return providers;
+}
+
+void QgsRedshiftProviderGuiMetadata::registerGui( QMainWindow *mainWindow )
+{
+  QgsRSRootItem::sMainWindow = mainWindow;
+}
+
+#ifndef HAVE_STATIC_PROVIDERS
+QGISEXTERN QgsProviderGuiMetadata *providerGuiMetadataFactory()
+{
+  return new QgsRedshiftProviderGuiMetadata();
+}
+#endif
