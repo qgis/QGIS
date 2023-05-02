0ec883941e755bd72a7f4038bbed4400c3094c42
jbp35 <19955142+jbp35@users.noreply.github.com>
2023-05-02 11:47:11 +0200

    Enable automatic scaling for high DPI displays

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -917,6 +917,14 @@ int main( int argc, char *argv[] )
               ).toUtf8().constData();
     exit( 1 ); //exit for now until a version of qgis is capable of running non interactive
   }
+  
+  // Set up for high DPI displays
+  // The following values are set by default in Qt 6
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+  QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
 
   // GUI customization is enabled according to settings (loaded when instance is created)
   // we force disabled here if --nocustomization argument is used
