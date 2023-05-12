c8dd71e4d31bf9e35123a4e7781f90d18f805b68
Matthias Kuhn <matthias@opengis.ch>
2023-05-12 06:53:15 +0200

    Set size for layout check

--- python/testing/__init__.py
+++ python/testing/__init__.py
@@ -52,7 +52,7 @@ from qgis.core import (
     QgsMultiRenderChecker,
     QgsMapSettings,
     QgsLayout,
-    QgsLayoutChecker
+    QgsLayoutChecker,
 )
 
 import unittest
@@ -156,14 +156,9 @@ class TestCase(_TestCase):
         return result
 
     @classmethod
-    def render_layout_check(cls,
-                            name: str,
-                            layout: QgsLayout,
-                            size: QSize,
-                            dpi: Optional[float] = 192) -> bool:
+    def render_layout_check(cls, name: str, layout: QgsLayout, size: QSize):
         checker = QgsLayoutChecker(name, layout)
-        checker.dots_per_meter = dpi / 25.4 * 1000
-        checker.size = size
+        checker.setSize(size)
         if cls.control_path_prefix():
             checker.setControlPathPrefix(cls.control_path_prefix())
         result, message = checker.testLayout()
