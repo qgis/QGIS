c15dbedc7a6cc51c0b8e71996eadc644664e29e0
jbp35 <19955142+jbp35@users.noreply.github.com>
2023-05-28 11:30:15 +0200

    created new qgs3DWindow class

---
+++ src/3d/qgs3dwindow.cpp
@@ -0,0 +1,122 @@
+/***************************************************************************
+  qgs3dwindow.cpp
+  --------------------------------------
+  Date                 : May 2023
+  Copyright            : (C) 2023 by Jean-Baptiste Peter
+  Email                : jbpeter at outlook dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <Qt3DCore/qaspectengine>
+#include <Qt3DCore/qentity>
+#include <Qt3DExtras/qforwardrenderer>
+#include <Qt3DRender/qrendersettings>
+#include <Qt3DRender/qrenderaspect>
+#include <Qt3DInput/qinputaspect>
+#include <Qt3DInput/qinputsettings>
+#include <Qt3DLogic/qlogicaspect>
+#include <Qt3DRender/qcamera>
+
+#include "qgs3dwindow.h"
+
+Qgs3DWindow::Qgs3DWindow(QScreen* screen, Qt3DRender::API api)
+  : m_aspectEngine(new Qt3DCore::QAspectEngine)
+  , m_renderAspect(new Qt3DRender::QRenderAspect)
+  , m_inputAspect(new Qt3DInput::QInputAspect)
+  , m_logicAspect(new Qt3DLogic::QLogicAspect)
+  , m_renderSettings(new Qt3DRender::QRenderSettings)
+  , m_forwardRenderer(new Qt3DExtras::QForwardRenderer)
+  , m_defaultCamera(new Qt3DRender::QCamera)
+  , m_inputSettings(new Qt3DInput::QInputSettings)
+  , m_root(new Qt3DCore::QEntity)
+  , m_userRoot(nullptr)
+  , m_initialized(false)
+{
+
+  // register aspects
+  m_aspectEngine->registerAspect(m_renderAspect);
+  m_aspectEngine->registerAspect(m_inputAspect);
+  m_aspectEngine->registerAspect(m_logicAspect);
+
+  m_defaultCamera->setParent(m_root);
+  m_forwardRenderer->setCamera(m_defaultCamera);
+  m_forwardRenderer->setSurface(this);
+  m_renderSettings->setActiveFrameGraph(m_forwardRenderer);
+  m_inputSettings->setEventSource(this);
+}
+
+Qgs3DWindow::~Qgs3DWindow()
+{
+  delete m_aspectEngine;
+}
+
+void Qgs3DWindow::registerAspect(Qt3DCore::QAbstractAspect* aspect)
+{
+  Q_ASSERT(!isVisible());
+  m_aspectEngine->registerAspect(aspect);
+}
+
+void Qgs3DWindow::registerAspect(const QString& name)
+{
+  Q_ASSERT(!isVisible());
+  m_aspectEngine->registerAspect(name);
+}
+
+void Qgs3DWindow::setRootEntity(Qt3DCore::QEntity* root)
+{
+  if (m_userRoot != root) {
+    if (m_userRoot != nullptr)
+      m_userRoot->setParent(static_cast<Qt3DCore::QNode*>(nullptr));
+    if (root != nullptr)
+      root->setParent(m_root);
+    m_userRoot = root;
+  }
+}
+
+void Qgs3DWindow::setActiveFrameGraph(Qt3DRender::QFrameGraphNode* activeFrameGraph)
+{
+  m_renderSettings->setActiveFrameGraph(activeFrameGraph);
+}
+
+Qt3DRender::QFrameGraphNode* Qgs3DWindow::activeFrameGraph() const
+{
+  return m_renderSettings->activeFrameGraph();
+}
+
+Qt3DExtras::QForwardRenderer* Qgs3DWindow::defaultFrameGraph() const
+{
+  return m_forwardRenderer;
+}
+
+Qt3DRender::QCamera* Qgs3DWindow::camera() const
+{
+  return m_defaultCamera;
+}
+
+Qt3DRender::QRenderSettings* Qgs3DWindow::renderSettings() const
+{
+  return m_renderSettings;
+}
+
+void Qgs3DWindow::showEvent(QShowEvent* e)
+{
+  if (!m_initialized) {
+    m_root->addComponent(m_renderSettings);
+    m_root->addComponent(m_inputSettings);
+    m_aspectEngine->setRootEntity(Qt3DCore::QEntityPtr(m_root));
+
+    m_initialized = true;
+  }
+  QWindow::showEvent(e);
+}
+
+void Qgs3DWindow::resizeEvent(QResizeEvent*)
+{
+  m_defaultCamera->setAspectRatio(float(width()) / std::max(1.f, static_cast<float>(height())));
+}
---
+++ src/3d/qgs3dwindow.h
@@ -0,0 +1,138 @@
+/***************************************************************************
+  qgs3dwindow.h
+  --------------------------------------
+  Date                 : May 2023
+  Copyright            : (C) 2023 by Jean-Baptiste Peter
+  Email                : jbpeter at outlook dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGS3DWINDOW_H
+#define QGS3DWINDOW_H
+
+#include <QtGui/QWindow>
+#include <Qt3DRender/qrenderapi.h>
+
+
+namespace Qt3DCore {
+  class QAspectEngine;
+  class QAbstractAspect;
+  class QEntity;
+}
+
+namespace Qt3DRender {
+  class QCamera;
+  class QFrameGraphNode;
+  class QRenderAspect;
+  class QRenderSettings;
+}
+
+namespace Qt3DExtras {
+  class QForwardRenderer;
+}
+
+namespace Qt3DInput {
+  class QInputAspect;
+  class QInputSettings;
+}
+
+namespace Qt3DLogic {
+  class QLogicAspect;
+}
+
+class Qgs3DWindow : public QWindow
+{
+  Q_OBJECT
+public:
+  /**
+   * Constructor for Qgs3DWindow.
+   */
+  Qgs3DWindow(QScreen* screen = nullptr, Qt3DRender::API = Qt3DRender::API::OpenGL);
+
+  /**
+   * Destructor for Qgs3DWindow.
+   */
+  ~Qgs3DWindow();
+
+  /**
+   * Registers the specified aspect.
+   */
+  void registerAspect(Qt3DCore::QAbstractAspect* aspect);
+
+  /**
+   * Registers the specified aspect name.
+   */
+  void registerAspect(const QString& name);
+
+  /**
+   * Sets the specified root entity of the scene.
+   */
+  void setRootEntity(Qt3DCore::QEntity* root);
+
+  /**
+   * Activates the specified activeFrameGraph.
+   */
+  void setActiveFrameGraph(Qt3DRender::QFrameGraphNode* activeFrameGraph);
+
+  /**
+   * Returns the node of the active frame graph.
+   */
+  Qt3DRender::QFrameGraphNode* activeFrameGraph() const;
+
+  /**
+   * Returns the node of the default framegraph
+   */
+  Qt3DExtras::QForwardRenderer* defaultFrameGraph() const;
+
+  /**
+   * Returns the default camera of the 3D Window.
+   */
+  Qt3DRender::QCamera* camera() const;
+
+  /**
+   * Returns the render settings of the 3D Window.
+   */
+  Qt3DRender::QRenderSettings* renderSettings() const;
+
+
+protected:
+  /**
+   * Manages the display events specified in e.
+   */
+  void showEvent(QShowEvent* e) override;
+
+  /**
+   * Resets the aspect ratio of the 3D window.
+   */
+  void resizeEvent(QResizeEvent*) override;
+
+private:
+  Qt3DCore::QAspectEngine* m_aspectEngine;
+
+  // Aspects
+  Qt3DRender::QRenderAspect* m_renderAspect;
+  Qt3DInput::QInputAspect* m_inputAspect;
+  Qt3DLogic::QLogicAspect* m_logicAspect;
+
+  // Renderer configuration
+  Qt3DRender::QRenderSettings* m_renderSettings;
+  Qt3DExtras::QForwardRenderer* m_forwardRenderer;
+  Qt3DRender::QCamera* m_defaultCamera;
+
+  // Input configuration
+  Qt3DInput::QInputSettings* m_inputSettings;
+
+  // Scene
+  Qt3DCore::QEntity* m_root;
+  Qt3DCore::QEntity* m_userRoot;
+
+  bool m_initialized;
+};
+
+#endif //QGS3DWINDOW_H
