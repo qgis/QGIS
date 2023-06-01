09b54827ea25ab852c887664e66c8d924176a4df
Nyall Dawson <nyall.dawson@gmail.com>
2023-06-02 09:34:29 +1000

    Use approach from https://github.com/OSGeo/gdal/pull/7887 to temporarily workaround broken fedora rawhide containers

--- ms-windows/mingw/mingwdeps.sh
+++ ms-windows/mingw/mingwdeps.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# To be removed
+# Workaround a bug where the initial /etc/dnf/dnf.conf file contains
+# just the "tsflags=nodocs" line
+printf '[main]\ngpgcheck=True\ninstallonly_limit=3\nclean_requirements_on_remove=True\nbest=False\nskip_if_unavailable=True\ntsflags=nodocs' > /etc/dnf/dnf.conf
+
 dnf install -y 'dnf-command(config-manager)' && \
 dnf config-manager --add-repo https://copr.fedorainfracloud.org/coprs/smani/mingw-extras/repo/fedora-rawhide/smani-mingw-extras-fedora-rawhide.repo && \
 dnf install -y --nogpgcheck \
