2f46999125fa3f3127c984b827e5f62cafc1bdc8
Juergen E. Fischer <jef@norbit.de>
2023-06-01 14:39:41 +0200

    spelling fix

--- src/gui/raster/qgsrasterrendererwidget.h
+++ src/gui/raster/qgsrasterrendererwidget.h
@@ -100,13 +100,13 @@ class GUI_EXPORT QgsRasterRendererWidget: public QWidget
     virtual QgsRasterMinMaxWidget *minMaxWidget() { return nullptr; }
 
     /**
-     * Returns the constrast enhancement \a algorithm to be used by the raster renderer.
+     * Returns the contrast enhancement \a algorithm to be used by the raster renderer.
      * \since QGIS 3.26
      */
     virtual QgsContrastEnhancement::ContrastEnhancementAlgorithm contrastEnhancementAlgorithm() const { return QgsContrastEnhancement::NoEnhancement; }
 
     /**
-     * Sets the constrast enhancement \a algorithm to be used by the raster renderer.
+     * Sets the contrast enhancement \a algorithm to be used by the raster renderer.
      * \since QGIS 3.26
      */
     virtual void setContrastEnhancementAlgorithm( QgsContrastEnhancement::ContrastEnhancementAlgorithm algorithm ) { Q_UNUSED( algorithm ) return; }
