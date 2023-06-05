b4e616c5a3266663430a3df3e6de2254b62a01f0
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-06-05 08:48:18 +0200

    sipify

---
+++ python/gui/auto_additions/qgsadvanceddigitizingfloater.py
@@ -0,0 +1,14 @@
+# The following has been generated automatically from src/gui/qgsadvanceddigitizingfloater.h
+# monkey patching scoped based enum
+QgsAdvancedDigitizingFloater.FloaterItem.XCoordinate.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.YCoordinate.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.MCoordinate.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.ZCoordinate.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.Angle.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.CommonAngleSnapping.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.Distance.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.Bearing.__doc__ = ""
+QgsAdvancedDigitizingFloater.FloaterItem.__doc__ = 'Available floater items\n\n' + '* ``XCoordinate``: ' + QgsAdvancedDigitizingFloater.FloaterItem.XCoordinate.__doc__ + '\n' + '* ``YCoordinate``: ' + QgsAdvancedDigitizingFloater.FloaterItem.YCoordinate.__doc__ + '\n' + '* ``MCoordinate``: ' + QgsAdvancedDigitizingFloater.FloaterItem.MCoordinate.__doc__ + '\n' + '* ``ZCoordinate``: ' + QgsAdvancedDigitizingFloater.FloaterItem.ZCoordinate.__doc__ + '\n' + '* ``Angle``: ' + QgsAdvancedDigitizingFloater.FloaterItem.Angle.__doc__ + '\n' + '* ``CommonAngleSnapping``: ' + QgsAdvancedDigitizingFloater.FloaterItem.CommonAngleSnapping.__doc__ + '\n' + '* ``Distance``: ' + QgsAdvancedDigitizingFloater.FloaterItem.Distance.__doc__ + '\n' + '* ``Bearing``: ' + QgsAdvancedDigitizingFloater.FloaterItem.Bearing.__doc__
+# --
+QgsAdvancedDigitizingFloater.FloaterItem.baseClass = QgsAdvancedDigitizingFloater
+FloaterItem = QgsAdvancedDigitizingFloater  # dirty hack since SIP seems to introduce the flags in module
