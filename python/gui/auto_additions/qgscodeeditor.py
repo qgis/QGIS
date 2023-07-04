d1a86d12c58da6aeab032969e6359221498da3f5
Nyall Dawson <nyall.dawson@gmail.com>
2023-07-04 19:22:29 +1000

    Fix handling of ' in enum docstrings in sipify

--- python/gui/auto_additions/qgscodeeditor.py
+++ python/gui/auto_additions/qgscodeeditor.py
@@ -3,7 +3,7 @@
 QgsCodeEditor.Mode.ScriptEditor.__doc__ = "Standard mode, allows for display and edit of entire scripts"
 QgsCodeEditor.Mode.OutputDisplay.__doc__ = "Read only mode for display of command outputs"
 QgsCodeEditor.Mode.CommandInput.__doc__ = "Command input mode"
-QgsCodeEditor.Mode.__doc__ = 'Code editor modes.\n\n.. versionadded:: 3.30\n\n' + '* ``ScriptEditor``: ' + QgsCodeEditor.Mode.ScriptEditor.__doc__ + '\n' + '* ``OutputDisplay``: ' + QgsCodeEditor.Mode.OutputDisplay.__doc__ + '\n' + '* ``CommandInput``: ' + QgsCodeEditor.Mode.CommandInput.__doc__
+QgsCodeEditor.Mode.__doc__ = "Code editor modes.\n\n.. versionadded:: 3.30\n\n" + '* ``ScriptEditor``: ' + QgsCodeEditor.Mode.ScriptEditor.__doc__ + '\n' + '* ``OutputDisplay``: ' + QgsCodeEditor.Mode.OutputDisplay.__doc__ + '\n' + '* ``CommandInput``: ' + QgsCodeEditor.Mode.CommandInput.__doc__
 # --
 QgsCodeEditor.Mode.baseClass = QgsCodeEditor
 # monkey patching scoped based enum
@@ -16,13 +16,13 @@ QgsCodeEditor.ErrorIndicators.__doc__ = "Error indicators"
 QgsCodeEditor.FoldingControls = QgsCodeEditor.MarginRole.FoldingControls
 QgsCodeEditor.FoldingControls.is_monkey_patched = True
 QgsCodeEditor.FoldingControls.__doc__ = "Folding controls"
-QgsCodeEditor.MarginRole.__doc__ = 'Margin roles.\n\nThis enum contains the roles which the different numbered margins are used for.\n\n.. versionadded:: 3.16\n\n' + '* ``LineNumbers``: ' + QgsCodeEditor.MarginRole.LineNumbers.__doc__ + '\n' + '* ``ErrorIndicators``: ' + QgsCodeEditor.MarginRole.ErrorIndicators.__doc__ + '\n' + '* ``FoldingControls``: ' + QgsCodeEditor.MarginRole.FoldingControls.__doc__
+QgsCodeEditor.MarginRole.__doc__ = "Margin roles.\n\nThis enum contains the roles which the different numbered margins are used for.\n\n.. versionadded:: 3.16\n\n" + '* ``LineNumbers``: ' + QgsCodeEditor.MarginRole.LineNumbers.__doc__ + '\n' + '* ``ErrorIndicators``: ' + QgsCodeEditor.MarginRole.ErrorIndicators.__doc__ + '\n' + '* ``FoldingControls``: ' + QgsCodeEditor.MarginRole.FoldingControls.__doc__
 # --
 QgsCodeEditor.MarginRole.baseClass = QgsCodeEditor
 # monkey patching scoped based enum
 QgsCodeEditor.Flag.CodeFolding.__doc__ = "Indicates that code folding should be enabled for the editor"
 QgsCodeEditor.Flag.ImmediatelyUpdateHistory.__doc__ = "Indicates that the history file should be immediately updated whenever a command is executed, instead of the default behavior of only writing the history on widget close. Since QGIS 3.32."
-QgsCodeEditor.Flag.__doc__ = 'Flags controlling behavior of code editor\n\n.. versionadded:: 3.28\n\n' + '* ``CodeFolding``: ' + QgsCodeEditor.Flag.CodeFolding.__doc__ + '\n' + '* ``ImmediatelyUpdateHistory``: ' + QgsCodeEditor.Flag.ImmediatelyUpdateHistory.__doc__
+QgsCodeEditor.Flag.__doc__ = "Flags controlling behavior of code editor\n\n.. versionadded:: 3.28\n\n" + '* ``CodeFolding``: ' + QgsCodeEditor.Flag.CodeFolding.__doc__ + '\n' + '* ``ImmediatelyUpdateHistory``: ' + QgsCodeEditor.Flag.ImmediatelyUpdateHistory.__doc__
 # --
 QgsCodeEditor.Flag.baseClass = QgsCodeEditor
 QgsCodeEditor.Flags.baseClass = QgsCodeEditor
