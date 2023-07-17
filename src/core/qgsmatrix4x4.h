ee726eda6c900bd2233a77b1eb128706f86b1272
Martin Dobias <wonder.sk@gmail.com>
2023-07-17 12:27:46 +0200

    Add QgsMatrix4x4
    This adds 4x4 matrix with double precision values for high precision transforms.
    Just like QgsVector3D is similar to QVector3D but uses doubles instead of floats,
    this is a similar approach with QgsMatrix4x4 vs QMatrix4x4.
    
    Compared to the Qt's QMatrix4x4, the API has been reduced to the absolute minimum
    to only provide functionality that we will need (create matrix, get matrix data,
    multiply with other matrix, multiply with vector).

---
+++ src/core/qgsmatrix4x4.h
@@ -0,0 +1,84 @@
+/***************************************************************************
+  qgsmatrix4x4.h
+  --------------------------------------
+  Date                 : July 2023
+  Copyright            : (C) 2023 by Martin Dobias
+  Email                : wonder dot sk at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSMATRIX4X4_H
+#define QGSMATRIX4X4_H
+
+#include "qgis_core.h"
+
+#include "qgsvector3d.h"
+
+#define SIP_NO_FILE
+
+/**
+ * \ingroup core
+ * \brief A simple 4x4 matrix implementation useful for transformation in 3D space.
+ *
+ * It is similar to QMatrix4x4, but working with double precision values.
+ * Most of the time, doing transform using QMatrix4x4 is fine, however QgsMatrix4x4
+ * is useful in situations where single precision floats are not enough.
+ * For example, when using transform matrix where translation component has
+ * values in order of millions.
+ *
+ * \warning Only for internal use
+ *
+ * \note Not available in Python bindings
+ * \since QGIS 3.34
+ */
+class CORE_EXPORT QgsMatrix4x4
+{
+  public:
+    //! Initializes identity matrix
+    QgsMatrix4x4() { setToIdentity(); }
+    //! Initializes matrix by setting all values in row-major order
+    QgsMatrix4x4( double m11, double m12, double m13, double m14,
+                  double m21, double m22, double m23, double m24,
+                  double m31, double m32, double m33, double m34,
+                  double m41, double m42, double m43, double m44 );
+
+    //! Returns pointer to the matrix data (stored in column-major order)
+    const double *constData() const { return *m; }
+    //! Returns pointer to the matrix data (stored in column-major order)
+    double *data() { return *m; }
+
+    //! Matrix-vector multiplication (vector is converted to homogenous coordinates [X,Y,Z,1] and back)
+    QgsVector3D map( const QgsVector3D &vector ) const
+    {
+      return *this * vector;
+    }
+
+    //! Returns whether this matrix is an identity matrix
+    bool isIdentity() const;
+    //! Sets matrix to be identity matrix
+    void setToIdentity();
+
+    friend QgsMatrix4x4 operator*( const QgsMatrix4x4 &m1, const QgsMatrix4x4 &m2 );
+    friend QgsVector3D operator*( const QgsMatrix4x4 &matrix, const QgsVector3D &vector );
+
+  private:
+    // Matrix data - in column-major order
+    double m[4][4];
+
+    //! Construct without initializing identity matrix.
+    explicit QgsMatrix4x4( int ) { }   // cppcheck-suppress uninitMemberVarPrivate
+};
+
+//! Matrix-matrix multiplication (useful to concatenate transforms)
+CORE_EXPORT QgsVector3D operator*( const QgsMatrix4x4 &matrix, const QgsVector3D &vector );
+//! Matrix-vector multiplication (vector is converted to homogenous coordinates [X,Y,Z,1] and back)
+CORE_EXPORT QgsMatrix4x4 operator*( const QgsMatrix4x4 &m1, const QgsMatrix4x4 &m2 );
+
+
+#endif
