f658e5de7b354dae1b2f29484a87af3770f642d3
Nyall Dawson <nyall.dawson@gmail.com>
2023-07-17 10:55:59 +1000

    Create class for representing tiled mesh node bounding volumes
    And implement for sphere, region and oriented box bounding volume
    types

---
+++ src/core/tiledmesh/qgstiledmeshboundingvolume.cpp
@@ -0,0 +1,252 @@
+/***************************************************************************
+                         qgstiledmeshboundingvolume.cpp
+                         --------------------
+    begin                : July 2023
+    copyright            : (C) 2023 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgstiledmeshboundingvolume.h"
+#include "qgscircle.h"
+#include "qgscoordinatetransform.h"
+#include "qgsvector3d.h"
+
+QgsAbstractTiledMeshNodeBoundingVolume::~QgsAbstractTiledMeshNodeBoundingVolume() = default;
+
+//
+// QgsTiledMeshNodeBoundingVolumeRegion
+//
+
+QgsTiledMeshNodeBoundingVolumeRegion::QgsTiledMeshNodeBoundingVolumeRegion( const QgsBox3d &region )
+  : mRegion( region )
+{
+
+}
+
+Qgis::TiledMeshBoundingVolumeType QgsTiledMeshNodeBoundingVolumeRegion::type() const
+{
+  return Qgis::TiledMeshBoundingVolumeType::Region;
+}
+
+QgsBox3d QgsTiledMeshNodeBoundingVolumeRegion::bounds( const QgsCoordinateTransform &transform, Qgis::TransformDirection direction ) const
+{
+  if ( !transform.isValid() || transform.isShortCircuited() )
+  {
+    return mRegion;
+  }
+  else
+  {
+    // transform each corner of the box, then collect the min/max x/y/z values of the result
+    QVector< double > x{ mRegion.xMinimum(), mRegion.xMinimum(), mRegion.xMaximum(), mRegion.xMaximum(), mRegion.xMinimum(), mRegion.xMinimum(), mRegion.xMaximum(), mRegion.xMaximum() };
+    QVector< double > y{ mRegion.yMinimum(), mRegion.yMaximum(), mRegion.yMinimum(), mRegion.yMaximum(), mRegion.yMinimum(), mRegion.yMaximum(), mRegion.yMinimum(), mRegion.yMaximum() };
+    QVector< double > z{ mRegion.zMinimum(), mRegion.zMinimum(), mRegion.zMinimum(), mRegion.zMinimum(), mRegion.zMaximum(), mRegion.zMaximum(), mRegion.zMaximum(), mRegion.zMaximum() };
+    transform.transformInPlace( x, y, z, direction );
+
+    const auto minMaxX = std::minmax_element( x.constBegin(), x.constEnd() );
+    const auto minMaxY = std::minmax_element( y.constBegin(), y.constEnd() );
+    const auto minMaxZ = std::minmax_element( z.constBegin(), z.constEnd() );
+    return QgsBox3d( *minMaxX.first, *minMaxY.first, *minMaxZ.first, *minMaxX.second, *minMaxY.second, *minMaxZ.second );
+  }
+}
+
+QgsTiledMeshNodeBoundingVolumeRegion *QgsTiledMeshNodeBoundingVolumeRegion::clone() const
+{
+  return new QgsTiledMeshNodeBoundingVolumeRegion( *this );
+}
+
+QgsAbstractGeometry *QgsTiledMeshNodeBoundingVolumeRegion::as2DGeometry( const QgsCoordinateTransform &transform, Qgis::TransformDirection direction ) const
+{
+  std::unique_ptr< QgsPolygon > polygon = std::make_unique< QgsPolygon >();
+
+  // transform using the region center point z
+  const double centerZ = 0.5 * ( mRegion.zMaximum() + mRegion.zMinimum() );
+  std::unique_ptr< QgsLineString > ext = std::make_unique< QgsLineString >(
+      QVector< double >() << mRegion.xMinimum()
+      << mRegion.xMaximum()
+      << mRegion.xMaximum()
+      << mRegion.xMinimum()
+      << mRegion.xMinimum(),
+      QVector< double >() << mRegion.yMinimum()
+      << mRegion.yMinimum()
+      << mRegion.yMaximum()
+      << mRegion.yMaximum()
+      << mRegion.yMinimum(),
+      QVector< double >() << centerZ
+      << centerZ
+      << centerZ
+      << centerZ
+      << centerZ );
+
+  if ( transform.isValid() && !transform.isShortCircuited() )
+  {
+    ext->transform( transform, direction, true );
+  }
+  ext->dropZValue();
+  polygon->setExteriorRing( ext.release() );
+
+  return polygon.release();
+}
+
+
+//
+// QgsTiledMeshNodeBoundingVolumeBox
+//
+
+QgsTiledMeshNodeBoundingVolumeBox::QgsTiledMeshNodeBoundingVolumeBox( const QgsOrientedBox3D &box )
+  : mBox( box )
+{
+}
+
+Qgis::TiledMeshBoundingVolumeType QgsTiledMeshNodeBoundingVolumeBox::type() const
+{
+  return Qgis::TiledMeshBoundingVolumeType::OrientedBox;
+}
+
+QgsBox3d QgsTiledMeshNodeBoundingVolumeBox::bounds( const QgsCoordinateTransform &transform, Qgis::TransformDirection direction ) const
+{
+  if ( transform.isValid() && !transform.isShortCircuited() )
+  {
+    const QVector< QgsVector3D > corners = mBox.corners();
+    QVector< double > x;
+    x.reserve( 8 );
+    QVector< double > y;
+    y.reserve( 8 );
+    QVector< double > z;
+    z.reserve( 8 );
+    for ( int i = 0; i < 8; ++i )
+    {
+      x.append( corners[i].x() );
+      y.append( corners[i].y() );
+      z.append( corners[i].z() );
+    }
+    transform.transformInPlace( x, y, z, direction );
+
+    const auto minMaxX = std::minmax_element( x.constBegin(), x.constEnd() );
+    const auto minMaxY = std::minmax_element( y.constBegin(), y.constEnd() );
+    const auto minMaxZ = std::minmax_element( z.constBegin(), z.constEnd() );
+    return QgsBox3d( *minMaxX.first, *minMaxY.first, *minMaxZ.first, *minMaxX.second, *minMaxY.second, *minMaxZ.second );
+  }
+  else
+  {
+    return mBox.extent();
+  }
+}
+
+QgsTiledMeshNodeBoundingVolumeBox *QgsTiledMeshNodeBoundingVolumeBox::clone() const
+{
+  return new QgsTiledMeshNodeBoundingVolumeBox( *this );
+}
+
+QgsAbstractGeometry *QgsTiledMeshNodeBoundingVolumeBox::as2DGeometry( const QgsCoordinateTransform &transform, Qgis::TransformDirection direction ) const
+{
+  std::unique_ptr< QgsPolygon > polygon = std::make_unique< QgsPolygon >();
+
+  const QVector< QgsVector3D > corners = mBox.corners();
+  QVector< double > x;
+  x.reserve( 8 );
+  QVector< double > y;
+  y.reserve( 8 );
+  QVector< double > z;
+  z.reserve( 8 );
+  for ( int i = 0; i < 8; ++i )
+  {
+    x.append( corners[i].x() );
+    y.append( corners[i].y() );
+    z.append( corners[i].z() );
+  }
+  if ( transform.isValid() && !transform.isShortCircuited() )
+  {
+    transform.transformInPlace( x, y, z, direction );
+  }
+
+  const QVector<double> x2DCenter
+  {
+    0.5 * ( x[0] + x[4] ),
+    0.5 * ( x[1] + x[5] ),
+    0.5 * ( x[2] + x[6] ),
+    0.5 * ( x[3] + x[7] ),
+    0.5 * ( x[0] + x[4] ),
+  };
+  const QVector<double> y2DCenter
+  {
+    0.5 * ( y[0] + y[4] ),
+    0.5 * ( y[1] + y[5] ),
+    0.5 * ( y[2] + y[6] ),
+    0.5 * ( y[3] + y[7] ),
+    0.5 * ( y[0] + y[4] ),
+  };
+
+  std::unique_ptr< QgsLineString > ext = std::make_unique< QgsLineString >( x2DCenter, y2DCenter );
+  polygon->setExteriorRing( ext.release() );
+  return polygon.release();
+}
+
+//
+// QgsTiledMeshNodeBoundingVolumeSphere
+//
+
+QgsTiledMeshNodeBoundingVolumeSphere::QgsTiledMeshNodeBoundingVolumeSphere( const QgsSphere &sphere )
+  : mSphere( sphere )
+{
+
+}
+
+Qgis::TiledMeshBoundingVolumeType QgsTiledMeshNodeBoundingVolumeSphere::type() const
+{
+  return Qgis::TiledMeshBoundingVolumeType::Sphere;
+}
+
+QgsBox3d QgsTiledMeshNodeBoundingVolumeSphere::bounds( const QgsCoordinateTransform &transform, Qgis::TransformDirection direction ) const
+{
+  if ( transform.isValid() && !transform.isShortCircuited() )
+  {
+    std::unique_ptr< QgsAbstractGeometry > centerSlice( as2DGeometry( transform, direction ) );
+
+    // a line through the z range of sphere, passing through center
+    QVector< double > x { mSphere.centerX(), mSphere.centerX() };
+    QVector< double > y { mSphere.centerY(), mSphere.centerY() };
+    QVector< double > z { mSphere.centerZ() - mSphere.radius(), mSphere.centerZ() + mSphere.radius() };
+    transform.transformInPlace( x, y, z, direction );
+
+    const QgsRectangle bounds2d( centerSlice->boundingBox() );
+    return QgsBox3d( bounds2d.xMinimum(), bounds2d.yMinimum(), std::min( z[0], z[1] ), bounds2d.xMaximum(), bounds2d.yMaximum(), std::max( z[0], z[1] ) );
+  }
+  else
+  {
+    return mSphere.boundingBox();
+  }
+}
+
+QgsTiledMeshNodeBoundingVolumeSphere *QgsTiledMeshNodeBoundingVolumeSphere::clone() const
+{
+  return new QgsTiledMeshNodeBoundingVolumeSphere( *this );
+}
+
+QgsAbstractGeometry *QgsTiledMeshNodeBoundingVolumeSphere::as2DGeometry( const QgsCoordinateTransform &transform, Qgis::TransformDirection direction ) const
+{
+  std::unique_ptr< QgsCircularString > exterior( mSphere.toCircle().toCircularString() );
+  std::unique_ptr< QgsCurvePolygon > polygon = std::make_unique< QgsCurvePolygon >();
+  if ( transform.isValid() && !transform.isShortCircuited() )
+  {
+    exterior->addZValue( mSphere.centerZ() );
+    // have to segmentize prior to transforming
+    std::unique_ptr< QgsCurve > segmentized( exterior->segmentize( ) );
+    segmentized->transform( transform, direction, true );
+    segmentized->dropZValue();
+    polygon->setExteriorRing( segmentized.release() );
+  }
+  else
+  {
+    polygon->setExteriorRing( exterior.release() );
+  }
+  return polygon.release();
+}
---
+++ src/core/tiledmesh/qgstiledmeshboundingvolume.h
@@ -0,0 +1,184 @@
+/***************************************************************************
+                         qgstiledmeshboundingvolume.h
+                         --------------------
+    begin                : July 2023
+    copyright            : (C) 2023 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ******************************************************************
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSTILEDMESHBOUNDINGVOLUME_H
+#define QGSTILEDMESHBOUNDINGVOLUME_H
+
+#include "qgis_core.h"
+#include "qgis_sip.h"
+#include "qgis.h"
+#include "qgsbox3d.h"
+#include "qgssphere.h"
+#include "qgsorientedbox3d.h"
+#include "qgscoordinatetransform.h"
+
+/**
+ * \ingroup core
+ * \brief Abstract base class for bounding volumes for tiled mesh nodes.
+ *
+ * \since QGIS 3.34
+ */
+class CORE_EXPORT QgsAbstractTiledMeshNodeBoundingVolume
+{
+  public:
+
+#ifdef SIP_RUN
+    SIP_CONVERT_TO_SUBCLASS_CODE
+    switch ( sipCpp->type() )
+    {
+      case Qgis::TiledMeshBoundingVolumeType::Region:
+        sipType = sipType_QgsTiledMeshNodeBoundingVolumeRegion;
+        break;
+      case Qgis::TiledMeshBoundingVolumeType::OrientedBox:
+        sipType = sipType_QgsTiledMeshNodeBoundingVolumeBox;
+        break;
+      case Qgis::TiledMeshBoundingVolumeType::Sphere:
+        sipType = sipType_QgsTiledMeshNodeBoundingVolumeSphere;
+        break;
+      default:
+        sipType = 0;
+        break;
+    };
+    SIP_END
+#endif
+
+    virtual ~QgsAbstractTiledMeshNodeBoundingVolume();
+
+    /**
+     * Returns the type of the volume;
+     */
+    virtual Qgis::TiledMeshBoundingVolumeType type() const = 0;
+
+    /**
+     * Returns the axis aligned bounding box of the volume.
+     *
+     * The optional \a transform and \a direction arguments should be used whenever the volume needs
+     * to be transformed into a specific destination CRS, in order to correctly handle 3D coordinate transforms.
+     */
+    virtual QgsBox3d bounds( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const = 0;
+
+    /**
+     * Returns a clone of the volume.
+     */
+    virtual QgsAbstractTiledMeshNodeBoundingVolume *clone() const = 0 SIP_FACTORY;
+
+    /**
+     * Return a new geometry representing the 2-dimensional X/Y center slice of the volume.
+     *
+     * Caller takes ownership of the returned geometry.
+     *
+     * The optional \a transform and \a direction arguments should be used whenever the volume needs
+     * to be transformed into a specific destination CRS, in order to correctly handle 3D coordinate transforms.
+     */
+    virtual QgsAbstractGeometry *as2DGeometry( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const = 0 SIP_FACTORY;
+
+};
+
+/**
+ * \ingroup core
+ * \brief A region bounding volume for tiled mesh nodes.
+ *
+ * \since QGIS 3.34
+ */
+class CORE_EXPORT QgsTiledMeshNodeBoundingVolumeRegion : public QgsAbstractTiledMeshNodeBoundingVolume
+{
+  public:
+
+    /**
+     * Constructor for QgsTiledMeshNodeBoundingVolumeRegion, with the specified \a region.
+     */
+    QgsTiledMeshNodeBoundingVolumeRegion( const QgsBox3d &region );
+
+    Qgis::TiledMeshBoundingVolumeType type() const FINAL;
+    QgsBox3d bounds( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const FINAL SIP_THROW( QgsCsException );
+    QgsTiledMeshNodeBoundingVolumeRegion *clone() const FINAL SIP_FACTORY;
+    QgsAbstractGeometry *as2DGeometry( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const FINAL SIP_THROW( QgsCsException ) SIP_FACTORY;
+
+    /**
+     * Returns the volume's region.
+     */
+    QgsBox3d region() const { return mRegion; }
+
+  private:
+    QgsBox3d mRegion;
+};
+
+/**
+ * \ingroup core
+ * \brief A oriented box bounding volume for tiled mesh nodes.
+ *
+ * \since QGIS 3.34
+ */
+class CORE_EXPORT QgsTiledMeshNodeBoundingVolumeBox : public QgsAbstractTiledMeshNodeBoundingVolume
+{
+  public:
+
+    /**
+     * Constructor for QgsTiledMeshNodeBoundingVolumeBox, with the specified oriented \a box.
+     */
+    QgsTiledMeshNodeBoundingVolumeBox( const QgsOrientedBox3D &box );
+
+    Qgis::TiledMeshBoundingVolumeType type() const FINAL;
+    QgsBox3d bounds( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const FINAL SIP_THROW( QgsCsException );
+    QgsTiledMeshNodeBoundingVolumeBox *clone() const FINAL SIP_FACTORY;
+    QgsAbstractGeometry *as2DGeometry( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const FINAL SIP_THROW( QgsCsException ) SIP_FACTORY;
+
+    /**
+     * Returns the volume's oriented box.
+     */
+    QgsOrientedBox3D box() const { return mBox; }
+
+  private:
+
+    QgsOrientedBox3D mBox;
+
+};
+
+/**
+ * \ingroup core
+ * \brief A spherical bounding volume for tiled mesh nodes.
+ *
+ * \since QGIS 3.34
+ */
+class CORE_EXPORT QgsTiledMeshNodeBoundingVolumeSphere: public QgsAbstractTiledMeshNodeBoundingVolume
+{
+  public:
+
+    /**
+     * Constructor for QgsTiledMeshNodeBoundingVolumeBox, with the specified \a sphere.
+     */
+    QgsTiledMeshNodeBoundingVolumeSphere( const QgsSphere &sphere );
+
+    Qgis::TiledMeshBoundingVolumeType type() const FINAL;
+    QgsBox3d bounds( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const FINAL SIP_THROW( QgsCsException );
+    QgsTiledMeshNodeBoundingVolumeSphere *clone() const FINAL SIP_FACTORY;
+    QgsAbstractGeometry *as2DGeometry( const QgsCoordinateTransform &transform = QgsCoordinateTransform(), Qgis::TransformDirection direction = Qgis::TransformDirection::Forward ) const FINAL SIP_THROW( QgsCsException ) SIP_FACTORY;
+
+    /**
+     * Returns the volume's sphere.
+     */
+    QgsSphere sphere() const { return mSphere; }
+
+  private:
+
+    QgsSphere mSphere;
+
+};
+
+
+#endif // QGSTILEDMESHBOUNDINGVOLUME_H
