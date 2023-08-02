b6e6eaf03a2cb5d4fb5864b959b8e9920f5e168e
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2023-08-02 09:48:20 +0200

    feat(Qgs3DSceneExporter): add test to validate feature duplication filtering for 3d scene export

--- src/3d/qgs3dsceneexporter.h
+++ src/3d/qgs3dsceneexporter.h
@@ -41,6 +41,7 @@ class QgsPolygon3DSymbol;
 class QgsLine3DSymbol;
 class QgsPoint3DSymbol;
 class QgsMeshEntity;
+class TestQgs3DRendering;
 
 #define SIP_NO_FILE
 
@@ -152,6 +153,7 @@ class _3D_EXPORT Qgs3DSceneExporter : public Qt3DCore::QEntity
     friend QgsPolygon3DSymbol;
     friend QgsLine3DSymbol;
     friend QgsPoint3DSymbol;
+    friend TestQgs3DRendering;
 };
 
 #endif // QGS3DSCENEEXPORTER_H
