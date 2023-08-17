b2748d9c3c6ad9278c042b49a90ae595d7462125
Nyall Dawson <nyall.dawson@gmail.com>
2023-08-17 10:18:11 +1000

    Ensure layer filter is retained after cloning layers
    Fixes #53774

--- src/core/vector/qgsvectorlayer.cpp
+++ src/core/vector/qgsvectorlayer.cpp
@@ -310,6 +310,7 @@ QgsVectorLayer *QgsVectorLayer::clone() const
 
   if ( mDataProvider )
     layer->setProviderEncoding( mDataProvider->encoding() );
+  layer->setSubsetString( subsetString() );
   layer->setDisplayExpression( displayExpression() );
   layer->setMapTipTemplate( mapTipTemplate() );
   layer->setMapTipsEnabled( mapTipsEnabled() );
