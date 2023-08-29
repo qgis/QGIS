9e7b06ec770666d9de7686940815b4f213c46720
Andrea Giudiceandrea <andreaerdna@libero.it>
2023-08-29 19:25:16 +0200

    Fix QgsMapLayer::saveNamedProperty and QgsMapLayer::saveSldStyleV2
    set resultFlag to False when the directory is not writable

--- src/core/qgsmaplayer.cpp
+++ src/core/qgsmaplayer.cpp
@@ -1392,6 +1392,7 @@ QString QgsMapLayer::saveNamedProperty( const QString &uri, QgsMapLayer::Propert
     const QFileInfo myDirInfo( myFileInfo.path() );  //excludes file name
     if ( !myDirInfo.isWritable() )
     {
+      resultFlag = false;
       return tr( "The directory containing your dataset needs to be writable!" );
     }
 
@@ -1698,6 +1699,7 @@ QString QgsMapLayer::saveSldStyle( const QString &uri, bool &resultFlag ) const
     const QFileInfo myDirInfo( myFileInfo.path() );  //excludes file name
     if ( !myDirInfo.isWritable() )
     {
+      resultFlag = false;
       return tr( "The directory containing your dataset needs to be writable!" );
     }
 
