090e949a0f97228321967d804d9c72b73648c856
vcloarec <vcloarec@gmail.com>
2023-08-31 10:22:31 -0400

    fix QgsMapCanvas::readProject() if sender is not QgsProject

--- src/gui/qgsmapcanvas.cpp
+++ src/gui/qgsmapcanvas.cpp
@@ -3214,8 +3214,6 @@ void QgsMapCanvas::setSnappingUtils( QgsSnappingUtils *utils )
 
 void QgsMapCanvas::readProject( const QDomDocument &doc )
 {
-  QgsProject *project = qobject_cast< QgsProject * >( sender() );
-
   QDomNodeList nodes = doc.elementsByTagName( QStringLiteral( "mapcanvas" ) );
   if ( nodes.count() )
   {
@@ -3270,12 +3268,12 @@ void QgsMapCanvas::readProject( const QDomDocument &doc )
   else
   {
     QgsDebugMsgLevel( QStringLiteral( "Couldn't read mapcanvas information from project" ), 2 );
-    if ( !project->viewSettings()->defaultViewExtent().isNull() )
+    if ( !QgsProject::instance()->viewSettings()->defaultViewExtent().isNull() )
     {
-      setReferencedExtent( project->viewSettings()->defaultViewExtent() );
+      setReferencedExtent( QgsProject::instance()->viewSettings()->defaultViewExtent() );
     }
 
-    setRotation( project->viewSettings()->defaultRotation() );
+    setRotation( QgsProject::instance()->viewSettings()->defaultRotation() );
     clearExtentHistory(); // clear the extent history on project load
   }
 }
