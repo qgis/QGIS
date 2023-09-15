0ad16cad09332962d75a00eb71ef3b2278048d0c
Mathieu Pellerin <nirvn.asia@gmail.com>
2023-09-15 09:12:50 +0700

    [ogr] Fix assert when opening GeoJSON (and other) datasets

--- src/core/providers/ogr/qgsogrproviderutils.cpp
+++ src/core/providers/ogr/qgsogrproviderutils.cpp
@@ -1553,7 +1553,7 @@ QgsOgrLayerUniquePtr QgsOgrProviderUtils::getLayer( const QString &dsName,
     {
       if ( !ds->canBeShared )
         continue;
-      Q_ASSERT( ds->refCount > 0 );
+      Q_ASSERT( sDeferDatasetClosingCounter > 0 || ds->refCount > 0 );
 
       QString layerName;
       OGRLayerH hLayer;
@@ -2296,7 +2296,7 @@ QgsOgrLayerUniquePtr QgsOgrProviderUtils::getLayer( const QString &dsName,
     {
       if ( !ds->canBeShared )
         continue;
-      Q_ASSERT( ds->refCount > 0 );
+      Q_ASSERT( sDeferDatasetClosingCounter > 0 || ds->refCount > 0 );
 
       auto iter2 = ds->setLayers.find( layerName );
       if ( iter2 == ds->setLayers.end() )
