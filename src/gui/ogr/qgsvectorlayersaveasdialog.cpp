f4e553447472e650ef15d49bb9de68dd650a6368
Even Rouault <even.rouault@spatialys.com>
2023-09-17 16:11:35 +0200

    SQLite layer export: do not set SRID= layer creation options that is interpreted by the OGR SQLite driver as meaning SRID=0 (fixes #54560)

--- src/gui/ogr/qgsvectorlayersaveasdialog.cpp
+++ src/gui/ogr/qgsvectorlayersaveasdialog.cpp
@@ -960,7 +960,8 @@ QStringList QgsVectorLayerSaveAsDialog::datasourceOptions() const
         {
           QgsVectorFileWriter::HiddenOption *opt =
             dynamic_cast<QgsVectorFileWriter::HiddenOption *>( it.value() );
-          options << QStringLiteral( "%1=%2" ).arg( it.key(), opt->mValue );
+          if ( !opt->mValue.isEmpty() )
+            options << QStringLiteral( "%1=%2" ).arg( it.key(), opt->mValue );
           break;
         }
       }
@@ -1019,7 +1020,8 @@ QStringList QgsVectorLayerSaveAsDialog::layerOptions() const
         {
           QgsVectorFileWriter::HiddenOption *opt =
             dynamic_cast<QgsVectorFileWriter::HiddenOption *>( it.value() );
-          options << QStringLiteral( "%1=%2" ).arg( it.key(), opt->mValue );
+          if ( !opt->mValue.isEmpty() )
+            options << QStringLiteral( "%1=%2" ).arg( it.key(), opt->mValue );
           break;
         }
       }
