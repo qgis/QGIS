9eaa1a722f8e266df8d5444871c3a5c410c48858
Julien Cabieces <julien.cabieces@oslandia.com>
2023-09-21 15:37:24 +0200

    Fixes reshape tool topo points

---
+++ src/app/maptools/qgsavoidintersectionsoperation.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+    qgsavoidintersectionsoperation.h
+    ---------------------
+    begin                : 2023/09/20
+    copyright            : (C) 2023 by Julien Cabieces
+    email                : julien dot cabieces at oslandia dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSAVOIDINTERSECTIONSOPERATION_H
+#define QGSAVOIDINTERSECTIONSOPERATION_H
+
+#include <QObject>
+
+#include "qgis.h"
+#include "qgis_gui.h"
+#include "qgsfeatureid.h"
+#include "qgspoint.h"
+
+class QgsVectorLayer;
+class QgsGeometry;
+class QgsMessageBar;
+
+/**
+ * \ingroup gui
+ * \brief Helper class to apply the avoid intersection operation on a geometry and treat resulting issues.
+ * \since QGIS 3.34
+*/
+class GUI_EXPORT QgsAvoidIntersectionsOperation : public QObject
+{
+    Q_OBJECT
+
+  public:
+
+    /**
+     * Contructor
+     */
+    QgsAvoidIntersectionsOperation() = default;
+
+    // TODO cartouche
+    // TODO don't return int, return enum
+    Qgis::GeometryOperationResult apply( QgsVectorLayer *layer, QgsFeatureId fid, QgsGeometry &geom,
+                                         const QHash<QgsVectorLayer *, QSet<QgsFeatureId> > &ignoreFeatures = ( QHash<QgsVectorLayer *, QSet<QgsFeatureId> >() ) );
+
+  signals:
+
+    /**
+     * emmit a \a message with corresponding \a level
+     */
+    void messageEmitted( const QString &message, Qgis::MessageLevel = Qgis::MessageLevel::Info );
+};
+
+#endif
