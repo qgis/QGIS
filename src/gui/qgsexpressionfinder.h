d700d15b822b8cc315840d8ec75cf07ee61e205a
Yoann Quenach de Quivillic <yoann.quenach@gmail.com>
2023-09-28 00:06:27 +0200

    Fix SIP check and add doc

--- src/gui/qgsexpressionfinder.h
+++ src/gui/qgsexpressionfinder.h
@@ -21,23 +21,56 @@
 #include "qgis_sip.h"
 #include "qgis_gui.h"
 
+#define SIP_NO_FILE
+
 class QgsCodeEditor;
 class QTextEdit;
 class QPlainTextEdit;
 
 /**
  * \ingroup gui
- * \class QgsExpressionFinder
+ * \brief Helper methods to locate expressions within text editors
+ * \note not available in Python bindings
+ * \since QGIS 3.36
  */
-class GUI_EXPORT QgsExpressionFinder SIP_SKIP
+class GUI_EXPORT QgsExpressionFinder
 {
   public:
+
+    /**
+     * Find an expression at the given position in the given text
+     *
+     * If an expression is found, start and end are set to the position of the
+     * opening and closing brakets of the expression, and expression is set to
+     * the trimmed expression (excluding the surrounding [% %] characters)
+     *
+     * Otherwise, start and end are set to startSelectionPos and endSelectionPos
+     * and expression is set to the selected text
+     */
     static void findExpressionAtPos( const QString &text, int startSelectionPos, int endSelectionPos, int &start, int &end, QString &expression );
 
+    /**
+     * Find the expression under the cursor in the given editor and select it
+     *
+     * If an expression is found, it is returned (excluding the surrounding [% %] characters)
+     * Otherwise, the selection is kept unchanged and the selected text is returned
+     */
     static QString findAndSelectExpression( QgsCodeEditor *editor );
 
+    /**
+     * Find the expression under the cursor in the given editor and select it
+     *
+     * If an expression is found, it is returned (excluding the surrounding [% %] characters)
+     * Otherwise, the selection is kept unchanged and the selected text is returned
+     */
     static QString findAndSelectExpression( QTextEdit *editor );
 
+    /**
+     * Find the expression under the cursor in the given editor and select it
+     *
+     * If an expression is found, it is returned (excluding the surrounding [% %] characters)
+     * Otherwise, the selection is kept unchanged and the selected text is returned
+     */
     static QString findAndSelectExpression( QPlainTextEdit *editor );
 
 
