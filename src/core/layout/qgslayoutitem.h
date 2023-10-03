01b2bb8f41eedfeccccaf07969e0cc929ef219d9
Nyall Dawson <nyall.dawson@gmail.com>
2023-10-03 13:23:07 +1000

    QGraphicsItem::setGraphicsEffect takes ownership, using a unique_ptr is not appropriate
    Refs #54012

--- src/core/layout/qgslayoutitem.h
+++ src/core/layout/qgslayoutitem.h
@@ -1272,7 +1272,7 @@ class CORE_EXPORT QgsLayoutItem : public QgsLayoutObject, public QGraphicsRectIt
 
     //! Composition blend mode for item
     QPainter::CompositionMode mBlendMode = QPainter::CompositionMode_SourceOver;
-    std::unique_ptr< QgsLayoutEffect > mEffect;
+    QPointer< QgsLayoutEffect > mEffect;
 
     //! Item opacity, between 0 and 1
     double mOpacity = 1.0;
