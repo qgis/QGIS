2bd25071f77f6551022d28862def0eeea7afa488
Nyall Dawson <nyall.dawson@gmail.com>
2023-10-04 16:59:11 +1000

    Ensure that we always call prepareGeometryChange BEFORE changing properties which effect the boundingRect of a layout item
    Refs #52079
    Refs https://bugreports.qt.io/browse/QTBUG-18021

--- src/core/layout/qgslayoutitemlabel.h
+++ src/core/layout/qgslayoutitemlabel.h
@@ -211,13 +211,8 @@ class CORE_EXPORT QgsLayoutItemLabel: public QgsLayoutItem
     // In case of negative margins, the bounding rect may be larger than the
     // label's frame
     QRectF boundingRect() const override;
-
-    // Reimplemented to call prepareGeometryChange after toggling frame
     void setFrameEnabled( bool drawFrame ) override;
-
-    // Reimplemented to call prepareGeometryChange after changing stroke width
     void setFrameStrokeWidth( QgsLayoutMeasurement strokeWidth ) override;
-
 
     /**
      * Returns the text format used for drawing text in the label.
@@ -253,6 +248,8 @@ class CORE_EXPORT QgsLayoutItemLabel: public QgsLayoutItem
   private slots:
 
     void refreshExpressionContext();
+    //! Updates the bounding rect of this item
+    void updateBoundingRect();
 
   private:
     bool mFirstRender = true;
@@ -296,6 +293,8 @@ class CORE_EXPORT QgsLayoutItemLabel: public QgsLayoutItem
     QString createStylesheet() const;
 
     std::unique_ptr< QgsDistanceArea > mDistanceArea;
+
+    QRectF mCurrentRectangle;
 
     friend class QgsLayoutItemHtml;
 };
