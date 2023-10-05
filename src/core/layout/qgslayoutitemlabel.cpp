520192e6d08223385d424b88dd59f7b6943aca66
Nyall Dawson <nyall.dawson@gmail.com>
2023-10-05 12:15:01 +1000

    Fix layout labels don't immediately react to changes when semi-transparent

--- src/core/layout/qgslayoutitemlabel.cpp
+++ src/core/layout/qgslayoutitemlabel.cpp
@@ -337,6 +337,7 @@ void QgsLayoutItemLabel::setFont( const QFont &f )
   mFormat.setFont( f );
   if ( f.pointSizeF() > 0 )
     mFormat.setSize( f.pointSizeF() );
+  invalidateCache();
 }
 
 QgsTextFormat QgsLayoutItemLabel::textFormat() const
@@ -347,6 +348,7 @@ QgsTextFormat QgsLayoutItemLabel::textFormat() const
 void QgsLayoutItemLabel::setTextFormat( const QgsTextFormat &format )
 {
   mFormat = format;
+  invalidateCache();
 }
 
 void QgsLayoutItemLabel::setMargin( const double m )
