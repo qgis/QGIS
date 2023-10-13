9a55cba86325eed3434ba5d085d97da338eac019
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2023-10-13 14:32:14 +0200

    Add copyright header

--- src/server/services/wms/qgspdfwriter.h
+++ src/server/services/wms/qgspdfwriter.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                        qgspdfwriter.h
+  -------------------------------------------------------------------
+Date                 : 09 October 2023
+Copyright            : (C) 2023
+email                : marco.hugentobler at sourcepole dot com
+
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSPDFWRITER_H
 #define QGSPDFWRITER_H
 
