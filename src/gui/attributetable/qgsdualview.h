e213e2928f38570613270d4513b3958c4acb8e09
Patrice Pineault <TurboGraphxBeige@runbox.com>
2023-11-05 15:25:57 -0500

    Adding "Field Calculator" menu item to the attribute table header right click popup. Fixes #55266

--- src/gui/attributetable/qgsdualview.h
+++ src/gui/attributetable/qgsdualview.h
@@ -353,6 +353,8 @@ class GUI_EXPORT QgsDualView : public QStackedWidget, private Ui::QgsDualViewBas
 
     void hideColumn();
 
+    void fieldCalculator();
+
     void resizeColumn();
 
     void resizeAllColumns();
