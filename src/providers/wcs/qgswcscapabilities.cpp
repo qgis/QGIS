99b1b17f39d0b42a15bf357d489d9a714ac81940
Mathieu Pellerin <nirvn.asia@gmail.com>
2023-11-09 09:09:41 +0700

    [wcs] Fix compilation of the WCS capabilities class against Qt 6.6

--- src/providers/wcs/qgswcscapabilities.cpp
+++ src/providers/wcs/qgswcscapabilities.cpp
@@ -1312,12 +1312,12 @@ QgsWcsCoverageSummary *QgsWcsCapabilities::coverageSummary( QString const &ident
   {
     if ( c->identifier == identifier )
     {
-      return c;
+      return &( *c );
     }
     else
     {
       // search sub coverages
-      QgsWcsCoverageSummary *subCoverage = coverageSummary( identifier, c );
+      QgsWcsCoverageSummary *subCoverage = coverageSummary( identifier, &( *c ) );
       if ( subCoverage )
       {
         return subCoverage;
@@ -1343,7 +1343,7 @@ QList<QgsWcsCoverageSummary> QgsWcsCapabilities::coverageSummaries( const QgsWcs
   for ( QVector<QgsWcsCoverageSummary>::const_iterator c = parent->coverageSummary.constBegin(); c != parent->coverageSummary.constEnd(); ++c )
   {
     list.append( *c );
-    list.append( coverageSummaries( c ) );
+    list.append( coverageSummaries( &( *c ) ) );
   }
   return list;
 }
