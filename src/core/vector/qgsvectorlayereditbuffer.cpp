58aa3e4be6f676206e2b95574491d3eb3daf039c
Tarot Osuji <tarot@sdf.org>
2023-11-19 17:02:12 +0900

    Revert "Recompute extent for shapefile before committing changes"
    This reverts commit 40690ab7ead3ca733a443266c7b6e4f11d775b52.

--- src/core/vector/qgsvectorlayereditbuffer.cpp
+++ src/core/vector/qgsvectorlayereditbuffer.cpp
@@ -802,10 +802,6 @@ bool QgsVectorLayerEditBuffer::commitChangesChangeAttributes( bool &attributesCh
       return false;
     }
 
-    // recompute extent for Shapefile
-    if ( L->storageType() == QLatin1String( "ESRI Shapefile" ) )
-      L->extent();
-
     if ( L->dataProvider()->changeGeometryValues( mChangedGeometries ) )
     {
       commitErrors << tr( "SUCCESS: %n geometries were changed.", "changed geometries count", mChangedGeometries.size() );
