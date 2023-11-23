e9cbd075b79e8938509c9d77eaa90e731a2cfea4
Nyall Dawson <nyall.dawson@gmail.com>
2023-11-24 07:39:52 +1000

    Fix sip import

--- tests/src/python/test_qgsmaplayer.py
+++ tests/src/python/test_qgsmaplayer.py
@@ -14,7 +14,7 @@ import os
 import shutil
 import tempfile
 
-import sip
+from qgis.PyQt import sip
 from qgis.PyQt.QtCore import QTemporaryDir
 from qgis.PyQt.QtXml import QDomDocument
 from qgis.core import (
