823b17d27f31000ef50cb023c5023718a04fe841
Jacky Volpes <jacky.volpes@oslandia.com>
2023-12-19 18:36:06 +0100

    Processing: Add test for Check Area algorithm

---
+++ tests/src/analysis/testqgsprocessingcheckgeometry.cpp
@@ -0,0 +1,121 @@
+/***************************************************************************
+                         testqgsprocessingcheckgeometry.cpp
+                         ---------------------
+    begin                : December 2023
+    copyright            : (C) 2023 by Jacky Volpes
+    email                : jacky dot volpes at oslandia dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "qgstest.h"
+#include "qgsprocessingregistry.h"
+#include "qgsprocessingutils.h"
+#include "qgsprocessingalgorithm.h"
+#include "qgsnativealgorithms.h"
+#include "qgsvectorlayer.h"
+#include "qgscategorizedsymbolrenderer.h"
+#include "qgsrasteranalysisutils.h"
+#include "qgsrasteranalysisutils.cpp"
+#include "qgslayoutmanager.h"
+#include "qgspallabeling.h"
+#include "annotations/qgsannotationmanager.h"
+#include "qgsvectorlayerlabeling.h"
+#include "qgsbookmarkmanager.h"
+#include "qgsexpressioncontextutils.h"
+#include "qgsrelationmanager.h"
+#include <qtestcase.h>
+
+class TestQgsProcessingCheckGeometry: public QgsTest
+{
+    Q_OBJECT
+
+  public:
+    TestQgsProcessingCheckGeometry() : QgsTest( QStringLiteral( "Processing Algorithms Check Geometry" ) ) {}
+
+  private slots:
+    void initTestCase();// will be called before the first testfunction is executed.
+    void cleanupTestCase(); // will be called after the last testfunction was executed.
+    void init() {} // will be called before each testfunction is executed.
+    void cleanup() {} // will be called after every testfunction.
+    void areaAlg();
+    // void multipartAlg();
+    // void dangleAlg();
+
+  private:
+
+    QgsVectorLayer *mLineLayer = nullptr;
+    QgsVectorLayer *mPolygonLayer = nullptr;
+
+    void exportToSpreadsheet( const QString &outputPath );
+};
+
+void TestQgsProcessingCheckGeometry::initTestCase()
+{
+  QgsApplication::init();
+  QgsApplication::initQgis();
+
+  // Set up the QgsSettings environment
+  QCoreApplication::setOrganizationName( QStringLiteral( "QGIS" ) );
+  QCoreApplication::setOrganizationDomain( QStringLiteral( "qgis.org" ) );
+  QCoreApplication::setApplicationName( QStringLiteral( "QGIS-TEST" ) );
+
+  QgsApplication::processingRegistry()->addProvider( new QgsNativeAlgorithms( QgsApplication::processingRegistry() ) );
+
+  const QDir testDataDir( QDir( TEST_DATA_DIR ).absoluteFilePath( "geometry_checker" ) );
+
+  //create a line layer that will be used in all tests
+  mLineLayer = new QgsVectorLayer( testDataDir.absoluteFilePath( "line_layer.shp" ), QStringLiteral( "lines" ), QStringLiteral( "ogr" ) );
+  // Register the layer with the registry
+  QgsProject::instance()->addMapLayers( QList<QgsMapLayer *>() << mLineLayer );
+  QVERIFY( mLineLayer->isValid() );
+
+  //create a poly layer that will be used in all tests
+  mPolygonLayer = new QgsVectorLayer( testDataDir.absoluteFilePath( "polygon_layer.shp" ), QStringLiteral( "polygons" ), QStringLiteral( "ogr" ) );
+  // Register the layer with the registry
+  QgsProject::instance()->addMapLayers( QList<QgsMapLayer *>() << mPolygonLayer );
+  QVERIFY( mPolygonLayer->isValid() );
+}
+
+void TestQgsProcessingCheckGeometry::cleanupTestCase()
+{
+  QgsApplication::exitQgis();
+}
+
+void TestQgsProcessingCheckGeometry::areaAlg()
+{
+  std::unique_ptr< QgsProcessingAlgorithm > alg(
+    QgsApplication::processingRegistry()->createAlgorithmById( QStringLiteral( "native:checkgeometryarea" ) )
+  );
+  QVERIFY( alg != nullptr );
+
+  QVariantMap parameters;
+  parameters.insert( QStringLiteral( "INPUT" ), QVariant::fromValue( mPolygonLayer ) );
+  parameters.insert( QStringLiteral( "AREATHRESHOLD" ), 0.04 );
+  parameters.insert( QStringLiteral( "OUTPUT" ), QgsProcessing::TEMPORARY_OUTPUT );
+  parameters.insert( QStringLiteral( "ERRORS" ), QgsProcessing::TEMPORARY_OUTPUT );
+
+  bool ok = false;
+  QgsProcessingFeedback feedback;
+  std::unique_ptr< QgsProcessingContext > context = std::make_unique< QgsProcessingContext >();
+
+  QVariantMap results;
+  results = alg->run( parameters, *context, &feedback, &ok );
+  QVERIFY( ok );
+
+  std::unique_ptr<QgsVectorLayer> outputLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "OUTPUT" ) ).toString() ) ) );
+  std::unique_ptr<QgsVectorLayer> errorsLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "ERRORS" ) ).toString() ) ) );
+  QVERIFY( outputLayer->isValid() );
+  QVERIFY( errorsLayer->isValid() );
+  QCOMPARE( outputLayer->featureCount(), 8 );
+  QCOMPARE( errorsLayer->featureCount(), 8 );
+}
+
+QGSTEST_MAIN( TestQgsProcessingCheckGeometry )
+#include "testqgsprocessingcheckgeometry.moc"
