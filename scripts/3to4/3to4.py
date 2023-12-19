69bc9a1bd313216a37fda283d81b6a3d57626a8a
Julien Cabieces <julien.cabieces@oslandia.com>
2023-12-19 14:52:19 +0100

    [PyQt6] unscoppe enums

--- scripts/3to4/3to4.py
+++ scripts/3to4/3to4.py
@@ -43,12 +43,15 @@ import argparse
 import ast
 import glob
 import os
+import inspect
 
 from collections import defaultdict
 
 from tokenize_rt import Offset, src_to_tokens, tokens_to_src, reversed_enumerate, Token
 
 from typing import Sequence
+
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 qmetatype_mapping = {"String": "QString",
                      "Invalid": "UnknownType",
@@ -59,58 +62,36 @@ qmetatype_mapping = {"String": "QString",
                      "StringList": "QStringList"
                      }
 
-# key: class that has moved from one module to another
-# value: (qt5 module, qt6 module)
-moved_classes = {"QAction": ("QtWidgets", "QtGui")}
-
-NEW_LINE = Token('NEWLINE', '\n')
-SPACE = Token('UNIMPORTANT_WS', ' ')
-COMMA = Token('OP', ',')
-POINT = Token('OP', '.')
-
-
-def import_tokens(module, classes):
-    classes_str = ",".join(classes)
-    return src_to_tokens(f"from qgis.PyQt.{module} import {classes_str}")
+qt_enums = {}
 
 
 def fix_file(filename: str, args: argparse.Namespace) -> int:
 
-    if (filename != "../python/plugins/processing/gui/BatchPanel.py"):
-        return 0
+    # if (filename != "/home/julien/work/QGIS/.worktrees/pyqt6/build-fedora/output/python/plugins/processing/algs/qgis/QgisAlgorithmProvider.py"):
+    #     return 0
 
     with (open(filename, encoding='UTF-8') as f):
         contents = f.read()
 
     fix_qvariant_type = [] # QVariant.Int, QVariant.Double ...
     fix_pyqt_import = [] # from PyQt5.QtXXX
-    fix_moved_classes = [] # classes changing from modules
-
-    new_import_qt5 = defaultdict(list)
-    new_import_qt6 = defaultdict(list)
-
-    last_import = None
+    fix_qt_enums = [] # Unscopping of enums
 
     tree = ast.parse(contents, filename=filename)
     for node in ast.walk(tree):
 
-        if (isinstance(node, ast.ImportFrom)):
-            last_import = Offset(node.end_lineno, node.end_col_offset)
-
-        if (isinstance(node, ast.ImportFrom) and node.module in ["PyQt5.QtWidgets", "qgis.PyQt.QtWidgets", "PyQt5.QtGui", "qgis.PyQt.QtGui"]):
-            for name in node.names:
-                if name.name in moved_classes:
-                    fix_moved_classes.append(Offset(name.lineno, name.col_offset))
-                    new_import_qt5[moved_classes[name.name][0]].append(name.name)
-                    new_import_qt6[moved_classes[name.name][1]].append(name.name)
-
         if (isinstance(node, ast.Attribute) and isinstance(node.value, ast.Name)
                 and node.value.id == "QVariant"):
             fix_qvariant_type.append(Offset(node.lineno, node.col_offset))
-        elif (isinstance(node, ast.ImportFrom) and node.module.startswith("PyQt5.")):
+
+        if (isinstance(node, ast.Attribute) and isinstance(node.value, ast.Name)
+                and (node.value.id, node.attr) in qt_enums):
+            fix_qt_enums.append(Offset(node.lineno, node.col_offset))
+
+        elif (isinstance(node, ast.ImportFrom) and node.module and node.module.startswith("PyQt5.")):
             fix_pyqt_import.append(Offset(node.lineno, node.col_offset))
 
-    if not fix_qvariant_type and not fix_pyqt_import and not fix_moved_classes:
+    if not fix_qvariant_type and not fix_pyqt_import and not fix_qt_enums:
         return 0
 
     tokens = src_to_tokens(contents)
@@ -127,35 +108,29 @@ def fix_file(filename: str, args: argparse.Namespace) -> int:
             assert tokens[i + 2].src == "PyQt5"
             tokens[i + 2] = tokens[i + 2]._replace(src="qgis.PyQt")
 
-        if token.offset in fix_moved_classes:
-            # TODO We need to deal with case where we have removed all imported classes
-
-            tokens.pop(i)
-            while tokens[i].src == "," or tokens[i].name in ['UNIMPORTANT_WS', 'NEWLINE', 'NL']:
-                tokens.pop(i)
-
-            # TODO Ugly, do better
-            if tokens[i - 1].src == '    ':
-                tokens.pop(i - 1)
-
-        if token.offset == last_import:
-
-            new_tokens = [NEW_LINE] * 2 + import_tokens("QtCore", ["QT_VERSION"])
-            new_tokens += [NEW_LINE] + src_to_tokens("if (QT_VERSION >= 0x060000):") + [NEW_LINE]
-            for module, classes in new_import_qt6.items():
-                new_tokens += [Token('INDENT', '    ')] + import_tokens(module, classes)
-            new_tokens += [NEW_LINE] + src_to_tokens("else:") + [NEW_LINE]
-            for module, classes in new_import_qt5.items():
-                new_tokens += [Token('INDENT', '    ')] + import_tokens(module, classes)
-
-            for tok in reversed(new_tokens):
-                tokens.insert(i, tok)
+        if token.offset in fix_qt_enums:
+            assert tokens[i + 1].src == "."
+            enum_name = qt_enums[(tokens[i].src, tokens[i + 2].src)]
+            assert enum_name
+            tokens[i + 2] = tokens[i + 2]._replace(src=f"{enum_name}.{tokens[i+2].src}")
 
     new_contents = tokens_to_src(tokens)
     with (open(filename, 'w') as f):
         f.write(new_contents)
 
     return new_contents != contents
+
+
+def get_class_enums(item):
+    if not inspect.isclass(item):
+        return
+
+    for key, value in item.__dict__.items():
+        if inspect.isclass(value) and type(value).__name__ == 'EnumType':
+            for e in value:
+                qt_enums[(item.__name__, e.name)] = f"{value.__name__}"
+        elif inspect.isclass(value):
+            get_class_enums(value)
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -168,8 +143,12 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     print(os.path.join(args.directory, "*.py"))
 
+    for module in QtCore, QtGui, QtWidgets:
+        for key, value in module.__dict__.items():
+            get_class_enums(value)
+
     ret = 0
-    for filename in glob.glob(os.path.join(args.directory, "*.py")):
+    for filename in glob.glob(os.path.join(args.directory, "**/*.py"), recursive=True):
         ret |= fix_file(filename, args)
     return ret
 
