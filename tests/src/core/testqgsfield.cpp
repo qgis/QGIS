7328f0e127bc75ae6174b9c86981906c0202ba08
CodeBardian <codebardian@gmail.com>
2023-12-30 12:39:14 +0100

    Add `editorWidgetSetup` to the copy constructor of `QgsFieldPrivate`

--- tests/src/core/testqgsfield.cpp
+++ tests/src/core/testqgsfield.cpp
@@ -104,8 +104,16 @@ void TestQgsField::copy()
   original.setReadOnly( true );
   original.setSplitPolicy( Qgis::FieldDomainSplitPolicy::GeometryRatio );
   original.setMetadata( {{ 1, QStringLiteral( "abc" )}, {2, 5 }} );
+
+  QVariantMap config;
+  config.insert( QStringLiteral( "a" ), "value_a" );
+  const QgsEditorWidgetSetup setup( QStringLiteral( "test" ), config );
+  original.setEditorWidgetSetup( setup );
+
   QgsField copy( original );
   QVERIFY( copy == original );
+  QCOMPARE( copy.editorWidgetSetup().type(), original.editorWidgetSetup().type() );
+  QCOMPARE( copy.editorWidgetSetup().config(), original.editorWidgetSetup().config() );
 
   copy.setName( QStringLiteral( "copy" ) );
   QCOMPARE( original.name(), QString( "original" ) );
@@ -976,8 +984,17 @@ void TestQgsField::editorWidgetSetup()
   const QgsEditorWidgetSetup setup( QStringLiteral( "test" ), config );
   field.setEditorWidgetSetup( setup );
 
+  const QgsField otherField = field;
+
   QCOMPARE( field.editorWidgetSetup().type(), setup.type() );
   QCOMPARE( field.editorWidgetSetup().config(), setup.config() );
+  // trigger copy-on-write with unrelated method call when private pointer is referenced more than once
+  field.setName( QStringLiteral( "original" ) );
+  // verify that editorWidgetSetup still remains
+  QCOMPARE( field.editorWidgetSetup().type(), setup.type() );
+  QCOMPARE( field.editorWidgetSetup().config(), setup.config() );
+  QCOMPARE( otherField.editorWidgetSetup().type(), setup.type() );
+  QCOMPARE( otherField.editorWidgetSetup().config(), setup.config() );
 }
 
 void TestQgsField::collection()
