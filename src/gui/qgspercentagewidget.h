f188bba8d000d15bda83e2bf10c9feaa9be13535
Nyall Dawson <nyall.dawson@gmail.com>
2024-01-04 08:09:58 +1000

    Add a simple widget for setting percentage values via spin and slider

---
+++ src/gui/qgspercentagewidget.h
@@ -0,0 +1,82 @@
+/***************************************************************************
+    qgspercentagewidget.h
+     -----------------
+    Date                 : January 2024
+    Copyright            : (C) 2024 Nyall Dawson
+    Email                : nyall.dawson@gmail.com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSPERCENTAGEWIDGET_H
+#define QGSPERCENTAGEWIDGET_H
+
+#include <QWidget>
+#include "qgis_sip.h"
+#include "qgis_gui.h"
+
+class QgsDoubleSpinBox;
+class QSlider;
+
+/**
+ * \class QgsPercentageWidget
+ * \ingroup gui
+ * \brief A widget for setting a percentage value.
+ * \since QGIS 3.36
+ */
+class GUI_EXPORT QgsPercentageWidget : public QWidget
+{
+    Q_OBJECT
+    Q_PROPERTY( double value READ value WRITE setValue NOTIFY valueChanged )
+
+  public:
+
+    /**
+     * Constructor for QgsPercentageWidget.
+     */
+    explicit QgsPercentageWidget( QWidget *parent SIP_TRANSFERTHIS = nullptr );
+
+    /**
+     * Returns the current percentage selected in the widget, as a factor from 0.0 to 1.0.
+     * \see setValue()
+     * \see valueChanged()
+     */
+    double value() const;
+
+  public slots:
+
+    /**
+     * Sets the current \a value to show in the widget, where \a value is a factor which ranges from 0.0 to 1.0.
+     *
+     * \see value()
+     * \see valueChanged()
+     */
+    void setValue( double value );
+
+  signals:
+
+    /**
+     * Emitted when the \a value is changed in the widget, where \a value is a factor which ranges from 0.0 to 1.0.
+     *
+     * \see setValue()
+     * \see value()
+     */
+    void valueChanged( double value );
+
+  private slots:
+
+    void spinChanged( double value );
+
+  private:
+
+    QgsDoubleSpinBox *mSpinBox = nullptr;
+    QSlider *mSlider = nullptr;
+
+};
+
+#endif // QGSPERCENTAGEWIDGET_H
---
+++ tests/src/python/test_qgspercentagewidget.py
@@ -0,0 +1,51 @@
+"""QGIS Unit tests for QgsPercentageWidget
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+
+
+from qgis.PyQt.QtTest import QSignalSpy
+from qgis.gui import QgsPercentageWidget
+import unittest
+from qgis.testing import start_app, QgisTestCase
+
+start_app()
+
+
+class TestQgsPercentageWidget(QgisTestCase):
+
+    def testGettersSetters(self):
+        """ test widget getters/setters """
+        w = QgsPercentageWidget()
+
+        w.setValue(0.2)
+        self.assertEqual(w.value(), 0.2)
+
+        # bad values
+        w.setValue(-0.2)
+        self.assertEqual(w.value(), 0.0)
+        w.setValue(100)
+        self.assertEqual(w.value(), 1.0)
+
+    def test_ChangedSignals(self):
+        """ test that signals are correctly emitted when setting value"""
+
+        w = QgsPercentageWidget()
+
+        spy = QSignalSpy(w.valueChanged)
+        w.setValue(0.2)
+
+        self.assertEqual(len(spy), 1)
+        self.assertEqual(spy[0][0], 0.2)
+
+        # bad value
+        w.setValue(100)
+        self.assertEqual(len(spy), 2)
+        self.assertEqual(spy[1][0], 1.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
