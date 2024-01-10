aa50c0c40e85a2265edecc679f5ee1d72fc2cc9e
Nyall Dawson <nyall.dawson@gmail.com>
2024-01-10 14:37:29 +1000

    Add file source widget for OGR provider

--- src/gui/providers/ogr/qgsogrguiprovider.cpp
+++ src/gui/providers/ogr/qgsogrguiprovider.cpp
@@ -24,12 +24,14 @@
 #include "qgsogrsourceselect.h"
 #include "qgsogrdbsourceselect.h"
 #include "qgsapplication.h"
-#include "qgsprovidermetadata.h"
 #include "qgsdataitemguiprovider.h"
 #include "qgsgeopackageitemguiprovider.h"
 #include "qgsogritemguiprovider.h"
 #include "qgsgeopackageprojectstorageguiprovider.h"
 #include "qgsprojectstorageguiprovider.h"
+#include "qgsprovidersourcewidgetprovider.h"
+#include "qgsogrfilesourcewidget.h"
+#include "qgsmaplayer.h"
 
 static const QString TEXT_PROVIDER_KEY = QStringLiteral( "ogr" );
 
@@ -59,6 +61,15 @@ class QgsGeoPackageSourceSelectProvider : public QgsSourceSelectProvider
     QIcon icon() const override { return QgsApplication::getThemeIcon( QStringLiteral( "/mActionAddGeoPackageLayer.svg" ) ); }
     QgsAbstractDataSourceWidget *createDataSourceWidget( QWidget *parent = nullptr, Qt::WindowFlags fl = Qt::Widget, QgsProviderRegistry::WidgetMode widgetMode = QgsProviderRegistry::WidgetMode::Embedded ) const override;
     QgsSourceSelectProvider::Capabilities capabilities() override;
+};
+
+class QgsOgrSourceWidgetProvider : public QgsProviderSourceWidgetProvider
+{
+  public:
+    QgsOgrSourceWidgetProvider();
+    QString providerKey() const override;
+    bool canHandleLayer( QgsMapLayer *layer ) const override;
+    QgsProviderSourceWidget *createWidget( QgsMapLayer *layer, QWidget *parent = nullptr ) override;
 };
 
 
@@ -101,6 +112,49 @@ QgsSourceSelectProvider::Capabilities QgsOgrVectorSourceSelectProvider::capabili
   return QgsSourceSelectProvider::Capability::ConfigureFromUri;
 }
 
+
+//
+// QgsOgrSourceWidgetProvider
+//
+
+QgsOgrSourceWidgetProvider::QgsOgrSourceWidgetProvider()
+{
+
+}
+
+QString QgsOgrSourceWidgetProvider::providerKey() const
+{
+  return QStringLiteral( "ogr" );
+}
+
+bool QgsOgrSourceWidgetProvider::canHandleLayer( QgsMapLayer *layer ) const
+{
+  if ( layer->providerType() != QLatin1String( "ogr" ) )
+    return false;
+
+  const QVariantMap parts = QgsProviderRegistry::instance()->decodeUri( QStringLiteral( "ogr" ), layer->source() );
+  if ( parts.value( QStringLiteral( "path" ) ).toString().isEmpty() )
+    return false;
+
+  return true;
+}
+
+QgsProviderSourceWidget *QgsOgrSourceWidgetProvider::createWidget( QgsMapLayer *layer, QWidget *parent )
+{
+  if ( layer->providerType() != QLatin1String( "ogr" ) )
+    return nullptr;
+
+  const QVariantMap parts = QgsProviderRegistry::instance()->decodeUri( QStringLiteral( "ogr" ), layer->source() );
+  if ( parts.value( QStringLiteral( "path" ) ).toString().isEmpty() )
+    return nullptr;
+
+  return new QgsOgrFileSourceWidget( parent );
+}
+
+//
+// QgsOgrGuiProviderMetadata
+//
+
 QgsOgrGuiProviderMetadata::QgsOgrGuiProviderMetadata()
   : QgsProviderGuiMetadata( TEXT_PROVIDER_KEY )
 {
@@ -131,7 +185,13 @@ QList<QgsProjectStorageGuiProvider *> QgsOgrGuiProviderMetadata::projectStorageG
   QList<QgsProjectStorageGuiProvider *> providers;
   providers << new QgsGeoPackageProjectStorageGuiProvider();
   return providers;
+}
 
+QList<QgsProviderSourceWidgetProvider *> QgsOgrGuiProviderMetadata::sourceWidgetProviders()
+{
+  QList<QgsProviderSourceWidgetProvider *> providers;
+  providers << new QgsOgrSourceWidgetProvider();
+  return providers;
 }
 
 ///@endcond
