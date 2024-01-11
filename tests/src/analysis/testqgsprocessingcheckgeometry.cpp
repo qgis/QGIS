20cca4de104b346a1563ed7d7071703bcf2c41bb
Jacky Volpes <jacky.volpes@oslandia.com>
2024-01-11 17:08:20 +0100

    Processing: Add Check Geometry algorithm and test: Self intersection

--- tests/src/analysis/testqgsprocessingcheckgeometry.cpp
+++ tests/src/analysis/testqgsprocessingcheckgeometry.cpp
@@ -37,6 +37,9 @@ class TestQgsProcessingCheckGeometry : public QgsTest
     void angleAlg_data();
     void angleAlg();
 
+    void selfIntersectionAlg_data();
+    void selfIntersectionAlg();
+
     void areaAlg();
     void holeAlg();
     void missingVertexAlg();
@@ -217,6 +220,44 @@ void TestQgsProcessingCheckGeometry::missingVertexAlg()
   QVERIFY( errorsLayer->isValid() );
   QCOMPARE( outputLayer->featureCount(), 5 );
   QCOMPARE( errorsLayer->featureCount(), 5 );
+}
+
+void TestQgsProcessingCheckGeometry::selfIntersectionAlg_data()
+{
+  QTest::addColumn<QgsVectorLayer *>( "layerToTest" );
+  QTest::addColumn<int>( "expectedErrorCount" );
+  QTest::newRow( "Line layer" ) << mLineLayer << 3;
+  QTest::newRow( "Polygon layer" ) << mPolygonLayer << 2;
+}
+
+void TestQgsProcessingCheckGeometry::selfIntersectionAlg()
+{
+  QFETCH( QgsVectorLayer *, layerToTest );
+  QFETCH( int, expectedErrorCount );
+  std::unique_ptr< QgsProcessingAlgorithm > alg(
+    QgsApplication::processingRegistry()->createAlgorithmById( QStringLiteral( "native:checkgeometryselfintersection" ) )
+  );
+  QVERIFY( alg != nullptr );
+
+  QVariantMap parameters;
+  parameters.insert( QStringLiteral( "INPUT" ), QVariant::fromValue( layerToTest ) );
+  parameters.insert( QStringLiteral( "OUTPUT" ), QgsProcessing::TEMPORARY_OUTPUT );
+  parameters.insert( QStringLiteral( "ERRORS" ), QgsProcessing::TEMPORARY_OUTPUT );
+
+  bool ok = false;
+  QgsProcessingFeedback feedback;
+  std::unique_ptr< QgsProcessingContext > context = std::make_unique< QgsProcessingContext >();
+
+  QVariantMap results;
+  results = alg->run( parameters, *context, &feedback, &ok );
+  QVERIFY( ok );
+
+  std::unique_ptr<QgsVectorLayer> outputLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "OUTPUT" ) ).toString() ) ) );
+  std::unique_ptr<QgsVectorLayer> errorsLayer( qobject_cast< QgsVectorLayer * >( context->getMapLayer( results.value( QStringLiteral( "ERRORS" ) ).toString() ) ) );
+  QVERIFY( outputLayer->isValid() );
+  QVERIFY( errorsLayer->isValid() );
+  QCOMPARE( outputLayer->featureCount(), expectedErrorCount );
+  QCOMPARE( errorsLayer->featureCount(), expectedErrorCount );
 }
 
 QGSTEST_MAIN( TestQgsProcessingCheckGeometry )
