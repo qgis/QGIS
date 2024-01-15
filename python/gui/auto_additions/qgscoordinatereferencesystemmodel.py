afb2d8d31c0d78df1f2170eefc7e5b49df26493b
Nyall Dawson <nyall.dawson@gmail.com>
2024-01-15 14:04:02 +1000

    Move all projection related gui files to src/gui/proj

--- python/gui/auto_additions/qgscoordinatereferencesystemmodel.py
+++ python/gui/auto_additions/qgscoordinatereferencesystemmodel.py
@@ -1,3 +1,3 @@
-# The following has been generated automatically from src/gui/qgscoordinatereferencesystemmodel.h
+# The following has been generated automatically from src/gui/proj/qgscoordinatereferencesystemmodel.h
 QgsCoordinateReferenceSystemProxyModel.Filters.baseClass = QgsCoordinateReferenceSystemProxyModel
 Filters = QgsCoordinateReferenceSystemProxyModel  # dirty hack since SIP seems to introduce the flags in module
