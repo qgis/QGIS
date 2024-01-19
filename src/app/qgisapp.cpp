d4ae590109bbf00f8c3e9ffc56f1119ca738ac9a
Juho Ervasti <juho.ervasti@gispo.fi>
2024-01-19 18:16:05 +0200

    Set project dirty when editing symbol with double click

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -693,6 +693,7 @@ void QgisApp::layerTreeViewDoubleClicked( const QModelIndex &index )
           if ( dlg.exec() )
           {
             node->setSymbol( symbol.release() );
+            markDirty();
           }
 
           return;
