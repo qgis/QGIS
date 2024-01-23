d820861322b4e92c77776c6b23192d0932d03cc6
Juho Ervasti <juho.ervasti@gispo.fi>
2024-01-23 08:54:50 +0200

    Remove reference to QgsProject::instance()

--- src/core/layertree/qgslayertreemodellegendnode.cpp
+++ src/core/layertree/qgslayertreemodellegendnode.cpp
@@ -635,7 +635,8 @@ bool QgsSymbolLegendNode::setData( const QVariant &value, int role )
 
   vlayer->renderer()->checkLegendSymbolItem( mItem.ruleKey(), value == Qt::Checked );
 
-  QgsProject::instance()->setDirty( true );
+  if ( QgsProject* project = vlayer->project() )
+     project->setDirty( true );
 
   emit dataChanged();
   vlayer->emitStyleChanged();
