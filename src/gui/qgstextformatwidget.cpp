qgis/QGIS

commit aeafbd47f4b263a85e65f0edeaca7216246e803b

Fix test

src/gui/qgstextformatwidget.cpp
@@ -868,12 +868,11 @@ void QgsTextFormatWidget::populateDataDefinedButtons()
 
 void QgsTextFormatWidget::registerDataDefinedButton( QgsPropertyOverrideButton *button, QgsPalLayerSettings::Property key )
 {
-  if ( mLayer->type() !=  Qgis::LayerType::Vector )
-  {
-    return;
-  }
-
   QgsVectorLayer *vLayer = qobject_cast< QgsVectorLayer * >( mLayer );
+
+  if ( !vLayer )
+    return;
+
   button->init( key, mDataDefinedProperties, QgsPalLayerSettings::propertyDefinitions(), vLayer, true );
   if ( !mButtons.contains( key ) )
   {
@@ -1842,12 +1841,11 @@ void QgsTextFormatWidget::createAuxiliaryField()
   if ( !mLayer )
     return;
 
-  if ( mLayer->type() != Qgis::LayerType::Vector )
-  {
-    return;
-  }
-
   QgsVectorLayer *vLayer = qobject_cast< QgsVectorLayer * >( mLayer );
+
+  if ( !vLayer )
+    return;
+
   // try to create an auxiliary layer if not yet created
   if ( !vLayer->auxiliaryLayer() )
   {
@@ -2153,8 +2151,8 @@ Qgis::GeometryType QgsTextFormatWidget::labelGeometryType() const
 {
   if ( mGeometryGeneratorGroupBox->isChecked() )
     return mGeometryGeneratorType->currentData().value<Qgis::GeometryType>();
-  else if ( mLayer && mLayer->type() == Qgis::LayerType::Vector )
-    return qobject_cast< QgsVectorLayer * >( mLayer )->geometryType();
+  else if ( QgsVectorLayer *vLayer = qobject_cast< QgsVectorLayer * >( mLayer ) )
+    return vLayer->geometryType();
   else
     return mGeomType;
 }

commit b0d3c7dca85eb07ddd4b2e64f59510461586acd5

Fix broken data defined override buttons in text format widget when no layer is associated
Regression from https://github.com/qgis/QGIS/pull/55786

src/gui/qgstextformatwidget.cpp
@@ -870,9 +870,6 @@ void QgsTextFormatWidget::registerDataDefinedButton( QgsPropertyOverrideButton *
 {
   QgsVectorLayer *vLayer = qobject_cast< QgsVectorLayer * >( mLayer );
 
-  if ( !vLayer )
-    return;
-
   button->init( key, mDataDefinedProperties, QgsPalLayerSettings::propertyDefinitions(), vLayer, true );
   if ( !mButtons.contains( key ) )
   {