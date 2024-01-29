0a01a0953bff58f9fe162842a08449e6522cda63
Even Rouault <even.rouault@spatialys.com>
2024-01-29 13:32:41 +0100

    [OAPIF] Improve a bit performance of /items response parsing
    Helps improving #56059

--- src/providers/wfs/oapif/qgsoapifitemsrequest.cpp
+++ src/providers/wfs/oapif/qgsoapifitemsrequest.cpp
@@ -38,6 +38,7 @@ QgsOapifItemsRequest::QgsOapifItemsRequest( const QgsDataSourceUri &baseUri, con
 
 bool QgsOapifItemsRequest::request( bool synchronous, bool forceRefresh )
 {
+  QgsDebugMsgLevel( QStringLiteral( " QgsOapifItemsRequest::request() start time: %1" ).arg( time( nullptr ) ), 5 );
   if ( !sendGET( QUrl::fromEncoded( mUrl.toLatin1() ), QString( "application/geo+json, application/json" ), synchronous, forceRefresh ) )
   {
     emit gotResponse();
@@ -51,14 +52,54 @@ QString QgsOapifItemsRequest::errorMessageWithReason( const QString &reason )
   return tr( "Download of items failed: %1" ).arg( reason );
 }
 
+// Remove extraneous indentation spaces from a JSON buffer
+static void removeUselessSpacesFromJSONBuffer( QByteArray &buffer )
+{
+  int j = 0;
+  bool inString = false;
+  const int bufferInitialSize = buffer.size();
+  char *ptr = buffer.data();
+  for ( int i = 0; i < bufferInitialSize; ++i )
+  {
+    const char ch = ptr[i];
+    if ( inString )
+    {
+      if ( ch == '"' )
+      {
+        inString = false;
+      }
+      else if ( ch == '\\' && i + 1 < bufferInitialSize && ptr[i + 1] == '"' )
+      {
+        ptr[j++] = ch;
+        ++i;
+      }
+    }
+    else
+    {
+      if ( ch == '"' )
+      {
+        inString = true;
+      }
+      else if ( ch == ' ' )
+      {
+        // strip spaces outside strings
+        continue;
+      }
+    }
+    ptr[j++] = ch;
+  }
+  buffer.resize( j );
+}
+
 void QgsOapifItemsRequest::processReply()
 {
+  QgsDebugMsgLevel( QStringLiteral( "processReply start time: %1" ).arg( time( nullptr ) ), 5 );
   if ( mErrorCode != QgsBaseNetworkRequest::NoError )
   {
     emit gotResponse();
     return;
   }
-  const QByteArray &buffer = mResponse;
+  QByteArray &buffer = mResponse;
   if ( buffer.isEmpty() )
   {
     mErrorMessage = tr( "empty response" );
@@ -67,21 +108,20 @@ void QgsOapifItemsRequest::processReply()
     return;
   }
 
-  QgsDebugMsgLevel( QStringLiteral( "parsing items response: " ) + buffer, 4 );
-
-  QTextCodec::ConverterState state;
-  QTextCodec *codec = QTextCodec::codecForName( "UTF-8" );
-  Q_ASSERT( codec );
-
-  const QString utf8Text = codec->toUnicode( buffer.constData(), buffer.size(), &state );
-  if ( state.invalidChars != 0 )
+  if ( buffer.size() <= 200 )
   {
-    mErrorCode = QgsBaseNetworkRequest::ApplicationLevelError;
-    mAppLevelError = ApplicationLevelError::JsonError;
-    mErrorMessage = errorMessageWithReason( tr( "Invalid UTF-8 content" ) );
-    emit gotResponse();
-    return;
+    QgsDebugMsgLevel( QStringLiteral( "parsing items response: " ) + buffer, 4 );
   }
+  else
+  {
+    QgsDebugMsgLevel( QStringLiteral( "parsing items response: " ) + buffer.left( 100 ) + QStringLiteral( "[... snip ...]" ) + buffer.right( 100 ), 4 );
+  }
+
+  // Remove extraneous indentation spaces from the string. This helps a bit
+  // improving JSON parsing performance afterwards
+  QgsDebugMsgLevel( QStringLiteral( "JSON compaction start time: %1" ).arg( time( nullptr ) ), 5 );
+  removeUselessSpacesFromJSONBuffer( buffer );
+  QgsDebugMsgLevel( QStringLiteral( "JSON compaction end time: %1" ).arg( time( nullptr ) ), 5 );
 
   const QString vsimemFilename = QStringLiteral( "/vsimem/oaipf_%1.json" ).arg( reinterpret_cast< quintptr >( &buffer ), QT_POINTER_SIZE * 2, 16, QLatin1Char( '0' ) );
   VSIFCloseL( VSIFileFromMemBuffer( vsimemFilename.toUtf8().constData(),
@@ -90,8 +130,10 @@ void QgsOapifItemsRequest::processReply()
                                     false ) );
   QgsProviderRegistry *pReg = QgsProviderRegistry::instance();
   const QgsDataProvider::ProviderOptions providerOptions;
+  QgsDebugMsgLevel( QStringLiteral( "OGR data source open start time: %1" ).arg( time( nullptr ) ), 5 );
   auto vectorProvider = std::unique_ptr<QgsVectorDataProvider>(
                           qobject_cast< QgsVectorDataProvider * >( pReg->createProvider( "ogr", vsimemFilename, providerOptions ) ) );
+  QgsDebugMsgLevel( QStringLiteral( "OGR data source open end time: %1" ).arg( time( nullptr ) ), 5 );
   if ( !vectorProvider || !vectorProvider->isValid() )
   {
     VSIUnlink( vsimemFilename.toUtf8().constData() );
@@ -108,6 +150,7 @@ void QgsOapifItemsRequest::processReply()
   {
     mBbox = vectorProvider->extent();
   }
+  QgsDebugMsgLevel( QStringLiteral( "OGR feature iteration start time: %1" ).arg( time( nullptr ) ), 5 );
   auto iter = vectorProvider->getFeatures();
   while ( true )
   {
@@ -116,15 +159,18 @@ void QgsOapifItemsRequest::processReply()
       break;
     mFeatures.push_back( QgsFeatureUniqueIdPair( f, QString() ) );
   }
+  QgsDebugMsgLevel( QStringLiteral( "OGR feature iteration end time: %1" ).arg( time( nullptr ) ), 5 );
   vectorProvider.reset();
   VSIUnlink( vsimemFilename.toUtf8().constData() );
 
   try
   {
-    const json j = json::parse( utf8Text.toStdString() );
+    QgsDebugMsgLevel( QStringLiteral( "json::parse() start time: %1" ).arg( time( nullptr ) ), 5 );
+    const json j = json::parse( buffer.constData(), buffer.constData()  + buffer.size() );
+    QgsDebugMsgLevel( QStringLiteral( "json::parse() end time: %1" ).arg( time( nullptr ) ), 5 );
     if ( j.is_object() && j.contains( "features" ) )
     {
-      const json features = j["features"];
+      const json &features = j["features"];
       if ( features.is_array() && features.size() == mFeatures.size() )
       {
         for ( size_t i = 0; i < features.size(); i++ )
@@ -132,7 +178,7 @@ void QgsOapifItemsRequest::processReply()
           const json &jFeature = features[i];
           if ( jFeature.is_object() && jFeature.contains( "id" ) )
           {
-            const json id = jFeature["id"];
+            const json &id = jFeature["id"];
             mFoundIdTopLevel = true;
             if ( id.is_string() )
             {
@@ -145,7 +191,7 @@ void QgsOapifItemsRequest::processReply()
           }
           if ( jFeature.is_object() && jFeature.contains( "properties" ) )
           {
-            const json properties = jFeature["properties"];
+            const json &properties = jFeature["properties"];
             if ( properties.is_object() && properties.contains( "id" ) )
             {
               mFoundIdInProperties = true;
@@ -178,5 +224,6 @@ void QgsOapifItemsRequest::processReply()
     return;
   }
 
+  QgsDebugMsgLevel( QStringLiteral( "processReply end time: %1" ).arg( time( nullptr ) ), 5 );
   emit gotResponse();
 }
