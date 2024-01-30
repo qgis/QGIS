b85db241c717f13a74ebed2138461e4c5df65464
Nyall Dawson <nyall.dawson@gmail.com>
2024-01-30 14:56:09 +1000

    Sipify

--- python/PyQt6/core/auto_additions/qgsdxfexport.py
+++ python/PyQt6/core/auto_additions/qgsdxfexport.py
@@ -42,9 +42,6 @@ QgsDxfExport.Flag.__bool__ = lambda flag: bool(_force_int(flag))
 QgsDxfExport.Flag.__eq__ = lambda flag1, flag2: _force_int(flag1) == _force_int(flag2)
 QgsDxfExport.Flag.__and__ = lambda flag1, flag2: _force_int(flag1) & _force_int(flag2)
 QgsDxfExport.Flag.__or__ = lambda flag1, flag2: QgsDxfExport.Flag(_force_int(flag1) | _force_int(flag2))
-def _force_int(v): return v if isinstance(v, int) else int(v.value)
-
-
 QgsDxfExport.DxfPolylineFlag.__bool__ = lambda flag: bool(_force_int(flag))
 QgsDxfExport.DxfPolylineFlag.__eq__ = lambda flag1, flag2: _force_int(flag1) == _force_int(flag2)
 QgsDxfExport.DxfPolylineFlag.__and__ = lambda flag1, flag2: _force_int(flag1) & _force_int(flag2)
--- python/PyQt6/core/auto_additions/qgsprovidermetadata.py
+++ python/PyQt6/core/auto_additions/qgsprovidermetadata.py
@@ -23,16 +23,10 @@ QgsProviderMetadata.FileBasedUris = QgsProviderMetadata.ProviderCapability.FileB
 QgsProviderMetadata.SaveLayerMetadata = QgsProviderMetadata.ProviderCapability.SaveLayerMetadata
 QgsProviderMetadata.ParallelCreateProvider = QgsProviderMetadata.ProviderCapability.ParallelCreateProvider
 QgsProviderMetadata.ProviderCapabilities = lambda flags=0: QgsProviderMetadata.ProviderCapability(flags)
-def _force_int(v): return v if isinstance(v, int) else int(v.value)
-
-
 QgsProviderMetadata.ProviderMetadataCapability.__bool__ = lambda flag: bool(_force_int(flag))
 QgsProviderMetadata.ProviderMetadataCapability.__eq__ = lambda flag1, flag2: _force_int(flag1) == _force_int(flag2)
 QgsProviderMetadata.ProviderMetadataCapability.__and__ = lambda flag1, flag2: _force_int(flag1) & _force_int(flag2)
 QgsProviderMetadata.ProviderMetadataCapability.__or__ = lambda flag1, flag2: QgsProviderMetadata.ProviderMetadataCapability(_force_int(flag1) | _force_int(flag2))
-def _force_int(v): return v if isinstance(v, int) else int(v.value)
-
-
 QgsProviderMetadata.ProviderCapability.__bool__ = lambda flag: bool(_force_int(flag))
 QgsProviderMetadata.ProviderCapability.__eq__ = lambda flag1, flag2: _force_int(flag1) == _force_int(flag2)
 QgsProviderMetadata.ProviderCapability.__and__ = lambda flag1, flag2: _force_int(flag1) & _force_int(flag2)
