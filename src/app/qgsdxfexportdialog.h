9c80e78ad2a92b664b907c471f10a3ed9ac58327
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-01-30 15:20:29 +0700

    [ui] Improve visibility of the output layer attribute in the DXF export dialog

--- src/app/qgsdxfexportdialog.h
+++ src/app/qgsdxfexportdialog.h
@@ -74,6 +74,15 @@ class QgsVectorLayerAndAttributeModel : public QgsLayerTreeModel
     void retrieveAllLayers( QgsLayerTreeNode *node, QSet<QString> &layers );
 };
 
+class QgsDxfExportLayerTreeView : public QgsLayerTreeView
+{
+    Q_OBJECT
+  public:
+    explicit QgsDxfExportLayerTreeView( QWidget *parent = nullptr );
+
+  protected:
+    void resizeEvent( QResizeEvent *event ) override;
+};
 
 class QgsDxfExportDialog : public QDialog, private Ui::QgsDxfExportDialogBase
 {
@@ -112,6 +121,7 @@ class QgsDxfExportDialog : public QDialog, private Ui::QgsDxfExportDialogBase
     QgsLayerTree *mLayerTreeGroup = nullptr;
     FieldSelectorDelegate *mFieldSelectorDelegate = nullptr;
     QgsVectorLayerAndAttributeModel *mModel = nullptr;
+    QgsDxfExportLayerTreeView *mTreeView = nullptr;
 
     QgsCoordinateReferenceSystem mCRS;
 };
