1229ba9fb6adb9af264f3998a59c8f6c97671abe
Nyall Dawson <nyall.dawson@gmail.com>
2024-02-05 14:05:14 +1000

    Resync internal poly2tri library

--- external/poly2tri/sweep/advancing_front.cc
+++ external/poly2tri/sweep/advancing_front.cc
@@ -36,75 +36,75 @@ namespace p2t {
 
 AdvancingFront::AdvancingFront(Node& head, Node& tail)
 {
-  head_ = &head;
-  tail_ = &tail;
-  search_node_ = &head;
+    head_ = &head;
+    tail_ = &tail;
+    search_node_ = &head;
 }
 
 Node* AdvancingFront::LocateNode(double x)
 {
-  Node* node = search_node_;
+    Node* node = search_node_;
 
-  if (x < node->value) {
-    while ((node = node->prev) != nullptr) {
-      if (x >= node->value) {
-        search_node_ = node;
-        return node;
-      }
+    if (x < node->value) {
+        while ((node = node->prev) != nullptr) {
+            if (x >= node->value) {
+                search_node_ = node;
+                return node;
+            }
+        }
+    } else {
+        while ((node = node->next) != nullptr) {
+            if (x < node->value) {
+                search_node_ = node->prev;
+                return node->prev;
+            }
+        }
     }
-  } else {
-    while ((node = node->next) != nullptr) {
-      if (x < node->value) {
-        search_node_ = node->prev;
-        return node->prev;
-      }
-    }
-  }
-  return nullptr;
+    return nullptr;
 }
 
 Node* AdvancingFront::FindSearchNode(double x)
 {
-  (void)x; // suppress compiler warnings "unused parameter 'x'"
-  // TODO: implement BST index
-  return search_node_;
+    (void)x; // suppress compiler warnings "unused parameter 'x'"
+    // TODO: implement BST index
+    return search_node_;
 }
 
 Node* AdvancingFront::LocatePoint(const Point* point)
 {
-  const double px = point->x;
-  Node* node = FindSearchNode(px);
-  const double nx = node->point->x;
+    const double px = point->x;
+    Node* node = FindSearchNode(px);
+    const double nx = node->point->x;
 
-  if (px == nx) {
-    if (point != node->point) {
-      // We might have two nodes with same x value for a short time
-      if (point == node->prev->point) {
-        node = node->prev;
-      } else if (point == node->next->point) {
-        node = node->next;
-      } else {
-        assert(0);
-      }
+    if (px == nx) {
+        if (point != node->point) {
+            // We might have two nodes with same x value for a short time
+            if (point == node->prev->point) {
+                node = node->prev;
+            } else if (point == node->next->point) {
+                node = node->next;
+            } else {
+                assert(0);
+            }
+        }
+    } else if (px < nx) {
+        while ((node = node->prev) != nullptr) {
+            if (point == node->point) {
+                break;
+            }
+        }
+    } else {
+        while ((node = node->next) != nullptr) {
+            if (point == node->point)
+                break;
+        }
     }
-  } else if (px < nx) {
-    while ((node = node->prev) != nullptr) {
-      if (point == node->point) {
-        break;
-      }
-    }
-  } else {
-    while ((node = node->next) != nullptr) {
-      if (point == node->point)
-        break;
-    }
-  }
-  if(node) search_node_ = node;
-  return node;
+    if(node) search_node_ = node;
+    return node;
 }
 
 AdvancingFront::~AdvancingFront()
 {
 }
 
-}
+} // namespace p2t
--- external/poly2tri/sweep/sweep_context.cc
+++ external/poly2tri/sweep/sweep_context.cc
@@ -1,5 +1,5 @@
 /*
- * Poly2Tri Copyright (c) 2009-2018, Poly2Tri Contributors
+ * Poly2Tri Copyright (c) 2009-2022, Poly2Tri Contributors
  * https://github.com/jhasse/poly2tri
  *
  * All rights reserved.
@@ -34,152 +34,152 @@
 
 namespace p2t {
 
-SweepContext::SweepContext(const std::vector<Point*>& polyline) : points_(polyline),
-  front_(nullptr),
-  head_(nullptr),
-  tail_(nullptr),
-  af_head_(nullptr),
-  af_middle_(nullptr),
-  af_tail_(nullptr)
+SweepContext::SweepContext(std::vector<Point*> polyline) : points_(std::move(polyline)),
+    front_(nullptr),
+    head_(nullptr),
+    tail_(nullptr),
+    af_head_(nullptr),
+    af_middle_(nullptr),
+    af_tail_(nullptr)
 {
-  InitEdges(points_);
+    InitEdges(points_);
 }
 
 void SweepContext::AddHole(const std::vector<Point*>& polyline)
 {
-  InitEdges(polyline);
-  for(unsigned int i = 0; i < polyline.size(); i++) {
-    points_.push_back(polyline[i]);
-  }
+    InitEdges(polyline);
+    for (auto i : polyline) {
+        points_.push_back(i);
+    }
 }
 
 void SweepContext::AddPoint(Point* point) {
-  points_.push_back(point);
+    points_.push_back(point);
 }
 
 std::vector<Triangle*> &SweepContext::GetTriangles()
 {
-  return triangles_;
+    return triangles_;
 }
 
 std::list<Triangle*> &SweepContext::GetMap()
 {
-  return map_;
+    return map_;
 }
 
 void SweepContext::InitTriangulation()
 {
-  double xmax(points_[0]->x), xmin(points_[0]->x);
-  double ymax(points_[0]->y), ymin(points_[0]->y);
+    double xmax(points_[0]->x), xmin(points_[0]->x);
+    double ymax(points_[0]->y), ymin(points_[0]->y);
 
-  // Calculate bounds.
-  for (unsigned int i = 0; i < points_.size(); i++) {
-    Point& p = *points_[i];
-    if (p.x > xmax)
-      xmax = p.x;
-    if (p.x < xmin)
-      xmin = p.x;
-    if (p.y > ymax)
-      ymax = p.y;
-    if (p.y < ymin)
-      ymin = p.y;
-  }
+    // Calculate bounds.
+    for (auto& point : points_) {
+        Point& p = *point;
+        if (p.x > xmax)
+            xmax = p.x;
+        if (p.x < xmin)
+            xmin = p.x;
+        if (p.y > ymax)
+            ymax = p.y;
+        if (p.y < ymin)
+            ymin = p.y;
+    }
 
-  double dx = kAlpha * (xmax - xmin);
-  double dy = kAlpha * (ymax - ymin);
-  head_ = new Point(xmin - dx, ymin - dy);
-  tail_ = new Point(xmax + dx, ymin - dy);
+    double dx = kAlpha * (xmax - xmin);
+    double dy = kAlpha * (ymax - ymin);
+    head_ = new Point(xmin - dx, ymin - dy);
+    tail_ = new Point(xmax + dx, ymin - dy);
 
-  // Sort points along y-axis
-  std::sort(points_.begin(), points_.end(), cmp);
+    // Sort points along y-axis
+    std::sort(points_.begin(), points_.end(), cmp);
 
 }
 
 void SweepContext::InitEdges(const std::vector<Point*>& polyline)
 {
-  size_t num_points = polyline.size();
-  for (size_t i = 0; i < num_points; i++) {
-    size_t j = i < num_points - 1 ? i + 1 : 0;
-    edge_list.push_back(new Edge(*polyline[i], *polyline[j]));
-  }
+    size_t num_points = polyline.size();
+    for (size_t i = 0; i < num_points; i++) {
+        size_t j = i < num_points - 1 ? i + 1 : 0;
+        edge_list.push_back(new Edge(*polyline[i], *polyline[j]));
+    }
 }
 
 Point* SweepContext::GetPoint(size_t index)
 {
-  return points_[index];
+    return points_[index];
 }
 
 void SweepContext::AddToMap(Triangle* triangle)
 {
-  map_.push_back(triangle);
+    map_.push_back(triangle);
 }
 
 Node* SweepContext::LocateNode(const Point& point)
 {
-  // TODO implement search tree
-  return front_->LocateNode(point.x);
+    // TODO implement search tree
+    return front_->LocateNode(point.x);
 }
 
 void SweepContext::CreateAdvancingFront()
 {
 
-  // Initial triangle
-  Triangle* triangle = new Triangle(*points_[0], *head_, *tail_);
+    // Initial triangle
+    Triangle* triangle = new Triangle(*points_[0], *head_, *tail_);
 
-  map_.push_back(triangle);
+    map_.push_back(triangle);
 
-  af_head_ = new Node(*triangle->GetPoint(1), *triangle);
-  af_middle_ = new Node(*triangle->GetPoint(0), *triangle);
-  af_tail_ = new Node(*triangle->GetPoint(2));
-  front_ = new AdvancingFront(*af_head_, *af_tail_);
+    af_head_ = new Node(*triangle->GetPoint(1), *triangle);
+    af_middle_ = new Node(*triangle->GetPoint(0), *triangle);
+    af_tail_ = new Node(*triangle->GetPoint(2));
+    front_ = new AdvancingFront(*af_head_, *af_tail_);
 
-  // TODO: More intuitive if head is middles next and not previous?
-  //       so swap head and tail
-  af_head_->next = af_middle_;
-  af_middle_->next = af_tail_;
-  af_middle_->prev = af_head_;
-  af_tail_->prev = af_middle_;
+    // TODO: More intuitive if head is middles next and not previous?
+    //       so swap head and tail
+    af_head_->next = af_middle_;
+    af_middle_->next = af_tail_;
+    af_middle_->prev = af_head_;
+    af_tail_->prev = af_middle_;
 }
 
 void SweepContext::RemoveNode(Node* node)
 {
-  delete node;
+    delete node;
 }
 
 void SweepContext::MapTriangleToNodes(Triangle& t)
 {
-  for (int i = 0; i < 3; i++) {
-    if (!t.GetNeighbor(i)) {
-      Node* n = front_->LocatePoint(t.PointCW(*t.GetPoint(i)));
-      if (n)
-        n->triangle = &t;
+    for (int i = 0; i < 3; i++) {
+        if (!t.GetNeighbor(i)) {
+            Node* n = front_->LocatePoint(t.PointCW(*t.GetPoint(i)));
+            if (n)
+                n->triangle = &t;
+        }
     }
-  }
 }
 
 void SweepContext::RemoveFromMap(Triangle* triangle)
 {
-  map_.remove(triangle);
+    map_.remove(triangle);
 }
 
 void SweepContext::MeshClean(Triangle& triangle)
 {
-  std::vector<Triangle *> triangles;
-  triangles.push_back(&triangle);
+    std::vector<Triangle *> triangles;
+    triangles.push_back(&triangle);
 
-  while(!triangles.empty()){
-	Triangle *t = triangles.back();
-	triangles.pop_back();
+    while(!triangles.empty()){
+        Triangle *t = triangles.back();
+        triangles.pop_back();
 
-    if (t != nullptr && !t->IsInterior()) {
-      t->IsInterior(true);
-      triangles_.push_back(t);
-      for (int i = 0; i < 3; i++) {
-        if (!t->constrained_edge[i])
-          triangles.push_back(t->GetNeighbor(i));
-      }
+        if (t != nullptr && !t->IsInterior()) {
+            t->IsInterior(true);
+            triangles_.push_back(t);
+            for (int i = 0; i < 3; i++) {
+                if (!t->constrained_edge[i])
+                    triangles.push_back(t->GetNeighbor(i));
+            }
+        }
     }
-  }
 }
 
 SweepContext::~SweepContext()
@@ -194,17 +194,13 @@ SweepContext::~SweepContext()
     delete af_middle_;
     delete af_tail_;
 
-    typedef std::list<Triangle*> type_list;
-
-    for(type_list::iterator iter = map_.begin(); iter != map_.end(); ++iter) {
-        Triangle* ptr = *iter;
+    for (auto ptr : map_) {
         delete ptr;
     }
 
-     for(unsigned int i = 0; i < edge_list.size(); i++) {
-        delete edge_list[i];
+    for (auto& i : edge_list) {
+        delete i;
     }
-
 }
 
 } // namespace p2t
