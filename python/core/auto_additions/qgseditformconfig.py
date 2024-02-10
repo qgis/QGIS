87f117f47b103bf7af35185001642c4fe7aed157
Nyall Dawson <nyall.dawson@gmail.com>
2024-02-11 09:45:44 +1000

    [sipify] Avoid leading and trailing newlines in enum value docstrings

--- python/core/auto_additions/qgseditformconfig.py
+++ python/core/auto_additions/qgseditformconfig.py
@@ -11,6 +11,6 @@ QgsEditFormConfig.Alias.is_monkey_patched = True
 QgsEditFormConfig.Alias.__doc__ = "Alias"
 QgsEditFormConfig.Editable = QgsEditFormConfig.DataDefinedProperty.Editable
 QgsEditFormConfig.Editable.is_monkey_patched = True
-QgsEditFormConfig.Editable.__doc__ = "Editable state \n.. versionadded:: 3.30\n"
+QgsEditFormConfig.Editable.__doc__ = "Editable state \n.. versionadded:: 3.30"
 QgsEditFormConfig.DataDefinedProperty.__doc__ = "Data defined properties.\nForm data defined overrides are stored in a property collection\nand they can be retrieved using the indexes specified in this\nenum.\n\n.. versionadded:: 3.14\n\n" + '* ``NoProperty``: ' + QgsEditFormConfig.DataDefinedProperty.NoProperty.__doc__ + '\n' + '* ``AllProperties``: ' + QgsEditFormConfig.DataDefinedProperty.AllProperties.__doc__ + '\n' + '* ``Alias``: ' + QgsEditFormConfig.DataDefinedProperty.Alias.__doc__ + '\n' + '* ``Editable``: ' + QgsEditFormConfig.DataDefinedProperty.Editable.__doc__
 # --
