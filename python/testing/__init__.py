4931f8ebe43ac97a181fcd9a7fa1bcf45071e07a
Nyall Dawson <nyall.dawson@gmail.com>
2024-02-12 11:25:03 +1000

    Gracefully handle NULL/not-NULL comparisons in QgisTestCase.checkLayersEqual
    We always want NULL values sorted first, and never want to try
    to directly compare NULL/not NULL values.
    
    Doing so raises an exception on the PyQt6 builds, where NULL === None
    and comparisons between None and not-None values are prohibited
    in Python

--- python/testing/__init__.py
+++ python/testing/__init__.py
@@ -450,7 +450,7 @@ class QgisTestCase(unittest.TestCase):
 
             return True
 
-        def sort_by_pk_or_fid(f):
+        def get_pk_or_fid(f):
             if 'pk' in kwargs and kwargs['pk'] is not None:
                 key = kwargs['pk']
                 if isinstance(key, list) or isinstance(key, tuple):
@@ -459,6 +459,14 @@ class QgisTestCase(unittest.TestCase):
                     return f[kwargs['pk']]
             else:
                 return f.id()
+
+        def sort_by_pk_or_fid(f):
+            pk = get_pk_or_fid(f)
+            # we want NULL values sorted first, and don't want to try to
+            # directly compare NULL against non-NULL values
+            if isinstance(pk, list):
+                pk = [(v == NULL, v) for v in pk]
+            return (pk == NULL, pk)
 
         expected_features = sorted(layer_expected.getFeatures(request), key=sort_by_pk_or_fid)
         result_features = sorted(layer_result.getFeatures(request), key=sort_by_pk_or_fid)
