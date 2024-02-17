2c2e20d693cc191c2e240473ccad737c30b9070e
uclaros <uclaros@gmail.com>
2024-02-17 23:23:24 +0200

    Update point cloud layer 3d renderer when Follow 2d symbology is enabled and a class is toggled in the layer tree

--- src/core/layertree/qgslayertreemodellegendnode.cpp
+++ src/core/layertree/qgslayertreemodellegendnode.cpp
@@ -1144,6 +1144,9 @@ bool QgsRasterSymbolLegendNode::setData( const QVariant &value, int role )
     pclayer->emitStyleChanged();
 
     pclayer->triggerRepaint();
+    if ( pclayer->sync3DRendererTo2DRenderer() )
+      pclayer->convertRenderer3DFromRenderer2D();
+
     return true;
   }
   else
