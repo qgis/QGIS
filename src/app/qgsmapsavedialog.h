4819c8ab3ce20c18fc1a21f7efe96f5b974c1b8c
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-02-19 21:10:50 +0700

    Update src/app/qgsmapsavedialog.h

--- src/app/qgsmapsavedialog.h
+++ src/app/qgsmapsavedialog.h
@@ -108,7 +108,7 @@ class APP_EXPORT QgsMapSaveDialog: public QDialog, private Ui::QgsMapSaveDialog
     QgsRectangle mExtent;
     int mDpi;
     QSize mSize;
-    double mDevicePixelRatio;
+    float mDevicePixelRatio;
 
     QString mInfoDetails;
 
