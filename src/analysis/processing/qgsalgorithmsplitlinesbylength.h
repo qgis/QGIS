a738296d6c79fddcbf450a2ed5c82bba52753e58
Nyall Dawson <nyall.dawson@gmail.com>
2024-02-23 09:18:32 +1000

    Regenerate primary keys for Split Lines by Length outputs
    Fixes #56486

--- src/analysis/processing/qgsalgorithmsplitlinesbylength.h
+++ src/analysis/processing/qgsalgorithmsplitlinesbylength.h
@@ -52,6 +52,7 @@ class QgsSplitLinesByLengthAlgorithm : public QgsProcessingFeatureBasedAlgorithm
     Qgis::WkbType outputWkbType( Qgis::WkbType inputWkbType ) const override;
     QgsFeatureList processFeature( const QgsFeature &feature,  QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
     Qgis::ProcessingFeatureSourceFlags sourceFlags() const override;
+    QgsFeatureSink::SinkFlags sinkFlags() const override;
 
   private:
 
