12ee2e4f7e976b83fb7a22bc17d41fb8e43f7543
Nyall Dawson <nyall.dawson@gmail.com>
2024-02-28 13:21:01 +1000

    Consistently return invalid QVariant for QgsVectorDataProvider::defaultValue
    Previously we mixed invalid QVariant values with null QVariants,
    resulting in a mix of results between Qt 5 and Qt 6 and across
    different data providers

--- src/core/providers/ogr/qgsogrprovider.cpp
+++ src/core/providers/ogr/qgsogrprovider.cpp
@@ -1411,8 +1411,8 @@ QVariant QgsOgrProvider::defaultValue( int fieldId ) const
     }
   }
 
-  ( void )mAttributeFields.at( fieldId ).convertCompatible( resultVar );
-  return resultVar;
+  const bool compatible = mAttributeFields.at( fieldId ).convertCompatible( resultVar );
+  return compatible && !QgsVariantUtils::isNull( resultVar ) ? resultVar : QVariant();
 }
 
 QString QgsOgrProvider::defaultValueClause( int fieldIndex ) const
