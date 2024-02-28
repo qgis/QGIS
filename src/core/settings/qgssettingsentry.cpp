9a46ec8acaadba2e730f39e90c9252779dfe0f03
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-28 11:24:07 +0100

    do not copy setting value if equals to default

--- src/core/settings/qgssettingsentry.cpp
+++ src/core/settings/qgssettingsentry.cpp
@@ -240,12 +240,15 @@ bool QgsSettingsEntryBase::copyValueFromKey( const QString &key, const QStringLi
 
   if ( settings.contains( oldCompleteKey ) )
   {
-    QVariant oldValue = settings.value( oldCompleteKey, mDefaultValue );
-    setVariantValue( oldValue, dynamicKeyPartList );
+    QVariant oldValue = settings->value( oldCompleteKey, mDefaultValue );
+    // do not copy if it is equal to the default value
+    if ( oldValue != defaultValueAsVariant() )
+      setVariantValue( oldValue, dynamicKeyPartList );
     if ( removeSettingAtKey )
       settings.remove( oldCompleteKey );
     return true;
   }
+
   return false;
 }
 
