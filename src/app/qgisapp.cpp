8fa05b0fdff9036a443994dd9e687c02d9d9ad99
Mathieu Pellerin <nirvn.asia@gmail.com>
2024-03-08 15:49:54 +0700

    [app] Fix zoom to [raster] layer native resolution on high DPI screens

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -12191,7 +12191,7 @@ void QgisApp::legendLayerZoomNative()
     }
     else
     {
-      mMapCanvas->zoomByFactor( std::sqrt( layer->rasterUnitsPerPixelX() * layer->rasterUnitsPerPixelX() + layer->rasterUnitsPerPixelY() * layer->rasterUnitsPerPixelY() ) / diagonalSize );
+      mMapCanvas->zoomByFactor( std::sqrt( layer->rasterUnitsPerPixelX() * layer->rasterUnitsPerPixelX() + layer->rasterUnitsPerPixelY() * layer->rasterUnitsPerPixelY() ) / ( diagonalSize / mMapCanvas->mapSettings().devicePixelRatio() ) );
     }
 
     mMapCanvas->refresh();
