88326eb4b16f36bbab83b64367409823dadfadec
Nyall Dawson <nyall.dawson@gmail.com>
2024-03-11 11:40:01 +1000

    Rework QgsGpsDetector to make it memory safe
    This is messy, as there's no way we can possibly make the current,
    stable API of this class safe. We have to resort to an opt-in
    "safe" mode which exposes a non-dangerous API.
    
    This should hopefully fix issues where the qt event loop causes
    destruction of the detected connection before listener slot is
    called and is able to take ownership of the signal argument...

--- src/core/gps/qgsgpsdetector.h
+++ src/core/gps/qgsgpsdetector.h
@@ -47,7 +47,22 @@ class CORE_EXPORT QgsGpsDetector : public QObject
 {
     Q_OBJECT
   public:
-    QgsGpsDetector( const QString &portName );
+
+    // TODO QGIS 4.0 -- remove useUnsafeSignals option
+
+    /**
+     * Constructor for QgsGpsDetector.
+     *
+     * If \a portName is specified, then only devices from the given port will be scanned. Otherwise
+     * all connection types will be attempted (including internal GPS devices).
+     *
+     * Since QGIS 3.38, the \a useUnsafeSignals parameter can be set to FALSE to avoid emitting the
+     * dangerous and fragile detected() signal. This is highly recommended, but is opt-in to avoid
+     * breaking stable QGIS 3.x API. If \a useUnsafeSignals is set to FALSE, only the safe connectionDetected() signal
+     * will be emitted and clients must manually take ownership of the detected connection via a call
+     * to takeConnection().
+     */
+    QgsGpsDetector( const QString &portName = QString(), bool useUnsafeSignals = true );
 
 #if defined( HAVE_QTSERIALPORT )
     static const QgsSettingsEntryEnumFlag<QSerialPort::StopBits> *settingsGpsStopBits SIP_SKIP;
@@ -58,6 +73,20 @@ class CORE_EXPORT QgsGpsDetector : public QObject
 
     ~QgsGpsDetector() override;
 
+    /**
+     * Returns the detected GPS connection, and removes it from the detector.
+     *
+     * The caller takes ownership of the connection. Only the first call to this
+     * method following a connectionDetected() signal will be able to retrieve the
+     * detected connection -- subsequent calls will return NULLPTR.
+     *
+     * \warning Do not call this method if the useUnsafeSignals option in the
+     * QgsGpsDetector constructor was set to TRUE.
+     *
+     * \since QGIS 3.38
+     */
+    QgsGpsConnection *takeConnection() SIP_TRANSFERBACK;
+
     static QList< QPair<QString, QString> > availablePorts();
 
   public slots:
@@ -67,15 +96,26 @@ class CORE_EXPORT QgsGpsDetector : public QObject
 
   signals:
 
-    // TODO QGIS 4.0 - this is horrible, fragile, leaky and crash prone API.
-    // don't transfer ownership with this signal, and add an explicit takeConnection member!
+    /**
+     * Emitted when a GPS connection is successfully detected.
+     *
+     * Call takeConnection() to take ownership of the detected connection.
+     *
+     * \since QGIS 3.38
+     */
+    void connectionDetected();
 
     /**
      * Emitted when the GPS connection has been detected. A single connection must listen for this signal and
      * immediately take ownership of the \a connection object.
+     *
+     * \deprecated This signal is dangerous and extremely unsafe! It is recommended to instead set the \a useUnsafeSignals parameter to FALSE in the QgsGpsDetector constructor and use the safe connectionDetected() signal instead.
      */
-    void detected( QgsGpsConnection *connection );
+    Q_DECL_DEPRECATED void detected( QgsGpsConnection *connection ) SIP_DEPRECATED;
 
+    /**
+     * Emitted when the detector could not find a valid GPS connection.
+     */
     void detectionFailed();
 
   private slots:
@@ -83,6 +123,7 @@ class CORE_EXPORT QgsGpsDetector : public QObject
     void connectionTimeout();
 
   private:
+    bool mUseUnsafeSignals = true;
     int mPortIndex = 0;
     int mBaudIndex = -1;
     QList< QPair< QString, QString > > mPortList;
