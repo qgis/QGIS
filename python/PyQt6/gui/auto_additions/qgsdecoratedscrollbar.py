f8dd821248f30ab65a1df8e044a5ece0440a0994
Nyall Dawson <nyall.dawson@gmail.com>
2024-05-08 11:26:43 +1000

    Port decorated scrollbar widget class from QtCreator
    Allows decorating scrollbars with colored highlight bars

---
+++ python/PyQt6/gui/auto_additions/qgsdecoratedscrollbar.py
@@ -0,0 +1,9 @@
+# The following has been generated automatically from src/gui/qgsdecoratedscrollbar.h
+# monkey patching scoped based enum
+QgsScrollBarHighlight.Priority.Invalid.__doc__ = "Invalid"
+QgsScrollBarHighlight.Priority.LowPriority.__doc__ = "Low priority, rendered below all other highlights"
+QgsScrollBarHighlight.Priority.NormalPriority.__doc__ = "Normal priority"
+QgsScrollBarHighlight.Priority.HighPriority.__doc__ = "High priority"
+QgsScrollBarHighlight.Priority.HighestPriority.__doc__ = "Highest priority, rendered above all other highlights"
+QgsScrollBarHighlight.Priority.__doc__ = "Priority, which dictates how overlapping highlights are rendered\n\n" + '* ``Invalid``: ' + QgsScrollBarHighlight.Priority.Invalid.__doc__ + '\n' + '* ``LowPriority``: ' + QgsScrollBarHighlight.Priority.LowPriority.__doc__ + '\n' + '* ``NormalPriority``: ' + QgsScrollBarHighlight.Priority.NormalPriority.__doc__ + '\n' + '* ``HighPriority``: ' + QgsScrollBarHighlight.Priority.HighPriority.__doc__ + '\n' + '* ``HighestPriority``: ' + QgsScrollBarHighlight.Priority.HighestPriority.__doc__
+# --
