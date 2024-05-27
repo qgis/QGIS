e6d7379c5da604481bb6bd82e5da43abd0fa8e64
Jean Felder <jean.felder@oslandia.com>
2024-05-27 14:53:50 +0200

    qgsgoochmaterialsettings: Merge datadefined and constant cases
    This is similar to the previous QgsPhongMaterialSettings change:
    
    This commit makes two changes to `QgsGoochMaterialSettings`:
    1. It merges `dataDefinedMaterial()` and `constantColorMaterial` into
    one new function: `buildMaterial`.
    
    2. There is one fragment shader file called `gooch.frag`. This is
    achieved by a `#define` logic. If `DATA_DEFINED` is defined, this is
    the dataDefined case. Otherwise, this is the constant case.

--- src/3d/materials/qgsgoochmaterialsettings.h
+++ src/3d/materials/qgsgoochmaterialsettings.h
@@ -134,7 +134,7 @@ class _3D_EXPORT QgsGoochMaterialSettings : public QgsAbstractMaterialSettings
     float mBeta = 0.5f;
 
     //! Constructs a material from shader files
-    Qt3DRender::QMaterial *dataDefinedMaterial() const;
+    Qt3DRender::QMaterial *buildMaterial( const QgsMaterialContext &context ) const;
 };
 
 
