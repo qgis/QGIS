bc413d42832f44f84d8f3b8cf096d747c8df63b1
Nyall Dawson <nyall.dawson@gmail.com>
2024-06-01 10:35:04 +1000

    Lint

--- tests/src/python/test_qgsmultipolygon.py
+++ tests/src/python/test_qgsmultipolygon.py
@@ -24,26 +24,29 @@ class TestQgsMultiPolygon(QgisTestCase):
         p = QgsMultiPolygon([])
         self.assertTrue(p.isEmpty())
 
-        value = QgsPolygon(QgsLineString([[1,2 ], [10,2 ], [10, 10], [1,2 ]]))
+        value = QgsPolygon(QgsLineString([[1, 2], [10, 2], [10, 10], [1, 2]]))
         p = QgsMultiPolygon([value])
         self.assertEqual(p.asWkt(), 'MultiPolygon (((1 2, 10 2, 10 10, 1 2)))')
         # constructor should have made internal copy
         del value
         self.assertEqual(p.asWkt(), 'MultiPolygon (((1 2, 10 2, 10 10, 1 2)))')
 
-        p = QgsMultiPolygon([QgsPolygon(QgsLineString([[1,2 ], [10,2 ], [10, 10], [1,2 ]])),
-                             QgsPolygon(QgsLineString([[100,2 ], [110,2 ], [110, 10], [100,2 ]]))])
+        p = QgsMultiPolygon([QgsPolygon(QgsLineString([[1, 2], [10, 2], [10, 10], [1, 2]])),
+                             QgsPolygon(QgsLineString([[100, 2], [110, 2], [110, 10], [100, 2]]))])
         self.assertEqual(p.asWkt(), 'MultiPolygon (((1 2, 10 2, 10 10, 1 2)),((100 2, 110 2, 110 10, 100 2)))')
 
         # with z
-        p = QgsMultiPolygon([QgsPolygon(QgsLineString([[1,2 , 3], [10,2 , 3], [10, 10, 3], [1,2 , 3]])),
-                             QgsPolygon(QgsLineString([[100,2 ,4 ], [110,2 , 4], [110, 10, 4], [100,2 , 4]]))])
-        self.assertEqual(p.asWkt(), 'MultiPolygonZ (((1 2 3, 10 2 3, 10 10 3, 1 2 3)),((100 2 4, 110 2 4, 110 10 4, 100 2 4)))')
+        p = QgsMultiPolygon([QgsPolygon(QgsLineString([[1, 2, 3], [10, 2, 3], [10, 10, 3], [1, 2, 3]])),
+                             QgsPolygon(QgsLineString([[100, 2, 4], [110, 2, 4], [110, 10, 4], [100, 2, 4]]))])
+        self.assertEqual(p.asWkt(),
+                         'MultiPolygonZ (((1 2 3, 10 2 3, 10 10 3, 1 2 3)),((100 2 4, 110 2 4, 110 10 4, 100 2 4)))')
 
         # with zm
-        p = QgsMultiPolygon([QgsPolygon(QgsLineString([[1,2 , 3, 5], [10,2 , 3, 5], [10, 10, 3, 5], [1,2 , 3, 5]])),
-                             QgsPolygon(QgsLineString([[100,2 ,4 , 6], [110,2 , 4, 6], [110, 10, 4, 6], [100,2 , 4, 6]]))])
-        self.assertEqual(p.asWkt(), 'MultiPolygonZM (((1 2 3 5, 10 2 3 5, 10 10 3 5, 1 2 3 5)),((100 2 4 6, 110 2 4 6, 110 10 4 6, 100 2 4 6)))')
+        p = QgsMultiPolygon([QgsPolygon(QgsLineString([[1, 2, 3, 5], [10, 2, 3, 5], [10, 10, 3, 5], [1, 2, 3, 5]])),
+                             QgsPolygon(
+                                 QgsLineString([[100, 2, 4, 6], [110, 2, 4, 6], [110, 10, 4, 6], [100, 2, 4, 6]]))])
+        self.assertEqual(p.asWkt(),
+                         'MultiPolygonZM (((1 2 3 5, 10 2 3 5, 10 10 3 5, 1 2 3 5)),((100 2 4 6, 110 2 4 6, 110 10 4 6, 100 2 4 6)))')
 
     def testFuzzyComparisons(self):
         ######
@@ -54,16 +57,20 @@ class TestQgsMultiPolygon(QgisTestCase):
         geom2 = QgsMultiPolygon()
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0), QgsPoint(6.0, 5.0), QgsPoint(6.0, 6.0), QgsPoint(5.0, 5.0)]))
+        p1.setExteriorRing(
+            QgsLineString([QgsPoint(5.0, 5.0), QgsPoint(6.0, 5.0), QgsPoint(6.0, 6.0), QgsPoint(5.0, 5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0), QgsPoint(0.001, 0.001), QgsPoint(0.003, 0.003), QgsPoint(0.0, 0.0)]))
+        p2.setExteriorRing(
+            QgsLineString([QgsPoint(0.0, 0.0), QgsPoint(0.001, 0.001), QgsPoint(0.003, 0.003), QgsPoint(0.0, 0.0)]))
         self.assertTrue(geom1.addGeometry(p1))
         self.assertTrue(geom1.addGeometry(p2))
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0), QgsPoint(6.0, 5.0), QgsPoint(6.0, 6.0), QgsPoint(5.0, 5.0)]))
+        p1.setExteriorRing(
+            QgsLineString([QgsPoint(5.0, 5.0), QgsPoint(6.0, 5.0), QgsPoint(6.0, 6.0), QgsPoint(5.0, 5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0), QgsPoint(0.002, 0.002), QgsPoint(0.003, 0.003), QgsPoint(0.0, 0.0)]))
+        p2.setExteriorRing(
+            QgsLineString([QgsPoint(0.0, 0.0), QgsPoint(0.002, 0.002), QgsPoint(0.003, 0.003), QgsPoint(0.0, 0.0)]))
         self.assertTrue(geom2.addGeometry(p1))
         self.assertTrue(geom2.addGeometry(p2))
 
@@ -85,16 +92,22 @@ class TestQgsMultiPolygon(QgisTestCase):
         geom2 = QgsMultiPolygon()
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0), QgsPoint(5.0, 5.0, 5.0)]))
+        p1.setExteriorRing(QgsLineString(
+            [QgsPoint(5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0), QgsPoint(5.0, 5.0, 5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.001), QgsPoint(0.003, 0.003, 0.003), QgsPoint(0.0, 0.0, 0.0)]))
+        p2.setExteriorRing(QgsLineString(
+            [QgsPoint(0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.001), QgsPoint(0.003, 0.003, 0.003),
+             QgsPoint(0.0, 0.0, 0.0)]))
         self.assertTrue(geom1.addGeometry(p1))
         self.assertTrue(geom1.addGeometry(p2))
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0), QgsPoint(5.0, 5.0, 5.0)]))
+        p1.setExteriorRing(QgsLineString(
+            [QgsPoint(5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0), QgsPoint(5.0, 5.0, 5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.002), QgsPoint(0.003, 0.003, 0.003), QgsPoint(0.0, 0.0, 0.0)]))
+        p2.setExteriorRing(QgsLineString(
+            [QgsPoint(0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.002), QgsPoint(0.003, 0.003, 0.003),
+             QgsPoint(0.0, 0.0, 0.0)]))
         self.assertTrue(geom2.addGeometry(p1))
         self.assertTrue(geom2.addGeometry(p2))
 
@@ -116,16 +129,24 @@ class TestQgsMultiPolygon(QgisTestCase):
         geom2 = QgsMultiPolygon()
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0, m=5.0), QgsPoint(6.0, 5.0, m=5.0), QgsPoint(6.0, 6.0, m=5.0), QgsPoint(5.0, 5.0, m=5.0)]))
+        p1.setExteriorRing(QgsLineString(
+            [QgsPoint(5.0, 5.0, m=5.0), QgsPoint(6.0, 5.0, m=5.0), QgsPoint(6.0, 6.0, m=5.0),
+             QgsPoint(5.0, 5.0, m=5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0, m=0.0), QgsPoint(0.001, 0.001, m=0.001), QgsPoint(0.003, 0.003, m=0.003), QgsPoint(0.0, 0.0, m=0.0)]))
+        p2.setExteriorRing(QgsLineString(
+            [QgsPoint(0.0, 0.0, m=0.0), QgsPoint(0.001, 0.001, m=0.001), QgsPoint(0.003, 0.003, m=0.003),
+             QgsPoint(0.0, 0.0, m=0.0)]))
         self.assertTrue(geom1.addGeometry(p1))
         self.assertTrue(geom1.addGeometry(p2))
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0, m=5.0), QgsPoint(6.0, 5.0, m=5.0), QgsPoint(6.0, 6.0, m=5.0), QgsPoint(5.0, 5.0, m=5.0)]))
+        p1.setExteriorRing(QgsLineString(
+            [QgsPoint(5.0, 5.0, m=5.0), QgsPoint(6.0, 5.0, m=5.0), QgsPoint(6.0, 6.0, m=5.0),
+             QgsPoint(5.0, 5.0, m=5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0, m=0.0), QgsPoint(0.001, 0.001, m=0.002), QgsPoint(0.003, 0.003, m=0.003), QgsPoint(0.0, 0.0, m=0.0)]))
+        p2.setExteriorRing(QgsLineString(
+            [QgsPoint(0.0, 0.0, m=0.0), QgsPoint(0.001, 0.001, m=0.002), QgsPoint(0.003, 0.003, m=0.003),
+             QgsPoint(0.0, 0.0, m=0.0)]))
         self.assertTrue(geom2.addGeometry(p1))
         self.assertTrue(geom2.addGeometry(p2))
 
@@ -147,16 +168,24 @@ class TestQgsMultiPolygon(QgisTestCase):
         geom2 = QgsMultiPolygon()
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0, 5.0), QgsPoint(5.0, 5.0, 5.0, 5.0)]))
+        p1.setExteriorRing(QgsLineString(
+            [QgsPoint(5.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0, 5.0),
+             QgsPoint(5.0, 5.0, 5.0, 5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.001, 0.001), QgsPoint(0.003, 0.003, 0.003, 0.003), QgsPoint(0.0, 0.0, 0.0, 0.0)]))
+        p2.setExteriorRing(QgsLineString(
+            [QgsPoint(0.0, 0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.001, 0.001), QgsPoint(0.003, 0.003, 0.003, 0.003),
+             QgsPoint(0.0, 0.0, 0.0, 0.0)]))
         self.assertTrue(geom1.addGeometry(p1))
         self.assertTrue(geom1.addGeometry(p2))
 
         p1 = QgsPolygon()
-        p1.setExteriorRing(QgsLineString([QgsPoint(5.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0, 5.0), QgsPoint(5.0, 5.0, 5.0, 5.0)]))
+        p1.setExteriorRing(QgsLineString(
+            [QgsPoint(5.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 5.0, 5.0, 5.0), QgsPoint(6.0, 6.0, 5.0, 5.0),
+             QgsPoint(5.0, 5.0, 5.0, 5.0)]))
         p2 = QgsPolygon()
-        p2.setExteriorRing(QgsLineString([QgsPoint(0.0, 0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.002, 0.002), QgsPoint(0.003, 0.003, 0.003, 0.003), QgsPoint(0.0, 0.0, 0.0, 0.0)]))
+        p2.setExteriorRing(QgsLineString(
+            [QgsPoint(0.0, 0.0, 0.0, 0.0), QgsPoint(0.001, 0.001, 0.002, 0.002), QgsPoint(0.003, 0.003, 0.003, 0.003),
+             QgsPoint(0.0, 0.0, 0.0, 0.0)]))
         self.assertTrue(geom2.addGeometry(p1))
         self.assertTrue(geom2.addGeometry(p2))
 
