22e4124cad032a7a36b519227ebd286f5edb131e
Nyall Dawson <nyall.dawson@gmail.com>
2024-06-05 13:31:58 +1000

    Fix broken prefetch capabilities for raster layers
    We had a mismatch between QgsRasterDataProvider::providerCapabilities
    vs comparing to flags from QgsRasterInterface::capabilities.
    
    This was allowed because both are old style int enums, and really
    need to be upgraded to enum classes!

--- src/core/raster/qgsrasterlayerrenderer.h
+++ src/core/raster/qgsrasterlayerrenderer.h
@@ -84,6 +84,7 @@ class CORE_EXPORT QgsRasterLayerRenderer : public QgsMapLayerRenderer
     std::unique_ptr<QgsRasterPipe> mPipe;
 
     QgsRasterDataProvider::ProviderCapabilities mProviderCapabilities;
+    int mInterfaceCapabilities = 0;
 
     //! feedback class for cancellation and preview generation
     QgsRasterLayerRendererFeedback *mFeedback = nullptr;
