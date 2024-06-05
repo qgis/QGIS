c6ae7eb95b209e663a74163cf0d7584ed3deda38
Julien Cabieces <julien.cabieces@oslandia.com>
2024-06-05 19:13:36 +0200

    test(MaskUI): add tests to check selective masking widget behavior

--- src/gui/qgsmaskingwidget.h
+++ src/gui/qgsmaskingwidget.h
@@ -71,6 +71,7 @@ class GUI_EXPORT QgsMaskingWidget: public QgsPanelWidget, private Ui::QgsMasking
 
     QPointer<QgsMessageBarItem> mMessageBarItem;
     bool mMustPopulate = false;
+    friend class TestQgsMaskingWidget;
 };
 
 
