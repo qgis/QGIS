291d09227e3592ec529a4ca02a7a3a60d63f99cc
Nyall Dawson <nyall.dawson@gmail.com>
2024-06-10 12:30:39 +1000

    Add SIP_OUT

--- src/core/symbology/qgsgraduatedsymbolrenderer.h
+++ src/core/symbology/qgsgraduatedsymbolrenderer.h
@@ -234,7 +234,7 @@ class CORE_EXPORT QgsGraduatedSymbolRenderer : public QgsFeatureRenderer
      * \param nclasses the number of classes
      * \param error Optional parameter for error reporting
      */
-    void updateClasses( const QgsVectorLayer *vl, int nclasses, QString *error = nullptr );
+    void updateClasses( const QgsVectorLayer *vl, int nclasses, QString *error SIP_OUT = nullptr );
 
     Q_NOWARN_DEPRECATED_PUSH;
 
