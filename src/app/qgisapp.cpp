8d264ed6cd2cf95927fabd1de7b87294675d5e11
Andrea Giudiceandrea <andreaerdna@libero.it>
2024-06-18 16:05:16 +0200

    Add Help button to the Layer Diagram Properties dlg

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -8084,7 +8084,7 @@ void QgisApp::diagramProperties()
   layout->addWidget( gui );
 
   QDialogButtonBox *buttonBox = new QDialogButtonBox(
-    QDialogButtonBox::Ok | QDialogButtonBox::Cancel | QDialogButtonBox::Apply,
+    QDialogButtonBox::Help | QDialogButtonBox::Ok | QDialogButtonBox::Cancel | QDialogButtonBox::Apply,
     Qt::Horizontal, &dlg );
   layout->addWidget( buttonBox );
 
@@ -8096,6 +8096,10 @@ void QgisApp::diagramProperties()
            &dlg, &QDialog::reject );
   connect( buttonBox->button( QDialogButtonBox::Apply ), &QAbstractButton::clicked,
            gui, &QgsDiagramProperties::apply );
+  connect( buttonBox->button( QDialogButtonBox::Help ), &QAbstractButton::clicked, gui, [ = ]
+  {
+    QgsHelp::openHelp( QStringLiteral( "working_with_vector/vector_properties.html#diagrams-properties" ) );
+  } );
 
   if ( dlg.exec() )
     gui->apply();
