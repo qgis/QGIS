61768df0fa61f415b6bebbda00a16b0d3e626ba0
Ivan Barsukov <i.barsukov@protonmail.com>
2024-06-22 16:05:06 +0700

    Fix plugin manager background for dark themes

--- src/app/pluginmanager/qgspluginmanager.cpp
+++ src/app/pluginmanager/qgspluginmanager.cpp
@@ -33,6 +33,7 @@
 #include <QRegularExpression>
 #include <QUrl>
 #include <QCheckBox>
+#include <QPalette>
 
 #include "qgsmessagelog.h"
 
@@ -699,7 +700,7 @@ void QgsPluginManager::showPluginDetails( QStandardItem *item )
 
   QString html = "<style>"
                  "  body {"
-                 "    background-color:white;"
+                 "    color:" + QPalette().color( QPalette::ColorRole::Text ).name() + ";"
                  "  }"
                  "  body, table {"
                  "    padding:0px;"
@@ -1368,7 +1369,7 @@ void QgsPluginManager::setCurrentTab( int idx )
     {
       tabInfoHTML += "<style>"
                      "  body, p {"
-                     "      background-color: white;"
+                     "      color: " + QPalette().color( QPalette::ColorRole::Text ).name() + ";"
                      "      margin: 2px;"
                      "      font-family: Verdana, Sans-serif;"
                      "      font-size: 10pt;"
