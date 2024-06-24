qgis/QGIS

commit b371267fbe117ecf54f56dd8c9948db0d89dfdd1

Improved 3d measure tool (#52208)
* cleanup 3d measure tool class from pickers

* update qgs3dmapscene and identify tool

* remove pickers from chunked entity and terrain entity

* add QgsRay3d::intersects( QgsBox3d );

* demterrain and tesselated polygon geometry intersection checks

* Add vertex markers to 3d rubber bands
Add moveLastPoint() support for 3d rubber bands

* Forward keyEvents to 3d map tools

* proof of concept, needs refactoring

* move intersection logic to ChunkedEntities

* move ray casting logic to Qgs3DUtils

* cleanup map tools

* rename intersectEntity to rayIntersection, further cleanup

* cleanup

* move RayCastContext param
fix last rubberband marker
use singleResult for pointclouds
cleanup

* replace autos

* fix FID fetching logic
fix rule based rendering hits
fix selected feature hits

* fix clearing identify results, startDrag tolerance

* use doubles when dealing with map coordinates
return world coords in point cloud RayHits

* dosctrings and method rename

* use raycastingtools::ray3d

* move RayHit and Context to raycastingutils

* sip skip new methods

* fix build

* remove unused

* added docstring

* fix forward declarations

* fix qt6 build

* Use anti-aliasing for better looking billboards

* Slightly improve styling of 3D rubber band

* Make it possible to have 3D rubber band always on top

Also adds some docs to the frame graph implementation

* Do not leave the final rubber band segment when measuring is done

* address review comments

* spelling typo

* make cppcheck happy

* address benoit and loic review comments

* remove previous bool QgsTerrainEntity::rayIntersection() implementation

* add consts, update test mask

---------

Co-authored-by: Martin Dobias <wonder.sk@gmail.com>

src/3d/qgs3daxis.cpp
@@ -733,17 +733,18 @@ void Qgs3DAxis::onCameraViewChange( float pitch, float yaw )
   if ( mMapSettings->terrainRenderingEnabled() )
   {
     QgsDebugMsgLevel( "Checking elevation from terrain...", 2 );
-    QVector3D intersectionPoint;
     QVector3D camPos = mCameraController->camera()->position();
-    QgsRayCastingUtils::Ray3D r( camPos, pos.toVector3D() - camPos );
-    if ( mMapScene->terrainEntity()->rayIntersection( r, intersectionPoint ) )
+    QgsRayCastingUtils::Ray3D ray( camPos, pos.toVector3D() - camPos, mCameraController->camera()->farPlane() );
+    const QVector<QgsRayCastingUtils::RayHit> hits = mMapScene->terrainEntity()->rayIntersection( ray, QgsRayCastingUtils::RayCastContext() );
+    if ( !hits.isEmpty() )
     {
-      elevation = intersectionPoint.y();
+      elevation = hits.at( 0 ).pos.y();
       QgsDebugMsgLevel( QString( "Computed elevation from terrain: %1" ).arg( elevation ), 2 );
     }
     else
+    {
       QgsDebugMsgLevel( "Unable to obtain elevation from terrain", 2 );
-
+    }
   }
   pos.set( pos.x(), elevation + mMapSettings->terrainElevationOffset(), pos.z() );
 

python/3d/auto_generated/qgs3dmapscene.sip.in
@@ -13,6 +13,7 @@
 
 
 
+
 class Qgs3DMapScene : QObject
 {
 %Docstring(signature="appended")
@@ -78,7 +79,6 @@ Returns number of pending jobs for all chunked entities
 Returns the current state of the scene
 %End
 
-
     float worldSpaceError( float epsilon, float distance );
 %Docstring
 Given screen error (in pixels) and distance from camera (in 3D world coordinates), this function
@@ -89,6 +89,8 @@ estimates the error in world space. Takes into account camera's field of view an
 %Docstring
 Exports the scene according to the scene export settings
 %End
+
+
 
 
     QgsRectangle sceneExtent();
@@ -177,7 +179,6 @@ Updates the temporale entities
     Qgs3DMapScene();
     Qgs3DMapScene( const Qgs3DMapScene &other );
 };
-
 /************************************************************************
  * This file has been generated automatically from                      *
  *                                                                      *

src/3d/chunks/qgschunkedentity_p.cpp
@@ -17,8 +17,6 @@
 
 #include <QElapsedTimer>
 #include <QVector4D>
-#include <Qt3DRender/QObjectPicker>
-#include <Qt3DRender/QPickTriangleEvent>
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 #include <Qt3DRender/QBuffer>
 typedef Qt3DRender::QBuffer Qt3DQBuffer;
@@ -547,13 +545,6 @@ void QgsChunkedEntity::onActiveJobFinished()
 
       mReplacementQueue->insertFirst( node->replacementQueueEntry() );
 
-      if ( mPickingEnabled )
-      {
-        Qt3DRender::QObjectPicker *picker = new Qt3DRender::QObjectPicker( node->entity() );
-        node->entity()->addComponent( picker );
-        connect( picker, &Qt3DRender::QObjectPicker::clicked, this, &QgsChunkedEntity::onPickEvent );
-      }
-
       emit newEntityCreated( entity );
     }
     else
@@ -662,72 +653,6 @@ void QgsChunkedEntity::cancelActiveJobs()
   }
 }
 
-
-void QgsChunkedEntity::setPickingEnabled( bool enabled )
-{
-  if ( mPickingEnabled == enabled )
-    return;
-
-  mPickingEnabled = enabled;
-
-  if ( enabled )
-  {
-    QgsChunkListEntry *entry = mReplacementQueue->first();
-    while ( entry )
-    {
-      QgsChunkNode *node = entry->chunk;
-      Qt3DRender::QObjectPicker *picker = new Qt3DRender::QObjectPicker( node->entity() );
-      node->entity()->addComponent( picker );
-      connect( picker, &Qt3DRender::QObjectPicker::clicked, this, &QgsChunkedEntity::onPickEvent );
-
-      entry = entry->next;
-    }
-  }
-  else
-  {
-    for ( Qt3DRender::QObjectPicker *picker : findChildren<Qt3DRender::QObjectPicker *>() )
-      picker->deleteLater();
-  }
-}
-
-void QgsChunkedEntity::onPickEvent( Qt3DRender::QPickEvent *event )
-{
-  Qt3DRender::QPickTriangleEvent *triangleEvent = qobject_cast<Qt3DRender::QPickTriangleEvent *>( event );
-  if ( !triangleEvent )
-    return;
-
-  Qt3DRender::QObjectPicker *picker = qobject_cast<Qt3DRender::QObjectPicker *>( sender() );
-  if ( !picker )
-    return;
-
-  Qt3DCore::QEntity *entity = qobject_cast<Qt3DCore::QEntity *>( picker->parent() );
-  if ( !entity )
-    return;
-
-  // go figure out feature ID from the triangle index
-  QgsFeatureId fid = FID_NULL;
-  for ( Qt3DRender::QGeometryRenderer *geomRenderer : entity->findChildren<Qt3DRender::QGeometryRenderer *>() )
-  {
-    // unfortunately we can't access which sub-entity triggered the pick event
-    // so as a temporary workaround let's just ignore the entity with selection
-    // and hope the event was the main entity (QTBUG-58206)
-    if ( geomRenderer->objectName() != QLatin1String( "main" ) )
-      continue;
-
-    if ( QgsTessellatedPolygonGeometry *g = qobject_cast<QgsTessellatedPolygonGeometry *>( geomRenderer->geometry() ) )
-    {
-      fid = g->triangleIndexToFeatureId( triangleEvent->triangleIndex() );
-      if ( !FID_IS_NULL( fid ) )
-        break;
-    }
-  }
-
-  if ( !FID_IS_NULL( fid ) )
-  {
-    emit pickedObject( event, fid );
-  }
-}
-
 double QgsChunkedEntity::calculateEntityGpuMemorySize( Qt3DCore::QEntity *entity )
 {
   long long usedGpuMemory = 0;
@@ -741,6 +666,13 @@ double QgsChunkedEntity::calculateEntityGpuMemorySize( Qt3DCore::QEntity *entity
     usedGpuMemory += tex->width() * tex->height() * 4;
   }
   return usedGpuMemory / 1024.0 / 1024.0;
+}
+
+QVector<QgsRayCastingUtils::RayHit> QgsChunkedEntity::rayIntersection( const QgsRayCastingUtils::Ray3D &ray, const QgsRayCastingUtils::RayCastContext &context ) const
+{
+  Q_UNUSED( ray )
+  Q_UNUSED( context )
+  return QVector<QgsRayCastingUtils::RayHit>();
 }
 
 /// @endcond

src/3d/chunks/qgschunkedentity_p.h
@@ -40,18 +40,20 @@ class QgsChunkLoaderFactory;
 class QgsChunkBoundsEntity;
 class QgsChunkQueueJobFactory;
 
+namespace QgsRayCastingUtils
+{
+  class Ray3D;
+  struct RayCastContext;
+  struct RayHit;
+}
+
 #include <QVector3D>
 #include <QMatrix4x4>
 
 #include <QTime>
 
-#include "qgsfeatureid.h"
 #include "qgschunknode_p.h"
 
-namespace Qt3DRender
-{
-  class QPickEvent;
-}
 
 /**
  * \ingroup 3d
@@ -98,11 +100,6 @@ class QgsChunkedEntity : public Qt3DCore::QEntity
     //! Returns number of jobs pending for this entity until it is fully loaded/updated in the current view
     int pendingJobsCount() const;
 
-    //! Enables or disables object picking on this entity. When enabled, pickedObject() signals will be emitted on mouse clicks
-    void setPickingEnabled( bool enabled );
-    //! Returns whether object picking is currently enabled
-    bool hasPickingEnabled() const { return mPickingEnabled; }
-
     //! Sets whether additive strategy is enabled - see usingAditiveStrategy()
     void setUsingAdditiveStrategy( bool additive ) { mAdditiveStrategy = additive; }
 
@@ -127,6 +124,17 @@ class QgsChunkedEntity : public Qt3DCore::QEntity
 
     static double calculateEntityGpuMemorySize( Qt3DCore::QEntity *entity );
 
+    /**
+     * Checks if \a ray intersects the entity by using the specified parameters in \a context and returns information about the hits.
+     * This method is typically used by map tools that need to identify the exact location on a 3d entity that the mouse cursor points at,
+     * as well as properties of the intersected entity (fid for vector layers, point cloud attributes for point cloud layers etc). The camera position
+     * is used as the ray's origin in that case.
+     * The number of successful hits returned depends on the entity's implementation (eg. point cloud entities use a tolerance and return multiple 'near' hits
+     * \note The ray uses World coordinates.
+     * \since QGIS 3.32
+     */
+    virtual QVector<QgsRayCastingUtils::RayHit> rayIntersection( const QgsRayCastingUtils::Ray3D &ray, const QgsRayCastingUtils::RayCastContext &context ) const;
+
   protected:
     //! Cancels the background job that is currently in progress
     void cancelActiveJob( QgsChunkQueueJob *job );
@@ -149,17 +157,12 @@ class QgsChunkedEntity : public Qt3DCore::QEntity
   private slots:
     void onActiveJobFinished();
 
-    void onPickEvent( Qt3DRender::QPickEvent *event );
-
   signals:
     //! Emitted when the number of pending jobs changes (some jobs have finished or some jobs have been just created)
     void pendingJobsCountChanged();
 
     //! Emitted when a new 3D entity has been created. Other components can use that to do extra work
     void newEntityCreated( Qt3DCore::QEntity *entity );
-
-    //! Emitted on mouse clicks when picking is enabled and there is a feature under the cursor
-    void pickedObject( Qt3DRender::QPickEvent *pickEvent, QgsFeatureId fid );
 
   protected:
     //! root node of the quadtree hierarchy
@@ -197,9 +200,6 @@ class QgsChunkedEntity : public Qt3DCore::QEntity
 
     //! jobs that are currently being processed (asynchronously in worker threads)
     QList<QgsChunkQueueJob *> mActiveJobs;
-
-    //! If picking is enabled, QObjectPicker objects will be assigned to chunks and pickedObject() signals fired on mouse click
-    bool mPickingEnabled = false;
 
     /**
      * With additive strategy enabled, also all parent nodes are added to active nodes.

src/3d/qgs3dmapscene.cpp
@@ -17,10 +17,6 @@
 
 #include <Qt3DRender/QCamera>
 #include <Qt3DRender/QMesh>
-#include <Qt3DRender/QObjectPicker>
-#include <Qt3DRender/QPickEvent>
-#include <Qt3DRender/QPickingSettings>
-#include <Qt3DRender/QPickTriangleEvent>
 #include <Qt3DRender/QRenderSettings>
 #include <Qt3DRender/QSceneLoader>
 #include <Qt3DExtras/QForwardRenderer>
@@ -48,7 +44,6 @@
 #include "qgsapplication.h"
 #include "qgsaabb.h"
 #include "qgsabstract3dengine.h"
-#include "qgs3dmapscenepickhandler.h"
 #include "qgs3dmapsettings.h"
 #include "qgs3dutils.h"
 #include "qgsabstract3drenderer.h"
@@ -96,9 +91,6 @@ Qgs3DMapScene::Qgs3DMapScene( Qgs3DMapSettings &map, QgsAbstract3DEngine *engine
   // even if there's no change. Switching to "on demand" should only re-render when something has changed
   // and we save quite a lot of resources
   mEngine->renderSettings()->setRenderPolicy( Qt3DRender::QRenderSettings::OnDemand );
-
-  // we want precise picking of terrain (also bounding volume picking does not seem to work - not sure why)
-  mEngine->renderSettings()->pickingSettings()->setPickMethod( Qt3DRender::QPickingSettings::TrianglePicking );
 
   QRect viewportRect( QPoint( 0, 0 ), mEngine->size() );
 
@@ -287,52 +279,6 @@ int Qgs3DMapScene::totalPendingJobsCount() const
   for ( QgsChunkedEntity *entity : std::as_const( mChunkEntities ) )
     count += entity->pendingJobsCount();
   return count;
-}
-
-void Qgs3DMapScene::registerPickHandler( Qgs3DMapScenePickHandler *pickHandler )
-{
-  if ( mPickHandlers.isEmpty() )
-  {
-    // we need to add object pickers
-    for ( Qt3DCore::QEntity *entity : mLayerEntities )
-    {
-      if ( QgsChunkedEntity *chunkedEntity = qobject_cast<QgsChunkedEntity *>( entity ) )
-        chunkedEntity->setPickingEnabled( true );
-    }
-  }
-
-  mPickHandlers.append( pickHandler );
-}
-
-void Qgs3DMapScene::unregisterPickHandler( Qgs3DMapScenePickHandler *pickHandler )
-{
-  mPickHandlers.removeOne( pickHandler );
-
-  if ( mPickHandlers.isEmpty() )
-  {
-    // we need to remove pickers
-    for ( Qt3DCore::QEntity *entity : mLayerEntities )
-    {
-      if ( QgsChunkedEntity *chunkedEntity = qobject_cast<QgsChunkedEntity *>( entity ) )
-        chunkedEntity->setPickingEnabled( false );
-    }
-  }
-}
-
-void Qgs3DMapScene::onLayerEntityPickedObject( Qt3DRender::QPickEvent *pickEvent, QgsFeatureId fid )
-{
-  QgsMapLayer *layer = mLayerEntities.key( qobject_cast<QgsChunkedEntity *>( sender() ) );
-  if ( !layer )
-    return;
-
-  QgsVectorLayer *vlayer = qobject_cast<QgsVectorLayer *>( layer );
-  if ( !vlayer )
-    return;
-
-  for ( Qgs3DMapScenePickHandler *pickHandler : std::as_const( mPickHandlers ) )
-  {
-    pickHandler->handlePickOnVectorLayer( vlayer, fid, pickEvent->worldIntersection(), pickEvent );
-  }
 }
 
 float Qgs3DMapScene::worldSpaceError( float epsilon, float distance )
@@ -794,9 +740,6 @@ void Qgs3DMapScene::addLayerEntity( QgsMapLayer *layer )
       {
         mChunkEntities.append( chunkedNewEntity );
         needsSceneUpdate = true;
-
-        chunkedNewEntity->setPickingEnabled( !mPickHandlers.isEmpty() );
-        connect( chunkedNewEntity, &QgsChunkedEntity::pickedObject, this, &Qgs3DMapScene::onLayerEntityPickedObject );
 
         connect( chunkedNewEntity, &QgsChunkedEntity::newEntityCreated, this, [this]( Qt3DCore::QEntity * entity )
         {

src/3d/qgs3dmapscene.h
@@ -20,9 +20,7 @@
 
 #include <Qt3DCore/QEntity>
 
-#include "qgsfeatureid.h"
-#include "qgsshadowrenderingframegraph.h"
-#include "qgsray3d.h"
+#include "qgsrectangle.h"
 #include "qgscameracontroller.h"
 
 #ifndef SIP_RUN
@@ -30,8 +28,6 @@ namespace Qt3DRender
 {
   class QRenderSettings;
   class QCamera;
-  class QPickEvent;
-  class QObjectPicker;
 }
 
 namespace Qt3DLogic
@@ -51,7 +47,6 @@ class Qgs3DAxis;
 class QgsAbstract3DEngine;
 class QgsAbstract3DRenderer;
 class QgsMapLayer;
-class Qgs3DMapScenePickHandler;
 class Qgs3DMapSettings;
 class QgsTerrainEntity;
 class QgsChunkedEntity;
@@ -62,6 +57,7 @@ class QgsShadowRenderingFrameGraph;
 class QgsPostprocessingEntity;
 class QgsChunkNode;
 class QgsDoubleRange;
+
 
 /**
  * \ingroup 3d
@@ -127,11 +123,6 @@ class _3D_EXPORT Qgs3DMapScene : public QObject
     //! Returns the current state of the scene
     SceneState sceneState() const { return mSceneState; }
 
-    //! Registers an object that will get results of pick events on 3D entities. Does not take ownership of the pick handler. Adds object picker components to 3D entities.
-    void registerPickHandler( Qgs3DMapScenePickHandler *pickHandler ) SIP_SKIP;
-    //! Unregisters previously registered pick handler. Pick handler is not deleted. Also removes object picker components from 3D entities.
-    void unregisterPickHandler( Qgs3DMapScenePickHandler *pickHandler ) SIP_SKIP;
-
     /**
      * Given screen error (in pixels) and distance from camera (in 3D world coordinates), this function
      * estimates the error in world space. Takes into account camera's field of view and the screen (3D view) size.
@@ -147,6 +138,20 @@ class _3D_EXPORT Qgs3DMapScene : public QObject
      * \since QGIS 3.18
      */
     QVector<const QgsChunkNode *> getLayerActiveChunkNodes( QgsMapLayer *layer ) SIP_SKIP;
+
+    /**
+     * Returns the layers that contain chunked entities
+     *
+     * \since QGIS 3.32
+     */
+    QList<QgsMapLayer *> layers() SIP_SKIP { return mLayerEntities.keys(); }
+
+    /**
+     * Returns the entity belonging to \a layer
+     *
+     * \since QGIS 3.32
+     */
+    Qt3DCore::QEntity *layerEntity( QgsMapLayer *layer ) const SIP_SKIP { return mLayerEntities.value( layer ); }
 
     /**
      * Returns the scene extent in the map's CRS
@@ -236,7 +241,6 @@ class _3D_EXPORT Qgs3DMapScene : public QObject
     void onLayersChanged();
     void createTerrainDeferred();
     void onBackgroundColorChanged();
-    void onLayerEntityPickedObject( Qt3DRender::QPickEvent *pickEvent, QgsFeatureId fid );
     void updateLights();
     void updateCameraLens();
     void onRenderersChanged();
@@ -285,8 +289,6 @@ class _3D_EXPORT Qgs3DMapScene : public QObject
     QMap<const QgsAbstract3DRenderer *, Qt3DCore::QEntity *> mRenderersEntities;
     bool mTerrainUpdateScheduled = false;
     SceneState mSceneState = Ready;
-    //! List of currently registered pick handlers (used by identify tool)
-    QList<Qgs3DMapScenePickHandler *> mPickHandlers;
     //! List of lights in the scene
     QList<Qt3DCore::QEntity *> mLightEntities;
     QList<QgsMapLayer *> mModelVectorLayers;
@@ -298,5 +300,4 @@ class _3D_EXPORT Qgs3DMapScene : public QObject
     Qgs3DAxis *m3DAxis = nullptr;
 
 };
-
 #endif // QGS3DMAPSCENE_H

src/3d/qgs3dmapscenepickhandler.h
@@ -1,45 +1,0 @@
-/***************************************************************************
-  qgs3dmapscenepickhandler.h
-  --------------------------------------
-  Date                 : Sep 2018
-  Copyright            : (C) 2018 by Martin Dobias
-  Email                : wonder dot sk at gmail dot com
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
-#ifndef QGS3DMAPSCENEPICKHANDLER_H
-#define QGS3DMAPSCENEPICKHANDLER_H
-
-#include "qgsfeatureid.h"
-#include <Qt3DRender/QPickEvent>
-
-class QVector3D;
-class QgsVectorLayer;
-
-/**
- * \ingroup 3d
- * \brief Abstract base class for handlers that process pick events from a 3D map scene.
- *
- * 3D entities in map scene get QObjectPicker components assigned and mouse press events trigger virtual methods
- * or pick handlers.
- *
- * This is used for identify tool to be able to identify entities coming from 3D renderers assigned to map layers.
- *
- * \since QGIS 3.4
- */
-class Qgs3DMapScenePickHandler
-{
-  public:
-    virtual ~Qgs3DMapScenePickHandler() = default;
-
-    //! Called when user clicked a 3D entity belonging to a feature of a vector layer
-    virtual void handlePickOnVectorLayer( QgsVectorLayer *vlayer, QgsFeatureId id, const QVector3D &worldIntersection, Qt3DRender::QPickEvent *event ) = 0;
-};
-
-#endif // QGS3DMAPSCENEPICKHANDLER_H

src/3d/qgs3dutils.cpp
@@ -29,6 +29,14 @@
 #include "qgsabstract3dengine.h"
 #include "qgsterraingenerator.h"
 #include "qgscameracontroller.h"
+#include "qgschunkedentity_p.h"
+#include "qgsterrainentity_p.h"
+#include "qgsraycastingutils_p.h"
+
+#include "qgsline3dsymbol.h"
+#include "qgspoint3dsymbol.h"
+#include "qgspolygon3dsymbol.h"
+
 #include "qgspointcloudrenderer.h"
 #include "qgspointcloud3dsymbol.h"
 #include "qgspointcloudlayer3drenderer.h"
@@ -783,4 +791,29 @@ std::unique_ptr<QgsPointCloudLayer3DRenderer> Qgs3DUtils::convert2DPointCloudRen
     return renderer3D;
   }
   return nullptr;
+}
+
+QHash<QgsMapLayer *, QVector<QgsRayCastingUtils::RayHit>> Qgs3DUtils::castRay( Qgs3DMapScene *scene, const QgsRay3D &ray, const QgsRayCastingUtils::RayCastContext &context )
+{
+  QgsRayCastingUtils::Ray3D r( ray.origin(), ray.direction(), context.maxDistance );
+  QHash<QgsMapLayer *, QVector<QgsRayCastingUtils:: RayHit>> results;
+  const QList<QgsMapLayer *> keys = scene->layers();
+  for ( QgsMapLayer *layer : keys )
+  {
+    Qt3DCore::QEntity *entity = scene->layerEntity( layer );
+
+    if ( QgsChunkedEntity *chunkedEntity = qobject_cast<QgsChunkedEntity *>( entity ) )
+    {
+      const QVector<QgsRayCastingUtils::RayHit> result = chunkedEntity->rayIntersection( r, context );
+      if ( !result.isEmpty() )
+        results[ layer ] = result;
+    }
+  }
+  if ( QgsTerrainEntity *terrain = scene->terrainEntity() )
+  {
+    const QVector<QgsRayCastingUtils::RayHit> result = terrain->rayIntersection( r, context );
+    if ( !result.isEmpty() )
+      results[ nullptr ] = result;  // Terrain hits are not tied to a layer so we use nullptr as their key here
+  }
+  return results;
 }

commit db95865380c8914e8fa2b7fe1a3155f4a767a3d6

qgs3daxis: Fix cube face picking
When clicking on a face of the cube, the view is rotated according
to the clicked face. However, this needs `TrianglePicking` to be
enabled to properly detect the face because it needs the
`primitiveIndex` of the hit. `TrianglePicking` was activated
by default up to commit b371267fbe117ecf54f56dd8c9948db0d89dfdd1 which
restored it to the default picking method: `BoundingVolumePicking`.

This issue is fixed by enabling `TrianglePicking` when entering the
viewport if the cube is visible. `TrianglePicking` is then deactivated
when leaving the viewport.

Fixes: commit b371267fbe117ecf54f56dd8c9948db0d89dfdd1

src/3d/qgs3daxis.cpp
@@ -81,6 +81,8 @@ Qgs3DAxis::~Qgs3DAxis()
 
 void Qgs3DAxis::init3DObjectPicking( )
 {
+  mDefaultPickingMethod = mMapScene->engine()->renderSettings()->pickingSettings()->pickMethod();
+
   // Create screencaster to be used by EventFilter:
   //   1- Perform ray casting tests by specifying "touch" coordinates in screen space
   //   2- connect screencaster results to onTouchedByRay
@@ -152,12 +154,23 @@ bool Qgs3DAxis::eventFilter( QObject *watched, QEvent *event )
         // if casted ray from pos matches an entity, call onTouchedByRay
         mScreenRayCaster->trigger( mLastClickedPos );
       }
-
-      // when we exit the viewport, reset the mouse cursor if needed
-      else if ( mPreviousCursor != Qt::ArrowCursor && mCanvas->cursor() == Qt::ArrowCursor )
+      // exit the viewport
+      else
       {
-        mCanvas->setCursor( mPreviousCursor );
-        mPreviousCursor = Qt::ArrowCursor;
+        // reset the mouse cursor if needed
+        if ( mPreviousCursor != Qt::ArrowCursor && mCanvas->cursor() == Qt::ArrowCursor )
+        {
+          mCanvas->setCursor( mPreviousCursor );
+          mPreviousCursor = Qt::ArrowCursor;
+        }
+
+        // reset the picking settings if needed
+        if ( mMapScene->engine()->renderSettings()->pickingSettings()->pickMethod() == Qt3DRender::QPickingSettings::TrianglePicking
+             && mDefaultPickingMethod != Qt3DRender::QPickingSettings::TrianglePicking )
+        {
+          mMapScene->engine()->renderSettings()->pickingSettings()->setPickMethod( mDefaultPickingMethod );
+          QgsDebugMsgLevel( "Disabling triangle picking", 2 );
+        }
       }
 
       mIsDragging = false; // drag ends
@@ -210,6 +223,14 @@ void Qgs3DAxis::onTouchedByRay( const Qt3DRender::QAbstractRayCaster::Hits &hits
         mPreviousCursor = mCanvas->cursor();
         mCanvas->setCursor( Qt::ArrowCursor );
         QgsDebugMsgLevel( "Enabling arrow cursor", 2 );
+
+        // The cube needs triangle picking to handle click on faces.
+        if ( mMapScene->engine()->renderSettings()->pickingSettings()->pickMethod() != Qt3DRender::QPickingSettings::TrianglePicking &&
+             mCubeRoot->isEnabled() )
+        {
+          mMapScene->engine()->renderSettings()->pickingSettings()->setPickMethod( Qt3DRender::QPickingSettings::TrianglePicking );
+          QgsDebugMsgLevel( "Enabling triangle picking", 2 );
+        }
       }
     }
   }

src/3d/qgs3daxis.h
@@ -29,6 +29,7 @@
 #include <QVector3D>
 
 #include <Qt3DRender/QLayer>
+#include <Qt3DRender/QRenderSettings>
 
 #include <QtWidgets/QMenu>
 #include "qgs3dmapsettings.h"
@@ -176,6 +177,7 @@ class _3D_EXPORT Qgs3DAxis : public QObject
     QPoint mLastClickedPos;
     Qt::MouseButton mLastClickedButton;
     QCursor mPreviousCursor = Qt::ArrowCursor;
+    Qt3DRender::QPickingSettings::PickMethod mDefaultPickingMethod;
     QMenu *mMenu = nullptr;
 
 };