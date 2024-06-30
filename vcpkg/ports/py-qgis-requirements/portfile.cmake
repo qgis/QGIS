6e24900fe638ca91ce962fe802bf731984e1ee1f
Matthias Kuhn <matthias@opengis.ch>
2024-06-30 07:48:20 +0200

    Add more python deps

--- vcpkg/ports/py-qgis-requirements/portfile.cmake
+++ vcpkg/ports/py-qgis-requirements/portfile.cmake
@@ -1,9 +1,16 @@
 set(BUILD_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
 
-file(MAKE_DIRECTORY ${BUILD_DIR})
+file(MAKE_DIRECTORY "${BUILD_DIR}")
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/${PYTHON3_SITE}")
+
+if(VCPKG_TARGET_IS_WINDOWS)
+  set(PYTHON_EXECUTABLE "${CURRENT_INSTALLED_DIR}/tools/python3/python${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+else()
+  set(PYTHON_EXECUTABLE "${CURRENT_INSTALLED_DIR}/tools/python3/python${PYTHON3_VERSION_MAJOR}.${PYTHON3_VERSION_MINOR}")
+endif()
 
 vcpkg_execute_required_process(
-        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python${VCPKG_HOST_EXECUTABLE_SUFFIX}" "-m" "pip" "install" "-r" "${CURRENT_PORT_DIR}/requirements.txt" "--target" "${CURRENT_PACKAGES_DIR}/${PYTHON3_SITE}"
+        COMMAND "${PYTHON_EXECUTABLE}" "-m" "pip" "install" "-r" "${CURRENT_PORT_DIR}/requirements.txt" "--target" "${CURRENT_PACKAGES_DIR}/${PYTHON3_SITE}" "--no-deps"
         WORKING_DIRECTORY "${BUILD_DIR}"
         LOGNAME "requirements-install-${TARGET_TRIPLET}-rel"
     )
