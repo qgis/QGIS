c8b26244831bc9f29a12159b19ecd263e476ee5c
uclaros <uclaros@gmail.com>
2024-07-04 12:45:51 +0300

    preview residuals while moving points

--- src/app/georeferencer/qgsgeorefmainwindow.cpp
+++ src/app/georeferencer/qgsgeorefmainwindow.cpp
@@ -744,6 +744,7 @@ void QgsGeoreferencerMainWindow::movePoint( QPoint canvasPixels )
   if ( mvPoint )
   {
     mvPoint->moveTo( canvasPixels, pointType );
+    mGCPListWidget->updateResiduals();
   }
 }
 
