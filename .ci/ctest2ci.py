b05941cdb5e91b723c4d67461fa8778439bda3bf
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-07-11 13:18:29 +0200

    remove useless code from workflow (#58065)
    * remove useless code from workflow
    
    This was never used because of limitations of running on PRs. Other approaches have been used since.
    
    * comment set-output

--- .ci/ctest2ci.py
+++ .ci/ctest2ci.py
@@ -111,7 +111,7 @@ for line in p.stdout:
 
     if not in_failing_test and re.search('[0-9]+% tests passed, [0-9]+ tests failed out of', updated_line):
         tests_failing = re.match(r'.* ([0-9]+) tests failed', updated_line).group(1)
-        updated_line += '\n::set-output name=TESTS_FAILING::{}'.format(tests_failing)
+        # updated_line += '\n::set-output name=TESTS_FAILING::{}'.format(tests_failing)
         end_fold()
 
         if re.search('100% tests passed', updated_line):
@@ -121,7 +121,7 @@ for line in p.stdout:
         start_fold('submit')
     elif re.search('Test results submitted to', updated_line):
         cdash_url = re.match(r'.*(http.*)$', updated_line).group(1)
-        updated_line += '\n::set-output name=CDASH_URL::{}'.format(cdash_url)
+        # updated_line += '\n::set-output name=CDASH_URL::{}'.format(cdash_url)
         end_fold()
 
     sys.stdout.write(updated_line)
