ada675b452c73b1a20c4405df0182d9a8854dbd9
Nyall Dawson <nyall.dawson@gmail.com>
2024-07-15 09:15:08 +1000

    Use metaenum instead of hardcoded list

--- src/gui/processing/qgsprocessingalgorithmdialogbase.cpp
+++ src/gui/processing/qgsprocessingalgorithmdialogbase.cpp
@@ -783,11 +783,7 @@ QString QgsProcessingAlgorithmDialogBase::formatHelp( QgsProcessingAlgorithm *al
   if ( algorithm->documentationFlags() != Qgis::ProcessingAlgorithmDocumentationFlags() )
   {
     QStringList flags;
-    for ( Qgis::ProcessingAlgorithmDocumentationFlag flag :
-          {
-            Qgis::ProcessingAlgorithmDocumentationFlag::RegeneratesPrimaryKey,
-            Qgis::ProcessingAlgorithmDocumentationFlag::RegeneratesPrimaryKeyInSomeScenarios
-          } )
+    for (  Qgis::ProcessingAlgorithmDocumentationFlag flag : qgsEnumList< Qgis::ProcessingAlgorithmDocumentationFlag>() )
     {
       if ( algorithm->documentationFlags() & flag )
       {
