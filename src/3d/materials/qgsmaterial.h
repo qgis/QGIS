eb3917515f703215a171a5ab60a0220a77c5f6db
Jean Felder <jean.felder@oslandia.com>
2024-07-18 13:19:51 +0200

    qgsmaterial: Add methods to setup clipping
    This changes allows to enable or disable clip planes on a
    material. Indeed, to enable clipping, it is necessary to compute
    `gl_ClipDistance` in the vertex or the geometry shader (where the
    `gl_Position` is computed). `gl_ClipDistance` needs to have the clip
    plane equations.
    
    With this change, two `uniform` parameters are set in the shaders of a
    `QgsMaterial` if clipping is enabled:
    - the clip plane equations as an array
    - the number of equations
    
    Also, it is necessary to ensure that the clipping is not computed if
    is not needed. This is achieved by adding a `#define CLIPPING` macro
    to the shaders if the clipping is enabled.
    
    There are two opposites functions:
    - `enableClipping` which adds the `#define CLIPPING` macro
     to the relevant shaders and adds the two uniform parameters
    - `disableClipping` which disables the `#define CLIPPING`
     macro from the relevant shaders and removes the two uniform parameters
     if necessary

--- src/3d/materials/qgsmaterial.h
+++ src/3d/materials/qgsmaterial.h
@@ -21,17 +21,18 @@
 #include "qgis_3d.h"
 
 #include <Qt3DRender/QMaterial>
+#include <QList>
+#include <QVector4D>
 
 namespace Qt3DRender
 {
   class QEffect;
 }
 
-///@cond PRIVATE
-
 /**
  * \ingroup 3d
  * \brief Base class for all materials used within QGIS 3D views.
+ * It provides common functionality (such as clipping) that all materials should support (where applicable).
  * \since QGIS 3.40
  */
 class _3D_EXPORT QgsMaterial : public Qt3DRender::QMaterial
@@ -39,11 +40,39 @@ class _3D_EXPORT QgsMaterial : public Qt3DRender::QMaterial
     Q_OBJECT
   public:
 
-    /*
+    /**
      * Constructor for QgsMaterial, with the specified \a parent node.
      */
     explicit QgsMaterial( Qt3DCore::QNode *parent = nullptr );
     ~QgsMaterial() override;
+
+    /**
+     * Adds two uniform parameters to define OpenGL clipping from \a clipPlanesEquations.
+     * It also adds a define macro appropriate to vertex/geometry shaders.
+     *
+     * \since QGIS 3.40
+    */
+    void enableClipping( const QList<QVector4D> &clipPlanesEquations );
+
+    /**
+     * Removes the uniform parameters used to define OpenGL clipping.
+     * It also removes the define macro used for clipping from vertex/geometry shaders.
+     * If clipping was not enabled, nothing happens.
+     *
+     * \since QGIS 3.40
+     */
+    void disableClipping();
+
+  private:
+
+    //! The name of the QParameter which contains the plane equations
+    static const QString CLIP_PLANE_ARRAY_PARAMETER_NAME;
+    //! The name of the QParameter which contains the number of planes
+    static const QString CLIP_PLANE_MAX_PLANE_PARAMETER_NAME;
+    //! The name of the define macro which enables clipping
+    static const QString CLIP_PLANE_DEFINE;
+
+    bool mClippingEnabled = false;
 
 };
 
