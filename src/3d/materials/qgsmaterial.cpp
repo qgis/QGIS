88fbfb07a183c5cbe9a3db5952c6846591bfd9a0
Jean Felder <jean.felder@oslandia.com>
2024-07-18 13:19:51 +0200

    qgsmaterial: Add methods to setup clipping
    This changes allows to enable or disable clip planes on a
    material. Indeed, to enable clipping, it is necessary to compute
    `gl_ClipDistance` in the vertex or the geometry shader (where the
    `gl_Position` is computed). `gl_ClipDistance` needs to have the clip
    plane equations.
    
    With this change, two `uniform` parameters are set in the shaders of a
    `QgsMaterial` if clipping is enabled:
    - the clip plane equations as an array
    - the number of equations
    
    Also, it is necessary to ensure that the clipping is not computed if
    is not needed. This is achieved by adding a `#define CLIPPING` macro
    to the shaders if the clipping is enabled.
    
    There are two opposites functions:
    - `enableClipping` which adds the `#define CLIPPING` macro
     to the relevant shaders and adds the two uniform parameters
    - `disableClipping` which disables the `#define CLIPPING`
     macro from the relevant shaders and removes the two uniform parameters
     if necessary

--- src/3d/materials/qgsmaterial.cpp
+++ src/3d/materials/qgsmaterial.cpp
@@ -14,8 +14,18 @@
  ***************************************************************************/
 
 #include "qgsmaterial.h"
+#include "qgs3dutils.h"
 
-///@cond PRIVATE
+#include <Qt3DRender/QEffect>
+#include <Qt3DRender/QParameter>
+#include <Qt3DRender/QTechnique>
+#include <Qt3DRender/QRenderPass>
+
+const QString QgsMaterial::CLIP_PLANE_ARRAY_PARAMETER_NAME = QStringLiteral( "clipPlane[0]" );
+const QString QgsMaterial::CLIP_PLANE_MAX_PLANE_PARAMETER_NAME = QStringLiteral( "max_plane_real" );
+const QString QgsMaterial::CLIP_PLANE_DEFINE = QStringLiteral( "CLIPPING" );
+
+
 QgsMaterial::QgsMaterial( QNode *parent )
   : QMaterial( parent )
 {
@@ -23,3 +33,96 @@ QgsMaterial::QgsMaterial( QNode *parent )
 }
 
 QgsMaterial::~QgsMaterial() = default;
+
+void QgsMaterial::enableClipping( const QList<QVector4D> &clipPlanesEquations )
+{
+  Qt3DRender::QEffect *materialEffect = effect();
+  if ( !materialEffect )
+    return;
+
+  // First, disable possible existing clipping parameters
+  disableClipping();
+  if ( clipPlanesEquations.isEmpty() )
+  {
+    return;
+  }
+
+  // Add #define CLIPPING to the relevant shaders
+  for ( Qt3DRender::QTechnique *technique : materialEffect->techniques() )
+  {
+    for ( Qt3DRender::QRenderPass *renderPass : technique->renderPasses() )
+    {
+      Qt3DRender::QShaderProgram *shaderProgram = renderPass->shaderProgram();
+      const QByteArray geomCode = shaderProgram->geometryShaderCode();
+      if ( !geomCode.isEmpty() )
+      {
+        const QByteArray newGeomCode = Qgs3DUtils::addDefinesToShaderCode( geomCode, QStringList( QgsMaterial::CLIP_PLANE_DEFINE ) );
+        shaderProgram->setGeometryShaderCode( newGeomCode );
+      }
+
+      const QByteArray vertexCode = shaderProgram->vertexShaderCode();
+      if ( !vertexCode.isEmpty() )
+      {
+        const QByteArray newVertexCode = Qgs3DUtils::addDefinesToShaderCode( vertexCode, QStringList( QgsMaterial::CLIP_PLANE_DEFINE ) );
+        shaderProgram->setVertexShaderCode( newVertexCode );
+      }
+    }
+  }
+
+  // Add the clipping parameters
+  const int nrClipPlanes = clipPlanesEquations.size();
+  QVariantList clipPlanesEquationsVariant = QVariantList();
+  for ( int i = 0; i < nrClipPlanes; ++i )
+  {
+    clipPlanesEquationsVariant << clipPlanesEquations[i];
+  }
+  Qt3DRender::QParameter *clipPlane = new Qt3DRender::QParameter( QgsMaterial::CLIP_PLANE_ARRAY_PARAMETER_NAME,   clipPlanesEquationsVariant );
+  Qt3DRender::QParameter *clipPlaneNumber = new Qt3DRender::QParameter( QgsMaterial::CLIP_PLANE_MAX_PLANE_PARAMETER_NAME, nrClipPlanes );
+
+  materialEffect->addParameter( clipPlane );
+  materialEffect->addParameter( clipPlaneNumber );
+
+  mClippingEnabled = true;
+}
+
+void QgsMaterial::disableClipping()
+{
+  Qt3DRender::QEffect *materialEffect = effect();
+  if ( !materialEffect || !mClippingEnabled )
+    return;
+
+  // Remove #define CLIPPING from the shaders
+  for ( Qt3DRender::QTechnique *technique : materialEffect->techniques() )
+  {
+    for ( Qt3DRender::QRenderPass *renderPass : technique->renderPasses() )
+    {
+      Qt3DRender::QShaderProgram *shaderProgram = renderPass->shaderProgram();
+      const QByteArray geomCode = shaderProgram->geometryShaderCode();
+      if ( !geomCode.isEmpty() )
+      {
+        const QByteArray newGeomCode = Qgs3DUtils::removeDefinesFromShaderCode( geomCode, QStringList( QgsMaterial::CLIP_PLANE_DEFINE ) );
+        shaderProgram->setGeometryShaderCode( newGeomCode );
+      }
+
+      const QByteArray vertexCode = shaderProgram->vertexShaderCode();
+      if ( !vertexCode.isEmpty() )
+      {
+        const QByteArray newVertexCode = Qgs3DUtils::removeDefinesFromShaderCode( vertexCode, QStringList( QgsMaterial::CLIP_PLANE_DEFINE ) );
+        shaderProgram->setVertexShaderCode( newVertexCode );
+      }
+    }
+  }
+
+  // Remove the parameters
+  for ( Qt3DRender::QParameter *parameter : materialEffect->parameters() )
+  {
+    const QString parameterName = parameter->name();
+    if ( parameterName == QgsMaterial::CLIP_PLANE_ARRAY_PARAMETER_NAME ||  parameterName == QgsMaterial::CLIP_PLANE_MAX_PLANE_PARAMETER_NAME )
+    {
+      materialEffect->removeParameter( parameter );
+      break;
+    }
+  }
+
+  mClippingEnabled = false;
+}
