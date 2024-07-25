a2dc0b2c09fc0356d847417a3c0a653ed0fef2fa
Till Frankenbach <Tillfrankenbach@web.de>
2024-07-25 09:50:46 +0200

    Apply suggestions from code review
    Small cleanup
    
    Set numpy equivalent of ARGB32 to None
    
    Add test for masking=False
    
    Make masking optional
    
    Reshape array to be 2D
    
    Adapt test to 2D array
    
    Adapt test line in docstring
    
    Add test to CMakeLists.txt
    
    Wrap import and method setting in try/except
    
    Raise ValueError when dataType is not supported
    
    Set numpy equivalent for CInt16 and CInt32 to None
    
    Apply str method to dataType in Error message

--- tests/src/python/test_qgsrasterblock.py
+++ tests/src/python/test_qgsrasterblock.py
@@ -1,7 +1,7 @@
 """QGIS Unit tests for QgsRasterBlock.
 
 From build dir, run:
-ctest -R PyQgsQgsRasterBlock -V
+ctest -R PyQgsRasterBlock -V
 
 .. note:: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -38,20 +38,24 @@ class TestQgsRasterBlock(QgisTestCase):
 
         createTestRasters(self, self.temp_path)
 
-    def testQgsRasterBlockBasic(self):
+    def testQgsRasterBlock(self):
         raster = QgsRasterLayer(self.uri_2x2_2_BANDS_INT16)
         self.assertTrue(raster.isValid())
         provider = raster.dataProvider()
 
         # test without noDataValue set
         block = provider.block(1, raster.extent(), raster.width(), raster.height())
-        self.assertTrue((block.as_numpy() == numpy.array([0, 2, 4, 4])).all())
+        expected_array = numpy.array([[0, 2], [4, 4]])
+        self.assertTrue((block.as_numpy() == expected_array).all())
 
         # test with noDataValue set
         block.setNoDataValue(0)
-        data = numpy.array([numpy.nan, 2, 4, 4])
+        data = numpy.array([[numpy.nan, 2], [4, 4]])
         expected_masked_array = numpy.ma.masked_array(data, mask=numpy.isnan(data))
         self.assertTrue((block.as_numpy() == expected_masked_array).all())
+
+        # test with noDataValue set and use_masking == False
+        self.assertTrue((block.as_numpy(use_masking=False) == expected_array).all())
 
 
 if __name__ == '__main__':
