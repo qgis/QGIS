1c67ac060fbba357240114b44d45c12b9d97cf39
Mauro Bettella <bettellam@gmail.com>
2024-08-02 19:13:03 +0200

    indentation code update

--- external/nmea/parse.c
+++ external/nmea/parse.c
@@ -386,7 +386,7 @@ int nmea_parse_GPGSV( const char *buff, int buff_sz, nmeaGPGSV *pack )
                        &( pack->sat_data[0].id ), &( pack->sat_data[0].elv ), &( pack->sat_data[0].azimuth ), &( pack->sat_data[0].sig ),
                        &( pack->sat_data[1].id ), &( pack->sat_data[1].elv ), &( pack->sat_data[1].azimuth ), &( pack->sat_data[1].sig ),
                        &( pack->sat_data[2].id ), &( pack->sat_data[2].elv ), &( pack->sat_data[2].azimuth ), &( pack->sat_data[2].sig ),
-                       &( pack->sat_data[3].id ), &( pack->sat_data[3].elv ), & (pack->sat_data[3].azimuth ), &( pack->sat_data[3].sig ),
+                       &( pack->sat_data[3].id ), &( pack->sat_data[3].elv ), &( pack->sat_data[3].azimuth ), &( pack->sat_data[3].sig ),
                        &( pack->signal_id ) );
   }
   else
