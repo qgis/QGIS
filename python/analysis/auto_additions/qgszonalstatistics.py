c23e35bb08dd9c373e2a067bb2ae9d595626ee06
Nyall Dawson <nyall.dawson@gmail.com>
2024-08-14 16:49:46 +1000

    calculateStatistics is not unambiguously static

--- python/analysis/auto_additions/qgszonalstatistics.py
+++ python/analysis/auto_additions/qgszonalstatistics.py
@@ -1,4 +1,3 @@
 # The following has been generated automatically from src/analysis/vector/qgszonalstatistics.h
 QgsZonalStatistics.displayName = staticmethod(QgsZonalStatistics.displayName)
 QgsZonalStatistics.shortName = staticmethod(QgsZonalStatistics.shortName)
-QgsZonalStatistics.calculateStatistics = staticmethod(QgsZonalStatistics.calculateStatistics)
