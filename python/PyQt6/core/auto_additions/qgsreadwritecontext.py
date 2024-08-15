428a1cd405cbe641d0df3d3e9d674922ea6c3bcc
Nyall Dawson <nyall.dawson@gmail.com>
2024-08-15 16:57:02 +1000

    Add missing files

---
+++ python/PyQt6/core/auto_additions/qgsreadwritecontext.py
@@ -0,0 +1,4 @@
+# The following has been generated automatically from src/core/qgsreadwritecontext.h
+QgsReadWriteContext.ReadWriteMessage.__doc__ = """Struct for QgsReadWriteContext error or warning messages
+
+.. versionadded:: 3.2"""
---
+++ python/server/auto_additions/qgsaccesscontrolfilter.py
@@ -0,0 +1,2 @@
+# The following has been generated automatically from src/server/qgsaccesscontrolfilter.h
+QgsAccessControlFilter.LayerPermissions.__doc__ = """Describe the layer permission"""
