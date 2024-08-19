0643d2169ba711756494eeb9f264226357ea9ac3
Julien Cabieces <julien.cabieces@oslandia.com>
2024-08-19 09:05:01 +0200

    fix(ColorWidget): update sip files since new sipify

--- python/gui/auto_additions/qgscolorwidgets.py
+++ python/gui/auto_additions/qgscolorwidgets.py
@@ -14,3 +14,4 @@ except NameError:
     pass
 QgsColorWidget.createDragIcon = staticmethod(QgsColorWidget.createDragIcon)
 QgsColorWidget.alterColor = staticmethod(QgsColorWidget.alterColor)
+QgsColorWidget.alterColorF = staticmethod(QgsColorWidget.alterColorF)
