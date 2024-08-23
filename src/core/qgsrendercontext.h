c87c286de6171bd00ef42d1c75f64de9e17bbacb
Nyall Dawson <nyall.dawson@gmail.com>
2024-08-23 13:25:15 +1000

    Fix bug with inverted polygon renderer vs OTF simplification
    The simplification MUST be applied prior to transforming geometries,
    or the precalculated simplification tolerance will no be correct
    and may be excessive (eg when the simplification tolerance
    was calculated for a map in meters, yet the rendered layer is
    in degrees -- in this case we would simplify the inverted geometry
    with a tolerance of ~1 degree!)
    
    Ensure that we apply the simplification FIRST, then transform,
    and disable further simplification when rendering the calculated
    inverted feature. This is also a performance boost, because we
    are simplifying geometries prior to transforming + calculating
    the inversion, so usually end up with a lot less vertices
    to handle in those operations...

--- src/core/qgsrendercontext.h
+++ src/core/qgsrendercontext.h
@@ -641,7 +641,16 @@ class CORE_EXPORT QgsRenderContext : public QgsTemporalRangeObject
      *
      * \see setVectorSimplifyMethod()
      */
-    const QgsVectorSimplifyMethod &vectorSimplifyMethod() const { return mVectorSimplifyMethod; }
+    QgsVectorSimplifyMethod &vectorSimplifyMethod() { return mVectorSimplifyMethod; }
+
+    /**
+     * Returns the simplification settings to use when rendering vector layers.
+     *
+     * The default is to use no simplification.
+     *
+     * \see setVectorSimplifyMethod()
+     */
+    const QgsVectorSimplifyMethod &vectorSimplifyMethod() const SIP_SKIP { return mVectorSimplifyMethod; }
 
     /**
      * Sets the simplification setting to use when rendering vector layers.
