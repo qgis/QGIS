7b5cdd26a0f20d7fbf65c6638e964280298b35ec
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-08-26 21:13:55 +0200

    fix indentation

--- tests/src/python/test_qgsgeometry.py
+++ tests/src/python/test_qgsgeometry.py
@@ -1,4 +1,4 @@
-        """QGIS Unit tests for QgsGeometry.
+"""QGIS Unit tests for QgsGeometry.
 
 .. note:: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
