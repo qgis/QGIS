578952150c9bfafc02acf36027d0d413832b9870
Jacky Volpes <jacky.volpes@oslandia.com>
2024-08-29 20:27:48 +0200

    fix(QgsVectorLayer-dependencies): emit dataChanged on commitChanges
    Now when a feature is externally modified on save (database trigger for
    example), the dataChanged signal is emitted for indexes and caches
    reconstruction.

--- src/core/vector/qgsvectorlayer.cpp
+++ src/core/vector/qgsvectorlayer.cpp
@@ -6117,15 +6117,6 @@ void QgsVectorLayer::emitDataChanged()
   mDataChangedFired = false;
 }
 
-void QgsVectorLayer::onAfterCommitChangesDependency()
-{
-  QGIS_PROTECT_QOBJECT_THREAD_ACCESS
-
-  mDataChangedFired = true;
-  reload();
-  mDataChangedFired = false;
-}
-
 bool QgsVectorLayer::setDependencies( const QSet<QgsMapLayerDependency> &oDeps )
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
@@ -6153,7 +6144,7 @@ bool QgsVectorLayer::setDependencies( const QSet<QgsMapLayerDependency> &oDeps )
       disconnect( lyr, &QgsVectorLayer::geometryChanged, this, &QgsVectorLayer::emitDataChanged );
       disconnect( lyr, &QgsVectorLayer::dataChanged, this, &QgsVectorLayer::emitDataChanged );
       disconnect( lyr, &QgsVectorLayer::repaintRequested, this, &QgsVectorLayer::triggerRepaint );
-      disconnect( lyr, &QgsVectorLayer::afterCommitChanges, this, &QgsVectorLayer::onAfterCommitChangesDependency );
+      disconnect( lyr, &QgsVectorLayer::afterCommitChanges, this, &QgsVectorLayer::reload );
     }
   }
 
@@ -6177,7 +6168,7 @@ bool QgsVectorLayer::setDependencies( const QSet<QgsMapLayerDependency> &oDeps )
       connect( lyr, &QgsVectorLayer::geometryChanged, this, &QgsVectorLayer::emitDataChanged );
       connect( lyr, &QgsVectorLayer::dataChanged, this, &QgsVectorLayer::emitDataChanged );
       connect( lyr, &QgsVectorLayer::repaintRequested, this, &QgsVectorLayer::triggerRepaint );
-      connect( lyr, &QgsVectorLayer::afterCommitChanges, this, &QgsVectorLayer::onAfterCommitChangesDependency );
+      connect( lyr, &QgsVectorLayer::afterCommitChanges, this, &QgsVectorLayer::reload );
     }
   }
 
