f435996ffd7bb2b6358395d6300dd6de66ead501
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-08-30 11:45:37 +0200

    ouch

--- tests/src/python/test_qgsgeometry.py
+++ tests/src/python/test_qgsgeometry.py
@@ -6230,7 +6230,7 @@ class TestQgsGeometry(QgisTestCase):
             outer = t[5]
             inner = t[6]
             o1 = QgsGeometry.createWedgeBuffer(point, azimuth, width, outer, inner)
-            o2 = QgsGeometry.createWedgeBufferFromAngles(point, azimuth, width, outer, inner)
+            o2 = QgsGeometry.createWedgeBufferFromAngles(point, startAngle, endAngle, outer, inner)
             exp = t[7]
             result1 = o1.asWkt()
             result2 = o2.asWkt()
