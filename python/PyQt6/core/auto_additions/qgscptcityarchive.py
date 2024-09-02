200f01454da926b171e0f21dd0f07ae3896615e9
Nyall Dawson <nyall.dawson@gmail.com>
2024-09-03 08:58:29 +1000

    Cleanup some CPT city docs

--- python/PyQt6/core/auto_additions/qgscptcityarchive.py
+++ python/PyQt6/core/auto_additions/qgscptcityarchive.py
@@ -7,6 +7,10 @@ QgsCptCityDataItem.AllRamps = QgsCptCityDataItem.Type.AllRamps
 QgsCptCityBrowserModel.Authors = QgsCptCityBrowserModel.ViewType.Authors
 QgsCptCityBrowserModel.Selections = QgsCptCityBrowserModel.ViewType.Selections
 QgsCptCityBrowserModel.List = QgsCptCityBrowserModel.ViewType.List
+try:
+    QgsCptCityDataItem.__attribute_docs__ = {'beginInsertItems': 'Emitted before child items are added to this item.\n\nThis signal *must* be followed by :py:func:`~QgsCptCityDataItem.endInsertItems`.\n\n:param parent: the parent item having children added\n:param first: index of first child item to be added\n:param last: index last child item, after the addition has occurred\n\n.. seealso:: :py:func:`endInsertItems`\n', 'endInsertItems': 'Emitted after child items have been added to this data item.\n\nThis signal will always be preceded by :py:func:`~QgsCptCityDataItem.beginInsertItems`.\n\n.. seealso:: :py:func:`beginInsertItems`\n', 'beginRemoveItems': 'Emitted before child items are removed from this data item.\n\nThis signal *must* be followed by :py:func:`~QgsCptCityDataItem.endRemoveItems`.\n\n:param parent: the parent item having children removed\n:param first: index of first child item to be removed\n:param last: index of the last child item to be removed\n\n.. seealso:: :py:func:`endRemoveItems`\n', 'endRemoveItems': 'Emitted after child items have been removed from this data item.\n\nThis signal will always be preceded by :py:func:`~QgsCptCityDataItem.beginRemoveItems`.\n\n.. seealso:: :py:func:`beginRemoveItems`\n'}
+except NameError:
+    pass
 QgsCptCityArchive.defaultBaseDir = staticmethod(QgsCptCityArchive.defaultBaseDir)
 QgsCptCityArchive.findFileName = staticmethod(QgsCptCityArchive.findFileName)
 QgsCptCityArchive.copyingInfo = staticmethod(QgsCptCityArchive.copyingInfo)
