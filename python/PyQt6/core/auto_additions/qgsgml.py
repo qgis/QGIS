7713f79572f6f27ae995277e09c86cb390d0a532
Nyall Dawson <nyall.dawson@gmail.com>
2024-09-03 08:38:43 +1000

    Some QgsGml doc cleanups

--- python/PyQt6/core/auto_additions/qgsgml.py
+++ python/PyQt6/core/auto_additions/qgsgml.py
@@ -1,5 +1,5 @@
 # The following has been generated automatically from src/core/qgsgml.h
 try:
-    QgsGml.__attribute_docs__ = {'dataProgressAndSteps': 'Also emit signal with progress and totalSteps together (this is better for the status message)\n'}
+    QgsGml.__attribute_docs__ = {'dataReadProgress': 'Emitted when data reading progresses.\n\n:param progress: specifies the number of bytes processed so far\n', 'totalStepsUpdate': 'Emitted when the total number of bytes to read changes.\n\n:param totalSteps: specifies the total number of bytes which must be processed\n', 'dataProgressAndSteps': 'Emitted when data reading progresses or the total number of bytes to read changes.\n\n:param progress: specifies the number of bytes processed so far\n:param totalSteps: specifies the total number of bytes which must be processed\n'}
 except NameError:
     pass
