88ad7cb1a0667103584b6201ddb6b13209c32431
ViperMiniQ <viperminiq@gmail.com>
2024-09-09 18:00:30 +0200

    remove unnecessary const bool ok

--- src/app/qgsvectorlayerdigitizingproperties.cpp
+++ src/app/qgsvectorlayerdigitizingproperties.cpp
@@ -43,9 +43,8 @@ QgsVectorLayerDigitizingPropertiesPage::QgsVectorLayerDigitizingPropertiesPage( 
     mGeometryPrecisionLineEdit->setValidator( new QDoubleValidator( mGeometryPrecisionLineEdit ) );
 
     const double precision( vlayer->geometryOptions()->geometryPrecision() );
-    const bool ok = true;
     QString precisionStr( QLocale().toString( precision, 'g', 15 ) );
-    if ( precision == 0.0 || ! ok )
+    if ( precision == 0.0 )
       precisionStr = QString();
     mGeometryPrecisionLineEdit->setText( precisionStr );
 
