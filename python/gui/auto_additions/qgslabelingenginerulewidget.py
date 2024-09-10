15ed38644b1c4cf1bd1b691cffc7ed4a1bad0a21
Nyall Dawson <nyall.dawson@gmail.com>
2024-09-10 12:46:20 +1000

    Sipify

--- python/gui/auto_additions/qgslabelingenginerulewidget.py
+++ python/gui/auto_additions/qgslabelingenginerulewidget.py
@@ -1,9 +1,6 @@
 # The following has been generated automatically from src/gui/labeling/qgslabelingenginerulewidget.h
 try:
     QgsLabelingEngineRuleWidget.__attribute_docs__ = {'changed': 'Emitted whenever the configuration of the rule is changed.\n'}
-except NameError:
-    pass
-try:
     QgsLabelingEngineRuleWidget.__group__ = ['labeling']
 except NameError:
     pass
