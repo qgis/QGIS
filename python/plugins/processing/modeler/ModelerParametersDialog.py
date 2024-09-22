7b2a8689de064c207c3754f69d6ec584a881f435
Andrea Giudiceandrea <andreaerdna@libero.it>
2024-09-22 23:22:13 +0200

    [gui][pocessing][modeler] Fix window's title

--- python/plugins/processing/modeler/ModelerParametersDialog.py
+++ python/plugins/processing/modeler/ModelerParametersDialog.py
@@ -72,7 +72,7 @@ class ModelerParametersDialog(QDialog):
         self.configuration = configuration
         self.context = createContext()
 
-        self.setWindowTitle(' - '.join([self._alg.group(), self._alg.displayName()]))
+        self.setWindowTitle(' - '.join([self._alg.group(), self._alg.displayName()]) if self._alg.group() else self._alg.displayName())
 
         self.widget = ModelerParametersWidget(alg, model, algName, configuration, context=self.context, dialog=self)
         QgsGui.enableAutoGeometryRestore(self)
