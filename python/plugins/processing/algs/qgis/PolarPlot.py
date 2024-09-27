44d0b0ba38b460b41a69a2ebe3f7ecfff3f7354d
Florian Neukirchen <mail@riannek.de>
2024-09-27 15:36:43 +0200

    Fix AttributeError: module 'plotly.graph_objs' has no attribute 'Area': it is go.Barplot in recent plotly versions

--- python/plugins/processing/algs/qgis/PolarPlot.py
+++ python/plugins/processing/algs/qgis/PolarPlot.py
@@ -89,8 +89,9 @@ class PolarPlot(QgisAlgorithm):
 
         values = vector.values(source, valuefieldname)
 
-        data = [go.Area(r=values[valuefieldname],
-                        t=np.degrees(np.arange(0.0, 2 * np.pi, 2 * np.pi / len(values[valuefieldname]))))]
+        data = [go.Barpolar(r=values[valuefieldname],
+                        theta=np.degrees(np.arange(0.0, 2 * np.pi, 2 * np.pi / len(values[valuefieldname]))))]
+
         plt.offline.plot(data, filename=output, auto_open=False)
 
         return {self.OUTPUT: output}
