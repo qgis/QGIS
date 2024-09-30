2d998f0515bc85e02543b5ddd9b10047391b85c7
Nyall Dawson <nyall.dawson@gmail.com>
2024-09-30 13:17:03 +1000

    Don't restore corrupted sizes/positions when restoring layout items
    This can happen when eg a broken item size causes a nan position,
    which breaks the layout size calculation and results in nan or
    massive x/y values. Restoring these leads to a broken
    layout which cannot be interacted with.
    
    Refs #55240

--- src/core/layout/qgslayoutpoint.cpp
+++ src/core/layout/qgslayoutpoint.cpp
@@ -70,7 +70,16 @@ QgsLayoutPoint QgsLayoutPoint::decodePoint( const QString &string )
   {
     return QgsLayoutPoint();
   }
-  return QgsLayoutPoint( parts[0].toDouble(), parts[1].toDouble(), QgsUnitTypes::decodeLayoutUnit( parts[2] ) );
+  const double x = parts[0].toDouble();
+  const double y = parts[1].toDouble();
+
+  // don't restore corrupted coordinates from xml. This can happen when eg a broken item size causes a nan position,
+  // which breaks the layout size calculation and results in nan or massive x/y values. Restoring these leads to a broken
+  // layout which cannot be interacted with.
+  if ( std::isnan( x ) || std::isnan( y ) || x > 9.99998e+06 || y > 9.99998e+06 )
+    return QgsLayoutPoint();
+
+  return QgsLayoutPoint( x, y, QgsUnitTypes::decodeLayoutUnit( parts[2] ) );
 }
 
 bool QgsLayoutPoint::operator==( const QgsLayoutPoint &other ) const
