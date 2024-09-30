7c569ca7e7ba33e6852170a649813f0f53e2d783
Nyall Dawson <nyall.dawson@gmail.com>
2024-09-30 13:41:02 +1000

    Fix missing canvas variables in some expression editors
    Always use the canvas method to create an expression context
    for expression widgets (when available), as it will always
    contain all the available variables.
    
    Fixes missing variables (eg @animation_*) in some contexts,
    eg in the labeling widget.

--- src/gui/symbology/qgslayerpropertieswidget.cpp
+++ src/gui/symbology/qgslayerpropertieswidget.cpp
@@ -246,22 +246,16 @@ QgsExpressionContext QgsLayerPropertiesWidget::createExpressionContext() const
     return *lExpressionContext;
 
   QgsExpressionContext expContext;
-  expContext << QgsExpressionContextUtils::globalScope()
-             << QgsExpressionContextUtils::projectScope( QgsProject::instance() )
-             << QgsExpressionContextUtils::atlasScope( nullptr );
-
   if ( auto *lMapCanvas = mContext.mapCanvas() )
   {
-    expContext << QgsExpressionContextUtils::mapSettingsScope( lMapCanvas->mapSettings() )
-               << new QgsExpressionContextScope( lMapCanvas->expressionContextScope() );
-    if ( const QgsExpressionContextScopeGenerator *generator = dynamic_cast< const QgsExpressionContextScopeGenerator * >( lMapCanvas->temporalController() ) )
-    {
-      expContext << generator->createExpressionContextScope();
-    }
+    expContext = lMapCanvas->createExpressionContext();
   }
   else
   {
-    expContext << QgsExpressionContextUtils::mapSettingsScope( QgsMapSettings() );
+    expContext << QgsExpressionContextUtils::globalScope()
+               << QgsExpressionContextUtils::projectScope( QgsProject::instance() )
+               << QgsExpressionContextUtils::atlasScope( nullptr )
+               << QgsExpressionContextUtils::mapSettingsScope( QgsMapSettings() );
   }
 
   expContext << QgsExpressionContextUtils::layerScope( mVectorLayer );
