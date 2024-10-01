54865c2e6a2b877a93387c9ea7db5e843cd54907
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-10-01 11:11:39 +0200

    make create annotation map tools available in public API

--- src/gui/annotations/qgscreateannotationitemmaptool_impl.h
+++ src/gui/annotations/qgscreateannotationitemmaptool_impl.h
@@ -16,22 +16,30 @@
 #define QGSCREATEANNOTATIONITEMMAPTOOLIMPL_H
 
 #include "qgis_gui.h"
-#include "qgis_sip.h"
 #include "qgscreateannotationitemmaptool.h"
 #include "qgsmaptoolcapture.h"
-#include "qgssettingstree.h"
 
 #define SIP_NO_FILE
 
 class QgsSettingsEntryString;
 class QgsSettingsTreeNode;
 
-///@cond PRIVATE
 
-class QgsMapToolCaptureAnnotationItem: public QgsMapToolCapture, public QgsCreateAnnotationItemMapToolInterface
+/**
+ * \class QgsMapToolCaptureAnnotationItem
+ * \ingroup gui
+ *
+ * \brief A base class to digitize annotation items using QgsMapToolCapture
+ *
+ * \note Not available in Python bindings.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsMapToolCaptureAnnotationItem: public QgsMapToolCapture, public QgsCreateAnnotationItemMapToolInterface
 {
     Q_OBJECT
   public:
+    //! Constructor
     QgsMapToolCaptureAnnotationItem( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget, CaptureMode mode );
     QgsCreateAnnotationItemMapToolHandler *handler() override;
     QgsMapTool *mapTool() override;
@@ -45,12 +53,20 @@ class QgsMapToolCaptureAnnotationItem: public QgsMapToolCapture, public QgsCreat
 
 };
 
-class QgsCreatePointTextItemMapTool: public QgsMapToolAdvancedDigitizing, public QgsCreateAnnotationItemMapToolInterface
+/**
+ * \class QgsCreatePointTextItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize point text items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreatePointTextItemMapTool: public QgsMapToolAdvancedDigitizing, public QgsCreateAnnotationItemMapToolInterface
 {
     Q_OBJECT
 
   public:
-
+    //! Constructor
     QgsCreatePointTextItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
     ~QgsCreatePointTextItemMapTool() override;
 
@@ -65,49 +81,80 @@ class QgsCreatePointTextItemMapTool: public QgsMapToolAdvancedDigitizing, public
 };
 
 
-class QgsCreateMarkerItemMapTool: public QgsMapToolCaptureAnnotationItem
+/**
+ * \class QgsCreateMarkerItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize marker items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreateMarkerItemMapTool: public QgsMapToolCaptureAnnotationItem
 {
     Q_OBJECT
 
   public:
-
+    //! Constructor
     QgsCreateMarkerItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
 
     void cadCanvasReleaseEvent( QgsMapMouseEvent *event ) override;
 
 };
 
-class QgsCreateLineItemMapTool: public QgsMapToolCaptureAnnotationItem
+/**
+ * \class QgsCreateLineItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize line items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreateLineItemMapTool: public QgsMapToolCaptureAnnotationItem
 {
     Q_OBJECT
 
   public:
-
+    //! Constructor
     QgsCreateLineItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
 
   private slots:
     void lineCaptured( const QgsCurve *line ) override;
 };
 
-class QgsCreatePolygonItemMapTool: public QgsMapToolCaptureAnnotationItem
+/**
+ * \class QgsCreatePolygonItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize polygon items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreatePolygonItemMapTool: public QgsMapToolCaptureAnnotationItem
 {
     Q_OBJECT
 
   public:
-
+    //! Constructor
     QgsCreatePolygonItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
 
   private slots:
     void polygonCaptured( const QgsCurvePolygon *polygon ) override;
 };
 
-
-class QgsCreateRectangleTextItemMapTool: public QgsMapToolAdvancedDigitizing, public QgsCreateAnnotationItemMapToolInterface
+/**
+ * \class QgsCreateRectangleTextItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize rectangle text items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreateRectangleTextItemMapTool: public QgsMapToolAdvancedDigitizing, public QgsCreateAnnotationItemMapToolInterface
 {
     Q_OBJECT
 
   public:
-
+    //! Constructor
     QgsCreateRectangleTextItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
 
     void cadCanvasPressEvent( QgsMapMouseEvent *event ) override;
@@ -118,7 +165,6 @@ class QgsCreateRectangleTextItemMapTool: public QgsMapToolAdvancedDigitizing, pu
     QgsMapTool *mapTool() override;
 
   private:
-
     QgsCreateAnnotationItemMapToolHandler *mHandler = nullptr;
 
     QRectF mRect;
@@ -126,8 +172,15 @@ class QgsCreateRectangleTextItemMapTool: public QgsMapToolAdvancedDigitizing, pu
     QObjectUniquePtr< QgsRubberBand > mRubberBand;
 };
 
-
-class QgsCreatePictureItemMapTool: public QgsMapToolAdvancedDigitizing, public QgsCreateAnnotationItemMapToolInterface
+/**
+ * \class QgsCreatePictureItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize picture items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreatePictureItemMapTool: public QgsMapToolAdvancedDigitizing, public QgsCreateAnnotationItemMapToolInterface
 {
     Q_OBJECT
 
@@ -135,6 +188,7 @@ class QgsCreatePictureItemMapTool: public QgsMapToolAdvancedDigitizing, public Q
     static inline QgsSettingsTreeNode *sTreePicture = QgsCreateAnnotationItemMapToolInterface::sTreeAnnotationTools->createChildNode( QStringLiteral( "picture-item" ) );
     static const QgsSettingsEntryString *settingLastSourceFolder;
 
+    //! Constructor
     QgsCreatePictureItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
 
     void cadCanvasPressEvent( QgsMapMouseEvent *event ) override;
@@ -145,7 +199,7 @@ class QgsCreatePictureItemMapTool: public QgsMapToolAdvancedDigitizing, public Q
     QgsMapTool *mapTool() override;
 
   private:
-
+    //! Constructor
     QgsCreateAnnotationItemMapToolHandler *mHandler = nullptr;
 
     QRectF mRect;
@@ -153,19 +207,25 @@ class QgsCreatePictureItemMapTool: public QgsMapToolAdvancedDigitizing, public Q
     QObjectUniquePtr< QgsRubberBand > mRubberBand;
 };
 
-
-class QgsCreateLineTextItemMapTool: public QgsMapToolCaptureAnnotationItem
+/**
+ * \class QgsCreateLineTextItemMapTool
+ * \ingroup gui
+ *
+ * \brief A map tool to digitize line text items.
+ *
+ * \since QGIS 3.40
+ */
+class GUI_EXPORT QgsCreateLineTextItemMapTool: public QgsMapToolCaptureAnnotationItem
 {
     Q_OBJECT
 
   public:
-
+    //! Constructor
     QgsCreateLineTextItemMapTool( QgsMapCanvas *canvas, QgsAdvancedDigitizingDockWidget *cadDockWidget );
 
   private slots:
     void lineCaptured( const QgsCurve *line ) override;
 };
 
-///@endcond PRIVATE
 
 #endif // QGSCREATEANNOTATIONITEMMAPTOOLIMPL_H
