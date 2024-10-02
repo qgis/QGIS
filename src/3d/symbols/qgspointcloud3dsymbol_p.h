58e47f19f4960daf4d7b4170fd1d1c40bc00e0d4
Martin Dobias <wonder.sk@gmail.com>
2024-10-02 22:06:46 +0200

    Make coordinates in point cloud chunks independent from scene origin
    Until now, coordinates of chunks of point clouds were using "world" coordinates,
    i.e. they were relative to the 3D scene origin point. As a part of preparation
    for large scene support (QEP #301), we need coordinates inside chunks to be
    independent from the scene origin point. For each point cloud chunk, we pick
    origin point as a corner of the chunk's 3D box, which should guarantee that
    coordinates within chunk (floats) will have reasonable numerical precision.
    
    I have also used the opportunity to simplify the code that handles point cloud
    chunk coordinates: rather than having flipped coordinates (x,-z,y) inside chunks,
    we are using map coordinates without flipping (x,y,z), only relative to the origin
    point of the chunk. The flipping of axes is handled by QTransform attached
    to each chunk. (Hopefully when all chunked entity implementations get updated,
    we will be able to remove all the axis flipping everywhere by just removing
    the rotation in associated QTransforms.)
    
    There's also a fix for a crash, when point cloud uses coloring based on classification
    and triangulation is enabled (the pointSize array was not getting populated for parent
    nodes).

--- src/3d/symbols/qgspointcloud3dsymbol_p.h
+++ src/3d/symbols/qgspointcloud3dsymbol_p.h
@@ -36,15 +36,17 @@
 class IndexedPointCloudNode;
 class QgsAABB;
 
-class QgsPointCloud3DSymbolHandler // : public QgsFeature3DHandler
+class QgsPointCloud3DSymbolHandler
 {
   public:
     QgsPointCloud3DSymbolHandler();
 
     virtual ~QgsPointCloud3DSymbolHandler() = default;
 
+    struct PointData;
+
     virtual bool prepare( const QgsPointCloud3DRenderContext &context ) = 0;// override;
-    virtual void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context ) = 0; // override;
+    virtual void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context, PointData *output = nullptr ) = 0; // override;
     virtual void finalize( Qt3DCore::QEntity *parent, const QgsPointCloud3DRenderContext &context ) = 0;// override;
 
     void triangulate( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context, const QgsAABB &bbox );
@@ -55,7 +57,8 @@ class QgsPointCloud3DSymbolHandler // : public QgsFeature3DHandler
     //! temporary data we will pass to the tessellator
     struct PointData
     {
-      QVector<QVector3D> positions;  // Contains triplets of float x,y,z for each point
+      QgsVector3D positionsOrigin;   // All "positions" are relative to this point, defined in map coordinates (with double precision)
+      QVector<QVector3D> positions;  // Contains triplets of float x,y,z for each point. These are in map coordinates, relative to "positionsOrigin"
       QVector<float> parameter;
       QVector<float> pointSizes; // Contains point sizes, in case they are overridden for classification renderer
       QVector<QVector3D> colors;
@@ -103,7 +106,7 @@ class QgsSingleColorPointCloud3DSymbolHandler : public QgsPointCloud3DSymbolHand
     QgsSingleColorPointCloud3DSymbolHandler();
 
     bool prepare( const QgsPointCloud3DRenderContext &context ) override;
-    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context ) override;
+    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context, PointData *output = nullptr ) override;
     void finalize( Qt3DCore::QEntity *parent, const QgsPointCloud3DRenderContext &context ) override;
 
   private:
@@ -120,7 +123,7 @@ class QgsColorRampPointCloud3DSymbolHandler : public QgsPointCloud3DSymbolHandle
     QgsColorRampPointCloud3DSymbolHandler();
 
     bool prepare( const QgsPointCloud3DRenderContext &context ) override;
-    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context ) override;
+    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context, PointData *output = nullptr ) override;
     void finalize( Qt3DCore::QEntity *parent, const QgsPointCloud3DRenderContext &context ) override;
 
   private:
@@ -137,7 +140,7 @@ class QgsRGBPointCloud3DSymbolHandler : public QgsPointCloud3DSymbolHandler
     QgsRGBPointCloud3DSymbolHandler();
 
     bool prepare( const QgsPointCloud3DRenderContext &context ) override;
-    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context ) override;
+    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context, PointData *output = nullptr ) override;
     void finalize( Qt3DCore::QEntity *parent, const QgsPointCloud3DRenderContext &context ) override;
 
   private:
@@ -154,7 +157,7 @@ class QgsClassificationPointCloud3DSymbolHandler : public QgsPointCloud3DSymbolH
     QgsClassificationPointCloud3DSymbolHandler();
 
     bool prepare( const QgsPointCloud3DRenderContext &context ) override;
-    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context ) override;
+    void processNode( QgsPointCloudIndex *pc, const IndexedPointCloudNode &n, const QgsPointCloud3DRenderContext &context, PointData *output = nullptr ) override;
     void finalize( Qt3DCore::QEntity *parent, const QgsPointCloud3DRenderContext &context ) override;
 
   private:
