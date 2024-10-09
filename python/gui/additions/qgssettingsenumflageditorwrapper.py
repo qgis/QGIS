c2ef9ea12564c4d8bba236a5313264a4344951d8
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-10-09 12:11:44 +0200

    set editor only if it is a combobox

--- python/gui/additions/qgssettingsenumflageditorwrapper.py
+++ python/gui/additions/qgssettingsenumflageditorwrapper.py
@@ -73,8 +73,8 @@ class PyQgsSettingsEnumEditorWidgetWrapper(QgsSettingsEditorWidgetWrapper):
 
     def configureEditorPrivate(self, editor: QWidget, setting: QgsSettingsEntryBase):
         self.setting = setting
-        self.editor = editor
-        if editor is not None:
+        if isinstance(editor, QComboBox):
+            self.editor = editor
             for i in range(self.setting.metaEnum().keyCount()):
                 value = self.setting.metaEnum().value(i)
                 key = self.setting.metaEnum().key(i)
