56e58a2f7470e6c566cea976ffc9cf6eb40d36ac
Nyall Dawson <nyall.dawson@gmail.com>
2024-10-15 11:13:57 +1000

    Remove duplicate inherited members

--- python/PyQt6/core/auto_additions/qgsdatabaseschemaitem.py
+++ python/PyQt6/core/auto_additions/qgsdatabaseschemaitem.py
@@ -1,6 +1,5 @@
 # The following has been generated automatically from src/core/browser/qgsdatabaseschemaitem.h
 try:
-    QgsDatabaseSchemaItem.iconDataCollection = staticmethod(QgsDatabaseSchemaItem.iconDataCollection)
     QgsDatabaseSchemaItem.__group__ = ['browser']
 except NameError:
     pass
