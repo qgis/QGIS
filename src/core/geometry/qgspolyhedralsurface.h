5fc6ceb91f3adf7f06725f75cbdd43396439e911
Nyall Dawson <nyall.dawson@gmail.com>
2024-10-15 11:15:05 +1000

    Suppress some duplInheritedMember warnings

--- src/core/geometry/qgspolyhedralsurface.h
+++ src/core/geometry/qgspolyhedralsurface.h
@@ -306,7 +306,7 @@ class CORE_EXPORT QgsPolyhedralSurface: public QgsSurface
      *
      * \note Not available in Python. Objects will be automatically be converted to the appropriate target type.
      */
-    inline static const QgsPolyhedralSurface *cast( const QgsAbstractGeometry *geom )
+    inline static const QgsPolyhedralSurface *cast( const QgsAbstractGeometry *geom ) // cppcheck-suppress duplInheritedMember
     {
       if ( !geom )
         return nullptr;
