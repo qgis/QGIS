5ee3e418a37b6ba02297436876a162cbc1a12d23
Mauro Bettella <bettellam@gmail.com>
2024-10-19 11:45:51 +0200

    Update parse.c update talkerId[1] GSV

--- external/nmea/parse.c
+++ external/nmea/parse.c
@@ -404,7 +404,7 @@ int nmea_parse_GPGSV( const char *buff, int buff_sz, nmeaGPGSV *pack )
     return 0;
   }
 
-  if ( pack->pack_type != 'P' && pack->pack_type != 'N' && pack->pack_type != 'L' && pack->pack_type != 'A' && pack->pack_type != 'B' && pack->pack_type != 'Q' )
+  if ( pack->talkerId[1] != 'P' && pack->talkerId[1] != 'N' && pack->talkerId[1] != 'L' && pack->talkerId[1] != 'A' && pack->talkerId[1] != 'B' && pack->talkerId[1] != 'Q' )
   {
     nmea_error( "GSV parse error!" );
     return 0;
