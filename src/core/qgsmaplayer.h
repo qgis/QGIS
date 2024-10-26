06c8de94dd6e0c0577147567d4ad776c3509bacc
Matthias Kuhn <matthias@opengis.ch>
2024-10-26 22:38:02 +0200

    Remove unneeded declare metatype

--- src/core/qgsmaplayer.h
+++ src/core/qgsmaplayer.h
@@ -2486,7 +2486,6 @@ class CORE_EXPORT QgsMapLayer : public QObject
     friend class TestQgsMapLayer;
 };
 
-Q_DECLARE_METATYPE( QgsMapLayer * )
 Q_DECLARE_OPERATORS_FOR_FLAGS( QgsMapLayer::LayerFlags )
 Q_DECLARE_OPERATORS_FOR_FLAGS( QgsMapLayer::StyleCategories )
 Q_DECLARE_OPERATORS_FOR_FLAGS( QgsMapLayer::ReadFlags )
