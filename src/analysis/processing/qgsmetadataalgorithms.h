65a8df1be6190f77fd68a37da312ab29fc129d24
Alexander Bruy <alexander.bruy@gmail.com>
2024-11-10 12:21:11 +0000

    add native set layer metadata algorithm

--- src/analysis/processing/qgsmetadataalgorithms.h
+++ src/analysis/processing/qgsmetadataalgorithms.h
@@ -52,6 +52,33 @@ class QgsCopyLayerMetadataAlgorithm : public QgsProcessingAlgorithm
     QString mLayerId;
 };
 
+/**
+ * Native apply layer metadata algorithm.
+ */
+class QgsApplyLayerMetadataAlgorithm : public QgsProcessingAlgorithm
+{
+  public:
+    QgsApplyLayerMetadataAlgorithm() = default;
+    QString name() const override;
+    QString displayName() const override;
+    QStringList tags() const override;
+    QString group() const override;
+    QString groupId() const override;
+    QString shortHelpString() const override;
+    void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) override;
+    QgsApplyLayerMetadataAlgorithm *createInstance() const override SIP_FACTORY;
+
+  protected:
+
+    bool prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+    QVariantMap processAlgorithm( const QVariantMap &parameters,
+                                  QgsProcessingContext &context, QgsProcessingFeedback * ) override;
+
+  private:
+
+    QString mLayerId;
+};
+
 ///@endcond PRIVATE
 
 #endif // QGSMETADATAALGORITHMS_H
