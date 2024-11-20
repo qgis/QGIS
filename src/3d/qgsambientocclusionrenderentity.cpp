c998b46d53854115179819b555e7aacfef30177f
Nyall Dawson <nyall.dawson@gmail.com>
2024-11-20 15:41:41 +1000

    Fix ambient occlusion kernel generation

--- src/3d/qgsambientocclusionrenderentity.cpp
+++ src/3d/qgsambientocclusionrenderentity.cpp
@@ -70,7 +70,7 @@ QgsAmbientOcclusionRenderEntity::QgsAmbientOcclusionRenderEntity( Qt3DRender::QT
       randomFloats( generator ) * 2.0 - 1.0
     );
     sample.normalize();
-    float scale = i / kernelSize;
+    float scale = static_cast< float >( i ) / kernelSize;
     scale = 0.1 + 0.9 * scale * scale;
     sample *= scale;
     ssaoKernelValues.push_back( sample );
