8f57b608e75fb3a668a01571874f9223ba850a10
Nyall Dawson <nyall.dawson@gmail.com>
2024-11-26 10:41:47 +1000

    Fix use of inline data for SVG content
    Fixes #59575

--- src/core/qgsabstractcontentcache.cpp
+++ src/core/qgsabstractcontentcache.cpp
@@ -45,15 +45,29 @@ void QgsAbstractContentCacheBase::onRemoteContentFetched( const QString &, bool 
 
 bool QgsAbstractContentCacheBase::parseBase64DataUrl( const QString &path, QString *mimeType, QString *data )
 {
-  const thread_local QRegularExpression sRx( QStringLiteral( "^data:(.*/.*?);?(?:base64)?,(.*)$" ) );
+  const thread_local QRegularExpression sRx( QStringLiteral( "^data:([a-zA-Z0-9+]*\\/[a-zA-Z0-9+]*?)(?:;(base64|utf8))?,(.*)$" ) );
   const QRegularExpressionMatch base64Match = sRx.match( path );
   if ( !base64Match.hasMatch() )
     return false;
 
+  const QString typeMatch = base64Match.captured( 2 );
+  const QString mimeMatch = base64Match.captured( 1 );
+
   if ( mimeType )
-    *mimeType = base64Match.captured( 1 );
+    *mimeType = mimeMatch;
   if ( data )
-    *data = base64Match.captured( 2 );
+    *data = base64Match.captured( 3 );
+
+  if ( typeMatch == QLatin1String( "base64" ) )
+    return true; // definitely base 64
+  else if ( typeMatch == QLatin1String( "utf8" ) )
+    return false; // definitely NOT base 64
+
+  // if we aren't certain it's base 64, and it has an xml mime type, assume it's not.
+  // see https://github.com/qgis/QGIS/issues/59575
+  if ( mimeMatch.endsWith( QLatin1String( "xml" ) ) || mimeMatch.endsWith( QLatin1String( "svg" ) ) )
+    return false;
+
   return true;
 }
 
