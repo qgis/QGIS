d70449e202264eee8dcb9c5b09456d970b5f3482
Nyall Dawson <nyall.dawson@gmail.com>
2024-11-27 13:08:18 +1000

    Resync O2 library embedded copy

--- external/o2/src/o2requestor.h
+++ external/o2/src/o2requestor.h
@@ -7,6 +7,7 @@
 #include <QNetworkAccessManager>
 #include <QUrl>
 #include <QByteArray>
+#include <QHttpMultiPart>
 
 #include "o0export.h"
 #include "o2reply.h"
@@ -18,25 +19,67 @@ class O0_EXPORT O2Requestor: public QObject {
     Q_OBJECT
 
 public:
-    explicit O2Requestor(QNetworkAccessManager *manager, O2 *authenticator, QObject *parent = 0);
-    ~O2Requestor();
+    explicit O2Requestor(QNetworkAccessManager *manager, O2 *authenticator, QObject *parent = nullptr);
+    ~O2Requestor() override;
+    
+    
+    /// Some services require the access token to be sent as a Authentication HTTP header
+    /// and refuse requests with the access token in the query.
+    /// This function allows to use or ignore the access token in the query.
+    /// The default value of `true` means that the query will contain the access token.
+    /// By setting the value to false, the query will not contain the access token.
+    /// See:
+    /// https://tools.ietf.org/html/draft-ietf-oauth-v2-bearer-16#section-4.3
+    /// https://tools.ietf.org/html/rfc6750#section-2.3
+    
+    void setAddAccessTokenInQuery(bool value);
+
+    /// Some services require the access token to be sent as a Authentication HTTP header.
+    /// This is the case for Twitch and Mixer.
+    /// When the access token expires and is refreshed, O2Requestor::retry() needs to update the Authentication HTTP header.
+    /// In order to do so, O2Requestor needs to know the format of the Authentication HTTP header.
+    void setAccessTokenInAuthenticationHTTPHeaderFormat(const QString &value);
 
 public Q_SLOTS:
     /// Make a GET request.
     /// @return Request ID or -1 if there are too many requests in the queue.
-    int get(const QNetworkRequest &req);
+    int get(const QNetworkRequest &req, int timeout = 60*1000);
 
     /// Make a POST request.
     /// @return Request ID or -1 if there are too many requests in the queue.
-    int post(const QNetworkRequest &req, const QByteArray &data);
+    int post(const QNetworkRequest &req, const QByteArray &data, int timeout = 60*1000);
+    int post(const QNetworkRequest &req, QHttpMultiPart* data, int timeout = 60*1000);
 
     /// Make a PUT request.
     /// @return Request ID or -1 if there are too many requests in the queue.
-    int put(const QNetworkRequest &req, const QByteArray &data);
+    int put(const QNetworkRequest &req, const QByteArray &data, int timeout = 60*1000);
+    int put(const QNetworkRequest &req, QHttpMultiPart* data, int timeout = 60*1000);
+
+    /// Make a DELETE request.
+    /// @return Request ID or -1 if there are too many requests in the queue.
+    int deleteResource(const QNetworkRequest &req, int timeout = 60*1000);
+
+    /// Make a HEAD request.
+    /// @return Request ID or -1 if there are too many requests in the queue.
+    int head(const QNetworkRequest &req, int timeout = 60*1000);
+
+    /// Make a custom request.
+    /// @return Request ID or -1 if there are too many requests in the queue.
+    int customRequest(const QNetworkRequest &req, const QByteArray &verb, const QByteArray &data, int timeout = 60*1000);
 
 Q_SIGNALS:
+
     /// Emitted when a request has been completed or failed.
     void finished(int id, QNetworkReply::NetworkError error, QByteArray data);
+
+    /// Emitted when a request has been completed or failed.
+    void finished(int id, QNetworkReply::NetworkError error, QString errorText, QByteArray data);
+
+    /// Emitted when a request has been completed or failed. Also reply headers will be provided.
+    void finished(int id, QNetworkReply::NetworkError error, QByteArray data, QList<QNetworkReply::RawHeaderPair> headers);
+
+    /// Emitted when a request has been completed or failed. Also reply headers will be provided.
+    void finished(int id, QNetworkReply::NetworkError error, QString errorText, QByteArray data, QList<QNetworkReply::RawHeaderPair> headers);
 
     /// Emitted when an upload has progressed.
     void uploadProgress(int id, qint64 bytesSent, qint64 bytesTotal);
@@ -61,23 +104,27 @@ protected Q_SLOTS:
     void onUploadProgress(qint64 uploaded, qint64 total);
 
 protected:
-    int setup(const QNetworkRequest &request, QNetworkAccessManager::Operation operation);
+    int setup(const QNetworkRequest &request, QNetworkAccessManager::Operation operation, const QByteArray &verb = QByteArray());
 
     enum Status {
         Idle, Requesting, ReRequesting
     };
 
-    QNetworkAccessManager *manager_;
-    O2 *authenticator_;
+    QNetworkAccessManager *manager_{nullptr};
+    O2 *authenticator_{nullptr};
     QNetworkRequest request_;
     QByteArray data_;
-    QNetworkReply *reply_;
-    Status status_;
-    int id_;
-    QNetworkAccessManager::Operation operation_;
+    QHttpMultiPart* multipartData_{nullptr};
+    QNetworkReply *reply_{nullptr};
+    Status status_{Idle};
+    int id_{1};
+    QNetworkAccessManager::Operation operation_{QNetworkAccessManager::GetOperation};
     QUrl url_;
     O2ReplyList timedReplies_;
-    QNetworkReply::NetworkError error_;
+    QNetworkReply::NetworkError error_{QNetworkReply::NoError};
+    bool addAccessTokenInQuery_{true};
+    QString accessTokenInAuthenticationHTTPHeaderFormat_;
+    bool rawData_{false};
 };
 
 #endif // O2REQUESTOR_H
--- external/o2/src/o2uber.cpp
+++ external/o2/src/o2uber.cpp
@@ -6,30 +6,33 @@
 #include <QString>
 #include <QStringList>
 #include <QUrl>
-#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#if QT_VERSION >= 0x050000
 #include <QUrlQuery>
 #endif
 
-#include "o2facebook.h"
+#include "o2uber.h"
 #include "o0globals.h"
 
-static const char *FbEndpoint = "https://graph.facebook.com/oauth/authorize?display=touch";
-static const char *FbTokenUrl = "https://graph.facebook.com/oauth/access_token";
-static const char *FbExpiresKey = "expires_in";
+static const char *UberEndpoint = "https://login.uber.com/oauth/v2/authorize";
+static const char *UberTokenUrl = "https://login.uber.com/oauth/v2/token";
+static const char *UberExpiresIn = "expires_in";
+static const char *UberGrantType = "authorization_code";
 
-O2Facebook::O2Facebook(QObject *parent): O2(parent) {
-    setRequestUrl(FbEndpoint);
-    setTokenUrl(FbTokenUrl);
+O2Uber::O2Uber(QObject *parent): O2(parent)
+{
+    setRequestUrl(UberEndpoint);
+    setTokenUrl(UberTokenUrl);
 }
 
-void O2Facebook::onVerificationReceived(const QMap<QString, QString> response) {
-    qDebug() << "O2Facebook::onVerificationReceived: Emitting closeBrowser()";
+void O2Uber::onVerificationReceived(const QMap<QString, QString> response){
+
+    qDebug() << "O2Uber::onVerificationReceived: Emitting closeBrowser()";
     Q_EMIT closeBrowser();
 
     if (response.contains("error")) {
-        qWarning() << "O2Facebook::onVerificationReceived: Verification failed";
-        for (auto it = response.constBegin(); it != response.constEnd(); ++it) {
-            qWarning() << "O2Facebook::onVerificationReceived:" << it.key() << it.value();
+        qWarning() << "O2Uber::onVerificationReceived: Verification failed";
+        for (auto it=response.constBegin(); it!=response.constEnd(); ++it) {
+            qWarning() << "O2Uber::onVerificationReceived:" << it.key() << it.value();
         }
         Q_EMIT linkingFailed();
         return;
@@ -43,32 +46,35 @@ void O2Facebook::onVerificationReceived(const QMap<QString, QString> response) {
 #if QT_VERSION < QT_VERSION_CHECK(5,0,0)
     url.addQueryItem(O2_OAUTH2_CLIENT_ID, clientId_);
     url.addQueryItem(O2_OAUTH2_CLIENT_SECRET, clientSecret_);
-    url.addQueryItem(O2_OAUTH2_SCOPE, scope_);
-    url.addQueryItem(O2_OAUTH2_GRANT_TYPE_CODE, code());
+    url.addQueryItem(O2_OAUTH2_GRANT_TYPE, UberGrantType);
     url.addQueryItem(O2_OAUTH2_REDIRECT_URI, redirectUri_);
+    url.addQueryItem(O2_OAUTH2_GRANT_TYPE_CODE, code());
+    url.addQueryItem(O2_OAUTH2_SCOPE, scope_);
 #else
     QUrlQuery query(url);
     query.addQueryItem(O2_OAUTH2_CLIENT_ID, clientId_);
     query.addQueryItem(O2_OAUTH2_CLIENT_SECRET, clientSecret_);
-    query.addQueryItem(O2_OAUTH2_SCOPE, scope_);
-    query.addQueryItem(O2_OAUTH2_GRANT_TYPE_CODE, code());
+    query.addQueryItem(O2_OAUTH2_GRANT_TYPE, UberGrantType);
     query.addQueryItem(O2_OAUTH2_REDIRECT_URI, redirectUri_);
+    query.addQueryItem(O2_OAUTH2_GRANT_TYPE_CODE, code());
+    query.addQueryItem(O2_OAUTH2_SCOPE, scope_);
     url.setQuery(query);
 #endif
 
     QNetworkRequest tokenRequest(url);
-    QNetworkReply *tokenReply = manager_->get(tokenRequest);
+    tokenRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QNetworkReply *tokenReply = manager_->post(tokenRequest, QByteArray());
     timedReplies_.add(tokenReply);
-    connect(tokenReply, &QNetworkReply::finished, this, &O2Facebook::onTokenReplyFinished, Qt::QueuedConnection);
+    connect(tokenReply, &QNetworkReply::finished, this, &O2Uber::onTokenReplyFinished, Qt::QueuedConnection);
 #if QT_VERSION < QT_VERSION_CHECK(5,15,0)
     connect(tokenReply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(onTokenReplyError(QNetworkReply::NetworkError)), Qt::QueuedConnection);
 #else
-    connect(tokenReply, &QNetworkReply::errorOccurred, this, &O2Facebook::onTokenReplyError, Qt::QueuedConnection);
+    connect(tokenReply, &QNetworkReply::errorOccurred, this, &O2Uber::onTokenReplyError, Qt::QueuedConnection);
 #endif
 }
 
-void O2Facebook::onTokenReplyFinished() {
-    qDebug() << "O2Facebook::onTokenReplyFinished";
+void O2Uber::onTokenReplyFinished(){
+    qDebug() << "O2Uber::onTokenReplyFinished";
 
     QNetworkReply *tokenReply = qobject_cast<QNetworkReply *>(sender());
     if (tokenReply->error() == QNetworkReply::NoError) {
@@ -85,13 +91,13 @@ void O2Facebook::onTokenReplyFinished() {
 
         // Interpret reply
         setToken(reply.value(O2_OAUTH2_ACCESS_TOKEN, QString()).toString());
-        setExpires(reply.value(FbExpiresKey).toInt());
+        setExpires(reply.value(UberExpiresIn).toInt());
         setRefreshToken(reply.value(O2_OAUTH2_REFRESH_TOKEN, QString()).toString());
         setExtraTokens(reply);
         timedReplies_.remove(tokenReply);
         setLinked(true);
         Q_EMIT linkingSucceeded();
     } else {
-        qWarning() << "O2Facebook::onTokenReplyFinished:" << tokenReply->errorString();
+        qWarning() << "O2Uber::onTokenReplyFinished:" << tokenReply->errorString();
     }
 }
