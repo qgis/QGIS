cf868f8f4cc5f883b7606510f95e6ed8211d5629
Even Rouault <even.rouault@spatialys.com>
2024-11-30 13:56:00 +0100

    QgsMapLayerConfigWidget/QgsRendererRasterPropertiesWidget: suppress -Woverloaded-virtual warning related to syncToLayer() method

--- src/gui/raster/qgsrendererrasterpropertieswidget.h
+++ src/gui/raster/qgsrendererrasterpropertieswidget.h
@@ -67,9 +67,9 @@ class GUI_EXPORT QgsRendererRasterPropertiesWidget : public QgsMapLayerConfigWid
     void apply() override;
 
 
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Woverloaded-virtual"
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
 #endif
 
     /**
@@ -77,8 +77,8 @@ class GUI_EXPORT QgsRendererRasterPropertiesWidget : public QgsMapLayerConfigWid
      * \param layer The layer to use for the widget
      */
     void syncToLayer( QgsRasterLayer *layer );
-#ifdef __clang__
-#pragma clang diagnostic pop
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
 #endif
 
   private slots:
