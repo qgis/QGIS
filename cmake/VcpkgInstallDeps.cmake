1bfb698e2a903b1ac3967c768553e225f4bc30e6
Matthias Kuhn <matthias@opengis.ch>
2025-01-04 11:34:23 +0100

    Fix bundle paths

--- cmake/VcpkgInstallDeps.cmake
+++ cmake/VcpkgInstallDeps.cmake
@@ -11,7 +11,7 @@ if(MSVC)
   install(FILES ${ALL_LIBS} DESTINATION "bin")
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
   file(GLOB ALL_LIBS
-    "${VCPKG_BASE_DIR}/bin/*.dylib"
+    "${VCPKG_BASE_DIR}/lib/*.dylib"
   )
   install(FILES ${ALL_LIBS} DESTINATION "${QGIS_LIB_SUBDIR}")
 endif()
@@ -25,19 +25,23 @@ endif()
 install(DIRECTORY "${PROJ_DATA_PATH}/" DESTINATION "${QGIS_DATA_SUBDIR}/proj")
 install(DIRECTORY "${VCPKG_BASE_DIR}/share/gdal/" DESTINATION "${QGIS_DATA_SUBDIR}/gdal")
 install(DIRECTORY "${VCPKG_BASE_DIR}/bin/Qca/" DESTINATION "${QGIS_LIB_SUBDIR}/Qca") # QCA plugins
+
 if(MSVC)
   install(DIRECTORY "${VCPKG_BASE_DIR}/Qt6/" DESTINATION "${QGIS_LIB_SUBDIR}/Qt6") # qt plugins (qml and others)
 else()
   install(DIRECTORY "${VCPKG_BASE_DIR}/Qt6/" DESTINATION "${APP_PLUGINS_DIR}/") # qt plugins (qml and others)
 endif()
+
 if(WITH_BINDINGS)
-  # TODO: validate on windows
   if(MSVC)
+    set(_SOURCE_PYTHON_DIR "${VCPKG_BASE_DIR}/tools/python3/")
+    set(_TARGET_PYTHON_DIR "bin")
   elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    cmake_path(GET Python_SITEARCH PARENT_PATH _PYTHON_DIR)
-    
-    install(DIRECTORY "${_PYTHON_DIR}"
-      DESTINATION "${APP_FRAMEWORKS_DIR}/lib"
-      PATTERN "*.sip" EXCLUDE)
+    cmake_path(GET Python_SITEARCH PARENT_PATH _SOURCE_PYTHON_DIR)
+    set(_TARGET_PYTHON_DIR "${APP_FRAMEWORKS_DIR}/lib")
   endif()
+
+  install(DIRECTORY "${_SOURCE_PYTHON_DIR}"
+    DESTINATION "${_TARGET_PYTHON_DIR}"
+    PATTERN "*.sip" EXCLUDE)
 endif()
