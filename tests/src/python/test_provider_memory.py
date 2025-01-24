c003cab5ceff49ced99fd11c0d2baff3c4f9ce0b
Alessandro Pasotti <elpaso@itopen.it>
2025-01-24 16:52:40 +0100

    memory layer expand test case

--- tests/src/python/test_provider_memory.py
+++ tests/src/python/test_provider_memory.py
@@ -739,9 +739,20 @@ class TestPyQgsMemoryProvider(QgisTestCase, ProviderTestCase):
 
         layer = QgsMemoryProviderUtils.createMemoryLayer("my name", QgsFields())
         self.assertTrue(layer.isValid())
+        self.assertFalse(layer.isSpatial())
         self.assertFalse(layer.dataProvider().capabilities() & Qgis.VectorProviderCapability.ChangeGeometries)
         self.assertFalse(layer.dataProvider().capabilities() & Qgis.VectorProviderCapability.CircularGeometries)
         self.assertFalse(layer.dataProvider().capabilities() & Qgis.VectorProviderCapability.CreateSpatialIndex)
+
+    def testSpatialLayerHasGeometryRelatedCapabilities(self):
+
+        layer = QgsMemoryProviderUtils.createMemoryLayer("my name", QgsFields(), QgsWkbTypes.Type.Point)
+        self.assertTrue(layer.isValid())
+        self.assertTrue(layer.isSpatial())
+        self.assertTrue(layer.dataProvider().capabilities() & Qgis.VectorProviderCapability.ChangeGeometries)
+        self.assertTrue(layer.dataProvider().capabilities() & Qgis.VectorProviderCapability.CircularGeometries)
+        self.assertTrue(layer.dataProvider().capabilities() & Qgis.VectorProviderCapability.CreateSpatialIndex)
+
 
     def testCreateMemoryLayer(self):
         """
