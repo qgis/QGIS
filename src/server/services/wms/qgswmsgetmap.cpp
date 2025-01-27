addb0b83160c1cb7300e3192190928b48addb8bb
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2025-01-27 14:17:24 +0100

    fix(qgisserver): add more feedback checkpoints

--- src/server/services/wms/qgswmsgetmap.cpp
+++ src/server/services/wms/qgswmsgetmap.cpp
@@ -52,6 +52,11 @@ namespace QgsWms
     QgsRenderer renderer( context );
     std::unique_ptr<QImage> result( renderer.getMap() );
 
+    if ( response.feedback() && response.feedback()->isCanceled() )
+    {
+      throw QgsServerException( QStringLiteral( "Remote socket has been closed!" ) );
+    }
+
     if ( result )
     {
       const QString format = request.parameters().value( QStringLiteral( "FORMAT" ), QStringLiteral( "PNG" ) );
