c4c27c68c909906ea2215bf96ce9cb4a56be04a7
Alessandro Pasotti <elpaso@itopen.it>
2025-01-29 15:28:50 +0100

    [server][wms] Add displayName to GFI JSON response
    In the gray zone between a feature and a bugfix...
    
    Fixes #59353

--- tests/src/python/test_qgsserver_wms_getfeatureinfo.py
+++ tests/src/python/test_qgsserver_wms_getfeatureinfo.py
@@ -1496,6 +1496,66 @@ class TestQgsServerWMSGetFeatureInfo(TestQgsServerWMSTestBase):
             self.assertNotIn("name_xyz", body)
             self.assertIn("id_xyz", body)
 
+    def test_getfeatureinfo_display_name(self):
+        """Test issue GH #59353"""
+
+        # create a memory layer with points
+        fields = QgsFields()
+        fields.append(QgsField("id_xyz", QVariant.Int))
+        fields.append(QgsField("name_xyz", QVariant.String))
+        layer = QgsMemoryProviderUtils.createMemoryLayer(
+            "points",
+            fields,
+            QgsWkbTypes.Point,
+            QgsCoordinateReferenceSystem("EPSG:4326"),
+        )
+        layer.setDisplayExpression("name_xyz || ' (' || id_xyz || ')'")
+
+        provider = layer.dataProvider()
+        self.assertTrue(layer.isValid())
+
+        # add some features
+        f = QgsFeature(fields)
+        f.setAttribute("id_xyz", 1)
+        f.setAttribute("name_xyz", "point1")
+        f.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(0, 0)))
+        provider.addFeature(f)
+
+        f = QgsFeature(fields)
+        f.setAttribute("id_xyz", 2)
+        f.setAttribute("name_xyz", "point2")
+        f.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(1, 1)))
+        provider.addFeature(f)
+
+        f = QgsFeature(fields)
+        f.setAttribute("id_xyz", 3)
+        f.setAttribute("name_xyz", "point3")
+        f.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(-1, -1)))
+        provider.addFeature(f)
+
+        project = QgsProject()
+        project.addMapLayer(layer)
+
+        # set up the WMS server
+        server = QgsServer()
+        request = QgsServerRequest()
+        w = 10
+        w2 = int(w / 2)
+
+        # Note: not implemented for , 'application/vnd.ogc.gml'
+        for info_format in ["text/plain", "application/json", "text/xml"]:
+
+            request.setUrl(
+                QUrl(
+                    f"?SERVICE=WMS&REQUEST=GetFeatureInfo&LAYERS=points&QUERY_LAYERS=points&INFO_FORMAT={info_format}&FEATURE_COUNT=1&WIDTH={w}&HEIGHT={w}&CRS=EPSG:4326&STYLES=&WITH_DISPLAY_NAME=true&BBOX=-1,-1,1,1&X={w2}&Y={w2}&VERSION=1.3.0"
+                )
+            )
+            response = QgsBufferServerResponse()
+
+            server.handleRequest(request, response, project)
+            body = response.body().data().decode("utf8").replace("\n", " ")
+            self.assertIn("point1 (1)", body)
+
 
 if __name__ == "__main__":
     unittest.main()
