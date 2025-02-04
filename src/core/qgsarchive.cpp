78fbae63cd444c052f5fbf07499b91cc8665562c
Nyall Dawson <nyall.dawson@gmail.com>
2025-02-04 11:47:22 +1000

    Cleanup project zip file handling
    Refs #52666
    
    (cherry picked from commit c2bb3e49862cdb9cc6b46f782e47218fecd36ab7)

--- src/core/qgsarchive.cpp
+++ src/core/qgsarchive.cpp
@@ -64,12 +64,13 @@ void QgsArchive::clear()
 
 bool QgsArchive::zip( const QString &filename )
 {
-  const QString tempPath = QStandardPaths::standardLocations( QStandardPaths::TempLocation ).at( 0 );
-  const QString uuid = QUuid::createUuid().toString();
-  QFile tmpFile( tempPath + QDir::separator() + uuid );
+  QTemporaryFile tmpFilePath( QDir::temp().absoluteFilePath( QStringLiteral( "qgis-project-XXXXXX.zip" ) ) );
+  tmpFilePath.open();
+  tmpFilePath.close();
+  const QString tempPath = tmpFilePath.fileName();
 
   // zip content
-  if ( ! QgsZipUtils::zip( tmpFile.fileName(), mFiles ) )
+  if ( ! QgsZipUtils::zip( tempPath, mFiles ) )
   {
     const QString err = QObject::tr( "Unable to zip content" );
     QgsMessageLog::logMessage( err, QStringLiteral( "QgsArchive" ) );
@@ -84,17 +85,17 @@ bool QgsArchive::zip( const QString &filename )
   // Clear temporary flag (see GH #32118)
   DWORD dwAttrs;
 #ifdef UNICODE
-  dwAttrs = GetFileAttributes( qUtf16Printable( tmpFile.fileName() ) );
-  SetFileAttributes( qUtf16Printable( tmpFile.fileName() ), dwAttrs & ~ FILE_ATTRIBUTE_TEMPORARY );
+  dwAttrs = GetFileAttributes( qUtf16Printable( tempPath ) );
+  SetFileAttributes( qUtf16Printable( tempPath ), dwAttrs & ~ FILE_ATTRIBUTE_TEMPORARY );
 #else
-  dwAttrs = GetFileAttributes( tmpFile.fileName().toLocal8Bit( ).data( ) );
-  SetFileAttributes( tmpFile.fileName().toLocal8Bit( ).data( ), dwAttrs & ~ FILE_ATTRIBUTE_TEMPORARY );
+  dwAttrs = GetFileAttributes( tempPath.toLocal8Bit( ).data( ) );
+  SetFileAttributes( tempPath.toLocal8Bit( ).data( ), dwAttrs & ~ FILE_ATTRIBUTE_TEMPORARY );
 #endif
 
 #endif // Q_OS_WIN
 
   // save zip archive
-  if ( ! tmpFile.rename( filename ) )
+  if ( !QFile::rename( tempPath, filename ) )
   {
     const QString err = QObject::tr( "Unable to save zip file '%1'" ).arg( filename );
     QgsMessageLog::logMessage( err, QStringLiteral( "QgsArchive" ) );
