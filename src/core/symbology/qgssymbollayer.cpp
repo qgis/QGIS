7c6a24effbc58395a807132f84ad3f592cd94d52
Julien Cabieces <julien.cabieces@oslandia.com>
2025-02-04 09:21:04 +0100

    design(MaskSymbolLayer): add clearMasks only in mask symbol layer

--- src/core/symbology/qgssymbollayer.cpp
+++ src/core/symbology/qgssymbollayer.cpp
@@ -921,10 +921,6 @@ QList<QgsSymbolLayerReference> QgsSymbolLayer::masks() const
   return {};
 }
 
-void QgsSymbolLayer::clearMasks()
-{
-}
-
 double QgsMarkerSymbolLayer::dxfSize( const QgsDxfExport &e, QgsSymbolRenderContext &context ) const
 {
   double size = mSize;
