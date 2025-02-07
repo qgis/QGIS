781d31ea0f0e99228d08e73545da3a507095ed4a
Alessandro Pasotti <elpaso@itopen.it>
2025-02-07 11:10:14 +0100

    [oapif] Fix regression missing auth headers
    Fixes #60473

--- src/providers/wfs/oapif/qgsoapifconformancerequest.cpp
+++ src/providers/wfs/oapif/qgsoapifconformancerequest.cpp
@@ -25,7 +25,7 @@ using namespace nlohmann;
 #include <QTextCodec>
 
 QgsOapifConformanceRequest::QgsOapifConformanceRequest( const QgsDataSourceUri &uri )
-  : QgsBaseNetworkRequest( QgsAuthorizationSettings( uri.username(), uri.password(), uri.authConfigId() ), "OAPIF" )
+  : QgsBaseNetworkRequest( QgsAuthorizationSettings( uri.username(), uri.password(), QgsHttpHeaders(), uri.authConfigId() ), "OAPIF" )
 {
   // Using Qt::DirectConnection since the download might be running on a different thread.
   // In this case, the request was sent from the main thread and is executed with the main
