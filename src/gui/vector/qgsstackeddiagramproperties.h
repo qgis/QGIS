0f3b0c45ea9c7fc07c5268d1eafd99147460e69a
Germán Carrillo <gcarrillo@linuxmail.org>
2025-02-11 10:53:13 +0100

    [ux] Show line drop indicator instead of box for stacked diagram items (items don't have children so far),  because of that, we can drop at any index as well

--- src/gui/vector/qgsstackeddiagramproperties.h
+++ src/gui/vector/qgsstackeddiagramproperties.h
@@ -24,6 +24,7 @@
 #include "qgis_gui.h"
 #include "qgsdiagramrenderer.h"
 #include "ui_qgsstackeddiagrampropertiesbase.h"
+#include "qgsproxystyle.h"
 
 #include <QWidget>
 #include <QDialog>
@@ -93,6 +94,19 @@ class GUI_EXPORT QgsStackedDiagramPropertiesModel : public QAbstractTableModel
     QgsDiagramLayerSettings mDiagramLayerSettings;
 };
 
+/**
+ * \ingroup gui
+ * \brief View style which shows drop indicator line between items
+ */
+class QgsStackedDiagramsViewStyle : public QgsProxyStyle
+{
+    Q_OBJECT
+
+  public:
+    explicit QgsStackedDiagramsViewStyle( QWidget *parent );
+
+    void drawPrimitive( PrimitiveElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget = nullptr ) const override;
+};
 
 /**
  * \ingroup gui
