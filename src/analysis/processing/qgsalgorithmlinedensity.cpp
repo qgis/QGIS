e816b8404da5aeabaf8c672b42c1f8b96b73d5b3
Alexander Bruy <alexander.bruy@gmail.com>
2025-02-12 13:36:00 +0000

    round width and height to the nearest integer in native raster creation Processing algorithms to match behaviour of gdal_rasterize (fix #43547)
    (cherry picked from commit 269fe485f22dcfb43e3542b105b01b4048d1e821)

--- src/analysis/processing/qgsalgorithmlinedensity.cpp
+++ src/analysis/processing/qgsalgorithmlinedensity.cpp
@@ -136,8 +136,10 @@ QVariantMap QgsLineDensityAlgorithm::processAlgorithm( const QVariantMap &parame
   const QFileInfo fi( outputFile );
   const QString outputFormat = QgsRasterFileWriter::driverForExtension( fi.suffix() );
 
-  const int rows = std::max( std::ceil( mExtent.height() / mPixelSize ), 1.0 );
-  const int cols = std::max( std::ceil( mExtent.width() / mPixelSize ), 1.0 );
+  // round up width and height to the nearest integer as GDAL does (e.g. in gdal_rasterize)
+  // see https://github.com/qgis/QGIS/issues/43547
+  const int rows = static_cast<int>( 0.5 + mExtent.height() / mPixelSize );
+  const int cols = static_cast<int>( 0.5 + mExtent.width() / mPixelSize );
 
   //build new raster extent based on number of columns and cellsize
   //this prevents output cellsize being calculated too small
