qgis/QGIS

commit 8af23b10024be91a8e97a81832907393544a7fd5

Initial framework for terrain settings refactoring

src/3d/qgs3dmapsettings.cpp
@@ -33,6 +33,8 @@
 #include "qgs3drendercontext.h"
 #include "qgsthreadingutils.h"
 #include "qgsmaplayerlistutils_p.h"
+#include "qgsterrainsettings.h"
+#include "qgsflatterrainsettings.h"
 
 #include <QDomDocument>
 #include <QDomElement>
@@ -45,6 +47,7 @@ Qgs3DMapSettings::Qgs3DMapSettings()
     QgsProject::instance()->setDirty();
   } );
   connectChangedSignalsToSettingsChanged();
+  mTerrainSettings = std::make_unique<QgsFlatTerrainSettings>();
 }
 
 Qgs3DMapSettings::Qgs3DMapSettings( const Qgs3DMapSettings &other )
@@ -54,12 +57,8 @@ Qgs3DMapSettings::Qgs3DMapSettings( const Qgs3DMapSettings &other )
   , mCrs( other.mCrs )
   , mBackgroundColor( other.mBackgroundColor )
   , mSelectionColor( other.mSelectionColor )
-  , mTerrainVerticalScale( other.mTerrainVerticalScale )
   , mTerrainGenerator( other.mTerrainGenerator ? other.mTerrainGenerator->clone() : nullptr )
-  , mMapTileResolution( other.mMapTileResolution )
-  , mMaxTerrainScreenError( other.mMaxTerrainScreenError )
-  , mMaxTerrainGroundError( other.mMaxTerrainGroundError )
-  , mTerrainElevationOffset( other.mTerrainElevationOffset )
+  , mTerrainSettings( other.mTerrainSettings ? other.mTerrainSettings->clone() : new QgsFlatTerrainSettings() )
   , mTerrainShadingEnabled( other.mTerrainShadingEnabled )
   , mTerrainShadingMaterial( other.mTerrainShadingMaterial )
   , mTerrainMapTheme( other.mTerrainMapTheme )
@@ -174,12 +173,7 @@ void Qgs3DMapSettings::readXml( const QDomElement &elem, const QgsReadWriteConte
 
   QDomElement elemTerrain = elem.firstChildElement( QStringLiteral( "terrain" ) );
   mTerrainRenderingEnabled = elemTerrain.attribute( QStringLiteral( "terrain-rendering-enabled" ), QStringLiteral( "1" ) ).toInt();
-  mTerrainVerticalScale = elemTerrain.attribute( QStringLiteral( "exaggeration" ), QStringLiteral( "1" ) ).toFloat();
-  mMapTileResolution = elemTerrain.attribute( QStringLiteral( "texture-size" ), QStringLiteral( "512" ) ).toInt();
-  mMaxTerrainScreenError = elemTerrain.attribute( QStringLiteral( "max-terrain-error" ), QStringLiteral( "3" ) ).toFloat();
-  mMaxTerrainGroundError = elemTerrain.attribute( QStringLiteral( "max-ground-error" ), QStringLiteral( "1" ) ).toFloat();
   mTerrainShadingEnabled = elemTerrain.attribute( QStringLiteral( "shading-enabled" ), QStringLiteral( "0" ) ).toInt();
-  mTerrainElevationOffset = elemTerrain.attribute( QStringLiteral( "elevation-offset" ), QStringLiteral( "0.0" ) ).toFloat();
 
   QDomElement elemTerrainShadingMaterial = elemTerrain.firstChildElement( QStringLiteral( "shading-material" ) );
   if ( !elemTerrainShadingMaterial.isNull() )
@@ -373,12 +367,7 @@ QDomElement Qgs3DMapSettings::writeXml( QDomDocument &doc, const QgsReadWriteCon
 
   QDomElement elemTerrain = doc.createElement( QStringLiteral( "terrain" ) );
   elemTerrain.setAttribute( QStringLiteral( "terrain-rendering-enabled" ), mTerrainRenderingEnabled ? 1 : 0 );
-  elemTerrain.setAttribute( QStringLiteral( "exaggeration" ), QString::number( mTerrainVerticalScale ) );
-  elemTerrain.setAttribute( QStringLiteral( "texture-size" ), mMapTileResolution );
-  elemTerrain.setAttribute( QStringLiteral( "max-terrain-error" ), QString::number( mMaxTerrainScreenError ) );
-  elemTerrain.setAttribute( QStringLiteral( "max-ground-error" ), QString::number( mMaxTerrainGroundError ) );
   elemTerrain.setAttribute( QStringLiteral( "shading-enabled" ), mTerrainShadingEnabled ? 1 : 0 );
-  elemTerrain.setAttribute( QStringLiteral( "elevation-offset" ), mTerrainElevationOffset );
 
   QDomElement elemTerrainShadingMaterial = doc.createElement( QStringLiteral( "shading-material" ) );
   mTerrainShadingMaterial.writeXml( elemTerrainShadingMaterial, context );
@@ -636,18 +625,21 @@ void Qgs3DMapSettings::setTerrainVerticalScale( double zScale )
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  if ( zScale == mTerrainVerticalScale )
+  if ( zScale == mTerrainSettings->verticalScale() )
     return;
 
-  mTerrainVerticalScale = zScale;
+  mTerrainSettings->setVerticalScale( zScale );
+  Q_NOWARN_DEPRECATED_PUSH
   emit terrainVerticalScaleChanged();
+  Q_NOWARN_DEPRECATED_POP
+  emit terrainSettingsChanged();
 }
 
 double Qgs3DMapSettings::terrainVerticalScale() const
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  return mTerrainVerticalScale;
+  return mTerrainSettings->verticalScale();
 }
 
 void Qgs3DMapSettings::setLayers( const QList<QgsMapLayer *> &layers )
@@ -725,75 +717,126 @@ void Qgs3DMapSettings::configureTerrainFromProject( QgsProjectElevationPropertie
   }
 }
 
+const QgsAbstractTerrainSettings *Qgs3DMapSettings::terrainSettings() const
+{
+  QGIS_PROTECT_QOBJECT_THREAD_ACCESS
+
+  return mTerrainSettings.get();
+}
+
+void Qgs3DMapSettings::setTerrainSettings( QgsAbstractTerrainSettings *settings )
+{
+  QGIS_PROTECT_QOBJECT_THREAD_ACCESS
+
+  bool hasChanged = false;
+  if ( settings == mTerrainSettings.get() )
+  {
+    // emit signals anyway. We don't know if the caller changed settings on the pointer before calling this..
+    hasChanged = true;
+  }
+  else
+  {
+    hasChanged = !settings->equals( mTerrainSettings.get() );
+    mTerrainSettings.reset( settings );
+  }
+
+  if ( hasChanged )
+  {
+    // emit all the signals, we don't know exactly what's changed
+    Q_NOWARN_DEPRECATED_PUSH
+    emit mapTileResolutionChanged();
+    emit maxTerrainScreenErrorChanged();
+    emit maxTerrainGroundErrorChanged();
+    emit terrainElevationOffsetChanged( mTerrainSettings->elevationOffset() );
+    emit terrainVerticalScaleChanged();
+    Q_NOWARN_DEPRECATED_POP
+
+    emit terrainSettingsChanged();
+  }
+}
+
 void Qgs3DMapSettings::setMapTileResolution( int res )
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  if ( mMapTileResolution == res )
+  if ( mTerrainSettings->mapTileResolution() == res )
     return;
 
-  mMapTileResolution = res;
+  mTerrainSettings->setMapTileResolution( res );
+  Q_NOWARN_DEPRECATED_PUSH
   emit mapTileResolutionChanged();
+  Q_NOWARN_DEPRECATED_POP
+  emit terrainSettingsChanged();
 }
 
 int Qgs3DMapSettings::mapTileResolution() const
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  return mMapTileResolution;
+  return mTerrainSettings->mapTileResolution();
 }
 
-void Qgs3DMapSettings::setMaxTerrainScreenError( float error )
+void Qgs3DMapSettings::setMaxTerrainScreenError( double error )
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  if ( mMaxTerrainScreenError == error )
+  if ( mTerrainSettings->maximumScreenError() == error )
     return;
 
-  mMaxTerrainScreenError = error;
+  mTerrainSettings->setMaximumScreenError( error );
+  Q_NOWARN_DEPRECATED_PUSH
   emit maxTerrainScreenErrorChanged();
+  Q_NOWARN_DEPRECATED_POP
+  emit terrainSettingsChanged();
 }
 
-float Qgs3DMapSettings::maxTerrainScreenError() const
+double Qgs3DMapSettings::maxTerrainScreenError() const
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  return mMaxTerrainScreenError;
+  return mTerrainSettings->maximumScreenError();
 }
 
-void Qgs3DMapSettings::setMaxTerrainGroundError( float error )
+void Qgs3DMapSettings::setMaxTerrainGroundError( double error )
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  if ( mMaxTerrainGroundError == error )
+  if ( mTerrainSettings->maximumGroundError() == error )
     return;
 
-  mMaxTerrainGroundError = error;
+  mTerrainSettings->setMaximumGroundError( error );
+  Q_NOWARN_DEPRECATED_PUSH
   emit maxTerrainGroundErrorChanged();
+  Q_NOWARN_DEPRECATED_POP
+
+  emit terrainSettingsChanged();
 }
 
-void Qgs3DMapSettings::setTerrainElevationOffset( float offset )
+void Qgs3DMapSettings::setTerrainElevationOffset( double offset )
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  if ( mTerrainElevationOffset == offset )
+  if ( mTerrainSettings->elevationOffset() == offset )
     return;
-  mTerrainElevationOffset = offset;
-  emit terrainElevationOffsetChanged( mTerrainElevationOffset );
+  mTerrainSettings->setElevationOffset( offset );
+  Q_NOWARN_DEPRECATED_PUSH
+  emit terrainElevationOffsetChanged( offset );
+  Q_NOWARN_DEPRECATED_POP
+  emit terrainSettingsChanged();
 }
 
-float Qgs3DMapSettings::terrainElevationOffset() const
+double Qgs3DMapSettings::terrainElevationOffset() const
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  return mTerrainElevationOffset;
+  return mTerrainSettings->elevationOffset();
 }
 
-float Qgs3DMapSettings::maxTerrainGroundError() const
+double Qgs3DMapSettings::maxTerrainGroundError() const
 {
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
-  return mMaxTerrainGroundError;
+  return mTerrainSettings->maximumGroundError();
 }
 
 void Qgs3DMapSettings::setTerrainGenerator( QgsTerrainGenerator *gen )
@@ -1441,11 +1484,7 @@ void Qgs3DMapSettings::connectChangedSignalsToSettingsChanged()
   connect( this, &Qgs3DMapSettings::selectionColorChanged, this, &Qgs3DMapSettings::settingsChanged );
   connect( this, &Qgs3DMapSettings::layersChanged, this, &Qgs3DMapSettings::settingsChanged );
   connect( this, &Qgs3DMapSettings::terrainGeneratorChanged, this, &Qgs3DMapSettings::settingsChanged );
-  connect( this, &Qgs3DMapSettings::terrainVerticalScaleChanged, this, &Qgs3DMapSettings::settingsChanged );
-  connect( this, &Qgs3DMapSettings::mapTileResolutionChanged, this, &Qgs3DMapSettings::settingsChanged );
-  connect( this, &Qgs3DMapSettings::maxTerrainScreenErrorChanged, this, &Qgs3DMapSettings::settingsChanged );
-  connect( this, &Qgs3DMapSettings::maxTerrainGroundErrorChanged, this, &Qgs3DMapSettings::settingsChanged );
-  connect( this, &Qgs3DMapSettings::terrainElevationOffsetChanged, this, &Qgs3DMapSettings::settingsChanged );
+  connect( this, &Qgs3DMapSettings::terrainSettingsChanged, this, &Qgs3DMapSettings::settingsChanged );
   connect( this, &Qgs3DMapSettings::terrainShadingChanged, this, &Qgs3DMapSettings::settingsChanged );
   connect( this, &Qgs3DMapSettings::terrainMapThemeChanged, this, &Qgs3DMapSettings::settingsChanged );
   connect( this, &Qgs3DMapSettings::renderersChanged, this, &Qgs3DMapSettings::settingsChanged );

python/3d/3d_auto.sip
@@ -29,4 +29,8 @@
 %Include auto_generated/symbols/qgspoint3dsymbol.sip
 %Include auto_generated/symbols/qgspolygon3dsymbol.sip
 %Include auto_generated/symbols/qgspointcloud3dsymbol.sip
+%Include auto_generated/terrain/qgsdemterrainsettings.sip
+%Include auto_generated/terrain/qgsflatterrainsettings.sip
+%Include auto_generated/terrain/qgsmeshterrainsettings.sip
+%Include auto_generated/terrain/qgsterrainsettings.sip
 %Include auto_generated/qgs3dmapexportsettings.sip

python/3d/auto_additions/qgs3dmapsettings.py
@@ -1,6 +1,6 @@
 # The following has been generated automatically from src/3d/qgs3dmapsettings.h
 try:
-    Qgs3DMapSettings.__attribute_docs__ = {'settingsChanged': 'Emitted when one of the configuration settings has changed\n\n.. versionadded:: 3.24\n', 'backgroundColorChanged': 'Emitted when the background color has changed\n', 'selectionColorChanged': 'Emitted when the selection color has changed\n', 'layersChanged': 'Emitted when the list of map layers for 3d rendering has changed.\n\n.. seealso:: :py:func:`setLayers`\n\n.. seealso:: :py:func:`layers`\n', 'terrainGeneratorChanged': 'Emitted when the terrain generator has changed\n', 'terrainVerticalScaleChanged': 'Emitted when the vertical scale of the terrain has changed\n', 'mapTileResolutionChanged': 'Emitted when the map tile resoulution has changed\n', 'maxTerrainScreenErrorChanged': 'Emitted when the maximum terrain screen error has changed\n', 'maxTerrainGroundErrorChanged': 'Emitted when the maximum terrain ground error has changed\n', 'terrainElevationOffsetChanged': 'Emitted when the terrain elevation offset is changed\n\n.. versionadded:: 3.16\n', 'terrainShadingChanged': 'Emitted when terrain shading enabled flag or terrain shading material has changed\n\n.. versionadded:: 3.6\n', 'terrainMapThemeChanged': "Emitted when terrain's map theme has changed\n\n.. versionadded:: 3.6\n", 'renderersChanged': "Emitted when the list of map's extra renderers have been modified\n\n.. versionadded:: 3.10\n", 'showTerrainBoundingBoxesChanged': "Emitted when the flag whether terrain's bounding boxes are shown has changed\n", 'showTerrainTilesInfoChanged': "Emitted when the flag whether terrain's tile info is shown has changed\n", 'showCameraViewCenterChanged': "Emitted when the flag whether camera's view center is shown has changed\n\n.. versionadded:: 3.4\n", 'showCameraRotationCenterChanged': "Emitted when the flag whether camera's rotation center is shown has changed\n\n.. versionadded:: 3.24\n", 'showLightSourceOriginsChanged': 'Emitted when the flag whether light source origins are shown has changed.\n\n.. versionadded:: 3.15\n', 'showLabelsChanged': 'Emitted when the flag whether labels are displayed on terrain tiles has changed\n', 'stopUpdatesChanged': 'Emitted when the flag whether to keep updating scene has changed\n\n.. versionadded:: 3.42\n', 'eyeDomeLightingEnabledChanged': 'Emitted when the flag whether eye dome lighting is used has changed\n\n.. versionadded:: 3.18\n', 'eyeDomeLightingStrengthChanged': 'Emitted when the eye dome lighting strength has changed\n\n.. versionadded:: 3.18\n', 'eyeDomeLightingDistanceChanged': 'Emitted when the eye dome lighting distance has changed\n\n.. versionadded:: 3.18\n', 'debugShadowMapSettingsChanged': 'Emitted when shadow map debugging has changed\n\n.. versionadded:: 3.18\n', 'debugDepthMapSettingsChanged': 'Emitted when depth map debugging has changed\n\n.. versionadded:: 3.18\n', 'pointLightsChanged': 'Emitted when the list of point lights changes\n\n.. versionadded:: 3.6\n', 'lightSourcesChanged': 'Emitted when any of the light source settings in the map changes.\n\n.. versionadded:: 3.26\n', 'directionalLightsChanged': 'Emitted when the list of directional lights changes\n\n.. versionadded:: 3.16\n', 'fieldOfViewChanged': 'Emitted when the camera lens field of view changes\n\n.. versionadded:: 3.8\n', 'projectionTypeChanged': 'Emitted when the camera lens projection type changes\n\n.. versionadded:: 3.18\n', 'cameraNavigationModeChanged': 'Emitted when the camera navigation mode was changed\n\n.. versionadded:: 3.18\n', 'cameraMovementSpeedChanged': 'Emitted when the camera movement speed was changed\n\n.. versionadded:: 3.18\n', 'skyboxSettingsChanged': 'Emitted when skybox settings are changed\n\n.. versionadded:: 3.16\n', 'shadowSettingsChanged': 'Emitted when shadow rendering settings are changed\n\n.. versionadded:: 3.16\n', 'ambientOcclusionSettingsChanged': 'Emitted when ambient occlusion rendering settings are changed\n\n.. versionadded:: 3.28\n', 'fpsCounterEnabledChanged': 'Emitted when the FPS counter is enabled or disabled\n\n.. versionadded:: 3.18\n', 'viewFrustumVisualizationEnabledChanged': "Emitted when the camera's view frustum visualization on the main 2D map canvas is enabled or disabled\n\n.. versionadded:: 3.26\n", 'axisSettingsChanged': 'Emitted when 3d axis rendering settings are changed\n\n.. versionadded:: 3.26\n', 'debugOverlayEnabledChanged': 'Emitted when the debug overaly is enabled or disabled\n\n.. versionadded:: 3.26\n', 'extentChanged': "Emitted when the 3d view's 2d extent has changed\n\n.. seealso:: :py:func:`setExtent`\n\n.. versionadded:: 3.30\n", 'showExtentIn2DViewChanged': "Emitted when the parameter to display 3d view's extent in the 2D canvas has changed\n\n.. seealso:: :py:func:`setShowExtentIn2DView`\n\n.. versionadded:: 3.32\n", 'showDebugPanelChanged': 'Emitted when the Show debug panel checkbox changes value\n\n.. seealso:: :py:func:`setShowDebugPanel`\n\n.. versionadded:: 3.42\n', 'originChanged': "Emitted when the world's origin point has been shifted\n\n.. seealso:: :py:func:`setOrigin`\n\n.. versionadded:: 3.42\n"}
-    Qgs3DMapSettings.__signal_arguments__ = {'terrainElevationOffsetChanged': ['newElevation: float'], 'fpsCounterEnabledChanged': ['fpsCounterEnabled: bool'], 'debugOverlayEnabledChanged': ['debugOverlayEnabled: bool'], 'showDebugPanelChanged': ['shown: bool']}
+    Qgs3DMapSettings.__attribute_docs__ = {'settingsChanged': 'Emitted when one of the configuration settings has changed\n\n.. versionadded:: 3.24\n', 'backgroundColorChanged': 'Emitted when the background color has changed\n', 'selectionColorChanged': 'Emitted when the selection color has changed\n', 'layersChanged': 'Emitted when the list of map layers for 3d rendering has changed.\n\n.. seealso:: :py:func:`setLayers`\n\n.. seealso:: :py:func:`layers`\n', 'terrainGeneratorChanged': 'Emitted when the terrain generator has changed\n', 'terrainSettingsChanged': 'Emitted when the terrain settings are changed.\n\n.. versionadded:: 3.42\n', 'terrainVerticalScaleChanged': 'Emitted when the vertical scale of the terrain has changed\n\n.. deprecated:: 3.42\n\n   Use :py:func:`~Qgs3DMapSettings.terrainSettingsChanged` instead.\n', 'mapTileResolutionChanged': 'Emitted when the map tile resoulution has changed\n\n.. deprecated:: 3.42\n\n   Use :py:func:`~Qgs3DMapSettings.terrainSettingsChanged` instead.\n', 'maxTerrainScreenErrorChanged': 'Emitted when the maximum terrain screen error has changed\n\n.. deprecated:: 3.42\n\n   Use :py:func:`~Qgs3DMapSettings.terrainSettingsChanged` instead.\n', 'maxTerrainGroundErrorChanged': 'Emitted when the maximum terrain ground error has changed\n\n.. deprecated:: 3.42\n\n   Use :py:func:`~Qgs3DMapSettings.terrainSettingsChanged` instead.\n', 'terrainElevationOffsetChanged': 'Emitted when the terrain elevation offset is changed\n\n.. deprecated:: 3.42\n\n   Use :py:func:`~Qgs3DMapSettings.terrainSettingsChanged` instead.\n', 'terrainShadingChanged': 'Emitted when terrain shading enabled flag or terrain shading material has changed\n\n.. versionadded:: 3.6\n', 'terrainMapThemeChanged': "Emitted when terrain's map theme has changed\n\n.. versionadded:: 3.6\n", 'renderersChanged': "Emitted when the list of map's extra renderers have been modified\n\n.. versionadded:: 3.10\n", 'showTerrainBoundingBoxesChanged': "Emitted when the flag whether terrain's bounding boxes are shown has changed\n", 'showTerrainTilesInfoChanged': "Emitted when the flag whether terrain's tile info is shown has changed\n", 'showCameraViewCenterChanged': "Emitted when the flag whether camera's view center is shown has changed\n\n.. versionadded:: 3.4\n", 'showCameraRotationCenterChanged': "Emitted when the flag whether camera's rotation center is shown has changed\n\n.. versionadded:: 3.24\n", 'showLightSourceOriginsChanged': 'Emitted when the flag whether light source origins are shown has changed.\n\n.. versionadded:: 3.15\n', 'showLabelsChanged': 'Emitted when the flag whether labels are displayed on terrain tiles has changed\n', 'stopUpdatesChanged': 'Emitted when the flag whether to keep updating scene has changed\n\n.. versionadded:: 3.42\n', 'eyeDomeLightingEnabledChanged': 'Emitted when the flag whether eye dome lighting is used has changed\n\n.. versionadded:: 3.18\n', 'eyeDomeLightingStrengthChanged': 'Emitted when the eye dome lighting strength has changed\n\n.. versionadded:: 3.18\n', 'eyeDomeLightingDistanceChanged': 'Emitted when the eye dome lighting distance has changed\n\n.. versionadded:: 3.18\n', 'debugShadowMapSettingsChanged': 'Emitted when shadow map debugging has changed\n\n.. versionadded:: 3.18\n', 'debugDepthMapSettingsChanged': 'Emitted when depth map debugging has changed\n\n.. versionadded:: 3.18\n', 'pointLightsChanged': 'Emitted when the list of point lights changes\n\n.. versionadded:: 3.6\n', 'lightSourcesChanged': 'Emitted when any of the light source settings in the map changes.\n\n.. versionadded:: 3.26\n', 'directionalLightsChanged': 'Emitted when the list of directional lights changes\n\n.. versionadded:: 3.16\n', 'fieldOfViewChanged': 'Emitted when the camera lens field of view changes\n\n.. versionadded:: 3.8\n', 'projectionTypeChanged': 'Emitted when the camera lens projection type changes\n\n.. versionadded:: 3.18\n', 'cameraNavigationModeChanged': 'Emitted when the camera navigation mode was changed\n\n.. versionadded:: 3.18\n', 'cameraMovementSpeedChanged': 'Emitted when the camera movement speed was changed\n\n.. versionadded:: 3.18\n', 'skyboxSettingsChanged': 'Emitted when skybox settings are changed\n\n.. versionadded:: 3.16\n', 'shadowSettingsChanged': 'Emitted when shadow rendering settings are changed\n\n.. versionadded:: 3.16\n', 'ambientOcclusionSettingsChanged': 'Emitted when ambient occlusion rendering settings are changed\n\n.. versionadded:: 3.28\n', 'fpsCounterEnabledChanged': 'Emitted when the FPS counter is enabled or disabled\n\n.. versionadded:: 3.18\n', 'viewFrustumVisualizationEnabledChanged': "Emitted when the camera's view frustum visualization on the main 2D map canvas is enabled or disabled\n\n.. versionadded:: 3.26\n", 'axisSettingsChanged': 'Emitted when 3d axis rendering settings are changed\n\n.. versionadded:: 3.26\n', 'debugOverlayEnabledChanged': 'Emitted when the debug overaly is enabled or disabled\n\n.. versionadded:: 3.26\n', 'extentChanged': "Emitted when the 3d view's 2d extent has changed\n\n.. seealso:: :py:func:`setExtent`\n\n.. versionadded:: 3.30\n", 'showExtentIn2DViewChanged': "Emitted when the parameter to display 3d view's extent in the 2D canvas has changed\n\n.. seealso:: :py:func:`setShowExtentIn2DView`\n\n.. versionadded:: 3.32\n", 'showDebugPanelChanged': 'Emitted when the Show debug panel checkbox changes value\n\n.. seealso:: :py:func:`setShowDebugPanel`\n\n.. versionadded:: 3.42\n', 'originChanged': "Emitted when the world's origin point has been shifted\n\n.. seealso:: :py:func:`setOrigin`\n\n.. versionadded:: 3.42\n"}
+    Qgs3DMapSettings.__signal_arguments__ = {'fpsCounterEnabledChanged': ['fpsCounterEnabled: bool'], 'debugOverlayEnabledChanged': ['debugOverlayEnabled: bool'], 'showDebugPanelChanged': ['shown: bool']}
 except (NameError, AttributeError):
     pass

python/3d/auto_additions/qgsdemterrainsettings.py
@@ -1,0 +1,5 @@
+# The following has been generated automatically from src/3d/terrain/qgsdemterrainsettings.h
+try:
+    QgsDemTerrainSettings.__group__ = ['terrain']
+except (NameError, AttributeError):
+    pass

python/3d/auto_additions/qgsflatterrainsettings.py
@@ -1,0 +1,5 @@
+# The following has been generated automatically from src/3d/terrain/qgsflatterrainsettings.h
+try:
+    QgsFlatTerrainSettings.__group__ = ['terrain']
+except (NameError, AttributeError):
+    pass

python/3d/auto_additions/qgsmeshterrainsettings.py
@@ -1,0 +1,5 @@
+# The following has been generated automatically from src/3d/terrain/qgsmeshterrainsettings.h
+try:
+    QgsMeshTerrainSettings.__group__ = ['terrain']
+except (NameError, AttributeError):
+    pass

python/3d/auto_additions/qgsterrainsettings.py
@@ -1,0 +1,5 @@
+# The following has been generated automatically from src/3d/terrain/qgsterrainsettings.h
+try:
+    QgsAbstractTerrainSettings.__group__ = ['terrain']
+except (NameError, AttributeError):
+    pass

commit d4aee893dc4c77d0afa40c03c2361e5fa6033c9d

qgs3dmapsettings: Fix terrain generation on 3d view creation
When a new 3d is created
`Qgs3DMapSettings::configureTerrainFromProject` is called to configure
the terrain. This method calls `Qgs3DMapSettings::setTerrainSettings`
which sets the terrain settings and creates the terrain generator if
necessary. This generator is not updated if the settings have not
changed.

However, on a new 3d view, the terrain settings (`mTerrainSettings`)
have already been created with the default values and the
`QgsAbstractTerrainSettings` parameter of
`Qgs3DMapSettings::setTerrainSettings` also contains the default
settings. Therefore, this call does nothing and the terrain generator
is not created. This results in a 3d view which does not contain any
terrain.

This issue is fixed by also checking if the terrain generator has
already been created. If that's not the case, the terrain generator is
now created.

The flatTerrain test is updated to test this workflow.

Fixes: 8af23b10024be91a8e97a81832907393544a7fd5

src/3d/qgs3dmapsettings.cpp
@@ -680,7 +680,8 @@ void Qgs3DMapSettings::setTerrainSettings( QgsAbstractTerrainSettings *settings 
   }
   else
   {
-    hasChanged = !settings->equals( mTerrainSettings.get() );
+    // ensure to generate the terrain if the settings have changed or if the terrain has never been generated.
+    hasChanged = !settings->equals( mTerrainSettings.get() ) || !mTerrainGenerator;
     mTerrainSettings.reset( settings );
   }
 

tests/src/3d/testqgs3drendering.cpp
@@ -31,6 +31,7 @@
 #include "qgs3dmapsettings.h"
 #include "qgs3dutils.h"
 #include "qgscameracontroller.h"
+#include "qgsflatterrainsettings.h"
 #include "qgsdemterrainsettings.h"
 #include "qgsflatterraingenerator.h"
 #include "qgsline3dsymbol.h"
@@ -282,9 +283,12 @@ void TestQgs3DRendering::testFlatTerrain()
   map->setExtent( fullExtent );
   map->setLayers( QList<QgsMapLayer *>() << mLayerRgb );
 
-  QgsFlatTerrainGenerator *flatTerrain = new QgsFlatTerrainGenerator;
-  flatTerrain->setCrs( map->crs(), map->transformContext() );
-  map->setTerrainGenerator( flatTerrain );
+  QgsFlatTerrainSettings *flatTerrainSettings = new QgsFlatTerrainSettings;
+  map->setTerrainSettings( flatTerrainSettings );
+
+  std::unique_ptr<QgsTerrainGenerator> generator = flatTerrainSettings->createTerrainGenerator( Qgs3DRenderContext::fromMapSettings( map ) );
+  QVERIFY( dynamic_cast<QgsFlatTerrainGenerator *>( generator.get() )->isValid() );
+  QCOMPARE( dynamic_cast<QgsFlatTerrainGenerator *>( generator.get() )->crs(), map->crs() );
 
   QgsOffscreen3DEngine engine;
   Qgs3DMapScene *scene = new Qgs3DMapScene( *map, &engine );