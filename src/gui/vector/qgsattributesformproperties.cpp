529d85b3d95170e2ef8a1495c6091650335e0bb3
signedav <david@opengis.ch>
2025-02-25 22:08:47 +0100

    Avoid break of legacy projects where the valuemap key values where directly in a map instead of maps in a list.

--- src/gui/vector/qgsattributesformproperties.cpp
+++ src/gui/vector/qgsattributesformproperties.cpp
@@ -2222,16 +2222,19 @@ void QgsAttributesFormProperties::pasteWidgetConfiguration()
           }
           if ( widgetType == QStringLiteral( "ValueMap" ) )
           {
-            QList<QVariant> translatedValueList;
-            const QList<QVariant> valueList = optionsMap[QStringLiteral( "map" )].toList();
-            for ( int i = 0, row = 0; i < valueList.count(); i++, row++ )
+            if ( optionsMap[QStringLiteral( "map" )].canConvert<QVariantList>() )
             {
-              QMap<QString, QVariant> translatedValueMap;
-              QString translatedKey = context.projectTranslator()->translate( QStringLiteral( "project:layers:%1:fields:%2:valuemapdescriptions" ).arg( mLayer->id(), fieldName ), valueList[i].toMap().constBegin().key() );
-              translatedValueMap[translatedKey] = valueList[i].toMap().constBegin().value();
-              translatedValueList.append( translatedValueMap );
+              QList<QVariant> translatedValueList;
+              const QList<QVariant> valueList = optionsMap[QStringLiteral( "map" )].toList();
+              for ( int i = 0, row = 0; i < valueList.count(); i++, row++ )
+              {
+                QMap<QString, QVariant> translatedValueMap;
+                QString translatedKey = context.projectTranslator()->translate( QStringLiteral( "project:layers:%1:fields:%2:valuemapdescriptions" ).arg( mLayer->id(), fieldName ), valueList[i].toMap().constBegin().key() );
+                translatedValueMap.insert( translatedKey, valueList[i].toMap().constBegin().value() );
+                translatedValueList.append( translatedValueMap );
+              }
+              optionsMap.insert( QStringLiteral( "map" ), translatedValueList );
             }
-            optionsMap[QStringLiteral( "map" )] = translatedValueList;
           }
           config.mEditorWidgetType = widgetType;
           config.mEditorWidgetConfig = optionsMap;
