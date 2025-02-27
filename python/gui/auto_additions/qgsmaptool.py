43fef67bb18b70e46c8a030b235c9ba1f6148a88
Nyall Dawson <nyall.dawson@gmail.com>
2025-02-28 09:39:16 +1000

    Fix PyQGIS signal docs for QgsMapTool

--- python/gui/auto_additions/qgsmaptool.py
+++ python/gui/auto_additions/qgsmaptool.py
@@ -1,9 +1,9 @@
 # The following has been generated automatically from src/gui/maptools/qgsmaptool.h
 try:
-    QgsMapTool.__attribute_docs__ = {'messageEmitted': 'emit a message\n', 'messageDiscarded': 'emit signal to clear previous message\n', 'activated': 'signal emitted once the map tool is activated\n', 'deactivated': 'signal emitted once the map tool is deactivated\n', 'reactivated': '\n.. versionadded:: 3.32\n'}
+    QgsMapTool.__attribute_docs__ = {'messageEmitted': 'Emitted when a ``message`` should be shown to the user in the application message bar.\n\n.. seealso:: :py:func:`messageDiscarded`\n', 'messageDiscarded': 'Emitted when the previous message from the tool should be cleared from the application message bar.\n\n.. seealso:: :py:func:`messageEmitted`\n', 'activated': 'Emitted when the map tool is activated.\n\n.. seealso:: :py:func:`deactivated`\n', 'deactivated': 'Emitted when the map tool is deactivated.\n\n.. seealso:: :py:func:`activated`\n', 'reactivated': 'Emitted when the map tool is activated, while it is already active.\n\n.. versionadded:: 3.32\n'}
     QgsMapTool.searchRadiusMM = staticmethod(QgsMapTool.searchRadiusMM)
     QgsMapTool.searchRadiusMU = staticmethod(QgsMapTool.searchRadiusMU)
-    QgsMapTool.__signal_arguments__ = {'messageEmitted': ['message: str', ': Qgis.MessageLevel = Qgis.MessageLevel.Info']}
+    QgsMapTool.__signal_arguments__ = {'messageEmitted': ['message: str', 'level: Qgis.MessageLevel = Qgis.MessageLevel.Info']}
     QgsMapTool.__group__ = ['maptools']
 except (NameError, AttributeError):
     pass
