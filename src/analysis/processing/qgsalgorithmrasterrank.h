fa9f86769b7613b4f42a10dd9158e97e121fa46f
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-03-03 14:20:54 +0700

    Processing raster rank algorithm (#60605)

---
+++ src/analysis/processing/qgsalgorithmrasterrank.h
@@ -0,0 +1,55 @@
+/***************************************************************************
+                         qgsalgorithmrasterrank.h
+                         ---------------------
+    begin                : February 2024
+    copyright            : (C) 2024 by Mathieu Pellerin
+    email                : mathieu at opengis dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSALGORITHMRASTERRANK_H
+#define QGSALGORITHMRASTERRANK_H
+
+#define SIP_NO_FILE
+
+#include "qgis_sip.h"
+#include "qgsprocessingalgorithm.h"
+#include "qgsapplication.h"
+
+///@cond PRIVATE
+
+/**
+ * Native raster rank algorithm.
+ */
+class QgsRasterRankAlgorithm : public QgsProcessingAlgorithm
+{
+  public:
+    QgsRasterRankAlgorithm() = default;
+    void initAlgorithm( const QVariantMap &configuration = QVariantMap() ) override;
+    QString name() const override;
+    QString displayName() const override;
+    QStringList tags() const override;
+    QString group() const override;
+    QString groupId() const override;
+    QString shortHelpString() const override;
+    QgsRasterRankAlgorithm *createInstance() const override SIP_FACTORY;
+
+  protected:
+    bool prepareAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+    QVariantMap processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
+
+    QList<int> mRanks;
+    std::vector<std::unique_ptr<QgsMapLayer>> mLayers;
+};
+
+///@endcond PRIVATE
+
+#endif // QGSALGORITHMRASTERRANK_H
