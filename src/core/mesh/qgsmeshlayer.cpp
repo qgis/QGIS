287fc305c5f0282ea6361ba1dcc1c49b7592af7e
Jean Felder <jean.felder@oslandia.com>
2025-03-13 13:31:34 +0100

    qgsmeshlayer: Ensure to have an empty closesPoint if search fails
    With this change, `projectedPoint` is reset to an empty QgsPointXY
    before the search begins. This ensures that an empty point is returned
    is the search fails.

--- src/core/mesh/qgsmeshlayer.cpp
+++ src/core/mesh/qgsmeshlayer.cpp
@@ -926,6 +926,7 @@ int QgsMeshLayer::closestEdge( const QgsPointXY &point, double searchRadius, Qgs
   // search for the closest edge in search area from point
   const QList<int> edgeIndexes = mesh->edgeIndexesForRectangle( searchRectangle );
   int selectedIndex = -1;
+  projectedPoint = QgsPointXY();
   if ( mesh->contains( QgsMesh::Edge ) &&
        mDataProvider->isValid() )
   {
