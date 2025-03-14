ca438db65d35eb59581117e576a6486f1582f535
Jan Caha <jan.caha@outlook.com>
2025-03-14 10:32:50 +0100

    generic QgsDatabaseSchemaSelectionDialog instead of specific for postgresql

---
+++ src/gui/qgsdatabaseschemaselectiondialog.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+    qgsdatabaseschemaselectiondialog.h
+    ---------------------
+    begin                : March 2025
+    copyright            : (C) 2025 by Jan Caha
+    email                : jan.caha at outlook dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSDATABASESCHEMASELECTIONDIALOG_H
+#define QGSDATABASESCHEMASELECTIONDIALOG_H
+
+#include <QDialog>
+#include <QDialogButtonBox>
+
+#include "qgsabstractdatabaseproviderconnection.h"
+#include "qgsdatabaseschemacombobox.h"
+
+///@cond PRIVATE
+#ifndef SIP_RUN
+
+/**
+ * \ingroup gui
+ * \brief The QgsDatabaseSchemaSelectionDialog class is a dialog which displays selection of schema for a specific database connection.
+ *
+ * \warning The provider must support the connection API methods in its QgsProviderMetadata implementation.
+ *
+ * \since QGIS 3.44
+ */
+class GUI_EXPORT QgsDatabaseSchemaSelectionDialog : public QDialog
+{
+    Q_OBJECT
+  public:
+    /**
+     * Constructor for QgsDatabaseSchemaSelectionDialog, for the specified \a connection.
+     *
+     * Ownership of \a connection is transferred.
+     */
+    explicit QgsDatabaseSchemaSelectionDialog( std::unique_ptr<QgsAbstractDatabaseProviderConnection> connection, QWidget *parent SIP_TRANSFERTHIS = nullptr );
+
+    /**
+     * Returns the name of the current schema selected in the combo box.
+     */
+    QString selectedSchema();
+
+  private:
+    QDialogButtonBox *mButtonBox;
+    QgsDatabaseSchemaComboBox *mCboSchema = nullptr;
+    std::unique_ptr<QgsAbstractDatabaseProviderConnection> mDatabaseConnection = nullptr;
+};
+#endif
+
+#endif // QGSDATABASESCHEMASELECTIONDIALOG_H
