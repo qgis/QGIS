59491efe0d4eb4418daa110c19b674819f7789fd
Elton Chan <61935006+eltonhcchan@users.noreply.github.com>
2025-03-16 20:38:29 +0800

    Add protocol values for ESRI Geoportal

--- python/plugins/MetaSearch/link_types.py
+++ python/plugins/MetaSearch/link_types.py
@@ -54,8 +54,8 @@ WCS_LINK_TYPES = [
     "OGC Web Coverage Service",
 ]
 
-AMS_LINK_TYPES = ["ESRI:ArcGIS:MapServer", "Esri REST: Map Service", "ESRI REST"]
+AMS_LINK_TYPES = ["ESRI:ArcGIS:MapServer", "Esri REST: Map Service", "ESRI REST", "MapServer"]
 
-AFS_LINK_TYPES = ["ESRI:ArcGIS:FeatureServer", "Esri REST: Feature Service"]
+AFS_LINK_TYPES = ["ESRI:ArcGIS:FeatureServer", "Esri REST: Feature Service", "FeatureServer"]
 
 GIS_FILE_LINK_TYPES = ["FILE:GEO"]
