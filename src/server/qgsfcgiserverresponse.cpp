f23a29d1a8b53710b03c30cde4e3c7b14c457efe
Juergen E. Fischer <jef@norbit.de>
2025-03-22 08:46:27 +0100

    no gettid() on OSX (followup 154ad712e9c25d55a5afcaa221e99016b9db29df)

--- src/server/qgsfcgiserverresponse.cpp
+++ src/server/qgsfcgiserverresponse.cpp
@@ -23,6 +23,7 @@
 #include "qgsmessagelog.h"
 #include <fcgi_stdio.h>
 #include <QDebug>
+#include <QThread>
 
 #include "qgslogger.h"
 
@@ -114,7 +115,7 @@ void QgsSocketMonitoringThread::run()
   }
 
 #if defined( Q_OS_UNIX ) && !defined( Q_OS_ANDROID )
-  const pid_t threadId = gettid();
+  quint64 threadId = reinterpret_cast<quint64>( QThread::currentThreadId() );
 
   mShouldStop.store( false );
   char c;
