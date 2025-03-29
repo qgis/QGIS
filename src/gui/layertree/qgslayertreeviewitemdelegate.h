3728bbd319377ba8e8f67c2f1e1d63c525fdd3bb
Alex Kent Hajnal <software@alephnull.net>
2025-03-29 16:29:24 -0400

    Added documentation to qgslayertreeviewitemdelegate.h

--- src/gui/layertree/qgslayertreeviewitemdelegate.h
+++ src/gui/layertree/qgslayertreeviewitemdelegate.h
@@ -46,7 +46,57 @@ class QgsLayerTreeViewProxyStyle : public QgsProxyStyle
 
 
 /**
- * Item delegate that adds drawing of indicators
+ * \ingroup gui
+ * \class QgsLayerTreeViewItemDelegate
+ * \brief Item delegate that adds drawing of layer status indicators to the
+ * Layers panel (legend).
+ *
+ * Plug-ins that customize the drawing of the legend entries should use this
+ * class as their base class (rather than QStyledItemDelegate); this ensures
+ * that the status icons are displayed.
+ *
+ * (Note that to add status icons to a legend layer one should use not subclass
+ * this class but rather use QgsLayerTreeView.addIndicator.)
+ *
+ * This class was not exported in earlier versions of QGIS.  To ensure backwards
+ * compatibility, plug-in code should test whether QgsLayerTreeViewItemDelegate
+ * is available and if it is not should fall back to using QStyledItemDelegate
+ * as the base class.  This can be done as follows:
+ *
+ * Import the best available class
+ * -------------------------------
+ *
+ *     try:
+ *         from qgis.gui import QgsLayerTreeViewItemDelegate
+ *         has_QgsLayerTreeViewItemDelegate = True
+ *     except:
+ *         # QgsLayerTreeViewItemDelegate isn't exported
+ *         # so fall back to using QStyledItemDelegate
+ *         from PyQt5.QtWidgets import QStyledItemDelegate
+ *         has_QgsLayerTreeViewItemDelegate = False
+ *
+ * Access the class
+ * ----------------
+ *
+ * The best way to access the imported class depends on the context.  Here are
+ * some examples of how it can be done in a backwards-compatible manner:
+ *
+ *  **Saving the existing delegate**
+ *
+ *     if has_QgsLayerTreeViewItemDelegate:
+ *         original_delegate = QgsLayerTreeViewItemDelegate( self.iface.layerTreeView() )
+ *     else:
+ *         original_delegate = QStyledItemDelegate( self.iface.layerTreeView() )
+ *
+ * **Passing a reference to the delegate class**
+ *
+ *     if has_QgsLayerTreeViewItemDelegate:
+ *         delegateClass = QgsLayerTreeViewItemDelegate
+ *     else:
+ *         delegateClass = QStyledItemDelegate
+ *
+ *     class CusomLayerItemDelegate( delegateClass ):
+ *         # …
  */
 class GUI_EXPORT QgsLayerTreeViewItemDelegate : public QStyledItemDelegate
 {
