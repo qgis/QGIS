ce038ccbe423779e826f5a37b9b61b7ef53da4ca
Alexander Bruy <alexander.bruy@gmail.com>
2025-04-05 17:16:45 +0100

    use enum class instead of plain enum and add test to ensure comparison with int works

--- python/PyQt6/analysis/auto_additions/qgsninecellfilter.py
+++ python/PyQt6/analysis/auto_additions/qgsninecellfilter.py
@@ -1,12 +1,27 @@
 # The following has been generated automatically from src/analysis/raster/qgsninecellfilter.h
-QgsNineCellFilter.Success = QgsNineCellFilter.Result.Success
-QgsNineCellFilter.InputLayerError = QgsNineCellFilter.Result.InputLayerError
-QgsNineCellFilter.DriverError = QgsNineCellFilter.Result.DriverError
-QgsNineCellFilter.CreateOutputError = QgsNineCellFilter.Result.CreateOutputError
-QgsNineCellFilter.InputBandError = QgsNineCellFilter.Result.InputBandError
-QgsNineCellFilter.OutputBandError = QgsNineCellFilter.Result.OutputBandError
-QgsNineCellFilter.RasterSizeError = QgsNineCellFilter.Result.RasterSizeError
-QgsNineCellFilter.Canceled = QgsNineCellFilter.Result.Canceled
+# monkey patching scoped based enum
+QgsNineCellFilter.Result.Success.__doc__ = "Operation completed successfully"
+QgsNineCellFilter.Result.InputLayerError.__doc__ = "Error reading input file"
+QgsNineCellFilter.Result.DriverError.__doc__ = "Could not open the driver for the specified format"
+QgsNineCellFilter.Result.CreateOutputError.__doc__ = "Error creating output file"
+QgsNineCellFilter.Result.InputBandError.__doc__ = "Error reading input raster band"
+QgsNineCellFilter.Result.OutputBandError.__doc__ = "Error reading output raster band"
+QgsNineCellFilter.Result.RasterSizeError.__doc__ = "Raster height is too small (need at least 3 rows)"
+QgsNineCellFilter.Result.Canceled.__doc__ = "User canceled calculation"
+QgsNineCellFilter.Result.__doc__ = """
+.. versionadded:: 3.44
+
+* ``Success``: Operation completed successfully
+* ``InputLayerError``: Error reading input file
+* ``DriverError``: Could not open the driver for the specified format
+* ``CreateOutputError``: Error creating output file
+* ``InputBandError``: Error reading input raster band
+* ``OutputBandError``: Error reading output raster band
+* ``RasterSizeError``: Raster height is too small (need at least 3 rows)
+* ``Canceled``: User canceled calculation
+
+"""
+# --
 try:
     QgsNineCellFilter.__abstract_methods__ = ['processNineCellWindow']
     QgsNineCellFilter.__group__ = ['raster']
