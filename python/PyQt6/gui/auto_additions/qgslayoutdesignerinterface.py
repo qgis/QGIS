24ffea84a4988bc1937b735d5c9c328405c517e5
Nyall Dawson <nyall.dawson@gmail.com>
2025-04-08 15:24:44 +1000

    Fix build error for PyQGIS docs
    Correctly convert typehints for c++ pointer arguments

--- python/PyQt6/gui/auto_additions/qgslayoutdesignerinterface.py
+++ python/PyQt6/gui/auto_additions/qgslayoutdesignerinterface.py
@@ -3,7 +3,7 @@ QgsLayoutDesignerInterface.ToolMoveItemContent = QgsLayoutDesignerInterface.Stan
 QgsLayoutDesignerInterface.ToolMoveItemNodes = QgsLayoutDesignerInterface.StandardTool.ToolMoveItemNodes
 try:
     QgsLayoutDesignerInterface.ExportResults.__attribute_docs__ = {'result': 'Result/error code of export.', 'labelingResults': 'Returns the labeling results for all map items included in the export. Map keys are the item UUIDs (see :py:func:`QgsLayoutItem.uuid()`).\n\nOwnership of the results remains with the layout designer.'}
-    QgsLayoutDesignerInterface.ExportResults.__annotations__ = {'result': 'QgsLayoutExporter.ExportResult', 'labelingResults': 'Dict[str, QgsLabelingResults *]'}
+    QgsLayoutDesignerInterface.ExportResults.__annotations__ = {'result': 'QgsLayoutExporter.ExportResult', 'labelingResults': 'Dict[str, QgsLabelingResults]'}
     QgsLayoutDesignerInterface.ExportResults.__group__ = ['layout']
 except (NameError, AttributeError):
     pass
--- python/gui/auto_additions/qgslayoutdesignerinterface.py
+++ python/gui/auto_additions/qgslayoutdesignerinterface.py
@@ -1,7 +1,7 @@
 # The following has been generated automatically from src/gui/layout/qgslayoutdesignerinterface.h
 try:
     QgsLayoutDesignerInterface.ExportResults.__attribute_docs__ = {'result': 'Result/error code of export.', 'labelingResults': 'Returns the labeling results for all map items included in the export. Map keys are the item UUIDs (see :py:func:`QgsLayoutItem.uuid()`).\n\nOwnership of the results remains with the layout designer.'}
-    QgsLayoutDesignerInterface.ExportResults.__annotations__ = {'result': 'QgsLayoutExporter.ExportResult', 'labelingResults': 'Dict[str, QgsLabelingResults *]'}
+    QgsLayoutDesignerInterface.ExportResults.__annotations__ = {'result': 'QgsLayoutExporter.ExportResult', 'labelingResults': 'Dict[str, QgsLabelingResults]'}
     QgsLayoutDesignerInterface.ExportResults.__group__ = ['layout']
 except (NameError, AttributeError):
     pass
