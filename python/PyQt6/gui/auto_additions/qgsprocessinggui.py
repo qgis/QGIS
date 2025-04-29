9df6ea74fe8366ed2beb13f62e00429fc579d202
Nyall Dawson <nyall.dawson@gmail.com>
2025-04-30 09:00:12 +1000

    Remove outdated files

--- python/PyQt6/gui/auto_additions/qgsprocessinggui.py
+++
@@ -1,8 +0,0 @@
-# The following has been generated automatically from src/gui/processing/qgsprocessinggui.h
-QgsProcessingGui.Standard = QgsProcessingGui.WidgetType.Standard
-QgsProcessingGui.Batch = QgsProcessingGui.WidgetType.Batch
-QgsProcessingGui.Modeler = QgsProcessingGui.WidgetType.Modeler
-try:
-    QgsProcessingGui.__group__ = ['processing']
-except (NameError, AttributeError):
-    pass
