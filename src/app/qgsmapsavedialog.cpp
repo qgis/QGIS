7d453b4d190be8f010444c1ba9cb5828f3558a86
viperminiq <viperminiq@gmail.com>
2025-04-30 07:35:35 +0200

    overrirde accept

--- src/app/qgsmapsavedialog.cpp
+++ src/app/qgsmapsavedialog.cpp
@@ -449,13 +449,20 @@ void QgsMapSaveDialog::copyToClipboard()
   QgsApplication::taskManager()->addTask( mapRendererTask );
 }
 
+void QgsMapSaveDialog::accept()
+{
+  // prevent the dialog from closing before saving the image/pdf
+  QgsMapSaveDialog::onAccepted();
+  QDialog::accept();
+}
+
 void QgsMapSaveDialog::onAccepted()
 {
   switch ( mDialogType )
   {
     case Image:
     {
-      const QPair<QString, QString> fileNameAndFilter = QgsGuiUtils::getSaveAsImageName( QgisApp::instance(), tr( "Choose a file name to save the map image as" ) );
+      const QPair<QString, QString> fileNameAndFilter = QgsGuiUtils::getSaveAsImageName( this, tr( "Choose a file name to save the map image as" ) );
       if ( !fileNameAndFilter.first.isEmpty() )
       {
         QgsMapSettings ms = QgsMapSettings();
@@ -503,7 +510,7 @@ void QgsMapSaveDialog::onAccepted()
     {
       QgsSettings settings;
       const QString lastUsedDir = settings.value( QStringLiteral( "UI/lastSaveAsImageDir" ), QDir::homePath() ).toString();
-      QString fileName = QFileDialog::getSaveFileName( QgisApp::instance(), tr( "Save Map As" ), lastUsedDir, tr( "PDF Format" ) + " (*.pdf *.PDF)" );
+      QString fileName = QFileDialog::getSaveFileName( this, tr( "Save Map As" ), lastUsedDir, tr( "PDF Format" ) + " (*.pdf *.PDF)" );
       // return dialog focus on Mac
       activateWindow();
       raise();
