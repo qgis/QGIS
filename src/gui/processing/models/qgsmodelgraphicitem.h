20e33c00f9688e6c17936003d2c58cf4bdb5e541
jonathanlurie <lurie.jo@gmail.com>
2025-05-13 18:01:20 +0200

    refectored param color logic

--- src/gui/processing/models/qgsmodelgraphicitem.h
+++ src/gui/processing/models/qgsmodelgraphicitem.h
@@ -159,8 +159,6 @@ class GUI_EXPORT QgsModelDesignerSocketGraphicItem : public QgsModelDesignerFlat
 {
     Q_OBJECT
   public:
-    static QColor typeToColorLookup( QString dataType );
-
     /**
      * Constructor for QgsModelDesignerSocketGraphicItem, with the specified \a parent item.
      *
