585aa5894cdfb47893603577db3113d6d93842e6
Even Rouault <even.rouault@spatialys.com>
2025-05-17 00:54:45 +0200

    [Tests] Fix test_core_ogcutils on QT6

--- tests/src/core/testqgsogcutils.cpp
+++ tests/src/core/testqgsogcutils.cpp
@@ -190,25 +190,25 @@ void TestQgsOgcUtils::testExpressionFromOgcFilterWFS20_data()
 
   QTest::newRow( "bbox coordinates" ) << QString(
     "<Filter>"
-    "<BBOX><ValueReference>Name>NAME</ValueReference><gml:Box srsName='foo'>"
+    "<BBOX><ValueReference>Name>NAME</ValueReference><gml:Box xmlns:gml=\"http://www.opengis.net/gml\" srsName=\"foo\">"
     "<gml:coordinates>135.2239,34.4879 135.8578,34.8471</gml:coordinates></gml:Box></BBOX>"
     "</Filter>"
   )
-                                      << QStringLiteral( "intersects_bbox($geometry, geom_from_gml('<Box srsName=\"foo\"><coordinates>135.2239,34.4879 135.8578,34.8471</coordinates></Box>'))" );
+                                      << QStringLiteral( "intersects_bbox($geometry, geom_from_gml('<gml:Box xmlns:gml=\"http://www.opengis.net/gml\" srsName=\"foo\"><gml:coordinates xmlns:gml=\"http://www.opengis.net/gml\">135.2239,34.4879 135.8578,34.8471</gml:coordinates></gml:Box>'))" );
 
   QTest::newRow( "bbox corner" )
     << QString(
-         "<fes:Filter>"
+         "<fes:Filter xmlns:fes=\"http://www.opengis.net/fes/2.0\">"
          "<fes:BBOX>"
          "<fes:ValueReference>my_geometry_name</fes:ValueReference>"
-         "<gml:Envelope>"
+         "<gml:Envelope xmlns:gml=\"http://www.opengis.net/gml\">"
          "<gml:lowerCorner>49 2</gml:lowerCorner>"
          "<gml:upperCorner>50 3</gml:upperCorner>"
          "</gml:Envelope>"
          "</fes:BBOX>"
          "</fes:Filter>"
        )
-    << QStringLiteral( "intersects_bbox($geometry, geom_from_gml('<Envelope><lowerCorner>49 2</lowerCorner><upperCorner>50 3</upperCorner></Envelope>'))" );
+    << QStringLiteral( "intersects_bbox($geometry, geom_from_gml('<gml:Envelope xmlns:gml=\"http://www.opengis.net/gml\"><gml:lowerCorner xmlns:gml=\"http://www.opengis.net/gml\">49 2</gml:lowerCorner><gml:upperCorner xmlns:gml=\"http://www.opengis.net/gml\">50 3</gml:upperCorner></gml:Envelope>'))" );
 }
 
 void TestQgsOgcUtils::testExpressionFromOgcFilterWFS20()
@@ -257,7 +257,7 @@ void TestQgsOgcUtils::testExpressionFromOgcFilter_data()
                        << QStringLiteral( "COUNT > 3" );
 
   QTest::newRow( "AND" ) << QString(
-    "<ogc:Filter>"
+    "<ogc:Filter xmlns:ogc=\"http://www.opengis.net/ogc\">"
     "<ogc:And>"
     "<ogc:PropertyIsGreaterThanOrEqualTo>"
     "<ogc:PropertyName>pop</ogc:PropertyName>"
@@ -358,7 +358,7 @@ void TestQgsOgcUtils::testExpressionFromOgcFilter_data()
 
   QTest::newRow( "is null" ) << QString(
     "<Filter>"
-    "<ogc:PropertyIsNull>"
+    "<ogc:PropertyIsNull xmlns:ogc=\"http://www.opengis.net/ogc\">"
     "<ogc:PropertyName>FIRST_NAME</ogc:PropertyName>"
     "</ogc:PropertyIsNull>"
     "</Filter>"
@@ -367,23 +367,23 @@ void TestQgsOgcUtils::testExpressionFromOgcFilter_data()
 
   QTest::newRow( "bbox with GML2 Box" ) << QString(
     "<Filter>"
-    "<BBOX><PropertyName>Name>NAME</PropertyName><gml:Box srsName='foo'>"
+    "<BBOX><PropertyName>Name>NAME</PropertyName><gml:Box xmlns:gml=\"http://www.opengis.net/gml\" srsName=\"foo\">"
     "<gml:coordinates>135.2239,34.4879 135.8578,34.8471</gml:coordinates></gml:Box></BBOX>"
     "</Filter>"
   )
-                                        << QStringLiteral( "intersects_bbox($geometry, geom_from_gml('<Box srsName=\"foo\"><coordinates>135.2239,34.4879 135.8578,34.8471</coordinates></Box>'))" );
+                                        << QStringLiteral( "intersects_bbox($geometry, geom_from_gml('<gml:Box xmlns:gml=\"http://www.opengis.net/gml\" srsName=\"foo\"><gml:coordinates xmlns:gml=\"http://www.opengis.net/gml\">135.2239,34.4879 135.8578,34.8471</gml:coordinates></gml:Box>'))" );
 
   QTest::newRow( "Intersects" ) << QString(
     "<Filter>"
     "<Intersects>"
     "<PropertyName>GEOMETRY</PropertyName>"
-    "<gml:Point>"
+    "<gml:Point xmlns:gml=\"http://www.opengis.net/gml\">"
     "<gml:coordinates>123,456</gml:coordinates>"
     "</gml:Point>"
     "</Intersects>"
     "</Filter>"
   )
-                                << QStringLiteral( "intersects($geometry, geom_from_gml('<Point><coordinates>123,456</coordinates></Point>'))" );
+                                << QStringLiteral( "intersects($geometry, geom_from_gml('<gml:Point xmlns:gml=\"http://www.opengis.net/gml\"><gml:coordinates xmlns:gml=\"http://www.opengis.net/gml\">123,456</gml:coordinates></gml:Point>'))" );
 
   QTest::newRow( "Literal conversion" ) << QString(
     "<Filter><PropertyIsEqualTo>"
