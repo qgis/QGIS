qgis/QGIS

commit 2078404ba7f7947c61123396feba1acd5e4c69e9

Globe: finally make it available from the GUI
This is a mix of additions and updates for the initial support of globe mode
in 3D views within QGIS desktop app. Many things are still missing, and will
be introduced in later PRs...

src/app/qgisapp.cpp
@@ -2897,6 +2897,7 @@ void QgisApp::createActions()
   connect( mActionSaveMapAsPdf, &QAction::triggered, this, [=] { saveMapAsPdf(); } );
   connect( mActionNewMapCanvas, &QAction::triggered, this, &QgisApp::newMapCanvas );
   connect( mActionNew3DMapCanvas, &QAction::triggered, this, &QgisApp::new3DMapCanvas );
+  connect( mActionNew3DMapCanvasGlobe, &QAction::triggered, this, &QgisApp::new3DMapCanvasGlobe );
   connect( mActionNewPrintLayout, &QAction::triggered, this, &QgisApp::newPrintLayout );
   connect( mActionNewReport, &QAction::triggered, this, &QgisApp::newReport );
   connect( mActionShowLayoutManager, &QAction::triggered, this, &QgisApp::showLayoutManager );
@@ -9263,6 +9264,7 @@ void QgisApp::populate3DMapviewsMenu( QMenu *menu )
   menu->addActions( acts );
   menu->addSeparator();
   menu->addAction( mActionNew3DMapCanvas );
+  menu->addAction( mActionNew3DMapCanvasGlobe );
   menu->addAction( mActionManage3DMapViews );
 
 #else
@@ -13186,10 +13188,15 @@ QgsElevationProfileWidget *QgisApp::createNewElevationProfile()
 
 void QgisApp::new3DMapCanvas()
 {
-  createNewMapCanvas3D( QString() );
+  createNewMapCanvas3D( QString(), Qgis::SceneMode::Local );
 }
 
-Qgs3DMapCanvas *QgisApp::createNewMapCanvas3D( const QString &name )
+void QgisApp::new3DMapCanvasGlobe()
+{
+  createNewMapCanvas3D( QString(), Qgis::SceneMode::Globe );
+}
+
+Qgs3DMapCanvas *QgisApp::createNewMapCanvas3D( const QString &name, Qgis::SceneMode sceneMode )
 {
 #ifdef HAVE_3D
   // initialize from project
@@ -13218,23 +13225,36 @@ Qgs3DMapCanvas *QgisApp::createNewMapCanvas3D( const QString &name )
     QgsSettings settings;
 
     Qgs3DMapSettings *map = new Qgs3DMapSettings;
-    if ( !prj->crs3D().isGeographic() )
+    if ( sceneMode == Qgis::SceneMode::Globe )
     {
-      map->setCrs( prj->crs3D() );
+      // Geocentric CRS based on WGS 84
+      map->setCrs( QgsCoordinateReferenceSystem( QStringLiteral( "EPSG:4978" ) ) );
+
+      // 3D axis is not very useful with geocentric CRS: disable it by default
+      Qgs3DAxisSettings axis;
+      axis.setMode( Qgs3DAxisSettings::Mode::Off );
+      map->set3DAxisSettings( axis );
     }
-    else
+    else  // local scene
     {
-      map->setCrs( QgsCoordinateReferenceSystem( QStringLiteral( "EPSG:3857" ) ) );
+      if ( !prj->crs3D().isGeographic() )
+      {
+        map->setCrs( prj->crs3D() );
+      }
+      else
+      {
+        map->setCrs( QgsCoordinateReferenceSystem( QStringLiteral( "EPSG:3857" ) ) );
+      }
     }
 
-    const QgsReferencedRectangle projectExtent = prj->viewSettings()->fullExtent();
-    const QgsRectangle fullExtent = Qgs3DUtils::tryReprojectExtent2D( projectExtent, projectExtent.crs(), map->crs(), prj->transformContext() );
     map->setSelectionColor( mMapCanvas->selectionColor() );
     map->setBackgroundColor( mMapCanvas->canvasColor() );
     map->setLayers( mMapCanvas->layers( true ) );
     map->setTemporalRange( mMapCanvas->temporalRange() );
 
-    const Qgis::NavigationMode defaultNavMode = settings.enumValue( QStringLiteral( "map3d/defaultNavigation" ), Qgis::NavigationMode::TerrainBased, QgsSettings::App );
+    Qgis::NavigationMode defaultNavMode = settings.enumValue( QStringLiteral( "map3d/defaultNavigation" ), Qgis::NavigationMode::TerrainBased, QgsSettings::App );
+    if ( defaultNavMode == Qgis::NavigationMode::TerrainBased && sceneMode == Qgis::SceneMode::Globe )
+      defaultNavMode = Qgis::NavigationMode::GlobeTerrainBased;
     map->setCameraNavigationMode( defaultNavMode );
 
     map->setCameraMovementSpeed( settings.value( QStringLiteral( "map3d/defaultMovementSpeed" ), 5, QgsSettings::App ).toDouble() );
@@ -13246,7 +13266,16 @@ Qgs3DMapCanvas *QgisApp::createNewMapCanvas3D( const QString &name )
     map->setPathResolver( QgsProject::instance()->pathResolver() );
     map->setMapThemeCollection( QgsProject::instance()->mapThemeCollection() );
 
-    map->configureTerrainFromProject( QgsProject::instance()->elevationProperties(), fullExtent );
+    if ( sceneMode == Qgis::SceneMode::Local )
+    {
+      const QgsReferencedRectangle projectExtent = prj->viewSettings()->fullExtent();
+      const QgsRectangle fullExtent = Qgs3DUtils::tryReprojectExtent2D( projectExtent, projectExtent.crs(), map->crs(), prj->transformContext() );
+      map->configureTerrainFromProject( QgsProject::instance()->elevationProperties(), fullExtent );
+    }
+    else
+    {
+      map->configureTerrainFromProject( QgsProject::instance()->elevationProperties(), QgsRectangle() );
+    }
 
     // new scenes default to a single directional light
     map->setLightSources( QList<QgsLightSource *>() << new QgsDirectionalLightSettings() );
@@ -13259,9 +13288,26 @@ Qgs3DMapCanvas *QgisApp::createNewMapCanvas3D( const QString &name )
 
     canvasWidget->setMapSettings( map );
 
-    const QgsRectangle canvasExtent = Qgs3DUtils::tryReprojectExtent2D( mMapCanvas->extent(), mMapCanvas->mapSettings().destinationCrs(), map->crs(), prj->transformContext() );
-    float dist = static_cast<float>( std::max( canvasExtent.width(), canvasExtent.height() ) );
-    canvasWidget->mapCanvas3D()->setViewFromTop( canvasExtent.center(), dist, static_cast<float>( mMapCanvas->rotation() ) );
+    if ( sceneMode == Qgis::SceneMode::Local )
+    {
+      const QgsRectangle canvasExtent = Qgs3DUtils::tryReprojectExtent2D( mMapCanvas->extent(), mMapCanvas->mapSettings().destinationCrs(), map->crs(), prj->transformContext() );
+      float dist = static_cast<float>( std::max( canvasExtent.width(), canvasExtent.height() ) );
+      canvasWidget->mapCanvas3D()->setViewFromTop( canvasExtent.center(), dist, static_cast<float>( mMapCanvas->rotation() ) );
+    }
+    else
+    {
+      double centerLat = 0, centerLon = 0;
+      double dist = 10'000'000;
+      const QgsRectangle canvasExtentLatLon = Qgs3DUtils::tryReprojectExtent2D( mMapCanvas->extent(), mMapCanvas->mapSettings().destinationCrs(), map->crs().toGeographicCrs(), prj->transformContext() );
+      if ( QgsRectangle( -180, -90, 180, 90 ).contains( canvasExtentLatLon ) )
+      {
+        centerLon = ( canvasExtentLatLon.xMinimum() + canvasExtentLatLon.xMaximum() ) / 2;
+        centerLat = ( canvasExtentLatLon.yMinimum() + canvasExtentLatLon.yMaximum() ) / 2;
+        constexpr double METERS_PER_DEGREE = 111'000;  // rough approximation (2*pi*R / 360)
+        dist = std::max( canvasExtentLatLon.width(), canvasExtentLatLon.height() ) * METERS_PER_DEGREE;
+      }
+      canvasWidget->mapCanvas3D()->cameraController()->resetGlobe( static_cast<float>( dist ), centerLat, centerLon );
+    }
 
     const Qgis::VerticalAxisInversion axisInversion = settings.enumValue( QStringLiteral( "map3d/axisInversion" ), Qgis::VerticalAxisInversion::WhenDragging, QgsSettings::App );
     if ( canvasWidget->mapCanvas3D()->cameraController() )

images/images.qrc
@@ -775,6 +775,7 @@
         <file>themes/default/mesh/SingleTop.svg</file>
         <file>themes/default/mesh/SingleBottom.svg</file>
         <file>themes/default/mActionNew3DMap.svg</file>
+        <file>themes/default/mActionNew3DMapGlobe.svg</file>
         <file>themes/default/mActionNewMap.svg</file>
         <file>themes/default/mActionNext.svg</file>
         <file>themes/default/mActionMapSettings.svg</file>

images/themes/default/mActionNew3DMapGlobe.svg
@@ -1,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   height="24"
+   width="24"
+   version="1.1"
+   id="svg27"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs31" />
+  <linearGradient
+     id="a"
+     gradientUnits="userSpaceOnUse"
+     x1="3.123"
+     x2="8.275"
+     y1="12.47"
+     y2="14.921">
+    <stop
+       offset="0"
+       stop-color="#bfbfbf"
+       id="stop2" />
+    <stop
+       offset="1"
+       stop-color="#e1e1e1"
+       id="stop4" />
+  </linearGradient>
+  <circle
+     style="fill:#e1e1e1;fill-opacity:1;stroke:none;stroke-opacity:1"
+     id="path243"
+     cx="11.741615"
+     cy="11.794744"
+     r="10.38117" />
+  <path
+     style="fill:#000000;fill-opacity:1;stroke:#aaaaaa;stroke-width:0.5;stroke-dasharray:none;stroke-opacity:1"
+     d="m 1.527484,11.958308 h 20.65333"
+     id="path3312" />
+  <path
+     style="fill:none;fill-opacity:1;stroke:#aaaaaa;stroke-width:0.5;stroke-dasharray:none;stroke-opacity:1"
+     d="M 3.0195293,16.929054 20.799589,16.895584"
+     id="path3453-6" />
+  <path
+     style="fill:none;fill-opacity:1;stroke:#aaaaaa;stroke-width:0.5;stroke-dasharray:none;stroke-opacity:1"
+     d="M 3.0728353,7.1807301 20.852895,7.1472612"
+     id="path3453" />
+  <path
+     style="fill:none;fill-opacity:1;stroke:#aaaaaa;stroke-width:0.5;stroke-dasharray:none;stroke-opacity:1"
+     d="M 11.969326,2.0753016 V 22.576437"
+     id="path3482" />
+  <path
+     style="fill:none;fill-opacity:1;stroke:#aaaaaa;stroke-width:0.5;stroke-dasharray:none;stroke-opacity:1"
+     d="M 5.3972836,20.282138 C 9.57573,16.909944 9.8627238,7.1594096 6.0299447,3.4736869 M 18.316777,20.423372 C 14.336247,16.92757 14.077998,7.1526595 18.284256,3.8875125"
+     id="path3484" />
+  <circle
+     style="fill:none;fill-opacity:1;stroke:#9a9a9a;stroke-opacity:1"
+     id="path243-3"
+     cx="11.937604"
+     cy="12.178329"
+     r="10.38117" />
+  <g
+     transform="translate(33)"
+     id="g25">
+    <rect
+       fill="#c4a000"
+       height="11"
+       rx="2.011"
+       width="11"
+       x="-20"
+       y="13"
+       id="rect17" />
+    <g
+       fill="#fcffff"
+       id="g23">
+      <path
+         d="m-15 14v2.063a2.501 2.501 0 0 0 -1.375.78l-1.781-1.03-.5.874 1.781 1.032a2.46 2.46 0 0 0 -.125.781c0 .275.043.534.125.781l-1.781 1.032.5.875 1.781-1.032c.353.4.833.67 1.375.782v2.062h1v-2.063a2.501 2.501 0 0 0 1.375-.78l1.781 1.03.5-.875-1.781-1.03a2.46 2.46 0 0 0 .125-.782 2.46 2.46 0 0 0 -.125-.781l1.781-1.032-.5-.875-1.781 1.032c-.353-.4-.833-.67-1.375-.782v-2.062zm.5 3.5a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+         id="path19" />
+      <path
+         d="m-19 19 9-.01v-2c0-2.99-1-2.99-4.5-2.99s-4.5 0-4.5 3z"
+         fill-rule="evenodd"
+         opacity=".3"
+         id="path21" />
+    </g>
+  </g>
+</svg>

python/3d/auto_additions/qgs3dmapscene.py
@@ -1,6 +1,6 @@
 # The following has been generated automatically from src/3d/qgs3dmapscene.h
 try:
-    Qgs3DMapScene.__attribute_docs__ = {'terrainEntityChanged': 'Emitted when the current terrain entity is replaced by a new one\n', 'terrainPendingJobsCountChanged': "Emitted when the number of terrain's pending jobs changes\n", 'totalPendingJobsCountChanged': 'Emitted when the total number of pending jobs changes\n\n.. versionadded:: 3.12\n', 'sceneStateChanged': "Emitted when the scene's state has changed\n", 'fpsCountChanged': 'Emitted when the FPS count changes\n', 'fpsCounterEnabledChanged': 'Emitted when the FPS counter is activated or deactivated\n', 'viewed2DExtentFrom3DChanged': 'Emitted when the viewed 2D extent seen by the 3D camera has changed\n\n.. versionadded:: 3.26\n', 'gpuMemoryLimitReached': "Emitted when one of the entities reaches its GPU memory limit\nand it is not possible to lower the GPU memory use by unloading\ndata that's not currently needed.\n"}
+    Qgs3DMapScene.__attribute_docs__ = {'terrainEntityChanged': 'Emitted when the current terrain entity is replaced by a new one\n', 'totalPendingJobsCountChanged': 'Emitted when the total number of pending jobs changes\n\n.. versionadded:: 3.12\n', 'sceneStateChanged': "Emitted when the scene's state has changed\n", 'fpsCountChanged': 'Emitted when the FPS count changes\n', 'fpsCounterEnabledChanged': 'Emitted when the FPS counter is activated or deactivated\n', 'viewed2DExtentFrom3DChanged': 'Emitted when the viewed 2D extent seen by the 3D camera has changed\n\n.. versionadded:: 3.26\n', 'gpuMemoryLimitReached': "Emitted when one of the entities reaches its GPU memory limit\nand it is not possible to lower the GPU memory use by unloading\ndata that's not currently needed.\n"}
     Qgs3DMapScene.openScenes = staticmethod(Qgs3DMapScene.openScenes)
     Qgs3DMapScene.__signal_arguments__ = {'fpsCountChanged': ['fpsCount: float'], 'fpsCounterEnabledChanged': ['fpsCounterEnabled: bool'], 'viewed2DExtentFrom3DChanged': ['extent: List[QgsPointXY]']}
 except (NameError, AttributeError):

python/3d/auto_generated/qgs3dmapscene.sip.in
@@ -31,6 +31,7 @@ Returns camera controller
 %End
 
 
+
     void viewZoomFull();
 %Docstring
 Resets camera view to show the whole scene (top view)
@@ -48,11 +49,6 @@ Resets camera view to show the extent ``extent`` (top view)
 Calculates the 2D extent viewed by the 3D camera as the vertices of the viewed trapezoid
 
 .. versionadded:: 3.26
-%End
-
-    int terrainPendingJobsCount() const;
-%Docstring
-Returns number of pending jobs of the terrain entity
 %End
 
     int totalPendingJobsCount() const;
@@ -215,10 +211,6 @@ Disables OpenGL clipping.
     void terrainEntityChanged();
 %Docstring
 Emitted when the current terrain entity is replaced by a new one
-%End
-    void terrainPendingJobsCountChanged();
-%Docstring
-Emitted when the number of terrain's pending jobs changes
 %End
 
     void totalPendingJobsCountChanged();

python/PyQt6/3d/auto_additions/qgs3dmapscene.py
@@ -2,7 +2,7 @@
 Qgs3DMapScene.Ready = Qgs3DMapScene.SceneState.Ready
 Qgs3DMapScene.Updating = Qgs3DMapScene.SceneState.Updating
 try:
-    Qgs3DMapScene.__attribute_docs__ = {'terrainEntityChanged': 'Emitted when the current terrain entity is replaced by a new one\n', 'terrainPendingJobsCountChanged': "Emitted when the number of terrain's pending jobs changes\n", 'totalPendingJobsCountChanged': 'Emitted when the total number of pending jobs changes\n\n.. versionadded:: 3.12\n', 'sceneStateChanged': "Emitted when the scene's state has changed\n", 'fpsCountChanged': 'Emitted when the FPS count changes\n', 'fpsCounterEnabledChanged': 'Emitted when the FPS counter is activated or deactivated\n', 'viewed2DExtentFrom3DChanged': 'Emitted when the viewed 2D extent seen by the 3D camera has changed\n\n.. versionadded:: 3.26\n', 'gpuMemoryLimitReached': "Emitted when one of the entities reaches its GPU memory limit\nand it is not possible to lower the GPU memory use by unloading\ndata that's not currently needed.\n"}
+    Qgs3DMapScene.__attribute_docs__ = {'terrainEntityChanged': 'Emitted when the current terrain entity is replaced by a new one\n', 'totalPendingJobsCountChanged': 'Emitted when the total number of pending jobs changes\n\n.. versionadded:: 3.12\n', 'sceneStateChanged': "Emitted when the scene's state has changed\n", 'fpsCountChanged': 'Emitted when the FPS count changes\n', 'fpsCounterEnabledChanged': 'Emitted when the FPS counter is activated or deactivated\n', 'viewed2DExtentFrom3DChanged': 'Emitted when the viewed 2D extent seen by the 3D camera has changed\n\n.. versionadded:: 3.26\n', 'gpuMemoryLimitReached': "Emitted when one of the entities reaches its GPU memory limit\nand it is not possible to lower the GPU memory use by unloading\ndata that's not currently needed.\n"}
     Qgs3DMapScene.openScenes = staticmethod(Qgs3DMapScene.openScenes)
     Qgs3DMapScene.__signal_arguments__ = {'fpsCountChanged': ['fpsCount: float'], 'fpsCounterEnabledChanged': ['fpsCounterEnabled: bool'], 'viewed2DExtentFrom3DChanged': ['extent: List[QgsPointXY]']}
 except (NameError, AttributeError):

python/PyQt6/3d/auto_generated/qgs3dmapscene.sip.in
@@ -31,6 +31,7 @@ Returns camera controller
 %End
 
 
+
     void viewZoomFull();
 %Docstring
 Resets camera view to show the whole scene (top view)
@@ -48,11 +49,6 @@ Resets camera view to show the extent ``extent`` (top view)
 Calculates the 2D extent viewed by the 3D camera as the vertices of the viewed trapezoid
 
 .. versionadded:: 3.26
-%End
-
-    int terrainPendingJobsCount() const;
-%Docstring
-Returns number of pending jobs of the terrain entity
 %End
 
     int totalPendingJobsCount() const;
@@ -215,10 +211,6 @@ Disables OpenGL clipping.
     void terrainEntityChanged();
 %Docstring
 Emitted when the current terrain entity is replaced by a new one
-%End
-    void terrainPendingJobsCountChanged();
-%Docstring
-Emitted when the number of terrain's pending jobs changes
 %End
 
     void totalPendingJobsCountChanged();

python/PyQt6/gui/auto_generated/qgisinterface.sip.in
@@ -97,9 +97,13 @@ Returns a list of all 3D map canvases open in the app.
 .. versionadded:: 3.36
 %End
 
-    virtual Qgs3DMapCanvas *createNewMapCanvas3D( const QString &name ) = 0;
+    virtual Qgs3DMapCanvas *createNewMapCanvas3D( const QString &name, Qgis::SceneMode sceneMode = Qgis::SceneMode::Local ) = 0;
 %Docstring
 Create a new 3D map canvas with the specified unique ``name``.
+
+.. note::
+
+   Scene mode (local or globe) can be selected since QGIS 3.44.
 
 .. seealso:: :py:func:`closeMapCanvas3D`
 

commit 4f15d0b86f979c3136e8e7fc3a25a457dfcbc091

qgisapp: Fix missing unused parameter with 3D build disabled
fixes: 2078404ba7f7947c61123396feba1acd5e4c69e9

src/app/qgisapp.cpp
@@ -13411,6 +13411,7 @@ Qgs3DMapCanvas *QgisApp::createNewMapCanvas3D( const QString &name, Qgis::SceneM
   }
 #else
   Q_UNUSED( name );
+  Q_UNUSED( sceneMode );
 #endif
   return nullptr;
 }