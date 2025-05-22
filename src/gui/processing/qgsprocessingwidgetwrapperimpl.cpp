778a03ae298107007b6f25b43f3552ad774e4a15
viperminiq <viperminiq@gmail.com>
2025-05-22 09:58:06 +0200

    fix drop event with qt copy action

--- src/gui/processing/qgsprocessingwidgetwrapperimpl.cpp
+++ src/gui/processing/qgsprocessingwidgetwrapperimpl.cpp
@@ -7097,7 +7097,8 @@ void QgsProcessingMultipleLayerLineEdit::dropEvent( QDropEvent *event )
   const QStringList uris = QgsProcessingMultipleInputPanelWidget::compatibleUrisFromMimeData( mParam, event->mimeData(), {} );
   if ( !uris.isEmpty() )
   {
-    event->acceptProposedAction();
+    event->setDropAction( Qt::CopyAction );
+    event->accept();
     QVariantList uriList;
     uriList.reserve( uris.size() );
     for ( const QString &uri : uris )
