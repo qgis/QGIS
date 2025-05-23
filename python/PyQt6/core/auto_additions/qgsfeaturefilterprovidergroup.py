d2377a0c48fa6ad341028ad9c1d91b0785089a73
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-05-23 09:47:01 +0700

    Sipify

--- python/PyQt6/core/auto_additions/qgsfeaturefilterprovidergroup.py
+++ python/PyQt6/core/auto_additions/qgsfeaturefilterprovidergroup.py
@@ -1,4 +1,4 @@
-# The following has been generated automatically from src/server/qgsfeaturefilterprovidergroup.h
+# The following has been generated automatically from src/core/qgsfeaturefilterprovidergroup.h
 try:
     QgsFeatureFilterProviderGroup.__overridden_methods__ = ['filterFeatures', 'layerAttributes', 'clone']
 except (NameError, AttributeError):
