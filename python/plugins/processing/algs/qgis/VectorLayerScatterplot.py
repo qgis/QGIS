e7173bec5425bb67ca555910636045e0ebfb3cd6
Klaus Agerskov <printzdk@gmail.com>
2025-06-04 16:08:05 +0200

    Added short description VectorLayerScatterplot.py

--- python/plugins/processing/algs/qgis/VectorLayerScatterplot.py
+++ python/plugins/processing/algs/qgis/VectorLayerScatterplot.py
@@ -136,6 +136,11 @@ class VectorLayerScatterplot(QgisAlgorithm):
     def displayName(self):
         return self.tr("Vector layer scatterplot")
 
+    def shortDescription(self):
+        return self.tr(
+            "This algorithm creates a simple X - Y scatter plot for a vector layer."
+        )
+
     def processAlgorithm(self, parameters, context, feedback):
         try:
             # importing plotly throws Python warnings from within the library - filter these out
