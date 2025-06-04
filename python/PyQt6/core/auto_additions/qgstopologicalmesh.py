206cd1229777863dfc2267269afbc801f14bd08f
Nyall Dawson <nyall.dawson@gmail.com>
2025-06-04 10:37:36 +1000

    Fix build

--- python/PyQt6/core/auto_additions/qgstopologicalmesh.py
+++ python/PyQt6/core/auto_additions/qgstopologicalmesh.py
@@ -1,9 +1,7 @@
 # The following has been generated automatically from src/core/mesh/qgstopologicalmesh.h
 try:
     QgsTopologicalMesh.createTopologicalMesh = staticmethod(QgsTopologicalMesh.createTopologicalMesh)
-    QgsTopologicalMesh.createNewTopologicalFaces = staticmethod(QgsTopologicalMesh.createNewTopologicalFaces)
     QgsTopologicalMesh.counterClockwiseFaces = staticmethod(QgsTopologicalMesh.counterClockwiseFaces)
-    QgsTopologicalMesh.checkTopologyOfVerticesAsFace = staticmethod(QgsTopologicalMesh.checkTopologyOfVerticesAsFace)
     QgsTopologicalMesh.checkTopology = staticmethod(QgsTopologicalMesh.checkTopology)
     QgsTopologicalMesh.vertexPositionInFace = staticmethod(QgsTopologicalMesh.vertexPositionInFace)
     QgsTopologicalMesh.__group__ = ['mesh']
@@ -15,9 +13,5 @@ except (NameError, AttributeError):
     pass
 try:
     QgsTopologicalMesh.Changes.__group__ = ['mesh']
-except (NameError, AttributeError):
-    pass
-try:
-    QgsMeshVertexCirculator.__group__ = ['mesh']
 except (NameError, AttributeError):
     pass
