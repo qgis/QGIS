b1fee3762b09bcd67841c37192c5448f679fd434
Germán Carrillo <gcarrillo@linuxmail.org>
2025-06-05 12:26:32 -0500

    Make sure the Processing tree view is expanded when a provider has been added/removed (i.e., when the model has been rebuilt)

--- python/PyQt6/gui/auto_additions/qgsprocessingtoolboxtreeview.py
+++ python/PyQt6/gui/auto_additions/qgsprocessingtoolboxtreeview.py
@@ -1,6 +1,6 @@
 # The following has been generated automatically from src/gui/processing/qgsprocessingtoolboxtreeview.h
 try:
-    QgsProcessingToolboxTreeView.__overridden_methods__ = ['keyPressEvent']
+    QgsProcessingToolboxTreeView.__overridden_methods__ = ['reset', 'keyPressEvent']
     QgsProcessingToolboxTreeView.__group__ = ['processing']
 except (NameError, AttributeError):
     pass
