58bc852ce3c546ec43afa5f202044468cd8b0867
Nyall Dawson <nyall.dawson@gmail.com>
2025-06-09 12:18:00 +1000

    Don't render effects which require rasterisation when forcing vector

--- src/core/effects/qgsblureffect.cpp
+++ src/core/effects/qgsblureffect.cpp
@@ -33,6 +33,13 @@ void QgsBlurEffect::draw( QgsRenderContext &context )
   if ( !enabled() || !context.painter() || source().isNull() )
     return;
 
+  if ( context.rasterizedRenderingPolicy() == Qgis::RasterizedRenderingPolicy::ForceVector )
+  {
+    //just draw unmodified source, we can't render this effect when forcing vectors
+    drawSource( *context.painter() );
+    return;
+  }
+
   switch ( mBlurMethod )
   {
     case StackBlur:
