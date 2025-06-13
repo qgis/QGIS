5ab5d5f2e068acd3fae5391868da57a26953de63
jonathanlurie <lurie.jo@gmail.com>
2025-06-13 15:16:20 +0200

    moved param colors to types

--- python/PyQt6/core/auto_additions/qgsprocessingparametertype.py
+++ python/PyQt6/core/auto_additions/qgsprocessingparametertype.py
@@ -1,6 +1,7 @@
 # The following has been generated automatically from src/core/processing/qgsprocessingparametertype.h
 try:
-    QgsProcessingParameterType.__virtual_methods__ = ['pythonImportString', 'className', 'flags', 'metadata', 'acceptedPythonTypes', 'acceptedStringValues', 'acceptedDataTypes']
+    QgsProcessingParameterType.defaultModelColor = staticmethod(QgsProcessingParameterType.defaultModelColor)
+    QgsProcessingParameterType.__virtual_methods__ = ['pythonImportString', 'className', 'flags', 'metadata', 'acceptedPythonTypes', 'acceptedStringValues', 'acceptedDataTypes', 'modelColor']
     QgsProcessingParameterType.__abstract_methods__ = ['create', 'description', 'name', 'id', 'acceptedParameterTypes', 'acceptedOutputTypes']
     QgsProcessingParameterType.__group__ = ['processing']
 except (NameError, AttributeError):
