de1832ac1c141cc6f420ed26e0765a2745253916
uclaros <uclaros@gmail.com>
2025-06-19 12:50:47 +0300

    Add test, also check resulting pointer

--- src/core/geometry/qgsgeos.cpp
+++ src/core/geometry/qgsgeos.cpp
@@ -3761,6 +3761,11 @@ geos::unique_ptr QgsGeos::reshapePolygon( const GEOSGeometry *polygon, const GEO
   }
   catch ( GEOSException & )
   {
+    // nothing to do: on exception newRing will be null
+  }
+
+  if ( !newRing )
+  {
     delete [] innerRings;
     return nullptr;
   }
