9b07a60ef79621d9a3b66a5d41a5d70d4e914f04
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2025-07-24 11:23:37 +0200

    add sfcgal sip files

---
+++ python/PyQt6/core/auto_additions/qgssfcgalgeometry.py
@@ -0,0 +1,5 @@
+# The following has been generated automatically from src/core/geometry/qgssfcgalgeometry.h
+try:
+    QgsSfcgalGeometry.__group__ = ['geometry']
+except (NameError, AttributeError):
+    pass
