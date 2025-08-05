9c357a52beb6c99102e03488ba9805600e6eda9c
Jean Felder <jean.felder@oslandia.com>
2025-08-05 19:02:38 +0200

    qgssfcgalgeometry: Replace errorMsg by a lastError workflow

--- tests/src/python/test_qgssfcgal.py
+++ tests/src/python/test_qgssfcgal.py
@@ -30,8 +30,8 @@ class TestQgsSFCGAL(QgisTestCase):
 
     def test_constructor(self):
         default_geom = QgsSfcgalGeometry(QgsPolyhedralSurface())
-        is_empty, err_msg = default_geom.isEmpty()
-        self.assertFalse(err_msg)
+        is_empty = default_geom.isEmpty()
+        self.assertFalse(default_geom.lastError())
         self.assertTrue(is_empty)
 
         surface = QgsPolyhedralSurface()
@@ -41,66 +41,66 @@ class TestQgsSFCGAL(QgisTestCase):
         self.assertEqual(surface.numPatches(), 1)
         self.assertTrue(surface.is3D())
         geom_surface = QgsSfcgalGeometry(surface)
-        is_empty, err_msg = geom_surface.isEmpty()
-        self.assertFalse(err_msg)
+        is_empty = geom_surface.isEmpty()
+        self.assertFalse(geom_surface.lastError())
         self.assertFalse(is_empty)
-        wkt_out, err_msg = geom_surface.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = geom_surface.asWkt(0)
+        self.assertFalse(geom_surface.lastError())
         self.assertEqual(wkt_out, surface_wkt)
 
         point = QgsSfcgalGeometry(QgsPoint(2, 3))
-        is_empty, err_msg = point.isEmpty()
-        self.assertFalse(err_msg)
+        is_empty = point.isEmpty()
+        self.assertFalse(point.lastError())
         self.assertFalse(is_empty)
-        is_valid, err_msg = point.isValid()
-        self.assertFalse(err_msg)
+        is_valid = point.isValid()
+        self.assertFalse(point.lastError())
         self.assertTrue(is_valid)
-        wkt_out, err_msg = point.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = point.asWkt(0)
+        self.assertFalse(point.lastError())
         self.assertEqual(wkt_out, "POINT (2 3)")
 
     def test_from_wkt(self):
         tin_wkt = "TIN M (((0 0 3,0 1 3,1 1 3,0 0 3)))"
-        geom, err_msg = QgsSfcgalGeometry.fromWkt(tin_wkt)
-        self.assertFalse(err_msg)
+        geom = QgsSfcgalGeometry.fromWkt(tin_wkt)
+        self.assertFalse(geom.lastError())
         self.assertTrue(isinstance(geom, QgsSfcgalGeometry))
-        is_empty, err_msg = geom.isEmpty()
-        self.assertFalse(err_msg)
+        is_empty = geom.isEmpty()
+        self.assertFalse(geom.lastError())
         self.assertFalse(is_empty)
 
-        wkt_out, err_msg = geom.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = geom.asWkt(0)
+        self.assertFalse(geom.lastError())
         self.assertEqual(wkt_out, tin_wkt)
 
     def test_from_wkb(self):
         wkb_in = b"\x01\xf8\x03\x00\x00\x02\x00\x00\x00\x01\xf9\x03\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\xf9\x03\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-        geom, err_msg = QgsSfcgalGeometry.fromWkb(QgsConstWkbPtr(wkb_in))
-        self.assertFalse(err_msg)
+        geom = QgsSfcgalGeometry.fromWkb(QgsConstWkbPtr(wkb_in))
+        self.assertFalse(geom.lastError())
         self.assertTrue(isinstance(geom, QgsSfcgalGeometry))
-        is_empty, err_msg = geom.isEmpty()
-        self.assertFalse(err_msg)
+        is_empty = geom.isEmpty()
+        self.assertFalse(geom.lastError())
         self.assertFalse(is_empty)
 
         expected_wkt = "TIN Z (((0 0 0,1 0 0,0 1 0,0 0 0)),((1 0 0,1 1 0,0 1 0,1 0 0)))"
-        wkt_out, err_msg = geom.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = geom.asWkt(0)
+        self.assertFalse(geom.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
-        wkb_out, err_msg = geom.asWkb()
-        self.assertFalse(err_msg)
+        wkb_out = geom.asWkb()
+        self.assertFalse(geom.lastError())
         self.assertEqual(wkb_out, wkb_in)
 
     def test_is_valid(self):
         surface_wkt = "POLYHEDRALSURFACE Z (((0 0 0,0 100 0,100 100 0,100 0 0,0 0 0)),((0 0 0,50 50 50,0 100 0,0 0 0)),((100 0 0,100 100 0,50 50 50,100 0 0)))"
-        sfcgal_geom, err_msg = QgsSfcgalGeometry.fromWkt(surface_wkt)
-        self.assertFalse(err_msg)
+        sfcgal_geom = QgsSfcgalGeometry.fromWkt(surface_wkt)
+        self.assertFalse(sfcgal_geom.lastError())
 
-        is_empty, err_msg = sfcgal_geom.isEmpty()
-        self.assertFalse(err_msg)
+        is_empty = sfcgal_geom.isEmpty()
+        self.assertFalse(sfcgal_geom.lastError())
         self.assertFalse(is_empty)
 
-        is_valid, err_msg = sfcgal_geom.isValid()
-        self.assertFalse(err_msg)
+        is_valid = sfcgal_geom.isValid()
+        self.assertFalse(sfcgal_geom.lastError())
         self.assertTrue(is_valid)
 
     def test_part_count(self):
@@ -126,14 +126,14 @@ class TestQgsSFCGAL(QgisTestCase):
             ],
         ]
         for geom in geoms:
-            sfcgal_geom, err_msg = QgsSfcgalGeometry.fromWkt(geom[0])
-            self.assertFalse(err_msg)
-            wkt_out, err_msg = sfcgal_geom.asWkt(0)
-            self.assertFalse(err_msg)
+            sfcgal_geom = QgsSfcgalGeometry.fromWkt(geom[0])
+            self.assertFalse(sfcgal_geom.lastError())
+            wkt_out = sfcgal_geom.asWkt(0)
+            self.assertFalse(sfcgal_geom.lastError())
             self.assertEqual(wkt_out, geom[0])
 
-            part_count, err_msg = sfcgal_geom.partCount()
-            self.assertFalse(err_msg)
+            part_count = sfcgal_geom.partCount()
+            self.assertFalse(sfcgal_geom.lastError())
             self.assertEqual(part_count, geom[1])
 
     def test_area(self):
@@ -151,133 +151,125 @@ class TestQgsSFCGAL(QgisTestCase):
             ],
         ]
         for geom in geoms:
-            sfcgal_geom, err_msg = QgsSfcgalGeometry.fromWkt(geom[0])
+            sfcgal_geom = QgsSfcgalGeometry.fromWkt(geom[0])
 
-            self.assertFalse(err_msg)
-            wkt_out, err_msg = sfcgal_geom.asWkt(0)
-            self.assertFalse(err_msg)
+            self.assertFalse(sfcgal_geom.lastError())
+            wkt_out = sfcgal_geom.asWkt(0)
+            self.assertFalse(sfcgal_geom.lastError())
             self.assertEqual(wkt_out, geom[0])
 
-            area, err_msg = sfcgal_geom.area()
-            self.assertFalse(err_msg)
+            area = sfcgal_geom.area()
+            self.assertFalse(sfcgal_geom.lastError())
             self.assertAlmostEqual(area, geom[1], 5)
 
     def test_scale(self):
         wkt = "LINESTRING Z (0 0 0,1 0 0,0 1 0,0 0 1,0 0 0)"
-        sfcgal_geom, err_msg = QgsSfcgalGeometry.fromWkt(wkt)
-        self.assertFalse(err_msg)
+        sfcgal_geom = QgsSfcgalGeometry.fromWkt(wkt)
+        self.assertFalse(sfcgal_geom.lastError())
 
-        scaled_geom, err_msg = sfcgal_geom.scale(QgsVector3D(1.5, 2, 0.5))
-        self.assertFalse(err_msg)
+        scaled_geom = sfcgal_geom.scale(QgsVector3D(1.5, 2, 0.5))
+        self.assertFalse(scaled_geom.lastError())
         expected_wkt = (
             "LINESTRING Z (0.0 0.0 0.0,1.5 0.0 0.0,0.0 2.0 0.0,0.0 0.0 0.5,0.0 0.0 0.0)"
         )
-        wkt_out, err_msg = scaled_geom.asWkt(1)
-        self.assertFalse(err_msg)
+        wkt_out = scaled_geom.asWkt(1)
+        self.assertFalse(scaled_geom.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
     def test_rotate(self):
         # 2D rotation
         wkt = "TRIANGLE ((1 1,2 2,2 1,1 1))"
-        sfcgal_geom, err_msg = QgsSfcgalGeometry.fromWkt(wkt)
-        self.assertFalse(err_msg)
+        sfcgal_geom = QgsSfcgalGeometry.fromWkt(wkt)
+        self.assertFalse(sfcgal_geom.lastError())
 
-        rotated_2d_geom, err_msg = sfcgal_geom.rotate2D(pi / 2.0, QgsPoint(1, 1))
-        self.assertFalse(err_msg)
+        rotated_2d_geom = sfcgal_geom.rotate2D(pi / 2.0, QgsPoint(1, 1))
+        self.assertFalse(rotated_2d_geom.lastError())
         expected_wkt = "TRIANGLE ((1 1,0 2,1 2,1 1))"
-        wkt_out, err_msg = rotated_2d_geom.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = rotated_2d_geom.asWkt(0)
+        self.assertFalse(rotated_2d_geom.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
         # 3D rotation
-        rotated_3d_geom, err_msg = sfcgal_geom.rotate3D(
+        rotated_3d_geom = sfcgal_geom.rotate3D(
             pi / 2.0, QgsVector3D(2, 1, 0), QgsPoint(1, 1, 0)
         )
-        self.assertFalse(err_msg)
+        self.assertFalse(rotated_3d_geom.lastError())
         expected_wkt = "TRIANGLE Z ((1 1 0,2 2 0,2 1 0,1 1 0))"
-        wkt_out, err_msg = rotated_3d_geom.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = rotated_3d_geom.asWkt(0)
+        self.assertFalse(rotated_3d_geom.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
     def test_intersection(self):
         point = QgsSfcgalGeometry(QgsPoint(2, 3))
 
-        polygon1, err_msg = QgsSfcgalGeometry.fromWkt(
-            "POLYGON ((0 0,10 0,10 10,0 10,0 0))"
-        )
-        self.assertFalse(err_msg)
+        polygon1 = QgsSfcgalGeometry.fromWkt("POLYGON ((0 0,10 0,10 10,0 10,0 0))")
+        self.assertFalse(polygon1.lastError())
 
-        intersects, err_msg = polygon1.intersects(point)
-        self.assertFalse(err_msg)
+        intersects = polygon1.intersects(point)
+        self.assertFalse(polygon1.lastError())
         self.assertTrue(intersects)
 
-        polygon2, err_msg = QgsSfcgalGeometry.fromWkt(
-            "POLYGON ((-1 -1,1 -1,1 1,-1 1,-1 -1))"
-        )
-        self.assertFalse(err_msg)
-        intersects, err_msg = polygon2.intersects(point)
-        self.assertFalse(err_msg)
+        polygon2 = QgsSfcgalGeometry.fromWkt("POLYGON ((-1 -1,1 -1,1 1,-1 1,-1 -1))")
+        self.assertFalse(polygon2.lastError())
+        intersects = polygon2.intersects(point)
+        self.assertFalse(polygon2.lastError())
         self.assertFalse(intersects)
 
-        intersects, err_msg = polygon2.intersects(polygon1)
-        self.assertFalse(err_msg)
+        intersects = polygon2.intersects(polygon1)
+        self.assertFalse(polygon2.lastError())
         self.assertTrue(intersects)
 
-        intersection, err_msg = polygon2.intersection(polygon1)
-        self.assertFalse(err_msg)
-        wkt_out, err_msg = intersection.asWkt(0)
-        self.assertFalse(err_msg)
+        intersection = polygon2.intersection(polygon1)
+        self.assertFalse(intersection.lastError())
+        wkt_out = intersection.asWkt(0)
+        self.assertFalse(intersection.lastError())
         expected_intersection = "POLYGON ((0 1,0 0,1 0,1 1,0 1))"
         self.assertEqual(wkt_out, expected_intersection)
 
     def test_combine(self):
         point = QgsSfcgalGeometry(QgsPoint(2, 3))
 
-        combined_geom, err_msg = point.combine([QgsPoint(4, 5)])
-        self.assertFalse(err_msg)
-        wkt_out, err_msg = combined_geom.asWkt(0)
+        combined_geom = point.combine([QgsPoint(4, 5)])
+        self.assertFalse(combined_geom.lastError())
+        wkt_out = combined_geom.asWkt(0)
         expected_wkt = "MULTIPOINT ((2 3),(4 5))"
-        self.assertFalse(err_msg)
+        self.assertFalse(combined_geom.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
     def test_difference(self):
-        polygon1, err_msg = QgsSfcgalGeometry.fromWkt(
-            "POLYGON ((0 0,10 0,10 10,0 10,0 0))"
-        )
-        self.assertFalse(err_msg)
-        polygon2, err_msg = QgsSfcgalGeometry.fromWkt(
-            "POLYGON ((-1 -1,1 -1,1 1,-1 1,-1 -1))"
-        )
-        self.assertFalse(err_msg)
+        polygon1 = QgsSfcgalGeometry.fromWkt("POLYGON ((0 0,10 0,10 10,0 10,0 0))")
+        self.assertFalse(polygon1.lastError())
+        polygon2 = QgsSfcgalGeometry.fromWkt("POLYGON ((-1 -1,1 -1,1 1,-1 1,-1 -1))")
+        self.assertFalse(polygon2.lastError())
 
-        diff_geom, err_msg = polygon1.difference(polygon2)
-        self.assertFalse(err_msg)
-        wkt_out, err_msg = diff_geom.asWkt(0)
+        diff_geom = polygon1.difference(polygon2)
+        self.assertFalse(diff_geom.lastError())
+        wkt_out = diff_geom.asWkt(0)
         expected_wkt = "POLYGON ((0 10,0 1,1 1,1 0,10 0,10 10,0 10))"
-        self.assertFalse(err_msg)
+        self.assertFalse(diff_geom.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
     def test_triangulate(self):
         poly_wkt = "POLYGON ((0.0 0.0,1.0 0.0,1.0 1.0,0.0 1.0,0.0 0.0),(0.2 0.2,0.2 0.8,0.8 0.8,0.8 0.2,0.2 0.2))"
-        polygon, err_msg = QgsSfcgalGeometry.fromWkt(poly_wkt)
-        self.assertFalse(err_msg)
+        polygon = QgsSfcgalGeometry.fromWkt(poly_wkt)
+        self.assertFalse(polygon.lastError())
 
-        triangulation, err_msg = polygon.triangulate()
-        self.assertFalse(err_msg)
-        self.assertEqual(triangulation.wkbType()[0], Qgis.WkbType.TIN)
-        self.assertEqual(triangulation.partCount()[0], 10)
+        triangulation = polygon.triangulate()
+        self.assertFalse(triangulation.lastError())
+        self.assertEqual(triangulation.wkbType(), Qgis.WkbType.TIN)
+        self.assertEqual(triangulation.partCount(), 10)
 
     def test_convexhull(self):
         poly_wkt = "POLYGON ((0.0 0.0,1.0 0.0,1.0 1.0,0.0 1.0,0.0 0.0),(0.2 0.2,0.2 0.8,0.8 0.8,0.8 0.2,0.2 0.2))"
-        polygon, err_msg = QgsSfcgalGeometry.fromWkt(poly_wkt)
-        self.assertFalse(err_msg)
+        polygon = QgsSfcgalGeometry.fromWkt(poly_wkt)
+        self.assertFalse(polygon.lastError())
 
-        convex_hull, err_msg = polygon.convexHull()
-        self.assertFalse(err_msg)
+        convex_hull = polygon.convexHull()
+        self.assertFalse(convex_hull.lastError())
 
         expected_wkt = "POLYGON ((0 0,1 0,1 1,0 1,0 0))"
-        wkt_out, err_msg = diff_geom.asWkt(0)
-        self.assertFalse(err_msg)
+        wkt_out = convex_hull.asWkt(0)
+        self.assertFalse(convex_hull.lastError())
         self.assertEqual(wkt_out, expected_wkt)
 
 
