7d5608992cba6c6fde3a1ad791ef5dd2fb44312f
Jean Felder <jean.felder@oslandia.com>
2025-08-05 16:06:48 +0200

    qgssfcgalgeometry: Fix SFCGAL typo

--- src/core/geometry/qgssfcgalgeometry.h
+++ src/core/geometry/qgssfcgalgeometry.h
@@ -56,35 +56,35 @@ class CORE_EXPORT QgsSfcgalGeometry
      *
      * Will not take ownership.
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      */
     QgsSfcgalGeometry( const QgsAbstractGeometry *qgsGeom, QString *errorMsg = nullptr );
 
     /**
      * Constructor with QgsAbstractGeometry reference.
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      */
     QgsSfcgalGeometry( const QgsAbstractGeometry &qgsGeom, QString *errorMsg = nullptr );
 
     /**
     * Constructor with QgsGeometry reference.
     *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      */
     QgsSfcgalGeometry( const QgsGeometry &qgsGeom, QString *errorMsg = nullptr );
 
     /**
      * Constructor from WKT
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
     */
     QgsSfcgalGeometry( const QString &wkt, QString *errorMsg = nullptr );
 
     /**
      * Copy constructor
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
     */
     QgsSfcgalGeometry( const QgsSfcgalGeometry &otherGeom, QString *errorMsg = nullptr );
 
@@ -102,14 +102,14 @@ class CORE_EXPORT QgsSfcgalGeometry
 
     /**
      * Returns type of the geometry as a WKB type (point / linestring / polygon etc.)
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return type of the geometry as a WKB type (point / linestring / polygon etc.)
      */
     Qgis::WkbType wkbType( QString *errorMsg SIP_OUT = nullptr ) const;
 
     /**
      * Returns type of the geometry as a OGC string in CamelCase
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return type of the geometry as a OGC string in CamelCase
      */
     QString geometryType( QString *errorMsg SIP_OUT = nullptr ) const SIP_HOLDGIL;
@@ -122,7 +122,7 @@ class CORE_EXPORT QgsSfcgalGeometry
     /**
      * Creates a new geometry from a WKB byte pointer
      * \param wkbPtr WKB byte pointer
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry from WKB
      */
     static std::unique_ptr<QgsSfcgalGeometry> fromWkb( const QgsConstWkbPtr &wkbPtr, QString *errorMsg SIP_OUT = nullptr );
@@ -131,7 +131,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Export the geometry as WKB
      *
      * \param flags argument specifies flags controlling WKB export behavior.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return WKB data
      */
     QByteArray asWkb( QgsAbstractGeometry::WkbFlags flags = QgsAbstractGeometry::WkbFlags(), QString *errorMsg SIP_OUT = nullptr ) const;
@@ -139,7 +139,7 @@ class CORE_EXPORT QgsSfcgalGeometry
     /**
      * Creates a new geometry from a WKT string.
      * \param wkt WTK string
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry from WKT
      */
     static std::unique_ptr<QgsSfcgalGeometry> fromWkt( const QString &wkt, QString *errorMsg SIP_OUT = nullptr );
@@ -148,7 +148,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Export the geometry as WKT
      *
      * \param precision Floating point precision for WKT coordinates. Setting to -1 yields rational number WKT (not decimal) f.e. "Point(1/3, 1/6, 1/4)". Note that this will produce WKT which is not compatible with other QGIS methods or external libraries.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return WKT data
      */
     QString asWkt( int precision = -1, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -156,7 +156,7 @@ class CORE_EXPORT QgsSfcgalGeometry
     /**
      * Returns the closure of the combinatorial boundary of the geometry (ie the topological boundary of the geometry).
      * For instance, a polygon geometry will have a boundary consisting of the linestrings for each ring in the polygon.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return boundary for geometry. May be NULLPTR for some geometry types.
      */
     std::unique_ptr<QgsSfcgalGeometry> boundary( QString *errorMsg SIP_OUT = nullptr ) const;
@@ -177,7 +177,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Returns true if this == other geometry modulo \a epsilon distance
      * \param other geometry to perform the operation
      * \param epsilon tolerance
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if this == other geometry modulo \a epsilon distance
      */
     bool fuzzyEqual( const QgsSfcgalGeometry &other, double epsilon, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -185,7 +185,7 @@ class CORE_EXPORT QgsSfcgalGeometry
     /**
      * Returns the inherent dimension of the geometry. For example, this is 0 for a point geometry,
      * 1 for a linestring and 2 for a polygon.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return geometry dimension
      */
     int dimension( QString *errorMsg SIP_OUT = nullptr ) const;
@@ -243,28 +243,28 @@ class CORE_EXPORT QgsSfcgalGeometry
      *
      * If the geometry is invalid, \a errorMsg will be filled with the reported geometry error.
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if valid
      */
     bool isValid( QString *errorMsg SIP_OUT = nullptr ) const;
 
     /**
      * Checks if \a geom is empty.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if empty
      */
     bool isEmpty( QString *errorMsg SIP_OUT = nullptr ) const;
 
     /**
      * Computes the area of \a geom.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return geometry area
      */
     double area( QString *errorMsg SIP_OUT = nullptr ) const;
 
     /**
      * Computes the max length of \a geom.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return geometry length
      */
     double length( QString *errorMsg SIP_OUT = nullptr ) const;
@@ -274,7 +274,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      *
      * \see QgsSfcgalEngine::isSimple
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if simple
      */
     bool isSimple( QString *errorMsg SIP_OUT = nullptr ) const;
@@ -282,7 +282,7 @@ class CORE_EXPORT QgsSfcgalGeometry
     /**
      * Calculates the centroid of this geometry.
      *
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return geometry centroid
      */
     QgsPoint centroid( QString *errorMsg ) const;
@@ -291,7 +291,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Translate this geometry by vector \a translation.
      *
      * \param translation translation vector (2D or 3D)
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> translate( const QgsVector3D &translation, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -301,7 +301,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      *
      * \param scaleFactor scale factor vector (2D or 3D)
      * \param center optional parameter. If specified, scaling will be performed relative to this center
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> scale( const QgsVector3D &scaleFactor, const QgsPoint &center = QgsPoint(), QString *errorMsg SIP_OUT = nullptr ) const;
@@ -311,7 +311,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      *
      * \param angle rotation angle in radians
      * \param center rotation center
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> rotate2D( double angle, const QgsPoint &center, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -322,7 +322,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * \param angle rotation angle in radians
      * \param axisVector rotation axis
      * \param center optional parameter. If specified, rotation will be applied around axis and center point
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> rotate3D( double angle, const QgsVector3D &axisVector, const QgsPoint &center = QgsPoint(), QString *errorMsg SIP_OUT = nullptr ) const;
@@ -331,7 +331,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Checks if \a otherGeom intersects this.
      *
      * \param otherGeom geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if intersection exists
      */
     bool intersects( const QgsAbstractGeometry *otherGeom, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -340,7 +340,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Checks if \a otherGeom intersects this.
      *
      * \param otherGeom geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if intersection exists
      */
     bool intersects( const QgsSfcgalGeometry &otherGeom, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -349,7 +349,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Calculate the intersection of this and \a otherGeom.
      *
      * \param otherGeom geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \param parameters can be used to specify parameters which control the intersection results
      * \return new geometry
      */
@@ -359,7 +359,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Calculate the intersection of this and \a otherGeom.
      *
      * \param otherGeom geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \param parameters can be used to specify parameters which control the intersection results
      * \return new geometry
      */
@@ -369,7 +369,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Calculate the combination of this and \a geomList.
      *
      * \param geomList list of geometries to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> combine( const QVector<QgsAbstractGeometry *> &geomList, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -378,7 +378,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Calculate the difference of this and \a otherGeom.
      *
      * \param otherGeom geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \param parameters can be used to specify parameters which control the difference results
      * \return new geometry
      */
@@ -388,7 +388,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * Calculate the difference of this and \a otherGeom.
      *
      * \param otherGeom geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \param parameters can be used to specify parameters which control the difference results
      * \return new geometry
      */
@@ -396,21 +396,21 @@ class CORE_EXPORT QgsSfcgalGeometry
 
     /**
      * Triangulates this geometry using constraint 2D Delaunay Triangulation (keep Z if defined)
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> triangulate( QString *errorMsg SIP_OUT = nullptr ) const;
 
     /**
      * Calculate the convex hull (bounding box).
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> convexHull( QString *errorMsg SIP_OUT = nullptr ) const;
 
     /**
      * Calculate the envelope (bounding box).
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> envelope( QString *errorMsg SIP_OUT = nullptr ) const;
@@ -421,7 +421,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * A 3D covers test is conducted when at least one geometry is 3D; otherwise, a 2D covers test is carried out.
      *
      * \param otherGeom second geometry to perform the operation
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return true if coverage exists
      */
     bool covers( const QgsSfcgalGeometry &otherGeom, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -435,7 +435,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * \param radius the buffer radius
      * \param segments the number of segments to use for approximating curved
      * \param joinStyle3D the type of buffer to compute
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> buffer3D( double radius, int segments, Qgis::JoinStyle3D joinStyle3D, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -448,7 +448,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * \param radius the buffer radius
      * \param segments the number of segments to use for approximating curved
      * \param joinStyle the type of buffer to compute. Only round is supported.
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> buffer2D( double radius, int segments, Qgis::JoinStyle joinStyle, QString *errorMsg ) const;
@@ -458,7 +458,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      *
      * \param tolerance The distance (in geometry unit) threshold
      * \param preserveTopology Whether to preserve topology during simplification
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> simplify( double tolerance, bool preserveTopology, QString *errorMsg SIP_OUT = nullptr ) const;
@@ -468,7 +468,7 @@ class CORE_EXPORT QgsSfcgalGeometry
      * If the operation fails, a null pointer is returned.
      *
      * \param extrusion extrusion vector (2D or 3D)
-     * \param errorMsg Error message returned by SFGCAL
+     * \param errorMsg Error message returned by SFCGAL
      * \return new geometry
      */
     std::unique_ptr<QgsSfcgalGeometry> extrude( const QgsVector3D &extrusion, QString *errorMsg SIP_OUT = nullptr ) const;
