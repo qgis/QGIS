9df18539846089bd8dfdafbf5e1049b249a35de8
MoritzMoreira <hackenberg@dev-gis.com>
2025-08-05 10:34:05 +0200

    test - generic part generic methods and members as a base for testing widget types with comboboxes

---
+++ tests/src/gui/testqgswidgetsconfigdlg.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+    testqgswidgetsconfigdlg.cpp
+     --------------------------------------
+    Date                 : 31 07 2025
+    Copyright            : (C) 2025 Moritz Hackenberg
+    Email                : hackenberg at dev-gis dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgstest.h"
+
+#include "qgsgui.h"
+#include "qgseditorwidgetregistry.h"
+#include "qgsapplication.h"
+#include "qgsproject.h"
+#include "qgsvectorlayer.h"
+#include "qgsattributeform.h"
+<<<<<<< Updated upstream
+=======
+#include "editorwidgets/qgsuniquevaluesconfigdlg.h"
+#include "qgsfieldformatter.h"
+  >>>>>>> Stashed changes
+
+#include <QComboBox>
+
+  class TestQgsWidgetsConfigDlg : public QObject
+{
+    Q_OBJECT
+  public:
+    TestQgsWidgetsConfigDlg() = default;
+
+  private slots:
+    void initTestCase();    // will be called before the first testfunction is executed.
+    void cleanupTestCase(); // will be called after the last testfunction was executed.
+    void init();            // will be called before each testfunction is executed.
+    void cleanup();         // will be called after every testfunction.
+
+  private:
+    void testAllowNull( const QString &widgetType );
+
+    QgsVectorLayer *mLayer;
+    QgsAttributeForm *mForm;
+    QVariantMap *mConfig;
+    QComboBox *mComboBox;
+};
+
+void TestQgsWidgetsConfigDlg::initTestCase()
+{
+  QgsApplication::init();
+  QgsApplication::initQgis();
+  QgsGui::editorWidgetRegistry()->initEditors();
+
+  // make a layer to check through
+  const QString def = QStringLiteral( "Point?field=col0:integer&field=col1:integer" );
+  mLayer = new QgsVectorLayer( def, QStringLiteral( "test" ), QStringLiteral( "memory" ) );
+
+  // set default value
+  mLayer->setDefaultValueDefinition( 1, QgsDefaultValue( QStringLiteral( "\"col0\"+1" ), true ) );
+
+  // build a form, set a feature
+  QgsFeature ft( mLayer->fields(), 1 );
+  mForm = new QgsAttributeForm( mLayer );
+  mForm->setMode( QgsAttributeEditorContext::AddFeatureMode );
+  mForm->setFeature( ft );
+}
+
+void TestQgsWidgetsConfigDlg::cleanupTestCase()
+{
+  delete mLayer;
+  QgsApplication::exitQgis();
+}
+
+void TestQgsWidgetsConfigDlg::init()
+{
+}
+
+void TestQgsWidgetsConfigDlg::cleanup()
+{
+}
+
+void TestQgsWidgetsConfigDlg::testAllowNull( const QString &widgetType )
+{
+  // set allow null in configuration and apply it to the layer
+  mConfig->insert( QStringLiteral( "AllowNull" ), true );
+  mLayer->setEditorWidgetSetup( 1, QgsEditorWidgetSetup( widgetType, *mConfig ) );
+
+  //set value in col0:
+  QgsEditorWidgetWrapper *ww0;
+  ww0 = qobject_cast<QgsEditorWidgetWrapper *>( mForm->mWidgets[0] );
+  ww0->setValue( 1 );
+  mComboBox = mForm->findChild<QComboBox *>();
+
+  mComboBox->setCurrentIndex( 0 );
+  QCOMPARE( mComboBox->currentText(), "(NULL)" );
+  mComboBox->setCurrentIndex( 1 );
+  QCOMPARE( mComboBox->currentText(), "(2)" );
+
+  mConfig->insert( QStringLiteral( "AllowNull" ), false );
+  QCOMPARE( mComboBox->currentText(), "(2)" );
+}
+
+QGSTEST_MAIN( TestQgsWidgetsConfigDlg )
+#include "testqgswidgetsconfigdlg.moc"
