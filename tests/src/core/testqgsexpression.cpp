eb764c4320109d88c664612edb0efa1c56ed07cd
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2025-08-10 14:42:18 +0300

    add tests source

--- tests/src/core/testqgsexpression.cpp
+++ tests/src/core/testqgsexpression.cpp
@@ -1843,6 +1843,8 @@ class TestQgsExpression : public QObject
       QTest::newRow( "regexp_replace invalid" ) << "regexp_replace('HeLLo','[[[', '-')" << true << QVariant();
       QTest::newRow( "reverse string" ) << "reverse('HeLLo')" << false << QVariant( "oLLeH" );
       QTest::newRow( "reverse empty string" ) << "reverse('')" << false << QVariant( "" );
+      // unaccent() tests aligned with PostgreSQL's contrib/unaccent/sql/unaccent.sql and some more tests
+      // Source: https://raw.githubusercontent.com/postgres/postgres/refs/heads/master/contrib/unaccent/sql/unaccent.sql
       QTest::newRow( "unaccent basic french" ) << "unaccent('Hôtel crème brûlée')" << false << QVariant( "Hotel creme brulee" );
       QTest::newRow( "unaccent basic romanian" ) << "unaccent('Românește')" << false << QVariant( "Romaneste" );
       QTest::newRow( "unaccent ligatures and Polish" ) << "unaccent('Æsir & Œuvre, Łódź')" << false << QVariant( "AEsir & OEuvre, Lodz" );
