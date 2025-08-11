923dd68fbddcaf4c452126a3a638bbebb70287ba
Jean Felder <jean.felder@oslandia.com>
2025-08-11 15:23:43 +0200

    qgsframegraph: Add a debug texture renderview getter

--- src/3d/framegraph/qgsframegraph.cpp
+++ src/3d/framegraph/qgsframegraph.cpp
@@ -539,3 +539,9 @@ QgsAmbientOcclusionRenderView &QgsFrameGraph::ambientOcclusionRenderView()
   QgsAbstractRenderView *rv = mRenderViewMap[QgsFrameGraph::AMBIENT_OCCLUSION_RENDERVIEW].get();
   return *( dynamic_cast<QgsAmbientOcclusionRenderView *>( rv ) );
 }
+
+QgsDebugTextureRenderView &QgsFrameGraph::debugTextureRenderView()
+{
+  QgsAbstractRenderView *renderView = mRenderViewMap[QgsFrameGraph::DEBUG_RENDERVIEW].get();
+  return *( dynamic_cast<QgsDebugTextureRenderView *>( renderView ) );
+}
