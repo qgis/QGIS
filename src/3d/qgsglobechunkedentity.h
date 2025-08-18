3c3656ad84d1b5f3ebce7759ee51f43aa84d5f82
Matthias Kuhn <matthias@opengis.ch>
2025-08-18 12:01:02 +0200

    Init variable

--- src/3d/qgsglobechunkedentity.h
+++ src/3d/qgsglobechunkedentity.h
@@ -55,7 +55,7 @@ class QgsGlobeChunkLoader : public QgsChunkLoader
   private:
     QgsTerrainTextureGenerator *mTextureGenerator;
     QgsCoordinateTransform mGlobeCrsToLatLon;
-    int mJobId;
+    int mJobId = -1;
     QImage mTexture;
 };
 
