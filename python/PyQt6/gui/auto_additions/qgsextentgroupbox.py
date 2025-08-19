26ec74c651b73d5188ee9d0d0693a668f7b6263f
lanckmann <nassim.boughedaoui@polymtl.ca>
2025-08-19 05:45:41 -0400

    Update Python bindings

--- python/PyQt6/gui/auto_additions/qgsextentgroupbox.py
+++ python/PyQt6/gui/auto_additions/qgsextentgroupbox.py
@@ -5,7 +5,7 @@ QgsExtentGroupBox.UserExtent = QgsExtentGroupBox.ExtentState.UserExtent
 QgsExtentGroupBox.ProjectLayerExtent = QgsExtentGroupBox.ExtentState.ProjectLayerExtent
 QgsExtentGroupBox.DrawOnCanvas = QgsExtentGroupBox.ExtentState.DrawOnCanvas
 try:
-    QgsExtentGroupBox.__attribute_docs__ = {'extentChanged': "Emitted when the widget's extent is changed.\n", 'extentLayerChanged': 'Emitted when the extent layer is changed.\n\n.. versionadded:: 3.44\n'}
-    QgsExtentGroupBox.__signal_arguments__ = {'extentChanged': ['r: QgsRectangle'], 'extentLayerChanged': ['layer: QgsMapLayer']}
+    QgsExtentGroupBox.__attribute_docs__ = {'extentChanged': "Emitted when the widget's extent is changed.\n", 'extentLayerChanged': 'Emitted when the extent layer is changed.\n\n.. versionadded:: 3.44\n', 'snapToGridChanged': 'Emitted when the snap-to-grid state is changed.\n\n:param enabled: whether snap-to-grid is enabled\n\n.. versionadded:: 3.46\n'}
+    QgsExtentGroupBox.__signal_arguments__ = {'extentChanged': ['r: QgsRectangle'], 'extentLayerChanged': ['layer: QgsMapLayer'], 'snapToGridChanged': ['enabled: bool']}
 except (NameError, AttributeError):
     pass
--- python/gui/auto_additions/qgsextentgroupbox.py
+++ python/gui/auto_additions/qgsextentgroupbox.py
@@ -1,6 +1,6 @@
 # The following has been generated automatically from src/gui/qgsextentgroupbox.h
 try:
-    QgsExtentGroupBox.__attribute_docs__ = {'extentChanged': "Emitted when the widget's extent is changed.\n", 'extentLayerChanged': 'Emitted when the extent layer is changed.\n\n.. versionadded:: 3.44\n'}
-    QgsExtentGroupBox.__signal_arguments__ = {'extentChanged': ['r: QgsRectangle'], 'extentLayerChanged': ['layer: QgsMapLayer']}
+    QgsExtentGroupBox.__attribute_docs__ = {'extentChanged': "Emitted when the widget's extent is changed.\n", 'extentLayerChanged': 'Emitted when the extent layer is changed.\n\n.. versionadded:: 3.44\n', 'snapToGridChanged': 'Emitted when the snap-to-grid state is changed.\n\n:param enabled: whether snap-to-grid is enabled\n\n.. versionadded:: 3.46\n'}
+    QgsExtentGroupBox.__signal_arguments__ = {'extentChanged': ['r: QgsRectangle'], 'extentLayerChanged': ['layer: QgsMapLayer'], 'snapToGridChanged': ['enabled: bool']}
 except (NameError, AttributeError):
     pass
