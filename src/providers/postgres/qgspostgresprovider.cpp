9d369ed2826c151ae431bc7edd4d355fe377c395
Jan Caha <jan.caha@outlook.com>
2025-08-25 19:15:09 +0200

    translate strings

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -5244,7 +5244,7 @@ QString QgsPostgresProvider::htmlMetadata() const
                                     .arg( QgsPostgresConn::quotedValue( mTableName ) );
 
   QgsPostgresResult resSpatialIndexes( connectionRO()->LoggedPQexec( "QgsPostgresProvider", sqlSpatialIndex ) );
-  QString spatialIndexText = QStringLiteral( "No spatial index." );
+  QString spatialIndexText = tr( "No spatial index." );
 
   if ( resSpatialIndexes.PQntuples() > 0 )
   {
@@ -5254,7 +5254,7 @@ QString QgsPostgresProvider::htmlMetadata() const
       spatialIndexes.append( resSpatialIndexes.PQgetvalue( i, 2 ) );
     }
 
-    spatialIndexText = QStringLiteral( "Spatial index/indices exists (%1)." ).arg( spatialIndexes.join( ", " ) );
+    spatialIndexText = tr( "Spatial index/indices exists (%1)." ).arg( spatialIndexes.join( ", " ) );
   }
 
   const QVariantMap additionalInformation {
