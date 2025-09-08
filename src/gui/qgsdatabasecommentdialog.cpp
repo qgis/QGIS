e99cd0ef0ca40d1234a7a4829194867fba6fd35a
Jan Caha <jan.caha@outlook.com>
2025-09-08 13:43:16 +0200

    database comment dialog

---
+++ src/gui/qgsdatabasecommentdialog.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+    qgsdatabasecommentdialog.cpp
+    ---------------------
+    begin                : September 2025
+    copyright            : (C) 2025 by Jan Caha
+    email                : jan.caha at outlook dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QApplication>
+#include <QMessageBox>
+#include <QGridLayout>
+#include <QLabel>
+
+#include "qgsdatabasecommentdialog.h"
+#include "moc_qgsdatabasecommentdialog.cpp"
+
+QgsDatabaseCommentDialog::QgsDatabaseCommentDialog( QString title, QString comment, QWidget *parent )
+  : QDialog( parent )
+{
+  setWindowTitle( title );
+
+  QGridLayout *layout = new QGridLayout( this );
+
+  mCommentEdit = new QPlainTextEdit( this );
+  mCommentEdit->setPlainText( comment );
+  layout->addWidget( new QLabel( tr( "Comment" ) ), 0, 0 );
+  layout->addWidget( mCommentEdit, 0, 1 );
+
+  mButtonBox = new QDialogButtonBox( this );
+  mButtonBox->setOrientation( Qt::Horizontal );
+  mButtonBox->setStandardButtons( QDialogButtonBox::Cancel | QDialogButtonBox::Ok );
+  connect( mButtonBox, &QDialogButtonBox::rejected, this, &QgsDatabaseCommentDialog::reject );
+  connect( mButtonBox, &QDialogButtonBox::accepted, this, &QgsDatabaseCommentDialog::accept );
+
+  layout->addWidget( mButtonBox, 1, 0, 2, 0 );
+}
+
+QString QgsDatabaseCommentDialog::comment() const
+{
+  return mCommentEdit->toPlainText();
+}
---
+++ src/gui/qgsdatabasecommentdialog.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+    qgsdatabasecommentdialog.h
+    ---------------------
+    begin                : September 2025
+    copyright            : (C) 2025 by Jan Caha
+    email                : jan.caha at outlook dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSDATABASECOMMENTDIALOG_H
+#define QGSDATABASECOMMENTDIALOG_H
+
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QPlainTextEdit>
+
+#include "qgsabstractdatabaseproviderconnection.h"
+#include "qgsdatabaseschemacombobox.h"
+
+/**
+ * \ingroup gui
+ * \brief Dialog which displays selection of a dialog for a specific database item.
+ *
+ * \warning The provider must support the connection API methods in its QgsProviderMetadata implementation.
+ *
+ * \since QGIS 4.0
+ */
+class GUI_EXPORT QgsDatabaseCommentDialog : public QDialog
+{
+    Q_OBJECT
+  public:
+    /**
+     * Constructor for QgsDatabaseCommentDialog, for the specified \a connection.
+     *
+     * Ownership of \a connection is transferred.
+     */
+    explicit QgsDatabaseCommentDialog( QString title, QString comment = QString(), QWidget *parent SIP_TRANSFERTHIS = nullptr );
+
+    /**
+     * Returns the comment for the current item.
+     */
+    QString comment() const;
+
+  private:
+    QDialogButtonBox *mButtonBox = nullptr;
+    QPlainTextEdit *mCommentEdit = nullptr;
+};
+#endif // QGSDATABASESCHEMACOMMENTDIALOG_H
