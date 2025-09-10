a01770e5cbe5c163d63781cb7967da9cb2240631
Nyall Dawson <nyall.dawson@gmail.com>
2025-09-10 10:44:32 +1000

    [feature] Support rendering annotation layer markers as 3d billboards
    This commit adds a new option for showing annotation layers in 3d maps.
    
    From the annotation layer properties, 3d tab, users can set the layer
    to render as 3d billboards. When activated, all MARKERS from the layer
    will be shown as floating billboard symbols above the 3d map. (lines,
    polygons, and text items from the layer are ignored).
    
    Users have control over the terrain clamping and offset for the
    billboards, and whether or not "callout" lines should be shown (vertical
    lines joining the billboard to the corresponding location on the
    terrain surface)

---
+++ src/3d/qgsannotationlayer3drenderer.h
@@ -0,0 +1,185 @@
+/***************************************************************************
+  qgsannotationlayer3drenderer.h
+  --------------------------------------
+  Date                 : September 2025
+  Copyright            : (C) 2025 by Nyall Dawson
+  Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSANNOTATIONLAYER3DRENDERER_H
+#define QGSANNOTATIONLAYER3DRENDERER_H
+
+#include "qgis_3d.h"
+#include "qgis_sip.h"
+
+#include "qgs3drendererregistry.h"
+#include "qgsabstract3drenderer.h"
+#include "qgsmaplayerref.h"
+
+class QgsAnnotationLayer;
+
+#ifdef SIP_RUN
+// this is needed for the "convert to subclass" code below to compile
+% ModuleHeaderCode
+#include "qgsannotationlayer3drenderer.h"
+  % End
+#endif
+
+  /**
+ * \ingroup core
+ * \brief Metadata for annotation layer 3D renderer to allow creation of its instances from XML.
+ *
+ * \warning This is not considered stable API, and may change in future QGIS releases. It is
+ * exposed to the Python bindings as a tech preview only.
+ *
+ * \since QGIS 4.0
+ */
+  class _3D_EXPORT QgsAnnotationLayer3DRendererMetadata : public Qgs3DRendererAbstractMetadata
+{
+  public:
+    QgsAnnotationLayer3DRendererMetadata();
+
+    //! Creates an instance of a 3D renderer based on a DOM element with renderer configuration
+    QgsAbstract3DRenderer *createRenderer( QDomElement &elem, const QgsReadWriteContext &context ) override SIP_FACTORY;
+};
+
+/**
+ * \ingroup qgis_3d
+ * \brief 3D renderers for annotation layers.
+ *
+ * \since QGIS 4.0
+ */
+class _3D_EXPORT QgsAnnotationLayer3DRenderer : public QgsAbstract3DRenderer
+{
+#ifdef SIP_RUN
+    SIP_CONVERT_TO_SUBCLASS_CODE
+    if ( dynamic_cast<QgsAnnotationLayer3DRenderer *>( sipCpp ) != nullptr )
+      sipType = sipType_QgsAnnotationLayer3DRenderer;
+    else
+      sipType = nullptr;
+    SIP_END
+#endif
+
+  public:
+    QgsAnnotationLayer3DRenderer();
+
+    /**
+     * Sets the annotation layer associated with the renderer.
+     *
+     * \see layer()
+     */
+    void setLayer( QgsAnnotationLayer *layer );
+
+    /**
+     * Returns the annotation layer associated with the renderer.
+     *
+     * \see setLayer()
+     */
+    QgsAnnotationLayer *layer() const;
+
+    QString type() const override;
+    QgsAnnotationLayer3DRenderer *clone() const override SIP_FACTORY;
+    Qt3DCore::QEntity *createEntity( Qgs3DMapSettings *map ) const override SIP_SKIP;
+
+    void writeXml( QDomElement &elem, const QgsReadWriteContext &context ) const override;
+    void readXml( const QDomElement &elem, const QgsReadWriteContext &context ) override;
+    void resolveReferences( const QgsProject &project ) override;
+
+    /**
+     * Returns the altitude clamping method, which determines the vertical position of annotations.
+     *
+     * \see setAltitudeClamping()
+     */
+    Qgis::AltitudeClamping altitudeClamping() const { return mAltClamping; }
+
+    /**
+     * Sets the altitude \a clamping method, which determines the vertical position of annotations.
+     *
+     * \see altitudeClamping()
+     */
+    void setAltitudeClamping( Qgis::AltitudeClamping clamping ) { mAltClamping = clamping; }
+
+    /**
+     * Returns the z offset, which is a fixed offset amount which should be added to z values for the annotations.
+     *
+     * \see setZOffset()
+     */
+    double zOffset() const { return mZOffset; }
+
+    /**
+     * Sets the z \a offset, which is a fixed offset amount which will be added to z values for the annotations.
+     *
+     * \see zOffset()
+     */
+    void setZOffset( double offset ) { mZOffset = offset; }
+
+    /**
+     * Returns TRUE if callout lines are shown, vertically joining the annotations to the terrain.
+     *
+     * \see setShowCalloutLines()
+     */
+    bool showCalloutLines() const;
+
+    /**
+     * Sets whether callout lines are shown, vertically joining the annotations to the terrain.
+     *
+     * \see showCalloutLines()
+     */
+    void setShowCalloutLines( bool show );
+
+    // TODO -- consider exposing via QgsSimpleLineMaterialSettings, for now, for testing only
+    /**
+     * Sets the callout line \a color.
+     *
+     * \see calloutLineColor()
+     * \note Not available in Python bindings
+     */
+    SIP_SKIP void setCalloutLineColor( const QColor &color );
+
+    /**
+     * Returns the callout line color.
+     *
+     * \see setCalloutLineColor()
+     * \note Not available in Python bindings
+     */
+    SIP_SKIP QColor calloutLineColor() const;
+
+    /**
+     * Sets the callout line \a width.
+     *
+     * \see calloutLineWidth()
+     * \note Not available in Python bindings
+     */
+    SIP_SKIP void setCalloutLineWidth( double width );
+
+    /**
+     * Returns the callout line width.
+     *
+     * \see setCalloutLineWidth()
+     * \note Not available in Python bindings
+     */
+    SIP_SKIP double calloutLineWidth() const;
+
+  private:
+#ifdef SIP_RUN
+    QgsAnnotationLayer3DRenderer( const QgsAnnotationLayer3DRenderer & );
+#endif
+
+    static constexpr double DEFAULT_Z_OFFSET = 50;
+
+    QgsMapLayerRef mLayerRef;
+    Qgis::AltitudeClamping mAltClamping = Qgis::AltitudeClamping::Relative;
+    double mZOffset = DEFAULT_Z_OFFSET;
+    bool mShowCalloutLines = true;
+    QColor mCalloutLineColor { 0, 0, 0 };
+    double mCalloutLineWidth = 2;
+};
+
+#endif // QGSANNOTATIONLAYER3DRENDERER_H
---
+++ src/app/3d/qgsannotationlayer3drendererwidget.cpp
@@ -0,0 +1,199 @@
+/***************************************************************************
+  qgsannotationlayer3drendererwidget.cpp
+  ------------------------------
+  Date                 : September 2025
+  Copyright            : (C) 2025 by Nyall Dawson
+  Email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsannotationlayer3drendererwidget.h"
+#include "moc_qgsannotationlayer3drendererwidget.cpp"
+
+#include "qgsapplication.h"
+#include "qgsannotationlayer.h"
+#include "qgsannotationlayer3drenderer.h"
+
+#include <QBoxLayout>
+#include <QCheckBox>
+
+QgsAnnotationLayer3DRendererWidget::QgsAnnotationLayer3DRendererWidget( QgsAnnotationLayer *layer, QgsMapCanvas *canvas, QWidget *parent )
+  : QgsMapLayerConfigWidget( layer, canvas, parent )
+{
+  setPanelTitle( tr( "3D View" ) );
+  setObjectName( QStringLiteral( "mOptsPage_3DView" ) );
+
+  setupUi( this );
+
+  mComboRendererType->addItem( QgsApplication::getThemeIcon( QStringLiteral( "rendererNullSymbol.svg" ) ), tr( "No Symbols" ), QVariant::fromValue( RendererType::None ) );
+  mComboRendererType->addItem( QgsApplication::getThemeIcon( QStringLiteral( "rendererSingleSymbol.svg" ) ), tr( "3D Billboards" ), QVariant::fromValue( RendererType::Billboards ) );
+  mComboRendererType->setCurrentIndex( mComboRendererType->findData( QVariant::fromValue( RendererType::None ) ) );
+  mStackedWidget->setCurrentWidget( mPageNoRenderer );
+
+  connect( mComboRendererType, qOverload< int >( &QComboBox::currentIndexChanged ), this, &QgsAnnotationLayer3DRendererWidget::rendererTypeChanged );
+
+  mComboClamping->addItem( tr( "Relative to Terrain" ), QVariant::fromValue( Qgis::AltitudeClamping::Relative ) );
+  mComboClamping->addItem( tr( "Absolute" ), QVariant::fromValue( Qgis::AltitudeClamping::Absolute ) );
+
+  connect( mComboClamping, qOverload<int>( &QComboBox::currentIndexChanged ), this, &QgsAnnotationLayer3DRendererWidget::clampingChanged );
+  mComboClamping->setCurrentIndex( mComboClamping->findData( QVariant::fromValue( Qgis::AltitudeClamping::Relative ) ) );
+  clampingChanged();
+
+  connect( mOffsetZSpinBox, qOverload< double >( &QDoubleSpinBox::valueChanged ), this, [this]( double ) {
+    if ( !mBlockChanges )
+      emit widgetChanged();
+  } );
+
+  connect( mCheckShowCallouts, &QCheckBox::toggled, this, [this] {
+    if ( !mBlockChanges )
+      emit widgetChanged();
+  } );
+
+  syncToLayer( layer );
+}
+
+void QgsAnnotationLayer3DRendererWidget::setRenderer( const QgsAnnotationLayer3DRenderer *renderer )
+{
+  mBlockChanges++;
+  mRenderer.reset( renderer ? renderer->clone() : nullptr );
+  if ( renderer )
+  {
+    mComboRendererType->setCurrentIndex( mComboRendererType->findData( QVariant::fromValue( RendererType::Billboards ) ) );
+    mComboClamping->setCurrentIndex( mComboClamping->findData( QVariant::fromValue( renderer->altitudeClamping() ) ) );
+    mOffsetZSpinBox->setValue( renderer->zOffset() );
+    mCheckShowCallouts->setChecked( renderer->showCalloutLines() );
+  }
+  else
+  {
+    mComboRendererType->setCurrentIndex( mComboRendererType->findData( QVariant::fromValue( RendererType::None ) ) );
+  }
+  mBlockChanges--;
+}
+
+std::unique_ptr< QgsAnnotationLayer3DRenderer > QgsAnnotationLayer3DRendererWidget::renderer()
+{
+  const RendererType type = mComboRendererType->currentData().value< RendererType >();
+  switch ( type )
+  {
+    case RendererType::None:
+      return nullptr;
+      break;
+    case RendererType::Billboards:
+    {
+      auto renderer = std::make_unique< QgsAnnotationLayer3DRenderer >();
+      renderer->setAltitudeClamping( mComboClamping->currentData().value< Qgis::AltitudeClamping >() );
+      renderer->setZOffset( mOffsetZSpinBox->value() );
+      renderer->setShowCalloutLines( mCheckShowCallouts->isChecked() );
+      return renderer;
+    }
+  }
+  BUILTIN_UNREACHABLE
+}
+
+void QgsAnnotationLayer3DRendererWidget::apply()
+{
+  std::unique_ptr< QgsAnnotationLayer3DRenderer > r = renderer();
+  mLayer->setRenderer3D( r.release() );
+}
+
+void QgsAnnotationLayer3DRendererWidget::rendererTypeChanged()
+{
+  const RendererType type = mComboRendererType->currentData().value< RendererType >();
+  switch ( type )
+  {
+    case RendererType::None:
+      mStackedWidget->setCurrentWidget( mPageNoRenderer );
+      break;
+    case RendererType::Billboards:
+      mStackedWidget->setCurrentWidget( mPageBillboards );
+      break;
+  }
+  if ( !mBlockChanges )
+    emit widgetChanged();
+}
+
+void QgsAnnotationLayer3DRendererWidget::clampingChanged()
+{
+  switch ( mComboClamping->currentData().value< Qgis::AltitudeClamping >() )
+  {
+    case Qgis::AltitudeClamping::Absolute:
+      mLabelClampingExplanation->setText(
+        QStringLiteral( "<p><b>%1</b></p><p>%2</p>" ).arg( tr( "All billboards will be placed at the same elevation." ), tr( "The terrain height will be ignored." ) )
+      );
+
+      break;
+    case Qgis::AltitudeClamping::Relative:
+      mLabelClampingExplanation->setText(
+        QStringLiteral( "<p><b>%1</b></p>" ).arg( tr( "Billboard elevation is relative to terrain height." ) )
+      );
+      break;
+    case Qgis::AltitudeClamping::Terrain:
+      mLabelClampingExplanation->setText(
+        QStringLiteral( "<p><b>%1</b></p><p>%2</p>" ).arg( tr( "Billboard elevation will be taken directly from the terrain height." ), tr( "Billboards will be placed directly on the terrain." ) )
+      );
+      break;
+  }
+  if ( !mBlockChanges )
+    emit widgetChanged();
+}
+
+void QgsAnnotationLayer3DRendererWidget::syncToLayer( QgsMapLayer *layer )
+{
+  mLayer = layer;
+  QgsAbstract3DRenderer *r = layer->renderer3D();
+  if ( r && r->type() == QLatin1String( "annotation" ) )
+  {
+    QgsAnnotationLayer3DRenderer *annotationRenderer = qgis::down_cast<QgsAnnotationLayer3DRenderer *>( r );
+    setRenderer( annotationRenderer );
+  }
+  else
+  {
+    setRenderer( nullptr );
+  }
+}
+
+//
+// QgsAnnotationLayer3DRendererWidgetFactory
+//
+
+QgsAnnotationLayer3DRendererWidgetFactory::QgsAnnotationLayer3DRendererWidgetFactory( QObject *parent )
+  : QObject( parent )
+{
+  setIcon( QIcon( ":/images/themes/default/3d.svg" ) );
+  setTitle( tr( "3D View" ) );
+}
+
+QgsMapLayerConfigWidget *QgsAnnotationLayer3DRendererWidgetFactory::createWidget( QgsMapLayer *layer, QgsMapCanvas *canvas, bool dockWidget, QWidget *parent ) const
+{
+  Q_UNUSED( dockWidget )
+  QgsAnnotationLayer *annotationLayer = qobject_cast<QgsAnnotationLayer *>( layer );
+  if ( !annotationLayer )
+    return nullptr;
+  return new QgsAnnotationLayer3DRendererWidget( annotationLayer, canvas, parent );
+}
+
+bool QgsAnnotationLayer3DRendererWidgetFactory::supportLayerPropertiesDialog() const
+{
+  return true;
+}
+
+bool QgsAnnotationLayer3DRendererWidgetFactory::supportsStyleDock() const
+{
+  return true;
+}
+
+bool QgsAnnotationLayer3DRendererWidgetFactory::supportsLayer( QgsMapLayer *layer ) const
+{
+  return layer->type() == Qgis::LayerType::Annotation;
+}
+
+QString QgsAnnotationLayer3DRendererWidgetFactory::layerPropertiesPagePositionHint() const
+{
+  return QStringLiteral( "mOptsPage_Rendering" );
+}
