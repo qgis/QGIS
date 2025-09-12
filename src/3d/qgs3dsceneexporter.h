82bbcd051d4347dead311b7175aae2477ce0f973
Jean Felder <jean.felder@oslandia.com>
2025-09-12 17:59:50 +0200

    tests(3d): Factor out the exporter tests
    Move it to a dedicated file.

--- src/3d/qgs3dsceneexporter.h
+++ src/3d/qgs3dsceneexporter.h
@@ -43,7 +43,7 @@ class QgsPolygon3DSymbol;
 class QgsLine3DSymbol;
 class QgsPoint3DSymbol;
 class QgsMeshEntity;
-class TestQgs3DRendering;
+class TestQgs3DExporter;
 
 #define SIP_NO_FILE
 
@@ -158,7 +158,7 @@ class _3D_EXPORT Qgs3DSceneExporter : public Qt3DCore::QEntity
     friend QgsPolygon3DSymbol;
     friend QgsLine3DSymbol;
     friend QgsPoint3DSymbol;
-    friend TestQgs3DRendering;
+    friend TestQgs3DExporter;
 };
 
 #endif // QGS3DSCENEEXPORTER_H
