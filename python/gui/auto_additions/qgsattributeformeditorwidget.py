be1c28e0b497b78e8b16fc8b4fb9124cbc36e796
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-09-13 19:38:50 +0700

    [attribute form][ui] Add UI to toggle reuse value remembrance

--- python/gui/auto_additions/qgsattributeformeditorwidget.py
+++ python/gui/auto_additions/qgsattributeformeditorwidget.py
@@ -2,6 +2,6 @@
 try:
     QgsAttributeFormEditorWidget.__attribute_docs__ = {'valueChanged': "Emitted when the widget's value changes\n\n:param value: new widget value\n\n.. deprecated:: 3.10\n\n   Use :py:func:`~QgsAttributeFormEditorWidget.valuesChanged` instead.\n", 'valuesChanged': "Emitted when the widget's value changes\n\n:param value: new widget value\n:param additionalFieldValues: of the potential additional fields\n\n.. versionadded:: 3.10\n"}
     QgsAttributeFormEditorWidget.__overridden_methods__ = ['createSearchWidgetWrappers']
-    QgsAttributeFormEditorWidget.__signal_arguments__ = {'valueChanged': ['value: object'], 'valuesChanged': ['value: object', 'additionalFieldValues: List[object]']}
+    QgsAttributeFormEditorWidget.__signal_arguments__ = {'valueChanged': ['value: object'], 'valuesChanged': ['value: object', 'additionalFieldValues: List[object]'], 'rememberLastValueChanged': ['index: int', 'remember: bool']}
 except (NameError, AttributeError):
     pass
