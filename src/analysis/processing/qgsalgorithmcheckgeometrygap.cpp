19a8599ca68a5c76be5aaeebb8ac3a3adb01f062
Alexander Bruy <alexander.bruy@gmail.com>
2025-10-01 13:50:30 +0100

    make small gaps check/fix work without QgsProject::instance()

--- src/analysis/processing/qgsalgorithmcheckgeometrygap.cpp
+++ src/analysis/processing/qgsalgorithmcheckgeometrygap.cpp
@@ -181,9 +181,7 @@ QVariantMap QgsGeometryCheckGapAlgorithm::processAlgorithm( const QVariantMap &p
 
   QgsProcessingMultiStepFeedback multiStepFeedback( 3, feedback );
 
-  const QgsProject *project = QgsProject::instance();
-
-  QgsGeometryCheckContext checkContext = QgsGeometryCheckContext( mTolerance, input->sourceCrs(), project->transformContext(), project, uniqueIdFieldIdx );
+  QgsGeometryCheckContext checkContext = QgsGeometryCheckContext( mTolerance, input->sourceCrs(), context.transformContext(), context.project(), uniqueIdFieldIdx );
 
   // Test detection
   QList<QgsGeometryCheckError *> checkErrors;
--- tests/src/analysis/testqgsprocessingcheckgeometry.cpp
+++ tests/src/analysis/testqgsprocessingcheckgeometry.cpp
@@ -669,6 +669,7 @@ void TestQgsProcessingCheckGeometry::gapAlg()
     bool ok = false;
     QgsProcessingFeedback feedback;
     auto context = std::make_unique< QgsProcessingContext >();
+    context->setProject( QgsProject::instance() );
 
     QVariantMap results;
     results = alg->run( parameters, *context, &feedback, &ok );
@@ -709,6 +710,7 @@ void TestQgsProcessingCheckGeometry::gapAlg()
     bool ok = false;
     QgsProcessingFeedback feedback;
     auto context = std::make_unique< QgsProcessingContext >();
+    context->setProject( QgsProject::instance() );
 
     QVariantMap results;
     results = alg->run( parameters, *context, &feedback, &ok );
