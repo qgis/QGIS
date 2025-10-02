65e1ef2f23cb9b6744a0d1d3bef72372887da16e
sewe2000 <s3vdev@proton.me>
2025-10-02 15:09:42 +0200

    Fix documentation links in qgisapp.h

--- src/app/qgisapp.h
+++ src/app/qgisapp.h
@@ -861,7 +861,7 @@ class APP_EXPORT QgisApp : public QMainWindow, private Ui::MainWindow
      * \note Ownership of \a blocker is not transferred, and the blocker must
      *       be unregistered when plugin is unloaded.
      *
-     * \see unregisterLayerCommitChangesBlocker()
+     * \see unregisterLayerChangesCommitBlocker()
      */
     void registerLayerChangesCommitBlocker( QgsLayerChangesCommitBlockerInterface *blocker );
 
@@ -873,7 +873,7 @@ class APP_EXPORT QgisApp : public QMainWindow, private Ui::MainWindow
 
     /**
      * Unregister a previously registered commit layer changes \a blocker.
-     * \see registerLayerCommitChangesBlocker()
+     * \see registerLayerChangesCommitBlocker()
     */
     void unregisterLayerChangesCommitBlocker( QgsLayerChangesCommitBlockerInterface *blocker );
 
