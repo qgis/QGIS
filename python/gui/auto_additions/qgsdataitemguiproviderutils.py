2489090bbd42b96766079729564ede7558a9d984
Jan Caha <jan.caha@outlook.com>
2025-10-14 15:26:58 +0200

    Browser DB Table Comment (#63109)
    * database comment dialog
    
    * create helper function to add to submenu or create it
    
    * edit database table comment
    
    * fix keyword
    
    * add file
    
    * fix docs
    
    * fix include
    
    * restructure to make cppcheck happy
    
    * add missing param
    
    * drop documentation of params
    
    * Apply suggestions from code review
    
    Co-authored-by: Nyall Dawson <nyall.dawson@gmail.com>
    
    * review fix
    
    * rename class
    
    * update sip
    
    * change action label
    
    * remove leftoverfiles
    
    * remove doc line
    
    * create dialog without pointer
    
    * use unique ptr so that it does not leak and windows build work
    
    * include qgis_sip.h
    
    * update sip files
    
    * rename class
    
    update sip
    
    * use unique ptr so that it does not leak and windows build work
    
    include qgis_sip.h
    
    update sip files
    
    * move  function to QgsDataItemGuiProviderUtils and make static
    
    * use the new API
    
    * set const
    
    * remove unnecessary class
    
    * fix sip
    
    ---------
    
    Co-authored-by: Nyall Dawson <nyall.dawson@gmail.com>

---
+++ python/gui/auto_additions/qgsdataitemguiproviderutils.py
@@ -0,0 +1,5 @@
+# The following has been generated automatically from src/gui/qgsdataitemguiproviderutils.h
+try:
+    QgsDataItemGuiProviderUtils.addToSubMenu = staticmethod(QgsDataItemGuiProviderUtils.addToSubMenu)
+except (NameError, AttributeError):
+    pass
