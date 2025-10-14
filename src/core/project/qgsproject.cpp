e3188def3f31f5a742d78acc064c3abfbb44d48b
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-10-14 09:54:52 +0700

    [qml] Fix unreliable QgsProject fileName Q_PROPERTY

--- src/core/project/qgsproject.cpp
+++ src/core/project/qgsproject.cpp
@@ -1294,6 +1294,7 @@ void QgsProject::clear()
 
   setDirty( false );
   emit homePathChanged();
+  emit fileNameChanged();
   if ( !mBlockChangeSignalsDuringClear )
   {
     emit verticalCrsChanged();
@@ -2020,6 +2021,7 @@ bool QgsProject::read( Qgis::ProjectReadFlags flags )
       mTranslator.reset( nullptr );
     }
   }
+  emit fileNameChanged();
   emit homePathChanged();
   return returnValue;
 }
@@ -3146,6 +3148,7 @@ bool QgsProject::write( const QString &filename )
   QGIS_PROTECT_QOBJECT_THREAD_ACCESS
 
   mFile.setFileName( filename );
+  emit fileNameChanged();
   mCachedHomePath.clear();
   return write();
 }
