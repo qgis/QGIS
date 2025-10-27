4990e470e4a84091e06c6a91c03b8bb55675f641
Jan Caha <jan.caha@outlook.com>
2025-10-27 11:31:34 +0100

    selection dialog for PG QGIS project versions

---
+++ src/providers/postgres/qgspostgresprojectversionsdialog.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+    qgspostgresprojectversionsdialog.h
+    ---------------------
+    begin                : October 2025
+    copyright            : (C) 2025 by Jan Caha
+    email                : jan.caha at outlook dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef QGSPOSTGRESPROJECTVERSIONSDIALOG_H
+#define QGSPOSTGRESPROJECTVERSIONSDIALOG_H
+
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QTableView>
+
+#include "qgspostgresprojectversionsmodel.h"
+
+class QgsPostgresProjectVersionsDialog : public QDialog
+{
+    Q_OBJECT
+  public:
+    QgsPostgresProjectVersionsDialog( const QString &connectionName, const QString &schema, const QString &project, QWidget *parent = nullptr );
+
+    void accept() override;
+
+  private:
+    QgsPostgresProjectVersionsModel *mModel = nullptr;
+    QTableView *mTableView = nullptr;
+    QDialogButtonBox *mButtonBox = nullptr;
+};
+
+#endif // QGSPOSTGRESPROJECTVERSIONSDIALOG_H
