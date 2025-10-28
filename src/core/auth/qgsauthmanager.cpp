122f3d2fabbec13fe7439a4e44b2c6a6d0c65263
Nyall Dawson <nyall.dawson@gmail.com>
2025-10-28 10:22:06 +1000

    Fix incorrect return value from masterPasswordRowsInDb, cleanup api
    We were incorrectly comparing the int* pointer to 0, not its value
    
    Just side step the whole issue by cleaning up the API and using an int& argument instead

--- src/core/auth/qgsauthmanager.cpp
+++ src/core/auth/qgsauthmanager.cpp
@@ -552,7 +552,7 @@ bool QgsAuthManager::verifyMasterPassword( const QString &compare )
     return false;
 
   int rows = 0;
-  if ( !masterPasswordRowsInDb( &rows ) )
+  if ( !masterPasswordRowsInDb( rows ) )
   {
     const char *err = QT_TR_NOOP( "Master password: FAILED to access database" );
     QgsDebugError( err );
@@ -3586,14 +3586,14 @@ bool QgsAuthManager::masterPasswordInput()
   return false;
 }
 
-bool QgsAuthManager::masterPasswordRowsInDb( int *rows ) const
+bool QgsAuthManager::masterPasswordRowsInDb( int &rows ) const
 {
   ensureInitialized();
 
   if ( isDisabled() )
     return false;
 
-  *rows = 0;
+  rows = 0;
 
   QMutexLocker locker( mMutex.get() );
 
@@ -3604,7 +3604,7 @@ bool QgsAuthManager::masterPasswordRowsInDb( int *rows ) const
   {
     try
     {
-      *rows += storage->masterPasswords( ).count();
+      rows += storage->masterPasswords( ).count();
     }
     catch ( const QgsNotSupportedException &e )
     {
@@ -3619,7 +3619,6 @@ bool QgsAuthManager::masterPasswordRowsInDb( int *rows ) const
   }
 
   return rows != 0;
-
 }
 
 bool QgsAuthManager::masterPasswordHashInDatabase() const
@@ -3630,7 +3629,7 @@ bool QgsAuthManager::masterPasswordHashInDatabase() const
     return false;
 
   int rows = 0;
-  if ( !masterPasswordRowsInDb( &rows ) )
+  if ( !masterPasswordRowsInDb( rows ) )
   {
     const char *err = QT_TR_NOOP( "Master password: FAILED to access database" );
     QgsDebugError( err );
