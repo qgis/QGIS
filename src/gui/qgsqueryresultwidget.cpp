dcd2f789634a3a5374ac405293e0da4541eb2163
Harrissou Sant-anna <delazj@gmail.com>
2025-10-30 08:04:56 +0100

    Add help button to SQL Query dialogs (#62712)

--- src/gui/qgsqueryresultwidget.cpp
+++ src/gui/qgsqueryresultwidget.cpp
@@ -29,6 +29,7 @@
 #include "qgsprovidermetadata.h"
 #include "qgscodeeditorwidget.h"
 #include "qgsfileutils.h"
+#include "qgshelp.h"
 #include "qgsstoredquerymanager.h"
 #include "qgsproject.h"
 #include "qgsnewnamedialog.h"
@@ -36,6 +37,7 @@
 #include "qgsdbqueryhistoryprovider.h"
 
 #include <QClipboard>
+#include <QDialogButtonBox>
 #include <QShortcut>
 #include <QFileDialog>
 #include <QMessageBox>
@@ -1217,8 +1219,16 @@ QgsQueryResultDialog::QgsQueryResultDialog( QgsAbstractDatabaseProviderConnectio
 
   mWidget = new QgsQueryResultWidget( this, connection );
   QVBoxLayout *l = new QVBoxLayout();
-  l->setContentsMargins( 0, 0, 0, 0 );
+  l->setContentsMargins( 6, 6, 6, 6 );
+
+  QDialogButtonBox *mButtonBox = new QDialogButtonBox( QDialogButtonBox::StandardButton::Close | QDialogButtonBox::StandardButton::Help );
+  connect( mButtonBox, &QDialogButtonBox::rejected, this, &QDialog::close );
+  connect( mButtonBox, &QDialogButtonBox::helpRequested, this, [] {
+    QgsHelp::openHelp( QStringLiteral( "managing_data_source/create_layers.html#execute-sql" ) );
+  } );
   l->addWidget( mWidget );
+  l->addWidget( mButtonBox );
+
   setLayout( l );
 }
 
@@ -1247,6 +1257,7 @@ QgsQueryResultMainWindow::QgsQueryResultMainWindow( QgsAbstractDatabaseProviderC
 
   mWidget = new QgsQueryResultWidget( nullptr, connection );
   setCentralWidget( mWidget );
+  mWidget->layout()->setContentsMargins( 6, 6, 6, 6 );
 
   connect( mWidget, &QgsQueryResultWidget::requestDialogTitleUpdate, this, &QgsQueryResultMainWindow::updateWindowTitle );
 
