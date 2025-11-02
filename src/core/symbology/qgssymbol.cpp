8f8e00c35ef6c1366025e417b8402d4065f93e5e
Nyall Dawson <nyall.dawson@gmail.com>
2025-11-03 08:54:53 +1000

    Fix some uncaught QgsCsException instances

--- src/core/symbology/qgssymbol.cpp
+++ src/core/symbology/qgssymbol.cpp
@@ -2267,7 +2267,14 @@ void QgsSymbol::renderFeature( const QgsFeature &feature, QgsRenderContext &cont
         z = 0.0;
         if ( ct.isValid() )
         {
-          ct.transformInPlace( x, y, z );
+          try
+          {
+            ct.transformInPlace( x, y, z );
+          }
+          catch ( QgsCsException & )
+          {
+            continue;
+          }
         }
         mapPoint.setX( x );
         mapPoint.setY( y );
