004facf3d520c257429a0ca975676e2c8373274e
Nyall Dawson <nyall.dawson@gmail.com>
2025-11-03 13:12:22 +1000

    Remove dead code

--- src/providers/spatialite/qgsspatialiteprovider.h
+++ src/providers/spatialite/qgsspatialiteprovider.h
@@ -333,8 +333,6 @@ class QgsSpatiaLiteProvider final : public QgsVectorDataProvider
     //! SpatiaLite minor version
     int mSpatialiteVersionMinor = 0;
 
-    //! Internal transaction handling (for addFeatures etc.)
-    int mSavepointId;
     static QAtomicInt sSavepointId;
 
     /**
