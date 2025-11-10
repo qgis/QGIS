f93c091b7359dd7cf644e14b453940c263ab9f3f
Nyall Dawson <nyall.dawson@gmail.com>
2025-11-10 11:22:00 +1000

    Gracefully handle some more possible exceptions

--- src/core/dxf/qgsdxfexport.cpp
+++ src/core/dxf/qgsdxfexport.cpp
@@ -2650,7 +2650,16 @@ void QgsDxfExport::createDDBlockInfo()
         QgsFeatureRequest request = QgsFeatureRequest().setSubsetOfAttributes( job->attributes, job->fields ).setFlags( Qgis::FeatureRequestFlag::NoGeometry ).setExpressionContext( job->renderContext.expressionContext() );
         QgsCoordinateTransform extentTransform = ct;
         extentTransform.setBallparkTransformsAreAppropriate( true );
-        request.setFilterRect( extentTransform.transformBoundingBox( mExtent, Qgis::TransformDirection::Reverse ) );
+        try
+        {
+          request.setFilterRect( extentTransform.transformBoundingBox( mExtent, Qgis::TransformDirection::Reverse ) );
+        }
+        catch ( QgsCsException &e )
+        {
+          QgsDebugError( QStringLiteral( "Could not transform extent to layer extent: %1" ).arg( e.what() ) );
+          continue;
+        }
+
         QgsFeatureIterator featureIt = job->featureSource.getFeatures( request );
 
         QHash <uint, QPair<int, DataDefinedBlockInfo> > blockSymbolMap; //symbolHash/occurrences/block Text
