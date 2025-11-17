e3015df99ad387854a14a695cde21d0e5a1627fc
viperminiq <viperminiq@gmail.com>
2025-11-17 11:09:00 +0100

    fix clang tidy cast warning

--- src/core/qgstessellator.cpp
+++ src/core/qgstessellator.cpp
@@ -584,7 +584,7 @@ void QgsTessellator::addTriangleVertices(
 
     // cppcheck-suppress negativeContainerIndex
     QVector3D point = points[ index ];
-    const float z = mNoZ ? 0.0 : point.z();
+    const double z = mNoZ ? 0.0 : point.z();
     QVector4D pt( point.x(), point.y(), z, 0 );
 
     pt = *transformMatrix * pt;
