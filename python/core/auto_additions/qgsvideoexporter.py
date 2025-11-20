ee2ab7a81c91dcf7f02edaa41a375d9cbd35090f
Nyall <nyall.dawson@gmail.com>
2025-11-21 04:13:07 +1000

    Python

---
+++ python/core/auto_additions/qgsvideoexporter.py
@@ -0,0 +1,5 @@
+# The following has been generated automatically from src/core/multimedia/qgsvideoexporter.h
+try:
+    QgsVideoExporter.__group__ = ['multimedia']
+except (NameError, AttributeError):
+    pass
