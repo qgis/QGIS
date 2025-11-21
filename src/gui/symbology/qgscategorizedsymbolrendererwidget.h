259a2184ac23ee1a2e9dba518d74a89eaa0f5d26
Jean Felder <jean.felder@oslandia.com>
2025-11-21 12:14:15 +0100

    qgscategorizedsymbolrendererwidget: Use QgsVectorLayerUtils
    Deprecate layerUniqueValues and use
    `QgsVectorLayerUtils::uniqueValues` instead.

--- src/gui/symbology/qgscategorizedsymbolrendererwidget.h
+++ src/gui/symbology/qgscategorizedsymbolrendererwidget.h
@@ -245,8 +245,10 @@ class GUI_EXPORT QgsCategorizedSymbolRendererWidget : public QgsRendererWidget, 
      * Returns the list of unique values in the current widget's layer for attribute name \a attrName.
      *
      * Called by addCategories() and deleteUnusedCategories()
+     *
+     * \deprecated QGIS 4.0. Use QgsVectorLayerUtils::uniqueValues instead.
      */
-    QList<QVariant> layerUniqueValues( const QString &attrName );
+    Q_DECL_DEPRECATED QList<QVariant> layerUniqueValues( const QString &attrName ) SIP_DEPRECATED;
 
     QList<QgsSymbol *> selectedSymbols() override;
     QgsCategoryList selectedCategoryList();
