eb2067fe3644b2f4aa2c5810bcc6235f9686bba1
Nyall <nyall.dawson@gmail.com>
2025-11-23 08:49:11 +1000

    Docs

--- python/core/auto_additions/qgsvideoexporter.py
+++ python/core/auto_additions/qgsvideoexporter.py
@@ -1,5 +1,6 @@
 # The following has been generated automatically from src/core/multimedia/qgsvideoexporter.h
 try:
+    QgsVideoExporter.__attribute_docs__ = {'finished': 'Emitted when the video export finishes.\n'}
     QgsVideoExporter.__group__ = ['multimedia']
 except (NameError, AttributeError):
     pass
