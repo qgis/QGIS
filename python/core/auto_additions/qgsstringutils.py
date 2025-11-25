1a0ff7a1281b3da7cdf19bc80a62ff839c009a81
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2025-11-25 16:33:09 +0200

    update logic so that it all works

--- python/core/auto_additions/qgsstringutils.py
+++ python/core/auto_additions/qgsstringutils.py
@@ -1,9 +1,7 @@
 # The following has been generated automatically from src/core/qgsstringutils.h
 try:
-    QgsStringReplacement.fromProperties = staticmethod(QgsStringReplacement.fromProperties)
-except (NameError, AttributeError):
-    pass
-try:
+    QgsStringUtils.__attribute_docs__ = {'UNACCENT_MAP': 'Lookup table used by :py:func:`~QgsStringUtils.unaccent`. Generated at build time.'}
+    QgsStringUtils.__annotations__ = {'UNACCENT_MAP': 'Dict[str, str]'}
     QgsStringUtils.capitalize = staticmethod(QgsStringUtils.capitalize)
     QgsStringUtils.ampersandEncode = staticmethod(QgsStringUtils.ampersandEncode)
     QgsStringUtils.levenshteinDistance = staticmethod(QgsStringUtils.levenshteinDistance)
@@ -19,6 +17,11 @@ try:
     QgsStringUtils.qRegExpEscape = staticmethod(QgsStringUtils.qRegExpEscape)
     QgsStringUtils.truncateMiddleOfString = staticmethod(QgsStringUtils.truncateMiddleOfString)
     QgsStringUtils.containsByWord = staticmethod(QgsStringUtils.containsByWord)
+    QgsStringUtils.createUnaccentMap = staticmethod(QgsStringUtils.createUnaccentMap)
     QgsStringUtils.unaccent = staticmethod(QgsStringUtils.unaccent)
+except (NameError, AttributeError):
+    pass
+try:
+    QgsStringReplacement.fromProperties = staticmethod(QgsStringReplacement.fromProperties)
 except (NameError, AttributeError):
     pass
