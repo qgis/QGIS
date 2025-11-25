19b3f1ccd7ba8c0402619806f28f2603d606b353
Dewey Dunnington <dewey@dunnington.ca>
2025-11-25 13:41:04 -0600

    indent header

--- src/core/qgsarrowiterator.h
+++ src/core/qgsarrowiterator.h
@@ -30,19 +30,19 @@
 ///@cond PRIVATE
 struct ArrowSchema
 {
-    // Array type description
-    const char *format;
-    const char *name;
-    const char *metadata;
-    int64_t flags;
-    int64_t n_children;
-    struct ArrowSchema **children;
-    struct ArrowSchema *dictionary;
+  // Array type description
+  const char *format;
+  const char *name;
+  const char *metadata;
+  int64_t flags;
+  int64_t n_children;
+  struct ArrowSchema **children;
+  struct ArrowSchema *dictionary;
 
-    // Release callback
-    void ( *release )( struct ArrowSchema * );
-    // Opaque producer-specific data
-    void *private_data;
+  // Release callback
+  void ( *release )( struct ArrowSchema * );
+  // Opaque producer-specific data
+  void *private_data;
 };
 /// @endcond
 #endif
@@ -51,20 +51,20 @@ struct ArrowSchema
 ///@cond PRIVATE
 struct ArrowArray
 {
-    // Array data description
-    int64_t length;
-    int64_t null_count;
-    int64_t offset;
-    int64_t n_buffers;
-    int64_t n_children;
-    const void **buffers;
-    struct ArrowArray **children;
-    struct ArrowArray *dictionary;
+  // Array data description
+  int64_t length;
+  int64_t null_count;
+  int64_t offset;
+  int64_t n_buffers;
+  int64_t n_children;
+  const void **buffers;
+  struct ArrowArray **children;
+  struct ArrowArray *dictionary;
 
-    // Release callback
-    void ( *release )( struct ArrowArray * );
-    // Opaque producer-specific data
-    void *private_data;
+  // Release callback
+  void ( *release )( struct ArrowArray * );
+  // Opaque producer-specific data
+  void *private_data;
 };
 ///@endcond
 #endif
@@ -78,16 +78,16 @@ struct ArrowArray
 ///@cond PRIVATE
 struct ArrowArrayStream
 {
-    // Callbacks providing stream functionality
-    int ( *get_schema )( struct ArrowArrayStream *, struct ArrowSchema *out );
-    int ( *get_next )( struct ArrowArrayStream *, struct ArrowArray *out );
-    const char *( *get_last_error )( struct ArrowArrayStream * );
+  // Callbacks providing stream functionality
+  int ( *get_schema )( struct ArrowArrayStream *, struct ArrowSchema *out );
+  int ( *get_next )( struct ArrowArrayStream *, struct ArrowArray *out );
+  const char *( *get_last_error )( struct ArrowArrayStream * );
 
-    // Release callback
-    void ( *release )( struct ArrowArrayStream * );
+  // Release callback
+  void ( *release )( struct ArrowArrayStream * );
 
-    // Opaque producer-specific data
-    void *private_data;
+  // Opaque producer-specific data
+  void *private_data;
 };
 ///@endcond
 #endif
