e91dee274d9ca4d9cbd1b7fa1afe209258f0a3d3
Jan Caha <jan.caha@outlook.com>
2025-11-26 16:50:45 +0100

    only use uri, provider key is already there

--- src/core/browser/qgsfielddomainsitem.cpp
+++ src/core/browser/qgsfielddomainsitem.cpp
@@ -62,7 +62,7 @@ QVector<QgsDataItem *> QgsFieldDomainsItem::createChildren()
           try
           {
             std::unique_ptr< QgsFieldDomain > domain( conn->fieldDomain( name ) );
-            QgsFieldDomainItem *fieldDomainItem { new QgsFieldDomainItem( this, domain.release(), connectionUri(), providerKey() ) };
+            QgsFieldDomainItem *fieldDomainItem { new QgsFieldDomainItem( this, domain.release(), connectionUri() ) };
             children.push_back( fieldDomainItem );
           }
           catch ( QgsProviderConnectionException &ex )
@@ -99,9 +99,9 @@ QString QgsFieldDomainsItem::connectionUri() const
 // QgsFieldDomainItem
 //
 
-QgsFieldDomainItem::QgsFieldDomainItem( QgsDataItem *parent, QgsFieldDomain *domain, const QString &connectionUri, const QString &providerKey )
+QgsFieldDomainItem::QgsFieldDomainItem( QgsDataItem *parent, QgsFieldDomain *domain, const QString &connectionUri )
   : QgsDataItem( Qgis::BrowserItemType::Custom, parent, domain->name(), parent->path() + '/' + domain->name(), parent->providerKey() )
-  , mDomain( domain ), mProviderKey( providerKey ), mConnectionUri( connectionUri )
+  , mDomain( domain ),  mConnectionUri( connectionUri )
 {
   // Precondition
   Q_ASSERT( dynamic_cast<QgsFieldDomainsItem *>( parent ) );
