30f7658fa403f81a88abad8d53ff779fc848140c
Nyall Dawson <nyall.dawson@gmail.com>
2025-12-03 16:12:56 +1000

    Rework prepare_commit.sh
    - Only do the code formatting, no sip logic (will be moved to separate
      file)
    - Run sort_includes.py as part of formatting
    - Run doxygen_space.py on ALL files, not just those from src/core

--- scripts/astyle.sh
+++ scripts/astyle.sh
@@ -101,7 +101,6 @@ astyleit() {
 	$ASTYLE --options="$ASTYLEOPTS" "$1"
         modified=$1.unify_includes_modified
 	cp "$1" "$modified"
-	scripts/doxygen_space.py "$modified"
 	diff "$1" "$modified" >/dev/null || mv "$modified" "$1"
 	rm -f "$modified"
 }
