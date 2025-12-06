baacd6d3de523ae10cd95f6cc1d220e4c02605ac
jbp35 <19955142+jbp35@users.noreply.github.com>
2025-12-06 16:06:12 +0100

    Add new QgsWebEngineView class

---
+++ python/gui/auto_additions/qgswebengineview.py
@@ -0,0 +1,6 @@
+# The following has been generated automatically from src/gui/web/qgswebengineview.h
+try:
+    QgsWebEngineView.__overridden_methods__ = ['dragEnterEvent', 'dropEvent', 'eventFilter']
+    QgsWebEngineView.__group__ = ['web']
+except (NameError, AttributeError):
+    pass
