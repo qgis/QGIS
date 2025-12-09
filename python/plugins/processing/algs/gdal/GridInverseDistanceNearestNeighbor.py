c3b3f5d4d7d85699886b6e28607ebbdf7e837262
Even Rouault <even.rouault@spatialys.com>
2025-12-09 18:42:12 +0100

    python/plugins/processing/algs/gdal: use QgsProcessingAlgorithm::parameterAsOutputFormat()

--- python/plugins/processing/algs/gdal/GridInverseDistanceNearestNeighbor.py
+++ python/plugins/processing/algs/gdal/GridInverseDistanceNearestNeighbor.py
@@ -266,7 +266,7 @@ class GridInverseDistanceNearestNeighbor(GdalAlgorithm):
         out = self.parameterAsOutputLayer(parameters, self.OUTPUT, context)
         self.setOutputValue(self.OUTPUT, out)
 
-        output_format = QgsRasterFileWriter.driverForExtension(os.path.splitext(out)[1])
+        output_format = self.outputFormat(parameters, self.OUTPUT, context)
         if not output_format:
             raise QgsProcessingException(self.tr("Output format is invalid"))
 
