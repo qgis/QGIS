7af969f21796cfba9530616293f8b9799607cfd4
Even Rouault <even.rouault@spatialys.com>
2025-12-09 22:32:26 +0100

    Add QgsFeedback::createScaledFeedback()

--- src/core/qgsfeedback.h
+++ src/core/qgsfeedback.h
@@ -49,6 +49,9 @@ class CORE_EXPORT QgsFeedback : public QObject
       : QObject( parent )
     {}
 
+    //! Destrucotr
+    ~QgsFeedback() override;
+
     //! Tells whether the operation has been canceled already
     bool isCanceled() const SIP_HOLDGIL { return mCanceled; }
 
@@ -98,6 +101,20 @@ class CORE_EXPORT QgsFeedback : public QObject
       mProcessedCount = processedCount;
       emit processedCountChanged( processedCount );
     }
+
+    /**
+     * Returns a feedback object whose [0, 100] progression range will
+     * be mapped to \a parentFeedback [\a startPercentage, \a endPercentage].
+     *
+     * Cancellation on the returned feedback causes cancellation of the
+     * parent feedback.
+     *
+     * parentFeedback must be kept alive while the returned feedback is used.
+     *
+     * \since QGIS 3.40
+     */
+    static std::unique_ptr<QgsFeedback> createScaledFeedback(
+      QgsFeedback *parentFeedback, double startPercentage, double endPercentage );
 
   public slots:
 
