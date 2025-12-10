7a56c9d9b5c0d68c383c532b7be9a8e734ba43f6
Matthias Kuhn <matthias@opengis.ch>
2025-12-10 11:53:06 +0100

    Add comment to avoid regressing

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -994,6 +994,7 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, bool skipBadLayers
   connect( mUserProfileManager, &QgsUserProfileManager::profilesChanged, this, &QgisApp::refreshProfileMenu );
   endProfile();
 
+  // Initialize QGIS (and the plugins) before the network
   QgsApplication::initQgis();
 
   // start the network logger early, we want all requests logged!
