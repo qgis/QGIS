2263e8584cfaa0a79f894e04b043e43c77fa6cd6
DelazJ <delazj@gmail.com>
2025-12-11 09:59:44 +0100

    Remove remaining mention of category field for polar plot algorithm

--- python/plugins/processing/algs/qgis/PolarPlot.py
+++ python/plugins/processing/algs/qgis/PolarPlot.py
@@ -36,7 +36,6 @@ from qgis.PyQt.QtCore import QCoreApplication
 class PolarPlot(QgisAlgorithm):
     INPUT = "INPUT"
     OUTPUT = "OUTPUT"
-    NAME_FIELD = "NAME_FIELD"
     VALUE_FIELD = "VALUE_FIELD"
 
     def group(self):
