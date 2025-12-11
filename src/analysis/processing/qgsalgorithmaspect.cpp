5b7b1a5eee12a5071291f70603cf1e14df7e92af
Alexander Bruy <alexander.bruy@gmail.com>
2025-12-11 12:46:43 +0000

    expose output nodata and raster creation options in the native Aspect algorithm

--- src/analysis/processing/qgsalgorithmaspect.cpp
+++ src/analysis/processing/qgsalgorithmaspect.cpp
@@ -70,6 +70,15 @@ void QgsAspectAlgorithm::initAlgorithm( const QVariantMap & )
   addParameter( new QgsProcessingParameterRasterLayer( QStringLiteral( "INPUT" ), QObject::tr( "Elevation layer" ) ) );
   addParameter( new QgsProcessingParameterNumber( QStringLiteral( "Z_FACTOR" ), QObject::tr( "Z factor" ), Qgis::ProcessingNumberParameterType::Double, 1, false, 0 ) );
 
+  auto outputNodataParam = std::make_unique<QgsProcessingParameterNumber>( QStringLiteral( "NODATA" ), QObject::tr( "Output NoData value" ), Qgis::ProcessingNumberParameterType::Double, -9999.0 );
+  outputNodataParam->setFlags( outputNodataParam->flags() | Qgis::ProcessingParameterFlag::Advanced );
+  addParameter( outputNodataParam.release() );
+
+  auto creationOptsParam = std::make_unique<QgsProcessingParameterString>( QStringLiteral( "CREATION_OPTIONS" ), QObject::tr( "Creation options" ), QVariant(), false, true );
+  creationOptsParam->setMetadata( QVariantMap( { { QStringLiteral( "widget_wrapper" ), QVariantMap( { { QStringLiteral( "widget_type" ), QStringLiteral( "rasteroptions" ) } } ) } } ) );
+  creationOptsParam->setFlags( creationOptsParam->flags() | Qgis::ProcessingParameterFlag::Advanced );
+  addParameter( creationOptsParam.release() );
+
   addParameter( new QgsProcessingParameterRasterDestination( QStringLiteral( "OUTPUT" ), QObject::tr( "Aspect" ) ) );
 }
 
@@ -81,12 +90,19 @@ QVariantMap QgsAspectAlgorithm::processAlgorithm( const QVariantMap &parameters,
     throw QgsProcessingException( invalidRasterError( parameters, QStringLiteral( "INPUT" ) ) );
 
   const double zFactor = parameterAsDouble( parameters, QStringLiteral( "Z_FACTOR" ), context );
+  const QString creationOptions = parameterAsString( parameters, QStringLiteral( "CREATION_OPTIONS" ), context ).trimmed();
+  const double outputNodata = parameterAsDouble( parameters, QStringLiteral( "NODATA" ), context );
 
   const QString outputFile = parameterAsOutputLayer( parameters, QStringLiteral( "OUTPUT" ), context );
   const QString outputFormat = parameterAsOutputRasterFormat( parameters, QStringLiteral( "OUTPUT" ), context );
 
   QgsAspectFilter aspect( inputLayer->source(), outputFile, outputFormat );
   aspect.setZFactor( zFactor );
+  if ( !creationOptions.isEmpty() )
+  {
+    aspect.setCreationOptions( creationOptions.split( '|' ) );
+  }
+  aspect.setOutputNodataValue( outputNodata );
   aspect.processRaster( feedback );
 
   QVariantMap outputs;
