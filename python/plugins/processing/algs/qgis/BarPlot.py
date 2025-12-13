b1182457475220efbc3081078466c22bbdefbf91
Vishal <24092245@scale.iitrpr.ac.in>
2025-12-13 05:52:57 +0000

    Refactor BarPlot: Remove legacy vector tool dependency

--- python/plugins/processing/algs/qgis/BarPlot.py
+++ python/plugins/processing/algs/qgis/BarPlot.py
@@ -6,12 +6,12 @@
     Copyright            : (C) 2013 by Victor Olaya
     Email                : volayaf at gmail dot com
 ***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
+* *
+* This program is free software; you can redistribute it and/or modify  *
+* it under the terms of the GNU General Public License as published by  *
+* the Free Software Foundation; either version 2 of the License, or     *
+* (at your option) any later version.                                   *
+* *
 ***************************************************************************
 """
 
@@ -23,16 +23,13 @@ import warnings
 
 from qgis.core import (
     QgsFeatureRequest,
+    QgsProcessingException,
     QgsProcessingParameterFeatureSource,
     QgsProcessingParameterField,
-    QgsProcessingException,
     QgsProcessingParameterFileDestination,
     QgsProcessingParameterString,
 )
 from processing.algs.qgis.QgisAlgorithm import QgisAlgorithm
-from processing.tools import vector
-
-from qgis.PyQt.QtCore import QCoreApplication
 
 
 class BarPlot(QgisAlgorithm):
@@ -117,9 +114,8 @@ class BarPlot(QgisAlgorithm):
                 import plotly.graph_objs as go
         except ImportError:
             raise QgsProcessingException(
-                QCoreApplication.translate(
-                    "BarPlot",
-                    "This algorithm requires the Python “plotly” library. Please install this library and try again.",
+                self.tr(
+                    "This algorithm requires the Python “plotly” library. Please install this library and try again."
                 )
             )
 
@@ -149,19 +145,18 @@ class BarPlot(QgisAlgorithm):
 
         output = self.parameterAsFileOutput(parameters, self.OUTPUT, context)
 
-        values = vector.values(source, valuefieldname)
+        # Improved data extraction without 'vector' module
+        x_data = []
+        y_data = []
+        # Use NoGeometry flag for performance optimization
+        req = QgsFeatureRequest().setFlags(QgsFeatureRequest.Flag.NoGeometry)
+        for f in source.getFeatures(req):
+            name_val = f[namefieldname]
+            # mimic vector.convert_nulls behavior: replace None with "<NULL>"
+            x_data.append(name_val if name_val is not None else "<NULL>")
+            y_data.append(f[valuefieldname])
 
-        x_var = vector.convert_nulls(
-            [
-                i[namefieldname]
-                for i in source.getFeatures(
-                    QgsFeatureRequest().setFlags(QgsFeatureRequest.Flag.NoGeometry)
-                )
-            ],
-            "<NULL>",
-        )
-
-        data = [go.Bar(x=x_var, y=values[valuefieldname])]
+        data = [go.Bar(x=x_data, y=y_data)]
 
         fig = go.Figure(
             data=data,
