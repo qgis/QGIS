4365931c0241bd2eae903ccac104439491505b1a
Alexander Bruy <alexander.bruy@gmail.com>
2025-12-15 14:40:26 +0000

    iexpose output nodata and raster creation options in the native Ruggedness index algorithm

--- src/analysis/processing/qgsalgorithmruggedness.cpp
+++ src/analysis/processing/qgsalgorithmruggedness.cpp
@@ -72,6 +72,15 @@ void QgsRuggednessAlgorithm::initAlgorithm( const QVariantMap & )
   addParameter( new QgsProcessingParameterRasterLayer( QStringLiteral( "INPUT" ), QObject::tr( "Elevation layer" ) ) );
   addParameter( new QgsProcessingParameterNumber( QStringLiteral( "Z_FACTOR" ), QObject::tr( "Z factor" ), Qgis::ProcessingNumberParameterType::Double, 1, false, 0 ) );
 
+  auto outputNodataParam = std::make_unique<QgsProcessingParameterNumber>( QStringLiteral( "NODATA" ), QObject::tr( "Output NoData value" ), Qgis::ProcessingNumberParameterType::Double, -9999.0 );
+  outputNodataParam->setFlags( outputNodataParam->flags() | Qgis::ProcessingParameterFlag::Advanced );
+  addParameter( outputNodataParam.release() );
+
+  auto creationOptsParam = std::make_unique<QgsProcessingParameterString>( QStringLiteral( "CREATION_OPTIONS" ), QObject::tr( "Creation options" ), QVariant(), false, true );
+  creationOptsParam->setMetadata( QVariantMap( { { QStringLiteral( "widget_wrapper" ), QVariantMap( { { QStringLiteral( "widget_type" ), QStringLiteral( "rasteroptions" ) } } ) } } ) );
+  creationOptsParam->setFlags( creationOptsParam->flags() | Qgis::ProcessingParameterFlag::Advanced );
+  addParameter( creationOptsParam.release() );
+
   addParameter( new QgsProcessingParameterRasterDestination( QStringLiteral( "OUTPUT" ), QObject::tr( "Ruggedness" ) ) );
 }
 
@@ -83,12 +92,19 @@ QVariantMap QgsRuggednessAlgorithm::processAlgorithm( const QVariantMap &paramet
     throw QgsProcessingException( invalidRasterError( parameters, QStringLiteral( "INPUT" ) ) );
 
   const double zFactor = parameterAsDouble( parameters, QStringLiteral( "Z_FACTOR" ), context );
+  const QString creationOptions = parameterAsString( parameters, QStringLiteral( "CREATION_OPTIONS" ), context ).trimmed();
+  const double outputNodata = parameterAsDouble( parameters, QStringLiteral( "NODATA" ), context );
 
   const QString outputFile = parameterAsOutputLayer( parameters, QStringLiteral( "OUTPUT" ), context );
   const QString outputFormat = parameterAsOutputRasterFormat( parameters, QStringLiteral( "OUTPUT" ), context );
 
   QgsRuggednessFilter ruggedness( inputLayer->source(), outputFile, outputFormat );
   ruggedness.setZFactor( zFactor );
+  if ( !creationOptions.isEmpty() )
+  {
+    ruggedness.setCreationOptions( creationOptions.split( '|' ) );
+  }
+  ruggedness.setOutputNodataValue( outputNodata );
   ruggedness.processRaster( feedback );
 
   QVariantMap outputs;
