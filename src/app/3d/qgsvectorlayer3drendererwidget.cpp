977a51e4c28368cc3006f904aabda394e96df883
Jean Felder <jean.felder@oslandia.com>
2025-12-15 03:14:38 +0100

    qgssinglesymbol3drendererwidget: Inherit from qgspanelwidget
    This will be needed by `Qgs3DSymbolButton` in the next commit.

--- src/app/3d/qgsvectorlayer3drendererwidget.cpp
+++ src/app/3d/qgsvectorlayer3drendererwidget.cpp
@@ -34,7 +34,7 @@
 #include "moc_qgsvectorlayer3drendererwidget.cpp"
 
 QgsSingleSymbol3DRendererWidget::QgsSingleSymbol3DRendererWidget( QgsVectorLayer *layer, QWidget *parent )
-  : QWidget( parent )
+  : QgsPanelWidget( parent )
   , mLayer( layer )
 {
   // If layer is null, the widget cannot be created.
@@ -69,14 +69,19 @@ void QgsSingleSymbol3DRendererWidget::setLayer( QgsVectorLayer *layer )
   if ( r && r->type() == "vector"_L1 )
   {
     QgsVectorLayer3DRenderer *vectorRenderer = static_cast<QgsVectorLayer3DRenderer *>( r );
-    widgetSymbol->setSymbol( vectorRenderer->symbol(), mLayer );
+    setSymbol( vectorRenderer->symbol() );
   }
   else
   {
     const std::unique_ptr<QgsAbstract3DSymbol> sym( QgsApplication::symbol3DRegistry()->defaultSymbolForGeometryType( mLayer->geometryType() ) );
     sym->setDefaultPropertiesFromLayer( mLayer );
-    widgetSymbol->setSymbol( sym.get(), mLayer );
+    setSymbol( sym.get() );
   }
+}
+
+void QgsSingleSymbol3DRendererWidget::setSymbol( const QgsAbstract3DSymbol *symbol )
+{
+  widgetSymbol->setSymbol( symbol, mLayer );
 }
 
 std::unique_ptr<QgsAbstract3DSymbol> QgsSingleSymbol3DRendererWidget::symbol()
