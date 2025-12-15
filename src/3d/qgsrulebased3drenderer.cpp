dc0263d50aa14c5f9637be669f50c1b24550422a
Jean Felder <jean.felder@oslandia.com>
2025-12-16 00:39:05 +0100

    qgsrulebased3drenderer: Make isFilterOK public

--- src/3d/qgsrulebased3drenderer.cpp
+++ src/3d/qgsrulebased3drenderer.cpp
@@ -337,12 +337,12 @@ QgsRuleBased3DRenderer::Rule::RegisterResult QgsRuleBased3DRenderer::Rule::regis
 }
 
 
-bool QgsRuleBased3DRenderer::Rule::isFilterOK( const QgsFeature &f, Qgs3DRenderContext &context ) const
+bool QgsRuleBased3DRenderer::Rule::isFilterOK( const QgsFeature &feature, Qgs3DRenderContext &context ) const
 {
   if ( !mFilter || mElseRule )
     return true;
 
-  context.expressionContext().setFeature( f );
+  context.expressionContext().setFeature( feature );
   QVariant res = mFilter->evaluate( &context.expressionContext() );
   return res.toInt() != 0;
 }
