75682818e283e4142f0b1491ed4e8e12bc707ea7
uclaros <uclaros@gmail.com>
2025-12-28 23:29:13 +0200

    Defer QGIS 4 todos to QGIS 5

--- python/plugins/processing/algs/gdal/hillshade.py
+++ python/plugins/processing/algs/gdal/hillshade.py
@@ -132,7 +132,7 @@ class hillshade(GdalAlgorithm):
         )
 
         # backwards compatibility parameter
-        # TODO QGIS 4: remove parameter and related logic
+        # TODO QGIS 5: remove parameter and related logic
         options_param = QgsProcessingParameterString(
             self.OPTIONS,
             self.tr("Additional creation options"),
