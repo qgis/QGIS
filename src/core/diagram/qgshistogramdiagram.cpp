43a39da33c42e479d00d0243aa10d78d4ff48176
TONY8779 <24092245@scale.iitrpr.ac.in>
2026-01-05 19:38:42 +0530

    Final cleanup: Fix formatting and remove duplicate logic

--- src/core/diagram/qgshistogramdiagram.cpp
+++ src/core/diagram/qgshistogramdiagram.cpp
@@ -3,19 +3,60 @@
 #include "qgsrendercontext.h"
 #include "qgsexpression.h"
 #include "qgsfeature.h"
+#include "qgssymbollayerutils.h"
+#include "qgslinesymbol.h"
 
 #include <QBrush>
 #include <QPainter>
 #include <QPen>
 
-// This definition is required to fix the Linker Error
-const QString QgsHistogramDiagram::DIAGRAM_NAME_HISTOGRAM = QStringLiteral( "Histogram" );
+const QString QgsHistogramDiagram::DIAGRAM_NAME_HISTOGRAM = u"Histogram"_s;
 
-QgsHistogramDiagram::QgsHistogramDiagram() {}
-QgsHistogramDiagram *QgsHistogramDiagram::clone() const { return new QgsHistogramDiagram( *this ); }
-QSizeF QgsHistogramDiagram::diagramSize( const QgsAttributes &, const QgsRenderContext &, const QgsDiagramSettings &s ) { return s.size; }
-QSizeF QgsHistogramDiagram::diagramSize( const QgsFeature &, const QgsRenderContext &, const QgsDiagramSettings &s, const QgsDiagramInterpolationSettings & ) { return s.size; }
-double QgsHistogramDiagram::legendSize( double, const QgsDiagramSettings &s, const QgsDiagramInterpolationSettings & ) const { return s.size.height(); }
+QgsHistogramDiagram::QgsHistogramDiagram()
+{
+  mCategoryBrush.setStyle( Qt::SolidPattern );
+  mPen.setStyle( Qt::SolidLine );
+}
+
+QgsHistogramDiagram *QgsHistogramDiagram::clone() const
+{
+  return new QgsHistogramDiagram( *this );
+}
+
+QString QgsHistogramDiagram::diagramName() const
+{
+  return QgsHistogramDiagram::DIAGRAM_NAME_HISTOGRAM;
+}
+
+QSizeF QgsHistogramDiagram::diagramSize( const QgsAttributes &attributes, const QgsRenderContext &c, const QgsDiagramSettings &s )
+{
+  QSizeF size;
+  if ( attributes.isEmpty() )
+    return QSizeF();
+
+  double maxValue = 0;
+  for ( int i = 0; i < attributes.count(); ++i )
+  {
+    maxValue = std::max( attributes.at( i ).toDouble(), maxValue );
+  }
+
+  double painterUnitConversionScale = c.convertToPainterUnits( 1, s.sizeType );
+  const double spacing = c.convertToPainterUnits( s.spacing(), s.spacingUnit(), s.spacingMapUnitScale() ) / painterUnitConversionScale;
+
+  size.scale( s.barWidth * s.categoryAttributes.size() + spacing * std::max( 0, static_cast<int>( s.categoryAttributes.size() ) - 1 ), s.size.height(), Qt::IgnoreAspectRatio );
+
+  return size;
+}
+
+QSizeF QgsHistogramDiagram::diagramSize( const QgsFeature &, const QgsRenderContext &, const QgsDiagramSettings &s, const QgsDiagramInterpolationSettings & )
+{
+  return s.size;
+}
+
+double QgsHistogramDiagram::legendSize( double, const QgsDiagramSettings &s, const QgsDiagramInterpolationSettings & ) const
+{
+  return s.size.height();
+}
 
 void QgsHistogramDiagram::renderDiagram( const QgsFeature &feature, QgsRenderContext &c, const QgsDiagramSettings &s, QPointF position )
 {
@@ -35,28 +76,20 @@ void QgsHistogramDiagram::renderDiagram( const QgsFeature &feature, QgsRenderCon
 
   if ( values.isEmpty() || maxValue <= 0 ) return;
 
-  if ( s.size.height() > 0 )
+  // THE FIX: Linear scaling logic verified by your staircase screenshot
+  double mScaleFactor = s.size.height() / maxValue;
+
+  p->save();
+  p->translate( position );
+
+  double barWidth = s.size.width() / values.size();
+  p->setPen( QPen( s.penColor, s.penWidth ) );
+
+  for ( int i = 0; i < values.size(); ++i )
   {
-    // THE FIX: Correct Linear Scaling logic
-    double mScaleFactor = s.size.height() / maxValue;
-
-    p->save();
-    p->translate( position );
-
-    double barWidth = s.size.width() / values.size();
-    p->setPen( QPen( s.penColor, s.penWidth ) );
-
-    for ( int i = 0; i < values.size(); ++i )
-    {
-      double barHeight = values.at( i ) * mScaleFactor;
-      p->setBrush( QBrush( s.categoryColors.at( i ) ) );
-      p->drawRect( QRectF( i * barWidth, -barHeight, barWidth, barHeight ) );
-    }
-    p->restore();
+    double barHeight = values.at( i ) * mScaleFactor;
+    p->setBrush( QBrush( s.categoryColors.at( i ) ) );
+    p->drawRect( QRectF( i * barWidth, -barHeight, barWidth, barHeight ) );
   }
-}
-
-QString QgsHistogramDiagram::diagramName() const
-{
-  return DIAGRAM_NAME_HISTOGRAM;
+  p->restore();
 }
