cd77735ed86fa04595a9039525050cb7f84dc4d0
Jan Caha <jan.caha@outlook.com>
2026-01-06 21:06:56 +0100

    set project title if name needs to be set

--- src/providers/postgres/qgspostgresdataitemguiprovider.cpp
+++ src/providers/postgres/qgspostgresdataitemguiprovider.cpp
@@ -1141,6 +1141,8 @@ void QgsPostgresDataItemGuiProvider::saveCurrentProject( QgsPGSchemaItem *schema
   pgProjectUri.schemaName = schemaItem->name();
   pgProjectUri.projectName = project->title().isEmpty() ? project->baseName() : project->title();
 
+  const QString previousTitle = project->title();
+
   if ( pgProjectUri.projectName.isEmpty() )
   {
     bool ok;
@@ -1148,6 +1150,7 @@ void QgsPostgresDataItemGuiProvider::saveCurrentProject( QgsPGSchemaItem *schema
     if ( ok && !projectName.isEmpty() )
     {
       pgProjectUri.projectName = projectName;
+      project->setTitle( projectName );
     }
     else
     {
@@ -1173,7 +1176,7 @@ void QgsPostgresDataItemGuiProvider::saveCurrentProject( QgsPGSchemaItem *schema
     notify( tr( "Save Project" ), tr( "Project “%1” exist in the database. Overwriting it." ).arg( pgProjectUri.projectName ), context, Qgis::MessageLevel::Info );
   }
 
-  QString previousFileName = project->fileName();
+  const QString previousFileName = project->fileName();
   project->setFileName( projectUri );
 
   // write project to the database
@@ -1183,6 +1186,7 @@ void QgsPostgresDataItemGuiProvider::saveCurrentProject( QgsPGSchemaItem *schema
     notify( tr( "Save Project" ), tr( "Unable to save project “%1” to “%2”." ).arg( project->title(), schemaItem->name() ), context, Qgis::MessageLevel::Warning );
     conn->unref();
     project->setFileName( previousFileName );
+    project->setTitle( previousTitle );
     return;
   }
 
