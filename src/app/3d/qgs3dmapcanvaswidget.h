1c641c8ef6cd573864c3aa80da6178856d28d14c
Jean Felder <jean.felder@oslandia.com>
2026-01-07 17:03:55 +0100

    qgs3dmapcanvaswidget: Add measurea area tool
    The measure tool button is updated to be able to switch between
    measure distance and measure area.

--- src/app/3d/qgs3dmapcanvaswidget.h
+++ src/app/3d/qgs3dmapcanvaswidget.h
@@ -119,6 +119,7 @@ class APP_EXPORT Qgs3DMapCanvasWidget : public QWidget
     void toggleAnimations();
     void cameraControl();
     void identify();
+    void measureArea();
     void measureLine();
     void changePointCloudAttributeByPaintbrush();
     void changePointCloudAttributeByPolygon();
@@ -171,7 +172,8 @@ class APP_EXPORT Qgs3DMapCanvasWidget : public QWidget
     QLabel *mLabelNavigationSpeed = nullptr;
     QTimer *mLabelNavSpeedHideTimeout = nullptr;
     Qgs3DMapToolIdentify *mMapToolIdentify = nullptr;
-    Qgs3DMapToolMeasureLine *mMapToolMeasureLine = nullptr;
+    Qgs3DMapToolMeasure *mMapToolMeasureLine = nullptr;
+    Qgs3DMapToolMeasure *mMapToolMeasureArea = nullptr;
     Qgs3DMapToolPointCloudChangeAttribute *mMapToolChangeAttribute = nullptr;
     QgsGeometry mCrossSectionLine;
     QObjectUniquePtr<QgsRubberBand> mCrossSectionRubberBand;
