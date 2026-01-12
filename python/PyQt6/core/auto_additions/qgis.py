40705c93026e4677dacb709b2442c5af227b10e8
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-01-12 08:48:33 +0100

    fixup! feat(qgsgeometry): introduce geometry backend

--- python/PyQt6/core/auto_additions/qgis.py
+++ python/PyQt6/core/auto_additions/qgis.py
@@ -247,15 +247,15 @@ Qgis.LayerFilter.__doc__ = """Filter for layers
 
 .. versionadded:: 3.34.
 
-* ``RasterLayer``: 
-* ``NoGeometry``: 
-* ``PointLayer``: 
-* ``LineLayer``: 
-* ``PolygonLayer``: 
-* ``HasGeometry``: 
-* ``VectorLayer``: 
-* ``PluginLayer``: 
-* ``WritableLayer``: 
+* ``RasterLayer``:
+* ``NoGeometry``:
+* ``PointLayer``:
+* ``LineLayer``:
+* ``PolygonLayer``:
+* ``HasGeometry``:
+* ``VectorLayer``:
+* ``PluginLayer``:
+* ``WritableLayer``:
 * ``MeshLayer``: QgsMeshLayer
 
   .. versionadded:: 3.6
@@ -851,10 +851,10 @@ Qgis.EmbeddedScriptType.__doc__ = """Type of Python Embedded in projects
 
 .. versionadded:: 3.40
 
-* ``Macro``: 
-* ``ExpressionFunction``: 
-* ``Action``: 
-* ``FormInitCode``: 
+* ``Macro``:
+* ``ExpressionFunction``:
+* ``Action``:
+* ``FormInitCode``:
 
 """
 # --
@@ -867,9 +867,9 @@ Qgis.ProjectTrustStatus.__doc__ = """Project trust status
 
 .. versionadded:: 4.0
 
-* ``Undetermined``: 
-* ``Trusted``: 
-* ``Untrusted``: 
+* ``Undetermined``:
+* ``Trusted``:
+* ``Untrusted``:
 
 """
 # --
@@ -3229,8 +3229,8 @@ note Directly mapped from GDALRATTableType enum values.
 
 .. versionadded:: 3.30
 
-* ``Thematic``: 
-* ``Athematic``: 
+* ``Thematic``:
+* ``Athematic``:
 
 """
 # --
@@ -4067,7 +4067,7 @@ Qgis.GeometryBackend.QGIS.__doc__ = "Use internal implementation"
 Qgis.GeometryBackend.GEOS.__doc__ = "Use GEOS implementation"
 Qgis.GeometryBackend.__doc__ = """Geometry backend for :py:class:`QgsGeometry`.
 
-.. versionadded:: 3.46
+.. versionadded:: 4.0
 
 * ``QGIS``: Use internal implementation
 * ``GEOS``: Use GEOS implementation
@@ -8411,7 +8411,7 @@ Prior to QGIS 3.30 this was available as :py:class:`QgsMapLayerAction`.Target
 * ``Layer``: Action targets a complete layer
 * ``SingleFeature``: Action targets a single feature from a layer
 * ``MultipleFeatures``: Action targets multiple features from a layer
-* ``AllActions``: 
+* ``AllActions``:
 
 """
 # --
@@ -11253,9 +11253,9 @@ Qgis.ProviderStyleStorageCapability.__doc__ = """The StorageCapability enum repr
 
 .. versionadded:: 3.34
 
-* ``SaveToDatabase``: 
-* ``LoadFromDatabase``: 
-* ``DeleteFromDatabase``: 
+* ``SaveToDatabase``:
+* ``LoadFromDatabase``:
+* ``DeleteFromDatabase``:
 
 """
 # --
@@ -12458,10 +12458,10 @@ Qgis.ExtrusionFace.__doc__ = """Extrusion face types for the :py:class:`QgsTesse
 
 .. versionadded:: 4.0
 
-* ``NoFace``: 
-* ``Walls``: 
-* ``Roof``: 
-* ``Floor``: 
+* ``NoFace``:
+* ``Walls``:
+* ``Roof``:
+* ``Floor``:
 
 """
 # --
@@ -12476,8 +12476,8 @@ Qgis.TriangulationAlgorithm.__doc__ = """Triangulation algorithms.
 
 .. versionadded:: 4.0
 
-* ``ConstrainedDelaunay``: 
-* ``Earcut``: 
+* ``ConstrainedDelaunay``:
+* ``Earcut``:
 
 """
 # --
