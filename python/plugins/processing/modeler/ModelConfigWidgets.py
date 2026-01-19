7a5e527e082d26050e6e2d004628f39120fe9831
Nyall Dawson <nyall.dawson@gmail.com>
2026-01-19 16:56:32 +1000

    Ugly but necessary hack

--- python/plugins/processing/modeler/ModelConfigWidgets.py
+++ python/plugins/processing/modeler/ModelConfigWidgets.py
@@ -31,13 +31,22 @@ from processing.modeler.ModelerParameterDefinitionDialog import (
     ModelerParameterDefinitionDialog,
     ModelerParameterDefinitionWidget,
 )
+from processing.modeler.ModelerParametersDialog import (
+    ModelerParametersWidget,
+    ModelerParametersDialog,
+)
 
 
 class ModelConfigWidgetFactory(QgsProcessingModelConfigWidgetFactory):
 
     def supportsComponent(self, component):
         return isinstance(
-            component, (QgsProcessingModelParameter, QgsProcessingModelOutput)
+            component,
+            (
+                QgsProcessingModelParameter,
+                QgsProcessingModelOutput,
+                QgsProcessingModelChildAlgorithm,
+            ),
         )
 
     def createWidget(
@@ -138,6 +147,36 @@ class ModelConfigWidgetFactory(QgsProcessingModelConfigWidgetFactory):
                 )
 
             widget.widgetChanged.connect(on_widget_changed)
+            return widget
+        elif isinstance(component, QgsProcessingModelChildAlgorithm):
+            algorithm = component.algorithm()
+            context = widgetContext.processingContextGenerator().processingContext()
+
+            # this is unfortunately VERY ugly. The old Python processing GUI api
+            # had the ModelerParametersDialog class so enmeshed in the parameter
+            # GUI classes that there is no way to rip this out without breaking
+            # API. So we HAVE to do a gross hack here and make a dialog, which
+            # we never show, but which allows us to keep the old API intact...
+            fake_dialog = ModelerParametersDialog(
+                algorithm,
+                model,
+                algName=component.childId(),
+                configuration=component.configuration(),
+            )
+
+            widget = ModelerParametersWidget(
+                algorithm,
+                model,
+                algName=component.childId(),
+                configuration=component.configuration(),
+                dialog=fake_dialog,
+                context=context,
+            )
+            widget.setComments(component.comment().description())
+            widget.setCommentColor(component.comment().color())
+
+            # make sure fake dialog exists for lifetime of widget
+            widget.__fake_dialog = fake_dialog
             return widget
 
         return None
