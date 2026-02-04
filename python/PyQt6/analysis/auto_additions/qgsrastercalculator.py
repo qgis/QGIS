047aae7de386f31a278883fbe7fe1cc2e9c32fc4
Nyall Dawson <nyall.dawson@gmail.com>
2026-02-05 08:15:44 +1000

    Add since

--- python/PyQt6/analysis/auto_additions/qgsrastercalculator.py
+++ python/PyQt6/analysis/auto_additions/qgsrastercalculator.py
@@ -26,7 +26,7 @@ QgsRasterCalculator.CalculationError.is_monkey_patched = True
 QgsRasterCalculator.CalculationError.__doc__ = "Error occurred while performing calculation"
 QgsRasterCalculator.OpenCLKernelBuildError = QgsRasterCalculator.Result.OpenCLKernelBuildError
 QgsRasterCalculator.OpenCLKernelBuildError.is_monkey_patched = True
-QgsRasterCalculator.OpenCLKernelBuildError.__doc__ = "Error building OpenCL kernel"
+QgsRasterCalculator.OpenCLKernelBuildError.__doc__ = "Error building OpenCL kernel. \n.. versionadded:: 4.0"
 QgsRasterCalculator.Result.__doc__ = """Result of the calculation
 
 * ``Success``: Calculation successful
@@ -37,7 +37,10 @@ QgsRasterCalculator.Result.__doc__ = """Result of the calculation
 * ``MemoryError``: Error allocating memory for result
 * ``BandError``: Invalid band number for input
 * ``CalculationError``: Error occurred while performing calculation
-* ``OpenCLKernelBuildError``: Error building OpenCL kernel
+* ``OpenCLKernelBuildError``: Error building OpenCL kernel.
+
+  .. versionadded:: 4.0
+
 
 """
 # --
