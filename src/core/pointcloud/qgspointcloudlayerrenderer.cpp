2a6d79dd9aac2744b5f66e149258006f4cf8a5b0
uclaros <uclaros@gmail.com>
2026-02-11 14:17:07 +0200

    fix automatically formatted indentation

--- src/core/pointcloud/qgspointcloudlayerrenderer.cpp
+++ src/core/pointcloud/qgspointcloudlayerrenderer.cpp
@@ -643,10 +643,10 @@ int QgsPointCloudLayerRenderer::renderNodesSorted( const QVector<QgsPointCloudNo
     sortedByteArray.append( allByteArrays.mid( pair.first * recordSize, recordSize ) );
 
   std::unique_ptr<QgsPointCloudBlock> bigBlock { new QgsPointCloudBlock( pointCount,
-      blockAttributes,
-      sortedByteArray,
-      blockScale,
-      blockOffset ) };
+        blockAttributes,
+        sortedByteArray,
+        blockScale,
+        blockOffset ) };
 
   QgsVector3D contextScale = context.scale();
   QgsVector3D contextOffset = context.offset();
