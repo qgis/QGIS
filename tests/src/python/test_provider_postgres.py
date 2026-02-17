c4364eab878db68d451171216669b2c4221aea32
Nyall Dawson <nyall.dawson@gmail.com>
2026-02-17 14:55:01 +1000

    [postgres] Implement urisReferToSame

--- tests/src/python/test_provider_postgres.py
+++ tests/src/python/test_provider_postgres.py
@@ -3801,6 +3801,91 @@ class TestPyQgsPostgresProvider(QgisTestCase, ProviderTestCase):
             },
         )
 
+    def test_urisReferToSame(self):
+        """
+        Test provider metadata urisReferToSame
+        """
+        metadata = QgsProviderRegistry.instance().providerMetadata("postgres")
+
+        uri1_parts = {
+            "host": "MY_HOST",
+            "dbname": "MY_DB",
+            "port": "2222",
+            "service": "MY_SERVICE",
+            "schema": "schema1",
+            "table": "table1",
+        }
+        uri2_parts = {
+            "host": "MY_HOST",
+            "dbname": "MY_DB",
+            "port": "2222",
+            "service": "MY_SERVICE",
+            "schema": "schema2",
+            "table": "table2",
+        }
+
+        uri1 = metadata.encodeUri(uri1_parts)
+        uri2 = metadata.encodeUri(uri2_parts)
+
+        self.assertTrue(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Group)
+        )
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Object)
+        )
+
+        uri2_parts["host"] = "MY_HOST2"
+        uri2 = metadata.encodeUri(uri2_parts)
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+        uri2_parts["host"] = "MY_HOST"
+        uri2_parts["dbname"] = "MY_DB2"
+        uri2 = metadata.encodeUri(uri2_parts)
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+        uri2_parts["dbname"] = "MY_DB"
+        uri2_parts["port"] = "3333"
+        uri2 = metadata.encodeUri(uri2_parts)
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+
+        uri2_parts["port"] = "2222"
+        uri2_parts["service"] = "MY_SERVICE2"
+        uri2 = metadata.encodeUri(uri2_parts)
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+
+        uri2_parts["service"] = "MY_SERVICE"
+        uri2_parts["schema"] = "schema1"
+        uri2 = metadata.encodeUri(uri2_parts)
+        self.assertTrue(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+        self.assertTrue(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Group)
+        )
+        self.assertFalse(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Object)
+        )
+        uri2_parts["table"] = "table1"
+        uri2 = metadata.encodeUri(uri2_parts)
+        self.assertTrue(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Connection)
+        )
+        self.assertTrue(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Group)
+        )
+        self.assertTrue(
+            metadata.urisReferToSame(uri1, uri2, Qgis.SourceHierarchyLevel.Object)
+        )
+
     def testHasSpatialIndex(self):
         for layer_name in ("hspi_table", "hspi_materialized_view"):
             columns = {
