7ba875ece576638598a5552fa52fc98ecbd9f28d
Julien Cabieces <julien.cabieces@oslandia.com>
2026-02-19 14:52:34 +0100

    fix(GeoPdfExport): Fix mix of grouped and global layers

--- src/core/qgsabstractgeopdfexporter.cpp
+++ src/core/qgsabstractgeopdfexporter.cpp
@@ -513,7 +513,7 @@ QString QgsAbstractGeospatialPdfExporter::createCompositionXml( const QList<Comp
 
     if ( !component.mapLayerId.isEmpty() )
     {
-      createdLayerIds.insert( component.mapLayerId );
+      createdLayerIds.insert( id );
     }
   }
 
