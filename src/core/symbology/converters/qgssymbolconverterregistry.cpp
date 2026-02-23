9075434385e2f11a69819ed1be5f5e4438d22b48
Nyall Dawson <nyall.dawson@gmail.com>
2026-02-23 14:25:42 +1000

    Add symbol converter registry

---
+++ src/core/symbology/converters/qgssymbolconverterregistry.cpp
@@ -0,0 +1,68 @@
+/***************************************************************************
+                             qgssymbolconverterregistry.cpp
+                             -----------------
+    begin                : February 2026
+    copyright            : (C) 2026 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgssymbolconverterregistry.h"
+
+#include "qgssymbolconverter.h"
+#include "qgssymbolconverteresrirest.h"
+#include "qgssymbolconverterqml.h"
+
+QgsSymbolConverterRegistry::QgsSymbolConverterRegistry( QObject *parent )
+  : QObject( parent )
+{}
+
+QgsSymbolConverterRegistry::~QgsSymbolConverterRegistry()
+{
+  qDeleteAll( mConverters );
+}
+
+void QgsSymbolConverterRegistry::addDefaultConverters()
+{
+  addConverter( new QgsSymbolConverterQml() );
+  addConverter( new QgsSymbolConverterEsriRest() );
+}
+
+bool QgsSymbolConverterRegistry::addConverter( QgsAbstractSymbolConverter *converter )
+{
+  if ( !converter )
+    return false;
+
+  if ( mConverters.contains( converter->name() ) )
+    return false;
+
+  mConverters.insert( converter->name(), converter );
+  return true;
+}
+
+QgsAbstractSymbolConverter *QgsSymbolConverterRegistry::converter( const QString &name ) const
+{
+  return mConverters.value( name, nullptr );
+}
+
+bool QgsSymbolConverterRegistry::removeConverter( const QString &name )
+{
+  if ( !mConverters.contains( name ) )
+    return false;
+
+  delete mConverters.take( name );
+  return true;
+}
+
+QStringList QgsSymbolConverterRegistry::converterNames() const
+{
+  return mConverters.keys();
+}
