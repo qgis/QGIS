6c58af6a0eafc6acec6f9a30cbfb6d9bafafd6fa
Nyall Dawson <nyall.dawson@gmail.com>
2026-02-23 10:17:38 +1000

    [api] Create QgsAbstractSymbolConverter class
    An abstract base class for converting between QgsSymbol objects
    and QVariant representations.
    
    This interface provides a standard mechanism for serializing
    QGIS symbols to a generic data container (based on QVariant) and
    deserializing them back into native QgsSymbol objects.

---
+++ python/PyQt6/core/auto_additions/qgssymbolconverter.py
@@ -0,0 +1,6 @@
+# The following has been generated automatically from src/core/symbology/converters/qgssymbolconverter.h
+try:
+    QgsAbstractSymbolConverter.__abstract_methods__ = ['toVariant', 'createSymbol']
+    QgsAbstractSymbolConverter.__group__ = ['symbology', 'converters']
+except (NameError, AttributeError):
+    pass
---
+++ src/core/symbology/converters/qgssymbolconverter.h
@@ -0,0 +1,70 @@
+/***************************************************************************
+                             qgssymbolconverter.h
+                             -----------------
+    begin                : February 2026
+    copyright            : (C) 2026 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSSYMBOLCONVERTER_H
+#define QGSSYMBOLCONVERTER_H
+
+#include "qgis_core.h"
+
+class QgsSymbol;
+
+/**
+ * \class QgsAbstractSymbolConverter
+ * \ingroup core
+ * \brief An abstract base class for converting between QgsSymbol objects and QVariant representations.
+ *
+ * This interface provides a standard mechanism for serializing QGIS symbols to a generic
+ * data container (based on QVariant) and deserializing them back into native QgsSymbol objects.
+ *
+ * Concrete implementations of this class are intended to handle specific external styling
+ * formats, such as SLD (Styled Layer Descriptor) or Mapbox GL styles.
+ *
+ * \since QGIS 4.2
+*/
+class CORE_EXPORT QgsAbstractSymbolConverter
+{
+
+  public:
+    virtual ~QgsAbstractSymbolConverter();
+
+    /**
+     * Converts a \a symbol into a QVariant representation.
+     *
+     * The resulting QVariant may be a dictionary (QVariantMap), a JSON string,
+     * an XML representation of the symbol, or some binary QByteArray value,
+     * depending on the concrete implementation (e.g., SLD vs. Mapbox GL).
+     *
+     * Returns an invalid variant if the \a symbol could not be converted,
+     * or if serialization of symbols is not supported for this
+     * converter.
+     */
+    virtual QVariant toVariant( const QgsSymbol *symbol ) const = 0;
+
+    /**
+     * Creates a new QgsSymbol from a QVariant representation.
+     *
+     * \param variant The QVariant containing the serialized symbol data.
+     * \returns A new QgsSymbol instance representing the data in the variant.
+     * The caller takes ownership of the returned object. Returns NULLPTR if the
+     * variant cannot be converted or parsed, or if the converter does not support
+     * deserialization of symbols.
+     */
+    virtual std::unique_ptr< QgsSymbol > createSymbol( const QVariant &variant ) const = 0;
+
+};
+
+#endif //QGSSYMBOLCONVERTER_H
