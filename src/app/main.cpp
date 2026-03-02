9e359b1e58fc4e3bcfabe5848cb784f09aa7197f
Andrea Giudiceandrea <andreaerdna@libero.it>
2026-03-03 00:50:31 +0100

    [app] Use _wputenv_s on Windows
    in order to correctly handle non ASCII chars in custom environment variables' name and value

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1431,7 +1431,7 @@ int main( int argc, char *argv[] )
         if ( systemEnvVars.contains( envVarName ) && envVarApply == QLatin1String( "unset" ) )
         {
 #ifdef Q_OS_WIN
-          putenv( QString( "%1=" ).arg( envVarName ).toUtf8().constData() );
+          _wputenv_s( envVarName.toStdWString().c_str(), L"" );
 #else
           unsetenv( envVarName.toUtf8().constData() );
 #endif
@@ -1440,7 +1440,7 @@ int main( int argc, char *argv[] )
         {
 #ifdef Q_OS_WIN
           if ( envVarApply != "undefined" || !getenv( envVarName.toUtf8().constData() ) )
-            putenv( QString( "%1=%2" ).arg( envVarName ).arg( envVarValue ).toUtf8().constData() );
+            _wputenv_s( envVarName.toStdWString().c_str(), envVarValue.toStdWString().c_str() );
 #else
           setenv( envVarName.toUtf8().constData(), envVarValue.toUtf8().constData(), envVarApply == QLatin1String( "undefined" ) ? 0 : 1 );
 #endif
