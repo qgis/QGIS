2b6afda0efad6df52c618f711ca3f5777ca08e71
Nyall Dawson <nyall.dawson@gmail.com>
2026-03-10 13:26:06 +1000

    Fix see also name

--- src/core/symbology/converters/qgssymbolconverter.h
+++ src/core/symbology/converters/qgssymbolconverter.h
@@ -133,7 +133,7 @@ class CORE_EXPORT QgsAbstractSymbolConverter
      *
      * This should be an untranslated string identifying the converter.
      *
-     * \see displayName()
+     * \see formatName()
      */
     virtual QString name() const = 0;
 
