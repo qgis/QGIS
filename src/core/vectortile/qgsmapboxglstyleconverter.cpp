2ee80f5e7887713ce2843cd3deedb6020b7b21e3
Till Frankenbach <till.frankenbach@gmail.com>
2026-03-30 09:19:37 +0200

    Set unit for mapboxGl symbol spacing to context target unit

--- src/core/vectortile/qgsmapboxglstyleconverter.cpp
+++ src/core/vectortile/qgsmapboxglstyleconverter.cpp
@@ -2086,7 +2086,7 @@ void QgsMapBoxGlStyleConverter::parseSymbolLayer(
     // main checkbox in labeling GUI
     QgsLabelThinningSettings thinningSettings = labelSettings.thinningSettings();
     thinningSettings.setAllowDuplicateRemoval( true );
-    thinningSettings.setMinimumDistanceToDuplicateUnit( Qgis::RenderUnit::Pixels );
+    thinningSettings.setMinimumDistanceToDuplicateUnit( context.targetUnit() );
     labelSettings.setThinningSettings( thinningSettings );
 
     QgsProperty spacingProp;
