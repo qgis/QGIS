1f2e1005c101fa281d8fff565353cfbe3de63481
Jan Caha <jan.caha@outlook.com>
2026-03-31 15:26:52 +0200

    add test for new logic

--- tests/src/core/testqgsdistancearea.cpp
+++ tests/src/core/testqgsdistancearea.cpp
@@ -52,6 +52,7 @@ class TestQgsDistanceArea : public QObject
     void regression16820();
     void regression61299();
     void setCrsEllipsoidLogic();
+    void setCrsEllipsoidLogic();
 };
 
 void TestQgsDistanceArea::initTestCase()
