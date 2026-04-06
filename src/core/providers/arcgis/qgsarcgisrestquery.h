3a3247a6fb80c11ed4a1e3fef63a7a161740eafb
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-04-06 14:41:22 +0700

    [arcgis] Fix crash with the legend fetcher trying to access a deleted object

--- src/core/providers/arcgis/qgsarcgisrestquery.h
+++ src/core/providers/arcgis/qgsarcgisrestquery.h
@@ -20,6 +20,7 @@
 #include "qgshttpheaders.h"
 #include "qgsrectangle.h"
 
+#include <QPointer>
 #include <QString>
 #include <QVariantMap>
 
@@ -234,7 +235,7 @@ class CORE_EXPORT QgsArcGisAsyncQuery : public QObject
     void handleReply();
 
   private:
-    QNetworkReply *mReply = nullptr;
+    QPointer<QNetworkReply> mReply;
     QByteArray *mResult = nullptr;
 };
 
