6bde2ba00c42bbb096696439a542396d5ba8f12b
Mathieu Pellerin <nirvn.asia@gmail.com>
2026-04-06 11:45:03 +0700

    Adjust backport

--- src/providers/wms/qgswmsprovider.cpp
+++ src/providers/wms/qgswmsprovider.cpp
@@ -1853,7 +1853,7 @@ bool QgsWmsProvider::setupXyzCapabilities( const QString &uri, const QgsRectangl
   if ( parsedUri.hasParam( u"tilePixelRatio"_s ) )
     tilePixelRatio = parsedUri.param( u"tilePixelRatio"_s ).toDouble();
 
-  if ( tilePixelRatio == 0 && parsedUri.param( u"url"_s ).contains( "openstreetmap"_L1, Qt::CaseInsensitive ) )
+  if ( tilePixelRatio == 0 && parsedUri.param( u"url"_s ).contains( "openstreetmap.org"_L1, Qt::CaseInsensitive ) )
   {
     // pixel ratio of XYZ tiles served on openstreetmap.org known, set accordingly to insure
     // tile downloads are not skyrocketing on high screen/output DPI.
