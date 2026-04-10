194e455f740258f4a2508183c4cc1799d7deb2e2
Jean Felder <jean.felder@oslandia.com>
2026-04-10 09:09:33 +0200

    qgs3dmeasuredialog: Set a parent to ensure visibility on wayland
    On wayland, the measure dialog is not visible because it does not have
    any parent. Fix the issue by using `Qgs3DMapCanvasWidget` as parent.

--- src/app/3d/qgs3dmaptoolmeasureline.cpp
+++ src/app/3d/qgs3dmaptoolmeasureline.cpp
@@ -17,7 +17,7 @@
 
 #include <memory>
 
-#include "qgs3dmapcanvas.h"
+#include "qgs3dmapcanvaswidget.h"
 #include "qgs3dmapscene.h"
 #include "qgs3dmeasuredialog.h"
 #include "qgs3dutils.h"
@@ -39,11 +39,11 @@
 
 using namespace Qt::StringLiterals;
 
-Qgs3DMapToolMeasureLine::Qgs3DMapToolMeasureLine( Qgs3DMapCanvas *canvas )
-  : Qgs3DMapTool( canvas )
+Qgs3DMapToolMeasureLine::Qgs3DMapToolMeasureLine( Qgs3DMapCanvasWidget *canvasWidget )
+  : Qgs3DMapTool( canvasWidget->mapCanvas3D() )
 {
   // Dialog
-  mDialog = std::make_unique<Qgs3DMeasureDialog>( this );
+  mDialog.reset( new Qgs3DMeasureDialog( this, canvasWidget ) );
   mDialog->setWindowFlags( mDialog->windowFlags() | Qt::Tool );
   mDialog->restorePosition();
 }
