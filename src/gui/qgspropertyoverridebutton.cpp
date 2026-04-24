46cb946405b7e6435fc475aab6e4d864ad5c3e57
Julien Cabieces <julien.cabieces@oslandia.com>
2026-04-24 10:43:12 +0200

    fix(PropertyButton): Fire modification signals on setToProperty

--- src/gui/qgspropertyoverridebutton.cpp
+++ src/gui/qgspropertyoverridebutton.cpp
@@ -309,10 +309,15 @@ void QgsPropertyOverrideButton::setToProperty( const QgsProperty &property )
     mFieldName.clear();
     mExpressionString.clear();
   }
+  const bool wasActive = mProperty.isActive();
   mProperty = property;
-  setActive( mProperty && mProperty.isActive() );
   updateSiblingWidgets( isActive() );
   updateGui();
+
+  if ( mProperty.isActive() != wasActive )
+    emit activated( mProperty.isActive() );
+
+  emit changed();
 }
 
 ///@cond PRIVATE
