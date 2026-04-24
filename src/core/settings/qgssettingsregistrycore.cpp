9c62cb3fc8f3d36d7fe66c8f645874b8b7e1c2d4
Matthias Kuhn <matthias@opengis.ch>
2026-04-24 12:40:04 +0200

    Fix building without opencl

--- src/core/settings/qgssettingsregistrycore.cpp
+++ src/core/settings/qgssettingsregistrycore.cpp
@@ -33,7 +33,9 @@
 #include "qgslocator.h"
 #include "qgsnetworkaccessmanager.h"
 #include "qgsogrproviderutils.h"
+#ifdef HAVE_OPENCL
 #include "qgsopenclutils.h"
+#endif
 #include "qgsowsconnection.h"
 #include "qgsprocessing.h"
 #include "qgsrasterlayer.h"
@@ -289,8 +291,10 @@ void QgsSettingsRegistryCore::migrateOldSettings()
   QgsImageCache::settingsMaxImageCacheSize->copyValueFromKey( u"qgis/maxImageCacheSize"_s, true );
   QgsSymbolLegendNode::settingsLegendSymbolMinimumSize->copyValueFromKey( u"qgis/legendsymbolMinimumSize"_s, true );
   QgsSymbolLegendNode::settingsLegendSymbolMaximumSize->copyValueFromKey( u"qgis/legendsymbolMaximumSize"_s, true );
+  #ifdef HAVE_OPENCL
   QgsOpenClUtils::settingsOpenClEnabled->copyValueFromKey( u"core/OpenClEnabled"_s, true );
   QgsOpenClUtils::settingsOpenClDefaultDevice->copyValueFromKey( u"core/OpenClDefaultDevice"_s, true );
+  #endif
   QgsOgrProviderUtils::settingsWalForSqlite3->copyValueFromKey( u"qgis/walForSqlite3"_s, true );
 
   QgsLayoutGridSettings::settingsGridStyle->copyValueFromKey( u"gui/LayoutDesigner/gridStyle"_s, true );
