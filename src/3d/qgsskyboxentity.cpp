b0166479af5360d4c5afd915b0374d1b22255223
Nyall Dawson <nyall.dawson@gmail.com>
2026-04-25 13:38:16 +1000

    Set remaining textures to SRGB format

--- src/3d/qgsskyboxentity.cpp
+++ src/3d/qgsskyboxentity.cpp
@@ -155,6 +155,7 @@ void QgsCubeFacesSkyboxEntity::reloadTexture()
   newCubeMap->setMinificationFilter( Qt3DRender::QTextureCubeMap::Linear );
   newCubeMap->setGenerateMipMaps( false );
   newCubeMap->setWrapMode( Qt3DRender::QTextureWrapMode( Qt3DRender::QTextureWrapMode::Repeat ) );
+  newCubeMap->setFormat( Qt3DRender::QAbstractTexture::SRGB8_Alpha8 );
 
   // all faces must have the SAME size, so take the maximum size from the input images
   int maxSize = 0;
