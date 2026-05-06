6d08bbfd4a72392eaf59b606f4472048b37856d8
viperminiq <viperminiq@gmail.com>
2026-05-06 22:37:58 +0200

    sipify

---
+++ python/PyQt6/3d/auto_additions/qgsfixedgradientbackgroundsettings.py
@@ -0,0 +1,5 @@
+# The following has been generated automatically from src/3d/qgsfixedgradientbackgroundsettings.h
+try:
+    QgsFixedGradientBackgroundSettings.__overridden_methods__ = ['type', 'clone', 'readXml', 'writeXml']
+except (NameError, AttributeError):
+    pass
