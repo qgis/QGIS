qgis/QGIS

commit e9a36df3d7bd6fee7459b44c1caf84889b50c515

qgssfcgalgeometry: Fix comparison operators in primitive case
`mPrimTransform` needs to be taken into account.

src/core/geometry/qgssfcgalgeometry.cpp
@@ -254,6 +254,7 @@ bool QgsSfcgalGeometry::operator==( const QgsSfcgalGeometry &other ) const
   throw QgsNotSupportedException( QObject::tr( "This operation requires a QGIS build based on SFCGAL 2.1 or later" ) );
 #else
   QString errorMsg;
+  sfcgal::errorHandler()->clearText( &errorMsg );
   bool out;
 
 #if SFCGAL_VERSION_NUM >= SFCGAL_MAKE_VERSION( 2, 3, 0 )
@@ -262,6 +263,10 @@ bool QgsSfcgalGeometry::operator==( const QgsSfcgalGeometry &other ) const
 
   if ( mIsPrimitive )
   {
+    if ( mPrimTransform != other.mPrimTransform )
+    {
+      return false;
+    }
     out = QgsSfcgalEngine::primitiveIsEqual( mSfcgalPrim.get(), other.mSfcgalPrim.get(), -1.0, &errorMsg );
   }
   else
@@ -292,6 +297,10 @@ bool QgsSfcgalGeometry::fuzzyEqual( const QgsSfcgalGeometry &other, double epsil
 
   if ( mIsPrimitive )
   {
+    if ( !mPrimTransform.fuzzyEqual( other.mPrimTransform ) )
+    {
+      return false;
+    }
     out = QgsSfcgalEngine::primitiveIsEqual( mSfcgalPrim.get(), other.mSfcgalPrim.get(), epsilon, &errorMsg );
   }
   else

tests/src/core/geometry/testqgssfcgal.cpp
@@ -470,6 +470,22 @@ void TestQgsSfcgal::isEqual()
   // should be accepted
   QVERIFY2( geomA.fuzzyEqual( *( geomB.get() ), 0.05 ), "Should be equals" );
 #endif
+
+// primitives
+#if SFCGAL_VERSION_NUM >= SFCGAL_MAKE_VERSION( 2, 3, 0 )
+  std::unique_ptr<QgsSfcgalGeometry> cube = QgsSfcgalGeometry::createCube( 5 );
+  QVERIFY2( geomA != *cube.get(), "primitive and geoms should not be equals" );
+
+  std::unique_ptr<QgsSfcgalGeometry> cube2 = cube->clone();
+  QVERIFY2( *cube2.get() == *cube.get(), "cubes should be equals" );
+
+  std::unique_ptr<QgsSfcgalGeometry> cubeTranslate = cube->translate( QgsVector3D( 1, 0, 0 ) );
+  QVERIFY2( *cubeTranslate.get() != *cube.get(), "cubes should not be equals" );
+
+  std::unique_ptr<QgsSfcgalGeometry> cubeSmallTranslate = cube->translate( QgsVector3D( 0.01, 0.01, 0.01 ) );
+  QVERIFY2( *cubeSmallTranslate.get() != *cube.get(), "cubes should not be equals" );
+  QVERIFY2( cubeSmallTranslate->fuzzyEqual( *( cube.get() ), 0.01 ), "Cubes Should be fuzzy equals" );
+#endif
 }
 
 void TestQgsSfcgal::boundary()

commit c4343b43c5685b14e75c07fc2530184d1e3a0265

qgssfcgalgeometry: Fix fuzzyequal in primitive comparison
Add misssing epsilon in transform comparison

Fixes: e9a36df3d7bd6fee7459b44c1caf84889b50c515

src/core/geometry/qgssfcgalgeometry.cpp
@@ -297,7 +297,7 @@ bool QgsSfcgalGeometry::fuzzyEqual( const QgsSfcgalGeometry &other, double epsil
 
   if ( mIsPrimitive )
   {
-    if ( !mPrimTransform.fuzzyEqual( other.mPrimTransform ) )
+    if ( !mPrimTransform.fuzzyEqual( other.mPrimTransform, epsilon ) )
     {
       return false;
     }