88001c726049fdb2e221101ecf80f5b1e54fa6c2
Nyall Dawson <nyall.dawson@gmail.com>
2026-05-20 15:30:05 +1000

    Support texture offset, data defined texture transform for phong texture material

--- src/3d/materials/qgsphongtexturedmaterial.cpp
+++ src/3d/materials/qgsphongtexturedmaterial.cpp
@@ -35,9 +35,15 @@ QgsPhongTexturedMaterial::QgsPhongTexturedMaterial( QNode *parent )
   , mDiffuseTextureParameter( new Qt3DRender::QParameter( u"diffuseTexture"_s, QVariant() ) )
   , mDiffuseTextureScaleParameter( new Qt3DRender::QParameter( u"texCoordScale"_s, 1.0f ) )
   , mDiffuseTextureRotationParameter( new Qt3DRender::QParameter( u"texCoordRotation"_s, 0.0f ) )
+  , mDiffuseTextureOffsetParameter( new Qt3DRender::QParameter( u"texCoordOffset"_s, QVariant::fromValue( QVector2D( 0, 0 ) ), this ) )
   , mSpecularParameter( new Qt3DRender::QParameter( u"specularColor"_s, QVariant() ) )
   , mShininessParameter( new Qt3DRender::QParameter( u"shininess"_s, 150.0f ) )
   , mOpacityParameter( new Qt3DRender::QParameter( u"opacity"_s, 1.0f ) )
+  , mEffect( new Qt3DRender::QEffect( this ) )
+  , mGL3Technique( new Qt3DRender::QTechnique( this ) )
+  , mGL3RenderPass( new Qt3DRender::QRenderPass( this ) )
+  , mGL3Shader( new Qt3DRender::QShaderProgram( this ) )
+  , mFilterKey( new Qt3DRender::QFilterKey( this ) )
 {
   setAmbient( QColor::fromRgbF( 0.05f, 0.05f, 0.05f, 1.0f ) );
   setSpecular( QColor::fromRgbF( 0.01f, 0.01f, 0.01f, 1.0f ) );
@@ -49,41 +55,45 @@ QgsPhongTexturedMaterial::~QgsPhongTexturedMaterial() = default;
 
 void QgsPhongTexturedMaterial::init()
 {
-  Qt3DRender::QEffect *effect = new Qt3DRender::QEffect();
-  effect->addParameter( mAmbientParameter );
-  effect->addParameter( mDiffuseTextureParameter );
-  effect->addParameter( mDiffuseTextureScaleParameter );
-  effect->addParameter( mDiffuseTextureRotationParameter );
-  effect->addParameter( mSpecularParameter );
-  effect->addParameter( mShininessParameter );
-  effect->addParameter( mOpacityParameter );
+  updateVertexShader();
 
-  Qt3DRender::QShaderProgram *gL3Shader = new Qt3DRender::QShaderProgram();
+  mGL3Technique->graphicsApiFilter()->setApi( Qt3DRender::QGraphicsApiFilter::OpenGL );
+  mGL3Technique->graphicsApiFilter()->setMajorVersion( 3 );
+  mGL3Technique->graphicsApiFilter()->setMinorVersion( 1 );
+  mGL3Technique->graphicsApiFilter()->setProfile( Qt3DRender::QGraphicsApiFilter::CoreProfile );
 
-  const QByteArray vertexShaderCode = Qt3DRender::QShaderProgram::loadSource( QUrl( u"qrc:/shaders/default.vert"_s ) );
-  const QByteArray finalVertexShaderCode = Qgs3DUtils::addDefinesToShaderCode( vertexShaderCode, QStringList( { "TEXTURE_ROTATION" } ) );
-  gL3Shader->setVertexShaderCode( finalVertexShaderCode );
+  mFilterKey->setParent( this );
+  mFilterKey->setName( u"renderingStyle"_s );
+  mFilterKey->setValue( u"forward"_s );
 
-  gL3Shader->setFragmentShaderCode( Qt3DRender::QShaderProgram::loadSource( QUrl( u"qrc:/shaders/diffuseSpecular.frag"_s ) ) );
+  mGL3Technique->addFilterKey( mFilterKey );
+  mGL3RenderPass->setShaderProgram( mGL3Shader );
+  mGL3Technique->addRenderPass( mGL3RenderPass );
+  mEffect->addTechnique( mGL3Technique );
 
-  Qt3DRender::QTechnique *gL3Technique = new Qt3DRender::QTechnique();
-  gL3Technique->graphicsApiFilter()->setApi( Qt3DRender::QGraphicsApiFilter::OpenGL );
-  gL3Technique->graphicsApiFilter()->setMajorVersion( 3 );
-  gL3Technique->graphicsApiFilter()->setMinorVersion( 1 );
-  gL3Technique->graphicsApiFilter()->setProfile( Qt3DRender::QGraphicsApiFilter::CoreProfile );
+  mGL3Shader->setFragmentShaderCode( Qt3DRender::QShaderProgram::loadSource( QUrl( u"qrc:/shaders/diffuseSpecular.frag"_s ) ) );
 
-  Qt3DRender::QFilterKey *filterKey = new Qt3DRender::QFilterKey( this );
-  filterKey->setName( u"renderingStyle"_s );
-  filterKey->setValue( u"forward"_s );
+  mEffect->addParameter( mAmbientParameter );
+  mEffect->addParameter( mDiffuseTextureParameter );
+  mEffect->addParameter( mDiffuseTextureScaleParameter );
+  mEffect->addParameter( mDiffuseTextureRotationParameter );
+  mEffect->addParameter( mDiffuseTextureOffsetParameter );
+  mEffect->addParameter( mSpecularParameter );
+  mEffect->addParameter( mShininessParameter );
+  mEffect->addParameter( mOpacityParameter );
 
-  gL3Technique->addFilterKey( filterKey );
+  setEffect( mEffect );
+}
 
-  Qt3DRender::QRenderPass *gL3RenderPass = new Qt3DRender::QRenderPass();
-  gL3RenderPass->setShaderProgram( gL3Shader );
-  gL3Technique->addRenderPass( gL3RenderPass );
-  effect->addTechnique( gL3Technique );
+void QgsPhongTexturedMaterial::updateVertexShader()
+{
+  QByteArray vertexCode = Qt3DRender::QShaderProgram::loadSource( QUrl( u"qrc:/shaders/default.vert"_s ) );
+  QStringList defines { u"TEXTURE_ROTATION"_s, u"TEXTURE_OFFSET"_s };
+  if ( mDataDefinedTextureTransformEnabled )
+    defines << u"DATA_DEFINED_TEXTURE_TRANSFORMS"_s;
 
-  setEffect( effect );
+  vertexCode = Qgs3DUtils::addDefinesToShaderCode( vertexCode, defines );
+  mGL3Shader->setVertexShaderCode( vertexCode );
 }
 
 void QgsPhongTexturedMaterial::setAmbient( const QColor &ambient )
@@ -106,6 +116,11 @@ void QgsPhongTexturedMaterial::setDiffuseTextureRotation( float textureRotation 
   mDiffuseTextureRotationParameter->setValue( textureRotation );
 }
 
+void QgsPhongTexturedMaterial::setDiffuseTextureOffset( float textureOffsetX, float textureOffsetY )
+{
+  mDiffuseTextureOffsetParameter->setValue( QVariant::fromValue( QVector2D( textureOffsetX, textureOffsetY ) ) );
+}
+
 void QgsPhongTexturedMaterial::setSpecular( const QColor &specular )
 {
   mSpecularParameter->setValue( Qgs3DUtils::srgbToLinear( specular ) );
@@ -119,6 +134,15 @@ void QgsPhongTexturedMaterial::setShininess( float shininess )
 void QgsPhongTexturedMaterial::setOpacity( float opacity )
 {
   mOpacityParameter->setValue( opacity );
+}
+
+void QgsPhongTexturedMaterial::setDataDefinedTextureTransformEnabled( bool enabled )
+{
+  if ( enabled == mDataDefinedTextureTransformEnabled )
+    return;
+
+  mDataDefinedTextureTransformEnabled = enabled;
+  updateVertexShader();
 }
 
 ///@endcond PRIVATE
