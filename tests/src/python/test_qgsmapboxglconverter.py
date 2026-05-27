b9ef4058b923724a7a997fb2289df1e2b81398d3
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-05-27 15:47:45 +0200

    assert value scaling alongside unit in symbol-spacing tests

--- tests/src/python/test_qgsmapboxglconverter.py
+++ tests/src/python/test_qgsmapboxglconverter.py
@@ -2961,6 +2961,7 @@ class TestQgsMapBoxGlStyleConverter(QgisTestCase):
         """Test symbol-spacing with a simple numeric value"""
         context = QgsMapBoxGlStyleConversionContext()
         context.setTargetUnit(Qgis.RenderUnit.Percentage)
+        context.setPixelSizeConversionFactor(2.0)
         style = {
             "layout": {
                 "text-field": "{name}",
@@ -2982,7 +2983,7 @@ class TestQgsMapBoxGlStyleConverter(QgisTestCase):
         dd = ls.dataDefinedProperties()
         prop = dd.property(QgsPalLayerSettings.Property.RemoveDuplicateLabelDistance)
         self.assertTrue(prop.isActive())
-        self.assertEqual(prop.asExpression(), "250")
+        self.assertEqual(prop.asExpression(), "500")
         self.assertEqual(
             ls.thinningSettings().minimumDistanceToDuplicateUnit(),
             Qgis.RenderUnit.Percentage,
@@ -2992,6 +2993,7 @@ class TestQgsMapBoxGlStyleConverter(QgisTestCase):
         """Test symbol-spacing with interpolate stops"""
         context = QgsMapBoxGlStyleConversionContext()
         context.setTargetUnit(Qgis.RenderUnit.Pixels)
+        context.setPixelSizeConversionFactor(2.0)
         style = {
             "layout": {
                 "text-field": "{name}",
@@ -3015,7 +3017,7 @@ class TestQgsMapBoxGlStyleConverter(QgisTestCase):
         self.assertTrue(prop.isActive())
         self.assertEqual(
             prop.asExpression(),
-            "CASE  WHEN @vector_tile_zoom >= 14 THEN (800)  WHEN @vector_tile_zoom >= 10 THEN (600) ELSE (300) END",
+            "CASE  WHEN @vector_tile_zoom >= 14 THEN (1600)  WHEN @vector_tile_zoom >= 10 THEN (1200) ELSE (600) END",
         )
         self.assertEqual(
             ls.thinningSettings().minimumDistanceToDuplicateUnit(),
@@ -3026,6 +3028,7 @@ class TestQgsMapBoxGlStyleConverter(QgisTestCase):
         """Test symbol-spacing with a QVariantMap stops definition"""
         context = QgsMapBoxGlStyleConversionContext()
         context.setTargetUnit(Qgis.RenderUnit.MapUnits)
+        context.setPixelSizeConversionFactor(2.0)
         style = {
             "layout": {
                 "text-field": "{name}",
@@ -3053,7 +3056,7 @@ class TestQgsMapBoxGlStyleConverter(QgisTestCase):
         self.assertTrue(prop.isActive())
         self.assertEqual(
             prop.asExpression(),
-            "scale_linear(@vector_tile_zoom,2,6,0.2,0)",
+            "(scale_linear(@vector_tile_zoom,2,6,0.2,0)) * 2",
         )
         self.assertEqual(
             ls.thinningSettings().minimumDistanceToDuplicateUnit(),
