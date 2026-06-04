e58cd0a46ea3b60994a5e2fc5c731e9298cf6766
Germán Carrillo <gcarrillo@linuxmail.org>
2026-06-04 10:01:18 -0500

    [geometry] Move equals/fuzzyDistanceEqual/fuzzyEqual/fuzzyHelper methods to QgsSimpleCurve

--- python/PyQt6/core/auto_additions/qgssimplecurve.py
+++ python/PyQt6/core/auto_additions/qgssimplecurve.py
@@ -1,6 +1,6 @@
 # The following has been generated automatically from src/core/geometry/qgssimplecurve.h
 try:
-    QgsSimpleCurve.__overridden_methods__ = ['xAt', 'yAt', 'zAt', 'mAt', 'wkbSize', 'asWkb', 'asWkt', 'fromWkb', 'fromWkt', 'clear', 'numPoints', 'nCoordinates', 'dimension', 'addMValue', 'addZValue', 'dropMValue', 'dropZValue', 'startPoint', 'endPoint', 'moveVertex', 'reversed', 'compareToSameClass']
+    QgsSimpleCurve.__overridden_methods__ = ['fuzzyEqual', 'fuzzyDistanceEqual', 'equals', 'xAt', 'yAt', 'zAt', 'mAt', 'wkbSize', 'asWkb', 'asWkt', 'fromWkb', 'fromWkt', 'clear', 'numPoints', 'nCoordinates', 'dimension', 'addMValue', 'addZValue', 'dropMValue', 'dropZValue', 'startPoint', 'endPoint', 'moveVertex', 'reversed', 'compareToSameClass']
     QgsSimpleCurve.__group__ = ['geometry']
 except (NameError, AttributeError):
     pass
