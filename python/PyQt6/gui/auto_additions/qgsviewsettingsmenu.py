4ec03b297a4141968ba52be511fca6f0fbce4690
Till Frankenbach <till.frankenbach@gmail.com>
2026-06-04 15:22:13 +0200

    Add version information to QgsViewSettingsMenu docs

---
+++ python/PyQt6/gui/auto_additions/qgsviewsettingsmenu.py
@@ -0,0 +1,5 @@
+# The following has been generated automatically from src/gui/qgsviewsettingsmenu.h
+try:
+    QgsViewSettingsMenu.__overridden_methods__ = ['mouseReleaseEvent']
+except (NameError, AttributeError):
+    pass
