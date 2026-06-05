11b7ade53c1638b09763ab14e3f2d9c70bfaab75
Till Frankenbach <till.frankenbach@gmail.com>
2026-06-05 09:07:18 +0200

    Implement key press event handling in QgsViewSettingsMenu

--- python/PyQt6/gui/auto_additions/qgsviewsettingsmenu.py
+++ python/PyQt6/gui/auto_additions/qgsviewsettingsmenu.py
@@ -1,5 +1,5 @@
 # The following has been generated automatically from src/gui/qgsviewsettingsmenu.h
 try:
-    QgsViewSettingsMenu.__overridden_methods__ = ['mouseReleaseEvent']
+    QgsViewSettingsMenu.__overridden_methods__ = ['mouseReleaseEvent', 'keyPressEvent']
 except (NameError, AttributeError):
     pass
