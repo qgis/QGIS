81e72087c85f3f20b5c3c6b05c21993a352e6eaf
Nyall Dawson <nyall.dawson@gmail.com>
2026-06-07 17:46:36 +1000

    Test tolerance

--- tests/src/python/test_qgsfonttextureatlasgenerator.py
+++ tests/src/python/test_qgsfonttextureatlasgenerator.py
@@ -169,12 +169,13 @@ class TestQgsFontTextureAtlasGenerator(QgisTestCase):
             self.assertEqual(atlas.atlasSize(), QSize(43, 48))
             self.assertEqual(atlas.rect("Y"), QRect(0, 0, 25, 24))
             self.assertEqual(atlas.rect("H"), QRect(0, 24, 23, 24))
+            self.assertEqual(atlas.rect("i"), QRect(25, 0, 9, 24))
         else:
             self.assertEqual(atlas.atlasSize(), QSize(48, 49))
             self.assertEqual(atlas.rect("Y"), QRect(0, 0, 25, 24))
             self.assertEqual(atlas.rect("H"), QRect(25, 0, 23, 24))
+            self.assertEqual(atlas.rect("i"), QRect(20, 24, 9, 25))
 
-        self.assertEqual(atlas.rect("i"), QRect(25, 0, 9, 24))
         self.assertEqual(atlas.rect("h"), QRect(23, 24, 20, 24))
         self.assertEqual(atlas.pixelOffsetForGrapheme("HY", 0), QPoint(0, 0))
         self.assertEqual(atlas.textureRectForGrapheme("HY", 0), QRect(0, 24, 23, 24))
