32123ae2de12fb49fcf9d55de6aabf997ff0b96a
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-06-08 15:17:23 +0200

    fixup! add dem height cache

--- src/3d/qgs3dutils.cpp
+++ src/3d/qgs3dutils.cpp
@@ -465,6 +465,7 @@ void Qgs3DUtils::clampAltitudes( QgsLineString *lineString, Qgis::AltitudeClampi
 
     const float z = ( terrainZ + geomZ ) * ( context.terrainSettings() ? static_cast<float>( context.terrainSettings()->verticalScale() ) : 1 ) + offset;
     lineString->setZAt( i, z );
+    QgsDebugMsgLevel( u"x:%1, y:%2, z:%3, h:%4c"_s.arg( lineString->xAt( i ) ).arg( lineString->yAt( i ) ).arg( lineString->zAt( i ) ).arg( terrainZ ), 2 );
   }
 }
 
@@ -575,7 +576,7 @@ void Qgs3DUtils::extractPointPositions(
       static_cast<float>( h - chunkOrigin.z() + translation.z() )
     ) );
     // clang-format on
-    QgsDebugMsgLevel( u"%1 %2 %3"_s.arg( positions.last().x() ).arg( positions.last().y() ).arg( positions.last().z() ), 2 );
+    QgsDebugMsgLevel( u"x:%1, y:%2 z:%3 (h:%4"_s.arg( positions.last().x() ).arg( positions.last().y() ).arg( positions.last().z(), h ), 2 );
   }
 }
 
