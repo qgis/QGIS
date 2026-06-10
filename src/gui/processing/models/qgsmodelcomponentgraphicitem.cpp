301044a3b2797b855398d9902127b460b70eee7e
Valentin Buira <valentin.buira@gmail.com>
2026-06-10 14:48:20 +0200

    Hide buttons during model execution

--- src/gui/processing/models/qgsmodelcomponentgraphicitem.cpp
+++ src/gui/processing/models/qgsmodelcomponentgraphicitem.cpp
@@ -1560,6 +1560,7 @@ void QgsModelChildAlgorithmGraphicItem::setResults( const QgsProcessingModelChil
     for ( QgsModelArrowItem *arrow : arrows )
     {
       arrow->setShowBadge( false );
+      arrow->RemoveDataViewerButton();
     }
   }
   else
@@ -1586,18 +1587,6 @@ void QgsModelChildAlgorithmGraphicItem::setResults( const QgsProcessingModelChil
                 arrow->setShowBadge( true );
                 arrow->badgeItem()->setValue( it.value() );
               }
-            }
-          }
-        }
-
-        // const QVariantMap outputs = results.outputs();
-        for ( auto it = outputs.constBegin(); it != outputs.constEnd(); ++it )
-        {
-          if ( const QgsProcessingOutputDefinition *outputDefinition = algorithm->outputDefinition( it.key() ); outputDefinition && outputDefinition->isMapLayer() )
-          {
-            for ( QgsModelArrowItem *arrow : arrows )
-            {
-              arrow->setDataViewerButton( child->childId(), it.key() );
             }
           }
         }
