cb78d38429ace7dab552957fa3f41b200738a67a
Till Frankenbach <till.frankenbach@gmail.com>
2026-06-10 14:32:26 +0200

    Add unit test to verify menu deletion

--- tests/src/python/test_qgsapplication.py
+++ tests/src/python/test_qgsapplication.py
@@ -13,12 +13,19 @@ __copyright__ = "Copyright 2012, The QGIS Project"
 
 import unittest
 
+from qgis.PyQt.QtCore import QCoreApplication, QEvent
+from qgis.PyQt.QtWidgets import QAction, QMenu
 from qgis.testing import QgisTestCase, start_app
+from qgis.testing.mocked import get_iface
 
 QGISAPP = start_app()
 
 
 class TestPyQgsApplication(QgisTestCase):
+    def setUp(self):
+        super().setUp()
+        self.iface = get_iface()
+
     def testInvalidThemeName(self):
         """Check using an invalid theme will fallback to  'default'"""
         QGISAPP.setUITheme("fooobar")
@@ -26,6 +33,29 @@ class TestPyQgsApplication(QgisTestCase):
         myResult = QGISAPP.themeName()
         myMessage = f"Expected:\n{myExpectedResult}\nGot:\n{myResult}\n"
         assert myExpectedResult == myResult, myMessage
+
+    def testMenusNotCreatedByRemove(self):
+        menu_name = "&Test Menu"
+        action = QAction("Temporary Action", self.iface.mainWindow())
+
+        self.iface.removePluginDatabaseMenu(menu_name, action)
+        self.iface.removePluginMenu(menu_name, action)
+        self.iface.removePluginRasterMenu(menu_name, action)
+        self.iface.removePluginVectorMenu(menu_name, action)
+        self.iface.removePluginWebMenu(menu_name, action)
+        self.iface.removePluginMeshMenu(menu_name, action)
+
+        QCoreApplication.sendPostedEvents(None, QEvent.Type.DeferredDelete)
+        QCoreApplication.processEvents()
+
+        self.assertTrue(
+            not any(
+                menu
+                for menu in self.iface.mainWindow().findChildren(QMenu)
+                if menu.title() == menu_name
+            ),
+            "Menu should have been deleted",
+        )
 
 
 if __name__ == "__main__":
