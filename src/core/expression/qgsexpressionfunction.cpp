f96f1f5b6e399bcf75bf11fddb98378ec6f16141
Alexander Bruy <alexander.bruy@gmail.com>
2026-06-11 14:13:36 +0100

    address review comments

--- src/core/expression/qgsexpressionfunction.cpp
+++ src/core/expression/qgsexpressionfunction.cpp
@@ -6728,9 +6728,9 @@ static QVariant fcnGetLayerProperty( const QVariantList &values, const QgsExpres
   const QString layerProperty = QgsExpressionUtils::getStringValue( values.at( 1 ), parent );
 
   bool translate = true;
-  if ( values.length() == 3 )
+  if ( values.length() >= 3 )
   {
-    translate = QgsExpressionUtils::getIntValue( values.at( 2 ), parent );
+    translate = QgsExpressionUtils::getTVLValue( values.at( 2 ), parent ) == QgsExpressionUtils::TVL::True;
   }
 
   bool foundLayer = false;
