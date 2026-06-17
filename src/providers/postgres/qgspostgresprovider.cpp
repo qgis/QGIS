1735fe3f1fb48cadb4e97089a681cce613c57102
Alessandro Pasotti <elpaso@itopen.it>
2026-06-17 16:20:29 +0200

    Update src/providers/postgres/qgspostgresprovider.cpp
    Co-authored-by: Even Rouault <even.rouault@spatialys.com>

--- src/providers/postgres/qgspostgresprovider.cpp
+++ src/providers/postgres/qgspostgresprovider.cpp
@@ -2536,11 +2536,7 @@ bool QgsPostgresProvider::addFeatures( QgsFeatureList &flist, Flags flags )
           const QgsField fld = field( attrIdx );
           const QString fieldTypeName = fld.typeName();
           // the conversion functions expects the list as a string, so convert it
-          if ( fieldTypeName == "json"_L1 )
-          {
-            v = QString::fromStdString( QgsJsonUtils::jsonFromVariant( value ).dump() );
-          }
-          else if ( fieldTypeName == "jsonb"_L1 )
+          if ( fieldTypeName == "json"_L1 || fieldTypeName == "jsonb"_L1 )
           {
             v = QString::fromStdString( QgsJsonUtils::jsonFromVariant( value ).dump() );
           }
