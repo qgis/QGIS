9932c8e28702b789a8e9e1833a5811cbca320847
Nyall Dawson <nyall.dawson@gmail.com>
2026-06-23 12:01:49 +1000

    Default to msaa off
    There's a few issues associated with this -- broken globe views,
    broken 3d map animation exports

--- src/3d/qgs3d.cpp
+++ src/3d/qgs3d.cpp
@@ -51,7 +51,7 @@
 
 using namespace Qt::StringLiterals;
 
-const QgsSettingsEntryBool *Qgs3D::settingMsaaEnabled = new QgsSettingsEntryBool( u"msaa-enabled"_s, QgsSettingsTree::sTree3DMap, true, u"Whether MSAA is enabled for 3D map rendering"_s );
+const QgsSettingsEntryBool *Qgs3D::settingMsaaEnabled = new QgsSettingsEntryBool( u"msaa"_s, QgsSettingsTree::sTree3DMap, false, u"Whether MSAA is enabled for 3D map rendering"_s );
 const QgsSettingsEntryEnumFlag<Qgis::TextureFilterQuality> *Qgs3D::settingTextureFilterQuality
   = new QgsSettingsEntryEnumFlag<Qgis::TextureFilterQuality>( u"texture-filter"_s, QgsSettingsTree::sTree3DMap, Qgis::TextureFilterQuality::Anisotropic16x, u"Texture filter quality"_s );
 const QgsSettingsEntryEnumFlag<Qgis::ShadowQuality> *Qgs3D::settingShadowQuality
