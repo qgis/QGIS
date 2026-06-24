80050e21375886979c6114a6967cc65a18048f7d
Julien Cabieces <julien.cabieces@oslandia.com>
2026-06-24 10:56:56 +0200

    fix(Tests): Use local server for filedownload python test

--- .docker/badssl/gen-certs.sh
+++ .docker/badssl/gen-certs.sh
@@ -18,3 +18,22 @@ openssl x509 -req -in /certs/expired.csr -CA /certs/ca.crt -CAkey /certs/ca.key 
   -CAcreateserial -out /certs/expired.crt \
   -not_before 20200101000000Z \
   -not_after  20200102000000Z
+
+# --- Untrusted root ---
+
+# 1. Generate a self-signed CA (not added to system trust)
+openssl req -x509 -newkey rsa:4096 -days 3650 -nodes \
+  -keyout ca.key -out ca.crt \
+  -subj "/CN=Untrusted Local CA"
+
+# 2. Generate untrusted key + CSR
+openssl req -newkey rsa:2048 -nodes \
+  -keyout /certs/untrusted.key -out /certs/untrusted.csr \
+  -subj "/CN=badssl"
+
+# 3. Sign the untrusted cert with your untrusted CA
+openssl x509 -req -in /certs/untrusted.csr -CA ca.crt -CAkey ca.key \
+  -CAcreateserial -out /certs/untrusted.crt -days 3650 \
+  -extfile <(echo "subjectAltName=DNS:badssl,DNS:localhost")
+
+cat /certs/untrusted.crt ca.crt > /certs/fullchain_untrusted.crt
