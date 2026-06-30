7ea72e104652b88b5983adc0929d732672497b65
Loïc Bartoletti <lbartoletti@users.noreply.github.com>
2026-06-30 18:41:39 +0200

    unbreak qgis with gdal >= 3.13.0  (#66615)
    unbreak qgis with gdal >= 3.13.0 after ffdb8e3b27035478c533f37087505e749cc15b9b

--- src/analysis/raster/qgsrelief.cpp
+++ src/analysis/raster/qgsrelief.cpp
@@ -163,13 +163,7 @@ QgsRelief::Result QgsRelief::processRaster( QgsFeedback *feedback )
   std::vector<unsigned char> resultGreenLine( xSize );
   std::vector<unsigned char> resultBlueLine( xSize );
 
-  bool hasReportsDuringClose = false;
-#if GDAL_VERSION_NUM >= GDAL_COMPUTE_VERSION( 3, 13, 0 )
-  hasReportsDuringClose = GDALDatasetGetCloseReportsProgress( outputDataset.get() );
-  const double maxProgressDuringBlockWriting = hasReportsDuringClose ? 50.0 : 100.0;
-#else
-  constexpr double maxProgressDuringBlockWriting = 100.0;
-#endif
+  const double maxProgressDuringBlockWriting = outputProvider->hasReportsDuringClose() ? 50.0 : 100.0;
 
   auto readRow = [&iter, this]( std::vector<float> &scanLine ) {
     int iterCols = 0;
@@ -314,7 +308,7 @@ QgsRelief::Result QgsRelief::processRaster( QgsFeedback *feedback )
     }
   }
 
-  if ( feedback && hasReportsDuringClose )
+  if ( feedback && outputProvider->hasReportsDuringClose() )
   {
     std::unique_ptr<QgsFeedback> scaledFeedback( QgsFeedback::createScaledFeedback( feedback, maxProgressDuringBlockWriting, 100.0 ) );
     if ( !outputProvider->closeWithProgress( scaledFeedback.get() ) )
