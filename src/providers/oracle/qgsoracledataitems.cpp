e21e15ded33b4107ec51b5a8854cf1cee2f302cb
Julien Cabieces <julien.cabieces@oslandia.com>
2026-07-01 18:34:06 +0200

    fix(Oracle): Set browser data item as populated when an error occurs
    If we consider it not populated, everytime a sibling will be refreshed
    or unfolded, QgsBrowserModel::fetchMore() would be called to load every
    siblings, even the one in error, leading to display an error and
    eventually to open the connexion dialog (if the last error was 30
    seconds later).
    
    It adds also an error item to properly disclose the situation.

--- src/providers/oracle/qgsoracledataitems.cpp
+++ src/providers/oracle/qgsoracledataitems.cpp
@@ -221,10 +221,35 @@ void QgsOracleConnectionItem::taskFinished()
 {
   QgsDebugMsgLevel( QStringLiteral( "Entering." ), 3 );
 
-  if ( mColumnTypeTask->status() == QgsTask::Complete )
-    setAllAsPopulated();
-  else
-    setState( Qgis::BrowserItemState::NotPopulated );
+  switch ( mColumnTypeTask->status() )
+  {
+    case QgsTask::Terminated:
+    {
+      if ( mColumnTypeTask->error().isEmpty() )
+      {
+        // task canceled
+        setState( Qgis::BrowserItemState::NotPopulated );
+      }
+      else
+      {
+        // an error occurred
+        addChildItem( new QgsErrorItem( this, mColumnTypeTask->error(), mPath + "/error" ), true );
+        setAllAsPopulated();
+      }
+      break;
+    }
+
+    case QgsTask::Complete:
+      setIcon( QIcon() );
+      setAllAsPopulated();
+      break;
+
+    case QgsTask::Queued:
+    case QgsTask::OnHold:
+    case QgsTask::Running:
+      QgsDebugError( "Invalid task state" );
+      break;
+  }
 
   mColumnTypeTask = nullptr;
 }
