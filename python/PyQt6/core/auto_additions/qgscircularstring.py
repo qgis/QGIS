65127b65723eecee8d35c2df18b590a556b8a572
Alessandro Pasotti <elpaso@itopen.it>
2026-07-06 08:54:18 +0200

    sipify

--- python/PyQt6/core/auto_additions/qgscircularstring.py
+++ python/PyQt6/core/auto_additions/qgscircularstring.py
@@ -1,7 +1,7 @@
 # The following has been generated automatically from src/core/geometry/qgscircularstring.h
 try:
     QgsCircularString.fromTwoPointsAndCenter = staticmethod(QgsCircularString.fromTwoPointsAndCenter)
-    QgsCircularString.__overridden_methods__ = ['geometryType', 'clone', 'clear', 'asGml2', 'asGml3', 'isValid', 'indexOf', 'length', 'curveToLine', 'snappedToGrid', 'simplifyByDistance', 'removeDuplicateNodes', 'draw', 'addToPainterPath', 'drawAsPolygon', 'insertVertex', 'deleteVertex', 'deleteVertices', 'closestSegment', 'pointAt', 'sumUpArea', 'sumUpArea3D', 'hasCurvedSegments', 'vertexAngle', 'segmentLength', 'distanceBetweenVertices', 'reversed', 'interpolatePoint', 'curveSubstring', 'createEmptyWithSameType', 'calculateBoundingBox3D']
+    QgsCircularString.__overridden_methods__ = ['geometryType', 'clone', 'clear', 'asGml2', 'asGml3', 'isEmpty', 'isValid', 'indexOf', 'length', 'curveToLine', 'snappedToGrid', 'simplifyByDistance', 'removeDuplicateNodes', 'draw', 'addToPainterPath', 'drawAsPolygon', 'insertVertex', 'deleteVertex', 'deleteVertices', 'closestSegment', 'pointAt', 'sumUpArea', 'sumUpArea3D', 'hasCurvedSegments', 'vertexAngle', 'segmentLength', 'distanceBetweenVertices', 'reversed', 'interpolatePoint', 'curveSubstring', 'createEmptyWithSameType', 'calculateBoundingBox3D']
     QgsCircularString.__group__ = ['geometry']
 except (NameError, AttributeError):
     pass
