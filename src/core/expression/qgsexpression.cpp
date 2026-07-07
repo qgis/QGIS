18c3ce9454c8a1568ad7d9655da625daf15068fe
Nyall Dawson <nyall.dawson@gmail.com>
2026-07-07 12:33:15 +1000

    Fix deadlock when rendering annotation layers
    It's not safe to use a static QRegularExpression, these need to be thread_local instead

--- src/core/expression/qgsexpression.cpp
+++ src/core/expression/qgsexpression.cpp
@@ -475,7 +475,7 @@ QString QgsExpression::replaceExpressionText( const QString &action, const QgsEx
   int index = 0;
   while ( index < action.size() )
   {
-    static const QRegularExpression sRegEx { u"\\[%(.*?)%\\]"_s, QRegularExpression::MultilineOption | QRegularExpression::DotMatchesEverythingOption };
+    const thread_local QRegularExpression sRegEx { u"\\[%(.*?)%\\]"_s, QRegularExpression::MultilineOption | QRegularExpression::DotMatchesEverythingOption };
 
     const QRegularExpressionMatch match = sRegEx.match( action, index );
     if ( !match.hasMatch() )
