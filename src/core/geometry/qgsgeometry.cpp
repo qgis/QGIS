b2b75afd1a8dc31bd12004d6a3e4957406e1d88b
Germán Carrillo <gcarrillo@linuxmail.org>
2026-07-24 14:51:17 +0200

    [tests] Make sure split geometry tests pass against GEOS 3.15:
      + some temporary skipped tests or parts of tests (e.g., topologicalpoints checks and self-intersection input),
      + some normalized geometry outputs,
      + some (temporary) adjustments to match GEOS 3.15 outputs (e.g., LineStrings instead of MultiLineString outputs and switch to a better result in GEOS 3.15 with a weird input LineString)

--- src/core/geometry/qgsgeometry.cpp
+++ src/core/geometry/qgsgeometry.cpp
@@ -1254,6 +1254,10 @@ Qgis::GeometryOperationResult QgsGeometry::splitGeometry(
   {
     return Qgis::GeometryOperationResult::InvalidBaseGeometry;
   }
+  if ( splitLine.size() == 0 )
+  {
+    return Qgis::GeometryOperationResult::InvalidInputGeometryType;
+  }
 
   // We're trying adding the split line's vertices to the geometry so that
   // snap to segment always produces a valid split (see https://github.com/qgis/QGIS/issues/29270)
@@ -1283,6 +1287,24 @@ Qgis::GeometryOperationResult QgsGeometry::splitGeometry(
   }
 
   QVector<QgsGeometry > newGeoms;
+#if GEOS_VERSION_MAJOR > 3 || ( GEOS_VERSION_MAJOR == 3 && GEOS_VERSION_MINOR >= 15 )
+  Q_UNUSED( skipIntersectionTest )
+
+  std::unique_ptr< QgsAbstractGeometry > splitGeom;
+  if ( splitLine.size() > 1 )
+  {
+    splitGeom.reset( new QgsLineString( splitLine ) );
+    splitGeom->dropZValue();
+    splitGeom->dropMValue();
+  }
+  else if ( splitLine.size() == 1 )
+  {
+    splitGeom.reset( new QgsPoint( splitLine[0].x(), splitLine[0].y() ) );
+  }
+  QgsGeos geos( tmpGeom.get() );
+  mLastError.clear();
+  QgsGeometryEngine::EngineOperationResult result = geos.splitGeometry( *splitGeom.get(), newGeoms, topological, topologyTestPoints, &mLastError );
+#else
   QgsLineString splitLineString( splitLine );
   splitLineString.dropZValue();
   splitLineString.dropMValue();
@@ -1290,6 +1312,7 @@ Qgis::GeometryOperationResult QgsGeometry::splitGeometry(
   QgsGeos geos( tmpGeom.get() );
   mLastError.clear();
   QgsGeometryEngine::EngineOperationResult result = geos.splitGeometry( splitLineString, newGeoms, topological, topologyTestPoints, &mLastError, skipIntersectionTest );
+#endif
 
   if ( result == QgsGeometryEngine::Success )
   {
