fc731e1bd9e33a3b5303d1a2b59d47e57858565d
Valentin Buira <valentin.buira@gmail.com>
2026-07-30 17:10:39 +0200

    Add tests for QgsSymbolLayerModel

--- src/gui/symbology/qgssymbollayermodel.h
+++ src/gui/symbology/qgssymbollayermodel.h
@@ -16,6 +16,8 @@
 #ifndef QGSSYMBOLLAYERMODEL_H
 #define QGSSYMBOLLAYERMODEL_H
 
+#include "qgis.h"
+#include "qgis_gui.h"
 #include "qgsvectorlayer.h"
 
 #include <QAbstractItemModel>
@@ -32,7 +34,7 @@ class QgsSymbolLayer;
  * \ingroup gui
  * \since QGIS 4.4
  */
-class QgsSymbolLayerModelNode : public QObject
+class GUI_EXPORT QgsSymbolLayerModelNode : public QObject
 {
     Q_OBJECT
   public:
@@ -126,6 +128,8 @@ class QgsSymbolLayerModelNode : public QObject
 
     //! whether the node should be shown in GUI as expanded
     bool mExpanded = true;
+
+    friend class TestQgsSymbolLayerModel;
 };
 
 /**
@@ -134,7 +138,7 @@ class QgsSymbolLayerModelNode : public QObject
  * \ingroup gui
  * \since QGIS 4.4
  */
-class QgsSymbolLayerModel : public QAbstractItemModel
+class GUI_EXPORT QgsSymbolLayerModel : public QAbstractItemModel
 {
     Q_OBJECT
 
