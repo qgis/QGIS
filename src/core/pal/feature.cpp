e25b3055eaf68dccff3ea613ba40883f3cb89762
Nyall Dawson <nyall.dawson@gmail.com>
2026-08-03 12:15:19 +1000

    [pal] Ensure placement of parallel labels is stable
    When generating candidates, we assign the cost based on the
    distance of the candidate from the center of the feature. This
    meant that when we generated an even number of candidates, we'd
    get exact duplicate costs for the candidates with exact matching
    distances from the center of the line.
    
    Break this tiebreaking with a tiny additional cost for labels
    further along the line from the center, so that there's a stable
    solution to the labeling problem with the parallel placement modes.
    
    (cherry picked from commit 82a02e2dee638955f57d52eaa3fec636dfb8c782)

--- src/core/pal/feature.cpp
+++ src/core/pal/feature.cpp
@@ -1145,8 +1145,25 @@ std::size_t FeaturePart::createCandidatesAlongLineNearStraightSegments( std::vec
         // penalize positions which are further from line anchor point of whole linestring (by default the middle of the line)
         // this only applies to non closed linestrings, since the middle of a closed linestring is effectively arbitrary
         // and irrelevant to labeling
-        double costLineCenter = 2 * std::fabs( labelTextAnchor - lineAnchorPoint ) / totalLineLength;  // 0 -> 1
-        cost += costLineCenter * 0.0005;  // < 0, 0.0005 >
+        double costLineCenter = 2 * std::fabs( labelTextAnchor - lineAnchorPoint ) / totalLineLength; // 0 -> 1
+
+        // add a little tie breaker amount -- otherwise if we are generating an even number of candidates, we may end up with
+        // two with exactly the same cost centered over the mid point of the feature. So add a tiny PLACEMENT_TIEBREAKER amount
+        // so that one of these is always preferred, giving us a stable labeling solution:
+        // eg:
+        //   Line:  ============|============[ Anchor ]==========|==============
+        //                      |  <-same dist-> | <-same dist-> |
+        //              [-- Candidate --]        |       [-- Candidate --]
+        //                      |                |               |
+        //                 Anchor < Mid          |          Anchor > Mid
+        //                                       |     cost += PLACEMENT_TIEBREAKER
+        if ( labelTextAnchor > lineAnchorPoint )
+        {
+          constexpr double PLACEMENT_TIEBREAKER = 0.000001234;
+          cost += PLACEMENT_TIEBREAKER;
+        }
+
+        cost += costLineCenter * 0.0005; // < 0, 0.0005 >
       }
 
       if ( placementIsFlexible )
