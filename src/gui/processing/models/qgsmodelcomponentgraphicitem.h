3efff4852460943b96163759f3c481d594325ed3
Valentin Buira <valentin.buira@gmail.com>
2026-08-11 11:19:01 +0200

    fix emit childAlgorithmDeleted

--- src/gui/processing/models/qgsmodelcomponentgraphicitem.h
+++ src/gui/processing/models/qgsmodelcomponentgraphicitem.h
@@ -326,7 +326,7 @@ class GUI_EXPORT QgsModelComponentGraphicItem : public QGraphicsObject, public Q
     /**
      * Emitted when a child algorithm is deleted from the model.
      */
-    void childAlgorithmDeleted();
+    void childAlgorithmDeleted( const QString &id );
 
     /**
      * Emitted when item requests that all connected arrows are repainted.
