9d48042ba7fa87d32fb0d0d6d67b094da7829adb
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-08-13 12:08:05 +0200

    fix windows builds

--- src/gui/symbology/qgssymbollayermodel.h
+++ src/gui/symbology/qgssymbollayermodel.h
@@ -155,6 +155,8 @@ class GUI_EXPORT QgsSymbolLayerModelNode
     QgsSymbolLayerModelNode *mParent = nullptr;
     std::deque<std::unique_ptr<QgsSymbolLayerModelNode>> mChildren;
 
+    Q_DISABLE_COPY( QgsSymbolLayerModelNode )
+
     friend class TestQgsSymbolLayerModel;
 };
 
