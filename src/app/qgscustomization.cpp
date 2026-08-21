aa21785072b73eb042890d2ad3bdf8bf23616098
Julien Cabieces <julien.cabieces@oslandia.com>
2026-08-21 10:03:33 +0200

    Revert "[Customization] fix position check"

--- src/app/qgscustomization.cpp
+++ src/app/qgscustomization.cpp
@@ -188,7 +188,7 @@ unsigned int QgsCustomization::QgsItem::childrenCount() const
 
 void QgsCustomization::QgsItem::insertChild( int position, std::unique_ptr<QgsItem> item )
 {
-  if ( position < 0 || position >= static_cast<int>( mChildItemList.size() ) )
+  if ( position < 0 || position > static_cast<int>( mChildItemList.size() ) )
   {
     QgsDebugError( u"Insert item impossible, invalid position"_s );
     return;
