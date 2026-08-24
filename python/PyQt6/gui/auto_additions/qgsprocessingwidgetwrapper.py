bd1980a615dc3894fc726180746d1e2c793e3499
Nyall Dawson <nyall.dawson@gmail.com>
2026-08-25 06:45:00 +1000

    Move Processing widget context related classes out to their own file

--- python/PyQt6/gui/auto_additions/qgsprocessingwidgetwrapper.py
+++ python/PyQt6/gui/auto_additions/qgsprocessingwidgetwrapper.py
@@ -41,16 +41,7 @@ try:
 except (NameError, AttributeError):
     pass
 try:
-    QgsProcessingWidgetContextGenerator.__abstract_methods__ = ['createWidgetContext']
-    QgsProcessingWidgetContextGenerator.__group__ = ['processing']
-except (NameError, AttributeError):
-    pass
-try:
     QgsProcessingHiddenWidgetWrapper.__overridden_methods__ = ['setWidgetValue', 'widgetValue', 'linkedVectorLayer', 'createWidget', 'createLabel']
     QgsProcessingHiddenWidgetWrapper.__group__ = ['processing']
-except (NameError, AttributeError):
-    pass
-try:
-    QgsProcessingParameterWidgetContext.__group__ = ['processing']
 except (NameError, AttributeError):
     pass
