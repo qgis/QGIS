6d2295a4929602eea6f129ec8fc07a3c7e3754c9
Nyall Dawson <nyall.dawson@gmail.com>
2026-08-26 09:05:07 +1000

    Port ModelerParametersPanelWidget to c++

---
+++ python/PyQt6/gui/auto_additions/qgsmodelchildalgorithmwidgets.py
@@ -0,0 +1,6 @@
+# The following has been generated automatically from src/gui/processing/models/qgsmodelchildalgorithmwidgets.h
+try:
+    QgsProcessingModelerParametersPanelWidget.__overridden_methods__ = ['processingContext']
+    QgsProcessingModelerParametersPanelWidget.__group__ = ['processing', 'models']
+except (NameError, AttributeError):
+    pass
