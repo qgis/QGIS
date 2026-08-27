fc25377172461a2f39df660eb9566804cd33a357
Aloha Churchill <churchill.aloha@gmail.com>
2026-08-27 19:07:25 +0200

    bugfix for https://github.com/qgis/QGIS/issues/67164

--- src/core/qgsstringutils.cpp
+++ src/core/qgsstringutils.cpp
@@ -73,8 +73,11 @@ QString QgsStringUtils::unaccent( const QString &input )
 QString QgsStringUtils::createUniqueId( const QString &base )
 {
   // A random UUID guarantees uniqueness; the base is a human-readable prefix.
+  constexpr int MAX_BASE_LENGTH = 36; // set to the length of a UUID string
   const QString uuid = QUuid::createUuid().toString( QUuid::StringFormat::WithoutBraces );
-  QString id = base.isEmpty() ? uuid : base + '_' + uuid;
+  // Cap length of base to avoid unchecked large name
+  const QString prefix = base.left( MAX_BASE_LENGTH );
+  QString id = prefix.isEmpty() ? uuid : prefix + '_' + uuid;
   // Tidy the id up to avoid characters that may cause problems elsewhere (e.g.
   // in some parts of XML). Replaces every non-word character (word characters
   // are the alphabet, numbers and underscore) with an underscore.
