24b0fea623286223b2f5e5111c9e9d756dad4429
Julien Cabieces <julien.cabieces@oslandia.com>
2026-09-01 13:45:59 +0200

    fix(GetCap): Do not expose Style for group
    They don't have actually style that could be ativated then with STYLE
    parameter. It was a regression introduce in 3.44 during a
    refactory. OGC WMS specification allows to have zero style.

--- src/server/services/wms/qgswmsgetcapabilities.cpp
+++ src/server/services/wms/qgswmsgetcapabilities.cpp
@@ -1216,13 +1216,6 @@ namespace QgsWms
 
           writeServerProperties( doc, layerElem, project, treeGroupChild->serverProperties(), name, version );
 
-          // There is no style assicated with layer tree group so just use a default one
-          const QString styleName = u"default"_s;
-          QDomElement styleElem = createStyleElement( doc, styleName );
-          writeLegendUrl( doc, styleElem, treeGroupChild->serverProperties()->legendUrl(), treeGroupChild->serverProperties()->legendUrlFormat(), name, styleName, project, request, serverIface->serverSettings() );
-
-          layerElem.appendChild( styleElem );
-
           // Layer tree name
           if ( projectSettings )
           {
