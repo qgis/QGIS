713e4d1458b4d1d439de2f62be48aa134c77da86
JuhoErvasti <juho.ervasti@gispo.fi>
2026-09-05 15:30:52 +0000

    fix: set project dirty when editing map themes
    fixes #67260

--- src/app/qgsmapthemes.cpp
+++ src/app/qgsmapthemes.cpp
@@ -77,11 +77,13 @@ QgsMapThemes *QgsMapThemes::instance()
 void QgsMapThemes::addPreset( const QString &name )
 {
   QgsProject::instance()->mapThemeCollection()->insert( name, currentState() );
+  QgsProject::instance()->setDirty();
 }
 
 void QgsMapThemes::updatePreset( const QString &name )
 {
   QgsProject::instance()->mapThemeCollection()->update( name, currentState() );
+  QgsProject::instance()->setDirty();
 }
 
 QMenu *QgsMapThemes::menu()
@@ -166,6 +168,7 @@ void QgsMapThemes::renameCurrentPreset()
         return;
 
       QgsProject::instance()->mapThemeCollection()->renameMapTheme( actionPreset->text(), dlg.name() );
+      QgsProject::instance()->setDirty();
     }
   }
 }
@@ -178,7 +181,10 @@ void QgsMapThemes::removeCurrentPreset()
     {
       int res = QMessageBox::question( QgisApp::instance(), tr( "Remove Theme" ), tr( "Are you sure you want to remove the existing theme “%1”?" ).arg( actionPreset->text() ), QMessageBox::Yes | QMessageBox::No, QMessageBox::No );
       if ( res == QMessageBox::Yes )
+      {
         QgsProject::instance()->mapThemeCollection()->removeMapTheme( actionPreset->text() );
+        QgsProject::instance()->setDirty();
+      }
       break;
     }
   }
