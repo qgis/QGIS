ca2b6249e572ff0cb630bf7ee9c5b49c7e7b9fd1
Jean Felder <jean.felder@oslandia.com>
2026-09-08 22:27:29 +0200

    qgsvectorlayer3drendererwidget: Switch to compact mode for material widget

--- src/app/3d/qgsvectorlayer3drendererwidget.cpp
+++ src/app/3d/qgsvectorlayer3drendererwidget.cpp
@@ -55,6 +55,7 @@ QgsSingleSymbol3DRendererWidget::QgsSingleSymbol3DRendererWidget( QgsVectorLayer
   scrollLayout->addWidget( scrollArea );
 
   widgetSymbol = new QgsSymbol3DWidget( mLayer, this );
+  widgetSymbol->setMode( Qgis::MaterialWidgetMode::Compact );
   scrollArea->setWidget( widgetSymbol );
 
   setLayout( scrollLayout );
