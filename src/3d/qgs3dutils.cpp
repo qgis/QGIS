17a7f224bf1e5d8c9f8efcdc9140e8956c77153d
viperminiq <viperminiq@gmail.com>
2026-09-08 08:38:29 +0200

    move shared methods to dedicated class for globe and vectors - exceptions, exceptions everywhere

--- src/3d/qgs3dutils.cpp
+++ src/3d/qgs3dutils.cpp
@@ -549,11 +549,11 @@ void Qgs3DUtils::extractPointPositions(
       geomZ = pt.z();
     }
 
-    const bool useTerrainHeight = context.crs().type() != Qgis::CrsType::Geocentric && context.terrainRenderingEnabled() && context.terrainGenerator();
+    const bool isGeocentric = context.crs().type() == Qgis::CrsType::Geocentric;
+    const bool useTerrainHeight = !isGeocentric && context.terrainRenderingEnabled() && context.terrainGenerator();
     const float terrainZ = useTerrainHeight
                              ? static_cast<float>( context.terrainGenerator()->heightAt( pt.x(), pt.y(), context ) * ( context.terrainSettings() ? context.terrainSettings()->verticalScale() : 1 ) )
                              : 0.f;
-    const bool isGeocentric = context.crs().type() == Qgis::CrsType::Geocentric;
 
     float h = 0.0f;
     switch ( altClamp )
