91544cba25948566af1a8324f961dd2aeb25db02
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-09 16:00:02 +1000

    [feature] Add native "flow direction" algorithm
    This is a direct port of the Flow Direction calculation from SAGA's
    "Channel Network and Drainage Basins" tool.
    
    It has been extracted as a standalone tool in Processing to allow
    for optimal use in QGIS models and workflows (as opposed to the
    all-in-one implementation in the SAGA equivalent).

--- src/analysis/processing/qgsnativealgorithms.cpp
+++ src/analysis/processing/qgsnativealgorithms.cpp
@@ -329,6 +329,7 @@ using namespace Qt::StringLiterals;
 #include "qgsmetadataalgorithms.h"
 #include "qgsprojectstylealgorithms.h"
 #include "qgsstylealgorithms.h"
+#include "qgsalgorithmflowdirection.h"
 
 ///@cond PRIVATE
 
@@ -508,6 +509,7 @@ void QgsNativeAlgorithms::loadAlgorithms()
   addAlgorithm( new QgsFindProjectionAlgorithm() );
   addAlgorithm( new QgsFixGeometriesAlgorithm() );
   addAlgorithm( new QgsFlattenRelationshipsAlgorithm() );
+  addAlgorithm( new QgsFlowDirectionD8Algorithm() );
   addAlgorithm( new QgsForceCCWAlgorithm() );
   addAlgorithm( new QgsForceCWAlgorithm() );
   addAlgorithm( new QgsForceRHRAlgorithm() );
