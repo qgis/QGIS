8617597f993ef53c6e84add47e35da443955ba00
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-10 12:09:01 +1000

    Add native ports of SAGA's 'Channel Network and Drainage Basins'
    Adds two new native algorithms which are direct ports of the SAGA
    'Channel Network and Drainage Basins' tool.:
    
    - Channel network and drainage basins from DEM: an all-in-one tool
      that requires only a DEM as the input
    - 'Channel network and drainage basins from multiple inputs':
      a more efficient version of the tool for when the flow direction
      and strahler orders have already been calculated.
    
    The outputs are vector basins, channels and junctions.

--- src/analysis/processing/qgsnativealgorithms.cpp
+++ src/analysis/processing/qgsnativealgorithms.cpp
@@ -332,6 +332,7 @@ using namespace Qt::StringLiterals;
 #include "qgsalgorithmflowconnectivity.h"
 #include "qgsalgorithmflowdirection.h"
 #include "qgsalgorithmstrahlerorder.h"
+#include "qgsalgorithmchannelnetwork.h"
 
 ///@cond PRIVATE
 
@@ -409,6 +410,8 @@ void QgsNativeAlgorithms::loadAlgorithms()
   addAlgorithm( new QgsCellStatisticsPercentRankFromRasterAlgorithm() );
   addAlgorithm( new QgsCellStatisticsPercentRankFromValueAlgorithm() );
   addAlgorithm( new QgsCentroidAlgorithm() );
+  addAlgorithm( new QgsChannelNetworkFromDemAlgorithm() );
+  addAlgorithm( new QgsChannelNetworkFromFlowDirAndOrderAlgorithm() );
   addAlgorithm( new QgsCheckValidityAlgorithm() );
   addAlgorithm( new QgsGeometryCheckSegmentLengthAlgorithm() );
   addAlgorithm( new QgsGeometryCheckAngleAlgorithm() );
