531e410d63e4bfa4069093fdf85286000804da70
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-11 10:36:26 +1000

    Require implementationSourceUri for gdal algorithms

--- python/plugins/processing/tests/AlgorithmsTestBase.py
+++ python/plugins/processing/tests/AlgorithmsTestBase.py
@@ -860,7 +860,7 @@ class GenericAlgorithmsTest(QgisTestCase):
                     f'Algorithm {alg.id()} shortDescription should not contain any HTML formatting "{alg.shortDescription()}"',
                 )
 
-        if alg.provider().id() in ("pdal", "qgis"):
+        if alg.provider().id() in ("pdal", "qgis", "gdal"):
             self.assertTrue(
                 alg.implementationSourceUri(),
                 f"Algorithm {alg.id()} has no QGS_MARK_ALGORITHM_SOURCE macro inserted!",
