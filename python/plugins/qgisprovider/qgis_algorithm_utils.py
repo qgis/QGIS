0da708ecf2c8dc072ae3842a481948e18be87040
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-11 11:19:23 +1000

    Add copyright header

--- python/plugins/qgisprovider/qgis_algorithm_utils.py
+++ python/plugins/qgisprovider/qgis_algorithm_utils.py
@@ -2,6 +2,24 @@
 QGIS algorithm utilities
 """
 
+"""
+***************************************************************************
+    qgis_algorithm_utils.py
+    ---------------------
+    Date                 : September 2026
+    Copyright            : (C) 2026 by Nyall Dawson
+    Email                : nyall.dawson@gmail.com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+
 import inspect
 from pathlib import Path
 
