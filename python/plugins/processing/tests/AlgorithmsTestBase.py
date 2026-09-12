f3fd1723df2a5ee7ba31463ab7f6226fac280c01
Alexander Bruy <alexander.bruy@gmail.com>
2026-09-12 11:43:10 +0100

    run source location test on native algorithms

--- python/plugins/processing/tests/AlgorithmsTestBase.py
+++ python/plugins/processing/tests/AlgorithmsTestBase.py
@@ -860,7 +860,7 @@ class GenericAlgorithmsTest(QgisTestCase):
                     f'Algorithm {alg.id()} shortDescription should not contain any HTML formatting "{alg.shortDescription()}"',
                 )
 
-        if alg.provider().id() in ("pdal",):
+        if alg.provider().id() in ("pdal", "native"):
             self.assertTrue(
                 alg.implementationSourceUri(),
                 f"Algorithm {alg.id()} has no QGS_MARK_ALGORITHM_SOURCE macro inserted!",
