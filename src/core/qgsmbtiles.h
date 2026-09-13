6df3f85a546cfefa71d731d36562b4d9ac1dd9fd
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-13 12:24:02 +1000

    Defer index creation until we've written all tiles
    Since we'll be writing 1000s of tiles, we don't want the index
    updated after every tile is written.

--- src/core/qgsmbtiles.h
+++ src/core/qgsmbtiles.h
@@ -48,11 +48,25 @@ class CORE_EXPORT QgsMbTiles
     bool isOpen() const;
 
     /**
+     * Finalizes the database after writing all tiles.
+     *
+     * This must be called if the database was created using a deferred index.
+     *
+     * \since QGIS 4.4
+     */
+    bool finalize();
+
+    /**
      * Creates a new MBTiles file and initializes it with metadata and tiles tables.
      * It is up to the caller to set appropriate metadata entries and add tiles afterwards.
-     * Returns TRUE on success. If the file exists already, returns FALSE.
+     *
+     * \param deferIndexCreation can be set to TRUE to defer creation of the tile_index. This results
+     * in much faster tile writes, as the index will not be updated after every tile write. If set to
+     * TRUE, a call to finalize() must be made before closing the database. (Since QGIS 4.4)
+     *
+     * \returns TRUE on success. If the file exists already, returns FALSE.
      */
-    bool create();
+    bool create( bool deferIndexCreation = false );
 
     //! Requests metadata value for the given key
     QString metadataValue( const QString &key ) const;
@@ -100,6 +114,7 @@ class CORE_EXPORT QgsMbTiles
   private:
     QString mFilename;
     sqlite3_database_unique_ptr mDatabase;
+    bool mDeferredIndexCreation = false;
 };
 
 
