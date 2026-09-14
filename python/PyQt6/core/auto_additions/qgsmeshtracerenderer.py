f8aea1fb9513c7df526e62a31e151ca64911ff9a
uclaros <uclaros@gmail.com>
2026-09-14 16:28:54 +0300

    Rename the mesh trace and stream field classes
    Keep QgsMeshVectorTraceAnimationGenerator as a deprecated subclass to preserve API

--- python/PyQt6/core/auto_additions/qgsmeshtracerenderer.py
+++ python/PyQt6/core/auto_additions/qgsmeshtracerenderer.py
@@ -1,5 +1,9 @@
 # The following has been generated automatically from src/core/mesh/qgsmeshtracerenderer.h
 try:
+    QgsVectorFieldTraceAnimationGenerator.__group__ = ['mesh']
+except (NameError, AttributeError):
+    pass
+try:
     QgsMeshVectorTraceAnimationGenerator.__group__ = ['mesh']
 except (NameError, AttributeError):
     pass
