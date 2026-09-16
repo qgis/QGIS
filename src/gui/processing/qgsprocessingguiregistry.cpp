cc0b145df50a19f1d3242c429c91d6c08a015dce
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-16 14:12:24 +1000

    Create widget wrapper for QgsProcessingParameterTileExtentMaxZoomList

--- src/gui/processing/qgsprocessingguiregistry.cpp
+++ src/gui/processing/qgsprocessingguiregistry.cpp
@@ -114,6 +114,7 @@ QgsProcessingGuiRegistry::QgsProcessingGuiRegistry()
   addParameterWidgetFactory( new QgsProcessingExecuteSqlWidgetWrapper() );
   addParameterWidgetFactory( new QgsProcessingInterpolationPixelSizeWidgetWrapper() );
   addParameterWidgetFactory( new QgsProcessingInterpolationSourceWidgetWrapper() );
+  addParameterWidgetFactory( new QgsProcessingTileExtentMaxZoomWidgetWrapper() );
 
   mModelConfigWidgetFactory = std::make_unique< QgsProcessingGuiInternalModelConfigWidgetFactory >();
   registerModelConfigWidgetFactory( mModelConfigWidgetFactory.get() );
