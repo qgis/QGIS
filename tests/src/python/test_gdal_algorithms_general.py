26488c23e3304ab08304d20c895992ae7ed3f2a0
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-16 10:52:53 +1000

    Move GDAL algorithm tests out of plugin dir

--- tests/src/python/test_gdal_algorithms_general.py
+++ tests/src/python/test_gdal_algorithms_general.py
@@ -24,6 +24,15 @@ import shutil
 import tempfile
 
 import nose2
+from processing.algs.gdal.GdalAlgorithm import GdalAlgorithm
+from processing.algs.gdal.GdalUtils import GdalUtils
+from processing.algs.gdal.ogr2ogr import ogr2ogr
+from processing.algs.gdal.OgrToPostGis import OgrToPostGis
+from processing.tests.TestData import (
+    wms_layer_1_1_1,
+    wms_layer_1_3_0,
+    wms_layer_1_3_0_frankfurt,
+)
 from qgis.core import (
     QgsApplication,
     QgsCoordinateReferenceSystem,
@@ -45,18 +54,10 @@ from qgis.core import (
 from qgis.PyQt.QtCore import QFile, QIODevice
 from qgis.PyQt.QtXml import QDomDocument
 from qgis.testing import QgisTestCase, start_app
+from utilities import unitTestDataPath
 
-from processing.algs.gdal.GdalAlgorithm import GdalAlgorithm
-from processing.algs.gdal.GdalUtils import GdalUtils
-from processing.algs.gdal.ogr2ogr import ogr2ogr
-from processing.algs.gdal.OgrToPostGis import OgrToPostGis
-from processing.tests.TestData import (
-    wms_layer_1_1_1,
-    wms_layer_1_3_0,
-    wms_layer_1_3_0_frankfurt,
-)
-
-testDataPath = os.path.join(os.path.dirname(__file__), "testdata")
+TEST_DATA_DIR = unitTestDataPath()
+testDataPath = os.path.join(TEST_DATA_DIR, "processing")
 
 
 class TestGdalAlgorithms(QgisTestCase):
