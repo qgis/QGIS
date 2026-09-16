93b9e213c3bdbd04e4cdce8ae0d1f85dcaad0601
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-17 07:22:49 +1000

    [api] Port processing ToolboxAction to c++ base class
    And add a fair chunk of python "magic" to avoid breaking API...

---
+++ python/PyQt6/gui/auto_additions/qgsprocessingprovideractions.py
@@ -0,0 +1,10 @@
+# The following has been generated automatically from src/gui/processing/qgsprocessingprovideractions.h
+try:
+    QgsProcessingToolboxAction.__virtual_methods__ = ['icon', 'getIcon', 'execute', 'trigger']
+    QgsProcessingToolboxAction.__group__ = ['processing']
+except (NameError, AttributeError):
+    pass
+try:
+    QgsProcessingActionContext.__group__ = ['processing']
+except (NameError, AttributeError):
+    pass
--- python/plugins/processing/gui/ToolboxAction.py
+++ python/plugins/processing/gui/ToolboxAction.py
@@ -19,18 +19,56 @@ __author__ = "Victor Olaya"
 __date__ = "August 2012"
 __copyright__ = "(C) 2012, Victor Olaya"
 
-from qgis.core import QgsApplication
+from functools import wraps
+from warnings import warn
+
+from qgis.gui import QgsProcessingToolboxAction
 from qgis.PyQt.QtCore import QCoreApplication
 
 
-class ToolboxAction:
-    def setData(self, toolbox):
-        self.toolbox = toolbox
+class ToolboxAction(QgsProcessingToolboxAction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._parent_inited = True
 
-    def getIcon(self):
-        return QgsApplication.getThemeIcon("/processingAlgorithm.svg")
+    def __init_subclass__(cls, **kwargs):
+        warn(
+            "The ToolboxAction class is deprecated, use QgsProcessingToolboxAction",
+            DeprecationWarning,
+            2,
+        )
+        super().__init_subclass__(**kwargs)
+
+        # Check if the subclass overrides __init__ directly -- if so, we have to
+        # do some grossness to FORCE that __init__ to call the super class __init__,
+        # or sip will raise an exception. And since we didn't require this super
+        # call in the older API, we have to do this with "magic" to avoid breakage...
+        if "__init__" in cls.__dict__:
+            orig_init = cls.__dict__["__init__"]
+
+            @wraps(orig_init)
+            def wrapped_init(self, *args, **user_kwargs):
+                # Ensure parent initialization runs first if it hasn't already
+                if not getattr(self, "_parent_inited", False):
+                    super().__init__()
+                    self._parent_inited = True
+                orig_init(self, *args, **user_kwargs)
+
+            cls.__init__ = wrapped_init
 
     def tr(self, string, context=""):
         if context == "":
             context = self.__class__.__name__
         return QCoreApplication.translate(context, string)
+
+
+def _get_name(self):
+    return self.actionName()
+
+
+def _set_name(self, _name):
+    self.setActionName(_name)
+
+
+ToolboxAction.name = property(_get_name)
+ToolboxAction.name = ToolboxAction.name.setter(_set_name)
