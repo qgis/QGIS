7157780a681880222a10716a7d38184330bae379
Jan Caha <jan.caha@lutraconsulting.co.uk>
2026-09-16 13:12:12 +0200

    pansharp was added in GDAL 3.12

--- python/plugins/processing/algs/gdal/pansharp.py
+++ python/plugins/processing/algs/gdal/pansharp.py
@@ -144,6 +144,22 @@ class pansharp(GdalAlgorithm):
     def _commandNameGdalCli(self) -> str:
         return "gdal raster pansharpen"
 
+    def commandName(self):
+        if GdalUtils.version() < 3120000:
+            return self._commandNameLegacy()
+        else:
+            return self._commandNameGdalCli()
+
+    def getConsoleCommands(self, parameters, context, feedback, executing=True):
+        if GdalUtils.version() < 3120000:
+            return self._getConsoleCommandsLegacy(
+                parameters, context, feedback, executing
+            )
+        else:
+            return self._getConsoleCommandsGdalCli(
+                parameters, context, feedback, executing
+            )
+
     def _getConsoleCommandsLegacy(self, parameters, context, feedback, executing=True):
         spectral = self.parameterAsRasterLayer(parameters, self.SPECTRAL, context)
         if spectral is None:
