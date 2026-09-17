6ea0f1f3a71211c1aaba5c51bfef084905f2e93a
Alexander Bruy <alexander.bruy@gmail.com>
2026-09-17 10:55:01 +0100

    add short description to create spatial index algorithm

--- src/analysis/processing/qgsalgorithmspatialindex.h
+++ src/analysis/processing/qgsalgorithmspatialindex.h
@@ -41,6 +41,7 @@ class QgsSpatialIndexAlgorithm : public QgsProcessingAlgorithm
     QString groupId() const override;
     Qgis::ProcessingAlgorithmFlags flags() const override;
     QString shortHelpString() const override;
+    QString shortDescription() const override;
     QgsSpatialIndexAlgorithm *createInstance() const override SIP_FACTORY;
 
   protected:
