d1234aae698620a7c9722d61811d7f133ccb7a9c
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-18 14:05:41 +1000

    Remove almost all of ModelerDialog
    Just leave the python object lifetime hack in place for now --
    as the callers get moved to c++ we will be able to eventually remove
    that too!

--- python/plugins/processing/modeler/ModelerDialog.py
+++ python/plugins/processing/modeler/ModelerDialog.py
@@ -22,25 +22,17 @@ __copyright__ = "(C) 2012, Victor Olaya"
 from qgis.gui import QgsModelDesignerDialog
 
 
-class ModelerDialog(QgsModelDesignerDialog):
+class ModelerDialog:
     dlgs = []
 
     @staticmethod
-    def create(model=None):
+    def create():
         """
         Workaround crappy sip handling of QMainWindow. It doesn't know that we are using the deleteonclose
         flag, so happily just deletes dialogs as soon as they go out of scope. The only workaround possible
         while we still have to drag around this Python code is to store a reference to the sip wrapper so that
         sip doesn't get confused. The underlying object will still be deleted by the deleteonclose flag though!
         """
-        dlg = ModelerDialog(model)
+        dlg = QgsModelDesignerDialog()
         ModelerDialog.dlgs.append(dlg)
         return dlg
-
-    def __init__(self, model=None, parent=None):
-        super().__init__(parent)
-
-        if model is not None:
-            _model = model.create()
-            _model.setSourceFilePath(model.sourceFilePath())
-            self.setModel(_model)
