38e5501b12770a18019662607d3878b7971e8cc1
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-18 13:45:54 +1000

    Use c++ api to get processing context for model designer

--- python/plugins/processing/modeler/ModelerDialog.py
+++ python/plugins/processing/modeler/ModelerDialog.py
@@ -19,12 +19,7 @@ __author__ = "Victor Olaya"
 __date__ = "August 2012"
 __copyright__ = "(C) 2012, Victor Olaya"
 
-from qgis.gui import (
-    QgsModelDesignerDialog,
-    QgsProcessingContextGenerator,
-)
-
-from processing.tools.dataobjects import createContext
+from qgis.gui import QgsModelDesignerDialog
 
 
 class ModelerDialog(QgsModelDesignerDialog):
@@ -44,19 +39,6 @@ class ModelerDialog(QgsModelDesignerDialog):
 
     def __init__(self, model=None, parent=None):
         super().__init__(parent)
-
-        self.processing_context = createContext()
-
-        class ContextGenerator(QgsProcessingContextGenerator):
-            def __init__(self, context):
-                super().__init__()
-                self.processing_context = context
-
-            def processingContext(self):
-                return self.processing_context
-
-        self.context_generator = ContextGenerator(self.processing_context)
-        self.registerProcessingContextGenerator(self.context_generator)
 
         if model is not None:
             _model = model.create()
