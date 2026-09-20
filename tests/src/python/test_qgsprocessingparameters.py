30e44d65dece28614b32258f0b6787f4a5f47229
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-21 09:37:15 +1000

    Add test from #67401

--- tests/src/python/test_qgsprocessingparameters.py
+++ tests/src/python/test_qgsprocessingparameters.py
@@ -14,14 +14,60 @@ import unittest
 
 from processing.core.Processing import Processing
 from qgis.core import (
+    Qgis,
     QgsApplication,
     QgsProcessingParameterGeometry,
+    QgsProcessingParameterString,
     QgsSettings,
     QgsWkbTypes,
 )
+from qgis.gui import (
+    QgsAbstractProcessingParameterWidgetWrapper,
+    QgsGui,
+    QgsProcessingGuiRegistry,
+    QgsProcessingParameterWidgetFactoryInterface,
+)
+from qgis.PyQt.QtWidgets import QLineEdit
 from qgis.testing import QgisTestCase, start_app
 
 start_app()
+
+
+class CustomStringParameterWidgetWrapper(QgsAbstractProcessingParameterWidgetWrapper):
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self._widget = None
+        self.setObjectName("CustomWrapper")
+
+    def createWidget(self):
+        self._widget = QLineEdit()
+        return self._widget
+
+    def setWidgetValue(self, value, context):
+        if self._widget is not None and value is not None:
+            self._widget.setText(str(value))
+
+    def widgetValue(self, context):
+        if self._widget is not None:
+            return self._widget.text()
+        return None
+
+
+class CustomStringParameterWidgetFactory(QgsProcessingParameterWidgetFactoryInterface):
+    NAME = "MyParameterWidget"
+
+    CREATED_WIDGET_WRAPPERS = 0
+
+    def createWidgetWrapper(self, *args, **kwds):
+        w = CustomStringParameterWidgetWrapper(*args, **kwds)
+        CustomStringParameterWidgetFactory.CREATED_WIDGET_WRAPPERS += 1
+        return w
+
+    def clone(self):
+        return CustomStringParameterWidgetFactory()
+
+    def parameterType(self):
+        return self.NAME
 
 
 class TestQgsProcessingParameters(QgisTestCase):
@@ -31,6 +77,13 @@ class TestQgsProcessingParameters(QgisTestCase):
         super().setUpClass()
         Processing.initialize()
         cls.registry = QgsApplication.instance().processingRegistry()
+
+        cls._factory = CustomStringParameterWidgetFactory()
+        QgsGui.processingGuiRegistry().addParameterWidgetFactory(cls._factory)
+
+    @classmethod
+    def tearDownClass(cls):
+        QgsGui.processingGuiRegistry().removeParameterWidgetFactory(cls._factory)
 
     def test_qgsprocessinggometry(self):  # spellok
         """Test QgsProcessingParameterGeometry initialization"""
@@ -43,6 +96,40 @@ class TestQgsProcessingParameters(QgisTestCase):
         types = param.geometryTypes()
 
         self.assertEqual(param.geometryTypes(), geomtypes)
+
+    def test_widget_wrapper_creation(self):
+        """Test creating widget wrapper for custom parameter."""
+        # Create parameter
+
+        param = QgsProcessingParameterString("TEST", "custom string", optional=False)
+
+        metadata = param.metadata()
+        metadata["widget_wrapper"] = {
+            "widget_type": (CustomStringParameterWidgetFactory.NAME)
+        }
+        param.setMetadata(metadata)
+        wrapper_type = Qgis.ProcessingMode.Standard
+
+        # use CustomStringParameterWidgetFactory directly
+        self.assertEqual(CustomStringParameterWidgetFactory.CREATED_WIDGET_WRAPPERS, 0)
+        wrapper1 = self._factory.createWidgetWrapper(param, wrapper_type)
+        self.assertEqual(CustomStringParameterWidgetFactory.CREATED_WIDGET_WRAPPERS, 1)
+        self.assertEqual(wrapper1.objectName(), "CustomWrapper")
+        self.assertIsInstance(wrapper1, QgsAbstractProcessingParameterWidgetWrapper)
+        self.assertIsInstance(wrapper1, CustomStringParameterWidgetWrapper)
+
+        # use the CustomStringParameterWidgetFactory, where we have registered the CustomStringParameterWidgetFactory
+        # to
+        reg: QgsProcessingGuiRegistry = QgsGui.processingGuiRegistry()
+
+        wrapper2 = reg.createParameterWidgetWrapper(param, wrapper_type)
+        self.assertEqual(CustomStringParameterWidgetFactory.CREATED_WIDGET_WRAPPERS, 2)
+        self.assertEqual(wrapper2.objectName(), "CustomWrapper")
+        self.assertIsInstance(wrapper2, QgsAbstractProcessingParameterWidgetWrapper)
+
+        # this fails, because QgsProcessingGuiRegistry returns the base class
+        # QgsAbstractProcessingParameterWidgetWrapper only
+        self.assertIsInstance(wrapper2, CustomStringParameterWidgetWrapper)
 
 
 if __name__ == "__main__":
